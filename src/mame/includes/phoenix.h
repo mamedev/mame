89f75d74a92be020658ceccad05915f2057e721e
Couriersud <couriersud@users.noreply.github.com>
2008-01-17 23:34:58 +0000

    phoenix.c: * Modified I8085 clock * Added dip locations * consolidated inputs * tagged inputs * combined memory maps
    pleiads: 
    * video timing from schematics
    * palette from schematics (resnet.h)
    * no more colortable
    
    survival:
    * palette from schematics (resnet.h)

--- src/mame/includes/phoenix.h
+++ src/mame/includes/phoenix.h
@@ -7,7 +7,7 @@
 #define MASTER_CLOCK		 	XTAL_11MHz
 
 #define PIXEL_CLOCK				(MASTER_CLOCK/2)
-#define CPU_CLOCK				(PIXEL_CLOCK/2)
+#define CPU_CLOCK				(PIXEL_CLOCK)
 #define HTOTAL					(512-160)
 #define HBSTART					(256)
 #define HBEND					(0)
@@ -53,9 +53,9 @@ VIDEO_UPDATE( naughtyb );
 /*----------- defined in video/phoenix.c -----------*/
 
 PALETTE_INIT( phoenix );
+PALETTE_INIT( survival );
 PALETTE_INIT( pleiads );
 VIDEO_START( phoenix );
-VIDEO_START( pleiads );
 VIDEO_UPDATE( phoenix );
 
 WRITE8_HANDLER( phoenix_videoram_w );
--- src/mame/video/phoenix.c
+++ src/mame/video/phoenix.c
@@ -62,65 +62,75 @@ static const res_net_info phoenix_net_info =
 	}
 };
 
+static const res_net_info pleiades_net_info =
+{
+	RES_NET_VCC_5V | RES_NET_VBIAS_5V | RES_NET_VIN_OPEN_COL,
+	{
+		{ RES_NET_AMP_NONE, 150, 270, 2, { 270, 1 } },
+		{ RES_NET_AMP_NONE, 150, 270, 2, { 270, 1 } },
+		{ RES_NET_AMP_NONE, 150, 270, 2, { 270, 1 } }  
+	}
+};
+
+static const res_net_info survival_net_info =
+{
+	RES_NET_VCC_5V | RES_NET_VBIAS_5V | RES_NET_VIN_OPEN_COL,
+	{
+		{ RES_NET_AMP_NONE, 270, 270, 2, { 180, 1 } },
+		{ RES_NET_AMP_NONE, 270, 270, 2, { 180, 1 } },
+		{ RES_NET_AMP_NONE, 270, 270, 2, { 180, 1 } }  
+	}
+};
+
 PALETTE_INIT( phoenix )
 {
 	int i;
-	int t[4]= {0,1,2,3};
 	rgb_t	*rgb;
-	#define COLOR(gfxn,offs) (colortable[machine->drv->gfxdecodeinfo[gfxn].color_codes_start + offs])
 
 	rgb = compute_res_net_all(color_prom, &phoenix_decode_info, &phoenix_net_info);
 	/* native order */
 	for (i=0;i<256;i++)
 	{
 		int col;
-		col = ((t[i & 0x03] << 3 ) & 0x18) | ((i>>2) & 0x07) | (i & 0x60);
+		col = ((i << 3 ) & 0x18) | ((i>>2) & 0x07) | (i & 0x60);
 		palette_set_color(machine,i,rgb[col]);
 	}
 	palette_normalize_range(machine->palette, 0, 255, 0, 255);
 	free(rgb);
+}
 
-	/* first bank of characters use colors 0x00-0x1f and 0x40-0x5f */
-	/* second bank of characters use colors 0x20-0x3f and 0x60-0x7f */
+PALETTE_INIT( survival )
+{
+	int i;
+	rgb_t	*rgb;
+
+	rgb = compute_res_net_all(color_prom, &phoenix_decode_info, &survival_net_info);
+	/* native order */
+	for (i=0;i<256;i++)
+	{
+		int col;
+		col = ((i << 3 ) & 0x18) | ((i>>2) & 0x07) | (i & 0x60);
+		palette_set_color(machine,i,rgb[col]);
+	}
+	palette_normalize_range(machine->palette, 0, 255, 0, 255);
+	free(rgb);
 }
 
 PALETTE_INIT( pleiads )
 {
 	int i;
-	#define COLOR(gfxn,offs) (colortable[machine->drv->gfxdecodeinfo[gfxn].color_codes_start + offs])
+	rgb_t	*rgb;
 
-
-	for (i = 0;i < machine->drv->total_colors;i++)
-	{
-		int bit0,bit1,r,g,b;
-
-
-		bit0 = (color_prom[0] >> 0) & 0x01;
-		bit1 = (color_prom[machine->drv->total_colors] >> 0) & 0x01;
-		r = 0x55 * bit0 + 0xaa * bit1;
-		bit0 = (color_prom[0] >> 2) & 0x01;
-		bit1 = (color_prom[machine->drv->total_colors] >> 2) & 0x01;
-		g = 0x55 * bit0 + 0xaa * bit1;
-		bit0 = (color_prom[0] >> 1) & 0x01;
-		bit1 = (color_prom[machine->drv->total_colors] >> 1) & 0x01;
-		b = 0x55 * bit0 + 0xaa * bit1;
-
-		palette_set_color(machine,i,MAKE_RGB(r,g,b));
-		color_prom++;
-	}
-
-	/* first bank of characters use colors 0x00-0x1f, 0x40-0x5f, 0x80-0x9f and 0xc0-0xdf */
-	/* second bank of characters use colors 0x20-0x3f, 0x60-0x7f, 0xa0-0xbf and 0xe0-0xff */
-	for (i = 0;i < 0x80;i++)
+	rgb = compute_res_net_all(color_prom, &phoenix_decode_info, &pleiades_net_info);
+	/* native order */
+	for (i=0;i<256;i++)
 	{
 		int col;
-
-
-		col = ((i & 0x1c) >> 2) | ((i & 0x03) << 3) | ((i & 0x60) << 1);
-
-		COLOR(0,i) = col;
-		COLOR(1,i) = col | 0x20;
-	}
+		col = ((i << 3 ) & 0x18) | ((i>>2) & 0x07) | (i & 0xE0);
+		palette_set_color(machine,i,rgb[col]);
+	}	
+	palette_normalize_range(machine->palette, 0, 255, 0, 255);
+	free(rgb);
 }
 
 /***************************************************************************
@@ -157,30 +167,6 @@ static TILE_GET_INFO( get_bg_tile_info )
 			0);
 }
 
-static TILE_GET_INFO( pleiads_get_fg_tile_info )
-{
-	int code;
-
-	code = videoram_pg[videoram_pg_index][tile_index];
-	SET_TILE_INFO(
-			1,
-			code,
-			(code >> 5) | (palette_bank << 3),
-			(tile_index & 0x1f) ? 0 : TILE_FORCE_LAYER0);	/* first row (column) is opaque */
-}
-
-static TILE_GET_INFO( pleiads_get_bg_tile_info )
-{
-	int code;
-
-	code = videoram_pg[videoram_pg_index][tile_index + 0x800];
-	SET_TILE_INFO(
-			0,
-			code,
-			(code >> 5) | (palette_bank << 3),
-			0);
-}
-
 /***************************************************************************
 
   Start the video hardware emulation.
@@ -213,28 +199,6 @@ VIDEO_START( phoenix )
 	state_save_register_global(videoram_pg_index);
 	state_save_register_global(palette_bank);
 	state_save_register_global(cocktail_mode);
-}
-
-VIDEO_START( pleiads )
-{
-	videoram_pg[0] = auto_malloc(0x1000);
-	videoram_pg[1] = auto_malloc(0x1000);
-
-	memory_configure_bank(1, 0, 1, videoram_pg[0], 0);
-	memory_configure_bank(1, 1, 1, videoram_pg[1], 0);
-	memory_set_bank(1, 0);
-
-	videoram_pg_index = 0;
-
-	fg_tilemap = tilemap_create(pleiads_get_fg_tile_info,tilemap_scan_rows,TILEMAP_TYPE_PEN,8,8,32,32);
-	bg_tilemap = tilemap_create(pleiads_get_bg_tile_info,tilemap_scan_rows,TILEMAP_TYPE_PEN,     8,8,32,32);
-
-	tilemap_set_transparent_pen(fg_tilemap,0);
-
-	tilemap_set_scrolldx(fg_tilemap, 0, (HTOTAL - HBSTART));
-	tilemap_set_scrolldx(bg_tilemap, 0, (HTOTAL - HBSTART));
-	tilemap_set_scrolldy(fg_tilemap, 0, (VTOTAL - VBSTART));
-	tilemap_set_scrolldy(bg_tilemap, 0, (VTOTAL - VBSTART));
 }
 
 /***************************************************************************
@@ -287,7 +251,7 @@ WRITE8_HANDLER( phoenix_videoreg_w )
 
 WRITE8_HANDLER( pleiads_videoreg_w )
 {
-    if (videoram_pg_index != (data & 1))
+	if (videoram_pg_index != (data & 1))
 	{
 		/* set memory bank */
 		videoram_pg_index = data & 1;
@@ -306,7 +270,7 @@ WRITE8_HANDLER( pleiads_videoreg_w )
 
     if (palette_bank != ((data >> 1) & 3))
 	{
-		palette_bank = ((data >> 1) & 3);
+   		palette_bank = ((data >> 1) & 3);
 
 		tilemap_mark_all_tiles_dirty(ALL_TILEMAPS);
 
@@ -329,14 +293,14 @@ WRITE8_HANDLER( phoenix_scroll_w )
 READ8_HANDLER( phoenix_input_port_0_r )
 {
 	if (cocktail_mode)
-		return (input_port_0_r(0) & 0x07) | (input_port_1_r(0) & 0xf8);
+		return (readinputportbytag("IN0") & 0x07) | (readinputportbytag("IN1") & 0xf8);
 	else
-		return input_port_0_r(0);
+		return readinputportbytag("IN0");
 }
 
 READ8_HANDLER( pleiads_input_port_0_r )
 {
-	int ret = phoenix_input_port_0_r(0) & 0xf7;
+	int ret = readinputportbytag("IN0") & 0xf7;
 
 	/* handle Pleiads protection */
 	switch (pleiads_protection_question)
@@ -359,7 +323,7 @@ READ8_HANDLER( pleiads_input_port_0_r )
 
 READ8_HANDLER( survival_input_port_0_r )
 {
-	int ret = phoenix_input_port_0_r(0);
+	int ret = readinputportbytag("IN0");
 
 	if (survival_protection_value)
 	{
