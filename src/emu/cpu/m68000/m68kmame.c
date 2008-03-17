e4e17a4fcde8ec84a79ea643592ed0741b9f18ac
Aaron Giles <aaron@aarongiles.com>
2008-03-17 16:51:19 +0000

    Fix 00150: alpinr2b, alpinesa : Assertion with -debug

--- src/emu/cpu/m68000/m68kmame.c
+++ src/emu/cpu/m68000/m68kmame.c
@@ -928,7 +928,7 @@ void m68020_get_info(UINT32 state, cpuinfo *info)
 		case CPUINFO_INT_CLOCK_MULTIPLIER:				info->i = 1;							break;
 		case CPUINFO_INT_CLOCK_DIVIDER:					info->i = 1;							break;
 		case CPUINFO_INT_MIN_INSTRUCTION_BYTES:			info->i = 2;							break;
-		case CPUINFO_INT_MAX_INSTRUCTION_BYTES:			info->i = 16;							break;
+		case CPUINFO_INT_MAX_INSTRUCTION_BYTES:			info->i = 20;							break;
 		case CPUINFO_INT_MIN_CYCLES:					info->i = 4;							break;
 		case CPUINFO_INT_MAX_CYCLES:					info->i = 158;							break;
 
@@ -1170,7 +1170,7 @@ void m68040_get_info(UINT32 state, cpuinfo *info)
 		case CPUINFO_INT_CLOCK_MULTIPLIER:				info->i = 1;							break;
 		case CPUINFO_INT_CLOCK_DIVIDER:					info->i = 1;							break;
 		case CPUINFO_INT_MIN_INSTRUCTION_BYTES:			info->i = 2;							break;
-		case CPUINFO_INT_MAX_INSTRUCTION_BYTES:			info->i = 16;							break;
+		case CPUINFO_INT_MAX_INSTRUCTION_BYTES:			info->i = 20;							break;
 		case CPUINFO_INT_MIN_CYCLES:					info->i = 4;							break;
 		case CPUINFO_INT_MAX_CYCLES:					info->i = 158;							break;
 
