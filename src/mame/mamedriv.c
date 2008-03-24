22664bb7b5cdd29f19b235cb6471c88c6ac11ae6
davidhay <davidhay@localhost>
2008-03-24 18:41:43 +0000

    reverted previous gnga graphics change, added the bootleg set which exhibits this behaviour instead.

--- src/mame/mamedriv.c
+++ src/mame/mamedriv.c
@@ -2283,6 +2283,7 @@ V-V                           TP-027
 	DRIVER( sinvasnb )	/* bootleg */
 	DRIVER( gng )		/*  9/1985 (c) 1985 */
 	DRIVER( gnga )		/*  9/1985 (c) 1985 */
+	DRIVER( gngbl )		/* bootleg */
 	DRIVER( gngt )		/*  9/1985 (c) 1985 */
 	DRIVER( makaimur )	/*  9/1985 (c) 1985 */
 	DRIVER( makaimuc )	/*  9/1985 (c) 1985 */
