e62e3229d34a4fbef8574ce4c3064e621ab79f28
Aaron Giles <aaron@aarongiles.com>
2008-06-12 16:58:03 +0000

    Missed file from previous checkin.

---
+++ src/mame/machine/seicop.h
@@ -0,0 +1,20 @@
+extern UINT16* cop_mcu_ram;
+
+READ16_HANDLER( copdxbl_0_r );
+WRITE16_HANDLER( copdxbl_0_w );
+
+READ16_HANDLER( heatbrl_mcu_r );
+WRITE16_HANDLER( heatbrl_mcu_w );
+READ16_HANDLER( cupsoc_mcu_r );
+WRITE16_HANDLER( cupsoc_mcu_w );
+READ16_HANDLER( godzilla_mcu_r );
+WRITE16_HANDLER( godzilla_mcu_w );
+READ16_HANDLER( denjinmk_mcu_r );
+WRITE16_HANDLER( denjinmk_mcu_w );
+READ16_HANDLER( sdgndmrb_mcu_r );
+WRITE16_HANDLER( sdgndmrb_mcu_w );
+READ16_HANDLER( legionna_mcu_r );
+WRITE16_HANDLER( legionna_mcu_w );
+
+READ16_HANDLER( raiden2_mcu_r );
+WRITE16_HANDLER( raiden2_mcu_w );
