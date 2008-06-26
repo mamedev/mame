49b7e8382f026276be59dc388af5e5b6e3d96e74
Aaron Giles <aaron@aarongiles.com>
2008-06-26 14:54:53 +0000

    From: Fabio Priuli [mailto:doge.fabio@gmail.com] Subject: fix to MT bug 1911 Hi,
    the attached patch fixes the DSW read in nomnlnd using input_port_read_safe (rather than adding a fake DSW port). Hopefully, this will be the last missing tag due to my patches: since last week all my changes are tested with mame_regtest before submit and, therefore, no missing inputs can be there anymore!
    
    Regards
          Fabio

--- src/mame/drivers/cosmic.c
+++ src/mame/drivers/cosmic.c
@@ -257,7 +257,7 @@ static READ8_HANDLER( cosmicg_port_0_r )
 
 static READ8_HANDLER( magspot_coinage_dip_r )
 {
-	return (input_port_read(machine, "DSW") & (1 << (7 - offset))) ? 0 : 1;
+	return (input_port_read_safe(machine, "DSW", 0) & (1 << (7 - offset))) ? 0 : 1;
 }
 
 
