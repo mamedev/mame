5712e5fa58d83ef6808af2193177d04d1c827be6
Andrew Gardner <andrew-gardner@users.noreply.github.com>
2008-07-14 15:26:25 +0000

    01986 : victroad, dogosoke: Last unknown dipswitch info 01959 : hypreact: DIP switches info.

--- src/mame/drivers/snk.c
+++ src/mame/drivers/snk.c
@@ -3179,9 +3179,9 @@ static INPUT_PORTS_START( victroad )
 	PORT_DIPNAME( 0x40 ,0x00, DEF_STR( Allow_Continue ) )
 	PORT_DIPSETTING(    0x40, DEF_STR( No ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( Yes ) )
-	PORT_DIPNAME( 0x80, 0x80, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x80, DEF_STR( On ) )
+	PORT_DIPNAME( 0x80, 0x80, "Credits Buy Lives During Play" )
+	PORT_DIPSETTING(    0x00, DEF_STR( No ) )
+	PORT_DIPSETTING(    0x80, DEF_STR( Yes ) )
 INPUT_PORTS_END
 
 
