f162f9d2f47cb3f0c8ad87575f2d2323b84a8c70
Roberto Zandona <robertoz@mamedev.org>
2008-08-31 09:23:58 +0000

    fixed game name in mamedriv

--- src/mame/mamedriv.c
+++ src/mame/mamedriv.c
@@ -6310,9 +6310,9 @@ BOMULEUL CHAJARA SEGA ST-V  1997/04/11
 	DRIVER( raiden2e )	/* (c) 1993 Seibu Kaihatsu */
 	DRIVER( raiden2f )	/* (c) 1993 Seibu Kaihatsu */
 	DRIVER( raidndx )	/* (c) 1993 Seibu Kaihatsu */
-	DRIVER( raidndxa )	/* (c) 1993 Seibu Kaihatsu */
 	DRIVER( raidndxj )	/* (c) 1993 Seibu Kaihatsu */
 	DRIVER( raidndxm )	/* (c) 1993 Seibu Kaihatsu + Metrotainment license */
+	DRIVER( raidndxt )	/* (c) 1993 Seibu Kaihatsu + Tuning license*/
 	DRIVER( zeroteam )	/* (c) 1993 Seibu Kaihatsu */
 	DRIVER( zeroteaa )	/* (c) 1993 Seibu Kaihatsu */
 	DRIVER( zeroteab )	/* (c) 1993 Seibu Kaihatsu */
