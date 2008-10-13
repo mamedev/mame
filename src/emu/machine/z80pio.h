3d85b66c5e0e6683feba4e623f19bd6ae3fa9b6f
Curt Coder <curtcoder@mail.com>
2008-10-13 14:16:07 +0000

    Added pinout, not worth mentioning in whatsnew.

--- src/emu/machine/z80pio.h
+++ src/emu/machine/z80pio.h
@@ -1,9 +1,32 @@
 /***************************************************************************
 
-    Z80 PIO implementation
+    Z80 PIO (Z8420) implementation
 
     Copyright Nicola Salmoria and the MAME Team.
     Visit http://mamedev.org for licensing and usage restrictions.
+
+***************************************************************************
+                            _____   _____
+                    D2   1 |*    \_/     | 40  D3
+                    D7   2 |             | 39  D4
+                    D6   3 |             | 38  D5
+                   _CE   4 |             | 37  _M1
+                  C/_D   5 |             | 36  _IORQ
+                  B/_A   6 |             | 35  RD
+                   PA7   7 |             | 34  PB7
+                   PA6   8 |             | 33  PB6
+                   PA5   9 |             | 32  PB5
+                   PA4  10 |   Z80-PIO   | 31  PB4
+                   GND  11 |             | 30  PB3
+                   PA3  12 |             | 29  PB2
+                   PA2  13 |             | 28  PB1
+                   PA1  14 |             | 27  PB0
+                   PA0  15 |             | 26  +5V
+                 _ASTB  16 |             | 25  CLK
+                 _BSTB  17 |             | 24  IEI
+                  ARDY  18 |             | 23  _INT
+                    D0  19 |             | 22  IEO
+                    D1  20 |_____________| 21  BRDY
 
 ***************************************************************************/
 
