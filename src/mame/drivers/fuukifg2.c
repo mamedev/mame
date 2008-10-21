8b368475fad5349888ad410e5b570d319ef2b423
Phil Bennett <philipjbennett@users.noreply.github.com>
2008-10-21 20:37:18 +0000

    Fixed recently regressed raster interrupt issues

--- src/mame/drivers/fuukifg2.c
+++ src/mame/drivers/fuukifg2.c
@@ -30,6 +30,8 @@ To Do:
 
 - The scroll values are generally wrong when flip screen is on and rasters are often incorrect
 
+- gogomile M6295 banking is incorrect (the story sequence should have speech)
+
 ***************************************************************************/
 
 #include "driver.h"
@@ -425,31 +427,23 @@ static const ym3812_interface fuuki16_ym3812_intf =
             also used for water effects and titlescreen linescroll on gogomile
 */
 
-static TIMER_CALLBACK( irq_off )
-{
-	cpunum_set_input_line(machine, 0, param, CLEAR_LINE);
-}
-
 static TIMER_CALLBACK( level_1_interrupt_callback )
 {
-	cpunum_set_input_line(machine, 0, 1, ASSERT_LINE);
-	timer_set(ATTOTIME_IN_USEC(1), NULL, 1, irq_off);
+	cpunum_set_input_line(machine, 0, 1, HOLD_LINE);
 	timer_set(video_screen_get_time_until_pos(machine->primary_screen, 248, 0), NULL, 0, level_1_interrupt_callback);
 }
 
 
 static TIMER_CALLBACK( vblank_interrupt_callback )
 {
-	cpunum_set_input_line(machine, 0, 3, ASSERT_LINE);	// VBlank IRQ
-	timer_set(ATTOTIME_IN_USEC(1), NULL, 3, irq_off);
+	cpunum_set_input_line(machine, 0, 3, HOLD_LINE);	// VBlank IRQ
 	timer_set(video_screen_get_time_until_vblank_start(machine->primary_screen), NULL, 0, vblank_interrupt_callback);
 }
 
 
 static TIMER_CALLBACK( raster_interrupt_callback )
 {
-	cpunum_set_input_line(machine, 0, 5, ASSERT_LINE);	// Raster Line IRQ
-	timer_set(ATTOTIME_IN_USEC(1), NULL, 5, irq_off);
+	cpunum_set_input_line(machine, 0, 5, HOLD_LINE);	// Raster Line IRQ
 	video_screen_update_partial(machine->primary_screen, video_screen_get_vpos(machine->primary_screen));
 	timer_adjust_oneshot(raster_interrupt_timer, video_screen_get_frame_period(machine->primary_screen), 0);
 }
@@ -689,7 +683,7 @@ ROM_END
 
 ***************************************************************************/
 
-GAME( 1995, gogomile, 0,        fuuki16, gogomile, 0, ROT0, "Fuuki", "Go Go! Mile Smile", 0 )
-GAME( 1995, gogomilj, gogomile, fuuki16, gogomilj, 0, ROT0, "Fuuki", "Susume! Mile Smile (Japan)", 0 )
+GAME( 1995, gogomile, 0,        fuuki16, gogomile, 0, ROT0, "Fuuki", "Go Go! Mile Smile", GAME_IMPERFECT_SOUND )
+GAME( 1995, gogomilj, gogomile, fuuki16, gogomilj, 0, ROT0, "Fuuki", "Susume! Mile Smile (Japan)", GAME_IMPERFECT_SOUND )
 GAME( 1996, pbancho,  0,        fuuki16, pbancho,  0, ROT0, "Fuuki", "Gyakuten!! Puzzle Bancho (Japan)", 0)
 
