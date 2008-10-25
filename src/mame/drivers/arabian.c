0658fcabefbeaa44e1fe6ae5710512e7eea43db9
stephh <stephh@users.noreply.github.com>
2008-10-25 08:11:24 +0000

    Fixed Dip Switches for 'arabian' and clones.

--- src/mame/drivers/arabian.c
+++ src/mame/drivers/arabian.c
@@ -5,7 +5,7 @@
     driver by Dan Boris
 
     Games supported:
-        * Arabian
+        * Arabian [2 sets]
 
     Known bugs:
         * none at this time
@@ -245,23 +245,23 @@ static INPUT_PORTS_START( arabian )
 	PORT_START("IN0")
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_COIN1 )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_COIN2 )
-	PORT_SERVICE( 0x04, IP_ACTIVE_HIGH )
+	PORT_SERVICE( 0x04, IP_ACTIVE_HIGH )                    /* also adds 1 credit */
 	PORT_BIT( 0xf8, IP_ACTIVE_HIGH, IPT_UNKNOWN )
 
 	PORT_START("DSW1")
-	PORT_DIPNAME( 0x01, 0x00, DEF_STR( Lives ))				PORT_DIPLOCATION("SW1:!1")
+	PORT_DIPNAME( 0x01, 0x00, DEF_STR( Lives ) )            PORT_DIPLOCATION("SW1:!1")
 	PORT_DIPSETTING(    0x00, "3" )
 	PORT_DIPSETTING(    0x01, "5" )
-	PORT_DIPNAME( 0x02, 0x02, DEF_STR( Cabinet ))			PORT_DIPLOCATION("SW1:!2")
-	PORT_DIPSETTING(    0x02, DEF_STR( Upright ))
-	PORT_DIPSETTING(    0x00, DEF_STR( Cocktail ))
-	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Flip_Screen ))		PORT_DIPLOCATION("SW1:!3")
-	PORT_DIPSETTING(    0x04, DEF_STR( Off ))
-	PORT_DIPSETTING(    0x00, DEF_STR( On ))
-	PORT_DIPNAME( 0x08, 0x00, DEF_STR( Difficulty ))		PORT_DIPLOCATION("SW1:!4") /* Carry Bowls to Next Life */
-	PORT_DIPSETTING(    0x00, DEF_STR( Easy ))
-	PORT_DIPSETTING(    0x08, DEF_STR( Hard ))
-	PORT_DIPNAME( 0xf0, 0x00, DEF_STR( Coinage ))			PORT_DIPLOCATION("SW1:!5,!6,!7,!8")
+	PORT_DIPNAME( 0x02, 0x02, DEF_STR( Cabinet ) )          PORT_DIPLOCATION("SW1:!2")
+	PORT_DIPSETTING(    0x02, DEF_STR( Upright ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( Cocktail ) )
+	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Flip_Screen ) )      PORT_DIPLOCATION("SW1:!3")
+	PORT_DIPSETTING(    0x04, DEF_STR( Off ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+	PORT_DIPNAME( 0x08, 0x00, DEF_STR( Difficulty ) )       PORT_DIPLOCATION("SW1:!4")    /* Carry Bowls to Next Life */
+	PORT_DIPSETTING(    0x00, DEF_STR( Easy ) )             /* not reset "ARABIAN" letters */
+	PORT_DIPSETTING(    0x08, DEF_STR( Hard ) )             /* reset "ARABIAN" letters */
+	PORT_DIPNAME( 0xf0, 0x00, DEF_STR( Coinage ) )          PORT_DIPLOCATION("SW1:!5,!6,!7,!8")
 	PORT_DIPSETTING(    0x10, "A 2/1 B 2/1" )
 	PORT_DIPSETTING(    0x20, "A 2/1 B 1/3" )
 	PORT_DIPSETTING(    0x00, "A 1/1 B 1/1" )
@@ -272,24 +272,24 @@ static INPUT_PORTS_START( arabian )
 	PORT_DIPSETTING(    0x70, "A 1/1 B 1/6" )
 	PORT_DIPSETTING(    0x80, "A 1/2 B 1/2" )
 	PORT_DIPSETTING(    0x90, "A 1/2 B 1/4" )
-	PORT_DIPSETTING(    0xa0, "A 1/2 B 1/6" )
+	PORT_DIPSETTING(    0xa0, "A 1/2 B 1/5" )
+	PORT_DIPSETTING(    0xe0, "A 1/2 B 1/6" )
 	PORT_DIPSETTING(    0xb0, "A 1/2 B 1/10" )
 	PORT_DIPSETTING(    0xc0, "A 1/2 B 1/11" )
 	PORT_DIPSETTING(    0xd0, "A 1/2 B 1/12" )
-	PORT_DIPSETTING(    0xf0, DEF_STR( Free_Play ))
-	/* 0xe0 gives A 1/2 B 1/6 */
+	PORT_DIPSETTING(    0xf0, DEF_STR( Free_Play ) )
 
 	PORT_START("COM0")
 	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_UNUSED )
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_START1 )
 	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_START2 )
-	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_COIN3 )		/* IN3 */
+	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_SERVICE1 )	/* IN3 : "AUX-S" */
 
 	PORT_START("COM1")
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_8WAY
-	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_8WAY
-	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP ) PORT_8WAY
-	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN ) PORT_8WAY
+	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT )  PORT_8WAY
+	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP )    PORT_8WAY
+	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN )  PORT_8WAY
 
 	PORT_START("COM2")
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_BUTTON1 )
@@ -299,9 +299,9 @@ static INPUT_PORTS_START( arabian )
 
 	PORT_START("COM3")
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_8WAY PORT_COCKTAIL
-	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_8WAY PORT_COCKTAIL
-	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP ) PORT_8WAY PORT_COCKTAIL
-	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN ) PORT_8WAY PORT_COCKTAIL
+	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT )  PORT_8WAY PORT_COCKTAIL
+	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP )    PORT_8WAY PORT_COCKTAIL
+	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN )  PORT_8WAY PORT_COCKTAIL
 
 	PORT_START("COM4")
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_COCKTAIL
@@ -310,16 +310,27 @@ static INPUT_PORTS_START( arabian )
 	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_UNKNOWN )	/* IN19 */
 
 	PORT_START("COM5")
-	PORT_DIPNAME( 0x01, 0x01, "Coin Counters" )				PORT_DIPLOCATION("SW2:!1")
+	PORT_DIPNAME( 0x01, 0x01, "Coin Counters" )             PORT_DIPLOCATION("SW2:!1")
 	PORT_DIPSETTING(    0x01, "1" )
 	PORT_DIPSETTING(    0x00, "2" )
-	PORT_DIPNAME( 0x02, 0x00, DEF_STR( Demo_Sounds ))		PORT_DIPLOCATION("SW2:!2")
-	PORT_DIPSETTING(    0x02, DEF_STR( Off ))
-	PORT_DIPSETTING(    0x00, DEF_STR( On ))
-	PORT_DIPNAME( 0x0c, 0x0c, DEF_STR( Bonus_Life ))		PORT_DIPLOCATION("SW2:!3,!4")
-	PORT_DIPSETTING(    0x04, "20K" )
-	PORT_DIPSETTING(    0x08, "40K" )
-	PORT_DIPSETTING(    0x0c, "20K, 50K, 100K, Every 100K" )
+	PORT_DIPNAME( 0x02, 0x00, DEF_STR( Demo_Sounds ) )      PORT_DIPLOCATION("SW2:!2")
+	PORT_DIPSETTING(    0x02, DEF_STR( Off ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0c, 0x0c, DEF_STR( Bonus_Life ) )       PORT_DIPLOCATION("SW2:!3,!4")
+	PORT_DIPSETTING(    0x0c, "30k 70k 40k+" )              /* last bonus life at 870k : max. 22 bonus lives */
+	PORT_DIPSETTING(    0x04, "20k only" )
+	PORT_DIPSETTING(    0x08, "40k only" )
+	PORT_DIPSETTING(    0x00, DEF_STR( None ) )
+INPUT_PORTS_END
+
+static INPUT_PORTS_START( arabiana )
+	PORT_INCLUDE( arabian )
+
+	PORT_MODIFY("COM5")
+	PORT_DIPNAME( 0x0c, 0x0c, DEF_STR( Bonus_Life ) )       PORT_DIPLOCATION("SW2:!3,!4")
+	PORT_DIPSETTING(    0x0c, "20k 50k 150k 100k+" )        /* last bonus life at 850k : max. 10 bonus lives */
+	PORT_DIPSETTING(    0x04, "20k only" )
+	PORT_DIPSETTING(    0x08, "40k only" )
 	PORT_DIPSETTING(    0x00, DEF_STR( None ) )
 INPUT_PORTS_END
 
@@ -437,5 +448,5 @@ static DRIVER_INIT( arabian )
  *
  *************************************/
 
-GAME( 1983, arabian,  0,       arabian, arabian, arabian, ROT270, "Sun Electronics",                   "Arabian",         0 )
-GAME( 1983, arabiana, arabian, arabian, arabian, arabian, ROT270, "[Sun Electronics] (Atari license)", "Arabian (Atari)", 0 )
+GAME( 1983, arabian,  0,        arabian,  arabian,  arabian,  ROT270, "Sun Electronics",                   "Arabian",         0 )
+GAME( 1983, arabiana, arabian,  arabian,  arabiana, arabian,  ROT270, "[Sun Electronics] (Atari license)", "Arabian (Atari)", 0 )
