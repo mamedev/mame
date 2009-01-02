99103e89bb35f17b1fa520d62b723f976330e260
Angelo Salese <angelosa@users.noreply.github.com>
2009-01-02 20:08:54 +0000

    Fixed layer enable,flip x & x/y position in goodejan.c driver [Angelo Salese]
    =============
    
    Several Seibu games (i.e. D-Con,sdgndmps,Sengoku Mahjong,goodejan/totmejan and the raiden2.c/legionna.c games) all have a standard 0x48 (0x50?) 
    regs-wide C.R.T.C. 
    
    I wonder if it is a custom Seibu crap or a known video chip,would be good if somebody can do tests on it. For sure,it should be abstracted,this 
    commit was made by looking mostly at Sengoku Mahjong.

--- src/mame/video/goodejan.c
+++ src/mame/video/goodejan.c
@@ -2,6 +2,7 @@
 
 static tilemap *bg_tilemap, *tx_tilemap;
 UINT16 *goodejan_bgvram,*goodejan_txvram;
+static UINT16 goodejan_layer_en;
 
 WRITE16_HANDLER( goodejan_bgvram_w )
 {
@@ -15,13 +16,21 @@ WRITE16_HANDLER( goodejan_txvram_w )
 	tilemap_mark_tile_dirty(tx_tilemap,offset);
 }
 
+WRITE16_HANDLER( goodejan_layer_en_w )
+{
+	/*---- ---- ---x ---- enable sprites*/
+	/*---- ---- ---- xxxx enable layers*/
+	goodejan_layer_en = data;
+}
+
 static TILE_GET_INFO( goodejan_bg_tile_info )
 {
 	int tile = goodejan_bgvram[tile_index]&0x0fff;
 	int color = (goodejan_bgvram[tile_index]&0xf000)>>12;
 
 	// WRONG!
-	if ((goodejan_bgvram[tile_index]&0x8000)==0x0000) tile+=0x1000;
+	tile|= (goodejan_bgvram[tile_index]&0x8000)>>3;
+//	if ((goodejan_bgvram[tile_index]&0x8000)==0x0000) tile+=0x1000;
 
 	SET_TILE_INFO(1, tile, color, 0);
 }
@@ -48,11 +57,13 @@ static void draw_sprites(running_machine *machine, bitmap_t *bitmap,const rectan
 		y = spriteram16[offs+3];//&0x1ff;
 		x = spriteram16[offs+2];//&0x1ff;
 
-		if (spriteram16[offs+2]&0x8000) x=0x10000-x;
-		if (spriteram16[offs+3]&0x8000) y=0x10000-y;
+		if (x&0x8000) x=0-(0x200-(x&0x1ff));
+		//else x&=0x1ff;
+		if (y&0x8000) y=0-(0x200-(y&0x1ff));
+		//else y&=0x1ff;
 
 		color = spriteram16[offs+0]&0x3f;
-		fx = spriteram16[offs+0]&0x40;
+		fx = spriteram16[offs+0]&0x4000;
 		fy = 0; /* To do - probably 0x2000 */
 		dy = ((spriteram16[offs+0]&0x0380)>>7) + 1;
 		dx = ((spriteram16[offs+0]&0x1c00)>>10) + 1;
@@ -86,19 +97,21 @@ WRITE16_HANDLER( goodejan_bg_scrolly_w )
 
 VIDEO_START( goodejan )
 {
-	bg_tilemap = tilemap_create(machine, goodejan_bg_tile_info,tilemap_scan_rows,     16,16,32,32);
+	bg_tilemap = tilemap_create(machine, goodejan_bg_tile_info,tilemap_scan_rows, 16,16,32,32);
 	tx_tilemap = tilemap_create(machine, goodejan_tx_tile_info,tilemap_scan_rows, 8, 8,32,32);
 	tilemap_set_transparent_pen(tx_tilemap,15);
 }
 
 VIDEO_UPDATE( goodejan )
 {
-	tilemap_draw(bitmap,cliprect,bg_tilemap,0,0);
-	draw_sprites(screen->machine, bitmap,cliprect, 2);
-	draw_sprites(screen->machine, bitmap,cliprect, 1);
-	draw_sprites(screen->machine, bitmap,cliprect, 0);
-	tilemap_draw(bitmap,cliprect,tx_tilemap,0,0);
-	draw_sprites(screen->machine, bitmap,cliprect, 3);
+	bitmap_fill(bitmap, cliprect, screen->machine->pens[0]); //black pen,likely to be selectable by a crt register
+
+	if(!(goodejan_layer_en & 1)) 	{ tilemap_draw(bitmap,cliprect,bg_tilemap,0,0); }
+	if(!(goodejan_layer_en & 0x10)) { draw_sprites(screen->machine, bitmap,cliprect, 2); }
+	if(!(goodejan_layer_en & 0x10)) { draw_sprites(screen->machine, bitmap,cliprect, 1); }
+	if(!(goodejan_layer_en & 0x10)) { draw_sprites(screen->machine, bitmap,cliprect, 0); }
+	if(!(goodejan_layer_en & 8))    { tilemap_draw(bitmap,cliprect,tx_tilemap,0,0); }
+	if(!(goodejan_layer_en & 0x10)) { draw_sprites(screen->machine, bitmap,cliprect, 3); }
 
 	return 0;
 }
