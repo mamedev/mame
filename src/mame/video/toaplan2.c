47f50be2417381d75b7fda1794ba359db279b462
Phil Bennett <philipjbennett@users.noreply.github.com>
2009-02-18 22:55:41 +0000

    GCC compile fix for last check-in

--- src/mame/video/toaplan2.c
+++ src/mame/video/toaplan2.c
@@ -1004,9 +1004,9 @@ static void toaplan2_scroll_reg_data_w(running_machine *machine, offs_t offset, 
 					if ((toaplan2_sub_cpu == CPU_2_Z80) && (data == 3))
 					{
 						/* HACK! When tilted, sound CPU needs to be reset. */
-						const device_config *ym;
+						const device_config *ym = device_list_find_by_tag(machine->config->devicelist, SOUND, "ym");
 
-						if (ym = device_list_find_by_tag(machine->config->devicelist, SOUND, "ym"))
+						if (ym != NULL)
 						{
 							if (!strcmp(device_get_info_string(ym, DEVINFO_STR_NAME), "YM3812"))
 							{
