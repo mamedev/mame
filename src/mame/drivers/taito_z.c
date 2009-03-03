a590e437bf9a4db01fc8227e9fc189b123bf5253
Phil Bennett <philipjbennett@users.noreply.github.com>
2009-03-03 11:16:55 +0000

    02711 spacegun: EE-ROM error after gun calibration. [Phil Bennett]

--- src/mame/drivers/taito_z.c
+++ src/mame/drivers/taito_z.c
@@ -1056,7 +1056,9 @@ static const eeprom_interface eeprom_intf =
 	"0101",			/* write command */
 	"0111",			/* erase command */
 	"0100000000",	/* lock command */
-	"0100111111" 	/* unlock command */
+	"0100111111",	/* unlock command */
+	0,				/* multi-read disabled */
+	1				/* reset delay */
 };
 
 static NVRAM_HANDLER( spacegun )
