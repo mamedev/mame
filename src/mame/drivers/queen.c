f710d96046f0bce23985b80a8782e0cac849b7be
Angelo Salese <angelosa@users.noreply.github.com>
2009-03-17 16:07:12 +0000

    Added basic memory boundaries to the Queen driver, not worth mentioning.

--- src/mame/drivers/queen.c
+++ src/mame/drivers/queen.c
@@ -7,9 +7,12 @@ N.revisione
 CPU main PCB is a standard EPIA
 ROMs    epia BIOS + solid state HD
 
-
-
- ------- better component list should be added here!!
+1x VIA EPIA5000EAG (main PCB) with:
+VT8231 South Bridge
+VIA Eden Processor
+VIA EPIA Companion Chip VT1612A (Audio CODEC)
+VIA EPIA Companion Chip VT6103 (Networking)
+processor speed is 533MHz <- likely to be a Celeron or a Pentium III class CPU -AS
 
  it's a 2002 era PC at least based on the BIOS,
   almost certainly newer than the standard 'PENTIUM' CPU
@@ -30,10 +33,37 @@ static VIDEO_UPDATE(queen)
 }
 
 static ADDRESS_MAP_START( queen_map, ADDRESS_SPACE_PROGRAM, 32 )
+	AM_RANGE(0x00000000, 0x0009ffff) AM_RAM
+	AM_RANGE(0x000a0000, 0x000bffff) AM_RAM
+	AM_RANGE(0x000c0000, 0x000fffff) AM_ROM AM_REGION("bios", 0) AM_WRITENOP
+	AM_RANGE(0x00100000, 0x01ffffff) AM_RAM
+	AM_RANGE(0x10000000, 0x100001ff) AM_RAM
+	AM_RANGE(0x20000000, 0x200001ff) AM_RAM
+	AM_RANGE(0x30000000, 0x300001ff) AM_RAM
+	AM_RANGE(0x40000000, 0x400001ff) AM_RAM
+	AM_RANGE(0x50000000, 0x500001ff) AM_RAM
 	AM_RANGE(0xfffc0000, 0xffffffff) AM_ROM AM_REGION("bios", 0)	/* System BIOS */
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START(queen_io, ADDRESS_SPACE_IO, 32)
+	AM_RANGE(0x0000, 0x001f) AM_RAM//AM_DEVREADWRITE8("dma8237_1", dma8237_r, dma8237_w, 0xffffffff)
+	AM_RANGE(0x0020, 0x003f) AM_RAM//AM_DEVREADWRITE8("pic8259_1", pic8259_r, pic8259_w, 0xffffffff)
+	AM_RANGE(0x0040, 0x005f) AM_RAM//AM_DEVREADWRITE8("pit8254", pit8253_r, pit8253_w, 0xffffffff)
+	AM_RANGE(0x0060, 0x006f) AM_RAM//AM_READWRITE(kbdc8042_32le_r,			kbdc8042_32le_w)
+	AM_RANGE(0x0070, 0x007f) AM_RAM//AM_READWRITE(mc146818_port32le_r,		mc146818_port32le_w)
+	AM_RANGE(0x0080, 0x009f) AM_RAM//AM_READWRITE(at_page32_r,				at_page32_w)
+	AM_RANGE(0x00a0, 0x00bf) AM_RAM//AM_DEVREADWRITE8("pic8259_2", pic8259_r, pic8259_w, 0xffffffff)
+	AM_RANGE(0x00c0, 0x00df) AM_RAM//AM_DEVREADWRITE("dma8237_2", at32_dma8237_2_r, at32_dma8237_2_w)
+	AM_RANGE(0x00e8, 0x00eb) AM_NOP
+	AM_RANGE(0x01f0, 0x01f7) AM_RAM//AM_DEVREADWRITE("ide", ide_r, ide_w)
+	AM_RANGE(0x0300, 0x03af) AM_NOP
+	AM_RANGE(0x03b0, 0x03df) AM_NOP
+	AM_RANGE(0x0278, 0x027b) AM_WRITENOP//AM_WRITE(pnp_config_w)
+	AM_RANGE(0x03f0, 0x03ff) AM_RAM//AM_DEVREADWRITE("ide", fdc_r, fdc_w)
+	AM_RANGE(0x0a78, 0x0a7b) AM_WRITENOP//AM_WRITE(pnp_data_w)
+	AM_RANGE(0x0cf8, 0x0cff) AM_RAM//AM_DEVREADWRITE("pcibus", pci_32le_r,	pci_32le_w)
+	AM_RANGE(0x4004, 0x4007) AM_RAM // - todo: identify these two.
+	AM_RANGE(0x5000, 0x5007) AM_RAM // /
 ADDRESS_MAP_END
 
 
@@ -42,7 +72,7 @@ INPUT_PORTS_END
 
 
 static MACHINE_DRIVER_START( queen )
-	MDRV_CPU_ADD("maincpu", PENTIUM, 400000000) // no idea
+	MDRV_CPU_ADD("maincpu", PENTIUM, 533000000) // Celeron or Pentium 3, 533 Mhz
 	MDRV_CPU_PROGRAM_MAP(queen_map, 0)
 	MDRV_CPU_IO_MAP(queen_io, 0)
 
@@ -71,4 +101,4 @@ ROM_START( queen )
 ROM_END
 
 
-GAME( 2002?, queen,  0,    queen, queen,  0, ROT0, "STG?", "Queen?", GAME_NOT_WORKING|GAME_NO_SOUND )
+GAME( 2002?, queen,  0,    queen, queen,  0, ROT0, "STG", "Queen?", GAME_NOT_WORKING|GAME_NO_SOUND )
