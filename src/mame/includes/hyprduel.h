5dcec26757afe9eac5b378fe7725b6d4de1c6feb
Yasuhiro Ogawa <wd40yasu@users.noreply.github.com>
2009-03-27 23:24:20 +0000

    missing from last commit

---
+++ src/mame/includes/hyprduel.h
@@ -0,0 +1,26 @@
+#define RASTER_LINES 262
+#define FIRST_VISIBLE_LINE 0
+#define LAST_VISIBLE_LINE 223
+
+/*----------- defined in drivers/hyprduel.c -----------*/
+
+extern int rastersplit;
+
+/*----------- defined in video/hyprduel.c -----------*/
+
+extern UINT16 *hyprduel_videoregs;
+extern UINT16 *hyprduel_screenctrl;
+extern UINT16 *hyprduel_tiletable;
+extern size_t hyprduel_tiletable_size;
+extern UINT16 *hyprduel_vram_0, *hyprduel_vram_1, *hyprduel_vram_2;
+extern UINT16 *hyprduel_window;
+
+WRITE16_HANDLER( hyprduel_paletteram_w );
+WRITE16_HANDLER( hyprduel_window_w );
+WRITE16_HANDLER( hyprduel_vram_0_w );
+WRITE16_HANDLER( hyprduel_vram_1_w );
+WRITE16_HANDLER( hyprduel_vram_2_w );
+WRITE16_HANDLER( hypr_scrollreg_w );
+WRITE16_HANDLER( hypr_scrollreg_init_w );
+VIDEO_START( hyprduel_14220 );
+VIDEO_UPDATE( hyprduel );
