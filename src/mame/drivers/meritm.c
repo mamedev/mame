fa7352c6bf04dd817690baa25969a37fe56eae03
Brian Troha <briantro@users.noreply.github.com>
2009-04-30 01:32:20 +0000

    remove "- megat4st doesn't boot, protection problem?" from todo list as this was fixed.  Better describe the issue for for the megat5 set as well.

--- src/mame/drivers/meritm.c
+++ src/mame/drivers/meritm.c
@@ -73,8 +73,7 @@ PROGRAM#    Program Version      Program Differences
   - clean up V9938 interrupt implementation
   - finish inputs, dsw, outputs (lamps)
   - problem with registering touches on the bottom of the screen (currently hacked to work)
-  - megat5: has jmp $0000 in the initialization code causing infinite loop, is rom bad?
-  - megat4st doesn't boot, protection problem?
+  - megat5: has jmp $0000 in the initialization code causing infinite loop, rom U38 is dumped at half size / bad dump
   - for pbst30 only roms were found, it appears that two roms with graphics data were missing, using pitbossm roms for now
  */
 
