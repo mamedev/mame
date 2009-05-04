298cf8348d53b2b2bca49df4c221f9588d327894
Nathan Woods <npwoods@mess.org>
2009-05-04 11:27:27 +0000

    Sync with MESS

--- src/emu/sound/wave.h
+++ src/emu/sound/wave.h
@@ -14,4 +14,9 @@
 DEVICE_GET_INFO( wave );
 #define SOUND_WAVE DEVICE_GET_INFO_NAME( wave )
 
+
+#define MDRV_SOUND_WAVE_ADD(_tag, _cass_tag) \
+	MDRV_SOUND_ADD( _tag, WAVE, 0 ) \
+	MDRV_DEVICE_CONFIG( _cass_tag )
+
 #endif /* __WAVE_H__ */
