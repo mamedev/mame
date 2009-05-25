8d6d6b77a6f90fb86ee9d03b82c9bf3be5768423
Aaron Giles <aaron@aarongiles.com>
2009-05-25 01:19:29 +0000

    The default DIP for this game has the "FULL TESTS" switched ON . This DIFF simply changes the default state to "OFF", allowing you to play the game and fixing MT Bug #3196.
    tafoid@yahoo.com

--- src/mame/drivers/royalmah.c
+++ src/mame/drivers/royalmah.c
@@ -1729,7 +1729,7 @@ static INPUT_PORTS_START( daisyari )
 	PORT_DIPNAME( 0x40, 0x00, DEF_STR( Unknown ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x40, DEF_STR( On ) )
-	PORT_DIPNAME( 0x80, 0x00, "Full Tests" )
+	PORT_DIPNAME( 0x80, 0x80, "Full Tests" )
 	PORT_DIPSETTING(    0x80, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
 
