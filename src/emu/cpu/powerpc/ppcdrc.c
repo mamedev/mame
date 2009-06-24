6c791836fabc3e1cacc5339e8974fd11e0e9b26b
Phil Bennett <philipjbennett@users.noreply.github.com>
2009-06-24 17:31:17 +0000

    Fixed PPCDRC incorrect instruction checksum order [Phil Bennett, R Belmont]

--- src/emu/cpu/powerpc/ppcdrc.c
+++ src/emu/cpu/powerpc/ppcdrc.c
@@ -2106,7 +2106,7 @@ static void generate_checksum_block(powerpc_state *ppc, drcuml_block *block, com
 	{
 		if (!(seqhead->flags & OPFLAG_VIRTUAL_NOOP))
 		{
-			void *base = memory_decrypted_read_ptr(ppc->program, seqhead->physpc);
+			void *base = memory_decrypted_read_ptr(ppc->program, seqhead->physpc ^ ppc->codexor);
 			UML_LOAD(block, IREG(0), base, IMM(0), DWORD);									// load    i0,base,dword
 			UML_CMP(block, IREG(0), IMM(seqhead->opptr.l[0]));								// cmp     i0,*opptr
 			UML_EXHc(block, IF_NE, ppc->impstate->nocode, IMM(seqhead->pc));				// exne    nocode,seqhead->pc
@@ -2120,20 +2120,20 @@ static void generate_checksum_block(powerpc_state *ppc, drcuml_block *block, com
 		for (curdesc = seqhead->next; curdesc != seqlast->next; curdesc = curdesc->next)
 			if (!(curdesc->flags & OPFLAG_VIRTUAL_NOOP))
 			{
-				void *base = memory_decrypted_read_ptr(ppc->program, seqhead->physpc);
+				void *base = memory_decrypted_read_ptr(ppc->program, seqhead->physpc ^ ppc->codexor);
 				UML_LOAD(block, IREG(0), base, IMM(0), DWORD);								// load    i0,base,dword
 				UML_CMP(block, IREG(0), IMM(curdesc->opptr.l[0]));							// cmp     i0,*opptr
 				UML_EXHc(block, IF_NE, ppc->impstate->nocode, IMM(seqhead->pc));			// exne    nocode,seqhead->pc
 			}
 #else
 		UINT32 sum = 0;
-		void *base = memory_decrypted_read_ptr(ppc->program, seqhead->physpc);
+		void *base = memory_decrypted_read_ptr(ppc->program, seqhead->physpc ^ ppc->codexor);
 		UML_LOAD(block, IREG(0), base, IMM(0), DWORD);										// load    i0,base,dword
 		sum += seqhead->opptr.l[0];
 		for (curdesc = seqhead->next; curdesc != seqlast->next; curdesc = curdesc->next)
 			if (!(curdesc->flags & OPFLAG_VIRTUAL_NOOP))
 			{
-				base = memory_decrypted_read_ptr(ppc->program, curdesc->physpc);
+				base = memory_decrypted_read_ptr(ppc->program, curdesc->physpc ^ ppc->codexor);
 				UML_LOAD(block, IREG(1), base, IMM(0), DWORD);								// load    i1,base,dword
 				UML_ADD(block, IREG(0), IREG(0), IREG(1));									// add     i0,i0,i1
 				sum += curdesc->opptr.l[0];
