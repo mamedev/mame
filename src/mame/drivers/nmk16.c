407d84ec148e9b8def67b0e6e1e80340121d5c17
Brian Troha <briantro@users.noreply.github.com>
2009-08-02 17:23:47 +0000

    Documents what the "language" dipswitch actually does for Rapid Hero.  When set to "Japanese", the main characters text "talk" on the Stage Clear screens.  When set to "English" you simply see "Stage 'x' Clear" (where 'x' is the current stage you just finished).

--- src/mame/drivers/nmk16.c
+++ src/mame/drivers/nmk16.c
@@ -2542,7 +2542,7 @@ static INPUT_PORTS_START( raphero )
 
 	PORT_START("DSW1")
 	PORT_SERVICE_DIPLOC( 0x01, IP_ACTIVE_LOW, "SW1:8" )
-	PORT_DIPNAME( 0x02, 0x00, DEF_STR( Language ) )		PORT_DIPLOCATION("SW1:7") /* Doesn't seem to have any effect */
+	PORT_DIPNAME( 0x02, 0x00, DEF_STR( Language ) )		PORT_DIPLOCATION("SW1:7") /* Main characters text "talk" at Stage Clear screen, but only when set to Japanese */
 	PORT_DIPSETTING(    0x02, DEF_STR( Japanese ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( English ) )
 	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Unused ) )		PORT_DIPLOCATION("SW1:6") /* The manual states this dip is "Unused" */
@@ -7116,7 +7116,7 @@ GAME( 1993, tdragon2, 0,        tdragon2, tdragon2, 0,        ROT270, "NMK",    
 GAME( 1993, tdragon2a,tdragon2, tdragon2, tdragon2, 0,        ROT270, "NMK",                          "Thunder Dragon 2 (1st Oct. 1993)", GAME_NO_COCKTAIL )
 GAME( 1993, bigbang,  tdragon2, tdragon2, tdragon2, 0,        ROT270, "NMK",                          "Big Bang (9th Nov. 1993)", GAME_NO_COCKTAIL )
 
-GAME( 1994, raphero,  0,        raphero,  raphero,  0,        ROT270, "Media Trading Corp",           "Rapid Hero (Japan?)", GAME_IMPERFECT_SOUND ) // 23rd July 1993 in test mode, (c)1994 on title screen
+GAME( 1994, raphero,  0,        raphero,  raphero,  0,        ROT270, "Media Trading Corp",           "Rapid Hero", GAME_IMPERFECT_SOUND ) // 23rd July 1993 in test mode, (c)1994 on title screen
 
 /* both sets of both these games show a date of 9th Mar 1992 in the test mode, they look like different revisions so I doubt this is accurate */
 GAME( 1992, sabotenb, 0,        bjtwin,   sabotenb, nmk,      ROT0,   "NMK / Tecmo",                  "Saboten Bombers (set 1)", GAME_NO_COCKTAIL )
