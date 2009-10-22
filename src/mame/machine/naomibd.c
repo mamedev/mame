a8ddc0a162222e2bff000e29c9a9ab7f41714e67
R. Belmont <rb6502@users.noreply.github.com>
2009-10-22 01:51:45 +0000

    Naomi: add protection data for 18 Wheeler [Deunan Knute, ANY]

--- src/mame/machine/naomibd.c
+++ src/mame/machine/naomibd.c
@@ -288,6 +288,7 @@ static const naomibd_config_table naomibd_translate_tbl[] =
 		          0xffffffff, 0xffffffff, 0xffffffff } },
 	{ "crzytaxi", 0,{ 0x0219, 0, 0, 0xffffffff, 0xffffffff, 0xffffffff } }, 
 	{ "jambo",    0,{ 0x0223, 0, 0, 0xffffffff, 0xffffffff, 0xffffffff } }, 
+	{ "18wheelr", 0,{ 0x1502, 0, 0, 0xffffffff, 0xffffffff, 0xffffffff } }, 
 };
 
 /***************************************************************************
