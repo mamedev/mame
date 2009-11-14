50317532f483d820925959cf757dc5b3a0823daf
Angelo Salese <angelosa@users.noreply.github.com>
2009-11-14 15:41:33 +0000

    Fixed gfx decoding and hooked up mc6845 in the Forte Card driver [Angelo Salese]

--- src/mame/drivers/fortecar.c
+++ src/mame/drivers/fortecar.c
@@ -4,9 +4,10 @@
 
 TODO:
 -eeprom
--bankswitch
 -inputs
 -missing color proms?
+
+bp 529 do pc=53e
 
 -----------------------------------------------------
 
@@ -28,24 +29,36 @@ dip 1X8
 #include "machine/eeprom.h"
 #include "sound/ay8910.h"
 #include "machine/8255ppi.h"
+#include "video/mc6845.h"
 
 static UINT8 *fortecar_ram;
 static int bank;
 
-static WRITE8_HANDLER( fortecar_videoregs_w )
+static VIDEO_START(fortecar)
 {
-	static UINT8 address;
+}
 
-	if(offset == 0)
-		address = data;
-	else
+static VIDEO_UPDATE(fortecar)
+{
+	int x,y,count;
+	count = 0;
+
+	for (y=0;y<0x1e;y++)
 	{
-		switch(address)
+		for(x=0;x<0x4b;x++)
 		{
-			default:
-				logerror("Video Register %02x called with %02x data\n",address,data);
+			int tile,color;
+
+			tile = (fortecar_ram[(count*4)+1] | (fortecar_ram[(count*4)+2]<<8)) & 0xfff;
+			color = fortecar_ram[(count*4)+3] & 3;
+
+			drawgfx_opaque(bitmap,cliprect,screen->machine->gfx[0],tile,color,0,0,x*8,y*8);
+			count++;
+
 		}
 	}
+
+	return 0;
 }
 
 static WRITE8_DEVICE_HANDLER( ppi0_portc_w )
@@ -67,26 +80,27 @@ static const ppi8255_interface ppi0intf =
 	DEVCB_NULL,					DEVCB_NULL,					DEVCB_HANDLER(ppi0_portc_w)
 };
 
-static WRITE8_HANDLER( rom_bank_w )
-{
-	int new_bank = (data&0xff)>>0;
-
-	if(bank!=new_bank) {
-		UINT8 *ROM = memory_region(space->machine, "maincpu");
-		UINT32 bankaddress;
-
-		bank = new_bank;
-		bankaddress = 0x10000 + 0x40 * bank;
-		memory_set_bankptr(space->machine, 1, &ROM[bankaddress]);
-	}
-}
 
 static WRITE8_DEVICE_HANDLER( ayporta_w )
 {
+	logerror("AY port A write %02x\n",data);
+
+	/*
+	lamps for POST?
+	0x01: RAM test d000-d7ff
+	0x02: VRAM test d800-ffff
+	0x04: Video SYNC test
+	0x08: ROM check
+	0x10: NVRAM check
+	0x20: IRQ test
+	0x40: Stack RAM check
+	*/
 }
 
+/* lamps? */
 static WRITE8_DEVICE_HANDLER( ayportb_w )
 {
+	//logerror("AY port B write %02x\n",data);
 }
 
 static const ay8910_interface ay8910_config =
@@ -101,18 +115,19 @@ static const ay8910_interface ay8910_config =
 
 static ADDRESS_MAP_START( fortecar_map, ADDRESS_SPACE_PROGRAM, 8 )
 	AM_RANGE(0x0000, 0xbfff) AM_ROM
-	AM_RANGE(0xc000, 0xc7ff) AM_READWRITE(SMH_BANK(1), SMH_ROM) //bank
+	AM_RANGE(0xc000, 0xc7ff) AM_ROM
 	AM_RANGE(0xd000, 0xd7ff) AM_RAM
 	AM_RANGE(0xd800, 0xffff) AM_RAM AM_BASE(&fortecar_ram)
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START( fortecar_ports, ADDRESS_SPACE_IO, 8 )
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
-	AM_RANGE(0x20, 0x21) AM_WRITE(fortecar_videoregs_w) // MC6845?
+	AM_RANGE(0x20, 0x20) AM_DEVWRITE("crtc", mc6845_address_w)
+	AM_RANGE(0x21, 0x21) AM_DEVWRITE("crtc", mc6845_register_w)
 	AM_RANGE(0x40, 0x40) AM_DEVREAD("ay", ay8910_r)
 	AM_RANGE(0x40, 0x41) AM_DEVWRITE("ay", ay8910_address_data_w)
-	AM_RANGE(0x60, 0x62) AM_DEVREADWRITE("fcppi0", ppi8255_r, ppi8255_w)//M5L8255AP
-	AM_RANGE(0x81, 0x81) AM_WRITE(rom_bank_w) //completely wrong,might not be there...
+	AM_RANGE(0x60, 0x63) AM_DEVREADWRITE("fcppi0", ppi8255_r, ppi8255_w)//M5L8255AP
+//	AM_RANGE(0x80, 0x81) //8251A UART
  	AM_RANGE(0xa0, 0xa0) AM_READ_PORT("IN0") //written too,multiplexer?
  	AM_RANGE(0xa1, 0xa1) AM_READ_PORT("IN1")
 ADDRESS_MAP_END
@@ -178,24 +193,12 @@ static INPUT_PORTS_START( fortecar )
 	PORT_DIPNAME( 0x02, 0x02, DEF_STR( Unknown ) )
 	PORT_DIPSETTING(    0x02, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x04, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x08, 0x08, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x08, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x10, 0x10, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x10, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x20, 0x20, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x20, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x40, 0x40, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x40, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x80, 0x80, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x80, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_POKER_HOLD1 )
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_POKER_HOLD2 )
+	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_POKER_HOLD3 )
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_POKER_HOLD4 )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_POKER_HOLD5 )
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_GAMBLE_DEAL )
 
 	PORT_START("DSW1")	/* 8bit */
 	PORT_DIPNAME( 0x01, 0x01, "DSW1" )
@@ -227,47 +230,78 @@ INPUT_PORTS_END
 static const gfx_layout tiles8x8_layout =
 {
 	8,8,
-	RGN_FRAC(1,1),
-	2,
-	{ 0,4 },
+	RGN_FRAC(1,3),
+	6,
+	{ 0,4, RGN_FRAC(1,3)+0, RGN_FRAC(1,3)+4, RGN_FRAC(2,3)+0, RGN_FRAC(2,3)+4 },
 	{ 8,9,10,11,0, 1, 2, 3 },
 	{ 0*16, 1*16, 2*16, 3*16, 4*16, 5*16, 6*16, 7*16 },
 	16*8
 };
 
 static GFXDECODE_START( fortecar )
-	GFXDECODE_ENTRY( "gfx1", 0, tiles8x8_layout, 0, 16 )
+	GFXDECODE_ENTRY( "gfx1", 0, tiles8x8_layout, 0, 4 )
 GFXDECODE_END
-
-static VIDEO_START(fortecar)
-{
-}
-
-static VIDEO_UPDATE(fortecar)
-{
-	int x,y,count;
-	count = 0;
-
-	for (y=0;y<0x1e;y++)
-	{
-		for(x=0;x<0x4b;x++)
-		{
-			int tile,color;
-
-			tile = fortecar_ram[(count*4)+1] | (fortecar_ram[(count*4)+2]<<8);
-			color = fortecar_ram[(count*4)+3];
-
-			drawgfx_opaque(bitmap,cliprect,screen->machine->gfx[0],tile,color,0,0,x*8,y*8);
-			count++;
-
-		}
-	}
-	return 0;
-}
 
 static MACHINE_RESET(fortecar)
 {
 	bank = -1;
+}
+
+/* WRONG, just to see something */
+static PALETTE_INIT( fortecar )
+{
+	int i,r,g,b;
+
+	for (i = 0; i < 0x40*4; i++ )
+	{
+		g = (i >> 0) & 3;
+		b = (i >> 2) & 3;
+		r = (i >> 4) & 3;
+
+		palette_set_color(machine, i, MAKE_RGB(pal2bit(r), pal2bit(g), pal2bit(b)));
+	}
+}
+
+static const mc6845_interface mc6845_intf =
+{
+	"screen",	/* screen we are acting on */
+	8,			/* number of pixels per video memory address */
+	NULL,		/* before pixel update callback */
+	NULL,		/* row update callback */
+	NULL,		/* after pixel update callback */
+	DEVCB_NULL,	/* callback for display state changes */
+	DEVCB_NULL,	/* callback for cursor state changes */
+	DEVCB_NULL,	/* HSYNC callback */
+	DEVCB_NULL,	/* VSYNC callback */
+	NULL		/* update address callback */
+};
+
+//51f
+static const UINT8 default_eeprom[128]=
+{
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+};
+
+static NVRAM_HANDLER(fortecar)
+{
+	if (read_or_write)
+		eeprom_save(file);
+	else
+	{
+		eeprom_init(machine, &eeprom_interface_93C46);
+
+		if (file)
+			eeprom_load(file);
+		else
+			eeprom_set_data(default_eeprom,128);
+	}
 }
 
 static MACHINE_DRIVER_START( fortecar )
@@ -286,15 +320,18 @@ static MACHINE_DRIVER_START( fortecar )
 	MDRV_SCREEN_VISIBLE_AREA(0, 640-1, 0, 256-1)
 
 	MDRV_MACHINE_RESET(fortecar)
-	MDRV_NVRAM_HANDLER(93C46) //GOLDSTAR GM76C256ALL-70
+	MDRV_NVRAM_HANDLER(fortecar)
 
 	MDRV_PPI8255_ADD("fcppi0", ppi0intf)
 
 	MDRV_GFXDECODE(fortecar)
 	MDRV_PALETTE_LENGTH(0x100)
+	MDRV_PALETTE_INIT(fortecar)
 
 	MDRV_VIDEO_START(fortecar)
 	MDRV_VIDEO_UPDATE(fortecar)
+
+	MDRV_MC6845_ADD("crtc", MC6845, 6000000/4, mc6845_intf)	/* unknown type / hand tuned to get ~60 fps */
 
 	MDRV_SPEAKER_STANDARD_MONO("mono")
 
@@ -304,14 +341,16 @@ static MACHINE_DRIVER_START( fortecar )
 MACHINE_DRIVER_END
 
 ROM_START( fortecar )
-	ROM_REGION( 0x14000, "maincpu", 0 )
-	ROM_LOAD( "fortecar.u7", 0x00000, 0x0c000, CRC(2a4b3429) SHA1(8fa630dac949e758678a1a36b05b3412abe8ae16)  )
-	ROM_CONTINUE(			 0x10000, 0x04000 )
+	ROM_REGION( 0x10000, "maincpu", 0 )
+	ROM_LOAD( "fortecar.u7", 0x00000, 0x010000, CRC(2a4b3429) SHA1(8fa630dac949e758678a1a36b05b3412abe8ae16)  )
 
 	ROM_REGION( 0x30000, "gfx1", 0 )
 	ROM_LOAD( "fortecar.u38", 0x00000, 0x10000, CRC(c2090690) SHA1(f0aa8935b90a2ab6043555ece69f926372246648) )
 	ROM_LOAD( "fortecar.u39", 0x10000, 0x10000, CRC(fc3ddf4f) SHA1(4a95b24c4edb67f6d59f795f86dfbd12899e01b0) )
 	ROM_LOAD( "fortecar.u40", 0x20000, 0x10000, CRC(9693bb83) SHA1(e3e3bc750c89a1edd1072ce3890b2ce498dec633) )
+
+	ROM_REGION( 0x200, "prom", 0 )
+	ROM_LOAD( "prom.x", 0x000, 0x200, NO_DUMP )
 ROM_END
 
 static DRIVER_INIT( fortecar )
