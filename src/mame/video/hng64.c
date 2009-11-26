58e1ff522ae30fa0aa73045ad70e0f601d669ad0
Angelo Salese <angelosa@users.noreply.github.com>
2009-11-26 00:48:24 +0000

    Removed an useless debug thing ;)

--- src/mame/video/hng64.c
+++ src/mame/video/hng64.c
@@ -123,7 +123,7 @@ static struct polygon *polys ;
  *   1    | YYYY YYYY YYYY YYYY XXXX XXXX XXXX XXXX | x/y zoom (*)
  *   2    | ---- -zzz zzzz zzzz ---- ---I cccc CCCC | Z-buffer value, 'Inline' chain flag, x/y chain
  *   3    | ---- ---- pppp pppp ---- ---- ---- ---- | palette entry
- *   4    | mmmm -?fF a??? tttt tttt tttt tttt tttt | mosaic, unknown (**) , flip bits, additive blending, unknown (***), tile number
+ *   4    | mmmm -?fF a??? tttt tttt tttt tttt tttt | mosaic factor, unknown (**) , flip bits, additive blending, unknown (***), tile number
  *   5    | ---- ---- ---- ---- ---- ---- ---- ---- | not used ??
  *   6    | ---- ---- ---- ---- ---- ---- ---- ---- | not used ??
  *   7    | ---- ---- ---- ---- ---- ---- ---- ---- | not used ??
@@ -205,9 +205,6 @@ static void draw_sprites(running_machine *machine, bitmap_t *bitmap, const recta
 		pal =(source[3]&0x00ff0000)>>16;
 		xflip=(source[4]&0x02000000)>>25;
 		yflip=(source[4]&0x01000000)>>24;
-
-		if(source[4]&0xf0000000)
-			printf("%08x\n",source[4]);
 
 //      if (!(source[4] == 0x00000000 || source[4] == 0x000000aa))
 //          mame_printf_debug("unknown : %.8x %.8x %.8x %.8x %.8x %.8x %.8x %.8x \n", source[0], source[1], source[2], source[3],
