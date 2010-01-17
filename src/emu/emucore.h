89d20f98666d8b9b0a6666d8aa62151091ce6f99
Couriersud <couriersud@users.noreply.github.com>
2010-01-17 19:21:04 +0000

    Undef two defines from Solaris stdlib.h which collide with mame.

--- src/emu/emucore.h
+++ src/emu/emucore.h
@@ -21,6 +21,12 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+// some cleanups for Solaris for things defined in stdlib.h
+#ifdef SDLMAME_SOLARIS
+#undef si_status
+#undef WWORD
+#endif
+
 // standard C++ includes
 #include <exception>
 
