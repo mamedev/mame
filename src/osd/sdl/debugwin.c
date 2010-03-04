db548c8689b1460a49485c8d458ef0a110d2ced1
Aaron Giles <aaron@aarongiles.com>
2010-03-04 09:32:38 +0000

    Fix compile error.

--- src/osd/sdl/debugwin.c
+++ src/osd/sdl/debugwin.c
@@ -1189,7 +1189,7 @@ on_memoryview_key_press_event             (GtkWidget   *widget,
 
 #else
 
-#include "emucore.h"
+#include "emu.h"
 
 // win32 stubs for linking
 void osd_wait_for_debugger(running_device *device, int firststop)
