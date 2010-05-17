a425813e5356c80406f2bc87e4cfdf80b73ac8b3
Michaël Banaan Ananas <happppp@users.noreply.github.com>
2010-05-17 11:50:25 +0000

    making clear it's not the same as Konami's Top Gunner (like "Top Secret" Capcom and Exidy), no whatsnew

--- src/mame/drivers/vertigo.c
+++ src/mame/drivers/vertigo.c
@@ -211,4 +211,4 @@ ROM_END
  *
  *************************************/
 
-GAME( 1986, topgunnr, 0, vertigo, vertigo, 0, ROT0, "Exidy", "Top Gunner", GAME_SUPPORTS_SAVE )
+GAME( 1986, topgunnr, 0, vertigo, vertigo, 0, ROT0, "Exidy", "Top Gunner (Exidy)", GAME_SUPPORTS_SAVE )
