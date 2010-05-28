765aa09be990c654517b181b3cd8bac94e7ab531
Scott Stone <tafoid@users.noreply.github.com>
2010-05-28 00:15:39 +0000

    Fixed a warning in Visual Studio 2010 that was introduced by a previous commit.  [Oliver Stoneberg]

--- src/osd/windows/winmain.c
+++ src/osd/windows/winmain.c
@@ -134,7 +134,7 @@ static sym_get_line_from_addr_64_ptr sym_get_line_from_addr_64;
 static get_module_information_ptr get_module_information;
 
 static char mapfile_name[MAX_PATH];
-static LPTOP_LEVEL_EXCEPTION_FILTER WINAPI pass_thru_filter;
+static LPTOP_LEVEL_EXCEPTION_FILTER pass_thru_filter;
 
 static HANDLE watchdog_reset_event;
 static HANDLE watchdog_exit_event;
