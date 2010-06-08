1110777ee553e92bd5eff6aea488fe74f48fb243
Couriersud <couriersud@users.noreply.github.com>
2010-06-08 19:34:21 +0000

    Fix 64bit compile on linux 4.4.0 - no whatsnew

--- src/emu/cpu/drcbex64.c
+++ src/emu/cpu/drcbex64.c
@@ -714,7 +714,7 @@ static drcbe_state *drcbex64_alloc(drcuml_state *drcuml, drccache *cache, runnin
 	/* get address spaces and accessors */
 	for (spacenum = 0; spacenum < ADDRESS_SPACES; spacenum++)
 	{
-		drcbe->space[spacenum] = device->space(spacenum);
+		drcbe->space[spacenum] = downcast<cpu_device *>(device)->space(spacenum);
 		if (drcbe->space[spacenum] != NULL)
 			drcbe->accessors[spacenum] = drcbe->space[spacenum]->accessors;
 	}
