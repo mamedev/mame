5fc84c4d2554adb448dcefb527a56afa471fe627
Roberto Zandona <robertoz@mamedev.org>
2010-07-12 16:32:47 +0000

    cb2001: added some opcodes

--- src/mame/drivers/cb2001.c
+++ src/mame/drivers/cb2001.c
@@ -56,14 +56,14 @@ static const UINT8 cb2001_decryption_table[256] = {
 //    wwww                     **** pppp                 debu           pppp      pppp
 	xxxx,0xea,xxxx,xxxx,0xd0,0xb0,0x5e,xxxx, xxxx,0xa2,xxxx,xxxx,0xa3,xxxx,xxxx,0xb3, /* 40 */
 //         !!!!           **** pppp pppp            pppp           pppp           pppp
-	xxxx,xxxx,0x2c,xxxx,0x9d,xxxx,0x42,0xc0, 0x04,xxxx,xxxx,xxxx,0xeb,0xab,xxxx,xxxx, /* 50 */
-//              pppp      ****      pppp pppp  ****                pppp pppp
+	xxxx,xxxx,0x2c,xxxx,0x9d,xxxx,0x42,0xc0, 0x04,xxxx,0xb7,xxxx,0xeb,0xab,xxxx,xxxx, /* 50 */
+//              pppp      ****      pppp pppp  ****      ****      pppp pppp
 	xxxx,xxxx,xxxx,xxxx,0x0a,xxxx,xxxx,xxxx, 0xa1,0xa5,xxxx,xxxx,xxxx,0xbb,0xba,xxxx, /* 60 */
 //                        pppp                 pppp pppp                pppp pppp
 	0xc3,0x53,0x02,0x58,xxxx,xxxx,0x24,xxxx, 0x72,xxxx,0xf3,xxxx,xxxx,0x43,xxxx,xxxx, /* 70 */
 //    pppp pppp pppp pppp           pppp       pppp      pppp           pppp
-	0x26,xxxx,xxxx,xxxx,xxxx,0x3d,0xfb,0xf6, xxxx,xxxx,0x59,xxxx,0x73,xxxx,xxxx,xxxx, /* 80 */
-//    pppp                     pppp **** pppp            pppp      pppp       
+	0x26,xxxx,xxxx,xxxx,xxxx,0x3d,0xfb,0xf6, xxxx,xxxx,0x59,xxxx,0x73,xxxx,0x02,xxxx, /* 80 */
+//    pppp                     pppp **** pppp            pppp      pppp      wwww 
 	xxxx,xxxx,0xe9,xxxx,xxxx,0xbe,xxxx,xxxx, xxxx,xxxx,0x57,xxxx,0xb9,xxxx,0xbf,xxxx, /* 90 */
 //              pppp           pppp                      pppp      pppp      pppp
 	0xc1,xxxx,0xe6,0x06,0xaa,0x9c,0xad,0xb8, 0x4e,xxxx,0x8d,0x50,0x51,0xa4,xxxx,0x1a, /* A0 */
@@ -72,8 +72,8 @@ static const UINT8 cb2001_decryption_table[256] = {
 //    pppp      pppp           pppp                 pppp pppp      pppp      pppp pppp
 	0xcb,0xec,0xee,xxxx,xxxx,0xe2,0x87,xxxx, xxxx,xxxx,0x76,0x61,0x48,xxxx,0x2e,xxxx, /* C0 */
 //    pppp pppp pppp           pppp pppp                 pppp **** ****      pppp
-	xxxx,0xf2,0x46,xxxx,0x60,xxxx,0x4f,0x47, 0x88,xxxx,xxxx,xxxx,xxxx,0xfa,0xc7,0x8b, /* D0 */
-//         ???? pppp      ****      pppp pppp  pppp                     **** !!!! pppp
+	xxxx,0xf2,0x46,xxxx,0x60,xxxx,0x4f,0x47, 0x88,xxxx,xxxx,0x04,xxxx,0xfa,0xc7,0x8b, /* D0 */
+//         ???? pppp      ****      pppp pppp  pppp           wwww      **** !!!! pppp
 	0x8a,0xb1,xxxx,0xc6,xxxx,0x5a,xxxx,0x04, 0x9a,0x52,xxxx,xxxx,xxxx,xxxx,xxxx,xxxx, /* E0 */
 //    pppp gggg      !!!!      ****      wwww  pppp ****
 	xxxx,0xae,0xfe,xxxx,xxxx,xxxx,xxxx,0x2a, xxxx,xxxx,xxxx,xxxx,0x81,xxxx,xxxx,xxxx, /* F0 */
