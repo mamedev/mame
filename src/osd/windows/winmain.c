364e13529e1686a13617ae51a59e43460baa5eb4
Aaron Giles <aaron@aarongiles.com>
2010-07-29 08:41:24 +0000

    Add notification of orientation of all monitors at startup via the output system. [Laszlo Schulteisz]

--- src/osd/windows/winmain.c
+++ src/osd/windows/winmain.c
@@ -487,6 +487,14 @@ void osd_init(running_machine *machine)
 	winsound_init(machine);
 	wininput_init(machine);
 	winoutput_init(machine);
+	
+	// notify listeners of screen configuration
+	astring tempstring;
+	for (win_window_info *info = win_window_list; info != NULL; info = info->next)
+	{
+		tempstring.printf("Orientation(%s)", utf8_from_tstring(info->monitor->info.szDevice));
+		output_set_value(tempstring, info->targetorient);
+	}
 
 	// hook up the debugger log
 	if (options_get_bool(machine->options(), WINOPTION_OSLOG))
