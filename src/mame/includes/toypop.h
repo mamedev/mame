f3f35e2af26fafac140d7ac7c13e76af90210d57
Andrew Gardner <andrew-gardner@users.noreply.github.com>
2010-10-06 06:13:57 +0000

    Reverts questionable timer changes.  [not worth mentioning]

--- src/mame/includes/toypop.h
+++ src/mame/includes/toypop.h
@@ -13,11 +13,6 @@ public:
 	int bitmapflip;
 	int palettebank;
 	int interrupt_enable_68k;
-    
-    emu_timer *io_timer_0;
-    emu_timer *io_timer_1;
-    emu_timer *io_timer_2;
-    emu_timer *irq_disable_timer;
 };
 
 
