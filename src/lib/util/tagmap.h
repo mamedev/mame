b276730d7d76470e215ca48e912b106d966daf48
Aaron Giles <aaron@aarongiles.com>
2010-12-12 16:26:31 +0000

    tagmap_hash is supposed to be fast. Calling strlen is not the way to be fast.

--- src/lib/util/tagmap.h
+++ src/lib/util/tagmap.h
@@ -164,15 +164,11 @@ public:
 
 INLINE UINT32 tagmap_hash(const char *string)
 {
-	UINT32 hash = (string[0] << 5) + string[1];
+	UINT32 hash = *string++;
 	char c;
 
-	if (strlen(string) > 2)
-	{
-		string += 2;
-		while ((c = *string++) != 0)
-			hash = ((hash << 5) | (hash >> 27)) + c;
-	}
+	while ((c = *string++) != 0)
+		hash = ((hash << 5) | (hash >> 27)) + c;
 
 	return hash;
 }
