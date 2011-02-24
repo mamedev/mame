c9eacdf091dfe3da52c5f42940753fa5b86b851f
Angelo Salese <angelosa@users.noreply.github.com>
2011-02-24 22:53:35 +0000

    Forgot to actually add the files ...

---
+++ src/mame/includes/cd32.h
@@ -0,0 +1,31 @@
+/***************************************************************************
+
+CuboCD32 definitions
+
+***************************************************************************/
+
+#ifndef __CUBOCD32_H__
+#define __CUBOCD32_H__
+
+#include "includes/amiga.h"
+
+class cd32_state : public amiga_state
+{
+public:
+	cd32_state(running_machine &machine, const driver_device_config_base &config)
+		: amiga_state(machine, config) { }
+
+	UINT16 potgo_value;
+	int cd32_shifter[2];
+	void (*input_hack)(running_machine *machine);
+	int oldstate[2];
+};
+
+/*----------- defined in machine/cd32.c -----------*/
+
+READ32_DEVICE_HANDLER( amiga_akiko32_r );
+WRITE32_DEVICE_HANDLER( amiga_akiko32_w );
+
+DECLARE_LEGACY_DEVICE(AKIKO, akiko);
+
+#endif /* __CUBOCD32_H__ */
