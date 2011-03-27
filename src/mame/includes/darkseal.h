d89c4e7b8b522148ceb2d9a655211d74bac8eb84
Aaron Giles <aaron@aarongiles.com>
2011-03-27 01:09:51 +0000

    Attempted fix for missing header file (at least it compiles).

--- src/mame/includes/darkseal.h
+++ src/mame/includes/darkseal.h
@@ -1,8 +1,12 @@
+#include "video/deco16ic.h"
+
 class darkseal_state : public driver_device
 {
 public:
 	darkseal_state(running_machine &machine, const driver_device_config_base &config)
-		: driver_device(machine, config) { }
+		: driver_device(machine, config),
+		  deco_tilegen1(*this, "tilegen1"),
+		  deco_tilegen2(*this, "tilegen2") { }
 
 	UINT16 *ram;
 	UINT16 *pf12_row;
@@ -15,7 +19,12 @@ public:
 	tilemap_t *pf1_tilemap;
 	tilemap_t *pf2_tilemap;
 	tilemap_t *pf3_tilemap;
+	UINT16 *pf1_rowscroll;
+	UINT16 *pf3_rowscroll;
 	int flipscreen;
+	
+	required_device<deco16ic_device> deco_tilegen1;
+	required_device<deco16ic_device> deco_tilegen2;
 };
 
 
