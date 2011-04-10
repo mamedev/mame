0ca7a8a7a842de4ae15b8f313b8256685766deae
Curt Coder <curtcoder@mail.com>
2011-04-10 11:27:46 +0000

    Added RES_R macro to rescap.h. (no whatsnew)

--- src/emu/machine/rescap.h
+++ src/emu/machine/rescap.h
@@ -2,6 +2,7 @@
 #define RC_MACROS_H
 
 /* Little helpers for magnitude conversions */
+#define RES_R(res) ((double)(res))
 #define RES_K(res) ((double)(res) * 1e3)
 #define RES_M(res) ((double)(res) * 1e6)
 #define CAP_U(cap) ((double)(cap) * 1e-6)
