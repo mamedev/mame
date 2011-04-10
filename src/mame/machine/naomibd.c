7ac5373f3ca2fee9163af68f59835b2e9cc8803b
R. Belmont <rb6502@users.noreply.github.com>
2011-04-10 18:21:40 +0000

    naomibd: Fix collision with system macro on NetBSD [Thomas Klausner]

--- src/mame/machine/naomibd.c
+++ src/mame/machine/naomibd.c
@@ -516,14 +516,14 @@ static void naomibd_m1_decode(naomibd_state *naomibd)
 
 // Streaming M2/M3 protection and decompression
 
-INLINE UINT16 bswap16(UINT16 in)
+INLINE UINT16 naomi_bswap16(UINT16 in)
 {
     return ((in>>8) | (in<<8));
 }
 
 static UINT16 naomibd_get_decrypted_stream(naomibd_state *naomibd)
 {
-	UINT16 wordn = bswap16(naomibd->prot.ptr[naomibd->prot.count++]);
+	UINT16 wordn = naomi_bswap16(naomibd->prot.ptr[naomibd->prot.count++]);
 
 	naomibd->prot.aux_word = block_decrypt(naomibd->dc_gamekey, naomibd->dc_seqkey, naomibd->prot.seed++, wordn);
 	wordn = (naomibd->prot.last_word&~3) | (naomibd->prot.aux_word&3);
