45d9031cc8f97a97d043a18c9db30a3b5260e3b6
R. Belmont <rb6502@users.noreply.github.com>
2011-04-23 21:54:20 +0000

    astring: Fix build on NetBSD and other systems where toupper() is a macro [Thomas Klausner]

--- src/lib/util/astring.h
+++ src/lib/util/astring.h
@@ -43,8 +43,15 @@
 #define __ASTRING_H__
 
 #include <stdarg.h>
+#include <ctype.h>
 #include "osdcomm.h"
 
+#ifdef toupper
+#undef toupper
+#endif
+#ifdef tolower
+#undef tolower
+#endif
 
 /***************************************************************************
     TYPE DEFINITIONS
