dbfbf4e0b080f9804faebe6add03204c9d999a00
R. Belmont <rb6502@users.noreply.github.com>
2011-04-27 16:46:21 +0000

    fix SDL compile (no whatsnew)

--- src/osd/sdl/drawogl.c
+++ src/osd/sdl/drawogl.c
@@ -1183,7 +1183,7 @@ static int drawogl_window_draw(sdl_window_info *window, UINT32 dc, int update)
 	texture_info *texture=NULL;
 	float vofs, hofs;
 	int  pendingPrimitive=GL_NO_PRIMITIVE, curPrimitive=GL_NO_PRIMITIVE, scrnum, is_vector;
-	const screen_device_config *screen;
+	const screen_device *screen;
 
 	if (video_config.novideo)
 	{
