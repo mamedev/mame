92ce55d23ca5cd1446b0e1337571c6828490cbed
Miodrag Milanovic <mmicko@gmail.com>
2011-05-04 19:22:37 +0000

    Cleaned slot implementation, and added few more macros (no whatsnew)

--- src/emu/dislot.c
+++ src/emu/dislot.c
@@ -5,7 +5,7 @@
 ***************************************************************************/
 
 #include "emu.h"
-
+#include "emuopts.h"
 
 device_slot_interface::device_slot_interface(const machine_config &mconfig, device_t &device)
 	: device_interface(device)
@@ -25,6 +25,19 @@ void device_slot_interface::static_set_slot_info(device_t &device, const slot_in
 	
 	slot->m_slot_interfaces = slots_info;
 	slot->m_default_card = default_card;
+}
+
+device_t* device_slot_interface::get_card_device()
+{
+	const char *subtag = device().machine().options().value(device().tag());	
+	device_t *dev = NULL;
+	if (subtag) {
+		device_slot_card_interface *intf = NULL;
+		dev = device().subdevice(subtag);
+		if (dev!=NULL && !dev->interface(intf))
+			throw emu_fatalerror("get_card_device called for device '%s' with no slot card interface", dev->tag());
+	}
+	return dev;
 }
 
 device_slot_card_interface::device_slot_card_interface(const machine_config &mconfig, device_t &device)
--- src/emu/dislot.h
+++ src/emu/dislot.h
@@ -20,7 +20,22 @@ struct slot_interface
 };
 
 #define MCFG_DEVICE_SLOT_INTERFACE(_slot_intf, _def_slot) \
-	device_slot_interface::static_set_slot_info(*device, _slot_intf, _def_slot);
+	device_slot_interface::static_set_slot_info(*device, SLOT_INTERFACE_NAME(_slot_intf), _def_slot);
+
+#define SLOT_INTERFACE_NAME(name)	slot_interface_##name
+
+#define SLOT_INTERFACE_START(name)								\
+	const slot_interface slot_interface_##name[] =				\
+	{															\
+
+#define SLOT_INTERFACE(tag,device) \
+	{ tag, device }, \
+
+#define SLOT_INTERFACE_END \
+		{ NULL, NULL }							\
+	};
+
+class device_slot_card_interface;
 
 class device_slot_interface : public device_interface
 {
@@ -32,6 +47,7 @@ public:
 	static void static_set_slot_info(device_t &device, const slot_interface *slots_info, const char *default_card);
 	const slot_interface* get_slot_interfaces() const { return m_slot_interfaces; };
 	const char * get_default_card() const { return m_default_card; };
+	device_t* get_card_device();
 protected:
 	const char *m_default_card;
 	const slot_interface *m_slot_interfaces;	
