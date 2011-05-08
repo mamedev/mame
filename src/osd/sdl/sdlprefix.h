2bb589beaf77f9ac14481cd2413bf610c97690f8
R. Belmont <rb6502@users.noreply.github.com>
2011-05-08 19:10:51 +0000

    Compile fix for kFreeBSD [Christian Marillat]

--- src/osd/sdl/sdlprefix.h
+++ src/osd/sdl/sdlprefix.h
@@ -42,7 +42,7 @@
 /* Large file support on IRIX needs _SGI_SOURCE */
 #undef _POSIX_SOURCE
 
-#elif defined(__linux__)
+#elif defined(__linux__) || defined(__FreeBSD_kernel__)
 #define SDLMAME_LINUX 1
 
 #elif defined(__FreeBSD__)
