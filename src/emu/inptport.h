c0fbaa4def380efec0adb5051481c3726dede21a
Olivier Galibert <galibert@pobox.com>
2011-06-05 15:51:01 +0000

    inptport: fix typo and MT #4360 - requires a recompile of all affected drivers to be effective (nwn)

--- src/emu/inptport.h
+++ src/emu/inptport.h
@@ -937,7 +937,7 @@ void INPUT_PORTS_NAME(_name)(device_t &owner, ioport_list &portlist, astring &er
 
 /* note that PORT_CENTERDELTA must appear after PORT_KEYDELTA */
 #define PORT_CENTERDELTA(_delta) \
-	curfield->delta = curfield->centerdelta = _delta;
+	curfield->centerdelta = _delta;
 
 #define PORT_CROSSHAIR(axis, scale, offset, altaxis) \
 	curfield->crossaxis = CROSSHAIR_AXIS_##axis; \
