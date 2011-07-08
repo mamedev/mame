93c730c79fdc6047c75d78cb936538ea5d68ddc4
smf- <smf-@users.noreply.github.com>
2011-07-08 18:39:06 +0000

    reduced number of cases.

--- src/emu/machine/generic.c
+++ src/emu/machine/generic.c
@@ -318,14 +318,10 @@ void coin_lockout_global_w(running_machine &machine, int on)
 
 static astring nvram_filename(running_machine &machine, astring &result)
 {
-	if (rom_default_bios(machine) == rom_system_bios(machine)) {
+	if (rom_system_bios(machine) == 0 || rom_default_bios(machine) == rom_system_bios(machine)) {
 		result.printf("%s",machine.basename());
 	} else {
-		if (rom_system_bios(machine)!=0) {
-			result.printf("%s_%d",machine.basename(),rom_system_bios(machine) - 1);
-		} else {
-			result.printf("%s",machine.basename());
-		}
+		result.printf("%s_%d",machine.basename(),rom_system_bios(machine) - 1);
 	}
 	return result;
 }
