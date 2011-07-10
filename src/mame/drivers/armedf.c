902bb93b740b15ab816ef57357bf7b5c450072da
Brian Troha <briantro@users.noreply.github.com>
2011-07-10 17:49:54 +0000

    Standardize names for the Terra Force roms so we don't get confused - NW

--- src/mame/drivers/armedf.c
+++ src/mame/drivers/armedf.c
@@ -1691,7 +1691,7 @@ ROM_START( terrafu ) /* Bootleg of the USA version?, uses some roms common to bo
 	ROM_LOAD( "tf-002.9d", 0x10000, 0x10000, CRC(148aa0c5) SHA1(8d8a565540e91b384a9c154522501921b7da4d4e) )
 
 	ROM_REGION( 0x4000, "blit_data", 0 )	/* data for mcu/blitter */
-	ROM_LOAD( "tf-10.11c", 0x0000, 0x4000, CRC(ac705812) SHA1(65be46ee959d8478cb6dffb25e61f7742276997b) )
+	ROM_LOAD( "10.11c", 0x0000, 0x4000, CRC(ac705812) SHA1(65be46ee959d8478cb6dffb25e61f7742276997b) )
 
 	ROM_REGION( 0x0100, "proms", 0 )	/* Unknown use */
 	ROM_LOAD( "n82s129an.11j", 0x0000, 0x0100, CRC(81244757) SHA1(6324f63e571f0f7a0bb9eb97f9994809db79493f) ) /* N82S129AN or compatible labled "TF" */
@@ -1721,8 +1721,8 @@ ROM_START( terrafj )
 	ROM_LOAD( "14.6a", 0x10000, 0x10000, CRC(744f5c9e) SHA1(696223a087bb575c7cfaba11e682b221ada461e4) )
 
 	ROM_REGION( 0x20000, "gfx4", 0 ) /* stolen from the japan bootleg set, needs PCB verification */
-	ROM_LOAD( "12.7d", 0x00000, 0x10000, BAD_DUMP CRC(d74085a1) SHA1(3f6ba85dbd6e48a502c115b2d322a586fc4f56c9) ) /* sprites */
-	ROM_LOAD( "13.9d", 0x10000, 0x10000, BAD_DUMP CRC(148aa0c5) SHA1(8d8a565540e91b384a9c154522501921b7da4d4e) )
+	ROM_LOAD( "tf-003.7d", 0x00000, 0x10000, BAD_DUMP CRC(d74085a1) SHA1(3f6ba85dbd6e48a502c115b2d322a586fc4f56c9) ) /* sprites */
+	ROM_LOAD( "tf-002.9d", 0x10000, 0x10000, BAD_DUMP CRC(148aa0c5) SHA1(8d8a565540e91b384a9c154522501921b7da4d4e) )
 
 	ROM_REGION( 0x4000, "blit_data", 0 )	/* data for mcu/blitter */
 	ROM_LOAD( "10.11c", 0x0000, 0x4000, CRC(ac705812) SHA1(65be46ee959d8478cb6dffb25e61f7742276997b) )
