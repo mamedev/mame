dea70e7f6aa831643707e25c819f7efd49e58288
Miodrag Milanovic <mmicko@gmail.com>
2011-07-27 08:19:32 +0000

    Fixed hlsl mem leak by Firewave (no whatsnew)

--- src/osd/windows/drawd3d.c
+++ src/osd/windows/drawd3d.c
@@ -507,12 +507,6 @@ static void drawd3d_window_destroy(win_window_info *window)
 	if (d3d == NULL)
 		return;
 
-	// free our effects
-	d3d->hlsl->delete_resources();
-
-	// delete the HLSL interface
-	global_free(d3d->hlsl);
-
 	// delete the device
 	device_delete(d3d);
 
@@ -898,6 +892,12 @@ static int device_create_resources(d3d_info *d3d)
 
 static void device_delete(d3d_info *d3d)
 {
+	// free our effects
+	d3d->hlsl->delete_resources();
+
+	// delete the HLSL interface
+	global_free(d3d->hlsl);
+
 	// free resources
 	device_delete_resources(d3d);
 
