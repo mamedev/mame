167deddcfe5ab8132a06e6ad3b15210fa33886f0
Miodrag Milanovic <mmicko@gmail.com>
2011-08-05 12:01:32 +0000

    Listcrc now display device name for additional roms (no whatsnew)

--- src/emu/clifront.c
+++ src/emu/clifront.c
@@ -460,15 +460,18 @@ void cli_frontend::listcrc(const char *gamename)
 
 	// iterate through matches, and then through ROMs
 	while (drivlist.next())
-		for (const rom_source *source = rom_first_source(drivlist.config()); source != NULL; source = rom_next_source(*source))
+		for (const rom_source *source = rom_first_source(drivlist.config()); source != NULL; source = rom_next_source(*source)) 
+		{
+			bool isdiver = (source == rom_first_source(drivlist.config()));
 			for (const rom_entry *region = rom_first_region(*source); region; region = rom_next_region(region))
 				for (const rom_entry *rom = rom_first_file(region); rom; rom = rom_next_file(rom))
 				{
 					// if we have a CRC, display it
 					UINT32 crc;
 					if (hash_collection(ROM_GETHASHDATA(rom)).crc(crc))
-						mame_printf_info("%08x %-16s %s\n", crc, ROM_GETNAME(rom), drivlist.driver().description);
+						mame_printf_info("%08x %-16s %s\n", crc, ROM_GETNAME(rom), isdiver ? drivlist.driver().description : source->name());
 				}
+		}
 }
 
 
