ae37642f30a1ad132fe026928e7056f576539c1e
Michaël Banaan Ananas <happppp@users.noreply.github.com>
2011-08-19 17:21:33 +0000

    rm unneeded check preventing it from being read during device start, fixes MT bug 4396.
    m_frame_period default/init value is already DEFAULT_FRAME_PERIOD (through constructor) if the device isn't started yet

--- src/emu/screen.h
+++ src/emu/screen.h
@@ -139,7 +139,7 @@ public:
 	attotime time_until_vblank_end() const;
 	attotime time_until_update() const { return (machine().config().m_video_attributes & VIDEO_UPDATE_AFTER_VBLANK) ? time_until_vblank_end() : time_until_vblank_start(); }
 	attotime scan_period() const { return attotime(0, m_scantime); }
-	attotime frame_period() const { return (this == NULL || !started()) ? DEFAULT_FRAME_PERIOD : attotime(0, m_frame_period); };
+	attotime frame_period() const { return (this == NULL) ? DEFAULT_FRAME_PERIOD : attotime(0, m_frame_period); };
 	UINT64 frame_number() const { return m_frame_number; }
 	int partial_updates() const { return m_partial_updates_this_frame; }
 
