2e4b539bb75e3a09cdc827574aa3d7a6daa9ea1b
Brian Troha <briantro@users.noreply.github.com>
2011-09-09 20:59:04 +0000

    Missed a "dot" in the new Cave SH3 game name.

--- src/mame/drivers/cavesh3.c
+++ src/mame/drivers/cavesh3.c
@@ -206,7 +206,7 @@ GAME( 2004, mushisam,  0,          cavesh3,    cavesh3,  0, ROT0, "Cave", "Mushi
 GAME( 2004, mushisama, mushisam,   cavesh3,    cavesh3,  0, ROT0, "Cave", "Mushihime Sama (2004/10/12 MASTER VER)",                     GAME_NOT_WORKING | GAME_NO_SOUND )
 GAME( 2005, mushitam,  0,          cavesh3,    cavesh3,  0, ROT0, "Cave", "Mushihime Tama (2005/09/09 MASTER VER)",                     GAME_NOT_WORKING | GAME_NO_SOUND )
 GAME( 2005, espgal2,   0,          cavesh3,    cavesh3,  0, ROT0, "Cave", "EspGaluda II (2005/11/14 MASTER VER)",                       GAME_NOT_WORKING | GAME_NO_SOUND )
-GAME( 2006, futari15,  0,          cavesh3,    cavesh3,  0, ROT0, "Cave", "Mushihime Sama Futari Ver 1.5 (2006/12/8 MASTER VER. 1.54)", GAME_NOT_WORKING | GAME_NO_SOUND )
+GAME( 2006, futari15,  0,          cavesh3,    cavesh3,  0, ROT0, "Cave", "Mushihime Sama Futari Ver 1.5 (2006/12/8.MASTER VER. 1.54)", GAME_NOT_WORKING | GAME_NO_SOUND )
 GAME( 2006, futari15a, futari15,   cavesh3,    cavesh3,  0, ROT0, "Cave", "Mushihime Sama Futari Ver 1.5 (2006/12/8 MASTER VER 1.54)",  GAME_NOT_WORKING | GAME_NO_SOUND )
 GAME( 2006, futari10,  futari15,   cavesh3,    cavesh3,  0, ROT0, "Cave", "Mushihime Sama Futari Ver 1.0 (2006/10/23 MASTER VER.)",     GAME_NOT_WORKING | GAME_NO_SOUND )
 GAME( 2006, ibara,     0,          cavesh3,    cavesh3,  0, ROT0, "Cave", "Ibara (2005/03/22 MASTER VER..)",                            GAME_NOT_WORKING | GAME_NO_SOUND )
