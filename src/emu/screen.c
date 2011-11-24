e314aeded23be46d14f618aec48d9473fe580b9e
Nathan Woods <npwoods@mess.org>
2011-11-24 13:24:14 +0000

    Fixing bug in frameskip screen updates

--- src/emu/screen.c
+++ src/emu/screen.c
@@ -1076,9 +1076,8 @@ bool screen_device::screen_update(bitmap_t &bitmap, const rectangle &cliprect)
 	if (m_screen_update != NULL) {
 		return (*m_screen_update)(this, &bitmap, &cliprect);
 	} else {
-		machine().driver_data<driver_device>()->screen_update(*this, bitmap, cliprect);
+		return machine().driver_data<driver_device>()->screen_update(*this, bitmap, cliprect);
 	}
-	return 0;
 }
 
 //-------------------------------------------------
