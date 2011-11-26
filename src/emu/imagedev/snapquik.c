fdca5aa2e2a99c167cc0636f7e3904c05c4cb797
Miodrag Milanovic <mmicko@gmail.com>
2011-11-26 17:38:28 +0000

    Rewritten snapquik.c to c++ (no whatsnew)

--- src/emu/imagedev/snapquik.c
+++ src/emu/imagedev/snapquik.c
@@ -9,69 +9,43 @@
 #include "emu.h"
 #include "snapquik.h"
 
-/***************************************************************************
-    TYPE DEFINITIONS
-***************************************************************************/
+// device type definition
+const device_type SNAPSHOT = &device_creator<snapshot_image_device>;
 
-typedef struct _snapquick_token snapquick_token;
-struct _snapquick_token
+//-------------------------------------------------
+//  snapshot_image_device - constructor
+//-------------------------------------------------
+
+snapshot_image_device::snapshot_image_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock)
+    : device_t(mconfig, SNAPSHOT, "Snapshot", tag, owner, clock),
+	  device_image_interface(mconfig, *this)
 {
-	emu_timer *timer;
-	snapquick_load_func load;
-};
-
-
-
-/***************************************************************************
-    INLINE FUNCTIONS
-***************************************************************************/
-
-/*-------------------------------------------------
-    assert_is_snapshot_or_quickload - asserts/confirms
-    that a given device is a snapshot or quickload
--------------------------------------------------*/
-
-INLINE void assert_is_snapshot_or_quickload(device_t *device)
-{
-	assert(device != NULL);
-	assert(downcast<const legacy_device_base *>(device)->inline_config() != NULL);
-	assert((device->type() == SNAPSHOT) || (device->type() == QUICKLOAD));
 }
 
-
-
-/*-------------------------------------------------
-    get_token - safely gets the snapshot/quickload data
--------------------------------------------------*/
-
-INLINE snapquick_token *get_token(device_t *device)
+snapshot_image_device::snapshot_image_device(const machine_config &mconfig, device_type type, const char *name, const char *tag, device_t *owner, UINT32 clock) :
+        device_t(mconfig, type, name, tag, owner, clock),
+		device_image_interface(mconfig, *this)
 {
-	assert_is_snapshot_or_quickload(device);
-	return (snapquick_token *) downcast<legacy_device_base *>(device)->token();
+}
+//-------------------------------------------------
+//  snapshot_image_device - destructor
+//-------------------------------------------------
+
+snapshot_image_device::~snapshot_image_device()
+{
 }
 
+//-------------------------------------------------
+//  device_config_complete - perform any
+//  operations now that the configuration is
+//  complete
+//-------------------------------------------------
 
-
-/*-------------------------------------------------
-    get_config - safely gets the quickload config
--------------------------------------------------*/
-
-INLINE const snapquick_config *get_config(device_t *device)
+void snapshot_image_device::device_config_complete()
 {
-	assert_is_snapshot_or_quickload(device);
-	return (const snapquick_config *) downcast<const legacy_device_base *>(device)->inline_config();
+	// set brief and instance name
+	update_names();
 }
-
-INLINE const snapquick_config *get_config_dev(const device_t *device)
-{
-	assert(device != NULL);
-	assert((device->type() == SNAPSHOT) || (device->type() == QUICKLOAD));
-	return (const snapquick_config *) downcast<const legacy_device_base *>(device)->inline_config();
-}
-
-/***************************************************************************
-    IMPLEMENTATION
-***************************************************************************/
 
 /*-------------------------------------------------
     TIMER_CALLBACK(process_snapshot_or_quickload)
@@ -79,127 +53,47 @@ INLINE const snapquick_config *get_config_dev(const device_t *device)
 
 static TIMER_CALLBACK(process_snapshot_or_quickload)
 {
-	device_image_interface *image = (device_image_interface *) ptr;
-	snapquick_token *token = get_token(&image->device());
+	reinterpret_cast<snapshot_image_device *>(ptr)->timer_callback();
+}
 
+void snapshot_image_device::timer_callback()
+{
 	/* invoke the load */
-	(*token->load)(*image,
-		image->filetype(),
-		image->length());
+	(*m_load)(*this, filetype(), length());
 
 	/* unload the device */
-	image->unload();
+	unload();
 }
 
+//-------------------------------------------------
+//  device_start - device-specific startup
+//-------------------------------------------------
 
-
-/*-------------------------------------------------
-    DEVICE_START( snapquick )
--------------------------------------------------*/
-
-static DEVICE_START( snapquick )
+void snapshot_image_device::device_start()
 {
-	snapquick_token *token = get_token(device);
-
 	/* allocate a timer */
-	token->timer = device->machine().scheduler().timer_alloc(FUNC(process_snapshot_or_quickload), (void *) dynamic_cast<device_image_interface *>(device));
-
+	m_timer = machine().scheduler().timer_alloc(FUNC(process_snapshot_or_quickload), (void *)this);
 }
 
-
-
 /*-------------------------------------------------
-    DEVICE_IMAGE_LOAD( snapquick )
+    call_load
 -------------------------------------------------*/
-
-static DEVICE_IMAGE_LOAD( snapquick )
+bool snapshot_image_device::call_load()
 {
-	const snapquick_config *config = get_config(image);
-	snapquick_token *token = get_token(image);
-
-	/* locate the load function */
-	token->load = (snapquick_load_func) reinterpret_cast<snapquick_load_func>(image.get_device_specific_call());
-
 	/* adjust the timer */
-
-		token->timer->adjust(
-		attotime(config->delay_seconds, config->delay_attoseconds),
-		0);
-
+	m_timer->adjust(attotime(m_delay_seconds, m_delay_attoseconds),0);
 	return IMAGE_INIT_PASS;
 }
 
+// device type definition
+const device_type QUICKLOAD = &device_creator<quickload_image_device>;
 
+//-------------------------------------------------
+//  quickload_image_device - constructor
+//-------------------------------------------------
 
-/*-------------------------------------------------
-    DEVICE_GET_INFO(snapquick) - device getinfo
-    function
--------------------------------------------------*/
-
-static DEVICE_GET_INFO(snapquick)
+quickload_image_device::quickload_image_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock)
+    : snapshot_image_device(mconfig, QUICKLOAD, "Quickload", tag, owner, clock)
 {
-	switch(state)
-	{
-		/* --- the following bits of info are returned as 64-bit signed integers --- */
-		case DEVINFO_INT_TOKEN_BYTES:					info->i = sizeof(snapquick_token); break;
-		case DEVINFO_INT_INLINE_CONFIG_BYTES:			info->i = sizeof(snapquick_config); break;
-		case DEVINFO_INT_IMAGE_READABLE:				info->i = 1; break;
-		case DEVINFO_INT_IMAGE_WRITEABLE:				info->i = 0; break;
-		case DEVINFO_INT_IMAGE_CREATABLE:				info->i = 0; break;
-
-		/* --- the following bits of info are returned as pointers to functions --- */
-		case DEVINFO_FCT_START:							info->start = DEVICE_START_NAME(snapquick); break;
-		case DEVINFO_FCT_IMAGE_LOAD:					info->f = (genf *) DEVICE_IMAGE_LOAD_NAME(snapquick); break;
-		case DEVINFO_FCT_SNAPSHOT_QUICKLOAD_LOAD:		info->f = (genf *) get_config_dev(device)->load; break;
-
-		/* --- the following bits of info are returned as NULL-terminated strings --- */
-		case DEVINFO_STR_SOURCE_FILE:					strcpy(info->s, __FILE__); break;
-		case DEVINFO_STR_IMAGE_FILE_EXTENSIONS:			strcpy(info->s, get_config_dev(device)->file_extensions); break;
-	}
 }
 
-
-
-/*-------------------------------------------------
-    DEVICE_GET_INFO(snapshot) - device getinfo
-    function
--------------------------------------------------*/
-
-DEVICE_GET_INFO(snapshot)
-{
-	switch(state)
-	{
-		/* --- the following bits of info are returned as 64-bit signed integers --- */
-		case DEVINFO_INT_IMAGE_TYPE:					info->i = IO_SNAPSHOT; break;
-
-		/* --- the following bits of info are returned as NULL-terminated strings --- */
-		case DEVINFO_STR_NAME:							strcpy(info->s, "Snapshot"); break;
-		case DEVINFO_STR_FAMILY:						strcpy(info->s, "Snapshot"); break;
-
-		default: DEVICE_GET_INFO_CALL(snapquick); break;
-	}
-}
-
-
-
-/*-------------------------------------------------
-    DEVICE_GET_INFO(quickload) - device getinfo
-    function
--------------------------------------------------*/
-
-DEVICE_GET_INFO(quickload)
-{
-	switch(state)
-	{
-		/* --- the following bits of info are returned as 64-bit signed integers --- */
-		case DEVINFO_INT_IMAGE_TYPE:					info->i = IO_QUICKLOAD; break;
-
-		/* --- the following bits of info are returned as NULL-terminated strings --- */
-		case DEVINFO_STR_NAME:							strcpy(info->s, "Quickload"); break;
-		case DEVINFO_STR_FAMILY:						strcpy(info->s, "Quickload"); break;
-
-		default: DEVICE_GET_INFO_CALL(snapquick); break;
-	}
-}
-DEFINE_LEGACY_IMAGE_DEVICE(SNAPSHOT, snapshot);
-DEFINE_LEGACY_IMAGE_DEVICE(QUICKLOAD, quickload);
--- src/emu/imagedev/snapquik.h
+++ src/emu/imagedev/snapquik.h
@@ -9,67 +9,78 @@
 #ifndef __SNAPQUIK_H__
 #define __SNAPQUIK_H__
 
-#include "image.h"
+typedef int (*snapquick_load_func)(device_image_interface &image, const char *file_type, int file_size);
 
-/***************************************************************************
-    CONSTANTS
-***************************************************************************/
-
-enum
+// ======================> snapshot_image_device
+class snapshot_image_device :	public device_t,
+								public device_image_interface
 {
-	DEVINFO_FCT_SNAPSHOT_QUICKLOAD_LOAD = DEVINFO_FCT_DEVICE_SPECIFIC
+public:
+	// construction/destruction
+	snapshot_image_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+	snapshot_image_device(const machine_config &mconfig, device_type type, const char *name, const char *tag, device_t *owner, UINT32 clock);
+	virtual ~snapshot_image_device();
+
+	// image-level overrides
+	virtual bool call_load();	
+	virtual iodevice_t image_type() const { return IO_SNAPSHOT; }
+
+	virtual bool is_readable()  const { return 1; }
+	virtual bool is_writeable() const { return 0; }
+	virtual bool is_creatable() const { return 0; }
+	virtual bool must_be_loaded() const { return 0; }
+	virtual bool is_reset_on_load() const { return 0; }
+	virtual const char *image_interface() const { return NULL; }
+	virtual const char *file_extensions() const { return m_file_extensions; }
+	virtual const option_guide *create_option_guide() const { return NULL; }
+	
+	void timer_callback();
+	void set_handler(snapquick_load_func load, const char *ext, seconds_t sec) { m_load = load; m_file_extensions = ext; m_delay_seconds = sec; };
+protected:
+	// device-level overrides
+    virtual void device_config_complete();
+	virtual void device_start();
+
+	snapquick_load_func	m_load;					/* loading function */
+	const char *		m_file_extensions;		/* file extensions */
+	seconds_t			m_delay_seconds;		/* loading delay (seconds) */
+	attoseconds_t		m_delay_attoseconds;	/* loading delay (attoseconds) */	
+	emu_timer 			*m_timer;
 };
 
+// device type definition
+extern const device_type SNAPSHOT;
 
+// ======================> quickload_image_device
+
+class quickload_image_device : public snapshot_image_device
+{
+public:
+	// construction/destruction
+	quickload_image_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+	
+	virtual iodevice_t image_type() const { return IO_QUICKLOAD; }
+};
+
+// device type definition
+extern const device_type QUICKLOAD;
 
 /***************************************************************************
-    MACROS
+    DEVICE CONFIGURATION MACROS
 ***************************************************************************/
-
-DECLARE_LEGACY_IMAGE_DEVICE(SNAPSHOT, snapshot);
-DECLARE_LEGACY_IMAGE_DEVICE(QUICKLOAD, quickload);
-
 #define SNAPSHOT_LOAD_NAME(name)	snapshot_load_##name
 #define SNAPSHOT_LOAD(name)			int SNAPSHOT_LOAD_NAME(name)(device_image_interface &image, const char *file_type, int snapshot_size)
 
 #define QUICKLOAD_LOAD_NAME(name)	quickload_load_##name
 #define QUICKLOAD_LOAD(name)		int QUICKLOAD_LOAD_NAME(name)(device_image_interface &image, const char *file_type, int quickload_size)
 
-/***************************************************************************
-    TYPE DEFINITIONS
-***************************************************************************/
 
-typedef int (*snapquick_load_func)(device_image_interface &image, const char *file_type, int file_size);
-
-typedef struct _snapquick_config snapquick_config;
-struct _snapquick_config
-{
-	snapquick_load_func	load;				/* loading function */
-	const char *		file_extensions;	/* file extensions */
-	seconds_t			delay_seconds;		/* loading delay (seconds) */
-	attoseconds_t		delay_attoseconds;	/* loading delay (attoseconds) */
-};
-
-/***************************************************************************
-    SNAPSHOT DEVICE CONFIGURATION MACROS
-***************************************************************************/
-
-#define MCFG_SNAPSHOT_ADD(_tag, _load, _file_extensions, _delay)	\
+#define MCFG_SNAPSHOT_ADD(_tag, _load, _file_extensions, _delay) \
 	MCFG_DEVICE_ADD(_tag, SNAPSHOT, 0) \
-	MCFG_DEVICE_CONFIG_DATAPTR(snapquick_config, load, SNAPSHOT_LOAD_NAME(_load))	\
-	MCFG_DEVICE_CONFIG_DATAPTR(snapquick_config, file_extensions, _file_extensions) \
-	MCFG_DEVICE_CONFIG_DATA64(snapquick_config, delay_seconds, (seconds_t) (_delay)) \
-	MCFG_DEVICE_CONFIG_DATA64(snapquick_config, delay_attoseconds, (attoseconds_t) (((_delay) - (int)(_delay)) * ATTOSECONDS_PER_SECOND)) \
-
-/***************************************************************************
-    QUICKLOAD DEVICE CONFIGURATION MACROS
-***************************************************************************/
+	static_cast<snapshot_image_device *>(device)->set_handler(SNAPSHOT_LOAD_NAME(_load), _file_extensions, _delay);
 
 #define MCFG_QUICKLOAD_ADD(_tag, _load, _file_extensions, _delay)	\
 	MCFG_DEVICE_ADD(_tag, QUICKLOAD, 0) \
-	MCFG_DEVICE_CONFIG_DATAPTR(snapquick_config, load, QUICKLOAD_LOAD_NAME(_load))	\
-	MCFG_DEVICE_CONFIG_DATAPTR(snapquick_config, file_extensions, _file_extensions) \
-	MCFG_DEVICE_CONFIG_DATA64(snapquick_config, delay_seconds, (seconds_t) (_delay)) \
-	MCFG_DEVICE_CONFIG_DATA64(snapquick_config, delay_attoseconds, (attoseconds_t) (((_delay) - (int)(_delay)) * ATTOSECONDS_PER_SECOND)) \
+	static_cast<quickload_image_device *>(device)->set_handler(QUICKLOAD_LOAD_NAME(_load), _file_extensions, _delay);
 
 #endif /* __SNAPQUIK_H__ */
