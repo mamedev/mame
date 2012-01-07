75231168628ceb2c34ec244479b5412de26d592c
Olivier Galibert <galibert@pobox.com>
2012-01-07 21:40:05 +0000

    mess sync (nw)

--- src/emu/machine/amigafdc.c
+++ src/emu/machine/amigafdc.c
@@ -11,13 +11,14 @@
 #include "formats/hxcmfm_dsk.h"
 #include "formats/ipf_dsk.h"
 #include "formats/mfi_dsk.h"
+#include "formats/dfi_dsk.h"
 #include "amigafdc.h"
 #include "machine/6526cia.h"
 
 const device_type AMIGA_FDC = &device_creator<amiga_fdc>;
 
 const floppy_format_type amiga_fdc::floppy_formats[] = {
-	FLOPPY_ADF_FORMAT, FLOPPY_MFM_FORMAT, FLOPPY_IPF_FORMAT, FLOPPY_MFI_FORMAT,
+	FLOPPY_ADF_FORMAT, FLOPPY_MFM_FORMAT, FLOPPY_IPF_FORMAT, FLOPPY_MFI_FORMAT, FLOPPY_DFI_FORMAT,
 	NULL
 };
 
