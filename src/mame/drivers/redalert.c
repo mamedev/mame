9d7bdff58d8cdbe99f0f03b02c0bef3bf5d31038
Michaël Banaan Ananas <happppp@users.noreply.github.com>
2012-01-09 22:45:13 +0000

    ww3 is a clone of redalert

--- src/mame/drivers/redalert.c
+++ src/mame/drivers/redalert.c
@@ -1,6 +1,6 @@
 /***************************************************************************
 
-    Irem Red Alert hardware
+    redalert.c, Irem M27 hardware
 
     If you have any questions about how this driver works, don't hesitate to
     ask.  - Mike Balfour (mab22@po.cwru.edu)
@@ -536,7 +536,7 @@ ROM_END
  *
  *************************************/
 
-GAME( 1981, panther,  0, panther,  panther,  0, ROT270, "Irem",       "Panther",    GAME_NO_SOUND | GAME_SUPPORTS_SAVE )
-GAME( 1981, redalert, 0, redalert, redalert, 0, ROT270, "Irem / GDI", "Red Alert",  GAME_IMPERFECT_SOUND | GAME_SUPPORTS_SAVE )
-GAME( 1981, ww3,      0, ww3,      redalert, 0, ROT270, "Irem",       "WW III",     GAME_IMPERFECT_SOUND | GAME_SUPPORTS_SAVE )
-GAME( 1981, demoneye, 0, demoneye, demoneye, 0, ROT270, "Irem",       "Demoneye-X", GAME_NOT_WORKING | GAME_IMPERFECT_GRAPHICS | GAME_IMPERFECT_SOUND | GAME_SUPPORTS_SAVE )
+GAME( 1981, panther,  0,        panther,  panther,  0, ROT270, "Irem",               "Panther",    GAME_NO_SOUND | GAME_SUPPORTS_SAVE )
+GAME( 1981, redalert, 0,        redalert, redalert, 0, ROT270, "Irem (GDI license)", "Red Alert",  GAME_IMPERFECT_SOUND | GAME_SUPPORTS_SAVE )
+GAME( 1981, ww3,      redalert, ww3,      redalert, 0, ROT270, "Irem",               "WW III",     GAME_IMPERFECT_SOUND | GAME_SUPPORTS_SAVE )
+GAME( 1981, demoneye, 0,        demoneye, demoneye, 0, ROT270, "Irem",               "Demoneye-X", GAME_NOT_WORKING | GAME_IMPERFECT_GRAPHICS | GAME_IMPERFECT_SOUND | GAME_SUPPORTS_SAVE )
