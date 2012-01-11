2294b2d7d15e8eb694ae97fde6d01334a06b7b64
Miodrag Milanovic <mmicko@gmail.com>
2012-01-11 13:20:34 +0000

    Fix for cassette extension extraction (no whatsnew)

--- src/emu/imagedev/cassette.c
+++ src/emu/imagedev/cassette.c
@@ -270,10 +270,10 @@ bool cassette_image_device::call_load()
 		{
 			is_writable = !is_readonly();
 			cassette_flags = is_writable ? (CASSETTE_FLAG_READWRITE|CASSETTE_FLAG_SAVEONEXIT) : CASSETTE_FLAG_READONLY;
+			astring fname = astring(m_mame_file->filename());
 			if (software_entry()==NULL) {
 				extension = filetype();
 			} else {
-				astring fname = astring(m_mame_file->filename());
 				int loc = fname.rchr(0,'.');
 				if (loc!=-1) {
 					extension = fname.substr(loc + 1,fname.len()-loc).cstr();
