749b1fa9e714003f8631e424087c8fc50b7a6d8f
R. Belmont <rb6502@users.noreply.github.com>
2012-01-11 03:49:08 +0000

    and 2 more (nw)

---
+++ src/emu/cpu/arm7/arm7thmb.h
@@ -0,0 +1,20 @@
+
+
+
+const void thumbblock00(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock01(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock02(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock03(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock04(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock05(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock06(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock07(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock08(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock09(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock0a(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock0b(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock0c(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock0d(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock0e(arm_state *cpustate, UINT32 pc, UINT32 insn);
+const void thumbblock0f(arm_state *cpustate, UINT32 pc, UINT32 insn);
+
