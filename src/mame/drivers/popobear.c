a5c3667e4649977f5ea47d9cb7212b6ecc076009
Angelo Salese <angelosa@users.noreply.github.com>
2012-01-18 20:43:32 +0000

    Another update, this is blitter based

--- src/mame/drivers/popobear.c
+++ src/mame/drivers/popobear.c
@@ -1,5 +1,7 @@
 /* Popo Bear */
 
+// looks like some kind of blitter
+// IGS-like?
 
 #include "emu.h"
 #include "cpu/m68000/m68000.h"
@@ -19,21 +21,64 @@ public:
 	required_device<cpu_device> m_maincpu;
 };
 
-READ16_HANDLER( unk_r )
-{
-	static int i = 0x00;
-	i ^=0xff;
-	return i;
-}
+/* stub read handlers */
+
+static READ16_HANDLER( popo_480001_r ) { static int i = 0x00;i ^=0xff;return i;}
+static READ16_HANDLER( popo_48001c_r ) { static int i = 0x00;i ^=0xff;return i;}
+static READ16_HANDLER( popo_480020_r ) { static int i = 0x00;i ^=0xff;return i;}
+static READ16_HANDLER( popo_48003a_r ) { static int i = 0x00;i ^=0xff;return i;}
+
+static READ16_HANDLER( popo_500000_r ) { static int i = 0x00;i ^=0xff;return i;}
+static READ16_HANDLER( popo_520000_r ) { static int i = 0x00;i ^=0xff;return i;}
+
+static READ16_HANDLER( popo_620000_r ) { static int i = 0x00;i ^=0xff;return i;}
+
+/* stub write handlers */
+
+static WRITE16_HANDLER( popo_480001_w ) { }
+static WRITE16_HANDLER( popo_480018_w ) { }
+static WRITE16_HANDLER( popo_48001a_w ) { }
+static WRITE16_HANDLER( popo_48001c_w ) { }
+static WRITE16_HANDLER( popo_480020_w ) { }
+static WRITE16_HANDLER( popo_480028_w ) { }
+static WRITE16_HANDLER( popo_48002c_w ) { }
+static WRITE16_HANDLER( popo_480030_w ) { }
+static WRITE16_HANDLER( popo_48003a_w ) { }
+
+static WRITE16_HANDLER( popo_550000_w ) { }
+static WRITE16_HANDLER( popo_550002_w ) { }
+
+static WRITE16_HANDLER( popo_600000_w ) { }
+static WRITE16_HANDLER( popo_620000_w ) { }
+
 
 static ADDRESS_MAP_START( popobear_mem, AS_PROGRAM, 16 )
 	AM_RANGE(0x000000, 0x03ffff) AM_ROM
 	AM_RANGE(0x210000, 0x21ffff) AM_RAM
 	AM_RANGE(0x3E0000, 0x3EFFFF) AM_RAM  AM_BASE_MEMBER(popobear_state, popobear_vram) 
 
-	
-	AM_RANGE(0x500000, 0x500001) AM_READ(unk_r)
+	/* Blitter stuff? */
+	AM_RANGE(0x480000, 0x480001) AM_READ(popo_480001_r) AM_WRITE(popo_480001_w)
+	AM_RANGE(0x480018, 0x480019) AM_WRITE(popo_480018_w)
+	AM_RANGE(0x48001a, 0x48001b) AM_WRITE(popo_48001a_w)
+	AM_RANGE(0x48001c, 0x48001d) AM_READ(popo_48001c_r) AM_WRITE(popo_48001c_w)
+	AM_RANGE(0x480020, 0x480021) AM_READ(popo_480020_r) AM_WRITE(popo_480020_w)
+	AM_RANGE(0x480028, 0x480029) AM_WRITE(popo_480028_w)
+	AM_RANGE(0x48002c, 0x48002d) AM_WRITE(popo_48002c_w)
+	AM_RANGE(0x480030, 0x480031) AM_WRITE(popo_480030_w)
+	AM_RANGE(0x48003a, 0x48003b) AM_READ(popo_48003a_r) AM_WRITE(popo_48003a_w)
 
+	AM_RANGE(0x480400, 0x48041f) AM_RAM AM_WRITE(paletteram16_xRRRRRGGGGGBBBBB_word_w) AM_BASE_GENERIC(paletteram) // looks palette like at least
+	
+	/* I/O maybe? */
+	AM_RANGE(0x500000, 0x500001) AM_READ(popo_500000_r)
+	AM_RANGE(0x520000, 0x520001) AM_READ(popo_520000_r)
+	AM_RANGE(0x550000, 0x550001) AM_WRITE(popo_550000_w)
+	AM_RANGE(0x550002, 0x550003) AM_WRITE(popo_550002_w)
+
+	/* these could be where the OKI hooks up? */
+	AM_RANGE(0x600000, 0x600001) AM_WRITE(popo_600000_w)
+	AM_RANGE(0x620000, 0x620001) AM_READ(popo_620000_r) AM_WRITE(popo_620000_w)
 ADDRESS_MAP_END
 
 static INPUT_PORTS_START( popobear )
@@ -51,8 +96,10 @@ static const gfx_layout tilelayout =
 	8*32
 };
 
+// gfx don't seem to be tiles..
 static GFXDECODE_START( popobear )
 	GFXDECODE_ENTRY( "gfx1", 0, tilelayout,  0, 1 )
+	GFXDECODE_ENTRY( "gfx2", 0, tilelayout,  0, 1 )
 GFXDECODE_END
 
 
@@ -92,11 +139,27 @@ VIDEO_START(popobear)
 
 }
 
+static TIMER_DEVICE_CALLBACK( popobear_irq )
+{
+	popobear_state *state = timer.machine().driver_data<popobear_state>();
+	int scanline = param;
+
+	if(scanline == 240)
+		device_set_input_line(state->m_maincpu, 2, HOLD_LINE);
+
+	if(scanline == 128)
+		device_set_input_line(state->m_maincpu, 3, HOLD_LINE);
+
+	if(scanline == 32)
+		device_set_input_line(state->m_maincpu, 5, HOLD_LINE);
+}
+
 static MACHINE_CONFIG_START( popobear, popobear_state )
 	MCFG_CPU_ADD("maincpu", M68000, 10000000 )
 	MCFG_CPU_PROGRAM_MAP(popobear_mem) 
 	// levels 2,3,5 look interesting
-	MCFG_CPU_VBLANK_INT("screen",irq2_line_hold)
+	//MCFG_CPU_VBLANK_INT("screen",irq3_line_hold)
+	MCFG_TIMER_ADD_SCANLINE("scantimer", popobear_irq, "screen", 0, 1)
 
 	MCFG_SCREEN_ADD("screen", RASTER)
 	MCFG_SCREEN_REFRESH_RATE(60)
@@ -124,11 +187,13 @@ ROM_START( popobear )
 	ROM_LOAD16_BYTE( "popobear_en-a-301_1.6.u3", 0x000001, 0x20000, CRC(b934adf6) SHA1(93431c7a19af812b549aad35cc1176a81805ffab) )
 	ROM_LOAD16_BYTE( "popobear_en-a-401_1.6.u4", 0x000000, 0x20000, CRC(0568af9c) SHA1(920531dbc4bbde2d1db062bd5c48b97dd50b7185) )
 
-	ROM_REGION( 0x400000, "gfx1", 0 ) /* tiles */
+	ROM_REGION( 0x200000, "gfx1", 0 )
 	ROM_LOAD16_BYTE(	"popobear_en-a-501.u5", 0x000000, 0x100000, CRC(185901a9) SHA1(7ff82b5751645df53435eaa66edce589684cc5c7) )
 	ROM_LOAD16_BYTE(	"popobear_en-a-601.u6", 0x000001, 0x100000, CRC(84fa9f3f) SHA1(34dd7873f88b0dae5fb81fe84e82d2b6b49f7332) )
-	ROM_LOAD16_BYTE(	"popobear_en-a-701.u7", 0x200000, 0x100000, CRC(45eba6d0) SHA1(0278602ed57ac45040619d590e6cc85e2cfeed31) )
-	ROM_LOAD16_BYTE(	"popobear_en-a-801.u8", 0x200001, 0x100000, CRC(2760f2e6) SHA1(58af59f486c9df930f7c124f89154f8f389a5bd7) )
+
+	ROM_REGION( 0x200000, "gfx2", 0 )
+	ROM_LOAD16_BYTE(	"popobear_en-a-701.u7", 0x000000, 0x100000, CRC(45eba6d0) SHA1(0278602ed57ac45040619d590e6cc85e2cfeed31) )
+	ROM_LOAD16_BYTE(	"popobear_en-a-801.u8", 0x000001, 0x100000, CRC(2760f2e6) SHA1(58af59f486c9df930f7c124f89154f8f389a5bd7) )
 
 	ROM_REGION( 0x040000, "oki", 0 ) /* Samples */
 	ROM_LOAD( "popobear_ta-a-901.u9", 0x00000, 0x40000,  CRC(f1e94926) SHA1(f4d6f5b5811d90d0069f6efbb44d725ff0d07e1c) )
