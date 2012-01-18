99ec1a86c8eac05b5b1482b5f8abeb31dc9cabb2
Angelo Salese <angelosa@users.noreply.github.com>
2012-01-18 19:40:22 +0000

    Updates from Haze

--- src/mame/drivers/popobear.c
+++ src/mame/drivers/popobear.c
@@ -14,13 +14,26 @@ public:
 		m_maincpu(*this,"maincpu")
 		{ }
 
+
+	UINT16 *popobear_vram;
 	required_device<cpu_device> m_maincpu;
 };
 
-
+READ16_HANDLER( unk_r )
+{
+	static int i = 0x00;
+	i ^=0xff;
+	return i;
+}
 
 static ADDRESS_MAP_START( popobear_mem, AS_PROGRAM, 16 )
 	AM_RANGE(0x000000, 0x03ffff) AM_ROM
+	AM_RANGE(0x210000, 0x21ffff) AM_RAM
+	AM_RANGE(0x3E0000, 0x3EFFFF) AM_RAM  AM_BASE_MEMBER(popobear_state, popobear_vram) 
+
+	
+	AM_RANGE(0x500000, 0x500001) AM_READ(unk_r)
+
 ADDRESS_MAP_END
 
 static INPUT_PORTS_START( popobear )
@@ -29,11 +42,11 @@ INPUT_PORTS_END
 
 static const gfx_layout tilelayout =
 {
-	8,8,
-	RGN_FRAC(1,2),
+	4,8,
+	RGN_FRAC(1,1),
 	8,
-	{0,1,2,3, RGN_FRAC(1,2)+0,RGN_FRAC(1,2)+1,RGN_FRAC(1,2)+2,RGN_FRAC(1,2)+3 },
-	{ 0, 4, 8, 12, 16,20,  24, 28 },
+	{ 0,1,2,3,4,5,6,7 },
+	{ 24, 16, 8, 0 },
 	{ 0*32, 1*32, 2*32, 3*32, 4*32, 5*32, 6*32, 7*32 },
 	8*32
 };
@@ -45,6 +58,32 @@ GFXDECODE_END
 
 SCREEN_UPDATE_IND16( popobear )
 {
+	popobear_state *state = screen.machine().driver_data<popobear_state>();
+	bitmap.fill(0, cliprect);
+
+	UINT16* popobear_vram = state->popobear_vram;
+	int count = 0;
+	for (int y=0;y<256;y++)
+	{
+		for (int x=0;x<128;x++)
+		{
+			UINT8 dat;
+			dat = (popobear_vram[count]&0xf000)>>12;
+			bitmap.pix16(y, (x*4)+0) =dat;
+
+			dat = (popobear_vram[count]&0x0f00)>>8;
+			bitmap.pix16(y, (x*4)+1) =dat;
+
+			dat = (popobear_vram[count]&0x00f0)>>4;
+			bitmap.pix16(y, (x*4)+2) =dat;
+
+			dat = (popobear_vram[count]&0x000f)>>0;
+			bitmap.pix16(y, (x*4)+3) =dat;
+
+			count++;
+		}
+	}
+
 	return 0;
 }
 
@@ -55,8 +94,10 @@ VIDEO_START(popobear)
 
 static MACHINE_CONFIG_START( popobear, popobear_state )
 	MCFG_CPU_ADD("maincpu", M68000, 10000000 )
-	MCFG_CPU_PROGRAM_MAP(popobear_mem)
-	
+	MCFG_CPU_PROGRAM_MAP(popobear_mem) 
+	// levels 2,3,5 look interesting
+	MCFG_CPU_VBLANK_INT("screen",irq2_line_hold)
+
 	MCFG_SCREEN_ADD("screen", RASTER)
 	MCFG_SCREEN_REFRESH_RATE(60)
 	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
@@ -65,7 +106,7 @@ static MACHINE_CONFIG_START( popobear, popobear_state )
 	MCFG_GFXDECODE(popobear)
 
 	MCFG_SCREEN_SIZE(64*8, 32*8)
-	MCFG_SCREEN_VISIBLE_AREA(0*8, 64*8-1, 0*8, 30*8-1)
+	MCFG_SCREEN_VISIBLE_AREA(0*8, 64*8-1, 0*8, 32*8-1)
 	MCFG_PALETTE_LENGTH(256)
 
 	MCFG_VIDEO_START(popobear)
@@ -93,4 +134,4 @@ ROM_START( popobear )
 	ROM_LOAD( "popobear_ta-a-901.u9", 0x00000, 0x40000,  CRC(f1e94926) SHA1(f4d6f5b5811d90d0069f6efbb44d725ff0d07e1c) )
 ROM_END
 
-GAME( 199?, popobear,    0, popobear,    popobear,    0, ROT0,  "BMC", "PoPo Bear", GAME_NOT_WORKING | GAME_IS_SKELETON )
+GAME( 2000, popobear,    0, popobear,    popobear,    0, ROT0,  "BMC", "PoPo Bear", GAME_NOT_WORKING | GAME_IS_SKELETON )
