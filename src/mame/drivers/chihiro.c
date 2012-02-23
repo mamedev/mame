4f311f5db77fe8acb516f5d9a5785aa80b4a859b
Miodrag Milanovic <mmicko@gmail.com>
2012-02-23 14:42:22 +0000

    Small fix (nw)

--- src/mame/drivers/chihiro.c
+++ src/mame/drivers/chihiro.c
@@ -838,6 +838,7 @@ protected:
     // device-level overrides
     virtual void device_start();
 	virtual void device_reset();
+	virtual void device_config_complete() { m_shortname = "ide_baseboard"; }
 };
 
 //**************************************************************************
