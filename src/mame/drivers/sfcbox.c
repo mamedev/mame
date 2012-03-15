8020fe95064792624232cddfdf70bb5fb9a6c742
Scott Stone <tafoid@users.noreply.github.com>
2012-03-15 21:35:41 +0000

    sfcbox.c: - Extensive documentation improvements and clarification of the Super Famicom Box hardware [Y~K] - Moved the attraction ROM to its real set, and added the real BIOS ROM [Y~K]

--- src/mame/drivers/sfcbox.c
+++ src/mame/drivers/sfcbox.c
@@ -4,55 +4,112 @@
   Preliminary driver file to handle emulation of the
   Nintendo Super Famicom Box.
 
-The Super Famicom Box was used in hotels in Japan, with it's installed Coin
+The Super Famicom Box was used in hotels in Japan, with its installed Coin
 Box, makes it a pay-for-play system.  It accepted 100 yen coins and gave 5
 minutes of playtime.  The interesting part of this system is that it used
 special multicarts that contained two or three games as part of a larger Cart,
 for which the system holds two at once.
 
+The "To Do" list:
+-----------------
+-Consider moving the 3 cartridges of the slot 2 in a sowtware list since they are interchangable
+-Hook the z180 clone, the DSP 1A/1B and the Super FX
+-Add the missing GROM4-1
+-Add the possibly alternate revision of the attract ROM, with Kirby holding a coin
+ (unless it is unlocked with some DIP switch)
 
 Specific Model Number information
 ---------------------------------
 
 Hardware:
+                                    _
+                                   | |--------------------------------------| GS 0871-102
+                                   | |--------------------------------------| GS 0871-102
+                       GD 0871-103 ||
+                                   ||   |-----------------------------------| PU 0871-101
+                                   ||     |_|
+ |---------------------------------||-----|_|-------------------------------| MAIN 0871-100A
+    __________________________________________
+   |------------------------------------------|    GS 0871-102
+   |------------------------------------------|    GS 0871-102
+  ___|__________________________            |
+ |------------------------------|           |      PU 0871-101
+ _|______________________|__________________|____
+|------------------------------------------------| MAIN 0871-100A
+
 
 PSS-001 - SUPER FAMICOM BOX - Main Unit
+This unit contains the three boards:
+the main board, the BIOS board and a passive board.
 
+1. The main board (MAIN 0871-100A) contains the Super Famicom core hardware as follow:
 CPU: S-CPU B (5A22-02 3MB 83)
-Video Controller: S-PPU1 (5C77-01 4AU 9J)
+Video Controller: S-PPU1 (5C77-01 4AU 9L)
 Video Controller: S-PPU2 C (5C78-03 3MB 9V)
-Working CPU RAM: S-WRAM A (9441 T90F)
-Sound CPU: S-SMP (SONY Nintendo'89)
-Sound DSP: S-DSP A (SONY'89 348AC8VZ)
+Working CPU RAM: S-WRAM A (9442 T94 F)
+Sound CPU: S-SMP (SONY Nintendo'89 JAPAN 3WK4V)
+Sound DSP: S-DSP A (SONY'89 347AB7VZ)
 D/A converter: NEC D6376 - PDIF output model can be modified
-RTC: S3520CF2 (Seiko)
-SRAM: SRM20257LM12 (S-MOS Systems) - SRAM accounting and control circuits are
+Slave (?) CPU: Hitachi 3M3R1 HD64180RF6X (Z180 clone)
+S-ENC A (9504 BA)
+MB90082 001 (9351 M02)
+
+2. The BIOS board (PU 0871-101) must be inserted into the main board.
+BIOS ROM: KROM 1, 512Kibit
+SRAM: SRM20257LM12 F27K 256 (S-MOS Systems) - SRAM accounting and control circuits are
       self-diagnostic features that set time and operational status of the
       game(s) that are installed.
+Battery: C2032
+Decoders: 74HC139A, 74HC237D, 74LS641, RTC S-3520CF (Seiko S3520CF2 C4446 J18)
+          and two other unreadable chips
+MB3790 (9413 M32)
 
-PSS-002 - SUPER FAMICOM BOX CASSETTE ?
+3. The passive board (GD 0871-103) to be inserted into the main board, has two cartridge slots.
+
+PSS-002 - SUPER FAMICOM BOX - Cartridge
 
 PSS-003 - SUPER FAMICOM BOX - Coin Box
 
 Software/Cartridge:
+The PSS-61 cartridge is required on the slot 1 for the machine to operate.
+The slot 2 may be free or contain PSS-62, PSS-63 or PSS-64 interchangably.
 
 PSS-61  - SUPER FAMICOM BOX Commercial Regular Cart
-This cartridge is required for the machine to operate
-- Star Fox (Nintendo)
-- Super Mario Kart (Nintendo)
-- Super Mario Collection (Nintendo)
+WARNING: This cartridge is required for the machine to operate.
+This game board (GS 0871-102) contains various chips in addition to the game ROMs:
+Attraction ROM: ATROM-4S-0, 4Mibit (is called Slave ProgramROM in BIOS menu ?)
+                Menu to select one of the 3 games (or 5, if a cartridge is inserted in slot 2)
+GameData ROM: GROM1-1, 256Kibit, most likely contains the graphics used by the attract menu
+              May also contain the extra text layers added to the game graphics
+Upper ROM: Super Mario Kart ROM (Nintendo), SHVC-MK-0, 4Mibit
+Upper ROM: Super Mario Collection ROM (Nintendo), SHVC-4M-1, 16Mibit
+Upper ROM: Star Fox ROM (Nintendo), SHVC-FO-1, 8Mibit
+DSP 1 B coprocessor: (DSP 1 A in earlier SFBOX units) it is needed to operate Super Mario Kart
+MARIO CHIP 1 coprocessor: Also known as "Super FX", it is needed to operate Star Fox
+Static RAM: 1Mibit
+Static RAM: 256 Kibit
 
 PSS-62  - SUPER FAMICOM BOX Commercial Optional Cart
-- New Super 3D Golf Simulation - Waialae No Kiseki (Waialae Golf) (T&E SOFT)
-- Super Mahjong 2 (I'MAX)
+GameData ROM: GROM2-1, 256Kibit
+Lower ROM: New Super 3D Golf Simulation - Waialae No Kiseki (Waialae Golf) (T&E SOFT), SHVC-GC-0, 4Mibit
+Lower ROM: Super Mahjong 2 (I'MAX), SHVC-2A-1, 8Mibit
 
 PSS-63  - SUPER FAMICOM BOX Commercial Optional Cart
-- Super Tetris 2 + Bombliss (BPS)
-- Super Donkey Kong (Nintendo)
+GameData ROM: GROM3-1, 256Kibit
+Lower ROM: Super Donkey Kong (Nintendo), SHVC-8X-1, 32Mibit
+Lower ROM: Super Tetris 2 + Bombliss (BPS), SHVC-T2-1, 8Mibit
 
 PSS-64  - SUPER FAMICOM BOX Commercial Optional Cart
-- Super Donkey Kong (Nintendo)
-- Super Bomberman 2 (Hudson Soft)
+GameData ROM: GROM4-1, undumped
+Lower ROM: Super Donkey Kong (Nintendo), SHVC-8X-1, 32Mibit
+Lower ROM: Super Bomberman 2 (Hudson Soft), SHVC-M4-0, 8Mibit
+
+How does the Super Famicom Box operates
+---------------------------------------
+-Operate with a key, goes into BIOS, does automated checks, PSS-61 must be inserted for system to operate.
+-Goes into the attraction ROM, there is a graphical menu to select a game.
+ Apparently the menu graphics are imported from the GROMs.
+-Goes into selected game, can go back to attraction ROM menu.
 
 ***************************************************************************/
 
@@ -250,11 +307,13 @@ ROM_START( sfcbox )
 	ROM_REGION( 0x1000000, "maincpu", ROMREGION_ERASE00 )
 
 //  ROM_REGION( 0x80000, "atrom", 0 )
-	ROM_REGION( 0x100000, "user3", 0 )
-	ROM_LOAD( "atrom-4s-0.rom5", 0x00000, 0x80000, CRC(ad3ec05c) SHA1(a3d336db585fe02a37c323422d9db6a33fd489a6) )
+//	ROM_REGION( 0x10000, "user3", 0 )
 
 	ROM_REGION( 0x100, "user5", 0 )		/* IPL ROM */
 	ROM_LOAD( "spc700.rom", 0x00, 0x40, CRC(44bb3a40) SHA1(97e352553e94242ae823547cd853eecda55c20f0) )	/* boot rom */
+
+	ROM_REGION( 0x10000, "krom", 0 )
+	ROM_LOAD( "krom1.ic1", 0x00000, 0x10000, CRC(c9010002) SHA1(f4c74086a83b728b1c1af3a021a60efa80eff5a4) )
 
 //  ROM_REGION( 0x1000, "addons", ROMREGION_ERASE00 )       /* add-on chip ROMs (DSP, SFX, etc) */
 //  ROM_REGION( MAX_SNES_CART_SIZE, "cart", ROMREGION_ERASE00 )
@@ -263,12 +322,15 @@ ROM_END
 ROM_START( pss61 )
 	ROM_REGION( 0x1000000, "maincpu", ROMREGION_ERASE00 )
 
+	ROM_REGION( 0x100000, "atrom", 0 )
+	ROM_LOAD( "atrom-4s-0.rom5", 0x00000, 0x80000, CRC(ad3ec05c) SHA1(a3d336db585fe02a37c323422d9db6a33fd489a6) )
+
 	ROM_REGION( 0x10000, "grom", 0 )
 	ROM_LOAD( "grom1-1.ic1", 0x0000, 0x8000, CRC(333bf9a7) SHA1(5d0cd9ca29e5580c3eebe9f136839987c879f979) )
 
 	ROM_REGION( 0x380000, "user3", 0 )
-	ROM_LOAD( "shvc-4m-1.rom3", 0x000000, 0x200000, CRC(91b28d56) SHA1(b83dd73d3d6049450bb8092d73c3af879804f58c) )
-	ROM_LOAD( "shvc-mk-0.rom6", 0x200000, 0x080000, CRC(c8002453) SHA1(cbb853bf911255c1d8eb27cd34fc7855a0dda218) )
+  ROM_LOAD( "shvc-mk-0.rom6", 0x000000, 0x080000, CRC(c8002453) SHA1(cbb853bf911255c1d8eb27cd34fc7855a0dda218) )
+  ROM_LOAD( "shvc-4m-1.rom3", 0x080000, 0x200000, CRC(91b28d56) SHA1(b83dd73d3d6049450bb8092d73c3af879804f58c) )
 	ROM_LOAD( "shvc-fo-1.ic20", 0x280000, 0x100000, CRC(ad668a41) SHA1(39ff7354a7fa02295c899b7a7ec3556998ac2636) ) /* Super FX hook needed for Star Fox */
 
 	ROM_REGION( 0x100, "user5", 0 )		/* IPL ROM */
@@ -333,8 +395,8 @@ ROM_START( pss64 )
 ROM_END
 
 
-GAME( 199?, sfcbox,      0,     sfcbox,      snes,    snes,    ROT0, "Nintendo",                   "Super Famicom Box BIOS", GAME_IS_BIOS_ROOT | GAME_NOT_WORKING )
-GAME( 199?, pss61, sfcbox, sfcbox, snes, snes, ROT0, "Nintendo", "Super Mario Kart / Super Mario Collection / Star Fox (Super Famicom Box)", GAME_NOT_WORKING )
-GAME( 199?, pss62, sfcbox, sfcbox, snes, snes, ROT0, "T&E Soft / I'Max", "New Super 3D Golf Simulation - Waialae No Kiseki / Super Mahjong 2 (Super Famicom Box)", GAME_NOT_WORKING )
-GAME( 199?, pss63, sfcbox, sfcbox, snes, snes, ROT0, "Nintendo / BPS", "Super Donkey Kong / Super Tetris 2 + Bombliss (Super Famicom Box)", GAME_NOT_WORKING )
+GAME( 1994, sfcbox,      0,     sfcbox,      snes,    snes,    ROT0, "Nintendo",                   "Super Famicom Box BIOS", GAME_IS_BIOS_ROOT | GAME_NOT_WORKING )
+GAME( 1994, pss61, sfcbox, sfcbox, snes, snes, ROT0, "Nintendo", "Super Mario Kart / Super Mario Collection / Star Fox (Super Famicom Box)", GAME_NOT_WORKING )
+GAME( 1994, pss62, sfcbox, sfcbox, snes, snes, ROT0, "T&E Soft / I'Max", "New Super 3D Golf Simulation - Waialae No Kiseki / Super Mahjong 2 (Super Famicom Box)", GAME_NOT_WORKING )
+GAME( 1994, pss63, sfcbox, sfcbox, snes, snes, ROT0, "Nintendo / BPS", "Super Donkey Kong / Super Tetris 2 + Bombliss (Super Famicom Box)", GAME_NOT_WORKING )
 GAME( 199?, pss64, sfcbox, sfcbox, snes, snes, ROT0, "Nintendo / Hudson Soft", "Super Donkey Kong / Super Bomberman 2 (Super Famicom Box)", GAME_NOT_WORKING )
