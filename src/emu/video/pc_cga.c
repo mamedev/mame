8d4e3936722903f00683fdb84fd217bac5f51231
Miodrag Milanovic <mmicko@gmail.com>
2012-07-02 09:33:59 +0000

    import from mess (nw)

--- src/emu/video/pc_cga.c
+++ src/emu/video/pc_cga.c
@@ -608,7 +608,7 @@ static MC6845_UPDATE_ROW( cga_text_blink_update_row )
 		UINT8 attr = videoram[ offset +1 ];
 		UINT8 data = cga.chr_gen[ chr * 8 + ra ];
 		UINT16 fg = attr & 0x0F;
-		UINT16 bg = attr >> 4;
+		UINT16 bg = (attr >> 4) & 0x07;
 
 		if ( i == cursor_x )
 		{
@@ -652,7 +652,7 @@ static MC6845_UPDATE_ROW( cga_text_blink_update_row_si )
 		UINT8 attr = videoram[ offset +1 ];
 		UINT8 data = cga.chr_gen[ chr * 8 + ra ];
 		UINT16 fg = attr & 0x0F;
-		UINT16 bg = attr >> 4;
+		UINT16 bg = (attr >> 4) & 0x07;
 		UINT8 xi;
 
 		if ( i == cursor_x )
