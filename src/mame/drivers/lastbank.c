b0cb630a92c25288cd77352469651eabc78e3abb
Angelo Salese <angelosa@users.noreply.github.com>
2012-07-19 03:50:46 +0000

    Bare bones hook ups to Last Bank, nw

--- src/mame/drivers/lastbank.c
+++ src/mame/drivers/lastbank.c
@@ -6,6 +6,7 @@
 
     TODO:
     - somebody should port CPU core contents in a shared file;
+    - Currently resets half-way through bootstrap routine, check why;
 
 ***************************************************************************/
 
@@ -13,12 +14,31 @@
 #include "cpu/z80/z80.h"
 #include "sound/2203intf.h"
 
+#define MASTER_CLOCK XTAL_14_31818MHz
+
 class lastbank_state : public driver_device
 {
 public:
 	lastbank_state(const machine_config &mconfig, device_type type, const char *tag)
-		: driver_device(mconfig, type, tag) { }
+		: driver_device(mconfig, type, tag),
+		m_maincpu(*this, "maincpu")
+		{ }
+
+	required_device<cpu_device> m_maincpu;
+
+	UINT8 m_rom_bank;
+	UINT8 m_irq_vector[3];
+	UINT8 m_irq_enable;
+
+	DECLARE_READ8_MEMBER(lastbank_rom_r);
+	DECLARE_READ8_MEMBER(lastbank_rom_bank_r);
+	DECLARE_WRITE8_MEMBER(lastbank_rom_bank_w);
+	DECLARE_READ8_MEMBER(lastbank_irq_vector_r);
+	DECLARE_WRITE8_MEMBER(lastbank_irq_vector_w);
+	DECLARE_READ8_MEMBER(lastbank_irq_enable_r);
+	DECLARE_WRITE8_MEMBER(lastbank_irq_enable_w);
 };
+
 
 static VIDEO_START( lastbank )
 {
@@ -30,10 +50,63 @@ static SCREEN_UPDATE_IND16( lastbank )
 	return 0;
 }
 
+
+READ8_MEMBER(lastbank_state::lastbank_rom_r)
+{
+	UINT8 *ROM = memregion("maincpu")->base();
+
+	return ROM[offset + 0x10000 + m_rom_bank * 0x2000];
+}
+
+READ8_MEMBER(lastbank_state::lastbank_rom_bank_r)
+{
+	return m_rom_bank;
+}
+
+WRITE8_MEMBER(lastbank_state::lastbank_rom_bank_w)
+{
+	m_rom_bank = data;
+}
+
+READ8_MEMBER(lastbank_state::lastbank_irq_vector_r)
+{
+	return m_irq_vector[offset];
+}
+
+WRITE8_MEMBER(lastbank_state::lastbank_irq_vector_w)
+{
+	m_irq_vector[offset] = data;
+}
+
+READ8_MEMBER(lastbank_state::lastbank_irq_enable_r)
+{
+	return m_irq_enable;
+}
+
+WRITE8_MEMBER(lastbank_state::lastbank_irq_enable_w)
+{
+	m_irq_enable = data;
+}
+
+static READ8_HANDLER( test_r )
+{
+	return -1;
+}
+
 static ADDRESS_MAP_START( lastbank_map, AS_PROGRAM, 8, lastbank_state )
-	AM_RANGE(0x0000, 0x7fff) AM_ROM /* TODO: ROM banks! */
+	AM_RANGE(0x0000, 0x5fff) AM_ROM
+	AM_RANGE(0x6000, 0x7fff) AM_READ(lastbank_rom_r)
 
 	AM_RANGE(0x8000, 0x9fff) AM_RAM
+
+	AM_RANGE(0xa800, 0xa800) AM_READ_PORT("IN0")
+	AM_RANGE(0xa801, 0xa801) AM_READ_PORT("IN1")
+	AM_RANGE(0xa802, 0xa802) AM_READ_PORT("IN2")
+	AM_RANGE(0xa803, 0xa803) AM_READ_PORT("IN3") AM_WRITENOP // mux for $a808 / $a80c
+	AM_RANGE(0xa804, 0xa804) AM_READ_PORT("IN4")
+	AM_RANGE(0xa808, 0xa808) AM_READNOP
+	AM_RANGE(0xa80c, 0xa80c) AM_READNOP
+	AM_RANGE(0xa800, 0xa81f) AM_READ_LEGACY(test_r)
 	/* TODO: RAM banks! */
 	AM_RANGE(0xc000, 0xcfff) AM_RAM
 	AM_RANGE(0xd000, 0xdfff) AM_RAM
@@ -42,11 +115,12 @@ static ADDRESS_MAP_START( lastbank_map, AS_PROGRAM, 8, lastbank_state )
 
 	//AM_RANGE(0xfe00, 0xfe03) AM_READWRITE_LEGACY(taitol_bankc_r, taitol_bankc_w)
 	//AM_RANGE(0xfe04, 0xfe04) AM_READWRITE_LEGACY(taitol_control_r, taitol_control_w)
+	AM_RANGE(0xfe00, 0xfeff) AM_RAM
 
-	//AM_RANGE(0xff00, 0xff02) AM_READWRITE_LEGACY(irq_adr_r, irq_adr_w)
-	//AM_RANGE(0xff03, 0xff03) AM_READWRITE_LEGACY(irq_enable_r, irq_enable_w)
+	AM_RANGE(0xff00, 0xff02) AM_READWRITE(lastbank_irq_vector_r, lastbank_irq_vector_w)
+	AM_RANGE(0xff03, 0xff03) AM_READWRITE(lastbank_irq_enable_r, lastbank_irq_enable_w)
 	//AM_RANGE(0xff04, 0xff07) AM_READWRITE_LEGACY(rambankswitch_r, rambankswitch_w)
-	//AM_RANGE(0xff08, 0xff08) AM_READWRITE_LEGACY(rombankswitch_r, rombankswitch_w)
+	AM_RANGE(0xff08, 0xff08) AM_READWRITE(lastbank_rom_bank_r, lastbank_rom_bank_w)
 
 ADDRESS_MAP_END
 
@@ -57,6 +131,14 @@ ADDRESS_MAP_END
 
 
 static INPUT_PORTS_START( lastbank )
+	PORT_START("IN0")
+	PORT_START("IN1")
+	PORT_START("IN2")
+	PORT_START("IN3")
+
+	PORT_START("IN4")
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNKNOWN ) //?
+	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_CUSTOM ) PORT_VBLANK("screen")
 
 INPUT_PORTS_END
 
@@ -103,7 +185,21 @@ static GFXDECODE_START( lastbank )
 	GFXDECODE_ENTRY( "maincpu",           0, char_layout,  0, 16 )  // Ram-based
 GFXDECODE_END
 
-#define MASTER_CLOCK XTAL_14_31818MHz
+static TIMER_DEVICE_CALLBACK( lastbank_irq_scanline )
+{
+	lastbank_state *state = timer.machine().driver_data<lastbank_state>();
+	int scanline = param;
+
+	if (scanline == 240 && (state->m_irq_enable & 4))
+	{
+		device_set_input_line_and_vector(state->m_maincpu, 0, HOLD_LINE, state->m_irq_vector[2]);
+	}
+
+	if (scanline == 0 && (state->m_irq_enable & 2))
+	{
+		device_set_input_line_and_vector(state->m_maincpu, 0, HOLD_LINE, state->m_irq_vector[1]);
+	}
+}
 
 static MACHINE_CONFIG_START( lastbank, lastbank_state )
 
@@ -111,6 +207,7 @@ static MACHINE_CONFIG_START( lastbank, lastbank_state )
 	MCFG_CPU_ADD("maincpu",Z80,MASTER_CLOCK/4) //!!! TC0091LVC !!!
 	MCFG_CPU_PROGRAM_MAP(lastbank_map)
 	MCFG_CPU_IO_MAP(lastbank_io)
+	MCFG_TIMER_ADD_SCANLINE("scantimer", lastbank_irq_scanline, "screen", 0, 1)
 
 //  MCFG_CPU_ADD("audiocpu",Z80,MASTER_CLOCK/4)
 
@@ -120,7 +217,7 @@ static MACHINE_CONFIG_START( lastbank, lastbank_state )
 	/* video hardware */
 	MCFG_SCREEN_ADD("screen", RASTER)
 	MCFG_SCREEN_REFRESH_RATE(60)
-	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(0))
+	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500))
 	MCFG_SCREEN_SIZE(32*8, 32*8)
 	MCFG_SCREEN_VISIBLE_AREA(0*8, 32*8-1, 2*8, 30*8-1)
 	MCFG_SCREEN_UPDATE_STATIC(lastbank)
@@ -143,8 +240,9 @@ MACHINE_CONFIG_END
 ***************************************************************************/
 
 ROM_START( lastbank )
-	ROM_REGION( 0x40000, "maincpu", 0 )
+	ROM_REGION( 0x50000, "maincpu", 0 )
 	ROM_LOAD( "3.u9", 0x00000, 0x40000, CRC(f430e1f0) SHA1(dd5b697f5c2250d98911f4c7d3e7d4cc16b0b40f) )
+	ROM_RELOAD(              0x10000, 0x40000 )
 
 	ROM_REGION( 0x40000, "audiocpu", 0 )
 	ROM_LOAD( "8.u48", 0x00000, 0x10000, CRC(3a7bfe10) SHA1(7dc543e11d3c0b9872fcc622339ade25383a1eb3) )
