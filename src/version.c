36e856cee3dd7fd28ccbe7223f10ec76e5ff75d6
Miodrag Milanovic <mmicko@gmail.com>
2012-07-30 07:58:23 +0000

    Version bump

--- src/version.c
+++ src/version.c
@@ -38,4 +38,4 @@
 ***************************************************************************/
 
 extern const char build_version[];
-const char build_version[] = "0.146u3 ("__DATE__")";
+const char build_version[] = "0.146u4 ("__DATE__")";
