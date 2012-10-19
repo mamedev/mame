f3f7fac05439eec04bd755dbf609518b9c25b5ce
mariuszw1 <mariuszw1@users.noreply.github.com>
2012-10-19 21:57:33 +0000

    Cops: added some inputs and outputs (nw)

--- src/mame/drivers/cops.c
+++ src/mame/drivers/cops.c
@@ -15,6 +15,9 @@
 
 #include "cops.lh"
 
+#define LOG_CDROM	1
+#define LOG_DACIA	0
+
 #define MAIN_CLOCK XTAL_4MHz
 
 class cops_state : public driver_device
@@ -47,7 +50,6 @@ public:
 	DECLARE_WRITE8_MEMBER(via1_irq);
 	DECLARE_WRITE8_MEMBER(via2_irq);
 	void dacia_receive(UINT8 data);
-	void laserdisc_w(UINT8 data);
 	DECLARE_WRITE8_MEMBER(dacia_w);
 	DECLARE_READ8_MEMBER(dacia_r);
 	DECLARE_WRITE8_MEMBER(cdrom_data_w);
@@ -64,6 +66,28 @@ public:
 
 	UINT8 m_cdrom_ctrl;
 	UINT8 m_cdrom_data;
+
+	// LDP-1450
+	UINT8 m_ld_command_to_send[5];
+	UINT8 m_ld_command_total_bytes;
+	UINT8 m_ld_command_current_byte;
+	UINT8 m_ld_frame[5];
+	UINT8 m_ld_frame_index;
+	emu_timer *m_ld_timer;
+	TIMER_CALLBACK_MEMBER(ld_timer_callback);
+	enum LD_INPUT_STATE
+	{
+		LD_INPUT_GET_COMMAND = 0,
+		LD_INPUT_TEXT_COMMAND,
+		LD_INPUT_TEXT_GET_X,
+		LD_INPUT_TEXT_GET_Y,
+		LD_INPUT_TEXT_GET_MODE,
+		LD_INPUT_TEXT_GET_STRING,
+		LD_INPUT_TEXT_GET_SET_WINDOW
+	} m_ld_input_state;
+
+	void laserdisc_w(UINT8 data);
+	void laserdisc_response_w(UINT8 data);
 };
 
 void cops_state::video_start()
@@ -73,6 +97,13 @@ void cops_state::video_start()
 
 UINT32 cops_state::screen_update( screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect )
 {
+	char ledtext[16 + 1];
+	for ( int i = 0; i < 16; i++ )
+	{
+		ledtext[i] = m_maincpu->space(AS_PROGRAM).read_byte(0x64 + i);
+	}
+	ledtext[16] = 0;
+	popmessage(ledtext);
 	return 0;
 }
 
@@ -87,65 +118,183 @@ WRITE8_MEMBER(cops_state::cdrom_data_w)
 	const char *regs[4] = { "CMD", "PARAM", "WRITE", "CTRL" };
 	m_cdrom_data = BITSWAP8(data,0,1,2,3,4,5,6,7);
 	UINT8 reg = ((m_cdrom_ctrl & 4) >> 1) | ((m_cdrom_ctrl & 8) >> 3);
-	logerror("%s:cdrom_data_w(reg = %s, data = %02x)\n", machine().describe_context(), regs[reg & 0x03], m_cdrom_data);
+	if (LOG_CDROM) logerror("%s:cdrom_data_w(reg = %s, data = %02x)\n", machine().describe_context(), regs[reg & 0x03], m_cdrom_data);
 }
 
 WRITE8_MEMBER(cops_state::cdrom_ctrl_w)
 {
-	logerror("%s:cdrom_ctrl_w(%02x)\n", machine().describe_context(), data);
+	if (LOG_CDROM) logerror("%s:cdrom_ctrl_w(%02x)\n", machine().describe_context(), data);
 	m_cdrom_ctrl = data;
 }
 
 READ8_MEMBER(cops_state::cdrom_data_r)
 {
-	logerror("%s:cdrom_data_r\n", machine().describe_context());
+	const char *regs[4] = { "STATUS", "RESULT", "READ", "FIFOST" };
+	UINT8 reg = ((m_cdrom_ctrl & 4) >> 1) | ((m_cdrom_ctrl & 8) >> 3);
+	if (LOG_CDROM) logerror("%s:cdrom_data_r(reg = %s)\n", machine().describe_context(), regs[reg & 0x03]);
 	return machine().rand()&0xff;
 }
 /*************************************
  *
- * 6552 DACIA & LDP-1450 Laserdisc
+ * LDP-1450 Laserdisc
  *
  *************************************/
 
+TIMER_CALLBACK_MEMBER(cops_state::ld_timer_callback)
+{
+	if ( m_ld_command_current_byte < m_ld_command_total_bytes )
+	{
+		dacia_receive(m_ld_command_to_send[m_ld_command_current_byte]);
+		m_ld_command_current_byte++;
+		if ( m_ld_command_current_byte == m_ld_command_total_bytes )
+		{
+			m_ld_command_current_byte = m_ld_command_total_bytes = 0;
+		}
+	}
+}
+
+void cops_state::laserdisc_response_w(UINT8 data)
+{
+	if ( m_ld_command_total_bytes >= 5 )
+	{
+		logerror( "LD Overflow!\n" );
+	}
+	m_ld_command_to_send[m_ld_command_total_bytes++] = data;
+}
+
 void cops_state::laserdisc_w(UINT8 data)
 {
-	switch( data )
+	switch( m_ld_input_state )
 	{
-		case 0x26: /* Video off */
-			dacia_receive(0x0a);
+		case LD_INPUT_TEXT_GET_X:
+			m_ld_input_state = LD_INPUT_TEXT_GET_Y;
 			break;
-		case 0x27: /* Video on */
-			dacia_receive(0x0a);
+		case LD_INPUT_TEXT_GET_Y:
+			m_ld_input_state = LD_INPUT_TEXT_GET_MODE;
 			break;
-		case 0x30: /* Digit */
-		case 0x31:
-		case 0x32:
-		case 0x33:
-		case 0x34:
-		case 0x35:
-		case 0x36:
-		case 0x37:
-		case 0x38:
-		case 0x39:
-			dacia_receive(0x0a);
+		case LD_INPUT_TEXT_GET_MODE:
+			m_ld_input_state = LD_INPUT_GET_COMMAND;
 			break;
-		case 0x3f: /* Stop */
-			dacia_receive(0x0a);
+		case LD_INPUT_TEXT_GET_SET_WINDOW:
+			m_ld_input_state = LD_INPUT_GET_COMMAND;
 			break;
-		case 0x40: /* Enter */
-			dacia_receive(0x0a);
+		case LD_INPUT_TEXT_GET_STRING:
+			if ( data == 0x1a )
+			{
+				m_ld_input_state = LD_INPUT_GET_COMMAND;
+			}
 			break;
-		case 0x43: /* Search */
-			dacia_receive(0x0a);
-			break;
-		case 0x56: /* C. L. (Reset) */
-			dacia_receive(0x0a);
-			break;
-		default:
-			logerror("Laserdisc command %02x\n", data);
+		case LD_INPUT_TEXT_COMMAND:
+		case LD_INPUT_GET_COMMAND:
+			{
+				switch( data )
+				{
+					case 0x00: /* text handling (start gotoxy) */
+						if ( m_ld_input_state == LD_INPUT_TEXT_COMMAND )
+						{
+							m_ld_input_state = LD_INPUT_TEXT_GET_X;
+						}
+						break;
+					case 0x01: /* text handling (end of text)*/
+						if ( m_ld_input_state == LD_INPUT_TEXT_COMMAND )
+						{
+							m_ld_input_state = LD_INPUT_TEXT_GET_STRING;
+						}
+						break;
+					case 0x02: /* text 'set window' command */
+						if ( m_ld_input_state == LD_INPUT_TEXT_COMMAND )
+						{
+							m_ld_input_state = LD_INPUT_TEXT_GET_SET_WINDOW;
+						}
+						break;
+					case 0x1a: /* text sent */
+						break;
+					case 0x24: /* Audio On */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x26: /* Video off */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x27: /* Video on */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x30: /* Digit */
+					case 0x31:
+					case 0x32:
+					case 0x33:
+					case 0x34:
+					case 0x35:
+					case 0x36:
+					case 0x37:
+					case 0x38:
+					case 0x39:
+						if ( m_ld_frame_index >= 5 )
+						{
+							m_ld_frame_index = 0;
+						}
+						m_ld_frame[m_ld_frame_index++] = data;
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x3a: /* Play (answer should have delay) */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x3f: /* Stop */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x40: /* Enter */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x43: /* Search */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x46: /* Channel 1 on */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x47: /* Channel 1 off */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x48: /* Channel 2 on */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x49: /* Channel 2 off */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x4f: /* Still */
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x55: /* 'frame mode' (unknown function) */
+						break;
+					case 0x56: /* C. L. (Reset) */
+						m_ld_input_state = LD_INPUT_GET_COMMAND;
+						laserdisc_response_w(0x0a);
+						break;
+					case 0x60: /* Addr Inq (get current frame number) */
+						for ( int i = 0; i < 5; i++ )
+						{
+							laserdisc_response_w(m_ld_frame[i]);
+						}
+						break;
+					case 0x80: /* text start */
+						m_ld_input_state = LD_INPUT_TEXT_COMMAND;
+						break;
+					case 0x81: /* Turn on text */
+						break;
+					case 0x82: /* Turn off text */
+						break;
+					default:
+						logerror("Laserdisc command %02x\n", data);
+						break;
+				}
+			}
 			break;
 	}
 }
+
+/*************************************
+ *
+ * 6552 DACIA
+ *
+ *************************************/
 
 void cops_state::dacia_receive(UINT8 data)
 {
@@ -165,7 +314,7 @@ READ8_MEMBER(cops_state::dacia_r)
 			m_dacia_receiver_full = 0;
 			return m_dacia_receiver_data;
 		default:
-			logerror("%s:dacia_r(%02x)\n", machine().describe_context(), offset);
+			if (LOG_DACIA) logerror("%s:dacia_r(%02x)\n", machine().describe_context(), offset);
 			return 0;
 	}
 }
@@ -179,7 +328,7 @@ WRITE8_MEMBER(cops_state::dacia_w)
 			laserdisc_w(data);
 			break;
 		default:
-			logerror("%s:dacia_w(%02x,%02x)\n", machine().describe_context(), offset, data);
+			if (LOG_DACIA) logerror("%s:dacia_w(%02x,%02x)\n", machine().describe_context(), offset, data);
 			break;
 	}
 }
@@ -249,14 +398,32 @@ WRITE8_MEMBER(cops_state::io1_w)
 			m_lcd_data_h = data;
 			break;
 		case 0x04: /* WOP4 */
+			output_set_value("Offroad Right 4 Lamp", data & 0x80);
+			output_set_value("Offroad Right 3 Lamp", data & 0x40);
+			output_set_value("Offroad Right 2 Lamp", data & 0x20);
+			output_set_value("Offroad Right 1 Lamp", data & 0x10);
+			output_set_value("Offroad Left 4 Lamp", data & 0x08);
+			output_set_value("Offroad Left 3 Lamp", data & 0x04);
+			output_set_value("Offroad Left 2 Lamp", data & 0x02);
+			output_set_value("Offroad Left 1 Lamp", data & 0x01);
 			break;
 		case 0x05: /* WOP5 */
+			output_set_value("Damage Lamp", data & 0x80);
+			output_set_value("Stop Lamp", data & 0x40);
+			output_set_value("Gun Active Right Lamp", data & 0x20);
+			output_set_value("Vest Hit 2 Lamp", data & 0x10);
+			output_set_value("Vest Hit 3 Lamp", data & 0x04);
+			output_set_value("Gun Active Left Lamp", data & 0x02);
+			output_set_value("Vest Hit 1 Lamp", data & 0x01);
 			break;
 		case 0x06: /* WOP6 */
+			logerror("WOP6: data = %02x\n", data);
 			break;
 		case 0x07: /* WOP? */
+			logerror("WOP7: data = %02x\n", data);
 			break;
 		case 0x08: /* WOP0 */
+			logerror("WOP8: data = %02x\n", data);
 			break;
 		default:
 			logerror("Unknown io1_w, offset = %03x, data = %02x\n", offset, data);
@@ -266,11 +433,38 @@ WRITE8_MEMBER(cops_state::io1_w)
 
 READ8_MEMBER(cops_state::io2_r)
 {
-	return 0;
+	switch( offset & 0x0f )
+	{
+		case 0x03:
+			return ioport("STEER")->read();
+		default:
+			logerror("Unknown io2_r, offset = %02x\n", offset);
+			return 0;
+	}
 }
 
 WRITE8_MEMBER(cops_state::io2_w)
 {
+	switch( offset & 0x0f )
+	{
+		case 0x02:
+			output_set_value("Flash Red Lamp", data & 0x01);
+			output_set_value("Flash Blue Lamp", data & 0x80);
+			if ( data & ~0x91 ) logerror("Unknown io2_w, offset = %02x, data = %02x\n", offset, data);
+			break;
+		case 0x04:
+			output_set_value("Bullet Lamp 6", data & 0x20);
+			output_set_value("Bullet Lamp 5", data & 0x10);
+			output_set_value("Bullet Lamp 4", data & 0x08);
+			output_set_value("Bullet Lamp 3", data & 0x04);
+			output_set_value("Bullet Lamp 2", data & 0x02);
+			output_set_value("Bullet Lamp 1", data & 0x01);
+			if ( data & ~0x3f ) logerror("Unknown io2_w, offset = %02x, data = %02x\n", offset, data);
+			break;
+		default:
+			logerror("Unknown io2_w, offset = %02x, data = %02x\n", offset, data);
+			break;
+	}
 }
 
 /*************************************
@@ -381,86 +575,34 @@ ADDRESS_MAP_END
 
 static INPUT_PORTS_START( cops )
 	PORT_START("SW0")
-	PORT_DIPNAME( 0x01, 0x00, "SW0" )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x01, DEF_STR( On ) )
-	PORT_DIPNAME( 0x02, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x02, DEF_STR( On ) )
-	PORT_DIPNAME( 0x04, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x04, DEF_STR( On ) )
-	PORT_DIPNAME( 0x08, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x08, DEF_STR( On ) )
-	PORT_DIPNAME( 0x10, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x10, DEF_STR( On ) )
-	PORT_DIPNAME( 0x20, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x20, DEF_STR( On ) )
-	PORT_DIPNAME( 0x40, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x40, DEF_STR( On ) )
-	PORT_DIPNAME( 0x80, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x80, DEF_STR( On ) )
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Switch A") PORT_CODE(KEYCODE_A) PORT_IMPULSE(1)
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Switch C") PORT_CODE(KEYCODE_C) PORT_IMPULSE(1)
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("PROGRAM") PORT_CODE(KEYCODE_P)
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_COIN2 )
+	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_SERVICE ) PORT_IMPULSE(1)
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_COIN1 )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_START )
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Switch B") PORT_CODE(KEYCODE_B) PORT_IMPULSE(1)
 
 	PORT_START("SW1")
-	PORT_DIPNAME( 0x01, 0x00, "SW1" )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x01, DEF_STR( On ) )
-	PORT_DIPNAME( 0x02, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x02, DEF_STR( On ) )
-	PORT_DIPNAME( 0x04, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x04, DEF_STR( On ) )
-	PORT_DIPNAME( 0x08, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x08, DEF_STR( On ) )
-	PORT_DIPNAME( 0x10, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x10, DEF_STR( On ) )
-	PORT_DIPNAME( 0x20, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x20, DEF_STR( On ) )
-	PORT_DIPNAME( 0x40, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x40, DEF_STR( On ) )
-	PORT_DIPNAME( 0x80, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x80, DEF_STR( On ) )
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_UNUSED ) // N.C.
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_BUTTON1 ) // Gas pedal
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("20P LEVEL") PORT_CODE(KEYCODE_Q)
+	PORT_BIT( 0x38, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_SERVICE )
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("100P LEVEL") PORT_CODE(KEYCODE_W)
 
 	PORT_START("SW2")
-	PORT_DIPNAME( 0x01, 0x00, "SW2" )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x01, DEF_STR( On ) )
-	PORT_DIPNAME( 0x02, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x02, DEF_STR( On ) )
-	PORT_DIPNAME( 0x04, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x04, DEF_STR( On ) )
-	PORT_DIPNAME( 0x08, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x08, DEF_STR( On ) )
-	PORT_DIPNAME( 0x10, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x10, DEF_STR( On ) )
-	PORT_DIPNAME( 0x20, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x20, DEF_STR( On ) )
-	PORT_DIPNAME( 0x40, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x40, DEF_STR( On ) )
-	PORT_DIPNAME( 0x80, 0x00, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x80, DEF_STR( On ) )
+	PORT_BIT( 0xff, IP_ACTIVE_LOW, IPT_UNUSED )
+
+	PORT_START("STEER")
+	PORT_BIT( 0xff, 0x80, IPT_PADDLE ) PORT_SENSITIVITY(50) PORT_KEYDELTA(10)
 INPUT_PORTS_END
 
 void cops_state::machine_start()
 {
+	m_ld_timer = machine().scheduler().timer_alloc(timer_expired_delegate(FUNC(cops_state::ld_timer_callback),this));
+	m_ld_timer->adjust(attotime::from_hz(167*5), 0, attotime::from_hz(167*5));
 }
 
 void cops_state::machine_reset()
@@ -470,6 +612,9 @@ void cops_state::machine_reset()
 	m_lcd_data_l = m_lcd_data_h = 0;
 
 	m_dacia_receiver_full = 0;
+	m_ld_input_state = LD_INPUT_GET_COMMAND;
+	m_ld_command_current_byte = m_ld_command_total_bytes = 0;
+	m_ld_frame_index = 0;
 }
 
 
