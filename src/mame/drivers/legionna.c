746441e0e0bcd4feb910447d15cec042140af111
Michaël Banaan Ananas <happppp@users.noreply.github.com>
2012-10-21 20:19:17 +0000

    New clone added ----------------- Seibu Cup Soccer (set 3) (not working)

--- src/mame/drivers/legionna.c
+++ src/mame/drivers/legionna.c
@@ -853,14 +853,17 @@ static INPUT_PORTS_START( cupsoc )
 	PORT_DIPSETTING(      0x0800, "2:00" )
 	PORT_DIPSETTING(      0x0c00, "2:30" )
 	PORT_DIPSETTING(      0x0400, "3:00" )
+	PORT_DIPSETTING(      0x0000, "x:xx" )
 	PORT_DIPNAME( 0x3000, 0x3000, "Time vs Player, 3 Players" )
 	PORT_DIPSETTING(      0x2000, "2:30" )
 	PORT_DIPSETTING(      0x3000, "3:00" )
 	PORT_DIPSETTING(      0x1000, "3:30" )
+	PORT_DIPSETTING(      0x0000, "x:xx" )
 	PORT_DIPNAME( 0xc000, 0xc000, "Time vs Player, 4 Players" )
 	PORT_DIPSETTING(      0x8000, "3:00" )
 	PORT_DIPSETTING(      0xc000, "3:30" )
 	PORT_DIPSETTING(      0x4000, "4:00" )
+	PORT_DIPSETTING(      0x0000, "x:xx" )
 
 	PORT_START("DSW2")
 	PORT_DIPNAME( 0x0003, 0x0003, DEF_STR( Difficulty ) )
@@ -879,8 +882,15 @@ static INPUT_PORTS_START( cupsoc )
 	PORT_DIPNAME( 0x0020, 0x0020, DEF_STR( Service_Mode ) )
 	PORT_DIPSETTING(      0x0020, DEF_STR( Off ) )
 	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
-	PORT_DIPNAME( 0xffc0, 0xffc0, DEF_STR( Unused ) )
-	PORT_DIPSETTING(      0xffc0, DEF_STR( Off ) )
+	PORT_DIPNAME( 0x0040, 0x0040, DEF_STR( Unused ) )
+	PORT_DIPSETTING(      0x0040, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0080, 0x0080, DEF_STR( Unused ) )
+	PORT_DIPSETTING(      0x0080, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	// probably not mapped, service lists 3*8 dips
+	PORT_DIPNAME( 0xff00, 0xff00, DEF_STR( Unused ) )
+	PORT_DIPSETTING(      0xff00, DEF_STR( Off ) )
 	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
 INPUT_PORTS_END
 
@@ -1961,6 +1971,43 @@ ROM_START( cupsoca )
 	ROM_LOAD( "copx-d1.bin", 0x000000, 0x080000, CRC(029bc402) SHA1(0f64e4c32d95abfa3920b39ed3cf0cc6eb50191b) )
 ROM_END
 
+ROM_START( cupsocb )
+	ROM_REGION( 0x100000, "maincpu", 0 )	/* 68000 code */
+	ROM_LOAD32_BYTE( "1-10n.bin", 0x000000, 0x040000, CRC(d4f37bf2) SHA1(af06364a602bd0ac2b9506de792bef003281e9d4) )
+	ROM_LOAD32_BYTE( "2-10q.bin", 0x000001, 0x040000, CRC(f06e8743) SHA1(e2f3c9b44b2830c9780df43ce10634e4e2fcb96d) )
+	ROM_LOAD32_BYTE( "3-10f.bin", 0x000002, 0x040000, CRC(226f65f9) SHA1(106a2f807aaf0f2e1fbcb1ffec6ccf4d2d7addd8) )
+	ROM_LOAD32_BYTE( "4-10k.bin", 0x000003, 0x040000, CRC(8ff16a9e) SHA1(c29986cec74e183d18eaaf69ba6ca20b75590298) )
+
+	ROM_REGION( 0x20000, "audiocpu", 0 )	/* Z80 code, banked data */
+	ROM_LOAD( "seibu7.8a",   0x000000, 0x08000, CRC(f63329f9) SHA1(51736de48efc14415cfdf169b43623d4c95fde2b) )
+	ROM_CONTINUE(            0x010000, 0x08000 )	/* banked stuff */
+	ROM_COPY( "audiocpu", 0, 0x018000, 0x08000 )
+
+	ROM_REGION( 0x020000, "gfx1", 0 )
+	ROM_LOAD16_BYTE( "soca_6.bin", 0x000000, 0x010000, CRC(a9e15910) SHA1(305541b16a87d0e38871240fa2e111bb9332e93c) )
+	ROM_LOAD16_BYTE( "soca_5.bin", 0x000001, 0x010000, CRC(73a3e024) SHA1(aeb359dd2dc9eb96330f494c44123bab3f5986a4) )
+
+	ROM_REGION( 0x200000, "gfx2", 0 )
+	ROM_LOAD( "obj.8c", 0x000000, 0x100000, CRC(e2377895) SHA1(1d1c7f31a08a464139cdaf383a5e1ade0717dc9f) )
+
+	ROM_REGION( 0x100000, "gfx3", 0 )	/* MBK tiles */
+	ROM_LOAD( "back-1.4y", 0x000000, 0x100000, CRC(3dfea0ec) SHA1(8f41d267e488e07831946ef898d593897f10bfe2) )
+
+	ROM_REGION( 0x020000, "gfx4", ROMREGION_ERASEFF )	/* not used */
+
+	ROM_REGION( 0x080000, "gfx5", 0 )	/* BK3 tiles */
+	ROM_LOAD( "back-2.6y", 0x000000, 0x080000, CRC(e07712af) SHA1(2a0285d6a1e0141838e898252b8d922a6263b05f) )
+
+	ROM_REGION( 0x080000, "gfx6", 0 )	/* LBK tiles */
+	ROM_COPY( "gfx5", 0x00000, 0x00000, 0x080000 )
+
+	ROM_REGION( 0x040000, "oki", 0 )	/* ADPCM samples */
+	ROM_LOAD( "seibu8.7a", 0x000000, 0x040000, CRC(6f594808) SHA1(218aa12068aa587c7656355f6a6b86d97c868774) )
+
+	ROM_REGION( 0x080000, "user1", 0 )
+	ROM_LOAD( "copx-d1.bin", 0x000000, 0x080000, CRC(029bc402) SHA1(0f64e4c32d95abfa3920b39ed3cf0cc6eb50191b) )
+ROM_END
+
 ROM_START( cupsocs )
 	ROM_REGION( 0x100000, "maincpu", 0 )	/* 68000 code */
 	ROM_LOAD32_BYTE( "1_10n.bin", 0x000000, 0x040000, CRC(b67835c5) SHA1(4fa562630d1f9cfb6f5bfff3295ebbdd227e4da5) )
@@ -1997,6 +2044,8 @@ ROM_START( cupsocs )
 	ROM_REGION( 0x080000, "user1", 0 )
 	ROM_LOAD( "copx-d1.bin", 0x000000, 0x080000, CRC(029bc402) SHA1(0f64e4c32d95abfa3920b39ed3cf0cc6eb50191b) )
 ROM_END
+
+
 
 ROM_START( cupsocs2 )
 	ROM_REGION( 0x100000, "maincpu", 0 )	/* 68000 code */
@@ -2311,6 +2360,7 @@ GAME( 1993, denjinmk, 0,        denjinmk, denjinmk, legionna_state, denjinmk,  R
 
 GAME( 1992, cupsoc,   0,        cupsoc,   cupsoc, driver_device,   0,         ROT0, "Seibu Kaihatsu", "Seibu Cup Soccer (set 1)", GAME_UNEMULATED_PROTECTION | GAME_NOT_WORKING )
 GAME( 1992, cupsoca,  cupsoc,   cupsoc,   cupsoc, driver_device,   0,         ROT0, "Seibu Kaihatsu", "Seibu Cup Soccer (set 2)", GAME_UNEMULATED_PROTECTION | GAME_NOT_WORKING )
+GAME( 1992, cupsocb,  cupsoc,   cupsoc,   cupsoc, driver_device,   0,         ROT0, "Seibu Kaihatsu", "Seibu Cup Soccer (set 3)", GAME_UNEMULATED_PROTECTION | GAME_NOT_WORKING )
 GAME( 1992, cupsocs,  cupsoc,   cupsocs,  cupsoc, driver_device,   0,         ROT0, "Seibu Kaihatsu", "Seibu Cup Soccer :Selection: (set 1)", GAME_UNEMULATED_PROTECTION | GAME_NOT_WORKING )
 GAME( 1992, cupsocs2, cupsoc,   cupsocs,  cupsoc, driver_device,   0,         ROT0, "Seibu Kaihatsu", "Seibu Cup Soccer :Selection: (set 2)", GAME_UNEMULATED_PROTECTION | GAME_NOT_WORKING )
 GAME( 1992, cupsocsb, cupsoc,   cupsocbl, cupsoc, legionna_state,  cupsoc,    ROT0, "bootleg", "Seibu Cup Soccer :Selection: (bootleg, set 1)", GAME_UNEMULATED_PROTECTION | GAME_NOT_WORKING )
