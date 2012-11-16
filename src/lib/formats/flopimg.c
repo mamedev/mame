b7c23bfec13b381021f5d33a217e2ad491aadad3
R. Belmont <rb6502@users.noreply.github.com>
2012-11-16 02:22:56 +0000

    Fix GCC 4.7 compile (nw)

--- src/lib/formats/flopimg.c
+++ src/lib/formats/flopimg.c
@@ -1192,12 +1192,12 @@ void floppy_image_format_t::mfm_w(UINT32 *buffer, int &offset, int n, UINT32 val
 
 void floppy_image_format_t::fm_w(UINT32 *buffer, int &offset, int n, UINT32 val, UINT32 size)
 {
-	int prec = offset ? bit_r(buffer, offset-1) : 0;
+//	int prec = offset ? bit_r(buffer, offset-1) : 0;
 	for(int i=n-1; i>=0; i--) {
 		int bit = (val >> i) & 1;
 		bit_w(buffer, offset++, true);
 		bit_w(buffer, offset++, bit, size);
-		prec = bit;
+//		prec = bit;
 	}
 }
 
