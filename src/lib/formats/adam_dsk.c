52e54ab3a6294290d2c3d5fcbd5b6acbd546f99a
Curt Coder <curtcoder@mail.com>
2012-11-28 15:58:02 +0000

    (MESS) Cleanup. (nw)

--- src/lib/formats/adam_dsk.c
+++ src/lib/formats/adam_dsk.c
@@ -65,23 +65,23 @@ const char *adam_format::extensions() const
 const adam_format::format adam_format::formats[] = {
 	{   /*  160K 5 1/4 inch double density single sided */
 		floppy_image::FF_525, floppy_image::SSDD,
-		2000,  8, 40, 1, 512, {}, 1, {}, 544, 22, 84
+		2000,  8, 40, 1, 512, {}, 1, {}, 100, 22, 84
 	},
 	{   /*  320K 5 1/4 inch double density */
 		floppy_image::FF_525, floppy_image::DSDD,
-		2000,  8, 40, 2, 512, {}, 1, {}, 544, 22, 84
+		2000,  8, 40, 2, 512, {}, 1, {}, 100, 22, 84
 	},
 	{   /*  640K 5 1/4 inch quad density */
 		floppy_image::FF_525, floppy_image::DSQD,
-		2000,  8, 80, 2, 512, {}, 1, {}, 544, 22, 84
+		2000,  8, 80, 2, 512, {}, 1, {}, 100, 22, 84
 	},
 	{   /*  720K 3 1/2 inch double density */
 		floppy_image::FF_35,  floppy_image::DSDD,
-		2000,  9, 80, 2, 512, {}, 1, {}, 544, 22, 84
+		2000,  9, 80, 2, 512, {}, 1, {}, 100, 22, 84
 	},
 	{   /* 1440K 3 1/2 inch high density */
 		floppy_image::FF_35,  floppy_image::DSHD,
-		1000, 18, 80, 2, 512, {}, 1, {}, 544, 22, 84
+		1000, 18, 80, 2, 512, {}, 1, {}, 100, 22, 84
 	},
 	{}
 };
