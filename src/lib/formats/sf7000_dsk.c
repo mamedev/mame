3df0bd2be6db200ff0a16d0860a227efe960bda2
Curt Coder <curtcoder@mail.com>
2012-12-10 21:51:26 +0000

    (MESS) sf7000: Verified floppy format gap sizes. [Curt Coder]

--- src/lib/formats/sf7000_dsk.c
+++ src/lib/formats/sf7000_dsk.c
@@ -61,15 +61,15 @@ const char *sf7000_format::extensions() const
 	return "sf7";
 }
 
-// Unverified gap sizes
 const sf7000_format::format sf7000_format::formats[] = {
 	{
+    // mfm h=00 n=01 sc=10 gpl=2a d=ff
 		floppy_image::FF_3, floppy_image::SSDD,
 		2000, // 2us, 300rpm
 		16, 40, 1,
 		256, {},
 		1, {},
-		80, 50, 22, 60
+		80, 50, 22, 42
 	},
 	{}
 };
