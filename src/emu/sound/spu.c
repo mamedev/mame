47dffb8317d59c978be85bf11ba014f10a798746
David Haywood <mamehaze@users.noreply.github.com>
2013-01-21 15:07:14 +0000

    fix debug compile, somebody should probably fix / remove the line as appropriate tho. (nw)

--- src/emu/sound/spu.c
+++ src/emu/sound/spu.c
@@ -1156,7 +1156,7 @@ READ16_MEMBER( spu_device::read )
 {
 	unsigned short ret=0, *rp=(unsigned short *)(reg+((offset*2)&0x1ff));
 
-	assert((addr&1)==0);
+	//assert((addr&1)==0);
 
 	m_stream->update();
 
