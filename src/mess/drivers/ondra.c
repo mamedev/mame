1e68d547c38f9eded8b8613de77f4b091ff3b3b2
Oliver Stöneberg <firewave@users.noreply.github.com>
2013-01-22 23:19:02 +0000

    [MESS] fixed interface for ondra cassette (nw)

--- src/mess/drivers/ondra.c
+++ src/mess/drivers/ondra.c
@@ -115,7 +115,7 @@ static const cassette_interface ondra_cassette_interface =
 	cassette_default_formats,
 	NULL,
 	(cassette_state)(CASSETTE_STOPPED | CASSETTE_MOTOR_ENABLED | CASSETTE_SPEAKER_ENABLED),
-	"ondra",
+	"ondra_cass",
 	NULL
 };
 
