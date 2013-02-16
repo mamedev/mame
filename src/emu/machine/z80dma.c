b526cecada97cc36c6053f1808acbcc0c6083761
Oliver Stöneberg <firewave@users.noreply.github.com>
2013-02-16 09:22:13 +0000

    fixed unfixed uninitialized variables in src/emu/machine/z80dma.c (nw)

--- src/emu/machine/z80dma.c
+++ src/emu/machine/z80dma.c
@@ -229,6 +229,7 @@ void z80dma_device::device_reset()
 	m_dma_enabled = 0;
 	m_read_num_follow = m_read_cur_follow = 0;
 	m_reset_pointer = 0;
+	m_is_read = false;
 
 	// disable interrupts
 	WR3 &= ~0x20;
