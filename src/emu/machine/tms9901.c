376a931588f1fd007276060131d33e41d3de8d64
Oliver Stöneberg <firewave@users.noreply.github.com>
2013-02-16 16:14:25 +0000

    avoid usage of uninitialized memory in tms9901_device::field_interrupts() (nw)

--- src/emu/machine/tms9901.c
+++ src/emu/machine/tms9901.c
@@ -507,11 +507,11 @@ void tms9901_device::device_reset(void)
 
 	m_int_state = 0;
 	m_old_int_state = -1;
+	m_clock_register = 0;
 	field_interrupts();
 
 	m_clock_mode = false;
 
-	m_clock_register = 0;
 	timer_reload();
 }
 
