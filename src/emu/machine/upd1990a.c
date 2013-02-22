8e391558d76de44302b3f522b76a1321bb1a7364
Oliver Stöneberg <firewave@users.noreply.github.com>
2013-02-22 08:15:35 +0000

    fixed uninitialized members in src/emu/machine/upd1990a.c (nw)

--- src/emu/machine/upd1990a.c
+++ src/emu/machine/upd1990a.c
@@ -57,7 +57,9 @@ enum
 upd1990a_rtc_device::upd1990a_rtc_device(const machine_config &mconfig, device_type type, const char* name, const char *tag, device_t *owner, UINT32 clock)
 	: device_t(mconfig, type, name, tag, owner, clock),
 		device_rtc_interface(mconfig, *this),
-		m_data_out(0)
+		m_data_out(0),
+		m_c(0),
+		m_clk(0)
 {
 }
 
