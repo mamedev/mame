a160fc3b218d1db7d5b8cbb968d38acde70d1c15
Brian Troha <briantro@users.noreply.github.com>
2013-03-14 00:09:11 +0000

    vp101.c: Add version 3.5 boot rom and add no_dump for Special Forces Elite Training PIC18C422 chip.

--- src/mame/drivers/vp101.c
+++ src/mame/drivers/vp101.c
@@ -109,7 +109,11 @@ ROM_END
 
 ROM_START(specfrce)
 	ROM_REGION(0x100000, "maincpu", 0)  /* Boot ROM */
-	ROM_LOAD( "specialforces_boot34.u4", 0x000000, 0x100000, CRC(db4862ac) SHA1(a1e886d424cf7d26605e29d972d48e8d44ae2d58) )
+	ROM_LOAD( "special_forces_boot_v3.4.u4", 0x000000, 0x100000, CRC(db4862ac) SHA1(a1e886d424cf7d26605e29d972d48e8d44ae2d58) )
+	ROM_LOAD( "special_forces_boot_v3.5.u4", 0x000000, 0x100000, CRC(ae8dfdf0) SHA1(d64130e710d0c70095ad8ebd4e2194b8c461be4a) ) /* Newer, but keep both in driver */
+
+	ROM_REGION(0x80000, "pic", 0)       /* PIC18c422 I/P program - read-protected, need dumped */
+	ROM_LOAD( "special_forces_et_u7_rev1.2.u7", 0x000000, 0x80000, NO_DUMP )
 
 	DISK_REGION( "ide" )
 	DISK_IMAGE_READONLY("sf010101", 0, SHA1(59b5e3d8e1d5537204233598830be2066aad0556) )
