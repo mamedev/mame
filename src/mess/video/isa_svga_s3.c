a6c60b425c4818f0d07e86d2e31b4e6e9cdeebf9
mahlemiut <mahlemiut@users.noreply.github.com>
2013-03-31 22:16:26 +0000

    (MESS) Added skeleton S3 ViRGE video card. [Barry Rodewald]

--- src/mess/video/isa_svga_s3.c
+++ src/mess/video/isa_svga_s3.c
@@ -59,7 +59,7 @@ const rom_entry *isa16_svga_s3_device::device_rom_region() const
 //-------------------------------------------------
 
 isa16_svga_s3_device::isa16_svga_s3_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock) :
-		device_t(mconfig, ISA16_SVGA_S3, "SVGA S3 Graphics Card", tag, owner, clock, "s3_764", __FILE__),
+		device_t(mconfig, ISA16_SVGA_S3, "Number Nine 9FX Vision 330 (S3 764) Graphics Card", tag, owner, clock, "s3_764", __FILE__),
 		device_isa16_card_interface(mconfig, *this)
 {
 }
@@ -104,5 +104,94 @@ void isa16_svga_s3_device::device_start()
 //-------------------------------------------------
 
 void isa16_svga_s3_device::device_reset()
+{
+}
+
+
+/*
+ * 	S3 Virge 2D/3D accelerator
+ */
+
+ROM_START( s3virge )
+	ROM_REGION(0x8000,"s3virge", 0)
+	ROM_LOAD("s3virge.bin", 0x00000, 0x8000, CRC(a7983a85) SHA1(e885371816d3237f7badd57ccd602cd863c9c9f8) )
+	ROM_IGNORE( 0x8000 )
+ROM_END
+
+//**************************************************************************
+//  GLOBAL VARIABLES
+//**************************************************************************
+
+const device_type ISA16_S3VIRGE = &device_creator<isa16_s3virge_device>;
+
+
+static MACHINE_CONFIG_FRAGMENT( vga_s3virge )
+	MCFG_SCREEN_ADD("screen", RASTER)
+	MCFG_SCREEN_RAW_PARAMS(XTAL_25_1748MHz,900,0,640,526,0,480)
+	MCFG_SCREEN_UPDATE_DEVICE("vga", s3virge_vga_device, screen_update)
+
+	MCFG_PALETTE_LENGTH(0x100)
+
+	MCFG_DEVICE_ADD("vga", S3VIRGE, 0)
+MACHINE_CONFIG_END
+
+//-------------------------------------------------
+//  machine_config_additions - device-specific
+//  machine configurations
+//-------------------------------------------------
+
+machine_config_constructor isa16_s3virge_device::device_mconfig_additions() const
+{
+	return MACHINE_CONFIG_NAME( vga_s3virge );
+}
+
+//-------------------------------------------------
+//  rom_region - device-specific ROM region
+//-------------------------------------------------
+
+const rom_entry *isa16_s3virge_device::device_rom_region() const
+{
+	return ROM_NAME( s3virge );
+}
+
+//**************************************************************************
+//  LIVE DEVICE
+//**************************************************************************
+
+//-------------------------------------------------
+//  isa16_vga_device - constructor
+//-------------------------------------------------
+
+isa16_s3virge_device::isa16_s3virge_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock) :
+		device_t(mconfig, ISA16_S3VIRGE, "S3 ViRGE Graphics Card", tag, owner, clock, "s3virge", __FILE__),
+		device_isa16_card_interface(mconfig, *this)
+{
+}
+
+//-------------------------------------------------
+//  device_start - device-specific startup
+//-------------------------------------------------
+READ8_MEMBER(isa16_s3virge_device::input_port_0_r ) { return 0xff; } //return space.machine().root_device().ioport("IN0")->read(); }
+
+void isa16_s3virge_device::device_start()
+{
+	set_isa_device();
+
+	m_vga = subdevice<s3virge_vga_device>("vga");
+
+	m_isa->install_rom(this, 0xc0000, 0xc7fff, 0, 0, "svga", "s3virge");
+
+	m_isa->install_device(0x03b0, 0x03bf, 0, 0, read8_delegate(FUNC(s3virge_vga_device::port_03b0_r),m_vga), write8_delegate(FUNC(s3virge_vga_device::port_03b0_w),m_vga));
+	m_isa->install_device(0x03c0, 0x03cf, 0, 0, read8_delegate(FUNC(s3virge_vga_device::port_03c0_r),m_vga), write8_delegate(FUNC(s3virge_vga_device::port_03c0_w),m_vga));
+	m_isa->install_device(0x03d0, 0x03df, 0, 0, read8_delegate(FUNC(s3virge_vga_device::port_03d0_r),m_vga), write8_delegate(FUNC(s3virge_vga_device::port_03d0_w),m_vga));
+
+	m_isa->install_memory(0xa0000, 0xbffff, 0, 0, read8_delegate(FUNC(s3virge_vga_device::mem_r),m_vga), write8_delegate(FUNC(s3virge_vga_device::mem_w),m_vga));
+}
+
+//-------------------------------------------------
+//  device_reset - device-specific reset
+//-------------------------------------------------
+
+void isa16_s3virge_device::device_reset()
 {
 }
--- src/mess/video/isa_svga_s3.h
+++ src/mess/video/isa_svga_s3.h
@@ -35,8 +35,30 @@ private:
 		ibm8514a_device *m_8514;
 };
 
+class isa16_s3virge_device :
+		public device_t,
+		public device_isa16_card_interface
+{
+public:
+		// construction/destruction
+		isa16_s3virge_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+
+		// optional information overrides
+		virtual machine_config_constructor device_mconfig_additions() const;
+		virtual const rom_entry *device_rom_region() const;
+
+		DECLARE_READ8_MEMBER(input_port_0_r);
+protected:
+		// device-level overrides
+		virtual void device_start();
+		virtual void device_reset();
+private:
+		s3virge_vga_device *m_vga;
+};
+
 
 // device type definition
 extern const device_type ISA16_SVGA_S3;
+extern const device_type ISA16_S3VIRGE;
 
 #endif  /* __ISA_VGA_H__ */
