225d353f1e0c9d02ca961218219c7399e12e50c3
smf- <smf-@users.noreply.github.com>
2013-04-06 02:27:34 +0000

    Reduced time before dma interrupt is fired, as it stops incorrect textures being displayed in a few games and the pse bios v2.2. [smf]

--- src/emu/cpu/psx/dma.c
+++ src/emu/cpu/psx/dma.c
@@ -152,7 +152,9 @@ void psxdma_device::dma_finished( int index )
 				if( n_address == 0xffffff )
 				{
 					dma->n_base = n_address;
-					dma_start_timer( index, 19000 );
+					//HACK: fixes pse bios 2.x & other texture uploading issues.
+					//dma_start_timer( index, 19000 );
+					dma_start_timer( index, 500 );
 					return;
 				}
 				if( n_total > 65535 )
