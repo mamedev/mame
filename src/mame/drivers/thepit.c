bc3895fa7467fd33ccd0b87d5e9ffd8b52dfd587
Brian Troha <briantro@users.noreply.github.com>
2013-07-22 15:51:13 +0000

    thepit.c: David figured out the Cabinet dipswitch so change the comment - NW

--- src/mame/drivers/thepit.c
+++ src/mame/drivers/thepit.c
@@ -325,7 +325,7 @@ static INPUT_PORTS_START( desertdn )
 	PORT_DIPNAME( 0x10, 0x00, DEF_STR( Cabinet ) )
 	PORT_DIPSETTING(    0x10, DEF_STR( Cocktail ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( Upright ) )
-	PORT_DIPNAME( 0x20, 0x00, DEF_STR( Unknown ) ) /* Cabinet Type or Bonus ?? */
+	PORT_DIPNAME( 0x20, 0x00, DEF_STR( Unknown ) ) /* Bonus ?? */
 	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x20, DEF_STR( On ) )
 	PORT_DIPNAME( 0x40, 0x00, "Timer Speed" )
