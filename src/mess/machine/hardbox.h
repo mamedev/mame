68e33427df578ae2f6c2015647ea3abd5b422650
Curt Coder <curtcoder@mail.com>
2013-07-24 13:11:13 +0000

    (MESS) Emulated the SSE HardBox, a Corvus interface for PET [Mike Naberezny]

---
+++ src/mess/machine/hardbox.h
@@ -0,0 +1,79 @@
+/**********************************************************************
+
+    SSE HardBox emulation
+
+    Copyright MESS Team.
+    Visit http://mamedev.org for licensing and usage restrictions.
+
+**********************************************************************/
+
+#pragma once
+
+#ifndef __PET_HARDBOX__
+#define __PET_HARDBOX__
+
+#include "emu.h"
+#include "cpu/z80/z80.h"
+#include "imagedev/harddriv.h"
+#include "includes/corvushd.h"
+#include "machine/cbmipt.h"
+#include "machine/i8255.h"
+#include "machine/ieee488.h"
+#include "machine/serial.h"
+
+
+
+//**************************************************************************
+//  TYPE DEFINITIONS
+//**************************************************************************
+
+// ======================> hardbox_device
+
+class hardbox_device :  public device_t,
+						public device_ieee488_interface
+{
+public:
+	// construction/destruction
+	hardbox_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+
+	// optional information overrides
+	virtual const rom_entry *device_rom_region() const;
+	virtual machine_config_constructor device_mconfig_additions() const;
+	virtual ioport_constructor device_input_ports() const;
+
+	DECLARE_READ8_MEMBER( ppi0_pa_r );
+	DECLARE_WRITE8_MEMBER( ppi0_pb_w );
+	DECLARE_READ8_MEMBER( ppi0_pc_r );
+
+	DECLARE_READ8_MEMBER( ppi1_pa_r );
+	DECLARE_WRITE8_MEMBER( ppi1_pb_w );
+	DECLARE_READ8_MEMBER( ppi1_pc_r );
+	DECLARE_WRITE8_MEMBER( ppi1_pc_w );
+
+protected:
+	// device-level overrides
+	virtual void device_start();
+	virtual void device_reset_after_children();
+
+	// device_ieee488_interface overrides
+	virtual void ieee488_ifc(int state);
+
+private:
+	enum
+	{
+		LED_A,
+		LED_B,
+		LED_READY
+	};
+
+	required_device<cpu_device> m_maincpu;
+
+    int m_ifc;  // Tracks previous state of IEEE-488 IFC line
+};
+
+// device type definition
+extern const device_type HARDBOX;
+
+
+
+#endif
--- src/mess/mess.mak
+++ src/mess/mess.mak
@@ -1184,6 +1184,7 @@ $(MESSOBJ)/cbm.a:               \
 	$(MESS_MACHINE)/d9060hd.o   \
 	$(MESS_MACHINE)/serialbox.o \
 	$(MESS_MACHINE)/softbox.o   \
+	$(MESS_MACHINE)/hardbox.o   \
 	$(MESS_MACHINE)/cmdhd.o     \
 	$(MESS_MACHINE)/fd2000.o    \
 	$(MESS_DRIVERS)/clcd.o      \
