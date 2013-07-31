18372ba95d3450d4d655dcd4e24bd32ea8575833
Robbbert <robbbert@users.noreply.github.com>
2013-07-31 10:47:09 +0000

    (MESS) dsb46 :  Added bankswitching

--- src/mess/drivers/dsb46.c
+++ src/mess/drivers/dsb46.c
@@ -30,16 +30,20 @@ public:
 		, m_terminal(*this, TERMINAL_TAG)
 	{ }
 
-	required_device<cpu_device> m_maincpu;
-	required_device<generic_terminal_device> m_terminal;
 	DECLARE_READ8_MEMBER(port00_r);
 	DECLARE_READ8_MEMBER(port01_r);
 	DECLARE_WRITE8_MEMBER(kbd_put);
+	DECLARE_WRITE8_MEMBER(port1a_w);
+	DECLARE_DRIVER_INIT(dsb46);
+	DECLARE_MACHINE_RESET(dsb46);
+private:
 	UINT8 m_term_data;
+	required_device<cpu_device> m_maincpu;
+	required_device<generic_terminal_device> m_terminal;
 };
 
 static ADDRESS_MAP_START( dsb46_mem, AS_PROGRAM, 8, dsb46_state )
-	AM_RANGE(0x0000, 0x07ff) AM_ROM AM_REGION("maincpu", 0)
+	AM_RANGE(0x0000, 0x07ff) AM_READ_BANK("read") AM_WRITE_BANK("write")
 	AM_RANGE(0x0800, 0xffff) AM_RAM
 ADDRESS_MAP_END
 
@@ -47,12 +51,33 @@ static ADDRESS_MAP_START( dsb46_io, AS_IO, 8, dsb46_state )
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
 	AM_RANGE(0x00, 0x00) AM_READ(port00_r) AM_DEVWRITE(TERMINAL_TAG, generic_terminal_device, write)
 	AM_RANGE(0x01, 0x01) AM_READ(port01_r)
-	// port 1a bankswitch between rom and ram
+	AM_RANGE(0x1a, 0x1a) AM_WRITE(port1a_w)
 ADDRESS_MAP_END
 
 static INPUT_PORTS_START( dsb46 )
 INPUT_PORTS_END
 
+DRIVER_INIT_MEMBER(dsb46_state, dsb46)
+{
+	UINT8 *RAM = memregion("maincpu")->base();
+	membank("read")->configure_entry(0, &RAM[0x10000]);
+	membank("read")->configure_entry(1, &RAM[0x00000]);
+	membank("write")->configure_entry(0, &RAM[0x00000]);
+}
+
+MACHINE_RESET_MEMBER( dsb46_state,dsb46 )
+{
+	membank("read")->set_entry(0);
+	membank("write")->set_entry(0);
+	m_term_data = 0;
+	m_maincpu->reset();
+}
+
+WRITE8_MEMBER( dsb46_state::port1a_w )
+{
+	membank("read")->set_entry(data & 1);
+}
+	
 READ8_MEMBER( dsb46_state::port01_r )
 {
 	return (m_term_data) ? 5 : 4;
@@ -80,17 +105,18 @@ static MACHINE_CONFIG_START( dsb46, dsb46_state )
 	MCFG_CPU_ADD("maincpu", Z80, 4000000)
 	MCFG_CPU_PROGRAM_MAP(dsb46_mem)
 	MCFG_CPU_IO_MAP(dsb46_io)
+	MCFG_MACHINE_RESET_OVERRIDE(dsb46_state, dsb46)
 
 	/* video hardware */
 	MCFG_GENERIC_TERMINAL_ADD(TERMINAL_TAG, terminal_intf)
 MACHINE_CONFIG_END
 
 ROM_START( dsb46 )
-	ROM_REGION( 0x800, "maincpu", 0 )
-	ROM_LOAD( "1538a.bin", 0x000, 0x800, CRC(65b3e26e) SHA1(afe1f03f266b7d13fdb1f1bc6762df5e0aa5c764) )
+	ROM_REGION( 0x10800, "maincpu", 0 )
+	ROM_LOAD( "1538a.bin", 0x10000, 0x800, CRC(65b3e26e) SHA1(afe1f03f266b7d13fdb1f1bc6762df5e0aa5c764) )
 
 	ROM_REGION( 0x4000, "ades", 0 )
 	ROM_LOAD( "ades.bin", 0x0000, 0x4000, CRC(d374abf0) SHA1(331f51a2bb81375aeffbe63c1ebc1d7cd779b9c3) )
 ROM_END
 
-COMP( 198?, dsb46,    0,      0,      dsb46,        dsb46, driver_device, 0,      "Davidge",  "DSB-4/6",  GAME_NOT_WORKING | GAME_NO_SOUND_HW )
+COMP( 198?, dsb46, 0, 0, dsb46, dsb46, dsb46_state, dsb46, "Davidge", "DSB-4/6",  GAME_NOT_WORKING | GAME_NO_SOUND_HW )
