7d87d88298c47cb424518c80aacc2b14072881b9
David Haywood <mamehaze@users.noreply.github.com>
2013-10-07 19:59:41 +0000

    missing files (nw)

---
+++ src/mame/machine/igs022.h
@@ -0,0 +1,27 @@
+/* IGS022 */
+
+
+class igs022_device : public device_t
+{
+public:
+	igs022_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+
+	UINT16*	m_sharedprotram;
+	void IGS022_handle_command();
+
+protected:
+	virtual void device_config_complete();
+	virtual void device_validity_check(validity_checker &valid) const;
+	virtual void device_start();
+	virtual void device_reset();
+
+	UINT32        m_kb_regs[0x100];
+
+	void IGS022_do_dma(UINT16 src, UINT16 dst, UINT16 size, UINT16 mode);
+	void IGS022_reset();
+
+};
+
+
+
+extern const device_type IGS022;
