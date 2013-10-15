2722fb464bcf36b949f803fcd1ed369bf349c2b9
Miodrag Milanovic <mmicko@gmail.com>
2013-10-15 18:03:00 +0000

    remove all drivers by default, seams like more politically clean approach

--- src/mame/audio/cclimber.h
+++
@@ -1,8 +0,0 @@
-#include "sound/samples.h"
-#include "sound/ay8910.h"
-
-extern const ay8910_interface cclimber_ay8910_interface;
-extern const samples_interface cclimber_samples_interface;
-DECLARE_WRITE8_HANDLER( cclimber_sample_trigger_w );
-DECLARE_WRITE8_HANDLER( cclimber_sample_rate_w );
-DECLARE_WRITE8_HANDLER( cclimber_sample_volume_w );
--- src/mame/machine/segag80.h
+++
@@ -1,3 +0,0 @@
-typedef UINT8 (*segag80_decrypt_func)(offs_t, UINT8);
-
-segag80_decrypt_func segag80_security(int chip);
