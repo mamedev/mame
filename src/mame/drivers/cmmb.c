aa5c6320b244b7eca43e510359ce75b397ef6ff1
Michaël Banaan Ananas <happppp@users.noreply.github.com>
2013-10-16 21:26:11 +0000

    this one is by Kale

--- src/mame/drivers/cmmb.c
+++ src/mame/drivers/cmmb.c
@@ -1,3 +1,5 @@
+// license:MAME
+// copyright-holders:Angelo Salese
 /***************************************************************************
 
 Centipede / Millipede / Missile Command / Let's Go Bowling
