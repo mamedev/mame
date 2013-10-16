ab4939c07e9b7ff8889f01d1418f20c2064a58c2
Couriersud <couriersud@users.noreply.github.com>
2013-10-16 20:38:48 +0000

    Updated license tags The 8257 used 8237 as a skeleton but ended up as a complete rewrite.

--- src/emu/machine/8257dma.c
+++ src/emu/machine/8257dma.c
@@ -1,3 +1,5 @@
+// license:BSD-3-Clause
+// copyright-holders:Couriersud
 /**********************************************************************
 
     8257 DMA interface and emulation
--- src/emu/machine/8257dma.h
+++ src/emu/machine/8257dma.h
@@ -1,3 +1,5 @@
+// license:BSD-3-Clause
+// copyright-holders:Couriersud
 /***************************************************************************
 
     Intel 8257 Programmable DMA Controller emulation
