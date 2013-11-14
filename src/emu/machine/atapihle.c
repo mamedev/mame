73a1093f30b32ed2e1cb0c26fa2acf9acadb19a3
smf- <smf-@users.noreply.github.com>
2013-11-14 22:50:01 +0000

    Set the error bit in the IDE status register if the command causes a check condition [smf]

--- src/emu/machine/atapihle.c
+++ src/emu/machine/atapihle.c
@@ -36,6 +36,9 @@ void atapi_hle_device::process_buffer()
 		ExecCommand();
 		GetLength(&m_data_size);
 
+		if (m_status_code == STATUS_CODE_CHECK_CONDITION)
+			m_status |= IDE_STATUS_ERR;
+
 		m_buffer_size = (m_cylinder_high << 8) | m_cylinder_low;
 		if (m_buffer_size == 0xffff)
 			m_buffer_size = 0xfffe;
