c859a6332e263ea40a1e4b4411dade31b31db250
Miodrag Milanovic <mmicko@gmail.com>
2014-01-29 16:45:35 +0000

    fix for sdl compile (nw)

--- src/osd/sdl/drawsdl.c
+++ src/osd/sdl/drawsdl.c
@@ -461,7 +461,7 @@ static int drawsdl_window_create(sdl_window_info *window, int width, int height)
 			if (!found)
 			{
 				mame_printf_verbose("window: Scale mode %s not supported!\n", sm->name);
-				ui_popup_time(3, "Scale mode %s not supported!", sm->name);
+				window->machine().ui().popup_time(3, "Scale mode %s not supported!", sm->name);
 			}
 		}
 	}
--- src/osd/sdl/window.c
+++ src/osd/sdl/window.c
@@ -587,7 +587,7 @@ void sdlwindow_modify_prescale(running_machine &machine, sdl_window_info *window
 			execute_async_wait(destroy_all_textures_wt, &wp);
 			window->prescale = new_prescale;
 		}
-		ui_popup_time(1, "Prescale %d", window->prescale);
+		machine.ui().popup_time(1, "Prescale %d", window->prescale);
 	}
 }
 
