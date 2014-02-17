9259c365642c230772e7aa1236e05cf1f628304f
Miodrag Milanovic <mmicko@gmail.com>
2014-02-17 16:32:37 +0000

    bmcpokr crash fix (nw)

--- src/mame/drivers/bmcpokr.c
+++ src/mame/drivers/bmcpokr.c
@@ -237,12 +237,10 @@ static MACHINE_CONFIG_START( bmcpokr, bmcpokr_state )
 	MCFG_SCREEN_REFRESH_RATE(60)
 	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
 	MCFG_SCREEN_UPDATE_DRIVER(bmcpokr_state, screen_update_bmcpokr)
-
-	MCFG_GFXDECODE_ADD("gfxdecode", bmcpokr)
-
 	MCFG_SCREEN_SIZE(64*8, 32*8)
 	MCFG_SCREEN_VISIBLE_AREA(0*8, 64*8-1, 0*8, 32*8-1)
 	MCFG_PALETTE_LENGTH(256)
+	MCFG_GFXDECODE_ADD("gfxdecode", bmcpokr)
 
 MACHINE_CONFIG_END
 
