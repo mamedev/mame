ca932a6d3d9da33a04dead1b0617ecfd2c73ad38
Miodrag Milanovic <mmicko@gmail.com>
2014-02-19 11:07:33 +0000

    fix SDL build (nw)

--- src/osd/sdl/texsrc.h
+++ src/osd/sdl/texsrc.h
@@ -41,9 +41,9 @@
 	#define DEST_NAME(name) name ## _32bpp
 	#define TEXSRC_TYPE UINT32
 	#define TEXSRC_TO_DEST(src) \
-		(texsource->palette[0x200 + src.r()]   | \
-			texsource->palette[0x100 + src.g()] | \
-			texsource->palette[src.b()] | 0xff000000)
+		(texsource->palette[0x200 + (((src) >> 16) & 0xff) ]   | \
+			texsource->palette[0x100 + (((src) >> 8) & 0xff) ] | \
+			texsource->palette[((src) & 0xff) ] | 0xff000000)
 	#define FUNC_NAME(name) name ## _rgb32_paletted
 #elif SDL_TEXFORMAT == SDL_TEXFORMAT_PALETTE16
 	#define DEST_TYPE UINT32
