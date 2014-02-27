18eaa848e9beaf34ff06ae443eb8538b8d862a5a
Miodrag Milanovic <mmicko@gmail.com>
2014-02-27 14:09:50 +0000

    removal of unused code (nw)

--- src/mame/includes/segamsys.h
+++ src/mame/includes/segamsys.h
@@ -26,41 +26,4 @@ extern DECLARE_WRITE8_HANDLER( sms_vdp_ctrl_w );
 
 extern void init_for_megadrive(running_machine &machine);
 extern void segae_md_sms_stop_scanline_timer(void);
-
-
-extern DECLARE_READ8_HANDLER( md_sms_vdp_vcounter_r );
-extern DECLARE_READ8_HANDLER( md_sms_vdp_data_r );
-extern DECLARE_WRITE8_HANDLER( md_sms_vdp_data_w );
-extern DECLARE_READ8_HANDLER( md_sms_vdp_ctrl_r );
-extern DECLARE_WRITE8_HANDLER( md_sms_vdp_ctrl_w );
-
-extern SCREEN_VBLANK(sms);
-extern DECLARE_READ8_HANDLER( sms_vdp_2_data_r );
-extern DECLARE_WRITE8_HANDLER( sms_vdp_2_data_w );
-extern DECLARE_READ8_HANDLER( sms_vdp_2_ctrl_r );
-extern DECLARE_WRITE8_HANDLER( sms_vdp_2_ctrl_w );
-extern SCREEN_VBLANK(systeme);
-extern SCREEN_UPDATE_RGB32(systeme);
-extern MACHINE_RESET(systeme);
-extern UINT8* sms_mainram;
-extern UINT8* vdp2_vram_bank0;
-extern UINT8* vdp2_vram_bank1;
-extern UINT8* vdp1_vram_bank0;
-extern UINT8* vdp1_vram_bank1;
-extern void segae_set_vram_banks(UINT8 data);
-DECLARE_READ8_HANDLER( sms_ioport_gg00_r );
-void init_extra_gg_ports(running_machine* machine, const char* tag);
-DECLARE_READ8_HANDLER (megatech_sms_ioport_dc_r);
-DECLARE_READ8_HANDLER (megatech_sms_ioport_dd_r);
-DECLARE_READ8_HANDLER( smsgg_backupram_r );
-DECLARE_WRITE8_HANDLER( smsgg_backupram_w );
 extern void megatech_set_genz80_as_sms_standard_map(running_machine &machine, const char* tag, int mapper);
-MACHINE_CONFIG_EXTERN(sms);
-extern void init_sms(running_machine &machine);
-extern void init_smspal(running_machine &machine);
-extern void init_smscm(running_machine &machine);
-extern void init_smsgg(running_machine &machine);
-
-INPUT_PORTS_EXTERN(sms);
-INPUT_PORTS_EXTERN(gamegear);
-extern UINT8* smsgg_backupram;
