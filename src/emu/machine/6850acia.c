77a244cac6ac20a8df85ed79ad8b9103a08c96ce
smf- <smf-@users.noreply.github.com>
2014-03-16 18:08:07 +0000

    fixed excessive logging (nw)

--- src/emu/machine/6850acia.c
+++ src/emu/machine/6850acia.c
@@ -356,9 +356,12 @@ WRITE_LINE_MEMBER( acia6850_device::write_rxc )
 					}
 					else
 					{
-						m_rx_counter = 0;
+						if (m_rx_counter != 1)
+						{
+							if (LOG) logerror("MC6850 '%s': RX FALSE START BIT\n", tag());
+						}
 
-						if (LOG) logerror("MC6850 '%s': RX FALSE START BIT\n", tag());
+						m_rx_counter = 0;
 					}
 					break;
 
