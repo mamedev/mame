076399b3fd660cc4e629f40d36948f80fadaccbd
Olivier Galibert <galibert@pobox.com>
2014-03-27 06:20:49 +0000

    (nw)

--- src/emu/cpu/h8/h8.c
+++ src/emu/cpu/h8/h8.c
@@ -188,8 +188,6 @@ void h8_device::execute_run()
 
 	while(icount > 0) {
 		while(icount > bcount) {
-// TODO: picount is unused since SVN r28981
-//			int picount = inst_state >= 0x10000 && inst_state < 0x20000 ? -100 : icount;
 			if(inst_state < 0x10000) {
 				PPC = NPC;
 				if(machine().debug_flags & DEBUG_FLAG_ENABLED)
