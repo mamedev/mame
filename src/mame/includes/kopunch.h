abdfcb16a45e4255de4a329f55bb0ceb92e92b09
Michaël Banaan Ananas <happppp@users.noreply.github.com>
2014-04-02 22:25:44 +0000

    kopunch has 4 ppi chips

--- src/mame/includes/kopunch.h
+++ src/mame/includes/kopunch.h
@@ -29,17 +29,19 @@ public:
 	UINT8 m_gfxbank;
 	UINT8 m_scrollx;
 
-	DECLARE_READ8_MEMBER(kopunch_in_r);
-	DECLARE_WRITE8_MEMBER(kopunch_lamp_w);
-	DECLARE_WRITE8_MEMBER(kopunch_coin_w);
-	DECLARE_WRITE8_MEMBER(kopunch_fg_w);
-	DECLARE_WRITE8_MEMBER(kopunch_bg_w);
-	DECLARE_WRITE8_MEMBER(kopunch_scroll_x_w);
-	DECLARE_WRITE8_MEMBER(kopunch_scroll_y_w);
-	DECLARE_WRITE8_MEMBER(kopunch_gfxbank_w);
+	DECLARE_READ8_MEMBER(sensors1_r);
+	DECLARE_READ8_MEMBER(sensors2_r);
+	DECLARE_WRITE8_MEMBER(lamp_w);
+	DECLARE_WRITE8_MEMBER(coin_w);
+	DECLARE_WRITE8_MEMBER(vram_fg_w);
+	DECLARE_WRITE8_MEMBER(vram_bg_w);
+	DECLARE_WRITE8_MEMBER(scroll_x_w);
+	DECLARE_WRITE8_MEMBER(scroll_y_w);
+	DECLARE_WRITE8_MEMBER(gfxbank_w);
 
 	DECLARE_INPUT_CHANGED_MEMBER(left_coin_inserted);
 	DECLARE_INPUT_CHANGED_MEMBER(right_coin_inserted);
+	INTERRUPT_GEN_MEMBER(vblank_interrupt);
 
 	TILE_GET_INFO_MEMBER(get_fg_tile_info);
 	TILE_GET_INFO_MEMBER(get_bg_tile_info);
@@ -48,6 +50,4 @@ public:
 
 	virtual void machine_start();
 	virtual void video_start();
-
-	INTERRUPT_GEN_MEMBER(kopunch_interrupt);
 };
