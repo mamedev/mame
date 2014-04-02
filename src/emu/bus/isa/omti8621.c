ef89442025262cebe6e3d614601c8a0aac8ee82d
Miodrag Milanovic <mmicko@gmail.com>
2014-04-02 12:47:09 +0000

    can hide internally used classes (nw)

--- src/emu/bus/isa/omti8621.c
+++ src/emu/bus/isa/omti8621.c
@@ -47,6 +47,50 @@ static int verbose = VERBOSE;
 // forward declaration of image class
 extern const device_type OMTI_DISK;
 
+class omti_disk_image_device :  public device_t,
+								public device_image_interface
+{
+public:
+	// construction/destruction
+	omti_disk_image_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+
+	// image-level overrides
+	virtual iodevice_t image_type() const { return IO_HARDDISK; }
+
+	virtual bool is_readable()  const { return 1; }
+	virtual bool is_writeable() const { return 1; }
+	virtual bool is_creatable() const { return 1; }
+	virtual bool must_be_loaded() const { return 0; }
+	virtual bool is_reset_on_load() const { return 0; }
+	virtual const char *image_interface() const { return NULL; }
+	virtual const char *file_extensions() const { return "awd"; }
+	virtual const option_guide *create_option_guide() const { return NULL; }
+
+	virtual bool call_create(int format_type, option_resolution *format_options);
+protected:
+	// device-level overrides
+	virtual void device_config_complete();
+	virtual void device_start();
+	virtual void device_reset();
+	
+	void omti_disk_config(UINT16 disk_type);
+public:
+	UINT16 m_type;
+	UINT16 m_cylinders;
+	UINT16 m_heads;
+	UINT16 m_sectors;
+	UINT32 m_sectorbytes;
+	UINT32 m_sector_count;
+
+	device_image_interface *m_image;
+
+	// configuration data
+	UINT8 m_config_data[10];
+
+	// ESDI defect list data
+	UINT8 m_esdi_defect_list[256];
+};
+
 /*
  * I/O register offsets
  */
--- src/emu/bus/nubus/nubus_image.h
+++ src/emu/bus/nubus/nubus_image.h
@@ -9,41 +9,7 @@
 //**************************************************************************
 //  TYPE DEFINITIONS
 //**************************************************************************
-
-// messimg_disk_image_device
-
-class messimg_disk_image_device :   public device_t,
-								public device_image_interface
-{
-public:
-	// construction/destruction
-	messimg_disk_image_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
-
-	// image-level overrides
-	virtual iodevice_t image_type() const { return IO_QUICKLOAD; }
-
-	virtual bool is_readable()  const { return 1; }
-	virtual bool is_writeable() const { return 1; }
-	virtual bool is_creatable() const { return 0; }
-	virtual bool must_be_loaded() const { return 0; }
-	virtual bool is_reset_on_load() const { return 0; }
-	virtual const char *image_interface() const { return NULL; }
-	virtual const char *file_extensions() const { return "img"; }
-	virtual const option_guide *create_option_guide() const { return NULL; }
-
-	virtual bool call_load();
-	virtual void call_unload();
-
-	protected:
-	// device-level overrides
-	virtual void device_config_complete();
-	virtual void device_start();
-	virtual void device_reset();
-public:
-	UINT32 m_size;
-	UINT8 *m_data;
-	bool m_ejected;
-};
+class messimg_disk_image_device;
 
 // ======================> nubus_image_device
 
