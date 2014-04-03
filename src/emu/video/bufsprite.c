1b5f24add6a738d6fb8fe96069a5c672769e082d
Miodrag Milanovic <mmicko@gmail.com>
2014-04-03 07:42:26 +0000

    removed some leftovers (nw)

--- src/emu/video/bufsprite.c
+++ src/emu/video/bufsprite.c
@@ -21,19 +21,3 @@ extern const device_type BUFFERED_SPRITERAM8 = &device_creator<buffered_spritera
 extern const device_type BUFFERED_SPRITERAM16 = &device_creator<buffered_spriteram16_device>;
 extern const device_type BUFFERED_SPRITERAM32 = &device_creator<buffered_spriteram32_device>;
 extern const device_type BUFFERED_SPRITERAM64 = &device_creator<buffered_spriteram64_device>;
-
-
-
-/* ----- sprite buffering ----- */
-
-/* buffered sprite RAM write handlers */
-WRITE8_HANDLER( buffer_spriteram_w ) { }
-WRITE16_HANDLER( buffer_spriteram16_w ) { }
-WRITE32_HANDLER( buffer_spriteram32_w ) { }
-WRITE8_HANDLER( buffer_spriteram_2_w ) { }
-WRITE16_HANDLER( buffer_spriteram16_2_w ) { }
-WRITE32_HANDLER( buffer_spriteram32_2_w ) { }
-
-/* perform the actual buffering */
-void buffer_spriteram(running_machine &machine, UINT8 *ptr, int length) { }
-void buffer_spriteram_2(running_machine &machine, UINT8 *ptr, int length) { }
