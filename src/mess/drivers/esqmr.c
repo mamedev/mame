b67f2f5c8a7eb179b948f5385995f5b8502913a6
R. Belmont <rb6502@users.noreply.github.com>
2014-04-06 23:28:39 +0000

    Latest readme (nw)

--- src/mess/drivers/esqmr.c
+++ src/mess/drivers/esqmr.c
@@ -17,10 +17,15 @@
 	Ensoniq, 1995
 
 	This is a 64-voice expandable synth module made by Ensoniq in 1995.
+	It is shipped with 12MB of 16-bit 44.1kHz wave data and can be expanded
+	with up to 3 ENSONIQ EXP Series Wave Expansion Boards containing up to a
+	maximum of 84MB of additional wave data.
+
 
 	PCB Layout
 	----------
 
+	PN: 4001028501 REV C
 	|--------------------------------------------------------|
 	| J6  J7          J12  J13               J21   J23    J22|
 	|                                                      J5|
@@ -72,19 +77,123 @@
 		  J23          - MIDI out connector
 		  J22          - MIDI thru connector
 		  J5           - Power input connector
-		  1370001501   - Unknown TQFP144 IC stamped with the Ensoniq logo. CPLD or gate array.
-		  ESP2         - Ensoniq ESP2 (ES5511) sound chip
-		  OTTOR2       - Ensoniq OTTOR2 (ES5506) sound chip
+		  1370001501   - Unknown TQFP144 IC stamped with the Ensoniq logo. Possibly CPLD?
+		  ESP2         - Ensoniq ESP2 sound chip
+		  OTTOR2       - Ensoniq OTTOR2 sound chip
 		  ROM*         - 4M x8-bit SOP44 mask ROM
-		  D43256       - NEC D43256 32k x8-bit SRAM
+		  D43256       - NEC D43256 32k x8-bit Static RAM
 		  HP_6N138     - HP/Agilent HP 6N138 Low Input Current High Gain Optocoupler
 		  4565         - JRC4565 Dual Operational Amplifier
 		  AD1861       - Analog Devices AD1861 16-bit/18-bit PCM Audio DAC
 		  MC68HC705C4A - Motorola MC68HC705C4A Microcontroller. Clock input is tied to the TQFP CPLD
 		  MC68340      - Motorola MC68340PV16E 68000-compatible 32-bit processor with on-board peripherals. Clock input 6.000MHz
 		  EPROM*       - 27C4001 EPROM
-		  FDT7130      - IDT7130 High Speed 1k x8-bit Dual Port Static RAM
+		  IDT7130      - IDT7130 High Speed 1k x8-bit Dual Port Static RAM
 		  KM681000     - Samsung KM681000 128k x8-bit Static RAM
+		  ADM691       - Analog Devices ADM691 Microprocessor Supervisory Circuit with Automatic Battery Backup Switching
+	
+
+	Additional notes from the manual:
+	---------------------------------
+
+	The MR-Rack can play special demonstration songs to give you an idea of how terrific it sounds.
+
+	To Play the MR-Rack Main Demo
+	1. Press the Audition button, and hold it down.
+	2. While still holding Audition, press the Save button.
+	3. Let go of both buttons.
+
+	In an unexpanded MR-Rack, the display shows:
+
+	Hit ENTER to Play:
+	MAINDEMO:MR Internal
+
+	If you've installed any Expansion boards or a ROM card containing MAINDEMO-type
+	demonstration songs, your display will differ. Turn the Value knob counter-clockwise
+	until the display looks as it does above.
+
+	Note: When MR-Rack demos are being viewed or playing, MIDI In is disabled.
+
+	4. Press Enter to play the demo.
+	5. Press Enter again to stop the demo.
+	6. When you're done listening to the demo song, press Exit to return to normal MR-Rack
+
+
+	The Version Number of Your MR-Rack Operating System:
+	You can easily find out what operating system (or O.S.) your MR-Rack is currently using.
+	To Find the Operating System:
+	1. Press the Save button and hold it down.
+	2. While still holding the Save button, press the System button.
+	The display briefly shows your current Operating System:
+
+	ENSONIQ MR-RACK
+	O.S. Version: #.##
+
+
+
+	ENSONIQ EXP Series Wave Expansion Boards
+	----------------------------------------
+
+	These are small plug-in boards containing a 256k x8-bit EPROM (27C020) and from 1 to 6
+	SOP44 mask ROMs. These add additional digital sound waves. These can be used with the MR
+	Rack, the full-size MR Keyboards and a few other models.
+
+	PCB Layout
+	----------
+
+	PN: 4001033401 REV B
+	|------------------|
+	|                  |
+	|    U6       U7   |
+	|-                 |
+	||                 |
+	||            U1   |
+	||    U2          -|
+	||                ||
+	||                ||
+	||    U3          ||
+	||                ||
+	||             U8 ||
+	||    U4          ||
+	||                ||
+	||                ||
+	||    U5          ||
+	|-                -|
+	|------------------|
+	Notes:
+		  U1       - AC138 logic chip
+		  U2 to U7 - 4M x 8-bit or 2M x 8-bit SOP44 mask ROM
+		  U8       - 27C020 EPROM
+
+	The EXP boards dumped so far are....
+
+							 MROM  # of
+	Name            Version  Size  MROMs
+	-----------------------------------
+	Piano           V1.00    4M    4 (multisampled pianos, aka "The Perfect Piano")
+	The Real World  V1.01    4M    6 (non-Western percusson and other "world music" sounds)
+	Drum            V1.00    2M    1 (additional drum kits that are built in to the MR keyboards)
+
+
+	Additional notes from the manual:
+	---------------------------------
+
+	The MR-Rack provides three displays which can identify any EXP Wave Expansion boards
+	you have installed.
+
+	To Identify an Installed Expansion Board
+	1. Press the System button.
+	2. Turn the Parameter knob until the display shows:
+
+	System parameters:
+	WaveEXP1:xxxxxxxxxxx
+
+	When an Expansion Board is installed, this read-only display will show the name of the
+	Expansion Board located in the first slot.
+
+	3. Turning the Parameter knob two more times will reveal two more displays which show
+	the names of the Expansion Boards in Wave EXP Slots 2 and 3 (if they're installed).
+	If there are no Expansion Boards installed, the display will show "WaveEXP1= **EMPTY**."
  
 ***************************************************************************/
 
