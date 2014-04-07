mamedev/mame

commit 3ffc11138778089771c3923dafc7179eb70fb5ca

i8251: Fixed DSR status bit. (nw)
git-svn-id: svn://dspnet.fr/mame/trunk@28777 749742ba-7341-0410-aadc-df50b521781e

src/emu/bus/rs232/terminal.h
@@ -27,6 +27,8 @@ protected:
 	virtual void rcv_complete();
 	virtual void send_key(UINT8 code);
 
+	virtual DECLARE_WRITE_LINE_MEMBER( input_dtr ) { output_dsr(state); }
+
 private:
 	required_ioport m_io_term_txbaud;
 	required_ioport m_io_term_rxbaud;

src/emu/machine/i8251.c
@@ -696,7 +696,7 @@ WRITE_LINE_MEMBER(i8251_device::write_cts)
 
 WRITE_LINE_MEMBER(i8251_device::write_dsr)
 {
-	m_dsr = state;
+	m_dsr = !state;
 }
 
 WRITE_LINE_MEMBER(i8251_device::write_rxc)

commit c2bd08ec266b7f20c057881b80252ea8a6aa1c90

fixed startup order issue introduced in r28777 [smf]

src/emu/bus/rs232/terminal.h
@@ -27,7 +27,7 @@ protected:
 	virtual void rcv_complete();
 	virtual void send_key(UINT8 code);
 
-	virtual DECLARE_WRITE_LINE_MEMBER( input_dtr ) { output_dsr(state); }
+	virtual DECLARE_WRITE_LINE_MEMBER( input_dtr ) { if (started()) { output_dsr(state); } }
 
 private:
 	required_ioport m_io_term_txbaud;