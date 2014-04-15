1b4f7107a2816f51d6fa2cdbe748710287850c78
Ivan Vangelista <mesgnet@yahoo.it>
2014-04-15 15:36:28 +0000

    cpc_expansion_slot_device: converted to devcb2 (nw)

--- src/emu/bus/cpc/cpc_rom.c
+++ src/emu/bus/cpc/cpc_rom.c
@@ -15,15 +15,6 @@ const device_type CPC_ROM = &device_creator<cpc_rom_device>;
 //  DEVICE CONFIG INTERFACE
 //**************************************************************************
 
-CPC_EXPANSION_INTERFACE(sub_exp_intf)
-{
-	DEVCB_DEVICE_LINE_MEMBER("^^", cpc_expansion_slot_device, irq_w),
-	DEVCB_DEVICE_LINE_MEMBER("^^", cpc_expansion_slot_device, nmi_w),
-	DEVCB_NULL,  // RESET
-	DEVCB_DEVICE_LINE_MEMBER("^^", cpc_expansion_slot_device, romdis_w),  // ROMDIS
-	DEVCB_DEVICE_LINE_MEMBER("^^", cpc_expansion_slot_device, romen_w)  // /ROMEN
-};
-
 // device machine config
 static MACHINE_CONFIG_FRAGMENT( cpc_rom )
 	MCFG_ROMSLOT_ADD("rom1")
@@ -34,7 +25,12 @@ static MACHINE_CONFIG_FRAGMENT( cpc_rom )
 	MCFG_ROMSLOT_ADD("rom6")
 
 	// pass-through
-	MCFG_CPC_EXPANSION_SLOT_ADD("exp",sub_exp_intf,cpc_exp_cards,NULL)
+	MCFG_DEVICE_ADD("exp", CPC_EXPANSION_SLOT, 0)
+	MCFG_DEVICE_SLOT_INTERFACE(cpc_exp_cards, NULL, false)
+	MCFG_CPC_EXPANSION_SLOT_OUT_IRQ_CB(DEVWRITELINE("^", cpc_expansion_slot_device, irq_w))
+	MCFG_CPC_EXPANSION_SLOT_OUT_NMI_CB(DEVWRITELINE("^", cpc_expansion_slot_device, nmi_w))
+	MCFG_CPC_EXPANSION_SLOT_OUT_ROMDIS_CB(DEVWRITELINE("^", cpc_expansion_slot_device, romdis_w))  // ROMDIS
+	MCFG_CPC_EXPANSION_SLOT_OUT_ROMEN_CB(DEVWRITELINE("^", cpc_expansion_slot_device, romen_w))  // /ROMEN
 
 MACHINE_CONFIG_END
 
