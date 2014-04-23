45fe0097ae0e29c6db8a61c9ba1f6a163520fded
Miodrag Milanovic <mmicko@gmail.com>
2014-04-23 08:36:18 +0000

    Fixed mac validation (nw)

--- src/mess/machine/mac.c
+++ src/mess/machine/mac.c
@@ -1650,7 +1650,8 @@ WRITE8_MEMBER(mac_state::mac_via2_out_b)
 
 	if (m_model == MODEL_MAC_II)
 	{
-		m_maincpu->set_hmmu_enable((data & 0x8) ? M68K_HMMU_DISABLE : M68K_HMMU_ENABLE_II);
+		m68000_base_device *m68k = downcast<m68000_base_device *>(m_maincpu.target());
+		m68k->set_hmmu_enable((data & 0x8) ? M68K_HMMU_DISABLE : M68K_HMMU_ENABLE_II);
 	}
 }
 
@@ -1824,7 +1825,8 @@ void mac_state::machine_reset()
 	// default to 32-bit mode on LC
 	if (m_model == MODEL_MAC_LC)
 	{
-		m_maincpu->set_hmmu_enable(M68K_HMMU_DISABLE);
+		m68000_base_device *m68k = downcast<m68000_base_device *>(m_maincpu.target());
+		m68k->set_hmmu_enable(M68K_HMMU_DISABLE);
 	}
 
 	m_last_taken_interrupt = -1;
