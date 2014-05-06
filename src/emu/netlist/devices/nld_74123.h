1f70685652ee48e3a1f978bc6cb52abaefbb13c8
Couriersud <couriersud@users.noreply.github.com>
2014-05-06 22:37:20 +0000

    Added first draft of 74123 to netlist code.

---
+++ src/emu/netlist/devices/nld_74123.h
@@ -0,0 +1,62 @@
+// license:GPL-2.0+
+// copyright-holders:Couriersud
+/*
+ * nld_74123.h
+ *
+ *  74123: PRECISION TIMERS
+ *
+ *           +--------------+
+ *        A1 |1     ++    16| VCC
+ *        B1 |2           15| RC1
+ *      CLR1 |3           14| C1
+ *       Q1Q |4   74123   13| Q1
+ *        Q2 |5           12| Q2Q
+ *        C2 |6           11| CLR2
+ *       RC2 |7           10| B2
+ *       GND |8            9| A2
+ *           +--------------+
+ *
+ *  Naming conventions follow Fairchild Semiconductor datasheet
+ *
+ */
+
+#ifndef NLD_74123_H_
+#define NLD_74123_H_
+
+#include "../nl_base.h"
+#include "../analog/nld_twoterm.h"
+
+#define TTL_74123(_name)                                                        \
+		NET_REGISTER_DEV(74123, _name)
+
+
+NETLIB_DEVICE(74123,
+public:
+	NETLIB_NAME(R) m_RP;
+	NETLIB_NAME(R) m_RN;
+
+	netlist_ttl_input_t m_A;
+	netlist_ttl_input_t m_B;
+	netlist_ttl_input_t m_CLRQ;
+	netlist_ttl_output_t m_Q;
+    netlist_ttl_output_t m_QQ;
+
+    netlist_analog_input_t m_CV;
+
+	netlist_sig_t m_last_trig;
+	UINT8         m_state;
+
+);
+
+#define TTL_74123_DIP(_name)                                                         \
+		NET_REGISTER_DEV(74123_dip, _name)
+
+NETLIB_DEVICE(74123_dip,
+
+    NETLIB_NAME(74123) m_1;
+    NETLIB_NAME(74123) m_2;
+
+);
+
+
+#endif /* NLD_74123_H_ */
