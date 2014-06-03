8a0691813194736219bed2fe3fc03e5717e57cb5
mahlemiut <mahlemiut@users.noreply.github.com>
2014-06-03 07:12:54 +0000

    revert r30583, it breaks -ramsize option (no whatsnew)

--- src/emu/clifront.c
+++ src/emu/clifront.c
@@ -197,8 +197,6 @@ int cli_frontend::execute(int argc, char **argv)
 				throw emu_fatalerror(MAMERR_FATALERROR, NULL);
 			}
 		}
-		
-		m_options.parse_standard_inis(option_errors);
 		// parse the command line, adding any system-specific options
 		if (!m_options.parse_command_line(argc, argv, option_errors))
 		{
