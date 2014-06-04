3c3568fcbbaea0f7528a3bd4b327ded7298e2d18
Curt Coder <curtcoder@mail.com>
2014-06-04 14:58:33 +0000

    (MESS) Fixed rom size. (nw)

--- src/emu/bus/ql/mp_fdi.c
+++ src/emu/bus/ql/mp_fdi.c
@@ -25,10 +25,10 @@ const device_type MICRO_PERIPHERALS_FLOPPY_DISK_INTERFACE = &device_creator<micr
 //-------------------------------------------------
 
 ROM_START( micro_peripherals_floppy_disk_interface )
-	ROM_REGION( 0x8000, "rom", 0 )
+	ROM_REGION( 0x2000, "rom", 0 )
 	ROM_DEFAULT_BIOS("v53e")
 	ROM_SYSTEM_BIOS( 0, "v53e", "v5.3E" )
-	ROMX_LOAD( "microp_disk system_v5.3e_1985.rom", 0x0000, 0x8000, CRC(9a8d8fa7) SHA1(f9f5e5d55f3046f63b4eae59222b81290d626e72), ROM_BIOS(1) )
+	ROMX_LOAD( "microp_disk system_v5.3e_1985.rom", 0x0000, 0x2000, CRC(9a8d8fa7) SHA1(f9f5e5d55f3046f63b4eae59222b81290d626e72), ROM_BIOS(1) )
 ROM_END
 
 
