b1a96bf2cc9e6b93c18b9bcc2fe179f43d9e4a4e
Curt Coder <curtcoder@mail.com>
2014-06-10 17:21:09 +0000

    (MESS) ql: Added preliminary CST Q+4 emulation. [Curt Coder]

--- src/emu/bus/ql/cst_q_plus4.h
+++ src/emu/bus/ql/cst_q_plus4.h
@@ -15,6 +15,7 @@
 #define __CST_Q_PLUS4__
 
 #include "exp.h"
+#include "machine/6821pia.h"
 
 
 
@@ -34,6 +35,12 @@ public:
 
 	// optional information overrides
 	virtual const rom_entry *device_rom_region() const;
+	virtual machine_config_constructor device_mconfig_additions() const;
+
+	DECLARE_WRITE_LINE_MEMBER( exp1_extintl_w ) { m_exp1_extinl = state; update_extintl(); }
+	DECLARE_WRITE_LINE_MEMBER( exp2_extintl_w ) { m_exp2_extinl = state; update_extintl(); }
+	DECLARE_WRITE_LINE_MEMBER( exp3_extintl_w ) { m_exp3_extinl = state; update_extintl(); }
+	DECLARE_WRITE_LINE_MEMBER( exp4_extintl_w ) { m_exp4_extinl = state; update_extintl(); }
 
 protected:
 	// device-level overrides
@@ -44,6 +51,18 @@ protected:
 	virtual void write(address_space &space, offs_t offset, UINT8 data);
 
 private:
+	void update_extintl() { m_slot->extintl_w(m_exp1_extinl || m_exp2_extinl || m_exp3_extinl || m_exp4_extinl); }
+
+	required_device<ql_expansion_slot_t> m_exp1;
+	required_device<ql_expansion_slot_t> m_exp2;
+	required_device<ql_expansion_slot_t> m_exp3;
+	required_device<ql_expansion_slot_t> m_exp4;
+	required_memory_region m_rom;
+
+	int m_exp1_extinl;
+	int m_exp2_extinl;
+	int m_exp3_extinl;
+	int m_exp4_extinl;
 };
 
 
