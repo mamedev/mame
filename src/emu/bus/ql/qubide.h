9ba193ef5f38ce8c6dffe50aa4070cbd49b3c133
Curt Coder <curtcoder@mail.com>
2014-06-12 18:34:19 +0000

    (MESS) ql: Qubide WIP. (nw)

--- src/emu/bus/ql/qubide.h
+++ src/emu/bus/ql/qubide.h
@@ -15,6 +15,7 @@
 #define __QUBIDE__
 
 #include "exp.h"
+#include "machine/ataintf.h"
 
 
 
@@ -34,16 +35,25 @@ public:
 
 	// optional information overrides
 	virtual const rom_entry *device_rom_region() const;
+	virtual machine_config_constructor device_mconfig_additions() const;
+	virtual ioport_constructor device_input_ports() const;
 
 protected:
 	// device-level overrides
 	virtual void device_start();
+	virtual void device_reset();
 
 	// device_ql_expansion_card_interface overrides
 	virtual UINT8 read(address_space &space, offs_t offset, UINT8 data);
 	virtual void write(address_space &space, offs_t offset, UINT8 data);
 
 private:
+	required_device<ata_interface_device> m_ata;
+	required_memory_region m_rom;
+	required_ioport m_j1_j5;
+
+	offs_t m_base;
+	UINT16 m_ata_data;
 };
 
 
