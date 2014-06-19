a98376d18b25a96c02c60afb2234a9096cea7eff
Scott Stone <tafoid@users.noreply.github.com>
2014-06-19 12:05:23 +0000

    Gave legacy device a proper setname to eliminate confusion with modern implementation (nw)

--- src/emu/bus/ti99_peb/hfdc.c
+++ src/emu/bus/ti99_peb/hfdc.c
@@ -355,7 +355,7 @@ const device_type TI99_HFDC = &device_creator<myarc_hfdc_device>;
 #define LOG logerror
 
 myarc_hfdc_legacy_device::myarc_hfdc_legacy_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock)
-	: ti_expansion_card_device(mconfig, TI99_HFDC_LEG, "Myarc Hard and Floppy Disk Controller LEGACY", tag, owner, clock, "ti99_hfdc", __FILE__),
+	: ti_expansion_card_device(mconfig, TI99_HFDC_LEG, "Myarc Hard and Floppy Disk Controller LEGACY", tag, owner, clock, "ti99_hfdc_leg", __FILE__),
 		m_hdc9234(*this, FDC_TAG),
 		m_clock(*this, CLOCK_TAG)
 {
