bf43a3db239df8ca7f4e23ad661ad98a3e3aecf6
Michaël Banaan Ananas <happppp@users.noreply.github.com>
2014-06-20 23:41:14 +0000

    tmpz84c011c small cleanup. note: if you want a handler for logging unmapped read/write, put it in the driver machine config, like we do with 8255, for example: MCFG_I8255_OUT_PORTB_CB(LOGGER("PPI8255 - unmapped write port B", 0))

--- src/emu/cpu/z80/tmpz84c011.h
+++ src/emu/cpu/z80/tmpz84c011.h
@@ -1,39 +1,48 @@
+/***************************************************************************
+
+    Toshiba TMPZ84C011, TLCS-Z80 ASSP Family
+    Z80 CPU, CTC, CGC(6/8MHz), I/O8x5
+
+***************************************************************************/
 
 #include "emu.h"
 #include "z80.h"
 
+
+// TMPZ84C011 PIO callbacks
 #define MCFG_TMPZ84C011_PORTA_READ_CB(_devcb) \
-	devcb = &tmpz84c011_device::set_inports0_cb(*device, DEVCB_##_devcb);
+	devcb = &tmpz84c011_device::set_inportsa_cb(*device, DEVCB_##_devcb);
 
 #define MCFG_TMPZ84C011_PORTB_READ_CB(_devcb) \
-	devcb = &tmpz84c011_device::set_inports1_cb(*device, DEVCB_##_devcb);
+	devcb = &tmpz84c011_device::set_inportsb_cb(*device, DEVCB_##_devcb);
 
 #define MCFG_TMPZ84C011_PORTC_READ_CB(_devcb) \
-	devcb = &tmpz84c011_device::set_inports2_cb(*device, DEVCB_##_devcb);
+	devcb = &tmpz84c011_device::set_inportsc_cb(*device, DEVCB_##_devcb);
 
 #define MCFG_TMPZ84C011_PORTD_READ_CB(_devcb) \
-	devcb = &tmpz84c011_device::set_inports3_cb(*device, DEVCB_##_devcb);
+	devcb = &tmpz84c011_device::set_inportsd_cb(*device, DEVCB_##_devcb);
 
 #define MCFG_TMPZ84C011_PORTE_READ_CB(_devcb) \
-	devcb = &tmpz84c011_device::set_inports4_cb(*device, DEVCB_##_devcb);
+	devcb = &tmpz84c011_device::set_inportse_cb(*device, DEVCB_##_devcb);
 
 
 #define MCFG_TMPZ84C011_PORTA_WRITE_CB(_devcb) \
-	devcb = &tmpz84c011_device::set_outports0_cb(*device, DEVCB_##_devcb);
+	devcb = &tmpz84c011_device::set_outportsa_cb(*device, DEVCB_##_devcb);
 
 #define MCFG_TMPZ84C011_PORTB_WRITE_CB(_devcb) \
-	devcb = &tmpz84c011_device::set_outports1_cb(*device, DEVCB_##_devcb);
+	devcb = &tmpz84c011_device::set_outportsb_cb(*device, DEVCB_##_devcb);
 
 #define MCFG_TMPZ84C011_PORTC_WRITE_CB(_devcb) \
-	devcb = &tmpz84c011_device::set_outports2_cb(*device, DEVCB_##_devcb);
+	devcb = &tmpz84c011_device::set_outportsc_cb(*device, DEVCB_##_devcb);
 
 #define MCFG_TMPZ84C011_PORTD_WRITE_CB(_devcb) \
-	devcb = &tmpz84c011_device::set_outports3_cb(*device, DEVCB_##_devcb);
+	devcb = &tmpz84c011_device::set_outportsd_cb(*device, DEVCB_##_devcb);
 
 #define MCFG_TMPZ84C011_PORTE_WRITE_CB(_devcb) \
-	devcb = &tmpz84c011_device::set_outports4_cb(*device, DEVCB_##_devcb);
+	devcb = &tmpz84c011_device::set_outportse_cb(*device, DEVCB_##_devcb);
 
 
+// CTC callbacks
 #define MCFG_TMPZ84C011_Z80CTC_INTR_CB(_devcb) \
 	devcb = &tmpz84c011_device::set_intr_callback(*device, DEVCB_##_devcb);
 
@@ -52,58 +61,43 @@ class tmpz84c011_device : public z80_device
 public:
 	tmpz84c011_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32);
 
-	template<class _Object> static devcb_base & set_outports0_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_outports0.set_callback(object); }
-	template<class _Object> static devcb_base & set_outports1_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_outports1.set_callback(object); }
-	template<class _Object> static devcb_base & set_outports2_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_outports2.set_callback(object); }
-	template<class _Object> static devcb_base & set_outports3_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_outports3.set_callback(object); }
-	template<class _Object> static devcb_base & set_outports4_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_outports4.set_callback(object); }
+	template<class _Object> static devcb_base & set_outportsa_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_outportsa.set_callback(object); }
+	template<class _Object> static devcb_base & set_outportsb_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_outportsb.set_callback(object); }
+	template<class _Object> static devcb_base & set_outportsc_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_outportsc.set_callback(object); }
+	template<class _Object> static devcb_base & set_outportsd_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_outportsd.set_callback(object); }
+	template<class _Object> static devcb_base & set_outportse_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_outportse.set_callback(object); }
 
-	template<class _Object> static devcb_base & set_inports0_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_inports0.set_callback(object); }
-	template<class _Object> static devcb_base & set_inports1_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_inports1.set_callback(object); }
-	template<class _Object> static devcb_base & set_inports2_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_inports2.set_callback(object); }
-	template<class _Object> static devcb_base & set_inports3_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_inports3.set_callback(object); }
-	template<class _Object> static devcb_base & set_inports4_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_inports4.set_callback(object); }
+	template<class _Object> static devcb_base & set_inportsa_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_inportsa.set_callback(object); }
+	template<class _Object> static devcb_base & set_inportsb_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_inportsb.set_callback(object); }
+	template<class _Object> static devcb_base & set_inportsc_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_inportsc.set_callback(object); }
+	template<class _Object> static devcb_base & set_inportsd_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_inportsd.set_callback(object); }
+	template<class _Object> static devcb_base & set_inportse_cb(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_inportse.set_callback(object); }
 
 	template<class _Object> static devcb_base &set_intr_callback(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_intr_cb.set_callback(object); }
 	template<class _Object> static devcb_base &set_zc0_callback(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_zc0_cb.set_callback(object); }
 	template<class _Object> static devcb_base &set_zc1_callback(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_zc1_cb.set_callback(object); }
 	template<class _Object> static devcb_base &set_zc2_callback(device_t &device, _Object object) { return downcast<tmpz84c011_device &>(device).m_zc2_cb.set_callback(object); }
 
-
-	DECLARE_READ8_MEMBER(tmpz84c011_pio_r);
-	DECLARE_WRITE8_MEMBER(tmpz84c011_pio_w);
-	DECLARE_READ8_MEMBER(tmpz84c011_0_pa_r);
-	DECLARE_READ8_MEMBER(tmpz84c011_0_pb_r);
-	DECLARE_READ8_MEMBER(tmpz84c011_0_pc_r);
-	DECLARE_READ8_MEMBER(tmpz84c011_0_pd_r);
-	DECLARE_READ8_MEMBER(tmpz84c011_0_pe_r);
-	DECLARE_WRITE8_MEMBER(tmpz84c011_0_pa_w);
-	DECLARE_WRITE8_MEMBER(tmpz84c011_0_pb_w);
-	DECLARE_WRITE8_MEMBER(tmpz84c011_0_pc_w);
-	DECLARE_WRITE8_MEMBER(tmpz84c011_0_pd_w);
-	DECLARE_WRITE8_MEMBER(tmpz84c011_0_pe_w);
-	DECLARE_READ8_MEMBER(tmpz84c011_0_dir_pa_r);
-	DECLARE_READ8_MEMBER(tmpz84c011_0_dir_pb_r);
-	DECLARE_READ8_MEMBER(tmpz84c011_0_dir_pc_r);
-	DECLARE_READ8_MEMBER(tmpz84c011_0_dir_pd_r);
-	DECLARE_READ8_MEMBER(tmpz84c011_0_dir_pe_r);
-	DECLARE_WRITE8_MEMBER(tmpz84c011_0_dir_pa_w);
-	DECLARE_WRITE8_MEMBER(tmpz84c011_0_dir_pb_w);
-	DECLARE_WRITE8_MEMBER(tmpz84c011_0_dir_pc_w);
-	DECLARE_WRITE8_MEMBER(tmpz84c011_0_dir_pd_w);
-	DECLARE_WRITE8_MEMBER(tmpz84c011_0_dir_pe_w);
-
-	DECLARE_READ8_MEMBER(porta_default_r);
-	DECLARE_READ8_MEMBER(portb_default_r);
-	DECLARE_READ8_MEMBER(portc_default_r);
-	DECLARE_READ8_MEMBER(portd_default_r);
-	DECLARE_READ8_MEMBER(porte_default_r);
-
-	DECLARE_WRITE8_MEMBER(porta_default_w);
-	DECLARE_WRITE8_MEMBER(portb_default_w);
-	DECLARE_WRITE8_MEMBER(portc_default_w);
-	DECLARE_WRITE8_MEMBER(portd_default_w);
-	DECLARE_WRITE8_MEMBER(porte_default_w);
+	DECLARE_READ8_MEMBER(tmpz84c011_pa_r);
+	DECLARE_READ8_MEMBER(tmpz84c011_pb_r);
+	DECLARE_READ8_MEMBER(tmpz84c011_pc_r);
+	DECLARE_READ8_MEMBER(tmpz84c011_pd_r);
+	DECLARE_READ8_MEMBER(tmpz84c011_pe_r);
+	DECLARE_WRITE8_MEMBER(tmpz84c011_pa_w);
+	DECLARE_WRITE8_MEMBER(tmpz84c011_pb_w);
+	DECLARE_WRITE8_MEMBER(tmpz84c011_pc_w);
+	DECLARE_WRITE8_MEMBER(tmpz84c011_pd_w);
+	DECLARE_WRITE8_MEMBER(tmpz84c011_pe_w);
+	DECLARE_READ8_MEMBER(tmpz84c011_dir_pa_r);
+	DECLARE_READ8_MEMBER(tmpz84c011_dir_pb_r);
+	DECLARE_READ8_MEMBER(tmpz84c011_dir_pc_r);
+	DECLARE_READ8_MEMBER(tmpz84c011_dir_pd_r);
+	DECLARE_READ8_MEMBER(tmpz84c011_dir_pe_r);
+	DECLARE_WRITE8_MEMBER(tmpz84c011_dir_pa_w);
+	DECLARE_WRITE8_MEMBER(tmpz84c011_dir_pb_w);
+	DECLARE_WRITE8_MEMBER(tmpz84c011_dir_pc_w);
+	DECLARE_WRITE8_MEMBER(tmpz84c011_dir_pd_w);
+	DECLARE_WRITE8_MEMBER(tmpz84c011_dir_pe_w);
 
 	DECLARE_WRITE_LINE_MEMBER(intr_cb_trampoline_w);
 	DECLARE_WRITE_LINE_MEMBER(zc0_cb_trampoline_w);
@@ -127,28 +121,26 @@ protected:
 		}
 	}
 
-
+private:
 	UINT8 m_pio_dir[5];
 	UINT8 m_pio_latch[5];
 
-private:
-	devcb_write8      m_outports0;
-	devcb_write8      m_outports1;
-	devcb_write8      m_outports2;
-	devcb_write8      m_outports3;
-	devcb_write8      m_outports4;
+	devcb_write8      m_outportsa;
+	devcb_write8      m_outportsb;
+	devcb_write8      m_outportsc;
+	devcb_write8      m_outportsd;
+	devcb_write8      m_outportse;
 
-	devcb_read8       m_inports0;
-	devcb_read8       m_inports1;
-	devcb_read8       m_inports2;
-	devcb_read8       m_inports3;
-	devcb_read8       m_inports4;
+	devcb_read8       m_inportsa;
+	devcb_read8       m_inportsb;
+	devcb_read8       m_inportsc;
+	devcb_read8       m_inportsd;
+	devcb_read8       m_inportse;
 
 	devcb_write_line   m_intr_cb;              // interrupt callback
 	devcb_write_line   m_zc0_cb;               // channel 0 zero crossing callbacks
 	devcb_write_line   m_zc1_cb;               // channel 1 zero crossing callbacks
 	devcb_write_line   m_zc2_cb;               // channel 2 zero crossing callbacks
-
 };
 
 extern const device_type TMPZ84C011;
