99378e4319ab06d0741212d0ec0192084b6fb9b3
Wilbert Pol <wilbert@jdg.info>
2014-06-23 18:18:55 +0000

    (MESS) msx.c: Updated bm_012 cartridge to use the tmpz84c015 implementation. (nw)

--- src/emu/bus/msx_cart/bm_012.c
+++ src/emu/bus/msx_cart/bm_012.c
@@ -23,10 +23,7 @@ const device_type MSX_CART_BM_012 = &device_creator<msx_cart_bm_012>;
 msx_cart_bm_012::msx_cart_bm_012(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock)
 	: device_t(mconfig, MSX_CART_BM_012, "MSX Cartridge - BM-012", tag, owner, clock, "msx_cart_bm_012", __FILE__)
 	, msx_cart_interface(mconfig, *this)
-	, m_tmpz84c015af_pio(*this, "tmpz84_pio")
-	, m_tmpz84c015af_ctc(*this, "tmpz84_ctc")
-	, m_tmpz84c015af_sio(*this, "tmpz84_sio")
-	, m_irq_priority(0)
+	, m_tmpz84c015af(*this, "tmpz84c015af")
 	, m_bm012_pio(*this, "bm012_pio")
 	, m_mdthru(*this, "mdthru")
 {
@@ -39,34 +36,6 @@ static ADDRESS_MAP_START( bm_012_memory_map, AS_PROGRAM, 8, msx_cart_bm_012 )
 ADDRESS_MAP_END
 
 
-static ADDRESS_MAP_START( bm_012_io_map, AS_IO, 8, msx_cart_bm_012 )
-	ADDRESS_MAP_UNMAP_HIGH
-	ADDRESS_MAP_GLOBAL_MASK(0xff)
-
-	// 10-13 - CTC channels 0-3
-	AM_RANGE(0x10, 0x13) AM_DEVREADWRITE("tmpz84_ctc", z80ctc_device, read, write)
-
-	// 18-1B - SIO
-	AM_RANGE(0x18, 0x1b) AM_DEVREADWRITE("tmpz84_sio", z80dart_device, ba_cd_r, ba_cd_w)
-
-	// 1C-1F - PIO
-	AM_RANGE(0x1c, 0x1f) AM_DEVREADWRITE("tmpz84_pio", z80pio_device, read_alt, write_alt)
-
-	// F0-F1 - WDT
-	// F4    - IRQ priority
-	AM_RANGE(0xf4, 0xf4) AM_WRITE(tmpz84c015af_f4_w)
-ADDRESS_MAP_END
-
-
-static const z80_daisy_config bm_012_daisy_chain[] =
-{
-	{ "tmpz84_pio" },
-	{ "tmpz84_sio" },
-	{ "tmpz84_ctc" },
-	{ NULL }
-};
-
-
 static MACHINE_CONFIG_FRAGMENT( msx_cart_bm_012 )
 	// 12MHz XTAL @ X1
 	// Toshiba TMPZ84C015AF-6 (@U5) components:
@@ -76,34 +45,25 @@ static MACHINE_CONFIG_FRAGMENT( msx_cart_bm_012 )
 	// - PIO
 	// - CGC
 	// - WDT
-	MCFG_CPU_ADD("tmpz84_cpu", Z80, XTAL_12MHz/2)         /* 6 MHz */
+	MCFG_CPU_ADD("tmpz84c015af", TMPZ84C015, XTAL_12MHz/2)         /* 6 MHz */
 	MCFG_CPU_PROGRAM_MAP(bm_012_memory_map)
-	MCFG_CPU_IO_MAP(bm_012_io_map)
-	MCFG_CPU_CONFIG(bm_012_daisy_chain)
-
-	MCFG_DEVICE_ADD("tmpz84_pio", Z80PIO, XTAL_12MHz/2)
-	MCFG_Z80PIO_OUT_INT_CB(INPUTLINE("tmpz84_cpu", INPUT_LINE_IRQ0))
-	MCFG_Z80PIO_IN_PA_CB(DEVREAD8("bm012_pio", z80pio_device, pa_r))
-	MCFG_Z80PIO_OUT_PA_CB(DEVWRITE8("bm012_pio", z80pio_device, pa_w))
-	MCFG_Z80PIO_IN_PB_CB(DEVREAD8("bm012_pio", z80pio_device, pb_r))
-	MCFG_Z80PIO_OUT_PB_CB(DEVWRITE8("bm012_pio", z80pio_device, pb_w))
-	MCFG_Z80PIO_OUT_BRDY_CB(DEVWRITELINE("bm012_pio", z80pio_device, strobe_b))
-
-	MCFG_DEVICE_ADD("tmpz84_ctc", Z80CTC, XTAL_12MHz/2)
-	MCFG_Z80CTC_INTR_CB(INPUTLINE("tmpz84_cpu", INPUT_LINE_IRQ0))
-
-	MCFG_Z80SIO0_ADD("tmpz84_sio", XTAL_12MHz/2, 0, 0, 0, 0)
-	MCFG_Z80DART_OUT_INT_CB(INPUTLINE("tmpz84_cpu", INPUT_LINE_IRQ0))
-	MCFG_Z80DART_OUT_TXDA_CB(DEVWRITELINE("mdout", midi_port_device, write_txd))
+	// PIO callbacks
+	MCFG_TMPZ84C015_IN_PA_CB(DEVREAD8("bm012_pio", z80pio_device, pa_r))
+	MCFG_TMPZ84C015_OUT_PA_CB(DEVWRITE8("bm012_pio", z80pio_device, pa_w))
+	MCFG_TMPZ84C015_IN_PB_CB(DEVREAD8("bm012_pio", z80pio_device, pb_r))
+	MCFG_TMPZ84C015_OUT_PB_CB(DEVWRITE8("bm012_pio", z80pio_device, pb_w))
+	MCFG_TMPZ84C015_OUT_BRDY_CB(DEVWRITELINE("bm012_pio", z80pio_device, strobe_b))
+	// SIO callbacks
+	MCFG_TMPZ84C015_OUT_TXDA_CB(DEVWRITELINE("mdout", midi_port_device, write_txd))
 
 	// Sony CXK5864BSP-10L  (8KB ram)
 	// Sharp LH0081A Z80A-PIO-0 - For communicating between the MSX and the TMP
 	MCFG_DEVICE_ADD("bm012_pio", Z80PIO, XTAL_3_579545MHz)  // ?????
-	MCFG_Z80PIO_OUT_PA_CB(DEVWRITE8("tmpz84_pio", z80pio_device, pa_w))
-	MCFG_Z80PIO_IN_PA_CB(DEVREAD8("tmpz84_pio", z80pio_device, pa_r))
-	MCFG_Z80PIO_OUT_PB_CB(DEVWRITE8("tmpz84_pio", z80pio_device, pb_w))
-	MCFG_Z80PIO_IN_PB_CB(DEVREAD8("tmpz84_pio", z80pio_device, pb_r))
-	MCFG_Z80PIO_OUT_BRDY_CB(DEVWRITELINE("tmpz84_pio", z80pio_device, strobe_b))
+	MCFG_Z80PIO_OUT_PA_CB(DEVWRITE8("tmpz84c015af", tmpz84c015_device, pa_w))
+	MCFG_Z80PIO_IN_PA_CB(DEVREAD8("tmpz84c015af", tmpz84c015_device, pa_r))
+	MCFG_Z80PIO_OUT_PB_CB(DEVWRITE8("tmpz84c015af", tmpz84c015_device, pb_w))
+	MCFG_Z80PIO_IN_PB_CB(DEVREAD8("tmpz84c015af", tmpz84c015_device, pb_r))
+	MCFG_Z80PIO_OUT_BRDY_CB(DEVWRITELINE("tmpz84c015af", tmpz84c015_device, strobe_b))
 
 	// MIDI ports
 	MCFG_MIDI_PORT_ADD("mdin", midiin_slot, "midiin")
@@ -122,7 +82,7 @@ machine_config_constructor msx_cart_bm_012::device_mconfig_additions() const
 
 
 ROM_START( msx_cart_bm_012 )
-	ROM_REGION(0x8000, "tmpz84_cpu", 0)
+	ROM_REGION(0x8000, "tmpz84c015af", 0)
 	// The rom chip at U4 is a 27256, but it contains the same 8KB duplicated 4 times
 	ROM_LOAD("midi_v1.00.u4", 0x0, 0x8000, CRC(840c9e74) SHA1(6d07637ad3a61b509221ed4650eed18442371010))
 ROM_END
@@ -143,21 +103,10 @@ void msx_cart_bm_012::device_start()
 }
 
 
-void msx_cart_bm_012::device_reset()
-{
-}
-
-
-WRITE8_MEMBER(msx_cart_bm_012::tmpz84c015af_f4_w)
-{
-	m_irq_priority = data;
-}
-
-
 WRITE_LINE_MEMBER(msx_cart_bm_012::midi_in)
 {
 	m_mdthru->write_txd(state);
-	m_tmpz84c015af_sio->rxb_w(state);
+	m_tmpz84c015af->rxb_w(state);
 }
 
 
--- src/emu/bus/msx_cart/bm_012.h
+++ src/emu/bus/msx_cart/bm_012.h
@@ -2,9 +2,7 @@
 #define __MSX_CART_BM_012_H
 
 #include "bus/msx_cart/cartridge.h"
-#include "machine/z80pio.h"
-#include "machine/z80ctc.h"
-#include "machine/z80dart.h"
+#include "cpu/z80/tmpz84c015.h"
 #include "bus/midi/midi.h"
 
 
@@ -21,18 +19,11 @@ public:
 	virtual machine_config_constructor device_mconfig_additions() const;
 	virtual const rom_entry *device_rom_region() const;
 	virtual void device_start();
-	virtual void device_reset();
 
-	DECLARE_WRITE8_MEMBER(tmpz84c015af_f4_w);
 	DECLARE_WRITE_LINE_MEMBER(midi_in);
 
 private:
-	// TMPZ84C015AF related
-	required_device<z80pio_device> m_tmpz84c015af_pio;
-	required_device<z80ctc_device> m_tmpz84c015af_ctc;
-	required_device<z80dart_device> m_tmpz84c015af_sio;
-	UINT8 m_irq_priority;
-
+	required_device<tmpz84c015_device> m_tmpz84c015af;
 	required_device<z80pio_device> m_bm012_pio;
 	required_device<midi_port_device> m_mdthru;
 };
