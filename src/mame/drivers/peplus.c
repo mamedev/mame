a7cf202315999c5c0b900eef88eb50ffa4646641
Brian Troha <briantro@users.noreply.github.com>
2014-07-10 02:57:46 +0000

    peplus.c: Yet another slight update in documentation - NW

--- src/mame/drivers/peplus.c
+++ src/mame/drivers/peplus.c
@@ -2042,7 +2042,7 @@ ROM_START( pepp0083 ) /* Normal board : Tens or Better (PP0083) */
 /*
 PayTable  10s+  2PR  3K   STR  FL  FH  4K  SF  RF  (Bonus)
 ----------------------------------------------------------
-  P39D     1     1    3    5    7  11  50 100 250    940
+  P39D     1     1    3    5    7  11  50 100 300    940
   % Range: 90.6-92.6%  Optimum: 94.6%  Hit Frequency: 48.6%
      Programs Available: PP0083
 */
@@ -4022,8 +4022,8 @@ Overall average for the 4 of a Kind bonus is 173.35 credits
 	ROM_LOAD( "mgo-cg2xxx.u73",  0x08000, 0x8000, NO_DUMP )
 	ROM_LOAD( "mbo-cg2xxx.u74",  0x10000, 0x8000, NO_DUMP )
 	ROM_LOAD( "mxo-cg2xxx.u75",  0x18000, 0x8000, NO_DUMP )
-	ROM_LOAD( "mro-cg2004.u72",  0x00000, 0x8000, CRC(e5e40ea5) SHA1(e0d9e50b30cc0c25c932b2bf444990df1fb2c38c) ) /* WRONG CG set!! MAX Bet 4K "BONUS" graphics is missing!! */
-	ROM_LOAD( "mgo-cg2004.u73",  0x08000, 0x8000, CRC(12607f1e) SHA1(248e1ecee4e735f5943c50f8c350ca95b81509a7) )
+	ROM_LOAD( "mro-cg2004.u72",  0x00000, 0x8000, CRC(e5e40ea5) SHA1(e0d9e50b30cc0c25c932b2bf444990df1fb2c38c) ) /* WRONG CG set!! MAX Bet 4K "BONUS" graphics is missing and */
+	ROM_LOAD( "mgo-cg2004.u73",  0x08000, 0x8000, CRC(12607f1e) SHA1(248e1ecee4e735f5943c50f8c350ca95b81509a7) ) /* all treasure chest graphics missing for bonus & attract screens */
 	ROM_LOAD( "mbo-cg2004.u74",  0x10000, 0x8000, CRC(78c3fb9f) SHA1(2b9847c511888de507a008dec981778ca4dbcd6c) )
 	ROM_LOAD( "mxo-cg2004.u75",  0x18000, 0x8000, CRC(5aaa4480) SHA1(353c4ce566c944406fce21f2c5045c856ef7a609) )
 
@@ -8211,8 +8211,9 @@ Known Wingboard compatible program roms:
    XMP00026 - Spanish
    XMP00030
 
-XMP00017 is not compatible with:
- Lucky Deal Poker, Shockwave Poker or Ace$ Bonus Poker
+Wingboard programs are not compatible with:
+ Lucky Deal Poker, Shockwave Poker, Ace$ Bonus Poker, Dealt Deuces Bonus, Barbaric Deuces, Pay the Ace (No Face)
+ and many other "specialty" poker games.
 
 The CG2298 graphics can support the following XnnnnnnP Data game types:
 
