f77740b23e714df1c33ff6deb2020b67148ab6ea
Wilbert Pol <wilbert@jdg.info>
2014-09-13 18:39:42 +0000

    (MESS) msx.c: Added a small workaround to support systems with 8KB RAM.

--- src/emu/bus/msx_slot/ram.h
+++ src/emu/bus/msx_slot/ram.h
@@ -5,11 +5,18 @@
 
 #define MCFG_MSX_SLOT_RAM_ADD(_tag, _startpage, _numpages) \
 	MCFG_MSX_INTERNAL_SLOT_ADD(_tag, MSX_SLOT_RAM, _startpage, _numpages)
+
+#define MCFG_MSX_SLOT_RAM_8KB \
+	msx_slot_ram_device::force_start_address(*device, 0xe000);
+
+
 class msx_slot_ram_device : public device_t,
 							public msx_internal_slot_interface
 {
 public:
 	msx_slot_ram_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+
+	static void force_start_address(device_t &device, UINT16 start) { downcast<msx_slot_ram_device &>(device).m_start_address = start; }
 
 	virtual void device_start();
 
@@ -20,6 +27,8 @@ private:
 	dynamic_array<UINT8> m_ram;
 };
 
+
 extern const device_type MSX_SLOT_RAM;
+
 
 #endif
