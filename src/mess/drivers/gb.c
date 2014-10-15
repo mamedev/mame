mamedev/mame

commit e9e513f506ca2e2f8c8592a5e5db0d8f6144a6f7

Changed LR35902 configuration and tagmap lookup reduction for gameboy.c (nw)
git-svn-id: svn://dspnet.fr/mame/trunk@20641 749742ba-7341-0410-aadc-df50b521781e

src/mess/drivers/gb.c
@@ -453,12 +453,6 @@ space. This mapper uses 32KB sized banks.
 static const UINT16 mgb_cpu_regs[6] = { 0xFFB0, 0x0013, 0x00D8, 0x014D, 0xFFFE, 0x0100 };   /* Game Boy Pocket / Super Game Boy 2 */
 static const UINT16 megaduck_cpu_regs[6] = { 0x0000, 0x0000, 0x0000, 0x0000, 0xFFFE, 0x0000 };  /* Megaduck */
 
-static const struct lr35902_config dmg_cpu_reset = { NULL, LR35902_FEATURE_HALT_BUG, gb_timer_callback };
-static const struct lr35902_config sgb_cpu_reset = { NULL, LR35902_FEATURE_HALT_BUG, gb_timer_callback };
-static const struct lr35902_config mgb_cpu_reset = { mgb_cpu_regs, LR35902_FEATURE_HALT_BUG, gb_timer_callback };
-static const struct lr35902_config cgb_cpu_reset = { NULL, 0, gb_timer_callback };
-static const struct lr35902_config megaduck_cpu_reset = { megaduck_cpu_regs, LR35902_FEATURE_HALT_BUG, gb_timer_callback };
-
 static ADDRESS_MAP_START(gb_map, AS_PROGRAM, 8, gb_state )
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(0x0000, 0x00ff) AM_ROMBANK("bank5")                    /* BIOS or ROM */
@@ -561,7 +555,8 @@ static MACHINE_CONFIG_START( gb_common, gb_state )
 	/* basic machine hardware */
 	MCFG_CPU_ADD("maincpu", LR35902, 4194304)           /* 4.194304 MHz */
 	MCFG_CPU_PROGRAM_MAP(gb_map)
-	MCFG_LR35902_CONFIG(dmg_cpu_reset)
+	MCFG_LR35902_TIMER_CB( WRITE8( gb_state, gb_timer_callback ) )
+	MCFG_LR35902_HALT_BUG
 	MCFG_CPU_VBLANK_INT_DRIVER("screen", gb_state,  gb_scanline_interrupt)  /* 1 dummy int each frame */
 
 	MCFG_QUANTUM_TIME(attotime::from_hz(60))
@@ -606,7 +601,8 @@ static MACHINE_CONFIG_DERIVED( supergb, gameboy )
 	MCFG_CPU_PROGRAM_MAP(sgb_map)
 
 	MCFG_CPU_MODIFY("maincpu")
-	MCFG_LR35902_CONFIG(sgb_cpu_reset)
+	MCFG_LR35902_TIMER_CB( WRITE8( gb_state, gb_timer_callback ) )
+	MCFG_LR35902_HALT_BUG
 
 	MCFG_MACHINE_START_OVERRIDE(gb_state, sgb )
 	MCFG_MACHINE_RESET_OVERRIDE(gb_state, sgb )
@@ -622,7 +618,10 @@ MACHINE_CONFIG_END
 
 static MACHINE_CONFIG_DERIVED( gbpocket, gameboy )
 	MCFG_CPU_MODIFY("maincpu")
-	MCFG_LR35902_CONFIG(mgb_cpu_reset)
+	MCFG_LR35902_TIMER_CB( WRITE8( gb_state, gb_timer_callback ) )
+	MCFG_LR35902_HALT_BUG
+	MCFG_LR35902_RESET_VALUES(mgb_cpu_regs)
+
 	MCFG_MACHINE_RESET_OVERRIDE(gb_state, gbpocket )
 	MCFG_PALETTE_INIT_OVERRIDE(gb_state,gbp)
 
@@ -633,7 +632,7 @@ MACHINE_CONFIG_END
 static MACHINE_CONFIG_DERIVED( gbcolor, gb_common )
 	MCFG_CPU_MODIFY("maincpu")
 	MCFG_CPU_PROGRAM_MAP( gbc_map)
-	MCFG_LR35902_CONFIG(cgb_cpu_reset)
+	MCFG_LR35902_TIMER_CB( WRITE8( gb_state, gb_timer_callback ) )
 
 	MCFG_MACHINE_START_OVERRIDE(gb_state,gbc)
 	MCFG_MACHINE_RESET_OVERRIDE(gb_state,gbc)
@@ -659,8 +658,10 @@ static MACHINE_CONFIG_START( megaduck, gb_state )
 	/* basic machine hardware */
 	MCFG_CPU_ADD("maincpu", LR35902, 4194304)           /* 4.194304 MHz */
 	MCFG_CPU_PROGRAM_MAP( megaduck_map)
+	MCFG_LR35902_TIMER_CB( WRITE8( gb_state, gb_timer_callback ) )
+	MCFG_LR35902_HALT_BUG
+	MCFG_LR35902_RESET_VALUES(megaduck_cpu_regs)
 	MCFG_CPU_VBLANK_INT_DRIVER("screen", gb_state,  gb_scanline_interrupt)  /* 1 int each scanline ! */
-	MCFG_LR35902_CONFIG(megaduck_cpu_reset)
 
 	MCFG_SCREEN_ADD("screen", LCD)
 	MCFG_SCREEN_REFRESH_RATE(DMG_FRAMES_PER_SECOND)

src/emu/cpu/lr35902/lr35902.c
@@ -54,19 +54,12 @@ const device_type LR35902 = &device_creator<lr35902_cpu_device>;
 
 
 lr35902_cpu_device::lr35902_cpu_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock)
-	: cpu_device(mconfig, LR35902, "LR35902", tag, owner, clock),
-		m_program_config("program", ENDIANNESS_LITTLE, 8, 16, 0)
+	: cpu_device(mconfig, LR35902, "LR35902", tag, owner, clock)
+	, m_program_config("program", ENDIANNESS_LITTLE, 8, 16, 0)
+	, m_timer_func(*this)
+	, m_features(0)
+	, c_regs(NULL)
 {
-	c_regs = NULL;
-	c_features = 0;
-	c_timer_expired_func = NULL;
-}
-
-
-void lr35902_cpu_device::static_set_config(device_t &device, const lr35902_config &config)
-{
-	lr35902_cpu_device &conf = downcast<lr35902_cpu_device &>(device);
-	static_cast<lr35902_config &>(conf) = config;
 }
 
 
@@ -77,10 +70,7 @@ void lr35902_cpu_device::static_set_config(device_t &device, const lr35902_confi
 inline void lr35902_cpu_device::cycles_passed(UINT8 cycles)
 {
 	m_icount -= cycles / m_gb_speed;
-	if ( m_timer_expired_func )
-	{
-		m_timer_expired_func( this, cycles );
-	}
+	m_timer_func( cycles );
 }
 
 
@@ -118,6 +108,8 @@ void lr35902_cpu_device::device_start()
 {
 	m_device = this;
 	m_program = &space(AS_PROGRAM);
+
+	m_timer_func.resolve_safe();
 
 	save_item(NAME(m_A));
 	save_item(NAME(m_F));
@@ -210,8 +202,6 @@ void lr35902_cpu_device::device_reset()
 		m_SP = c_regs[4];
 		m_PC = c_regs[5];
 	}
-	m_timer_expired_func = c_timer_expired_func;
-	m_features = c_features;
 
 	m_enable = 0;
 	m_IE = 0;

src/emu/cpu/lr35902/lr35902.h
@@ -4,20 +4,16 @@
 #define __LR35902_H__
 
 
-#define MCFG_LR35902_CONFIG(_config) \
-	lr35902_cpu_device::static_set_config(*device, _config);
+#define MCFG_LR35902_TIMER_CB(_devcb) \
+	lr35902_cpu_device::set_timer_cb(*device, DEVCB2_##_devcb); \
 
-class lr35902_cpu_device;
+#define MCFG_LR35902_HALT_BUG \
+	lr35902_cpu_device::set_halt_bug(*device); \
 
-// Perhaps replace this with a standard device callback
-typedef void (*lr35902_timer_fired_func)(lr35902_cpu_device *device, int cycles);
+// This should be removed/improved once all gameboy boot roms have been dumped
+#define MCFG_LR35902_RESET_VALUES(_regs) \
+	lr35902_cpu_device::set_reset_values(*device, _regs); \
 
-struct lr35902_config
-{
-	const UINT16    *c_regs;
-	UINT8           c_features;
-	lr35902_timer_fired_func c_timer_expired_func;
-};
 
 enum
 {
@@ -29,18 +25,17 @@ enum
 	LR35902_SPEED,
 };
 
-// This and the features configuration could be removed if we introduce proper subclasses
-#define LR35902_FEATURE_HALT_BUG    0x01
 
-
-class lr35902_cpu_device :  public cpu_device,
-							public lr35902_config
+class lr35902_cpu_device :  public cpu_device
 {
 public:
 	// construction/destruction
 	lr35902_cpu_device(const machine_config &mconfig, const char *_tag, device_t *_owner, UINT32 _clock);
 
-	static void static_set_config(device_t &device, const lr35902_config &config);
+	// static configuration helpers
+	template<class _Object> static devcb2_base &set_timer_cb(device_t &device, _Object object) { return downcast<lr35902_cpu_device &>(device).m_timer_func.set_callback(object); }
+	static void set_halt_bug(device_t &device) { downcast<lr35902_cpu_device &>(device).m_features |= LR35902_FEATURE_HALT_BUG; }
+	static void set_reset_values(device_t &device, const UINT16 *regs) { downcast<lr35902_cpu_device &>(device).c_regs = regs; }
 
 	UINT8 get_speed();
 	void set_speed( UINT8 speed_request );
@@ -52,6 +47,8 @@ public:
 	void set_if( UINT8 data ) { m_IF = data; }
 
 protected:
+	static const UINT8 LR35902_FEATURE_HALT_BUG = 0x01;
+
 	// device-level overrides
 	virtual void device_start();
 	virtual void device_reset();
@@ -103,8 +100,8 @@ protected:
 	lr35902_cpu_device *m_device;
 	address_space *m_program;
 	int m_icount;
-	/* Timer stuff */
-	lr35902_timer_fired_func m_timer_expired_func;
+	/* Timer callback */
+	devcb2_write8 m_timer_func;
 	/* Fetch & execute related */
 	int     m_execution_state;
 	UINT8   m_op;
@@ -114,6 +111,7 @@ protected:
 	int m_enable;
 	int m_doHALTbug;
 	UINT8   m_features;
+	const UINT16 *c_regs;
 	const struct lr35902_config *m_config;
 
 	/* Flag bit definitions */

src/mess/includes/gb.h
@@ -109,7 +109,18 @@ class gb_state : public driver_device
 {
 public:
 	gb_state(const machine_config &mconfig, device_type type, const char *tag)
-		: driver_device(mconfig, type, tag) { }
+		: driver_device(mconfig, type, tag)
+		, m_maincpu(*this, "maincpu")
+		, m_bank1(*this, "bank1")
+		, m_bank2(*this, "bank2")
+		, m_bank3(*this, "bank3")
+		, m_bank4(*this, "bank4")
+		, m_bank5(*this, "bank5")
+		, m_bank6(*this, "bank6")
+		, m_bank10(*this, "bank10")
+		, m_bank11(*this, "bank11")
+		, m_inputs(*this, "INPUTS")
+	{ }
 
 	UINT32 screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 
@@ -178,7 +189,7 @@ public:
 	UINT8 m_mmm01_bank;
 	UINT8 m_mmm01_bank_mask;
 	gb_lcd_t m_lcd;
-	void (*update_scanline)( running_machine &machine );
+	void (gb_state::*update_scanline) ();
 
 	bitmap_ind16 m_bitmap;
 	DECLARE_WRITE8_MEMBER(gb_rom_bank_select_mbc1);
@@ -255,6 +266,42 @@ public:
 	TIMER_CALLBACK_MEMBER(gb_video_init_vbl);
 	TIMER_CALLBACK_MEMBER(gb_lcd_timer_proc);
 	TIMER_CALLBACK_MEMBER(gbc_lcd_timer_proc);
+	DECLARE_WRITE8_MEMBER(gb_timer_callback);
+
+protected:
+	required_device<lr35902_cpu_device> m_maincpu;
+	required_memory_bank m_bank1;   // all
+	optional_memory_bank m_bank2;   // dmg, sgb, cgb
+	optional_memory_bank m_bank3;   // cgb
+	optional_memory_bank m_bank4;   // dmg, sgb, cgb
+	optional_memory_bank m_bank5;   // dmg, sgb, cgb
+	optional_memory_bank m_bank6;   // dmg, sgb, cgb
+	required_memory_bank m_bank10;  // all
+	optional_memory_bank m_bank11;  // dmg, sgb, cgb
+	required_ioport m_inputs;
+
+	void gb_timer_increment();
+	void gb_timer_check_irq();
+	void gb_init_regs();
+	void gb_rom16_0000( UINT8 *addr );
+	void gb_rom16_4000( UINT8 *addr );
+	void gb_rom8_4000( UINT8 *addr );
+	void gb_rom8_6000( UINT8 *addr );
+	void gb_init();
+	void gb_set_mbc1_banks();
+	void gb_set_mbc1_kor_banks();
+	void gb_select_sprites();
+	void gb_update_sprites();
+	void gb_update_scanline();
+	void sgb_update_sprites();
+	void sgb_refresh_border();
+	void sgb_update_scanline();
+	void cgb_update_sprites();
+	void cgb_update_scanline();
+	void gb_video_reset( int mode );
+	void gbc_hdma(UINT16 length);
+	void gb_increment_scanline();
+	void gb_lcd_switch_on();
 };
 
 
@@ -262,9 +309,6 @@ public:
 
 DEVICE_START(gb_cart);
 DEVICE_IMAGE_LOAD(gb_cart);
-void gb_timer_callback(lr35902_cpu_device *device, int cycles);
-
-
 
 
 /* -- Super Game Boy specific -- */
@@ -287,9 +331,6 @@ enum
 	GB_VIDEO_SGB,
 	GB_VIDEO_CGB
 };
-
-void gb_video_reset( running_machine &machine, int mode );
-UINT8 *gb_get_vram_ptr(running_machine &machine);
 
 
 #endif /* GB_H_ */

commit 95e0496a0d2d7f007aeb94465595275d783af6a7

(MESS) gb.c: Give gbcolor it's own machine config, fixes regression introduced in r20641. Fixes MT005730.

src/mess/drivers/gb.c
@@ -818,10 +818,10 @@ static MACHINE_CONFIG_DERIVED( gbpocket, gameboy )
 	MCFG_GB_LCD_MGB_ADD("lcd")
 MACHINE_CONFIG_END
 
-static MACHINE_CONFIG_DERIVED( gbcolor, gameboy )
+static MACHINE_CONFIG_START( gbcolor, gb_state )
 
 	/* basic machine hardware */
-	MCFG_CPU_MODIFY("maincpu") // todo XTAL_8_388MHz
+	MCFG_CPU_ADD("maincpu", LR35902, XTAL_4_194304Mhz) // todo XTAL_8_388MHz
 	MCFG_CPU_PROGRAM_MAP(gbc_map)
 	MCFG_LR35902_TIMER_CB( WRITE8(gb_state, gb_timer_callback ) )
 
@@ -829,19 +829,37 @@ static MACHINE_CONFIG_DERIVED( gbcolor, gameboy )
 	MCFG_MACHINE_RESET_OVERRIDE(gb_state,gbc)
 
 	/* video hardware */
-	MCFG_PALETTE_MODIFY("palette")
-	MCFG_PALETTE_ENTRIES(32768)
+	MCFG_SCREEN_ADD("screen", LCD)
+	MCFG_SCREEN_REFRESH_RATE(DMG_FRAMES_PER_SECOND)
+	MCFG_SCREEN_VBLANK_TIME(0)
+	MCFG_SCREEN_UPDATE_DEVICE("lcd", gb_lcd_device, screen_update)
+	MCFG_SCREEN_PALETTE("palette")
+
+	MCFG_DEFAULT_LAYOUT(layout_lcd)
+//  MCFG_SCREEN_SIZE(20*8, 18*8)
+	MCFG_SCREEN_SIZE( 458, 154 )
+	MCFG_SCREEN_VISIBLE_AREA(0*8, 20*8-1, 0*8, 18*8-1)
+
+	MCFG_GFXDECODE_ADD("gfxdecode", "palette", gb)
+
+	MCFG_PALETTE_ADD("palette", 32768)
 	MCFG_PALETTE_INIT_OWNER(gb_state,gbc)
 
-	MCFG_DEVICE_REMOVE("lcd")
 	MCFG_GB_LCD_CGB_ADD("lcd")
+
+	/* sound hardware */
+	MCFG_SPEAKER_STANDARD_STEREO("lspeaker", "rspeaker")
+	MCFG_SOUND_ADD("custom", GAMEBOY, 0)
+	MCFG_SOUND_ROUTE(0, "lspeaker", 0.50)
+	MCFG_SOUND_ROUTE(1, "rspeaker", 0.50)
 
 	/* internal ram */
 	MCFG_RAM_ADD(RAM_TAG)
 	MCFG_RAM_DEFAULT_SIZE("48K") /* 2 pages of 8KB VRAM, 8 pages of 4KB RAM */
 
-	MCFG_DEVICE_REMOVE("cart_list")
-	MCFG_DEVICE_REMOVE("gbc_list")
+	/* cartslot */
+	MCFG_GB_CARTRIDGE_ADD("gbslot", gb_cart, NULL)
+
 	MCFG_SOFTWARE_LIST_ADD("cart_list","gbcolor")
 	MCFG_SOFTWARE_LIST_COMPATIBLE_ADD("gb_list","gameboy")
 MACHINE_CONFIG_END