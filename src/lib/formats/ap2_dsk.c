2d2afa9ed9a025c6de6e8f9ebf9f714d382baea0
Olivier Galibert <galibert@pobox.com>
2014-10-21 18:27:59 +0200

    ap2_dsk: Fix broken detection of dsk [O. Galibert]

--- src/lib/formats/ap2_dsk.c
+++ src/lib/formats/ap2_dsk.c
@@ -563,9 +563,9 @@ int a2_16sect_format::identify(io_generic *io, UINT32 form_factor)
 		UINT32 expected_size = 35 * 16 * 256;
 
 		// check standard size plus some oddball sizes in our softlist
-		if ((size = expected_size) || (size == 143403) || (size == 143363) || (size == 143358))
+		if ((size == expected_size) || (size == 143403) || (size == 143363) || (size == 143358))
 		{
-			return 1;
+			return 50;
 		}
 
 		return 0;
