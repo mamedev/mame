63682564b2d06af646027d50ec9db384c7fb3aa0
smf- <smf-@users.noreply.github.com>
2014-10-23 20:49:32 +0100

    fixed the other __MSC_VER (nw)

--- src/lib/winpcap/pcap-stdinc.h
+++ src/lib/winpcap/pcap-stdinc.h
@@ -60,7 +60,7 @@
 
 #define caddr_t char*
 
-#if defined(_MSC_VER) && (__MSC_VER < 1500)
+#if defined(_MSC_VER) && (_MSC_VER < 1500)
 #define snprintf _snprintf
 #define vsnprintf _vsnprintf
 #define strdup _strdup
