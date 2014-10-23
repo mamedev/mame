700981f762b78a70c6d3632fdb9941b87e826cde
Scott Stone <Tafoid@users.noreply.github.com>
2014-10-23 17:23:08 -0400

    delete assert no longer needed (nw)

--- src/emu/bus/snes/snes_slot.c
+++ src/emu/bus/snes/snes_slot.c
@@ -1359,7 +1359,6 @@ void base_sns_cart_slot_device::internal_header_logging(UINT8 *ROM, UINT32 len)
 
 	logerror( "\tSize:          %d megabits [%d]\n", 1 << (ROM[hilo_mode + 0x17] - 7), ROM[hilo_mode + 0x17]);
 	logerror( "\tSRAM:          %d kilobits [%d]\n", ROM[hilo_mode + 0x18] * 8, ROM[hilo_mode + 0x18] );
-	assert(ROM[hilo_mode + 0x19] < ARRAY_LENGTH(countries));
 	if (ROM[hilo_mode + 0x19] < 0x10)
 		logerror( "\tCountry:       %s [%d]\n", countries[ROM[hilo_mode + 0x19]], ROM[hilo_mode + 0x19]);
 	else
