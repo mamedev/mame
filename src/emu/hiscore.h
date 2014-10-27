5e47fa98b8cb5ee89bcd382984d5a39162898932
R-type <r-type@nowhere.org>
2014-10-28 00:40:45 +0100

    libretro hiscore.c/.h  patch

---
+++ src/emu/hiscore.h
@@ -0,0 +1,19 @@
+/***************************************************************************
+
+    hiscore.h
+
+    Manages the hiscore system.
+
+    Copyright (c) 1996-2006, Nicola Salmoria and the MAME Team.
+    Visit http://mamedev.org for licensing and usage restrictions.
+
+***************************************************************************/
+
+#pragma once
+
+#ifndef __HISCORE_H__
+#define __HISCORE_H__
+
+void hiscore_init( running_machine &machine );
+
+#endif	/* __HISCORE_H__ */
