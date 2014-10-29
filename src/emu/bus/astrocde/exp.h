ca67460d242824df80f5146962941d56b410743e
etabeta78 <etabeta78@users.noreply.github.com>
2014-10-29 07:13:14 +0100

    (MESS) astrocde: installing expansion handlers only if a slot card is present avoids the appearance of gfx garbage at start. garbage disappeared when pressing reset, but it is better in this way, of course. nw.

--- src/emu/bus/astrocde/exp.h
+++ src/emu/bus/astrocde/exp.h
@@ -13,7 +13,7 @@ public:
 	virtual ~device_astrocade_card_interface();
 
 	// reading and writing
-	virtual DECLARE_READ8_MEMBER(read) { return 0; }
+	virtual DECLARE_READ8_MEMBER(read) { return 0xff; }
 	virtual DECLARE_WRITE8_MEMBER(write) {}
 
 protected:
@@ -33,12 +33,14 @@ public:
 	// device-level overrides
 	virtual void device_start();
 
+	bool get_card_mounted() { return m_card_mounted; }
+	
 	// reading and writing
 	virtual DECLARE_READ8_MEMBER(read);
 	virtual DECLARE_WRITE8_MEMBER(write);
 
 protected:
-
+	bool m_card_mounted;
 	device_astrocade_card_interface* m_card;
 };
 
