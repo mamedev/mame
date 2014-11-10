8d958c329dc9532ee3ee1fc81fc0ae29f8ac5ebf
hap <happppp@users.noreply.github.com>
2014-11-10 17:38:44 +0100

    there were some typos in stopthie.bin (game still goes in an infinite loop tho)

--- src/mess/drivers/stopthie.c
+++ src/mess/drivers/stopthie.c
@@ -28,7 +28,7 @@ INPUT_PORTS_END
 
 READ8_MEMBER(stopthie_state::stopthie_read_k)
 {
-	UINT8 data = 0xFF;
+	UINT8 data = 0;
 
 	if (LOG)
 		logerror( "stopthie_read_k\n" );
@@ -54,15 +54,17 @@ WRITE16_MEMBER(stopthie_state::stopthie_write_r)
 static const UINT16 stopthie_output_pla[0x20] =
 {
 	/* O output PLA configuration currently unknown */
-	0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00,
-	0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00,
-	0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00,
-	0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00,
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+	0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+	0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+	0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
 };
 
 
 static MACHINE_CONFIG_START( stopthie, stopthie_state )
-	MCFG_CPU_ADD( "maincpu", TMS0980, 5000000 ) /* Clock is wrong */
+
+	/* basic machine hardware */
+	MCFG_CPU_ADD( "maincpu", TMS0980, 500000 ) /* Clock is wrong */
 	MCFG_TMS1XXX_OUTPUT_PLA( stopthie_output_pla )
 	MCFG_TMS1XXX_READ_K( READ8( stopthie_state, stopthie_read_k ) )
 	MCFG_TMS1XXX_WRITE_O( WRITE16( stopthie_state, stopthie_write_o ) )
@@ -74,7 +76,7 @@ MACHINE_CONFIG_END
 ROM_START( stopthie )
 	ROM_REGION( 0x1000, "maincpu", 0 )
 	/* Taken from patent 4341385, might have made mistakes when creating this rom */
-	ROM_LOAD16_WORD( "stopthie.bin", 0x0000, 0x1000, BAD_DUMP CRC(49ef83ad) SHA1(407151f707aa4a62b7e034a1bcb957c42ea36707) )
+	ROM_LOAD16_WORD( "stopthie.bin", 0x0000, 0x1000, BAD_DUMP CRC(63162ce9) SHA1(2ff88a139020c48869fcacc04b0786b27530a802) )
 ROM_END
 
 /***************************************************************************
