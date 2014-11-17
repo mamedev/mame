10f04d506c1b5af4e6af46997e9db4b75daf8666
Osso <mesgnet@yahoo.it>
2014-11-17 18:39:01 +0100

    Fixed mikrosha.c regression (nw)

--- src/mess/drivers/mikrosha.c
+++ src/mess/drivers/mikrosha.c
@@ -36,7 +36,7 @@ MACHINE_RESET_MEMBER(mikrosha_state,mikrosha)
 {
 	if (m_cart->exists())
 		m_maincpu->space(AS_PROGRAM).install_read_handler(0x8000, 0x8000+m_cart->get_rom_size()-1, read8_delegate(FUNC(generic_slot_device::read_rom),(generic_slot_device*)m_cart));
-	radio86_state::machine_reset();
+	MACHINE_RESET_CALL_MEMBER(radio86);
 }
 
 /* Address maps */
