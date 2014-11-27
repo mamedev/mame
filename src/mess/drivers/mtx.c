d5722b3ca2b6496f8ee2b484d7640bbdc8bca18b
etabeta78 <etabeta78@users.noreply.github.com>
2014-11-27 08:32:48 +0100

    (MESS) added software lists for Altos 5 floppies and Memotech MTX tapes. [Riley110]

--- src/mess/drivers/mtx.c
+++ src/mess/drivers/mtx.c
@@ -323,6 +323,7 @@ static MACHINE_CONFIG_START( mtx512, mtx_state )
 	MCFG_SNAPSHOT_ADD("snapshot", mtx_state, mtx, "mtx", 1)
 	MCFG_CASSETTE_ADD("cassette")
 	MCFG_CASSETTE_DEFAULT_STATE(CASSETTE_STOPPED | CASSETTE_MOTOR_ENABLED | CASSETTE_SPEAKER_MUTED)
+	MCFG_CASSETTE_INTERFACE("mtx_cass")
 
 	MCFG_TIMER_DRIVER_ADD_PERIODIC("cassette_timer", mtx_state, cassette_tick, attotime::from_hz(44100))
 
@@ -330,6 +331,8 @@ static MACHINE_CONFIG_START( mtx512, mtx_state )
 	MCFG_RAM_ADD(RAM_TAG)
 	MCFG_RAM_DEFAULT_SIZE("64K")
 	MCFG_RAM_EXTRA_OPTIONS("96K,128K,160K,192K,224K,256K,288K,320K,352K,384K,416K,448K,480K,512K")
+
+	MCFG_SOFTWARE_LIST_ADD("cass_list", "mtx_cass")
 MACHINE_CONFIG_END
 
 /*-------------------------------------------------
