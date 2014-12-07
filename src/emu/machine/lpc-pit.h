19df7acfc6cb8c656da790ad57279c31fc8ee88b
Olivier Galibert <galibert@pobox.com>
2014-12-07 22:37:04 +0100

    duhhhhhhhhhhhhhhhhhhhhhh (nw)

---
+++ src/emu/machine/lpc-pit.h
@@ -0,0 +1,30 @@
+#ifndef LPC_PIT_H
+#define LPC_PIT_H
+
+#include "lpc.h"
+
+#define MCFG_LPC_PIT_ADD(_tag) \
+	MCFG_DEVICE_ADD(_tag, LPC_PIT, 0)
+
+class lpc_pit_device : public lpc_device {
+public:
+	lpc_pit_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+
+	virtual void map_device(UINT64 memory_window_start, UINT64 memory_window_end, UINT64 memory_offset, address_space *memory_space,
+							UINT64 io_window_start, UINT64 io_window_end, UINT64 io_offset, address_space *io_space);
+
+	DECLARE_READ8_MEMBER( status_r);
+	DECLARE_WRITE8_MEMBER(access_w);
+	DECLARE_WRITE8_MEMBER(control_w);
+
+protected:
+	void device_start();
+	void device_reset();
+
+private:
+	DECLARE_ADDRESS_MAP(map, 32);
+};
+
+extern const device_type LPC_PIT;
+
+#endif
