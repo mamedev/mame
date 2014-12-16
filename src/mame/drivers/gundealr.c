710552ae7a1286e6d9940a056c4a2377b7f0aa63
briantro <briantro@users.noreply.github.com>
2014-12-15 18:39:50 -0600

    Verified clocks for Yam! Yam!? and Last Day / D-Day [system11]
    Out of what's new: Changed machine configs to reflect actual OSC used.

--- src/mame/drivers/gundealr.c
+++ src/mame/drivers/gundealr.c
@@ -44,6 +44,16 @@ Interrupts:
 Runs in interrupt mode 0, the interrupt vectors are 0xcf (RST 08h) and
 0xd7 (RST 10h)
 
+PCB:  DY-90010001
+  CPU: Z80B
+Sound: YM2203C + Y3014B DAC
+  MCU: Unknown 64 pin DIL
+  OSC: 12MHz, 5MHz
+
+Clock measurements:
+Z80 CPU - 12MHz/2
+ YM2203 - 12MHz/8
+
 ***************************************************************************/
 
 #include "emu.h"
@@ -393,7 +403,7 @@ TIMER_DEVICE_CALLBACK_MEMBER(gundealr_state::gundealr_scanline)
 static MACHINE_CONFIG_START( gundealr, gundealr_state )
 
 	/* basic machine hardware */
-	MCFG_CPU_ADD("maincpu", Z80, 8000000)   /* 8 MHz ??? */
+	MCFG_CPU_ADD("maincpu", Z80, XTAL_12MHz/2)   /* 6 MHz verified for Yam! Yam!? */
 	MCFG_CPU_PROGRAM_MAP(main_map)
 	MCFG_CPU_IO_MAP(main_portmap)
 	MCFG_TIMER_DRIVER_ADD_SCANLINE("scantimer", gundealr_state, gundealr_scanline, "screen", 0, 1)
@@ -415,7 +425,7 @@ static MACHINE_CONFIG_START( gundealr, gundealr_state )
 	/* sound hardware */
 	MCFG_SPEAKER_STANDARD_MONO("mono")
 
-	MCFG_SOUND_ADD("ymsnd", YM2203, 1500000)
+	MCFG_SOUND_ADD("ymsnd", YM2203, XTAL_12MHz/8) /* 1.5Mhz verified for Yam! Yam!? */
 	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.25)
 MACHINE_CONFIG_END
 
@@ -531,55 +541,50 @@ ROM_START( gundealrt )
 	ROM_LOAD( "2.6b",         0x00000, 0x20000, CRC(508ed0d0) SHA1(ea6b2d07e2e3d4f6c2a622a73b150ee7709b28de) )
 ROM_END
 
-ROM_START( yamyam )
+ROM_START( yamyam ) /* DY-90010001 PCB */
 	ROM_REGION( 0x30000, "maincpu", 0 ) /* 64k for code + 128k for banks */
 	ROM_LOAD( "3.10f",       0x00000, 0x20000, CRC(96ae9088) SHA1(a605882dcdcf1e8cf8b0112f614e696d59acfd97) )
 	ROM_RELOAD(               0x10000, 0x20000 )    /* banked at 0x8000-0xbfff */
 
-	ROM_REGION( 0x10000, "mcu", 0 ) //unknown type, there must be one
+	ROM_REGION( 0x10000, "mcu", 0 ) // unknown 64 pin MCU at J9 with internal ROM code
 	ROM_LOAD( "mcu", 0x0000, 0x10000, NO_DUMP)
 
-	ROM_REGION( 0x10000, "gfx1", 0 )
+	ROM_REGION( 0x10000, "gfx1", 0 ) /* only gfx are different, code is the same */
 	ROM_LOAD( "b2.16d",       0x00000, 0x10000, CRC(cb4f84ee) SHA1(54319ecbd74b763757eb6d17c8f7be0705ab0714) )
 
 	ROM_REGION( 0x20000, "gfx2", 0 )
 	ROM_LOAD( "1.16a",       0x00000, 0x20000, CRC(b122828d) SHA1(90994ba548893a2eacdd58351cfa3952f4af926a) )
 ROM_END
 
-/* only gfx are different, code is the same */
-ROM_START( yamyamk )
+ROM_START( yamyamk ) /* DY-90010001 PCB */
 	ROM_REGION( 0x30000, "maincpu", 0 ) /* 64k for code + 128k for banks */
 	ROM_LOAD( "3.10f",       0x00000, 0x20000, CRC(96ae9088) SHA1(a605882dcdcf1e8cf8b0112f614e696d59acfd97) )
 	ROM_RELOAD(               0x10000, 0x20000 )    /* banked at 0x8000-0xbfff */
 
-	ROM_REGION( 0x10000, "mcu", 0 ) //unknown type, there must be one
+	ROM_REGION( 0x10000, "mcu", 0 ) // unknown 64 pin MCU at J9 with internal ROM code
 	ROM_LOAD( "mcu", 0x0000, 0x10000, NO_DUMP)
 
-	ROM_REGION( 0x10000, "gfx1", 0 )
+	ROM_REGION( 0x10000, "gfx1", 0 ) /* only gfx are different, code is the same */
 	ROM_LOAD( "2.16d",       0x00000, 0x10000, CRC(dc9691d8) SHA1(118a05a1c94020d6739ed8c805c61b8ab003b6af) )
 
 	ROM_REGION( 0x20000, "gfx2", 0 )
 	ROM_LOAD( "1.16a",       0x00000, 0x20000, CRC(b122828d) SHA1(90994ba548893a2eacdd58351cfa3952f4af926a) )
 ROM_END
 
-/* only gfx are different, code is the same */
-ROM_START( wiseguy )
+ROM_START( wiseguy ) /* DY-90010001 PCB */
 	ROM_REGION( 0x30000, "maincpu", 0 ) /* 64k for code + 128k for banks */
 	ROM_LOAD( "b3.f10",       0x00000, 0x20000, CRC(96ae9088) SHA1(a605882dcdcf1e8cf8b0112f614e696d59acfd97) )
 	ROM_RELOAD(               0x10000, 0x20000 )    /* banked at 0x8000-0xbfff */
 
-	ROM_REGION( 0x10000, "mcu", 0 ) //unknown type, there must be one
+	ROM_REGION( 0x10000, "mcu", 0 ) // unknown 64 pin MCU at J9 with internal ROM code
 	ROM_LOAD( "mcu", 0x0000, 0x10000, NO_DUMP)
 
-	ROM_REGION( 0x10000, "gfx1", 0 )
+	ROM_REGION( 0x10000, "gfx1", 0 ) /* only gfx are different, code is the same */
 	ROM_LOAD( "wguyb2.16d",   0x00000, 0x10000, CRC(1c684c46) SHA1(041bc500e31b02a8bf3ce4683a67de998f938ccc) )
 
 	ROM_REGION( 0x20000, "gfx2", 0 )
 	ROM_LOAD( "1.16a",       0x00000, 0x20000, CRC(b122828d) SHA1(90994ba548893a2eacdd58351cfa3952f4af926a) )
 ROM_END
-
-
-
 
 
 
