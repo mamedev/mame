3ef1938bef49dbc7158ea95ded99e302933fe2f9
Olivier Galibert <galibert@pobox.com>
2014-12-19 18:25:09 +0100

    init order issue (nw)

--- src/emu/cpu/h8/h8_timer16.c
+++ src/emu/cpu/h8/h8_timer16.c
@@ -162,7 +162,8 @@ void h8_timer16_channel_device::device_start()
 {
 	intc = owner()->siblingdevice<h8_intc_device>(intc_tag);
 	channel_active = false;
-	
+	clock_type = DIV_1;
+
 	save_item(NAME(tgr_clearing));
 	save_item(NAME(tcr));
 	save_item(NAME(tier));
