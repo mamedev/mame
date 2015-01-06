f8f06c9a5c23b3a5082813f998b08baba809c1f9
etabeta78 <etabeta78@users.noreply.github.com>
2015-01-06 17:17:36 +0100

    reverted previous change, since it does not interact well with the rest of the creation code. a better fix is in progress :)

--- src/emu/ui/filesel.c
+++ src/emu/ui/filesel.c
@@ -328,10 +328,6 @@ void ui_menu_file_create::handle()
 					reset(UI_MENU_RESET_REMEMBER_POSITION);
 				}
 				break;
-
-			case IPT_UI_CANCEL:
-				ui_menu::stack_pop(machine());
-				break;
 		}
 	}
 }
