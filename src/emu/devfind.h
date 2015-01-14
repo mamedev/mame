53ee1c4138a2c4a761c2fe75d44c9d0cf8556f30
couriersud <couriersud@arcor.de>
2015-01-14 01:54:46 +0100

    Avoid inline osd calls in header file.

--- src/emu/devfind.h
+++ src/emu/devfind.h
@@ -57,6 +57,8 @@ protected:
 	void *find_memshare(UINT8 width, size_t &bytes, bool required);
 	bool report_missing(bool found, const char *objname, bool required);
 
+    void printf_warning(const char *format, ...) ATTR_PRINTF(2,3);
+
 	// internal state
 	finder_base *m_next;
 	device_t &m_base;
@@ -113,8 +115,7 @@ public:
 		this->m_target = dynamic_cast<_DeviceClass *>(device);
 		if (device != NULL && this->m_target == NULL)
 		{
-			void osd_printf_warning(const char *format, ...) ATTR_PRINTF(1,2);
-			osd_printf_warning("Device '%s' found but is of incorrect type (actual type is %s)\n", this->m_tag, device->name());
+			this->printf_warning("Device '%s' found but is of incorrect type (actual type is %s)\n", this->m_tag, device->name());
 		}
 		return this->report_missing(this->m_target != NULL, "device", _Required);
 	}
