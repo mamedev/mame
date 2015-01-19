76edf0d3e6500b2936a18c7d27ee0a81271d2b1b
etabeta78 <etabeta78@users.noreply.github.com>
2015-01-19 06:36:15 +0100

    ui: allow -str run to skip the file manager for systems with mandatory carts. nw.

--- src/emu/ui/ui.c
+++ src/emu/ui/ui.c
@@ -310,13 +310,13 @@ void ui_manager::display_startup_screens(bool first_time, bool show_disclaimer)
 	const int maxstate = 4;
 	int str = machine().options().seconds_to_run();
 	bool show_gameinfo = !machine().options().skip_gameinfo();
-	bool show_warnings = true;
+	bool show_warnings = true, show_mandatory_fileman = true;
 	int state;
 
 	// disable everything if we are using -str for 300 or fewer seconds, or if we're the empty driver,
 	// or if we are debugging
 	if (!first_time || (str > 0 && str < 60*5) || &machine().system() == &GAME_NAME(___empty) || (machine().debug_flags & DEBUG_FLAG_ENABLED) != 0)
-		show_gameinfo = show_warnings = show_disclaimer = FALSE;
+		show_gameinfo = show_warnings = show_disclaimer = show_mandatory_fileman = FALSE;
 
 	#ifdef SDLMAME_EMSCRIPTEN
 	// also disable for the JavaScript port since the startup screens do not run asynchronously
@@ -355,7 +355,7 @@ void ui_manager::display_startup_screens(bool first_time, bool show_disclaimer)
 				break;
 
 			case 3:
-				if (image_mandatory_scan(machine(), messagebox_text).len() > 0)
+				if (show_mandatory_fileman && image_mandatory_scan(machine(), messagebox_text).len() > 0)
 				{
 					astring warning;
 					warning.cpy("This driver requires images to be loaded in the following device(s): ").cat(messagebox_text.substr(0, messagebox_text.len() - 2));
