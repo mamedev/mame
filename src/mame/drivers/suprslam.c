33839937be0c0a4a30be7c5e892a073db2a5c14e
andrew-gardner <andrew.gardner@liu.se>
2015-03-01 11:29:57 +0100

    Replaced a bunch of set_base()s with set_entry()s (nw)

--- src/mame/drivers/suprslam.c
+++ src/mame/drivers/suprslam.c
@@ -115,11 +115,7 @@ WRITE8_MEMBER(suprslam_state::pending_command_clear_w)
 
 WRITE8_MEMBER(suprslam_state::suprslam_sh_bankswitch_w)
 {
-	UINT8 *RAM = memregion("audiocpu")->base();
-	int bankaddress;
-
-	bankaddress = 0x10000 + (data & 0x03) * 0x8000;
-	membank("bank1")->set_base(&RAM[bankaddress]);
+    membank("bank1")->set_entry(data & 0x03);
 }
 
 /*** MEMORY MAPS *************************************************************/
@@ -288,6 +284,8 @@ void suprslam_state::machine_start()
 	save_item(NAME(m_screen_bank));
 	save_item(NAME(m_bg_bank));
 	save_item(NAME(m_pending_command));
+
+    membank("bank1")->configure_entries(0, 4, memregion("audiocpu")->base() + 0x10000, 0x8000);
 }
 
 void suprslam_state::machine_reset()
