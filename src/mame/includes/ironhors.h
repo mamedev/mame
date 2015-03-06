77c98e46a6f2fc39cf0a12b2aba5567d3ca2bf44
Ivan Vangelista <mesgnet@yahoo.it>
2015-03-06 18:33:26 +0100

    hotblock.c, ironhors.c: added save state support (nw)

--- src/mame/includes/ironhors.h
+++ src/mame/includes/ironhors.h
@@ -9,16 +9,22 @@ class ironhors_state : public driver_device
 public:
 	ironhors_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag),
+		m_maincpu(*this, "maincpu"),
+		m_soundcpu(*this, "soundcpu"),
+		m_gfxdecode(*this, "gfxdecode"),
+		m_palette(*this, "palette"),
 		m_interrupt_enable(*this, "int_enable"),
 		m_scroll(*this, "scroll"),
 		m_colorram(*this, "colorram"),
 		m_videoram(*this, "videoram"),
 		m_spriteram2(*this, "spriteram2"),
-		m_spriteram(*this, "spriteram"),
-		m_maincpu(*this, "maincpu"),
-		m_soundcpu(*this, "soundcpu"),
-		m_gfxdecode(*this, "gfxdecode"),
-		m_palette(*this, "palette") { }
+		m_spriteram(*this, "spriteram") { }
+
+	/* devices */
+	required_device<cpu_device> m_maincpu;
+	required_device<cpu_device> m_soundcpu;
+	required_device<gfxdecode_device> m_gfxdecode;
+	required_device<palette_device> m_palette;
 
 	/* memory pointers */
 	required_shared_ptr<UINT8> m_interrupt_enable;
@@ -34,31 +40,29 @@ public:
 	int        m_charbank;
 	int        m_spriterambank;
 
-	/* devices */
-	required_device<cpu_device> m_maincpu;
-	required_device<cpu_device> m_soundcpu;
-	required_device<gfxdecode_device> m_gfxdecode;
-	required_device<palette_device> m_palette;
-
-	DECLARE_WRITE8_MEMBER(ironhors_sh_irqtrigger_w);
-	DECLARE_WRITE8_MEMBER(ironhors_videoram_w);
-	DECLARE_WRITE8_MEMBER(ironhors_colorram_w);
-	DECLARE_WRITE8_MEMBER(ironhors_charbank_w);
-	DECLARE_WRITE8_MEMBER(ironhors_palettebank_w);
-	DECLARE_WRITE8_MEMBER(ironhors_flipscreen_w);
-	DECLARE_WRITE8_MEMBER(ironhors_filter_w);
+	DECLARE_WRITE8_MEMBER(sh_irqtrigger_w);
+	DECLARE_WRITE8_MEMBER(videoram_w);
+	DECLARE_WRITE8_MEMBER(colorram_w);
+	DECLARE_WRITE8_MEMBER(charbank_w);
+	DECLARE_WRITE8_MEMBER(palettebank_w);
+	DECLARE_WRITE8_MEMBER(flipscreen_w);
+	DECLARE_WRITE8_MEMBER(filter_w);
 	DECLARE_READ8_MEMBER(farwest_soundlatch_r);
+	
 	TILE_GET_INFO_MEMBER(get_bg_tile_info);
 	TILE_GET_INFO_MEMBER(farwest_get_bg_tile_info);
+	
 	virtual void machine_start();
 	virtual void machine_reset();
 	virtual void video_start();
 	DECLARE_PALETTE_INIT(ironhors);
 	DECLARE_VIDEO_START(farwest);
-	UINT32 screen_update_ironhors(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	
+	UINT32 screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 	UINT32 screen_update_farwest(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	TIMER_DEVICE_CALLBACK_MEMBER(ironhors_irq);
-	TIMER_DEVICE_CALLBACK_MEMBER(farwest_irq);
 	void draw_sprites( bitmap_ind16 &bitmap, const rectangle &cliprect );
 	void farwest_draw_sprites( bitmap_ind16 &bitmap, const rectangle &cliprect );
+	
+	TIMER_DEVICE_CALLBACK_MEMBER(irq);
+	TIMER_DEVICE_CALLBACK_MEMBER(farwest_irq);
 };
