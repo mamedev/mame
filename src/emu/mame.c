490ed5ce9b8f27412f8e69c7b0c47b106e75b632
Oliver Stöneberg <oliverst@online.de>
2015-03-17 15:48:28 +0100

    disabled additional logging of popmessage() messages (nw)

--- src/emu/mame.c
+++ src/emu/mame.c
@@ -287,6 +287,7 @@ void CLIB_DECL popmessage(const char *format, ...)
 		// pop it in the UI
 		machine_manager::instance()->machine()->ui().popup_time(temp.len() / 40 + 2, "%s", temp.cstr());
 		
+		/*
 		// also write to error.log
 		logerror("popmessage: %s\n", temp.cstr());
 		
@@ -294,6 +295,7 @@ void CLIB_DECL popmessage(const char *format, ...)
 		// and to command-line in a DEBUG build
 		osd_printf_info("popmessage: %s\n", temp.cstr());
 #endif
+		*/
 	}
 }
 
