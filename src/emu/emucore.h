37f02619b519bb3dff18d8541d08be4d8b5ad7ea
Vas Crabb <vas@vastheman.com>
2015-03-20 23:06:56 +1100

    Fix build - come on guys, there's no way this could've possibly worked for anyone

--- src/emu/emucore.h
+++ src/emu/emucore.h
@@ -35,10 +35,10 @@
 #undef assert
 #undef assert_always
 
-#ifdef MAME_DEBUG_FAST
-#define assert(x)               ((void)0)
-#define assert_always(x, msg)   ((void)0)
-#elif MAME_DEBUG
+#if defined(MAME_DEBUG_FAST)
+#define assert(x)               do { } while (0)
+#define assert_always(x, msg)   do { } while (0)
+#elif defined(MAME_DEBUG)
 #define assert(x)               do { if (!(x)) throw emu_fatalerror("assert: %s:%d: %s", __FILE__, __LINE__, #x); } while (0)
 #define assert_always(x, msg)   do { if (!(x)) throw emu_fatalerror("Fatal error: %s\nCaused by assert: %s:%d: %s", msg, __FILE__, __LINE__, #x); } while (0)
 #else
