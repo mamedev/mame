9f29fc0916ea0e39150572194fb99b0d45f77a7c
Oliver Stöneberg <oliverst@online.de>
2015-03-20 11:39:41 +0100

    removed a few more unnecessary assert.h includes (nw)

--- src/lib/formats/wavfile.c
+++ src/lib/formats/wavfile.c
@@ -7,7 +7,6 @@
 *********************************************************************/
 
 #include <stdio.h>
-#include <assert.h>
 
 #include "wavfile.h"
 #include "cassimg.h"
--- src/mess/tools/imgtool/imgtool.h
+++ src/mess/tools/imgtool/imgtool.h
@@ -11,7 +11,6 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <assert.h>
 
 #include "corestr.h"
 #include "formats/flopimg.h"
