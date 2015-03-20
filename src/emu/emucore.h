77e1b51b1d7b3cd7c201029e20656460f14e7132
Oliver Stöneberg <oliverst@online.de>
2015-03-20 15:42:27 +0100

    assert_always is basically fatalerror() - so it should always bail out (nw)

--- src/emu/emucore.h
+++ src/emu/emucore.h
@@ -221,7 +221,7 @@ inline void operator--(_Type &value, int) { value = (_Type)((int)value - 1); }
 
 #if defined(MAME_DEBUG_FAST)
 #define assert(x)               do { } while (0)
-#define assert_always(x, msg)   do { } while (0)
+#define assert_always(x, msg)   do { if (!(x)) throw emu_fatalerror("Fatal error: %s\nCaused by assert: %s:%d: %s", msg, __FILE__, __LINE__, #x); } while (0)
 #elif defined(MAME_DEBUG)
 #define assert(x)               do { if (!(x)) throw emu_fatalerror("assert: %s:%d: %s", __FILE__, __LINE__, #x); } while (0)
 #define assert_always(x, msg)   do { if (!(x)) throw emu_fatalerror("Fatal error: %s\nCaused by assert: %s:%d: %s", msg, __FILE__, __LINE__, #x); } while (0)
