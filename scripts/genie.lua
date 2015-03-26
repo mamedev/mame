c29a2b4517317e5d15354ed52b2aaaea3efdf1dc
Miodrag Milanovic <mmicko@gmail.com>
2015-03-26 16:37:53 +0100

    sym file generate on mingw in postbuild process (nw)

--- scripts/genie.lua
+++ scripts/genie.lua
@@ -474,7 +474,14 @@ if _OPTIONS["MAP"] then
 
 	end	
 end
-
+if _OPTIONS["SYMBOLS"] then
+	configuration { "mingw*" }
+		postbuildcommands {
+			"$(SILENT) echo Dumping symbols.",
+			"$(SILENT) objdump --section=.text --line-numbers --syms --demangle $(TARGET) >$(subst .exe,.sym,$(TARGET))"
+		}
+	configuration { }
+end
 	buildoptions {
 		"-Wno-unknown-pragmas",
 	}
