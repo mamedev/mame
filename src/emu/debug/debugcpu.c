38c6213665bdbcb2b8d95d6f6550c5aa0bd3e57f
Vas Crabb <vas@vastheman.com>
2015-04-11 22:30:28 +1000

    Replace simple_set with std::set

--- src/emu/debug/debugcpu.c
+++ src/emu/debug/debugcpu.c
@@ -1994,12 +1994,10 @@ void device_debug::memory_write_hook(address_space &space, offs_t address, UINT6
 {
 	if (m_track_mem)
 	{
-		dasm_memory_access newAccess(space.spacenum(), address, data, history_pc(0));
-		dasm_memory_access* trackedAccess = m_track_mem_set.find(newAccess);
-		if (trackedAccess)
-			trackedAccess->m_pc = newAccess.m_pc;
-		else
-			m_track_mem_set.insert(newAccess);
+		dasm_memory_access const newAccess(space.spacenum(), address, data, history_pc(0));
+		std::pair<std::set<dasm_memory_access>::iterator, bool> trackedAccess = m_track_mem_set.insert(newAccess);
+		if (!trackedAccess.second)
+			trackedAccess.first->m_pc = newAccess.m_pc;
 	}
 	watchpoint_check(space, WATCHPOINT_WRITE, address, data, mem_mask);
 }
@@ -2041,14 +2039,14 @@ offs_t device_debug::disassemble(char *buffer, offs_t pc, const UINT8 *oprom, co
 	// make sure we get good results
 	assert((result & DASMFLAG_LENGTHMASK) != 0);
 #ifdef MAME_DEBUG
-if (m_memory != NULL && m_disasm != NULL)
-{
-	address_space &space = m_memory->space(AS_PROGRAM);
-	int bytes = space.address_to_byte(result & DASMFLAG_LENGTHMASK);
-	assert(bytes >= m_disasm->min_opcode_bytes());
-	assert(bytes <= m_disasm->max_opcode_bytes());
-	(void) bytes; // appease compiler
-}
+	if (m_memory != NULL && m_disasm != NULL)
+	{
+		address_space &space = m_memory->space(AS_PROGRAM);
+		int bytes = space.address_to_byte(result & DASMFLAG_LENGTHMASK);
+		assert(bytes >= m_disasm->min_opcode_bytes());
+		assert(bytes <= m_disasm->max_opcode_bytes());
+		(void) bytes; // appease compiler
+	}
 #endif
 
 	return result;
@@ -2589,7 +2587,7 @@ bool device_debug::track_pc_visited(const offs_t& pc) const
 	if (m_track_pc_set.empty())
 		return false;
 	const UINT32 crc = compute_opcode_crc32(pc);
-	return m_track_pc_set.contains(dasm_pc_tag(pc, crc));
+	return m_track_pc_set.find(dasm_pc_tag(pc, crc)) != m_track_pc_set.end();
 }
 
 
@@ -2618,8 +2616,8 @@ offs_t device_debug::track_mem_pc_from_space_address_data(const address_spacenum
 	const offs_t missing = (offs_t)(-1);
 	if (m_track_mem_set.empty())
 		return missing;
-	dasm_memory_access* mem_access = m_track_mem_set.find(dasm_memory_access(space, address, data, 0));
-	if (mem_access == NULL) return missing;
+	std::set<dasm_memory_access>::iterator const mem_access = m_track_mem_set.find(dasm_memory_access(space, address, data, 0));
+	if (mem_access == m_track_mem_set.end()) return missing;
 	return mem_access->m_pc;
 }
 
@@ -2632,12 +2630,13 @@ offs_t device_debug::track_mem_pc_from_space_address_data(const address_spacenum
 void device_debug::comment_add(offs_t addr, const char *comment, rgb_t color)
 {
 	// create a new item for the list
-	const UINT32 crc = compute_opcode_crc32(addr);
-	dasm_comment newComment = dasm_comment(addr, crc, comment, color);
-	if (!m_comment_set.insert(newComment))
+	UINT32 const crc = compute_opcode_crc32(addr);
+	dasm_comment const newComment = dasm_comment(addr, crc, comment, color);
+	std::pair<std::set<dasm_comment>::iterator, bool> const inserted = m_comment_set.insert(newComment);
+	if (!inserted.second)
 	{
 		// Insert returns false if comment exists
-		m_comment_set.remove(newComment);
+		m_comment_set.erase(inserted.first);
 		m_comment_set.insert(newComment);
 	}
 
@@ -2654,9 +2653,9 @@ void device_debug::comment_add(offs_t addr, const char *comment, rgb_t color)
 bool device_debug::comment_remove(offs_t addr)
 {
 	const UINT32 crc = compute_opcode_crc32(addr);
-	bool success = m_comment_set.remove(dasm_comment(addr, crc, "", 0xffffffff));
-	if (success) m_comment_change++;
-	return success;
+	size_t const removed = m_comment_set.erase(dasm_comment(addr, crc, "", 0xffffffff));
+	if (removed != 0U) m_comment_change++;
+	return removed != 0U;
 }
 
 
@@ -2667,8 +2666,8 @@ bool device_debug::comment_remove(offs_t addr)
 const char *device_debug::comment_text(offs_t addr) const
 {
 	const UINT32 crc = compute_opcode_crc32(addr);
-	dasm_comment* comment = m_comment_set.find(dasm_comment(addr, crc, "", 0));
-	if (comment == NULL) return NULL;
+	std::set<dasm_comment>::iterator comment = m_comment_set.find(dasm_comment(addr, crc, "", 0));
+	if (comment == m_comment_set.end()) return NULL;
 	return comment->m_text;
 }
 
@@ -2682,8 +2681,7 @@ bool device_debug::comment_export(xml_data_node &curnode)
 {
 	// iterate through the comments
 	astring crc_buf;
-	simple_set_iterator<dasm_comment> iter(m_comment_set);
-	for (dasm_comment* item = iter.first(); item != iter.last(); item = iter.next())
+	for (std::set<dasm_comment>::iterator item = m_comment_set.begin(); item != m_comment_set.end(); ++item)
 	{
 		xml_data_node *datanode = xml_add_child(&curnode, "comment", xml_normalize_string(item->m_text));
 		if (datanode == NULL)
