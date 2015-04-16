08747b36ecb01d5d77e33908372ac132fdd61453
Miodrag Milanovic <mmicko@gmail.com>
2015-04-16 13:44:11 +0200

    test for linking (nw)

--- scripts/genie.lua
+++ scripts/genie.lua
@@ -1,6 +1,5 @@
 premake.check_paths = true
 premake.make.override = { "TARGET" }
-premake.make.linkoptions_after = true
 MAME_DIR = (path.getabsolute("..") .. "/")
 local MAME_BUILD_DIR = (MAME_DIR .. "build/")
 local naclToolchain = ""
