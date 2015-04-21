edd9d41c040fa836b29ccb4d851507f83accdf69
Curt Coder <curtcoder@mail.com>
2015-04-21 09:18:03 +0300

    (MESS) victor9k: Added writing to sector dump disk images. [Curt Coder]

--- src/lib/formats/victor9k_dsk.h
+++ src/lib/formats/victor9k_dsk.h
@@ -31,13 +31,10 @@ public:
 	virtual const char *description() const;
 	virtual const char *extensions() const;
 
-	int find_size(io_generic *io, UINT32 form_factor);
 	virtual int identify(io_generic *io, UINT32 form_factor);
-	void log_boot_sector(UINT8 *data);
-	floppy_image_format_t::desc_e* get_sector_desc(const format &f, int &current_size, int sector_count);
-	void build_sector_description(const format &f, UINT8 *sectdata, UINT32 sect_offs, desc_s *sectors, int sector_count) const;
 	virtual bool load(io_generic *io, UINT32 form_factor, floppy_image *image);
-	virtual bool supports_save() const;
+	virtual bool save(io_generic *io, floppy_image *image);
+	virtual bool supports_save() const { return true; }
 
 	static int get_rpm(int head, int track);
 
@@ -48,6 +45,14 @@ protected:
 	static const int sectors_per_track[2][80];
 	static const int speed_zone[2][80];
 	static const int rpm[9];
+
+	int find_size(io_generic *io, UINT32 form_factor);
+	void log_boot_sector(UINT8 *data);
+	floppy_image_format_t::desc_e* get_sector_desc(const format &f, int &current_size, int sector_count);
+	void build_sector_description(const format &f, UINT8 *sectdata, UINT32 sect_offs, desc_s *sectors, int sector_count) const;
+	int get_image_offset(const format &f, int head, int track);
+	int compute_track_size(const format &f, int head, int track);
+	void extract_sectors(floppy_image *image, const format &f, desc_s *sdesc, int track, int head, int sector_count);
 };
 
 extern const floppy_format_type FLOPPY_VICTOR_9000_FORMAT;
