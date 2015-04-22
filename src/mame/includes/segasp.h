74f8b64c190807fb08fc18804c97b6099c3c3b6a
David Haywood <mamehaze@users.noreply.github.com>
2015-04-22 15:02:35 +0100

    additional SP info from MetalliC

---
+++ src/mame/includes/segasp.h
@@ -0,0 +1,20 @@
+/* Sega SP (Spider)
+
+ Naomi derived platform
+
+*/
+
+#include "naomi.h"
+
+class segasp_state : public naomi_state
+{
+public:
+	segasp_state(const machine_config &mconfig, device_type type, const char *tag)
+		: naomi_state(mconfig, type, tag)
+	{
+
+	}
+	
+protected:
+};
+
