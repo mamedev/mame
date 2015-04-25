0fbbb8234240ad34c646cbef67af62700b156e83
couriersud <couriersud@arcor.de>
2015-04-25 14:11:15 +0200

    Commented out a printf. (nw)

--- src/emu/cpu/m68000/m68kcpu.c
+++ src/emu/cpu/m68000/m68kcpu.c
@@ -677,7 +677,7 @@ static void m68k_presave(m68000_base_device *m68k)
 static void m68k_postload(m68000_base_device *m68k)
 {
 	m68ki_set_sr_noint_nosp(m68k, m68k->save_sr);
-	fprintf(stderr, "Reloaded, pc=%x\n", REG_PC(m68k));
+	//fprintf(stderr, "Reloaded, pc=%x\n", REG_PC(m68k));
 	m68k->stopped = (m68k->save_stopped ? STOP_LEVEL_STOP : 0) | (m68k->save_halted  ? STOP_LEVEL_HALT : 0);
 	m68ki_jump(m68k, REG_PC(m68k));
 }
