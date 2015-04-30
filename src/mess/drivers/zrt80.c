mamedev/mame

commit 2c4c5212ad6e847faa3a8b9d357b8f3d7d5e1392

ins8250_device: converted to devcb2 (nw)
git-svn-id: svn://dspnet.fr/mame/trunk@30287 749742ba-7341-0410-aadc-df50b521781e

src/mess/drivers/zrt80.c
@@ -250,17 +250,6 @@ MC6845_UPDATE_ROW( zrt80_state::crtc_update_row )
 	}
 }
 
-
-static const ins8250_interface zrt80_com_interface =
-{
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_CPU_INPUT_LINE("maincpu", INPUT_LINE_IRQ0),
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL
-};
-
 WRITE8_MEMBER( zrt80_state::kbd_put )
 {
 	m_term_data = data;
@@ -312,7 +301,8 @@ static MACHINE_CONFIG_START( zrt80, zrt80_state )
 	MCFG_MC6845_CHAR_WIDTH(8) /*?*/
 	MCFG_MC6845_UPDATE_ROW_CB(zrt80_state, crtc_update_row)
 
-	MCFG_INS8250_ADD( "ins8250", zrt80_com_interface, 2457600 )
+	MCFG_DEVICE_ADD( "ins8250", INS8250, 2457600 )
+	MCFG_INS8250_OUT_RTS_CB(INPUTLINE("maincpu", INPUT_LINE_IRQ0))
 	MCFG_DEVICE_ADD(KEYBOARD_TAG, GENERIC_KEYBOARD, 0)
 	MCFG_GENERIC_KEYBOARD_CB(WRITE8(zrt80_state, kbd_put))
 MACHINE_CONFIG_END

src/emu/bus/isa/com.c
@@ -11,42 +11,6 @@
 #include "bus/rs232/null_modem.h"
 #include "machine/ins8250.h"
 
-static const ins8250_interface genpc_com_interface[2]=
-{
-	{
-		DEVCB_DEVICE_LINE_MEMBER("serport0", rs232_port_device, write_txd),
-		DEVCB_DEVICE_LINE_MEMBER("serport0", rs232_port_device, write_dtr),
-		DEVCB_DEVICE_LINE_MEMBER("serport0", rs232_port_device, write_rts),
-		DEVCB_DEVICE_LINE_MEMBER(DEVICE_SELF_OWNER, isa8_com_device, pc_com_interrupt_1),
-		DEVCB_NULL,
-		DEVCB_NULL
-	},
-	{
-		DEVCB_DEVICE_LINE_MEMBER("serport1", rs232_port_device, write_txd),
-		DEVCB_DEVICE_LINE_MEMBER("serport1", rs232_port_device, write_dtr),
-		DEVCB_DEVICE_LINE_MEMBER("serport1", rs232_port_device, write_rts),
-		DEVCB_DEVICE_LINE_MEMBER(DEVICE_SELF_OWNER, isa8_com_device, pc_com_interrupt_2),
-		DEVCB_NULL,
-		DEVCB_NULL
-	}/*,
-    {
-        DEVCB_DEVICE_LINE_MEMBER("serport2", rs232_port_device, write_txd),
-        DEVCB_DEVICE_LINE_MEMBER("serport2", rs232_port_device, write_dtr),
-        DEVCB_DEVICE_LINE_MEMBER("serport2", rs232_port_device, write_rts),
-        DEVCB_DEVICE_LINE_MEMBER(DEVICE_SELF_OWNER, isa8_com_device, pc_com_interrupt_1),
-        DEVCB_NULL,
-        DEVCB_NULL
-    },
-    {
-        DEVCB_DEVICE_LINE_MEMBER("serport3", rs232_port_device, write_txd),
-        DEVCB_DEVICE_LINE_MEMBER("serport3", rs232_port_device, write_dtr),
-        DEVCB_DEVICE_LINE_MEMBER("serport3", rs232_port_device, write_rts),
-        DEVCB_DEVICE_LINE_MEMBER(DEVICE_SELF_OWNER, isa8_com_device, pc_com_interrupt_2),
-        DEVCB_NULL,
-        DEVCB_NULL
-    }*/
-};
-
 static SLOT_INTERFACE_START(isa_com)
 	SLOT_INTERFACE("microsoft_mouse", MSFT_SERIAL_MOUSE)
 	SLOT_INTERFACE("msystems_mouse", MSYSTEM_SERIAL_MOUSE)
@@ -55,10 +19,26 @@ static SLOT_INTERFACE_START(isa_com)
 SLOT_INTERFACE_END
 
 static MACHINE_CONFIG_FRAGMENT( com_config )
-	MCFG_INS8250_ADD( "uart_0", genpc_com_interface[0], XTAL_1_8432MHz )
-	MCFG_INS8250_ADD( "uart_1", genpc_com_interface[1], XTAL_1_8432MHz )
-	//MCFG_INS8250_ADD( "uart_2", genpc_com_interface[2], XTAL_1_8432MHz )
-	//MCFG_INS8250_ADD( "uart_3", genpc_com_interface[3], XTAL_1_8432MHz )
+	MCFG_DEVICE_ADD( "uart_0", INS8250, XTAL_1_8432MHz )
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE("serport0", rs232_port_device, write_txd))
+	MCFG_INS8250_OUT_DTR_CB(DEVWRITELINE("serport0", rs232_port_device, write_dtr))
+	MCFG_INS8250_OUT_RTS_CB(DEVWRITELINE("serport0", rs232_port_device, write_rts))
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(isa8_com_device, pc_com_interrupt_1))
+	MCFG_DEVICE_ADD( "uart_1", INS8250, XTAL_1_8432MHz )
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE("serport1", rs232_port_device, write_txd))
+	MCFG_INS8250_OUT_DTR_CB(DEVWRITELINE("serport1", rs232_port_device, write_dtr))
+	MCFG_INS8250_OUT_RTS_CB(DEVWRITELINE("serport1", rs232_port_device, write_rts))
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(isa8_com_device, pc_com_interrupt_2))
+	/*MCFG_DEVICE_ADD( "uart_2", INS8250, XTAL_1_8432MHz )
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE("serport2", rs232_port_device, write_txd))
+	MCFG_INS8250_OUT_DTR_CB(DEVWRITELINE("serport2", rs232_port_device, write_dtr))
+	MCFG_INS8250_OUT_RTS_CB(DEVWRITELINE("serport2", rs232_port_device, write_rts))
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(isa8_com_device, pc_com_interrupt_1))
+	MCFG_DEVICE_ADD( "uart_3", INS8250, XTAL_1_8432MHz )
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE("serport3", rs232_port_device, write_txd))
+	MCFG_INS8250_OUT_DTR_CB(DEVWRITELINE("serport3", rs232_port_device, write_dtr))
+	MCFG_INS8250_OUT_RTS_CB(DEVWRITELINE("serport3", rs232_port_device, write_rts))
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(isa8_com_device, pc_com_interrupt_2))*/
 
 	MCFG_RS232_PORT_ADD( "serport0", isa_com, "microsoft_mouse" )
 	MCFG_RS232_RXD_HANDLER(DEVWRITELINE("uart_0", ins8250_uart_device, rx_w))
@@ -148,10 +128,26 @@ void isa8_com_device::device_reset()
 }
 
 static MACHINE_CONFIG_FRAGMENT( com_at_config )
-	MCFG_NS16450_ADD( "uart_0", genpc_com_interface[0], XTAL_1_8432MHz ) /* Verified: IBM P/N 6320947 Serial/Parallel card uses an NS16450N */
-	MCFG_NS16450_ADD( "uart_1", genpc_com_interface[1], XTAL_1_8432MHz )
-//  MCFG_NS16450_ADD( "uart_2", genpc_com_interface[2], XTAL_1_8432MHz )
-//  MCFG_NS16450_ADD( "uart_3", genpc_com_interface[3], XTAL_1_8432MHz )
+	MCFG_DEVICE_ADD( "uart_0", NS16450, XTAL_1_8432MHz ) /* Verified: IBM P/N 6320947 Serial/Parallel card uses an NS16450N */
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE("serport0", rs232_port_device, write_txd))
+	MCFG_INS8250_OUT_DTR_CB(DEVWRITELINE("serport0", rs232_port_device, write_dtr))
+	MCFG_INS8250_OUT_RTS_CB(DEVWRITELINE("serport0", rs232_port_device, write_rts))
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(isa8_com_device, pc_com_interrupt_1))
+	MCFG_DEVICE_ADD( "uart_1", NS16450, XTAL_1_8432MHz )
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE("serport1", rs232_port_device, write_txd))
+	MCFG_INS8250_OUT_DTR_CB(DEVWRITELINE("serport1", rs232_port_device, write_dtr))
+	MCFG_INS8250_OUT_RTS_CB(DEVWRITELINE("serport1", rs232_port_device, write_rts))
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(isa8_com_device, pc_com_interrupt_2))
+	/*MCFG_DEVICE_ADD( "uart_2", NS16450, XTAL_1_8432MHz )
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE("serport2", rs232_port_device, write_txd))
+	MCFG_INS8250_OUT_DTR_CB(DEVWRITELINE("serport2", rs232_port_device, write_dtr))
+	MCFG_INS8250_OUT_RTS_CB(DEVWRITELINE("serport2", rs232_port_device, write_rts))
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(isa8_com_device, pc_com_interrupt_1))
+	MCFG_DEVICE_ADD( "uart_3", NS16450, XTAL_1_8432MHz )
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE("serport3", rs232_port_device, write_txd))
+	MCFG_INS8250_OUT_DTR_CB(DEVWRITELINE("serport3", rs232_port_device, write_dtr))
+	MCFG_INS8250_OUT_RTS_CB(DEVWRITELINE("serport3", rs232_port_device, write_rts))
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(isa8_com_device, pc_com_interrupt_2))*/
 	MCFG_RS232_PORT_ADD( "serport0", isa_com, "microsoft_mouse" )
 	MCFG_RS232_RXD_HANDLER(DEVWRITELINE("uart_0", ins8250_uart_device, rx_w))
 	MCFG_RS232_DCD_HANDLER(DEVWRITELINE("uart_0", ins8250_uart_device, dcd_w))

src/emu/bus/s100/wunderbus.c
@@ -60,52 +60,6 @@ WRITE_LINE_MEMBER( s100_wunderbus_device::pic_int_w )
 	m_bus->irq_w(state);
 }
 
-
-//-------------------------------------------------
-//  ins8250_interface ace1_intf
-//-------------------------------------------------
-
-static ins8250_interface ace1_intf =
-{
-	DEVCB_DEVICE_LINE_MEMBER(RS232_A_TAG, rs232_port_device, write_txd),
-	DEVCB_DEVICE_LINE_MEMBER(RS232_A_TAG, rs232_port_device, write_dtr),
-	DEVCB_DEVICE_LINE_MEMBER(RS232_A_TAG, rs232_port_device, write_rts),
-	DEVCB_DEVICE_LINE_MEMBER(I8259A_TAG, pic8259_device, ir3_w),
-	DEVCB_NULL,
-	DEVCB_NULL
-};
-
-
-//-------------------------------------------------
-//  ins8250_interface ace2_intf
-//-------------------------------------------------
-
-static ins8250_interface ace2_intf =
-{
-	DEVCB_DEVICE_LINE_MEMBER(RS232_B_TAG, rs232_port_device, write_txd),
-	DEVCB_DEVICE_LINE_MEMBER(RS232_B_TAG, rs232_port_device, write_dtr),
-	DEVCB_DEVICE_LINE_MEMBER(RS232_B_TAG, rs232_port_device, write_rts),
-	DEVCB_DEVICE_LINE_MEMBER(I8259A_TAG, pic8259_device, ir4_w),
-	DEVCB_NULL,
-	DEVCB_NULL
-};
-
-
-//-------------------------------------------------
-//  ins8250_interface ace3_intf
-//-------------------------------------------------
-
-static ins8250_interface ace3_intf =
-{
-	DEVCB_DEVICE_LINE_MEMBER(RS232_C_TAG, rs232_port_device, write_txd),
-	DEVCB_DEVICE_LINE_MEMBER(RS232_C_TAG, rs232_port_device, write_dtr),
-	DEVCB_DEVICE_LINE_MEMBER(RS232_C_TAG, rs232_port_device, write_rts),
-	DEVCB_DEVICE_LINE_MEMBER(I8259A_TAG, pic8259_device, ir5_w),
-	DEVCB_NULL,
-	DEVCB_NULL
-};
-
-
 static DEVICE_INPUT_DEFAULTS_START( terminal )
 	DEVICE_INPUT_DEFAULTS( "RS232_TXBAUD", 0xff, RS232_BAUD_110 )
 	DEVICE_INPUT_DEFAULTS( "RS232_RXBAUD", 0xff, RS232_BAUD_110 )
@@ -136,9 +90,21 @@ WRITE_LINE_MEMBER( s100_wunderbus_device::rtc_tp_w )
 
 static MACHINE_CONFIG_FRAGMENT( s100_wunderbus )
 	MCFG_PIC8259_ADD(I8259A_TAG, DEVWRITELINE(DEVICE_SELF, s100_wunderbus_device, pic_int_w), VCC, NULL)
-	MCFG_INS8250_ADD(INS8250_1_TAG, ace1_intf, XTAL_18_432MHz/10)
-	MCFG_INS8250_ADD(INS8250_2_TAG, ace2_intf, XTAL_18_432MHz/10)
-	MCFG_INS8250_ADD(INS8250_3_TAG, ace3_intf, XTAL_18_432MHz/10)
+	MCFG_DEVICE_ADD(INS8250_1_TAG, INS8250, XTAL_18_432MHz/10)
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE(RS232_A_TAG, rs232_port_device, write_txd))
+	MCFG_INS8250_OUT_DTR_CB(DEVWRITELINE(RS232_A_TAG, rs232_port_device, write_dtr))
+	MCFG_INS8250_OUT_RTS_CB(DEVWRITELINE(RS232_A_TAG, rs232_port_device, write_rts))
+	MCFG_INS8250_OUT_INT_CB(DEVWRITELINE(I8259A_TAG, pic8259_device, ir3_w))
+	MCFG_DEVICE_ADD(INS8250_2_TAG, INS8250, XTAL_18_432MHz/10)
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE(RS232_B_TAG, rs232_port_device, write_txd))
+	MCFG_INS8250_OUT_DTR_CB(DEVWRITELINE(RS232_B_TAG, rs232_port_device, write_dtr))
+	MCFG_INS8250_OUT_RTS_CB(DEVWRITELINE(RS232_B_TAG, rs232_port_device, write_rts))
+	MCFG_INS8250_OUT_INT_CB(DEVWRITELINE(I8259A_TAG, pic8259_device, ir4_w))
+	MCFG_DEVICE_ADD(INS8250_3_TAG, INS8250, XTAL_18_432MHz/10)
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE(RS232_C_TAG, rs232_port_device, write_txd))
+	MCFG_INS8250_OUT_DTR_CB(DEVWRITELINE(RS232_C_TAG, rs232_port_device, write_dtr))
+	MCFG_INS8250_OUT_RTS_CB(DEVWRITELINE(RS232_C_TAG, rs232_port_device, write_rts))
+	MCFG_INS8250_OUT_INT_CB(DEVWRITELINE(I8259A_TAG, pic8259_device, ir5_w))
 
 	MCFG_RS232_PORT_ADD(RS232_A_TAG, default_rs232_devices, "terminal")
 	MCFG_RS232_RXD_HANDLER(DEVWRITELINE(INS8250_1_TAG, ins8250_uart_device, rx_w))

src/emu/machine/ins8250.c
@@ -91,7 +91,13 @@ const device_type PC16552D = &device_creator<pc16552_device>;
 
 ins8250_uart_device::ins8250_uart_device(const machine_config &mconfig, device_type type, const char* name, const char *tag, device_t *owner, UINT32 clock, const char *shortname)
 		: device_t(mconfig, type, name, tag, owner, clock, shortname, __FILE__),
-			device_serial_interface(mconfig, *this)
+			device_serial_interface(mconfig, *this),
+			m_out_tx_cb(*this),
+			m_out_dtr_cb(*this),
+			m_out_rts_cb(*this),
+			m_out_int_cb(*this),
+			m_out_out1_cb(*this),
+			m_out_out2_cb(*this)
 {
 }
 
@@ -172,7 +178,7 @@ void ins8250_uart_device::update_interrupt()
 	}
 
 	/* set or clear the int */
-	m_out_int_func(state);
+	m_out_int_cb(state);
 }
 
 /* set pending bit and trigger int */
@@ -298,10 +304,10 @@ WRITE8_MEMBER( ins8250_uart_device::ins8250_w )
 				}
 				else
 				{
-					m_out_dtr_func(m_regs.mcr & 1);
-					m_out_rts_func(m_regs.mcr & 2);
-					m_out_out1_func(m_regs.mcr & 4);
-					m_out_out2_func(m_regs.mcr & 8);
+					m_out_dtr_cb(m_regs.mcr & 1);
+					m_out_rts_cb(m_regs.mcr & 2);
+					m_out_out1_cb(m_regs.mcr & 4);
+					m_out_out2_cb(m_regs.mcr & 8);
 				}
 			}
 			break;
@@ -476,7 +482,7 @@ void ins8250_uart_device::tra_complete()
 
 void ins8250_uart_device::tra_callback()
 {
-	m_out_tx_func(transmit_register_get_data_bit());
+	m_out_tx_cb(transmit_register_get_data_bit());
 }
 
 void ins8250_uart_device::update_msr(int bit, UINT8 state)
@@ -511,12 +517,12 @@ WRITE_LINE_MEMBER(ins8250_uart_device::cts_w)
 
 void ins8250_uart_device::device_start()
 {
-	m_out_tx_func.resolve(m_out_tx_cb, *this);
-	m_out_dtr_func.resolve(m_out_dtr_cb, *this);
-	m_out_rts_func.resolve(m_out_rts_cb, *this);
-	m_out_int_func.resolve(m_out_int_cb, *this);
-	m_out_out1_func.resolve(m_out_out1_cb, *this);
-	m_out_out2_func.resolve(m_out_out2_cb, *this);
+	m_out_tx_cb.resolve_safe();
+	m_out_dtr_cb.resolve_safe();
+	m_out_rts_cb.resolve_safe();
+	m_out_int_cb.resolve_safe();
+	m_out_out1_cb.resolve_safe();
+	m_out_out2_cb.resolve_safe();
 	set_tra_rate(0);
 	set_rcv_rate(0);
 	memset(&m_regs, 0x00, sizeof(m_regs));
@@ -533,29 +539,11 @@ void ins8250_uart_device::device_reset()
 	m_int_pending = 0;
 	receive_register_reset();
 	transmit_register_reset();
-	m_out_rts_func(0);
-	m_out_dtr_func(0);
-	m_out_out1_func(0);
-	m_out_out2_func(0);
-	m_out_tx_func(1);
-}
-
-void ins8250_uart_device::device_config_complete()
-{
-	const ins8250_interface *intf = reinterpret_cast<const ins8250_interface *>(static_config());
-	if(intf != NULL)
-	{
-		*static_cast<ins8250_interface *>(this) = *intf;
-	}
-	else
-	{
-		memset(&m_out_tx_cb, 0, sizeof(m_out_tx_cb));
-		memset(&m_out_dtr_cb, 0, sizeof(m_out_dtr_cb));
-		memset(&m_out_rts_cb, 0, sizeof(m_out_rts_cb));
-		memset(&m_out_int_cb, 0, sizeof(m_out_int_cb));
-		memset(&m_out_out1_cb, 0, sizeof(m_out_out1_cb));
-		memset(&m_out_out2_cb, 0, sizeof(m_out_out2_cb));
-	}
+	m_out_rts_cb(0);
+	m_out_dtr_cb(0);
+	m_out_out1_cb(0);
+	m_out_out2_cb(0);
+	m_out_tx_cb(1);
 }
 
 void ins8250_uart_device::device_timer(emu_timer &timer, device_timer_id id, int param, void *ptr)

src/emu/machine/ins8250.h
@@ -12,22 +12,20 @@
 /***************************************************************************
     CLASS DEFINITIONS
 ***************************************************************************/
-struct ins8250_interface
-{
-	devcb_write_line    m_out_tx_cb;
-	devcb_write_line    m_out_dtr_cb;
-	devcb_write_line    m_out_rts_cb;
-	devcb_write_line    m_out_int_cb;
-	devcb_write_line    m_out_out1_cb;
-	devcb_write_line    m_out_out2_cb;
-};
 
 class ins8250_uart_device : public device_t,
-							public device_serial_interface,
-							public ins8250_interface
+							public device_serial_interface
 {
 public:
 	ins8250_uart_device(const machine_config &mconfig, device_type type, const char* name, const char *tag, device_t *owner, UINT32 clock, const char *shortname);
+	
+	template<class _Object> static devcb2_base &set_out_tx_callback(device_t &device, _Object object) { return downcast<ins8250_uart_device &>(device).m_out_tx_cb.set_callback(object); }
+	template<class _Object> static devcb2_base &set_out_dtr_callback(device_t &device, _Object object) { return downcast<ins8250_uart_device &>(device).m_out_dtr_cb.set_callback(object); }
+	template<class _Object> static devcb2_base &set_out_rts_callback(device_t &device, _Object object) { return downcast<ins8250_uart_device &>(device).m_out_rts_cb.set_callback(object); }
+	template<class _Object> static devcb2_base &set_out_int_callback(device_t &device, _Object object) { return downcast<ins8250_uart_device &>(device).m_out_int_cb.set_callback(object); }
+	template<class _Object> static devcb2_base &set_out_out1_callback(device_t &device, _Object object) { return downcast<ins8250_uart_device &>(device).m_out_out1_cb.set_callback(object); }
+	template<class _Object> static devcb2_base &set_out_out2_callback(device_t &device, _Object object) { return downcast<ins8250_uart_device &>(device).m_out_out2_cb.set_callback(object); }
+	
 	DECLARE_WRITE8_MEMBER( ins8250_w );
 	DECLARE_READ8_MEMBER( ins8250_r );
 	DECLARE_WRITE_LINE_MEMBER( dcd_w );
@@ -38,7 +36,6 @@ public:
 
 protected:
 	virtual void device_start();
-	virtual void device_config_complete();
 	virtual void device_reset();
 	virtual void rcv_complete();
 	virtual void tra_complete();
@@ -76,12 +73,12 @@ protected:
 private:
 	UINT8 m_int_pending;
 
-	devcb_resolved_write_line   m_out_tx_func;
-	devcb_resolved_write_line   m_out_dtr_func;
-	devcb_resolved_write_line   m_out_rts_func;
-	devcb_resolved_write_line   m_out_int_func;
-	devcb_resolved_write_line   m_out_out1_func;
-	devcb_resolved_write_line   m_out_out2_func;
+	devcb2_write_line    m_out_tx_cb;
+	devcb2_write_line    m_out_dtr_cb;
+	devcb2_write_line    m_out_rts_cb;
+	devcb2_write_line    m_out_int_cb;
+	devcb2_write_line    m_out_out1_cb;
+	devcb2_write_line    m_out_out2_cb;
 
 	void update_interrupt();
 	void update_msr(int bit, UINT8 state);
@@ -147,23 +144,22 @@ extern const device_type NS16550;
     DEVICE CONFIGURATION MACROS
 ***************************************************************************/
 
-#define MCFG_INS8250_ADD(_tag, _intrf, _clock) \
-	MCFG_DEVICE_ADD(_tag, INS8250, _clock) \
-	MCFG_DEVICE_CONFIG(_intrf)
+#define MCFG_INS8250_OUT_TX_CB(_devcb) \
+	devcb = &ins8250_uart_device::set_out_tx_callback(*device, DEVCB2_##_devcb);
 
+#define MCFG_INS8250_OUT_DTR_CB(_devcb) \
+	devcb = &ins8250_uart_device::set_out_dtr_callback(*device, DEVCB2_##_devcb);
 
-#define MCFG_NS16450_ADD(_tag, _intrf, _clock) \
-	MCFG_DEVICE_ADD(_tag, NS16450, _clock) \
-	MCFG_DEVICE_CONFIG(_intrf)
+#define MCFG_INS8250_OUT_RTS_CB(_devcb) \
+	devcb = &ins8250_uart_device::set_out_rts_callback(*device, DEVCB2_##_devcb);
 
+#define MCFG_INS8250_OUT_INT_CB(_devcb) \
+	devcb = &ins8250_uart_device::set_out_int_callback(*device, DEVCB2_##_devcb);
 
-#define MCFG_NS16550_ADD(_tag, _intrf, _clock) \
-	MCFG_DEVICE_ADD(_tag, NS16550, _clock) \
-	MCFG_DEVICE_CONFIG(_intrf)
+#define MCFG_INS8250_OUT_OUT1_CB(_devcb) \
+	devcb = &ins8250_uart_device::set_out_out1_callback(*device, DEVCB2_##_devcb);
 
-#define MCFG_PC16552D_ADD(_tag, _intf0, _intf1, _clock) \
-	MCFG_DEVICE_ADD(_tag, PC16552D, 0) \
-	MCFG_NS16550_ADD(_tag ":chan0", _intf0, _clock) \
-	MCFG_NS16550_ADD(_tag ":chan1", _intf1, _clock)
+#define MCFG_INS8250_OUT_OUT2_CB(_devcb) \
+	devcb = &ins8250_uart_device::set_out_out2_callback(*device, DEVCB2_##_devcb);
 
 #endif

src/mame/drivers/firebeat.c
@@ -1095,25 +1095,6 @@ static void comm_uart_irq_callback(running_machine &machine, int channel, int va
     //m_maincpu->set_input_line(INPUT_LINE_IRQ2, ASSERT_LINE);
 }
 */
-static const ins8250_interface firebeat_com0_interface =
-{
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL
-};
-
-static const ins8250_interface firebeat_com1_interface =
-{
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL
-};
 
 /*****************************************************************************/
 
@@ -1190,25 +1171,6 @@ WRITE_LINE_MEMBER(firebeat_state::midi_uart_ch1_irq_callback)
 		m_maincpu->set_input_line(INPUT_LINE_IRQ1, CLEAR_LINE);
 }
 
-static const ins8250_interface firebeat_midi0_interface =
-{
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_DRIVER_LINE_MEMBER(firebeat_state, midi_uart_ch0_irq_callback),
-	DEVCB_NULL,
-	DEVCB_NULL
-};
-
-static const ins8250_interface firebeat_midi1_interface =
-{
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_DRIVER_LINE_MEMBER(firebeat_state, midi_uart_ch1_irq_callback),
-	DEVCB_NULL,
-	DEVCB_NULL
-};
 /*
 static const int keyboard_notes[24] =
 {
@@ -1778,8 +1740,14 @@ static MACHINE_CONFIG_START( firebeat, firebeat_state )
 	MCFG_SOUND_ROUTE(0, "lspeaker", 1.0)
 	MCFG_SOUND_ROUTE(1, "rspeaker", 1.0)
 
-	MCFG_PC16552D_ADD("duart_com", firebeat_com0_interface, firebeat_com1_interface, XTAL_19_6608MHz) // pgmd to 9600baud
-	MCFG_PC16552D_ADD("duart_midi", firebeat_midi0_interface, firebeat_midi1_interface, XTAL_24MHz) // in all memory maps, pgmd to 31250baud
+	MCFG_DEVICE_ADD("duart_com", PC16552D, 0)  // pgmd to 9600baud
+	MCFG_DEVICE_ADD("duart_com:chan0", NS16550, XTAL_19_6608MHz)
+	MCFG_DEVICE_ADD("duart_com:chan1", NS16550, XTAL_19_6608MHz)
+	MCFG_DEVICE_ADD("duart_midi", PC16552D, 0)  // in all memory maps, pgmd to 31250baud
+	MCFG_DEVICE_ADD("duart_midi:chan0", NS16550, XTAL_24MHz)
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(firebeat_state, midi_uart_ch0_irq_callback))
+	MCFG_DEVICE_ADD("duart_midi:chan1", NS16550, XTAL_24MHz)
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(firebeat_state, midi_uart_ch1_irq_callback))
 MACHINE_CONFIG_END
 
 static MACHINE_CONFIG_START( firebeat2, firebeat_state )
@@ -1834,8 +1802,14 @@ static MACHINE_CONFIG_START( firebeat2, firebeat_state )
 	MCFG_SOUND_ROUTE(0, "lspeaker", 1.0)
 	MCFG_SOUND_ROUTE(1, "rspeaker", 1.0)
 
-	MCFG_PC16552D_ADD("duart_com", firebeat_com0_interface, firebeat_com1_interface, XTAL_19_6608MHz)
-	MCFG_PC16552D_ADD("duart_midi", firebeat_midi0_interface, firebeat_midi1_interface, XTAL_24MHz)
+	MCFG_DEVICE_ADD("duart_com", PC16552D, 0)
+	MCFG_DEVICE_ADD("duart_com:chan0", NS16550, XTAL_19_6608MHz)
+	MCFG_DEVICE_ADD("duart_com:chan1", NS16550, XTAL_19_6608MHz)
+	MCFG_DEVICE_ADD("duart_midi", PC16552D, 0)
+	MCFG_DEVICE_ADD("duart_midi:chan0", NS16550, XTAL_24MHz)
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(firebeat_state, midi_uart_ch0_irq_callback))
+	MCFG_DEVICE_ADD("duart_midi:chan1", NS16550, XTAL_24MHz)
+	MCFG_INS8250_OUT_INT_CB(WRITELINE(firebeat_state, midi_uart_ch1_irq_callback))
 	MCFG_MIDI_KBD_ADD("kbd0", DEVWRITELINE("duart_midi:chan0", ins8250_uart_device, rx_w), 31250)
 	MCFG_MIDI_KBD_ADD("kbd1", DEVWRITELINE("duart_midi:chan1", ins8250_uart_device, rx_w), 31250)
 MACHINE_CONFIG_END

src/mame/drivers/magtouch.c
@@ -98,23 +98,6 @@ public:
 	virtual void machine_start();
 };
 
-
-/*************************************
- *
- *  Microtouch <-> ins8250 interface
- *
- *************************************/
-
-static const ins8250_interface magtouch_com0_interface =
-{
-	DEVCB_DEVICE_LINE_MEMBER("microtouch", microtouch_serial_device, rx),
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_DEVICE_LINE_MEMBER("pic8259_1", pic8259_device, ir4_w),
-	DEVCB_NULL,
-	DEVCB_NULL
-};
-
 /*************************************
  *
  *  ROM banking
@@ -191,7 +174,9 @@ static MACHINE_CONFIG_START( magtouch, magtouch_state )
 	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
 
 	MCFG_FRAGMENT_ADD( pcat_common )
-	MCFG_NS16450_ADD( "ns16450_0", magtouch_com0_interface, XTAL_1_8432MHz )
+	MCFG_DEVICE_ADD( "ns16450_0", NS16450, XTAL_1_8432MHz )
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE("microtouch", microtouch_serial_device, rx))
+	MCFG_INS8250_OUT_INT_CB(DEVWRITELINE("pic8259_1", pic8259_device, ir4_w))
 	MCFG_MICROTOUCH_SERIAL_ADD( "microtouch", 9600, DEVWRITELINE("ns16450_0", ins8250_uart_device, rx_w) ) // rate?
 MACHINE_CONFIG_END
 

src/mame/drivers/meritm.c
@@ -253,23 +253,6 @@ public:
 #define UART_CLK    XTAL_1_8432MHz // standard 8250 clock
 
 
-
-/*************************************
- *
- *  Microtouch <-> pc16550 interface
- *
- *************************************/
-
-static const ins8250_interface meritm_ns16550_interface =
-{
-	DEVCB_DEVICE_LINE_MEMBER("microtouch", microtouch_serial_device, rx),
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL,
-	DEVCB_NULL
-};
-
 /*************************************
  *
  *  Microtouch touch coordinate transformation
@@ -1121,7 +1104,8 @@ static MACHINE_CONFIG_DERIVED( meritm_crt250_crt252_crt258, meritm_crt250_questi
 	MCFG_CPU_IO_MAP(meritm_crt250_crt258_io_map)
 	MCFG_MACHINE_START_OVERRIDE(meritm_state,meritm_crt250_crt252_crt258)
 
-	MCFG_NS16550_ADD("ns16550", meritm_ns16550_interface, UART_CLK)
+	MCFG_DEVICE_ADD("ns16550", NS16550, UART_CLK)
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE("microtouch", microtouch_serial_device, rx))
 	MCFG_MICROTOUCH_SERIAL_ADD("microtouch", 9600, DEVWRITELINE("ns16550", ins8250_uart_device, rx_w))
 	MCFG_MICROTOUCH_TOUCH_CB(meritm_state, meritm_touch_coord_transform)
 MACHINE_CONFIG_END
@@ -1138,7 +1122,8 @@ static MACHINE_CONFIG_DERIVED( meritm_crt260, meritm_crt250 )
 	MCFG_WATCHDOG_TIME_INIT(attotime::from_msec(1200))  // DS1232, TD connected to VCC
 	MCFG_MACHINE_START_OVERRIDE(meritm_state,meritm_crt260)
 
-	MCFG_NS16550_ADD("ns16550", meritm_ns16550_interface, UART_CLK)
+	MCFG_DEVICE_ADD("ns16550", NS16550, UART_CLK)
+	MCFG_INS8250_OUT_TX_CB(DEVWRITELINE("microtouch", microtouch_serial_device, rx))
 	MCFG_MICROTOUCH_SERIAL_ADD("microtouch", 9600, DEVWRITELINE("ns16550", ins8250_uart_device, rx_w))
 	MCFG_MICROTOUCH_TOUCH_CB(meritm_state, meritm_touch_coord_transform)
 MACHINE_CONFIG_END

commit 4e152d7a49b4d2f4e6b85cfc2b2a228123d60051

(MESS) ZRT80 : fixed regression from r30287 (2014-05-07) (nw)

src/mess/drivers/zrt80.c
@@ -302,7 +302,7 @@ static MACHINE_CONFIG_START( zrt80, zrt80_state )
 	MCFG_MC6845_UPDATE_ROW_CB(zrt80_state, crtc_update_row)
 
 	MCFG_DEVICE_ADD( "ins8250", INS8250, 2457600 )
-	MCFG_INS8250_OUT_RTS_CB(INPUTLINE("maincpu", INPUT_LINE_IRQ0))
+	MCFG_INS8250_OUT_INT_CB(INPUTLINE("maincpu", INPUT_LINE_IRQ0))
 	MCFG_DEVICE_ADD(KEYBOARD_TAG, GENERIC_KEYBOARD, 0)
 	MCFG_GENERIC_KEYBOARD_CB(WRITE8(zrt80_state, kbd_put))
 MACHINE_CONFIG_END
@@ -319,5 +319,5 @@ ROM_END
 
 /* Driver */
 
-/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY   FULLNAME       FLAGS */
+/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT  CLASS           INIT    COMPANY                   FULLNAME       FLAGS */
 COMP( 1982, zrt80,  0,       0,      zrt80,     zrt80, driver_device,    0, "Digital Research Computers", "ZRT-80", 0)