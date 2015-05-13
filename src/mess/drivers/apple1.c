47e6b78bb39cd1f67b22e378e12ce35218f2591a
Miodrag Milanovic <mmicko@gmail.com>
2015-05-13 09:35:49 +0200

    added Colin as well (nw)

--- src/mess/drivers/apple1.c
+++ src/mess/drivers/apple1.c
@@ -1,5 +1,5 @@
 // license:???
-// copyright-holders:Paul Daniels, R.Belmont
+// copyright-holders:Paul Daniels, Colin Howel, R.Belmont
 /**********************************************************************
 
 Apple I
