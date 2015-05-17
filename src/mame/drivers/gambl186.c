f4cab7c4df63ca6248125e0595c953966e4507d9
Peter Ferrie <peter.ferrie@gmail.com>
2015-05-16 18:20:02 -0700

    gambl186: further improvements to EEPROM behaviour [Peter Ferrie]

--- src/mame/drivers/gambl186.c
+++ src/mame/drivers/gambl186.c
@@ -146,7 +146,7 @@ READ16_MEMBER(gambl186_state::comms_r)
                     case 5:
                     {
                         m_comms_expect = 13;
-                        m_comms_blocks = 5;
+                        m_comms_blocks = 4;
                         break;
                     }
 
@@ -226,9 +226,14 @@ READ16_MEMBER(gambl186_state::comms_r)
                             {
                                 m_comms_expect = 3;
 
-                                if (m_comms_blocks < 5)
+                                if (m_comms_blocks < 4)
                                 {
                                     m_comms_data[m_comms_ind] += 5; //compensate for ack
+
+                                    if (m_comms_blocks == 2)
+                                    {
+                                        m_comms_expect = 2;
+                                    }
                                 }
 
                                 break;
