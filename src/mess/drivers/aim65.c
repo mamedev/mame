3deab48d09ef0eb06b5f5029d7d2d5328a14df03
Curt Coder <curtcoder@mail.com>
2015-05-22 22:26:00 +0300

    (MESS) aim65: Refactored to use the new 6532. (nw)

--- src/mess/drivers/aim65.c
+++ src/mess/drivers/aim65.c
@@ -36,8 +36,8 @@ Bugs
 static ADDRESS_MAP_START( aim65_mem, AS_PROGRAM, 8, aim65_state )
 	AM_RANGE( 0x1000, 0x9fff ) AM_NOP /* User available expansions */
 	AM_RANGE( 0xa000, 0xa00f ) AM_MIRROR(0x3f0) AM_DEVREADWRITE("via6522_1", via6522_device, read, write) // user via
-	AM_RANGE( 0xa400, 0xa47f ) AM_RAM /* RIOT RAM */
-	AM_RANGE( 0xa480, 0xa497 ) AM_DEVREADWRITE("riot", riot6532_device, read, write)
+	AM_RANGE( 0xa400, 0xa47f ) AM_DEVICE("riot", mos6532_t, ram_map)
+	AM_RANGE( 0xa480, 0xa497 ) AM_DEVICE("riot", mos6532_t, io_map)
 	AM_RANGE( 0xa498, 0xa7ff ) AM_NOP /* Not available */
 	AM_RANGE( 0xa800, 0xa80f ) AM_MIRROR(0x3f0) AM_DEVREADWRITE("via6522_0", via6522_device, read, write) // system via
 	AM_RANGE( 0xac00, 0xac03 ) AM_DEVREADWRITE("pia6821", pia6821_device, read, write)
@@ -196,10 +196,10 @@ static MACHINE_CONFIG_START( aim65, aim65_state )
 	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.25)
 
 	/* other devices */
-	MCFG_DEVICE_ADD("riot", RIOT6532, AIM65_CLOCK)
-	MCFG_RIOT6532_OUT_PA_CB(WRITE8(aim65_state, aim65_riot_a_w))
-	MCFG_RIOT6532_IN_PB_CB(READ8(aim65_state, aim65_riot_b_r))
-	MCFG_RIOT6532_IRQ_CB(INPUTLINE("maincpu", M6502_IRQ_LINE))
+	MCFG_DEVICE_ADD("riot", MOS6532n, AIM65_CLOCK)
+	MCFG_MOS6530n_OUT_PA_CB(WRITE8(aim65_state, aim65_riot_a_w))
+	MCFG_MOS6530n_IN_PB_CB(READ8(aim65_state, aim65_riot_b_r))
+	MCFG_MOS6530n_IRQ_CB(INPUTLINE("maincpu", M6502_IRQ_LINE))
 
 	MCFG_DEVICE_ADD("via6522_0", VIA6522, 0)
 	MCFG_VIA6522_READPB_HANDLER(READ8(aim65_state, aim65_pb_r))
