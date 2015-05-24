c26b5dadc48ad48d8a8cbbad082cb951dfca08e1
Robbbert <pac0446@bigpond.net.au>
2015-05-24 13:58:21 +1000

    (MESS) camplynx: added TAP cassette format.

---
+++ src/lib/formats/camplynx_cas.h
@@ -0,0 +1,15 @@
+// license:BSD-3-Clause
+// copyright-holders:Robbbert
+/*********************************************************************
+
+    camplynx_cas.h
+
+    Format code for Camputers Lynx cassette images (TAP format)
+
+*********************************************************************/
+
+
+#include "cassimg.h"
+
+CASSETTE_FORMATLIST_EXTERN(lynx48k_cassette_formats);
+CASSETTE_FORMATLIST_EXTERN(lynx128k_cassette_formats);
