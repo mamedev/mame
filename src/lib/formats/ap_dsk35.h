b5af929c5b38ee15bd7bb89866358b14836c7f0a
arbee <rb6502@users.noreply.github.com>
2015-05-23 22:13:10 -0400

    License self-service (nw)

--- src/lib/formats/ap_dsk35.h
+++ src/lib/formats/ap_dsk35.h
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:Olivier Galibert
+// copyright-holders:Nathan Woods, R. Belmont
 /*********************************************************************
 
     ap_dsk35.h
