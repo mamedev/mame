7510822c3af052392aa21c84c89967d3d0d3e7df
Miodrag Milanovic <mmicko@gmail.com>
2015-05-26 08:35:56 +0200

    sync main driver and res of files

--- src/mess/machine/apple3.c
+++ src/mess/machine/apple3.c
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:R. Belmont, Nathan Woods
+// copyright-holders:Nathan Woods,R. Belmont
 /***************************************************************************
 
     machine/apple3.c
