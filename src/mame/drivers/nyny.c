50243c0d1bc2f41e4baf910bbabd366bb4203dee
hap <happppp@users.noreply.github.com>
2015-05-26 18:33:50 +0200

    Zsolt did a nyny.c rewrite

--- src/mame/drivers/nyny.c
+++ src/mame/drivers/nyny.c
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:Darren Olafson
+// copyright-holders:Darren Olafson, Zsolt Vasvari
 /***************************************************************************
 
     New York! New York! hardware
