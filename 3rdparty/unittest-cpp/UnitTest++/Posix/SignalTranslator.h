eab88c568793f9d84fc612d83d25337df4d7a2ee
Curt Coder <curtcoder@mail.com>
2015-05-27 16:39:12 +0300

    Fixed clang build. (nw)

--- 3rdparty/unittest-cpp/UnitTest++/Posix/SignalTranslator.h
+++ 3rdparty/unittest-cpp/UnitTest++/Posix/SignalTranslator.h
@@ -22,8 +22,8 @@ private:
     struct sigaction m_old_SIGTRAP_action;
     struct sigaction m_old_SIGSEGV_action;
     struct sigaction m_old_SIGBUS_action;
-    struct sigaction m_old_SIGABRT_action;
-    struct sigaction m_old_SIGALRM_action;
+    //struct sigaction m_old_SIGABRT_action;
+    //struct sigaction m_old_SIGALRM_action;
 };
 
 #if !defined (__GNUC__)
@@ -35,7 +35,7 @@ private:
 #define UNITTEST_THROW_SIGNALS_POSIX_ONLY \
 	UnitTest::SignalTranslator sig; \
 	if (UNITTEST_EXTENSION sigsetjmp(*UnitTest::SignalTranslator::s_jumpTarget, 1) != 0) \
-        throw ("Unhandled system exception"); 
+        throw ("Unhandled system exception");
 
 }
 
