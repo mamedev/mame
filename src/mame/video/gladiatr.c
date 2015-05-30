87bbbfffc3ce0588a63db2d6647e829299b52ead
Miodrag Milanovic <mmicko@gmail.com>
2015-05-30 07:51:29 +0200

    Got answer from Steve Ellenoff (nw)

--- src/mame/video/gladiatr.c
+++ src/mame/video/gladiatr.c
@@ -1,4 +1,4 @@
-// license:???
+// license:BSD-3-Clause
 // copyright-holders:Victor Trucco,Steve Ellenoff,Phil Stroffolino,Tatsuyuki Satoh,Tomasz Slanina,Nicola Salmoria
 /***************************************************************************
 
