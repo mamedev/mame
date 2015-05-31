b1c1b6979a91596acb396716e52dabbc9bb49bac
Miodrag Milanovic <mmicko@gmail.com>
2015-05-31 06:52:52 +0200

    removed dumper (nw)

--- src/mame/drivers/fortecar.c
+++ src/mame/drivers/fortecar.c
@@ -1,5 +1,5 @@
 // license:???
-// copyright-holders:Angelo Salese, Roberto Fresca, Rob Ragon
+// copyright-holders:Angelo Salese, Roberto Fresca
 /*************************************************************************************************
 
   Forte Card
