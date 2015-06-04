27138786f8a654be620b8059db8dfe7b211ceb74
hap <happppp@users.noreply.github.com>
2015-06-04 12:19:20 +0200

    fixed .lst files whitespace problem [LnmVolbo]

--- src/build/makelist.py
+++ src/build/makelist.py
@@ -28,7 +28,7 @@ def parse_file(srcfile):
                 if c==13 and line[srcptr]==10:
                     srcptr+=1
                 continue
-            if c==' ':
+            if c==' ' or c==9:
                 continue
             if in_comment==1 and c=='*' and line[srcptr]=='/' :
                 srcptr+=1
