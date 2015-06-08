dfd5a68ff67f7c976851c7f055667bd102407a16
couriersud <couriersud@arcor.de>
2015-06-09 00:32:50 +0200

    Another go at fixing gcc 4.4

--- src/emu/netlist/tools/nl_convert.h
+++ src/emu/netlist/tools/nl_convert.h
@@ -10,10 +10,6 @@
 #ifndef NL_CONVERT_H_
 #define NL_CONVERT_H_
 
-//#include <cstddef>
-//#include <cstdarg>
-#include <cstring>
-
 #include "plib/pstring.h"
 #include "plib/plists.h"
 #include "plib/pparser.h"
