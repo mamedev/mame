9fc4731399cfb1791fcbac63e1976fce0dbd3f18
fulivi <fulivi@users.noreply.github.com>
2015-06-10 12:23:23 +0200

    hp64k: preliminary version of hp64k driver

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -128,6 +128,7 @@ CPUS["UCOM4"] = true
 CPUS["HMCS40"] = true
 CPUS["E0C6200"] = true
 CPUS["MELPS4"] = true
+CPUS["HPHYBRID"] = true
 
 --------------------------------------------------
 -- specify available sound cores; some of these are
@@ -1597,6 +1598,7 @@ files {
 	MAME_DIR .. "src/mess/drivers/hp9845.c",    
 	MAME_DIR .. "src/mess/drivers/hp9k.c",      
 	MAME_DIR .. "src/mess/drivers/hp9k_3xx.c",  
+        MAME_DIR .. "src/mess/drivers/hp64k.c",
 }
 
 createMESSProjects(_target, _subtarget, "hec2hrp")
