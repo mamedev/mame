d8f35df3bba271719f935eb36bdfc21a075b1e7b
T. Joseph Carter <iKarith@users.noreply.github.com>
2015-06-30 20:09:11 -0700

    Disable MAME's memory operator overrides for now

--- src/lib/util/corealloc.c
+++ src/lib/util/corealloc.c
@@ -100,7 +100,10 @@ memory_entry *memory_entry::s_freehead = NULL;
 //  OPERATOR REPLACEMENTS
 //**************************************************************************
 
-#ifndef NO_MEM_TRACKING
+// BEGIN libretro overrides to disable MAME's operator rplacements
+//ifndef NO_MEM_TRACKING
+#if 0
+// END libretro overrides to disable MAME's operator rplacements
 
 // standard new/delete operators (try to avoid using)
 void *operator new(std::size_t size) throw (std::bad_alloc) { return malloc_file_line(size, NULL, 0, false, true, false); }
