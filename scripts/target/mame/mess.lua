a43fac75ee6c89b66a22ca55052e0b1ee152a33e
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2015-07-05 19:16:40 -0300

    New non-working driver: Metamáquina 2 desktop 3d printer (This also provides an implementation of the AVR8 EIJMP instruction)

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -837,6 +837,7 @@ function linkProjects_mame_mess(_target, _subtarget)
 		"toshiba",
 		"trainer",
 		"trs",
+		"ultimachine",
 		"ultratec",
 		"unisys",
 		"veb",
@@ -2442,6 +2443,11 @@ files {
 	MAME_DIR .. "src/mess/machine/trs80m2kb.c", 
 	MAME_DIR .. "src/mess/drivers/tandy2k.c",
 	MAME_DIR .. "src/mess/machine/tandy2kb.c", 
+}
+
+createMESSProjects(_target, _subtarget, "ultimachine")
+files {
+	MAME_DIR .. "src/mess/drivers/rambo.c",
 }
 
 createMESSProjects(_target, _subtarget, "ultratec")
