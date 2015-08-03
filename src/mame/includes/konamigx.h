a05b184d18717ae8119abf2085b24e3ca2c74fe8
Dirk Best <mail@dirk-best.de>
2015-08-03 16:24:48 +0200

    konamigx: use standard palette device ram for type 1 and 2

--- src/mame/includes/konamigx.h
+++ src/mame/includes/konamigx.h
@@ -96,8 +96,6 @@ public:
 	DECLARE_WRITE16_MEMBER(K053990_martchmp_word_w);
 	DECLARE_WRITE32_MEMBER(fantjour_dma_w);
 	DECLARE_WRITE32_MEMBER(konamigx_type3_psac2_bank_w);
-	DECLARE_WRITE32_MEMBER(konamigx_palette_w);
-	DECLARE_WRITE32_MEMBER(konamigx_palette2_w);
 	DECLARE_WRITE32_MEMBER(konamigx_555_palette_w);
 	DECLARE_WRITE32_MEMBER(konamigx_555_palette2_w);
 	DECLARE_WRITE32_MEMBER(konamigx_tilebank_w);
