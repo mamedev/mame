70572b304eca6edd3821335ed7f15a46319369dd
Dirk Best <mail@dirk-best.de>
2015-08-05 14:38:35 +0200

    macrossp: use standard palette device ram and screen brightness instead of changing the palette

--- src/mame/includes/macrossp.h
+++ src/mame/includes/macrossp.h
@@ -28,10 +28,10 @@ public:
 		m_text_linezoom(*this, "text_linezoom"),
 		m_text_videoregs(*this, "text_videoregs"),
 
-		m_paletteram(*this, "paletteram"),
 		m_mainram(*this, "mainram"),
 		m_maincpu(*this, "maincpu"),
 		m_audiocpu(*this, "audiocpu"),
+		m_screen(*this, "screen"),
 		m_gfxdecode(*this, "gfxdecode"),
 		m_palette(*this, "palette")
 	{
@@ -51,7 +51,6 @@ public:
 	required_shared_ptr<UINT32> m_text_videoram;
 	required_shared_ptr<UINT32> m_text_linezoom;
 	required_shared_ptr<UINT32> m_text_videoregs;
-	required_shared_ptr<UINT32> m_paletteram;
 	required_shared_ptr<UINT32> m_mainram;
 	UINT32 *         m_spriteram_old;
 	UINT32 *         m_spriteram_old2;
@@ -71,14 +70,14 @@ public:
 	/* devices */
 	required_device<cpu_device> m_maincpu;
 	required_device<cpu_device> m_audiocpu;
+	required_device<screen_device> m_screen;
 	required_device<gfxdecode_device> m_gfxdecode;
 	required_device<palette_device> m_palette;
 
-	DECLARE_WRITE32_MEMBER(paletteram32_macrossp_w);
 	DECLARE_READ32_MEMBER(macrossp_soundstatus_r);
 	DECLARE_WRITE32_MEMBER(macrossp_soundcmd_w);
 	DECLARE_READ16_MEMBER(macrossp_soundcmd_r);
-	DECLARE_WRITE32_MEMBER(macrossp_palette_fade_w);
+	DECLARE_WRITE16_MEMBER(palette_fade_w);
 	DECLARE_WRITE32_MEMBER(macrossp_speedup_w);
 	DECLARE_WRITE32_MEMBER(quizmoon_speedup_w);
 	DECLARE_WRITE32_MEMBER(macrossp_scra_videoram_w);
