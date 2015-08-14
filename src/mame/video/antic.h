c9d255b26f295343ed577eda97f485959c9d2e06
Wilbert Pol <wilbertpol@users.noreply.github.com>
2015-08-14 13:50:22 +0200

    antic.c: reduce tagmap lookups (nw)

--- src/mame/video/antic.h
+++ src/mame/video/antic.h
@@ -389,6 +389,9 @@ private:
 
 	const char *m_gtia_tag;
 	gtia_device  *m_gtia;
+	required_device<cpu_device> m_maincpu;
+	optional_ioport m_djoy_b;
+	optional_ioport m_artifacts;
 
 	UINT32 m_tv_artifacts;
 	int m_render1, m_render2, m_render3;
