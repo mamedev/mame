5a36c4380024e42d7c5bd68e09f4a3f347755a28
Vladimir Kryvian <newinferno@gmail.com>
2015-08-26 10:04:41 +0300

    Fixed GetModuleHandleUni

--- src/osd/windows/winutil.c
+++ src/osd/windows/winutil.c
@@ -130,7 +130,6 @@ BOOL win_is_gui_application(void)
 HMODULE WINAPI GetModuleHandleUni()
 {
 	MEMORY_BASIC_INFORMATION mbi;
-	static int dummy;
-	VirtualQuery(&dummy, &mbi, sizeof(mbi));
+	VirtualQuery(GetModuleHandleUni, &mbi, sizeof(mbi));
 	return (HMODULE)mbi.AllocationBase;
 }
