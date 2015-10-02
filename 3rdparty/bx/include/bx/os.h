4206253e65dcecd6b289c45169adae15427470c6
Thomas Klausner <wiz@NetBSD.org>
2015-10-02 09:57:01 +0200

    Add NetBSD support to bx.
    Signed-off-by: Thomas Klausner <wiz@NetBSD.org>

--- 3rdparty/bx/include/bx/os.h
+++ 3rdparty/bx/include/bx/os.h
@@ -15,6 +15,7 @@
 #elif  BX_PLATFORM_ANDROID \
 	|| BX_PLATFORM_EMSCRIPTEN \
 	|| BX_PLATFORM_FREEBSD \
+	|| BX_PLATFORM_NETBSD \
 	|| BX_PLATFORM_IOS \
 	|| BX_PLATFORM_LINUX \
 	|| BX_PLATFORM_NACL \
@@ -24,6 +25,7 @@
 
 #	include <sched.h> // sched_yield
 #	if BX_PLATFORM_FREEBSD \
+	|| BX_PLATFORM_NETBSD \
 	|| BX_PLATFORM_IOS \
 	|| BX_PLATFORM_NACL \
 	|| BX_PLATFORM_OSX \
@@ -103,7 +105,7 @@ namespace bx
 		return (pid_t)::syscall(SYS_gettid);
 #elif BX_PLATFORM_IOS || BX_PLATFORM_OSX
 		return (mach_port_t)::pthread_mach_thread_np(pthread_self() );
-#elif BX_PLATFORM_FREEBSD || BX_PLATFORM_NACL
+#elif BX_PLATFORM_FREEBSD || BX_PLATFORM_NACL || BX_PLATFORM_NETBSD
 		// Casting __nc_basic_thread_data*... need better way to do this.
 		return *(uint32_t*)::pthread_self();
 #else
