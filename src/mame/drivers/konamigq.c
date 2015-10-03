cc10c712d09d65afe32e455f7efb714d2d83142a
Guru <mr-t-guru@users.noreply.github.com>
2015-10-03 21:30:34 +0800

    konamigq.c: documented gun connections and some clocks (nw)

--- src/mame/drivers/konamigq.c
+++ src/mame/drivers/konamigq.c
@@ -13,38 +13,59 @@
   ----------
 
   GQ420  PWB354905B
-  |----------------------------------------------------------|
-  |CN14     420A01.2G  420A02.3M           CN6  CN7   CN8    |
-  |056602    6264                                            |
-  |LA4705    6264         058141  424800                     |
-  |          68000                                           |
-  |  18.432MHz      PAL   056800  424800                     |
-  |  32MHz    48MHz PAL           424800                     |
-  |  RESET_SW             TMS57002                           |
-  |                               M514256                    |
-  |                               M514256                    |
-  |J  000180                                                 |
-  |A                              MACH110                    |
-  |M         53.693175MHz         (000619A)                  |
-  |M         KM4216V256                                      |
-  |A         KM4216V256     CXD8538Q        PAL (000613)     |
-  |          KM4216V256                     PAL (000612)     |
-  | TEST_SW  KM4216V256                     PAL (000618)     |
-  |   CXD2923AR   67.7376MHz                                 |
-  | DIPSW(8)                   93C46       NCR 53CF96-2      |
-  | KM48V514  KM48V514         420B03.27P    HD_LED          |
-  | KM48V514  KM48V514                                       |
-  | CN3                CXD8530BQ      TOSHIBA MK1924FBV      |
-  | KM48V514  KM48V514                (U420UAA04)            |
-  | KM48V514  KM48V514                                       |
-  |----------------------------------------------------------|
+  |----------------------------------------------------------------|
+  |CN14          420A01.2G          420A02.3M         CN6 CN7 CN8  |
+  |           056602  6264    |------| 424800                      |
+  |           LA4705  6264    |058141|                             |
+  |                   68000   |------| 424800                      |
+  |     18.432MHz   PAL(000607)                                    |
+  |     32MHz 48kHz PAL       |------| 424800                      |
+  |                (000608)   |058800|                             |
+  |    RESET_SW               |------| M514256                     |
+  |                         |--------| M514256                     |
+  |J                        |TMS57002|                             |
+  |A                        |--------| MACH110                     |
+  |M  000180          53.693175MHz    (000619A)                    |
+  |M                        |--------|                             |
+  |A  KM4216V256 KM4216V256 |CXD8538Q|           PAL (000613)      |
+  |   KM4216V256 KM4216V256 |        |           PAL (000612)      |
+  | TEST_SW                 |--------|           PAL (000618)      |
+  |   CXD2923AR        67.7376MHz           93C46   NCR53CF96-2    |
+  | DIPSW(8)           |---------|          420B03.27P    HDD_LED  |
+  |  KM48V514 KM48V514 |CXD8530BQ|             |-----------------| |
+  |  KM48V514 KM48V514 |         |             |543MB SCSI HDRIVE| |
+  |CN3  UPA1556        |---------|             |TOSHIBA MK1924FBV| |
+  |  KM48V514 KM48V514                         |(420UAA04)       | |
+  |  KM48V514 KM48V514                         |-----------------| |
+  |----------------------------------------------------------------|
 
   Notes:
-        CN6, CN7, CN8: For connection of guns.
-        CN3 : For connection of extra controls/buttons.
-        CN14: For connection of additional speaker for stereo output.
-        68000 clock: 8.000MHz
-        Vsync: 60Hz
+        CN6/7/8 - 5-pin connector for guns. All 3 connectors are wired the same.
+                  Pinout is...
+                  1- gun opto
+                  2- ground
+                  3- trigger
+                  4- +5v
+                  5- reload (see below)
+                 
+                  The Crypt Killer shotguns have 6 wires coming out of the gun.
+                  The wire colors and connections are....
+                  Black  - Ground; joined to CN6/7/8 pin 2
+                  Red    - +5v; joined to CN6/7/8 pin 4
+                  Yellow - Gun opto; joined to CN6/7/8 pin 1
+                  White  - Trigger; joined to CN6/7/8 pin 3
+                  Purple - Reload; joined to CN6/7/8 pin 5
+                  Grey   - Reload ground; this wire must be joined to any ground for the reload to work
+                 
+        CN3     - For connection of extra controls/buttons (e.g. player 3 start etc)
+        CN14    - For connection of additional speaker for stereo output
+        68000   - Clock input 8.000MHz [32/4]
+        53CF96  - Clock input 16.000MHz [32/2]
+        TMS57002- Clock input 24.000MHz [48/2]
+        056602  - Konami custom ceramic module. Clock inputs 18.432MHz, 1.536MHz[18.432/12], 48kHz
+        000180  - Konami custom ceramic module containing LM1203 and resistors/caps etc
+        Vsync   - 59.825Hz
+        HSync   - 15.613kHz
 */
 
 #include "emu.h"
