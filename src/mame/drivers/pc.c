0ea7d7882e6b16a6d7c4b38e8cd417312a1f8196
rfka01 <rfka01@freenet.de>
2015-10-27 20:08:04 +0100

    Correct number of ISA slots

--- src/mame/drivers/pc.c
+++ src/mame/drivers/pc.c
@@ -285,6 +285,8 @@ static MACHINE_CONFIG_START( siemens, pc_state )
 	MCFG_ISA8_SLOT_ADD("mb:isa", "isa2", pc_isa8_cards, "fdc_xt", false)
 	MCFG_ISA8_SLOT_ADD("mb:isa", "isa3", pc_isa8_cards, "lpt", false)
 	MCFG_ISA8_SLOT_ADD("mb:isa", "isa4", pc_isa8_cards, "com", false)
+	MCFG_ISA8_SLOT_ADD("mb:isa", "isa5", pc_isa8_cards, "hdc", false)
+	MCFG_ISA8_SLOT_ADD("mb:isa", "isa6", pc_isa8_cards, NULL, false)
 
 	/* keyboard */
 	MCFG_PC_KBDC_SLOT_ADD("mb:pc_kbdc", "kbd", pc_xt_keyboards, STR_KBD_IBM_PC_XT_83)
