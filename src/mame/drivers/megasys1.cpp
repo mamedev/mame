4db6a6e37e1f2f67eb06761671c2c55c24581653
angelosa <salese_corp_ltd@email.it>
2015-12-10 01:13:00 +0100

    Fix MT #5423

--- src/mame/drivers/megasys1.cpp
+++ src/mame/drivers/megasys1.cpp
@@ -172,12 +172,15 @@ TIMER_DEVICE_CALLBACK_MEMBER(megasys1_state::megasys1A_scanline)
 {
 	int scanline = param;
 
+	// irq 1 is vblank (confirmed by Saint Dragon "press start" behaviour), 2 & 3 unknown
+	
 	if(scanline == 240) // vblank-out irq
-		m_maincpu->set_input_line(2, HOLD_LINE);
-
-	if(scanline == 0)
 		m_maincpu->set_input_line(1, HOLD_LINE);
 
+	if(scanline == 0)
+		m_maincpu->set_input_line(2, HOLD_LINE);
+
+	// RTE in stdragon
 	if(scanline == 128)
 		m_maincpu->set_input_line(3, HOLD_LINE);
 }
