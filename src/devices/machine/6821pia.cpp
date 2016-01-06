a51cfc710be06cdf9a8ed141c5ac466f2f4eb102
Joakim Larsson Edström <joakimlarsson42@gmail.com>
2016-01-06 01:52:37 +0100

    Stop PIA from spamming debug console by default

--- src/devices/machine/6821pia.cpp
+++ src/devices/machine/6821pia.cpp
@@ -14,7 +14,7 @@
 //  MACROS
 //**************************************************************************
 
-#define VERBOSE 1
+#define VERBOSE 0
 
 #define LOG(x)  do { if (VERBOSE) logerror x; } while (0)
 
