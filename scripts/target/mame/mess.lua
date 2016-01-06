02cc567a19e9ebe5ffefa3ca7188dde35b11a4e0
Miso Kim <meeso.kim@gmail.com>
2016-01-07 03:00:02 +0900

    add spc1500 to mess.lua
    mess.lua update for supporting spc1500

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -2399,6 +2399,7 @@ files {
 createMESSProjects(_target, _subtarget, "samsung")
 files {
 	MAME_DIR .. "src/mame/drivers/spc1000.cpp",
+	MAME_DIR .. "src/mame/drivers/spc1500.cpp",
 }
 
 createMESSProjects(_target, _subtarget, "sanyo")
