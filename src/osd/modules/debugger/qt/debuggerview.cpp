08c8c26b629290d4776002ca14ccc2c1907b58fd
Miodrag Milanovic <mmicko@gmail.com>
2016-01-08 13:42:19 +0100

    remove usage of NO_MEM_TRACKING macro (nw)

--- src/osd/modules/debugger/qt/debuggerview.cpp
+++ src/osd/modules/debugger/qt/debuggerview.cpp
@@ -1,7 +1,5 @@
 // license:BSD-3-Clause
 // copyright-holders:Andrew Gardner
-#define NO_MEM_TRACKING
-
 #include <QtWidgets/QScrollBar>
 #include <QtWidgets/QApplication>
 #include <QtGui/QPainter>
