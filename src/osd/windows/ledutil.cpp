833094f88c468f6e52bcf6ce769dab0460e871e3
Miodrag Milanovic <mmicko@gmail.com>
2016-01-10 18:42:20 +0100

    disable debugging (nw)

--- src/osd/windows/ledutil.cpp
+++ src/osd/windows/ledutil.cpp
@@ -52,7 +52,7 @@ typedef int running_machine;
 
 // note you need to compile as a console app to have any of
 // these printfs show up
-#define DEBUG_VERSION       1
+#define DEBUG_VERSION       0
 
 #if DEBUG_VERSION
 #define DEBUG_PRINTF(x)     printf x
