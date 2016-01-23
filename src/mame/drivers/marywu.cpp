18d4fc2bdf67e5773a90aa9ca477a37cdc0941b0
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-01-23 21:28:43 -0200

    [marywu] hooking up KC8279 (i8279 keyboard/display controller)

--- src/mame/drivers/marywu.cpp
+++ src/mame/drivers/marywu.cpp
@@ -10,7 +10,7 @@
 
   TODO:
   * Figure out where exactly all devices are mapped to (the devices are
-    2 sound chips, the KC8279 controller the 2kb SRAM, the 8bit dipswitches,
+    2 sound chips, the 2kb SRAM, the 8bit dipswitches,
     31 LEDs, 13 modules of double-digit 7-seg displays and 4 push-buttons).
   * we may also have user inputs from the coin slot and from the
     cabinet buttons, for making bets.
@@ -19,6 +19,7 @@
 #include "emu.h"
 #include "cpu/mcs51/mcs51.h"
 #include "sound/ay8910.h"
+#include "machine/i8279.h"
 
 class marywu_state : public driver_device
 {
@@ -26,7 +27,15 @@ public:
     marywu_state(const machine_config &mconfig, device_type type, const char *tag)
         : driver_device(mconfig, type, tag)
     { }
+
+    DECLARE_WRITE8_MEMBER(display_7seg_data_w);
 };
+
+WRITE8_MEMBER( marywu_state::display_7seg_data_w )
+{
+    output_set_digit_value(0, data & 0x0F);
+    output_set_digit_value(1, data & 0xF0);
+}
 
 static ADDRESS_MAP_START( program_map, AS_PROGRAM, 8, marywu_state )
     AM_RANGE(0x0000, 0x7fff) AM_ROM
@@ -34,7 +43,8 @@ ADDRESS_MAP_END
 
 static ADDRESS_MAP_START( io_map, AS_IO, 8, marywu_state )
     AM_RANGE(0x8000, 0x87ff) AM_RAM //HM6116: 2kbytes of Static RAM /* This is a guess. Maybe it is also mirrored? */
-    //AM_RANGE(0xB000, 0xB001) AM_NOP /*maybe this is where the keyboard/display "KC8279" controller is hooked up ? */
+    AM_RANGE(0xB000, 0xB000) AM_DEVREADWRITE("i8279", i8279_device, data_r, data_w)
+    AM_RANGE(0xB001, 0xB001) AM_DEVREADWRITE("i8279", i8279_device, status_r, cmd_w)
     AM_RANGE(0x9001, 0x9002) AM_DEVREADWRITE("ay1", ay8910_device, data_r, address_data_w) /* This is a guess */
     AM_RANGE(0x9003, 0x9004) AM_DEVREADWRITE("ay2", ay8910_device, data_r, address_data_w) /* This is a guess */
 ADDRESS_MAP_END
@@ -44,6 +54,12 @@ static MACHINE_CONFIG_START( marywu , marywu_state )
     MCFG_CPU_ADD("maincpu", I80C31, XTAL_10_738635MHz) //actual CPU is a Winbond w78c31b-24
     MCFG_CPU_PROGRAM_MAP(program_map)
     MCFG_CPU_IO_MAP(io_map)
+
+    /* Keyboard interface */
+    MCFG_DEVICE_ADD("i8279", I8279, XTAL_10_738635MHz) /* should it be perhaps a fraction of the XTAL clock ? */
+//    MCFG_I8279_OUT_SL_CB(WRITE8(marywu_state, marywu_scanlines_w))          // scan SL lines
+//    MCFG_I8279_IN_RL_CB(READ8(marywu_state, marywu_kbd_r))                  // kbd RL lines
+    MCFG_I8279_OUT_DISP_CB(WRITE8(marywu_state, display_7seg_data_w))
 
     /* sound hardware */
     MCFG_SPEAKER_STANDARD_MONO("mono")
