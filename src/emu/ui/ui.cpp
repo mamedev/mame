3b94f23dadf95154662afb6ce50412144b8b457a
angelosa <salese_corp_ltd@email.it>
2016-01-24 01:36:01 +0100

    Print messages for BTANBs, nw

--- src/emu/ui/ui.cpp
+++ src/emu/ui/ui.cpp
@@ -1019,7 +1019,9 @@ std::string &ui_manager::warnings_string(std::string &str)
 						MACHINE_IMPERFECT_SOUND |  \
 						MACHINE_IMPERFECT_GRAPHICS | \
 						MACHINE_IMPERFECT_KEYBOARD | \
-						MACHINE_NO_COCKTAIL)
+						MACHINE_NO_COCKTAIL| \
+						MACHINE_IS_INCOMPLETE| \
+						MACHINE_NO_SOUND_HW )
 
 	str.clear();
 
@@ -1083,6 +1085,20 @@ std::string &ui_manager::warnings_string(std::string &str)
 			str.append(" requires external artwork files\n");
 		}
 
+		if (machine().system().flags & MACHINE_IS_INCOMPLETE )
+		{
+			str.append("This ");
+			str.append(emulator_info::get_gamenoun());
+			str.append(" was never completed. It may exhibit strange behavior or missing elements that are not bugs in the emulation.\n");
+		}
+		
+		if (machine().system().flags & MACHINE_NO_SOUND_HW )
+		{
+			str.append("This ");
+			str.append(emulator_info::get_gamenoun());
+			str.append(" has no sound hardware, MAME will produce no sounds, this is expected behaviour.\n");
+		}
+		
 		// if there's a NOT WORKING, UNEMULATED PROTECTION or GAME MECHANICAL warning, make it stronger
 		if (machine().system().flags & (MACHINE_NOT_WORKING | MACHINE_UNEMULATED_PROTECTION | MACHINE_MECHANICAL))
 		{
