bb032db5db3888d28c513b47e63b4dd1e10f45ca
angelosa <salese_corp_ltd@email.it>
2016-02-01 21:38:02 +0100

    Note, nw

--- src/mame/drivers/overdriv.cpp
+++ src/mame/drivers/overdriv.cpp
@@ -230,9 +230,9 @@ static ADDRESS_MAP_START( overdriv_slave_map, AS_PROGRAM, 16, overdriv_state )
 	AM_RANGE(0x128000, 0x128001) AM_READWRITE(cpuB_ctrl_r, cpuB_ctrl_w) /* enable K053247 ROM reading, plus something else */
 	AM_RANGE(0x130004, 0x130005) AM_WRITE(objdma_w)
 	AM_RANGE(0x130000, 0x130007) AM_DEVREADWRITE8("k053246", k053247_device, k053246_r,k053246_w,0xffff)
-	//AM_RANGE(0x140000, 0x140001) used in later stages
+	//AM_RANGE(0x140000, 0x140001) used in later stages, set after writes at 0x208000-0x20bfff range
 	AM_RANGE(0x200000, 0x203fff) AM_RAM AM_SHARE("share1")
-	AM_RANGE(0x208000, 0x20bfff) AM_RAM
+	AM_RANGE(0x208000, 0x20bfff) AM_RAM // sprite indirect table?
 	AM_RANGE(0x218000, 0x219fff) AM_DEVREAD("k053250_1", k053250_device, rom_r)
 	AM_RANGE(0x220000, 0x221fff) AM_DEVREAD("k053250_2", k053250_device, rom_r)
 ADDRESS_MAP_END
