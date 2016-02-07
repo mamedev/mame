e3d8dbb3645c5976ce0af573f75be6d2f2367d24
Miodrag Milanovic <mmicko@gmail.com>
2016-02-07 19:18:45 +0100

    optimized code a bit, reused Aarons code (nw)

--- src/osd/modules/render/drawbgfx.cpp
+++ src/osd/modules/render/drawbgfx.cpp
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:Miodrag Milanovic, Dario Manesku, Branimir Karadzic
+// copyright-holders:Miodrag Milanovic,Dario Manesku,Branimir Karadzic,Aaron Giles
 //============================================================
 //
 //  drawbgfx.c - BGFX drawer
@@ -586,6 +586,172 @@ uint32_t u32Color(uint8_t _r, uint8_t _g, uint8_t _b, uint8_t _a = 255)
 		;
 }
 
+static UINT32 getABGR(UINT32 ARGB) {
+	return
+		((ARGB >> 24) << 24) |          // Alpha
+		((ARGB >> 16) & 0xFF) |         // Red  -> Blue
+		((ARGB >> 8) & 0xFF) << 8 |     // Green
+		((ARGB)& 0xFF) << 16;           // Blue -> Red
+}
+
+
+//============================================================
+//  copyline_palette16
+//============================================================
+
+static inline void copyline_palette16(UINT32 *dst, const UINT16 *src, int width, const rgb_t *palette)
+{
+	for (int x = 0; x < width; x++)
+		*dst++ = 0xff000000 | getABGR(palette[*src++]);
+}
+
+
+//============================================================
+//  copyline_palettea16
+//============================================================
+
+static inline void copyline_palettea16(UINT32 *dst, const UINT16 *src, int width, const rgb_t *palette)
+{
+	for (int x = 0; x < width; x++)
+		*dst++ = getABGR(palette[*src++]);
+}
+
+
+//============================================================
+//  copyline_rgb32
+//============================================================
+
+static inline void copyline_rgb32(UINT32 *dst, const UINT32 *src, int width, const rgb_t *palette)
+{
+	int x;
+	
+	// palette (really RGB map) case
+	if (palette != NULL)
+	{
+		for (x = 0; x < width; x++)
+		{
+			rgb_t srcpix = *src++;
+			*dst++ = 0xff000000 | palette[0x200 + srcpix.b()] | palette[0x100 + srcpix.g()] | palette[srcpix.r()];
+		}
+	}
+
+	// direct case
+	else
+	{
+		for (x = 0; x < width; x++)
+			*dst++ = getABGR(0xff000000 | *src++);
+	}
+}
+
+
+//============================================================
+//  copyline_argb32
+//============================================================
+
+static inline void copyline_argb32(UINT32 *dst, const UINT32 *src, int width, const rgb_t *palette)
+{
+	int x;
+	// palette (really RGB map) case
+	if (palette != NULL)
+	{
+		for (x = 0; x < width; x++)
+		{
+			rgb_t srcpix = *src++;
+			*dst++ = (srcpix & 0xff000000) | palette[0x200 + srcpix.b()] | palette[0x100 + srcpix.g()] | palette[srcpix.r()];
+		}
+	}
+
+	// direct case
+	else
+	{
+		for (x = 0; x < width; x++)
+			*dst++ = getABGR(*src++);
+	}
+}
+
+static inline UINT32 ycc_to_rgb(UINT8 y, UINT8 cb, UINT8 cr)
+{
+	/* original equations:
+
+	C = Y - 16
+	D = Cb - 128
+	E = Cr - 128
+
+	R = clip(( 298 * C           + 409 * E + 128) >> 8)
+	G = clip(( 298 * C - 100 * D - 208 * E + 128) >> 8)
+	B = clip(( 298 * C + 516 * D           + 128) >> 8)
+
+	R = clip(( 298 * (Y - 16)                    + 409 * (Cr - 128) + 128) >> 8)
+	G = clip(( 298 * (Y - 16) - 100 * (Cb - 128) - 208 * (Cr - 128) + 128) >> 8)
+	B = clip(( 298 * (Y - 16) + 516 * (Cb - 128)                    + 128) >> 8)
+
+	R = clip(( 298 * Y - 298 * 16                        + 409 * Cr - 409 * 128 + 128) >> 8)
+	G = clip(( 298 * Y - 298 * 16 - 100 * Cb + 100 * 128 - 208 * Cr + 208 * 128 + 128) >> 8)
+	B = clip(( 298 * Y - 298 * 16 + 516 * Cb - 516 * 128                        + 128) >> 8)
+
+	R = clip(( 298 * Y - 298 * 16                        + 409 * Cr - 409 * 128 + 128) >> 8)
+	G = clip(( 298 * Y - 298 * 16 - 100 * Cb + 100 * 128 - 208 * Cr + 208 * 128 + 128) >> 8)
+	B = clip(( 298 * Y - 298 * 16 + 516 * Cb - 516 * 128                        + 128) >> 8)
+	*/
+	int r, g, b, common;
+
+	common = 298 * y - 298 * 16;
+	r = (common + 409 * cr - 409 * 128 + 128) >> 8;
+	g = (common - 100 * cb + 100 * 128 - 208 * cr + 208 * 128 + 128) >> 8;
+	b = (common + 516 * cb - 516 * 128 + 128) >> 8;
+
+	if (r < 0) r = 0;
+	else if (r > 255) r = 255;
+	if (g < 0) g = 0;
+	else if (g > 255) g = 255;
+	if (b < 0) b = 0;
+	else if (b > 255) b = 255;
+
+	return rgb_t(0xff, b, g, r);
+}
+
+//============================================================
+//  copyline_yuy16_to_argb
+//============================================================
+
+static inline void copyline_yuy16_to_argb(UINT32 *dst, const UINT16 *src, int width, const rgb_t *palette, int xprescale)
+{
+	int x;
+
+	assert(width % 2 == 0);
+
+	// palette (really RGB map) case
+	if (palette != NULL)
+	{
+		for (x = 0; x < width / 2; x++)
+		{
+			UINT16 srcpix0 = *src++;
+			UINT16 srcpix1 = *src++;
+			UINT8 cb = srcpix0 & 0xff;
+			UINT8 cr = srcpix1 & 0xff;
+			for (int x2 = 0; x2 < xprescale; x2++)
+				*dst++ = ycc_to_rgb(palette[0x000 + (srcpix0 >> 8)], cb, cr);
+			for (int x2 = 0; x2 < xprescale; x2++)
+				*dst++ = ycc_to_rgb(palette[0x000 + (srcpix1 >> 8)], cb, cr);
+		}
+	}
+
+	// direct case
+	else
+	{
+		for (x = 0; x < width; x += 2)
+		{
+			UINT16 srcpix0 = *src++;
+			UINT16 srcpix1 = *src++;
+			UINT8 cb = srcpix0 & 0xff;
+			UINT8 cr = srcpix1 & 0xff;
+			for (int x2 = 0; x2 < xprescale; x2++)
+				*dst++ = ycc_to_rgb(srcpix0 >> 8, cb, cr);
+			for (int x2 = 0; x2 < xprescale; x2++)
+				*dst++ = ycc_to_rgb(srcpix1 >> 8, cb, cr);
+		}
+	}
+}
 int renderer_bgfx::draw(int update)
 {
 	initVertexDecls();
@@ -636,121 +802,138 @@ int renderer_bgfx::draw(int update)
 		uint64_t flags = BGFX_STATE_RGB_WRITE;
 		switch (prim->flags & PRIMFLAG_BLENDMODE_MASK)
 		{
-			case PRIMFLAG_BLENDMODE(BLENDMODE_NONE):
-				break;
-			case PRIMFLAG_BLENDMODE(BLENDMODE_ALPHA):
-				flags |= BGFX_STATE_BLEND_FUNC(BGFX_STATE_BLEND_SRC_ALPHA, BGFX_STATE_BLEND_INV_SRC_ALPHA);
-				break;
-			case PRIMFLAG_BLENDMODE(BLENDMODE_RGB_MULTIPLY):
-				flags |= BGFX_STATE_BLEND_FUNC(BGFX_STATE_BLEND_DST_COLOR, BGFX_STATE_BLEND_ZERO);
-				break;
-			case PRIMFLAG_BLENDMODE(BLENDMODE_ADD):
-				flags |= BGFX_STATE_BLEND_FUNC(BGFX_STATE_BLEND_SRC_ALPHA, BGFX_STATE_BLEND_ONE);
+		case PRIMFLAG_BLENDMODE(BLENDMODE_NONE):
+			break;
+		case PRIMFLAG_BLENDMODE(BLENDMODE_ALPHA):
+			flags |= BGFX_STATE_BLEND_FUNC(BGFX_STATE_BLEND_SRC_ALPHA, BGFX_STATE_BLEND_INV_SRC_ALPHA);
+			break;
+		case PRIMFLAG_BLENDMODE(BLENDMODE_RGB_MULTIPLY):
+			flags |= BGFX_STATE_BLEND_FUNC(BGFX_STATE_BLEND_DST_COLOR, BGFX_STATE_BLEND_ZERO);
+			break;
+		case PRIMFLAG_BLENDMODE(BLENDMODE_ADD):
+			flags |= BGFX_STATE_BLEND_FUNC(BGFX_STATE_BLEND_SRC_ALPHA, BGFX_STATE_BLEND_ONE);
 		}
-
+		bool alpha = false;
 		switch (prim->type)
 		{
 			/**
 			 * Try to stay in one Begin/End block as long as possible,
 			 * since entering and leaving one is most expensive..
 			 */
-			case render_primitive::LINE:
-			
-				drawLine(prim->bounds.x0, prim->bounds.y0, prim->bounds.x1, prim->bounds.y1,
-					1.0f,
-					u32Color(prim->color.r * 255, prim->color.g * 255, prim->color.b * 255, prim->color.a * 255),
-					1.0f);
+		case render_primitive::LINE:
+
+			drawLine(prim->bounds.x0, prim->bounds.y0, prim->bounds.x1, prim->bounds.y1,
+				1.0f,
+				u32Color(prim->color.r * 255, prim->color.g * 255, prim->color.b * 255, prim->color.a * 255),
+				1.0f);
+			bgfx::setState(flags);
+			bgfx::submit(0, m_progLine);
+			break;
+
+		case render_primitive::QUAD:
+			if (prim->texture.base == nullptr) {
+				render_quad_texuv uv;
+				uv.tl.u = uv.tl.v = uv.tr.u = uv.tr.v = 0;
+				uv.bl.u = uv.bl.v = uv.br.u = uv.br.v = 0;
+				screenQuad(prim->bounds.x0, prim->bounds.y0, prim->bounds.x1, prim->bounds.y1,
+					u32Color(prim->color.r * 255, prim->color.g * 255, prim->color.b * 255, prim->color.a * 255), uv);
 				bgfx::setState(flags);
-				bgfx::submit(0, m_progLine);
-				break;
-
-			case render_primitive::QUAD:
-				if (prim->texture.base == nullptr) {
-					render_quad_texuv uv;
-					uv.tl.u = uv.tl.v = uv.tr.u = uv.tr.v = 0;
-					uv.bl.u = uv.bl.v = uv.br.u = uv.br.v = 0;					
-					screenQuad(prim->bounds.x0, prim->bounds.y0,prim->bounds.x1, prim->bounds.y1,
-						u32Color(prim->color.r * 255, prim->color.g * 255, prim->color.b * 255, prim->color.a * 255),uv);
-					bgfx::setState(flags);
-					bgfx::submit(0, m_progQuad);
-				} else {
-					screenQuad(prim->bounds.x0, prim->bounds.y0, prim->bounds.x1, prim->bounds.y1,
-						u32Color(prim->color.r * 255, prim->color.g * 255, prim->color.b * 255, prim->color.a * 255),prim->texcoords);
-					bgfx::TextureHandle m_texture;					
-					// render based on the texture coordinates
-					switch (prim->flags & PRIMFLAG_TEXFORMAT_MASK)
-					{
+				bgfx::submit(0, m_progQuad);
+			}
+			else {
+				screenQuad(prim->bounds.x0, prim->bounds.y0, prim->bounds.x1, prim->bounds.y1,
+					u32Color(prim->color.r * 255, prim->color.g * 255, prim->color.b * 255, prim->color.a * 255), prim->texcoords);
+				bgfx::TextureHandle m_texture;
+				// render based on the texture coordinates
+				switch (prim->flags & PRIMFLAG_TEXFORMAT_MASK)
+				{
 					case PRIMFLAG_TEXFORMAT(TEXFORMAT_PALETTEA16):
+						alpha = true;
 					case PRIMFLAG_TEXFORMAT(TEXFORMAT_PALETTE16):
+					{
+						auto mem = bgfx::alloc(prim->texture.width*prim->texture.height * 4);
+						if (alpha)
 						{
-							auto mem = bgfx::alloc(prim->texture.width*prim->texture.height * 4);
-							
-							int y, x;
-
-							for (y = 0; y < prim->texture.height; y++)
+							for (int y = 0; y < prim->texture.height; y++)
 							{
-								unsigned char *pARGB32 = (unsigned char *)prim->texture.base + y*prim->texture.rowpixels*2;
-								unsigned char *pRGBA8 = (unsigned char *)mem->data + y*prim->texture.width * 4;
-								for (x = 0; x < prim->texture.width*2; x+=2)
-								{
-									pRGBA8[x*2 + 0] = prim->texture.palette[pARGB32[x + 0] + pARGB32[x + 1] * 256].r();
-									pRGBA8[x*2 + 1] = prim->texture.palette[pARGB32[x + 0] + pARGB32[x + 1] * 256].g();
-									pRGBA8[x*2 + 2] = prim->texture.palette[pARGB32[x + 0] + pARGB32[x + 1] * 256].b();
-									pRGBA8[x*2 + 3] = prim->texture.palette[pARGB32[x + 0] + pARGB32[x + 1] * 256].a();
-								}
+								copyline_palettea16((UINT32*)mem->data + y*prim->texture.width, (UINT16*)prim->texture.base + y*prim->texture.rowpixels, prim->texture.width, prim->texture.palette);
 							}
-							m_texture = bgfx::createTexture2D((uint16_t)prim->texture.width
-								, (uint16_t)prim->texture.height
-								, 1
-								, bgfx::TextureFormat::RGBA8
-								, 0
-								, mem
-								);
-						} 
-						break;
-						case PRIMFLAG_TEXFORMAT(TEXFORMAT_YUY16):
-							break;
-						case PRIMFLAG_TEXFORMAT(TEXFORMAT_RGB32):
-						case PRIMFLAG_TEXFORMAT(TEXFORMAT_ARGB32):
-						{
-	
-							auto mem = bgfx::alloc(prim->texture.width*prim->texture.height * 4);
-							int y, x;
-
-							for (y = 0; y < prim->texture.height; y++)
-							{
-								unsigned char *pARGB32 = (unsigned char *)prim->texture.base + y*prim->texture.rowpixels*4;
-								unsigned char *pRGBA8 = (unsigned char *)mem->data + y*prim->texture.width *4;
-								for (x = 0; x < prim->texture.width *4; x+=4 )
-								{
-									pRGBA8[x] =   pARGB32[x+2];
-									pRGBA8[x+1] = pARGB32[x+1];
-									pRGBA8[x+2] = pARGB32[x+0];
-									pRGBA8[x + 3] = pARGB32[x + 3];
-								}
-							}
-							m_texture = bgfx::createTexture2D((uint16_t)prim->texture.width
-								, (uint16_t)prim->texture.height
-								, 1
-								, bgfx::TextureFormat::RGBA8
-								, 0
-								, mem
-								);
 						}
-							break;
+						else
+						{
+							for (int y = 0; y < prim->texture.height; y++)
+							{
+								copyline_palette16((UINT32*)mem->data + y*prim->texture.width, (UINT16*)prim->texture.base + y*prim->texture.rowpixels, prim->texture.width, prim->texture.palette);
+							}
+						}
 
-						default:
-							break;
+						m_texture = bgfx::createTexture2D((uint16_t)prim->texture.width
+							, (uint16_t)prim->texture.height
+							, 1
+							, bgfx::TextureFormat::RGBA8
+							, 0
+							, mem
+							);
 					}
-					bgfx::setTexture(0, m_s_texColor, m_texture);
-					bgfx::setState(flags);
-					bgfx::submit(0, m_progQuadTexture);
-					bgfx::destroyTexture(m_texture);
-				}
-				break;
+					break;
+				case PRIMFLAG_TEXFORMAT(TEXFORMAT_YUY16):
+					{
+						auto mem = bgfx::alloc(prim->texture.width*prim->texture.height * 4);
+						for (int y = 0; y < prim->texture.height; y++)
+						{
+							copyline_yuy16_to_argb((UINT32*)mem->data + y*prim->texture.width, (UINT16*)prim->texture.base + y*prim->texture.rowpixels, prim->texture.width, prim->texture.palette, 1);
+						}
+						m_texture = bgfx::createTexture2D((uint16_t)prim->texture.width
+							, (uint16_t)prim->texture.height
+							, 1
+							, bgfx::TextureFormat::RGBA8
+							, 0
+							, mem
+							);
+					}
+					break;
+				case PRIMFLAG_TEXFORMAT(TEXFORMAT_ARGB32):
+					alpha = true;
+				case PRIMFLAG_TEXFORMAT(TEXFORMAT_RGB32):
+					{
+						auto mem = bgfx::alloc(prim->texture.width*prim->texture.height * 4);
+						if (alpha)
+						{
+							for (int y = 0; y < prim->texture.height; y++)
+							{
+								copyline_argb32((UINT32*)mem->data + y*prim->texture.width, (UINT32*)prim->texture.base + y*prim->texture.rowpixels, prim->texture.width, prim->texture.palette);
+							}
+						}
+						else
+						{
+							for (int y = 0; y < prim->texture.height; y++)
+							{
+								copyline_rgb32((UINT32*)mem->data + y*prim->texture.width, (UINT32*)prim->texture.base + y*prim->texture.rowpixels, prim->texture.width, prim->texture.palette);
+							}
+						}
 
-			default:
-				throw emu_fatalerror("Unexpected render_primitive type");
+						m_texture = bgfx::createTexture2D((uint16_t)prim->texture.width
+							, (uint16_t)prim->texture.height
+							, 1
+							, bgfx::TextureFormat::RGBA8
+							, 0
+							, mem
+							);
+					}
+					break;
+
+				default:
+					break;
+				}
+				bgfx::setTexture(0, m_s_texColor, m_texture);
+				bgfx::setState(flags);
+				bgfx::submit(0, m_progQuadTexture);
+				bgfx::destroyTexture(m_texture);
+			}
+			break;
+
+		default:
+			throw emu_fatalerror("Unexpected render_primitive type");
 		}
 	}
 
