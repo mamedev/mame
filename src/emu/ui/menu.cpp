2fda7e23f2ff096a27b316a2504e6d333c70640e
dankan1890 <mewuidev2@gmail.com>
2016-02-07 03:53:47 +0100

    menu: fixed search path for snapshots. (nw)

--- src/emu/ui/menu.cpp
+++ src/emu/ui/menu.cpp
@@ -1613,9 +1613,13 @@ void ui_menu::get_title_search(std::string &snaptext, std::string &searchstr)
 	snaptext.assign(arts_info[ui_globals::curimage_view].title);
 
 	// get search path
-	path_iterator path(machine().ui().options().value(arts_info[ui_globals::curimage_view].path));
+	if (ui_globals::curimage_view == SNAPSHOT_VIEW)
+		searchstr = machine().options().value(arts_info[ui_globals::curimage_view].path);
+	else
+		searchstr = machine().ui().options().value(arts_info[ui_globals::curimage_view].path);
+
+	path_iterator path(searchstr.c_str());
 	std::string curpath;
-	searchstr.assign(machine().ui().options().value(arts_info[ui_globals::curimage_view].path));
 
 	// iterate over path and add path for zipped formats
 	while (path.next(curpath))
