8e30db1e7441983971abaf8fa8c04a8c5475c6b0
David Haywood <mamehaze@users.noreply.github.com>
2016-02-09 18:04:52 +0000

    making a start on this '96 Flag Rally driver
    dumped thanks to Nosunosu, ShouTime

---
+++ src/mame/drivers/flagrall.cpp
@@ -0,0 +1,225 @@
+// license:BSD-3-Clause
+// copyright-holders:David Haywood
+
+#include "emu.h"
+#include "cpu/m68000/m68000.h"
+#include "sound/okim6295.h"
+
+class flagrall_state : public driver_device
+{
+public:
+	flagrall_state(const machine_config &mconfig, device_type type, const char *tag)
+		: driver_device(mconfig, type, tag),
+	//	m_bg_videoram(*this, "bg_videoram"),
+	//	m_fg_videoram(*this, "fg_videoram"),
+		m_maincpu(*this, "maincpu"),
+		m_gfxdecode(*this, "gfxdecode") { }
+
+	/* memory pointers */
+//	required_shared_ptr<UINT16> m_bg_videoram;
+//	required_shared_ptr<UINT16> m_fg_videoram;
+
+	/* video-related */
+//	tilemap_t    *m_bg_tilemap;
+//	tilemap_t    *m_fg_tilemap;
+//	DECLARE_WRITE16_MEMBER(flagrall_bg_videoram_w);
+//	DECLARE_WRITE16_MEMBER(flagrall_fg_videoram_w);
+//	TILE_GET_INFO_MEMBER(get_flagrall_bg_tile_info);
+//	TILE_GET_INFO_MEMBER(get_flagrall_fg_tile_info);
+	virtual void video_start() override;
+	UINT32 screen_update_flagrall(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	required_device<cpu_device> m_maincpu;
+	required_device<gfxdecode_device> m_gfxdecode;
+};
+
+
+/*
+WRITE16_MEMBER(flagrall_state::flagrall_bg_videoram_w)
+{
+	COMBINE_DATA(&m_bg_videoram[offset]);
+	m_bg_tilemap->mark_tile_dirty(offset);
+}
+
+TILE_GET_INFO_MEMBER(flagrall_state::get_flagrall_bg_tile_info)
+{
+	int tileno = m_bg_videoram[tile_index];
+	SET_TILE_INFO_MEMBER(0, tileno, 0, 0);
+}
+
+
+WRITE16_MEMBER(flagrall_state::flagrall_fg_videoram_w)
+{
+	COMBINE_DATA(&m_fg_videoram[offset]);
+	m_fg_tilemap->mark_tile_dirty(offset);
+}
+
+TILE_GET_INFO_MEMBER(flagrall_state::get_flagrall_fg_tile_info)
+{
+	int tileno = m_fg_videoram[tile_index];
+	SET_TILE_INFO_MEMBER(1, tileno, 1, 0);
+}
+*/
+
+void flagrall_state::video_start()
+{
+//	m_bg_tilemap = &machine().tilemap().create(m_gfxdecode, tilemap_get_info_delegate(FUNC(flagrall_state::get_flagrall_bg_tile_info),this), TILEMAP_SCAN_ROWS, 8, 8, 64, 32);
+//	m_fg_tilemap = &machine().tilemap().create(m_gfxdecode, tilemap_get_info_delegate(FUNC(flagrall_state::get_flagrall_fg_tile_info),this), TILEMAP_SCAN_ROWS, 4, 4, 128, 64);
+//	m_fg_tilemap->set_transparent_pen(0xff);
+}
+
+UINT32 flagrall_state::screen_update_flagrall(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect)
+{
+//	m_bg_tilemap->draw(screen, bitmap, cliprect, 0, 0);
+//	m_fg_tilemap->draw(screen, bitmap, cliprect, 0, 0);
+	return 0;
+}
+
+
+static ADDRESS_MAP_START( flagrall_map, AS_PROGRAM, 16, flagrall_state )
+	AM_RANGE(0x000000, 0x07ffff) AM_ROM
+	AM_RANGE(0x100000, 0x10ffff) AM_RAM // main ram
+
+//	AM_RANGE(0x200000, 0x2003ff) AM_RAM_DEVWRITE("palette", palette_device, write) AM_SHARE("palette") // clears 0x200 - 0x3ff on startup, but writes 00 values to the other half at times?
+	AM_RANGE(0x240000, 0x240fff) AM_RAM // ??
+	AM_RANGE(0x280000, 0x280fff) AM_RAM // ??
+	AM_RANGE(0x2c0000, 0x2c07ff) AM_RAM // ??
+
+	AM_RANGE(0x340000, 0x340001) AM_WRITENOP // ??
+	AM_RANGE(0x380000, 0x380001) AM_WRITENOP // ??
+	AM_RANGE(0x3c0000, 0x3c0001) AM_WRITENOP // ??
+
+	AM_RANGE(0x400000, 0x400001) AM_READ_PORT("DSW")
+	AM_RANGE(0x440000, 0x440001) AM_READ_PORT("DSW")
+	AM_RANGE(0x4c0000, 0x4c0001) AM_READ_PORT("DSW")
+
+	AM_RANGE(0x4c0000, 0x4c0001) AM_WRITENOP // ?? oki?
+ADDRESS_MAP_END
+
+
+static INPUT_PORTS_START( flagrall )
+	PORT_START("DSW")
+	PORT_DIPNAME( 0x0001, 0x0001, "0" )
+	PORT_DIPSETTING(      0x0001, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0002, 0x0002, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0002, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0004, 0x0004, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0004, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0008, 0x0008, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0008, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0010, 0x0010, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0010, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0020, 0x0020, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0020, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0040, 0x0040, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0040, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0080, 0x0080, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0080, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0100, 0x0100, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0100, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0200, 0x0200, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0200, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0400, 0x0400, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0400, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0800, 0x0800, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0800, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x1000, 0x1000, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x1000, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x2000, 0x2000, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x2000, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x4000, 0x4000, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x4000, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+	PORT_DIPNAME( 0x8000, 0x8000, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x8000, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
+INPUT_PORTS_END
+
+static const gfx_layout flagrall_layout =
+{
+	16,16,
+	RGN_FRAC(1,1),
+	8,
+	{ 0,1,2,3,4,5,6,7 },
+	{ 0*8,1*8,2*8,3*8,4*8,5*8,6*8,7*8,8*8,9*8,10*8,11*8,12*8,13*8,14*8,15*8 },
+	{ 0*128, 1*128, 2*128, 3*128, 4*128, 5*128, 6*128, 7*128, 8*128, 9*128, 10*128, 11*128, 12*128, 13*128, 14*128, 15*128 },
+	16*128,
+};
+
+
+
+static GFXDECODE_START( flagrall )
+	GFXDECODE_ENTRY( "gfx1", 0, flagrall_layout,   0x0, 2  ) /* bg tiles */
+	GFXDECODE_ENTRY( "gfx2", 0, flagrall_layout,  0x0, 2  ) /* fg tiles */
+GFXDECODE_END
+
+
+static MACHINE_CONFIG_START( flagrall, flagrall_state )
+
+	MCFG_CPU_ADD("maincpu", M68000, 16000000 ) // ?
+	MCFG_CPU_PROGRAM_MAP(flagrall_map)
+	MCFG_CPU_VBLANK_INT_DRIVER("screen", flagrall_state,  irq4_line_hold)
+
+	MCFG_GFXDECODE_ADD("gfxdecode", "palette", flagrall)
+
+	MCFG_SCREEN_ADD("screen", RASTER)
+	MCFG_SCREEN_REFRESH_RATE(60)
+	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(0))
+	MCFG_SCREEN_SIZE(64*8, 32*8)
+	MCFG_SCREEN_VISIBLE_AREA(8*8, 48*8-1, 2*8, 30*8-1)
+	MCFG_SCREEN_UPDATE_DRIVER(flagrall_state, screen_update_flagrall)
+	MCFG_SCREEN_PALETTE("palette")
+
+	MCFG_PALETTE_ADD("palette", 0x200)
+	MCFG_PALETTE_FORMAT(xRRRRRGGGGGBBBBB)
+
+	MCFG_SPEAKER_STANDARD_STEREO("lspeaker", "rspeaker")
+
+	MCFG_OKIM6295_ADD("oki", 16000000/16, OKIM6295_PIN7_HIGH) // not verified
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "lspeaker", 0.47)
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "rspeaker", 0.47)
+MACHINE_CONFIG_END
+
+
+ROM_START( flagrall )
+	ROM_REGION( 0x80000, "maincpu", 0 ) /* 68000 Code */
+	ROM_LOAD16_BYTE( "11_u34.bin", 0x00001, 0x40000, CRC(24dd439d) SHA1(88857ad5ed69f29de86702dcc746d35b69b3b93d) )
+	ROM_LOAD16_BYTE( "12_u35.bin", 0x00000, 0x40000, CRC(373b71a5) SHA1(be9ab93129e2ffd9bfe296c341dbdf47f1949ac7) )
+
+	ROM_REGION( 0x100000, "oki", 0 ) /* Samples */
+	// only one OKI, both roms have sample tables, presumably banked
+	ROM_LOAD( "13_su4.bin", 0x00000, 0x80000, CRC(7b0630b3) SHA1(c615e6630ffd12c122762751c25c249393bf7abd) )
+	ROM_LOAD( "14_su6.bin", 0x80000, 0x40000, CRC(593b038f) SHA1(b00dcf321fe541ee52c34b79e69c44f3d7a9cd7c) )
+
+	ROM_REGION( 0x300000, "gfx1", 0 )
+	ROM_LOAD32_BYTE( "1_u5.bin",  0x000000, 0x080000, CRC(9377704b) SHA1(ac516a8ba6d1a70086469504c2a46d47a1f4560b) )
+	ROM_LOAD32_BYTE( "5_u6.bin",  0x000001, 0x080000, CRC(1ac0bd0c) SHA1(ab71bb84e61f5c7168601695f332a8d4a30d9948) )
+	ROM_LOAD32_BYTE( "2_u7.bin",  0x000002, 0x080000, CRC(5f6db2b3) SHA1(84caa019d3b75be30a14d19ccc2f28e5e94028bd) )
+	ROM_LOAD32_BYTE( "6_u8.bin",  0x000003, 0x080000, CRC(79e4643c) SHA1(274f2741f39c63e32f49c6a1a72ded1263bdcdaa) )
+		
+	ROM_LOAD32_BYTE( "3_u58.bin", 0x200000, 0x040000, CRC(c913df7d) SHA1(96e89ecb9e5f4d596d71d7ba35af7b2af4670342) )
+	ROM_LOAD32_BYTE( "4_u59.bin", 0x200001, 0x040000, CRC(cb192384) SHA1(329b4c1a4dc388d9f4ce063f9a54cbf3b967682a) )
+	ROM_LOAD32_BYTE( "7_u60.bin", 0x200002, 0x040000, CRC(f187a7bf) SHA1(f4ce9ac9fe376250fe426de6ee404fc7841ef08a) )
+	ROM_LOAD32_BYTE( "8_u61.bin", 0x200003, 0x040000, CRC(b73fa441) SHA1(a5a3533563070c870276ead5e2f9cb9aaba303cc))
+
+	ROM_REGION( 0x100000, "gfx2", 0 )
+	ROM_LOAD( "9_u103.bin",  0x00000, 0x80000, CRC(01e6d654) SHA1(821d61a5b16f5cb76e2a805c8504db1ef38c3a48) )
+	ROM_LOAD( "10_u102.bin", 0x80000, 0x80000, CRC(b1fd3279) SHA1(4a75581e13d43bef441ce81eae518c2f6bc1d5f8) )
+ROM_END
+
+
+GAME( 199?, flagrall, 0,        flagrall, flagrall, driver_device, 0, ROT0,  "<unknown>", "Flag Rally '96", MACHINE_SUPPORTS_SAVE ) // or '96 Flag Rally?
+
