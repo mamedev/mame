782af5c10ebf921b1782c4599e9a62898a66d9e8
Miodrag Milanovic <mmicko@gmail.com>
2016-02-21 12:59:52 +0100

    Fix linux compile (nw)

--- src/osd/modules/render/bgfx/uniform.cpp
+++ src/osd/modules/render/bgfx/uniform.cpp
@@ -7,6 +7,7 @@
 //============================================================
 
 #include "uniform.h"
+#include <string.h>
 
 bgfx_uniform::bgfx_uniform(std::string name, bgfx::UniformType::Enum type)
     : m_name(name)
