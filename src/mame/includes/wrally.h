ff26df5e0f122199faa268ee7396c04ebb8e0c67
Ivan Vangelista <mesgnet@yahoo.it>
2016-02-27 11:25:30 +0100

    wrally.cpp: oki memory map instead of memcpy (nw)

--- src/mame/includes/wrally.h
+++ src/mame/includes/wrally.h
@@ -29,12 +29,13 @@ public:
 	DECLARE_WRITE16_MEMBER(vram_w);
 	DECLARE_WRITE16_MEMBER(flipscreen_w);
 	DECLARE_WRITE16_MEMBER(okim6295_bankswitch_w);
-	DECLARE_WRITE16_MEMBER(wrally_coin_counter_w);
-	DECLARE_WRITE16_MEMBER(wrally_coin_lockout_w);
+	DECLARE_WRITE16_MEMBER(coin_counter_w);
+	DECLARE_WRITE16_MEMBER(coin_lockout_w);
 
 	TILE_GET_INFO_MEMBER(get_tile_info_screen0);
 	TILE_GET_INFO_MEMBER(get_tile_info_screen1);
 
+	virtual void machine_start() override;
 	virtual void video_start() override;
 
 	UINT32 screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
