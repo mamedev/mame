7acc22e9019fbe12da7fac2341f737f6be3fe456
RobertoFresca <robbie@mamedev.org>
2016-02-29 02:16:35 -0300

    New clones added or promoted from NOT_WORKING status ---------------------------------------------------- Jingle Bell (EU, V141) [Roberto Fresca, Ioannis Bampoulas]
    New clones marked as NOT_WORKING
    --------------------------------
    Jingle Bell (US, V157) [Roberto Fresca, Ioannis Bampoulas]
    Jingle Bell (EU, V155) [Roberto Fresca, Ioannis Bampoulas]
    Jingle Bell (EU, V153) [Roberto Fresca, Ioannis Bampoulas]

--- src/mame/drivers/igs009.cpp
+++ src/mame/drivers/igs009.cpp
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:Luca Elia, David Haywood
+// copyright-holders:Luca Elia, David Haywood, Roberto Fresca
 /***************************************************************************
 
                           -= IGS009 Based Games =-
@@ -15,12 +15,16 @@ NVRAM   :   Battery for main RAM
   The interesting part is the background tilemap, that is designed specifically
   for simulating the nine reels of a slot machine.
 
-----------------------------------------------------------------------
-Year  Game                Manufacturer    Notes
-----------------------------------------------------------------------
-1995? Jingle Bell         IGS
-1998  Grand Prix '98      Romtec          1 reel gfx rom is bad
-----------------------------------------------------------------------
+---------------------------------------------------------------------------
+Year  Game                         Manufacturer    Notes
+---------------------------------------------------------------------------
+1997  Jingle Bell (US, V157)       IGS             NMI issues...
+1997  Jingle Bell (EU, V155)       IGS             NMI issues...
+1997  Jingle Bell (EU, V153)       IGS             NMI issues...
+1995  Jingle Bell (EU, V141)       IGS             Working
+1995? Jingle Bell (Italy, V133I)   IGS             Working
+1998  Grand Prix '98               Romtec          1 reel gfx rom is bad
+---------------------------------------------------------------------------
 
 ***************************************************************************/
 
@@ -919,6 +923,106 @@ ROM_START( jingbell )
 	ROM_LOAD( "palce22v10h-ch-jin-u27.u27", 0x000, 0x2dd, BAD_DUMP CRC(5c4e9024) SHA1(e9d1e4df3d79c21f4ce053a84bb7b7a43d650f91) )
 ROM_END
 
+/* Jingle Bells (V157 US)
+   Original IGS.
+   For amusement.
+*/
+ROM_START( jingbella )
+	ROM_REGION( 0x10000, "maincpu", 0 )
+	ROM_LOAD( "27c512_v157_us.u44", 0x00000, 0x10000, CRC(37f95b60) SHA1(db2022802ce553ce7a0e8403408f3194b2f10007) )
+
+	ROM_REGION( 0x8000, "data", 0 )
+	ROM_LOAD( "27c256_v157_us.u43", 0x0000, 0x8000, CRC(a7affa15) SHA1(f9d33e32b57ad267d383e075663994e0af0b3016) )
+
+	ROM_REGION( 0x30000, "reels", 0 )
+	ROM_LOAD( "27c512_v157_us.u17", 0x00000, 0x10000, CRC(cadd7910) SHA1(aa514ddb29c8c9a77478d56bea4ae71995fdd518) )
+	ROM_LOAD( "27c512_v157_us.u16", 0x10000, 0x10000, CRC(a9e1f5aa) SHA1(68d7f4e9e9a5bbce0904e406ee6fe82e9e52a9ba) )
+	ROM_LOAD( "27c512_v157_us.u15", 0x20000, 0x10000, CRC(865b7d3a) SHA1(c1dff3a27d747ee499aaee0c4468534f0249a3e5) )
+
+	ROM_REGION( 0xc0000, "tiles", 0 )
+	ROM_LOAD( "27c2001_v157_us.u25", 0x00000, 0x40000, CRC(daa56ce5) SHA1(4f14a8efac16b03bd14dd26d586bcb8d5bef65c1) )
+	ROM_LOAD( "27c2001_v157_us.u24", 0x40000, 0x40000, CRC(b10b38e1) SHA1(397b2d899e47c6249fbbb6e6262d0390d9b796e6) )
+	ROM_LOAD( "27c2001_v157_us.u23", 0x80000, 0x40000, CRC(a3304b5a) SHA1(bf51cb1f728758d50ce27275aa19ef649f6b34b9) )
+
+	ROM_REGION( 0x40000, "oki", 0 )
+	ROM_LOAD( "mx29f001t_v157_us_sp.u38", 0x00000, 0x20000, CRC(a42d73b1) SHA1(93157e9630d5c8bb34c71186415d0aa8c5d51951) )
+ROM_END
+
+/* Jingle Bells (V155 EU)
+   Original IGS.
+   For amusement.
+*/
+ROM_START( jingbellb )
+	ROM_REGION( 0x10000, "maincpu", 0 )
+	ROM_LOAD( "27c512_v155.u44", 0x00000, 0x10000, CRC(0813d12b) SHA1(4c35b656d1e54619bbfd26cbc017eba336e6abda) )
+
+	ROM_REGION( 0x8000, "data", 0 )
+	ROM_LOAD( "27c256_v155.u43", 0x0000, 0x8000, CRC(a7affa15) SHA1(f9d33e32b57ad267d383e075663994e0af0b3016) )
+
+	ROM_REGION( 0x30000, "reels", 0 )
+	ROM_LOAD( "27c512_v155.u17", 0x00000, 0x10000, CRC(cadd7910) SHA1(aa514ddb29c8c9a77478d56bea4ae71995fdd518) )
+	ROM_LOAD( "27c512_v155.u16", 0x10000, 0x10000, CRC(a9e1f5aa) SHA1(68d7f4e9e9a5bbce0904e406ee6fe82e9e52a9ba) )
+	ROM_LOAD( "27c512_v155.u15", 0x20000, 0x10000, CRC(865b7d3a) SHA1(c1dff3a27d747ee499aaee0c4468534f0249a3e5) )
+
+	ROM_REGION( 0xc0000, "tiles", 0 )
+	ROM_LOAD( "27c2001_v155.u25", 0x00000, 0x40000, CRC(daa56ce5) SHA1(4f14a8efac16b03bd14dd26d586bcb8d5bef65c1) )
+	ROM_LOAD( "27c2001_v155.u24", 0x40000, 0x40000, CRC(b10b38e1) SHA1(397b2d899e47c6249fbbb6e6262d0390d9b796e6) )
+	ROM_LOAD( "27c2001_v155.u23", 0x80000, 0x40000, CRC(a3304b5a) SHA1(bf51cb1f728758d50ce27275aa19ef649f6b34b9) )
+
+	ROM_REGION( 0x40000, "oki", 0 )
+	ROM_LOAD( "27c1001_v155_sp.u38", 0x00000, 0x20000, CRC(a42d73b1) SHA1(93157e9630d5c8bb34c71186415d0aa8c5d51951) )
+ROM_END
+
+/* Jingle Bells (V153 EU)
+   Original IGS.
+   For amusement.
+*/
+ROM_START( jingbellc )
+	ROM_REGION( 0x10000, "maincpu", 0 )
+	ROM_LOAD( "27c512_v153.u44", 0x00000, 0x10000, CRC(fd3bc092) SHA1(a92dc9bc15d7a4816887d66d46cea925d230c5b8) )
+
+	ROM_REGION( 0x8000, "data", 0 )
+	ROM_LOAD( "27c256_v153.u43", 0x0000, 0x8000, CRC(a7affa15) SHA1(f9d33e32b57ad267d383e075663994e0af0b3016) )
+
+	ROM_REGION( 0x30000, "reels", 0 )
+	ROM_LOAD( "27c512_v153.u17", 0x00000, 0x10000, CRC(cadd7910) SHA1(aa514ddb29c8c9a77478d56bea4ae71995fdd518) )
+	ROM_LOAD( "27c512_v153.u16", 0x10000, 0x10000, CRC(a9e1f5aa) SHA1(68d7f4e9e9a5bbce0904e406ee6fe82e9e52a9ba) )
+	ROM_LOAD( "27c512_v153.u15", 0x20000, 0x10000, CRC(865b7d3a) SHA1(c1dff3a27d747ee499aaee0c4468534f0249a3e5) )
+
+	ROM_REGION( 0xc0000, "tiles", 0 )
+	ROM_LOAD( "27c2001_v153.u25", 0x00000, 0x40000, CRC(daa56ce5) SHA1(4f14a8efac16b03bd14dd26d586bcb8d5bef65c1) )
+	ROM_LOAD( "27c2001_v153.u24", 0x40000, 0x40000, CRC(b10b38e1) SHA1(397b2d899e47c6249fbbb6e6262d0390d9b796e6) )
+	ROM_LOAD( "27c2001_v153.u23", 0x80000, 0x40000, CRC(a3304b5a) SHA1(bf51cb1f728758d50ce27275aa19ef649f6b34b9) )
+
+	ROM_REGION( 0x40000, "oki", 0 )
+	ROM_LOAD( "27c1001_v153_sp.u38", 0x00000, 0x20000, CRC(a42d73b1) SHA1(93157e9630d5c8bb34c71186415d0aa8c5d51951) )
+ROM_END
+
+/* Jingle Bells (V141 EU)
+   Original IGS.
+   For amusement.
+*/
+ROM_START( jingbelld )
+	ROM_REGION( 0x10000, "maincpu", 0 )
+	ROM_LOAD( "27c512_v141.u44", 0x00000, 0x10000, CRC(6997a903) SHA1(991508d85e55a3c6f811070352e06ae6bf65cc2e) )
+
+	ROM_REGION( 0x8000, "data", 0 )
+	ROM_LOAD( "27c256_v141.u43", 0x0000, 0x8000, CRC(a7affa15) SHA1(f9d33e32b57ad267d383e075663994e0af0b3016) )
+
+	ROM_REGION( 0x30000, "reels", 0 )
+	ROM_LOAD( "27c512_v141.u17", 0x00000, 0x10000, CRC(cadd7910) SHA1(aa514ddb29c8c9a77478d56bea4ae71995fdd518) )
+	ROM_LOAD( "27c512_v141.u16", 0x10000, 0x10000, CRC(a9e1f5aa) SHA1(68d7f4e9e9a5bbce0904e406ee6fe82e9e52a9ba) )
+	ROM_LOAD( "27c512_v141.u15", 0x20000, 0x10000, CRC(865b7d3a) SHA1(c1dff3a27d747ee499aaee0c4468534f0249a3e5) )
+
+	ROM_REGION( 0xc0000, "tiles", 0 )
+	ROM_LOAD( "27c2001_v141.u25", 0x00000, 0x40000, CRC(f53bac7e) SHA1(f4375da0780fba59fcb65e24a33099af35e4d286) )
+	ROM_LOAD( "27c2001_v141.u24", 0x40000, 0x40000, CRC(bddd6001) SHA1(2a6395e9593352d3ea2d477a7f41805f389c9c50) )
+	ROM_LOAD( "27c2001_v141.u23", 0x80000, 0x40000, CRC(e8322c75) SHA1(c3385538fb673a4ab14c315ce8bce792eb264ec7) )
+
+	ROM_REGION( 0x40000, "oki", 0 )
+	ROM_LOAD( "27c1001_v141_sp.u38", 0x00000, 0x20000, CRC(a42d73b1) SHA1(93157e9630d5c8bb34c71186415d0aa8c5d51951) )
+ROM_END
+
 
 DRIVER_INIT_MEMBER(igs009_state,jingbell)
 {
@@ -1011,4 +1115,8 @@ ROM_END
 
 /*    YEAR   NAME       PARENT    MACHINE   INPUT     STATE          INIT      ROT    COMPANY           FULLNAME                     FLAGS  */
 GAME( 1995?, jingbell,  0,        jingbell, jingbell, igs009_state,  jingbell, ROT0, "IGS",            "Jingle Bell (Italy, V133I)", MACHINE_SUPPORTS_SAVE )
+GAME( 1997,  jingbella, jingbell, jingbell, jingbell, igs009_state,  jingbell, ROT0, "IGS",            "Jingle Bell (US, V157)",     MACHINE_NOT_WORKING | MACHINE_SUPPORTS_SAVE )
+GAME( 1997,  jingbellb, jingbell, jingbell, jingbell, igs009_state,  jingbell, ROT0, "IGS",            "Jingle Bell (EU, V155)",     MACHINE_NOT_WORKING | MACHINE_SUPPORTS_SAVE )
+GAME( 1997,  jingbellc, jingbell, jingbell, jingbell, igs009_state,  jingbell, ROT0, "IGS",            "Jingle Bell (EU, V153)",     MACHINE_NOT_WORKING | MACHINE_SUPPORTS_SAVE )
+GAME( 1995,  jingbelld, jingbell, jingbell, jingbell, igs009_state,  jingbell, ROT0, "IGS",            "Jingle Bell (EU, V141)",     MACHINE_SUPPORTS_SAVE )
 GAME( 1998,  gp98,      0,        gp98,     jingbell, driver_device, 0,        ROT0, "Romtec Co. Ltd", "Grand Prix '98 (V100K)",     MACHINE_NOT_WORKING | MACHINE_IMPERFECT_GRAPHICS | MACHINE_SUPPORTS_SAVE )
