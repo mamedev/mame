3b3c516a3d8a507dac26ffbc348c1c650da94c4b
David Haywood <mamehaze@users.noreply.github.com>
2016-03-01 19:35:32 +0000

    Black Touch '96 hardware is a Korean clone of 'SNK68' hardware so start to refactor, and share code

---
+++ src/mame/video/snk68_spr.h
@@ -0,0 +1,45 @@
+typedef device_delegate<void (int&, int&, int&, int&)> snk68_tile_indirection_delegate;
+
+#define MCFG_SNK68_SPR_GFXDECODE(_gfxtag) \
+	snk68_spr_device::static_set_gfxdecode_tag(*device, "^" _gfxtag);
+#define MCFG_SNK68_SPR_PALETTE(_palette_tag) \
+	snk68_spr_device::static_set_palette_tag(*device, "^" _palette_tag);
+#define MCFG_SNK68_SPR_SET_TILE_INDIRECT( _class, _method) \
+	snk68_spr_device::set_tile_indirect_cb(*device, snk68_tile_indirection_delegate(&_class::_method, #_class "::" #_method, NULL, (_class *)0));
+
+class snk68_spr_device : public device_t
+{
+public:
+	snk68_spr_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+
+	// static configuration
+	static void static_set_gfxdecode_tag(device_t &device, const char *tag);
+	static void static_set_palette_tag(device_t &device, const char *tag);
+	static void set_tile_indirect_cb(device_t &device,snk68_tile_indirection_delegate newtilecb);
+
+	DECLARE_READ16_MEMBER(spriteram_r);
+	DECLARE_WRITE16_MEMBER(spriteram_w);
+	void draw_sprites(bitmap_ind16 &bitmap, const rectangle &cliprect, int group);
+	void draw_sprites_all(bitmap_ind16 &bitmap, const rectangle &cliprect);
+
+	snk68_tile_indirection_delegate m_newtilecb;
+	
+	void tile_callback_noindirect(int& tile, int& fx, int& ft, int& region);
+	void set_flip(int flip);
+
+protected:
+	virtual void device_start() override;
+	virtual void device_reset() override;
+
+private:
+	optional_device<gfxdecode_device> m_gfxdecode;
+	optional_device<palette_device> m_palette;
+	required_shared_ptr<UINT16> m_spriteram;
+	required_device<screen_device> m_screen;
+	int m_flipscreen;
+};
+
+
+extern const device_type SNK68_SPR;
+
+
