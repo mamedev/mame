dbdf21ee465acadf3e643d62d6bedfcc297ada85
ImJezze <jezze@gmx.net>
2016-03-02 19:26:09 +0100

    Fixed infinite loop
    - fixed infinite loop while building slider list, when more than one
    screen was created (reverted from commit 546f7a24876b91ccd880b518c718beee78aede82)

--- src/osd/windows/window.cpp
+++ src/osd/windows/window.cpp
@@ -274,11 +274,35 @@ void windows_osd_interface::update_slider_list()
 {
 	for (win_window_info *window = win_window_list; window != nullptr; window = window->m_next)
 	{
-		// every created window uses the same renderer
 		if (window->m_renderer && window->m_renderer->sliders_dirty())
 		{
-			m_sliders = window->m_renderer->get_slider_list();
+			build_slider_list();
 			return;
+		}
+	}
+}
+
+void windows_osd_interface::build_slider_list()
+{
+	m_sliders = nullptr;
+	slider_state *curr = m_sliders;
+	for (win_window_info *info = win_window_list; info != nullptr; info = info->m_next)
+	{
+		slider_state *window_sliders = info->m_renderer->get_slider_list();
+		if (window_sliders != nullptr)
+		{
+			if (m_sliders == nullptr)
+			{
+				m_sliders = curr = window_sliders;
+			}
+			else
+			{
+				while (curr->next != nullptr)
+				{
+					curr = curr->next;
+				}
+				curr->next = window_sliders;
+			}
 		}
 	}
 }
