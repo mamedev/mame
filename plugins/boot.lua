2d8aa85227823467e19f280ff9dc5ac63b8a0e1b
Miodrag Milanovic <mmicko@gmail.com>
2016-03-05 07:56:50 +0100

    fix lua script on boot (nw)

--- plugins/boot.lua
+++ plugins/boot.lua
@@ -1,6 +1,5 @@
 require('lfs')
-local uv = require('luv')
-local cwd = uv.cwd()
+local cwd = lfs.currentdir()
 package.path = cwd .. "/plugins/?.lua;" .. cwd .. "/plugins/?/init.lua"
 
 local json = require('json')
