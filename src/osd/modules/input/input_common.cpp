2d3c0abaefce5456e9bb7600157eba3e0589ebc2
Miodrag Milanovic <mmicko@gmail.com>
2016-03-08 16:13:07 +0100

    compile fix (nw)

--- src/osd/modules/input/input_common.cpp
+++ src/osd/modules/input/input_common.cpp
@@ -33,7 +33,7 @@
 #include <windows.h>
 #define KEY_TRANS_ENTRY0(mame, sdlsc, sdlkey, disc, virtual, ascii, UI) { ITEM_ID_##mame, KEY_ ## disc, virtual, ascii, "ITEM_ID_"#mame, (char *) UI }
 #define KEY_TRANS_ENTRY1(mame, sdlsc, sdlkey, disc, virtual, ascii)     { ITEM_ID_##mame, KEY_ ## disc, virtual, ascii, "ITEM_ID_"#mame, (char*) #mame }
-#elseif defined(OSD_SDL)
+#elif defined(OSD_SDL)
 // SDL include
 #include <sdlinc.h>
 #define KEY_TRANS_ENTRY0(mame, sdlsc, sdlkey, disc, virtual, ascii, UI) { ITEM_ID_##mame, SDL_SCANCODE_ ## sdlsc, SDLK_ ## sdlkey, ascii, "ITEM_ID_"#mame, (char *) UI }
