f464df3073abe73fef5ac560fab62bebc4ac2df9
therealmogminer@gmail.com <therealmogminer@gmail.com>
2016-03-18 17:20:55 +0100

    Setting a sampler counts as setting a uniform in bgfx, nw

--- src/osd/modules/render/bgfx/uniform.cpp
+++ src/osd/modules/render/bgfx/uniform.cpp
@@ -29,7 +29,10 @@ bgfx_uniform::~bgfx_uniform()
 
 void bgfx_uniform::upload()
 {
-	bgfx::setUniform(m_handle, m_data);
+	if (m_type != bgfx::UniformType::Int1)
+	{
+		bgfx::setUniform(m_handle, m_data);
+	}
 }
 
 bgfx_uniform* bgfx_uniform::set(float* value)
