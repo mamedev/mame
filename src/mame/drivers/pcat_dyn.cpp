8fc8fc5ca3a402ce4ee0292c2d8695d9ffcf1013
cracyc <cracyc@users.noreply.github.com>
2016-03-19 21:13:00 -0500

    pcat_dyn: add sound and mark toursol working [Carl]

--- src/mame/drivers/pcat_dyn.cpp
+++ src/mame/drivers/pcat_dyn.cpp
@@ -9,7 +9,7 @@ Unmodified 486 PC-AT HW.
 Jet Way Information Co. OP495SLC motherboard
  - AMD Am486-DX40 CPU
  - Trident TVGA9000i video card
- - Breve Technologies audio adapter
+ - Breve Technologies audio adapter, WSS+Sound Blaster
  - CH Products RollerMouse serial trackball, Mouse Systems compatible
 
 preliminary driver by Angelo Salese
@@ -28,23 +28,30 @@ If the output isn't satisfactory, it prints "I/O BOARD FAILURE".
 #include "machine/ds128x.h"
 #include "machine/ins8250.h"
 #include "machine/nvram.h"
+#include "sound/ad1848.h"
 #include "bus/rs232/rs232.h"
 #include "bus/rs232/ser_mouse.h"
+#include "bus/isa/isa.h"
+#include "bus/isa/sblaster.h"
 
 class pcat_dyn_state : public pcat_base_state
 {
 public:
 	pcat_dyn_state(const machine_config &mconfig, device_type type, const char *tag)
 		: pcat_base_state(mconfig, type, tag)
+		, m_isabus(*this, "isa")
 		, m_prgbank(*this, "prgbank")
 		, m_nvram_bank(*this, "nvram_bank")
 		, m_nvram_mem(0x2000){ }
 
+	required_device<isa8_device> m_isabus;
 	required_memory_bank m_prgbank;
 	required_memory_bank m_nvram_bank;
 	std::vector<UINT8> m_nvram_mem;
 	DECLARE_WRITE8_MEMBER(bank1_w);
 	DECLARE_WRITE8_MEMBER(bank2_w);
+	DECLARE_READ8_MEMBER(audio_r);
+	DECLARE_WRITE8_MEMBER(dma8237_1_dack_w);
 	virtual void machine_start() override;
 	void nvram_init(nvram_device &nvram, void *base, size_t size);
 };
@@ -59,6 +66,16 @@ void pcat_dyn_state::machine_start()
 void pcat_dyn_state::nvram_init(nvram_device &nvram, void *base, size_t size)
 {
 	memcpy(base, memregion("nvram")->base(), size);
+}
+
+READ8_MEMBER(pcat_dyn_state::audio_r)
+{
+	switch(offset)
+	{
+		case 3:
+			return 4;
+	}
+	return 0;
 }
 
 WRITE8_MEMBER(pcat_dyn_state::bank1_w)
@@ -91,9 +108,12 @@ static ADDRESS_MAP_START( pcat_io, AS_IO, 32, pcat_dyn_state )
 	AM_RANGE(0x03c0, 0x03cf) AM_DEVREADWRITE8("vga", trident_vga_device, port_03c0_r, port_03c0_w, 0xffffffff)
 	AM_RANGE(0x03d0, 0x03df) AM_DEVREADWRITE8("vga", trident_vga_device, port_03d0_r, port_03d0_w, 0xffffffff)
 	AM_RANGE(0x03f8, 0x03ff) AM_DEVREADWRITE8("ns16550", ns16550_device, ins8250_r, ins8250_w, 0xffffffff)
+	AM_RANGE(0x0530, 0x0533) AM_READ8(audio_r, 0xffffffff)
+	AM_RANGE(0x0534, 0x0537) AM_DEVREADWRITE8("ad1848", ad1848_device, read, write, 0xffffffff)
 ADDRESS_MAP_END
 
-
+//TODO: use atmb device
+WRITE8_MEMBER( pcat_dyn_state::dma8237_1_dack_w ){ m_isabus->dack_w(1, data); }
 
 static INPUT_PORTS_START( pcat_dyn )
 	// M,N,Numpad 6 -- Hang
@@ -113,6 +133,19 @@ INPUT_PORTS_END
 static SLOT_INTERFACE_START(pcat_dyn_com)
 	SLOT_INTERFACE("msmouse", MSYSTEM_SERIAL_MOUSE)
 SLOT_INTERFACE_END
+
+static SLOT_INTERFACE_START( pcat_dyn_isa8_cards )
+	SLOT_INTERFACE("sb15",  ISA8_SOUND_BLASTER_1_5)
+SLOT_INTERFACE_END
+
+static DEVICE_INPUT_DEFAULTS_START( pcat_dyn_sb_def )
+	DEVICE_INPUT_DEFAULTS("CONFIG", 0x03, 0x01)
+DEVICE_INPUT_DEFAULTS_END
+
+static MACHINE_CONFIG_FRAGMENT( pcat_dyn_sb_conf )
+	MCFG_DEVICE_MODIFY("pc_joy")
+	MCFG_DEVICE_SLOT_INTERFACE(pc_joysticks, nullptr, true) // remove joystick
+MACHINE_CONFIG_END
 
 static MACHINE_CONFIG_START( pcat_dyn, pcat_dyn_state )
 	/* basic machine hardware */
@@ -134,6 +167,14 @@ static MACHINE_CONFIG_START( pcat_dyn, pcat_dyn_state )
 	MCFG_MC146818_IRQ_HANDLER(DEVWRITELINE("pic8259_2", pic8259_device, ir0_w))
 	MCFG_MC146818_CENTURY_INDEX(0x32)
 
+	MCFG_SOUND_ADD("ad1848", AD1848, 0)
+	MCFG_AD1848_IRQ_CALLBACK(DEVWRITELINE("pic8259_1", pic8259_device, ir5_w))
+	MCFG_AD1848_DRQ_CALLBACK(DEVWRITELINE("dma8237_1", am9517a_device, dreq0_w))
+
+	MCFG_DEVICE_MODIFY("dma8237_1")
+	MCFG_I8237_OUT_IOW_0_CB(DEVWRITE8("ad1848", ad1848_device, dack_w))
+	MCFG_I8237_OUT_IOW_1_CB(WRITE8(pcat_dyn_state, dma8237_1_dack_w))
+
 	MCFG_NVRAM_ADD_CUSTOM_DRIVER("nvram", pcat_dyn_state, nvram_init)
 
 	MCFG_DEVICE_ADD( "ns16550", NS16550, XTAL_1_8432MHz )
@@ -148,6 +189,22 @@ static MACHINE_CONFIG_START( pcat_dyn, pcat_dyn_state )
 	MCFG_RS232_DSR_HANDLER(DEVWRITELINE("ns16550", ins8250_uart_device, dsr_w))
 	MCFG_RS232_RI_HANDLER(DEVWRITELINE("ns16550", ins8250_uart_device, ri_w))
 	MCFG_RS232_CTS_HANDLER(DEVWRITELINE("ns16550", ins8250_uart_device, cts_w))
+
+	MCFG_DEVICE_ADD("isa", ISA8, 0)
+	MCFG_ISA8_CPU(":maincpu")
+	MCFG_ISA_OUT_IRQ2_CB(DEVWRITELINE("pic8259_2", pic8259_device, ir2_w))
+	MCFG_ISA_OUT_IRQ3_CB(DEVWRITELINE("pic8259_1", pic8259_device, ir3_w))
+	//MCFG_ISA_OUT_IRQ4_CB(DEVWRITELINE("pic8259_1", pic8259_device, ir4_w))
+	//MCFG_ISA_OUT_IRQ5_CB(DEVWRITELINE("pic8259_1", pic8259_device, ir5_w))
+	MCFG_ISA_OUT_IRQ6_CB(DEVWRITELINE("pic8259_1", pic8259_device, ir6_w))
+	MCFG_ISA_OUT_IRQ7_CB(DEVWRITELINE("pic8259_1", pic8259_device, ir7_w))
+	MCFG_ISA_OUT_DRQ1_CB(DEVWRITELINE("dma8237_1", am9517a_device, dreq1_w))
+	MCFG_ISA_OUT_DRQ2_CB(DEVWRITELINE("dma8237_1", am9517a_device, dreq2_w))
+	MCFG_ISA_OUT_DRQ3_CB(DEVWRITELINE("dma8237_1", am9517a_device, dreq3_w))
+
+	MCFG_ISA8_SLOT_ADD("isa", "isa1", pcat_dyn_isa8_cards, "sb15", true)
+	MCFG_DEVICE_CARD_DEVICE_INPUT_DEFAULTS("sb15", pcat_dyn_sb_def)
+	MCFG_DEVICE_CARD_MACHINE_CONFIG("sb15", pcat_dyn_sb_conf)
 MACHINE_CONFIG_END
 
 /***************************************
@@ -170,6 +227,7 @@ ROM_START(toursol)
 	ROM_LOAD("sol.u23", 0x80000, 0x40000, CRC(d1e39bd4) SHA1(39c7ee43cddb53fba0f7c0572ddc40289c4edd07))
 	ROM_LOAD("sol.u24", 0xc0000, 0x40000, CRC(555341e0) SHA1(81fee576728855e234ff7aae06f54ae9705c3ab5))
 	ROM_FILL(0x2a3e6, 1, 0xeb) // skip prot(?) check
+	ROM_FILL(0x51bd2, 2, 0x90) // opl2 probe expects timer expiration too quickly
 
 	ROM_REGION(0x2000, "nvram", 0)
 	ROM_LOAD("sol.u28", 0, 0x2000, CRC(c9374d50) SHA1(49173bc69f70bb2a7e8af9d03e2538b34aa881d8))
@@ -201,5 +259,5 @@ ROM_START(toursol1)
 ROM_END
 
 
-GAME( 1995, toursol,  0,       pcat_dyn, pcat_dyn, driver_device, 0, ROT0, "Dynamo", "Tournament Solitaire (V1.06, 08/03/95)", MACHINE_NOT_WORKING|MACHINE_NO_SOUND )
+GAME( 1995, toursol,  0,       pcat_dyn, pcat_dyn, driver_device, 0, ROT0, "Dynamo", "Tournament Solitaire (V1.06, 08/03/95)", MACHINE_UNEMULATED_PROTECTION )
 GAME( 1995, toursol1, toursol, pcat_dyn, pcat_dyn, driver_device, 0, ROT0, "Dynamo", "Tournament Solitaire (V1.04, 06/22/95)", MACHINE_NOT_WORKING|MACHINE_NO_SOUND )
