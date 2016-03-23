d0867ac3f2bdcfd4d1d2361ce8b6408a185908f4
Brad Hughes <bradhugh@outlook.com>
2016-03-23 12:44:30 -0400

    Removing unused functions from winutf8.cpp. Also adding UWP implementations for Set/GetWindowText.

--- src/osd/windows/winutf8.h
+++ src/osd/windows/winutf8.h
@@ -13,10 +13,6 @@
 
 // wrappers for kernel32.dll
 void win_output_debug_string_utf8(const char *string);
-DWORD win_get_module_file_name_utf8(HMODULE module, char *filename, DWORD size);
-BOOL win_set_file_attributes_utf8(const char* filename, DWORD fileattributes);
-BOOL win_copy_file_utf8(const char* existingfilename, const char* newfilename, BOOL failifexists);
-BOOL win_move_file_utf8(const char* existingfilename, const char* newfilename);
 
 // wrappers for user32.dll
 int win_message_box_utf8(HWND window, const char *text, const char *caption, UINT type);
