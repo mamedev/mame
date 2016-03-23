3fcb69a1f20b09a69e51a0ccdea0a22ea8447770
Jeffrey Clark <dude@zaplabs.com>
2016-03-23 15:31:58 -0500

    Fix cross compile (nw)

--- src/osd/modules/input/input_dinput.cpp
+++ src/osd/modules/input/input_dinput.cpp
@@ -15,7 +15,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <tchar.h>
-#include <wrl\client.h>
+#include <wrl/client.h>
 
 // undef WINNT for dinput.h to prevent duplicate definition
 #undef WINNT
