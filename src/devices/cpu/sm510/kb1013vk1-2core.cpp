2bdf6d70ed96a308f333643d8e7f2b59fed6e889
hap <happppp@users.noreply.github.com>
2016-04-11 16:15:25 +0200

    hh_sm510: added nupogodi driver skeleton

--- src/devices/cpu/sm510/kb1013vk1-2core.cpp
+++ src/devices/cpu/sm510/kb1013vk1-2core.cpp
@@ -14,11 +14,12 @@ const device_type KB1013VK12 = &device_creator<kb1013vk12_device>;
 
 
 // internal memory maps
-static ADDRESS_MAP_START(program_2_7k, AS_PROGRAM, 8, sm510_base_device)
-	AM_RANGE(0x0000, 0x00ff) AM_ROM
+static ADDRESS_MAP_START(program_1_8k, AS_PROGRAM, 8, sm510_base_device)
+	AM_RANGE(0x000, 0x6ff) AM_ROM
+	AM_RANGE(0x700, 0x73f) AM_MIRROR(0x0c0) AM_ROM
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START(data_96_32x4, AS_DATA, 8, sm510_base_device)
+static ADDRESS_MAP_START(data_5x13x4, AS_DATA, 8, sm510_base_device)
 	AM_RANGE(0x00, 0x0c) AM_RAM
 	AM_RANGE(0x10, 0x1c) AM_RAM
 	AM_RANGE(0x20, 0x2c) AM_RAM
@@ -28,7 +29,7 @@ ADDRESS_MAP_END
 
 // device definitions
 kb1013vk12_device::kb1013vk12_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock)
-	: sm500_device(mconfig, KB1013VK12, "KB1013VK1-2", tag, owner, clock, 1 /* stack levels */, 12 /* prg width */, ADDRESS_MAP_NAME(program_2_7k), 7 /* data width */, ADDRESS_MAP_NAME(data_96_32x4), "kb1013vk1-2", __FILE__)
+	: sm500_device(mconfig, KB1013VK12, "KB1013VK1-2", tag, owner, clock, 1 /* stack levels */, 11 /* prg width */, ADDRESS_MAP_NAME(program_1_8k), 7 /* data width */, ADDRESS_MAP_NAME(data_5x13x4), "kb1013vk1-2", __FILE__)
 { }
 
 
--- src/devices/cpu/sm510/sm500core.cpp
+++ src/devices/cpu/sm510/sm500core.cpp
@@ -15,11 +15,11 @@ const device_type SM500 = &device_creator<sm500_device>;
 
 
 // internal memory maps
-static ADDRESS_MAP_START(program_2_7k, AS_PROGRAM, 8, sm510_base_device)
-	AM_RANGE(0x0000, 0x00ff) AM_ROM
+static ADDRESS_MAP_START(program_1_2k, AS_PROGRAM, 8, sm510_base_device)
+	AM_RANGE(0x000, 0x4bf) AM_ROM
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START(data_96_32x4, AS_DATA, 8, sm510_base_device)
+static ADDRESS_MAP_START(data_4x10x4, AS_DATA, 8, sm510_base_device)
 	AM_RANGE(0x00, 0x09) AM_RAM
 	AM_RANGE(0x10, 0x19) AM_RAM
 	AM_RANGE(0x20, 0x29) AM_RAM
@@ -29,7 +29,7 @@ ADDRESS_MAP_END
 
 // device definitions
 sm500_device::sm500_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock)
-	: sm510_base_device(mconfig, SM500, "SM500", tag, owner, clock, 1 /* stack levels */, 12 /* prg width */, ADDRESS_MAP_NAME(program_2_7k), 6 /* data width */, ADDRESS_MAP_NAME(data_96_32x4), "sm500", __FILE__)
+	: sm510_base_device(mconfig, SM500, "SM500", tag, owner, clock, 1 /* stack levels */, 11 /* prg width */, ADDRESS_MAP_NAME(program_1_2k), 6 /* data width */, ADDRESS_MAP_NAME(data_4x10x4), "sm500", __FILE__)
 { }
 
 sm500_device::sm500_device(const machine_config &mconfig, device_type type, const char *name, const char *tag, device_t *owner, UINT32 clock, int stack_levels, int prgwidth, address_map_constructor program, int datawidth, address_map_constructor data, const char *shortname, const char *source)
