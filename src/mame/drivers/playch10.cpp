5d8e00ae584833cd994168e0738f757e06e7cb7c
David Haywood <mamehaze@users.noreply.github.com>
2016-04-11 03:29:40 +0100

    new WORKING Tiny Toon Adventures (prototype) (PlayChoice-10) [Tony Swann]
    it's not actually very interesting, it's the final USA NES version code with the generic 'New Game 2' menu rom (no instructions etc,) that was also seen on the Virus prototype, so no acutal new data here.

--- src/mame/drivers/playch10.cpp
+++ src/mame/drivers/playch10.cpp
@@ -1420,6 +1420,27 @@ ROM_START( pc_ngai3 )   /* Ninja Gaiden 3 */
 	ROM_LOAD( "security.prm", 0x00, 0x10, CRC(13755943) SHA1(b7d809b0f60ef489777ccb35868f5c1e777356e0) )
 ROM_END
 
+// this is identcal to the USA NES release with the generic 'New Game 2' menu rom.
+// TT-CHR.U1           = nes-ni-0 chr          nes:ttoonadvu Tiny Toon Adventures (USA)
+// TT-GM2.U3           = u2                    pc_virus   Virus (Dr. Mario prototype, PlayChoice-10)
+// TT-PRG.U5           = nes-ni-0 prg          nes:ttoonadvu Tiny Toon Adventures (USA)
+ROM_START( pc_ttoon )   /* Tiny Toon Adventures */
+	BIOS_CPU
+	ROM_LOAD( "TT-GM2.U3",   0x0c000, 0x2000, CRC(d2764d91) SHA1(393b54148e9250f14d83318aed6686cc04b923e6) ) /* extra bios code for this game */
+	BIOS_GFX
+
+	ROM_REGION( 0x50000, "cart", 0 )
+	ROM_LOAD( "TT-PRG.U5",    0x10000, 0x20000, CRC(9cb55b96) SHA1(437c326a4575895b9d7e567cab4f70b2f44ed8dd) )   /* banked */
+	ROM_RELOAD(          0x30000, 0x20000 )
+
+	ROM_REGION( 0x020000, "gfx2", 0 )   /* cart gfx */
+	ROM_LOAD( "TT-CHR.U1",    0x00000, 0x20000, CRC(a024ae14) SHA1(2e797a173161a61c14ce299e3c5a31c6029f2b50) )
+
+	ROM_REGION( 0x10, "rp5h01", 0 ) /* rp5h01 data */
+	ROM_LOAD( "security.u6", 0x00, 0x10, CRC(5b4f6930) SHA1(bd152d6907fe55f80125b34360fdb44cfc348906) )
+ROM_END
+
+
 ROM_START( pc_radr2 )   /* Rad Racer II */
 	BIOS_CPU
 	ROM_LOAD( "qr-u3",    0x0c000, 0x2000, CRC(0c8fea63) SHA1(7ac04b151df732bd16708655352b7f13926f004f) ) /* extra bios code for this game */
@@ -1649,6 +1670,20 @@ DRIVER_INIT_MEMBER(playch10_state,virus)
 	DRIVER_INIT_CALL(pcfboard);
 }
 
+DRIVER_INIT_MEMBER(playch10_state,ttoon)
+{
+	UINT8 *ROM = memregion("rp5h01")->base();
+	UINT32 len = memregion("rp5h01")->bytes();
+	for (int i = 0; i < len; i++)
+	{
+		ROM[i] = BITSWAP8(ROM[i],0,1,2,3,4,5,6,7);
+		ROM[i] ^= 0xff;
+	}
+
+	/* common init */
+	DRIVER_INIT_CALL(pcgboard);
+}
+
 /*     YEAR  NAME      PARENT    BIOS      MACHINE   INPUT     INIT      MONITOR  */
 
 /* Standard Games */
@@ -1717,6 +1752,8 @@ GAME( 1990, pc_ngai2, playch10, playch10, playch10, playch10_state, pcgboard, RO
 GAME( 1991, pc_ngai3, playch10, playch10, playch10, playch10_state, pcgboard, ROT0, "Tecmo (Nintendo of America license)",      "Ninja Gaiden Episode III: The Ancient Ship of Doom (PlayChoice-10)", 0 )
 GAME( 1991, pc_pwbld, playch10, playch10, playch10, playch10_state, pcgboard, ROT0, "Taito (Nintendo of America license)",      "Power Blade (PlayChoice-10)", 0 )
 GAME( 1991, pc_rkats, playch10, playch10, playch10, playch10_state, pcgboard, ROT0, "Atlus (Nintendo of America license)",      "Rockin' Kats (PlayChoice-10)", 0 )
+GAME( 1991, pc_ttoon, playch10, playch10, playch10, playch10_state, ttoon,    ROT0, "Capcom USA (Nintendo of America license)", "Tiny Toon Adventures (prototype) (PlayChoice-10)", 0 ) // Code is final USA NES version of the game, (which is MMC3C according to nes.xml, but this cart has MMC3B)
+
 /* variant with 4 screen mirror */
 GAME( 1990, pc_radr2, playch10, playch10, playch10, playch10_state, pcgboard_type2, ROT0, "Square (Nintendo of America license)", "Rad Racer II (PlayChoice-10)", 0 )
 GAME( 1985, pc_gntlt, playch10, playch10, playch10, playch10_state, pcgboard_type2, ROT0, "Atari / Tengen (Nintendo of America license)", "Gauntlet (PlayChoice-10)", MACHINE_IMPERFECT_GRAPHICS )
