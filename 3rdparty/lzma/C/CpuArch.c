7610a47418e964ea97047cc71f57f4dd4b061850
Jindřich Makovička <makovick@gmail.com>
2016-04-13 09:45:21 +0200

    Fix warnings on C prototypes

--- 3rdparty/lzma/C/CpuArch.c
+++ 3rdparty/lzma/C/CpuArch.c
@@ -137,7 +137,7 @@ int x86cpuid_GetFirm(const Cx86cpuid *p)
   return -1;
 }
 
-Bool CPU_Is_InOrder()
+Bool CPU_Is_InOrder(void)
 {
   Cx86cpuid p;
   int firm;
@@ -168,7 +168,7 @@ Bool CPU_Is_InOrder()
 
 #if !defined(MY_CPU_AMD64) && defined(_WIN32)
 #include <windows.h>
-static Bool CPU_Sys_Is_SSE_Supported()
+static Bool CPU_Sys_Is_SSE_Supported(void)
 {
   OSVERSIONINFO vi;
   vi.dwOSVersionInfoSize = sizeof(vi);
@@ -181,7 +181,7 @@ static Bool CPU_Sys_Is_SSE_Supported()
 #define CHECK_SYS_SSE_SUPPORT
 #endif
 
-Bool CPU_Is_Aes_Supported()
+Bool CPU_Is_Aes_Supported(void)
 {
   Cx86cpuid p;
   CHECK_SYS_SSE_SUPPORT
--- 3rdparty/lzma/C/CpuArch.h
+++ 3rdparty/lzma/C/CpuArch.h
@@ -212,8 +212,8 @@ int x86cpuid_GetFirm(const Cx86cpuid *p);
 #define x86cpuid_GetModel(ver)  (((ver >> 12) &  0xF0) | ((ver >> 4) & 0xF))
 #define x86cpuid_GetStepping(ver) (ver & 0xF)
 
-Bool CPU_Is_InOrder();
-Bool CPU_Is_Aes_Supported();
+Bool CPU_Is_InOrder(void);
+Bool CPU_Is_Aes_Supported(void);
 
 #endif
 
