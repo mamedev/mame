729d850c6b0dc909161d1e2aa8381d8f2af4458a
David Haywood <mamehaze@users.noreply.github.com>
2016-05-01 14:40:52 +0100

    gfxdecodes (nw)
    still need to decrypt CPU code tho

--- src/mame/drivers/metlfrzr.cpp
+++ src/mame/drivers/metlfrzr.cpp
@@ -60,17 +60,33 @@ static const gfx_layout tiles8x8_layout =
 	8,8,
 	RGN_FRAC(1,1),
 	4,
-	{ 0, 1, 2, 3 },
-	{ 0, 4, 8, 12, 16, 20, 24, 28 },
-	{ 0*32, 1*32, 2*32, 3*32, 4*32, 5*32, 6*32, 7*32 },
+	{ 0, 4, 8, 12 },
+	{ 0, 1, 2, 3, 16, 17, 18, 19 }, 
+//	{ 19, 18, 17, 16, 3, 2, 1, 0 }, // maybe display is flipped?
+//	{ 0*32, 1*32, 2*32, 3*32, 4*32, 5*32, 6*32, 7*32 },
+	{ 7*32, 6*32, 5*32, 4*32, 3*32, 2*32, 1*32, 0*32 },
 	32*8
 };
+
+static const gfx_layout tiles16x16_layout =
+{
+	16,16,
+	RGN_FRAC(1,1),
+	4,
+	{ 0, 4, 8, 12 },
+//	{ 0, 1, 2, 3, 16, 17, 18, 19, 64*8+0, 64*8+1, 64*8+2, 64*8+3, 64*8+16, 64*8+17, 64*8+18, 64*8+19 },
+	{ 64*8+19, 64*8+18, 64*8+17, 64*8+16, 64*8+3, 64*8+2, 64*8+1, 64*8+0, 19, 18, 17, 16, 3, 2, 1, 0 },
+//	{ 0*32,1*32,2*32,3*32,4*32,5*32,6*32,7*32, 8*32, 9*32, 10*32, 11*32, 12*32, 13*32, 14*32, 15*32 },
+	{ 15*32,14*32,13*32,12*32,11*32,10*32,9*32,8*32, 7*32, 6*32, 5*32, 4*32, 3*32, 2*32, 1*32, 0*32 },
+	128*8
+};
+
 
 static GFXDECODE_START( metlfrzr )
 	GFXDECODE_ENTRY( "gfx1", 0, tiles8x8_layout, 0, 16 )
 	GFXDECODE_ENTRY( "gfx2", 0, tiles8x8_layout, 0, 16 )
-	GFXDECODE_ENTRY( "gfx3", 0, tiles8x8_layout, 0, 16 )
-	GFXDECODE_ENTRY( "gfx4", 0, tiles8x8_layout, 0, 16 )
+	GFXDECODE_ENTRY( "gfx3", 0, tiles16x16_layout, 0, 16 )
+	GFXDECODE_ENTRY( "gfx4", 0, tiles16x16_layout, 0, 16 )
 GFXDECODE_END
 
 static MACHINE_CONFIG_START( metlfrzr, metlfrzr_state )
@@ -118,19 +134,19 @@ ROM_START( metlfrzr )
 	ROM_LOAD( "3.4h", 0x0000, 0x8000, CRC(36f88e54) SHA1(5cbea56c7e547c353ae2f9256caaceb20e5e8503) )
 
 	ROM_REGION( 0x20000, "gfx1", 0 )
-	ROM_LOAD16_BYTE( "10.5a",   0x00000, 0x10000, CRC(3313e74a) SHA1(8622dfb5c013173d5bb037254f4c23b1282404e1) )
-	ROM_LOAD16_BYTE( "12.7a",   0x00001, 0x10000, CRC(6da5fda9) SHA1(9d7b0b26598f31da589fece3535a4d1405b03fc2) )
+	ROM_LOAD16_BYTE( "10.5a",   0x00001, 0x10000, CRC(3313e74a) SHA1(8622dfb5c013173d5bb037254f4c23b1282404e1) )
+	ROM_LOAD16_BYTE( "12.7a",   0x00000, 0x10000, CRC(6da5fda9) SHA1(9d7b0b26598f31da589fece3535a4d1405b03fc2) )
 
 	ROM_REGION( 0x20000, "gfx2", 0 )
-	ROM_LOAD16_BYTE( "11.6a",   0x00000, 0x10000, CRC(fa6490b8) SHA1(9a4c1e09b9e8fb256fec0a5ed120fece8a12e1c8) )
-	ROM_LOAD16_BYTE( "13.9a",   0x00001, 0x10000, CRC(a4f689ec) SHA1(e58bfede3fabf4cfca76c20aafb3e9fb604777c9) )
+	ROM_LOAD16_BYTE( "11.6a",   0x00001, 0x10000, CRC(fa6490b8) SHA1(9a4c1e09b9e8fb256fec0a5ed120fece8a12e1c8) )
+	ROM_LOAD16_BYTE( "13.9a",   0x00000, 0x10000, CRC(a4f689ec) SHA1(e58bfede3fabf4cfca76c20aafb3e9fb604777c9) )
 
 	ROM_REGION( 0x20000, "gfx3", 0 )
-	ROM_LOAD16_BYTE( "14.13a",   0x00000, 0x10000, CRC(a9cd5225) SHA1(f3d5e29ee08fb563fdc1af3c64128f2cd2feb987) )
-	ROM_LOAD16_BYTE( "16.11a",   0x00001, 0x10000, CRC(92f2cb49) SHA1(498021d94b0fde216207076491702af2324a2dcc) )
+	ROM_LOAD16_BYTE( "14.13a",   0x00001, 0x10000, CRC(a9cd5225) SHA1(f3d5e29ee08fb563fdc1af3c64128f2cd2feb987) )
+	ROM_LOAD16_BYTE( "16.11a",   0x00000, 0x10000, CRC(92f2cb49) SHA1(498021d94b0fde216207076491702af2324a2dcc) )
 	ROM_REGION( 0x20000, "gfx4", 0 )
-	ROM_LOAD16_BYTE( "15.12a",   0x00000, 0x10000, CRC(ce5c4c8b) SHA1(2351d66ba51e80097ce53bfd448ac24901844cda) )
-	ROM_LOAD16_BYTE( "17.10a",   0x00001, 0x10000, CRC(3fec33f7) SHA1(af086ba30fc4521a0114da2824f5baa04d225a89) )
+	ROM_LOAD16_BYTE( "15.12a",   0x00001, 0x10000, CRC(ce5c4c8b) SHA1(2351d66ba51e80097ce53bfd448ac24901844cda) )
+	ROM_LOAD16_BYTE( "17.10a",   0x00000, 0x10000, CRC(3fec33f7) SHA1(af086ba30fc4521a0114da2824f5baa04d225a89) )
 
 
 	ROM_REGION( 0x20000, "proms", 0 )
@@ -146,4 +162,4 @@ ROM_END
 
 
 
-GAME( 1989, metlfrzr,  0,    metlfrzr, metlfrzr, driver_device,  0, ROT0, "Seibu", "Metal Freezer", MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
+GAME( 1989, metlfrzr,  0,    metlfrzr, metlfrzr, driver_device,  0, ROT90, "Seibu", "Metal Freezer", MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
