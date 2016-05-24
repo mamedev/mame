e208f1d54082b8ed4a03d13d266b7014dc65b218
ImJezze <jezze@gmx.net>
2016-05-24 19:40:47 +0200

    Revert partial fix for border style issue (nw) - reverted from commit f911dfc and a6ccd3b, because it wasted up to 10 percent of the performance in fullscreen mode (actual fix for MT-06209 is not harmed)

--- src/osd/modules/render/drawd3d.cpp
+++ src/osd/modules/render/drawd3d.cpp
@@ -578,7 +578,7 @@ int renderer_d3d9::initialize()
 		return false;
 	}
 
-	// create the device immediately for the full screen case (defer for window mode in update_window_size())
+	// create the device immediately for the full screen case (defer for window mode)
 	auto win = assert_window();
 	if (win->fullscreen() && device_create(win->main_window()->platform_window<HWND>()))
 	{
@@ -841,7 +841,7 @@ try_again:
 	m_presentation.MultiSampleType               = D3DMULTISAMPLE_NONE;
 	m_presentation.SwapEffect                    = D3DSWAPEFFECT_DISCARD;
 	m_presentation.hDeviceWindow                 = win->platform_window<HWND>();
-	m_presentation.Windowed                      = !win->fullscreen() || !video_config.switchres || win->win_has_menu();
+	m_presentation.Windowed                      = !win->fullscreen() || win->win_has_menu();
 	m_presentation.EnableAutoDepthStencil        = FALSE;
 	m_presentation.AutoDepthStencilFormat        = D3DFMT_D16;
 	m_presentation.Flags                         = 0;
