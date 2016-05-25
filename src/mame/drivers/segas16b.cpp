16d0839f766b9716b7512882dc70e5582f9cadef
MetalliC <0vetal0@gmail.com>
2016-05-25 17:57:31 +0300

    chdman: ignore ATA ident CHS values for > 8GB images minor docs update

--- src/mame/drivers/segas16b.cpp
+++ src/mame/drivers/segas16b.cpp
@@ -8139,6 +8139,7 @@ ROM_END
 //  Wonder Boy III, Sega System 16B
 //  CPU: FD1094 (317-0085)
 //  ROM Board type: 171-5358
+// Sega ID# for ROM board: 834-6854
 //
 //  Pos.   Silk        Type        Part                Pos.   Silk        Type        Part
 //  A1     EPR-12198   27C512      68000 program       B1     EPR-12190   27C512      Sprite data
--- src/tools/chdman.cpp
+++ src/tools/chdman.cpp
@@ -1790,6 +1790,10 @@ static void do_create_hd(parameters_t &params)
 		cylinders = (identdata[3] << 8) | identdata[2];
 		heads = (identdata[7] << 8) | identdata[6];
 		sectors = (identdata[13] << 8) | identdata[12];
+
+		// ignore CHS for > 8GB drives
+		if (input_file && filesize > 0x200000000)
+			cylinders = 0;
 	}
 
 	// extract geometry from the parent if we have one
