aecbc11dd862851a022b722ee508f78897941ccd
Curt Coder <curtcoder@mail.com>
2016-06-09 22:09:18 +0300

    This is not mine. (nw)

--- src/mame/drivers/jackal.cpp
+++ src/mame/drivers/jackal.cpp
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:Curt Coder
+// copyright-holders:?
 // thanks-to:Kenneth Lin (original driver author)
 /***************************************************************************
 
