37d8de37fa1c4aec64ba5553cae530067476bb2c
Curt Coder <curtcoder@mail.com>
2016-06-10 10:00:25 +0300

    Revert "This is not mine. (nw)"
    This reverts commit aecbc11dd862851a022b722ee508f78897941ccd.

--- src/mame/drivers/jackal.cpp
+++ src/mame/drivers/jackal.cpp
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:?
+// copyright-holders:Curt Coder
 // thanks-to:Kenneth Lin (original driver author)
 /***************************************************************************
 
