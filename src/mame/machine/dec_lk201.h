1558ddb1bdef98ad6a94839813209c1377c7dfe1
Bavarese <Bavarese@users.noreply.github.com>
2016-06-11 11:48:25 +0200

    Update dec_lk201.h

--- src/mame/machine/dec_lk201.h
+++ src/mame/machine/dec_lk201.h
@@ -12,19 +12,66 @@
 //  MACROS / CONSTANTS
 //**************************************************************************
 
+#define LK_CMD_POWER_UP     0xfd    /* init power-up sequence */
+#define LK_STAT_PWRUP_OK	0x00	/* the power-up self test OK */
+#define LK_STAT_PWRUP_KDOWN	0x3d	/* a key was down during the test */
+#define LK_STAT_PWRUP_ERROR	0x3e	/* keyboard self test failure */
+
 #define LK_CMD_LEDS_ON          0x13    /* light LEDs - 1st param: led bitmask */
 #define LK_CMD_LEDS_OFF         0x11    /* turn off LEDs */
 
 #define LK_CMD_DIS_KEYCLK       0x99    /* disable the keyclick */
 #define LK_CMD_ENB_KEYCLK       0x1b    /* enable the keyclick - 1st param: volume */
-//#define LK_CMD_DIS_CTLCLK       0xb9    /* disable the Ctrl keyclick */
-//#define LK_CMD_ENB_CTLCLK       0xbb    /* enable the Ctrl keyclick */
+#define LK_CMD_DIS_CTLCLK       0xb9    /* disable the Ctrl keyclick */
+#define LK_CMD_ENB_CTLCLK       0xbb    /* enable the Ctrl keyclick */
 #define LK_CMD_SOUND_CLK        0x9f    /* emit a keyclick  - 1st param: volume */
 #define LK_CMD_DIS_BELL         0xa1    /* disable the bell */
 #define LK_CMD_ENB_BELL         0x23    /* enable the bell - 1st param: volume */
 #define LK_CMD_BELL             0xa7    /* emit a bell - 1st param: volume */
 
-#define LK_CMD_POWER_UP         0xfd    /* init power-up sequence */
+#define LK_PARAM		    0x80	/* start/end parameter list */
+
+#define LK_CMD_RESUME		0x8b	/* resume transmission to the host */
+#define LK_CMD_INHIBIT		0x89	/* stop transmission to the host */
+
+#define LK_CMD_TMP_NORPT	0xc1	/* disable typematic - TEMPORARY AUTO-REPEAT INHIBIT */
+/* for the currently pressed key */
+#define LK_CMD_ENB_RPT		0xe3	/* enable typematic - ENABLE AUTO-REPEAT ACROSSS KEYBOARD */
+/* for RPT_DOWN groups */
+#define LK_CMD_DIS_RPT		0xe1	/* disable typematic - DISABLE AUTO-REPEAT ACROSSS KEYBOARD */
+/* for RPT_DOWN groups */
+#define LK_CMD_RPT_TO_DOWN	0xd9	/* CHANGE ALL AUTO-REPEAT TO "down only" (alt.description: set RPT_DOWN groups to DOWN ) */
+#define LK_CMD_REQ_ID		0xab	/* request the keyboard ID */
+#define LK_CMD_POWER_UP		0xfd	/* init power-up sequence */
+#define LK_CMD_TEST_MODE	0xcb	/* enter the factory test mode */
+#define LK_CMD_TEST_EXIT	0x80	/* exit the factory test mode */
+#define LK_CMD_SET_DEFAULTS	0xd3	/* set power-up defaults */
+
+/* LK201 RESPONDS TO SPECIAL KEYS: */
+#define LK_KEY_SHIFT	0xae
+#define LK_KEY_CTRL		0xaf
+#define LK_KEY_LOCK		0xb0
+#define LK_KEY_COMP		0xb1
+
+/* LK201 SPECIAL RESPONSES (codes above 64 decimal): */
+#define LK_KEY_RELEASE		0xb3	/* all keys released */
+#define LK_KEY_REPEAT		0xb4	/* repeat the last key */
+#define LK_STAT_RESUME_ERR	0xb5	/* keystrokes lost while inhibited */
+#define LK_STAT_ERROR		0xb6	/* an invalid command received */
+#define LK_STAT_INHIBIT_ACK	0xb7	/* transmission inhibited - KEYBOARD LOCKED ACK */ 
+#define LK_STAT_TEST_ACK	0xb8	/* the factory test mode entered - TEST MODE ACK */
+#define LK_STAT_MODE_KEYDOWN	0xb9	/* a key is down on a change - PREFIX TO KEYS DOWN */
+
+/* to the DOWN_UP mode; */
+/* the keycode follows */
+#define LK_STAT_MODE_ACK	0xba	/* the mode command succeeded - MODE CHANGE ACK */
+
+// TCR - Timer Compare Register 
+#define TCR_OCIE 0x40 // Bit 6 (output compare IRQ enable)
+#define TCR_OLVL 0x01 // Bit 1 (output level)         TODO: defines result (level) after successful compare!    TODO 2: AUTO-RESETS (when...?)
+
+// TSR - Timer Status Register 
+#define TSR_OCFL 0x40 // TSR (68HC05 output compare flag)  TODO: SET BY INTERAL COMPARE !
 
 //**************************************************************************
 //  INTERFACE CONFIGURATION MACROS
@@ -45,16 +92,16 @@ public:
 	// construction/destruction
 	lk201_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
 
-	DECLARE_READ8_MEMBER( ddr_r );
-	DECLARE_WRITE8_MEMBER( ddr_w );
-	DECLARE_READ8_MEMBER( ports_r );
-	DECLARE_WRITE8_MEMBER( ports_w );
-	DECLARE_READ8_MEMBER( sci_r );
-	DECLARE_WRITE8_MEMBER( sci_w );
-	DECLARE_READ8_MEMBER( spi_r );
-	DECLARE_WRITE8_MEMBER( spi_w );
-	DECLARE_READ8_MEMBER( timer_r );
-	DECLARE_WRITE8_MEMBER( timer_w );
+	DECLARE_READ8_MEMBER(ddr_r);
+	DECLARE_WRITE8_MEMBER(ddr_w);
+	DECLARE_READ8_MEMBER(ports_r);
+	DECLARE_WRITE8_MEMBER(ports_w);
+	DECLARE_READ8_MEMBER(sci_r);
+	DECLARE_WRITE8_MEMBER(sci_w);
+	DECLARE_READ8_MEMBER(spi_r);
+	DECLARE_WRITE8_MEMBER(spi_w);
+	DECLARE_READ8_MEMBER(timer_r);
+	DECLARE_WRITE8_MEMBER(timer_w);
 
 	template<class _Object> static devcb_base &set_tx_handler(device_t &device, _Object wr) { return downcast<lk201_device &>(device).m_tx_handler.set_callback(wr); }
 
@@ -95,6 +142,7 @@ private:
 	} m_timer;
 
 	emu_timer *m_count;
+	emu_timer *m_beeper;
 
 	UINT8 sci_ctl2;
 	UINT8 sci_status;
@@ -127,6 +175,8 @@ private:
 
 	void send_port(address_space &space, UINT8 offset, UINT8 data);
 	void update_interrupts();
+
+	int m_kbd_state;
 
 	devcb_write_line m_tx_handler;
 };
