56aeaaf61ac3d5672e3b0240d5c32f8b7bdc4c36
hap <happppp@users.noreply.github.com>
2016-06-17 20:08:52 +0200

    remove leftover

--- src/mame/drivers/unkhorse.cpp
+++ src/mame/drivers/unkhorse.cpp
@@ -46,7 +46,6 @@ public:
 
 	DECLARE_READ8_MEMBER(input_r);
 	DECLARE_WRITE8_MEMBER(output_w);
-	DECLARE_WRITE_LINE_MEMBER(timer_out);
 
 	virtual void machine_start() override;
 	UINT32 screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
