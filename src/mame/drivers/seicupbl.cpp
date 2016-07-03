ea42b8bbaf0312afa2f1f931e54b3a71f0492e89
Robbbert <Robbbert@users.noreply.github.com>
2016-07-04 09:54:42 +1000

    Fixed the build.

--- src/mame/drivers/seicupbl.cpp
+++ src/mame/drivers/seicupbl.cpp
@@ -73,10 +73,10 @@ public:
 	
 protected:
 	// driver_device overrides
-	virtual void machine_start();
-	virtual void machine_reset();
+	virtual void machine_start() override;
+	virtual void machine_reset() override;
 
-	virtual void video_start();
+	virtual void video_start() override;
 	
 private:
 	void draw_sprites(screen_device &screen, bitmap_ind16 &bitmap,const rectangle &cliprect);
