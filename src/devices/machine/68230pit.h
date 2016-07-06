998c1358b4c2f143cb8ff424d137f6cbc81d9d09
Joakim Larsson Edstrom <joakimlarsson42@gmail.com>
2016-07-06 20:48:33 +0200

    Added support for the 24 bit timer

--- src/devices/machine/68230pit.h
+++ src/devices/machine/68230pit.h
@@ -197,7 +197,7 @@ protected:
 	virtual void device_start () override;
 	virtual void device_reset () override;
 	virtual void device_timer (emu_timer &timer, device_timer_id id, int param, void *ptr) override;
-	//	virtual void execute_run () override;
+
 	int m_icount;
 
 	devcb_write8        m_pa_out_cb;
