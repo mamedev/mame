6735af0124e70ab90d62a4931a7fc7302f583978
Dirk Best <mail@dirk-best.de>
2016-07-30 16:55:31 +0200

    apricot: use device_buffered_serial_interface and device_matrix_keyboard_interface for the hle keyboard emulation to reduce code duplication and implement a fifo for the keyboard

--- src/devices/bus/apricot/keyboard/hle.h
+++ src/devices/bus/apricot/keyboard/hle.h
@@ -13,6 +13,7 @@
 
 #include "emu.h"
 #include "keyboard.h"
+#include "machine/keyboard.h"
 
 
 //**************************************************************************
@@ -21,13 +22,14 @@
 
 // ======================> apricot_keyboard_hle_device
 
-class apricot_keyboard_hle_device : public device_t, public device_apricot_keyboard_interface, public device_serial_interface
+class apricot_keyboard_hle_device : public device_t,
+                                    public device_apricot_keyboard_interface,
+									public device_buffered_serial_interface<8>,
+									protected device_matrix_keyboard_interface<13>
 {
 public:
 	// construction/destruction
 	apricot_keyboard_hle_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
-
-	DECLARE_INPUT_CHANGED_MEMBER(key_callback);
 
 	// from host
 	virtual void out_w(int state) override;
@@ -38,19 +40,19 @@ protected:
 	virtual void device_start() override;
 	virtual void device_reset() override;
 
-	// device_serial_interface overrides
+	// device_buffered_serial_interface overrides
 	virtual void tra_callback() override;
 	virtual void tra_complete() override;
-	virtual void rcv_callback() override;
-	virtual void rcv_complete() override;
+	virtual void received_byte(UINT8 byte) override;
+
+	// device_matrix_keyboard_interface overrides
+	virtual void key_make(UINT8 row, UINT8 column) override;
+	virtual void key_break(UINT8 row, UINT8 column) override;
+	void transmit_byte(UINT8 byte);
 
 	virtual void device_timer(emu_timer &timer, device_timer_id id, int param, void *ptr) override;
 
 private:
-	int m_rxd;
-
-	UINT8 m_data_in;
-	UINT8 m_data_out;
 };
 
 
