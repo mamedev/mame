854ac627c2c9c774f7c813846b345034503116d0
Miodrag Milanovic <mmicko@gmail.com>
2016-09-03 14:45:20 +0200

    Fix PugiXML interface (nw)

--- 3rdparty/pugixml/src/pugixml.hpp
+++ 3rdparty/pugixml/src/pugixml.hpp
@@ -273,7 +273,7 @@ namespace pugi
 		// Construct writer from a FILE* object; void* is used to avoid header dependencies on stdio
 		xml_writer_file(void* file);
 
-		virtual void write(const void* data, size_t size);
+		virtual void write(const void* data, size_t size) override;
 
 	private:
 		void* file;
@@ -288,7 +288,7 @@ namespace pugi
 		xml_writer_stream(std::basic_ostream<char, std::char_traits<char> >& stream);
 		xml_writer_stream(std::basic_ostream<wchar_t, std::char_traits<wchar_t> >& stream);
 
-		virtual void write(const void* data, size_t size);
+		virtual void write(const void* data, size_t size) override;
 
 	private:
 		std::basic_ostream<char, std::char_traits<char> >* narrow_stream;
@@ -1214,7 +1214,7 @@ namespace pugi
 		explicit xpath_exception(const xpath_parse_result& result);
 
 		// Get error message
-		virtual const char* what() const throw();
+		virtual const char* what() const throw() override;
 
 		// Get parse result
 		const xpath_parse_result& result() const;
