8e3f6ef5ac571d89d874c617260b7fcc3698ae3f
James Wallace <james-wallace-ghub@users.noreply.github.com>
2016-09-23 14:58:48 +0100

    thanks-to fixes 1(nw)

--- src/mame/drivers/mpu4.cpp
+++ src/mame/drivers/mpu4.cpp
@@ -1,5 +1,6 @@
 // license:BSD-3-Clause
 // copyright-holders:James Wallace
+// thanks-to:Chris Wren, Tony Friery, MFME
 /* MPU4 hardware emulation
   for sets see the various includes prefixed 'mpu4'
 */
