6cd263c667bd71fac0a8e9eb3aa0fb81425ddb29
Ivan Vangelista <mesgnet@yahoo.it>
2016-10-08 10:01:58 +0200

    ay8910.h: list some more aliases, as per Robbie (nw)

--- src/devices/sound/ay8910.h
+++ src/devices/sound/ay8910.h
@@ -18,7 +18,7 @@ YM2149: higher resolution, selectable clock divider
 YM3439: same as 2149
 YMZ284: 0 I/O port, different clock divider
 YMZ294: 0 I/O port
-OKI M5255: differences not known
+OKI M5255, Winbond WF19054, JFC 95101, File KC89C72, Toshiba T7766A : differences to be listed
 */
 
 #define ALL_8910_CHANNELS -1
