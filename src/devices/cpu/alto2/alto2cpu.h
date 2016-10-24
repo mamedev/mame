3bec26611e2fcfe667674d36f40f39a853cbe5cb
smf- <smf-@users.noreply.github.com>
2016-10-24 16:54:01 +0100

    add virtual for consistency (nw)

--- src/devices/cpu/alto2/alto2cpu.h
+++ src/devices/cpu/alto2/alto2cpu.h
@@ -233,7 +233,7 @@ protected:
 	//! device (P)ROMs
 	virtual const tiny_rom_entry *device_rom_region() const override;
 	//! device_state_interface overrides
-	void state_string_export(const device_state_entry &entry, std::string &str) const override;
+	virtual void state_string_export(const device_state_entry &entry, std::string &str) const override;
 
 	//! device_disasm_interface overrides
 	virtual uint32_t disasm_min_opcode_bytes() const override { return 4; }
