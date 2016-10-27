331830424ea615d0ee86f4e47579c7a7e9a5af96
Nathan Woods <npwoods@mess.org>
2016-10-27 06:46:26 -0400

    Changed the i8008 disassembler to use 'std::ostream &' internally

--- src/devices/cpu/i8008/8008dasm.cpp
+++ src/devices/cpu/i8008/8008dasm.cpp
@@ -16,7 +16,7 @@
 static const char reg[] = { 'a', 'b', 'c', 'd', 'e', 'h', 'l', 'm' };
 static const char flag_names[] = { 'c', 'z', 's', 'p' };
 
-CPU_DISASSEMBLE( i8008 )
+static offs_t internal_disasm_i8008(cpu_device *device, std::ostream &stream, offs_t pc, const uint8_t *oprom, const uint8_t *opram, int options)
 {
 	uint32_t flags = 0;
 	unsigned PC = pc;
@@ -25,91 +25,101 @@ CPU_DISASSEMBLE( i8008 )
 	{
 		case 0x03:  // starting with 11
 					if (op==0xff) {
-						sprintf (buffer,"hlt");
+						util::stream_format(stream, "hlt");
 					} else {
-						sprintf (buffer,"l%c%c",reg[(op >> 3) & 7],reg[op & 7]);
+						util::stream_format(stream, "l%c%c",reg[(op >> 3) & 7],reg[op & 7]);
 					}
 					break;
 		case 0x00:  // starting with 00
 					switch(op & 7) {
 						case 0 :    if(((op >> 3) & 7)==0) {
-										sprintf (buffer,"hlt");
+										util::stream_format(stream, "hlt");
 									} else {
 										if(((op >> 3) & 7)==7) {
-											sprintf (buffer,"illegal");
+											util::stream_format(stream, "illegal");
 										} else {
-											sprintf (buffer,"in%c",reg[(op >> 3) & 7]);
+											util::stream_format(stream, "in%c",reg[(op >> 3) & 7]);
 										}
 									}
 									break;
 						case 1 :    if(((op >> 3) & 7)==0) {
-										sprintf (buffer,"hlt");
+										util::stream_format(stream, "hlt");
 									} else {
 										if(((op >> 3) & 7)==7) {
-											sprintf (buffer,"illegal");
+											util::stream_format(stream, "illegal");
 										} else {
-											sprintf (buffer,"dc%c",reg[(op >> 3) & 7]);
+											util::stream_format(stream, "dc%c",reg[(op >> 3) & 7]);
 										}
 									}
 									break;
 						case 2 :    {
 										switch((op >> 3) & 7) {
-											case 0 :    sprintf (buffer,"rlc"); break;
-											case 1 :    sprintf (buffer,"rrc"); break;
-											case 2 :    sprintf (buffer,"ral"); break;
-											case 3 :    sprintf (buffer,"rar"); break;
-											default :   sprintf (buffer,"illegal"); break;
+											case 0 :    util::stream_format(stream, "rlc"); break;
+											case 1 :    util::stream_format(stream, "rrc"); break;
+											case 2 :    util::stream_format(stream, "ral"); break;
+											case 3 :    util::stream_format(stream, "rar"); break;
+											default :   util::stream_format(stream, "illegal"); break;
 										}
 									}
 									break;
-						case 3 :    sprintf (buffer,"r%c%c",(BIT(op,5) ? 't' : 'f'),flag_names[(op>>3)&3]); break;
+						case 3 :    util::stream_format(stream, "r%c%c",(BIT(op,5) ? 't' : 'f'),flag_names[(op>>3)&3]); break;
 						case 4 :    {
 										switch((op >> 3) & 7) {
-											case 0 :    sprintf (buffer,"adi %02x",ARG(pc)); pc++; break;
-											case 1 :    sprintf (buffer,"aci %02x",ARG(pc)); pc++; break;
-											case 2 :    sprintf (buffer,"sui %02x",ARG(pc)); pc++; break;
-											case 3 :    sprintf (buffer,"sbi %02x",ARG(pc)); pc++; break;
-											case 4 :    sprintf (buffer,"ndi %02x",ARG(pc)); pc++; break;
-											case 5 :    sprintf (buffer,"xri %02x",ARG(pc)); pc++; break;
-											case 6 :    sprintf (buffer,"ori %02x",ARG(pc)); pc++; break;
-											case 7 :    sprintf (buffer,"cpi %02x",ARG(pc)); pc++; break;
+											case 0 :    util::stream_format(stream, "adi %02x",ARG(pc)); pc++; break;
+											case 1 :    util::stream_format(stream, "aci %02x",ARG(pc)); pc++; break;
+											case 2 :    util::stream_format(stream, "sui %02x",ARG(pc)); pc++; break;
+											case 3 :    util::stream_format(stream, "sbi %02x",ARG(pc)); pc++; break;
+											case 4 :    util::stream_format(stream, "ndi %02x",ARG(pc)); pc++; break;
+											case 5 :    util::stream_format(stream, "xri %02x",ARG(pc)); pc++; break;
+											case 6 :    util::stream_format(stream, "ori %02x",ARG(pc)); pc++; break;
+											case 7 :    util::stream_format(stream, "cpi %02x",ARG(pc)); pc++; break;
 										}
 									}
 									break;
-						case 5 :    sprintf (buffer,"rst %02x",(op>>3) & 7); break;
-						case 6 :    sprintf (buffer,"l%ci %02x",reg[(op >> 3) & 7],ARG(pc)); pc++; break;
-						case 7 :    sprintf (buffer,"ret"); break;
+						case 5 :    util::stream_format(stream, "rst %02x",(op>>3) & 7); break;
+						case 6 :    util::stream_format(stream, "l%ci %02x",reg[(op >> 3) & 7],ARG(pc)); pc++; break;
+						case 7 :    util::stream_format(stream, "ret"); break;
 					}
 					break;
 		case 0x01:  // starting with 01
 					switch(op & 7) {
-						case 0 :    sprintf (buffer,"j%c%c %02x%02x",(BIT(op,5)? 't' : 'f'),flag_names[(op>>3)&3], ARG(pc+1) & 0x3f,ARG(pc)); pc+=2; break;
-						case 2 :    sprintf (buffer,"c%c%c %02x%02x",(BIT(op,5)? 't' : 'f'),flag_names[(op>>3)&3], ARG(pc+1) & 0x3f,ARG(pc)); pc+=2; break;
-						case 4 :    sprintf (buffer,"jmp %02x%02x",ARG(pc+1) & 0x3f,ARG(pc)); pc+=2; break;
-						case 6 :    sprintf (buffer,"cal %02x%02x",ARG(pc+1) & 0x3f,ARG(pc)); pc+=2; break;
+						case 0 :    util::stream_format(stream, "j%c%c %02x%02x",(BIT(op,5)? 't' : 'f'),flag_names[(op>>3)&3], ARG(pc+1) & 0x3f,ARG(pc)); pc+=2; break;
+						case 2 :    util::stream_format(stream, "c%c%c %02x%02x",(BIT(op,5)? 't' : 'f'),flag_names[(op>>3)&3], ARG(pc+1) & 0x3f,ARG(pc)); pc+=2; break;
+						case 4 :    util::stream_format(stream, "jmp %02x%02x",ARG(pc+1) & 0x3f,ARG(pc)); pc+=2; break;
+						case 6 :    util::stream_format(stream, "cal %02x%02x",ARG(pc+1) & 0x3f,ARG(pc)); pc+=2; break;
 						case 1 :
 						case 3 :
 						case 5 :
 						case 7 :    if (((op>>4)&3)==0) {
-										sprintf (buffer,"inp %02x",(op >> 1) & 0x07);
+										util::stream_format(stream, "inp %02x",(op >> 1) & 0x07);
 									} else {
-										sprintf (buffer,"out %02x",(op >> 1) & 0x1f);
+										util::stream_format(stream, "out %02x",(op >> 1) & 0x1f);
 									}
 									break;
 					}
 					break;
 		case 0x02:  // starting with 10
 					switch((op >> 3) & 7) {
-						case 0 :    sprintf (buffer,"ad%c",reg[op & 7]); break;
-						case 1 :    sprintf (buffer,"ac%c",reg[op & 7]); break;
-						case 2 :    sprintf (buffer,"su%c",reg[op & 7]); break;
-						case 3 :    sprintf (buffer,"sb%c",reg[op & 7]); break;
-						case 4 :    sprintf (buffer,"nd%c",reg[op & 7]); break;
-						case 5 :    sprintf (buffer,"xr%c",reg[op & 7]); break;
-						case 6 :    sprintf (buffer,"or%c",reg[op & 7]); break;
-						case 7 :    sprintf (buffer,"cp%c",reg[op & 7]); break;
+						case 0 :    util::stream_format(stream, "ad%c",reg[op & 7]); break;
+						case 1 :    util::stream_format(stream, "ac%c",reg[op & 7]); break;
+						case 2 :    util::stream_format(stream, "su%c",reg[op & 7]); break;
+						case 3 :    util::stream_format(stream, "sb%c",reg[op & 7]); break;
+						case 4 :    util::stream_format(stream, "nd%c",reg[op & 7]); break;
+						case 5 :    util::stream_format(stream, "xr%c",reg[op & 7]); break;
+						case 6 :    util::stream_format(stream, "or%c",reg[op & 7]); break;
+						case 7 :    util::stream_format(stream, "cp%c",reg[op & 7]); break;
 					}
 					break;
 	}
 	return (pc - PC) | flags | DASMFLAG_SUPPORTED;
+}
+
+
+CPU_DISASSEMBLE(i8008)
+{
+	std::ostringstream stream;
+	offs_t result = internal_disasm_i8008(device, stream, pc, oprom, opram, options);
+	std::string stream_str = stream.str();
+	strcpy(buffer, stream_str.c_str());
+	return result;
 }
