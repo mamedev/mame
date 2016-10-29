705dde329e2f138bcfae02a7f97b931e03af888f
smf- <smf-@users.noreply.github.com>
2016-10-29 12:16:30 +0100

    removed msm5205 volume trampoline (nw)

--- src/mame/machine/pce_cd.cpp
+++ src/mame/machine/pce_cd.cpp
@@ -1043,14 +1043,14 @@ TIMER_CALLBACK_MEMBER(pce_cd_device::adpcm_fadeout_callback)
 	if (m_adpcm_volume <= 0)
 	{
 		m_adpcm_volume = 0.0;
-		m_msm->set_volume(0.0);
 		m_adpcm_fadeout_timer->adjust(attotime::never);
 	}
 	else
 	{
-		m_msm->set_volume(m_adpcm_volume);
 		m_adpcm_fadeout_timer->adjust(attotime::from_usec(param), param);
 	}
+
+	m_msm->set_output_gain(ALL_OUTPUTS, m_adpcm_volume / 100.0);
 }
 
 TIMER_CALLBACK_MEMBER(pce_cd_device::adpcm_fadein_callback)
@@ -1060,14 +1060,14 @@ TIMER_CALLBACK_MEMBER(pce_cd_device::adpcm_fadein_callback)
 	if (m_adpcm_volume >= 100.0)
 	{
 		m_adpcm_volume = 100.0;
-		m_msm->set_volume(100.0);
 		m_adpcm_fadein_timer->adjust(attotime::never);
 	}
 	else
 	{
-		m_msm->set_volume(m_adpcm_volume);
 		m_adpcm_fadein_timer->adjust(attotime::from_usec(param), param);
 	}
+
+	m_msm->set_output_gain(ALL_OUTPUTS, m_adpcm_volume / 100.0);
 }
 
 
