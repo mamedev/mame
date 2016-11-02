fbcb76f6e78092ece7ea6acb5105ad32d8c419ec
smf- <smf-@users.noreply.github.com>
2016-11-02 17:07:42 +0000

    fix for visual studio debug builds asserting on &[0] when size is zero (nw)

--- src/devices/bus/msx_cart/cartridge.cpp
+++ src/devices/bus/msx_cart/cartridge.cpp
@@ -78,25 +78,25 @@ msx_cart_interface::msx_cart_interface(const machine_config &mconfig, device_t &
 void msx_cart_interface::rom_alloc(uint32_t size)
 {
 	m_rom.resize(size);
-	memset(&m_rom[0], 0xff, size);
+	std::fill_n(m_rom.begin(), size, 0xff);
 }
 
 void msx_cart_interface::rom_vlm5030_alloc(uint32_t size)
 {
 	m_rom_vlm5030.resize(size);
-	memset(&m_rom_vlm5030[0], 0xff, size);
+	std::fill_n(m_rom_vlm5030.begin(), size, 0xff);
 }
 
 void msx_cart_interface::ram_alloc(uint32_t size)
 {
 	m_ram.resize(size);
-	memset(&m_ram[0], 0x00, size);
+	std::fill_n(m_ram.begin(), size, 0x00);
 }
 
 void msx_cart_interface::sram_alloc(uint32_t size)
 {
 	m_sram.resize(size);
-	memset(&m_sram[0], 0x00, size);
+	std::fill_n(m_sram.begin(), size, 0x00);
 }
 
 
