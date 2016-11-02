fb9f1157cfb2fffe1b1dbdbc357e6d5acad6b442
angelosa <salese_corp_ltd@email.it>
2016-11-02 01:10:19 +0100

    darkmist.cpp: added actual transparent pen mixing from proms, fixes ranking screen and gameplay area in Dark Mist [Angelo Salese]

--- src/mame/drivers/darkmist.cpp
+++ src/mame/drivers/darkmist.cpp
@@ -59,7 +59,7 @@ static ADDRESS_MAP_START( memmap, AS_PROGRAM, 8, darkmist_state )
 	AM_RANGE(0xd681, 0xd681) AM_DEVREAD("t5182", t5182_device, sharedram_semaphore_snd_r)
 	AM_RANGE(0xd682, 0xd682) AM_DEVWRITE("t5182", t5182_device, sharedram_semaphore_main_acquire_w)
 	AM_RANGE(0xd683, 0xd683) AM_DEVWRITE("t5182", t5182_device, sharedram_semaphore_main_release_w)
-	AM_RANGE(0xd800, 0xdfff) AM_RAM AM_SHARE("videoram")
+	AM_RANGE(0xd800, 0xdfff) AM_RAM_WRITE(tx_vram_w) AM_SHARE("videoram")
 	AM_RANGE(0xe000, 0xefff) AM_RAM AM_SHARE("workram")
 	AM_RANGE(0xf000, 0xffff) AM_RAM AM_SHARE("spriteram")
 ADDRESS_MAP_END
@@ -214,10 +214,10 @@ static const gfx_layout tilelayout =
 
 
 static GFXDECODE_START( darkmist )
-	GFXDECODE_ENTRY( "tx_gfx", 0, charlayout,  0, 16*4 )
-	GFXDECODE_ENTRY( "bg_gfx", 0, tilelayout,  0, 16*4 )
-	GFXDECODE_ENTRY( "fg_gfx", 0, tilelayout,  0, 16*4 )
-	GFXDECODE_ENTRY( "spr_gfx", 0, tilelayout,  0, 16*4 )
+	GFXDECODE_ENTRY( "tx_gfx", 0, charlayout,  0x300, 16 )
+	GFXDECODE_ENTRY( "bg_gfx", 0, tilelayout,  0x000, 16 )
+	GFXDECODE_ENTRY( "fg_gfx", 0, tilelayout,  0x100, 16 )
+	GFXDECODE_ENTRY( "spr_gfx", 0, tilelayout, 0x200, 16 )
 GFXDECODE_END
 
 TIMER_DEVICE_CALLBACK_MEMBER(darkmist_state::scanline)
@@ -241,7 +241,6 @@ static MACHINE_CONFIG_START( darkmist, darkmist_state )
 	MCFG_TIMER_DRIVER_ADD_SCANLINE("scantimer", darkmist_state, scanline, "screen", 0, 1)
 
 	MCFG_DEVICE_ADD("t5182", T5182, 0)
-
 
 	/* video hardware */
 	MCFG_SCREEN_ADD("screen", RASTER)
--- src/mame/includes/darkmist.h
+++ src/mame/includes/darkmist.h
@@ -9,6 +9,7 @@ public:
 		: driver_device(mconfig, type, tag),
 		m_maincpu(*this, "maincpu"),
 		m_t5182(*this, "t5182"),
+		m_screen(*this, "screen"),
 		m_gfxdecode(*this, "gfxdecode"),
 		m_palette(*this, "palette"),
 		m_spritebank(*this, "spritebank"),
@@ -16,10 +17,15 @@ public:
 		m_videoram(*this, "videoram"),
 		m_workram(*this, "workram"),
 		m_spriteram(*this, "spriteram"),
+		m_bg_clut(*this, "bg_clut"),
+		m_fg_clut(*this, "fg_clut"),
+		m_spr_clut(*this, "spr_clut"),
+		m_tx_clut(*this, "tx_clut"),
 		m_decrypted_opcodes(*this, "decrypted_opcodes") { }
 
 	required_device<cpu_device> m_maincpu;
 	required_device<t5182_device> m_t5182;
+	required_device<screen_device> m_screen;
 	required_device<gfxdecode_device> m_gfxdecode;
 	required_device<palette_device> m_palette;
 
@@ -28,6 +34,10 @@ public:
 	required_shared_ptr<uint8_t> m_videoram;
 	required_shared_ptr<uint8_t> m_workram;
 	required_shared_ptr<uint8_t> m_spriteram;
+	required_region_ptr<uint8_t> m_bg_clut;
+	required_region_ptr<uint8_t> m_fg_clut;
+	required_region_ptr<uint8_t> m_spr_clut;
+	required_region_ptr<uint8_t> m_tx_clut;
 	optional_shared_ptr<uint8_t> m_decrypted_opcodes;
 
 	int m_hw;
@@ -36,7 +46,8 @@ public:
 	tilemap_t *m_txtilemap;
 
 	DECLARE_WRITE8_MEMBER(hw_w);
-
+	DECLARE_WRITE8_MEMBER(tx_vram_w);
+	
 	TILE_GET_INFO_MEMBER(get_bgtile_info);
 	TILE_GET_INFO_MEMBER(get_fgtile_info);
 	TILE_GET_INFO_MEMBER(get_txttile_info);
@@ -46,7 +57,10 @@ public:
 	virtual void video_start() override;
 	DECLARE_PALETTE_INIT(darkmist);
 
+	bitmap_ind16 m_temp_bitmap;
+	void draw_sprites(bitmap_ind16 &bitmap, const rectangle &cliprect);
 	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	void mix_layer(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, uint8_t* clut);
 	void decrypt_fgbgtiles(uint8_t* rgn, int size);
 	void decrypt_gfx();
 	void decrypt_snd();
