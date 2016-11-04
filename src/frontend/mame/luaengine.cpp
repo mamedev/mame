421b5e26386e9d0e18ccf5b19e09c1a0831076e3
Miodrag Milanovic <mmicko@gmail.com>
2016-11-04 15:22:32 +0100

    Added lua-linenoise by Carls request (nw)

--- src/frontend/mame/luaengine.cpp
+++ src/frontend/mame/luaengine.cpp
@@ -33,6 +33,7 @@
 extern "C" {
 	int luaopen_zlib(lua_State *L);
 	int luaopen_lfs(lua_State *L);
+	int luaopen_linenoise(lua_State *L);
 }
 
 namespace sol
@@ -523,6 +524,7 @@ lua_engine::lua_engine()
 	// Get package.preload so we can store builtins in it.
 	sol()["package"]["preload"]["zlib"] = &luaopen_zlib;
 	sol()["package"]["preload"]["lfs"] = &luaopen_lfs;
+	sol()["package"]["preload"]["linenoise"] = &luaopen_linenoise;
 
 	lua_gc(m_lua_state, LUA_GCRESTART, 0);
 }
