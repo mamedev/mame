8bc4fe62488e472f2e20cdc4a1661fd814b2b56b
Not6 <r-type@gmail.com>
2016-11-19 15:58:20 +0100

    OSD retro use old smallui as default font

--- scripts/src/emu.lua
+++ scripts/src/emu.lua
@@ -250,11 +250,17 @@ dependency {
 	{ MAME_DIR .. "src/emu/video.cpp",   GEN_DIR .. "emu/layout/snap.lh" },
 
 }
-
+-- RETRO HACK : use old smallui ui.bdc
+if _OPTIONS["osd"]=="retro" then
+custombuildtask {
+	{ MAME_DIR .. "src/osd/libretro/libretro-internal/ui.bdc", GEN_DIR .. "emu/uismall.fh",     {  MAME_DIR .. "scripts/build/file2str.py" }, {"@echo Converting ui.bdc...", PYTHON .. " $(1) $(<) $(@) font_uismall uint8_t" }},
+}
+else
 custombuildtask {
 	{ MAME_DIR .. "scripts/font/NotoSans-Bold.bdc", GEN_DIR .. "emu/uismall.fh",     {  MAME_DIR .. "scripts/build/file2str.py" }, {"@echo Converting NotoSans-Bold.bdc...", PYTHON .. " $(1) $(<) $(@) font_uismall uint8_t" }},
 }
-
+end
+-- RETRO HACK
 custombuildtask {
 	{ MAME_DIR .. "src/frontend/mame/ui/uicmd14.png"        , GEN_DIR .. "emu/ui/uicmd14.fh",  {  MAME_DIR.. "scripts/build/png2bdc.py",  MAME_DIR .. "scripts/build/file2str.py" }, {"@echo Converting uicmd14.png...", PYTHON .. " $(1) $(<) temp_cmd.bdc", PYTHON .. " $(2) temp_cmd.bdc $(@) font_uicmd14 uint8_t" }},
 
