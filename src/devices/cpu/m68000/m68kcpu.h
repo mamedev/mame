cc979fbbf3be702bd085a02f7d4a5f6f6378f1dd
arbee <rb6502@users.noreply.github.com>
2016-11-26 22:01:01 -0500

    m68k: respect 68020 "freeze I-cache" bit; micro20 68020 cache test now passes [R. Belmont]

--- src/devices/cpu/m68000/m68kcpu.h
+++ src/devices/cpu/m68000/m68kcpu.h
@@ -689,9 +689,15 @@ static inline uint32_t m68ki_ic_readimm16(m68000_base_device *m68k, uint32_t add
 			// do a cache fill if the line is invalid or the tags don't match
 			if ((!m68k->ic_valid[idx]) || (m68k->ic_address[idx] != tag))
 			{
+				// if the cache is frozen, don't update it
+				if (m68k->cacr & M68K_CACR_FI)
+				{
+					return m68k->readimm16(address);
+				}
+
 				uint32_t data = m68k->read32(address & ~3);
 
-//              printf("m68k: doing cache fill at %08x (tag %08x idx %d)\n", address, tag, idx);
+	            //printf("m68k: doing cache fill at %08x (tag %08x idx %d)\n", address, tag, idx);
 
 				// if no buserror occurred, validate the tag
 				if (!m68k->mmu_tmp_buserror_occurred)
