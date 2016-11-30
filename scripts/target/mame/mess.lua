3812a7c5862e5894b5ed38be4a89a9742933fb5e
Joakim Larsson Edstrom <joakimlarsson42@gmail.com>
2016-11-30 17:31:54 +0100

    WIP: Apple Laser Writer II NT skeleton driver added [LN, Edstrom]

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -1383,6 +1383,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/lisa.cpp",
 	MAME_DIR .. "src/mame/includes/lisa.h",
 	MAME_DIR .. "src/mame/machine/lisa.cpp",
+	MAME_DIR .. "src/mame/drivers/lwriter.cpp",
 	MAME_DIR .. "src/mame/drivers/mac128.cpp",
 	MAME_DIR .. "src/mame/drivers/mac.cpp",
 	MAME_DIR .. "src/mame/includes/mac.h",
