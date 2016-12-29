2be714ea8c275c1797483459739512a6a06b97bf
Maxim Poliakovski <maximumspatium@googlemail.com>
2016-12-29 16:42:45 +0100

    ppcdrc.cpp: Clean up indentation of the previous commit.

--- src/devices/cpu/powerpc/ppcdrc.cpp
+++ src/devices/cpu/powerpc/ppcdrc.cpp
@@ -1944,9 +1944,9 @@ void ppc_device::generate_branch(drcuml_block *block, compiler_state *compiler, 
 	{
 		generate_update_cycles(block, &compiler_temp, mem(srcptr), true);    // <subtract cycles>
 
-        /* clear two LSBs of the target address to prevent branching to an invalid address */
-        UML_AND(block, I0, mem(srcptr), 0xFFFFFFFC);                         // and i0, 0xFFFFFFFC
-        UML_HASHJMP(block, m_core->mode, I0, *m_nocode);                     // hashjmp <mode>,i0,nocode
+		/* clear two LSBs of the target address to prevent branching to an invalid address */
+		UML_AND(block, I0, mem(srcptr), 0xFFFFFFFC);      // and i0, 0xFFFFFFFC
+		UML_HASHJMP(block, m_core->mode, I0, *m_nocode);  // hashjmp <mode>,i0,nocode
 	}
 
 	/* update the label */
