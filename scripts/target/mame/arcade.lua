d2877791fdd21560dfe490b3c04517c4018fd723
David Haywood <mamehaze@users.noreply.github.com>
2017-01-04 00:29:13 +0000

    new NOT WORKING - Happy Fish (V2 PCB, 302-in-1) [The Dumping Union]
    we've been sitting on this for a while, progress was shown a few years ago, but is apparently lost.  Smitdogg should be able to clarify the exact part numbers, they're not readable in the pictures.

--- scripts/target/mame/arcade.lua
+++ scripts/target/mame/arcade.lua
@@ -4450,6 +4450,7 @@ files {
 	MAME_DIR .. "src/mame/includes/gumbo.h",
 	MAME_DIR .. "src/mame/video/gumbo.cpp",
 	MAME_DIR .. "src/mame/drivers/gunpey.cpp",
+	MAME_DIR .. "src/mame/drivers/hapyfish.cpp",
 	MAME_DIR .. "src/mame/drivers/hideseek.cpp",
 	MAME_DIR .. "src/mame/drivers/hazelgr.cpp",
 	MAME_DIR .. "src/mame/drivers/headonb.cpp",
