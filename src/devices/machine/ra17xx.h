f5bcff30f6c5a890b6a2e481cb8435d692439577
AJR <ajrhacker@users.noreply.github.com>
2017-01-06 22:11:19 -0500

    PPS-4 refinements and modernization (nw) - Split PPS-4 and PPS-4/2 device types; better explain and properly implement the difference - Use callbacks for discrete input/output ports instead of fake memory addresses - A17xx reads the address bus directly during I/O accesses - Misc. chip documentation

--- src/devices/machine/ra17xx.h
+++ src/devices/machine/ra17xx.h
@@ -16,6 +16,7 @@
 #define __RA17XX_H__
 
 #include "device.h"
+#include "cpu/pps4/pps4.h"
 
 /*************************************
  *
@@ -29,6 +30,10 @@
 /* Set the write line handler */
 #define MCFG_RA17XX_WRITE(_devcb) \
 	ra17xx_device::set_iowr(*device, DEVCB_##_devcb);
+
+#define MCFG_RA17XX_CPU(_tag) \
+	ra17xx_device::set_cpu_tag(*device, "^" _tag);
+
 class ra17xx_device : public device_t
 {
 public:
@@ -40,6 +45,8 @@ public:
 
 	template<class _Object> static devcb_base &set_iord(device_t &device, _Object object) { return downcast<ra17xx_device &>(device).m_iord.set_callback(object); }
 	template<class _Object> static devcb_base &set_iowr(device_t &device, _Object object) { return downcast<ra17xx_device &>(device).m_iowr.set_callback(object); }
+	static void set_cpu_tag(device_t &device, const char *tag) { downcast<ra17xx_device &>(device).m_cpu.set_tag(tag); }
+
 protected:
 	// device-level overrides
 	virtual void device_start() override;
@@ -51,6 +58,7 @@ private:
 	bool            m_enable;     //!< true if outputs are enabled
 	devcb_read8     m_iord;       //!< input line (read, offset = line, data = 0/1)
 	devcb_write8    m_iowr;       //!< output line (write, offset = line, data = 0/1)
+	required_device<pps4_device> m_cpu;
 };
 
 extern const device_type RA17XX;
