74af888a3021f321f5c5af73d600ff5db2833335
Vas Crabb <vas@vastheman.com>
2017-01-10 15:12:21 +1100

    Clean up scripts from #1861 (nw): * use spaces for indentation * better error reporting * some optimisation * slightly more intelligent preservation of tabulation

--- src/tools/discrepancy-spotter.py
+++ src/tools/discrepancy-spotter.py
@@ -1,34 +1,51 @@
 #!/usr/bin/python
+##
+## license:BSD-3-Clause
+## copyright-holders:Zoe Blade
 
 # Find discrepancies in arcade ROM dump names, by Zoe Blade
-# For Python 2
+# For Python 2 and 3
 
+import sys
 import xml.etree.ElementTree
 
-print('Loading XML file...')
-root = xml.etree.ElementTree.parse('arcade.xml').getroot()
-print('Done.')
 
-for childMachine in root.iter('machine'):
-	if not childMachine.get('cloneof'):
-		continue
+def checkPair(parentMachine, childMachine):
+    for childRom in childMachine.iter('rom'):
+        for parentRom in parentMachine.iter('rom'):
+            if parentRom.get('sha1') == childRom.get('sha1'):
+                # ROM pair found
+                if parentRom.get('name') != childRom.get('name'):
+                    # The names don't match
+                    sys.stdout.write('%s %s: %s -> %s\n' % (childMachine.get('sourcefile'), childMachine.get('name'), childRom.get('name'), parentRom.get('name')))
+                else:
+                    break
 
-	for parentMachine in root.iter('machine'):
-		if not parentMachine.get('name') == childMachine.get('cloneof'):
-			continue
 
-		# Machine pair found
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        sys.stderr.write('Usage:\n%s [arcade.xml]\n' % sys.argv[0])
+        sys.exit(1)
 
-		for childRom in childMachine.iter('rom'):
-			for parentRom in parentMachine.iter('rom'):
-				if not parentRom.get('sha1') == childRom.get('sha1'):
-					continue
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = 'arcade.xml'
 
-				# ROM pair found
+    sys.stderr.write('Loading XML file...')
+    sys.stderr.flush()
+    try:
+        root = xml.etree.ElementTree.parse(filename).getroot()
+    except Exception as e:
+        sys.stderr.write('\n%s: error parsing %s: %s\n' % (sys.argv[0], filename, e))
+        sys.exit(2)
+    sys.stderr.write('done.\n')
 
-				if parentRom.get('name') == childRom.get('name'):
-					break
+    for childMachine in root.iter('machine'):
+        if childMachine.get('cloneof'):
+            for parentMachine in root.iter('machine'):
+                if parentMachine.get('name') == childMachine.get('cloneof'):
+                    # Machine pair found
+                    checkPair(parentMachine, childMachine)
 
-				# The names don't match
-
-				print(childMachine.get('sourcefile') + ' ' + childMachine.get('name') + ': ' + childRom.get('name') + ' -> ' + parentRom.get('name'))
+    sys.exit(0)
