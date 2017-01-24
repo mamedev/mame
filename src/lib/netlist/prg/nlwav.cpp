9db7b63e9a5fe3ea6712fab5b0f0dd864dfc06bc
Stiletto <stilett0@users.noreply.github.com>
2017-01-24 17:29:49 -0500

    Updates "2016" strings to "2017 where relevant.
    Updates "2016" strings to "2017 where relevant.

--- src/lib/netlist/prg/nlwav.cpp
+++ src/lib/netlist/prg/nlwav.cpp
@@ -256,7 +256,7 @@ int main(int argc, char *argv[])
 	{
 		pout(
 			"nlwav (netlist) 0.1\n"
-			"Copyright (C) 2016 Couriersud\n"
+			"Copyright (C) 2017 Couriersud\n"
 			"License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>.\n"
 			"This is free software: you are free to change and redistribute it.\n"
 			"There is NO WARRANTY, to the extent permitted by law.\n\n"
