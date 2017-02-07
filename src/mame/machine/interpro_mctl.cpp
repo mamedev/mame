5963478743503183711cad06fac5173a2b266824
Patrick Mackinlay <pmackinlay@hotmail.com>
2017-02-07 12:30:22 +0700

    improved mcga/fmcc skeleton

---
+++ src/mame/machine/interpro_mctl.cpp
@@ -0,0 +1,96 @@
+// license:BSD-3-Clause
+// copyright-holders:Patrick Mackinlay
+
+/*
+* An implementation of the MCGA device found on Intergraph InterPro family workstations. There is no
+* public documentation on this device, so the implementation is being built to follow the logic of the
+* system boot ROM and its diagnostic tests.
+*
+* Please be aware that code in here is not only broken, it's likely wrong in many cases.
+*
+* TODO
+*   - too long to list
+*/
+#include "interpro_mcga.h"
+
+#define VERBOSE 0
+#if VERBOSE
+#define LOG_MCGA(...) logerror(__VA_ARGS__)
+#else
+#define LOG_MCGA(...) {}
+#endif
+
+DEVICE_ADDRESS_MAP_START(map, 32, interpro_mcga_device)
+	AM_RANGE(0x00, 0x03) AM_READWRITE16(reg00_r, reg00_w, 0xffff)
+	AM_RANGE(0x08, 0x0b) AM_READWRITE16(control_r, control_w, 0xffff)
+	AM_RANGE(0x10, 0x13) AM_READWRITE16(error_r, error_w, 0xffff)
+	AM_RANGE(0x18, 0x1b) AM_READWRITE8(frcrd_r, frcrd_w, 0xff)
+	AM_RANGE(0x20, 0x23) AM_READWRITE8(cbsub_r, cbsub_w, 0xff)
+	AM_RANGE(0x28, 0x2b) AM_READWRITE16(reg28_r, reg28_w, 0xffff)
+	AM_RANGE(0x30, 0x33) AM_READWRITE16(reg30_r, reg30_w, 0xffff)
+	AM_RANGE(0x38, 0x3b) AM_READWRITE16(memsize_r, memsize_w, 0xffff)
+ADDRESS_MAP_END
+
+DEVICE_ADDRESS_MAP_START(map, 32, interpro_fmcc_device)
+	AM_RANGE(0x00, 0x03) AM_READWRITE16(reg00_r, reg00_w, 0xffff)
+	AM_RANGE(0x08, 0x0b) AM_READWRITE16(control_r, control_w, 0xffff)
+	AM_RANGE(0x10, 0x13) AM_READWRITE16(error_r, error_w, 0xffff)
+	AM_RANGE(0x18, 0x1b) AM_READWRITE8(frcrd_r, frcrd_w, 0xff)
+	AM_RANGE(0x20, 0x23) AM_READWRITE8(cbsub_r, cbsub_w, 0xff)
+	AM_RANGE(0x28, 0x2b) AM_READWRITE16(reg28_r, reg28_w, 0xffff)
+	AM_RANGE(0x30, 0x33) AM_READWRITE16(reg30_r, reg30_w, 0xffff)
+	AM_RANGE(0x38, 0x3b) AM_READWRITE16(memsize_r, memsize_w, 0xffff)
+	AM_RANGE(0x48, 0x4b) AM_READWRITE16(error_control_r, error_control_w, 0xffff)
+ADDRESS_MAP_END
+
+const device_type INTERPRO_MCGA = &device_creator<interpro_mcga_device>;
+const device_type INTERPRO_FMCC = &device_creator<interpro_fmcc_device>;
+
+interpro_mcga_device::interpro_mcga_device(const machine_config &mconfig, device_type type, const char *name, const char *tag, device_t *owner, uint32_t clock, const char *shortname, const char *source)
+	: device_t(mconfig, type, name, tag, owner, clock, shortname, source)
+{
+}
+
+interpro_mcga_device::interpro_mcga_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
+	: interpro_mcga_device(mconfig, INTERPRO_MCGA, "InterPro MCGA", tag, owner, clock, "mcga", __FILE__)
+{
+}
+
+interpro_fmcc_device::interpro_fmcc_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
+	: interpro_mcga_device(mconfig, INTERPRO_FMCC, "InterPro FMCC", tag, owner, clock, "fmcc", __FILE__)
+{
+}
+
+void interpro_mcga_device::device_start()
+{
+}
+
+void interpro_mcga_device::device_reset()
+{
+	m_reg[0] = 0x00ff;
+	m_control = MCGA_CTRL_ENREFRESH | MCGA_CTRL_CBITFRCSUB | MCGA_CTRL_CBITFRCRD;
+	m_reg[1] = 0x00ff;
+	m_memsize = 0x0340;
+}
+
+WRITE16_MEMBER(interpro_mcga_device::control_w)
+{
+	m_control = data & MCGA_CTRL_MASK;
+
+	// HACK: set or clear error status depending on ENMMBE bit
+	if (data & MCGA_CTRL_ENMMBE)
+		m_error |= MCGA_ERROR_VALID;
+	//		else
+	//			error &= ~MCGA_ERROR_VALID;
+}
+
+WRITE16_MEMBER(interpro_fmcc_device::control_w)
+{
+	m_control = data & FMCC_CTRL_MASK;
+
+	// HACK: set or clear error status depending on ENMMBE bit
+	if (data & MCGA_CTRL_ENMMBE)
+		m_error |= MCGA_ERROR_VALID;
+	//		else
+	//			error &= ~MCGA_ERROR_VALID;
+}
---
+++ src/mame/machine/interpro_mctl.h
@@ -0,0 +1,86 @@
+// license:BSD-3-Clause
+// copyright-holders:Patrick Mackinlay
+
+#pragma once
+
+#ifndef INTERPRO_MCGA_H_
+#define INTERPRO_MCGA_H_
+
+#include "emu.h"
+
+// mcga control register
+#define MCGA_CTRL_OPTMASK    0x0003
+#define MCGA_CTRL_CBITFRCRD  0x0004
+#define MCGA_CTRL_CBITFRCSUB 0x0008
+#define MCGA_CTRL_ENREFRESH  0x0010
+#define MCGA_CTRL_ENMSBE     0x0100
+#define MCGA_CTRL_ENMMBE     0x0200
+#define MCGA_CTRL_ENECC      0x0400
+#define MCGA_CTRL_WRPROT     0x8000
+
+// rom writes bit 0x80 to test if fmcc or mcga
+#define MCGA_CTRL_MASK       0x871f
+#define FMCC_CTRL_MASK       0x8fff
+
+// mcga error register
+#define MCGA_ERROR_VALID	0x00008000
+
+class interpro_mcga_device : public device_t
+{
+public:
+	interpro_mcga_device(const machine_config &mconfig, device_type type, const char *name, const char *tag, device_t *owner, uint32_t clock, const char *shortname, const char *source);
+	interpro_mcga_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+	virtual DECLARE_ADDRESS_MAP(map, 32);
+
+	DECLARE_READ16_MEMBER(reg00_r) { return m_reg[0]; }
+	DECLARE_WRITE16_MEMBER(reg00_w) { m_reg[0] = data; }
+	DECLARE_READ16_MEMBER(control_r) { return m_control; }
+	DECLARE_WRITE16_MEMBER(control_w);
+	DECLARE_READ16_MEMBER(error_r) { return m_error; }
+	DECLARE_WRITE16_MEMBER(error_w) { m_error = data; }
+	DECLARE_READ8_MEMBER(frcrd_r) { return m_frcrd; }
+	DECLARE_WRITE8_MEMBER(frcrd_w) { m_frcrd = data; }
+	DECLARE_READ8_MEMBER(cbsub_r) { return m_cbsub; }
+	DECLARE_WRITE8_MEMBER(cbsub_w) { m_cbsub = data; }
+	DECLARE_READ16_MEMBER(reg28_r) { return m_reg[1]; }
+	DECLARE_WRITE16_MEMBER(reg28_w) { m_reg[1] = data; }
+	DECLARE_READ16_MEMBER(reg30_r) { return m_reg[2]; }
+	DECLARE_WRITE16_MEMBER(reg30_w) { m_reg[2] = data; }
+	DECLARE_READ16_MEMBER(memsize_r) { return m_memsize; }
+	DECLARE_WRITE16_MEMBER(memsize_w) { m_memsize = data; }
+
+protected:
+	virtual void device_start() override;
+	virtual void device_reset() override;
+
+	u16 m_control, m_error, m_memsize;
+	u8 m_frcrd, m_cbsub;
+
+	u16 m_reg[3];
+
+private:
+
+};
+
+class interpro_fmcc_device : public interpro_mcga_device
+{
+public:
+	interpro_fmcc_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+	virtual DECLARE_ADDRESS_MAP(map, 32) override;
+
+	DECLARE_WRITE16_MEMBER(control_w) override;
+
+	DECLARE_READ16_MEMBER(error_control_r) { return m_error_control; }
+	DECLARE_WRITE16_MEMBER(error_control_w) { m_error_control = data; }
+
+private:
+	u16 m_error_control;
+};
+
+// device type definition
+extern const device_type INTERPRO_MCGA;
+extern const device_type INTERPRO_FMCC;
+
+#endif
