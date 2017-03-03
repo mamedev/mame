adfc7be3490d40e94e884536ebf994169429da37
AJR <ajrhacker@users.noreply.github.com>
2017-03-02 22:14:46 -0500

    Fix build (nw)

--- src/mame/drivers/popper.cpp
+++ src/mame/drivers/popper.cpp
@@ -42,6 +42,8 @@
 #include "cpu/z80/z80.h"
 #include "sound/ay8910.h"
 #include "video/resnet.h"
+#include "screen.h"
+#include "speaker.h"
 
 
 //**************************************************************************
