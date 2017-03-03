25983e83e53cf06855a119f9c958845eb9696f4a
AJR <ajrhacker@users.noreply.github.com>
2017-03-03 08:35:47 -0500

    Device validation fixes, part FDC (nw)

--- src/devices/machine/wozfdc.cpp
+++ src/devices/machine/wozfdc.cpp
@@ -55,7 +55,7 @@ diskii_fdc::diskii_fdc(const machine_config &mconfig, const char *tag, device_t 
 }
 
 appleiii_fdc::appleiii_fdc(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) :
-	wozfdc_device(mconfig, DISKII_FDC, "Apple III floppy controller", tag, owner, clock, "a3fdc", __FILE__)
+	wozfdc_device(mconfig, APPLEIII_FDC, "Apple III floppy controller", tag, owner, clock, "a3fdc", __FILE__)
 {
 }
 
