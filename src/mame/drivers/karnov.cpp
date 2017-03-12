1d027e1d22bea481f6ac12470457d418b790dbfb
Ivan Vangelista <mesgnet@yahoo.it>
2017-03-12 08:53:42 +0100

    New not working clone __________________ Penguin Brothers (Japan, bootleg) [Rod_Wod, Team Europe and The Dumping Union]
    This has quite different hardware: standard 68000 instead of TMP68301 and 4 FPGAs (3 A40MX04 and 1 A54SX16A) . Also, GFX regs/offsets and sound need work. (nw)

--- src/mame/drivers/karnov.cpp
+++ src/mame/drivers/karnov.cpp
@@ -849,6 +849,10 @@ static MACHINE_CONFIG_START( karnov, karnov_state )
 MACHINE_CONFIG_END
 
 static MACHINE_CONFIG_DERIVED( karnovjbl, karnov )
+	/* X-TALs: 
+	Top board next to #9 is 20.000 MHz
+	Top board next to the microcontroller is 6.000 MHz
+	Bottom board next to the ribbon cable is 12.000 MHz*/
 	MCFG_CPU_MODIFY("audiocpu")
 	MCFG_CPU_PROGRAM_MAP(karnovjbl_sound_map)
 
