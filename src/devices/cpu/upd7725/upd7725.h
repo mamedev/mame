065f5476f5328d39652ecaa3a307596f436a296b
Lord-Nightmare <Lord-Nightmare@users.noreply.github.com>
2017-03-17 19:07:01 -0400

    uPD7725: Corrected which registers are actually reset by /RESET, and fixed/updated IRQ system, should behave more properly now. [Lord Nightmare]

--- src/devices/cpu/upd7725/upd7725.h
+++ src/devices/cpu/upd7725/upd7725.h
@@ -191,8 +191,11 @@ private:
 	void exec_ld(uint32_t opcode);
 
 	int m_icount;
-	int m_irq; // old irq line state, for detecting rising edges.
-
+	bool m_irq; // old irq line state, for detecting rising edges.
+	// m_irq_firing: if an irq has fired; 0 = not fired or has already finished firing
+	// 1 = next opcode is the first half of int firing 'NOP+push pc'
+	// 2 = next opcode is the second half of int firing 'JMP 0100'
+	int m_irq_firing; 
 	address_space *m_program, *m_data;
 	direct_read_data *m_direct;
 
