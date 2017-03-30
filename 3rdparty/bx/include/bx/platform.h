ced308774203d9ff121b58e56ba28908ce9d6b8b
Vas Crabb <vas@vastheman.com>
2017-03-30 16:29:33 +1100

    assume LLVM libc++ if __llvm__ is defined but __GLIBC__ is undefined

--- 3rdparty/bx/include/bx/platform.h
+++ 3rdparty/bx/include/bx/platform.h
@@ -239,7 +239,7 @@
 #	elif defined(__MINGW32__) || defined(__MINGW64__)
 #		undef  BX_CRT_MINGW
 #		define BX_CRT_MINGW 1
-#	elif defined(__apple_build_version__) || defined(__ORBIS__) || defined(__EMSCRIPTEN__)
+#	elif defined(__apple_build_version__) || defined(__ORBIS__) || defined(__EMSCRIPTEN__) || defined(__llvm__)
 #		undef  BX_CRT_LIBCXX
 #		define BX_CRT_LIBCXX 1
 #	endif //
