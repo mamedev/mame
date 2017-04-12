7f9b1a42e51e2869eed34e425306c605dc225071
Robbbert <Robbbert@users.noreply.github.com>
2017-04-12 17:21:38 +1000

    (nw) Hopefully fixed arcade-only build.

--- scripts/target/mame/arcade.lua
+++ scripts/target/mame/arcade.lua
@@ -281,7 +281,7 @@ VIDEOS["CESBLIT"] = true
 --VIDEOS["CRT9007"] = true
 --VIDEOS["CRT9021"] = true
 --VIDEOS["CRT9212"] = true
---VIDEOS["CRTC_EGA"] = true
+VIDEOS["CRTC_EGA"] = true
 --VIDEOS["DL1416"] = true
 VIDEOS["DM9368"] = true
 --VIDEOS["EF9340_1"] = true
@@ -336,7 +336,7 @@ VIDEOS["TMS34061"] = true
 VIDEOS["TMS9927"] = true
 VIDEOS["TMS9928A"] = true
 --VIDEOS["UPD3301"] = true
---VIDEOS["UPD7220"] = true
+VIDEOS["UPD7220"] = true
 --VIDEOS["UPD7227"] = true
 VIDEOS["V9938"] = true
 --VIDEOS["VIC4567"] = true
@@ -387,7 +387,7 @@ MACHINES["AICARTC"] = true
 MACHINES["AM53CF96"] = true
 MACHINES["AM9517A"] = true
 MACHINES["AMIGAFDC"] = true
---MACHINES["AT_KEYBC"] = true
+MACHINES["AT_KEYBC"] = true
 MACHINES["AT28C16"] = true
 MACHINES["AT29X"] = true
 MACHINES["AT45DBXX"] = true
@@ -405,7 +405,7 @@ MACHINES["CR589"] = true
 --MACHINES["CS8221"] = true
 MACHINES["CXD1095"] = true
 --MACHINES["DM9334"] = true
---MACHINES["DP8390"] = true
+MACHINES["DP8390"] = true
 MACHINES["DS1204"] = true
 MACHINES["DS1302"] = true
 --MACHINES["DS1315"] = true
@@ -495,7 +495,7 @@ MACHINES["MOS6551"] = true
 --MACHINES["MOS8706"] = true
 --MACHINES["MOS8722"] = true
 --MACHINES["MOS8726"] = true
---MACHINES["MPU401"] = true
+MACHINES["MPU401"] = true
 MACHINES["MSM5832"] = true
 MACHINES["MSM58321"] = true
 MACHINES["MSM6242"] = true
@@ -508,8 +508,8 @@ MACHINES["NETLIST"] = true
 MACHINES["NMC9306"] = true
 --MACHINES["NSC810"] = true
 MACHINES["NSCSI"] = true
---MACHINES["PC_FDC"] = true
---MACHINES["PC_LPT"] = true
+MACHINES["PC_FDC"] = true
+MACHINES["PC_LPT"] = true
 --MACHINES["PCCARD"] = true
 MACHINES["PCF8593"] = true
 MACHINES["PCI"] = true
@@ -705,6 +705,10 @@ FORMATS["GUAB_DSK"] = true
 FORMATS["AMI_DSK"] = true
 FORMATS["SC3000_BIT"] = true
 FORMATS["WD177X_DSK"] = true
+FORMATS["NASLITE_DSK"] = true
+FORMATS["UPD765_DSK"] = true
+FORMATS["PC_DSK"] = true
+FORMATS["BASICDSK"] = true
 
 --------------------------------------------------
 -- this is the list of driver libraries that
@@ -2507,6 +2511,8 @@ files {
 	MAME_DIR .. "src/mame/drivers/merit.cpp",
 	MAME_DIR .. "src/mame/drivers/meritm.cpp",
 	MAME_DIR .. "src/mame/drivers/mtouchxl.cpp",
+	MAME_DIR .. "src/mame/machine/at.h",
+	MAME_DIR .. "src/mame/machine/at.cpp",
 }
 
 createMAMEProjects(_target, _subtarget, "metro")
