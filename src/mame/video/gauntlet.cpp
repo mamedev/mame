18bf62d621f5be946cdfca98d5a4ca9344d5e1c2
AJR <ajrhacker@users.noreply.github.com>
2017-04-11 23:40:30 -0400

    Move "optional" basemem and extmem arrays out of tilemap_t (nw)
    This is intended as a first step towards divorcing tilemap_t and tilemap_device.

--- src/mame/video/gauntlet.cpp
+++ src/mame/video/gauntlet.cpp
@@ -21,7 +21,7 @@
 
 TILE_GET_INFO_MEMBER(gauntlet_state::get_alpha_tile_info)
 {
-	uint16_t data = tilemap.basemem_read(tile_index);
+	uint16_t data = m_alpha_tilemap->basemem_read(tile_index);
 	int code = data & 0x3ff;
 	int color = ((data >> 10) & 0x0f) | ((data >> 9) & 0x20);
 	int opaque = data & 0x8000;
@@ -31,7 +31,7 @@ TILE_GET_INFO_MEMBER(gauntlet_state::get_alpha_tile_info)
 
 TILE_GET_INFO_MEMBER(gauntlet_state::get_playfield_tile_info)
 {
-	uint16_t data = tilemap.basemem_read(tile_index);
+	uint16_t data = m_playfield_tilemap->basemem_read(tile_index);
 	int code = ((m_playfield_tile_bank * 0x1000) + (data & 0xfff)) ^ 0x800;
 	int color = 0x10 + (m_playfield_color_bank * 8) + ((data >> 12) & 7);
 	SET_TILE_INFO_MEMBER(0, code, color, (data >> 15) & 1);
