48eb8248ab5f75396463c33aada77d24cab9b01c
Scott Stone <tafoid@gmail.com>
2017-04-20 13:53:09 -0400

    Turn off global logging (nw)

--- src/devices/machine/6522via.cpp
+++ src/devices/machine/6522via.cpp
@@ -39,8 +39,8 @@
 #define LOG_READ    (1U <<  3)
 #define LOG_INT     (1U <<  4)
 
-#define VERBOSE (LOG_SHIFT)
-#define LOG_OUTPUT_FUNC printf
+//#define VERBOSE (LOG_SHIFT)
+//#define LOG_OUTPUT_FUNC printf
 
 #include "logmacro.h"
 
