3fc3f8254ec04dc5afcc5b7ce221c41f79db88d2
Happy <Happy-yappH@users.noreply.github.com>
2017-04-25 23:36:30 -0600

    N64, RSP, drc : Wrap PC to 12 bits in a few more places.

--- src/devices/cpu/rsp/rspdrc.cpp
+++ src/devices/cpu/rsp/rspdrc.cpp
@@ -671,7 +671,7 @@ void rsp_device::generate_checksum_block(drcuml_block *block, compiler_state *co
 
 			if (seqhead->delay.first() != nullptr && seqhead->physpc != seqhead->delay.first()->physpc)
 			{
-				base = m_direct->read_ptr(seqhead->delay.first()->physpc | 0x1000);
+				base = m_direct->read_ptr((seqhead->delay.first()->physpc & 0x00000fff) | 0x1000);
 				assert(base != nullptr);
 				UML_LOAD(block, I1, base, 0, SIZE_DWORD, SCALE_x4);                 // load    i1,base,dword
 				UML_ADD(block, I0, I0, I1);                     // add     i0,i0,i1
@@ -702,7 +702,7 @@ void rsp_device::generate_checksum_block(drcuml_block *block, compiler_state *co
 
 				if (curdesc->delay.first() != nullptr && (curdesc == seqlast || (curdesc->next() != nullptr && curdesc->next()->physpc != curdesc->delay.first()->physpc)))
 				{
-					base = m_direct->read_ptr(curdesc->delay.first()->physpc | 0x1000);
+					base = m_direct->read_ptr((curdesc->delay.first()->physpc & 0x00000fff) | 0x1000);
 					assert(base != nullptr);
 					UML_LOAD(block, I1, base, 0, SIZE_DWORD, SCALE_x4);                 // load    i1,base,dword
 					UML_ADD(block, I0, I0, I1);                     // add     i0,i0,i1
@@ -813,7 +813,7 @@ void rsp_device::generate_delay_slot_and_branch(drcuml_block *block, compiler_st
 	/* set the link if needed -- before the delay slot */
 	if (linkreg != 0)
 	{
-		UML_MOV(block, R32(linkreg), (int32_t)(desc->pc + 8));                    // mov    <linkreg>,desc->pc + 8
+		UML_MOV(block, R32(linkreg), (int32_t)((desc->pc + 8) & 0x0000fff));                    // mov    <linkreg>,desc->pc + 8
 	}
 
 	/* compile the delay slot using temporary compiler state */
