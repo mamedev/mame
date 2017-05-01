cbca538ae90e1c9ec5f6eefecf859cebb3a50bfd
Dirk Best <mail@dirk-best.de>
2017-05-01 13:05:33 +0200

    chinsan: Merge mayumi driver
    * Interrupt enable and flip screen hooked up for chinsan
    * NVRAM support for chinsan

--- src/mame/drivers/chinsan.cpp
+++ src/mame/drivers/chinsan.cpp
@@ -1,10 +1,12 @@
 // license: BSD-3-Clause
-// copyright-holders: David Haywood, Dirk Best
+// copyright-holders: David Haywood, Uki, Dirk Best
 /***************************************************************************
 
 	Ganbare Chinsan Ooshoubu (がんばれ珍さん！大勝負)
+	© 1987 Sanritsu
 
-    © 1987 Sanritsu
+	Kiki-Ippatsu Mayumi-chan (危機一髪真由美ちゃん)
+	© 1988 Victory L.L.C. (developed by Sanritsu)
 
     TODO:
     - Figure out the rest of the dip switches
@@ -20,6 +22,7 @@
 #include "emu.h"
 #include "cpu/z80/z80.h"
 #include "machine/mc8123.h"
+#include "machine/nvram.h"
 #include "machine/i8255.h"
 #include "sound/2203intf.h"
 #include "sound/msm5205.h"
@@ -42,7 +45,9 @@ public:
 		m_adpcm(*this, "adpcm"),
 		m_inputs_p1{ {*this, "p1_0"}, {*this, "p1_1"}, {*this, "p1_2"}, {*this, "p1_3"}, {*this, "p1_4"} },
 		m_inputs_p2{ {*this, "p2_0"}, {*this, "p2_1"}, {*this, "p2_2"}, {*this, "p2_3"}, {*this, "p2_4"} },
+		m_bank1(*this, "bank1"), m_bank0d(*this, "bank0d"), m_bank1d(*this, "bank1d"),
 		m_tilemap(nullptr),
+		m_int_enabled(false),
 		m_port_select(0xff),
 		m_adpcm_pos(0), m_adpcm_idle(1), m_adpcm_data(0), m_trigger(0)
 	{ }
@@ -51,13 +56,13 @@ public:
 	DECLARE_READ8_MEMBER(input_p2_r);
 	DECLARE_READ8_MEMBER(input_p1_r);
 
-	DECLARE_PALETTE_INIT(chinsan);
 	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 	TILE_GET_INFO_MEMBER(tile_info);
 
 	DECLARE_WRITE8_MEMBER(adpcm_w);
 	DECLARE_WRITE_LINE_MEMBER(adpcm_int_w);
 
+	INTERRUPT_GEN_MEMBER(vblank_int);
 	DECLARE_WRITE8_MEMBER(ctrl_w);
 	DECLARE_DRIVER_INIT(chinsan);
 
@@ -66,18 +71,22 @@ protected:
 	virtual void machine_reset() override;
 
 private:
-	required_device<mc8123_device> m_maincpu;
+	required_device<z80_device> m_maincpu;
 	required_shared_ptr<uint8_t> m_video_ram;
 	required_shared_ptr<uint8_t> m_color_ram;
 	required_device<gfxdecode_device> m_gfxdecode;
-	required_device<msm5205_device> m_adpcm;
+	optional_device<msm5205_device> m_adpcm;
 	required_ioport m_inputs_p1[5];
 	required_ioport m_inputs_p2[5];
+	required_memory_bank m_bank1;
+	optional_memory_bank m_bank0d;
+	optional_memory_bank m_bank1d;
 
 	std::unique_ptr<uint8_t[]> m_decrypted_opcodes;
 
 	tilemap_t *m_tilemap;
 
+	bool m_int_enabled;
 	uint8_t m_port_select;
 	uint32_t m_adpcm_pos;
 	uint8_t m_adpcm_idle;
@@ -93,7 +102,7 @@ private:
 static ADDRESS_MAP_START( chinsan_map, AS_PROGRAM, 8, chinsan_state )
 	AM_RANGE(0x0000, 0x7fff) AM_ROM
 	AM_RANGE(0x8000, 0xbfff) AM_ROMBANK("bank1")
-	AM_RANGE(0xc000, 0xdfff) AM_RAM
+	AM_RANGE(0xc000, 0xdfff) AM_RAM AM_SHARE("nvram")
 	AM_RANGE(0xe000, 0xefff) AM_RAM AM_SHARE("video_ram")
 	AM_RANGE(0xf000, 0xf7ff) AM_RAM AM_SHARE("color_ram")
 ADDRESS_MAP_END
@@ -103,12 +112,19 @@ static ADDRESS_MAP_START( decrypted_opcodes_map, AS_DECRYPTED_OPCODES, 8, chinsa
 	AM_RANGE(0x8000, 0xbfff) AM_ROMBANK("bank1d")
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START( chinsan_io, AS_IO, 8, chinsan_state )
+static ADDRESS_MAP_START( chinsan_io_map, AS_IO, 8, chinsan_state )
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
 	AM_RANGE(0x00, 0x03) AM_DEVREADWRITE("ppi", i8255_device, read, write)
 	AM_RANGE(0x10, 0x11) AM_DEVREADWRITE("ymsnd", ym2203_device, read, write)
 	AM_RANGE(0x20, 0x20) AM_WRITE(adpcm_w)
 	AM_RANGE(0x30, 0x30) AM_WRITE(ctrl_w)
+ADDRESS_MAP_END
+
+static ADDRESS_MAP_START( mayumi_io_map, AS_IO, 8, chinsan_state )
+	ADDRESS_MAP_GLOBAL_MASK(0xff)
+	AM_RANGE(0x30, 0x30) AM_READ_PORT("extra") AM_WRITE(ctrl_w)
+	AM_RANGE(0xc0, 0xc3) AM_DEVREADWRITE("ppi", i8255_device, read, write)
+	AM_RANGE(0xd0, 0xd1) AM_DEVREADWRITE("ymsnd", ym2203_device, read, write)
 ADDRESS_MAP_END
 
 
@@ -133,7 +149,9 @@ static INPUT_PORTS_START( chinsan )
 	PORT_DIPSETTING(   0x00, DEF_STR( On ))
 	PORT_DIPSETTING(   0x20, DEF_STR( Off ))
 	PORT_DIPUNKNOWN_DIPLOC(0x40, 0x40, "SW-1:7")
-	PORT_DIPUNKNOWN_DIPLOC(0x80, 0x80, "SW-1:8")
+	PORT_DIPNAME(0x80, 0x80, DEF_STR( Flip_Screen )) PORT_DIPLOCATION("SW-1:8")
+	PORT_DIPSETTING(   0x00, DEF_STR( On ))
+	PORT_DIPSETTING(   0x80, DEF_STR( Off ))
 
 	PORT_START("DSW2")
 	PORT_DIPUNKNOWN_DIPLOC(0x01, 0x01, "SW-2:1")
@@ -246,6 +264,53 @@ static INPUT_PORTS_START( chinsan )
 	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_SERVICE )
 INPUT_PORTS_END
 
+static INPUT_PORTS_START( mayumi )
+	PORT_INCLUDE(chinsan)
+
+	PORT_MODIFY("DSW1")
+	PORT_DIPUNKNOWN_DIPLOC(0x01, 0x01, "SW-1:8")
+	PORT_DIPUNKNOWN_DIPLOC(0x02, 0x02, "SW-1:7")
+	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Service_Mode )) PORT_DIPLOCATION("SW-1:6")
+	PORT_DIPSETTING(    0x04, DEF_STR( Off ))
+	PORT_DIPSETTING(    0x00, DEF_STR( On ))
+	PORT_DIPNAME( 0x18, 0x00, DEF_STR( Coinage ))      PORT_DIPLOCATION("SW-1:5,4")
+	PORT_DIPSETTING(    0x00, DEF_STR( 1C_1C ))
+	PORT_DIPSETTING(    0x08, DEF_STR( 1C_2C ))
+	PORT_DIPSETTING(    0x10, DEF_STR( 1C_3C ))
+	PORT_DIPSETTING(    0x18, DEF_STR( 2C_1C ))
+	PORT_DIPUNKNOWN_DIPLOC(0x20, 0x20, "SW-1:3")
+	PORT_DIPUNKNOWN_DIPLOC(0x40, 0x40, "SW-1:2")
+	PORT_DIPNAME(0x80, 0x80, DEF_STR( Flip_Screen ))   PORT_DIPLOCATION("SW-1:1")
+	PORT_DIPSETTING(   0x00, DEF_STR( On ))
+	PORT_DIPSETTING(   0x80, DEF_STR( Off ))
+
+	PORT_MODIFY("DSW2")
+	PORT_DIPUNKNOWN_DIPLOC(0x01, 0x01, "SW-2:8")
+	PORT_DIPUNKNOWN_DIPLOC(0x02, 0x02, "SW-2:7")
+	PORT_DIPUNKNOWN_DIPLOC(0x04, 0x04, "SW-2:6")
+	PORT_DIPUNKNOWN_DIPLOC(0x08, 0x08, "SW-2:5")
+	PORT_DIPUNKNOWN_DIPLOC(0x10, 0x10, "SW-2:4")
+	PORT_DIPUNKNOWN_DIPLOC(0x20, 0x20, "SW-2:3")
+	PORT_DIPUNKNOWN_DIPLOC(0x40, 0x40, "SW-2:2")
+	PORT_DIPUNKNOWN_DIPLOC(0x80, 0x80, "SW-2:1")
+
+	PORT_MODIFY("p1_4")
+	PORT_BIT( 0xff, IP_ACTIVE_LOW, IPT_UNKNOWN )
+
+	PORT_MODIFY("p2_4")
+	PORT_BIT( 0xff, IP_ACTIVE_LOW, IPT_UNKNOWN )
+
+	PORT_START("extra")
+	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_UNKNOWN )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW , IPT_COIN1 )
+	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_UNKNOWN )
+	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_SERVICE )  PORT_TOGGLE
+	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_SERVICE1 ) PORT_TOGGLE PORT_NAME("Statistics")
+	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_UNKNOWN )
+	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_UNKNOWN )
+	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_MEMORY_RESET )
+INPUT_PORTS_END
+
 
 //**************************************************************************
 //  INPUT PORT HANDLING
@@ -283,19 +348,6 @@ READ8_MEMBER( chinsan_state::input_p2_r )
 	if (BIT(m_port_select, 4) == 0) data &= m_inputs_p2[4]->read();
 
 	return data;
-}
-
-
-//**************************************************************************
-//  PALETTE
-//**************************************************************************
-
-PALETTE_INIT_MEMBER( chinsan_state, chinsan )
-{
-	uint8_t *src = memregion("color_proms")->base();
-
-	for (int i = 0; i < 0x100; i++)
-		palette.set_pen_color(i, pal4bit(src[i + 0x000]), pal4bit(src[i + 0x100]), pal4bit(src[i + 0x200]));
 }
 
 
@@ -372,23 +424,48 @@ WRITE_LINE_MEMBER( chinsan_state::adpcm_int_w )
 //  MACHINE EMULATION
 //**************************************************************************
 
+INTERRUPT_GEN_MEMBER( chinsan_state::vblank_int )
+{
+	if (m_int_enabled)
+		device.execute().set_input_line(INPUT_LINE_IRQ0, HOLD_LINE);
+}
+
 WRITE8_MEMBER( chinsan_state::ctrl_w )
 {
 	// 76------  rom bank
-	// --543210  unknown
+	// --5432--  unknown
+	// ------1-  flip screen
+	// -------0  interrupt enable
 
-	membank("bank1")->set_entry(data >> 6);
-	membank("bank1d")->set_entry(data >> 6);
+	m_int_enabled = bool(BIT(data, 0));
+	flip_screen_set(BIT(data, 1));
+
+	if (m_bank1d.found())
+	{
+		int bank = (BIT(data, 7) << 1) | BIT(data, 6);
+		m_bank1->set_entry(bank);
+		m_bank1d->set_entry(bank);
+	}
+	else
+	{
+		// bank bits have switched for mayumi
+		int bank = (BIT(data, 6) << 1) | BIT(data, 7);
+		m_bank1->set_entry(bank);
+	}
 }
 
 void chinsan_state::machine_start()
 {
 	m_tilemap = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(FUNC(chinsan_state::tile_info), this), TILEMAP_SCAN_ROWS, 8, 8, 64, 32);
 
-	membank("bank1")->configure_entries(0, 4, memregion("maincpu")->base() + 0x8000, 0x4000);
-	membank("bank0d")->set_base(m_decrypted_opcodes.get());
-	membank("bank1d")->configure_entries(0, 4, m_decrypted_opcodes.get() + 0x8000, 0x4000);
+	m_bank1->configure_entries(0, 4, memregion("maincpu")->base() + 0x8000, 0x4000);
 
+	if (m_bank0d.found())
+		m_bank0d->set_base(m_decrypted_opcodes.get());
+	if (m_bank1d.found())
+		m_bank1d->configure_entries(0, 4, m_decrypted_opcodes.get() + 0x8000, 0x4000);
+
+	save_item(NAME(m_int_enabled));
 	save_item(NAME(m_port_select));
 	save_item(NAME(m_adpcm_pos));
 	save_item(NAME(m_adpcm_idle));
@@ -398,6 +475,7 @@ void chinsan_state::machine_start()
 
 void chinsan_state::machine_reset()
 {
+	m_int_enabled = false;
 	m_port_select = 0xff;
 	m_adpcm_pos = 0;
 	m_adpcm_idle = 1;
@@ -420,9 +498,11 @@ DRIVER_INIT_MEMBER( chinsan_state, chinsan )
 static MACHINE_CONFIG_START( chinsan, chinsan_state )
 	MCFG_CPU_ADD("maincpu", MC8123, XTAL_10MHz/2) // 317-5012
 	MCFG_CPU_PROGRAM_MAP(chinsan_map)
-	MCFG_CPU_IO_MAP(chinsan_io)
+	MCFG_CPU_IO_MAP(chinsan_io_map)
 	MCFG_CPU_DECRYPTED_OPCODES_MAP(decrypted_opcodes_map)
-	MCFG_CPU_VBLANK_INT_DRIVER("screen", chinsan_state, irq0_line_hold)
+	MCFG_CPU_VBLANK_INT_DRIVER("screen", chinsan_state, vblank_int)
+
+	MCFG_NVRAM_ADD_0FILL("nvram")
 
 	MCFG_DEVICE_ADD("ppi", I8255A, 0)
 	MCFG_I8255_OUT_PORTA_CB(WRITE8(chinsan_state, input_select_w))
@@ -439,8 +519,7 @@ static MACHINE_CONFIG_START( chinsan, chinsan_state )
 	MCFG_SCREEN_PALETTE("palette")
 
 	MCFG_GFXDECODE_ADD("gfxdecode", "palette", chinsan)
-	MCFG_PALETTE_ADD("palette", 256)
-	MCFG_PALETTE_INIT_OWNER(chinsan_state, chinsan)
+	MCFG_PALETTE_ADD_RRRRGGGGBBBB_PROMS("palette", 256)
 
 	// sound hardware
 	MCFG_SPEAKER_STANDARD_MONO("mono")
@@ -457,6 +536,18 @@ static MACHINE_CONFIG_START( chinsan, chinsan_state )
 	MCFG_MSM5205_VCLK_CB(WRITELINE(chinsan_state, adpcm_int_w))
 	MCFG_MSM5205_PRESCALER_SELECTOR(MSM5205_S64_4B) // 8kHz
 	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.50)
+MACHINE_CONFIG_END
+
+static MACHINE_CONFIG_DERIVED( mayumi, chinsan )
+	// standard Z80 instead of MC-8123
+	MCFG_DEVICE_REMOVE("maincpu")
+	MCFG_CPU_ADD("maincpu", Z80, XTAL_10MHz/2)
+	MCFG_CPU_PROGRAM_MAP(chinsan_map)
+	MCFG_CPU_IO_MAP(mayumi_io_map)
+	MCFG_CPU_VBLANK_INT_DRIVER("screen", chinsan_state, vblank_int)
+
+	// no ADPCM
+	MCFG_DEVICE_REMOVE("adpcm")
 MACHINE_CONFIG_END
 
 
@@ -484,10 +575,26 @@ ROM_START( chinsan )
 	ROM_REGION( 0x20, "user2", 0 )
 	ROM_LOAD( "mm60.2c", 0x000, 0x020, CRC(88477178) SHA1(03c1c9e3e88a5ae9970cb4b872ad4b6e4d77a6da) )
 
-	ROM_REGION( 0x300, "color_proms", 0 )
+	ROM_REGION( 0x300, "proms", 0 )
 	ROM_LOAD( "mm63.10n", 0x000, 0x100, CRC(b65e3567) SHA1(f146af51dfaa5b4bf44c4e27f1a0292f8fd07ce9) ) // r
 	ROM_LOAD( "mm62.9n",  0x100, 0x100, CRC(b5a1dbe5) SHA1(770a791c061ce422f860bb8d32f82bbbf9b4d12a) ) // g
 	ROM_LOAD( "mm61.9m",  0x200, 0x100, CRC(57024262) SHA1(e084e6baa3c529217f6f8e37c9dd5f0687ba2fc4) ) // b
+ROM_END
+
+ROM_START( mayumi )
+	ROM_REGION( 0x18000, "maincpu", 0 )
+	ROM_LOAD( "my00.bin",  0x00000, 0x08000, CRC(33189e37) SHA1(cbf75f56360ef7da5b7b1207b58cd0d72bcaf207) )
+	ROM_LOAD( "my01.bin",  0x08000, 0x10000, CRC(5280fb39) SHA1(cee7653f4353031701ec1608881b37073b178d9f) ) // banked
+
+	ROM_REGION( 0x30000, "gfx1", 0 )
+	ROM_LOAD( "my10.bin", 0x00000, 0x10000, CRC(3b4f4f97) SHA1(50bda1484e965f15630bd2e05861d74ddeb0d88e) )
+	ROM_LOAD( "my20.bin", 0x10000, 0x10000, CRC(18544029) SHA1(74bd8bb422db33bd7af08afbf9b801bd31a3f199) )
+	ROM_LOAD( "my30.bin", 0x20000, 0x10000, CRC(7f22d53f) SHA1(f8e5874ba0fa003ba0d6a504b2169acdf1491484) )
+
+	ROM_REGION( 0x0300, "proms", 0 )
+	ROM_LOAD( "my-9m.bin", 0x0000,  0x0100, CRC(b18fd669) SHA1(e2b1477c1bc49994b0b652d63a2205363aab9a74) ) // r
+	ROM_LOAD( "my-9l.bin", 0x0100,  0x0100, CRC(f3fef561) SHA1(247f579fe91ad7e516c93a873b2ecca780bf6da0) ) // g
+	ROM_LOAD( "my-9k.bin", 0x0200,  0x0100, CRC(3e7a8012) SHA1(24129586a1c39f68dad274b5afbdd6c027ab0901) ) // b
 ROM_END
 
 
@@ -495,5 +602,6 @@ ROM_END
 //  SYSTEM DRIVERS
 //**************************************************************************
 
-//    YEAR  NAME      PARENT  MACHINE  INPUT    CLASS          INIT     ROTATION  COMPANY     FULLNAME                                         FLAGS
-GAME( 1987, chinsan,  0,      chinsan, chinsan, chinsan_state, chinsan, ROT0,     "Sanritsu", "Ganbare Chinsan Ooshoubu (MC-8123A, 317-5012)", MACHINE_SUPPORTS_SAVE )
+//    YEAR  NAME      PARENT  MACHINE  INPUT    CLASS          INIT     ROTATION  COMPANY           FULLNAME                                         FLAGS
+GAME( 1987, chinsan,  0,      chinsan, chinsan, chinsan_state, chinsan, ROT0,     "Sanritsu",       "Ganbare Chinsan Ooshoubu (MC-8123A, 317-5012)", MACHINE_SUPPORTS_SAVE )
+GAME( 1988, mayumi,   0,      mayumi,  mayumi,  driver_device, 0,       ROT0,     "Victory L.L.C.", "Kiki-Ippatsu Mayumi-chan",                      MACHINE_SUPPORTS_SAVE )
