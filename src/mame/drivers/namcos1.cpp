dcc592cff49b0c301635c736886a464694bb5587
Ivan Vangelista <mesgnet@yahoo.it>
2017-05-29 18:59:45 +0200

    namcos1.cpp: fixed MT06589 (nw)

--- src/mame/drivers/namcos1.cpp
+++ src/mame/drivers/namcos1.cpp
@@ -2679,6 +2679,7 @@ ROM_START( puzlclub )
 	ROM_LOAD( "pc1-c5.bin",         0xa0000, 0x20000, CRC(bc443c27) SHA1(af841b6a2b783b0d9b9bbc33083afbb56e8bff69) )
 	ROM_LOAD( "pc1-c6.bin",         0xc0000, 0x20000, CRC(ec0a3dc5) SHA1(a5148e99f3198196fd635ff4ac0275393e6f7033) )
 	ROM_LOAD( "pc1-c7.bin",         0xe0000, 0x20000, NO_DUMP ) // title screen gfxs are here, might not exist.
+	ROM_FILL(                       0xe0000, 0x20000, 0xff)
 
 	ROM_REGION( 0x100000, "gfx3", ROMREGION_ERASEFF ) /* sprites */
 	/* no sprites */
