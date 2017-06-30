0f7132f39778738bd9c33fdbd3228320427c6a57
Patrick Mackinlay <pmackinlay@hotmail.com>
2017-06-30 13:19:14 +0700

    formatting

--- src/devices/machine/ncr5390.cpp
+++ src/devices/machine/ncr5390.cpp
@@ -481,7 +481,7 @@ void ncr5390_device::step(bool timeout)
 			break;
 
 		// check for command complete
-		if ((dma_command && tcounter == 0 && fifo_pos == 0)               // dma in/out: transfer counter == 0 and fifo empty
+		if ((dma_command && tcounter == 0 && fifo_pos == 0)                 // dma in/out: transfer counter == 0 and fifo empty
 		|| (!dma_command && (xfr_phase & S_INP) == 0 && fifo_pos == 0)      // non-dma out: fifo empty
 		|| (!dma_command && (xfr_phase & S_INP) == S_INP && fifo_pos == 1)) // non-dma in: every byte
 			bus_complete();
