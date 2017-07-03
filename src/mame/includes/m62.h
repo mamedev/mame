17871880c303ddef8936c13d2e1c286197a31383
David Haywood <davidhaywood@users.noreply.github.com>
2017-07-03 19:39:34 +0100

    m62 proposed cleanups.
    I've split the massive PROM blocks up into something more modern looking (the single PROM region with all PROMs crammed into it is an old MAME thing from when only one PROM region was allowed)  I've also made use of multipe palettes and gfxdecode structures (required using rgb32 to draw to)
    
    As the boards are quite modular this seems to make sense to me, the palette for the sprites and the sprite decode + sprite palette decode is the same across all games while the rest of the hardware changes a bit.  This should allow us to step closer to converting the various boards into devices and piecing them together more logically.

--- src/mame/includes/m62.h
+++ src/mame/includes/m62.h
@@ -12,9 +12,17 @@ public:
 		m_m62_tileram(*this, "m62_tileram"),
 		m_m62_textram(*this, "m62_textram"),
 		m_scrollram(*this, "scrollram"),
+		m_sprite_height_prom(*this, "spr_height_prom"),
+		m_sprite_color_proms(*this, "spr_color_proms"),
+		m_chr_color_proms(*this, "chr_color_proms"),
+		m_fg_color_proms(*this, "fg_color_proms"),
 		m_maincpu(*this, "maincpu"),
-		m_gfxdecode(*this, "gfxdecode"),
-		m_palette(*this, "palette"),
+		m_fg_decode(*this, "fg_decode"),
+		m_spr_decode(*this, "spr_decode"),
+		m_chr_decode(*this, "chr_decode"),
+		m_fg_palette(*this, "fg_palette"),
+		m_spr_palette(*this, "spr_palette"),
+		m_chr_palette(*this, "chr_palette"),
 		m_audio(*this, "irem_audio")
 	{ }
 
@@ -29,8 +37,10 @@ public:
 	tilemap_t*             m_bg_tilemap;
 	tilemap_t*             m_fg_tilemap;
 	int                  m_flipscreen;
-
-	const uint8_t          *m_sprite_height_prom;
+	required_region_ptr<uint8_t> m_sprite_height_prom;
+	required_region_ptr<uint8_t> m_sprite_color_proms;
+	required_region_ptr<uint8_t> m_chr_color_proms;
+	optional_region_ptr<uint8_t> m_fg_color_proms;
 	int32_t                m_m62_background_hscroll;
 	int32_t                m_m62_background_vscroll;
 	uint8_t                m_kidniki_background_bank;
@@ -88,41 +98,48 @@ public:
 	TILE_GET_INFO_MEMBER(get_youjyudn_bg_tile_info);
 	TILE_GET_INFO_MEMBER(get_youjyudn_fg_tile_info);
 	TILE_GET_INFO_MEMBER(get_horizon_bg_tile_info);
+	DECLARE_MACHINE_START(battroad);
+	void machine_init_save();
 	virtual void machine_start() override;
 	virtual void machine_reset() override;
 	virtual void video_start() override;
-	DECLARE_PALETTE_INIT(m62);
+	DECLARE_PALETTE_INIT(m62_spr);
+	DECLARE_PALETTE_INIT(m62_chr);
+	DECLARE_PALETTE_INIT(m62_lotlot_fg);
+	DECLARE_PALETTE_INIT(m62_battroad_fg);
 	DECLARE_VIDEO_START(kungfum);
 	DECLARE_VIDEO_START(battroad);
-	DECLARE_PALETTE_INIT(battroad);
 	DECLARE_VIDEO_START(ldrun2);
 	DECLARE_VIDEO_START(ldrun4);
 	DECLARE_VIDEO_START(lotlot);
-	DECLARE_PALETTE_INIT(lotlot);
 	DECLARE_VIDEO_START(kidniki);
 	DECLARE_VIDEO_START(spelunkr);
 	DECLARE_VIDEO_START(spelunk2);
 	DECLARE_PALETTE_INIT(spelunk2);
 	DECLARE_VIDEO_START(youjyudn);
 	DECLARE_VIDEO_START(horizon);
-	uint32_t screen_update_ldrun(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_kungfum(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_battroad(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_ldrun3(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_ldrun4(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_lotlot(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_kidniki(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_spelunkr(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_spelunk2(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_youjyudn(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_horizon(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	void m62_amplify_contrast(palette_t *palette, uint32_t numcolors);
+	uint32_t screen_update_ldrun(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+	uint32_t screen_update_kungfum(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+	uint32_t screen_update_battroad(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+	uint32_t screen_update_ldrun3(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+	uint32_t screen_update_ldrun4(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+	uint32_t screen_update_lotlot(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+	uint32_t screen_update_kidniki(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+	uint32_t screen_update_spelunkr(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+	uint32_t screen_update_spelunk2(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+	uint32_t screen_update_youjyudn(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+	uint32_t screen_update_horizon(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+	void m62_amplify_contrast(bool include_fg);
 	void register_savestate(  );
-	void draw_sprites( bitmap_ind16 &bitmap, const rectangle &cliprect, int colormask, int prioritymask, int priority );
+	void draw_sprites( bitmap_rgb32 &bitmap, const rectangle &cliprect, int colormask, int prioritymask, int priority );
 	void m62_start( tilemap_get_info_delegate tile_get_info, int rows, int cols, int x1, int y1, int x2, int y2 );
 	void m62_textlayer( tilemap_get_info_delegate tile_get_info, int rows, int cols, int x1, int y1, int x2, int y2 );
 	required_device<cpu_device> m_maincpu;
-	required_device<gfxdecode_device> m_gfxdecode;
-	required_device<palette_device> m_palette;
+	optional_device<gfxdecode_device> m_fg_decode;
+	required_device<gfxdecode_device> m_spr_decode;
+	required_device<gfxdecode_device> m_chr_decode;
+	optional_device<palette_device> m_fg_palette;
+	required_device<palette_device> m_spr_palette;
+	required_device<palette_device> m_chr_palette;
 	required_device<irem_audio_device> m_audio;
 };
