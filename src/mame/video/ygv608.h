4ea3dde4f3e4ccfd95ca2f8e02199294f78f513a
Olivier Galibert <galibert@pobox.com>
2017-07-03 15:17:36 +0200

    Misc fixes (nw)

--- src/mame/video/ygv608.h
+++ src/mame/video/ygv608.h
@@ -89,7 +89,7 @@ protected:
 	// device-level overrides
 	virtual void device_start() override;
 
-	virtual const address_space_config *memory_space_config(address_spacenum spacenum = AS_IO) const override;
+	virtual std::vector<std::pair<int, const address_space_config *>> memory_space_config() const override;
 	
 	void device_timer(emu_timer &timer, device_timer_id id, int param, void *ptr) override;
 	address_space *m_iospace;
