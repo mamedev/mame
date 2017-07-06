748677f43b575c87cc2bd24ecef429211433577b
Patrick Mackinlay <pmackinlay@hotmail.com>
2017-07-06 10:13:26 +0700

    z80scc: renamed WRDY to WREQ to match datasheet, minor touch-up

--- src/devices/machine/z80scc.cpp
+++ src/devices/machine/z80scc.cpp
@@ -174,12 +174,12 @@ z80scc_device::z80scc_device(const machine_config &mconfig, device_type type, co
 	m_out_txda_cb(*this),
 	m_out_dtra_cb(*this),
 	m_out_rtsa_cb(*this),
-	m_out_wrdya_cb(*this),
+	m_out_wreqa_cb(*this),
 	m_out_synca_cb(*this),
 	m_out_txdb_cb(*this),
 	m_out_dtrb_cb(*this),
 	m_out_rtsb_cb(*this),
-	m_out_wrdyb_cb(*this),
+	m_out_wreqb_cb(*this),
 	m_out_syncb_cb(*this),
 	m_out_int_cb(*this),
 	m_out_rxdrqa_cb(*this),
@@ -249,12 +249,12 @@ void z80scc_device::device_start()
 	m_out_txda_cb.resolve_safe();
 	m_out_dtra_cb.resolve_safe();
 	m_out_rtsa_cb.resolve_safe();
-	m_out_wrdya_cb.resolve_safe();
+	m_out_wreqa_cb.resolve_safe();
 	m_out_synca_cb.resolve_safe();
 	m_out_txdb_cb.resolve_safe();
 	m_out_dtrb_cb.resolve_safe();
 	m_out_rtsb_cb.resolve_safe();
-	m_out_wrdyb_cb.resolve_safe();
+	m_out_wreqb_cb.resolve_safe();
 	m_out_syncb_cb.resolve_safe();
 	m_out_int_cb.resolve_safe();
 	m_out_rxdrqa_cb.resolve_safe();
@@ -1792,9 +1792,9 @@ void z80scc_channel::do_sccreg_wr1(uint8_t data)
 	LOG("- External Interrupt Enable %u\n", (data & WR1_EXT_INT_ENABLE) ? 1 : 0);
 	LOG("- Transmit Interrupt Enable %u\n", (data & WR1_TX_INT_ENABLE) ? 1 : 0);
 	LOG("- Parity is special condition %u\n", (data & WR1_PARITY_IS_SPEC_COND) ? 1 : 0);
-	LOG("- Wait/Ready Enable %u\n", (data & WR1_WRDY_ENABLE) ? 1 : 0);
-	LOG("- Wait/Ready Function %s\n", (data & WR1_WRDY_FUNCTION) ? "Ready" : "Wait");
-	LOG("- Wait/Ready on %s\n", (data & WR1_WRDY_ON_RX_TX) ? "Receive" : "Transmit");
+	LOG("- Wait/DMA Request Enable %u\n", (data & WR1_WREQ_ENABLE) ? 1 : 0);
+	LOG("- Wait/DMA Request Function %s\n", (data & WR1_WREQ_FUNCTION) ? "Request" : "Wait");
+	LOG("- Wait/DMA Request on %s\n", (data & WR1_WREQ_ON_RX_TX) ? "Receive" : "Transmit");
 
 	check_waitrequest();
 
@@ -2876,19 +2876,19 @@ WRITE_LINE_MEMBER(z80scc_channel::write_rx)
 void z80scc_channel::check_waitrequest()
 {
 	// don't do anything if wait/request function is not enabled
-	if ((m_wr1 & WR1_WRDY_ENABLE) == 0)
+	if ((m_wr1 & WR1_WREQ_ENABLE) == 0)
 		return;
 
 	// wait/request function for receive not implemented
-	if (m_wr1 & WR1_WRDY_ON_RX_TX)
+	if (m_wr1 & WR1_WREQ_ON_RX_TX)
 		return;
 
 	// if dma request function is enabled
-	if (m_wr1 & WR1_WRDY_FUNCTION)
+	if (m_wr1 & WR1_WREQ_FUNCTION)
 	{
 		// assert /W//REQ if transmit buffer is empty, clear if it's not
 		int state = (m_rr0 & RR0_TX_BUFFER_EMPTY) ? ASSERT_LINE : CLEAR_LINE;
 
-		m_index ? m_uart->m_out_wrdyb_cb(state) : m_uart->m_out_wrdya_cb(state);
+		(m_index ? m_uart->m_out_wreqb_cb : m_uart->m_out_wreqa_cb)(state);
 	}
 }
