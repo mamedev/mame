e7ca79b07c93f62003dba9cdcad0b4654e23e7bc
rfka01 <rfka01@freenet.de>
2017-07-08 01:11:34 +0200

    Added the original PC 10 / PC 20 / PC 30 in two BIOS variants ... Complains about 'Timer2 control function failed', keyboard not working

--- src/mame/drivers/pc.cpp
+++ src/mame/drivers/pc.cpp
@@ -62,6 +62,19 @@ Options: 8087 FPU
 Expansion: Expansion box: 2x ISA
 
 
+Commodore PC10 / PC20 / PC30
+Links: http://www.zimmers.net/cbmpics/cpcs.html , https://de.wikipedia.org/wiki/Commodore_PC-10_bis_PC-60 , http://mingos-commodorepage.tumblr.com/post/123656301482/commodore-pc-20-beim-pc-20-handelt-es-sich-um
+http://www.richardlagendijk.nl/cip/computer/item/pc20ii/de
+Form Factor: Desktop
+CPU: 8088 @ 4.77 MHz
+RAM: 256K / 512K / 640K
+BUS: 5x ISA
+Video: MDA
+Mass storage: PC10: 1 or 2x 5.25" 360K , PC20: 1x 360K + 10MB HD, PC30: 1x 360K + 20MB HD
+On board ports: Floppy, serial, parallel, speaker
+Options: 8087 FPU
+
+
 Commodore PC-10 III
 =============
 Links: http://dostalgie.de/downloads/pc10III-20III/PC10III_OM_COMMODORE_EN_DE.pdf ; ftp://ftp.zimmers.net/pub/cbm-pc/documents/PC-8088-Information.txt
@@ -435,6 +448,15 @@ static MACHINE_CONFIG_START( cfg_single_360K )
 	MCFG_DEVICE_REMOVE("fdc:1")
 MACHINE_CONFIG_END
 
+// Commodore PC 10 / PC 20 / PC 30
+static MACHINE_CONFIG_DERIVED( compc10, pccga )
+	MCFG_DEVICE_MODIFY("isa1")
+	MCFG_SLOT_DEFAULT_OPTION("mda")
+	
+	MCFG_DEVICE_MODIFY(RAM_TAG)
+	MCFG_RAM_DEFAULT_SIZE("256K")
+	MCFG_RAM_EXTRA_OPTIONS("512K, 640K")
+MACHINE_CONFIG_END
 
 //Data General One
 static MACHINE_CONFIG_DERIVED( dgone, pccga )
@@ -716,6 +738,15 @@ ROM_START( compc1 )
 	ROM_LOAD("pc1_char.bin", 0x0000, 0x4000, CRC(ee6c27f0) SHA1(e769cc3a49a1d708bd74eb4ac85bb6ea67220d38))
 ROM_END
 
+ROM_START( compc10 )
+	ROM_REGION(0x10000, "bios", 0)
+	ROM_DEFAULT_BIOS("v205")
+	ROM_SYSTEM_BIOS(0, "v203", "v2.03")
+	ROMX_LOAD("380258-03", 0xc000, 0x4000, CRC(fbe53865) SHA1(a6d6433c055d1c328f71403a2ed2fd5908c23d40), ROM_BIOS(1))
+	ROM_SYSTEM_BIOS(1, "v205", "v2.05")
+	ROMX_LOAD("380258-04", 0xc000, 0x4000, CRC(e61084da) SHA1(dfb360a6ec6cb1250d8a6243f12a0d702e8479cb), ROM_BIOS(2))
+ROM_END
+
 // Note: Commodore PC20-III, PC10-III and COLT share the same BIOS
 ROM_START( pc10iii )
 	ROM_REGION(0x10000, "bios", 0)
@@ -894,6 +925,7 @@ ROM_END
 COMP( 1984,   dgone,            ibm5150,    0,          dgone,          pccga,    pc_state, 0,        "Data General",                    "Data General/One" ,    MACHINE_NOT_WORKING ) // CGA, 2x 3.5" disk drives
 COMP( 1985,   bw230,            ibm5150,    0,          bondwell,       bondwell, pc_state, bondwell, "Bondwell Holding",                "BW230 (PRO28 Series)", 0 )
 COMP( 1984,   compc1,           ibm5150,    0,          pccga,          pccga,    pc_state, 0,        "Commodore Business Machines",     "Commodore PC-1" ,      MACHINE_NOT_WORKING )
+COMP( 1984,   compc10,          ibm5150,    0,          compc10,          pccga,    pc_state, 0,        "Commodore Business Machines",     "Commodore PC 10",      MACHINE_NOT_WORKING )
 COMP( 1987,   pc10iii,          ibm5150,    0,          pccga,          pccga,    pc_state, 0,        "Commodore Business Machines",     "Commodore PC-10 III",  MACHINE_NOT_WORKING )
 COMP( 1992,   iskr3104,         ibm5150,    0,          iskr3104,       pccga,    pc_state, 0,        "Schetmash",                       "Iskra 3104",           MACHINE_NOT_WORKING )
 COMP( 1989,   mk88,             ibm5150,    0,          mk88,           pccga,    pc_state, 0,        "<unknown>",                       "MK-88",                MACHINE_NOT_WORKING )
