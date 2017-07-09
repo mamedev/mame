2acac0a88d7194a21429f9d2c781c7ce3f1ef98b
AJR <ajrhacker@users.noreply.github.com>
2017-07-09 03:58:37 -0400

    gunfront: Correct "Upright Controls" DIP switch - OFF is Single, and should not be the default

--- src/mame/drivers/taito_f2.cpp
+++ src/mame/drivers/taito_f2.cpp
@@ -1976,9 +1976,9 @@ static INPUT_PORTS_START( gunfront )
 	PORT_DIPNAME( 0x40, 0x40, DEF_STR( Allow_Continue ) )   PORT_DIPLOCATION("SW2:7")
 	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x40, DEF_STR( On ) )
-	PORT_DIPNAME( 0x80, 0x00, "Upright Controls" )      PORT_DIPLOCATION("SW2:8") /* ie single or two players at once */
-	PORT_DIPSETTING(    0x80, DEF_STR( Single ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( Dual ) )
+	PORT_DIPNAME( 0x80, 0x80, "Upright Controls" )      PORT_DIPLOCATION("SW2:8") /* ie single or two players at once */
+	PORT_DIPSETTING(    0x00, DEF_STR( Single ) )
+	PORT_DIPSETTING(    0x80, DEF_STR( Dual ) )
 
 	PORT_START("IN0")
 	TAITO_JOY_UDLR_2_BUTTONS_START( 1 )
