b06f8ad76de9c3338d6e316e1711d723a3eff0f5
David Haywood <davidhaywood@users.noreply.github.com>
2017-07-17 01:18:04 +0100

    A number of DS5002FP chips were dumped using the method published by Morten Shearman Kirkegaard and Peter Wilhelmsen as documented at http://www.afdelingp.dk/files/articles/ds5002fp/ds5002fp.pdf
    namely Touch and Go, World Rally 2, and TH Strikes Back
    
    a 2nd verification pass is still needed on them from additional boards, as bad bits existed in World Rally 2 even prior to extraction (steering logic was broken but bad byte was identified and fixed as the code for that command handler was identical to World Rally 1) and TH Strikes Back is crashing at the first boss (could also be a CPU core issue)  Glass was also dumped, but at the time the data wasn't extracted, only the code, and the board has since died, so needs redoing.
    
    therefore
    
    Machines promoted to WORKING
    World Rally 2 [Morten Shearman Kirkegaard,Peter Wilhelmsen, David Haywood]
    Touch & Go (World)  [Morten Shearman Kirkegaard,Peter Wilhelmsen, David Haywood]
    Touch & Go (Non North America) [Morten Shearman Kirkegaard,Peter Wilhelmsen, David Haywood]
    Touch & Go (earlier revision)  [Morten Shearman Kirkegaard,Peter Wilhelmsen, David Haywood]
    
    If anybody feels like poking around the TH Strikes Back code to figure out what's going wrong that would be appreciated, there's an enemy drops through the floor right before the boss which I think might be the issue moreso than the boss command.
    
    there's definitely some room for improvement with this, eg. making a common interface device that's shared by the drivers, and actually saving the Dallas content as NVRAM etc.  Some of the games actually abuse the internal Dallas RAM and use it as work ram (so I'm entirely unsurprised that some of them have corrupt bytes at this point, all it would take is the game crashing)

--- src/mame/includes/thoop2.h
+++ src/mame/includes/thoop2.h
@@ -10,24 +10,19 @@ public:
 		m_palette(*this, "palette"),
 		m_videoram(*this, "videoram"),
 		m_vregs(*this, "vregs"),
-		m_spriteram(*this, "spriteram") { }
-
-	required_device<cpu_device> m_maincpu;
-	required_device<gfxdecode_device> m_gfxdecode;
-	required_device<palette_device> m_palette;
-
-	required_shared_ptr<uint16_t> m_videoram;
-	required_shared_ptr<uint16_t> m_vregs;
-	required_shared_ptr<uint16_t> m_spriteram;
-
-	int m_sprite_count[5];
-	std::unique_ptr<int[]> m_sprite_table[5];
-	tilemap_t *m_pant[2];
+		m_spriteram(*this, "spriteram"),
+		m_shareram(*this, "shareram"),
+		m_mcu_ram(*this, "mcu")	
+	{ }
 
 	DECLARE_WRITE16_MEMBER(OKIM6295_bankswitch_w);
 	DECLARE_WRITE16_MEMBER(coin_w);
-	DECLARE_READ16_MEMBER(DS5002FP_R);
 	DECLARE_WRITE16_MEMBER(vram_w);
+
+	DECLARE_READ8_MEMBER(dallas_ram_r);
+	DECLARE_WRITE8_MEMBER(dallas_ram_w);
+	DECLARE_READ8_MEMBER(dallas_share_r);
+	DECLARE_WRITE8_MEMBER(dallas_share_w);
 
 	virtual void machine_start() override;
 	virtual void video_start() override;
@@ -37,4 +32,19 @@ public:
 	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 	void sort_sprites();
 	void draw_sprites(bitmap_ind16 &bitmap, const rectangle &cliprect, int pri);
+
+private:
+	int m_sprite_count[5];
+	std::unique_ptr<int[]> m_sprite_table[5];
+	tilemap_t *m_pant[2];
+
+	required_device<cpu_device> m_maincpu;
+	required_device<gfxdecode_device> m_gfxdecode;
+	required_device<palette_device> m_palette;
+
+	required_shared_ptr<uint16_t> m_videoram;
+	required_shared_ptr<uint16_t> m_vregs;
+	required_shared_ptr<uint16_t> m_spriteram;
+	required_shared_ptr<uint16_t> m_shareram;
+	required_region_ptr<uint8_t> m_mcu_ram;
 };
