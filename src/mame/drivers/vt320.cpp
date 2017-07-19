05dfd9d73e8e16d05244e889782c9c4cfb9c09fd
Ivan Vangelista <mesgnet@yahoo.it>
2017-07-19 18:24:49 +0200

    new not working machine ------------------------------------ VT330 [Toshi]

--- src/mame/drivers/vt320.cpp
+++ src/mame/drivers/vt320.cpp
@@ -114,15 +114,22 @@ ROM_START( vt320 )
 	//DOL: http://web.archive.org/web/20060905115711/http://cmcnabb.cc.vt.edu/dec94mds/vt320dol.txt
 	ROM_SYSTEM_BIOS( 0, "vt320v11", "VT320 V1.1" )
 	// 23-054E7 below can also appear (same contents?) as 23-048E7 which is a mask rom
-	ROMX_LOAD( "23-054e7.e9", 0x0000, 0x10000, CRC(be98f9a4) SHA1(b8044d42ffaadb734fbd047fbca9c8aadeb0bf6c), ROM_BIOS(1)) // EPROM
+	ROMX_LOAD( "23-054e7.e9", 0x0000, 0x10000, CRC(be98f9a4) SHA1(b8044d42ffaadb734fbd047fbca9c8aadeb0bf6c), ROM_BIOS(1) ) // EPROM
 	ROM_SYSTEM_BIOS( 1, "vt320", "VT320 V1.2" )
-	ROMX_LOAD( "23-104e7.e9", 0x0000, 0x10000, CRC(5f419b5a) SHA1(dbc429b32d6baefd8a56862717d6e7fea1fb0c1c), ROM_BIOS(2))
+	ROMX_LOAD( "23-104e7.e9", 0x0000, 0x10000, CRC(5f419b5a) SHA1(dbc429b32d6baefd8a56862717d6e7fea1fb0c1c), ROM_BIOS(2) )
+ROM_END
+
+ROM_START( vt330 )
+	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
+	ROM_DEFAULT_BIOS( "vt330" )
+	ROM_SYSTEM_BIOS( 0, "vt330", "VT330" )
+	ROMX_LOAD( "vt330-lm8835236e6", 0x0000, 0x8000, CRC(38379339) SHA1(394e8511581abc796c8c612149eff280146b0ac8), ROM_BIOS(1) ) // 27256 EPROM
 ROM_END
 
 /* Driver */
 
 /*    YEAR  NAME     PARENT  COMPAT   MACHINE    INPUT   STATE         INIT  COMPANY                          FULLNAME   FLAGS */
-COMP( 1987, vt320,   0,      0,       vt320,     vt320,  vt320_state,  0,    "Digital Equipment Corporation", "VT320",   MACHINE_NOT_WORKING | MACHINE_NO_SOUND)
-//COMP( 1989?, vt330,  0,      0,       vt320,     vt320,  vt320_state,  0,    "Digital Equipment Corporation", "VT330",   MACHINE_NOT_WORKING)
-//COMP( 1989?, vt340,  0,      0,       vt320,     vt320,  vt320_state,  0,    "Digital Equipment Corporation", "VT340",   MACHINE_NOT_WORKING)
-//COMP( 1990?, vt340p, 0,      0,       vt320,     vt320,  vt320_state,  0,    "Digital Equipment Corporation", "VT340+",  MACHINE_NOT_WORKING)
+COMP( 1987, vt320,   0,      0,       vt320,     vt320,  vt320_state,  0,    "Digital Equipment Corporation", "VT320",   MACHINE_IS_SKELETON )
+COMP( 1987, vt330,   0,      0,       vt320,     vt320,  vt320_state,  0,    "Digital Equipment Corporation", "VT330",   MACHINE_IS_SKELETON )
+//COMP( 1989?, vt340,  0,      0,       vt320,     vt320,  vt320_state,  0,    "Digital Equipment Corporation", "VT340",   MACHINE_IS_SKELETON )
+//COMP( 1990?, vt340p, 0,      0,       vt320,     vt320,  vt320_state,  0,    "Digital Equipment Corporation", "VT340+",  MACHINE_IS_SKELETON )
