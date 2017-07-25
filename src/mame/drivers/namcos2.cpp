f692422dc448b455aac545c540895f1db10d5afd
MASH <mameinfo@ewetel.net>
2017-07-25 17:45:07 +0100

    Fixed missing sound in Metal Hawk
    A typo in MAME 0.182 caused missing sound in Metal Hawk.
    Angelo cut out this accidentally from metlhawk MACHINE_CONFIG :
    //MCFG_CPU_PERIODIC_INT_DRIVER(namcos2_shared_state, irq0_line_hold, 2*60)
    //MCFG_CPU_PERIODIC_INT_DRIVER(namcos2_shared_state, irq1_line_hold, 120)
    
    See https://git.redump.net/mame/commit/?id=179883f35ee104169e31fcc20d79281d029a396b

--- src/mame/drivers/namcos2.cpp
+++ src/mame/drivers/namcos2.cpp
@@ -2069,8 +2069,8 @@ static MACHINE_CONFIG_START( metlhawk )
 
 	MCFG_CPU_ADD("audiocpu", M6809, M68B09_CPU_CLOCK) /* 2.048MHz (49.152MHz OSC/24) - Sound handling */
 	MCFG_CPU_PROGRAM_MAP(sound_default_am)
-	//MCFG_CPU_PERIODIC_INT_DRIVER(namcos2_shared_state, irq0_line_hold, 2*60)
-	//MCFG_CPU_PERIODIC_INT_DRIVER(namcos2_shared_state, irq1_line_hold, 120)
+	MCFG_CPU_PERIODIC_INT_DRIVER(namcos2_shared_state, irq0_line_hold, 2*60)
+	MCFG_CPU_PERIODIC_INT_DRIVER(namcos2_shared_state, irq1_line_hold, 120)
 
 	MCFG_CPU_ADD("mcu", HD63705, C65_CPU_CLOCK) /* 2.048MHz (49.152MHz OSC/24) - I/O handling */
 	MCFG_CPU_PROGRAM_MAP(mcu_default_am)
