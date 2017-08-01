d8b1cb0191e530180995c5c9f0cb890cf96cf95c
AJR <ajrhacker@users.noreply.github.com>
2017-08-01 01:01:48 -0400

    Attempted fix for QT debugger (nw)

--- src/osd/modules/debugger/qt/memorywindow.cpp
+++ src/osd/modules/debugger/qt/memorywindow.cpp
@@ -347,10 +347,10 @@ void DebuggerMemView::mousePressEvent(QMouseEvent* event)
 			const debug_view_memory_source* source = downcast<const debug_view_memory_source*>(memView->source());
 			address_space* addressSpace = source->space();
 			const int nativeDataWidth = addressSpace->data_width() / 8;
-			const uint64_t memValue = source->device()->machine().debugger().cpu().read_memory(*addressSpace,
+			const uint64_t memValue = source->device()->memory().read_memory(addressSpace->spacenum(),
 														addressSpace->address_to_byte(address),
 														nativeDataWidth,
-														true);
+														TRANSLATE_READ_DEBUG);
 			const offs_t pc = source->device()->debug()->track_mem_pc_from_space_address_data(addressSpace->spacenum(),
 																								address,
 																								memValue);
