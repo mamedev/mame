224cfaeb6a3ceac946a0bf6321a822e161c494c6
Dirk Best <mail@dirk-best.de>
2017-08-03 13:09:52 +0200

    jvc_dsk: Autodetect double sided floppy images without header, fixes #2405

--- src/lib/formats/jvc_dsk.cpp
+++ src/lib/formats/jvc_dsk.cpp
@@ -68,7 +68,15 @@ bool jvc_format::parse_header(io_generic *io, int &header_size, int &tracks, int
 		// no break
 	case 1: sectors = header[0];
 		// no break
-	case 0: tracks = (size - header_size) / sector_size / sectors / heads;
+	case 0:	tracks = (size - header_size) / sector_size / sectors / heads;
+
+		// assume double sided when larger than 84 tracks without header
+		if (tracks > 84 && header_size == 0)
+		{
+			tracks /= 2;
+			heads = 2;
+		}
+
 		break;
 	}
 
