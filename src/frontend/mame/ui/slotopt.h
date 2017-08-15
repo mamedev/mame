710fc0dbd885f1a3569164e26cdfe43697cd8863
npwoods <npwoods@alumni.cmu.edu>
2017-08-15 03:14:39 -0400

    Fix to MameTesters bug #6657 (#2560)
    The source of the issue is that the logic for rotating slot options was broken.  It would count selectable options and skip over options that were not selectable, except for the specific scenario where the index was zero.  I ended up largely replacing the logic to be more C++-ish.

--- src/frontend/mame/ui/slotopt.h
+++ src/frontend/mame/ui/slotopt.h
@@ -24,21 +24,28 @@ public:
 	virtual ~menu_slot_devices() override;
 
 private:
+	enum class step_t
+	{
+		NEXT,
+		PREVIOUS
+	};
+
 	virtual void populate(float &customtop, float &custombottom) override;
 	virtual void custom_render(void *selectedref, float top, float bottom, float origx1, float origy1, float origx2, float origy2) override;
 	virtual void handle() override;
 
 	device_slot_option *get_current_option(device_slot_interface &slot) const;
-	int get_current_index(device_slot_interface &slot) const;
-	const char *get_next_slot(device_slot_interface &slot) const;
-	const char *get_previous_slot(device_slot_interface &slot) const;
 	void set_slot_device(device_slot_interface &slot, const char *val);
 	void record_current_options();
 	bool try_refresh_current_options();
+	void rotate_slot_device(device_slot_interface &slot, step_t step);
 
 	// variables
 	std::unique_ptr<machine_config>                 m_config;
 	std::unordered_map<std::string, std::string>    m_slot_options;
+	std::string										m_current_option_list_slot_tag;
+	std::vector<std::string>						m_current_option_list;
+	std::vector<std::string>::const_iterator		m_current_option_list_iter;
 };
 
 } // namespace ui
