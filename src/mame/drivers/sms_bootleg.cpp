4e8068f6fbff2161230cffbf3abe68e9133b2b44
Scott Stone <tafoid@gmail.com>
2017-08-24 22:01:52 -0400

    New machines marked as NOT_WORKING ---------------------------------- Super Game (Sega Master System Multi-game bootleg) (alt games)  [David Haywood, TeamEurope]

--- src/mame/drivers/sms_bootleg.cpp
+++ src/mame/drivers/sms_bootleg.cpp
@@ -3,6 +3,10 @@
 
 /*
 
+----------------------------------
+for Sono Corp Japan
+----------------------------------
+
 this is an SMS multi-game bootleg with 32 games
 
 
@@ -49,10 +53,10 @@ Masic Tree ( port08_w 03 )
 Mouse ( port08_w 13 )
 
 
-list reordered based on ROMs and banking  (the low 4 bits are clear the ROM select, the upper 4 bits are the bank select)
+list reordered based on ROMs and banking  (the low 4 bits are the ROM select, the upper 4 bits are the bank select)
 you can clearly see
 
-ROM K1.bin  / ROM K2.bin
+ROM K2.bin / ROM K1.bin 
 Super Bubble     ( port08_w 00 ) (8 banks - 2 roms)
 
 ROM K3.bin
@@ -91,7 +95,7 @@ port08_w xa would be K11 (unpopulated)
 port08_w xb would be K12 (unpopulated)
 
 
-ROM rom4.bin? (MISSING FROM DUMP!)
+ROM rom4.bin
 Invaders  << ??      ( port08_w 1c )
 Astro << ??          ( port08_w 2c )
 Super Mario  << ??   ( port08_w 3c )
@@ -110,7 +114,115 @@ Hello Kang Si    ( port08_w 0d )
 ROM rom2.bin
 Buk Doo Gun      ( port08_w 0e )
 
+----------------------------------
+for TV Tuning set
+----------------------------------
 
+Games ordered by MENU
+
+Tri-Formation ( port08_w 01 )
+Tetris ( port08_w 08 )
+Wonderboy ( port08_w 0c )
+Alex Kidd ( port08_w 0b )
+Super Mario ( port08_w 64 )
+Hello Kang Si ( port08_w 0d )
+Solomon's Key  ( port08_w 02 )
+Buk Doo Gun ( port08_w 0e )
+
+Invaders ( port08_w 63 )
+Galaxian ( port08_w 55 )
+Galaga ( port08_w 75 )
+Flicky ( port08_w 45 )
+Teddy Boy ( port08_w 68 )
+Ghost House ( port08_w 74 )
+Bomb Jack ( port08_w 65 )
+Kings Vally ( port08_w 46 )
+
+Pippols ( port08_w 56 )
+Dragon Story ( port08_w 47 )
+Spy Vs Spy ( port08_w 57 )
+Pitfall 2 ( port08_w 78 )
+Drol ( port08_w 49 )
+Pit Pot ( port08_w 59 )
+Hyper Sport ( port08_w 4a )
+Super Tank ( port08_w 5a )
+
+Congo Bongo ( port08_w 44 )
+Circus ( port08_w 54 )
+Road Fighter ( port08_w 67 )
+Astro ( port08_w 73 )
+Goonies ( port08_w 66 )
+Road Runner I ( port08_w 76 )
+Maisc Tree ( port08_w 43 )
+Mouse ( port08_w 53 )
+
+
+Games ordered by ROM
+
+ROM 01.K1
+Tri-Formation ( port08_w 01 )
+
+ROM 02.K2
+Game Menus
+
+ROM 03.K3
+Solomon's Key  ( port08_w 02 )
+
+ROM 04.K4
+Maisc Tree ( port08_w 43 )
+Mouse ( port08_w 53 ) [Hustle Chumy]
+Invaders ( port08_w 63 )
+Astro ( port08_w 73 )
+
+ROM 05.K5
+Congo Bongo ( port08_w 44 )
+Circus ( port08_w 54 )
+Super Mario ( port08_w 64 ) [Super Boy 1]
+Ghost House ( port08_w 74 )
+
+ROM 06.K6
+Flicky ( port08_w 45 )
+Galaxian ( port08_w 55 )
+Bomb Jack ( port08_w 65 )
+Galaga ( port08_w 75 )
+
+ROM 07.K7
+Kings Vally ( port08_w 46 )
+Pippols ( port08_w 56 )
+Goonies ( port08_w 66 )
+Road Runner I ( port08_w 76 ) [Lode Runner]
+
+ROM 08.K8
+Dragon Story ( port08_w 47 )
+Spy Vs Spy ( port08_w 57 )
+Road Fighter ( port08_w 67 )
+
+ROM 09.K9
+Tetris ( port08_w 08 )
+Teddy Boy ( port08_w 68 )
+Pitfall 2 ( port08_w 78 )
+
+ROM 10.K10
+Drol ( port08_w 49 )
+Pit Pot ( port08_w 59 )
+
+ROM 11.K11
+Hyper Sport ( port08_w 4a ) [Hyper Sports 2]
+Super Tank ( port08_w 5a )
+
+ROM 12.K12
+Alex Kidd ( port08_w 0b )
+
+ROM 13.ROM4
+Wonderboy ( port08_w 0c )
+
+ROM 14.ROM3
+Hello Kang Si ( port08_w 0d )
+
+ROM 15.ROM2
+Buk Doo Gun ( port08_w 0e )
+
+----------------------------------
 
 A Korean version has been seen too (unless this can be switched?)
 
@@ -132,12 +244,12 @@ ADDRESS_MAP_END
 
 WRITE8_MEMBER(smsbootleg_state::port08_w)
 {
-	printf("port08_w %02x\n", data);
+//	logerror("port08_w %02x\n", data);
 }
 
 WRITE8_MEMBER(smsbootleg_state::port18_w)
 {
-	printf("port18_w %02x\n", data);
+	logerror("port18_w %02x\n", data);
 }
 
 
@@ -278,7 +390,6 @@ DRIVER_INIT_MEMBER(smsbootleg_state,sms_supergame)
 ROM_START( smssgame )
 	ROM_REGION( 0x200000, "maincpu", 0 )
 	ROM_LOAD( "rom1.bin", 0x00000, 0x10000, CRC(0e1f258e) SHA1(9240dc0d01e3061c0c8807c07c0a1d033ebe9116) ) // yes, this rom is smaller (menu rom)
-
 	ROM_LOAD( "rom2.bin",0x020000, 0x20000, CRC(c1478323) SHA1(27b524a234f072e81ef41fb89a5fff5617e9b951) ) // Buk Doo Sun
 	ROM_LOAD( "rom3.bin",0x040000, 0x20000, CRC(96c8705d) SHA1(ba4f4af0cfdad1d63a08201ed186c79aea062b95) ) // ? Kung Fu game (Hello Kang Si?)
 	// k12 unpopulated
@@ -293,13 +404,93 @@ ROM_START( smssgame )
 	ROM_LOAD( "K3.bin",  0x100000, 0x20000, CRC(9bb92096) SHA1(3ca17b7a9aa20b97cac1f78ba13f70bed1b37463) ) // Solomon's Key
 	ROM_LOAD( "K2.bin",  0x120000, 0x20000, CRC(a12439f4) SHA1(e957d4fe275e982bedef28af8cc2957da27dc512) ) // Final Bubble Bobble (1/2)
 	ROM_LOAD( "K1.bin",  0x140000, 0x20000, CRC(dadffecd) SHA1(68ebb968539049a9e193da5200856b9f956f7e02) ) // Final Bubble Bobble (2/2)
-
 	// this mask rom appears to be taken straight from an SMS multi-game cart, bank 0 of it is even a menu just for the games in this ROM! same style, so presumably the same developer (Seo Jin 1990 copyright)
-	ROM_LOAD( "SG11004A 79ST0086END 9045.rom4",0x180000, 0x080000, CRC(cdbfe86e) SHA1(83d6f261471dca20f8d2e33b9807d670e9b4eb9c) )
+	ROM_LOAD( "SG11004A 79ST0086END 9045.rom4",0x180000, 0x080000, CRC(cdbfe86e) SHA1(83d6f261471dca20f8d2e33b9807d670e9b4eb9c) ) // Invaders, Astro, Super Mario, Ghost House, Bomb Jack, Galaga, Goonies, Road Runner I, Road Fighter, Tetris, Wonder Boy
 
 	// there seems to be some kind of MCU for the timer?
 ROM_END
 
 
+ROM_START( smssgamea )
+	ROM_REGION( 0x800000, "maincpu", 0 )
+	ROM_LOAD( "02.K2",   0x000000, 0x10000, CRC(66ed320e) SHA1(e838cb98fbd295259707f8f7ce433b28baa846e3) ) // menu is here on this one
+	ROM_RELOAD(          0x010000, 0x10000)
+	ROM_RELOAD(          0x020000, 0x10000)
+	ROM_RELOAD(          0x030000, 0x10000)
+	ROM_RELOAD(          0x040000, 0x10000)
+	ROM_RELOAD(          0x050000, 0x10000)
+	ROM_RELOAD(          0x060000, 0x10000)
+	ROM_RELOAD(          0x070000, 0x10000)
+	ROM_LOAD( "01.K1",   0x080000, 0x20000, CRC(18fd8607) SHA1(f24fbe863e19b513369858bf1260355e92444071) ) // Tri-Formation
+	ROM_RELOAD(          0x0a0000, 0x20000)
+	ROM_RELOAD(          0x0c0000, 0x20000)
+	ROM_RELOAD(          0x0e0000, 0x20000)
+	ROM_LOAD( "03.K3",   0x100000, 0x20000, CRC(9bb92096) SHA1(3ca17b7a9aa20b97cac1f78ba13f70bed1b37463) ) // Solomon's Key
+	ROM_RELOAD(          0x120000, 0x20000)
+	ROM_RELOAD(          0x140000, 0x20000)
+	ROM_RELOAD(          0x160000, 0x20000)
+	ROM_LOAD( "04.K4",   0x180000, 0x20000, CRC(28f6f4a9) SHA1(87809d93b8393b3186672c217fa1dec8b152af16) ) // Maisc Tree, Mouse, Invaders, Astro
+	ROM_RELOAD(          0x1a0000, 0x20000)
+	ROM_RELOAD(          0x1c0000, 0x20000)
+	ROM_RELOAD(          0x1e0000, 0x20000)
+	ROM_LOAD( "05.K5",   0x200000, 0x20000, CRC(350591a4) SHA1(ceb3c4a0fc85c5fbc5a045e9c83c3e7ec4d535cc) ) // Congo Bongo, Circus, Super Mario, Ghost House
+	ROM_RELOAD(          0x220000, 0x20000)
+	ROM_RELOAD(          0x240000, 0x20000)
+	ROM_RELOAD(          0x260000, 0x20000)
+	ROM_LOAD( "06.K6",   0x280000, 0x20000, CRC(9c5e7cc7) SHA1(4613928e30b7faaa41d550fa41906e13a6059513) ) // Flicky, Galaxian, Bomb Jack, Galaga
+	ROM_RELOAD(          0x2a0000, 0x20000)
+	ROM_RELOAD(          0x2c0000, 0x20000)
+	ROM_RELOAD(          0x2e0000, 0x20000)
+	ROM_LOAD( "07.K7",   0x300000, 0x20000, CRC(8046a2c0) SHA1(c80298dd56db8c09cac5263e4c01a627ab1a4cda) ) // Kings Vally, Pippols, Goonies, Road Runner I
+	ROM_RELOAD(          0x320000, 0x20000)
+	ROM_RELOAD(          0x340000, 0x20000)
+	ROM_RELOAD(          0x360000, 0x20000)
+	ROM_LOAD( "08.K8",   0x380000, 0x20000, CRC(ee366e0f) SHA1(3770aa71372e7dbdfd357b239a0fbdf8880dc135) ) // Dragon Story, Spy Vs Spy, Road Fighter
+	ROM_RELOAD(          0x3a0000, 0x20000)
+	ROM_RELOAD(          0x3c0000, 0x20000)
+	ROM_RELOAD(          0x3e0000, 0x20000)
+	ROM_LOAD( "09.K9",   0x400000, 0x20000, CRC(50a66ef6) SHA1(8eb8d1a7ecca99d1722534be269a6264d49b9dd4) ) // Tetris, Teddy Boy, Pitfall 2
+	ROM_RELOAD(          0x420000, 0x20000)
+	ROM_RELOAD(          0x440000, 0x20000)
+	ROM_RELOAD(          0x460000, 0x20000)
+	ROM_LOAD( "10.K10",  0x480000, 0x10000, CRC(ca7ab2df) SHA1(11a85f03ec21d481c5cdfcfb749da20b8569d09a) ) // Drol, Pit Pot
+	ROM_RELOAD(          0x490000, 0x10000)
+	ROM_RELOAD(          0x4a0000, 0x10000)
+	ROM_RELOAD(          0x4b0000, 0x10000)
+	ROM_RELOAD(          0x4c0000, 0x10000)
+	ROM_RELOAD(          0x4d0000, 0x10000)
+	ROM_RELOAD(          0x4e0000, 0x10000)
+	ROM_RELOAD(          0x4f0000, 0x10000)
+	ROM_LOAD( "11.K11",  0x500000, 0x10000, CRC(b03b612f) SHA1(537b7d72e1e06e17db6206a37f2480c14f46b9fc) ) // Hyper Sports, Super Tank
+	ROM_RELOAD(          0x510000, 0x10000)
+	ROM_RELOAD(          0x520000, 0x10000)
+	ROM_RELOAD(          0x530000, 0x10000)
+	ROM_RELOAD(          0x540000, 0x10000)
+	ROM_RELOAD(          0x550000, 0x10000)
+	ROM_RELOAD(          0x560000, 0x10000)
+	ROM_RELOAD(          0x570000, 0x10000)
+	ROM_LOAD( "12.K12",  0x580000, 0x20000, CRC(eb1e8693) SHA1(3283cdcfc25f34a43f317093cd39e10a52bc3ae7) ) // Alex Kidd
+	ROM_RELOAD(          0x5a0000, 0x20000)
+	ROM_RELOAD(          0x5c0000, 0x20000)
+	ROM_RELOAD(          0x5e0000, 0x20000)
+	ROM_LOAD( "13.ROM4", 0x600000, 0x20000, CRC(8767f1c9) SHA1(683cedb001e859c2c7ccde2571104f1eb9f09c2f) ) // Wonderboy
+	ROM_RELOAD(          0x620000, 0x20000)
+	ROM_RELOAD(          0x640000, 0x20000)
+	ROM_RELOAD(          0x660000, 0x20000)
+	ROM_LOAD( "14.ROM3", 0x680000, 0x20000, CRC(889bb269) SHA1(0a92b339c19240bfea29ee24fee3e7d780b0cd5c) ) // Hello Kang Si
+	ROM_RELOAD(          0x6a0000, 0x20000)
+	ROM_RELOAD(          0x6c0000, 0x20000)
+	ROM_RELOAD(          0x6e0000, 0x20000)
+	ROM_LOAD( "15.ROM2", 0x700000, 0x20000, CRC(c1478323) SHA1(27b524a234f072e81ef41fb89a5fff5617e9b951) ) // Buk Doo Gun
+	ROM_RELOAD(          0x720000, 0x20000)
+	ROM_RELOAD(          0x740000, 0x20000)
+	ROM_RELOAD(          0x760000, 0x20000)
+	ROM_FILL(            0x780000, 0x80000, 0xff) // ROM1 position not populated
 
-GAME( 199?, smssgame,  0,    sms_supergame, sms_supergame, smsbootleg_state,  sms_supergame, ROT0, "Sono Corp Japan", "Super Game (Sega Master System Multi-game bootleg)", MACHINE_NOT_WORKING )
+	// there seems to be some kind of MCU for the timer?
+ROM_END
+
+
+// these haven't been set as clones because they contain different games
+GAME( 199?, smssgame,  0,    sms_supergame, sms_supergame, smsbootleg_state,  sms_supergame,  ROT0, "Sono Corp Japan", "Super Game (Sega Master System Multi-game bootleg)", MACHINE_NOT_WORKING )
+GAME( 1990, smssgamea, 0,    sms_supergame, sms_supergame, smsbootleg_state,  sms_supergame,  ROT0, "Seo Jin (TV-Tuning license)", "Super Game (Sega Master System Multi-game bootleg) (alt games)", MACHINE_NOT_WORKING ) // for German market?
