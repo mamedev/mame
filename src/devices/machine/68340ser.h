5f7f9c4933cd830174615e820688d157bde952c3
Joakim Larsson Edstrom <joakimlarsson42@gmail.com>
2017-09-04 22:09:00 +0200

    68340: deskeletonized the serial module as a device derived from the duart device defined in mc68681.cpp

--- src/devices/machine/68340ser.h
+++ src/devices/machine/68340ser.h
@@ -5,11 +5,57 @@
 
 #pragma once
 
+#include "machine/mc68681.h"
 
-class m68340_serial
+class m68340_cpu_device;
+
+class m68340_serial : public m68340_serial_device
 {
+	friend class m68340_cpu_device;
+
 public:
-	void reset();
+	m68340_serial(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+	// device-level overrides
+	virtual void device_start() override;
+
+	READ8_MEMBER( read ) override;
+	WRITE8_MEMBER( write ) override;
+	DECLARE_WRITE_LINE_MEMBER(irq_w);
+
+protected:
+	m68340_cpu_device *m_cpu;
+
+	// Module registers not in the DUART part
+	uint8_t m_mcrh;
+	uint8_t m_mcrl;
+	uint8_t m_ilr;
+	uint8_t m_ivr;
+
+	enum {
+		REG_MCRH	= 0,
+		REG_MCRL	= 1,
+		REG_ILR		= 4,
+		REG_IVR		= 5,
+	};
+
+	enum {
+		REG_MCRH_STP	= 0x80,
+		REG_MCRH_FRZ1	= 0x40,
+		REG_MCRH_FRZ2	= 0x20,
+		REG_MCRH_ICCS	= 0x10,
+	};
+
+	enum {
+		REG_MCRL_SUPV	= 0x80,
+		REG_MCRL_ARBLV	= 0x0f,
+	};
+
+	enum {
+		REG_ILR_MASK	= 0x07
+	};
 };
+
+DECLARE_DEVICE_TYPE(M68340_SERIAL_MODULE, m68340_serial)
 
 #endif // MAME_MACHINE_68340SER_H
