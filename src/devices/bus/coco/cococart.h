35526fa55615cdf8bcf453fa961b1e99c38d339c
AJR <ajrhacker@users.noreply.github.com>
2017-09-28 14:42:00 -0400

    coco3: Make banked cartridges actually work

--- src/devices/bus/coco/cococart.h
+++ src/devices/bus/coco/cococart.h
@@ -119,7 +119,8 @@ public:
 	void twiddle_q_lines();
 
 	// cart base
-	uint8_t* get_cart_base();
+	uint8_t *get_cart_base();
+	uint32_t get_cart_size();
 	void set_cart_base_update(cococart_base_update_delegate update);
 
 private:
@@ -184,6 +185,7 @@ public:
 	virtual void set_sound_enable(bool sound_enable);
 
 	virtual uint8_t* get_cart_base();
+	virtual uint32_t get_cart_size();
 	void set_cart_base_update(cococart_base_update_delegate update);
 
 	virtual void interface_config_complete() override;
