798035cde30c3cd04cd258891234fd37fc287922
angelosa <salese_corp_ltd@email.it>
2017-10-17 04:41:52 +0200

    Proposed fix for MT #06154 (nw)

--- src/emu/ioport.cpp
+++ src/emu/ioport.cpp
@@ -3388,7 +3388,13 @@ analog_field::analog_field(ioport_field &field)
 
 			// relative controls reverse from 1 past their max range
 			if (m_wraps)
-				m_reverse_val -= INPUT_RELATIVE_PER_PIXEL;
+			{
+				// wrap by 1 in positional input type
+				if(field.type() == IPT_POSITIONAL || field.type() == IPT_POSITIONAL_V)
+					m_reverse_val --;
+				else
+					m_reverse_val -= INPUT_RELATIVE_PER_PIXEL;
+			}
 		}
 	}
 
