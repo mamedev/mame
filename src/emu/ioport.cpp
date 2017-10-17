9033470d20af8c3fb360870c1ee2b5cebdf3c25f
angelosa <salese_corp_ltd@email.it>
2017-10-17 04:42:45 +0200

    Hit save (nw)

--- src/emu/ioport.cpp
+++ src/emu/ioport.cpp
@@ -3389,7 +3389,7 @@ analog_field::analog_field(ioport_field &field)
 			// relative controls reverse from 1 past their max range
 			if (m_wraps)
 			{
-				// wrap by 1 in positional input type
+				// FIXME: positional needs -1, using INPUT_RELATIVE_PER_PIXEL skips a position (and reads outside the table array)
 				if(field.type() == IPT_POSITIONAL || field.type() == IPT_POSITIONAL_V)
 					m_reverse_val --;
 				else
