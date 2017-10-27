2ae08904db11e0b8b932561bc669fdd2e1fcff04
Robbbert <Robbbert@users.noreply.github.com>
2017-10-28 01:02:18 +1100

    New Not Working machine: Advanced Computer Design

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -3406,6 +3406,7 @@ files {
 
 createMESSProjects(_target, _subtarget, "skeleton")
 files {
+	MAME_DIR .. "src/mame/drivers/acd.cpp",
 	MAME_DIR .. "src/mame/drivers/aceex.cpp",
 	MAME_DIR .. "src/mame/drivers/akaiax80.cpp",
 	MAME_DIR .. "src/mame/drivers/alesis_qs.cpp",
