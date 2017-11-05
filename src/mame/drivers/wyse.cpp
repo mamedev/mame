e364cd685bbcfa713326e2f7ff384b6d65d7dccb
hap <happppp@users.noreply.github.com>
2017-11-05 23:09:38 +0100

    wyse: rename romsets to machine names instead of company (nw)

--- src/mame/drivers/wyse.cpp
+++ src/mame/drivers/wyse.cpp
@@ -47,23 +47,23 @@ static MACHINE_CONFIG_START( wyse )
 	MCFG_CPU_IO_MAP(io_map)
 MACHINE_CONFIG_END
 
-ROM_START( wyse )
+ROM_START( wy50 )
 	ROM_REGION( 0x10000, "maincpu", 0 )
 	ROM_LOAD( "2301_E.u6", 0x0000, 0x2000, CRC(2a62ea25) SHA1(f69c596aab307ef1872df29d353b5a61ff77bb74) )
 	ROM_REGION( 0x1000, "chargen", 0 )
 	ROM_LOAD( "2201_B.u16", 0x0000, 0x1000, CRC(ee318814) SHA1(0ac64b60ff978e607a087e9e6f4d547811c015c5) )
 ROM_END
 
-ROM_START( wyse30p )
+ROM_START( wy30p )
 	ROM_REGION( 0x10000, "maincpu", 0 )
 	ROM_LOAD( "250971-02.u4", 0x0000, 0x4000, CRC(3666549c) SHA1(23c432da2083df4b355daf566dd6514d1f9a7690) )
 ROM_END
 
-ROM_START( wyse160 )
+ROM_START( wy160 )
 	ROM_REGION( 0x10000, "maincpu", 0 ) // cpu not identified
 	ROM_LOAD( "251167-06.bin", 0x00000, 0x010000, CRC(36e920df) SHA1(8fb7f51b4f47ef63b21d421227d6fef98001e4e9) )
 ROM_END
 
-COMP( 1984, wyse,    0,    0, wyse, wyse, wyse_state, 0, "Wyse", "WY-50",  MACHINE_IS_SKELETON )
-COMP( 1990, wyse160, wyse, 0, wyse, wyse, wyse_state, 0, "Wyse", "WY-160", MACHINE_IS_SKELETON )
-COMP( 1991, wyse30p, wyse, 0, wyse, wyse, wyse_state, 0, "Wyse", "WY-30+", MACHINE_IS_SKELETON )
+COMP( 1984, wy50,  0,    0, wyse, wyse, wyse_state, 0, "Wyse", "WY-50",  MACHINE_IS_SKELETON )
+COMP( 1990, wy160, wy50, 0, wyse, wyse, wyse_state, 0, "Wyse", "WY-160", MACHINE_IS_SKELETON )
+COMP( 1991, wy30p, wy50, 0, wyse, wyse, wyse_state, 0, "Wyse", "WY-30+", MACHINE_IS_SKELETON )
