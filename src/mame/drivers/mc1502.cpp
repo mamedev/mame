6829ecb3b037d6bfbe0b84e818d17d712f71bce6
Sergey Svishchev <shattered@users.noreply.github.com>
2017-11-15 00:30:14 +0300

    mc1502: add BIOS 7.2 and todo list

--- src/mame/drivers/mc1502.cpp
+++ src/mame/drivers/mc1502.cpp
@@ -6,6 +6,11 @@
 
     Driver file for Elektronika MS 1502
 
+    To do:
+    - fix video errors caused by 465caf8038a120b4c1ffad9df67a1dc7474e5bb1
+      "cga: treat as fixed sync monitor (nw)"
+    - debug video init in BIOS 7.2
+
 ***************************************************************************/
 
 #include "emu.h"
@@ -373,6 +378,10 @@ ROM_START( mc1502 )
 	ROM_SYSTEM_BIOS(9, "v52_91", "v5.2 10/11/91")
 	ROMX_LOAD( "msv5~2b0.pgm", 0xc000, 0x2000, CRC(f7f370e9) SHA1(e069a35005581a02856853b57dd511ab8e10054b),ROM_BIOS(10))
 	ROMX_LOAD( "msv5~2b1.pgm", 0xe000, 0x2000, CRC(d50e1c43) SHA1(22724dec0052ee9e52f44f5914f2f5f3fae14612),ROM_BIOS(10))
+
+	// 7.2
+	ROM_SYSTEM_BIOS(10, "v72", "v7.2 01/21/96")
+	ROMX_LOAD( "7.2_1.bin", 0xe000, 0x2000, CRC(80912ad4) SHA1(cc54b77b2db4cc5d614efafd04367d2f06400fc8),ROM_BIOS(11))
 
 	ROM_REGION(0x2000,"gfx1", ROMREGION_ERASE00)
 	ROM_LOAD( "symgen.rom", 0x0000, 0x2000, CRC(b2747a52) SHA1(6766d275467672436e91ac2997ac6b77700eba1e))
