112fbd83abe96bb2a6358528087ee35fd511b48e
AJR <ajrhacker@users.noreply.github.com>
2017-12-01 16:56:20 -0500

    Clones promoted to working -------------------------- SWTPC 6800 Computer System (with MIKBUG) [AJR]

--- scripts/src/bus.lua
+++ scripts/src/bus.lua
@@ -1400,6 +1400,8 @@ if (BUSES["SS50"]~=null) then
 	files {
 		MAME_DIR .. "src/devices/bus/ss50/interface.cpp",
 		MAME_DIR .. "src/devices/bus/ss50/interface.h",
+		MAME_DIR .. "src/devices/bus/ss50/mpc.cpp",
+		MAME_DIR .. "src/devices/bus/ss50/mpc.h",
 		MAME_DIR .. "src/devices/bus/ss50/mps.cpp",
 		MAME_DIR .. "src/devices/bus/ss50/mps.h",
 	}
