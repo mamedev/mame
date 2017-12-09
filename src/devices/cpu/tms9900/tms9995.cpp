01339444972e008dd2537acc42629efbdf7b9f12
Robbbert <Robbbert@users.noreply.github.com>
2017-12-09 22:42:50 +1100

    TMS9995: fixed debugger do command (Thanks to Ryan Holtz)

--- src/devices/cpu/tms9900/tms9995.cpp
+++ src/devices/cpu/tms9900/tms9995.cpp
@@ -349,10 +349,10 @@ void tms9995_device::device_reset()
 
 const char* tms9995_device::s_statename[20] =
 {
-	"PC ", "WP ", "ST ", "IR ",
-	"R0 ", "R1 ", "R2 ", "R3 ",
-	"R4 ", "R5 ", "R6 ", "R7 ",
-	"R8 ", "R9 ", "R10", "R11",
+	"PC",  "WP",  "ST",  "IR",
+	"R0",  "R1",  "R2",  "R3",
+	"R4",  "R5",  "R6",  "R7",
+	"R8",  "R9",  "R10", "R11",
 	"R12", "R13", "R14", "R15"
 };
 
