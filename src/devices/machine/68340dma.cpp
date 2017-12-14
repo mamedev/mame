61d941630e75c96ceccb0e268cbfd76cceb69bef
AJR <ajrhacker@users.noreply.github.com>
2017-12-13 22:28:21 -0500

    Fix debug build (nw)

--- src/devices/machine/68340dma.cpp
+++ src/devices/machine/68340dma.cpp
@@ -8,9 +8,6 @@
 
 READ32_MEMBER( m68340_cpu_device::m68340_internal_dma_r )
 {
-	assert(m68340DMA);
-	//m68340_dma &dma = *m68340DMA;
-
 	int pc = space.device().safe_pc();
 	logerror("%08x m68340_internal_dma_r %08x, (%08x)\n", pc, offset*4,mem_mask);
 
@@ -19,9 +16,6 @@ READ32_MEMBER( m68340_cpu_device::m68340_internal_dma_r )
 
 WRITE32_MEMBER( m68340_cpu_device::m68340_internal_dma_w )
 {
-	assert(m68340DMA);
-	//m68340_dma &dma = *m68340DMA;
-
 	int pc = space.device().safe_pc();
 	logerror("%08x m68340_internal_dma_w %08x, %08x (%08x)\n", pc, offset*4,data,mem_mask);
 }
