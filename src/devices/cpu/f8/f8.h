35d0ff717978252570b807163e039389f4106d7a
hap <happppp@users.noreply.github.com>
2017-12-21 18:20:22 +0100

    f8: fix min/max cycles (nw)

--- src/devices/cpu/f8/f8.h
+++ src/devices/cpu/f8/f8.h
@@ -41,8 +41,8 @@ protected:
 	virtual void device_reset() override;
 
 	// device_execute_interface overrides
-	virtual uint32_t execute_min_cycles() const override { return 1; }
-	virtual uint32_t execute_max_cycles() const override { return 7; }
+	virtual uint32_t execute_min_cycles() const override { return 4; }
+	virtual uint32_t execute_max_cycles() const override { return 26; }
 	virtual uint32_t execute_input_lines() const override { return 1; }
 	virtual void execute_run() override;
 	virtual void execute_set_input(int inputnum, int state) override;
