c25b98226d633149162fbf99ea06919646acb9b6
Jonathan Holt <j.holt@ctidigital.com>
2018-01-05 13:35:45 +0000

    Updates "2017" strings to "2018" where relevant.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MAME Documentation'
-copyright = u'2017, MAMEdev Team'
+copyright = u'2018, MAMEdev Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
