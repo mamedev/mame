30c9c6fcd2c7b036ed0d774ea5afd7b62f77d22f
AJR <ajrhacker@users.noreply.github.com>
2018-01-05 15:22:42 -0500

    debugcpu.cpp: Eliminate unnecessary downcast (nw)

--- src/emu/debug/debugcpu.cpp
+++ src/emu/debug/debugcpu.cpp
@@ -88,11 +88,14 @@ debugger_cpu::debugger_cpu(running_machine &machine)
 
 	/* first CPU is visible by default */
 	for (device_t &device : device_iterator(m_machine.root_device()))
-		if (dynamic_cast<cpu_device *>(&device) != nullptr)
+	{
+		auto *cpu = dynamic_cast<cpu_device *>(&device);
+		if (cpu != nullptr)
 		{
-			m_visiblecpu = downcast<cpu_device *>(&device);
+			m_visiblecpu = cpu;
 			break;
 		}
+	}
 
 	/* add callback for breaking on VBLANK */
 	if (m_machine.first_screen() != nullptr)
