ae12b93bada133c0d15a5504ebaa824d95b6c390
angelosa <salese_corp_ltd@email.it>
2018-01-07 01:54:26 +0100

    This looks a fault in emulation, please comply ty (nw)

--- src/devices/video/stvvdp1.cpp
+++ src/devices/video/stvvdp1.cpp
@@ -1044,6 +1044,8 @@ void saturn_state::drawpixel_generic(int x, int y, int patterndata, int offsetcn
 					m_vdp1.framebuffer_draw_lines[y][x] = stv_vdp1_apply_gouraud_shading( x, y, pix );
 					break;
 				default:
+				    // TODO: mode 5: prohibited, mode 6: gouraud shading + half-luminance, mode 7: gouraud-shading + half-transparent
+					popmessage("VDP1 PMOD = %02x, contact MAMEdev",stv2_current_sprite.CMDPMOD & 0x7);
 					m_vdp1.framebuffer_draw_lines[y][x] = pix;
 					break;
 			}
