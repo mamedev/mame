57a379c15ab7692b49b5256557d80b67f912d064
rfka01 <rfka01@users.noreply.github.com>
2018-01-08 00:59:37 +0100

    alphatpx: Add HSYNC callback [rfka01]

--- src/mame/drivers/alphatpx.cpp
+++ src/mame/drivers/alphatpx.cpp
@@ -644,14 +644,58 @@ static MACHINE_CONFIG_START( alphatp3 )
 	MCFG_FLOPPY_DRIVE_ADD("fdc:1", alphatp3_floppies, "525qd", floppy_image_device::default_floppy_formats)
 MACHINE_CONFIG_END
 
-static MACHINE_CONFIG_DERIVED (alphatp2, alphatp3)
-	MCFG_DEVICE_REMOVE("fdc:0")
-	MCFG_DEVICE_REMOVE("fdc:1")
+static MACHINE_CONFIG_START( alphatp2 )
+	MCFG_CPU_ADD("maincpu", I8085A, XTAL_6MHz)
+	MCFG_CPU_PROGRAM_MAP(alphatp3_mem)
+	MCFG_CPU_IO_MAP(alphatp3_io)
+
+	MCFG_QUANTUM_PERFECT_CPU("maincpu")
+
+	MCFG_CPU_ADD("kbdmcu", I8041, XTAL_12_8544MHz/2)
+	MCFG_MCS48_PORT_T0_IN_CB(READLINE(alphatpx_state, kbd_matrix_r))
+	MCFG_MCS48_PORT_P1_OUT_CB(WRITE8(alphatpx_state, kbd_matrix_w))
+	MCFG_MCS48_PORT_P2_IN_CB(READ8(alphatpx_state, kbd_port2_r))
+	MCFG_MCS48_PORT_P2_OUT_CB(WRITE8(alphatpx_state, kbd_port2_w))
+
+	MCFG_DEVICE_ADD("bankdev", ADDRESS_MAP_BANK, 0)
+	MCFG_DEVICE_PROGRAM_MAP(alphatp3_map)
+	MCFG_ADDRESS_MAP_BANK_ENDIANNESS(ENDIANNESS_LITTLE)
+	MCFG_ADDRESS_MAP_BANK_DATA_WIDTH(8)
+	MCFG_ADDRESS_MAP_BANK_ADDR_WIDTH(18)
+	MCFG_ADDRESS_MAP_BANK_STRIDE(0x10000)
+
+	// video hardware
+	MCFG_SCREEN_ADD_MONOCHROME("screen", RASTER, rgb_t::green())
+	MCFG_SCREEN_RAW_PARAMS(XTAL_12_8544MHz, 824, 0, 640, 312, 0, 288)
+	MCFG_SCREEN_UPDATE_DRIVER(alphatpx_state, screen_update)
+
+	MCFG_PALETTE_ADD_MONOCHROME("palette")
+
+	MCFG_DEVICE_ADD("crtc", CRT5037, XTAL_12_8544MHz)
+	MCFG_TMS9927_CHAR_WIDTH(8)
+	MCFG_TMS9927_HSYN_CALLBACK(INPUTLINE("maincpu", I8085_RST55_LINE))
+	MCFG_TMS9927_VSYN_CALLBACK(INPUTLINE("maincpu", I8085_RST65_LINE)) MCFG_DEVCB_XOR(1)
+	MCFG_VIDEO_SET_SCREEN("screen")
+
+	MCFG_GFXDECODE_ADD("gfxdecode", "palette", alphatp3)
+
+	// sound hardware
+	MCFG_SPEAKER_STANDARD_MONO( "mono" )
+	MCFG_SOUND_ADD( "beeper", BEEP, 1060 )
+	MCFG_SOUND_ROUTE( ALL_OUTPUTS, "mono", 1.00 )
+
+	MCFG_DEVICE_ADD("uart", I8251, 0)
+	// XTAL_4_9152MHz serial clock
+
+	MCFG_FD1791_ADD("fdc", XTAL_4MHz / 4)
+	MCFG_WD_FDC_INTRQ_CALLBACK(WRITELINE(alphatpx_state, fdcirq_w))
+	MCFG_WD_FDC_DRQ_CALLBACK(WRITELINE(alphatpx_state, fdcdrq_w))
+	MCFG_WD_FDC_HLD_CALLBACK(WRITELINE(alphatpx_state, fdchld_w))
 	MCFG_FLOPPY_DRIVE_ADD("fdc:0", alphatp2_floppies, "525ssdd", floppy_image_device::default_floppy_formats)
 	MCFG_FLOPPY_DRIVE_ADD("fdc:1", alphatp2_floppies, "525ssdd", floppy_image_device::default_floppy_formats)
 MACHINE_CONFIG_END
 
-static MACHINE_CONFIG_DERIVED (alphatp2u, alphatp3)
+static MACHINE_CONFIG_DERIVED (alphatp2u, alphatp2)
 	MCFG_DEVICE_REMOVE("fdc:0")
 	MCFG_DEVICE_REMOVE("fdc:1")
 	MCFG_FLOPPY_DRIVE_ADD("fdc:0", alphatp2su_floppies, "525dd", floppy_image_device::default_floppy_formats)
