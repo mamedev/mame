406391d316004584935bf7f27908380b4cedd6b5
firewave <firewave@users.noreply.github.com>
2018-01-14 01:47:44 +0100

    plugins/data: fixed table lookup (nw)

--- plugins/data/database.lua
+++ plugins/data/database.lua
@@ -12,7 +12,7 @@ end
 
 if db then
 	local found = false
-	db:exec("select * from sqlite_master where name = version", function() found = true return 0 end)
+	db:exec("select * from sqlite_master where name = 'version'", function() found = true return 0 end)
 	if not found then
 		db:exec([[
 			CREATE TABLE version (
