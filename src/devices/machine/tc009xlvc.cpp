c3822f9780e67e55d31f8baf06340fa8adb88058
firewave <firewave@users.noreply.github.com>
2018-01-20 04:08:06 +0100

    devices/machine/tc009xlvc.cpp: added assert to prevent out-of-bounds access (nw)

--- src/devices/machine/tc009xlvc.cpp
+++ src/devices/machine/tc009xlvc.cpp
@@ -276,7 +276,7 @@ void tc0091lvc_device::device_start()
 			break;
 
 	//printf("m_gfx_index %d\n", m_gfx_index);
-
+	assert(m_gfx_index != MAX_GFX_ELEMENTS);
 	device_palette_interface &palette = m_gfxdecode->palette();
 	m_gfxdecode->set_gfx(m_gfx_index, std::make_unique<gfx_element>(&palette, char_layout, (uint8_t *)m_pcg_ram, 0, palette.entries() / 16, 0));
 }
