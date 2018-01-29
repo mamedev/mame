3dd081a08ddc628fdafa9279c093f18f015fabfc
Olivier Galibert <galibert@pobox.com>
2018-01-29 19:18:20 +0100

    Fix some fallout (nw)

--- src/mame/includes/mexico86.h
+++ src/mame/includes/mexico86.h
@@ -11,9 +11,8 @@ class mexico86_state : public driver_device
 public:
 	mexico86_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag),
-		m_objectram(*this, "objectram"),
 		m_protection_ram(*this, "protection_ram"),
-		m_videoram(*this, "videoram"),
+		m_mainram(*this, "mainram"),
 		m_maincpu(*this, "maincpu"),
 		m_audiocpu(*this, "audiocpu"),
 		m_subcpu(*this, "sub"),
@@ -25,9 +24,8 @@ public:
 	}
 
 	/* memory pointers */
-	required_shared_ptr<u8> m_objectram;
 	required_shared_ptr<u8> m_protection_ram;
-	required_shared_ptr<u8> m_videoram;
+	required_shared_ptr<u8> m_mainram;
 
 	/* video-related */
 	int      m_charbank;
