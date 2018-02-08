6e095d8b07ae9f6fb400584842a2316d1aa293ad
briantro <briantro@users.noreply.github.com>
2018-02-07 19:31:41 -0600

    segas16b.cpp: Minor doc update for Sonic Boom (nw)

--- src/mame/drivers/segas16b.cpp
+++ src/mame/drivers/segas16b.cpp
@@ -7873,7 +7873,9 @@ ROM_END
 //  Sonic Boom, Sega System 16B
 //  CPU: FD1094 (317-0053)
 //  ROM Board type: 171-5358
-//  Sega ID# for ROM board: 834-6532-01
+//  Sega game ID: 833-6531-01 SONIC BOOM
+//    Main board: 837-6533
+//     Rom board: 834-6532-01
 //
 //  Pos.   Silk        Type        Part                Pos.   Silk        Type        Part
 //
