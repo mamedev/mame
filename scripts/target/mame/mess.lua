cde11d74f7d440bf7213e15800083808890af432
arbee <rb6502@users.noreply.github.com>
2018-02-08 15:52:02 -0500

    New machines added as MACHINE_NOT_WORKING ----------------------------------------- Intelligence Advance E/R Lerncomputer [R. Belmont, Sean Riddle, Team Europe, rfka01]

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -3400,6 +3400,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/socrates.cpp",
 	MAME_DIR .. "src/mame/audio/socrates.cpp",
 	MAME_DIR .. "src/mame/audio/socrates.h",
+	MAME_DIR .. "src/mame/drivers/inteladv.cpp",
 }
 
 createMESSProjects(_target, _subtarget, "wang")
