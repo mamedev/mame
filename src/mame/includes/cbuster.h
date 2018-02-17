a96d6f410e3996c3827d8735d6cea11d3fa28c18
cam900 <dbtlrchl@naver.com>
2018-02-17 18:26:56 +0900

    cbuster : Minor fix

--- src/mame/includes/cbuster.h
+++ src/mame/includes/cbuster.h
@@ -16,33 +16,35 @@ class cbuster_state : public driver_device
 public:
 	cbuster_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag),
-		m_pf_rowscroll(*this, "pf%u_rowscroll", 1),
-		m_paletteram(*this, "palette"),
-		m_paletteram_ext(*this, "palette_ext"),
-		m_sprgen(*this, "spritegen"),
 		m_maincpu(*this, "maincpu"),
+		m_audiocpu(*this, "audiocpu"),
 		m_deco_tilegen(*this, "tilegen%u", 1),
 		m_palette(*this, "palette"),
 		m_spriteram(*this, "spriteram"),
-		m_soundlatch(*this, "soundlatch")
+		m_soundlatch(*this, "soundlatch"),
+		m_sprgen(*this, "spritegen"),
+		m_pf_rowscroll(*this, "pf%u_rowscroll", 1),
+		m_paletteram(*this, "palette"),
+		m_paletteram_ext(*this, "palette_ext")
 	{ }
+	
+	/* devices */
+	required_device<cpu_device> m_maincpu;
+	required_device<cpu_device> m_audiocpu;
+	required_device_array<deco16ic_device, 2> m_deco_tilegen;
+	required_device<palette_device> m_palette;
+	required_device<buffered_spriteram16_device> m_spriteram;
+	required_device<generic_latch_8_device> m_soundlatch;
+	required_device<decospr_device> m_sprgen;
 
 	/* memory pointers */
 	required_shared_ptr_array<uint16_t, 4> m_pf_rowscroll;
 	required_shared_ptr<uint16_t> m_paletteram;
 	required_shared_ptr<uint16_t> m_paletteram_ext;
-	optional_device<decospr_device> m_sprgen;
 
 	/* misc */
 	uint16_t    m_prot;
 	int       m_pri;
-
-	/* devices */
-	required_device<cpu_device> m_maincpu;
-	required_device_array<deco16ic_device, 2> m_deco_tilegen;
-	required_device<palette_device> m_palette;
-	required_device<buffered_spriteram16_device> m_spriteram;
-	required_device<generic_latch_8_device> m_soundlatch;
 
 	DECLARE_WRITE16_MEMBER(twocrude_control_w);
 	DECLARE_READ16_MEMBER(twocrude_control_r);
