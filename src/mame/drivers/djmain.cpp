mamedev/mame

commit 3e40fd3f9b5e09b9979bf8fe4dabbf6af927d7a7

vgmplay : Add K054539 Support qdrmfgp, djmain, konamigq : Moved sample RAM to K054539 address map plygonet : Add K054321

src/mame/drivers/djmain.cpp
@@ -88,17 +88,11 @@ hard drive  3.5 adapter     long 3.5 IDE cable      3.5 adapter   PCB
  *
  *************************************/
 
-void djmain_state::sndram_set_bank()
-{
-	m_sndram = memregion("shared")->base() + 0x80000 * m_sndram_bank;
-}
-
 WRITE32_MEMBER(djmain_state::sndram_bank_w)
 {
 	if (ACCESSING_BITS_16_31)
 	{
 		m_sndram_bank = (data >> 16) & 0x1f;
-		sndram_set_bank();
 	}
 }
 
@@ -106,6 +100,7 @@ READ32_MEMBER(djmain_state::sndram_r)
 {
 	uint32_t data = 0;
 
+	offset |= 0x80000 * m_sndram_bank;
 	if (ACCESSING_BITS_24_31)
 		data |= m_sndram[offset * 4] << 24;
 
@@ -123,6 +118,7 @@ READ32_MEMBER(djmain_state::sndram_r)
 
 WRITE32_MEMBER(djmain_state::sndram_w)
 {
+	offset |= 0x80000 * m_sndram_bank;
 	if (ACCESSING_BITS_24_31)
 		m_sndram[offset * 4] = data >> 24;
 
@@ -426,6 +422,9 @@ static ADDRESS_MAP_START(maincpu_djmaina, AS_PROGRAM, 32, djmain_state)
 	AM_RANGE(0xf00000, 0xf01fff) AM_DEVREADWRITE("k056832", k056832_device, ram_long_r, ram_long_w)  // VIDEO RAM (tilemap)
 ADDRESS_MAP_END
 
+static ADDRESS_MAP_START(k054539_map, 0, 8, djmain_state)
+	AM_RANGE(0x000000, 0xffffff) AM_RAM AM_SHARE("sndram")
+ADDRESS_MAP_END
 
 
 /*************************************
@@ -1351,8 +1350,6 @@ void djmain_state::machine_start()
 	save_item(NAME(m_pending_vb_int));
 	save_item(NAME(m_v_ctrl));
 	save_item(NAME(m_obj_regs));
-
-	machine().save().register_postload(save_prepost_delegate(FUNC(djmain_state::sndram_set_bank), this));
 }
 
 
@@ -1360,7 +1357,6 @@ void djmain_state::machine_reset()
 {
 	/* reset sound ram bank */
 	m_sndram_bank = 0;
-	sndram_set_bank();
 
 	/* reset LEDs */
 	output().set_led_value(0, 1);
@@ -1411,12 +1407,12 @@ MACHINE_CONFIG_START(djmain_state::djmainj)
 	MCFG_SPEAKER_STANDARD_STEREO("lspeaker", "rspeaker")
 
 	MCFG_DEVICE_ADD("k054539_1", K054539, XTAL(18'432'000))
-	MCFG_K054539_REGION_OVERRRIDE("shared")
+	MCFG_DEVICE_ADDRESS_MAP(0, k054539_map)
 	MCFG_SOUND_ROUTE(0, "lspeaker", 1.0)
 	MCFG_SOUND_ROUTE(1, "rspeaker", 1.0)
 
 	MCFG_DEVICE_ADD("k054539_2", K054539, XTAL(18'432'000))
-	MCFG_K054539_REGION_OVERRRIDE("shared")
+	MCFG_DEVICE_ADDRESS_MAP(0, k054539_map)
 	MCFG_SOUND_ROUTE(0, "lspeaker", 1.0)
 	MCFG_SOUND_ROUTE(1, "rspeaker", 1.0)
 MACHINE_CONFIG_END
@@ -1460,8 +1456,6 @@ ROM_START( bm1stmix )
 	DISK_IMAGE( "753jaa11", 0, SHA1(2e70cf31a853322f29f99b6f292c187a2cf33015) ) /* ver 1.00 JA */
 	// There is an alternate image
 	//DISK_IMAGE( "753jaa11", 0, MD5(260c9b72f4a03055e3abad61c6225324) SHA1(2cc3e149744516bf2353a2b47d33bc9d2072b6c4) ) /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bm2ndmix )
@@ -1483,8 +1477,6 @@ ROM_START( bm2ndmix )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "853jaa11", 0, SHA1(9683ff8462491252b6eb2e5b3aa6496884c01506) ) /* ver 1.10 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bm2ndmxa )
@@ -1506,8 +1498,6 @@ ROM_START( bm2ndmxa )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "853jaa11", 0, SHA1(9683ff8462491252b6eb2e5b3aa6496884c01506) ) /* ver 1.10 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bm3rdmix )
@@ -1529,8 +1519,6 @@ ROM_START( bm3rdmix )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "825jaa11", 0, SHA1(048919977232bbce046406a7212586cf39b77cf2) ) /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bmcompmx )
@@ -1552,8 +1540,6 @@ ROM_START( bmcompmx )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "858jaa11", 0, SHA1(bc590472046336a1000f29901fe3fd7b29747e47) ) /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( hmcompmx )
@@ -1575,8 +1561,6 @@ ROM_START( hmcompmx )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "858jaa11", 0, SHA1(bc590472046336a1000f29901fe3fd7b29747e47) ) /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bm4thmix )
@@ -1598,8 +1582,6 @@ ROM_START( bm4thmix )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "847jaa11", 0, SHA1(8cad631531b5616d6a4b0a99d988f4b525932dc7) ) /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bm5thmix )
@@ -1621,8 +1603,6 @@ ROM_START( bm5thmix )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "981jaa11", 0, SHA1(dc7353fa436d96ae174a58d3a38ca9928a63727f) ) /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bmclubmx )
@@ -1646,8 +1626,6 @@ ROM_START( bmclubmx )
 	DISK_IMAGE( "993hdda01", 0, SHA1(f5d4df1dd27ce6ee2d0897852342691d55b63bfb) )
 	// this image has not been verified
 	//  DISK_IMAGE( "993jaa11", 0, MD5(e26eb62d7cf3357585f5066da6063143) )  /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bmcompm2 )
@@ -1669,8 +1647,6 @@ ROM_START( bmcompm2 )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "988jaa11", 0, SHA1(12a0988c631dd3331e54b8417a9659402afe168b) ) /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( hmcompm2 )
@@ -1692,8 +1668,6 @@ ROM_START( hmcompm2 )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "988jaa11", 0, SHA1(12a0988c631dd3331e54b8417a9659402afe168b) ) /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bmdct )
@@ -1715,8 +1689,6 @@ ROM_START( bmdct )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "995jaa11", 0, SHA1(8fec3c4d97f64f48b9867230a97cda4347496075) ) /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bmcorerm )
@@ -1738,8 +1710,6 @@ ROM_START( bmcorerm )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "a05jaa11", 0, SHA1(7ebc41cc3e9a0a922b49201b34e29201522eb726) ) /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bm6thmix )
@@ -1761,8 +1731,6 @@ ROM_START( bm6thmix )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "a21jaa11", 0, SHA1(ed0a07212a360e75934fc22c56265842cf0829b6) ) /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bm7thmix )
@@ -1786,8 +1754,6 @@ ROM_START( bm7thmix )
 	DISK_IMAGE( "b07jaa11", 0, SHA1(e4925494f0a801abb4d3aa6524c379eb445d8dff) ) /* ver 1.00 JA */
 	// this image has not been verified
 	//DISK_IMAGE( "b07jab11", 0, MD5(0e9440787ca69567792095085e2a3619) )    /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( bmfinal )
@@ -1811,8 +1777,6 @@ ROM_START( bmfinal )
 	DISK_IMAGE( "c01jaa11", 0, SHA1(0a53c4412a72a886f5fb98c12c529d056d625244) ) /* ver 1.00 JA */
 	// this image has not been verified
 	//DISK_IMAGE( "c01jaa11", 0, MD5(8bb7e6b6bc63cac8a4f2997307c25748) )    /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( popn1 )
@@ -1834,8 +1798,6 @@ ROM_START( popn1 )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "803aaa11", 0, SHA1(8684c1a1f0de9496bac0333c0302e35359173d8a) )  /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( popn2 )
@@ -1857,8 +1819,6 @@ ROM_START( popn2 )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "831jhdda01", 0, SHA1(ef62d5fcc1a36235fc932e6ecef71dc845d1d72d) )
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( popn3 )
@@ -1880,8 +1840,6 @@ ROM_START( popn3 )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "980hdda01", 0, SHA1(57ece2b63c32d194d4e41b4e4a45aaab30150fd4) )
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( popnstage )
@@ -1903,8 +1861,6 @@ ROM_START( popnstage )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "970jba11", 0, SHA1(72e92b73b22a8f35e9faca93211e5acf781e66bb) )  /* GQ970  pop'n stage 1.5 ver1.00  1999/10/15 (C) KONAMI */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 #if 0
@@ -1928,8 +1884,6 @@ ROM_START( bm3rdmxb )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "825jab11", 0, MD5(f4360da10a932ba90e93469df7426d1d) SHA1(1) )  /* ver 1.01 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( popn1j )
@@ -1951,8 +1905,6 @@ ROM_START( popn1j )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "803jaa11", 0, MD5(54a8ac87857d81740621c622e27736d7) )  /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 
 ROM_START( popnstex )
@@ -1974,8 +1926,6 @@ ROM_START( popnstex )
 
 	DISK_REGION( "ata:0:hdd:image" )            /* IDE HARD DRIVE */
 	DISK_IMAGE( "970jba11", 0, MD5(1616905838fdb2b521d53499c6c2a7a4) )  /* ver 1.00 JA */
-
-	ROM_REGION( 0x1000000, "shared", ROMREGION_ERASE00 )        /* K054539 RAM */
 ROM_END
 #endif
 

src/devices/sound/k054539.cpp
@@ -19,20 +19,19 @@
 DEFINE_DEVICE_TYPE(K054539, k054539_device, "k054539", "K054539 ADPCM")
 
 k054539_device::k054539_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
-	: device_t(mconfig, K054539, tag, owner, clock),
-		device_sound_interface(mconfig, *this),
-		flags(0),
-		ram(nullptr),
-		reverb_pos(0),
-		cur_ptr(0),
-		cur_limit(0),
-		cur_zone(nullptr),
-		m_rom(*this, DEVICE_SELF),
-		rom_mask(0),
-		stream(nullptr),
-		m_timer(nullptr),
-		m_timer_state(0),
-		m_timer_handler(*this)
+	: device_t(mconfig, K054539, tag, owner, clock)
+	, device_sound_interface(mconfig, *this),
+	, device_rom_interface(mconfig, *this, 24)
+	, flags(0)
+	, ram(nullptr)
+	, reverb_pos(0)
+	, cur_ptr(0)
+	, cur_limit(0)
+	, rom_addr(0)
+	, stream(nullptr)
+	, m_timer(nullptr)
+	, m_timer_state(0)
+	, m_timer_handler(*this)
 {
 }
 
@@ -168,7 +167,7 @@ void k054539_device::sound_stream_update(sound_stream &stream, stream_sample_t *
 				int rdelta = (base1[6] | (base1[7] << 8)) >> 3;
 				rdelta = (rdelta + reverb_pos) & 0x3fff;
 
-				int cur_pos = (base1[0x0c] | (base1[0x0d] << 8) | (base1[0x0e] << 16)) & rom_mask;
+				int cur_pos = (base1[0x0c] | (base1[0x0d] << 8) | (base1[0x0e] << 16));
 
 				int fdelta, pdelta;
 				if(base2[0] & 0x20) {
@@ -200,10 +199,10 @@ void k054539_device::sound_stream_update(sound_stream &stream, stream_sample_t *
 						cur_pos += pdelta;
 
 						cur_pval = cur_val;
-						cur_val = (int16_t)(m_rom[cur_pos] << 8);
+						cur_val = (int16_t)(read_byte(cur_pos) << 8);
 						if(cur_val == (int16_t)0x8000 && (base2[1] & 1)) {
-							cur_pos = (base1[0x08] | (base1[0x09] << 8) | (base1[0x0a] << 16)) & rom_mask;
-							cur_val = (int16_t)(m_rom[cur_pos] << 8);
+							cur_pos = (base1[0x08] | (base1[0x09] << 8) | (base1[0x0a] << 16));
+							cur_val = (int16_t)(read_byte(cur_pos) << 8);
 						}
 						if(cur_val == (int16_t)0x8000) {
 							keyoff(ch);
@@ -223,10 +222,10 @@ void k054539_device::sound_stream_update(sound_stream &stream, stream_sample_t *
 						cur_pos += pdelta;
 
 						cur_pval = cur_val;
-						cur_val = (int16_t)(m_rom[cur_pos] | m_rom[cur_pos+1]<<8);
+						cur_val = (int16_t)(read_byte(cur_pos) | read_byte(cur_pos+1)<<8);
 						if(cur_val == (int16_t)0x8000 && (base2[1] & 1)) {
-							cur_pos = (base1[0x08] | (base1[0x09] << 8) | (base1[0x0a] << 16)) & rom_mask;
-							cur_val = (int16_t)(m_rom[cur_pos] | m_rom[cur_pos+1]<<8);
+							cur_pos = (base1[0x08] | (base1[0x09] << 8) | (base1[0x0a] << 16));
+							cur_val = (int16_t)(read_byte(cur_pos) | read_byte(cur_pos+1)<<8);
 						}
 						if(cur_val == (int16_t)0x8000) {
 							keyoff(ch);
@@ -251,10 +250,10 @@ void k054539_device::sound_stream_update(sound_stream &stream, stream_sample_t *
 						cur_pos += pdelta;
 
 						cur_pval = cur_val;
-						cur_val = m_rom[cur_pos>>1];
+						cur_val = read_byte(cur_pos>>1);
 						if(cur_val == 0x88 && (base2[1] & 1)) {
-							cur_pos = ((base1[0x08] | (base1[0x09] << 8) | (base1[0x0a] << 16)) & rom_mask) << 1;
-							cur_val = m_rom[cur_pos>>1];
+							cur_pos = (base1[0x08] | (base1[0x09] << 8) | (base1[0x0a] << 16)) << 1;
+							cur_val = read_byte(cur_pos>>1);
 						}
 						if(cur_val == 0x88) {
 							keyoff(ch);
@@ -321,13 +320,6 @@ void k054539_device::init_chip()
 	cur_ptr = 0;
 	memset(ram.get(), 0, 0x4000);
 
-	rom_mask = 0xffffffffU;
-	for(int i=0; i<32; i++)
-		if((1U<<i) >= m_rom.bytes()) {
-			rom_mask = (1U<<i) - 1;
-			break;
-		}
-
 	stream = stream_alloc(0, 2, clock() / 384);
 
 	save_item(NAME(voltab));
@@ -341,6 +333,7 @@ void k054539_device::init_chip()
 	save_item(NAME(reverb_pos));
 	save_item(NAME(cur_ptr));
 	save_item(NAME(cur_limit));
+	save_item(NAME(rom_addr));
 
 	save_item(NAME(m_timer_state));
 }
@@ -436,18 +429,16 @@ WRITE8_MEMBER(k054539_device::write)
 		break;
 
 		case 0x22d:
-			if(regs[0x22e] == 0x80)
-				cur_zone[cur_ptr] = data;
+			if(rom_addr == 0x80)
+				ram[cur_ptr] = data;
 			cur_ptr++;
 			if(cur_ptr == cur_limit)
 				cur_ptr = 0;
 		break;
 
 		case 0x22e:
-			cur_zone =
-				data == 0x80 ? ram.get() :
-				&m_rom[0x20000*data];
-			cur_limit = data == 0x80 ? 0x4000 : 0x20000;
+			rom_addr = data;
+			cur_limit = rom_addr == 0x80 ? 0x4000 : 0x20000;
 			cur_ptr = 0;
 		break;
 
@@ -483,8 +474,7 @@ WRITE8_MEMBER(k054539_device::write)
 void k054539_device::device_post_load()
 {
 	int data = regs[0x22e];
-	cur_zone = data == 0x80 ? ram.get() : &m_rom[0x20000*data];
-	cur_limit = data == 0x80 ? 0x4000 : 0x20000;
+	cur_limit = rom_addr == 0x80 ? 0x4000 : 0x20000;
 }
 
 READ8_MEMBER(k054539_device::read)
@@ -492,7 +482,7 @@ READ8_MEMBER(k054539_device::read)
 	switch(offset) {
 	case 0x22d:
 		if(regs[0x22f] & 0x10) {
-			uint8_t res = cur_zone[cur_ptr];
+			uint8_t res = (rom_addr == 0x80) ? ram[cur_ptr] : read_byte((0x20000*rom_addr)+cur_ptr);
 			cur_ptr++;
 			if(cur_ptr == cur_limit)
 				cur_ptr = 0;
@@ -548,5 +538,19 @@ void k054539_device::device_start()
 
 void k054539_device::device_reset()
 {
+	regs[0x22c] = 0;
+	regs[0x22f] = 0;
+	memset(ram.get(), 0, 0x4000);
 	m_timer->enable(false);
 }
+
+
+//-------------------------------------------------
+//  rom_bank_updated - the rom bank has changed
+//-------------------------------------------------
+
+void k053260_device::rom_bank_updated()
+{
+	m_stream->update();
+}
+

src/devices/sound/k054539.h
@@ -23,7 +23,9 @@
 		devcb = &k054539_device::set_timer_handler(*device, DEVCB_##_devcb);
 
 
-class k054539_device : public device_t, public device_sound_interface
+class k054539_device : public device_t,
+						public device_sound_interface,
+						public device_rom_interface
 {
 public:
 	// control flags, may be set at DRIVER_INIT().
@@ -41,7 +43,6 @@ public:
 
 	// static configuration helpers
 	static void set_analog_callback(device_t &device, cb_delegate &&cb) { downcast<k054539_device &>(device).m_apan_cb = std::move(cb); }
-	static void set_override(device_t &device, const char *rgnoverride) { downcast<k054539_device &>(device).m_rom.set_tag(rgnoverride); }
 	template <class Object> static devcb_base &set_timer_handler(device_t &device, Object &&cb) { return downcast<k054539_device &>(device).m_timer_handler.set_callback(std::forward<Object>(cb)); }
 
 
@@ -72,6 +73,9 @@ protected:
 
 	// device_sound_interface overrides
 	virtual void sound_stream_update(sound_stream &stream, stream_sample_t **inputs, stream_sample_t **outputs, int samples) override;
+	
+	// device_rom_interface overrides
+	virtual void rom_bank_updated() override;
 
 private:
 	struct channel {
@@ -94,9 +98,7 @@ private:
 
 	int32_t cur_ptr;
 	int cur_limit;
-	unsigned char *cur_zone;
-	required_region_ptr<uint8_t> m_rom;
-	uint32_t rom_mask;
+	uint32_t rom_addr;
 
 	channel channels[8];
 	sound_stream *stream;

src/mame/drivers/konamigq.cpp
@@ -93,7 +93,8 @@ public:
 		m_soundcpu(*this, "soundcpu"),
 		m_dasp(*this, "dasp"),
 		m_am53cf96(*this, "am53cf96"),
-		m_k056800(*this, "k056800")
+		m_k056800(*this, "k056800"),
+		m_pcmram(*this, "pcmram")
 	{
 	}
 
@@ -102,6 +103,7 @@ public:
 	required_device<tms57002_device> m_dasp;
 	required_device<am53cf96_device> m_am53cf96;
 	required_device<k056800_device> m_k056800;
+	required_shared_ptr<uint8_t> m_pcmram;
 
 	uint8_t *m_p_n_pcmram;
 	uint8_t m_sector_buffer[ 512 ];
@@ -151,12 +153,12 @@ WRITE16_MEMBER(konamigq_state::eeprom_w)
 
 WRITE8_MEMBER(konamigq_state::pcmram_w)
 {
-	m_p_n_pcmram[ offset ] = data;
+	m_pcmram[ offset ] = data;
 }
 
 READ8_MEMBER(konamigq_state::pcmram_r)
 {
-	return m_p_n_pcmram[ offset ];
+	return m_pcmram[ offset ];
 }
 
 /* Video */
@@ -242,6 +244,13 @@ static ADDRESS_MAP_START( konamigq_dasp_map, AS_DATA, 8, konamigq_state )
 ADDRESS_MAP_END
 
 
+/* K058141 memory handling */
+static ADDRESS_MAP_START( konamigq_k054539_map, 0, 8, konamigq_state )
+	AM_RANGE(0x000000, 0x07ffff) AM_ROM AM_REGION("k054539", 0)
+	AM_RANGE(0x080000, 0x3fffff) AM_RAM AM_SHARE("pcmram")
+ADDRESS_MAP_END
+
+
 /* 058141 */
 WRITE_LINE_MEMBER(konamigq_state::k054539_irq_gen)
 {
@@ -297,12 +306,10 @@ void konamigq_state::scsi_dma_write( uint32_t *p_n_psxram, uint32_t n_address, i
 
 DRIVER_INIT_MEMBER(konamigq_state,konamigq)
 {
-	m_p_n_pcmram = memregion( "shared" )->base() + 0x80000;
 }
 
 MACHINE_START_MEMBER(konamigq_state,konamigq)
 {
-	save_pointer(NAME(m_p_n_pcmram), 0x380000);
 	save_item(NAME(m_sector_buffer));
 	save_item(NAME(m_sound_ctrl));
 	save_item(NAME(m_sound_intck));
@@ -354,13 +361,13 @@ MACHINE_CONFIG_START(konamigq_state::konamigq)
 	MCFG_K056800_INT_HANDLER(INPUTLINE("soundcpu", M68K_IRQ_1))
 
 	MCFG_DEVICE_ADD("k054539_1", K054539, XTAL(18'432'000))
-	MCFG_K054539_REGION_OVERRRIDE("shared")
+	MCFG_DEVICE_ADDRESS_MAP(0, konamigq_k054539_map)
 	MCFG_K054539_TIMER_HANDLER(WRITELINE(konamigq_state, k054539_irq_gen))
 	MCFG_SOUND_ROUTE(0, "lspeaker", 1.0)
 	MCFG_SOUND_ROUTE(1, "rspeaker", 1.0)
 
 	MCFG_DEVICE_ADD("k054539_2", K054539, XTAL(18'432'000))
-	MCFG_K054539_REGION_OVERRRIDE("shared")
+	MCFG_DEVICE_ADDRESS_MAP(0, konamigq_k054539_map)
 	MCFG_SOUND_ROUTE(0, "lspeaker", 1.0)
 	MCFG_SOUND_ROUTE(1, "rspeaker", 1.0)
 MACHINE_CONFIG_END
@@ -453,7 +460,7 @@ ROM_START( cryptklr )
 	ROM_REGION( 0x80000, "soundcpu", 0 ) /* 68000 sound program */
 	ROM_LOAD16_WORD_SWAP( "420a01.2g", 0x000000, 0x080000, CRC(84fc2613) SHA1(e06f4284614d33c76529eb43b168d095200a9eac) )
 
-	ROM_REGION( 0x400000, "shared", 0 )
+	ROM_REGION( 0x80000, "k054539", 0 )
 	ROM_LOAD( "420a02.3m",    0x000000, 0x080000, CRC(2169c3c4) SHA1(6d525f10385791e19eb1897d18f0bab319640162) )
 
 	ROM_REGION32_LE( 0x080000, "maincpu:rom", 0 ) /* bios */

commit 2d658809a11804fd40adcbd719e17ba658622f42

djmain Fixes All sets in djmain.c: MAME exception MT#06884, introduced by https://github.com/mamedev/mame/commit/3e40fd3f9b5e09b9979bf8fe4dabbf6af927d7a7 [smf]

src/mame/drivers/djmain.cpp
@@ -100,7 +100,7 @@ READ32_MEMBER(djmain_state::sndram_r)
 {
 	uint32_t data = 0;
 
-	offset |= 0x80000 * m_sndram_bank;
+	offset |= 0x20000 * m_sndram_bank;
 	if (ACCESSING_BITS_24_31)
 		data |= m_sndram[offset * 4] << 24;
 
@@ -118,7 +118,7 @@ READ32_MEMBER(djmain_state::sndram_r)
 
 WRITE32_MEMBER(djmain_state::sndram_w)
 {
-	offset |= 0x80000 * m_sndram_bank;
+	offset |= 0x20000 * m_sndram_bank;
 	if (ACCESSING_BITS_24_31)
 		m_sndram[offset * 4] = data >> 24;
 