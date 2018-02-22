4811af9c9fffbbbdf0b4e02a235e58a6b4426aa4
Lord-Nightmare <Lord-Nightmare@users.noreply.github.com>
2018-02-22 01:14:13 -0500

    osi.cpp: add some notes about the character generator rom label, and enable a commented out uk101 bios variant. (nw)

--- src/mame/drivers/osi.cpp
+++ src/mame/drivers/osi.cpp
@@ -840,7 +840,8 @@ ROM_START( sb2m600b )
 	ROMX_LOAD( "basic3.rom", 0xb000, 0x0800, CRC(ac37d575) SHA1(11407eb24d1ba7afb889b7677c987e8be1a61aab), ROM_BIOS(2) )
 
 	ROM_REGION( 0x0800, "chargen",0)
-	ROM_LOAD( "chgsup2.u41", 0x0000, 0x0800, CRC(735f5e0a) SHA1(87c6271497c5b00a974d905766e91bb965180594) )
+	ROM_LOAD( "chgsup2.u41", 0x0000, 0x0800, CRC(735f5e0a) SHA1(87c6271497c5b00a974d905766e91bb965180594) ) // see below, is this the same rom, but on a different pcb/form factor?
+	// Label: "<Synertek Logo> 7837E // C28339M // CARGENV1.0"; This mask ROM is on the OSI 540 PCB, as seen at http://www.classic-computers.org.nz/blog/images/2014-06-14-540-board.jpg at location ?E3?
 
 	ROM_REGION( 0x0800, "user1",0)
 	// Another bios rom
@@ -858,9 +859,11 @@ ROM_START( uk101 )
 	ROM_LOAD( "basuk03.ic11",  0xb000, 0x0800, CRC(0d011242) SHA1(54bd33522a5d1991086eeeff3a4f73c026be45b6) )
 	ROM_LOAD( "basuk04.ic12",  0xb800, 0x0800, CRC(667223e8) SHA1(dca78be4b98317413376d69119942d692e39575a) )
 	// This monitor came from another emulator and works well on the 64x16 screen
-	ROM_LOAD( "monuk02.ic13",  0xf800, 0x0800, CRC(e5b7028d) SHA1(74f0934014fdf83d33c8d3579e562b53c0683270) )
+	ROM_SYSTEM_BIOS(0, "final", "64x16 screen final? rom")
+	ROMX_LOAD( "monuk02.ic13",  0xf800, 0x0800, CRC(e5b7028d) SHA1(74f0934014fdf83d33c8d3579e562b53c0683270), ROM_BIOS(1) )
 	// This monitor is for a 32x32 screen, and could be the prototype referred to in Practical Electronics
-	//ROM_LOAD( "monuk02.ic13",  0xf800, 0x0800, CRC(04ac5822) SHA1(2bbbcd0ca18103fd68afcf64a7483653b925d83e) )
+	ROM_SYSTEM_BIOS(1, "proto", "32x32 screen proto? rom")
+	ROMX_LOAD( "monuk02_alt.ic13",  0xf800, 0x0800, CRC(04ac5822) SHA1(2bbbcd0ca18103fd68afcf64a7483653b925d83e), ROM_BIOS(2) )
 
 	ROM_REGION( 0x800, "chargen", 0 )
 	ROM_LOAD( "chguk101.ic41", 0x0000, 0x0800, CRC(fce2c84a) SHA1(baa66a7a48e4d62282671ef53abfaf450b888b70) )
