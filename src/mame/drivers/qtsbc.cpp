5681d1d387bb5c7659cf4649fd712cee6ddcae3e
Robbbert <Robbbert@users.noreply.github.com>
2018-03-15 09:48:38 +1100

    qtsbc : removed myself

--- src/mame/drivers/qtsbc.cpp
+++ src/mame/drivers/qtsbc.cpp
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:Robbbert, AJR
+// copyright-holders:AJR
 /***************************************************************************
 
 QT Computer Systems SBC +2/4
