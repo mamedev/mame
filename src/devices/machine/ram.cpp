17043f615a86e9b8d5ae648b1cc0ebb340723487
Vas Crabb <vas@vastheman.com>
2018-04-03 03:42:59 +1000

    restore RAM default fill (nw)

--- src/devices/machine/ram.cpp
+++ src/devices/machine/ram.cpp
@@ -163,7 +163,7 @@ void ram_device::device_start()
 
 	// allocate space for the ram
 	m_pointer = std::make_unique<u8 []>(m_size);
-	std::fill_n(m_pointer.get(), m_size, 0);
+	std::fill_n(m_pointer.get(), m_size, m_default_value);
 
 	// register for state saving
 	save_item(NAME(m_size));
