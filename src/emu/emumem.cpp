8ebd748b1b842da8cbd4d6d627bd40065bc407f4
smf- <smf-@users.noreply.github.com>
2018-04-07 08:51:27 +0100

    Fix fencepost in memory_manager::region_containing, which caused regions to be registered for state saving. [smf]

--- src/emu/emumem.cpp
+++ src/emu/emumem.cpp
@@ -1905,7 +1905,7 @@ memory_region *memory_manager::region_containing(const void *memory, offs_t byte
 
 	// look through the region list and return the first match
 	for (auto &region : m_regionlist)
-		if (data >= region.second->base() && (data + bytes) < region.second->end())
+		if (data >= region.second->base() && (data + bytes) <= region.second->end())
 			return region.second.get();
 
 	// didn't find one
