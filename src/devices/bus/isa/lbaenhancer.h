ba1642e3ad85eb1db821dee721267aa74878b975
Bavarese <Bavarese@users.noreply.github.com>
2018-04-16 19:40:49 +0200

    Create lbaenhancer (header file)

---
+++ src/devices/bus/isa/lbaenhancer.h
@@ -0,0 +1,46 @@
+// license:BSD-3-Clause
+// copyright-holders:Bavarese
+/**********************************************************************
+
+    VS Systems LBA Enhancer (ISA; 1995).
+
+ **********************************************************************/
+
+#ifndef MAME_BUS_ISA_LBA_GEN_H
+#define MAME_BUS_ISA_LBA_GEN_H
+
+#pragma once
+
+#include "isa.h"
+
+
+#define LBA_GEN_ROMSIZE 0x04000
+
+//**************************************************************************
+//  TYPE DEFINITIONS
+//**************************************************************************
+
+class lba_gen_device : public device_t,
+		       public device_isa8_card_interface
+{
+public:
+	// construction/destruction
+	lba_gen_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+protected:
+	// device-level overrides
+	virtual void device_start() override;
+	virtual void device_reset() override;
+
+	// optional information overrides
+	virtual void device_add_mconfig(machine_config &config) override;
+	virtual ioport_constructor device_input_ports() const override;
+
+	virtual const tiny_rom_entry *device_rom_region() const override;
+private:
+};
+
+// device type definition
+DECLARE_DEVICE_TYPE(LBA_GEN, lba_gen_device)
+
+#endif // MAME_BUS_ISA_LBA_GEN_H
