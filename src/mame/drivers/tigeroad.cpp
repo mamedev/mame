e71a5bcba538619f88983a412e68232f7881fdef
angelosa <salese_corp_ltd@email.it>
2018-04-18 00:57:58 +0200

    Note about F1 Dream ending (nw)

--- src/mame/drivers/tigeroad.cpp
+++ src/mame/drivers/tigeroad.cpp
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:Phil Stroffolino
+// copyright-holders:Phil Stroffolino, Bryan McPhail
 /***************************************************************************
 
 Tiger Road     (C) 1987 Capcom (licensed to Romstar)
@@ -11,6 +11,14 @@ Bouncing Balls (c) 1991 Comad
 
 Please contact Phil Stroffolino (phil@maya.com) if there are any questions
 regarding this driver.
+
+TODO:
+- F1 Dream throws an address error if player wins all the races (i.e. when the 
+  game is supposed to give an ending):
+  010C68: 102E 001C      move.b  ($1c,A6), D0       ; reads 0xf from work RAM (misaligned)
+  010C6C: 207B 000E      movea.l ($e,PC,D0.w), A0   ; table from 0x10c7c onward
+  010C70: 4E90           jsr     (A0)               ; throws address error here
+  None of the available 5 vectors seems to fit here, btanb? 
 
 **************************************************************************
 
