aa44132bcb0da64895a5c9ea712d1e24c92d4288
Bavarese <Bavarese@users.noreply.github.com>
2018-04-17 20:05:02 +0200

    Update lbaenhancer.cpp

--- src/devices/bus/isa/lbaenhancer.cpp
+++ src/devices/bus/isa/lbaenhancer.cpp
@@ -22,9 +22,8 @@
       then repartition and reformat (Fdisk and Format). Data is lost.
 
 	Other notes: the ROM bank should be excluded in EMM386.
-				 C8000 - CBFFF is sometimes occupied (by graphics). 
-
-				 Changes to the ROM location require a hard reset.
+                     C8000 - CBFFF is sometimes occupied (by graphics). 
+                     Changes to the ROM location require a hard reset!
 				 
     Requirements:
       Compatible OS (DOS >= 6.22) and LBA capable drive(s).
