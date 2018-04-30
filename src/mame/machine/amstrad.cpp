f0e300e234888cde54fde9cb65f197f253f58a83
mahlemiut <bsr@xnet.co.nz>
2018-05-01 00:25:17 +1200

    amstrad: restore CPR bank mask, force it to be based on the size of an actual ROM

--- src/mame/machine/amstrad.cpp
+++ src/mame/machine/amstrad.cpp
@@ -2936,7 +2936,14 @@ void amstrad_state::enumerate_roms()
 	if (m_system_type == SYSTEM_PLUS || m_system_type == SYSTEM_GX4000)
 	{
 		uint8_t *crt = m_region_cart->base();
-		int bank_num = (m_cart->get_rom_size() / 0x4000);
+		int bank_mask = 0x7ffff;
+
+		if (m_cart->get_rom_size() <= 0x20000)
+			bank_mask = 0x1ffff;
+		else if (m_cart->get_rom_size() <= 0x40000)
+			bank_mask = 0x3ffff;
+		else if (m_cart->get_rom_size() <= 0x80000)
+			bank_mask = 0x7ffff;
 
 		/* ROMs are stored on the inserted cartridge in the Plus/GX4000 */
 		for (int i = 0; i < 128; i++) // fill ROM table
@@ -2946,7 +2953,7 @@ void amstrad_state::enumerate_roms()
 
 		for(int i = 128; i < 160; i++)
 		{
-			m_Amstrad_ROM_Table[i] = &crt[((i - 128) % bank_num) * 0x4000];
+			m_Amstrad_ROM_Table[i] = &crt[((i - 128) & bank_mask) * 0x4000];
 		}
 		m_Amstrad_ROM_Table[7] = &crt[0xc000];
 		slot7 = true;
