6762946a67a2c5d2a8755f3e9150bf68e1361329
Ivan Vangelista <mesgnet@yahoo.it>
2018-05-15 19:15:20 +0200

    gatron.cpp: output_finder (nw)

--- src/mame/video/gatron.cpp
+++ src/mame/video/gatron.cpp
@@ -23,16 +23,14 @@
 #include "includes/gatron.h"
 
 
-WRITE8_MEMBER(gatron_state::gat_videoram_w)
+WRITE8_MEMBER(gatron_state::videoram_w)
 {
-	uint8_t *videoram = m_videoram;
-	videoram[offset] = data;
+	m_videoram[offset] = data;
 	m_bg_tilemap->mark_tile_dirty(offset);
 }
 
 TILE_GET_INFO_MEMBER(gatron_state::get_bg_tile_info)
 {
-	uint8_t *videoram = m_videoram;
 /*  - bits -
     7654 3210
     xxxx xxxx   tiles code.
@@ -40,22 +38,20 @@ TILE_GET_INFO_MEMBER(gatron_state::get_bg_tile_info)
     only one color code
 */
 
-	int code = videoram[tile_index];
+	int code = m_videoram[tile_index];
 
 	SET_TILE_INFO_MEMBER(0, code, 0, 0);
 }
 
 void gatron_state::video_start()
 {
-	m_bg_tilemap = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(FUNC(gatron_state::get_bg_tile_info),this), TILEMAP_SCAN_COLS, 8, 16, 48, 16);
+	m_lamps.resolve();
+
+	m_bg_tilemap = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(FUNC(gatron_state::get_bg_tile_info), this), TILEMAP_SCAN_COLS, 8, 16, 48, 16);
 }
 
-uint32_t gatron_state::screen_update_gat(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect)
+uint32_t gatron_state::screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect)
 {
 	m_bg_tilemap->draw(screen, bitmap, cliprect, 0, 0);
 	return 0;
-}
-
-PALETTE_INIT_MEMBER(gatron_state, gatron)
-{
 }
