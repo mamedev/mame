7698173e295c217b9ace663e7e59f9a4549ef862
Sven Schnelle <svens@stackframe.org>
2018-05-28 22:28:44 +0200

    Add HP98543 medium-res color graphics card (#3575)
    * Remove vram_rw and ctrl_rw wrapper functions
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    
    * topcat: replace hardcoded display width by m_fb_width
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    
    * topcat: prepare topcat for multi plane operation
    
    framebuffer vram is only 1 bit wide.
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    
    * 98544: set single topcat plane
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    
    * add nereid mapper used on HP98543/98545 graphic cards
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    
    * prepare for multi-topcat operation
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    
    * add HP 98543 4 plane graphics card
    
    * hp9k_3xx: add hp98543 card
    
    * topcat,nerid: disable VERBOSE
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    
    * hp98543: use required_device_array
    
    * 98543: use required_region_ptr
    
    * 98544: use required_region_ptr
    
    * topcat: add save state support
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    
    * nereid: add save state support
    
    * hp98544,hp98543,topcat: use shared memory space
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    
    * nereid: remove redundant variable
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    
    * topcat, nereid: remove trailing underscore from include guard
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    
    * topcat,hp98543: code style fixes proposed in PR
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>

--- src/devices/bus/hp_dio/hp98543.h
+++ src/devices/bus/hp_dio/hp98543.h
@@ -1,53 +1,59 @@
 // license:BSD-3-Clause
-// copyright-holders:R. Belmont
+// copyright-holders:Sven Schnelle
 
-#ifndef MAME_BUS_HPDIO_98544_H
-#define MAME_BUS_HPDIO_98544_H
+#ifndef MAME_BUS_HPDIO_98543_H
+#define MAME_BUS_HPDIO_98543_H
 
 #pragma once
 
 #include "hp_dio.h"
 #include "video/topcat.h"
-//**************************************************************************
-//  TYPE DEFINITIONS
-//**************************************************************************
+#include "video/nereid.h"
+#include "machine/ram.h"
 
-// ======================> dio16_98544_device
-
-class dio16_98544_device :
+class dio16_98543_device :
 	public device_t,
 	public device_dio16_card_interface,
 	public device_memory_interface
 {
 public:
-	// construction/destruction
-	dio16_98544_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+	dio16_98543_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
 
 	DECLARE_READ16_MEMBER(rom_r);
 	DECLARE_WRITE16_MEMBER(rom_w);
 
-	required_device<topcat_device> m_topcat;
-	uint32_t screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
- protected:
-	dio16_98544_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock);
+	DECLARE_READ16_MEMBER(ctrl_r);
+	DECLARE_WRITE16_MEMBER(ctrl_w);
+	DECLARE_READ16_MEMBER(vram_r);
+	DECLARE_WRITE16_MEMBER(vram_w);
 
-	// device-level overrides
+	required_device_array<topcat_device, 4> m_topcat;
+	required_device<nereid_device> m_nereid;
+
+	uint32_t screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+protected:
+	dio16_98543_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock);
+
 	virtual void device_start() override;
 	virtual void device_reset() override;
 
-	// optional information overrides
 	virtual void device_add_mconfig(machine_config &config) override;
 	virtual const tiny_rom_entry *device_rom_region() const override;
+
 	virtual space_config_vector memory_space_config() const override;
 private:
 	const address_space_config m_space_config;
 	void map(address_map &map);
 
+	static constexpr int m_h_pix = 1024;
+	static constexpr int m_v_pix = 400;
+
 	required_region_ptr<uint8_t> m_rom;
 	required_shared_ptr<uint8_t> m_vram;
+
 };
 
 // device type definition
-DECLARE_DEVICE_TYPE(HPDIO_98544, dio16_98544_device)
+DECLARE_DEVICE_TYPE(HPDIO_98543, dio16_98543_device)
 
-#endif // MAME_BUS_HPDIO_98544_H
+#endif // MAME_BUS_HPDIO_98543_H
