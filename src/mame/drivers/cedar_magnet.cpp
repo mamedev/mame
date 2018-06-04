e82b476e40baac3dd41197cf6ab2b83412bee70d
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2018-06-04 12:47:37 +0100

    typ

--- src/mame/drivers/cedar_magnet.cpp
+++ src/mame/drivers/cedar_magnet.cpp
@@ -866,8 +866,8 @@ ROM_END
 
 ROM_START( mag_war )
 	BIOS_ROM
-	/* Data after 0xd56b0 would did not read consistently, however the game only appears to use the first 24 tracks (up to 0x48fff)
-	   as it loads once on startup, not during gameplay and all tracks before that gave consistent reads.  There is data after this
+	/* Data after 0xd56b0 would not read consistently, however the game only appears to use the first 24 tracks (up to 0x48fff)
+	   as it loads once on startup, not during gameplay, and all tracks before that gave consistent reads.  There is data after this
 	   point but it is likely leftovers from another game / whatever was on the disk before, so for our purposes this should be fine.
 
 	   Some bullets do seem to spawn from locations where there are no enemies, but I think this is just annoying game design.
