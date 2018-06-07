48ae202b1e732a08f140c4d594b7dbe15fe1d421
cracyc <cracyc@users.noreply.github.com>
2018-06-06 21:09:41 -0500

    plugins/discord: use domain sockets and pipes [Carl]

--- plugins/discord/init.lua
+++ plugins/discord/init.lua
@@ -10,76 +10,77 @@ exports.author = { name = "Carl" }
 local discord = exports
 
 function discord.startplugin()
-	local socket = emu.file("rw")
+	local pipe = emu.file("rw")
+	local json = require("json")
 	local nonce = 1
 	local starttime = 0
 
 	local function init()
-		local err = socket:open("socket.127.0.0.1:6463")
+		local path
+		if package.config:sub(1,1) == '\\' then
+			path = "\\\\.\\pipe\\discord-ipc-0"
+		else
+			path = os.getenv("XDG_RUNTIME_DIR") or os.getenv("TMPDIR") or os.getenv("TMP") or os.getenv("TEMP") or '/tmp'
+			path = "domain." .. path .. "/discord-ipc-0"
+		end
+		local err = pipe:open(path)
 		if err then
 			error("discord: unable to connect, " .. err .. "\n")
 		end
-		socket:write("GET /\n\n")
+		local output = json.stringify({v = 1, client_id = "453309506152169472"})
+		--print(output)
+		pipe:write(string.pack("<I4I4", 0, #output) .. output)
 		local time = os.time()
 		local data = ""
 		repeat
-			local res = socket:read(100)
+			local res = pipe:read(100)
 			data = data .. res
 		until #res == 0 and #data > 0 or time + 1 < os.time()
 		--print(data)
-		if not data:find("Authorization Required", 1, true) then
-			error("discord: bad RPC reply\n")
+		if data:find("code", 1, true) then
+			error("discord: bad RPC reply, " .. data:sub(8) .. "\n")
 		end
 	end
 
 	local function update(stop)
-		local json = require("json")
 		local activity
-		if not stop then
-			local running = emu.romname() ~= "___empty"
-			local state = not running and "In menu" or "Playing"
-			local details = running and manager:machine():system().description or nil
-			if emu.softname() ~= "" then
-				for name, dev in pairs(manager:machine().images) do
-					if dev:longname() then
-						details = details .. " (" .. dev:longname() .. ")"
-						break
-					end
+		local running = emu.romname() ~= "___empty"
+		local state = not running and "In menu" or "Playing"
+		local details = running and manager:machine():system().description or nil
+		if emu.softname() ~= "" then
+			for name, dev in pairs(manager:machine().images) do
+				if dev:longname() then
+					details = details .. " (" .. dev:longname() .. ")"
+					break
 				end
 			end
-			activity = {
-				state = state,
-				details = details,
-				timestamps = {
-					start = starttime
-				}
-			}
 		end
+		activity = {
+			state = state,
+			details = details,
+			timestamps = {
+				start = starttime
+			}
+		}
 		local status = {
 			cmd = "SET_ACTIVITY",
 			args = {
-				pid = 1234, -- we have no access to the pid here, except on linux
+				pid = emu.pid(),
 				activity = activity,
 			},
-			nonce = tostring(nonce)
+			nonce = nonce
 		}
 		nonce = nonce + 1
-		local err = socket:open("socket.127.0.0.1:6463")
-		if err then
-			return
-		end
 		local output = json.stringify(status)
-		output = "POST /rpc?v=1&client_id=453309506152169472\nContent-Type: application/json\nContent-Length: "
-						.. tostring(#output) .. "\n\n" .. output
 		--print(output)
-		socket:write(output)
+		pipe:write(string.pack("<I4I4", 1, #output) .. output)
 		local time = os.time()
 		local data = ""
 		repeat
-			local res = socket:read(100)
+			local res = pipe:read(100)
 			data = data .. res
 		until #res == 0 and #data > 0 or time + 1 < os.time()
-		--print(data)
+		print(data)
 	end
 		
 	do
