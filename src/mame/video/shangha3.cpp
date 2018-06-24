f871b84c9ea89f444b8c546e6878803d6b6d72e8
Vas Crabb <vas@vastheman.com>
2018-06-24 20:52:40 +1000

    small cleanup (nw)

--- src/mame/video/shangha3.cpp
+++ src/mame/video/shangha3.cpp
@@ -144,8 +144,8 @@ WRITE16_MEMBER(shangha3_state::blitter_go_w)
 		{
 			rectangle myclip;
 
-//			if (shangha3_ram[offs+11] || shangha3_ram[offs+12])
-//			logerror("offs %04x: sx %04x sy %04x zoom %04x %04x %04x %04x fx %d fy %d\n",offs,sx,sy,zoomx,shangha3_ram[offs+11]),shangha3_ram[offs+12],zoomy,flipx,flipy);
+//          if (shangha3_ram[offs+11] || shangha3_ram[offs+12])
+//              logerror("offs %04x: sx %04x sy %04x zoom %04x %04x %04x %04x fx %d fy %d\n",offs,sx,sy,zoomx,shangha3_ram[offs+11]),shangha3_ram[offs+12],zoomy,flipx,flipy);
 
 			myclip.set(sx, sx + sizex, sy, sy + sizey);
 			myclip &= rawbitmap.cliprect();
