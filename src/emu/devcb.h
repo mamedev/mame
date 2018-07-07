c8f77c0d3d78cc8943d6cd0e18f3e03b8e0f9b41
AJR <ajrhacker@users.noreply.github.com>
2018-07-07 17:46:49 -0400

    devcb.h: Further attempted GCC build fixing (nw)

--- src/emu/devcb.h
+++ src/emu/devcb.h
@@ -640,7 +640,7 @@ private:
 			, m_delegate(std::forward<T>(func), name, tag, std::add_pointer_t<devcb_read::delegate_device_class_t<T> >(nullptr))
 		{ }
 		template <typename T>
-		delegate_builder(devcb_read &target, bool append, device_t &devbase, typename devcb_read::delegate_device_class_t<T> &obj, T &&func, char const *name)
+		delegate_builder(devcb_read &target, bool append, device_t &devbase, devcb_read::template delegate_device_class_t<T> &obj, T &&func, char const *name)
 			: builder_base(target, append)
 			, transform_base<output_t, delegate_builder>(DefaultMask & delegate_traits<Delegate>::default_mask)
 			, m_devbase(devbase)
