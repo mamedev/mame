d057dca066e7b80d24484a7f568cf48595f79cfd
Robbbert <Robbbert@users.noreply.github.com>
2018-07-21 00:25:49 +1000

    trs80 work in progress - split into separate sources - fixed paste - added uart and rs232 - added baud rate clock or brg depending on hardware - fixed uart status on sys80 - fixed cassette waveform - lots of tidy up

--- src/mame/includes/trs80m3.h
+++ src/mame/includes/trs80m3.h
@@ -1,14 +1,15 @@
 // license:BSD-3-Clause
-// copyright-holders:Juergen Buchmueller, Robbbert
+// copyright-holders:Robbbert
 //*****************************************************************************
 
-#ifndef MAME_INCLUDES_TRS80_H
-#define MAME_INCLUDES_TRS80_H
+#ifndef MAME_INCLUDES_TRS80M3_H
+#define MAME_INCLUDES_TRS80M3_H
 
 #pragma once
 
 #include "bus/centronics/ctronics.h"
 #include "cpu/z80/z80.h"
+#include "machine/ram.h"
 #include "machine/bankdev.h"
 #include "imagedev/cassette.h"
 #include "imagedev/flopdrv.h"
@@ -25,17 +26,16 @@
 #include "formats/trs_cas.h"
 
 
-class trs80_state : public driver_device
+class trs80m3_state : public driver_device
 {
 public:
-	trs80_state(const machine_config &mconfig, device_type type, const char *tag)
+	trs80m3_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag)
 		, m_maincpu(*this, "maincpu")
 		, m_region_maincpu(*this, "maincpu")
 		, m_p_chargen(*this, "chargen")
 		, m_p_videoram(*this, "videoram")
-		, m_p_gfxram(*this, "gfxram")  // LNW80 only
-		, m_lnw_bank(*this, "lnw_banked_mem")  // LNW80 only
+		, m_bankdev(*this, "bankdev")
 		, m_centronics(*this, "centronics")
 		, m_cent_data_out(*this, "cent_data_out")
 		, m_cent_status_in(*this, "cent_status_in")
@@ -48,73 +48,83 @@ public:
 		, m_floppy3(*this, "fdc:3")
 		, m_speaker(*this, "speaker")
 		, m_cassette(*this, "cassette")
-		, m_io_baud(*this, "BAUD")
 		, m_io_config(*this, "CONFIG")
 		, m_io_keyboard(*this, "LINE%u", 0)
+		, m_mainram(*this, RAM_TAG)
+		, m_m4_bank(*this, "m4_banked_mem")
+		, m_m4p_bank(*this, "m4p_banked_mem")
+		, m_32kbanks(*this, "bank%u", 0U)
+		, m_16kbank(*this, "16kbank")
+		, m_vidbank(*this, "vidbank")
 		{ }
 
-	void sys80(machine_config &config);
-	void trs80(machine_config &config);
-	void lnw80(machine_config &config);
-	void radionic(machine_config &config);
-	void model1(machine_config &config);
-	void ht1080z(machine_config &config);
+	void model4p(machine_config &config);
+	void model3(machine_config &config);
+	void cp500(machine_config &config);
+	void model4(machine_config &config);
 
-	void init_trs80l2();
-	void init_trs80();
+	void init_trs80m3();
+	void init_trs80m4();
+	void init_trs80m4p();
 
 private:
 	DECLARE_FLOPPY_FORMATS(floppy_formats);
 	DECLARE_WRITE8_MEMBER(port_ff_w);
-	DECLARE_WRITE8_MEMBER(lnw80_fe_w);
-	DECLARE_WRITE8_MEMBER(sys80_fe_w);
-	DECLARE_WRITE8_MEMBER(sys80_f8_w);
+	DECLARE_WRITE8_MEMBER(port_f4_w);
+	DECLARE_WRITE8_MEMBER(port_ec_w);
 	DECLARE_WRITE8_MEMBER(port_ea_w);
 	DECLARE_WRITE8_MEMBER(port_e8_w);
-	DECLARE_READ8_MEMBER(lnw80_fe_r);
+	DECLARE_WRITE8_MEMBER(port_e4_w);
+	DECLARE_WRITE8_MEMBER(port_e0_w);
+	DECLARE_WRITE8_MEMBER(port_9c_w);
+	DECLARE_WRITE8_MEMBER(port_90_w);
+	DECLARE_WRITE8_MEMBER(port_88_w);
+	DECLARE_WRITE8_MEMBER(port_84_w);
 	DECLARE_READ8_MEMBER(port_ff_r);
-	DECLARE_READ8_MEMBER(sys80_f9_r);
+	DECLARE_READ8_MEMBER(port_ec_r);
 	DECLARE_READ8_MEMBER(port_ea_r);
 	DECLARE_READ8_MEMBER(port_e8_r);
-	DECLARE_READ8_MEMBER(irq_status_r);
+	DECLARE_READ8_MEMBER(port_e4_r);
+	DECLARE_READ8_MEMBER(port_e0_r);
 	DECLARE_READ8_MEMBER(printer_r);
 	DECLARE_WRITE8_MEMBER(printer_w);
-	DECLARE_WRITE8_MEMBER(cassunit_w);
-	DECLARE_WRITE8_MEMBER(motor_w);
 	DECLARE_READ8_MEMBER(keyboard_r);
 	DECLARE_READ8_MEMBER(wd179x_r);
+	DECLARE_READ8_MEMBER(cp500_port_f4_r);
 
 	INTERRUPT_GEN_MEMBER(rtc_interrupt);
 	INTERRUPT_GEN_MEMBER(fdc_interrupt);
 	TIMER_CALLBACK_MEMBER(cassette_data_callback);
 	DECLARE_WRITE_LINE_MEMBER(intrq_w);
-	DECLARE_QUICKLOAD_LOAD_MEMBER( trs80_cmd );
-	DECLARE_MACHINE_RESET(lnw80);
-	DECLARE_PALETTE_INIT(lnw80);
-	uint32_t screen_update_trs80(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_ht1080z(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_lnw80(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	uint32_t screen_update_radionic(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	DECLARE_WRITE_LINE_MEMBER(drq_w);
+	DECLARE_QUICKLOAD_LOAD_MEMBER(trs80_cmd);
+	DECLARE_MACHINE_RESET(trs80m3);
+	DECLARE_MACHINE_RESET(cp500);
+	uint32_t screen_update_trs80m3(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 
-	void lnw80_io(address_map &map);
-	void lnw80_mem(address_map &map);
-	void lnw_banked_mem(address_map &map);
-	void m1_io(address_map &map);
-	void m1_mem(address_map &map);
-	void sys80_io(address_map &map);
-	void trs80_io(address_map &map);
-	void trs80_mem(address_map &map);
+	void cp500_io(address_map &map);
+	void m3_io(address_map &map);
+	void m3_mem(address_map &map);
+	void m4_io(address_map &map);
+	void m4p_io(address_map &map);
+	void m4_banked_mem(address_map &map);
+	void m4p_banked_mem(address_map &map);
 
+	uint8_t m_model4;
 	uint8_t m_mode;
 	uint8_t m_irq;
 	uint8_t m_mask;
-	uint8_t m_tape_unit;
+	uint8_t m_nmi_mask;
+	uint8_t m_port_ec;
 	bool m_reg_load;
-	u8 m_lnw_mode;
-	bool m_cassette_data;
+	uint8_t m_nmi_data;
+	uint8_t m_cassette_data;
 	emu_timer *m_cassette_data_timer;
 	double m_old_cassette_val;
+	uint16_t m_start_address;
+	uint8_t m_crtc_reg;
 	uint8_t m_size_store;
+	bool m_a11_flipflop;
 	uint16_t m_timeout;
 	floppy_image_device *m_floppy;
 	virtual void machine_start() override;
@@ -123,8 +133,7 @@ private:
 	required_memory_region m_region_maincpu;
 	required_region_ptr<u8> m_p_chargen;
 	optional_shared_ptr<u8> m_p_videoram;
-	optional_shared_ptr<u8> m_p_gfxram;
-	optional_device<address_map_bank_device> m_lnw_bank;
+	optional_device<address_map_bank_device> m_bankdev;
 	optional_device<centronics_device> m_centronics;
 	optional_device<output_latch_device> m_cent_data_out;
 	optional_device<input_buffer_device> m_cent_status_in;
@@ -137,9 +146,14 @@ private:
 	optional_device<floppy_connector> m_floppy3;
 	required_device<speaker_sound_device> m_speaker;
 	required_device<cassette_image_device> m_cassette;
-	optional_ioport m_io_baud;
 	optional_ioport m_io_config;
 	required_ioport_array<8> m_io_keyboard;
+	optional_device<ram_device>                 m_mainram;
+	optional_device<address_map_bank_device>    m_m4_bank;
+	optional_device<address_map_bank_device>    m_m4p_bank;
+	optional_memory_bank_array<2>               m_32kbanks;
+	optional_memory_bank                        m_16kbank;
+	optional_memory_bank                        m_vidbank;
 };
 
-#endif // MAME_INCLUDES_TRS80_H
+#endif // MAME_INCLUDES_TRS80M3_H
