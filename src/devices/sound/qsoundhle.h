31448b29021205b8d42276703bdd188492bfdbdb
superctr <themvrules@gmail.com>
2018-08-02 23:05:25 +0200

    Add improved qsound_hle core

--- src/devices/sound/qsoundhle.h
+++ src/devices/sound/qsoundhle.h
@@ -1,8 +1,8 @@
 // license:BSD-3-Clause
-// copyright-holders:Paul Leaman, Miguel Angel Horna
+// copyright-holders:superctr, Valley Bell
 /*********************************************************
 
-    Capcom System QSound™ (HLE)
+  Capcom QSound DL-1425 (HLE)
 
 *********************************************************/
 #ifndef MAME_SOUND_QSOUNDHLE_H
@@ -24,6 +24,7 @@ public:
 
 protected:
 	// device_t implementation
+	tiny_rom_entry const *device_rom_region() const override;
 	virtual void device_start() override;
 	virtual void device_reset() override;
 
@@ -34,23 +35,114 @@ protected:
 	virtual void rom_bank_updated() override;
 
 private:
+	struct qsound_voice {
+		uint16_t bank;
+		int16_t addr; // top word is the sample address
+		uint16_t phase;
+		uint16_t rate;
+		int16_t loop_len;
+		int16_t end_addr;
+		int16_t volume;
+		int16_t echo;
+	};
+
+	struct qsound_adpcm {
+		uint16_t start_addr;
+		uint16_t end_addr;
+		uint16_t bank;
+		int16_t volume;
+		uint16_t flag;
+		int16_t cur_vol;
+		int16_t step_size;
+		uint16_t cur_addr;
+	};
+
+	// Q1 Filter
+	struct qsound_fir {
+		int tap_count;	// usually 95
+		int delay_pos;
+		int16_t table_pos;
+		int16_t taps[95];
+		int16_t delay_line[95];
+	};
+
+	// Delay line
+	struct qsound_delay {
+		int16_t delay;
+		int16_t volume;
+		int16_t write_pos;
+		int16_t read_pos;
+		int16_t delay_line[51];
+	};
+
+	struct qsound_echo {
+		uint16_t end_pos;
+
+		int16_t feedback;
+		int16_t length;
+		int16_t last_sample;
+		int16_t delay_line[1024];
+		int16_t delay_pos;
+	};
+
 	// MAME resources
 	sound_stream *m_stream;
 
-	struct qsound_channel
-	{
-		u16 reg[8];    // channel control registers
+	uint16_t m_data_latch;
+	int16_t m_out[2];
 
-		// work variables
-		int lvol;           // left volume
-		int rvol;           // right volume
-	} m_channel[16];
+	int16_t m_pan_tables[2][2][98];
+	int16_t m_adpcm_shift[24];
+	uint16_t m_filter_data[5][95];
+	uint16_t m_filter_data2[209]; // Overlapping data (95+19+95)
 
-	int m_pan_table[33];    // pan volume table
-	u16 m_data;        // register latch data
+	qsound_voice m_voice[16];
+	qsound_adpcm m_adpcm[3];
 
-	inline s16 read_sample(u32 offset) { return u16(read_byte(offset)) << 8; }
-	void write_data(u8 address, u16 data);
+	uint16_t m_voice_pan[16+3];
+	int16_t m_voice_output[16+3];
+
+	qsound_echo m_echo;
+
+	qsound_fir m_filter[2];
+	qsound_fir m_alt_filter[2];
+
+	qsound_delay m_wet[2];
+	qsound_delay m_dry[2];
+
+	uint16_t m_state;
+	uint16_t m_next_state;
+
+	uint16_t m_delay_update;
+
+	int m_state_counter;
+	int m_ready_flag;
+
+	uint16_t *register_map[256];
+
+	void write_data(uint8_t addr, uint16_t data);
+	uint16_t read_data(uint8_t addr);
+
+	void init_register_map();
+	void update_sample();
+
+	// DSP states
+	void state_init();
+	void state_refresh_filter_1();
+	void state_refresh_filter_2();
+	void state_normal_update();
+
+	// sub functions
+	inline int16_t get_sample(uint16_t bank,uint16_t address);
+	inline int16_t* get_filter_table(uint16_t offset);
+
+	inline int16_t pcm_update(struct qsound_voice *v, int32_t *echo_out);
+	inline void adpcm_update(int voice_no, int nibble);
+
+	inline int16_t echo(struct qsound_echo *r,int32_t input);
+	inline int32_t fir(struct qsound_fir *f, int16_t input);
+	inline int32_t delay(struct qsound_delay *q, int32_t input);
+	inline void delay_update(struct qsound_delay *q);
 };
 
 DECLARE_DEVICE_TYPE(QSOUND_HLE, qsound_hle_device)
