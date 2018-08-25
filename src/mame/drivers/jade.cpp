b83c31c7b900d2f6afeba2517a6d5d2fea4bf014
AJR <ajrhacker@users.noreply.github.com>
2018-08-25 18:48:43 -0400

    jade: "make sure you remove all reference to jgz80, because we *know* that's wrong" [Robbbert] (nw)

--- src/mame/drivers/jade.cpp
+++ src/mame/drivers/jade.cpp
@@ -91,7 +91,7 @@ MACHINE_CONFIG_END
 /* ROM definition */
 ROM_START( jade )
 	ROM_REGION( 0x800, "roms", 0 )
-	ROM_LOAD( "jgz80.rom",   0x0000, 0x0800, CRC(90c4a1ef) SHA1(8a93a11051cc27f3edca24f0f4297ebe0099964e) )
+	ROM_LOAD( "jade.rom", 0x0000, 0x0800, CRC(90c4a1ef) SHA1(8a93a11051cc27f3edca24f0f4297ebe0099964e) )
 ROM_END
 
 /* Driver */
