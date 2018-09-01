42d424bf914692c1487708633fed44d1a3bf926c
smf- <smf-@users.noreply.github.com>
2018-09-01 09:40:25 +0100

    mcr68: mapped 6840 to lower data bits on all mcr68 games, which matches the blasted schematics. I used cswidth as different games write at odd or even addresses, even though the boards look visually similar. Byte writes will work in either case as the 68000 puts the same data on the upper and lower part of the bus. Reads are a mix of odd and even addresses, International team laser and spy hunter 2 exclusively read at even addresses. It's unclear whether this is a bug in those games or not. [smf]

--- src/mame/drivers/mcr68.cpp
+++ src/mame/drivers/mcr68.cpp
@@ -284,7 +284,7 @@ void mcr68_state::mcr68_map(address_map &map)
 	map(0x071000, 0x071fff).ram();
 	map(0x080000, 0x080fff).ram().share("spriteram");
 	map(0x090000, 0x09007f).w("palette", FUNC(palette_device::write16)).share("palette");
-	map(0x0a0000, 0x0a000f).rw(m_ptm, FUNC(ptm6840_device::read), FUNC(ptm6840_device::write)).umask16(0xff00);
+	map(0x0a0000, 0x0a000f).rw(m_ptm, FUNC(ptm6840_device::read), FUNC(ptm6840_device::write)).umask16(0x00ff).cswidth(16);
 	map(0x0b0000, 0x0bffff).w("watchdog", FUNC(watchdog_timer_device::reset16_w));
 	map(0x0d0000, 0x0dffff).portr("IN0");
 	map(0x0e0000, 0x0effff).portr("IN1");
@@ -1523,9 +1523,6 @@ void mcr68_state::init_blasted()
 
 	/* handle control writes */
 	m_maincpu->space(AS_PROGRAM).install_write_handler(0x0c0000, 0x0cffff, write16_delegate(FUNC(mcr68_state::blasted_control_w),this));
-
-	/* 6840 is mapped to the lower 8 bits */
-	m_maincpu->space(AS_PROGRAM).install_readwrite_handler(0x0a0000, 0x0a000f, read8_delegate(FUNC(ptm6840_device::read), &(*m_ptm)), write8_delegate(FUNC(ptm6840_device::write), &(*m_ptm)), 0x00ff);
 }
 
 void mcr68_state::init_intlaser()
@@ -1554,9 +1551,6 @@ void mcr68_state::init_archrivl()
 
 	/* 49-way joystick handling is a bit tricky */
 	m_maincpu->space(AS_PROGRAM).install_read_handler(0x0e0000, 0x0effff, read16_delegate(FUNC(mcr68_state::archrivl_port_1_r),this));
-
-	/* 6840 is mapped to the lower 8 bits */
-	m_maincpu->space(AS_PROGRAM).install_readwrite_handler(0x0a0000, 0x0a000f, read8_delegate(FUNC(ptm6840_device::read), &(*m_ptm)), write8_delegate(FUNC(ptm6840_device::write), &(*m_ptm)), 0x00ff);
 }
 
 READ16_MEMBER(mcr68_state::archrivlb_port_1_r)
@@ -1576,9 +1570,6 @@ void mcr68_state::init_archrivlb()
 
 	/* 49-way joystick replaced by standard 8way stick */
 	m_maincpu->space(AS_PROGRAM).install_read_handler(0x0e0000, 0x0effff, read16_delegate(FUNC(mcr68_state::archrivlb_port_1_r),this));
-
-	/* 6840 is mapped to the lower 8 bits */
-	m_maincpu->space(AS_PROGRAM).install_readwrite_handler(0x0a0000, 0x0a000f, read8_delegate(FUNC(ptm6840_device::read), &(*m_ptm)), write8_delegate(FUNC(ptm6840_device::write), &(*m_ptm)), 0x00ff);
 }
 
 
