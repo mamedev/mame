c56e8d8b838855757e4664fe267142867d996661
hap <happppp@users.noreply.github.com>
2018-09-14 13:52:01 +0200

    namcoic: move to drivers/namcos2 (nw)

--- src/mame/drivers/namcos2.cpp
+++ src/mame/drivers/namcos2.cpp
@@ -447,6 +447,98 @@ Again this board has a different graphics layout, also the protection checks
 are done at $a00000 as opposed to $d00000 on a standard board. Similar
 $a00000 checks have been seen on the Final Lap boards.
 
+
+Custom Chips Notes (moved to here from Stroff's old namcoic.c)
+==================
+
+System 21 here presumably refers to the Winning Run PCB, not the later games?
+
+Custom Chips:                       Final Lap   Assault     LuckyWld    System21    NA1/2       NB1/2
+    C45     Land Generator            *                       *
+    C65     I/O Controller (older)    *           *
+    C67     TMS320C25 (DSP int rom)
+	C68     I/O Controller (newer)                            *           *
+    C70                                                                               *
+    C95                               *           *
+    C102    ROZ:Memory Access Control             *
+    C106    OBJ:X-Axis Zoom Control   *           *
+    C107    Land Line Buffer          *
+    C116    Screen Waveform Generator *           *           *                                   *
+    C121    Yamaha YM2151 Sound Gen   *           *           *
+    C123    GFX:Tile Mem Decoder      *           *           *                                   *
+    C134    OBJ:Address Generator     *           *
+    C135    OBJ:Line matching         *           *
+    C137    Clock Generator IC        *           *           *           *                       *
+    C138                                                                  *
+    C139    Serial I/F Controller     *           *           *           *
+    C140    24 Channel PCM            *           *           *
+    C145    GFX:Tile Memory Access    *           *           *                                   *
+    C146    OBJ:Line Buf Steering     *           *
+    C148    CPU Bus Manager           *           *           *           *
+    C149    Mouse/Trackball Decoder   *           *           *           *
+    C156    Pixel Stream Combo        *           *           *                                   *
+    C160    Control                                                                               *
+    C165                                                                  *
+    C169    ROZ(B)                                            *                                   *
+    C187                                                      *           *                       *
+    C210                                                                              *
+    C215                                                                              *
+    C218                                                                              *
+    C219                                                                              *
+    C329    CPU?                                                                                  *
+    C347    GfxObj                                                                                *
+    C352    PCM                                                                                   *
+    C355    Motion Obj(B)                                     *           *                       *
+    C373    LAND-related                                      *
+    C382                                                                                          *
+    C383                                                                                          *
+    C384    GFX(3)                                                                                *
+    C385                                                                                          *
+    C390    Key Custom                                                                            *
+
+
+General Support
+---------------
+C65  - This is the I/O Microcontroller, handles all input/output devices. 63705 uC, CPU4 in Namco System2.
+C137 - Takes System clock and generates all sub-system clocks, doesn't need emulation, not accessed via CPU
+C139 - Serial Interface Controller
+C148 - Does some Memory Decode, Interrupt Handling, 3 bit PIO port, Bus Controller
+C149 - Does decoding of mouse/trackball input streams for the I/O Controller. (Offset Square wave)
+
+
+Tile Fields Static/Scrolled
+---------------------------
+Combination of these two devices and associated RAM & TileGFX produces a pixel stream that is fed
+into the Pixel stream decoder.
+
+C145 - Tile Screen Memory Access controller
+C123 - Tile Memory decoder Part 1, converts X,Y,Tile into character ROM address index
+
+
+Pixel Stream Decode
+-------------------
+These two devices take the pixel streams from the tilefield generator and the associated graphics board
+and combine them to form an RGB data stream that is fed to the monitor.
+
+C156 - Pixel stream combiner
+Takes tile field & graphics board streams and generates the prioritized pixel, then does the lookup to
+go from palettised to 24bit RGB pixel.
+
+C116 - Screen Waveform Generator
+Takes RGB24 pixel stream from C156 and generates the waveform signals for the monitor, also generates
+the line interrupt and controls screen blanking,shift, etc.
+
+Object Control
+--------------
+C106 - Generates memory output clocks to generate X-Axis Zoom for Line Buffer Writes
+C134 - Object Memory Address Generator. Sequences the sprite memory contents to the hardware.
+C135 - Checks is object is displayed on Current output line.
+C146 - Steers the Decode Object Pixel data to the correct line buffer A or B
+
+ROZ
+---
+C102 - Controls CPU access to ROZ Memory Area.
+
 ***************************************************************************/
 
 #include "emu.h"
@@ -4513,7 +4605,7 @@ ROM_START( rthun2j )
 	ROM_REGION( 0x100000, "c140", 0 ) /* Sound voices */
 	ROM_LOAD( "rst1_voi1.bin",  0x000000, 0x080000, CRC(e42027cd) SHA1(fa3a81118c7f112289c27023236dec2e9cbc78b5) )
 	ROM_LOAD( "rst1_voi2.bin",  0x080000, 0x080000, CRC(0c4c2b66) SHA1(7723cbef755439a66d026015596fe1547ccd65b1) )
-		
+
 	/* stuff below isn't used but loaded because it was on the board .. */
 	ROM_REGION( 0x0950, "plds", 0 )
 	ROM_LOAD( "pal12l10.8d",  0x0000, 0x0040, CRC(d3ae64a6) SHA1(8e56f447908246e84d5a79df1a1cd3d5c8a040fb) )
