4d25a5c614735bd19c9262a91e3cd183f8568ecb
fulivi <fulivi@users.noreply.github.com>
2018-09-22 17:53:29 +0200

    hp9825: first release of HP9825B emulator

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -2243,6 +2243,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/hp2620.cpp",
 	MAME_DIR .. "src/mame/drivers/hp700.cpp",
 	MAME_DIR .. "src/mame/drivers/hp2640.cpp",
+	MAME_DIR .. "src/mame/drivers/hp9825.cpp",
 }
 
 createMESSProjects(_target, _subtarget, "hec2hrp")
