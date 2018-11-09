3dcc8ec15583d2f117ee02e20bd44d76e44b7de2
cam900 <dbtlrchl@naver.com>
2018-11-09 10:47:32 +0900

    stadhero.cpp : Minor cleanups, Add notes Removal some MCFGs, Fix some namings, Cleanup gfxdecode, Reduce unnecessary handlers

--- src/mame/includes/stadhero.h
+++ src/mame/includes/stadhero.h
@@ -8,11 +8,11 @@
 class stadhero_state : public driver_device
 {
 public:
-	stadhero_state(const machine_config &mconfig, device_type type, const char *tag)
-		: driver_device(mconfig, type, tag),
+	stadhero_state(const machine_config &mconfig, device_type type, const char *tag) :
+		driver_device(mconfig, type, tag),
 		m_maincpu(*this, "maincpu"),
 		m_audiocpu(*this, "audiocpu"),
-		m_tilegen1(*this, "tilegen1"),
+		m_tilegen(*this, "tilegen"),
 		m_spritegen(*this, "spritegen"),
 		m_gfxdecode(*this, "gfxdecode"),
 		m_soundlatch(*this, "soundlatch"),
@@ -24,7 +24,7 @@ public:
 private:
 	required_device<cpu_device> m_maincpu;
 	required_device<cpu_device> m_audiocpu;
-	required_device<deco_bac06_device> m_tilegen1;
+	required_device<deco_bac06_device> m_tilegen;
 	required_device<deco_mxc06_device> m_spritegen;
 	required_device<gfxdecode_device> m_gfxdecode;
 	required_device<generic_latch_8_device> m_soundlatch;
@@ -34,13 +34,13 @@ private:
 
 	tilemap_t *m_pf1_tilemap;
 
-	DECLARE_WRITE16_MEMBER(stadhero_control_w);
-	DECLARE_WRITE16_MEMBER(stadhero_pf1_data_w);
+	DECLARE_WRITE16_MEMBER(int_ack_w);
+	DECLARE_WRITE16_MEMBER(pf1_data_w);
 
 	virtual void video_start() override;
 
 	TILE_GET_INFO_MEMBER(get_pf1_tile_info);
-	uint32_t screen_update_stadhero(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 	void audio_map(address_map &map);
 	void main_map(address_map &map);
 };
