8b72488a86d9dc13945d5db52826be30de2316d7
AJR <ajrhacker@users.noreply.github.com>
2018-11-15 11:50:17 -0500

    vp122: Merge driver with cit220.cpp (nw)

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -3864,7 +3864,6 @@ files {
 	MAME_DIR .. "src/mame/drivers/vector4.cpp",
 	MAME_DIR .. "src/mame/drivers/vectrix.cpp",
 	MAME_DIR .. "src/mame/drivers/vp60.cpp",
-	MAME_DIR .. "src/mame/drivers/vp122.cpp",
 	MAME_DIR .. "src/mame/includes/vp415.h",
 	MAME_DIR .. "src/mame/drivers/vp415.cpp",
 	MAME_DIR .. "src/mame/drivers/vsmilepro.cpp",
