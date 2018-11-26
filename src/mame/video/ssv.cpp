4ea84e0e96e68be4cda269d8c17c00dcf0c1f1d4
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2018-11-26 15:40:46 +0000

    copyright holders (nw)

--- src/mame/video/ssv.cpp
+++ src/mame/video/ssv.cpp
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:Luca Elia
+// copyright-holders:Luca Elia, Roberto Zandona, David Haywood
 /***************************************************************************
 
                     -= Seta, Sammy, Visco (SSV) System =-
