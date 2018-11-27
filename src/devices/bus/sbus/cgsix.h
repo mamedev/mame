ac6053ecb0c1d2cea91fc2bd4aa72d9e3df6b41f
Vas Crabb <vas@vastheman.com>
2018-11-27 21:52:59 +1100

    metadata and flags (nw)

--- src/devices/bus/sbus/cgsix.h
+++ src/devices/bus/sbus/cgsix.h
@@ -18,6 +18,8 @@
 class sbus_cgsix_device : public device_t, public device_sbus_card_interface
 {
 protected:
+	static constexpr feature_type imperfect_features() { return feature::GRAPHICS; }
+
 	// construction/destruction
 	sbus_cgsix_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock, const uint32_t vram_size)
 		: sbus_cgsix_device(mconfig, type, tag, owner, clock)
