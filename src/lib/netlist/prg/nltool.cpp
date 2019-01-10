a5461d9d6bf214cb5e348c892c72f528a7d237b9
Stiletto <stilett0@users.noreply.github.com>
2019-01-10 17:01:26 -0500

    2018 -> 2019 changes (nw)
    * 2018 -> 2019 changes (nw)

--- src/lib/netlist/prg/nltool.cpp
+++ src/lib/netlist/prg/nltool.cpp
@@ -696,7 +696,7 @@ int tool_app_t::execute()
 	{
 		pout(
 			"nltool (netlist) 0.1\n"
-			"Copyright (C) 2018 Couriersud\n"
+			"Copyright (C) 2019 Couriersud\n"
 			"License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>.\n"
 			"This is free software: you are free to change and redistribute it.\n"
 			"There is NO WARRANTY, to the extent permitted by law.\n\n"
