95491792aa9547b8b16207ae205d897f03bdf6dc
AJR <ajrhacker@users.noreply.github.com>
2019-01-19 16:20:40 -0500

    devices/bus: More floppy MCFG_ removal (nw)

--- src/devices/bus/compucolor/floppy.cpp
+++ src/devices/bus/compucolor/floppy.cpp
@@ -52,9 +52,10 @@ static void compucolor_floppies(device_slot_interface &device)
 //  device_add_mconfig - add device configuration
 //-------------------------------------------------
 
-MACHINE_CONFIG_START(compucolor_floppy_device::device_add_mconfig)
-	MCFG_FLOPPY_DRIVE_ADD_FIXED("floppy", compucolor_floppies, "525sssd", compucolor_floppy_device::floppy_formats)
-MACHINE_CONFIG_END
+void compucolor_floppy_device::device_add_mconfig(machine_config &config)
+{
+	FLOPPY_CONNECTOR(config, m_floppy, compucolor_floppies, "525sssd", compucolor_floppy_device::floppy_formats);
+}
 
 
 
@@ -89,7 +90,7 @@ compucolor_floppy_port_device::compucolor_floppy_port_device(const machine_confi
 compucolor_floppy_device::compucolor_floppy_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
 	: device_t(mconfig, COMPUCOLOR_FLOPPY, tag, owner, clock)
 	, device_compucolor_floppy_port_interface(mconfig, *this)
-	, m_floppy(*this, "floppy:525sssd")
+	, m_floppy(*this, "floppy")
 	, m_rw(1)
 	, m_stp(0)
 	, m_sel(1)
@@ -183,19 +184,23 @@ void compucolor_floppy_device::stepper_w(uint8_t data)
 {
 	if (!m_sel)
 	{
+		floppy_image_device *floppy = m_floppy->get_device();
+		if (floppy == nullptr)
+			return;
+
 		if ((m_stp == 1 && data == 4) || (m_stp == 2 && data == 1) || (m_stp == 4 && data == 2))
 		{
 			// step in
-			m_floppy->dir_w(1);
-			m_floppy->stp_w(0);
-			m_floppy->stp_w(1);
+			floppy->dir_w(1);
+			floppy->stp_w(0);
+			floppy->stp_w(1);
 		}
 		else if ((m_stp == 1 && data == 2) || (m_stp == 2 && data == 4) || (m_stp == 4 && data == 1))
 		{
 			// step out
-			m_floppy->dir_w(0);
-			m_floppy->stp_w(0);
-			m_floppy->stp_w(1);
+			floppy->dir_w(0);
+			floppy->stp_w(0);
+			floppy->stp_w(1);
 		}
 	}
 
@@ -209,7 +214,9 @@ void compucolor_floppy_device::stepper_w(uint8_t data)
 
 void compucolor_floppy_device::select_w(int state)
 {
-	m_floppy->mon_w(state);
+	floppy_image_device *floppy = m_floppy->get_device();
+	if (floppy != nullptr)
+		floppy->mon_w(state);
 
 	if (!m_sel && state)
 	{
@@ -226,8 +233,10 @@ void compucolor_floppy_device::select_w(int state)
 
 bool compucolor_floppy_device::read_bit()
 {
+	floppy_image_device *floppy = m_floppy->get_device();
+
 	attotime when = machine().time();
-	attotime edge = m_floppy->get_next_transition(when);
+	attotime edge = (floppy == nullptr) ? attotime::never : floppy->get_next_transition(when);
 	attotime next = when + m_period;
 
 	return (edge.is_never() || edge >= next) ? 0 : 1;
