1d5acb9c5ae2ca0b9243c595d4bb7d6389e9396b
cam900 <dbtlrchl@naver.com>
2019-01-27 09:56:21 +0900

    dcheese.cpp : Cleanups Remove MCFGs, Runtime tag lookups, Use shorter/correct type values, Fix some namings, Spacings, Reduce unnecessary lines, Defines

--- src/mame/includes/dcheese.h
+++ src/mame/includes/dcheese.h
@@ -11,6 +11,8 @@
 
 #pragma once
 
+#include "cpu/m68000/m68000.h"
+#include "cpu/m6809/m6809.h"
 #include "machine/gen_latch.h"
 #include "sound/bsmt2000.h"
 #include "emupal.h"
@@ -23,6 +25,9 @@ public:
 		driver_device(mconfig, type, tag),
 		m_palrom(*this, "palrom"),
 		m_gfxrom(*this, "gfx"),
+		m_eepromout_io(*this, "EEPROMOUT"),
+		m_2a0002_io(*this, "2a0002"),
+		m_2a000e_io(*this, "2a000e"),
 		m_maincpu(*this, "maincpu"),
 		m_audiocpu(*this, "audiocpu"),
 		m_screen(*this, "screen"),
@@ -48,27 +53,30 @@ protected:
 	virtual void device_timer(emu_timer &timer, device_timer_id id, int param, void *ptr) override;
 
 private:
-	required_region_ptr<uint16_t> m_palrom;
-	required_region_ptr<uint8_t> m_gfxrom;
+	required_region_ptr<u16> m_palrom;
+	required_region_ptr<u8> m_gfxrom;
+	required_ioport m_eepromout_io;
+	required_ioport m_2a0002_io;
+	required_ioport m_2a000e_io;
 
 	/* video-related */
-	uint16_t   m_blitter_color[2];
-	uint16_t   m_blitter_xparam[16];
-	uint16_t   m_blitter_yparam[16];
-	uint16_t   m_blitter_vidparam[32];
+	u16   m_blitter_color[2];
+	u16   m_blitter_xparam[16];
+	u16   m_blitter_yparam[16];
+	u16   m_blitter_vidparam[32];
 
 	std::unique_ptr<bitmap_ind16> m_dstbitmap;
 	emu_timer *m_blitter_timer;
 	emu_timer *m_signal_irq_timer;
 
 	/* misc */
-	uint8_t    m_irq_state[5];
-	uint8_t    m_sound_control;
-	uint8_t    m_sound_msb_latch;
+	u8    m_irq_state[5];
+	u8    m_sound_control;
+	u8    m_sound_msb_latch;
 
 	/* devices */
-	required_device<cpu_device> m_maincpu;
-	required_device<cpu_device> m_audiocpu;
+	required_device<m68000_device> m_maincpu;
+	required_device<m6809_device> m_audiocpu;
 	required_device<screen_device> m_screen;
 	required_device<bsmt2000_device> m_bsmt;
 	required_device<generic_latch_8_device> m_soundlatch;
@@ -84,9 +92,9 @@ private:
 	DECLARE_WRITE16_MEMBER(blitter_unknown_w);
 	DECLARE_READ16_MEMBER(blitter_vidparam_r);
 	void dcheese_palette(palette_device &palette) const;
-	uint32_t screen_update_dcheese(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	INTERRUPT_GEN_MEMBER(dcheese_vblank);
-	void dcheese_signal_irq(int which);
+	u32 screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	INTERRUPT_GEN_MEMBER(vblank);
+	void signal_irq(u8 which);
 	void update_irq_state();
 	IRQ_CALLBACK_MEMBER(irq_callback);
 	void update_scanline_irq();
