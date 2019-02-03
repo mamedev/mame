b6fb151b5e66fe7cb5692cd6012bbc4af3eeadbb
AJR <ajrhacker@users.noreply.github.com>
2019-02-03 10:15:57 -0500

    Provide 1 MHz clock for uPD7002 (nw)

--- src/devices/bus/bbc/exp/mertec.cpp
+++ src/devices/bus/bbc/exp/mertec.cpp
@@ -52,7 +52,7 @@ void bbc_mertec_device::device_add_mconfig(machine_config &config)
 	//m_pia->irq_handler().set("irqs", FUNC(input_merger_device::in_w<0>));
 
 	/* adc */
-	UPD7002(config, m_upd7002, 0);
+	UPD7002(config, m_upd7002, DERIVED_CLOCK(1, 8));
 	m_upd7002->set_get_analogue_callback(FUNC(bbc_mertec_device::get_analogue_input), this);
 	m_upd7002->set_eoc_callback(FUNC(bbc_mertec_device::upd7002_eoc), this);
 
