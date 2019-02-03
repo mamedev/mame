44f375b47d7f750ff5eb8f249eeb066291796738
AJR <ajrhacker@users.noreply.github.com>
2019-02-03 13:12:12 -0500

    Add skeleton CRT9028 device (nw)

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -297,6 +297,7 @@ VIDEOS["CDP1862"] = true
 --VIDEOS["CESBLIT"] = true
 VIDEOS["CRT9007"] = true
 VIDEOS["CRT9021"] = true
+VIDEOS["CRT9028"] = true
 VIDEOS["CRT9212"] = true
 VIDEOS["CRTC_EGA"] = true
 VIDEOS["DL1416"] = true
