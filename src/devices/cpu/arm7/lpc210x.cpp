536408762930a91f92208d85e2ecac270f5ec140
Ivan Vangelista <mesgnet@yahoo.it>
2019-02-09 10:16:30 +0100

    misc MACHINE_CONFIG macros removal (nw)

--- src/devices/cpu/arm7/lpc210x.cpp
+++ src/devices/cpu/arm7/lpc210x.cpp
@@ -276,5 +276,6 @@ void lpc210x_device::write_timer(address_space &space, int timer, int offset, ui
 
 
 
-MACHINE_CONFIG_START(lpc210x_device::device_add_mconfig)
-MACHINE_CONFIG_END
+void lpc210x_device::device_add_mconfig(machine_config &config)
+{
+}
