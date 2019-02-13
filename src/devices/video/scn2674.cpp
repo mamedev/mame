a561a0ea391fdf68ecebeab386969316580e4f33
AJR <ajrhacker@users.noreply.github.com>
2019-02-12 21:36:45 -0500

    scn2674: Blank display line by line

--- src/devices/video/scn2674.cpp
+++ src/devices/video/scn2674.cpp
@@ -1161,6 +1161,9 @@ TIMER_CALLBACK_MEMBER(scn2674_device::scanline_timer)
 			address = m_display_buffer_first_address;
 	}
 
+	if (!m_display_enabled)
+		std::fill_n(&m_bitmap.pix32(m_linecounter), m_character_per_row * m_hpixels_per_column, rgb_t::black());
+
 	if (m_gfx_enabled || (charrow == (m_scanline_per_char_row - 1)))
 		m_address = address;
 }
@@ -1186,10 +1189,7 @@ TIMER_CALLBACK_MEMBER(scn2674_device::vblank_timer)
 
 uint32_t scn2674_device::screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect)
 {
-	if (!m_display_enabled)
-		m_bitmap.fill(rgb_t::black(), cliprect);
-	else
-		copybitmap(bitmap, m_bitmap, 0, 0, 0, 0, cliprect);
+	copybitmap(bitmap, m_bitmap, 0, 0, 0, 0, cliprect);
 
 	return 0;
 }
