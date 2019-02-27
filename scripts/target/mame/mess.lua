5f4a5874227e36bb2365d54fe9db2d7b37ecd329
arbee <rb6502@users.noreply.github.com>
2019-02-26 20:50:14 -0500

    New machines added as MACHINE_NOT_WORKING ----------------------------------------- Advanced Pico BEENA: Fresh Pretty Cure [Team Europe, R. Belmont]
    This is a little weird because it's a new system and we have a cartridge dump but not the BIOS.

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -3033,6 +3033,7 @@ files {
 
 createMESSProjects(_target, _subtarget, "segacons")
 files {
+	MAME_DIR .. "src/mame/drivers/apbeena.cpp",
 	MAME_DIR .. "src/mame/drivers/dccons.cpp",
 	MAME_DIR .. "src/mame/includes/dccons.h",
 	MAME_DIR .. "src/mame/machine/dccons.cpp",
