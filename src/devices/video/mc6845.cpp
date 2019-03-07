mamedev/mame

commit c4f0852c32f806d19a519ae108d15fa4cc78bc86

mc6845.cpp: Do not reset line counter when screen parameters changed
Fix MT 06997, 06854 and 06423.
Also others games like "Profanation 2" works now.

src/devices/video/mc6845.cpp
@@ -582,8 +582,6 @@ void mc6845_device::recompute_parameters(bool postload)
 		m_hsync_off_pos = hsync_off_pos;
 		m_vsync_on_pos = vsync_on_pos;
 		m_vsync_off_pos = vsync_off_pos;
-		if (!postload) // set m_line_counter to 0 on normal operation, but not on postload
-			m_line_counter = 0;
 	}
 }
 

commit 97377ac34a7e3fdac5459950b9367739914c34b6

mc6845.cpp: Fix mk88 machine
Regression caused by commit c4f0852c.
Thanks to Tafoid for catch it.

src/devices/video/mc6845.cpp
@@ -582,6 +582,8 @@ void mc6845_device::recompute_parameters(bool postload)
 		m_hsync_off_pos = hsync_off_pos;
 		m_vsync_on_pos = vsync_on_pos;
 		m_vsync_off_pos = vsync_off_pos;
+		if ( (!m_reconfigure_cb.isnull()) && (!postload) )
+			m_line_counter = 0;
 	}
 }
 