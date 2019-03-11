5e467907737da49c67bcae772cc4abf03a8836b8
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2019-03-11 12:45:18 +0000

    new NOT WORKING machines --- Jumping Popira [Sean Riddle, Peter Wilhelmsen, ShouTime, David Haywood]
    (seeprom? troubles, i've documented a hack to bypass them right now, but then it boots in an invalid state thinking you've cleared all songs with some corruption due to that)

--- src/mame/includes/xavix.h
+++ src/mame/includes/xavix.h
@@ -903,8 +903,24 @@ public:
 	{ }
 
 	void xavix_i2c_taiko(machine_config &config);
+	void xavix_i2c_jpopira(machine_config &config);
 
 	DECLARE_CUSTOM_INPUT_MEMBER(i2c_r);
+
+	void init_jpopira()
+	{
+		init_xavix();
+#if 0	
+		// SEEPROM init stuff?
+		uint8_t *rom = memregion("bios")->base();
+
+		rom[0xcbfe] = 0xea;
+		rom[0xcbff] = 0xea;
+
+		rom[0xcc04] = 0xea;
+		rom[0xcc05] = 0xea;
+#endif
+	};
 
 protected:
 	virtual void write_io1(uint8_t data, uint8_t direction) override;
