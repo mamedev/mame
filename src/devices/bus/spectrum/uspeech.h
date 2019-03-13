169643596920a522f0b94168dae78e092831b4de
Nigel Barnes <Pernod70@users.noreply.github.com>
2019-03-13 17:20:15 +0000

    spectrum_uspeech: Use measured oscillator frequencies, and toggle ROM also on IORQ as used in Rockfall1/2. [Thomas Busse]

--- src/devices/bus/spectrum/uspeech.h
+++ src/devices/bus/spectrum/uspeech.h
@@ -44,6 +44,7 @@ protected:
 	virtual DECLARE_READ_LINE_MEMBER(romcs) override;
 	virtual uint8_t mreq_r(offs_t offset) override;
 	virtual void mreq_w(offs_t offset, uint8_t data) override;
+	virtual uint8_t iorq_r(offs_t offset) override;
 
 private:
 	required_device<sp0256_device> m_nsp;
