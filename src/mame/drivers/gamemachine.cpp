783a4188de79ff35f813fbe5e8f0425c715bf27c
couriersud <couriersud@gmx.org>
2019-03-17 01:02:47 +0100

    gamemachine: document random mode for Shooting Gallery. (nw)

--- src/mame/drivers/gamemachine.cpp
+++ src/mame/drivers/gamemachine.cpp
@@ -39,7 +39,7 @@ Calculator:
   [0  ] [1  ] [2  ] [3  ] [4  ] [5  ] [6  ] [7  ] [8  ] [9  ]
 
 Shooting Gallery:
-  [RET] [Cyc] [Zig] [   ] [   ] [   ] [   ] [   ] [   ] [   ] * Cyclic, Zigzag
+  [RET] [Cyc] [Zig] [Rnd] [   ] [   ] [   ] [   ] [   ] [   ] * Cyclic, Zigzag, Random
   [   ] [   ] [   ] [   ] [   ] [   ] [   ] [   ] [   ] [   ]
 
 Black Jack:
