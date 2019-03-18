4f2da56718857b0afa7b2a387bef299e78be55ed
AJR <ajrhacker@users.noreply.github.com>
2019-03-17 22:57:52 -0400

    aha1542: Add more ROMs, rename and split into three variants - AHA-1542C [modem7, minuszerodegrees.net] - AHA-1542CF v2.11 [Jolaes76, VOGONS Vintage Driver Library] - AHA-1542CP [modem7, minuszerodegrees.net]

--- src/devices/bus/isa/aha1542c.cpp
+++ src/devices/bus/isa/aha1542c.cpp
@@ -2,7 +2,7 @@
 // copyright-holders:Darkstar
 /**********************************************************************
  *
- *    Adaptec AHA-1542{,C,CF} SCSI Controller
+ *    Adaptec AHA-1542{C,CF,CP} SCSI Controller
  *
  **********************************************************************/
 
@@ -75,7 +75,7 @@
  */
 
 #include "emu.h"
-#include "aha1542.h"
+#include "aha1542c.h"
 #include "cpu/z80/z80.h"
 
 // I/O Port interface
@@ -134,34 +134,54 @@
 #define CMD_EXTBIOS     0x28    // return extended BIOS information
 #define CMD_MBENABLE    0x29    // set mailbox interface enable
 
-DEFINE_DEVICE_TYPE(AHA1542, aha1542_device, "aha1542", "AHA1542 SCSI Controller")
+DEFINE_DEVICE_TYPE(AHA1542C, aha1542c_device, "aha1542c", "AHA-1542C SCSI Controller")
+DEFINE_DEVICE_TYPE(AHA1542CF, aha1542cf_device, "aha1542cf", "AHA-1542CF SCSI Controller")
+DEFINE_DEVICE_TYPE(AHA1542CP, aha1542cp_device, "aha1542cp", "AHA-1542CP SCSI Controller")
 
-#define Z84C0010_TAG "u5"
+#define Z84C0010_TAG "z84c0010"
 
-READ8_MEMBER( aha1542_device::aha1542_r )
+READ8_MEMBER( aha1542c_device::aha1542_r )
 {
 	logerror("%s aha1542_r(): offset=%d\n", machine().describe_context(), offset);
 	return 0xff;
 }
 
-WRITE8_MEMBER( aha1542_device::aha1542_w )
+WRITE8_MEMBER( aha1542c_device::aha1542_w )
 {
 	logerror("%s aha1542_w(): offset=%d data=0x%02x\n", machine().describe_context(), offset, data);
 }
 
-//-------------------------------------------------
-//  ROM( aha1542 )
-//-------------------------------------------------
 
-ROM_START( aha1542 )
+ROM_START( aha1542c )
 	ROM_REGION( 0x8000, "aha1542", 0 )
-	ROM_LOAD( "553601-00.u16", 0x0000, 0x8000, CRC(ab22fc02) SHA1(f9f783e0272fc14ba3de32316997f1f6cadc67d0) ) /* Adaptec 1540CF/1542CF BIOS v2.01 */
+	ROM_LOAD( "534201-00_d_bios_144c.u15", 0x0000, 0x8000, CRC(35178004) SHA1(2b38f2e40cd02a1b32966ead7b202b0fca130cb8) )
 
 	ROM_REGION( 0x8000, Z84C0010_TAG, 0 )
-	ROM_LOAD( "553801-00.u15", 0x0000, 0x8000, CRC(7824397e) SHA1(35bc2c8fab31aad3190a478f2dc8f3a72958cf04) ) /* ADAPTEC, INC MCODE */
+	ROM_LOAD( "534001-00_d_mcode_a3c2.u5", 0x0000, 0x8000, CRC(220dd5a2) SHA1(4fc51c9dd63b45a50edcd56baa706d61decbef38) )
 ROM_END
 
-void aha1542_device::z84c0010_mem(address_map &map)
+ROM_START( aha1542cf )
+	ROM_REGION( 0x8000, "aha1542", 0 )
+	ROM_SYSTEM_BIOS( 0, "v201", "Adaptec 1540CF/1542CF BIOS v2.01" )
+	ROMX_LOAD( "553601-00_c_bios_c38d.u16", 0x0000, 0x8000, CRC(ab22fc02) SHA1(f9f783e0272fc14ba3de32316997f1f6cadc67d0), ROM_BIOS(0) )
+	ROM_SYSTEM_BIOS( 1, "v211", "Adaptec 1540CF/1542CF BIOS v2.11" )
+	ROMX_LOAD( "aha1542cf-v2.11-lower-socket.bin", 0x0000, 0x8000, CRC(fddd0b83) SHA1(aabd227cb338d8812e0bb5c17c08ea06c5aedd36), ROM_BIOS(1) )
+
+	ROM_REGION( 0x8000, Z84C0010_TAG, 0 )
+	ROMX_LOAD( "553801-00_c_mcode_563d.u15", 0x0000, 0x8000, CRC(7824397e) SHA1(35bc2c8fab31aad3190a478f2dc8f3a72958cf04), ROM_BIOS(0) )
+	ROMX_LOAD( "aha1542cf-v2.11-upper-socket.bin", 0x0000, 0x8000, CRC(896873cd) SHA1(6edbdd9b0b15ef31ca0741cac31556d2d5266b6e), ROM_BIOS(1) )
+ROM_END
+
+ROM_START( aha1542cp )
+	ROM_REGION( 0x8000, "aha1542", 0 )
+	ROM_LOAD( "908501-00_d_bios_a91e.u7", 0x0000, 0x8000, CRC(0646c35e) SHA1(3a7c2731abd8295438cfa1f2a525be53e9512b1a) )
+
+	ROM_REGION( 0x8000, Z84C0010_TAG, 0 )
+	ROM_LOAD( "908301-00_f_mcode_17c9.u12", 0x0000, 0x8000, CRC(04494022) SHA1(431dfc26312556ddd24fccc429b2b3e93bac5c2f) )
+ROM_END
+
+
+void aha1542c_device::z84c0010_mem(address_map &map)
 {
 	map(0x0000, 0x7fff).rom().region(Z84C0010_TAG, 0);
 	map(0x8000, 0x800f).noprw();        // something is mapped there
@@ -170,32 +190,57 @@ void aha1542_device::z84c0010_mem(address_map &map)
 	map(0xb000, 0xb000).noprw();        // something?
 }
 
-const tiny_rom_entry *aha1542_device::device_rom_region() const
+const tiny_rom_entry *aha1542c_device::device_rom_region() const
 {
-	return ROM_NAME( aha1542 );
+	return ROM_NAME( aha1542c );
 }
 
-void aha1542_device::device_add_mconfig(machine_config &config)
+const tiny_rom_entry *aha1542cf_device::device_rom_region() const
 {
-	z80_device &cpu(Z80(config, Z84C0010_TAG, XTAL(12'000'000)));
-	cpu.set_addrmap(AS_PROGRAM, &aha1542_device::z84c0010_mem);
+	return ROM_NAME( aha1542cf );
 }
 
-aha1542_device::aha1542_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) :
-	device_t(mconfig, AHA1542, tag, owner, clock),
+const tiny_rom_entry *aha1542cp_device::device_rom_region() const
+{
+	return ROM_NAME( aha1542cp );
+}
+
+void aha1542c_device::device_add_mconfig(machine_config &config)
+{
+	z80_device &cpu(Z80(config, Z84C0010_TAG, 10'000'000));
+	cpu.set_addrmap(AS_PROGRAM, &aha1542c_device::z84c0010_mem);
+}
+
+aha1542c_device::aha1542c_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock) :
+	device_t(mconfig, type, tag, owner, clock),
 	device_isa16_card_interface(mconfig, *this)
 {
 }
 
-void aha1542_device::device_start()
+aha1542c_device::aha1542c_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) :
+	aha1542c_device(mconfig, AHA1542C, tag, owner, clock)
+{
+}
+
+aha1542cf_device::aha1542cf_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) :
+	aha1542c_device(mconfig, AHA1542CF, tag, owner, clock)
+{
+}
+
+aha1542cp_device::aha1542cp_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) :
+	aha1542c_device(mconfig, AHA1542CP, tag, owner, clock)
+{
+}
+
+void aha1542c_device::device_start()
 {
 	set_isa_device();
 	m_isa->install_rom(this, 0xdc000, 0xdffff, "aha1542", "aha1542");
-	m_isa->install_device(0x330, 0x333, read8_delegate(FUNC( aha1542_device::aha1542_r ), this),
-	write8_delegate(FUNC( aha1542_device::aha1542_w ), this) );
+	m_isa->install_device(0x330, 0x333, read8_delegate(FUNC( aha1542cf_device::aha1542_r ), this),
+	write8_delegate(FUNC( aha1542cf_device::aha1542_w ), this) );
 }
 
 
-void aha1542_device::device_reset()
+void aha1542c_device::device_reset()
 {
 }
--- src/devices/bus/isa/isa_cards.cpp
+++ src/devices/bus/isa/isa_cards.cpp
@@ -31,7 +31,7 @@
 #include "ide.h"
 #include "xtide.h"
 #include "side116.h"
-#include "aha1542.h"
+#include "aha1542c.h"
 #include "wd1002a_wx1.h"
 #include "wd1007a.h"
 #include "mcd.h"
@@ -153,7 +153,9 @@ void pc_isa16_cards(device_slot_interface &device)
 	// 16-bit
 	device.option_add("ide", ISA16_IDE);
 	device.option_add("ne2000", NE2000);
-	device.option_add("aha1542", AHA1542);
+	device.option_add("aha1542c", AHA1542C);
+	device.option_add("aha1542cf", AHA1542CF);
+	device.option_add("aha1542cp", AHA1542CP);
 	device.option_add("gus",ISA16_GUS);
 	device.option_add("sblaster_16", ISA16_SOUND_BLASTER_16);
 	device.option_add("svga_s3", ISA16_SVGA_S3);
