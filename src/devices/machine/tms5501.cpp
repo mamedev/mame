8d8157ec52aba33cd53b1fcff085dcd8dc1b4614
Robbbert <Robbbert@users.noreply.github.com>
2019-03-21 01:00:58 +1100

    (nw) better

--- src/devices/machine/tms5501.cpp
+++ src/devices/machine/tms5501.cpp
@@ -263,12 +263,12 @@ READ8_MEMBER( tms5501_device::rst_r )
 
 READ8_MEMBER( tms5501_device::sta_r )
 {
+	if(is_transmit_register_empty())
+		m_sta |= STA_XBE;
+
 	uint8_t data = m_sta;
 
 	m_sta &= ~STA_OE;
-
-	if(is_transmit_register_empty())
-		m_sta |= STA_XBE;
 
 	return data;
 }
