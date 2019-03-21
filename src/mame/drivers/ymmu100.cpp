05e49154337704c9ba0451aeaf87f2a3ce61be03
arbee <rb6502@users.noreply.github.com>
2019-03-21 00:17:56 -0400

    New machines added as NOT_WORKING --------------------------------- Yamaha VL70-m [R. Belmont, O. Galibert]

--- src/mame/drivers/ymmu100.cpp
+++ src/mame/drivers/ymmu100.cpp
@@ -132,6 +132,7 @@
 #include "bus/midi/midiinport.h"
 #include "bus/midi/midioutport.h"
 #include "cpu/h8/h83002.h"
+#include "cpu/h8/h83003.h"
 #include "cpu/h8/h8s2655.h"
 #include "video/hd44780.h"
 #include "sound/swp30.h"
@@ -170,12 +171,14 @@ public:
 		: driver_device(mconfig, type, tag)
 		, m_maincpu(*this, "maincpu")
 		, m_mu80cpu(*this, "mu80cpu")
+		, m_vl70cpu(*this, "vl70cpu")
 		, m_swp30(*this, "swp30")
 		, m_lcd(*this, "lcd")
 		, m_ioport_p7(*this, "P7")
 		, m_ioport_p8(*this, "P8")
 	{ }
 
+	void vl70(machine_config &config);
 	void mu80(machine_config &config);
 	void mu100(machine_config &config);
 
@@ -269,6 +272,12 @@ private:
 	};
 
 	enum {
+		P6_LCD_RS     = 0x04,
+		P6_LCD_RW     = 0x02,
+		P6_LCD_ENABLE = 0x01
+	};
+
+	enum {
 		PA_LCD_RS     = 0x02,
 		PA_LCD_ENABLE = 0x20,
 		PA_LCD_RW     = 0x40
@@ -276,7 +285,8 @@ private:
 
 	optional_device<h8s2655_device> m_maincpu;
 	optional_device<h83002_device> m_mu80cpu;
-	required_device<swp30_device> m_swp30;
+	optional_device<h83003_device> m_vl70cpu;
+	optional_device<swp30_device> m_swp30;
 	required_device<hd44780_device> m_lcd;
 	required_ioport m_ioport_p7;
 	required_ioport m_ioport_p8;
@@ -305,6 +315,10 @@ private:
 	u16 pa_r_mu80();
 	void pb_w_mu80(u16 data);
 	u16 pb_r_mu80();
+	void p6_w_vl70(u16 data);
+	u16 p6_r_vl70();
+	void pa_w_vl70(u16 data);
+	u16 pa_r_vl70();
 	void pf_w(u16 data);
 	void pg_w(u16 data);
 
@@ -315,6 +329,8 @@ private:
 	void mu100_map(address_map &map);
 	void mu80_iomap(address_map &map);
 	void mu80_map(address_map &map);
+	void vl70_iomap(address_map &map);
+	void vl70_map(address_map &map);
 	void swp30_map(address_map &map);
 };
 
@@ -545,6 +561,12 @@ void mu100_state::mu80_map(address_map &map)
 	map(0x200000, 0x20ffff).ram(); // 64K work RAM
 }
 
+void mu100_state::vl70_map(address_map &map)
+{
+	map(0x000000, 0x1fffff).rom().region("vl70cpu", 0);
+	map(0x200000, 0x20ffff).ram(); // 64K work RAM
+}
+
 void mu100_state::mu100_map(address_map &map)
 {
 	map(0x000000, 0x1fffff).rom().region("maincpu", 0);
@@ -733,6 +755,50 @@ u16 mu100_state::pa_r_mu80()
 {
 	return cur_pa;
 }
+
+void mu100_state::p6_w_vl70(u16 data)
+{
+	if(!(cur_p6 & P6_LCD_ENABLE) && (data & P6_LCD_ENABLE)) {
+	if(!(cur_p6 & P6_LCD_RW)) {
+		if(cur_p6 & P6_LCD_RS)
+			m_lcd->data_write(cur_pa);
+		else
+			m_lcd->control_write(cur_pa);
+		}
+	}
+
+//  if(!(cur_pa9 & 0x08) && (data & 0x08))
+//      cur_ic32 = cur_pa;
+
+	cur_p6 = data;
+}
+
+u16 mu100_state::p6_r_vl70()
+{
+	return cur_p6;
+}
+
+void mu100_state::pa_w_vl70(u16 data)
+{
+	cur_pa = data;
+}
+
+u16 mu100_state::pa_r_vl70()
+{
+	if((cur_p6 & P6_LCD_ENABLE)) {
+		if(cur_p6 & P6_LCD_RW)
+		{
+			if(cur_p6 & P6_LCD_RS)
+				return m_lcd->data_read();
+			else
+				return m_lcd->control_read();
+		} else
+			return 0x00;
+	}
+
+	return cur_pa;
+}
+
 void mu100_state::mu80_iomap(address_map &map)
 {
 	map(h8_device::PORT_A, h8_device::PORT_A).rw(FUNC(mu100_state::pa_r_mu80), FUNC(mu100_state::pa_w_mu80));
@@ -742,6 +808,12 @@ void mu100_state::mu80_iomap(address_map &map)
 	map(h8_device::ADC_4, h8_device::ADC_4).r(FUNC(mu100_state::adc4_r));
 	map(h8_device::ADC_6, h8_device::ADC_6).r(FUNC(mu100_state::adc6_r));
 	map(h8_device::ADC_7, h8_device::ADC_7).r(FUNC(mu100_state::adc7_r));
+}
+
+void mu100_state::vl70_iomap(address_map &map)
+{
+	map(h8_device::PORT_6, h8_device::PORT_6).rw(FUNC(mu100_state::p6_r_vl70), FUNC(mu100_state::p6_w_vl70));
+	map(h8_device::PORT_A, h8_device::PORT_A).rw(FUNC(mu100_state::pa_r_vl70), FUNC(mu100_state::pa_w_vl70));
 }
 
 void mu100_state::mu100_iomap(address_map &map)
@@ -842,6 +914,43 @@ void mu100_state::mu80(machine_config &config)
 	m_mu80cpu->subdevice<h8_sci_device>("sci0")->tx_handler().set(mdout, FUNC(midi_port_device::write_txd));
 }
 
+void mu100_state::vl70(machine_config &config)
+{
+	H83003(config, m_vl70cpu, 10_MHz_XTAL);
+	m_vl70cpu->set_addrmap(AS_PROGRAM, &mu100_state::vl70_map);
+	m_vl70cpu->set_addrmap(AS_IO, &mu100_state::vl70_iomap);
+
+	HD44780(config, m_lcd);
+	m_lcd->set_lcd_size(4, 20);
+
+	auto &screen = SCREEN(config, "screen", SCREEN_TYPE_LCD);
+	screen.set_refresh_hz(50);
+	screen.set_vblank_time(ATTOSECONDS_IN_USEC(2500)); /* not accurate, asynchronous updating anyway */
+	screen.set_screen_update(FUNC(mu100_state::screen_update));
+	screen.set_size(900, 241);
+	screen.set_visarea(0, 899, 0, 240);
+
+	SPEAKER(config, "lspeaker").front_left();
+	SPEAKER(config, "rspeaker").front_right();
+
+//  SWP30(config, m_swp30);
+//  m_swp30->set_addrmap(0, &mu100_state::swp30_map);
+//  m_swp30->add_route(0, "lspeaker", 1.0);
+//  m_swp30->add_route(1, "rspeaker", 1.0);
+
+	auto &mdin_a(MIDI_PORT(config, "mdin_a"));
+	midiin_slot(mdin_a);
+	mdin_a.rxd_handler().set("vl70cpu:sci1", FUNC(h8_sci_device::rx_w));
+
+	auto &mdin_b(MIDI_PORT(config, "mdin_b"));
+	midiin_slot(mdin_b);
+	mdin_b.rxd_handler().set("vl70cpu:sci0", FUNC(h8_sci_device::rx_w));
+
+	auto &mdout(MIDI_PORT(config, "mdout"));
+	midiout_slot(mdout);
+	m_vl70cpu->subdevice<h8_sci_device>("sci0")->tx_handler().set(mdout, FUNC(midi_port_device::write_txd));
+}
+
 #define ROM_LOAD16_WORD_SWAP_BIOS(bios,name,offset,length,hash) \
 		ROMX_LOAD(name, offset, length, hash, ROM_GROUPWORD | ROM_REVERSE | ROM_BIOS(bios))
 
@@ -897,7 +1006,18 @@ ROM_START( mu80 )
 	ROM_LOAD( "mu100-font.bin", 0x0000, 0x1000, BAD_DUMP CRC(a7d6c1d6) SHA1(9f0398d678bdf607cb34d83ee535f3b7fcc97c41) )
 ROM_END
 
+ROM_START( vl70 )
+	ROM_REGION( 0x200000, "vl70cpu", 0 )
+	ROM_LOAD16_WORD_SWAP( "vl70m_v111_27c160.bin", 0x000000, 0x200000, CRC(efdba9f0) SHA1(cfa9fb7d2a991e4752393c9677e4ddcbe10866c7) )
+
+	ROM_REGION( 0x1800000, "swp30", ROMREGION_ERASE00 )
+
+	ROM_REGION( 0x1000, "lcd", 0)
+	// Hand made, 3 characters unused
+	ROM_LOAD( "mu100-font.bin", 0x0000, 0x1000, BAD_DUMP CRC(a7d6c1d6) SHA1(9f0398d678bdf607cb34d83ee535f3b7fcc97c41) )
+ROM_END
+
 CONS( 1997, mu100,  0,     0, mu100, mu100, mu100_state,  empty_init, "Yamaha", "MU100",                  MACHINE_NOT_WORKING )
 CONS( 1997, mu100r, mu100, 0, mu100, mu100, mu100r_state, empty_init, "Yamaha", "MU100 Rackable version", MACHINE_NOT_WORKING )
 CONS( 1994, mu80,   mu100, 0, mu80,  mu100, mu100_state,  empty_init, "Yamaha", "MU80",                   MACHINE_NOT_WORKING )
-
+CONS( 1996, vl70,   mu100, 0, vl70,  mu100, mu100_state,  empty_init, "Yamaha", "VL70-m",                  MACHINE_NOT_WORKING )
