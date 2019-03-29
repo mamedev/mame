19023869ecc28326930e5274c8ad8a48bca59436
cam900 <dbtlrchl@naver.com>
2019-03-30 02:23:32 +0900

    c6280.cpp : Fix LFO enable bit

--- src/devices/sound/c6280.cpp
+++ src/devices/sound/c6280.cpp
@@ -118,7 +118,7 @@ void c6280_device::sound_stream_update(sound_stream &stream, stream_sample_t **i
 			}
 			else
 			{
-				if ((m_lfo_control & 0x80) && (ch < 2))
+				if ((m_lfo_control & 3) && (~m_lfo_control & 0x80) && (ch < 2))
 				{
 					if (ch == 0) // CH 0 only, CH 1 is muted
 					{
@@ -133,9 +133,7 @@ void c6280_device::sound_stream_update(sound_stream &stream, stream_sample_t **i
 							m_channel[1].m_counter += m_wave_freq_tab[(lfo_step * m_lfo_frequency) & 0xfff]; // TODO : multiply? verify this from real hardware.
 							m_channel[1].m_counter &= 0x1FFFF;
 							lfo_data = m_channel[1].m_waveform[lfooffset];
-							if (m_lfo_control & 3)
-								step += ((lfo_data - 16) << (((m_lfo_control & 3)-1)<<1)); // verified from patent, TODO : same in real hardware?
-
+							step += ((lfo_data - 16) << (((m_lfo_control & 3)-1)<<1)); // verified from patent, TODO : same in real hardware?
 							offset = (m_channel[0].m_counter >> 12) & 0x1F;
 							m_channel[0].m_counter += m_wave_freq_tab[step & 0xfff];
 							m_channel[0].m_counter &= 0x1FFFF;
