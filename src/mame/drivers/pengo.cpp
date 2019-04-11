05598c5270c4e391787315cd30e597192bb08581
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2019-04-11 13:10:54 +0100

    (nw)

--- src/mame/drivers/pengo.cpp
+++ src/mame/drivers/pengo.cpp
@@ -959,16 +959,16 @@ void pengo_state::decode_schick_extra(int size, uint8_t* rom)
 
 				switch (srcdec & 0x51)
 				{
-				case 0x00: srcdec = (srcdec & ~0x51) | 0x10; break;
-				case 0x01: srcdec = (srcdec & ~0x51) | 0x11; break;
-				case 0x10: srcdec = (srcdec & ~0x51) | 0x50; break;
-				case 0x11: srcdec = (srcdec & ~0x51) | 0x51; break; // push/pull opcodes, see f1a1, f1a2, f1fd etc
-				case 0x40: srcdec = (srcdec & ~0x51) | 0x00; break;
-				case 0x41: srcdec = (srcdec & ~0x51) | 0x01; break;
-				case 0x50: srcdec = (srcdec & ~0x51) | 0x40; break;
+				case 0x00: srcdec = (srcdec & ~0x51) | 0x10; break; // looks good for ld ops
+				case 0x01: srcdec = (srcdec & ~0x51) | 0x01; break; // not good?
+				case 0x10: srcdec = (srcdec & ~0x51) | 0x11; break;
+				case 0x11: srcdec = (srcdec & ~0x51) | 0x51; break; // push/pull opcodes, see f1a1, f1a2, f1fd etc  (d3 case too) 
+				case 0x40: srcdec = (srcdec & ~0x51) | 0x00; break; // ok for some NOPs? and jr ops
+				case 0x41: srcdec = (srcdec & ~0x51) | 0x40; break; // maybe, ret z at f3be
+				case 0x50: srcdec = (srcdec & ~0x51) | 0x50; break;
 				case 0x51: srcdec = (srcdec & ~0x51) | 0x41; break; // jmp
 				}
-
+				// maybe, ret z at f3be
 				rom[A] = srcdec;
 			}
 			else
