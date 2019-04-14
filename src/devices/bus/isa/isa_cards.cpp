e01935498f6a857e7d206c292b671e1bfab46838
AJR <ajrhacker@users.noreply.github.com>
2019-04-14 18:18:12 -0400

    Add skeleton device for UltraStor Ultra-14F SCSI controller [Metropoli BBS Archive]

--- src/devices/bus/isa/isa_cards.cpp
+++ src/devices/bus/isa/isa_cards.cpp
@@ -42,6 +42,7 @@
 #include "bt54x.h"
 #include "dcb.h"
 #include "ultra12f.h"
+#include "ultra14f.h"
 
 // sound
 #include "adlib.h"
@@ -189,4 +190,5 @@ void pc_isa16_cards(device_slot_interface &device)
 	device.option_add("bt545s", BT545S);
 	device.option_add("dcb", NOVELL_DCB);
 	device.option_add("ultra12f", ULTRA12F);
+	device.option_add("ultra14f", ULTRA14F);
 }
