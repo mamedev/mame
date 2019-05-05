0492f88c9995484ed9e59fffd9c9c4826c38696a
AJR <ajrhacker@users.noreply.github.com>
2019-05-04 21:40:38 -0400

    New machines marked as NOT_WORKING ---------------------------------- Telex 274-61C Sixteen Station Control Unit [Al Kossow, Bitsavers]

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -3999,6 +3999,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/talkingfb.cpp",
 	MAME_DIR .. "src/mame/drivers/tavernie.cpp",
 	MAME_DIR .. "src/mame/drivers/tecnbras.cpp",
+	MAME_DIR .. "src/mame/drivers/telex274.cpp",
 	MAME_DIR .. "src/mame/drivers/terak.cpp",
 	MAME_DIR .. "src/mame/drivers/terco.cpp",
 	MAME_DIR .. "src/mame/drivers/terminal.cpp",
