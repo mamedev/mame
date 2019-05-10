c7fa391a2a4d364040971c0cdc16cf4506032c2a
Jack Li <jackrjli@berkeley.edu>
2019-05-10 00:01:35 -0700

    Added support for multiple buttons loaded from a file

--- plugins/autofire/init.lua
+++ plugins/autofire/init.lua
@@ -10,28 +10,83 @@ exports.author = { name = "Jack Li" }
 local autofire = exports
 
 function autofire.startplugin()
-	local key = 'KEYCODE_Q'
-	local on_frames = 1
-	local off_frames = 60
-	local counter = 0
 
-	local function per_frame()
-		if emu.romname() == '___empty' then
-			return
-		end
-		local keycode = manager:machine():input():code_from_token(key)
+	-- List of autofire buttons, each being a table with keys:
+	--   'port' - port name of the button being autofired
+	--   'field' - field name of the button being autofired
+	--   'key' - token of the keybinding
+	--   'on_frames' - number of frames button is pressed
+	--   'off_frames' - number of frames button is released
+	--   'button' - reference to ioport_field (not persisted)
+	--   'counter' - position in autofire cycle (not persisted)
+	-- port, field, key, on_frames, and off_frames are loaded from/saved to a file on start/stop.
+	local buttons = {}
+
+	local function process_button(button)
+		local keycode = manager:machine():input():code_from_token(button.key)
 		local pressed = manager:machine():input():code_pressed(keycode)
-		local p1b1 = manager:machine():ioport().ports[':P1'].fields['P1 Button 1']
 		if pressed then
-			p1b1:set_value(counter < on_frames and 1 or 0)
-			counter = (counter + 1) % (on_frames + off_frames)
+			button.button:set_value(button.counter < button.on_frames and 1 or 0)
+			button.counter = (button.counter + 1) % (button.on_frames + button.off_frames)
 		else
-			counter = 0
-			p1b1:set_value(0)
+			button.counter = 0
+			button.button:set_value(0)
 		end
 	end
 
-	emu.register_frame_done(per_frame)
+	local function process_frame()
+		for i, button in ipairs(buttons) do
+			process_button(button)
+		end
+	end
+
+	local function initialize_button(settings)
+		if settings.port and settings.field and settings.key and settings.on_frames and settings.off_frames then
+			local new_button = {}
+			new_button.port = settings.port
+			new_button.field = settings.field
+			new_button.key = settings.key
+			new_button.on_frames = settings.on_frames
+			new_button.off_frames = settings.off_frames
+			new_button.counter = 0
+			local port = manager:machine():ioport().ports[settings.port]
+			if port then
+				local field = port.fields[settings.field]
+				if field then
+					new_button.button = field
+					return new_button
+				end
+			end
+		end
+		return nil
+	end
+
+	local function load_settings()
+		buttons = {}
+		local json = require('json')
+		local file = io.open(lfs.env_replace(manager:machine():options().entries.pluginspath:value():match("([^;]+)")) .. "/autofire/cfg/" .. emu.romname() .. ".cfg", "r")
+		if not file then
+			return
+		end
+		local loaded_settings = json.parse(file:read('a'))
+		if not loaded_settings then
+			return
+		end
+		for index, button_settings in ipairs(loaded_settings) do
+			local new_button = initialize_button(button_settings)
+			if new_button then
+				buttons[#buttons + 1] = new_button
+			end
+		end
+	end
+
+	local function save_settings()
+		print('saving settings')
+	end
+
+	emu.register_frame_done(process_frame)
+	emu.register_start(load_settings)
+	emu.register_stop(save_settings)
 end
 
 return exports
