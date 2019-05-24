55f09eff67e06a5659814d5d659432fd38702327
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2019-05-24 18:09:45 +0100

    some alt tile bases for reference (nw)

--- src/mame/drivers/trkfldch.cpp
+++ src/mame/drivers/trkfldch.cpp
@@ -89,10 +89,15 @@ uint32_t trkfldch_state::screen_update_trkfldch(screen_device &screen, bitmap_in
 	// at 0xe9c (actually 0x0d0c when fully populated) in trkfldch
 	// 7861 / 7860 point here most of the time in both games (so maybe DMA source, or just uses a direct pointer)
 
-	for (int i = 0x1189; i < 0x1600; i += 5)
+	//for (int i = 0x0d0c+0x100*5; i >= 0x0d0c; i -= 5)
+	for (int i = 0x1189+0x100*5; i >= 0x1189; i -= 5)
 	{
 	//	printf("entry %02x %02x %02x %02x %02x\n", m_mainram[i + 0], m_mainram[i + 1], m_mainram[i + 2], m_mainram[i + 3], m_mainram[i + 4]);
-		int tilegfxbase = 0x3780;
+	//	int tilegfxbase = 0x1f80; // select mode 
+	//	int tilegfxbase = 0x2780; // 2nd demo (+0x800 from above)
+		int tilegfxbase = 0x3780; // 1st demo (+0x1000 from above)
+
+		
 
 		int y = m_mainram[i + 1];
 		int x = m_mainram[i + 3];
@@ -225,12 +230,12 @@ static INPUT_PORTS_START( trkfldch )
 	PORT_DIPNAME( 0x01, 0x01, "IN0" )
 	PORT_DIPSETTING(    0x01, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_NAME("O") // selects / forward
-	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_16WAY
-	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN ) PORT_16WAY
-	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP ) PORT_16WAY
-	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_16WAY
-	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON4 ) PORT_NAME("X") // goes back
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_NAME("O") // selects / forward
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_JOYSTICK_RIGHT ) PORT_16WAY
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_JOYSTICK_DOWN ) PORT_16WAY
+	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_JOYSTICK_UP ) PORT_16WAY
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_JOYSTICK_LEFT ) PORT_16WAY
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_NAME("X") // goes back
 	PORT_DIPNAME( 0x80, 0x80, DEF_STR( Unknown ) )
 	PORT_DIPSETTING(    0x80, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
