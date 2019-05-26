6824d336576c2e9b1bf2d2129b3e5f1ce024d27b
Enik Land <enikland@gmx.com>
2019-05-25 21:51:45 -0300

    minor comment changes

--- src/devices/bus/sms_ctrl/smsctrl.h
+++ src/devices/bus/sms_ctrl/smsctrl.h
@@ -64,7 +64,7 @@ public:
 
 	template <typename T> void set_screen_tag(T &&tag) { m_screen.set_tag(std::forward<T>(tag)); }
 
-	// for devices that interact with the machine's screen
+	// for peripherals that interact with the machine's screen
 	required_device<screen_device> m_screen;
 
 protected:
