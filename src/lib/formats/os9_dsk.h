57ff55f95a6aca8c3841961ef8f0c4ba60376ff9
tim lindner <tlindner@macmess.org>
2019-05-25 18:08:44 -0700

    I need to figure out why these keep getting reverted in my feature branches.

--- src/lib/formats/os9_dsk.h
+++ src/lib/formats/os9_dsk.h
@@ -4,11 +4,7 @@
 
     formats/os9_dsk.h
 
-<<<<<<< HEAD
-    TRD disk images
-=======
     OS-9 disk images
->>>>>>> upstream/master
 
 *********************************************************************/
 #ifndef MAME_FORMATS_OS9_DSK_H
