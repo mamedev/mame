037e3c41d321f6e52c1166701d8d692bc7d52633
Angelo Salese <lordkale4@gmail.com>
2019-05-29 23:42:42 +0200

    kill redundant note (nw)

--- src/mame/drivers/cntsteer.cpp
+++ src/mame/drivers/cntsteer.cpp
@@ -482,10 +482,6 @@ WRITE8_MEMBER(cntsteer_state::cntsteer_vregs_w)
 		case 0: m_scrolly = data; break;
 		case 1: m_scrollx = data; break;
 		case 2: m_bg_bank = (data & 0x01) << 9;
-				// TODO: this may be shuffled 
-				// 4 -> title screen (correct?)
-				// 0 -> gameplay (supposedly a 2 is better)
-				// needs to know what it tries to write after stage 1 ...
 				m_bg_color_bank = (data & 7);
 				m_bg_tilemap->mark_all_dirty();
 				break;
