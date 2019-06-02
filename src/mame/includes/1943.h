d95d51cdaea02a19c89a8462852ee403bd58fde4
cam900 <dbtlrchl@naver.com>
2019-06-03 08:06:39 +0900

    1943.cpp : Updates Correct sprite priority handling routine, Reduce runtime tag lookups, Fix namings, Simplify handlers, Gfxdecodes, Use shorter / correct type values

--- src/mame/includes/1943.h
+++ src/mame/includes/1943.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #include "emupal.h"
+#include "screen.h"
 
 class _1943_state : public driver_device
 {
@@ -25,7 +26,11 @@ public:
 		m_bgscrollx(*this, "bgscrollx"),
 		m_spriteram(*this, "spriteram"),
 		m_gfxdecode(*this, "gfxdecode"),
-		m_palette(*this, "palette")
+		m_palette(*this, "palette"),
+		m_screen(*this, "screen"),
+		m_tilerom(*this, "tilerom"),
+		m_proms(*this, "proms"),
+		m_mainbank(*this, "mainbank")
 	{ }
 
 	void _1943(machine_config &config);
@@ -36,14 +41,18 @@ public:
 private:
 	/* devices / memory pointers */
 	required_device<cpu_device> m_maincpu;
-	required_shared_ptr<uint8_t> m_videoram;
-	required_shared_ptr<uint8_t> m_colorram;
-	required_shared_ptr<uint8_t> m_scrollx;
-	required_shared_ptr<uint8_t> m_scrolly;
-	required_shared_ptr<uint8_t> m_bgscrollx;
-	required_shared_ptr<uint8_t> m_spriteram;
+	required_shared_ptr<u8> m_videoram;
+	required_shared_ptr<u8> m_colorram;
+	required_shared_ptr<u8> m_scrollx;
+	required_shared_ptr<u8> m_scrolly;
+	required_shared_ptr<u8> m_bgscrollx;
+	required_shared_ptr<u8> m_spriteram;
 	required_device<gfxdecode_device> m_gfxdecode;
 	required_device<palette_device> m_palette;
+	required_device<screen_device> m_screen;
+	required_region_ptr<u8> m_tilerom;
+	required_region_ptr<u8> m_proms;
+	required_memory_bank m_mainbank;
 
 	/* video-related */
 	tilemap_t *m_fg_tilemap;
@@ -55,25 +64,28 @@ private:
 	int     m_bg2_on;
 
 	/* protection */
-	uint8_t   m_prot_value;
-	DECLARE_WRITE8_MEMBER(c1943_protection_w);
-	DECLARE_READ8_MEMBER(c1943_protection_r);
-	DECLARE_READ8_MEMBER(_1943b_c007_r);
+	u8   m_prot_value;
+	void protection_w(u8 data);
+	u8 protection_r();
+	u8 _1943b_c007_r();
 
-	DECLARE_WRITE8_MEMBER(c1943_videoram_w);
-	DECLARE_WRITE8_MEMBER(c1943_colorram_w);
-	DECLARE_WRITE8_MEMBER(c1943_c804_w);
-	DECLARE_WRITE8_MEMBER(c1943_d806_w);
+	void videoram_w(offs_t offset, u8 data);
+	void colorram_w(offs_t offset, u8 data);
+	void c804_w(u8 data);
+	void d806_w(u8 data);
 
-	TILE_GET_INFO_MEMBER(c1943_get_bg2_tile_info);
-	TILE_GET_INFO_MEMBER(c1943_get_bg_tile_info);
-	TILE_GET_INFO_MEMBER(c1943_get_fg_tile_info);
+	TILE_GET_INFO_MEMBER(get_bg2_tile_info);
+	TILE_GET_INFO_MEMBER(get_bg_tile_info);
+	TILE_GET_INFO_MEMBER(get_fg_tile_info);
 	virtual void machine_start() override;
 	virtual void machine_reset() override;
 	virtual void video_start() override;
 	void _1943_palette(palette_device &palette) const;
-	uint32_t screen_update_1943(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	void draw_sprites(bitmap_ind16 &bitmap, const rectangle &cliprect, int priority);
+	u32 screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	void _1943_drawgfx(bitmap_ind16 &dest_bmp,const rectangle &clip,gfx_element *gfx,
+							u32 code,u32 color,bool flipx,bool flipy,int offsx,int offsy,
+							u8 transparent_color);
+	void draw_sprites(bitmap_ind16 &bitmap, const rectangle &cliprect);
 
 	void c1943_map(address_map &map);
 	void sound_map(address_map &map);
