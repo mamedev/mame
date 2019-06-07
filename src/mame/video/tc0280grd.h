b335ee617eb2de2bc8638bd165a824cfa70bf0e3
cam900 <dbtlrchl@naver.com>
2019-06-07 23:13:53 +0900

    tc0280grd.cpp : Internalize gfxdecode, Allow masked priority taito_f2.cpp : Updates Simplify handler, gfxdecodes, Allow masked priority for ROZ layer, Fix namings, Correct finalb 6bpp gfx decode behavior into mix ROM data

--- src/mame/video/tc0280grd.h
+++ src/mame/video/tc0280grd.h
@@ -5,26 +5,25 @@
 
 #pragma once
 
-class tc0280grd_device : public device_t
+class tc0280grd_device : public device_t, public device_gfx_interface
 {
 public:
 	tc0280grd_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
 
 	// configuration
-	template <typename T> void set_gfxdecode_tag(T &&tag) { m_gfxdecode.set_tag(std::forward<T>(tag)); }
-	void set_gfx_region(int gfxregion) { m_gfxnum = gfxregion; }
+	void set_color_base(u16 base) { m_colorbase = base; }
 
 	u16 tc0280grd_word_r(offs_t offset);
 	void tc0280grd_word_w(offs_t offset, u16 data, u16 mem_mask = ~0);
 	void tc0280grd_ctrl_word_w(offs_t offset, u16 data, u16 mem_mask = ~0);
 	void tc0280grd_tilemap_update(int base_color);
-	void tc0280grd_zoom_draw(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int xoffset, int yoffset, u32 priority);
+	void tc0280grd_zoom_draw(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int xoffset, int yoffset, u8 priority, u8 priority_mask = 0xff);
 
 	u16 tc0430grw_word_r(offs_t offset);
 	void tc0430grw_word_w(offs_t offset, u16 data, u16 mem_mask = ~0);
 	void tc0430grw_ctrl_word_w(offs_t offset, u16 data, u16 mem_mask = ~0);
 	void tc0430grw_tilemap_update(int base_color);
-	void tc0430grw_zoom_draw(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int xoffset, int yoffset, u32 priority);
+	void tc0430grw_zoom_draw(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int xoffset, int yoffset, u8 priority, u8 priority_mask = 0xff);
 
 protected:
 	// device-level overrides
@@ -39,11 +38,13 @@ private:
 
 	u16         m_ctrl[8];
 	int         m_base_color;
-	int         m_gfxnum;
-	required_device<gfxdecode_device> m_gfxdecode;
+
+	// decoding info
+	DECLARE_GFXDECODE_MEMBER(gfxinfo);
+	u16 m_colorbase;
 
 	TILE_GET_INFO_MEMBER(get_tile_info);
-	void zoom_draw(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int xoffset, int yoffset, u32 priority, int xmultiply);
+	void zoom_draw(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int xoffset, int yoffset, u8 priority, int xmultiply, u8 priority_mask = 0xff);
 };
 
 DECLARE_DEVICE_TYPE(TC0280GRD, tc0280grd_device)
