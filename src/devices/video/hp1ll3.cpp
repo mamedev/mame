1ea0eb1ed0d5b854266275806b4e4c45c589a258
Sergey Svishchev <shattered@users.noreply.github.com>
2019-06-25 22:32:25 +0300

    various: de-unroll loops in pixel drawing routines (nw)

--- src/devices/video/hp1ll3.cpp
+++ src/devices/video/hp1ll3.cpp
@@ -823,22 +823,10 @@ uint32_t hp1ll3_device::screen_update(screen_device &screen, bitmap_ind16 &bitma
 		{
 			uint16_t const gfx = m_videoram[x];
 
-			*p++ = BIT(gfx, 15);
-			*p++ = BIT(gfx, 14);
-			*p++ = BIT(gfx, 13);
-			*p++ = BIT(gfx, 12);
-			*p++ = BIT(gfx, 11);
-			*p++ = BIT(gfx, 10);
-			*p++ = BIT(gfx, 9);
-			*p++ = BIT(gfx, 8);
-			*p++ = BIT(gfx, 7);
-			*p++ = BIT(gfx, 6);
-			*p++ = BIT(gfx, 5);
-			*p++ = BIT(gfx, 4);
-			*p++ = BIT(gfx, 3);
-			*p++ = BIT(gfx, 2);
-			*p++ = BIT(gfx, 1);
-			*p++ = BIT(gfx, 0);
+			for (int i = 15; i >= 0; i--)
+			{
+				*p++ = BIT(gfx, i);
+			}
 		}
 	}
 
