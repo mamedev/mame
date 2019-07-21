cd42885b174c75c41658d3f752eff3208645d995
npwoods <npwoods@mess.org>
2019-07-21 15:19:38 -0400

    Created a -skip_mandatory_fileman option

--- src/frontend/mame/ui/ui.cpp
+++ src/frontend/mame/ui/ui.cpp
@@ -283,7 +283,7 @@ void mame_ui_manager::display_startup_screens(bool first_time)
 	const int maxstate = 3;
 	int str = machine().options().seconds_to_run();
 	bool show_gameinfo = !machine().options().skip_gameinfo();
-	bool show_warnings = true, show_mandatory_fileman = true;
+	bool show_warnings = true, show_mandatory_fileman = !machine().options().skip_mandatory_fileman();
 	bool video_none = strcmp(downcast<osd_options &>(machine().options()).video(), "none") == 0;
 
 	// disable everything if we are using -str for 300 or fewer seconds, or if we're the empty driver,
