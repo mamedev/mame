58731dcc11cb7b9f7d1f4c009713307685b71b74
feos <feos-theos@yandex.ru>
2019-08-17 16:56:11 +0300

    tiny cleanup as requested in #5348

--- src/frontend/mame/ui/ui.cpp
+++ src/frontend/mame/ui/ui.cpp
@@ -304,7 +304,7 @@ void mame_ui_manager::display_startup_screens(bool first_time)
 	int str = machine().options().seconds_to_run();
 	bool show_gameinfo = !machine().options().skip_gameinfo();
 	bool show_warnings = true, show_mandatory_fileman = true;
-	bool video_none = strcmp(downcast<osd_options &>(machine().options()).video(), "none") == 0;
+	bool video_none = strcmp(downcast<osd_options &>(machine().options()).video(), OSDOPTVAL_NONE) == 0;
 
 	// disable everything if we are using -str for 300 or fewer seconds, or if we're the empty driver,
 	// or if we are debugging, or if there's no mame window to send inputs to
