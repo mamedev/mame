3e6b9f975625c9036157da7f2ea48400b3bfb8ed
Angelo Salese <angelosa@users.noreply.github.com>
2019-09-20 15:38:24 +0200

    Rewritten Acorn VIDC10 into own device file [Angelo Salese] #5671
    * Improved raster effects on games that dynamically change palette on active frame;
    * Added stereo sound support;

--- scripts/target/mame/arcade.lua
+++ scripts/target/mame/arcade.lua
@@ -982,7 +982,6 @@ files {
 	MAME_DIR .. "src/mame/drivers/aristmk5.cpp",
 	MAME_DIR .. "src/mame/machine/archimds.cpp",
 	MAME_DIR .. "src/mame/includes/archimds.h",
-	MAME_DIR .. "src/mame/video/archimds.cpp",
 }
 
 createMAMEProjects(_target, _subtarget, "adp")
