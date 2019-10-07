fdca3846f5adf60048d76fb84b9efd7c119b4888
Julian Sikorski <belegdol+github@gmail.com>
2019-10-07 23:15:49 +0200

    Add astc-codec decoder

--- scripts/src/3rdparty.lua
+++ scripts/src/3rdparty.lua
@@ -1206,11 +1206,25 @@ project "bimg"
 
 	includedirs {
 		MAME_DIR .. "3rdparty/bimg/include",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/include",
 	}
 
 	files {
 		MAME_DIR .. "3rdparty/bimg/src/image.cpp",
 		MAME_DIR .. "3rdparty/bimg/src/image_gnf.cpp",
+
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/src/decoder/astc_file.cc",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/src/decoder/codec.cc",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/src/decoder/endpoint_codec.cc",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/src/decoder/footprint.cc",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/src/decoder/integer_sequence_codec.cc",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/src/decoder/intermediate_astc_block.cc",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/src/decoder/logical_astc_block.cc",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/src/decoder/partition.cc",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/src/decoder/physical_astc_block.cc",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/src/decoder/quantization.cc",
+		MAME_DIR .. "3rdparty/bimg/3rdparty/astc-codec/src/decoder/weight_infill.cc",
 	}
 
 --------------------------------------------------
