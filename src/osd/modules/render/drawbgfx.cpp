3d4bd27551010b1d33a02e5e7473f07f58e4b03c
MooglyGuy <therealmogminer@gmail.com>
2019-10-23 20:42:08 +0200

    -bgfx: Uncomment D3D12 backend detection, as it apparently no longer crashes on exit on Win7, nw

--- src/osd/modules/render/drawbgfx.cpp
+++ src/osd/modules/render/drawbgfx.cpp
@@ -278,11 +278,10 @@ int renderer_bgfx::create()
 		{
 			init.type = bgfx::RendererType::Direct3D11;
 		}
-// Throws exception on exit
-//		else if (backend == "dx12" || backend == "d3d12")
-//		{
-//			init.type = bgfx::RendererType::Direct3D12;
-//		}
+		else if (backend == "dx12" || backend == "d3d12")
+		{
+			init.type = bgfx::RendererType::Direct3D12;
+		}
 		else if (backend == "gles")
 		{
 			init.type = bgfx::RendererType::OpenGLES;
