e7624770d2b1bda2804f67077e5f59dbd4cf02e9
AJR <ajrhacker@users.noreply.github.com>
2019-10-29 09:38:17 -0400

    cars2lap: Move to another driver with additional notes (nw)

--- src/mame/drivers/prestige.cpp
+++ src/mame/drivers/prestige.cpp
@@ -896,11 +896,6 @@ ROM_START( gkidabc )
 	ROM_LOAD("27-5730-00.bin", 0x00000, 0x20000, CRC(64664708) SHA1(74212c2dec1caa41dbc933b50f857904a8ac623b))
 ROM_END
 
-ROM_START( cars2lap )
-	ROM_REGION( 0x200000, "maincpu", 0 )
-	ROM_LOAD("n25s16.u6", 0x00000, 0x200000, CRC(ec1ba96e) SHA1(51b8844ae77adf20f74f268d380d268c9ce19785))
-ROM_END
-
 
 
 /* Driver */
@@ -921,4 +916,3 @@ COMP( 19??, gmmc,     0,       0,      prestige, prestige, prestige_state, empty
 
 // these systems need to be moved into a separate driver
 COMP( 1996, gkidabc,  0,       0,      prestige, prestige, prestige_state, empty_init, "VTech",  "Genius KID ABC Fan (Germany)",         MACHINE_IS_SKELETON )
-COMP( 2012, cars2lap, 0,       0,      prestige, prestige, prestige_state, empty_init, "VTech",  "CARS 2 Laptop (Germany)",              MACHINE_IS_SKELETON )
