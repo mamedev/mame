3d3e6fd44067de5d5791bfe8aeaa6589ff3ab5c0
cam900 <dbtlrchl@naver.com>
2019-10-30 11:45:36 +0900

    315_5124.cpp : Add notes

--- src/devices/video/315_5124.cpp
+++ src/devices/video/315_5124.cpp
@@ -182,6 +182,7 @@ void sega315_5246_device::sega315_5246_palette(palette_device &palette) const
 
 void sega315_5377_device::sega315_5377_palette(palette_device &palette) const
 {
+	// TODO : linear?
 	for (int i = 0; i < 4096; i++)
 	{
 		const u8 r = i & 0x000f;
