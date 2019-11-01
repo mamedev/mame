b1246642eb6e3f48f1fe19b7ae6584c1e544e88b
couriersud <couriersud@gmx.org>
2019-11-01 15:38:38 +0100

    Fix gcc version 5 compile. (nw)
    Cross compiling for windows on certain ubuntu versions fails without
    this.

--- scripts/genie.lua
+++ scripts/genie.lua
@@ -93,10 +93,16 @@ end
 function addprojectflags()
 	local version = str_to_version(_OPTIONS["gcc_version"])
 	if _OPTIONS["gcc"]~=nil and string.find(_OPTIONS["gcc"], "gcc") then
-		buildoptions_cpp {
-			"-Wsuggest-override",
-			"-flifetime-dse=1",
-		}
+		if version >= 50100 then
+			buildoptions_cpp {
+				"-Wsuggest-override",
+			}
+		end
+		if version >= 60000 then
+			buildoptions_cpp {
+				"-flifetime-dse=1",
+			}
+		end
 	end
 end
 
