74ade68194006625397f4ef8aa1fdf223d633ba3
Robert Hildinger <rdh_pub@me.com>
2019-11-03 03:16:45 -0600

    galaga: Implement accurate starfield based on reverse eng. 05xx (#5824)
    * galaga: Implement accurate starfield based on reverse eng. 05xx
    ----------------------------------------------------------------
    This commit implements an accurate starfield for Galaga and
    Bosconian based on reverse engineering the Namco 05xx chip.
    Documentation and notes have been added inline to the code
    for the galaga video driver.
    
    Changes in this commit:
    
    1. Regenerates starfield colors based on LFSR state for every frame
    2. Pixel accurate to Namco original 05xx
    3. Restores one line horizontal starfield shift for every 256
       vertical pixel shifts
    4. Properly handles LFSR reset (_STARCLR)
    5. Restores missing 4 stars from previous implementations
    6. Fixes potential issue with the number of stars on screen at any
       one time
    7. Restores 4 pixel/line scrolling capability (not currently used
       for any driver)
    
    * galaga: inplement accurate starfield - change request 1
    -------------------------------------------------------
    1. Remove stdlib.h from src/mame/video/galaga.cpp
    
    * galaga: inplement accurate starfield - change request 2
    -------------------------------------------------------
    1. Refactor starfield generator into stand-alone video
       device
    2. Simplify Bosconian and Galaga video drivers
    3. Add in documentation about different LFSR form used
       by Wolfgang Scherr and Jindřich Makovička.
    
    * galaga: inplement accurate starfield - change request 3
    ---------------------------------------------------------
    Minor changes from pull request review

---
+++ src/mame/video/starfield_05xx.h
@@ -0,0 +1,40 @@
+// license:BSD-3-Clause
+// copyright-holders:Robert Hildinger
+#ifndef MAME_VIDEO_STARFIELD_05XX_H
+#define MAME_VIDEO_STARFIELD_05XX_H
+
+// used by galaga, bosconian, and their various clones
+class starfield_05xx_device : public device_t
+{
+public:
+	starfield_05xx_device(machine_config const &mconfig, char const *tag, device_t *owner, uint32_t clock);
+
+	void enable_starfield(uint8_t on);
+	void set_scroll_speed(uint8_t index_x, uint8_t index_y);
+	void set_active_starfield_sets(uint8_t set_a, uint8_t set_b);
+	void set_starfield_config(uint16_t off_x, uint16_t off_y, uint16_t lim_x);
+	void draw_starfield(bitmap_ind16 &bitmap, const rectangle &cliprect, int flip);
+
+protected:
+	virtual void device_start() override;
+	virtual void device_reset() override;
+
+private:
+	uint16_t get_next_lfsr_state(uint16_t lfsr);
+	
+	uint8_t  m_enable;
+	uint16_t m_lfsr;
+	uint16_t m_pre_vis_cycle_count;
+	uint16_t m_post_vis_cycle_count;
+	uint8_t  m_set_a;
+	uint8_t  m_set_b;
+	
+	uint16_t m_offset_x;
+	uint16_t m_offset_y;
+	uint16_t m_limit_x;
+};
+
+
+DECLARE_DEVICE_TYPE(STARFIELD_05XX, starfield_05xx_device)
+
+#endif // MAME_VIDEO_STARFIELD_05XX_H
