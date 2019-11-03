5b6b3f4e75ed5b39c64e927d23084c45dee69228
couriersud <couriersud@gmx.org>
2019-11-03 23:28:01 +0100

    pongf, rebound: Performance increases. [Couriersud]
    Switched nl_pongf to dynamic timestepping. Pongf now runs about 20%
    faster than previously. About the same for rebound.

--- src/mame/machine/nl_pongf.cpp
+++ src/mame/machine/nl_pongf.cpp
@@ -16,10 +16,13 @@
 
 NETLIST_START(pongf)
 
-	SOLVER(Solver, 48000)
+	SOLVER(Solver, 5000)
 	PARAM(Solver.PARALLEL, 0) // Don't do parallel solvers
-	PARAM(Solver.ACCURACY, 1e-4) // works and is sufficient
-	PARAM(Solver.DYNAMIC_LTE,     1e-4) // Default is not enough for paddle control if using LTE
+	PARAM(Solver.VNTOL, 1e-4) // works and is sufficient
+	PARAM(Solver.DYNAMIC_LTE, 1e-1) // Aggressive timestepping
+	PARAM(Solver.METHOD, "MAT_CR")
+	PARAM(Solver.DYNAMIC_TS, 1)
+	PARAM(Solver.DYNAMIC_MIN_TIMESTEP, 2e-5)
 	PARAM(NETLIST.USE_DEACTIVATE, 1)
 
 	ANALOG_INPUT(V5, 5)
