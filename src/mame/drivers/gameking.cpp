0b0dadd8359a6cb8093bb8892f74477504282a34
AJR <ajrhacker@users.noreply.github.com>
2019-11-07 10:03:42 -0500

    gamekin3: It helps not to make mistakes like this (nw)

--- src/mame/drivers/gameking.cpp
+++ src/mame/drivers/gameking.cpp
@@ -1,5 +1,5 @@
 // license:GPL-2.0+
-// copyright-holders:Peter Trauner
+// copyright-holders:Peter Trauner, AJR
 /* TimeTop - GameKing */
 /*
   PeT mess@utanet.at 2015
@@ -101,8 +101,7 @@ void gameking_state::gameking_mem(address_map &map)
 void gameking_state::gameking3_mem(address_map &map)
 {
 	map(0x000000, 0x07ffff).rom().region("maincpu", 0);
-	map(0x800000, 0x802fff).ram();
-	map(0x804800, 0x8048ff).ram();
+	map(0x800000, 0x807fff).ram();
 }
 
 
@@ -116,6 +115,13 @@ static INPUT_PORTS_START( gameking )
 	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_JOYSTICK_RIGHT ) //?
 	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_JOYSTICK_DOWN )
 	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_JOYSTICK_UP)
+INPUT_PORTS_END
+
+static INPUT_PORTS_START( gameking3 )
+	PORT_INCLUDE( gameking )
+	PORT_MODIFY("JOY")
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_START) PORT_NAME("Start")
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_SELECT) PORT_NAME("Select") //?
 INPUT_PORTS_END
 
 static constexpr rgb_t gameking_pens[] =
@@ -333,6 +339,6 @@ ROM_END
 CONS( 2003, gameking, 0, 0, gameking1, gameking, gameking_state, init_gameking, "TimeTop", "GameKing GM-218", MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
 // the GameKing 2 (GM-219) is probably identical HW
 
-CONS( 2003, gamekin3, 0, 0, gameking3, gameking, gameking_state, init_gameking, "TimeTop", "GameKing 3",      MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
+CONS( 2003, gamekin3, 0, 0, gameking3, gameking3, gameking_state, init_gameking, "TimeTop", "GameKing 3",      MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
 // gameking 3: similiar cartridges, accepts gameking cartridges, gameking3 cartridges not working on gameking (illegal cartridge scroller)
 // my gameking bios backup solution might work on it
