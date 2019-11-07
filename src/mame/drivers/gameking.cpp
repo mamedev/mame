4c5332ed008008e34b49c3e9fd09ad5e3a6b5370
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2019-11-07 00:13:04 +0000

    ugly rendering so you can see what gameking3 is showing at least (feel free to ignore, this LCD is not something I understand) (nw)

--- src/mame/drivers/gameking.cpp
+++ src/mame/drivers/gameking.cpp
@@ -34,44 +34,63 @@ public:
 	gameking_state(const machine_config &mconfig, device_type type, const char *tag) :
 		driver_device(mconfig, type, tag),
 		m_maincpu(*this, "maincpu"),
+		m_screen(*this, "screen"),
+		m_palette(*this, "palette"),
 		m_cart(*this, "cartslot"),
-		m_io_joy(*this, "JOY"),
-		m_palette(*this, "palette")
+		m_io_joy(*this, "JOY")
 	{ }
 
 	void gameking(machine_config &config);
-	void gameking3(machine_config &config);
 	void gameking1(machine_config &config);
 
 	void init_gameking();
 
+	void gameking_mem(address_map &map);
+
+	uint8_t input_r();
+	uint8_t input2_r();
+
+	void timer_w(uint8_t data);
+
+	DECLARE_DEVICE_IMAGE_LOAD_MEMBER(cart_load);
+
 protected:
 	virtual void machine_start() override;
 
-private:
-	void gameking_palette(palette_device &palette) const;
-	void timer_w(uint8_t data);
-	uint8_t input_r();
-	uint8_t input2_r();
+	uint32_t screen_update_gameking(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+
+	required_device<st2xxx_device> m_maincpu;
+	required_device<screen_device> m_screen;
+	optional_device<palette_device> m_palette;
+	
+	void gameking_palette(palette_device& palette) const;
+
 	TIMER_CALLBACK_MEMBER(gameking_timer);
 	TIMER_CALLBACK_MEMBER(gameking_timer2);
 
-	uint32_t screen_update_gameking(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	DECLARE_DEVICE_IMAGE_LOAD_MEMBER(cart_load);
-
-	void gameking_mem(address_map &map);
-	void gameking3_mem(address_map &map);
-
-	required_device<cpu_device> m_maincpu;
 	required_device<generic_slot_device> m_cart;
 	required_ioport m_io_joy;
-	required_device<palette_device> m_palette;
 
 	emu_timer *timer1;
 	emu_timer *timer2;
 
 	uint8_t m_timer;
 };
+
+
+class gameking3_state : public gameking_state
+{
+public:
+	gameking3_state(const machine_config& mconfig, device_type type, const char* tag) :
+		gameking_state(mconfig, type, tag)
+	{ }
+
+	void gameking3(machine_config &config);
+	void gameking3_mem(address_map &map);
+
+	uint32_t screen_update_gameking3(screen_device& screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+};
+
 
 
 void gameking_state::timer_w(uint8_t data)
@@ -97,7 +116,7 @@ void gameking_state::gameking_mem(address_map &map)
 	map(0x000000, 0x07ffff).rom().region("maincpu", 0);
 }
 
-void gameking_state::gameking3_mem(address_map &map)
+void gameking3_state::gameking3_mem(address_map &map)
 {
 	map(0x000000, 0x07ffff).rom().region("maincpu", 0);
 	map(0x800000, 0x802fff).ram();
@@ -130,7 +149,6 @@ void gameking_state::gameking_palette(palette_device &palette) const
 	palette.set_pen_colors(0, gameking_pens);
 }
 
-
 uint32_t gameking_state::screen_update_gameking(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect)
 {
 	address_space *maincpu_ram = &m_maincpu->space(AS_PROGRAM);
@@ -148,6 +166,44 @@ uint32_t gameking_state::screen_update_gameking(screen_device &screen, bitmap_in
 			bitmap.pix16(y, x+1)=(data>>4)&3;
 			bitmap.pix16(y, x)=(data>>6)&3;
 		}
+	}
+	return 0;
+}
+
+uint32_t gameking3_state::screen_update_gameking3(screen_device& screen, bitmap_rgb32 &bitmap, const rectangle& cliprect)
+{
+	address_space* maincpu_ram = &m_maincpu->space(AS_PROGRAM);
+	offs_t lssa = m_maincpu->state_int(st2xxx_device::ST_LSSA);
+	if (lssa < 0x0080)
+		return 0;
+
+	int count = 0;
+	for (int y = 0; y < 27; y++)
+	{
+		for (int x = 0; x < 40; x++)
+		{
+			// this is incorrect, but it seems to be at least 12 bits per pixel when compared to the 2 bits for gameking
+			uint8_t data = maincpu_ram->read_byte(lssa + count);
+			uint8_t data2 = maincpu_ram->read_byte(lssa + count+40);
+			uint8_t data3 = maincpu_ram->read_byte(lssa + count+80);
+
+			int r = (data & 0x0f)<<4;
+			int g = (data2 & 0x0f)<<4;
+			int b = (data3 & 0x0f)<<4;
+
+			bitmap.pix32(y, (x * 2) + 1) = (r << 16) | (g << 8) | (b << 0);
+			
+			r = (data & 0xf0);
+			g = (data2 & 0xf0);
+			b = (data3 & 0xf0);
+	
+			bitmap.pix32(y, (x * 2) + 0) = (r << 16) | (g << 8) | (b << 0);
+
+			count += 1;
+		}
+
+		count += 40;
+		count += 40;
 	}
 	return 0;
 }
@@ -204,20 +260,20 @@ void gameking_state::machine_start()
 void gameking_state::gameking(machine_config &config)
 {
 	/* basic machine hardware */
-	st2xxx_device &maincpu(ST2204(config, m_maincpu, 6000000));
-	maincpu.set_addrmap(AS_DATA, &gameking_state::gameking_mem);
-	maincpu.in_pa_callback().set(FUNC(gameking_state::input_r));
-	maincpu.in_pb_callback().set(FUNC(gameking_state::input2_r));
-	maincpu.out_pc_callback().set(FUNC(gameking_state::timer_w)); // wrong
-	maincpu.in_pl_callback().set_constant(6); // bios protection endless loop
+	ST2204(config, m_maincpu, 6000000);
+	m_maincpu->set_addrmap(AS_DATA, &gameking_state::gameking_mem);
+	m_maincpu->in_pa_callback().set(FUNC(gameking_state::input_r));
+	m_maincpu->in_pb_callback().set(FUNC(gameking_state::input2_r));
+	m_maincpu->out_pc_callback().set(FUNC(gameking_state::timer_w)); // wrong
+	m_maincpu->in_pl_callback().set_constant(6); // bios protection endless loop
 
 	/* video hardware */
-	screen_device &screen(SCREEN(config, "screen", SCREEN_TYPE_LCD));
-	screen.set_refresh_hz(60);
-	screen.set_size(48, 32);
-	screen.set_visarea_full();
-	screen.set_screen_update(FUNC(gameking_state::screen_update_gameking));
-	screen.set_palette(m_palette);
+	SCREEN(config, m_screen, SCREEN_TYPE_LCD);
+	m_screen->set_refresh_hz(60);
+	m_screen->set_size(48, 32);
+	m_screen->set_visarea_full();
+	m_screen->set_screen_update(FUNC(gameking_state::screen_update_gameking));
+	m_screen->set_palette(m_palette);
 
 	PALETTE(config, m_palette, FUNC(gameking_state::gameking_palette), ARRAY_LENGTH(gameking_pens));
 
@@ -225,16 +281,33 @@ void gameking_state::gameking(machine_config &config)
 	GENERIC_CARTSLOT(config, "cartslot", generic_plain_slot, "gameking_cart", "bin").set_device_load(FUNC(gameking_state::cart_load));
 }
 
+
 void gameking_state::gameking1(machine_config &config)
 {
 	gameking(config);
 	SOFTWARE_LIST(config, "cart_list").set_original("gameking");
 }
 
-void gameking_state::gameking3(machine_config &config)
+void gameking3_state::gameking3(machine_config &config)
 {
-	gameking(config);
-	m_maincpu->set_addrmap(AS_DATA, &gameking_state::gameking3_mem);
+	/* basic machine hardware */
+	ST2204(config, m_maincpu, 6000000);
+	m_maincpu->set_addrmap(AS_DATA, &gameking3_state::gameking3_mem);
+	m_maincpu->in_pa_callback().set(FUNC(gameking_state::input_r));
+	m_maincpu->in_pb_callback().set(FUNC(gameking_state::input2_r));
+	m_maincpu->out_pc_callback().set(FUNC(gameking_state::timer_w)); // wrong
+	m_maincpu->in_pl_callback().set_constant(6); // bios protection endless loop
+
+	/* video hardware */
+	SCREEN(config, m_screen, SCREEN_TYPE_LCD);
+	m_screen->set_refresh_hz(60);
+	m_screen->set_size(80, 27);
+	m_screen->set_visarea_full();
+	m_screen->set_screen_update(FUNC(gameking3_state::screen_update_gameking3));
+
+	/* cartridge */
+	GENERIC_CARTSLOT(config, "cartslot", generic_plain_slot, "gameking_cart", "bin").set_device_load(FUNC(gameking_state::cart_load));
+
 	SOFTWARE_LIST(config, "cart_list").set_original("gameking");
 	SOFTWARE_LIST(config, "cart_list_3").set_original("gameking3");
 }
@@ -253,6 +326,6 @@ ROM_END
 CONS( 2003, gameking, 0, 0, gameking1, gameking, gameking_state, init_gameking, "TimeTop", "GameKing GM-218", MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
 // the GameKing 2 (GM-219) is probably identical HW
 
-CONS( 2003, gamekin3, 0, 0, gameking3, gameking, gameking_state, init_gameking, "TimeTop", "GameKing 3",      MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
+CONS( 2003, gamekin3, 0, 0, gameking3, gameking, gameking3_state, init_gameking, "TimeTop", "GameKing 3",      MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
 // gameking 3: similiar cartridges, accepts gameking cartridges, gameking3 cartridges not working on gameking (illegal cartridge scroller)
 // my gameking bios backup solution might work on it
