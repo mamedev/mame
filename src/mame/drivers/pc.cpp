524b29dc9b8f8c7bd4bb1110dc2a3300f2234feb
rfka01 <rfka01@freenet.de>
2019-11-10 12:18:50 +0100

    pc.cpp: added Olystar 20F, some simplifications

--- src/mame/drivers/pc.cpp
+++ src/mame/drivers/pc.cpp
@@ -55,8 +55,8 @@ public:
 	void pccga(machine_config &config);
 	void mk88(machine_config &config);
 	void eppc(machine_config &config);
+	void olystar20f(machine_config &config);
 	void olytext30(machine_config &config);
-	void laser_xt3(machine_config &config);
 	void zenith(machine_config &config);
 	void eagle1600(machine_config &config);
 	void eaglespirit(machine_config &config);
@@ -198,7 +198,7 @@ void pc_state::cfg_single_720K(device_t *device)
 void pc_state::pccga(machine_config &config)
 {
 	/* basic machine hardware */
-	i8088_cpu_device &maincpu(I8088(config, "maincpu", 4772720)); /* 4.77 MHz */
+	i8088_cpu_device &maincpu(I8088(config, "maincpu", XTAL(14'318'181)/3)); /* 4.77 MHz */
 	maincpu.set_addrmap(AS_PROGRAM, &pc_state::pc8_map);
 	maincpu.set_addrmap(AS_IO, &pc_state::pc8_io);
 	maincpu.set_irq_acknowledge_callback("mb:pic8259", FUNC(pic8259_device::inta_cb));
@@ -1046,39 +1046,6 @@ Options: 8087 FPU
 
 ******************************************************************************/
 
-void pc_state::laser_turbo_xt(machine_config &config)
-{
-	i8088_cpu_device &maincpu(I8088(config, "maincpu", XTAL(14'318'181)/3)); /* 4.77 MHz */
-	maincpu.set_addrmap(AS_PROGRAM, &pc_state::pc8_map);
-	maincpu.set_addrmap(AS_IO, &pc_state::pc8_io);
-	maincpu.set_irq_acknowledge_callback("mb:pic8259", FUNC(pic8259_device::inta_cb));
-
-	ibm5160_mb_device &mb(IBM5160_MOTHERBOARD(config, "mb", 0));
-	mb.set_cputag(m_maincpu);
-	mb.int_callback().set_inputline(m_maincpu, 0);
-	mb.nmi_callback().set_inputline(m_maincpu, INPUT_LINE_NMI);
-	mb.set_input_default(DEVICE_INPUT_DEFAULTS_NAME(pccga));
-
-	// FIXME: determine ISA bus clock
-	ISA8_SLOT(config, "isa1", 0, "mb:isa", pc_isa8_cards, "cga", false);
-	ISA8_SLOT(config, "isa2", 0, "mb:isa", pc_isa8_cards, "com", false); // Multi I/O card (includes FDC)
-	ISA8_SLOT(config, "isa3", 0, "mb:isa", pc_isa8_cards, "fdc_xt", false);
-	ISA8_SLOT(config, "isa4", 0, "mb:isa", pc_isa8_cards, nullptr, false);
-	ISA8_SLOT(config, "isa5", 0, "mb:isa", pc_isa8_cards, nullptr, false);
-	ISA8_SLOT(config, "isa6", 0, "mb:isa", pc_isa8_cards, nullptr, false);
-	ISA8_SLOT(config, "isa7", 0, "mb:isa", pc_isa8_cards, nullptr, false);
-	ISA8_SLOT(config, "isa8", 0, "mb:isa", pc_isa8_cards, nullptr, false);
-
-	/* keyboard */
-	PC_KBDC_SLOT(config, "kbd", pc_xt_keyboards, STR_KBD_IBM_PC_XT_83).set_pc_kbdc_slot(subdevice("mb:pc_kbdc"));
-
-	/* internal ram */
-	RAM(config, RAM_TAG).set_default_size("640K").set_extra_options("512K,768K,896K,1024K,1408K,1536K,1664K");
-
-	/* software lists */
-	SOFTWARE_LIST(config, "disk_list").set_original("ibm5150");
-}
-
 ROM_START( laser_turbo_xt )
 	ROM_REGION(0x10000, "bios", 0)
 	ROM_LOAD("laser_turbo_xt.bin", 0x0e000, 0x02000, CRC(0a6121d3) SHA1(59b1f8dd6fe981ef9a7700adebf6e1adda7cee90)) // version 1.11 - 27c64d
@@ -1330,6 +1297,38 @@ ROM_START( nms9100 )
 	ROMX_LOAD("philipsxt.bin", 0x8000, 0x8000, CRC(2f3135e7) SHA1(d2fc4c06cf09e2c5a62017f0977b084be8bf9bbd), ROM_BIOS(2))
 ROM_END
 
+/************************************************* AEG Olympia Olystar 20F ***
+Form Factor: Desktop
+uses an Acer 710IIN motherboard, BIOS-Version 4.06
+CPU: AMD P8088-1, FPU socket available
+Chips: Acer M1101, 2201A, UM8250B, WD37C65B-PL , Paradise PVC4
+OSC: 14.31818, 30.000000MHz, 16.000, 1.832
+RAM: 640K (256K, 512K, 768K, 1024K)
+Bus: two ISA8 slots on a riser card
+Video: Hercules/CGA compatible, on board
+Mass storage: Floppy 720KB, HD 20MB on WD MFM-controller
+On board ports: parallel, serial, Video, keyboard (Mini-DIN)
+
+*****************************************************************************/
+
+void pc_state::olystar20f(machine_config &config)
+{
+	pccga(config);
+
+	subdevice<isa8_slot_device>("isa2")->set_option_machine_config("fdc_xt", cfg_single_720K);
+	subdevice<isa8_slot_device>("isa3")->set_default_option(nullptr);
+	subdevice<isa8_slot_device>("isa5")->set_default_option("hdc");
+	subdevice<ram_device>(RAM_TAG)->set_default_size("640K").set_extra_options("64K, 128K, 256K, 512K, 768K, 1024K"); // the BIOS detects 2432KB extension RAM in the 640K setting ...
+}
+
+ROM_START( olystar20f )
+	ROM_REGION(0x10000, "bios", 0)
+	ROM_LOAD("20f_ebios_u43_v4.06.bin", 0x8000, 0x8000, CRC(0dddb623) SHA1(d821f48ddc7c77868b3f5952fa12f41911bea406))
+
+	ROM_REGION(0x2000,"gfx1", 0) // on board PVC4 based graphics card (similar to Commodore PC AGA and Schneider EuroPC)
+	ROM_LOAD("20f_u11_v1.3.bin", 0x0000, 0x2000, CRC(d252ee8d) SHA1(035385521abc3d1b79967b5302a87d08f9383215))
+ROM_END
+
 /***************************************************************************
 
   Game driver(s)
@@ -1340,6 +1339,7 @@ ROM_END
 COMP( 1989, mk88,           ibm5150, 0,      mk88,           pccga,    pc_state, empty_init,    "<unknown>",                       "MK-88",                 MACHINE_NOT_WORKING )
 COMP( 1991, poisk2,         ibm5150, 0,      poisk2,         pccga,    pc_state, empty_init,    "<unknown>",                       "Poisk-2",               MACHINE_NOT_WORKING )
 COMP( 1990, mc1702,         ibm5150, 0,      pccga,          pccga,    pc_state, empty_init,    "<unknown>",                       "Elektronika MC-1702",   MACHINE_NOT_WORKING )
+COMP( 198?, olystar20f,     ibm5150, 0,      olystar20f,     pccga,    pc_state, empty_init,    "AEG Olympia",                     "Olystar 20F",           MACHINE_NOT_WORKING )
 COMP( 198?, olytext30,      ibm5150, 0,      olytext30,      pccga,    pc_state, empty_init,    "AEG Olympia",                     "Olytext 30",            MACHINE_NOT_WORKING )
 COMP( 1987, ataripc1,       ibm5150, 0,      ataripc1,       pccga,    pc_state, empty_init,    "Atari",                           "PC1",                   0 )
 COMP( 1988, ataripc3,       ibm5150, 0,      pccga,          pccga,    pc_state, empty_init,    "Atari",                           "PC3",                   0 )
@@ -1372,6 +1372,6 @@ COMP( 1992, iskr3104,       ibm5150, 0,      iskr3104,       pccga,    pc_state,
 COMP( 1985, sicpc1605,      ibm5150, 0,      siemens,        pccga,    pc_state, empty_init,    "Siemens",                         "Sicomp PC16-05",        MACHINE_NOT_WORKING )
 COMP( 1985, pc7000,         ibm5150, 0,      pccga,          pccga,    pc_state, empty_init,    "Sharp",                           "PC-7000",               MACHINE_NOT_WORKING )
 COMP( 198?, hstrtpls,       ibm5150, 0,      pccga,          pccga,    pc_state, empty_init,    "Vendex",                          "HeadStart Plus",        MACHINE_NOT_WORKING )
-COMP( 1988, laser_turbo_xt, ibm5150, 0,      laser_turbo_xt, 0,        pc_state, empty_init,    "VTech",                           "Laser Turbo XT",        0 )
-COMP( 1989, laser_xt3,      ibm5150, 0,      laser_turbo_xt, 0,        pc_state, empty_init,    "VTech",                           "Laser XT/3",            0 )
+COMP( 1988, laser_turbo_xt, ibm5150, 0,      pccga,          pccga,    pc_state, empty_init,    "VTech",                           "Laser Turbo XT",        0 )
+COMP( 1989, laser_xt3,      ibm5150, 0,      pccga,          pccga,    pc_state, empty_init,    "VTech",                           "Laser XT/3",            0 )
 COMP( 1987, zdsupers,       ibm5150, 0,      zenith,         pccga,    pc_state, empty_init,    "Zenith Data Systems",             "SuperSport",            0 )
