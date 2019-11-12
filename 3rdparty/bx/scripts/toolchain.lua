20720d58bb5443dd78f0424ba59901f26777723c
Petr Petrov <petr@gkmsoft.ru>
2019-11-12 13:01:47 +0300

    Removed rudimentary _ITERATOR_DEBUG_LEVEL=0 and _HAS_ITERATOR_DEBUGGING=0 definitions for MS compilers

--- 3rdparty/bx/scripts/toolchain.lua
+++ 3rdparty/bx/scripts/toolchain.lua
@@ -556,8 +556,6 @@ function toolchain(_buildDir, _libDir)
 			"WIN32",
 			"_WIN32",
 			"_HAS_EXCEPTIONS=0",
-			"_HAS_ITERATOR_DEBUGGING=0",
-			"_ITERATOR_DEBUG_LEVEL=0",
 			"_SCL_SECURE=0",
 			"_SECURE_SCL=0",
 			"_SCL_SECURE_NO_WARNINGS",
