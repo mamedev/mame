3ed0c5c22e76a721d5c90a7ae3fc5ad0b56973ca
Joakim Larsson Edstrom <Joakim.Edstrom@edstromious.se>
2019-11-21 12:17:18 +0100

    alfaskop41xx.cpp: Added three skeleton drivers for various parts of Alfaskop 41 system. The 4110 prints a boot prompt but that's it

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -3976,6 +3976,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/akaiax80.cpp",
 	MAME_DIR .. "src/mame/drivers/alcat7100.cpp",
 	MAME_DIR .. "src/mame/drivers/alesis_qs.cpp",
+	MAME_DIR .. "src/mame/drivers/alfaskop41xx.cpp",
 	MAME_DIR .. "src/mame/drivers/alphasma.cpp",
 	MAME_DIR .. "src/mame/drivers/am1000.cpp",
 	MAME_DIR .. "src/mame/drivers/ampex.cpp",
