7e680c0052cc790f576a76e72c6f1d00fded0ef3
AJR <ajrhacker@users.noreply.github.com>
2019-12-28 13:28:49 -0500

    ns5652: One not dumped (nw)

--- src/mame/drivers/ns5652.cpp
+++ src/mame/drivers/ns5652.cpp
@@ -30,7 +30,7 @@ private:
 
 void ns5652_state::mem_map(address_map &map)
 {
-	map(0x0000, 0x0bff).mirror(0xd000).rom().region("eproms", 0);
+	map(0x0000, 0x0bff).mirror(0xd000).rom().region("program", 0);
 	map(0xe800, 0xebff).ram(); // 4x MM2114J-3
 }
 
@@ -47,13 +47,16 @@ void ns5652_state::ns5652(machine_config &config)
 }
 
 ROM_START(ns5652)
-	ROM_REGION16_LE(0x1800, "eproms", 0) // all MM2708Q
+	ROM_REGION16_LE(0x1800, "program", 0) // all MM2708Q
 	ROM_LOAD16_BYTE("5652_001b.bin", 0x0000, 0x0400, CRC(03acf738) SHA1(e512ccf64473e0b7291d8cc14f44858cac2048e6))
 	ROM_LOAD16_BYTE("5652_004b.bin", 0x0001, 0x0400, CRC(b238b1ba) SHA1(90735194cc7f111fc7c1cdde1a9aab4945b00a7e))
 	ROM_LOAD16_BYTE("5652_002b.bin", 0x0800, 0x0400, CRC(2fd33c25) SHA1(5f1bab6c149c19b8c57f9f014d7aecd5d287fae0))
 	ROM_LOAD16_BYTE("5652_005b.bin", 0x0801, 0x0400, CRC(e1d559ed) SHA1(3093d28b661275c00de8145f8424f584a4854072))
 	ROM_LOAD16_BYTE("5652_003b.bin", 0x1000, 0x0400, CRC(24abf1f8) SHA1(ef22ca58e59d8301aab9175ef7ac9dc97feae9ec))
 	ROM_LOAD16_BYTE("5652_006b.bin", 0x1001, 0x0400, CRC(db1dca74) SHA1(05149e85237a742850446c01249c83ba373e66b3))
+
+	ROM_REGION(0x100, "prom", 0) // MM5203Q (256x8 or 512x4)
+	ROM_LOAD("5930_001a.bin", 0x000, 0x100, NO_DUMP)
 ROM_END
 
 COMP(19??, ns5652, 0, 0, ns5652, ns5652, ns5652_state, empty_init, "National Semiconductor", "unknown INS8900 Multibus card (980305652)", MACHINE_IS_SKELETON)
