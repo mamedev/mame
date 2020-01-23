97c5477f4097f13befdc74d1abc3e7c789a8926a
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2020-01-23 03:00:51 +0000

    new WORKING machines --- Cosmo Girl (Senario, Plug and Play) [Sean Riddle, David Haywood] The Apprentice (Senario, Plug and Play) [Sean Riddle, David Haywood]

--- src/mame/drivers/spg2xx_senario.cpp
+++ src/mame/drivers/spg2xx_senario.cpp
@@ -7,7 +7,8 @@
 	these check for flash ROM and actually save user data at 0x700000 in the flash ROM
 
 	TODO:
-	Are the LEDs on the controllers meant to go out as players select answers like with pvmil, or are they just to show that the controller is connected?
+	senmil - Are the LEDs on the controllers meant to go out as players select answers like with pvmil, or are they just to show that the controller is connected?
+	sencosmo - fix Flash hookup
 */
 
 #include "includes/spg2xx.h"
@@ -20,8 +21,21 @@ public:
 		spg2xx_game_state(mconfig, type, tag)
 	{ }
 
+protected:
+	//virtual void machine_start() override;
+	//virtual void machine_reset() override;
+
+private:
+};
+
+class spg2xx_senario_bbs_state : public spg2xx_senario_state
+{
+public:
+	spg2xx_senario_bbs_state(const machine_config& mconfig, device_type type, const char* tag) :
+		spg2xx_senario_state(mconfig, type, tag)
+	{ }
+
 	void senbbs(machine_config& config);
-	
 	void mem_map_flash(address_map &map);
 
 protected:
@@ -31,11 +45,34 @@ protected:
 private:
 };
 
-class spg2xx_senario_mil_state : public spg2xx_senario_state
+class spg2xx_senario_cosmo_state : public spg2xx_senario_bbs_state
+{
+public:
+	spg2xx_senario_cosmo_state(const machine_config& mconfig, device_type type, const char* tag) :
+		spg2xx_senario_bbs_state(mconfig, type, tag),
+		m_romregion(*this, "flash")
+	{ }
+
+	void sencosmo(machine_config& config);
+	void mem_map_flash_bypass(address_map& map);
+
+protected:
+	//virtual void machine_start() override;
+	//virtual void machine_reset() override;
+
+	DECLARE_READ16_MEMBER(read_bypass) { return m_romregion[offset]; }
+	DECLARE_WRITE16_MEMBER(write_bypass) { logerror("Write to ROM area %08x %04x\n", offset, data); }
+
+private:
+	required_region_ptr<uint16_t> m_romregion;
+};
+
+
+class spg2xx_senario_mil_state : public spg2xx_senario_bbs_state
 {
 public:
 	spg2xx_senario_mil_state(const machine_config& mconfig, device_type type, const char* tag) :
-		spg2xx_senario_state(mconfig, type, tag),
+		spg2xx_senario_bbs_state(mconfig, type, tag),
 		m_portc_data(0)
 	{ }
 
@@ -113,10 +150,62 @@ static INPUT_PORTS_START( senbbs ) // reset with Select and Spin held down for t
 	PORT_BIT( 0xffff, IP_ACTIVE_LOW, IPT_UNKNOWN )
 INPUT_PORTS_END
 
-void spg2xx_senario_state::mem_map_flash(address_map &map)
+static INPUT_PORTS_START( senappren )
+	PORT_START("P1")
+	PORT_BIT( 0x0001, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_PLAYER(2) PORT_NAME("Red Team Select")
+	PORT_BIT( 0x0002, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_PLAYER(2) PORT_NAME("Red Team Ok")
+	PORT_BIT( 0x0004, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_PLAYER(1) PORT_NAME("Blue Team Select")
+	PORT_BIT( 0x0008, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_PLAYER(1) PORT_NAME("Blue Team Ok")
+	PORT_BIT( 0x0010, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x0020, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_CODE(KEYCODE_1) PORT_NAME("Console Ok")
+	PORT_BIT( 0x0040, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_CODE(KEYCODE_5) PORT_NAME("Console Select")
+	PORT_BIT( 0x7f80, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x8000, IP_ACTIVE_HIGH, IPT_UNKNOWN ) // Low Battery sensor
+
+	PORT_START("P2")
+	PORT_BIT( 0xffff, IP_ACTIVE_LOW, IPT_UNKNOWN )
+
+	PORT_START("P3")
+	PORT_BIT( 0xffff, IP_ACTIVE_LOW, IPT_UNKNOWN )
+INPUT_PORTS_END
+
+
+static INPUT_PORTS_START( sencosmo ) // hold Pause during power on for Test Menu
+	PORT_START("P1")
+	PORT_BIT( 0x0001, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_PLAYER(1) PORT_NAME("Player 1 A")
+	PORT_BIT( 0x0002, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_PLAYER(1) PORT_NAME("Player 1 B")
+	PORT_BIT( 0x0004, IP_ACTIVE_LOW, IPT_BUTTON3 ) PORT_PLAYER(1) PORT_NAME("Player 1 C")
+	PORT_BIT( 0x0008, IP_ACTIVE_LOW, IPT_BUTTON4 ) PORT_PLAYER(1) PORT_NAME("Player 1 D")
+	PORT_BIT( 0x0030, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x0040, IP_ACTIVE_LOW, IPT_BUTTON5 ) PORT_CODE(KEYCODE_1) PORT_NAME("Console Pause")
+	PORT_BIT( 0x0080, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x0100, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_PLAYER(2) PORT_NAME("Player 2 A")
+	PORT_BIT( 0x0200, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_PLAYER(2) PORT_NAME("Player 2 B")
+	PORT_BIT( 0x0400, IP_ACTIVE_LOW, IPT_BUTTON3 ) PORT_PLAYER(2) PORT_NAME("Player 2 C")
+	PORT_BIT( 0x0800, IP_ACTIVE_LOW, IPT_BUTTON4 ) PORT_PLAYER(2) PORT_NAME("Player 2 D")
+	PORT_BIT( 0x1000, IP_ACTIVE_LOW, IPT_BUTTON6 ) PORT_CODE(KEYCODE_5) PORT_NAME("Console Power")
+	PORT_BIT( 0x6000, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x8000, IP_ACTIVE_HIGH, IPT_UNKNOWN ) // Low Battery sensor
+
+	PORT_START("P2")
+	PORT_BIT( 0xffff, IP_ACTIVE_LOW, IPT_UNKNOWN )
+
+	PORT_START("P3")
+	PORT_BIT( 0xffff, IP_ACTIVE_LOW, IPT_UNKNOWN )
+INPUT_PORTS_END
+
+
+void spg2xx_senario_bbs_state::mem_map_flash(address_map &map)
 {
 	map(0x000000, 0x3fffff).rw("flash", FUNC(spansion_s29gl064s_device::read), FUNC(spansion_s29gl064s_device::write));
 }
+
+// this is meant to be flash, but it crashes with invalid flash command when answering questions?
+void spg2xx_senario_cosmo_state::mem_map_flash_bypass(address_map &map)
+{
+	map(0x000000, 0x1fffff).rw(FUNC(spg2xx_senario_cosmo_state::read_bypass), FUNC(spg2xx_senario_cosmo_state::write_bypass));
+}
+
 
 WRITE16_MEMBER(spg2xx_senario_mil_state::portc_w)
 {
@@ -134,10 +223,10 @@ READ16_MEMBER(spg2xx_senario_mil_state::portc_r)
 	return ret;
 }
 
-void spg2xx_senario_state::senbbs(machine_config &config)
+void spg2xx_senario_bbs_state::senbbs(machine_config &config)
 {
 	SPG24X(config, m_maincpu, XTAL(27'000'000), m_screen);
-	m_maincpu->set_addrmap(AS_PROGRAM, &spg2xx_senario_state::mem_map_flash);
+	m_maincpu->set_addrmap(AS_PROGRAM, &spg2xx_senario_bbs_state::mem_map_flash);
 
 	spg2xx_base(config);
 
@@ -148,25 +237,49 @@ void spg2xx_senario_state::senbbs(machine_config &config)
 	SPANSION_S29GL064S(config, "flash");
 }
 
+void spg2xx_senario_cosmo_state::sencosmo(machine_config& config)
+{
+	senbbs(config);
+	m_maincpu->set_addrmap(AS_PROGRAM, &spg2xx_senario_cosmo_state::mem_map_flash_bypass);
+}
+
+
 void spg2xx_senario_mil_state::senmil(machine_config& config)
 {
-	spg2xx_senario_state::senbbs(config);
+	senbbs(config);
 
 	m_maincpu->portc_in().set(FUNC(spg2xx_senario_mil_state::portc_r));
 	m_maincpu->portc_out().set(FUNC(spg2xx_senario_mil_state::portc_w));
 }
 
-// note not using ROM_LOAD16_WORD_SWAP because this is a Flash ROM region, and we end up with wrong endian if we do
+// is ROM_REGION16_BE correct here, allows us to use ROM_LOAD16_WORD_SWAP as when loading SunPlus stuff in other cases
+
+ROM_START( senapren )
+	ROM_REGION16_BE( 0x800000, "flash", ROMREGION_ERASE00 )
+	ROM_LOAD16_WORD_SWAP( "apprentice.bin", 0x000000, 0x200000, BAD_DUMP CRC(1c919e72) SHA1(20efcb992bd3ff8ab78470bd484f4f0b226e6c15) ) // needs further verification
+	// That one has a SOP44 COB instead of a TSOP48 chip.  Pin 1 is N/C, 32 is grounded, and 33 is tied high.  That means a max of A0-A20 = 21 16-bit address lines, for 4MB
+	// Data repeated dumped as 4MB, so 2MB? (but game attempts to write to 0x700000 for flash user data which would erase game data in a 2MB ROM)
+ROM_END
+
+ROM_START( sencosmo )
+	ROM_REGION16_BE( 0x400000, "flash", ROMREGION_ERASE00 )
+	ROM_LOAD16_WORD_SWAP( "cosmo.bin", 0x000000, 0x400000, BAD_DUMP CRC(1ec50795) SHA1(621c4e03b5713f3678d2935f8938f15c5d4a5fdf) ) // needs further verification
+	// attempts to write to 0x380000 for flash user data? different Flash type?
+ROM_END
 
 ROM_START( senmil )
-	ROM_REGION( 0x800000, "flash", ROMREGION_ERASE00 )
-	ROM_LOAD( "wwtbam_nouserdata.bin", 0x000000, 0x800000, CRC(b2626df6) SHA1(f06943d63dbb1c9d211cb35b40dcb18cb8b39ecd) )
+	ROM_REGION16_BE( 0x800000, "flash", ROMREGION_ERASE00 )
+	ROM_LOAD16_WORD_SWAP( "wwtbam_nouserdata.bin", 0x000000, 0x800000, CRC(b2626df6) SHA1(f06943d63dbb1c9d211cb35b40dcb18cb8b39ecd) )
 ROM_END
 
 ROM_START( senbbs )
-	ROM_REGION( 0x800000, "flash", ROMREGION_ERASE00 )
-	ROM_LOAD( "bigbonusslots.bin", 0x000000, 0x800000, CRC(071effc3) SHA1(892c05a8b64a388b331ad0d361bf4c523c6c14c9) )
+	ROM_REGION16_BE( 0x800000, "flash", ROMREGION_ERASE00 )
+	ROM_LOAD16_WORD_SWAP( "bigbonusslots.bin", 0x000000, 0x800000, CRC(071effc3) SHA1(892c05a8b64a388b331ad0d361bf4c523c6c14c9) )
 ROM_END
 
-CONS( 2005, senbbs,      0,     0,        senbbs,       senbbs,    spg2xx_senario_state,     empty_init, "Senario", "Big Bonus Slots (Senario, Plug and Play)", MACHINE_IMPERFECT_SOUND | MACHINE_IMPERFECT_GRAPHICS )
+
+
+CONS( 2005, senbbs,      0,     0,        senbbs,       senbbs,    spg2xx_senario_bbs_state, empty_init, "Senario", "Big Bonus Slots (Senario, Plug and Play)", MACHINE_IMPERFECT_SOUND | MACHINE_IMPERFECT_GRAPHICS )
+CONS( 2005, senapren,    0,     0,        senbbs,       senappren, spg2xx_senario_bbs_state, empty_init, "Senario", "The Apprentice (Senario, Plug and Play)", MACHINE_IMPERFECT_SOUND | MACHINE_IMPERFECT_GRAPHICS )
+CONS( 2005, sencosmo,    0,     0,        sencosmo,     sencosmo,  spg2xx_senario_cosmo_state, empty_init, "Senario", "Cosmo Girl (Senario, Plug and Play)", MACHINE_IMPERFECT_SOUND | MACHINE_IMPERFECT_GRAPHICS )
 CONS( 2006, senmil,      0,     0,        senmil,       senmil,    spg2xx_senario_mil_state, empty_init, "Senario", "Who Wants to Be a Millionaire? (Senario, Plug and Play, US)", MACHINE_IMPERFECT_SOUND | MACHINE_IMPERFECT_GRAPHICS )
