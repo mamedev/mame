bf5cf7715bcbf375d404452b7537a064966ad23c
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2020-01-30 19:36:13 +0000

    majgnc input improvements (nw)

--- src/mame/drivers/nes_vt.cpp
+++ src/mame/drivers/nes_vt.cpp
@@ -479,15 +479,13 @@ public:
 protected:
 
 private:
-	virtual DECLARE_READ8_MEMBER(in0_r) override;
-	virtual DECLARE_READ8_MEMBER(in1_r) override;
-	virtual DECLARE_WRITE8_MEMBER(in0_w) override;
 };
 
 /* Standard I/O handlers (NES Controller clone) */
 
 READ8_MEMBER(nes_vt_state::in0_r)
 {
+	//logerror("%s: in0_r\n", machine().describe_context());
 	uint8_t ret = 0x40;
 	ret |= m_latch0 & 1;
 	m_latch0 >>= 1;
@@ -496,6 +494,7 @@ READ8_MEMBER(nes_vt_state::in0_r)
 
 READ8_MEMBER(nes_vt_state::in1_r)
 {
+	//logerror("%s: in1_r\n", machine().describe_context());
 	uint8_t ret = 0x40;
 	ret |= m_latch1 & 1;
 	m_latch1 >>= 1;
@@ -504,6 +503,7 @@ READ8_MEMBER(nes_vt_state::in1_r)
 
 WRITE8_MEMBER(nes_vt_state::in0_w)
 {
+	//logerror("%s: in0_w %02x\n", machine().describe_context(), data);
 	if (data & 0x01)
 		return;
 
@@ -1548,7 +1548,7 @@ WRITE8_MEMBER(nes_vt_state::extra_io_control_w)
 	0x80 Extra I/O port 3 enable (1 = enable, 0 = disable)
 	*/
 
-	logerror("%s: extra_io_control_w %02x\n", data);
+	logerror("%s: extra_io_control_w %02x\n", machine().describe_context(), data);
 }
 
 READ8_MEMBER(nes_vt_state::extrain_01_r)
@@ -1694,20 +1694,6 @@ READ8_MEMBER(nes_vt_sudoku_state::in1_r)
 }
 
 WRITE8_MEMBER(nes_vt_sudoku_state::in0_w)
-{
-}
-
-READ8_MEMBER(nes_vt_majgnc_state::in0_r)
-{
-	return 0x00;// machine().rand();
-}
-
-READ8_MEMBER(nes_vt_majgnc_state::in1_r)
-{
-	return 0x00;// machine().rand();
-}
-
-WRITE8_MEMBER(nes_vt_majgnc_state::in0_w)
 {
 }
 
@@ -2209,56 +2195,35 @@ static INPUT_PORTS_START( majgnc )
 	PORT_INCLUDE(nes_vt)
 
 	PORT_MODIFY("IO0")
-	PORT_DIPNAME( 0x01, 0x01, "0" )
-	PORT_DIPSETTING(    0x01, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x02, 0x02, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x02, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x04, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x08, 0x08, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x08, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x10, 0x10, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x10, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x20, 0x20, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x20, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x40, 0x40, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x40, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x80, 0x80, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x80, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_PLAYER(1) PORT_NAME("A")
+	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_PLAYER(1) PORT_NAME("B")
+	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_SELECT ) PORT_PLAYER(1)
+	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_START ) PORT_PLAYER(1)
+	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP ) PORT_PLAYER(1) PORT_8WAY
+	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN ) PORT_PLAYER(1) PORT_8WAY
+	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(1) PORT_8WAY
+	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(1) PORT_8WAY
 
 	PORT_MODIFY("IO1")
-	PORT_DIPNAME( 0x01, 0x01, "1" )
-	PORT_DIPSETTING(    0x01, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x02, 0x02, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x02, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x04, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x08, 0x08, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x08, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x10, 0x10, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x10, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x20, 0x20, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x20, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x40, 0x40, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x40, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x80, 0x80, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x80, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+	PORT_BIT( 0xff, IP_ACTIVE_HIGH, IPT_UNUSED )
+
+	PORT_START("EXTRAIN0")
+	PORT_BIT( 0x0f, IP_ACTIVE_HIGH, IPT_UNUSED )
+
+	PORT_START("EXTRAIN1")
+	PORT_BIT( 0x0f, IP_ACTIVE_HIGH, IPT_UNUSED )
+
+	PORT_START("EXTRAIN2")
+	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(2) PORT_8WAY
+	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(2) PORT_8WAY
+	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN ) PORT_PLAYER(2) PORT_8WAY
+	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP ) PORT_PLAYER(2) PORT_8WAY
+
+	PORT_START("EXTRAIN3")
+	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_START ) PORT_PLAYER(2)
+	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_SELECT ) PORT_PLAYER(2)
+	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_PLAYER(2) PORT_NAME("B")
+	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_PLAYER(2) PORT_NAME("A")
 INPUT_PORTS_END
 
 void nes_vt_sudoku_state::init_sudoku()
