e8c7e76bd8c9b54d3de3d07a71f5f911fa3ccb48
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2020-02-07 11:31:18 +0000

    move these (nw)

--- src/mame/drivers/nes_vt.cpp
+++ src/mame/drivers/nes_vt.cpp
@@ -252,23 +252,6 @@ protected:
 private:
 };
 
-class nes_vt_ts_state : public nes_vt_state
-{
-public:
-	nes_vt_ts_state(const machine_config& mconfig, device_type type, const char* tag) :
-		nes_vt_state(mconfig, type, tag)
-	{
-		m_initial_e000_bank = 0x03; // or the banking is just different / ROM is scrambled
-	}
-
-	void nes_vt_ts(machine_config& config);
-
-protected:
-	void nes_vt_ts_map(address_map& map);
-
-private:
-};
-
 class nes_vt_pjoy_state : public nes_vt_state
 {
 public:
@@ -1851,16 +1834,6 @@ void nes_vt_dg_state::nes_vt_fa_map(address_map &map)
 	map(0x4242, 0x4242).w(FUNC(nes_vt_dg_state::vtfp_4242_w));
 }
 
-void nes_vt_ts_state::nes_vt_ts_map(address_map& map)
-{
-	nes_vt_map(map);
-	map(0x0800, 0x1fff).ram(); // how much RAM?
-
-	map(0x5000, 0x57ff).ram(); // plays music if you map this as RAM
-
-	map(0x2040, 0x207f).ram(); // strange regs in vdp area
-}
-
 void nes_vt_state::prg_map(address_map &map)
 {
 	map(0x0000, 0x1fff).bankr("prg_bank0");
@@ -2148,13 +2121,6 @@ void nes_vt_vh2009_state::nes_vt_vh2009(machine_config &config)
 	m_maincpu->set_addrmap(AS_PROGRAM, &nes_vt_vh2009_state::nes_vt_map);
 
 	//m_ppu->set_palette_mode(PAL_MODE_NEW_VG); // gives better title screens, but worse ingame, must be able to switch
-}
-
-void nes_vt_ts_state::nes_vt_ts(machine_config &config)
-{
-	nes_vt(config);
-
-	m_maincpu->set_addrmap(AS_PROGRAM, &nes_vt_ts_state::nes_vt_ts_map);
 }
 
 static INPUT_PORTS_START( nes_vt_fp )
@@ -2645,17 +2611,6 @@ ROM_END
 
 
 
-ROM_START( bandgpad )
-	ROM_REGION( 0x100000, "mainrom", 0 )
-	ROM_LOAD( "gamepad.bin", 0x00000, 0x100000, CRC(e2fbb532) SHA1(e9170a7739a8355acbf263fe2b1d291951dc07f0) )
-ROM_END
-
-ROM_START( ts_handy11 )
-	ROM_REGION( 0x100000, "mainrom", 0 )
-	ROM_LOAD( "tvplaypowercontroller.bin", 0x00000, 0x100000, CRC(9c7fe9ff) SHA1(c872e91ca835b66c9dd3b380e8374b51f12bcae0) ) // 29LV008B
-ROM_END
-
-
 // earlier version of vdogdemo
 CONS( 200?, vdogdeme,  0,  0,  nes_vt,    nes_vt, nes_vt_state, empty_init, "VRT", "V-Dog (prototype, earlier)", MACHINE_NOT_WORKING )
 
@@ -2835,9 +2790,4 @@ CONS( 2017, fapocket,   0,        0,  nes_vt_fa, nes_vt_fa, nes_vt_dg_state, emp
 
 // Plays intro music but then crashes. same hardware as SY-88x but uses more features
 CONS( 2016, mog_m320,   0,        0,  nes_vt_hh, nes_vt, nes_vt_hh_state, empty_init, "MOGIS",    "MOGIS M320 246 in 1 Handheld", MACHINE_NOT_WORKING )
-
-// UMC 6578 based - NOT VT
-CONS( 1997, bandgpad,  0,  0,  nes_vt_ts,    nes_vt, nes_vt_ts_state, empty_init, "Bandai", "Gamppad (Bandai)", MACHINE_NOT_WORKING )
-// possibly newer than 2001 but most games have a 2001 copyright.  Most games are higher colour versions of NES games, so it's an enhanced NES chipset at least but maybe not VT?
-CONS( 2001, ts_handy11,  0,  0,  nes_vt_ts,    nes_vt, nes_vt_ts_state, empty_init, "Techno Source", "Handy Boy 11-in-1 (TV Play Power)", MACHINE_NOT_WORKING )
 
