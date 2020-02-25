d04d3df50e9e76d5eee6110b9e9dcd65e20f9bb8
MetalliC <0vetal0@gmail.com>
2020-02-25 17:30:40 +0200

    few notes (nw)

--- src/mame/drivers/naomi.cpp
+++ src/mame/drivers/naomi.cpp
@@ -3564,7 +3564,8 @@ Region byte encoding is as follows:
    First half is BIOS, second half is game settings and is blanked/reprogrammed by the BIOS if game cartridge exchange was detected
    area 0x1A000-0x1BFFF is write protected and contain 12 bytes of unit-specific unique information (probably serial number, manufacture date, etc),
    2 dumps included for reference
-   fpr-24363.ic48 - later Sega's boot ROM, have removed Sammy logo, only last 1/4 used (2 high address bits tied to Vcc).
+   fpr-24363.ic48 - BOOT ROM from Sega 837-14624R PCB, in comparison with Sammy's BOOT contain minor code change to keep video output disabled during Sammy logo display routine.
+                    Only last 128KB of whole 512KB ROM actually used (2 high address pins tied to Vcc).
 */
 #define AW_BIOS \
 	ROM_REGION64_LE( 0x200000, "awflash", 0) \
