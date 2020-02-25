95b9b6e3d9687787be54257c0e88f0b2581a92fb
firewave <firewave@users.noreply.github.com>
2020-02-25 07:59:34 +0100

    fixed merge error

--- src/osd/modules/diagnostics/diagnostics_win32.cpp
+++ src/osd/modules/diagnostics/diagnostics_win32.cpp
@@ -663,15 +663,6 @@ sampling_profiler::~sampling_profiler()
 }
 
 
-//-------------------------------------------------
-//  sampling_profiler - destructor
-//-------------------------------------------------
-
-sampling_profiler::~sampling_profiler()
-{
-}
-
-
 ////-------------------------------------------------
 //  start - begin gathering profiling information
 //-------------------------------------------------
