cf3bba5bf8396b0097f21472d960e0f52659968e
AJR <ajrhacker@users.noreply.github.com>
2020-02-25 21:48:34 -0500

    New machines marked as NOT_WORKING ---------------------------------- Roland R-8 Human Rhythm Composer (v2.02) [DBWBP] Roland R-8 Mk II Human Rhythm Composer (v1.0.3) [DBWBP]

--- src/mame/drivers/roland_r8.cpp
+++ src/mame/drivers/roland_r8.cpp
@@ -19,11 +19,12 @@ public:
 	}
 
 	void r8(machine_config &config);
+	void r8mk2(machine_config &config);
 
 private:
 	void mem_map(address_map &map);
 
-	required_device<upd78210_device> m_maincpu;
+	required_device<upd78k2_device> m_maincpu;
 };
 
 
@@ -43,11 +44,29 @@ void roland_r8_state::r8(machine_config &config)
 	m_maincpu->set_addrmap(AS_PROGRAM, &roland_r8_state::mem_map);
 }
 
+void roland_r8_state::r8mk2(machine_config &config)
+{
+	UPD78213(config, m_maincpu, 12_MHz_XTAL);
+	m_maincpu->set_addrmap(AS_PROGRAM, &roland_r8_state::mem_map);
+}
+
+
+ROM_START(r8)
+	ROM_REGION(0x20000, "maincpu", 0)
+	ROM_LOAD("roland r-8_2.02_27c010.bin", 0x00000, 0x20000, CRC(45d0f64f) SHA1(55f0831db74cbdeae20cd7f1ff28af27dafba9b9))
+ROM_END
 
 ROM_START(r8m)
 	ROM_REGION(0x20000, "maincpu", 0)
 	ROM_LOAD("rolandr8mv104.bin", 0x00000, 0x20000, CRC(5e95e2f6) SHA1(b4e1a8f15f72a9db9aa8fd41ee3c3ebd10460587))
 ROM_END
 
+ROM_START(r8mk2)
+	ROM_REGION(0x20000, "maincpu", 0)
+	ROM_LOAD("roland r8 mkii eprom v1.0.3.bin", 0x00000, 0x20000, CRC(128a9a0c) SHA1(94bd8c76efe270754219f2899f31b62fc4f9060d))
+ROM_END
 
-SYST(1990, r8m, 0, 0, r8, r8, roland_r8_state, empty_init, "Roland", "R-8M Total Percussion Sound Module (v1.04)", MACHINE_IS_SKELETON)
+
+SYST(1989, r8,    0,  0, r8,    r8, roland_r8_state, empty_init, "Roland", "R-8 Human Rhythm Composer (v2.02)", MACHINE_IS_SKELETON)
+SYST(1990, r8m,   r8, 0, r8,    r8, roland_r8_state, empty_init, "Roland", "R-8M Total Percussion Sound Module (v1.04)", MACHINE_IS_SKELETON)
+SYST(1992, r8mk2, 0,  0, r8mk2, r8, roland_r8_state, empty_init, "Roland", "R-8 Mk II Human Rhythm Composer (v1.0.3)", MACHINE_IS_SKELETON)
