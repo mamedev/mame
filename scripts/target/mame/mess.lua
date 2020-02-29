19316ea1fe346af81a5035fcdb20b95a1a441ec9
AJR <ajrhacker@users.noreply.github.com>
2020-02-28 23:03:23 -0500

    New machines marked as NOT_WORKING ---------------------------------- Elka Synthex [DBWBP]

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -4307,6 +4307,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/solbourne.cpp",
 	MAME_DIR .. "src/mame/drivers/swyft.cpp",
 	MAME_DIR .. "src/mame/drivers/symbolics.cpp",
+	MAME_DIR .. "src/mame/drivers/synthex.cpp",
 	MAME_DIR .. "src/mame/drivers/sys2900.cpp",
 	MAME_DIR .. "src/mame/drivers/sys9002.cpp",
 	MAME_DIR .. "src/mame/drivers/systec.cpp",
