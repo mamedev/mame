4080e50b9e51bddbeb234a70dca19f6b64cc4ec7
AJR <ajrhacker@users.noreply.github.com>
2020-03-03 23:41:20 -0500

    poly800mdk: Style (nw)

--- src/mame/drivers/poly800.cpp
+++ src/mame/drivers/poly800.cpp
@@ -144,5 +144,5 @@ ROM_START(poly800ii)
 ROM_END
 
 SYST(1984, poly800,    0, 0, poly800,    poly800, poly800_state,   empty_init, "Korg", "Poly-800 Programmable Polyphonic Synthesizer", MACHINE_IS_SKELETON)
-SYST(1984, poly800mdk, 0, 0, poly800mdk, poly800, poly800_state,   empty_init, "Korg", "Poly-800 Programmable Polyphonic Synthesizer with MIDI Dump Kit", MACHINE_IS_SKELETON)
+SYST(1984, poly800mdk, 0, 0, poly800mdk, poly800, poly800_state,   empty_init, "Korg", "Poly-800 Programmable Polyphonic Synthesizer (MIDI Dump Kit)", MACHINE_IS_SKELETON)
 SYST(1986, poly800ii,  0, 0, poly800ii,  poly800, poly800ii_state, empty_init, "Korg", "Poly-800II Programmable Polyphonic Synthesizer", MACHINE_IS_SKELETON)
