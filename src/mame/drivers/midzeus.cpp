20f65de06d28c0b2edcb8cc2aa8fb115ece3cb1b
hap <happppp@users.noreply.github.com>
2020-03-10 14:57:41 +0100

    ok, but the note in the above comment box (nw)

--- src/mame/drivers/midzeus.cpp
+++ src/mame/drivers/midzeus.cpp
@@ -453,12 +453,16 @@ WRITE32_MEMBER(midzeus2_state::crusnexo_leds_w)
 }
 
 
+
 /*************************************
  *
  *  Firewire/IEEE 1394 access (Zeus 2 only)
  *
+ *  Hardware: TSB12LV01A link layer controller and IBM IBM21S851 physical layer (PHY) transceiver.
+ *
  *************************************/
-READ32_MEMBER(midzeus_state::firewire_r) //Hardware: TSB12LV01A link layer controller and IBM IBM21S851 physical layer (PHY) transceiver.
+
+READ32_MEMBER(midzeus_state::firewire_r)
 {
 	uint32_t retVal = 0;
 	if (offset < 0x40)
