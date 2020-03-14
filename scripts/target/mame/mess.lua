2ca1e6fe12a3f94035ea24b3c27a4acf447f1830
AJR <ajrhacker@users.noreply.github.com>
2020-03-14 19:33:09 -0400

    New machines marked as NOT_WORKING ---------------------------------- Roland Juno-6 (JU-6) Polyphonic Synthesizer [DBWBP]

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -3238,6 +3238,7 @@ files {
 createMESSProjects(_target, _subtarget, "roland")
 files {
 	MAME_DIR .. "src/mame/drivers/alphajuno.cpp",
+	MAME_DIR .. "src/mame/drivers/juno6.cpp",
 	MAME_DIR .. "src/mame/drivers/roland_cm32p.cpp",
 	MAME_DIR .. "src/mame/drivers/roland_d10.cpp",
 	MAME_DIR .. "src/mame/drivers/roland_d50.cpp",
