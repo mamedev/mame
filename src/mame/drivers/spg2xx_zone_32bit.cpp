b06eaa28cab2d7f77b6d1ed870ad64c7b5ddb54a
Robbbert <Robbbert@users.noreply.github.com>
2020-03-18 14:47:47 +1100

    (nw) spg2xx_zone_32bit : added missing newline

--- src/mame/drivers/spg2xx_zone_32bit.cpp
+++ src/mame/drivers/spg2xx_zone_32bit.cpp
@@ -681,3 +681,4 @@ CONS( 200?, zon32bit,  0, 0, zon32bit, zon32bit, zon32bit_state,  empty_init,   
 CONS( 200?, mywicodx,  0, 0, zon32bit, zon32bit, mywicodx_state,  empty_init,      "<unknown>",                                   "My Wico Deluxe (Family Sport 85-in-1)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND | MACHINE_IMPERFECT_GRAPHICS )
 
 CONS( 200?, oplayer,   0, 0, zon32bit, oplayer, oplayer_100in1_state, init_oplayer, "OPlayer", "OPlayer Mobile Game Console (MGS03-white) (Family Sport 100-in-1)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND | MACHINE_IMPERFECT_GRAPHICS )
+
