a7dfc7d96d9c590cb28694b108c8b394237d47a1
AJR <ajrhacker@users.noreply.github.com>
2020-03-21 11:13:34 -0400

    New machines marked as NOT_WORKING ---------------------------------- Korg Z3 Guitar Synthesizer [DBWBP]

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -2648,6 +2648,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/korgdss1.cpp",
 	MAME_DIR .. "src/mame/drivers/korgdw8k.cpp",
 	MAME_DIR .. "src/mame/drivers/korgm1.cpp",
+	MAME_DIR .. "src/mame/drivers/korgz3.cpp",
 	MAME_DIR .. "src/mame/drivers/poly800.cpp",
 	MAME_DIR .. "src/mame/drivers/polysix.cpp",
 }
