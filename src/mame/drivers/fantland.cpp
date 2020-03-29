b195de87a18df706550ec3720e0f1e9a5c91fa16
f205v <f205v.f205v@gmail.com>
2020-03-29 13:54:37 +0200

    Better description (#6497)
    Better description of components on "whellrun", identified 2x sound chips SN76489AN and measured clocks for all chips.

--- src/mame/drivers/fantland.cpp
+++ src/mame/drivers/fantland.cpp
@@ -12,12 +12,12 @@ Sound   :   YM2151 [+ DAC] / 4 x MSM5205
 Video   :   2 x I.G.1BB 48844758V
 
 ------------------------------------------------------------------------------------------------------------------------
-Year + Game             Main CPU    Sound CPU    Sound            Video
+Year + Game             Main CPU  Sound CPU  Sound                         Video
 ------------------------------------------------------------------------------------------------------------------------
->=1987  Born To Fight   V20         8088         4 x MSM5205      2 x I.G.1BB 48844758V
->=1987  Fantasy Land    8086?       8086?        YM2151 + DAC     ?
-1988?   Wheels Runner   V20         Z80          YM3526 + ?       2 x PLCC84 FPGA
-1989    Galaxy Gunners  8088        V20          YM2151           2 x I.G.1BB 48844758V (labeled "INGA 1" and "INGA 2")
+>=1987  Born To Fight   V20       8088       4x MSM5205                    2x I.G.1BB 48844758V
+>=1987  Fantasy Land    8086?     8086?      YM2151 + DAC                  ?
+1988    Wheels Runner   V20       Z80        2x SN76489 + YM3526 + YM3014  2x PLCC84 FPGA (labeled "INGA 1" and "INGA 2")
+1989    Galaxy Gunners  8088      V20        YM2151                        2x I.G.1BB 48844758V (labeled "INGA 1" and "INGA 2")
 ------------------------------------------------------------------------------------------------------------------------
 
 [fantland, galaxygn]
@@ -1350,19 +1350,18 @@ Wheels Runner by International Games
 PCB:
 (revision 8801)
 
-1x NEC D70108C-8 (NEC V20)
-1x SGS Z8400BB1 (Z80B)
-1x YM3526 (sound)
-1x Y3014B (DAC)
+CPUs:
+1x NEC D70108C-8 (NEC V20) - 5,996MHz(@19)
+1x SGS Z8400BB1 (Z80B) - 3,497MHz(@6)
+2x SN76489AN (sound) - 3,497MHz(@14)
+1x YM3526 (sound) - 48,58kHz(@20) - 874,3kHz(@23) - 3,497MHz(@24)
+1x Y3014B (DAC) - 874,3kHz(@5)
 1x LM324A (sound)
 1x TDA2002 (sound)
 1x oscillator 18.000
 1x oscillator 14.000
-1x ALTERA EP1210PC
-2x INGA (black quad chips with 84 legs, maybe FPGA)
 
 ROMs:
-
 15x M27512
 3x PAL16R6CN (read protected)
 2x PAL20L8aCNS (read protected)
@@ -1371,11 +1370,22 @@ ROMs:
 1x TIBPAL16r8-25CN (read protected)
 eprom location 2,5,6 are empty
 
-Notes:
+RAMs:
+2x TMM2015BP-10 (SRAM 2k x 8)
+2x CY7C128-55PC (SRAM 2k x 8)
+1x TMM2063P-10 (SRAM 8k x 8)
+1x CXK58256PM-12 (SRAM 32k x 8)
+2x HM62256LP-12 (SRAM 32k x 8)
+7x TMS4464-12NL (DRAM 64k x 4)
 
+PLDs:
+2x most probably TPC1020AFN-084C with erased markings, labeled INGA1,INGA2
+1x ALTERA EP1210PC - 5,996MHz(@1)
+
+Notes:
 1x JAMMA edge connector
 1x trimmer (volume)
-2x 8 bit dip switch
+2x 8 DIP switches banks (SW1,SW2)
 
 Hardware info by f205v
 
