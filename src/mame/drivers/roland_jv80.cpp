f1462d0c223c3b3694eb56bc18daf05e9367e941
AJR <ajrhacker@users.noreply.github.com>
2020-04-04 23:53:24 -0400

    jv880: Minor corrections for undumped part numbers (nw)

--- src/mame/drivers/roland_jv80.cpp
+++ src/mame/drivers/roland_jv80.cpp
@@ -48,14 +48,14 @@ void roland_jv80_state::jv880(machine_config &config)
 
 ROM_START(jv880)
 	ROM_REGION(0x8000, "maincpu", ROMREGION_ERASE00)
-	ROM_LOAD("roland_r15199810_643532bf.ic16", 0x0000, 0x8000, NO_DUMP)
+	ROM_LOAD("roland_r15199810_6435328b86f.ic16", 0x0000, 0x8000, NO_DUMP)
 
 	ROM_REGION(0x40000, "progrom", 0)
 	ROM_LOAD("roland_jv-880_v1.01.ic17", 0x00000, 0x40000, CRC(5f19c95f) SHA1(38ec496f16dfa02d35f934cf32d8302aaf5f236e))
 
 	ROM_REGION(0x400000, "waverom", 0)
-	ROM_LOAD("roland_r15209312_lh5375n2.ic27", 0x000000, 0x200000, NO_DUMP)
-	ROM_LOAD("roland_r15209313_lh5375n3.ic25", 0x200000, 0x200000, NO_DUMP)
+	ROM_LOAD("roland-a_r15209312_lh5375n2.ic27", 0x000000, 0x200000, NO_DUMP)
+	ROM_LOAD("roland-b_r15209313_lh5375n3.ic25", 0x200000, 0x200000, NO_DUMP)
 ROM_END
 
 //SYST(1992, jv80, 0, 0, jv80, jv80, roland_jv80_state, empty_init, "Roland", "JV-80 Multi Timbral Synthesizer", MACHINE_IS_SKELETON)
