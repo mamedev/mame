25e32b0b18be0942fc1456bbcd7c08f62bbbad77
ClawGrip <clawgrip@hotmail.com>
2020-04-07 10:19:22 +0200

    meritsdx.cpp: Add notes (nw) (#6527)

--- src/mame/drivers/meritsdx.cpp
+++ src/mame/drivers/meritsdx.cpp
@@ -5,7 +5,7 @@
     Skeleton driver for Merit Scorpion DX darts machines
 
     Hardware overview:
-    Main CPU: AMD Elan SC300-33KC (Am386SXLV CPU core)
+    Main CPU: AMD Elan SC300-33KC (Am386SXLV CPU core + 6845 CGA)
     Others: Dallas 1232, M48T08-150PC1, F82C735A, PCMCIA slot (for a modem),
             LCD CGA screen and magnetic stripe card reader
     OSC: 24.000 MHz
@@ -13,7 +13,10 @@
     Notes:
     Uses a ROM-DOS as its operating system.
     The card reader emulates a PS2 keyboard. The keyboard BIOS is undumped
-    (it's a Phoenix BIOS on a protected Intel MCU)
+    (it's a Phoenix BIOS on a protected Intel 8242 MCU).
+    The Flash ROMs contain user data (from a championship on Spain). Once emulated,
+    they must be reflashed with a factory reset (can be done on the emulated
+    system) and updated on the driver.
 
 *******************************************************************************/
 
@@ -65,6 +68,7 @@ ROM_START(scrpndx)
 	ROM_REGION(0x80000, "romdos", 0)
 	ROM_LOAD( "4999-01-00_u20-r1_c1997_mii.u20", 0x00000, 0x80000, CRC(3c32355e) SHA1(56f846e655184b66db19cd01481e0016c463cf7d) )
 
+	// The Flash ROMs contain user data
 	ROM_REGION(0x400000, "flashroms", 0)
 	ROM_LOAD( "am29f800bb.u22", 0x000000, 0x100000, CRC(9b7510a5) SHA1(76ed78da976f8898a7a084fa5a37ea66aba886ff) )
 	ROM_LOAD( "am29f800bb.u23", 0x100000, 0x100000, CRC(8dc09838) SHA1(01e150daecac5f148a8cc6fb23715cf8b2cbd48a) )
@@ -77,4 +81,4 @@ ROM_START(scrpndx)
 ROM_END
 
 
-GAME(1996, scrpndx,         0,  scrpndx, scrpndx, meritsdx_state, empty_init, ROT0, "Merit", "Scorpion DX", MACHINE_IS_SKELETON_MECHANICAL) // OCT 15 1996 -- ASM 15:30
+GAME(1996, scrpndx, 0, scrpndx, scrpndx, meritsdx_state, empty_init, ROT0, "Merit", "Scorpion DX", MACHINE_IS_SKELETON_MECHANICAL) // OCT 15 1996 -- ASM 15:30
