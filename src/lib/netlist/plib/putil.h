eb7a0a545b4de776991d6a6647b1d271f8efddd8
couriersud <couriersud@gmx.org>
2020-04-18 15:44:42 +0200

    netlist: basename function now accepts a suffix to remove. (nw)

--- src/lib/netlist/plib/putil.h
+++ src/lib/netlist/plib/putil.h
@@ -230,7 +230,7 @@ namespace plib
 
 	namespace util
 	{
-		pstring basename(const pstring &filename);
+		pstring basename(const pstring &filename, const pstring &suffix = "");
 		pstring path(const pstring &filename);
 		pstring buildpath(std::initializer_list<pstring> list );
 		pstring environment(const pstring &var, const pstring &default_val);
