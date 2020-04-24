fb135fd894608e1d18f3324c0983007c4c016e8a
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2020-04-24 16:58:02 +0100

    some backgrounds (nw)

--- src/mame/drivers/goori.cpp
+++ src/mame/drivers/goori.cpp
@@ -9,6 +9,7 @@
 #include "sound/okim6295.h"
 #include "emupal.h"
 #include "screen.h"
+#include "tilemap.h"
 //#include "sound/3812intf.h"
 //#include "sound/okim6295.h"
 //#include "sound/ym2151.h"
@@ -17,39 +18,68 @@
 class goori_state : public driver_device
 {
 public:
-	goori_state(const machine_config &mconfig, device_type type, const char *tag) :
+	goori_state(const machine_config& mconfig, device_type type, const char* tag) :
 		driver_device(mconfig, type, tag),
 		m_maincpu(*this, "maincpu"),
 		m_palette(*this, "palette"),
 		m_gfxdecode(*this, "gfxdecode"),
 		m_oki(*this, "oki"),
 		m_spriteram(*this, "spriteram"),
+		m_bg_videoram(*this, "bg_videoram"),
 		m_screen(*this, "screen")
 	{ }
 
-	void goori(machine_config &config);
+	void goori(machine_config& config);
 
 protected:
 
 	virtual void machine_start() override;
-	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	virtual void video_start() override;
 
-	void goori_map(address_map &map);
+	uint32_t screen_update(screen_device& screen, bitmap_ind16& bitmap, const rectangle& cliprect);
+
+private:
+	void goori_map(address_map& map);
 
 	required_device<cpu_device> m_maincpu;
 	required_device<palette_device> m_palette;
 	required_device<gfxdecode_device> m_gfxdecode;
 	optional_device<okim6295_device> m_oki;
-
 	required_shared_ptr<uint16_t> m_spriteram;
+	required_shared_ptr<uint16_t> m_bg_videoram;
 	required_device<screen_device> m_screen;
-private:
 
+	tilemap_t* m_bg_tilemap;
+	DECLARE_WRITE16_MEMBER(bg_videoram_w);
+	TILE_GET_INFO_MEMBER(get_bg_tile_info);
 };
+
+
+WRITE16_MEMBER(goori_state::bg_videoram_w)
+{
+	COMBINE_DATA(&m_bg_videoram[offset]);
+	m_bg_tilemap->mark_tile_dirty(offset/2);
+}
+
+TILE_GET_INFO_MEMBER(goori_state::get_bg_tile_info)
+{
+	int tilenolow = (m_bg_videoram[(tile_index * 2) + 1] & 0xff00) >> 8;
+	int tilenohigh = (m_bg_videoram[(tile_index * 2) + 0] & 0xff00) >> 8;
+
+	int tile = tilenolow | (tilenohigh << 8);
+
+	tileinfo.set(1, tile, 0x1f, 0);
+}
+
+void goori_state::video_start()
+{
+	m_bg_tilemap = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(*this, FUNC(goori_state::get_bg_tile_info)), TILEMAP_SCAN_ROWS, 16, 16, 32, 32);
+}
 
 uint32_t goori_state::screen_update(screen_device& screen, bitmap_ind16& bitmap, const rectangle& cliprect)
 {
-	bitmap.fill(0, cliprect);
+	//bitmap.fill(0, cliprect);
+	m_bg_tilemap->draw(screen, bitmap, cliprect, 0, 0);
 
 	// is this sprite format a clone of anything? (looks VERY similar to snowbros but this hardware also has a higer resolution, a tile layer and 8bpp)
 	for (int i = 0; i < 0x2000 / 2; i += 8)
@@ -57,11 +87,11 @@ uint32_t goori_state::screen_update(screen_device& screen, bitmap_ind16& bitmap,
 		// 0 unused
 		// 1 unused
 		// 2 unused
-		// 3
-		// 4
-		// 5
-		// 6
-		// 7
+		// 3 colour and xpos msb
+		// 4 xpos
+		// 5 ypos
+		// 6 tile low
+		// 7 tile high (and flip)
 
 		uint16_t tile = m_spriteram[i + 6] | ((m_spriteram[i + 7] & 0x3f) << 8);
 		int x = m_spriteram[i + 4];
@@ -88,7 +118,7 @@ void goori_state::goori_map(address_map &map)
 {
 	map(0x000000, 0x0fffff).rom();
 	map(0x100000, 0x10ffff).ram(); // RAM main
-	map(0x400000, 0x400fff).ram(); // RAM tilemaps
+	map(0x400000, 0x400fff).ram().w(FUNC(goori_state::bg_videoram_w)).share("bg_videoram"); // 8-bit?
 
 	map(0x500000, 0x500001).portr("DSW1");
 	map(0x500002, 0x500003).portr("DSW2");
@@ -187,13 +217,25 @@ static const gfx_layout layout_16x16x8 =
 	8,
 	{ 8,9,10,11, 0,1,2,3},
 	{ 0,4,16,20,32,36,48,52, 512+0, 512+4, 512+16, 512+20, 512+32, 512+36, 512+48, 512+52 },
-	{   STEP8(0,8*8), STEP8(1024,8*8)},
+	{ STEP8(0,8*8), STEP8(1024,8*8)},
 	8*8*8*4,
+};
+
+
+static const gfx_layout layout_16x16x8_alt =
+{
+	16,16,
+	RGN_FRAC(1,1),
+	8,
+	{ 0,1,2,3,4,5,6,7 },
+	{ 0 * 8, 1 * 8, 2 * 8, 3 * 8, 4 * 8, 5 * 8, 6 * 8, 7 * 8, 8 * 8, 9 * 8, 10 * 8, 11 * 8, 12 * 8, 13 * 8, 14 * 8, 15 * 8 },
+	{ STEP16(0,16*8) },
+	16*16*8,
 };
 
 static GFXDECODE_START( gfx_unico )
 	GFXDECODE_ENTRY( "gfx1", 0, layout_16x16x8, 0x0, 0x20 ) // [0] Sprites
-	GFXDECODE_ENTRY( "gfx2", 0, layout_16x16x8, 0x0, 0x20 ) // [1] Layers
+	GFXDECODE_ENTRY( "gfx2", 0, layout_16x16x8_alt, 0x0, 0x20 ) // [1] Layers
 GFXDECODE_END
 
 void goori_state::machine_start()
