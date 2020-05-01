644e330b71acc10f46c632b30c81fc08ad602656
couriersud <couriersud@gmx.org>
2020-05-01 15:16:02 +0200

    netlist: Readding accidentially delete nld_ne555.h. (nw)

---
+++ src/lib/netlist/devices/nld_ne555.h
@@ -0,0 +1,21 @@
+// license:GPL-2.0+
+// copyright-holders:Couriersud
+
+#ifndef NLD_NE555_H_
+#define NLD_NE555_H_
+
+#include "netlist/nl_setup.h"
+
+#define NE555(name)                                                             \
+		NET_REGISTER_DEV(NE555, name)
+
+#define NE555_DIP(name)                                                         \
+		NET_REGISTER_DEV(NE555_DIP, name)
+
+#define MC1455P(name)                                                           \
+		NET_REGISTER_DEV(MC1455P, name)
+
+#define MC1455P_DIP(name)                                                       \
+		NET_REGISTER_DEV(MC1455P_DIP, name)
+
+#endif /* NLD_NE555_H_ */
