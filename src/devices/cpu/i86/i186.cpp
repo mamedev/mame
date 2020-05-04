16550d533fdcc4d563e5ba06cb146436a1af7b58
AJR <ajrhacker@users.noreply.github.com>
2020-05-03 22:24:56 -0400

    i86, i186, i286: Add byte registers to debugger state; improve tracking of previous PC for debugger
    i286: Don't clear INT state upon acknowledgment; add exception hook for debugger
    
    i86: Correct a fortuitously harmless typo in state registration (nw)

--- src/devices/cpu/i86/i186.cpp
+++ src/devices/cpu/i86/i186.cpp
@@ -586,7 +586,7 @@ void i80186_cpu_device::device_start()
 	state_add( I8086_VECTOR, "V", m_int_vector).formatstr("%02X");
 
 	state_add( I8086_PC, "PC", m_pc ).callimport().formatstr("%05X");
-	state_add( STATE_GENPCBASE, "CURPC", m_pc ).callimport().formatstr("%05X").noshow();
+	state_add<uint32_t>( STATE_GENPCBASE, "CURPC", [this] { return (m_sregs[CS] << 4) + m_prev_ip; }).mask(0xfffff).noshow();
 	state_add( I8086_HALT, "HALT", m_halt ).mask(1);
 
 	// Most of these mnemonics are borrowed from the Intel 80C186EA/80C188EA User's Manual.
