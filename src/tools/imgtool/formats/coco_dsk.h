470853e9ab27c9aa62140c4d9690f582f1bd4a84
tim lindner <tlindner@macmess.org>
2020-05-14 22:42:08 -0700

    removed dragon floppy options declaration

--- src/tools/imgtool/formats/coco_dsk.h
+++ src/tools/imgtool/formats/coco_dsk.h
@@ -17,7 +17,6 @@
 /**************************************************************************/
 
 LEGACY_FLOPPY_OPTIONS_EXTERN(coco);
-LEGACY_FLOPPY_OPTIONS_EXTERN(dragon);
 
 FLOPPY_IDENTIFY(coco_dmk_identify);
 FLOPPY_CONSTRUCT(coco_dmk_construct);
