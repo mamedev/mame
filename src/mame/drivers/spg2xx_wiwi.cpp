b2b97e8113d1ff054b0e4e32c276ba0799df06bb
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2020-06-29 17:56:10 +0100

    stability hack (nw)

--- src/mame/drivers/spg2xx_wiwi.cpp
+++ src/mame/drivers/spg2xx_wiwi.cpp
@@ -130,6 +130,9 @@ void spg2xx_game_marc250_state::machine_start()
 	m_pulse_timer2 = timer_alloc(1);
 	m_pulse_timer2->adjust(attotime::never);
 
+	// hack, makes x-racer3 and some others more stable, TODO: find out what is really wrong
+	m_maincpu->set_clock_scale(2.0000f); 
+
 }
 
 void spg2xx_game_marc250_state::machine_reset()
