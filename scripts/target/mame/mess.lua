4088eb80cffa1d54d9a25a521a5b7c0e2377febf
Robbbert <Robbbert@users.noreply.github.com>
2020-07-20 00:51:01 +1000

    ut88,unistar,tricep: cleanup

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -1995,7 +1995,6 @@ files {
 	MAME_DIR .. "src/mame/drivers/ut88.cpp",
 	MAME_DIR .. "src/mame/includes/ut88.h",
 	MAME_DIR .. "src/mame/machine/ut88.cpp",
-	MAME_DIR .. "src/mame/video/ut88.cpp",
 	MAME_DIR .. "src/mame/drivers/vector06.cpp",
 	MAME_DIR .. "src/mame/includes/vector06.h",
 	MAME_DIR .. "src/mame/machine/vector06.cpp",
--- src/mame/includes/ut88.h
+++ src/mame/includes/ut88.h
@@ -16,40 +16,39 @@
 #include "imagedev/cassette.h"
 #include "emupal.h"
 
-class ut88_base_state : public driver_device
+class ut88_common : public driver_device
 {
 public:
-	ut88_base_state(const machine_config &mconfig, device_type type, const char *tag)
+	ut88_common(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag)
+		, m_maincpu(*this, "maincpu")
 		, m_cassette(*this, "cassette")
-		, m_region_maincpu(*this, "maincpu")
 		, m_io_line0(*this, "LINE0")
 		, m_io_line1(*this, "LINE1")
 		, m_io_line2(*this, "LINE2")
-		, m_maincpu(*this, "maincpu")
 	{
 	}
 
 protected:
 	uint8_t tape_r();
 
+	required_device<cpu_device> m_maincpu;
 	required_device<cassette_image_device> m_cassette;
-	required_memory_region m_region_maincpu;
 	required_ioport m_io_line0;
 	required_ioport m_io_line1;
 	required_ioport m_io_line2;
-	required_device<cpu_device> m_maincpu;
 };
 
-class ut88_state : public ut88_base_state
+class ut88_state : public ut88_common
 {
 public:
 	ut88_state(const machine_config &mconfig, device_type type, const char *tag)
-		: ut88_base_state(mconfig, type, tag)
-		, m_ppi(*this, "ppi8255")
+		: ut88_common(mconfig, type, tag)
+		, m_ppi(*this, "ppi")
 		, m_dac(*this, "dac")
-		, m_p_videoram(*this, "videoram")
-		, m_bank1(*this, "bank1")
+		, m_vram(*this, "videoram")
+		, m_rom(*this, "maincpu")
+		, m_ram(*this, "mainram")
 		, m_io_line3(*this, "LINE3")
 		, m_io_line4(*this, "LINE4")
 		, m_io_line5(*this, "LINE5")
@@ -63,16 +62,8 @@ public:
 	void ut88(machine_config &config);
 
 private:
-	virtual void driver_init() override;
-	virtual void machine_reset() override;
-	virtual void device_timer(emu_timer &timer, device_timer_id id, int param, void *ptr) override;
-
-	enum
-	{
-		TIMER_RESET,
-		TIMER_UPDATE_DISPLAY
-	};
-
+	void machine_start() override;
+	void machine_reset() override;
 	uint8_t keyboard_r(offs_t offset);
 	void keyboard_w(offs_t offset, uint8_t data);
 	void sound_w(uint8_t data);
@@ -87,10 +78,12 @@ private:
 
 	int m_keyboard_mask;
 
+	memory_passthrough_handler *m_rom_shadow_tap;
 	required_device<i8255_device> m_ppi;
 	required_device<dac_bit_interface> m_dac;
-	required_shared_ptr<uint8_t> m_p_videoram;
-	required_memory_bank m_bank1;
+	required_shared_ptr<uint8_t> m_vram;
+	required_region_ptr<u8> m_rom;
+	required_shared_ptr<u8> m_ram;
 	required_ioport m_io_line3;
 	required_ioport m_io_line4;
 	required_ioport m_io_line5;
@@ -101,20 +94,20 @@ private:
 	required_device<palette_device> m_palette;
 };
 
-class ut88mini_state : public ut88_base_state
+class ut88mini_state : public ut88_common
 {
 public:
 	ut88mini_state(const machine_config &mconfig, device_type type, const char *tag)
-		: ut88_base_state(mconfig, type, tag)
-		, m_region_proms(*this, "proms")
+		: ut88_common(mconfig, type, tag)
+		, m_proms(*this, "proms")
 		, m_digits(*this, "digit%u", 0U)
 	{ }
 
 	void ut88mini(machine_config &config);
 
 private:
-	virtual void machine_start() override;
-	virtual void machine_reset() override;
+	void machine_start() override;
+	void machine_reset() override;
 	virtual void device_timer(emu_timer &timer, device_timer_id id, int param, void *ptr) override;
 
 	void io_map(address_map &map);
@@ -122,14 +115,13 @@ private:
 
 	enum
 	{
-		TIMER_RESET,
 		TIMER_UPDATE_DISPLAY
 	};
 
 	uint8_t keyboard_r();
 	void led_w(offs_t offset, uint8_t data);
 
-	required_memory_region m_region_proms;
+	required_memory_region m_proms;
 
 	int m_lcd_digit[6];
 	output_finder<6> m_digits;
