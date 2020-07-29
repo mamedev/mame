03c45ffbc779831926d3f3fe2c36e97e4060c2ec
Ryan Holtz <ryan.holtz@arrowheadgs.com>
2020-07-29 14:23:44 +0200

    -starcrus: Added netlist sound. [Ryan Holtz, Colin Howell, Aaron Giles]

---
+++ src/mame/audio/nl_starcrus.h
@@ -0,0 +1,10 @@
+// license:BSD-3-Clause
+// copyright-holders:Ryan Holtz
+#ifndef MAME_AUDIO_NL_STARCRUS_H
+#define MAME_AUDIO_NL_STARCRUS_H
+
+#pragma once
+
+NETLIST_EXTERNAL(starcrus)
+
+#endif // MAME_AUDIO_NL_STARCRUS_H
