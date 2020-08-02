4db4f2026b32b63019fca868bf8da800a2d43772
arbee <rb6502@users.noreply.github.com>
2020-08-01 21:45:38 -0400

    apple2: add DIP switch to "The Mill" 6809 card to select between original and OS-9 memory mappings. [R. Belmont]

--- src/devices/bus/a2bus/a2themill.h
+++ src/devices/bus/a2bus/a2themill.h
@@ -33,6 +33,7 @@ protected:
 	virtual void device_start() override;
 	virtual void device_reset() override;
 	virtual void device_add_mconfig(machine_config &config) override;
+	virtual ioport_constructor device_input_ports() const override;
 
 	// overrides of standard a2bus slot functions
 	virtual uint8_t read_c0nx(uint8_t offset) override;
@@ -41,6 +42,8 @@ protected:
 
 private:
 	required_device<cpu_device> m_6809;
+	required_ioport m_cfgsw;
+
 	bool m_bEnabled;
 	bool m_flipAddrSpace;
 	bool m_6809Mode;
