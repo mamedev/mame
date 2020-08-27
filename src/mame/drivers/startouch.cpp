7d1864d1a5f405dadb9e74d7c47ba71ffaaea32c
ClawGrip <clawgrip@hotmail.com>
2020-08-27 22:03:35 +0200

    startouch.cpp: Sorted out BIOS versions (#7141)

--- src/mame/drivers/startouch.cpp
+++ src/mame/drivers/startouch.cpp
@@ -7,7 +7,7 @@
     as Star Touch / EuroPlay 2001.
 
     Hardware overview:
-    MB Soyo M5EH V1.2 (1MB cache) or similar (e.g. Biostar M5ATD)
+    MB Soyo M5EH or similar (e.g. Biostar M5ATD)
     16384 KB RAM
     Intel Pentium MMX 233 MHz or compatible (e.g. Cyrix M II-300GP 66MHz Bus 3.5x 2.9V)
 
@@ -65,27 +65,27 @@ ROM_START(europl01)
 
 	// Sleic used different motherboards for this machine. By now, we're adding the all the known BIOSes here
 	ROM_REGION(0x20000, "mb_bios", 0)
-	ROM_SYSTEM_BIOS(0, "soyo_m5eh_aw4_51", "Soyo M5EH with Award 4.51pg BIOS")     // Soyo M5EH V1.2 (1MB cache)
-	ROMX_LOAD("award_pci-pnp_586_222951562_v4.51pg.u2", 0x00000, 0x20000, CRC(5bb1bcbc) SHA1(6e2a7b5b3fc892ed20d0b12a1a533231c8953177), ROM_BIOS(0))
-	ROM_SYSTEM_BIOS(1, "bst_m5atd_aw4_51", "Biostar M5ATD with Award 4.51pg BIOS") // Biostar M5ATD V1.2 (ALi M5819P + ALi M1543 B1 + ALi M1531 B1 + UMC UM61L6464F-6)
-	ROMX_LOAD("award_pci_pnp_586_w29c011a.u11",         0x00000, 0x20000, CRC(1ec5749b) SHA1(3dd1dac852b00c8108aaf9c89f47ae1922d645f0), ROM_BIOS(1))
-	ROM_SYSTEM_BIOS(2, "unknown1_aw4_51",  "Unknown PCB 1 with Award 4.51pg BIOS") // Unknown PCB/CPU/Chipset
-	ROMX_LOAD("award_1998_pci-pnp_586_149278871_w29c011.bin", 0x00000, 0x20000, CRC(3c6aea4d) SHA1(9e56b0f27c204a0eaaf1174070fc95faacc84b0b), ROM_BIOS(2))
-	ROM_SYSTEM_BIOS(3, "unknown2_aw4_51",  "Unknown PCB 2 with Award 4.51pg BIOS") // Unknown PCB/CPU/Chipset
-	ROMX_LOAD("award_1998_pci-pnp_586_223123413_39sf010.bin", 0x00000, 0x20000, CRC(d30fe6c2) SHA1(022cf24d982b82e4c13ebbe974adae3a1638d1cd), ROM_BIOS(3))
+	ROM_SYSTEM_BIOS(0, "soyo_5ehm13_1aa1", "Soyo 5EHM (Award BIOS 5EH V1.3-1AA1)")                                                                 // Soyo 5EHM V1.3
+	ROMX_LOAD("award_1998_pci-pnp_586_223123413.bin", 0x00000, 0x20000, CRC(d30fe6c2) SHA1(022cf24d982b82e4c13ebbe974adae3a1638d1cd), ROM_BIOS(0)) //   39SF010
+	ROM_SYSTEM_BIOS(1, "soyo_5ehm12_1ca2", "Soyo 5EHM (Award BIOS 5EH V1.2-1CA2)")                                                                 // Soyo 5EHM V1.2 (1MB cache)
+	ROMX_LOAD("award_1998_pci-pnp_586_222951562.u2",  0x00000, 0x20000, CRC(5bb1bcbc) SHA1(6e2a7b5b3fc892ed20d0b12a1a533231c8953177), ROM_BIOS(1)) //   39SF010
+	ROM_SYSTEM_BIOS(2, "bst_m5atd1228b_1", "Biostar M5ATD (Award BIOS ATD1228B, set 1)")                                                           // Biostar M5ATD V1.2 (ALi M5819P + ALi M1543 B1 + ALi M1531 B1 + UMC UM61L6464F-6)
+	ROMX_LOAD("award_1998_pci_pnp_586_149197780.u11", 0x00000, 0x20000, CRC(1ec5749b) SHA1(3dd1dac852b00c8108aaf9c89f47ae1922d645f0), ROM_BIOS(2)) //   W29C011
+	ROM_SYSTEM_BIOS(3, "bst_m5atd1228b_2", "Biostar M5ATD (Award BIOS ATD1228B, set 2)")                                                           // Biostar M5ATD
+	ROMX_LOAD("award_1998_pci-pnp_586_149278871.bin", 0x00000, 0x20000, CRC(3c6aea4d) SHA1(9e56b0f27c204a0eaaf1174070fc95faacc84b0b), ROM_BIOS(3)) //   W29C011
 
 	ROM_REGION(0x8000, "vga_bios", 0)
 	ROM_LOAD("s3_86c775-86c785_video_bios_v1.01.04.u5", 0x0000, 0x8000, CRC(e718418f) SHA1(1288ce51bb732a346eb7c61d5bdf80ea22454d45))
 
-	ROM_REGION(0x20000, "hd_firmware", 0)
+	ROM_REGION(0x20000, "hd_firmware", 0) // Samsung SV0322A
 	ROM_LOAD("jk200-35.bin", 0x00000, 0x20000, CRC(601fa709) SHA1(13ded4826a64209faac8bc81708172b81195ab96))
 
 	ROM_REGION(0x66da4, "dongle", 0)
 	ROM_LOAD("9b91f19d.hsp", 0x00000, 0x66da4, CRC(0cf78908) SHA1(c596f415accd6b91be85ea8c1b89ea380d0dc6c8))
 
-	DISK_REGION( "ide:0:hdd:image" )
-	DISK_IMAGE("sleic-petaco_startouch_2001_v2.0", 0, SHA1(3164a5786d6b9bb0dd9910b4d27a77a6b746dedf)) // labeled startouch_2001 but when run game title is EuroPlay 2001
+	DISK_REGION( "ide:0:hdd:image" ) // Sleic-Petaco Star Touch 2001. Version 2.0. Date 06/April/2001
+	DISK_IMAGE("sleic-petaco_startouch_2001_v2.0", 0, SHA1(3164a5786d6b9bb0dd9910b4d27a77a6b746dedf)) // Labeled "Star Touch 2001" but when run game title is EuroPlay 2001
 ROM_END
 
 
-GAME(2001?, europl01, 0, europl01, europl01, startouch_state, empty_init, ROT0, "Sleic / Petaco", "EuroPlay 2001", MACHINE_IS_SKELETON)
+GAME(2001, europl01, 0, europl01, europl01, startouch_state, empty_init, ROT0, "Sleic / Petaco", "EuroPlay 2001", MACHINE_IS_SKELETON)
