a541a6808b694e0d70a8982280fea0aac54ade83
Rhett Aultman <roadriverrail@gmail.com>
2020-08-29 14:06:44 -0400

    Drop unused "uthernet_intf" option
    The "uthernet_intef" option was a remnant from the original hack of the cs8900a
    support; TFE has been stripped from its cs8900a emulation and no longer needs
    the interface name to run libpcap calls, so the option has been removed.

--- src/emu/emuopts.h
+++ src/emu/emuopts.h
@@ -170,7 +170,6 @@
 #define OPTION_UI                   "ui"
 #define OPTION_RAMSIZE              "ramsize"
 #define OPTION_NVRAM_SAVE           "nvram_save"
-#define OPTION_UTHERNET_INTF        "uthernet_intf"
 
 // core comm options
 #define OPTION_COMM_LOCAL_HOST      "comm_localhost"
