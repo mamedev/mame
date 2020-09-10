c4373361d4d73c933f5cdb6ef0c22d0d0847d6aa
hap <happppp@users.noreply.github.com>
2020-09-10 22:29:29 +0200

    New machines marked as NOT_WORKING ---------------------------------- Constellation Expert [hap, Berger]

--- src/mame/drivers/novag_cexpert.cpp
+++ src/mame/drivers/novag_cexpert.cpp
@@ -3,19 +3,19 @@
 // thanks-to:Berger
 /******************************************************************************
 
-Novag Constellation Forte
+Novag Constellation Expert (model 853)
+
+The u3 ROM contains this message: (it's David Kittinger's company)
+Copyright (c) 1985, Intelligent Heuristic Programming, Inc
 
 Hardware notes:
 - R65C02P4 @ 5MHz (10MHz XTAL)
-- 2*2KB RAM(NEC D449C-3), 2*32KB ROM(27C256)
-- HLCD0538P, 10-digit 7seg LCD display
-- TTL, 18 LEDs, 8*8 chessboard buttons
+- 2*2KB RAM(NEC D449C-3), 2*32KB ROM
+- 64+8 leds, magnet sensors chessboard
 - ports for optional printer and chess clock
 
-I/O is similar to supercon
-
 TODO:
-- add power-off NMI? does nothing, it will just go into an infinite loop
+- based on a copy-paste of cforte driver, need to finish it and make internal artwork
 
 ******************************************************************************/
 
@@ -26,32 +26,30 @@ TODO:
 #include "machine/nvram.h"
 #include "machine/timer.h"
 #include "sound/beep.h"
-#include "video/hlcd0538.h"
 #include "video/pwm.h"
 #include "speaker.h"
 
 // internal artwork
-#include "novag_cforte.lh" // clickable
+//#include "novag_cexpert.lh" // clickable
 
 
 namespace {
 
-class cforte_state : public driver_device
+class cexpert_state : public driver_device
 {
 public:
-	cforte_state(const machine_config &mconfig, device_type type, const char *tag) :
+	cexpert_state(const machine_config &mconfig, device_type type, const char *tag) :
 		driver_device(mconfig, type, tag),
 		m_maincpu(*this, "maincpu"),
 		m_irq_on(*this, "irq_on"),
 		m_display(*this, "display"),
 		m_board(*this, "board"),
-		m_lcd(*this, "hlcd0538"),
 		m_beeper(*this, "beeper"),
 		m_inputs(*this, "IN.%u", 0)
 	{ }
 
 	// machine configs
-	void cforte(machine_config &config);
+	void cexpert(machine_config &config);
 
 protected:
 	virtual void machine_start() override;
@@ -62,7 +60,6 @@ private:
 	required_device<timer_device> m_irq_on;
 	required_device<pwm_display_device> m_display;
 	required_device<sensorboard_device> m_board;
-	required_device<hlcd0538_device> m_lcd;
 	required_device<beep_device> m_beeper;
 	required_ioport_array<8> m_inputs;
 
@@ -75,7 +72,6 @@ private:
 
 	// I/O handlers
 	void update_display();
-	void lcd_output_w(u64 data);
 	void mux_w(u8 data);
 	void control_w(u8 data);
 	u8 input1_r();
@@ -85,7 +81,7 @@ private:
 	u8 m_led_select = 0;
 };
 
-void cforte_state::machine_start()
+void cexpert_state::machine_start()
 {
 	// register for savestates
 	save_item(NAME(m_inp_mux));
@@ -98,65 +94,31 @@ void cforte_state::machine_start()
     I/O
 ******************************************************************************/
 
-// HLCD0538
-
-void cforte_state::lcd_output_w(u64 data)
+void cexpert_state::update_display()
 {
-	// 4 rows used
-	u32 rowdata[4];
-	for (int i = 0; i < 4; i++)
-		rowdata[i] = (data >> i & 1) ? u32(data >> 8) : 0;
-
-	// 2 segments per row
-	for (int dig = 0; dig < 13; dig++)
-	{
-		data = 0;
-		for (int i = 0; i < 4; i++)
-			data |= ((rowdata[i] >> (2*dig) & 3) << (2*i));
-
-		data = bitswap<8>(data,7,2,0,4,6,5,3,1);
-		m_display->write_row(dig+3, data);
-	}
-
-	m_display->update();
+	m_display->matrix(1 << m_led_select, m_inp_mux);
 }
 
-
-// TTL/generic
-
-void cforte_state::update_display()
-{
-	// 3 led rows
-	m_display->matrix_partial(0, 3, m_led_select, m_inp_mux);
-}
-
-void cforte_state::mux_w(u8 data)
+void cexpert_state::mux_w(u8 data)
 {
 	// d0-d7: input mux, led data
 	m_inp_mux = data;
 	update_display();
 }
 
-void cforte_state::control_w(u8 data)
+void cexpert_state::control_w(u8 data)
 {
-	// d0: HLCD0538 data in
-	// d1: HLCD0538 clk
-	// d2: HLCD0538 lcd
-	m_lcd->data_w(data & 1);
-	m_lcd->clk_w(data >> 1 & 1);
-	m_lcd->lcd_w(data >> 2 & 1);
+	// d0-d2: clock/printer?
 
-	// d3: ? (goes high at power-off NMI)
+	// d3: enable beeper
+	m_beeper->set_state(data >> 3 & 1);
 
-	// d4-d6: select led row
-	m_led_select = data >> 4 & 7;
+	// d4-d7: 74145 to led select
+	m_led_select = data >> 4 & 0xf;
 	update_display();
-
-	// d7: enable beeper
-	m_beeper->set_state(data >> 7 & 1);
 }
 
-u8 cforte_state::input1_r()
+u8 cexpert_state::input1_r()
 {
 	u8 data = 0;
 
@@ -168,7 +130,7 @@ u8 cforte_state::input1_r()
 	return ~data;
 }
 
-u8 cforte_state::input2_r()
+u8 cexpert_state::input2_r()
 {
 	u8 data = 0;
 
@@ -188,14 +150,14 @@ u8 cforte_state::input2_r()
     Address Maps
 ******************************************************************************/
 
-void cforte_state::main_map(address_map &map)
+void cexpert_state::main_map(address_map &map)
 {
-	map(0x0000, 0x0fff).ram().share("nvram");
-	map(0x1c00, 0x1c00).nopw(); // accessory?
-	map(0x1d00, 0x1d00).nopw(); // "
-	map(0x1e00, 0x1e00).rw(FUNC(cforte_state::input2_r), FUNC(cforte_state::mux_w));
-	map(0x1f00, 0x1f00).rw(FUNC(cforte_state::input1_r), FUNC(cforte_state::control_w));
-	map(0x2000, 0xffff).rom();
+	map(0x0000, 0x0fff).ram(); //.share("nvram");
+	map(0x1000, 0x1000).nopw(); // accessory?
+	map(0x1100, 0x1100).nopw(); // "
+	map(0x1200, 0x1200).rw(FUNC(cexpert_state::input2_r), FUNC(cexpert_state::mux_w));
+	map(0x1300, 0x1300).rw(FUNC(cexpert_state::input1_r), FUNC(cexpert_state::control_w));
+	map(0x1800, 0xffff).rom();
 }
 
 
@@ -204,7 +166,7 @@ void cforte_state::main_map(address_map &map)
     Input Ports
 ******************************************************************************/
 
-static INPUT_PORTS_START( cforte )
+static INPUT_PORTS_START( cexpert )
 	PORT_START("IN.0")
 	PORT_BIT(0x01, IP_ACTIVE_HIGH, IPT_KEYPAD) PORT_CODE(KEYCODE_I) PORT_NAME("New Game")
 	PORT_BIT(0x02, IP_ACTIVE_HIGH, IPT_KEYPAD) PORT_CODE(KEYCODE_8) PORT_NAME("Player/Player / Gambit/Large / King")
@@ -244,33 +206,31 @@ INPUT_PORTS_END
     Machine Configs
 ******************************************************************************/
 
-void cforte_state::cforte(machine_config &config)
+void cexpert_state::cexpert(machine_config &config)
 {
 	/* basic machine hardware */
 	R65C02(config, m_maincpu, 10_MHz_XTAL/2);
-	m_maincpu->set_addrmap(AS_PROGRAM, &cforte_state::main_map);
+	m_maincpu->set_addrmap(AS_PROGRAM, &cexpert_state::main_map);
 
-	const attotime irq_period = attotime::from_hz(32.768_kHz_XTAL/128); // 256Hz
-	TIMER(config, m_irq_on).configure_periodic(FUNC(cforte_state::irq_on<M6502_IRQ_LINE>), irq_period);
-	m_irq_on->set_start_delay(irq_period - attotime::from_usec(11)); // active for 11us
-	TIMER(config, "irq_off").configure_periodic(FUNC(cforte_state::irq_off<M6502_IRQ_LINE>), irq_period);
+	const attotime irq_period = attotime::from_hz(15440 / 32.0); // 555 timer (measured), to 4020
+	TIMER(config, m_irq_on).configure_periodic(FUNC(cexpert_state::irq_on<M6502_IRQ_LINE>), irq_period);
+	m_irq_on->set_start_delay(irq_period - attotime::from_nsec(15200)); // active for 15.2us
+	TIMER(config, "irq_off").configure_periodic(FUNC(cexpert_state::irq_off<M6502_IRQ_LINE>), irq_period);
 
-	NVRAM(config, "nvram", nvram_device::DEFAULT_ALL_1);
+	//NVRAM(config, "nvram", nvram_device::DEFAULT_ALL_1);
 
-	SENSORBOARD(config, m_board).set_type(sensorboard_device::BUTTONS);
+	SENSORBOARD(config, m_board).set_type(sensorboard_device::MAGNETS);
 	m_board->init_cb().set(m_board, FUNC(sensorboard_device::preset_chess));
 	m_board->set_delay(attotime::from_msec(200));
-	m_board->set_nvram_enable(true);
+	//m_board->set_nvram_enable(true);
 
 	/* video hardware */
-	HLCD0538(config, m_lcd).write_cols().set(FUNC(cforte_state::lcd_output_w));
-	PWM_DISPLAY(config, m_display).set_size(3+13, 8);
-	m_display->set_segmask(0x3ff0, 0xff);
-	config.set_default_layout(layout_novag_cforte);
+	PWM_DISPLAY(config, m_display).set_size(9, 8);
+	//config.set_default_layout(layout_novag_cexpert);
 
 	/* sound hardware */
 	SPEAKER(config, "mono").front_center();
-	BEEP(config, m_beeper, 32.768_kHz_XTAL/32); // 1024Hz
+	BEEP(config, m_beeper, 15440 / 16); // 965Hz
 	m_beeper->add_route(ALL_OUTPUTS, "mono", 0.25);
 }
 
@@ -280,16 +240,10 @@ void cforte_state::cforte(machine_config &config)
     ROM Definitions
 ******************************************************************************/
 
-ROM_START( cfortea )
+ROM_START( cexpert )
 	ROM_REGION( 0x10000, "maincpu", 0 )
-	ROM_LOAD("a_903_l", 0x0000, 0x8000, CRC(01e7e306) SHA1(6a2b982bb0f412a63f5d3603958dd863e38669d9) ) // NEC D27C256AD-12
-	ROM_LOAD("a_903_h", 0x8000, 0x8000, CRC(c5a5573f) SHA1(7e11eb2f3d96bc41386a14a19635427a386ec1ec) ) // "
-ROM_END
-
-ROM_START( cforteb )
-	ROM_REGION( 0x10000, "maincpu", 0 )
-	ROM_LOAD("forte_b_l.bin", 0x0000, 0x8000, CRC(e3d194a1) SHA1(80457580d7c57e07895fd14bfdaf14b30952afca) )
-	ROM_LOAD("forte_b_h.bin", 0x8000, 0x8000, CRC(dd824be8) SHA1(cd8666b6b525887f9fc48a730b71ceabcf07f3b9) )
+	ROM_LOAD("novag_8533-503.u3", 0x0000, 0x8000, CRC(e384de2f) SHA1(13b56e2870e3755073e7a7cc63bae995cd468562) )
+	ROM_LOAD("novag_8532-502.u2", 0x8000, 0x8000, CRC(c2c367b5) SHA1(e000871c93a5e0fc2f8b29f3d2fec0607a91559b) )
 ROM_END
 
 } // anonymous namespace
@@ -300,6 +254,5 @@ ROM_END
     Drivers
 ******************************************************************************/
 
-//    YEAR  NAME     PARENT  CMP MACHINE  INPUT   STATE         INIT        COMPANY, FULLNAME, FLAGS
-CONS( 1986, cfortea, 0,       0, cforte,  cforte, cforte_state, empty_init, "Novag", "Constellation Forte (version A)", MACHINE_SUPPORTS_SAVE | MACHINE_CLICKABLE_ARTWORK )
-CONS( 1986, cforteb, cfortea, 0, cforte,  cforte, cforte_state, empty_init, "Novag", "Constellation Forte (version B)", MACHINE_SUPPORTS_SAVE | MACHINE_CLICKABLE_ARTWORK )
+//    YEAR  NAME     PARENT  CMP MACHINE  INPUT    STATE          INIT        COMPANY, FULLNAME, FLAGS
+CONS( 1985, cexpert, 0,       0, cexpert, cexpert, cexpert_state, empty_init, "Novag", "Constellation Expert", MACHINE_NOT_WORKING | MACHINE_SUPPORTS_SAVE | MACHINE_CLICKABLE_ARTWORK )
