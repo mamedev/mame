d7aa2158ab788a301eae358f52c45bcbda56963a
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2020-10-13 02:48:26 -0300

    New non-working driver: SONY DFS-500 DME Video Mixer (1994)
    And a few new skeleton drivers for some other SONY video equipment:
    * U-Matic vo5850PM
    * BETACAM-SP UVW-1200
    * BETACAM-SP UVW-1600
    * BETACAM-SP UVW-1800

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -3648,7 +3648,9 @@ files {
 
 createMESSProjects(_target, _subtarget, "sony")
 files {
+	MAME_DIR .. "src/mame/drivers/betacam.cpp",
 	MAME_DIR .. "src/mame/drivers/bvm.cpp",
+	MAME_DIR .. "src/mame/drivers/dfs500.cpp",
 	MAME_DIR .. "src/mame/drivers/pockstat.cpp",
 	MAME_DIR .. "src/mame/drivers/psx.cpp",
 	MAME_DIR .. "src/mame/machine/psxcd.cpp",
@@ -3656,6 +3658,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/pve500.cpp",
 	MAME_DIR .. "src/mame/drivers/smc777.cpp",
 	MAME_DIR .. "src/mame/drivers/ps2sony.cpp",
+	MAME_DIR .. "src/mame/drivers/umatic.cpp",
 }
 
 createMESSProjects(_target, _subtarget, "sony_news")
