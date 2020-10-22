aa9fb25346404ba179920f36994d0d22384cd0e4
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2020-10-22 14:44:45 +0100

    continued refactor

--- src/mame/drivers/nes_vt32_vt369.cpp
+++ src/mame/drivers/nes_vt32_vt369.cpp
@@ -22,7 +22,7 @@
   ***************************************************************************/
 
 #include "emu.h"
-#include "machine/nes_vt09_soc.h"
+#include "machine/nes_vt32_vt369_soc.h"
 
 class nes_vt3x_base_state : public driver_device
 {
