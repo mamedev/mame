52907efc188432e01ce1c3be66e149e7e7224841
ClawGrip <clawgrip@hotmail.com>
2020-10-28 00:28:16 +0100

    microdar.cpp: Fix parent-clone relationship for Olakoa sets

--- src/mame/drivers/microdar.cpp
+++ src/mame/drivers/microdar.cpp
@@ -384,4 +384,4 @@ GAME(199?, dibif727, dibifuca, microdar, microdar, microdar_state, empty_init, R
 GAME(1997, cfarwest, 0,        microdar, microdar, microdar_state, empty_init, ROT0, "Compumatic",          "Far West (Compumatic)",                          MACHINE_IS_SKELETON_MECHANICAL)
 GAME(1997, prospdp,  0,        prospdp,  microdar, microdar_state, empty_init, ROT0, "Compumatic",          "Unknown Compumatic ProSPDP based darts machine", MACHINE_IS_SKELETON_MECHANICAL)
 GAME(1997, diolakoa, 0,        microdv5, microdar, microdar_state, empty_init, ROT0, "Compumatic / Olakoa", "Diana Olakoa (v8.38)",                           MACHINE_IS_SKELETON_MECHANICAL)
-GAME(1997, diola827, 0,        microdv5, microdar, microdar_state, empty_init, ROT0, "Compumatic / Olakoa", "Diana Olakoa (v8.27)",                           MACHINE_IS_SKELETON_MECHANICAL)
+GAME(1997, diola827, diolakoa, microdv5, microdar, microdar_state, empty_init, ROT0, "Compumatic / Olakoa", "Diana Olakoa (v8.27)",                           MACHINE_IS_SKELETON_MECHANICAL)
