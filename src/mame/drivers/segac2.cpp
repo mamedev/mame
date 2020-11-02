06832489551c6cb6bb8defe83b4f8cf85d2936b8
David Haywood <28625134+DavidHaywood@users.noreply.github.com>
2020-11-02 15:51:04 +0000

    - weclemans.cpp cleaned up MACHINE_START_OVERRIDE / MACHINE_RESET_OVERRIDE / VIDEO_START [David Haywood] (#7423)
    -  tsamurai.cpp cleaned up MACHINE_START_OVERRIDE / MACHINE_RESET_OVERRIDE / VIDEO_START  [David Haywood]
      - tubep.cpp cleaned up MACHINE_START_OVERRIDE / MACHINE_RESET_OVERRIDE / VIDEO_START  [David Haywood]
      - segac2.cpp cleaned up MACHINE_START_OVERRIDE / MACHINE_RESET_OVERRIDE / VIDEO_START  [David Haywood]
    
    new WORKING machines
    ------
    Street Fighter II: Special Champion Edition (MSI Plug & Play) (Europe) [Team Europe]
    
    new WORKING software list entries
    -----
    ekara_pal.xml:
    uk_vol4 e-kara UK / Europe Volume 4 (UK-E004) [David Haywood. Team Europe]
    uk_vol5 e-kara UK / Europe Volume 5 (UK-E005) [David Haywood. Team Europe]

--- src/mame/drivers/segac2.cpp
+++ src/mame/drivers/segac2.cpp
@@ -101,6 +101,7 @@ namespace {
 
 typedef device_delegate<int (int in)> segac2_prot_delegate;
 
+// does this need to inherit from md_base? really we only need the VDP and some basics like the maincpu
 class segac2_state : public md_base_state
 {
 public:
@@ -150,6 +151,10 @@ public:
 	void init_pclubjv4();
 	void init_pclubjv5();
 
+protected:
+	virtual void machine_start() override;
+	virtual void machine_reset() override;
+
 private:
 	// for Print Club only
 	int m_cam_data;
@@ -174,9 +179,7 @@ private:
 	uint8_t       m_sound_banks;      /* number of sound banks */
 
 	void segac2_common_init(segac2_prot_delegate prot_func);
-	DECLARE_VIDEO_START(segac2_new);
-	DECLARE_MACHINE_START(segac2);
-	DECLARE_MACHINE_RESET(segac2);
+
 
 	uint32_t screen_update_segac2_new(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
 	int m_segac2_bg_pal_lookup[4];
@@ -251,7 +254,7 @@ public:
 
 ******************************************************************************/
 
-MACHINE_START_MEMBER(segac2_state,segac2)
+void segac2_state::machine_start()
 {
 	save_item(NAME(m_prot_write_buf));
 	save_item(NAME(m_prot_read_buf));
@@ -260,7 +263,7 @@ MACHINE_START_MEMBER(segac2_state,segac2)
 }
 
 
-MACHINE_RESET_MEMBER(segac2_state,segac2)
+void segac2_state::machine_reset()
 {
 //  megadriv_scanline_timer = machine().device<timer_device>("md_scan_timer");
 //  megadriv_scanline_timer->adjust(attotime::zero);
@@ -1497,11 +1500,6 @@ WRITE_LINE_MEMBER(segac2_state::segac2_irq2_interrupt)
 
 ******************************************************************************/
 
-VIDEO_START_MEMBER(segac2_state,segac2_new)
-{
-	VIDEO_START_CALL_MEMBER(megadriv);
-}
-
 // C2 doesn't use the internal VDP CRAM, instead it uses the digital output of the chip
 //  and applies it's own external colour circuity
 uint32_t segac2_state::screen_update_segac2_new(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect)
@@ -1598,9 +1596,6 @@ void segac2_state::segac(machine_config &config)
 	m_maincpu->set_addrmap(AS_PROGRAM, &segac2_state::main_map);
 	m_maincpu->set_irq_acknowledge_callback(FUNC(md_base_state::genesis_int_callback));
 
-	MCFG_MACHINE_START_OVERRIDE(segac2_state,segac2)
-	MCFG_MACHINE_RESET_OVERRIDE(segac2_state,segac2)
-
 	NVRAM(config, "nvram", nvram_device::DEFAULT_ALL_1); // borencha requires 0xff fill or there is no sound (it lacks some of the init code of the borench set)
 
 	SEGA_315_5296(config, m_io, XL2_CLOCK/6); // clock divider guessed
@@ -1635,8 +1630,6 @@ void segac2_state::segac(machine_config &config)
 	screen.screen_vblank().set(FUNC(segac2_state::screen_vblank_megadriv));
 
 	PALETTE(config, m_palette).set_entries(2048*3);
-
-	MCFG_VIDEO_START_OVERRIDE(segac2_state,segac2_new)
 
 	/* sound hardware */
 	SPEAKER(config, "mono").front_center();
