4a0b4ea52d72c05f853e03b5af44c64a2431872f
cam900 <dbtlrchl@naver.com>
2020-11-10 20:58:45 +0900

    undrfire.cpp: Fix spacing

--- src/mame/drivers/undrfire.cpp
+++ src/mame/drivers/undrfire.cpp
@@ -76,7 +76,7 @@
 
     Chase Bombers proto sports lots of gfx bugs;
 
-	Sprites are delayed, but not yet implemented
+    Sprites are delayed, but not yet implemented
 
     Gun calibration
     ---------------
