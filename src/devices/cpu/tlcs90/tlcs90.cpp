ce99e12e4afafe74929bef2a0f37e5912c5df523
hap <happppp@users.noreply.github.com>
2020-11-14 18:42:37 +0100

    tlcs90: add missing break;

--- src/devices/cpu/tlcs90/tlcs90.cpp
+++ src/devices/cpu/tlcs90/tlcs90.cpp
@@ -1022,7 +1022,7 @@ void tlcs90_device::decode()
 				if (b0 == 0xfe) {
 					OPCC( LDI+b1-0x58,14,18 )   NONE( 1 )       NONE( 2 )                   return;
 				}
-				return;
+				break;
 
 				case 0x60:                                                                              // ADD A,g
 				case 0x61:                                                                              // ADC A,g
@@ -1078,6 +1078,7 @@ void tlcs90_device::decode()
 				if (b0 == 0xfe) {
 					OPCC( RET,6,14 )    CC( 1, b1 - 0xd0 )      NONE( 2 )                   return;     // RET cc
 				}
+				break;
 			}   break;
 
 		case 0xff:
