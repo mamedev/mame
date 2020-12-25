f26a25384a3412955f81811f8e2fd6ac5f230f6f
Nigel Barnes <Pernod70@users.noreply.github.com>
2020-12-25 00:05:59 +0000

    New machines marked as NOT_WORKING ---------------------------------- Datacast Controller (The Stock Exchange) [Tony Jewell, Nigel Barnes]

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -4404,6 +4404,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/d6800.cpp",
 	MAME_DIR .. "src/mame/drivers/d6809.cpp",
 	MAME_DIR .. "src/mame/drivers/daruma.cpp",
+	MAME_DIR .. "src/mame/drivers/datacast.cpp",
 	MAME_DIR .. "src/mame/drivers/design.cpp",
 	MAME_DIR .. "src/mame/drivers/dg680.cpp",
 	MAME_DIR .. "src/mame/drivers/diablo1300.cpp",
