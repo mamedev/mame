c69946cedfac413c1932445866f722a24bde47ba
ClawGrip <clawgrip@hotmail.com>
2021-01-08 18:52:12 +0100

    Separate the AlphaSmart 3000 on a different driver

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -4354,6 +4354,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/alesis_qs.cpp",
 	MAME_DIR .. "src/mame/drivers/alfaskop41xx.cpp",
 	MAME_DIR .. "src/mame/drivers/alphasma.cpp",
+	MAME_DIR .. "src/mame/drivers/alphasma3k.cpp",
 	MAME_DIR .. "src/mame/drivers/am1000.cpp",
 	MAME_DIR .. "src/mame/drivers/ampex.cpp",
 	MAME_DIR .. "src/mame/drivers/ampex210.cpp",
