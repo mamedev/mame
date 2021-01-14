0c01abeb5567db4d6993acc138f967b6230b194b
Бранимир Караџић <branimirkaradzic@gmail.com>
2021-01-14 15:52:13 -0800

    Happy New Year!

--- 3rdparty/bx/tools/bin2c/bin2c.cpp
+++ 3rdparty/bx/tools/bin2c/bin2c.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2020 Branimir Karadzic. All rights reserved.
+ * Copyright 2011-2021 Branimir Karadzic. All rights reserved.
  * License: https://github.com/bkaradzic/bx#license-bsd-2-clause
  */
 
@@ -196,7 +196,7 @@ void help(const char* _error = NULL)
 
 	bx::write(stdOut, &err
 		, "bin2c, binary to C\n"
-		  "Copyright 2011-2020 Branimir Karadzic. All rights reserved.\n"
+		  "Copyright 2011-2021 Branimir Karadzic. All rights reserved.\n"
 		  "License: https://github.com/bkaradzic/bx#license-bsd-2-clause\n\n"
 		);
 
