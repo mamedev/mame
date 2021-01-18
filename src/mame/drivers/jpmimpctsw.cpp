2e61c44c7b7d0ee92bdb743f701ae73751e96e63
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2021-01-18 17:24:47 +0000

    cashbox key is a different place on these compared to the video games (and probably more)

--- src/mame/drivers/jpmimpctsw.cpp
+++ src/mame/drivers/jpmimpctsw.cpp
@@ -504,6 +504,15 @@ INPUT_PORTS_START( j6sonic ) // only runs with 5p or 10p stake and either 8 GBP 
 	PORT_INCLUDE( j6_stake_5_10 )
 
 	PORT_INCLUDE( j6_start_ex_co_3nud_cancel )
+
+	// move the cashbox door
+	PORT_MODIFY("J10_2")
+	PORT_DIPNAME( 0x02, 0x02, "J10_2: 1")
+	PORT_DIPSETTING(    0x02, DEF_STR( Off ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+
+	PORT_MODIFY("J9_2")
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_INTERLOCK) PORT_NAME("Cashbox Door")  PORT_CODE(KEYCODE_W) PORT_TOGGLE
 INPUT_PORTS_END
 
 INPUT_PORTS_START( j6sonicg ) // only runs without a key?
@@ -1622,6 +1631,15 @@ INPUT_PORTS_START( j6roller )
 	PORT_INCLUDE( j6_stake_5_10 )
 
 	PORT_INCLUDE( j6_start_ex_co_3nud_cancel )
+
+	// move the cashbox door
+	PORT_MODIFY("J10_2")
+	PORT_DIPNAME( 0x02, 0x02, "J10_2: 1")
+	PORT_DIPSETTING(    0x02, DEF_STR( Off ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+
+	PORT_MODIFY("J9_2")
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_INTERLOCK) PORT_NAME("Cashbox Door")  PORT_CODE(KEYCODE_W) PORT_TOGGLE
 INPUT_PORTS_END
 
 INPUT_PORTS_START( j6cdivr )
