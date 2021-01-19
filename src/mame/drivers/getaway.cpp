00b7f71435f7b4ce025d1d95d148a1627ccc3bad
hap <happppp@users.noreply.github.com>
2021-01-20 00:41:08 +0100

    getaway: show the complete vram for now

--- src/mame/drivers/getaway.cpp
+++ src/mame/drivers/getaway.cpp
@@ -212,7 +212,8 @@ void getaway_state::getaway(machine_config &config)
 	SCREEN(config, m_screen, SCREEN_TYPE_RASTER);
 	m_screen->set_refresh_hz(60);
 	m_screen->set_size(32*8, 32*8);
-	m_screen->set_visarea(0*8, 32*8-1, 4*8, 28*8-1);
+	//m_screen->set_visarea(0*8, 32*8-1, 4*8, 28*8-1);
+	m_screen->set_visarea_full();
 	m_screen->set_screen_update(FUNC(getaway_state::screen_update));
 	m_screen->screen_vblank().set(FUNC(getaway_state::vblank_irq));
 	m_screen->set_palette("palette");
