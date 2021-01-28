d887cf7dfeee178cd0dc192b5d20ae11f3a245a9
Tiago Pierezan Camargo <camargo@discworld.com.br>
2021-01-28 11:10:40 -0300

    libretro: refactor osdlib_retro for mame 0.228

--- src/osd/modules/lib/osdlib_retro.cpp
+++ src/osd/modules/lib/osdlib_retro.cpp
@@ -1,28 +1,24 @@
 
 #include <stdlib.h>
-#ifdef __GNUC__
-#include <unistd.h>
-#endif
-#ifndef _WIN32
-#include <sys/mman.h>
-#else
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <mmsystem.h>
-#endif
 #include <sys/types.h>
 #include <signal.h>
 #include <time.h>
-#ifdef __GNUC__
+
+#if defined(_WIN32)
+#include <windows.h>
+#include <memoryapi.h>
+#else
+#include <dlfcn.h>
+#include <sys/mman.h>
 #include <sys/time.h>
-#endif
-#ifdef SDLMAME_EMSCRIPTEN
-#include <emscripten.h>
+#include <unistd.h>
 #endif
 
 // MAME headers
-#include "osdcore.h"
 #include "osdlib.h"
+#include "osdcomm.h"
+#include "osdcore.h"
+#include "strconv.h"
 
 //============================================================
 //  osd_getenv
@@ -144,3 +140,223 @@ int osd_getpid(void)
 	return getpid();
 #endif
 }
+
+//============================================================
+//  osd_dynamic_bind
+//============================================================
+#if defined(_WIN32)
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+// for classic desktop applications
+#define load_library(filename) LoadLibrary(filename)
+#else
+// for Windows Store universal applications
+#define load_library(filename) LoadPackagedLibrary(filename, 0)
+#endif
+#endif
+namespace osd {
+
+namespace {
+
+#if defined(_WIN32)
+class dynamic_module_win32_impl : public dynamic_module
+{
+public:
+	dynamic_module_win32_impl(std::vector<std::string> &&libraries) : m_libraries(std::move(libraries))
+	{
+	}
+
+	virtual ~dynamic_module_win32_impl() override
+	{
+		if (m_module)
+			FreeLibrary(m_module);
+	}
+
+protected:
+	virtual generic_fptr_t get_symbol_address(char const *symbol) override
+	{
+		/*
+		 * given a list of libraries, if a first symbol is successfully loaded from
+		 * one of them, all additional symbols will be loaded from the same library
+		 */
+		if (m_module)
+			return reinterpret_cast<generic_fptr_t>(GetProcAddress(m_module, symbol));
+
+		for (auto const &library : m_libraries)
+		{
+			osd::text::tstring const tempstr = osd::text::to_tstring(library);
+			HMODULE const module = load_library(tempstr.c_str());
+
+			if (module)
+			{
+				auto const function = reinterpret_cast<generic_fptr_t>(GetProcAddress(module, symbol));
+
+				if (function)
+				{
+					m_module = module;
+					return function;
+				}
+				else
+				{
+					FreeLibrary(module);
+				}
+			}
+		}
+
+		return nullptr;
+	}
+
+private:
+	std::vector<std::string> m_libraries;
+	HMODULE                  m_module = nullptr;
+};
+#else
+class dynamic_module_posix_impl : public dynamic_module
+{
+public:
+	dynamic_module_posix_impl(std::vector<std::string> &&libraries) : m_libraries(std::move(libraries))
+	{
+	}
+
+	virtual ~dynamic_module_posix_impl() override
+	{
+		if (m_module)
+			dlclose(m_module);
+	}
+
+protected:
+	virtual generic_fptr_t get_symbol_address(char const *symbol) override
+	{
+		/*
+		 * given a list of libraries, if a first symbol is successfully loaded from
+		 * one of them, all additional symbols will be loaded from the same library
+		 */
+		if (m_module)
+			return reinterpret_cast<generic_fptr_t>(dlsym(m_module, symbol));
+
+		for (auto const &library : m_libraries)
+		{
+			void *const module = dlopen(library.c_str(), RTLD_LAZY);
+
+			if (module != nullptr)
+			{
+				generic_fptr_t const function = reinterpret_cast<generic_fptr_t>(dlsym(module, symbol));
+
+				if (function)
+				{
+					m_module = module;
+					return function;
+				}
+				else
+				{
+					dlclose(module);
+				}
+			}
+		}
+
+		return nullptr;
+	}
+
+private:
+	std::vector<std::string> m_libraries;
+	void *                   m_module = nullptr;
+};
+#endif
+} // anonymous namespace
+
+bool invalidate_instruction_cache(void const *start, std::size_t size)
+{
+#if defined(_WIN32)
+	return FlushInstructionCache(GetCurrentProcess(), start, size) != 0;
+#else
+#if !defined(SDLMAME_EMSCRIPTEN)
+	char const *const begin(reinterpret_cast<char const *>(start));
+	char const *const end(begin + size);
+	__builtin___clear_cache(const_cast<char *>(begin), const_cast<char *>(end));
+#endif
+	return true;
+#endif
+}
+
+void *virtual_memory_allocation::do_alloc(std::initializer_list<std::size_t> blocks, std::size_t &size, std::size_t &page_size)
+{
+#if defined(_WIN32)
+	SYSTEM_INFO info;
+	GetSystemInfo(&info);
+	SIZE_T s(0);
+	for (std::size_t b : blocks)
+		s += (b + info.dwPageSize - 1) / info.dwPageSize;
+	s *= info.dwPageSize;
+	if (!s)
+		return nullptr;
+	LPVOID const result(VirtualAlloc(nullptr, s, MEM_COMMIT, PAGE_NOACCESS));
+	if (result)
+	{
+		size = s;
+		page_size = info.dwPageSize;
+	}
+	return result;
+#else
+	long const p(sysconf(_SC_PAGE_SIZE));
+	if (0 >= p)
+		return nullptr;
+	std::size_t s(0);
+	for (std::size_t b : blocks)
+		s += (b + p - 1) / p;
+	s *= p;
+	if (!s)
+		return nullptr;
+#if defined(SDLMAME_BSD) || defined(SDLMAME_MACOSX) || defined(SDLMAME_EMSCRIPTEN)
+	int const fd(-1);
+#else
+	// TODO: portable applications are supposed to use -1 for anonymous mappings - detect whatever requires 0 specifically
+	int const fd(0);
+#endif
+	void *const result(mmap(nullptr, s, PROT_NONE, MAP_ANON | MAP_SHARED, fd, 0));
+	if (result == (void *)-1)
+		return nullptr;
+	size = s;
+	page_size = p;
+	return result;
+#endif
+}
+
+void virtual_memory_allocation::do_free(void *start, std::size_t size)
+{
+#if defined(_WIN32)
+	VirtualFree(start, 0, MEM_RELEASE);
+#else
+	munmap(reinterpret_cast<char *>(start), size);
+#endif
+}
+
+bool virtual_memory_allocation::do_set_access(void *start, std::size_t size, unsigned access)
+{
+#if defined(_WIN32)
+	DWORD p, o;
+	if (access & EXECUTE)
+		p = (access & WRITE) ? PAGE_EXECUTE_READWRITE : (access & READ) ? PAGE_EXECUTE_READ : PAGE_EXECUTE;
+	else
+		p = (access & WRITE) ? PAGE_READWRITE : (access & READ) ? PAGE_READONLY : PAGE_NOACCESS;
+	return VirtualProtect(start, size, p, &o) != 0;
+#else
+	int prot((NONE == access) ? PROT_NONE : 0);
+	if (access & READ)
+		prot |= PROT_READ;
+	if (access & WRITE)
+		prot |= PROT_WRITE;
+	if (access & EXECUTE)
+		prot |= PROT_EXEC;
+	return mprotect(reinterpret_cast<char *>(start), size, prot) == 0;
+#endif
+}
+
+dynamic_module::ptr dynamic_module::open(std::vector<std::string> &&names)
+{
+#if defined(_WIN32)
+	return std::make_unique<dynamic_module_win32_impl>(std::move(names));
+#else
+	return std::make_unique<dynamic_module_posix_impl>(std::move(names));
+#endif
+}
+
+} // namespace osd
