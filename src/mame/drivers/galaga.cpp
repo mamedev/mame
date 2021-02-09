c21a31286c77413b98310efcf1417056d486305c
Mike Harris <mike.harris@gmail.com>
2021-02-08 23:03:56 -0800

    galaga: re-order namco06 callbacks, for consistency.

--- src/mame/drivers/galaga.cpp
+++ src/mame/drivers/galaga.cpp
@@ -1614,12 +1614,12 @@ void bosco_state::bosco(machine_config &config)
 	n06xx_0.rw_callback<0>().set("51xx", FUNC(namco_51xx_device::rw));
 	n06xx_0.read_callback<0>().set("51xx", FUNC(namco_51xx_device::read));
 	n06xx_0.write_callback<0>().set("51xx", FUNC(namco_51xx_device::write));
-	n06xx_0.read_callback<2>().set("50xx_1", FUNC(namco_50xx_device::read));
 	n06xx_0.chip_select_callback<2>().set("50xx_1", FUNC(namco_50xx_device::chip_select));
 	n06xx_0.rw_callback<2>().set("50xx_1", FUNC(namco_50xx_device::rw));
+	n06xx_0.read_callback<2>().set("50xx_1", FUNC(namco_50xx_device::read));
 	n06xx_0.write_callback<2>().set("50xx_1", FUNC(namco_50xx_device::write));
-	n06xx_0.write_callback<3>().set("54xx", FUNC(namco_54xx_device::write));
 	n06xx_0.chip_select_callback<3>().set("54xx", FUNC(namco_54xx_device::chip_select));
+	n06xx_0.write_callback<3>().set("54xx", FUNC(namco_54xx_device::write));
 
 	namco_06xx_device &n06xx_1(NAMCO_06XX(config, "06xx_1", MASTER_CLOCK/6/64));
 	n06xx_1.set_maincpu(m_subcpu);
