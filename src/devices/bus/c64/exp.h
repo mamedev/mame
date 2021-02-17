83bc1bcdc09d6d134c1964375195230c5cf4f7bd
Ivan Vangelista <mesgnet@yahoo.it>
2021-02-17 13:08:45 +0100

    devices/bus/c64/exp: reworked cart loading [hap]

--- src/devices/bus/c64/exp.h
+++ src/devices/bus/c64/exp.h
@@ -153,8 +153,8 @@ protected:
 	std::unique_ptr<uint8_t[]> m_romx;
 	std::unique_ptr<uint8_t[]> m_nvram;
 
-	uint32_t m_roml_size;
-	uint32_t m_romh_size;
+	size_t m_roml_size;
+	size_t m_romh_size;
 
 	int m_game;
 	int m_exrom;
