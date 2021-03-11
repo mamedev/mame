257cfab8a1f17ebdcdf3cf2114bb6f37b187ca61
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2021-03-11 18:23:13 +0000

    switch ixion sprite/tilemap priority, remove out of date m62 note, demoted 2 games that don't appear to have ever worked in a satisfactory manner

--- src/mame/video/zaxxon.cpp
+++ src/mame/video/zaxxon.cpp
@@ -463,6 +463,18 @@ uint32_t zaxxon_state::screen_update_razmataz(screen_device &screen, bitmap_ind1
 	return 0;
 }
 
+uint32_t zaxxon_state::screen_update_ixion(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect)
+{
+	draw_background(bitmap, cliprect, false);
+	// On Ixion the fg tilemap is used to blank out erased tiles, the sprites must appear above these
+	// whereas on Razzmatazz the fg tilemap is used to mask off sprites near the edges so must appear
+	// above them.
+	// Is there a missing priority control bit or is this a hardwired change on the PCB?
+	m_fg_tilemap->draw(screen, bitmap, cliprect, 0, 0);
+	draw_sprites(bitmap, cliprect, 0x140, 0x180);
+	return 0;
+}
+
 
 uint32_t zaxxon_state::screen_update_congo(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect)
 {
