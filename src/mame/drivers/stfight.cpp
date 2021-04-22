e5ae9d5b2075db498fda4844e3c9104984993b1a
sasuke-arcade <58130089+sasuke-arcade@users.noreply.github.com>
2021-04-23 08:40:22 +0900

    stfight.cpp: Remove YM2203 frequency hack, and set prescaler at machine_start (#7992)

--- src/mame/drivers/stfight.cpp
+++ src/mame/drivers/stfight.cpp
@@ -245,11 +245,10 @@ conventional RAM. See the memory map for sprite data format.
 TODO:
 - handle transparency in text layer properly (how?)
 - second bank of sf02 is this used? (probably NOT)
-- stfight/empcity YM2203s should be clocked at 1.5MHz but this results in
-  the sound and music being 1/3 of the pitch they should be. The game never
-  writes the YM2203s' divider registers yet other games (e.g. Lock-On)
-  suggest the default values are correct.
-  cshootert however, sounds too high-pitched at 1.5MHz*3.
+- empcity/stfight never writes the YM2203s' divider registers but it expects
+  0x2f, there's a workaround for it in machine_start
+- empcity/stfight has an NMI handler, but it's not hooked up in MAME, missing
+  comms somewhere?
 - Each version of empcity/stfight has a different protection code stored in the
   MCU (at $1D2) so each 68705 will need to be dumped.
   We currently use hacked versions of the empcityu MCU for each different set.
@@ -483,14 +482,13 @@ void stfight_state::stfight_base(machine_config &config)
 	/* sound hardware */
 	SPEAKER(config, "mono").front_center();
 
-	// YM2203_PITCH_HACK - These should be clocked at 1.5Mhz (see TODO list)
-	ym2203_device &ym1(YM2203(config, "ym1", 12_MHz_XTAL / 8 * 3));
+	ym2203_device &ym1(YM2203(config, m_ym1, 12_MHz_XTAL / 8));
 	ym1.add_route(0, "mono", 0.15);
 	ym1.add_route(1, "mono", 0.15);
 	ym1.add_route(2, "mono", 0.15);
 	ym1.add_route(3, "mono", 0.10);
 
-	ym2203_device &ym2(YM2203(config, "ym2", 12_MHz_XTAL / 8 * 3));
+	ym2203_device &ym2(YM2203(config, m_ym2, 12_MHz_XTAL / 8));
 	ym2.add_route(0, "mono", 0.15);
 	ym2.add_route(1, "mono", 0.15);
 	ym2.add_route(2, "mono", 0.15);
