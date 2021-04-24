8c80f0844e65f06e17e141cbd74dcb43e0659ac5
Aaron Giles <aaron@aarongiles.com>
2021-04-24 02:25:22 -0700

    Ensure CPUs held in reset are set up properly.

--- src/emu/diexec.cpp
+++ src/emu/diexec.cpp
@@ -379,7 +379,7 @@ void device_execute_interface::interface_pre_start()
 
 	// fill in the initial states
 	int const index = device_enumerator(device().machine().root_device()).indexof(*this);
-	m_suspend = SUSPEND_REASON_RESET;
+	suspend(SUSPEND_REASON_RESET);
 	m_profiler = profile_type(index + PROFILER_DEVICE_FIRST);
 	m_inttrigger = index + TRIGGER_INT;
 
