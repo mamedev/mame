650f3eca7e6179d5796006a88eb9d7a554542b07
tim lindner <tlindner@macmess.org>
2021-05-25 23:03:40 -0700

    fine tuning

--- src/devices/bus/coco/coco_max.cpp
+++ src/devices/bus/coco/coco_max.cpp
@@ -13,8 +13,7 @@
 
 #include "machine/ram.h"
 
-#define MOUSE_DELTA			10
-#define MOUSE_SENSITIVITY	100
+#define MOUSE_SENSITIVITY	75
 #define COCOMAX_X_TAG		"cocomax_x"
 #define COCOMAX_Y_TAG		"cocomax_y"
 #define COCOMAX_BUTTONS		"cocomax_buttons"
@@ -28,9 +27,9 @@
 
 INPUT_PORTS_START( cocomax_mouse )
 	PORT_START(COCOMAX_X_TAG)
-	PORT_BIT( 0xff, 0x80,  IPT_MOUSE_X) PORT_NAME("CoCo Max Mouse X") PORT_SENSITIVITY(MOUSE_SENSITIVITY) PORT_KEYDELTA(MOUSE_DELTA) PORT_MINMAX(0x00,0xff) PORT_CODE_DEC(KEYCODE_4_PAD) PORT_CODE_INC(KEYCODE_6_PAD) PORT_CODE_DEC(JOYCODE_X_LEFT_SWITCH) PORT_CODE_INC(JOYCODE_X_RIGHT_SWITCH) PORT_PLAYER(1)
+	PORT_BIT( 0xff, 0x00,  IPT_AD_STICK_X) PORT_NAME("CoCo Max Mouse X") PORT_SENSITIVITY(MOUSE_SENSITIVITY) PORT_MINMAX(0x00,0xff) PORT_PLAYER(1)
 	PORT_START(COCOMAX_Y_TAG)
-	PORT_BIT( 0xff, 0x80,  IPT_MOUSE_Y) PORT_NAME("CoCo Max Mouse Y") PORT_SENSITIVITY(MOUSE_SENSITIVITY) PORT_KEYDELTA(MOUSE_DELTA) PORT_MINMAX(0x00,0xff) PORT_CODE_DEC(KEYCODE_8_PAD) PORT_CODE_INC(KEYCODE_2_PAD) PORT_CODE_DEC(JOYCODE_Y_UP_SWITCH) PORT_CODE_INC(JOYCODE_Y_DOWN_SWITCH) PORT_PLAYER(1)
+	PORT_BIT( 0xff, 0x00,  IPT_AD_STICK_Y) PORT_NAME("CoCo Max Mouse Y") PORT_SENSITIVITY(MOUSE_SENSITIVITY) PORT_MINMAX(0x00,0xff) PORT_PLAYER(1)
 	PORT_START(COCOMAX_BUTTONS)
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_BUTTON1) PORT_NAME("CoCo Max Left Button") PORT_CODE(KEYCODE_0_PAD) PORT_CODE(MOUSECODE_BUTTON1) PORT_PLAYER(1)
 	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_BUTTON2) PORT_NAME("CoCo Max Right Button") PORT_CODE(KEYCODE_DEL) PORT_CODE(MOUSECODE_BUTTON2) PORT_PLAYER(1)
@@ -58,7 +57,7 @@ namespace
 		virtual void device_reset() override;
 		virtual ioport_constructor device_input_ports() const override;
 
-		u8 ff60_read(offs_t offset);
+		u8 ff90_read(offs_t offset);
 
 	private:
 		required_ioport m_mouse_x;
@@ -114,8 +113,8 @@ void coco_pak_max_device::device_start()
 	// save state
  	save_item(NAME(m_a2d_result));
 
-	// install $ff60-$ff63 handler
-	install_read_handler(0xff60, 0xff63, read8sm_delegate(*this, FUNC(coco_pak_max_device::ff60_read)));
+	// install $ff90-$ff93 handler
+	install_read_handler(0xff90, 0xff93, read8sm_delegate(*this, FUNC(coco_pak_max_device::ff90_read)));
 }
 
 
@@ -135,23 +134,23 @@ void coco_pak_max_device::device_reset()
 //  ff60_read
 //-------------------------------------------------
 
-u8 coco_pak_max_device::ff60_read(offs_t offset)
+u8 coco_pak_max_device::ff90_read(offs_t offset)
 {
 	uint8_t result = m_a2d_result;
 
 	switch (offset)
 	{
 		case 0:
-			m_a2d_result = m_mouse_x->read();
-			break;
-		case 1:
 			m_a2d_result = m_mouse_y->read();
 			break;
+		case 1:
+			m_a2d_result = m_mouse_x->read();
+			break;
 		case 2:
-			m_a2d_result = BIT(m_buttons->read(), 0) ? 0xff : 0;
+			m_a2d_result = BIT(m_buttons->read(), 0) ? 0 : 0xff;
 			break;
 		case 3:
-			m_a2d_result = BIT(m_buttons->read(), 1) ? 0xff : 0;
+			m_a2d_result = BIT(m_buttons->read(), 1) ? 0 : 0xff;
 			break;
 		default:
 			break;
