fd4f9f237c3faf0f722cf003a727e767032960f2
Olivier Galibert <galibert@pobox.com>
2021-05-26 20:25:13 +0200

    Copy/paste fix

--- src/lib/formats/all.cpp
+++ src/lib/formats/all.cpp
@@ -1387,7 +1387,7 @@ void mame_formats_full_list(mame_formats_enumerator &en)
 	en.add(vtech1_cassette_formats); // vt_cas.h
 	en.add(vtech2_cassette_formats); // vt_cas.h
 #endif
-#ifdef HAS_FORMATS_VT_CAS
+#ifdef HAS_FORMATS_VT_DSJ
 	en.add(FLOPPY_VTECH_BIN_FORMAT); // vt_dsk.h
 	en.add(FLOPPY_VTECH_DSK_FORMAT); // vt_dsk.h
 #endif
