6e5f1cbb772680bfdb84817a8b3aeb5dbc99480e
Ivan Vangelista <mesgnet@yahoo.it>
2021-06-01 18:12:44 +0200

    New machines marked as NOT_WORKING ---------------------------------- Play Sonic 4 [Juan Romero, ClawGrip]

--- scripts/target/mame/arcade.lua
+++ scripts/target/mame/arcade.lua
@@ -5064,6 +5064,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/pipeline.cpp",
 	MAME_DIR .. "src/mame/drivers/pkscram.cpp",
 	MAME_DIR .. "src/mame/drivers/playcenter.cpp",
+	MAME_DIR .. "src/mame/drivers/plsonic4.cpp",
 	MAME_DIR .. "src/mame/drivers/pntnpuzl.cpp",
 	MAME_DIR .. "src/mame/drivers/policetr.cpp",
 	MAME_DIR .. "src/mame/includes/policetr.h",
