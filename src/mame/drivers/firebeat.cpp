ce76a151480b664793a43badd01ee7b9c773eb34
Ivan Vangelista <mesgnet@yahoo.it>
2021-06-02 07:58:37 +0200

    New working clones ------------------ Asura Buster - Eternal Warriors (USA) [Darksoft, Brian Troha, The Dumping Union] Asura Buster - Eternal Warriors (Japan, set 2) [CPsystem3, Brian Troha, The Dumping Union]
    - firebeat.cpp: removed outdated comment [Guru]

--- src/mame/drivers/firebeat.cpp
+++ src/mame/drivers/firebeat.cpp
@@ -2705,7 +2705,6 @@ GAME( 2000, popnanm,  0,      firebeat_popn, popn, firebeat_popn_state, init_pop
 GAME( 2001, popnanm2, 0,      firebeat_popn, popn, firebeat_popn_state, init_popn_jp, ROT0, "Konami", "Pop'n Music Animelo 2", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND )
 
 // Requires ST-224 emulation for optional toggleable external effects, but otherwise is fully playable
-// Core Remix and 6th Mix are marked as MACHINE_NOT_WORKING because of missing HDD dumps
 GAME( 2000, bm3,      0, firebeat_bm3, bm3, firebeat_bm3_state, init_bm3, ROT0, "Konami", "Beatmania III", MACHINE_IMPERFECT_SOUND )
 GAME( 2000, bm3core,  0, firebeat_bm3, bm3, firebeat_bm3_state, init_bm3, ROT0, "Konami", "Beatmania III Append Core Remix", MACHINE_IMPERFECT_SOUND )
 GAME( 2001, bm36th,   0, firebeat_bm3, bm3, firebeat_bm3_state, init_bm3, ROT0, "Konami", "Beatmania III Append 6th Mix", MACHINE_IMPERFECT_SOUND )
