2dd082e07c5b828b7ac14f250f55d05f0d0a4bc3
hap <happppp@users.noreply.github.com>
2021-06-09 20:47:00 +0200

    c140: correct offset range when reading keyon status [dink]

--- src/devices/sound/c140.cpp
+++ src/devices/sound/c140.cpp
@@ -478,7 +478,7 @@ inline u8 c140_device::keyon_status_read(u16 offset)
 u8 c140_device::c140_r(offs_t offset)
 {
 	offset &= 0x1ff;
-	if ((offset & 0xf) == 0x5)
+	if ((offset & 0xf) == 0x5 && offset < 0x180)
 		return keyon_status_read(offset);
 	return m_REG[offset];
 }
@@ -552,6 +552,8 @@ void c140_device::c140_w(offs_t offset, u8 data)
 
 u8 c219_device::c219_r(offs_t offset)
 {
+	offset &= 0x1ff;
+
 	// assume same as c140
 	// TODO: what happens here on reading unmapped voice regs?
 	return c140_r(offset);
