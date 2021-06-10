daeb517490087653db910907692ace3a45616dba
tim lindner <tlindner@macmess.org>
2021-06-09 17:29:52 -0700

    mcx-128 is quite a card

--- src/devices/bus/mc10/mc10_mcx128.cpp
+++ src/devices/bus/mc10/mc10_mcx128.cpp
@@ -6,6 +6,35 @@
 
     Code for emulating Darren Atkinson's MCX-128 cartridge
 
+	Features:
+		128K of RAM expansion
+		16K of ROM expansion
+
+	New MC-10 memory map defined by this cartridge:
+
+	0000 - 0003	6803 Ports
+	0004 - 0007	Expansion RAM
+	0008 - 000E	6803 Status / Control
+	000F		Expansion RAM
+	0010 - 0013	6803 UART
+	0014		6803 RAM Control Reg
+	0015 - 001F	Unused
+	0020 - 007F	Expansion RAM
+	0080 - 00FF	On-chip CPU RAM / Expansion RAM
+	0100 - 3FFF	Expansion RAM
+	4000 - 5FFF	Built-In RAM / Expansion RAM
+	6000 - BEFF	Expansion RAM
+	BF00		RAM Bank Control Reg
+	BF01		ROM Map Control Reg
+	BF80 - BFFF	Keyboard / VDG / Sound
+	C000 - DFFF	EPROM or Expansion RAM
+	E000 - FFFF	Built-in ROM, EPROM or Expansion RAM
+
+	Deficiency:
+
+	* Writing to 6803 chip RAM at $80 to $ff should be mirrored
+	  to external RAM.
+
 ***************************************************************************/
 
 #include "emu.h"
@@ -51,6 +80,8 @@ namespace
 
 		u8 control_register_read(offs_t offset);
 		void control_register_write(offs_t offset, u8 data);
+
+		void write_ram_mirror(address_space &space, offs_t offset, u8 data);
 
 		void view_map0(address_map &map);
 		void view_map1(address_map &map);
@@ -101,8 +132,8 @@ void mc10_pak_mcx128_device::view_map0(address_map &map)
  	map(0x0004, 0x0007).bankrw("bank0");
  	map(0x000f, 0x000f).bankrw("bank1");
  	map(0x0020, 0x3fff).bankrw("bank2");
-//	0x4000, 0x4fff: internal RAM
- 	map(0x5000, 0xbeff).bankrw("bank4");
+ 	map(0x4000, 0x5fff).bankr("bank3").w(FUNC(mc10_pak_mcx128_device::write_ram_mirror));
+ 	map(0x6000, 0xbeff).bankrw("bank4");
  	map(0xbf00, 0xbf01).rw(FUNC(mc10_pak_mcx128_device::control_register_read), FUNC(mc10_pak_mcx128_device::control_register_write));
  	map(0xc000, 0xfeff).rom().region("eprom",0x0000).bankw("bank5");
  	map(0xff00, 0xffff).rom().region("eprom",0x3f00).bankw("bank7");
@@ -113,8 +144,8 @@ void mc10_pak_mcx128_device::view_map1(address_map &map)
  	map(0x0004, 0x0007).bankrw("bank0");
  	map(0x000f, 0x000f).bankrw("bank1");
  	map(0x0020, 0x3fff).bankrw("bank2");
-//	0x4000, 0x4fff: internal RAM
- 	map(0x5000, 0xbeff).bankrw("bank4");
+ 	map(0x4000, 0x5fff).bankr("bank3").w(FUNC(mc10_pak_mcx128_device::write_ram_mirror));
+ 	map(0x6000, 0xbeff).bankrw("bank4");
  	map(0xbf00, 0xbf01).rw(FUNC(mc10_pak_mcx128_device::control_register_read), FUNC(mc10_pak_mcx128_device::control_register_write));
  	map(0xc000, 0xdfff).bankrw("bank5");
  	map(0xe000, 0xfeff).rom().region("eprom",0x2000).bankw("bank6");
@@ -126,8 +157,8 @@ void mc10_pak_mcx128_device::view_map2(address_map &map)
  	map(0x0004, 0x0007).bankrw("bank0");
  	map(0x000f, 0x000f).bankrw("bank1");
  	map(0x0020, 0x3fff).bankrw("bank2");
-//	0x4000, 0x4fff: internal RAM
- 	map(0x5000, 0xbeff).bankrw("bank4");
+ 	map(0x4000, 0x5fff).bankr("bank3").w(FUNC(mc10_pak_mcx128_device::write_ram_mirror));
+ 	map(0x6000, 0xbeff).bankrw("bank4");
  	map(0xbf00, 0xbf01).rw(FUNC(mc10_pak_mcx128_device::control_register_read), FUNC(mc10_pak_mcx128_device::control_register_write));
  	map(0xc000, 0xdfff).bankrw("bank5");
  	map(0xe000, 0xfeff).bankw("bank6");
@@ -140,8 +171,8 @@ void mc10_pak_mcx128_device::view_map3(address_map &map)
  	map(0x0004, 0x0007).bankrw("bank0");
  	map(0x000f, 0x000f).bankrw("bank1");
  	map(0x0020, 0x3fff).bankrw("bank2");
-//	0x4000, 0x4fff: internal RAM
- 	map(0x5000, 0xbeff).bankrw("bank4");
+ 	map(0x4000, 0x5fff).bankr("bank3").w(FUNC(mc10_pak_mcx128_device::write_ram_mirror));
+ 	map(0x6000, 0xbeff).bankrw("bank4");
  	map(0xbf00, 0xbf01).rw(FUNC(mc10_pak_mcx128_device::control_register_read), FUNC(mc10_pak_mcx128_device::control_register_write));
  	map(0xc000, 0xfeff).bankrw("bank5");
  	map(0xff00, 0xffff).bankrw("bank7");
@@ -215,13 +246,13 @@ void mc10_pak_mcx128_device::device_start()
 	m_bank2->configure_entry(0, &m_share[0x00020]);
 	m_bank2->configure_entry(1, &m_share[0x10020]);
 
-	// 0x4000 - 0x4fff
+	// 0x4000 - 0x5fff
 	m_bank3->configure_entry(0, &m_share[0x08000]);
 	m_bank3->configure_entry(1, &m_share[0x18000]);
 
-	// 0x5000 - 0xbeff
-	m_bank4->configure_entry(0, &m_share[0x09000]);
-	m_bank4->configure_entry(1, &m_share[0x19000]);
+	// 0x6000 - 0xbeff
+	m_bank4->configure_entry(0, &m_share[0x0a000]);
+	m_bank4->configure_entry(1, &m_share[0x1a000]);
 
 	// 0xc000 - 0xdfff
 	m_bank5->configure_entry(0, &m_share[0x04000]);
@@ -261,6 +292,20 @@ void mc10_pak_mcx128_device::device_reset()
 void mc10_pak_mcx128_device::device_post_load()
 {
 	update_banks();
+}
+
+void mc10_pak_mcx128_device::write_ram_mirror(address_space &space, offs_t offset, u8 data)
+{
+	std::optional<int> cur_slot = m_view.entry();
+	m_share[0x08000+offset] = data;
+
+	if(cur_slot.has_value())
+	{
+		int cur_slot_int = *cur_slot;
+		m_view.disable();
+		space.write_byte(0x4000+offset, data);
+		m_view.select(cur_slot_int);
+	}
 }
 
 u8 mc10_pak_mcx128_device::control_register_read(offs_t offset)
