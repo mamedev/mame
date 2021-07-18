71102ba542a9849524690ae1095eb45efee71594
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2021-07-18 12:53:11 +0100

    actually commit the version I meant to commit

--- src/mame/video/namco_c355spr.cpp
+++ src/mame/video/namco_c355spr.cpp
@@ -398,8 +398,8 @@ void namco_c355spr_device::get_single_sprite(const u16 *pSource, c355_sprite *sp
 
 	int tile_index   = spriteformat16[linkno * 4 + 0];
 	const u16 format = spriteformat16[linkno * 4 + 1];
-	int dx           = spriteformat16[linkno * 4 + 2]; // should this also be signed like dy?
-	const int dy     = spriteformat16[linkno * 4 + 3] & 0x1ff;
+	int dx           = spriteformat16[linkno * 4 + 2];
+	int dy           = spriteformat16[linkno * 4 + 3];
 	int num_cols     = (format >> 4) & 0xf;
 	int num_rows     = (format) & 0xf;
 
@@ -432,14 +432,28 @@ void namco_c355spr_device::get_single_sprite(const u16 *pSource, c355_sprite *sp
 	}
 	u32 zoomy = (vsize << 16) / (num_rows * 16);
 	int dy_zoomed = ((dy & 0xff) * zoomy + 0x8000) >> 16;
-	if (dy & 0x100) dy_zoomed = -dy_zoomed;
+
 	if (!flipy)
 	{
-		vpos += dy_zoomed;
+		if (dy & 0x100)
+		{
+			vpos += dy_zoomed;
+		}
+		else
+		{
+			vpos -= dy_zoomed;
+		}
 	}
 	else
 	{
-		vpos -= dy_zoomed;
+		if (dy & 0x100)
+		{
+			vpos -= dy_zoomed;
+		}
+		else
+		{
+			vpos += dy_zoomed;
+		}
 	}
 
 	sprite_ptr->flipx = flipx;
