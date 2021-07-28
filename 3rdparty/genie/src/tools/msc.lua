b13f0b7cf903f2ce75485c4b2d7df85c1cba30e7
Miodrag Milanovic <mmicko@gmail.com>
2021-07-28 08:58:42 +0200

    Remove Genie

--- 3rdparty/genie/src/tools/msc.lua
+++
@@ -1,9 +0,0 @@
---
--- msc.lua
--- Interface for the MS C/C++ compiler.
--- Copyright (c) 2009 Jason Perkins and the Premake project
---
-
-	
-	premake.msc = { }
-	premake.msc.namestyle = "windows"
--- 3rdparty/genie/tests/test.bat
+++
@@ -1,3 +0,0 @@
-
-CALL ..\\bin\\debug\\premake4 /scripts=..\\src test
-::CALL ..\\bin\\release\\premake4 /scripts=..\\src test
