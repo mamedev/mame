2ad1f4d9aafb12c0e0b4058dd269f48651699d4a
0kmg <9137159+0kmg@users.noreply.github.com>
2021-07-28 07:44:55 -0800

    bus/nes: Added support for YUNG-08 SMB2 FDS conversions. (#8366)
    Software list items promoted to working (nes.xml)
    ---------------------------------------
    Super Mario Bros. 2 (YUNG-08)
    Super Mario Bros. 2 (YUNG-08, no protection)

--- src/devices/bus/nes/nes_ines.hxx
+++ src/devices/bus/nes/nes_ines.hxx
@@ -402,7 +402,7 @@ static const nes_mmc mmc_list[] =
 	// 365 is this asderp95 in nes.xml?
 	// 366 K-3131GS and K-3131SS 4-in-1 carts
 	// 367 7-in-1 cart that is a close variant of mapper 205
-	// { 368, BTL_YUNG08 }            // SMB2 FDS conversion
+	{ 368, BTL_YUNG08 },            // SMB2 FDS conversion
 	// 369 Super Mario Bros Party multicart
 	// 370 Golden Mario Party II multicart
 	// 371 Spanish PEC-586 computer main cart
