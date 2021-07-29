05dce9ffd4925901d4194fe4cd4d2b1ba9296f8d
0kmg <9137159+0kmg@users.noreply.github.com>
2021-07-28 23:38:21 -0800

    bus/nes: Fixed a multicart game (mapper 229).
    Software list items promoted to working
    ---------------------------------------
    31 in 1

--- src/devices/bus/nes/multigame.h
+++ src/devices/bus/nes/multigame.h
@@ -687,15 +687,9 @@ class nes_bmc_31in1_device : public nes_nrom_device
 {
 public:
 	// construction/destruction
-	nes_bmc_31in1_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+	nes_bmc_31in1_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
 
-	virtual void write_h(offs_t offset, uint8_t data) override;
-
-	virtual void pcb_reset() override;
-
-protected:
-	// device-level overrides
-	virtual void device_start() override;
+	virtual void write_h(offs_t offset, u8 data) override;
 };
 
 
