8d4b8ef3fba406cd7b6b3e309e89a4d4e98d2668
algestam <henrik@algestam.se>
2021-07-31 13:33:33 +0200

    hh_sm510: rescan display for auslalom (#8377)

--- src/mame/drivers/hh_sm510.cpp
+++ src/mame/drivers/hh_sm510.cpp
@@ -17,7 +17,7 @@ Use -autosave to at least make them remember the highscores.
 TODO:
 - improve display decay simulation? but SVG doesn't support setting brightness
   per segment, adding pwm_display_device right now has no added value
-- improve/redo SVG of: exospace, auslalom
+- improve/redo SVG of: exospace
 - confirm gnw_bfight rom (assumed to be the same as gnw_bfightn)
 - confirm gnw_climber rom (assumed to be the same as gnw_climbern)
 - confirm gnw_smb rom (assumed to be the same as gnw_smbn)
@@ -9373,7 +9373,7 @@ INPUT_PORTS_END
 
 void auslalom_state::auslalom(machine_config &config)
 {
-	kb1013vk12_common(config, 1780, 1080); // R mask option ?
+	kb1013vk12_common(config, 1732, 1080); // R mask option ?
 }
 
 // roms
@@ -9382,8 +9382,8 @@ ROM_START( auslalom )
 	ROM_REGION( 0x1000, "maincpu", 0 )
 	ROM_LOAD( "im-23.bin", 0x0000, 0x0740, CRC(3b6e726f) SHA1(eabd04722811d1cc6519db9386b14a535f5aa865) )
 
-	ROM_REGION( 85857, "screen", 0)
-	ROM_LOAD( "auslalom.svg", 0, 85857, BAD_DUMP CRC(47381b27) SHA1(de0410331669c07c32aaed391b8da469696adbe7) )
+	ROM_REGION( 117520, "screen", 0)
+	ROM_LOAD( "auslalom.svg", 0, 117520, CRC(2f90fd4c) SHA1(f0de58b1fe2f7c18fc219f9f9a94c227ca1245e4) )
 ROM_END
 
 
