8e11d3ba56c59d08ce1ceaf66de3effd144bb5b9
Golden Child <ggoldnchild@gmail.com>
2021-08-03 18:51:38 -0700

    pass directory and filename to write_snapshot_file

--- src/devices/bus/a2bus/silentype.h
+++ src/devices/bus/a2bus/silentype.h
@@ -2,7 +2,7 @@
 // copyright-holders: Golden Child
 /*********************************************************************
 
-    a2silentype.h
+    silentype.h
 
     Implementation of the Apple II Silentype Printer
 
@@ -54,10 +54,11 @@ protected:
 
 	bitmap_rgb32 m_bitmap;
 
-	int m_xpos=250;
-	int m_ypos=0;
-	uint16_t m_shift_reg,m_parallel_reg;
-	int m_romenable=0;  // start off disabled
+	int m_xpos = 250;
+	int m_ypos = 0;
+	uint16_t m_shift_reg = 0;
+	uint16_t m_parallel_reg = 0;
+	int m_romenable = 0;  // start off disabled
 
 	required_device<screen_device> m_screen;
 
@@ -65,27 +66,18 @@ protected:
 	int left_offset = 3;
 
 	double headtemp[7] = {0.0}; // initialize to zero - nan bugs
-//  int heattime=3500;   // time in usec to hit max temp  (smaller numbers mean faster)  5 levels
+	int heattime = 3000;   // time in usec to hit max temp  (smaller numbers mean faster)
+	int decaytime = 1000;  // time in usec to cool off
 
-// equal 4 levels
-//  int heattime=4500;   // time in usec to hit max temp  (smaller numbers mean faster)
-// int heattime=2000;   // time in usec to hit max temp  (smaller numbers mean faster)  5 levels saturated in the middle
-//      int heattime=4000;   // time in usec to hit max temp  (smaller numbers mean faster)  5 levels
-				int heattime=3000;   // time in usec to hit max temp  (smaller numbers mean faster)  5 levels  25 percent   68/255 was darkest at 4000
-	int decaytime=1000;  // time in usec to cool off
-
-//  int hstepper;
-//  int vstepper;
 	int hstepperlast = 0;
 	int vstepperlast = 0;
-//  int headbits;
 	int lastheadbits = 0;
 	int xdirection;
 	int newpageflag;
 
 	int page_count=0;
 
-	double last_update_time;
+	double last_update_time = 0.0;  // strange behavior if we don't initialize
 
  private:
 	uint32_t screen_update_silentype(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
@@ -96,23 +88,18 @@ protected:
 	const int PAPER_HEIGHT = 11 * dpi;   // 11  inches high at 60 dpi
 	const int PAPER_SCREEN_HEIGHT = 384; // match the height of the apple II driver
 
-	uint32_t BITS(uint32_t x, u8 m, u8 n) {return (((x)>>(n))&(((uint32_t)1<<((m)-(n)+1))-1));}
+	uint32_t BITS(uint32_t x, u8 m, u8 n) {return ( ((x) >> (n)) & ( ((uint32_t) 1 << ((m) - (n) + 1)) - 1));}
 
-	int wrap(int x,int mod){if (x<0) return (x+((-1*(x/mod))+1)*mod)%mod; else return x % mod;}
-	// mod=4 for x = -25,5 do if (x<0) then print(x, (x+((-1*(math.floor(x/mod)))+1)*mod)%mod) else print(x, x % mod) end end
-	int getbit(int a, int bit) {return (a & bit)!=0;}
-// this setbit works on a mask 1<<bit
-//  int setbit(int& a, int bit, int value) {a=(a&~bit)|(bit*((value!=0)?1:0)); return a;}
+	int wrap(int x, int mod) {if (x<0) return (x + ((-1 * (x / mod)) + 1) * mod) % mod; else return x % mod;}
+	void write_snapshot_to_file(std::string directory, std::string name);
 
-// this setbit works on a bitnumber
-//int setbit(int& a, int bit, int value) {a=(a&~(1<<bit))|((1<<bit)*((value!=0)?1:0)); return a;}
-int setbit(int& a, int bit, int value) {a=(a&~(1<<bit))|((((value!=0)?(1<<bit):0))); return a;}
-	void write_snapshot_to_file();
-
+	void adjust_headtemp(u8 pin_status, double time_elapsed,  double& temp);
 	void darken_pixel(double headtemp, u32& pixel);
 };
 
 
 // device type definition
+
 DECLARE_DEVICE_TYPE(A2BUS_SILENTYPE, a2bus_silentype_device)
+
 #endif // MAME_BUS_A2BUS_A2SILENTYPE_H
