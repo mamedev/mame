b2c4029300a9df43f3110817b20975186774e953
ClawGrip <clawgrip@hotmail.com>
2021-08-19 15:22:24 +0200

    arcade.lua: Add 'opercoin.cpp'

--- scripts/target/mame/arcade.lua
+++ scripts/target/mame/arcade.lua
@@ -5061,6 +5061,7 @@ files {
 	MAME_DIR .. "src/mame/includes/oneshot.h",
 	MAME_DIR .. "src/mame/video/oneshot.cpp",
 	MAME_DIR .. "src/mame/drivers/onetwo.cpp",
+	MAME_DIR .. "src/mame/drivers/opercoin.cpp",
 	MAME_DIR .. "src/mame/drivers/othello.cpp",
 	MAME_DIR .. "src/mame/drivers/pachifev.cpp",
 	MAME_DIR .. "src/mame/drivers/pasha2.cpp",
