4c6d6fe936581927f30e76a2293b490f20a43c8f
0kmg <9137159+0kmg@users.noreply.github.com>
2021-08-24 14:08:35 -0800

    bus/nes: Added support for a Highway Star bootleg.
    - Renamed hstarfds to highwayk. It cannot be an FDS bootleg since Highway Star only appeared on cart.
    - Demoted all Rad Racer/Highway Star games to partially supported due to severity of graphical issues (car dashboard flickers to top of screen, etc).
    
    New working software list additions (nes.xml)
    -----------------------------------
    Highway Star (Whirlwind Manu bootleg) [krzysiobal]

--- src/devices/bus/nes/nes_ines.hxx
+++ src/devices/bus/nes/nes_ines.hxx
@@ -347,7 +347,7 @@ static const nes_mmc mmc_list[] =
 	{ 309, UNL_LH51 },             // Ai Senshi Nicol alt FDS conversion
 	// 310 variant of mapper 125?
 	// 311 Unused (previously assigned in error to a bad SMB2 pirate dump)
-	{ 312, KAISER_KS7013B },       // Highway Star FDS conversion
+	{ 312, KAISER_KS7013B },       // Highway Star Kaiser bootleg
 	{ 313, BMC_RESETTXROM0 },
 	{ 314, BMC_64IN1NR },
 	// 315 820732C and 830134C multicarts, not in nes.xml?
@@ -453,7 +453,7 @@ static const nes_mmc mmc_list[] =
 	{ 415, BTL_0353 },             // Lucky (Roger) Rabbit FDS conversion
 	// 416 4-in-1 that includes mapper 50 SMB2j pirate
 	{ 417, BTL_BATMANFS },         // "Fine Studio" Batman bootleg
-	// { 418, UNL_LH42 }              // Highway Star alt FDS conversion
+	{ 418, UNL_LH42 },             // Highway Star Whirlwind Manu bootleg
 	// 419 VT03 PnPs
 	// 420 Kasheng A971210 board
 	// 421 JY SC871115C board
