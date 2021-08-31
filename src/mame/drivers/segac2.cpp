770fdaf7075fe8f65858df815687c3eea5716761
hap <happppp@users.noreply.github.com>
2021-08-31 08:42:42 +0200

    segac2: ooparts pcb does have upd sample rom

--- src/mame/drivers/segac2.cpp
+++ src/mame/drivers/segac2.cpp
@@ -61,7 +61,7 @@
     TODO:   - Puyo Puyo ends up with a black screen after doing memory tests
             - Battery-backed RAM needs to be figured out
             - Correct ROM labels for: ssonicbr, ooparts, headonch
-            - Does ooparts have a sample ROM?
+            - Does ooparts actually use upd samples? PCB has the same rom as tfrceac
 
     Thanks: (in no particular order) to any MameDev that helped me out .. (OG, Mish etc.)
             Charles MacDonald for his C2Emu .. without it working out what were bugs in my code
@@ -1423,8 +1423,11 @@ static INPUT_PORTS_START( ssonicbr )
 	PORT_DIPNAME( 0x01, 0x01, DEF_STR( Demo_Sounds ) ) PORT_DIPLOCATION("SW2:1")
 	PORT_DIPSETTING(    0x01, DEF_STR( On ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-	//"SW2:2" unused
-	//"SW2:3" unused
+	PORT_DIPNAME( 0x06, 0x06, DEF_STR( Difficulty ) ) PORT_DIPLOCATION("SW2:2,3")
+	PORT_DIPSETTING(    0x04, DEF_STR( Easy ) )
+	PORT_DIPSETTING(    0x06, DEF_STR( Medium ) )
+	PORT_DIPSETTING(    0x02, DEF_STR( Hard ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( Hardest ) )
 	//"SW2:4" unused
 	//"SW2:5" unused
 	//"SW2:6" unused
@@ -2168,7 +2171,7 @@ ROM_START( ooparts ) // hack: modified to be playable with digital joystick
 	ROM_LOAD16_BYTE( "ooparts.ic33", 0x100001, 0x080000, CRC(42755dc2) SHA1(cd0aa79418b922266c5d41bf24b9136f9f105dc5) )
 
 	ROM_REGION( 0x040000, "upd", 0 )
-	ROM_LOAD( "ooparts.ic4", 0x000000, 0x040000, NO_DUMP )
+	ROM_LOAD( "epr-13655.ic4", 0x000000, 0x040000, CRC(e09961f6) SHA1(e109b5f41502b765d191f22e3bbcff97d6defaa1) )
 ROM_END
 
 
