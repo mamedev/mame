f17fb8295f40933c3a6294ace23604e07f982f68
hap <happppp@users.noreply.github.com>
2021-09-03 14:18:04 +0200

    changela: bool(~BIT(x)) is always true (does not fix steering problem)

--- src/mame/drivers/changela.cpp
+++ src/mame/drivers/changela.cpp
@@ -49,7 +49,7 @@ void changela_state::changela_68705_port_a_w(uint8_t data)
 void changela_state::changela_68705_port_c_w(uint8_t data)
 {
 	/* PC3 is connected to the CLOCK input of the LS374, so we latch the data on rising edge */
-	if (BIT(data, 3) && ~BIT(m_port_c_out, 3))
+	if (BIT(data, 3) && !BIT(m_port_c_out, 3))
 		m_mcu_out = m_port_a_out & (BIT(m_port_c_out, 2) ? 0xff : m_mcu_in);
 
 	/* PC2 is connected to the /OE input of the LS374 */
