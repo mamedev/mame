dfa5b141d632c7fc1d1ab4f15e73ce234f0c7f27
0kmg <9137159+0kmg@users.noreply.github.com>
2021-09-03 12:35:05 -0800

    bus/nes: Improved emulation of N625092-related multi-game cartridges. (#8526)
    * Fixes corrupt graphics in multiple games on all seven multi-game carts (including the new addition).
    * Fixes flickering menu for several cartridges, making them usable.
    
    New working software list additions (nes.xml)
    -----------------------------------
    5000000 in 1 (JPx72) [anonymous]

--- src/devices/bus/nes/multigame.h
+++ src/devices/bus/nes/multigame.h
@@ -137,28 +137,6 @@ private:
 };
 
 
-// ======================> nes_n625092_device
-
-class nes_n625092_device : public nes_nrom_device
-{
-public:
-	// construction/destruction
-	nes_n625092_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
-
-	virtual void write_h(offs_t offset, uint8_t data) override;
-
-	virtual void pcb_reset() override;
-
-protected:
-	// device-level overrides
-	virtual void device_start() override;
-
-private:
-	void set_prg(uint8_t reg1, uint8_t reg2);
-	uint8_t m_latch1, m_latch2;
-};
-
-
 // ======================> nes_a65as_device
 
 class nes_a65as_device : public nes_nrom_device
@@ -1163,6 +1141,27 @@ public:
 	nes_bmc_k1029_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
 
 	virtual void write_h(offs_t offset, u8 data) override;
+};
+
+
+// ======================> nes_n625092_device
+
+class nes_n625092_device : public nes_vram_protect_device
+{
+public:
+	// construction/destruction
+	nes_n625092_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
+
+	virtual void write_h(offs_t offset, u8 data) override;
+
+	virtual void pcb_reset() override;
+
+protected:
+	// device-level overrides
+	virtual void device_start() override;
+
+private:
+	u16 m_latch[2];
 };
 
 
