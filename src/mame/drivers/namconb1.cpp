f5ba4ab9edc2b656646d4fb5ce92f7288eb241bd
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2021-10-19 19:51:04 +0100

    remove deco_zoomspr device, refactor deco32 - drgngun, lockload to use the namco_c355spr device instead

--- src/mame/drivers/namconb1.cpp
+++ src/mame/drivers/namconb1.cpp
@@ -995,6 +995,7 @@ void namconb1_state::namconb1(machine_config &config)
 	m_c355spr->set_palxor(0x0);
 	m_c355spr->set_buffer(2); // triple buffered
 	m_c355spr->set_color_base(0);
+	m_c355spr->set_draw_2_lists(false); // prevents bad tile on top left of vshoot during certain scenes
 
 	NAMCO_C123TMAP(config, m_c123tmap);
 	m_c123tmap->set_palette(m_c116);
