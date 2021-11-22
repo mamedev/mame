6ff40d0e515e7833f2c48c6fa880224b0af81825
algestam <henrik@algestam.se>
2021-11-22 22:30:47 +0100

    New working clone ----------------- Kot-rybolov [algestam, Milan Galcik]

--- src/mame/drivers/hh_sm510.cpp
+++ src/mame/drivers/hh_sm510.cpp
@@ -1390,10 +1390,14 @@ ROM_END
   (Razvedchiki kosmosa, export version: Explorers of Space).
 
   The following Mickey Mouse Elektronika clones are emulated in MAME:
-  * IM-02: Nu, pogodi! (Ну, погоди!)
-  * IM-13: Explorers of Space (Разведчики космоса)
-  * IM-16: Fowling (Охота)
-  * IM-22: Monkey Goalkeeper (Весёлые футболисты)
+
+  Model  Title               Transliteration      Export version      Note
+  ---------------------------------------------------------------------------------
+  IM-02  Ну, погоди!         Nu, pogodi!          -                   -
+  IM-13  Разведчики космоса  Razvedchiki kosmosa  Explorers of Space  Modified ROM (see note above)
+  IM-16  Охота               Okhota               Fowling             -
+  IM-22  Весёлые футболисты  Vesjolye futbolisty  Monkey Goalkeeper   -
+  IM-32  Кот-рыболов         Kot-rybolov          -                   -
 
 ***************************************************************************/
 
@@ -1410,6 +1414,7 @@ public:
 	void exospace(machine_config &config);
 	void fowling(machine_config &config);
 	void monkeygk(machine_config &config);
+	void krybolov(machine_config & config);
 };
 
 // config
@@ -1476,6 +1481,11 @@ void gnw_mmouse_state::monkeygk(machine_config &config)
 	kb1013vk12_common(config, 1655, 1080); // R mask option ?
 }
 
+void gnw_mmouse_state::krybolov(machine_config &config)
+{
+	kb1013vk12_common(config, 1638, 1080); // R mask option ?
+}
+
 // roms
 
 ROM_START( gnw_mmouse )
@@ -1524,6 +1534,14 @@ ROM_START( monkeygk )
 
 	ROM_REGION( 131901, "screen", 0)
 	ROM_LOAD( "monkeygk.svg", 0, 131901, CRC(85811308) SHA1(288aa41bade08c61e0d346b9c1109179564e34ed) )
+ROM_END
+
+ROM_START( krybolov )
+	ROM_REGION( 0x1000, "maincpu", 0 )
+	ROM_LOAD( "im-32.bin", 0x0000, 0x0740, CRC(cb820c32) SHA1(7e94fc255f32db725d5aa9e196088e490c1a1443) )
+
+	ROM_REGION( 132804, "screen", 0)
+	ROM_LOAD( "krybolov.svg", 0, 132804, CRC(4e3e70d3) SHA1(18f1300afa601deb6ac01dcf7dca88187b7940a3) )
 ROM_END
 
 
@@ -9576,6 +9594,7 @@ CONS( 1984, nupogodi,     gnw_mmouse,  0, nupogodi,     gnw_mmouse,   gnw_mmouse
 CONS( 1989, exospace,     gnw_mmouse,  0, exospace,     exospace,     gnw_mmouse_state,   empty_init, "bootleg (Elektronika)", "Explorers of Space", MACHINE_SUPPORTS_SAVE | MACHINE_REQUIRES_ARTWORK )
 CONS( 1989, fowling,      gnw_mmouse,  0, fowling,      gnw_mmouse,   gnw_mmouse_state,   empty_init, "bootleg (Elektronika)", "Fowling", MACHINE_SUPPORTS_SAVE | MACHINE_REQUIRES_ARTWORK )
 CONS( 1989, monkeygk,     gnw_mmouse,  0, monkeygk,     gnw_mmouse,   gnw_mmouse_state,   empty_init, "bootleg (Elektronika)", "Monkey Goalkeeper", MACHINE_SUPPORTS_SAVE | MACHINE_REQUIRES_ARTWORK )
+CONS( 1989, krybolov,     gnw_mmouse,  0, krybolov,     gnw_mmouse,   gnw_mmouse_state,   empty_init, "bootleg (Elektronika)", "Kot-rybolov", MACHINE_SUPPORTS_SAVE | MACHINE_REQUIRES_ARTWORK )
 CONS( 1981, gnw_fire,     0,           0, gnw_fire,     gnw_fire,     gnw_fire_state,     empty_init, "Nintendo", "Game & Watch: Fire (Wide Screen)", MACHINE_SUPPORTS_SAVE | MACHINE_REQUIRES_ARTWORK )
 CONS( 1989, spacebridge,  gnw_fire,    0, spacebridge,  gnw_fire,     gnw_fire_state,     empty_init, "bootleg (Elektronika)", "Space Bridge", MACHINE_SUPPORTS_SAVE | MACHINE_REQUIRES_ARTWORK )
 CONS( 1982, gnw_tbridge,  0,           0, gnw_tbridge,  gnw_tbridge,  gnw_tbridge_state,  empty_init, "Nintendo", "Game & Watch: Turtle Bridge", MACHINE_SUPPORTS_SAVE | MACHINE_REQUIRES_ARTWORK )
