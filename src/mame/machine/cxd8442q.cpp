61bbba91c92fc01f960955ba738d2a0a6134d858
Brice Onken <32155223+briceonk@users.noreply.github.com>
2021-11-24 22:57:34 -0800

    srcclean

--- src/mame/machine/cxd8442q.cpp
+++ src/mame/machine/cxd8442q.cpp
@@ -46,7 +46,7 @@ namespace
 	static constexpr int DMA_TIMER = 1;
 }
 
-cxd8442q_device::cxd8442q_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) 
+cxd8442q_device::cxd8442q_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
 	: device_t(mconfig, CXD8442Q, tag, owner, clock), out_irq(*this),
 	fifo_channels{apfifo_channel(*this), apfifo_channel(*this), apfifo_channel(*this), apfifo_channel(*this)}
 {
