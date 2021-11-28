8f95b1df3e12b8d744879346441718252e93b99a
Sergey Svishchev <shattered@users.noreply.github.com>
2021-11-28 12:14:45 +0300

    dl11: DEC DL11-type serial line unit

---
+++ src/devices/machine/dl11.h
@@ -0,0 +1,103 @@
+// license:BSD-3-Clause
+// copyright-holders:Sergey Svishchev
+/***************************************************************************
+
+	DEC DL11-type SLU (serial line unit)
+
+***************************************************************************/
+
+#ifndef MAME_MACHINE_DL11_H
+#define MAME_MACHINE_DL11_H
+
+#pragma once
+
+#include "machine/z80daisy.h"
+#include "machine/pdp11.h"
+#include "diserial.h"
+
+
+/* registers */
+
+#define DLRCSR_RD       (CSR_DONE|CSR_IE)
+#define DLRCSR_WR       (CSR_IE)
+
+#define DLRBUF_ERR      0100000
+#define DLRBUF_OVR      0040000
+#define DLRBUF_RBRK     0020000
+#define DLRBUF_PERR     0020000
+
+#define DLTCSR_XBRK     0000001
+#define DLTCSR_RD       (CSR_DONE|CSR_IE|DLTCSR_XBRK)
+#define DLTCSR_WR       (CSR_IE|DLTCSR_XBRK)
+
+
+/***************************************************************************
+    TYPE DEFINITIONS
+***************************************************************************/
+
+// ======================> dl11_device
+
+class dl11_device : public device_t,
+					public device_serial_interface,
+					public device_z80daisy_interface
+{
+public:
+	// construction/destruction
+	dl11_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+	void set_rxc(int clock) { m_rxc = clock; }
+	void set_txc(int clock) { m_txc = clock; }
+	void set_rxvec(int vec) { m_rxvec = vec; }
+	void set_txvec(int vec) { m_txvec = vec; }
+
+	auto txd_wr_callback() { return m_write_txd.bind(); }
+	auto txrdy_wr_callback() { return m_write_txrdy.bind(); }
+	auto rxrdy_wr_callback() { return m_write_rxrdy.bind(); }
+
+	uint16_t read(offs_t offset);
+	void write(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+
+	DECLARE_READ_LINE_MEMBER( rxrdy_r );
+	DECLARE_READ_LINE_MEMBER( txrdy_r );
+
+	DECLARE_WRITE_LINE_MEMBER( rx_w ) { device_serial_interface::rx_w(state); }
+
+protected:
+	// device-level overrides
+	virtual void device_start() override;
+	virtual void device_reset() override;
+
+	// device_serial_interface overrides
+	virtual void tra_callback() override;
+	virtual void tra_complete() override;
+	virtual void rcv_complete() override;
+
+	// device_z80daisy_interface overrides
+	virtual int z80daisy_irq_state() override;
+	virtual int z80daisy_irq_ack() override;
+	virtual void z80daisy_irq_reti() override;
+
+private:
+	devcb_write_line   m_write_txd;
+	devcb_write_line   m_write_rxrdy;
+	devcb_write_line   m_write_txrdy;
+
+	line_state	m_rxrdy;
+	line_state	m_txrdy;
+
+	int m_rxc;
+	int m_txc;
+	int m_rxvec;
+	int m_txvec;
+
+	uint16_t m_rcsr;
+	uint16_t m_rbuf;
+	uint16_t m_tcsr;
+	uint16_t m_tbuf;
+};
+
+
+// device type definition
+DECLARE_DEVICE_TYPE(DL11, dl11_device)
+
+#endif
