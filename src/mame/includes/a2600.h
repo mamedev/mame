682974c0d6d7bc1dbdff98d8f53ceaebda4d2041
wilbertpol <wilbertpol@users.noreply.github.com>
2021-12-14 15:33:40 +0000

    New machines marked as NOT_WORKING (#8889)
    New machines marked as NOT_WORKING
    ----------------------------------
    Atari 2600 Point of Purchase Display Unit [Wilbert Pol]

--- src/mame/includes/a2600.h
+++ src/mame/includes/a2600.h
@@ -52,6 +52,8 @@ protected:
 
 	virtual void machine_start() override;
 
+	void a2600_base_ntsc(machine_config &config);
+
 	void switch_A_w(uint8_t data);
 	uint8_t switch_A_r();
 	void switch_B_w(uint8_t data);
@@ -106,5 +108,39 @@ protected:
 private:
 	required_device<vcs_cart_slot_device> m_cart;
 };
+
+
+class a2600_pop_state : public a2600_base_state
+{
+public:
+	a2600_pop_state(const machine_config &mconfig, device_type type, const char *tag)
+		: a2600_base_state(mconfig, type, tag)
+		, m_bank(*this, "bank")
+		, m_a8(*this, "A8")
+		, m_swb(*this, "SWB")
+	{ }
+
+	void a2600_pop(machine_config &config);
+
+protected:
+	virtual void machine_start() override;
+	virtual void machine_reset() override;
+
+private:
+	void memory_map(address_map &map);
+	void banked_map(address_map &map);
+
+	uint8_t rom_switch_r(offs_t offset);
+	void rom_switch_w(offs_t offset, uint8_t data);
+	TIMER_CALLBACK_MEMBER(reset_timer_callback);
+	TIMER_CALLBACK_MEMBER(game_select_button_timer_callback);
+
+	required_memory_bank m_bank;
+	required_ioport m_a8;
+	required_ioport m_swb;
+	emu_timer *m_reset_timer;
+	emu_timer *m_game_select_button_timer;
+};
+
 
 #endif // MAME_INCLUDES_A2600_H
