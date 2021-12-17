aba3777d81f3f0376b12d228305cde903b970bab
TellowKrinkle <tellowkrinkle@gmail.com>
2021-12-16 20:26:32 -0600

    osd: Remove 4-thread limit from non-high-freq work queues
    Gives chdman access to all cores

--- src/osd/osdsync.cpp
+++ src/osd/osdsync.cpp
@@ -85,14 +85,15 @@ static void spin_while_not(const volatile _AtomType * volatile atom, const _Main
 //  osd_num_processors
 //============================================================
 
-int osd_get_num_processors()
+int osd_get_num_processors(bool heavy_mt)
 {
 #if defined(SDLMAME_EMSCRIPTEN)
 	// multithreading is not supported at this time
 	return 1;
 #else
+	unsigned int threads = std::thread::hardware_concurrency();
 	// max out at 4 for now since scaling above that seems to do poorly
-	return std::min(std::thread::hardware_concurrency(), 4U);
+	return heavy_mt ? threads : std::min(std::thread::hardware_concurrency(), 4U);
 #endif
 }
 
@@ -211,7 +212,7 @@ int osd_num_processors = 0;
 //  FUNCTION PROTOTYPES
 //============================================================
 
-static int effective_num_processors();
+static int effective_num_processors(bool heavy_mt);
 static void * worker_thread_entry(void *param);
 static void worker_thread_process(osd_work_queue *queue, work_thread_info *thread);
 static bool queue_has_list_items(osd_work_queue *queue);
@@ -251,7 +252,7 @@ int thread_adjust_priority(std::thread *thread, int adjust)
 osd_work_queue *osd_work_queue_alloc(int flags)
 {
 	int threadnum;
-	int numprocs = effective_num_processors();
+	int numprocs = effective_num_processors(!(flags & WORK_QUEUE_FLAG_HIGH_FREQ));
 	osd_work_queue *queue;
 	int osdthreadnum = 0;
 	int allocthreadnum;
@@ -639,9 +640,9 @@ void osd_work_item_release(osd_work_item *item)
 //  effective_num_processors
 //============================================================
 
-static int effective_num_processors()
+static int effective_num_processors(bool heavy_mt)
 {
-	int physprocs = osd_get_num_processors();
+	int physprocs = osd_get_num_processors(heavy_mt);
 
 	// osd_num_processors == 0 for 'auto'
 	if (osd_num_processors > 0)
