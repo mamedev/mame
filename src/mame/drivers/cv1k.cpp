56737283cf24b6144d0fd2846d6565c831f30ac1
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2021-12-30 16:15:17 +0000

    sync with master

--- src/mame/drivers/cv1k.cpp
+++ src/mame/drivers/cv1k.cpp
@@ -906,20 +906,34 @@ ROM_START( akatana )
 	ROM_LOAD16_WORD_SWAP( "u24", 0x400000, 0x400000, CRC(10760fed) SHA1(b70f4506c00f3901ff38f5efd4b897af1afc7a0c) )
 ROM_END
 
-
-ROM_START( sdojbl )
+ROM_START( ddpsdoj )
 	ROM_REGION( 0x400000, "maincpu", ROMREGION_ERASEFF)
-	ROM_LOAD16_WORD_SWAP( "sdojak.u4", 0x000000, 0x400000, CRC(a878ff4c) SHA1(cf5f2f987a775099992009c376466c44170e0e3f) )
+	ROM_LOAD16_WORD_SWAP("u4", 0x000000, 0x400000, CRC(e2a4411c) SHA1(f8b2b6326dd8eeae99b8b1ab2bd5a3f0b9c7f027) )
+	ROM_IGNORE( 0x000100 ) // Flash extra bytes
 
 	ROM_REGION( 0x8400000, "game", ROMREGION_ERASEFF)
-	ROM_LOAD( "sdojak.u2", 0x000000, 0x8400000, CRC(54353425) SHA1(5c749cd2e93a1b51bfdf1a0d4fd436b4d17acf9f) )
+	ROM_LOAD("u2", 0x000000, 0x8400000, CRC(668e4cd6) SHA1(da0b10865df3a3e46cf8a109ca88a551faba4483) )
 
 	ROM_REGION( 0x800000, "ymz770", ROMREGION_ERASEFF)
-	ROM_LOAD16_WORD_SWAP( "sdojak.u23", 0x000000, 0x400000, CRC(32b91544) SHA1(49e914bfdf33a328fbfa7efb42c5b89915e37643) )
-	ROM_LOAD16_WORD_SWAP( "sdojak.u24", 0x400000, 0x400000, CRC(7b9e749f) SHA1(374085a60098cc62501abae4d148bfd88c88149f) )
+	ROM_LOAD16_WORD_SWAP("u23", 0x000000, 0x400000, CRC(ac94801c) SHA1(cbcc6d5d89860bc961967e1d3b7c329adaf200c5) )
+	ROM_IGNORE( 0x000100 ) // Flash extra bytes
+	ROM_LOAD16_WORD_SWAP("u24", 0x400000, 0x400000, CRC(f593045b) SHA1(91b92870d0dd2a7817cb0059cc750e2393686f4c) )
+	ROM_IGNORE( 0x000100 ) // Flash extra bytes
 ROM_END
 
-ROM_START( sdojbla )
+ROM_START( ddpsdojbl )
+	ROM_REGION( 0x400000, "maincpu", ROMREGION_ERASEFF)
+	ROM_LOAD16_WORD_SWAP( "u4", 0x000000, 0x400000, CRC(a878ff4c) SHA1(cf5f2f987a775099992009c376466c44170e0e3f) )
+
+	ROM_REGION( 0x8400000, "game", ROMREGION_ERASEFF)
+	ROM_LOAD( "u2", 0x000000, 0x8400000, CRC(54353425) SHA1(5c749cd2e93a1b51bfdf1a0d4fd436b4d17acf9f) )
+
+	ROM_REGION( 0x800000, "ymz770", ROMREGION_ERASEFF)
+	ROM_LOAD16_WORD_SWAP( "u23", 0x000000, 0x400000, CRC(32b91544) SHA1(49e914bfdf33a328fbfa7efb42c5b89915e37643) )
+	ROM_LOAD16_WORD_SWAP( "u24", 0x400000, 0x400000, CRC(7b9e749f) SHA1(374085a60098cc62501abae4d148bfd88c88149f) )
+ROM_END
+
+ROM_START( ddpsdojbla )
 	ROM_REGION( 0x400000, "maincpu", ROMREGION_ERASEFF)
 	ROM_LOAD16_WORD_SWAP( "u4", 0x000000, 0x400000, CRC(5a3b94e8) SHA1(a546f2baa366ca02dbd462b8ee7808c2643927cb) )
 
@@ -927,10 +941,9 @@ ROM_START( sdojbla )
 	ROM_LOAD( "u2", 0x000000, 0x8400000, CRC(7e19adc7) SHA1(6a6e2220096c80471d391d93979e0c883f733c1b) )
 
 	ROM_REGION( 0x800000, "ymz770", ROMREGION_ERASEFF)
-	ROM_LOAD16_WORD_SWAP( "sdojak.u23", 0x000000, 0x400000, CRC(32b91544) SHA1(49e914bfdf33a328fbfa7efb42c5b89915e37643) )
-	ROM_LOAD16_WORD_SWAP( "sdojak.u24", 0x400000, 0x400000, CRC(7b9e749f) SHA1(374085a60098cc62501abae4d148bfd88c88149f) )
+	ROM_LOAD16_WORD_SWAP( "u23", 0x000000, 0x400000, CRC(32b91544) SHA1(49e914bfdf33a328fbfa7efb42c5b89915e37643) )
+	ROM_LOAD16_WORD_SWAP( "u24", 0x400000, 0x400000, CRC(7b9e749f) SHA1(374085a60098cc62501abae4d148bfd88c88149f) )
 ROM_END
-
 
 uint64_t cv1k_state::speedup_r()
 {
@@ -1055,10 +1068,12 @@ GAME( 2010, dfkbl,      0,        cv1k_d, cv1k, cv1k_state, init_ddpdfk,   ROT27
 // CA021  Akai Katana
 GAME( 2010, akatana,    0,        cv1k_d, cv1k, cv1k_state, init_ddpdfk,   ROT0,   "Cave",               "Akai Katana (2010/ 8/13 MASTER VER.)",                            MACHINE_IMPERFECT_TIMING )
 
+// CA??? DoDonPachi SaiDaiOuJou
+GAME( 2012, ddpsdoj,    0,        cv1k_d, cv1k, cv1k_state, init_ddpdfk,   ROT270, "Cave",               "DoDonPachi SaiDaiOuJou (2012/ 4/20)",                             MACHINE_IMPERFECT_TIMING )
+// These bootleg sets have been seen on converted PCBs.  The former set is an obvious defacement of the original game, while the 'dehack' set is a 'scam' set designed to
+// make that defacement less obvious and the game appear original by removing some hacked elements entirely and restoring other assets using resources from the Xbox 360 port.
+GAME( 2021, ddpsdojbl,  ddpsdoj,  cv1k_d, cv1k, cv1k_state, init_ddpdfk,   ROT270, "bootleg",            "DoDonPachi SaiDaiOuJou ('& Knuckles' bootleg)",                   MACHINE_IMPERFECT_TIMING )
+GAME( 2021, ddpsdojbla, ddpsdoj,  cv1k_d, cv1k, cv1k_state, init_ddpdfk,   ROT270, "bootleg",            "DoDonPachi SaiDaiOuJou ('& Knuckles' bootleg, 'dehack' hack)",    MACHINE_IMPERFECT_TIMING )
+
 // CMDL01 Medal Mahjong Moukari Bancho
 GAME( 2007, mmmbanc,    0,        cv1k,   cv1k, cv1k_state, init_pinkswts, ROT0,   "Cave (AMI license)", "Medal Mahjong Moukari Bancho (2007/06/05 MASTER VER.)",           MACHINE_NOT_WORKING )
-
-// CA??? DoDonPachi SaiDaiOuJou
-// the original set is not currently supported, these bootlegs are being used on converted PCBs.
-GAME( 2021, sdojbl,     0,        cv1k_d, cv1k, cv1k_state, init_ddpdfk,   ROT270, "bootleg",            "DoDonPachi SaiDaiOuJou ('& Knuckles' bootleg)",                   MACHINE_IMPERFECT_TIMING )
-GAME( 2021, sdojbla,    sdojbl,   cv1k_d, cv1k, cv1k_state, init_ddpdfk,   ROT270, "bootleg",            "DoDonPachi SaiDaiOuJou ('& Knuckles' bootleg, 'dehack' hack)",    MACHINE_IMPERFECT_TIMING )
