0c8d9011950dd4af80a2401a64749eb0d27ec769
Dirk Best <mail@dirk-best.de>
2022-01-16 15:49:04 +0100

    falco500: Rewrite driver
    - Move from falco5220.cpp to falco500.cpp
    - Implement many ASIC features and support most video options
    - Fully hook up RS232 ports
    - Hook up bell
    - Add keyboard
    - Add detailed hardware descriptions for 3 models
    
    New machines marked as NOT_WORKING
    ----------------------------------
    Falco 500e [Bitsavers]

--- scripts/target/mame/mess.lua
+++ scripts/target/mame/mess.lua
@@ -2471,7 +2471,9 @@ files {
 
 createMESSProjects(_target, _subtarget, "falco")
 files {
-	MAME_DIR .. "src/mame/drivers/falco5220.cpp",
+	MAME_DIR .. "src/mame/drivers/falco500.cpp",
+	MAME_DIR .. "src/mame/machine/f5220_kbd.cpp",
+	MAME_DIR .. "src/mame/machine/f5220_kbd.h",
 	MAME_DIR .. "src/mame/drivers/falcots.cpp",
 	MAME_DIR .. "src/mame/drivers/falcots28.cpp",
 }
