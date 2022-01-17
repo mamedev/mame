5342d0e7f5d6d804cfed3be1290a8327b68a2e96
Angelo Salese <angelosa@users.noreply.github.com>
2022-01-17 23:30:04 +0100

    Fix several issues in DC-based HW (#9150)
    - Streamlined logging across chips to use logmacro.h, removed popmessages and printfs in production code;
    - Add missing include guards in header files;
    - split Atomiswave into own file from naomi.cpp (dc_atomiswave.cpp);
    - powervr2.cpp: YUV pitch follows U size, fixes #8999
    - powervr2.cpp: move Elan related stuff back into naomi2_state;
    - naomi.cpp: support for player 2 keyboard inputs;
    - naomi.cpp: add proper inputs to alpilot/alpilotj and sstrkfgt;
    - naomi.cpp: hookup lightgun for deathcox;
    - naomi.cpp: make G2-DMA timings slightly more accurate (fixes sfz3ugd silent BGMs regression caused by implicit insta-DMAs)
    - dc_g2if.cpp: wrote a device for G2 DMA interface, fixes #9000 ;
    - dc_g2if.cpp: add E1/E2/DD channels thru template, add area protection, add illegal address and overflow exceptions (fixes loopchk g2 bus tests 0302 and 0303)
    - aica.cpp: $2814 CA reads doesn't need shifting, fixes ADX repeating sample/hang bugs in many entries [Angelo Salese, MetalliC];
    - powervr2.cpp: make ISP/TSP irq to be slower, fixes regression bug with Capcom fighters having frame hiccups [David Haywood, Angelo Salese]
    - dc_atomiswave.cpp: converted aw_modem_r/_w to 32-bit, added 3p/4p inputs to ggisuka, expose EXID to an input/output ioports for future extensions;
    - dc_atomiswave.cpp: extend xtrmhnt2 ALL.Net hack for -drc, move around state machine to derive ALL.Net external device mapping instead of driver_init fn, demote to MUP for obvious reasons;
    - dc_atomiswave: add Area 1 mirrors, fix maxspeed title screen animation;
    - [MT#8143](https://mametesters.org/view.php?id=8143) is fixed
    
    New NOT_WORKING software list additions
    ---------------------------------------
    dc.xml: DC Checker for Repair v2.05R (World), Loop Checker v1.00 (World), GD Drive Repair Program v0.1 (World) [Hidden Palace]

---
+++ src/mame/includes/dc_atomiswave.h
@@ -0,0 +1,69 @@
+// license:LGPL-2.1+
+// copyright-holders: Samuele Zannoli, R. Belmont, ElSemi, David Haywood, Angelo Salese, Olivier Galibert, MetalliC
+
+#ifndef MAME_INCLUDES_DC_ATOMISWAVE_H
+#define MAME_INCLUDES_DC_ATOMISWAVE_H
+
+#pragma once
+
+#include "machine/awboard.h"
+#include "machine/intelfsh.h"
+#include "machine/dc-ctrl.h"
+#include "machine/nvram.h"
+#include "machine/aicartc.h"
+//#include "machine/m3comm.h"
+//#include "machine/gunsense.h"
+//#include "machine/segashiobd.h"
+//#include "sound/aica.h"
+#include "dc.h"
+//#include "naomi.h"
+
+class atomiswave_state : public dc_state
+{
+public:
+	atomiswave_state(const machine_config &mconfig, device_type type, const char *tag)
+		: dc_state(mconfig, type, tag)
+		, m_awflash(*this, "awflash")
+		, m_exid_in(*this, "EXID_IN")
+		, m_exid_out(*this, "EXID_OUT")
+	{
+		m_aw_ctrl_type = 0xf0;
+	}
+
+	void aw_base(machine_config &config);
+	void aw1c(machine_config &config);
+	void aw2c(machine_config &config);
+	void aw4c(machine_config &config);
+
+	void init_atomiswave();
+
+protected:
+	virtual void aw_map(address_map &map);
+	void aw_port(address_map &map);
+
+private:
+	required_device<macronix_29l001mc_device> m_awflash;
+	optional_ioport m_exid_in;
+	optional_ioport m_exid_out;
+
+	uint32_t aw_modem_r(offs_t offset, uint32_t mem_mask = ~0);
+	void aw_modem_w(offs_t offset, uint32_t data, uint32_t mem_mask = ~0);
+
+	u8 m_aw_ctrl_type;
+//	inline int decode_reg32_64(uint32_t offset, uint64_t mem_mask, uint64_t *shift);
+};
+
+class atomiswave_xtrmhnt2_state : public atomiswave_state
+{
+public:
+	atomiswave_xtrmhnt2_state(const machine_config &mconfig, device_type type, const char *tag)
+		: atomiswave_state(mconfig, type, tag)
+	   { }
+
+protected:
+	virtual void aw_map(address_map &map) override;
+private:
+	uint64_t allnet_hack_r(offs_t offset, uint64_t mem_mask = ~0);
+};
+
+#endif // MAME_INCLUDES_DC_ATOMISWAVE_H
