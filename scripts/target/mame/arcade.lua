7c1ec79705837675f6a3960eb5ebd43eb008f0f0
ClawGrip <clawgrip@hotmail.com>
2022-01-21 08:51:47 +0100

    New NOT_WORKING machine (#9178)
    New machines marked as NOT_WORKING
    -----------------------
    Neo Mania [ClawGrip, CrispX]

--- scripts/target/mame/arcade.lua
+++ scripts/target/mame/arcade.lua
@@ -5035,6 +5035,7 @@ files {
 	MAME_DIR .. "src/mame/drivers/multfish_boot.cpp",
 	MAME_DIR .. "src/mame/drivers/murogem.cpp",
 	MAME_DIR .. "src/mame/drivers/murogmbl.cpp",
+	MAME_DIR .. "src/mame/drivers/neomania.cpp",
 	MAME_DIR .. "src/mame/drivers/neoprint.cpp",
 	MAME_DIR .. "src/mame/drivers/news.cpp",
 	MAME_DIR .. "src/mame/includes/news.h",
