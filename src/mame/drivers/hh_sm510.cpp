c878e8ef5fed325ec410c1cb9390f099d38b09a3
hap <happppp@users.noreply.github.com>
2022-01-31 14:53:44 +0100

    m62: shrink io memory maps

--- src/mame/drivers/hh_sm510.cpp
+++ src/mame/drivers/hh_sm510.cpp
@@ -137,7 +137,6 @@ The MCUs used were not imported from Sharp, but cloned by USSR, renamed to
 #include "emu.h"
 #include "includes/hh_sm510.h"
 
-#include "cpu/sm510/sm500.h"
 #include "screen.h"
 #include "speaker.h"
 
