f6053b1e7d0b372ac24cfecdd07af4d973473ec4
shattered <shattered@users.noreply.github.com>
2022-02-15 16:25:09 +0000

    Minor code cleanup; moved 'krokha' to arcade section. (#9262)

--- src/mame/drivers/ec184x.cpp
+++ src/mame/drivers/ec184x.cpp
@@ -30,7 +30,7 @@
 #define LOG_DEBUG     (1U <<  2)
 
 //#define VERBOSE (LOG_DEBUG)
-//#define LOG_OUTPUT_FUNC printf
+//#define LOG_OUTPUT_FUNC osd_printf_info
 #include "logmacro.h"
 
 #define LOGKBD(...) LOGMASKED(LOG_KEYBOARD, __VA_ARGS__)
