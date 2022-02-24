f5b221339ac7fd97457428b1f898b4e2d2d7cf29
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2022-02-24 21:21:06 +0000

    stub some accesses so we can get an idea of memory areas

--- src/mame/drivers/generalplus_gpl_unknown.cpp
+++ src/mame/drivers/generalplus_gpl_unknown.cpp
@@ -61,10 +61,30 @@ private:
 	required_device<screen_device> m_screen;
 	required_region_ptr<uint16_t> m_spirom;
 
+	uint16_t reg3001_r(offs_t offset);
+	void reg3001_w(offs_t offset, uint16_t data);
+	uint16_t reg3002_r(offs_t offset);
+	void reg3002_w(offs_t offset, uint16_t data);
+	uint16_t reg3003_r(offs_t offset);
+	void reg3003_w(offs_t offset, uint16_t data);
 	uint16_t reg3004_r(offs_t offset);
+	uint16_t reg3005_r(offs_t offset);
+	void reg3005_w(offs_t offset, uint16_t data);
 
 	void reg3034_w(offs_t offset, uint16_t data);
+	void reg3041_w(offs_t offset, uint16_t data);
+
+	uint16_t reg3050_r(offs_t offset);
+	void reg3050_w(offs_t offset, uint16_t data);
+
 	void reg3051_w(offs_t offset, uint16_t data);
+	void reg3092_w(offs_t offset, uint16_t data);
+
+	uint16_t reg3094_r(offs_t offset);
+	uint16_t reg3095_r(offs_t offset);
+
+	uint16_t reg30e4_r(offs_t offset);
+	uint16_t reg30e5_r(offs_t offset);
 
 	TIMER_DEVICE_CALLBACK_MEMBER(scanline);
 
@@ -80,10 +100,51 @@ uint32_t generalplus_gpl_unknown_state::screen_update(screen_device &screen, bit
 static INPUT_PORTS_START( generalplus_gpl_unknown )
 INPUT_PORTS_END
 
+uint16_t generalplus_gpl_unknown_state::reg3001_r(offs_t offset)
+{
+	return machine().rand();
+}
+
+void generalplus_gpl_unknown_state::reg3001_w(offs_t offset, uint16_t data)
+{
+	//logerror("%s: reg3001_w %04x\n", machine().describe_context(), data);
+}
+
+uint16_t generalplus_gpl_unknown_state::reg3002_r(offs_t offset)
+{
+	return machine().rand();
+}
+
+void generalplus_gpl_unknown_state::reg3002_w(offs_t offset, uint16_t data)
+{
+	//logerror("%s: reg3002_w %04x\n", machine().describe_context(), data);
+}
+
+uint16_t generalplus_gpl_unknown_state::reg3003_r(offs_t offset)
+{
+	return machine().rand();
+}
+
+void generalplus_gpl_unknown_state::reg3003_w(offs_t offset, uint16_t data)
+{
+	//logerror("%s: reg3003_w %04x\n", machine().describe_context(), data);
+}
+
 uint16_t generalplus_gpl_unknown_state::reg3004_r(offs_t offset)
 {
 	return machine().rand();
 }
+
+uint16_t generalplus_gpl_unknown_state::reg3005_r(offs_t offset)
+{
+	return machine().rand();
+}
+
+void generalplus_gpl_unknown_state::reg3005_w(offs_t offset, uint16_t data)
+{
+	//logerror("%s: reg3005_w %04x\n", machine().describe_context(), data);
+}
+
 
 void generalplus_gpl_unknown_state::reg3034_w(offs_t offset, uint16_t data)
 {
@@ -91,8 +152,52 @@ void generalplus_gpl_unknown_state::reg3034_w(offs_t offset, uint16_t data)
 	//logerror("%s: reg3034_w %04x\n", machine().describe_context(), data);
 }
 
+void generalplus_gpl_unknown_state::reg3041_w(offs_t offset, uint16_t data)
+{
+	//logerror("%s: reg3041_w %04x\n", machine().describe_context(), data);
+}
+
+uint16_t generalplus_gpl_unknown_state::reg3050_r(offs_t offset)
+{
+	return machine().rand();
+}
+
+void generalplus_gpl_unknown_state::reg3050_w(offs_t offset, uint16_t data)
+{
+	//logerror("%s: reg3050_w %04x\n", machine().describe_context(), data);
+}
+
+uint16_t generalplus_gpl_unknown_state::reg30e4_r(offs_t offset)
+{
+	return machine().rand();
+}
+
+uint16_t generalplus_gpl_unknown_state::reg30e5_r(offs_t offset)
+{
+	return machine().rand();
+}
+
+/*
+mapacman vectors
+
+004015: 4844        - just reti
+        4845 fiq    - has code, acks by writing 8000 to 3051, very similar to irq0?)
+        4809 boot vector
+        4854 irq 0  - has code, acks by writing 8000 to 3051
+        4863 irq 1  - just acks by writing 4000 to 3051, no payload
+        486A irq 2 (used - needed to pass check on value in RAM changing in boot) acks with 2000 to 3051
+        487D irq 3 (used - needed to pass check on value in RAM changing shortly after) acks with 0100 to 3051?
+        4886 irq 4 - just acks by writing 10 to 3051, no payload 
+        488C irq 5 - push/pop/return
+        488F irq 6 - push/pop/return 
+        4892 irq 7 - minimal payload, no 3051 ack?
+*/
+
 void generalplus_gpl_unknown_state::reg3051_w(offs_t offset, uint16_t data)
 {
+	if (data & 0x8000)
+		m_maincpu->set_input_line(UNSP_IRQ0_LINE, CLEAR_LINE);
+
 	if (data & 0x2000)
 		m_maincpu->set_input_line(UNSP_IRQ2_LINE, CLEAR_LINE);
 
@@ -103,6 +208,21 @@ void generalplus_gpl_unknown_state::reg3051_w(offs_t offset, uint16_t data)
 	logerror("%s: reg3051_w %04x (IRQ Ack?)\n", machine().describe_context(), data);
 }
 
+void generalplus_gpl_unknown_state::reg3092_w(offs_t offset, uint16_t data)
+{
+	//logerror("%s: reg3092_w %04x (Video?)\n", machine().describe_context(), data);
+}
+
+uint16_t generalplus_gpl_unknown_state::reg3094_r(offs_t offset)
+{
+	return machine().rand();
+}
+
+uint16_t generalplus_gpl_unknown_state::reg3095_r(offs_t offset)
+{
+	return machine().rand();
+}
+
 
 void generalplus_gpl_unknown_state::map(address_map &map)
 {
@@ -110,11 +230,25 @@ void generalplus_gpl_unknown_state::map(address_map &map)
 	//map(0x001000, 0x0017ff).ram(); // acts like open bus?
 
 	//map(0x003000, 0x003fff).ram(); // system regs
+	map(0x003001, 0x003001).rw(FUNC(generalplus_gpl_unknown_state::reg3001_r), FUNC(generalplus_gpl_unknown_state::reg3001_w));
+	map(0x003002, 0x003002).rw(FUNC(generalplus_gpl_unknown_state::reg3002_r), FUNC(generalplus_gpl_unknown_state::reg3002_w));
+	map(0x003003, 0x003003).rw(FUNC(generalplus_gpl_unknown_state::reg3003_r), FUNC(generalplus_gpl_unknown_state::reg3003_w));
 	map(0x003004, 0x003004).r(FUNC(generalplus_gpl_unknown_state::reg3004_r));
+	map(0x003005, 0x003005).rw(FUNC(generalplus_gpl_unknown_state::reg3005_r), FUNC(generalplus_gpl_unknown_state::reg3005_w));
 
 	map(0x003034, 0x003034).w(FUNC(generalplus_gpl_unknown_state::reg3034_w));
 
+	map(0x003041, 0x003041).w(FUNC(generalplus_gpl_unknown_state::reg3041_w));
+
+	map(0x003050, 0x003050).rw(FUNC(generalplus_gpl_unknown_state::reg3050_r), FUNC(generalplus_gpl_unknown_state::reg3050_w));
 	map(0x003051, 0x003051).w(FUNC(generalplus_gpl_unknown_state::reg3051_w));
+
+	map(0x003092, 0x003092).w(FUNC(generalplus_gpl_unknown_state::reg3092_w));
+	map(0x003094, 0x003094).r(FUNC(generalplus_gpl_unknown_state::reg3094_r));
+	map(0x003095, 0x003095).r(FUNC(generalplus_gpl_unknown_state::reg3095_r));
+
+	map(0x0030e4, 0x0030e4).r(FUNC(generalplus_gpl_unknown_state::reg30e4_r));
+	map(0x0030e5, 0x0030e5).r(FUNC(generalplus_gpl_unknown_state::reg30e5_r));
 
 	map(0x004000, 0x00bfff).rom().region("maincpu", 0x0000);
 	map(0x00c000, 0x00ffff).rom().region("maincpu", 0x0000);
@@ -139,6 +273,11 @@ TIMER_DEVICE_CALLBACK_MEMBER(generalplus_gpl_unknown_state::scanline)
 	if (scanline == 50)
 	{
 		m_maincpu->set_input_line(UNSP_IRQ2_LINE, ASSERT_LINE);
+	}
+
+	if (scanline == 90)
+	{
+		m_maincpu->set_input_line(UNSP_IRQ0_LINE, ASSERT_LINE);
 	}
 
 	if (scanline == 126)
