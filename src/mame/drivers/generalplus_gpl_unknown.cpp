a554a4ffed7137c231ff78056483379b924a8a4c
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2022-02-24 20:14:33 +0000

    added internal ROM for mapacman [Sean Riddle, stefo, jrideburg, Team Europe, f205v, Osso1]

--- src/mame/drivers/generalplus_gpl_unknown.cpp
+++ src/mame/drivers/generalplus_gpl_unknown.cpp
@@ -58,7 +58,7 @@ private:
 
 	required_device<palette_device> m_palette;
 	required_device<screen_device> m_screen;
-	required_region_ptr<uint8_t> m_spirom;
+	required_region_ptr<uint16_t> m_spirom;
 
 	void map(address_map &map);
 };
@@ -75,11 +75,15 @@ INPUT_PORTS_END
 
 void generalplus_gpl_unknown_state::map(address_map &map)
 {
-	map(0x000000, 0x000fff).ram(); // unknown, RAM?
-	// 1000-3fff are probably registers
-	map(0x004000, 0x00ffff).ram(); // internal ROM? there are jumps to this region + many fetches for jump tables etc. doesn't seem the usual unSP 8000-ffff ROM mapping
+	map(0x000000, 0x000fff).ram(); // RAM
+	//map(0x001000, 0x0017ff).ram(); // acts like open bus?
 
-	map(0x200000, 0x3fffff).ram(); // almost certainly RAM where the SPI ROM gets copied (or just ROM if it can execute directly from SPI here)
+	//map(0x003000, 0x003fff).ram(); // system regs
+
+	map(0x004000, 0x00bfff).rom().region("maincpu", 0x0000);
+	map(0x00c000, 0x00ffff).rom().region("maincpu", 0x0000);
+
+	map(0x200000, 0x3fffff).rom().region("spi", 0x0000); // has direct access to SPI ROM
 }
 
 
@@ -89,28 +93,20 @@ void generalplus_gpl_unknown_state::machine_start()
 
 void generalplus_gpl_unknown_state::machine_reset()
 {
-	address_space& mem = m_maincpu->space(AS_PROGRAM);
-	for (int j = 0; j < 0x200000/2; j++)
-	{
-		uint16_t data = (m_spirom[(j*2)+1] << 8) | (m_spirom[(j*2)+0]);
-		mem.write_word(0x200000+j, data);
-	}
-
-	m_maincpu->set_state_int(UNSP_PC, 0x1000); // this is not the boot code, everything in the SPI ROM seems like relatively small functions (that never touch internal registers) only the very last one in SPI ROM has no callers from in the SPI ROM code
-	m_maincpu->set_state_int(UNSP_SR, 0x0020);
 }
 
 void generalplus_gpl_unknown_state::generalplus_gpl_unknown(machine_config &config)
 {
 
-	UNSP_12(config, m_maincpu, 96000000); // unknown CPU, no unSP2.0 opcodes?
+	UNSP_20(config, m_maincpu, 96000000); // internal ROM uses unsp2.0 opcodes, unknown clock
 	m_maincpu->set_addrmap(AS_PROGRAM, &generalplus_gpl_unknown_state::map);
+	m_maincpu->set_vectorbase(0x4010); // there is also a set of vectors for what looks to be a burn-in test at 4000, maybe external pin selects?
 
-	SCREEN(config, m_screen, SCREEN_TYPE_RASTER);
+	SCREEN(config, m_screen, SCREEN_TYPE_LCD);
 	m_screen->set_refresh_hz(60);
 	m_screen->set_vblank_time(ATTOSECONDS_IN_USEC(10));
-	m_screen->set_size(64*8, 32*8);
-	m_screen->set_visarea(0*8, 320-1, 0*8, 240-1); // not the correct resolution
+	m_screen->set_size(128, 128);
+	m_screen->set_visarea(0, 128-1, 0, 128-1); // not the correct resolution
 	m_screen->set_screen_update(FUNC(generalplus_gpl_unknown_state::screen_update));
 	//m_screen->screen_vblank().set(FUNC(generalplus_gpl_unknown_state::screen_vblank));
 
@@ -118,44 +114,44 @@ void generalplus_gpl_unknown_state::generalplus_gpl_unknown(machine_config &conf
 }
 
 
-ROM_START( mapacman )
-	ROM_REGION( 0x18000, "maincpu", ROMREGION_ERASEFF )
-	ROM_LOAD16_WORD_SWAP( "internal.rom", 0x000000, 0x18000, NO_DUMP )
+ROM_START( mapacman ) // this is the single game (no games hidden behind solder pads) release
+	ROM_REGION16_BE( 0x18000, "maincpu", ROMREGION_ERASEFF )
+	ROM_LOAD16_WORD_SWAP( "mapacman_internal.rom", 0x000000, 0x10000, CRC(9ea69d2a) SHA1(17f5001794f4454bf5856cfa170834509d68bed0) )
 
-	ROM_REGION( 0x800000, "spi", ROMREGION_ERASEFF )
-	ROM_LOAD( "fm25q16a.bin", 0x000000, 0x200000, CRC(aeb472ac) SHA1(500c24b725f6d3308ef8cbdf4259f5be556c7c92) )
+	ROM_REGION16_BE( 0x800000, "spi", ROMREGION_ERASEFF )
+	ROM_LOAD16_WORD_SWAP( "fm25q16a.bin", 0x000000, 0x200000, CRC(aeb472ac) SHA1(500c24b725f6d3308ef8cbdf4259f5be556c7c92) )
 ROM_END
 
 ROM_START( taspinv )
-	ROM_REGION( 0x18000, "maincpu", ROMREGION_ERASEFF )
+	ROM_REGION16_BE( 0x18000, "maincpu", ROMREGION_ERASEFF )
 	ROM_LOAD16_WORD_SWAP( "internal.rom", 0x000000, 0x18000, NO_DUMP )
 
-	ROM_REGION( 0x800000, "spi", ROMREGION_ERASEFF )
-	ROM_LOAD( "tinyarcade_spaceinvaders.bin", 0x000000, 0x200000, CRC(11ac4c77) SHA1(398d5eff83a4e94487ed810819085a0e44582908) )
+	ROM_REGION16_BE( 0x800000, "spi", ROMREGION_ERASEFF )
+	ROM_LOAD16_WORD_SWAP( "tinyarcade_spaceinvaders.bin", 0x000000, 0x200000, CRC(11ac4c77) SHA1(398d5eff83a4e94487ed810819085a0e44582908) )
 ROM_END
 
 ROM_START( tagalaga )
-	ROM_REGION( 0x18000, "maincpu", ROMREGION_ERASEFF )
+	ROM_REGION16_BE( 0x18000, "maincpu", ROMREGION_ERASEFF )
 	ROM_LOAD16_WORD_SWAP( "internal.rom", 0x000000, 0x18000, NO_DUMP )
 
-	ROM_REGION( 0x800000, "spi", ROMREGION_ERASEFF )
-	ROM_LOAD( "tinyarcadegalaga_fm25q16a_a14015.bin", 0x000000, 0x200000, CRC(2a91460c) SHA1(ce297642d2d51ce568e93c0c57432446633b2077) )
+	ROM_REGION16_BE( 0x800000, "spi", ROMREGION_ERASEFF )
+	ROM_LOAD16_WORD_SWAP( "tinyarcadegalaga_fm25q16a_a14015.bin", 0x000000, 0x200000, CRC(2a91460c) SHA1(ce297642d2d51ce568e93c0c57432446633b2077) )
 ROM_END
 
 ROM_START( parcade )
-	ROM_REGION( 0x18000, "maincpu", ROMREGION_ERASEFF )
+	ROM_REGION16_BE( 0x18000, "maincpu", ROMREGION_ERASEFF )
 	ROM_LOAD16_WORD_SWAP( "internal.rom", 0x000000, 0x18000, NO_DUMP )
 
-	ROM_REGION( 0x800000, "spi", ROMREGION_ERASEFF )
-	ROM_LOAD( "palacearcade_gpr25l3203_c22016.bin", 0x000000, 0x400000, CRC(98fbd2a1) SHA1(ffc19aadd53ead1f9f3472475606941055ca09f9) )
+	ROM_REGION16_BE( 0x800000, "spi", ROMREGION_ERASEFF )
+	ROM_LOAD16_WORD_SWAP( "palacearcade_gpr25l3203_c22016.bin", 0x000000, 0x400000, CRC(98fbd2a1) SHA1(ffc19aadd53ead1f9f3472475606941055ca09f9) )
 ROM_END
 
 ROM_START( taturtf )
-	ROM_REGION( 0x18000, "maincpu", ROMREGION_ERASEFF )
+	ROM_REGION16_BE( 0x18000, "maincpu", ROMREGION_ERASEFF )
 	ROM_LOAD16_WORD_SWAP( "internal.rom", 0x000000, 0x18000, NO_DUMP )
 
-	ROM_REGION( 0x800000, "spi", ROMREGION_ERASEFF )
-	ROM_LOAD( "tinyarcadeturtlefighter_25q32bst16_684016.bin", 0x000000, 0x400000, CRC(8e046f2d) SHA1(e48492cf953f22a47fa2b88a8f96a1e459b8c487) )
+	ROM_REGION16_BE( 0x800000, "spi", ROMREGION_ERASEFF )
+	ROM_LOAD16_WORD_SWAP( "tinyarcadeturtlefighter_25q32bst16_684016.bin", 0x000000, 0x400000, CRC(8e046f2d) SHA1(e48492cf953f22a47fa2b88a8f96a1e459b8c487) )
 ROM_END
 
 
