2c2b8dccc837faa3b37b0cdcfb1e428e073c6c64
Robbbert <Robbbert@users.noreply.github.com>
2022-03-25 23:38:20 +1100

    init vars for coverity (drivers/i)

--- src/mame/drivers/igs009.cpp
+++ src/mame/drivers/igs009.cpp
@@ -107,13 +107,13 @@ private:
 	required_shared_ptr<uint8_t> m_fg_color_ram;
 	output_finder<7> m_leds;
 
-	tilemap_t *m_reel_tilemap[4];
-	tilemap_t *m_fg_tilemap;
-	int m_video_enable;
-	int m_nmi_enable;
-	int m_hopper;
-	uint8_t m_out[3];
-	uint8_t m_igs_magic[2];
+	tilemap_t *m_reel_tilemap[4]{};
+	tilemap_t *m_fg_tilemap = nullptr;
+	int m_video_enable = 0;
+	int m_nmi_enable = 0;
+	int m_hopper = 0;
+	uint8_t m_out[3]{};
+	uint8_t m_igs_magic[2]{};
 };
 
 
