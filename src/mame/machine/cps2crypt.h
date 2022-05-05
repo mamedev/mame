ea8d17407755da22e218b9392063497f8f615d0b
Vas Crabb <vas@vastheman.com>
2022-05-06 05:12:12 +1000

    cps2.cpp: Added TOURNAMENT board communication simulation. [Vas Crabb, Darksoft, Gregory Lewandowski]

--- src/mame/machine/cps2crypt.h
+++ src/mame/machine/cps2crypt.h
@@ -1,7 +1,15 @@
 // license:BSD-3-Clause
 // copyright-holders:Paul Leaman, Andreas Naive, Nicola Salmoria,Charles MacDonald
 /******************************************************************************
-
 CPS-2 Encryption
 
-*/
+******************************************************************************/
+
+#ifndef MAME_MACHINE_CPS2CRYPT_H
+#define MAME_MACHINE_CPS2CRYPT_H
+
+#pragma once
+
+void cps2_decrypt(running_machine &machine, uint16_t *rom, uint16_t *dec, int length, const uint32_t *master_key, uint32_t lower_limit, uint32_t upper_limit);
+
+#endif // MAME_MACHINE_CPS2CRYPT_H
