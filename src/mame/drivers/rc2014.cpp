ec4f9edfb4820d25528ebdb8e486238a7b1b4e02
Miodrag Milanovic <mmicko@gmail.com>
2022-05-07 13:41:33 +0200

    add comments in driver

--- src/mame/drivers/rc2014.cpp
+++ src/mame/drivers/rc2014.cpp
@@ -1,5 +1,10 @@
 // license:BSD-3-Clause
 // copyright-holders:Miodrag Milanovic
+/**************************************************************************************
+
+    RC2014 Modular Computer
+
+**************************************************************************************/
 
 #include "emu.h"
 #include "bus/rc2014/rc2014.h"
@@ -15,6 +20,17 @@ public:
 		, m_rc2014_bus(*this, "bus")
 	{ }
 
+	//
+	// RC2014 Classic
+	//
+	// Z80 CPU module
+	// Clock/Reset
+	// 32K RAM module
+	// Switchable ROM module
+	// Serial I/O
+	//
+	// Backplane-5 - 5 x 40pin slots
+	//
 	void rc2014(machine_config &config)
 	{
 		RC2014_BUS(config, m_rc2014_bus, 0);
@@ -25,6 +41,19 @@ public:
 		RC2014_SLOT(config, "bus:5", m_rc2014_bus, rc2014_bus_modules, "serial");
 	}
 
+	//
+	// RC2014 Classic II
+	//
+	// Clock/Reset
+	// Z80 CPU module
+	// 32K RAM module
+	// Switchable ROM module
+	// Serial I/O
+	//
+	// Backplane-8 - 8 x 40pin slots
+	//
+	// all modules are in slightly different form factor
+	//
 	void rc2014cl2(machine_config &config)
 	{
 		RC2014_BUS(config, m_rc2014_bus, 0);
@@ -38,6 +67,19 @@ public:
 		RC2014_SLOT(config, "bus:8", m_rc2014_bus, rc2014_bus_modules, nullptr);
 	}
 
+
+	//
+	// RC2014 Zed
+	//
+	// Z80 2.1 CPU Module
+	// Dual Clock Module
+	// 512k ROM 512k RAM Module
+	// Dual Serial SIO/2 Module
+	//
+	// Backplane 8 - 8 x 40pin slots
+	//
+	// Some modules are extended bus modules in standard slots
+	// 
 	void rc2014zed(machine_config &config)
 	{
 		RC2014_BUS(config, m_rc2014_bus, 0);
@@ -70,6 +112,18 @@ public:
 		, m_rc2014_bus(*this, "bus")
 	{ }
 
+	//
+	// RC2014 PRo
+	//
+	// Z80 2.1 CPU Module
+	// Dual Clock Module
+	// 64k RAM
+	// Pageable ROM
+	// Dual Serial SIO/2 Module
+	// Compact Flash storage
+	//
+	// Backplane Pro - 12 x extended slots
+	// 
 	void rc2014pro(machine_config &config)
 	{
 		RC2014_EXT_BUS(config, m_rc2014_bus, 0);
@@ -87,6 +141,16 @@ public:
 		RC2014_EXT_SLOT(config, "bus:12", m_rc2014_bus, rc2014_ext_bus_modules, nullptr);
 	}
 	
+	//
+	// RC2014 Zed
+	//
+	// Z80 2.1 CPU Module
+	// Dual Clock Module
+	// 512k ROM 512k RAM Module
+	// Dual Serial SIO/2 Module
+	//
+	// Backplane Pro - 12 x extended slots
+	//
 	void rc2014zedp(machine_config &config)
 	{
 		RC2014_EXT_BUS(config, m_rc2014_bus, 0);
@@ -119,6 +183,6 @@ ROM_END
 //    YEAR  NAME        PARENT    COMPAT    MACHINE    INPUT    CLASS            INIT           COMPANY           FULLNAME             FLAGS
 COMP( 2016, rc2014,     0,        0,        rc2014,    0,       rc2014_state,    empty_init,    "RFC2795 Ltd",    "RC2014 Classic",    MACHINE_SUPPORTS_SAVE )
 COMP( 2017, rc2014pro,  rc2014,   0,        rc2014pro, 0,       rc2014pro_state, empty_init,    "RFC2795 Ltd",    "RC2014 Pro",        MACHINE_SUPPORTS_SAVE )
-COMP( 2020, rc2014cl2,  rc2014,   0,        rc2014cl2, 0,       rc2014_state,    empty_init,    "RFC2795 Ltd",    "RC2014 Classic 2",  MACHINE_SUPPORTS_SAVE )
+COMP( 2020, rc2014cl2,  rc2014,   0,        rc2014cl2, 0,       rc2014_state,    empty_init,    "RFC2795 Ltd",    "RC2014 Classic II", MACHINE_SUPPORTS_SAVE )
 COMP( 2018, rc2014zed , rc2014,   0,        rc2014zed ,0,       rc2014_state,    empty_init,    "RFC2795 Ltd",    "RC2014 Zed",        MACHINE_SUPPORTS_SAVE )
 COMP( 2018, rc2014zedp, rc2014,   0,        rc2014zedp,0,       rc2014pro_state, empty_init,    "RFC2795 Ltd",    "RC2014 Zed Pro",    MACHINE_SUPPORTS_SAVE )
