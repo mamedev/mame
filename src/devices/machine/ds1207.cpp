8b6d4b879e7fd35d3b499067331563ef1c549b02
Paul Arnold <paul@paul-arnold.co.uk>
2022-06-22 15:39:23 +0100

    Add new devices DS1207 and SCC66470

---
+++ src/devices/machine/ds1207.cpp
@@ -0,0 +1,627 @@
+// license:BSD-3-Clause
+// copyright-holders:Paul-Arnold
+/*
+ * ds1207.c
+ *
+ * Time Key
+ *
+ * Based on ds1204 by smf.
+ *
+ * File format is as follows:
+ * 00-01 unique command pattern
+ * 02-09 identification pattern
+ * 0a-11 security match
+ * 12-41 secure memory data
+ * 42-43 days left
+ * 44-4b start time (from time_t)
+ * 4c    control
+ *
+ * Control bits:
+ * bit 0 - OSC ENABLED
+ * bit 1 - OSC RUNNING
+ * bit 2 - DAYS LOCKED
+ * bit 3 - DAYS EXPIRED
+ *
+ * The unique command pattern can be user specific but a number of off the shelf devices exist.
+ * For these devices the pattern should be as follows:
+ *   DS1207-G01 0x00 0xb0
+ *   DS1207-G02 0x04 0xb0
+ *   DS1207-G03 0x08 0xb0
+ *   DS1207-G04 0x0c 0xb0
+ *   DS1207-G05 0x10 0xb0
+ */
+#include "emu.h"
+#include "ds1207.h"
+
+#include <cstdio>
+
+
+#define VERBOSE_LEVEL ( 0 )
+
+inline void ATTR_PRINTF(3, 4) ds1207_device::verboselog(int n_level, const char *s_fmt, ...)
+{
+	if(VERBOSE_LEVEL >= n_level)
+	{
+		va_list v;
+		char buf[ 32768 ];
+		va_start(v, s_fmt);
+		vsprintf(buf, s_fmt, v);
+		va_end(v);
+		logerror("%s: ds1207(%s) %s", machine().describe_context(), tag(), buf);
+	}
+}
+
+// device type definition
+DEFINE_DEVICE_TYPE(DS1207, ds1207_device, "ds1207", "DS1207 Time Key")
+
+ds1207_device::ds1207_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
+	: device_t(mconfig, DS1207, tag, owner, clock),
+	  device_nvram_interface(mconfig, *this),
+	  m_region(*this, DEVICE_SELF),
+	  m_rst(0),
+	  m_clk(0),
+	  m_dqw(0), m_dqr(0), m_state(0), m_bit(0)
+{
+}
+
+void ds1207_device::device_reset()
+{
+	adjust_days_left(); // compensate for time machine has been turned off
+}
+
+void ds1207_device::device_start()
+{
+	new_state(STATE_STOP);
+	m_dqr = DQ_HIGH_IMPEDANCE;
+
+	memset(m_command, 0, sizeof(m_command));
+	memset(m_compare_register, 0, sizeof(m_compare_register));
+	m_last_update_time = 0;
+	memset(m_day_clock, 0, sizeof(m_day_clock));
+
+	save_item(NAME(m_rst));
+	save_item(NAME(m_clk));
+	save_item(NAME(m_dqw));
+	save_item(NAME(m_dqr));
+	save_item(NAME(m_state));
+	save_item(NAME(m_bit));
+	save_item(NAME(m_command));
+	save_item(NAME(m_compare_register));
+	save_item(NAME(m_unique_pattern));
+	save_item(NAME(m_identification));
+	save_item(NAME(m_security_match));
+	save_item(NAME(m_secure_memory));
+	save_item(NAME(m_day_clock));
+	save_item(NAME(m_days_left));
+	save_item(NAME(m_start_time));
+	save_item(NAME(m_device_state));
+	save_item(NAME(m_last_update_time));
+}
+
+void ds1207_device::nvram_default()
+{
+	memset(m_unique_pattern, 0, sizeof(m_unique_pattern));
+	memset(m_identification, 0, sizeof(m_identification));
+	memset(m_security_match, 0, sizeof(m_security_match));
+	memset(m_secure_memory, 0, sizeof(m_secure_memory));
+	memset(m_days_left, 0, sizeof(m_days_left));
+	memset(m_start_time, 0, sizeof(m_start_time));
+	m_device_state = 0;
+
+	int expected_bytes = sizeof(m_unique_pattern) + sizeof(m_identification) + sizeof(m_security_match) + sizeof(m_secure_memory)
+	                     + sizeof(m_days_left) + sizeof(m_start_time) + sizeof(m_device_state);;
+
+	if(!m_region.found())
+	{
+		logerror("ds1207(%s) region not found\n", tag());
+	}
+
+	else if(m_region->bytes() != expected_bytes)
+	{
+		logerror("ds1207(%s) region length 0x%x expected 0x%x\n", tag(), m_region->bytes(), expected_bytes);
+	}
+
+	else
+	{
+		uint8_t *region = m_region->base();
+
+		memcpy(m_unique_pattern, region, sizeof(m_unique_pattern));
+		region += sizeof(m_unique_pattern);
+		memcpy(m_identification, region, sizeof(m_identification));
+		region += sizeof(m_identification);
+		memcpy(m_security_match, region, sizeof(m_security_match));
+		region += sizeof(m_security_match);
+		memcpy(m_secure_memory, region, sizeof(m_secure_memory));
+		region += sizeof(m_secure_memory);
+		memcpy(m_days_left, region, sizeof(m_days_left));
+		region += sizeof(m_days_left);
+		memcpy(m_start_time, region, sizeof(m_start_time));
+		region += sizeof(m_start_time);
+		memcpy(&m_device_state, region, sizeof(m_device_state));
+		region += sizeof(m_device_state);
+	}
+}
+
+bool ds1207_device::nvram_read(util::read_stream &file)
+{
+	size_t actual;
+	bool result = !file.read(m_unique_pattern, sizeof(m_unique_pattern), actual) && actual == sizeof(m_unique_pattern);
+	result = result && !file.read(m_identification, sizeof(m_identification), actual) && actual == sizeof(m_identification);
+	result = result && !file.read(m_security_match, sizeof(m_security_match), actual) && actual == sizeof(m_security_match);
+	result = result && !file.read(m_secure_memory, sizeof(m_secure_memory), actual) && actual == sizeof(m_secure_memory);
+	result = result && !file.read(m_days_left, sizeof(m_days_left), actual) && actual == sizeof(m_days_left);
+	result = result && !file.read(m_start_time, sizeof(m_start_time), actual) && actual == sizeof(m_start_time);
+	result = result && !file.read(&m_device_state, sizeof(m_device_state), actual) && actual == sizeof(m_device_state);
+	return result;
+}
+
+bool ds1207_device::nvram_write(util::write_stream &file)
+{
+	size_t actual;
+	bool result = !file.write(m_unique_pattern, sizeof(m_unique_pattern), actual) && actual == sizeof(m_unique_pattern);
+	result = result && !file.write(m_identification, sizeof(m_identification), actual) && actual == sizeof(m_identification);
+	result = result && !file.write(m_security_match, sizeof(m_security_match), actual) && actual == sizeof(m_security_match);
+	result = result && !file.write(m_secure_memory, sizeof(m_secure_memory), actual) && actual == sizeof(m_secure_memory);
+	result = result && !file.write(m_days_left, sizeof(m_days_left), actual) && actual == sizeof(m_days_left);
+	result = result && !file.write(m_start_time, sizeof(m_start_time), actual) && actual == sizeof(m_start_time);
+	result = result && !file.write(&m_device_state, sizeof(m_device_state), actual) && actual == sizeof(m_device_state);
+	return result;
+}
+
+void ds1207_device::new_state(int state)
+{
+	m_state = state;
+	m_bit = 0;
+}
+
+void ds1207_device::writebit(uint8_t *buffer)
+{
+	if(m_clk)
+	{
+		int index = m_bit / 8;
+		int mask = 1 << (m_bit % 8);
+
+		if(m_dqw)
+		{
+			buffer[ index ] |= mask;
+		}
+		else
+		{
+			buffer[ index ] &= ~mask;
+		}
+
+		m_bit++;
+	}
+}
+
+void ds1207_device::readbit(uint8_t *buffer)
+{
+	if(!m_clk)
+	{
+		int index = m_bit / 8;
+		int mask = 1 << (m_bit % 8);
+
+		if(buffer[ index ] & mask)
+		{
+			m_dqr = 1;
+		}
+		else
+		{
+			m_dqr = 0;
+		}
+	}
+
+	else
+	{
+		m_bit++;
+	}
+}
+
+WRITE_LINE_MEMBER(ds1207_device::write_rst)
+{
+	if(m_rst != state)
+	{
+		m_rst = state;
+		verboselog(2, "rst=%d\n", m_rst);
+
+		if(m_rst)
+		{
+			new_state(STATE_PROTOCOL);
+		}
+		else
+		{
+			switch(m_state)
+			{
+				case STATE_WRITE_IDENTIFICATION:
+					verboselog(0, "reset during write identification (bit=%d)\n", m_bit);
+					break;
+
+				case STATE_WRITE_SECURITY_MATCH:
+					verboselog(0, "reset during write security match (bit=%d)\n", m_bit);
+					break;
+
+				case STATE_WRITE_SECURE_MEMORY:
+					verboselog(0, "reset during write secure memory (bit=%d)\n", m_bit);
+					break;
+			}
+
+			new_state(STATE_STOP);
+			m_dqr = DQ_HIGH_IMPEDANCE;
+		}
+	}
+}
+
+WRITE_LINE_MEMBER(ds1207_device::write_clk)
+{
+	if(m_clk != state)
+	{
+		m_clk = state;
+		verboselog(2, "clk=%d (bit=%d)\n", m_clk, m_bit);
+
+		if(m_clk)
+		{
+			m_dqr = DQ_HIGH_IMPEDANCE;
+		}
+
+		switch(m_state)
+		{
+			case STATE_PROTOCOL:
+				writebit(m_command);
+
+				if(m_bit == 24)
+				{
+					verboselog(1, "-> command %02x %02x %02x (%02x %02x)\n",
+					           m_command[ 0 ], m_command[ 1 ], m_command[ 2 ], m_unique_pattern[ 0 ], m_unique_pattern[ 1 ]);
+							   
+					if(m_command[ 2 ] == m_unique_pattern[ 1 ] && (m_command[ 1 ] & ~3) == m_unique_pattern[ 0 ])
+					{
+						set_start_time();
+						adjust_time_into_day();
+
+						if(m_command[ 0 ] == COMMAND_READ && (m_command[ 1 ] & 3) == CYCLE_NORMAL)
+						{
+							new_state(STATE_READ_IDENTIFICATION);
+						}
+						else if(m_command[ 0 ] == COMMAND_READ_DAY_CLOCK && (m_command[ 1 ] & 3) == CYCLE_PROGRAM)
+						{
+							new_state(STATE_READ_DAY_CLOCK);
+						}
+						else if(m_command[ 0 ] == COMMAND_READ_DAYS_REMAINING && (m_command[ 1 ] & 3) == CYCLE_PROGRAM)
+						{
+							new_state(STATE_READ_DAYS_REMAINING);
+						}
+						else if(!(m_device_state & DAYS_EXPIRED))
+						{
+							if(m_command[ 0 ] == COMMAND_WRITE && (m_command[ 1 ] & 3) == CYCLE_NORMAL)
+							{
+								new_state(STATE_READ_IDENTIFICATION);
+							}
+							else if((m_command[ 1 ] & 3) == CYCLE_PROGRAM)
+							{
+								if(m_command[ 0 ] == COMMAND_WRITE)
+								{
+									new_state(STATE_WRITE_IDENTIFICATION);
+								}
+								else if(m_command[ 0 ] == COMMAND_WRITE_DAYS_REMAINING)
+								{
+									if(!(m_device_state & DAYS_LOCKED))
+									{
+										new_state(STATE_WRITE_DAYS_REMAINING);
+									}
+									else
+									{
+										new_state(STATE_STOP);
+									}
+								}
+								else if(m_command[ 0 ] == COMMAND_LOCK_DAYS_COUNT)
+								{
+									m_device_state |= DAYS_LOCKED;
+
+									new_state(STATE_STOP);
+								}
+								else if(m_command[ 0 ] == COMMAND_STOP_OSCILLATOR)
+								{
+									if(!(m_device_state & DAYS_LOCKED))
+									{
+										m_device_state &= ~(OSC_ENABLED | OSC_RUNNING);
+									}
+
+									new_state(STATE_STOP);
+								}
+								else if(m_command[ 0 ] == COMMAND_ARM_OSCILLATOR)
+								{
+									m_device_state |= OSC_ENABLED;
+								}
+								else
+								{
+									new_state(STATE_STOP);
+								}
+							}
+							else
+							{
+								new_state(STATE_STOP);
+							}
+						}
+						else
+						{
+							new_state(STATE_STOP);
+						}
+					}
+					else
+					{
+						new_state(STATE_STOP);
+					}
+				}
+				break;
+
+			case STATE_READ_IDENTIFICATION:
+				readbit(m_identification);
+
+				if(m_bit == 64)
+				{
+					verboselog(1, "<- identification %02x %02x %02x %02x %02x %02x %02x %02x\n",
+					           m_identification[ 0 ], m_identification[ 1 ], m_identification[ 2 ], m_identification[ 3 ],
+					           m_identification[ 4 ], m_identification[ 5 ], m_identification[ 6 ], m_identification[ 7 ]);
+
+					new_state(STATE_WRITE_COMPARE_REGISTER);
+				}
+				break;
+
+			case STATE_WRITE_COMPARE_REGISTER:
+				writebit(m_compare_register);
+
+				if(m_bit == 64)
+				{
+					verboselog(1, "-> compare register %02x %02x %02x %02x %02x %02x %02x %02x (%02x %02x %02x %02x %02x %02x %02x %02x)\n",
+					           m_compare_register[ 0 ], m_compare_register[ 1 ], m_compare_register[ 2 ], m_compare_register[ 3 ],
+					           m_compare_register[ 4 ], m_compare_register[ 5 ], m_compare_register[ 6 ], m_compare_register[ 7 ],
+					           m_security_match[ 0 ], m_security_match[ 1 ], m_security_match[ 2 ], m_security_match[ 3 ],
+					           m_security_match[ 4 ], m_security_match[ 5 ], m_security_match[ 6 ], m_security_match[ 7 ]);
+
+					if(memcmp(m_compare_register, m_security_match, sizeof(m_compare_register)) == 0)
+					{
+						if(m_command[ 0 ] == COMMAND_READ)
+						{
+							new_state(STATE_READ_SECURE_MEMORY);
+						}
+						else
+						{
+							new_state(STATE_WRITE_SECURE_MEMORY);
+						}
+					}
+					else
+					{
+						new_state(STATE_OUTPUT_GARBLED_DATA);
+					}
+				}
+				break;
+
+			case STATE_READ_SECURE_MEMORY:
+				readbit(m_secure_memory);
+
+				if(m_bit == 384)
+				{
+					new_state(STATE_STOP);
+				}
+				break;
+
+			case STATE_WRITE_SECURE_MEMORY:
+				writebit(m_secure_memory);
+
+				if(m_bit == 384)
+				{
+					new_state(STATE_STOP);
+				}
+				break;
+
+			case STATE_WRITE_IDENTIFICATION:
+				writebit(m_identification);
+
+				if(m_bit == 64)
+				{
+					verboselog(1, "-> identification %02x %02x %02x %02x %02x %02x %02x %02x\n",
+					           m_identification[ 0 ], m_identification[ 1 ], m_identification[ 2 ], m_identification[ 3 ],
+					           m_identification[ 4 ], m_identification[ 5 ], m_identification[ 6 ], m_identification[ 7 ]);
+
+					new_state(STATE_WRITE_SECURITY_MATCH);
+				}
+				break;
+
+			case STATE_WRITE_SECURITY_MATCH:
+				writebit(m_security_match);
+
+				if(m_bit == 64)
+				{
+					verboselog(1, ">- security match %02x %02x %02x %02x %02x %02x %02x %02x\n",
+					           m_security_match[ 0 ], m_security_match[ 1 ], m_security_match[ 2 ], m_security_match[ 3 ],
+					           m_security_match[ 4 ], m_security_match[ 5 ], m_security_match[ 6 ], m_security_match[ 7 ]);
+
+					new_state(STATE_STOP);
+				}
+				break;
+
+			case STATE_OUTPUT_GARBLED_DATA:
+				if(!m_clk && m_command[ 0 ] == COMMAND_READ)
+				{
+					m_dqr = machine().rand() & 1;
+					m_bit++;
+				}
+				else if(m_clk && m_command[ 0 ] == COMMAND_WRITE)
+				{
+					m_bit++;
+				}
+
+				if(m_bit == 64)
+				{
+					if(m_command[ 0 ] == COMMAND_READ)
+					{
+						verboselog(1, "<- random\n");
+					}
+					else
+					{
+						verboselog(1, "-> ignore\n");
+					}
+
+					new_state(STATE_STOP);
+				}
+				break;
+
+			case STATE_READ_DAY_CLOCK:
+				readbit(m_day_clock);
+
+				if(m_bit == 20)
+				{
+					new_state(STATE_STOP);
+				}
+				break;
+
+			case STATE_READ_DAYS_REMAINING:
+				readbit(m_days_left);
+
+				if(m_bit == 9)
+				{
+					new_state(STATE_STOP);
+				}
+				break;
+
+			case STATE_WRITE_DAYS_REMAINING:
+				writebit(m_days_left);
+
+				if(m_bit == 9)
+				{
+					new_state(STATE_STOP);
+				}
+				break;
+		}
+	}
+}
+
+WRITE_LINE_MEMBER(ds1207_device::write_dq)
+{
+	if(m_dqw != state)
+	{
+		m_dqw = state;
+
+		verboselog(2, "dqw=%d\n", m_dqw);
+	}
+}
+
+READ_LINE_MEMBER(ds1207_device::read_dq)
+{
+	if(m_dqr == DQ_HIGH_IMPEDANCE)
+	{
+		verboselog(2, "dqr=high impedance\n");
+		return 0;
+	}
+
+	verboselog(2, "dqr=%d (bit=%d)\n", m_dqr, m_bit);
+	return m_dqr;
+}
+
+void ds1207_device::adjust_time_into_day()
+{
+	if(!(m_device_state & DAYS_EXPIRED) && (m_device_state & OSC_ENABLED) && (m_device_state & OSC_RUNNING))
+	{
+		uint64_t day_clock;
+		uint64_t cur_time;
+		uint64_t diff_time;
+
+		day_clock = m_day_clock[ 0 ] | (m_day_clock[ 1 ] << 8) | (m_day_clock[ 2 ] << 16);
+		
+		cur_time = machine().time().as_ticks(32768) / 2700;
+		diff_time = cur_time - m_last_update_time;
+		m_last_update_time = cur_time;
+
+		day_clock += diff_time;
+
+		m_day_clock[ 0 ] = day_clock & 0xff;
+		m_day_clock[ 1 ] = (day_clock >> 8) & 0xff;
+		m_day_clock[ 2 ] = (day_clock >> 16) & 0xff;
+
+		if(day_clock >= 1048576)
+		{
+			adjust_days_left();
+		}
+	}
+}
+
+void ds1207_device::adjust_days_left()
+{
+	time_t current_time, start_time, time_diff;
+	uint16_t days_elapsed, days_left;
+	uint32_t day_clock;
+	if(!(m_device_state & DAYS_EXPIRED) && (m_device_state & OSC_ENABLED) && (m_device_state & OSC_RUNNING))
+	{
+		time(&current_time);
+
+		start_time = 0;
+
+		for(int i = 0; i < 8 ; i++)
+		{
+			start_time <<= 8;
+			start_time |= m_start_time[ 7 - i ];
+		}
+		
+		if(current_time > start_time)
+		{
+			time_diff = current_time - start_time;
+		}
+		else
+		{
+			time_diff = 0;
+		}
+		
+		days_elapsed = time_diff / (24*60*60);
+
+		days_left = m_days_left[ 0 ] | (m_days_left[ 1 ] << 8);
+
+		time_diff %= (24*60*60);// seconds into day
+		
+		day_clock = (time_diff * 32768)/2700;// time into day
+
+		m_day_clock[ 0 ] = day_clock & 0xff;
+		m_day_clock[ 1 ] = (day_clock >> 8) & 0xff;
+		m_day_clock[ 2 ] = (day_clock >> 16) & 0xff;
+
+		if(days_elapsed > 0)
+		{
+			if(days_elapsed > days_left)
+			{
+				days_left = 0xffff;
+				m_device_state |= DAYS_EXPIRED;
+			}
+			else
+			{
+				days_left -= days_elapsed;
+			}
+
+			m_days_left[ 0 ] = days_left & 0xff;
+			m_days_left[ 1 ] = (days_left >> 8) & 0x1;
+
+			start_time += days_elapsed * 24 * 60 * 60;
+
+			for(int i = 0; i < 8 ; i++)
+			{
+				m_start_time[ i ] = (start_time >> (i * 8)) & 0xff;
+			}
+		}
+	}
+}
+
+void ds1207_device::set_start_time()
+{
+	if(!(m_device_state & DAYS_EXPIRED) && m_device_state & OSC_ENABLED && !(m_device_state & OSC_RUNNING))
+	{
+		time_t current_time;
+
+		time(&current_time);
+
+		for(int i = 0; i < 8 ; i++)
+		{
+			m_start_time[ i ] = (current_time >> (i * 8)) & 0xff;
+		}
+		m_day_clock [ 0 ] = m_day_clock[ 1 ] = m_day_clock[ 2 ] = 0;
+
+		m_device_state |= OSC_RUNNING;
+	}
+}
---
+++ src/devices/machine/scc66470.h
@@ -0,0 +1,115 @@
+// license:BSD-3-Clause
+// copyright-holders:Paul Arnold
+/***************************************************************************
+
+    scc66470.h
+
+***************************************************************************/
+
+#ifndef MAME_VIDEO_SCC66470_H
+#define MAME_VIDEO_SCC66470_H
+
+#pragma once
+
+//**************************************************************************
+//  TYPE DEFINITIONS
+//**************************************************************************
+
+
+// ======================> scc66470_device
+
+class scc66470_device : public device_t, public device_video_interface
+{
+public:
+	auto irq()
+	{
+		return m_irqcallback.bind();
+	}
+
+	// construction/destruction
+	scc66470_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
+
+	uint16_t ipa_r(offs_t offset, uint16_t mem_mask = ~0);
+	void ipa_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	bool display_enabled();
+	uint8_t *line(int line);
+	unsigned int width();
+	unsigned int height();
+	unsigned int total_height();
+	//void vblank_update();
+	void map(address_map &map);
+	void set_vectors(uint16_t *src);
+	int dram_dtack_cycles();
+	void dram_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	uint16_t dram_r(offs_t offset, uint16_t mem_mask = ~0);
+
+
+protected:
+	virtual void device_start() override;
+	virtual void device_reset() override;
+
+	devcb_write_line m_irqcallback;
+
+	uint8_t m_line_data[768];
+	uint32_t m_working_dcp ;
+
+	std::unique_ptr<uint16_t[]> m_dram;
+
+	uint16_t m_csr;
+	uint16_t m_dcr;
+	uint16_t m_vsr;
+	uint8_t m_bcr;
+	uint16_t m_dcr2;
+	uint16_t m_dcp;
+	uint16_t m_swm;
+	uint8_t m_stm;
+	uint16_t m_reg_a;
+	uint16_t m_reg_b;
+	uint16_t m_pcr;
+	uint8_t m_mask;
+	uint8_t m_shift;
+	uint8_t m_index;
+	uint8_t m_fc;
+	uint8_t m_bc;
+	uint8_t m_tc;
+	uint8_t m_csr_r;
+
+private:
+	void set_vsr(uint32_t vsr);
+	void set_dcp(uint32_t dcp);
+	uint32_t get_vsr();
+	uint32_t get_dcp();
+	unsigned int border_width();
+	int border_height();
+	void csr_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void dcr_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void vsr_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void bcr_w(offs_t offset, uint8_t data);
+	void dcr2_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void dcp_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void swm_w(offs_t offset, uint8_t data);
+	void stm_w(offs_t offset, uint8_t data);
+	void reg_a_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void reg_b_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void pcr_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void mask_w(offs_t offset, uint8_t data);
+	void shift_w(offs_t offset, uint8_t data);
+	void index_w(offs_t offset, uint8_t data);
+	void fc_w(offs_t offset, uint8_t data);
+	void bc_w(offs_t offset, uint8_t data);
+	void tc_w(offs_t offset, uint8_t data);
+	uint8_t csr_r(offs_t offset);
+	uint16_t reg_b_r(offs_t offset, uint16_t mem_mask = ~0);
+	int pixac_trigger();
+	void perform_pixac_op();
+
+	TIMER_CALLBACK_MEMBER(process_ica);
+	TIMER_CALLBACK_MEMBER(process_dca);
+	emu_timer *m_ica_timer;
+	emu_timer *m_dca_timer;
+};
+
+// device type definition
+DECLARE_DEVICE_TYPE(SCC66470, scc66470_device)
+
+#endif // MAME_VIDEO_SCC66470_H
