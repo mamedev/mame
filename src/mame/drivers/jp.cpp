b1fd575411ea797bc1ca94b380ffc6ae932a4b55
ClawGrip <clawgrip@hotmail.com>
2022-06-23 11:18:20 +0200

    Small fix

--- src/mame/drivers/jp.cpp
+++ src/mame/drivers/jp.cpp
@@ -746,7 +746,7 @@ GAME(1986,  olympus,   0,        jps, jp, jp_state, empty_init, ROT0, "Juegos Po
 /* Juan Paredes, the founder of Juegos Populares (and Petaco before that), started another pinball company named Automáticos Montecarlo (with another
    partner named Rosés) just before closing Juegos Populares because of economic issues, and trasferred some of the intelectual property from one company
    to the other. Automáticos Montecarlo manufactured two pinballs just relabeling Juegos Populares machines (using the same ROMs), Lortium and Pimbal
-   (Pimbal was a reissue with improved hardware of Quijote, also from Juegos Populares), and started a completely new pinball design named Mediterráneo,
-   which couldn't be finished, as Juan Paredes died on a traffic accident when the proyect was on a very preliminary stage. */
+   (Pimbal was a reissue of Quijote, also from Juegos Populares), and started a completely new pinball design named Mediterráneo, which couldn't be
+   finished, as Juan Paredes died on a traffic accident when the proyect was on a very preliminary stage. */
 GAME(1987,  lortium,   0,        jp,  jp, jp_state, empty_init, ROT0, u8"Juegos Populares / Automáticos Montecarlo", "Lortium",               MACHINE_IS_SKELETON_MECHANICAL | MACHINE_SUPPORTS_SAVE )
 GAME(19??,  pimbal,    0,        jp,  jp, jp_state, empty_init, ROT0, u8"Juegos Populares / Automáticos Montecarlo", "Pimbal (Pinball 3000)", MACHINE_IS_SKELETON_MECHANICAL | MACHINE_SUPPORTS_SAVE )
