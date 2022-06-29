e86f3c445e4e78617799dca05f48bbeb5bcc5bb0
npwoods <npwoods@mess.org>
2022-06-29 16:37:04 -0400

    Fixed CBMDOS FS module in response to recent FS changes

--- src/lib/formats/fs_cbmdos.cpp
+++ src/lib/formats/fs_cbmdos.cpp
@@ -30,21 +30,6 @@ namespace {
 
 class impl : public filesystem_t {
 public:
-	class root_dir : public idir_t {
-	public:
-		root_dir(impl &i) : m_fs(i) {}
-		virtual ~root_dir() = default;
-
-		virtual void drop_weak_references() override;
-		virtual meta_data metadata() override;
-		virtual std::vector<dir_entry> contents() override;
-		virtual file_t file_get(u64 key) override;
-		virtual dir_t dir_get(u64 key) override;
-
-	private:
-		impl &m_fs;
-	};
-
 	struct cbmdos_dirent
 	{
 		u8		m_next_directory_track;
@@ -64,46 +49,33 @@ public:
 	class block_iterator
 	{
 	public:
-		block_iterator(impl &fs, u8 first_track, u8 first_sector);
+		block_iterator(const impl &fs, u8 first_track, u8 first_sector);
 		bool next();
 		const void *data() const;
+		const std::array<cbmdos_dirent, 4> &dirent_data() const;
 		u8 size() const;
 
 	private:
-		impl &				m_fs;
-		fsblk_t::block_t	m_block;
-		u8					m_track;
-		u8					m_sector;
-	};
-
-	class file : public ifile_t
-	{
-	public:
-		file(impl &fs, cbmdos_dirent &&dirent);
-		virtual ~file() = default;
-
-		virtual void drop_weak_references() override;
-
-		virtual meta_data metadata() override;
-		virtual std::vector<u8> read_all() override;
-
-	private:
-		impl &			m_fs;
-		cbmdos_dirent	m_dirent;
+		const impl &					m_fs;
+		fsblk_t::block_t				m_block;
+		std::set<std::tuple<u8, u8>>	m_visited_set;
+		u8								m_track;
+		u8								m_sector;
 	};
 
 	impl(fsblk_t &blockdev);
 	virtual ~impl() = default;
 
-	virtual meta_data metadata() override;
-	virtual dir_t root() override;
+	virtual meta_data volume_metadata() override;
+	virtual std::pair<err_t, meta_data> metadata(const std::vector<std::string> &path) override;
+	virtual std::pair<err_t, std::vector<dir_entry>> directory_contents(const std::vector<std::string> &path) override;
+	virtual std::pair<err_t, std::vector<u8>> file_read(const std::vector<std::string> &path) override;
 
 private:
-	dir_t m_root;
-
-	void drop_root_ref();
 	fsblk_t::block_t read_sector(int track, int sector) const;
-	static const std::array<cbmdos_dirent, 4> &dir_entries(const fsblk_t::block_t &block);
+	std::optional<cbmdos_dirent> dirent_from_path(const std::vector<std::string> &path) const;
+	void iterate_directory_entries(const std::function<bool(const cbmdos_dirent &dirent)> &callback) const;
+	meta_data metadata_from_dirent(const cbmdos_dirent &dirent) const;
 };
 
 // methods
@@ -258,10 +230,10 @@ impl::impl(fsblk_t &blockdev)
 
 
 //-------------------------------------------------
-//  impl::metadata
+//  impl::volume_metadata
 //-------------------------------------------------
 
-meta_data impl::metadata()
+meta_data impl::volume_metadata()
 {
 	auto bam_block = read_sector(18, 0);
 	std::string_view disk_name = std::string_view((const char *) bam_block.rodata() + 0x90, 16);
@@ -273,24 +245,54 @@ meta_data impl::metadata()
 
 
 //-------------------------------------------------
-//  impl::root
+//  impl::metadata
 //-------------------------------------------------
 
-filesystem_t::dir_t impl::root()
+std::pair<err_t, meta_data> impl::metadata(const std::vector<std::string> &path)
 {
-	if (!m_root)
-		m_root = new root_dir(*this);
-	return m_root.strong();
+	std::optional<cbmdos_dirent> dirent = dirent_from_path(path);
+	if (!dirent)
+		return std::make_pair(ERR_NOT_FOUND, meta_data());
+
+	return std::make_pair(ERR_OK, metadata_from_dirent(*dirent));
 }
 
 
 //-------------------------------------------------
-//  impl::drop_root_ref
+//  impl::directory_contents
 //-------------------------------------------------
 
-void impl::drop_root_ref()
+std::pair<err_t, std::vector<dir_entry>> impl::directory_contents(const std::vector<std::string> &path)
 {
-	m_root = nullptr;
+	std::vector<dir_entry> results;
+	auto callback = [this, &results](const cbmdos_dirent &ent)
+	{
+		results.emplace_back(dir_entry_type::file, metadata_from_dirent(ent));
+		return false;
+	};
+	iterate_directory_entries(callback);
+	return std::make_pair(ERR_OK, std::move(results));
+}
+
+
+//-------------------------------------------------
+//  impl::file_read
+//-------------------------------------------------
+
+std::pair<err_t, std::vector<u8>> impl::file_read(const std::vector<std::string> &path)
+{
+	// find the file
+	std::optional<cbmdos_dirent> dirent = dirent_from_path(path);
+	if (!dirent)
+		return std::make_pair(ERR_NOT_FOUND, std::vector<u8>());
+
+	// and get the data
+	std::vector<u8> result;
+	block_iterator iter(*this, dirent->m_file_first_track, dirent->m_file_first_sector);
+	while (iter.next())
+		result.insert(result.end(), (const u8 *)iter.data(), (const u8 *)iter.data() + iter.size());
+
+	return std::make_pair(ERR_OK, std::move(result));
 }
 
 
@@ -312,183 +314,57 @@ fsblk_t::block_t impl::read_sector(int track, int sector) const
 
 
 //-------------------------------------------------
-//  impl::dir_entries
+//  impl::dirent_from_path
 //-------------------------------------------------
 
-const std::array<impl::cbmdos_dirent, 4> &impl::dir_entries(const fsblk_t::block_t &block)
+std::optional<impl::cbmdos_dirent> impl::dirent_from_path(const std::vector<std::string> &path) const
 {
-	return *reinterpret_cast<const std::array<impl::cbmdos_dirent, 4> *>(block.rodata());
-}
+	if (path.size() != 1)
+		return { };
+	std::string_view path_part = path[0];
 
-
-//-------------------------------------------------
-//  impl::root_dir::drop_weak_references
-//-------------------------------------------------
-
-void impl::root_dir::drop_weak_references()
-{
-	m_fs.drop_root_ref();
-}
-
-
-//-------------------------------------------------
-//  impl::root_dir::metadata
-//-------------------------------------------------
-
-meta_data impl::root_dir::metadata()
-{
-	return meta_data();
-}
-
-
-//-------------------------------------------------
-//  impl::root_dir::contents
-//-------------------------------------------------
-
-std::vector<dir_entry> impl::root_dir::contents()
-{
-	std::vector<dir_entry> results;
-	std::set<std::tuple<u8, u8>> visited_set;
-
-	u8 dir_track = 18;
-	u8 dir_sector = 1;
-	while (dir_track != 0)
+	std::optional<cbmdos_dirent> result;
+	auto callback = [&result, path_part](const cbmdos_dirent &dirent)
 	{
-		// this logic checks for a degenerate scenario where the directory structure contains
-		// a cyle
-		auto visited_tuple = std::make_tuple(dir_track, dir_sector);
-		if (visited_set.find(visited_tuple) != visited_set.end())
-			break;
-		visited_set.insert(visited_tuple);
-
-		// process directory entries in this sector
-		auto dir_block = m_fs.read_sector(dir_track, dir_sector);
-		auto &entries = dir_entries(dir_block);
-		u64 key = ((u64)dir_track) << 16 | ((u64)dir_sector) << 8;
-		for (const cbmdos_dirent &ent : entries)
-		{
-			if (ent.m_file_type != 0x00)
-			{
-				std::string_view file_name = strtrimright_cbm(ent.m_file_name);
-				results.emplace_back(std::string(file_name), dir_entry_type::file, key);
-			}
-			key++;
-		}
-
-		// advance to the next directory sector
-		dir_track = entries[0].m_next_directory_track;
-		dir_sector = entries[0].m_next_directory_sector;
-	}
-	return results;
-}
-
-
-//-------------------------------------------------
-//  impl::root_dir::file_get
-//-------------------------------------------------
-
-filesystem_t::file_t impl::root_dir::file_get(u64 key)
-{
-	u8 track = (u8)(key >> 16);
-	u8 sector = (u8)(key >> 8);
-	auto dir_block = m_fs.read_sector(track, sector);
-	const cbmdos_dirent &ent = dir_entries(dir_block)[(u8)key];
-	return file_t(new file(m_fs, cbmdos_dirent(ent)));
-}
-
-
-//-------------------------------------------------
-//  impl::root_dir::dir_get
-//-------------------------------------------------
-
-filesystem_t::dir_t impl::root_dir::dir_get(u64 key)
-{
-	throw std::logic_error("Directories not supported");
-}
-
-
-//-------------------------------------------------
-//  impl::block_iterator ctor
-//-------------------------------------------------
-
-impl::block_iterator::block_iterator(impl &fs, u8 first_track, u8 first_sector)
-	: m_fs(fs)
-	, m_track(first_track)
-	, m_sector(first_sector)
-{
-}
-
-
-//-------------------------------------------------
-//  impl::block_iterator::next
-//-------------------------------------------------
-
-bool impl::block_iterator::next()
-{
-	bool result;
-	if (m_track != 0x00)
-	{
-		m_block = m_fs.read_sector(m_track, m_sector);
-		m_track = m_block.r8(0);
-		m_sector = m_block.r8(1);
-		result = true;
-	}
-	else
-	{
-		result = false;
-	}
+		bool found = strtrimright_cbm(dirent.m_file_name) == path_part;
+		if (found)
+			result = dirent;
+		return found;
+	};
+	iterate_directory_entries(callback);
 	return result;
 }
 
 
 //-------------------------------------------------
-//  impl::block_iterator::data
+//  impl::iterate_directory_entries
 //-------------------------------------------------
 
-const void *impl::block_iterator::data() const
+void impl::iterate_directory_entries(const std::function<bool(const cbmdos_dirent &dirent)> &callback) const
 {
-	return m_block.rodata() + 2;
+	block_iterator iter(*this, 18, 1);
+	while (iter.next())
+	{
+		for (const cbmdos_dirent &ent : iter.dirent_data())
+		{
+			if (ent.m_file_type != 0x00)
+			{
+				if (callback(ent))
+					return;
+			}
+		}
+	}
 }
 
 
 //-------------------------------------------------
-//  impl::block_iterator::size
+//  impl::metadata_from_dirent
 //-------------------------------------------------
 
-u8 impl::block_iterator::size() const
-{
-	return m_track != 0x00 ? 254 : m_sector - 1;
-}
-
-
-//-------------------------------------------------
-//  impl::file ctor
-//-------------------------------------------------
-
-impl::file::file(impl &fs, cbmdos_dirent &&dirent)
-	: m_fs(fs)
-	, m_dirent(std::move(dirent))
-{
-}
-
-
-//-------------------------------------------------
-//  impl::file::drop_weak_references
-//-------------------------------------------------
-
-void impl::file::drop_weak_references()
-{
-}
-
-
-//-------------------------------------------------
-//  impl::file::metadata
-//-------------------------------------------------
-
-meta_data impl::file::metadata()
+meta_data impl::metadata_from_dirent(const cbmdos_dirent &dirent) const
 {
 	std::string file_type;
-	switch (m_dirent.m_file_type)
+	switch (dirent.m_file_type)
 	{
 	case 0x80:
 		file_type = "DEL";
@@ -506,19 +382,19 @@ meta_data impl::file::metadata()
 		file_type = "REL";
 		break;
 	default:
-		file_type = util::string_format("$0x02X", (int)m_dirent.m_file_type);
+		file_type = util::string_format("$0x02X", (int)dirent.m_file_type);
 		break;
 	}
 
 	// compute the file size
 	u32 file_size = 0;
-	block_iterator iter(m_fs, m_dirent.m_file_first_track, m_dirent.m_file_first_sector);
+	block_iterator iter(*this, dirent.m_file_first_track, dirent.m_file_first_sector);
 	while (iter.next())
 		file_size += iter.size();
 
 	// build the metadata and return it
 	meta_data result;
-	result.set(meta_name::name, strtrimright_cbm(m_dirent.m_file_name));
+	result.set(meta_name::name, strtrimright_cbm(dirent.m_file_name));
 	result.set(meta_name::file_type, std::move(file_type));
 	result.set(meta_name::length, file_size);
 	return result;
@@ -526,18 +402,75 @@ meta_data impl::file::metadata()
 
 
 //-------------------------------------------------
-//  impl::file::read_all
+//  impl::block_iterator ctor
 //-------------------------------------------------
 
-std::vector<u8> impl::file::read_all()
+impl::block_iterator::block_iterator(const impl &fs, u8 first_track, u8 first_sector)
+	: m_fs(fs)
+	, m_track(first_track)
+	, m_sector(first_sector)
 {
-	std::vector<u8> result;
+}
 
-	block_iterator iter(m_fs, m_dirent.m_file_first_track, m_dirent.m_file_first_sector);
-	while (iter.next())
-		result.insert(result.end(), (const u8 *)iter.data(), (const u8 *)iter.data() + iter.size());
 
+//-------------------------------------------------
+//  impl::block_iterator::next
+//-------------------------------------------------
+
+bool impl::block_iterator::next()
+{
+	bool result;
+	if (m_track != 0x00)
+	{
+		// check for the degenerate scenario where we have a cycle (this should not happen
+		// on a non-corrupt disk)
+		auto visited_tuple = std::make_tuple(m_track, m_sector);
+		if (m_visited_set.find(visited_tuple) != m_visited_set.end())
+			return false;
+		m_visited_set.insert(visited_tuple);
+
+		// with that out of the way, proceed
+		m_block = m_fs.read_sector(m_track, m_sector);
+		m_track = m_block.r8(0);
+		m_sector = m_block.r8(1);
+		result = true;
+	}
+	else
+	{
+		// the iterator has already completed
+		result = false;
+	}
 	return result;
+}
+
+
+//-------------------------------------------------
+//  impl::block_iterator::data
+//-------------------------------------------------
+
+const void *impl::block_iterator::data() const
+{
+	return m_block.rodata() + 2;
+}
+
+
+//-------------------------------------------------
+//  impl::block_iterator::dirent_data
+//-------------------------------------------------
+
+const std::array<impl::cbmdos_dirent, 4> &impl::block_iterator::dirent_data() const
+{
+	return *reinterpret_cast<const std::array<impl::cbmdos_dirent, 4> *>(m_block.rodata());
+}
+
+
+//-------------------------------------------------
+//  impl::block_iterator::size
+//-------------------------------------------------
+
+u8 impl::block_iterator::size() const
+{
+	return m_track != 0x00 ? 254 : m_sector - 1;
 }
 
 }
