aa1561999c1cd6b4070651d92b349b4d3c26e8a8
hap <happppp@users.noreply.github.com>
2022-07-02 21:19:34 +0200

    plygonet: still got a lockup at quantum 600

--- src/mame/konami/plygonet.cpp
+++ src/mame/konami/plygonet.cpp
@@ -1054,7 +1054,7 @@ void polygonet_state::plygonet(machine_config &config)
 	Z80(config, m_audiocpu, 8000000);
 	m_audiocpu->set_addrmap(AS_PROGRAM, &polygonet_state::sound_map);
 
-	config.set_maximum_quantum(attotime::from_hz(600)); // occasional lockup in-game otherwise
+	config.set_maximum_quantum(attotime::from_hz(6000)); // occasional lockup in-game otherwise
 
 	EEPROM_ER5911_8BIT(config, m_eeprom);
 
