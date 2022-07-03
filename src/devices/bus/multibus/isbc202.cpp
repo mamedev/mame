mamedev/mame

commit 4287970f0d963d380c55be9ebd6852e6222f5e25

multibus: various improvements
* expose card-independent bus address spaces
* support multiple slots and options
* route interrupts to/from cards

src/devices/bus/multibus/multibus.cpp
@@ -8,66 +8,82 @@
 
 *********************************************************************/
 
+/*
+ * TODO:
+ *  - shared interrupts
+ *  - vectored and non-vectored irq acknowledge
+ *  - inhibit lines
+ *  - lock/unlock
+ *  - bus arbitration/priority
+ */
+
 #include "emu.h"
 #include "multibus.h"
-#include "isbc202.h"
 
 // device type definition
+DEFINE_DEVICE_TYPE(MULTIBUS, multibus_device, "multibus", "Intel Multibus")
 DEFINE_DEVICE_TYPE(MULTIBUS_SLOT, multibus_slot_device, "multibus_slot", "Intel Multibus slot")
 
-multibus_slot_device::multibus_slot_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
-	: device_t(mconfig , MULTIBUS_SLOT , tag , owner , clock)
-	, device_single_card_slot_interface<device_multibus_interface>(mconfig , *this)
-	, m_irq_cb(*this)
-	, m_irqa_cb(*this)
-{
-	option_reset();
-	option_add("isbc202" , ISBC202);
-	set_default_option(nullptr);
-	set_fixed(false);
-}
-
-multibus_slot_device::~multibus_slot_device()
+multibus_device::multibus_device(machine_config const &mconfig, char const *tag, device_t *owner, u32 clock)
+	: device_t(mconfig, MULTIBUS, tag, owner, clock)
+	, device_memory_interface(mconfig, *this)
+	, m_mem_config("mem", ENDIANNESS_LITTLE, 16, 24)
+	, m_pio_config("pio", ENDIANNESS_LITTLE, 16, 16)
+	, m_int_cb(*this)
 {
 }
 
-void multibus_slot_device::install_io_rw(address_space& space)
+device_memory_interface::space_config_vector multibus_device::memory_space_config() const
 {
-	device_multibus_interface *card = get_card_device();
-	if (card) {
-		card->install_io_rw(space);
-	}
+	return space_config_vector { std::make_pair(AS_PROGRAM, &m_mem_config), std::make_pair(AS_IO, &m_pio_config) };
 }
 
-void multibus_slot_device::install_mem_rw(address_space& space)
+void multibus_device::device_start()
 {
-	device_multibus_interface *card = get_card_device();
-	if (card) {
-		card->install_mem_rw(space);
-	}
+	m_int_cb.resolve_all_safe();
+}
+
+multibus_slot_device::multibus_slot_device(machine_config const &mconfig, char const *tag, device_t *owner, u32 clock)
+	: device_t(mconfig, MULTIBUS_SLOT, tag, owner, clock)
+	, device_slot_interface(mconfig, *this)
+	, m_bus(*this, finder_base::DUMMY_TAG)
+{
 }
 
 void multibus_slot_device::device_start()
 {
-	m_irq_cb.resolve_all_safe();
-	m_irqa_cb.resolve_safe();
 }
 
-device_multibus_interface::device_multibus_interface(const machine_config &mconfig , device_t &device)
-	: device_interface(device , "multibus")
+void multibus_slot_device::device_resolve_objects()
+{
+	device_multibus_interface *const card(dynamic_cast<device_multibus_interface *>(get_card_device()));
+
+	if (card)
+		card->set_bus_device(*m_bus);
+}
+
+device_multibus_interface::device_multibus_interface(machine_config const &mconfig, device_t &device)
+	: device_interface(device, "multibus")
+	, m_bus(nullptr)
 {
 }
 
-device_multibus_interface::~device_multibus_interface()
+void device_multibus_interface::set_bus_device(multibus_device &bus_device)
 {
+	m_bus = &bus_device;
 }
 
-WRITE_LINE_MEMBER( multibus_slot_device::irq0_w ) { m_irq_cb[0](state); }
-WRITE_LINE_MEMBER( multibus_slot_device::irq1_w ) { m_irq_cb[1](state); }
-WRITE_LINE_MEMBER( multibus_slot_device::irq2_w ) { m_irq_cb[2](state); }
-WRITE_LINE_MEMBER( multibus_slot_device::irq3_w ) { m_irq_cb[3](state); }
-WRITE_LINE_MEMBER( multibus_slot_device::irq4_w ) { m_irq_cb[4](state); }
-WRITE_LINE_MEMBER( multibus_slot_device::irq5_w ) { m_irq_cb[5](state); }
-WRITE_LINE_MEMBER( multibus_slot_device::irq6_w ) { m_irq_cb[6](state); }
-WRITE_LINE_MEMBER( multibus_slot_device::irq7_w ) { m_irq_cb[7](state); }
-WRITE_LINE_MEMBER( multibus_slot_device::irqa_w ) { m_irqa_cb(state); }
+void device_multibus_interface::int_w(unsigned number, int state)
+{
+	switch (number)
+	{
+	case 0: int_w<0>(state); break;
+	case 1: int_w<1>(state); break;
+	case 2: int_w<2>(state); break;
+	case 3: int_w<3>(state); break;
+	case 4: int_w<4>(state); break;
+	case 5: int_w<5>(state); break;
+	case 6: int_w<6>(state); break;
+	case 7: int_w<7>(state); break;
+	}
+}

src/devices/bus/multibus/multibus.h
@@ -76,66 +76,74 @@
 
 #pragma once
 
-class device_multibus_interface;
-
-class multibus_slot_device : public device_t,
-							 public device_single_card_slot_interface<device_multibus_interface>
+class multibus_device
+	: public device_t
+	, public device_memory_interface
 {
 public:
-	multibus_slot_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
-	virtual ~multibus_slot_device();
+	multibus_device(machine_config const &mconfig, char const *tag, device_t *owner, u32 clock);
 
-	static constexpr unsigned BUS_CLOCK = 9830400;
+	// interrupt interface
+	template <unsigned I> auto int_callback() { return m_int_cb[I].bind(); }
+	template <unsigned I> void int_w(int state) { m_int_cb[I](state); }
 
-	// Install r/w functions in I/O space
-	void install_io_rw(address_space& space);
+protected:
+	// device_t overrides
+	virtual void device_start() override;
 
-	// Set memory space
-	void install_mem_rw(address_space& space);
+	// device_memory_interface overrides
+	virtual space_config_vector memory_space_config() const override;
 
-	auto irq0_callback() { return m_irq_cb[0].bind(); }
-	auto irq1_callback() { return m_irq_cb[1].bind(); }
-	auto irq2_callback() { return m_irq_cb[2].bind(); }
-	auto irq3_callback() { return m_irq_cb[3].bind(); }
-	auto irq4_callback() { return m_irq_cb[4].bind(); }
-	auto irq5_callback() { return m_irq_cb[5].bind(); }
-	auto irq6_callback() { return m_irq_cb[6].bind(); }
-	auto irq7_callback() { return m_irq_cb[7].bind(); }
-	auto irqa_callback() { return m_irqa_cb.bind(); }
+private:
+	address_space_config const m_mem_config;
+	address_space_config const m_pio_config;
 
-	DECLARE_WRITE_LINE_MEMBER( irq0_w );
-	DECLARE_WRITE_LINE_MEMBER( irq1_w );
-	DECLARE_WRITE_LINE_MEMBER( irq2_w );
-	DECLARE_WRITE_LINE_MEMBER( irq3_w );
-	DECLARE_WRITE_LINE_MEMBER( irq4_w );
-	DECLARE_WRITE_LINE_MEMBER( irq5_w );
-	DECLARE_WRITE_LINE_MEMBER( irq6_w );
-	DECLARE_WRITE_LINE_MEMBER( irq7_w );
-	DECLARE_WRITE_LINE_MEMBER( irqa_w );
+	devcb_write_line::array<8> m_int_cb;
+};
+
+class multibus_slot_device
+	: public device_t
+	, public device_slot_interface
+{
+public:
+	multibus_slot_device(machine_config const &mconfig, char const *tag, device_t *owner, u32 clock);
+
+	template <typename T, typename U>
+	multibus_slot_device(machine_config const &mconfig, char const *tag, device_t *owner, T &&bus_tag, U &&slot_options, char const *default_option, bool const fixed)
+		: multibus_slot_device(mconfig, tag, owner, DERIVED_CLOCK(1,1))
+	{
+		m_bus.set_tag(std::forward<T>(bus_tag));
+		option_reset();
+		slot_options(*this);
+		set_default_option(default_option);
+		set_fixed(fixed);
+	}
 
 protected:
 	virtual void device_start() override;
+	virtual void device_resolve_objects() override;
 
-	devcb_write_line::array<8> m_irq_cb;
-	devcb_write_line           m_irqa_cb;
-
+private:
+	required_device<multibus_device> m_bus;
 };
 
 class device_multibus_interface : public device_interface
 {
-public:
-	// Install r/w functions in I/O space
-	virtual void install_io_rw(address_space& space) = 0;
-
-	// Set CPU memory space
-	virtual void install_mem_rw(address_space& space) = 0;
-
 protected:
-	device_multibus_interface(const machine_config &mconfig , device_t &device);
-	virtual ~device_multibus_interface();
+	friend class multibus_slot_device;
+
+	device_multibus_interface(machine_config const &mconfig, device_t &device);
+
+	void set_bus_device(multibus_device &bus_device);
+
+	template <unsigned I> void int_w(int state) { m_bus->int_w<I>(state); }
+	void int_w(unsigned number, int state);
+
+	multibus_device *m_bus;
 };
 
 // device type declaration
+DECLARE_DEVICE_TYPE(MULTIBUS, multibus_device)
 DECLARE_DEVICE_TYPE(MULTIBUS_SLOT, multibus_slot_device)
 
 #endif /* MAME_BUS_MULTIBUS_MULTIBUS_H */

commit 85de47a59bbe0dd123207dd6ba9ae20bb0a5236c

imds2: fixed regression introduced by 4287970f0d9

src/devices/bus/multibus/isbc202.cpp
@@ -266,11 +266,11 @@ uint8_t isbc202_device::io_r(address_space &space, offs_t offset)
 
 	case 3:
 		// Read result byte (no auto XACK)
-		if (m_cpu == nullptr) {
-			m_cpu = dynamic_cast<cpu_device*>(&space.device());
+		if (!m_rd_2nd_pass) {
 			set_start(3 , true);
+			m_rd_2nd_pass = true;
 		} else {
-			m_cpu = nullptr;
+			m_rd_2nd_pass = false;
 			res = m_data_low_out;
 		}
 		break;
@@ -297,10 +297,10 @@ void isbc202_device::io_w(address_space &space, offs_t offset, uint8_t data)
 	case 4:
 	case 5:
 	case 6:
-		if (m_cpu != nullptr) {
-			LOG("CPU != NULL!\n");
+		if (m_rd_2nd_pass) {
+			LOG("Read 2nd pass when writing!\n");
 		}
-		m_cpu = dynamic_cast<cpu_device*>(&space.device());
+		m_rd_2nd_pass = false;
 		m_cpu_data = data;
 		set_start(offset , false);
 		break;
@@ -361,7 +361,7 @@ void isbc202_device::device_start()
 	save_item(NAME(m_op_us));
 	save_item(NAME(m_px_s1s0));
 	save_item(NAME(m_cmd));
-	save_item(NAME(m_cpu_rd));
+	save_item(NAME(m_rd_2nd_pass));
 	save_item(NAME(m_ready_in));
 	save_item(NAME(m_ready_ff));
 	save_item(NAME(m_gate_lower));
@@ -419,7 +419,7 @@ void isbc202_device::device_reset()
 	m_inputs[ IN_SEL_TIMEOUT ] = true;
 	m_inputs[ IN_SEL_F ] = false;
 
-	m_cpu = nullptr;
+	m_rd_2nd_pass = false;
 
 	m_irq = false;
 
@@ -740,18 +740,15 @@ void isbc202_device::set_output()
 		// 1        Reset RDY latches (0)
 		// 0        -
 		if (BIT(m_mask , 5)) {
-			if (m_cpu != nullptr) {
-				// Release CPU from wait state
-				LOG_BUS("CPU out of wait state\n");
-				m_cpu->trigger(1);
-				if (!m_cpu_rd) {
-					m_cpu = nullptr;
-				}
-				// Ensure the MCU executes a few instruction before the CPU
-				machine().scheduler().boost_interleave(attotime::from_usec(1) , attotime::from_usec(5));
+			// Release CPU from wait state
+			LOG_BUS("CPU out of wait state\n");
+			if (m_cpu_rd) {
+				set_wait_io_rd(0);
 			} else {
-				LOG("No CPU to wake up?\n");
+				set_wait_io_wr(0);
 			}
+			// Ensure the MCU executes a few instruction before the CPU
+			machine().scheduler().boost_interleave(attotime::from_usec(1) , attotime::from_usec(5));
 			m_inputs[ IN_SEL_START ] = false;
 		}
 		if (BIT(m_mask , 4)) {
@@ -1007,14 +1004,13 @@ void isbc202_device::set_start(uint8_t off , bool read)
 	m_cmd = off;
 	m_inputs[ IN_SEL_START ] = true;
 	// Put CPU in wait state
-	m_cpu->spin_until_trigger(1);
+	if (read) {
+		set_wait_io_rd(1);
+	} else {
+		set_wait_io_wr(1);
+	}
 	m_cpu_rd = read;
 	LOG_BUS("CPU in wait state (rd=%d)\n" , read);
-	if (read) {
-		// If CPU is suspended when reading, rewind PC so that the
-		// "IN" instruction is repeated when CPU is released
-		m_cpu->set_pc(m_cpu->pc() - 2);
-	}
 }
 
 void isbc202_device::set_rd_wr(bool new_rd , bool new_wr)

src/devices/bus/multibus/isbc202.h
@@ -100,7 +100,7 @@ private:
 	floppy_image_device *m_current_drive;
 	uint8_t m_px_s1s0;  // C-A16-11 & C-A16-13
 	uint8_t m_cmd;      // C-A8
-	cpu_device *m_cpu;  // When != nullptr: CPU is suspended in wait state
+	bool m_rd_2nd_pass;
 	bool m_cpu_rd;
 	uint8_t m_ready_in;
 	uint8_t m_ready_ff; // I-A44 & I-A43

src/devices/bus/multibus/multibus.cpp
@@ -30,6 +30,8 @@ multibus_device::multibus_device(machine_config const &mconfig, char const *tag,
 	, m_mem_config("mem", ENDIANNESS_LITTLE, 16, 24)
 	, m_pio_config("pio", ENDIANNESS_LITTLE, 16, 16)
 	, m_int_cb(*this)
+	, m_wait_rd_cb(*this)
+	, m_wait_wr_cb(*this)
 {
 }
 
@@ -41,6 +43,8 @@ device_memory_interface::space_config_vector multibus_device::memory_space_confi
 void multibus_device::device_start()
 {
 	m_int_cb.resolve_all_safe();
+	m_wait_rd_cb.resolve_safe();
+	m_wait_wr_cb.resolve_safe();
 }
 
 multibus_slot_device::multibus_slot_device(machine_config const &mconfig, char const *tag, device_t *owner, u32 clock)

src/devices/bus/multibus/multibus.h
@@ -87,6 +87,12 @@ public:
 	template <unsigned I> auto int_callback() { return m_int_cb[I].bind(); }
 	template <unsigned I> void int_w(int state) { m_int_cb[I](state); }
 
+	// wait signal (split for read/write operations)
+	auto wait_io_rd_cb() { return m_wait_rd_cb.bind(); }
+	auto wait_io_wr_cb() { return m_wait_wr_cb.bind(); }
+	void set_wait_io_rd(int state) { m_wait_rd_cb(state); }
+	void set_wait_io_wr(int state) { m_wait_wr_cb(state); }
+
 protected:
 	// device_t overrides
 	virtual void device_start() override;
@@ -99,6 +105,8 @@ private:
 	address_space_config const m_pio_config;
 
 	devcb_write_line::array<8> m_int_cb;
+	devcb_write_line m_wait_rd_cb;
+	devcb_write_line m_wait_wr_cb;
 };
 
 class multibus_slot_device
@@ -138,6 +146,9 @@ protected:
 
 	template <unsigned I> void int_w(int state) { m_bus->int_w<I>(state); }
 	void int_w(unsigned number, int state);
+
+	void set_wait_io_rd(int state) { m_bus->set_wait_io_rd(state); }
+	void set_wait_io_wr(int state) { m_bus->set_wait_io_wr(state); }
 
 	multibus_device *m_bus;
 };

src/mame/intel/imds2.cpp
@@ -103,6 +103,9 @@ public:
 
 	void imds2(machine_config &config);
 
+	DECLARE_WRITE_LINE_MEMBER(wait_io_rd);
+	DECLARE_WRITE_LINE_MEMBER(wait_io_wr);
+
 private:
 	uint8_t ipc_mem_read(offs_t offset);
 	void ipc_mem_write(offs_t offset, uint8_t data);
@@ -304,7 +307,33 @@ void imds2_state::imds2(machine_config &config)
 	m_ioc->parallel_int_cb().set(m_ipclocpic, FUNC(pic8259_device::ir5_w));
 
 	MULTIBUS(config, m_bus, 9'830'400);
+	m_bus->wait_io_rd_cb().set(FUNC(imds2_state::wait_io_rd));
+	m_bus->wait_io_wr_cb().set(FUNC(imds2_state::wait_io_wr));
 	MULTIBUS_SLOT(config, m_slot, m_bus, imds2_cards, nullptr, false); // FIXME: isbc202
+}
+
+WRITE_LINE_MEMBER(imds2_state::wait_io_rd)
+{
+	if (state) {
+		// Put CPU in wait state
+		m_ipccpu->spin_until_trigger(1);
+		// Rewind PC so that the "IN" instruction is repeated when CPU is released
+		m_ipccpu->set_pc(m_ipccpu->pc() - 2);
+	} else {
+		// Release CPU from wait state
+		m_ipccpu->trigger(1);
+	}
+}
+
+WRITE_LINE_MEMBER(imds2_state::wait_io_wr)
+{
+	if (state) {
+		// Put CPU in wait state
+		m_ipccpu->spin_until_trigger(1);
+	} else {
+		// Release CPU from wait state
+		m_ipccpu->trigger(1);
+	}
 }
 
 ROM_START(imds2)