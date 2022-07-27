3faaf1f6f3330e102ac2f1c11c341c718883b784
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2022-07-27 13:52:56 +0100

    cloned the mcs51 core as axc51, stripped it back, will allow monon work to continue without making a mess of the original mcs51 core.

---
+++ src/devices/cpu/axc51/axc51.h
@@ -0,0 +1,368 @@
+// license:BSD-3-Clause
+// copyright-holders:Steve Ellenoff, Manuel Abadia, Couriersud
+
+#ifndef MAME_CPU_AXC51_AXC51_H
+#define MAME_CPU_AXC51_AXC51_H
+
+#pragma once
+
+
+enum
+{
+	AXC51_PC=1, AXC51_SP, AXC51_PSW, AXC51_ACC, AXC51_B, AXC51_DPTR, AXC51_DPH, AXC51_DPL, AXC51_IE, AXC51_IP,
+	AXC51_P0, AXC51_P1, AXC51_P2, AXC51_P3,
+	AXC51_R0, AXC51_R1, AXC51_R2, AXC51_R3, AXC51_R4, AXC51_R5, AXC51_R6, AXC51_R7, AXC51_RB,
+	AXC51_TCON, AXC51_TMOD, AXC51_TL0, AXC51_TL1, AXC51_TH0, AXC51_TH1
+};
+
+enum
+{
+	AXC51_INT0_LINE = 0,    /* P3.2: External Interrupt 0 */
+	AXC51_INT1_LINE,        /* P3.3: External Interrupt 1 */
+	AXC51_RX_LINE,          /* P3.0: Serial Port Receive Line */
+	AXC51_T0_LINE,          /* P3,4: Timer 0 External Input */
+	AXC51_T1_LINE,          /* P3.5: Timer 1 External Input */
+	AXC51_T2_LINE,          /* P1.0: Timer 2 External Input */
+	AXC51_T2EX_LINE,        /* P1.1: Timer 2 Capture Reload Trigger */
+};
+
+
+class axc51base_cpu_device : public cpu_device
+{
+public:
+	/* At least CMOS devices may be forced to read from ports configured as output.
+	 * All you need is a low impedance output connect to the port.
+	 */
+	void set_port_forced_input(uint8_t port, uint8_t forced_input) { m_forced_inputs[port] = forced_input; }
+
+	template <unsigned N> auto port_in_cb() { return m_port_in_cb[N].bind(); }
+	template <unsigned N> auto port_out_cb() { return m_port_out_cb[N].bind(); }
+	auto serial_rx_cb() { return m_serial_rx_cb.bind(); }
+	auto serial_tx_cb() { return m_serial_tx_cb.bind(); }
+
+	void program_internal(address_map &map);
+	void data_internal(address_map &map);
+protected:
+	// construction/destruction
+	axc51base_cpu_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock, int program_width, int data_width, uint8_t features = 0);
+	axc51base_cpu_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock, address_map_constructor program_map, address_map_constructor data_map, int program_width, int data_width, uint8_t features = 0);
+
+	// device-level overrides
+	virtual void device_start() override;
+	virtual void device_reset() override;
+
+	// device_execute_interface overrides
+	virtual uint64_t execute_clocks_to_cycles(uint64_t clocks) const noexcept override { return (clocks + 12 - 1) / 12; }
+	virtual uint64_t execute_cycles_to_clocks(uint64_t cycles) const noexcept override { return (cycles * 12); }
+	virtual uint32_t execute_min_cycles() const noexcept override { return 1; }
+	virtual uint32_t execute_max_cycles() const noexcept override { return 20; }
+	virtual uint32_t execute_input_lines() const noexcept override { return 6; }
+	virtual void execute_run() override;
+	virtual void execute_set_input(int inputnum, int state) override;
+
+	// device_memory_interface overrides
+	virtual space_config_vector memory_space_config() const override;
+
+	// device_state_interface overrides
+	virtual void state_string_export(const device_state_entry &entry, std::string &str) const override;
+
+	// device_disasm_interface overrides
+	virtual std::unique_ptr<util::disasm_interface> create_disassembler() override;
+
+protected:
+	address_space_config m_program_config;
+	address_space_config m_data_config;
+	address_space_config m_io_config;
+
+	//Internal stuff
+	uint16_t  m_ppc;            //previous pc
+	uint16_t  m_pc;             //current pc
+	uint16_t  m_features;       //features of this cpu
+	uint8_t   m_rwm;            //Signals that the current instruction is a read/write/modify instruction
+
+	int     m_inst_cycles;        /* cycles for the current instruction */
+	const uint32_t m_rom_size;    /* size (in bytes) of internal program ROM/EPROM */
+	int     m_ram_mask;           /* second ram bank for indirect access available ? */
+	int     m_num_interrupts;     /* number of interrupts supported */
+	int     m_recalc_parity;      /* recalculate parity before next instruction */
+	uint32_t  m_last_line_state;    /* last state of input lines line */
+	int     m_t0_cnt;             /* number of 0->1 transitions on T0 line */
+	int     m_t1_cnt;             /* number of 0->1 transitions on T1 line */
+	int     m_t2_cnt;             /* number of 0->1 transitions on T2 line */
+	int     m_t2ex_cnt;           /* number of 0->1 transitions on T2EX line */
+	int     m_cur_irq_prio;       /* Holds value of the current IRQ Priority Level; -1 if no irq */
+	uint8_t   m_irq_active;         /* mask which irq levels are serviced */
+	uint8_t   m_irq_prio[8];        /* interrupt priority */
+
+	uint8_t   m_forced_inputs[4];   /* allow read even if configured as output */
+
+	// JB-related hacks
+	uint8_t m_last_op;
+	uint8_t m_last_bit;
+
+	int     m_icount;
+
+	struct axc51_uart
+	{
+		uint8_t   data_out;       //Data to send out
+		uint8_t   bits_to_send;   //How many bits left to send when transmitting out the serial port
+
+		int     smod_div;       /* signal divided by 2^SMOD */
+		int     rx_clk;         /* rx clock */
+		int     tx_clk;         /* tx clock */
+		uint8_t   delay_cycles;   //Gross Hack;
+	} m_uart;            /* internal uart */
+
+	/* Internal Ram */
+	required_shared_ptr<uint8_t> m_sfr_ram;           /* 128 SFR - these are in 0x80 - 0xFF */
+	required_shared_ptr<uint8_t> m_scratchpad;        /* 128 RAM (8031/51) + 128 RAM in second bank (8032/52) */
+
+	/* SFR Callbacks */
+	virtual void sfr_write(size_t offset, uint8_t data);
+	virtual uint8_t sfr_read(size_t offset);
+
+	/* Memory spaces */
+	memory_access<16, 0, 0, ENDIANNESS_LITTLE>::cache m_program;
+	memory_access< 9, 0, 0, ENDIANNESS_LITTLE>::specific m_data;
+	memory_access<17, 0, 0, ENDIANNESS_LITTLE>::specific m_io;
+
+	devcb_read8::array<4> m_port_in_cb;
+	devcb_write8::array<4> m_port_out_cb;
+
+	/* Serial Port TX/RX Callbacks */
+	devcb_write8 m_serial_tx_cb;    //Call back function when sending data out of serial port
+	devcb_read8 m_serial_rx_cb;    //Call back function to retrieve data when receiving serial port data
+
+	// for the debugger
+	uint8_t m_rtemp;
+
+	static const uint8_t axc51_cycles[256];
+
+	uint8_t iram_iread(offs_t a);
+	void iram_iwrite(offs_t a, uint8_t d);
+	void clear_current_irq();
+	uint8_t r_acc();
+	uint8_t r_psw();
+	virtual offs_t external_ram_iaddr(offs_t offset, offs_t mem_mask);
+	uint8_t iram_read(size_t offset);
+	void iram_write(size_t offset, uint8_t data);
+	void push_pc();
+	void pop_pc();
+	void set_parity();
+	uint8_t bit_address_r(uint8_t offset);
+	void bit_address_w(uint8_t offset, uint8_t bit);
+	void do_add_flags(uint8_t a, uint8_t data, uint8_t c);
+	void do_sub_flags(uint8_t a, uint8_t data, uint8_t c);
+	void transmit_receive(int source);
+	void update_timer_t0(int cycles);
+	void update_timer_t1(int cycles);
+	void update_timer_t2(int cycles);
+	void update_timers(int cycles);
+	void serial_transmit(uint8_t data);
+	void serial_receive();
+	void update_serial(int cycles);
+	void update_irq_prio(uint8_t ipl, uint8_t iph);
+	void execute_op(uint8_t op);
+	void check_irqs();
+	void burn_cycles(int cycles);
+	void acall(uint8_t r);
+	void add_a_byte(uint8_t r);
+	void add_a_mem(uint8_t r);
+	void add_a_ir(uint8_t r);
+	void add_a_r(uint8_t r);
+	void addc_a_byte(uint8_t r);
+	void addc_a_mem(uint8_t r);
+	void addc_a_ir(uint8_t r);
+	void addc_a_r(uint8_t r);
+	void ajmp(uint8_t r);
+	void anl_mem_a(uint8_t r);
+	void anl_mem_byte(uint8_t r);
+	void anl_a_byte(uint8_t r);
+	void anl_a_mem(uint8_t r);
+	void anl_a_ir(uint8_t r);
+	void anl_a_r(uint8_t r);
+	void anl_c_bitaddr(uint8_t r);
+	void anl_c_nbitaddr(uint8_t r);
+	void cjne_a_byte(uint8_t r);
+	void cjne_a_mem(uint8_t r);
+	void cjne_ir_byte(uint8_t r);
+	void cjne_r_byte(uint8_t r);
+	void clr_bitaddr(uint8_t r);
+	void clr_c(uint8_t r);
+	void clr_a(uint8_t r);
+	void cpl_bitaddr(uint8_t r);
+	void cpl_c(uint8_t r);
+	void cpl_a(uint8_t r);
+	void da_a(uint8_t r);
+	void dec_a(uint8_t r);
+	void dec_mem(uint8_t r);
+	void dec_ir(uint8_t r);
+	void dec_r(uint8_t r);
+	void div_ab(uint8_t r);
+	void djnz_mem(uint8_t r);
+	void djnz_r(uint8_t r);
+	void inc_a(uint8_t r);
+	void inc_mem(uint8_t r);
+	void inc_ir(uint8_t r);
+	void inc_r(uint8_t r);
+	void inc_dptr(uint8_t r);
+	void jb(uint8_t r);
+	void jbc(uint8_t r);
+	void jc(uint8_t r);
+	void jmp_iadptr(uint8_t r);
+	void jnb(uint8_t r);
+	void jnc(uint8_t r);
+	void jnz(uint8_t r);
+	void jz(uint8_t r);
+	void lcall(uint8_t r);
+	void ljmp(uint8_t r);
+	void mov_a_byte(uint8_t r);
+	void mov_a_mem(uint8_t r);
+	void mov_a_ir(uint8_t r);
+	void mov_a_r(uint8_t r);
+	void mov_mem_byte(uint8_t r);
+	void mov_mem_mem(uint8_t r);
+	void mov_ir_byte(uint8_t r);
+	void mov_r_byte(uint8_t r);
+	void mov_mem_ir(uint8_t r);
+	void mov_mem_r(uint8_t r);
+	void mov_dptr_byte(uint8_t r);
+	void mov_bitaddr_c(uint8_t r);
+	void mov_ir_mem(uint8_t r);
+	void mov_r_mem(uint8_t r);
+	void mov_mem_a(uint8_t r);
+	void mov_ir_a(uint8_t r);
+	void mov_r_a(uint8_t r);
+	void movc_a_iapc(uint8_t r);
+	void mov_c_bitaddr(uint8_t r);
+	void movc_a_iadptr(uint8_t r);
+	void movx_a_idptr(uint8_t r);
+	void movx_a_ir(uint8_t r);
+	void movx_idptr_a(uint8_t r);
+	void movx_ir_a(uint8_t r);
+	void mul_ab(uint8_t r);
+	void nop(uint8_t r);
+	void orl_mem_a(uint8_t r);
+	void orl_mem_byte(uint8_t r);
+	void orl_a_byte(uint8_t r);
+	void orl_a_mem(uint8_t r);
+	void orl_a_ir(uint8_t r);
+	void orl_a_r(uint8_t r);
+	void orl_c_bitaddr(uint8_t r);
+	void orl_c_nbitaddr(uint8_t r);
+	void pop(uint8_t r);
+	void push(uint8_t r);
+	void ret(uint8_t r);
+	void reti(uint8_t r);
+	void rl_a(uint8_t r);
+	void rlc_a(uint8_t r);
+	void rr_a(uint8_t r);
+	void rrc_a(uint8_t r);
+	void setb_c(uint8_t r);
+	void setb_bitaddr(uint8_t r);
+	void sjmp(uint8_t r);
+	void subb_a_byte(uint8_t r);
+	void subb_a_mem(uint8_t r);
+	void subb_a_ir(uint8_t r);
+	void subb_a_r(uint8_t r);
+	void swap_a(uint8_t r);
+	void xch_a_mem(uint8_t r);
+	void xch_a_ir(uint8_t r);
+	void xch_a_r(uint8_t r);
+	void xchd_a_ir(uint8_t r);
+	void xrl_mem_a(uint8_t r);
+	void xrl_mem_byte(uint8_t r);
+	void xrl_a_byte(uint8_t r);
+	void xrl_a_mem(uint8_t r);
+	void xrl_a_ir(uint8_t r);
+	void xrl_a_r(uint8_t r);
+	void illegal(uint8_t r);
+};
+
+
+
+DECLARE_DEVICE_TYPE(AXC51CORE, axc51core_cpu_device)
+DECLARE_DEVICE_TYPE(AX208, ax208_cpu_device)
+DECLARE_DEVICE_TYPE(AX208P, ax208p_cpu_device)
+
+
+
+class axc51core_cpu_device : public axc51base_cpu_device
+{
+public:
+	// construction/destruction
+	axc51core_cpu_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+	axc51core_cpu_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock,  address_map_constructor program_map, address_map_constructor data_map, int program_width, int data_width, uint8_t features = 0);
+
+protected:
+	virtual std::unique_ptr<util::disasm_interface> create_disassembler() override;
+};
+
+
+class ax208_cpu_device : public axc51core_cpu_device
+{
+public:
+	// construction/destruction
+	ax208_cpu_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+	void set_spi_ptr(uint8_t* ptr, size_t size) { m_spiptr = ptr; m_spisize = size; }
+
+protected:
+	ax208_cpu_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock);
+
+	virtual void device_reset() override;
+
+	virtual std::unique_ptr<util::disasm_interface> create_disassembler() override;
+	virtual const tiny_rom_entry *device_rom_region() const override;
+
+	void ax208_internal_program_mem(address_map &map);
+	void ax208_internal_data_mem(address_map &map);
+
+	virtual void sfr_write(size_t offset, uint8_t data) override;
+	virtual uint8_t sfr_read(size_t offset) override;
+
+	enum
+	{
+		AXC51_DPCON = 0x86,
+
+		AXC51_IRTCON = 0x9f,
+
+		AXC51_SPIDMAADR = 0xd6,
+		AXC51_SPIDMACNT = 0xd7,
+		AXC51_SPICON = 0xd8,
+		AXC51_SPIBUF = 0xd9,
+		AXC51_SPIBAUD = 0xda
+	};
+
+	uint8_t spicon_r();
+	uint8_t spibuf_r();
+	uint8_t dpcon_r();
+
+	void spidmaadr_w(uint8_t data);
+	void spidmacnt_w(uint8_t data);
+
+	void spicon_w(uint8_t data);
+	void spibuf_w(uint8_t data);
+	void spibaud_w(uint8_t data);
+	void dpcon_w(uint8_t data);
+
+	uint8_t* m_spiptr;
+	size_t m_spisize;
+
+	uint32_t m_spiaddr;
+
+	virtual offs_t external_ram_iaddr(offs_t offset, offs_t mem_mask) override;
+};
+
+class ax208p_cpu_device : public ax208_cpu_device
+{
+public:
+	// construction/destruction
+	ax208p_cpu_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+protected:
+	virtual const tiny_rom_entry *device_rom_region() const override;
+};
+
+
+#endif // MAME_CPU_AXC51_AXC51_H
