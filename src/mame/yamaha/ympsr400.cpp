e43bc4bc3e9d9872439bb474ab1db576a6312e4f
AJR <ajrhacker@users.noreply.github.com>
2022-09-16 23:24:58 -0400

    Emulate MN18801A MMU

--- src/mame/yamaha/ympsr400.cpp
+++ src/mame/yamaha/ympsr400.cpp
@@ -9,6 +9,8 @@
 #include "emu.h"
 #include "cpu/m6805/m6805.h"
 #include "cpu/mn1880/mn1880.h"
+#include "sound/multipcm.h"
+#include "speaker.h"
 
 namespace {
 
@@ -37,14 +39,15 @@ private:
 
 void psr400_state::program_map(address_map &map)
 {
-	// TODO: 2 MB external program memory space using MMU
-	map(0x0000, 0xffff).rom().region("program", 0);
+	// 2 MB external program memory space using MMU
+	map(0x000000, 0x1fffff).rom().region("program", 0);
 }
 
 void psr400_state::data_map(address_map &map)
 {
-	// TODO: 2 MB external data memory space using MMU
-	map(0x0100, 0x1fff).ram();
+	// 2 MB external data memory space using MMU
+	map(0x000080, 0x03ffff).mirror(0xc0000).ram(); // 2x 1M-bit PSRAM (only one on PSR-400)
+	map(0x100000, 0x10000f).mirror(0xffff0).rw("gew8", FUNC(multipcm_device::read), FUNC(multipcm_device::write));
 }
 
 static INPUT_PORTS_START(psr500)
@@ -52,14 +55,20 @@ INPUT_PORTS_END
 
 void psr400_state::psr500(machine_config &config)
 {
-	MN1880(config, m_maincpu, 10_MHz_XTAL); // MN18801A (also has 500 kHz secondary resonator)
+	MN18801A(config, m_maincpu, 10_MHz_XTAL); // MN18801A (also has 500 kHz secondary resonator)
 	m_maincpu->set_addrmap(AS_PROGRAM, &psr400_state::program_map);
 	m_maincpu->set_addrmap(AS_DATA, &psr400_state::data_map);
 
 	HD63705(config, m_mpscpu, 8_MHz_XTAL).set_disable(); // HD63B05V0D73P (mislabeled HD63B50 on schematic)
 
-	//YMW258F(config, "gew8", 9.4_MHz_XTAL);
-	//YM3413(config, "ldsp");
+	SPEAKER(config, "lspeaker").front_left();
+	SPEAKER(config, "rspeaker").front_right();
+
+	multipcm_device &gew8(MULTIPCM(config, "gew8", 9.4_MHz_XTAL)); // YMW-258-F
+	gew8.add_route(1, "lspeaker", 1.0);
+	gew8.add_route(0, "rspeaker", 1.0);
+
+	//YM3413(config, "ldsp"); // PSR-500 only (has its own 256K-bit PSRAM)
 }
 
 ROM_START(psr500)
@@ -70,7 +79,7 @@ ROM_START(psr500)
 	ROM_RELOAD(0x0c0000, 0x040000)
 	ROM_LOAD("xj921b0.ic5", 0x100000, 0x100000, CRC(dd1a8afc) SHA1(5d5b47577faeed165f0bd73283f148d112e4d1e9))
 
-	ROM_REGION(0x100000, "waverom", 0)
+	ROM_REGION(0x100000, "gew8", 0)
 	ROM_LOAD("xj426b0.ic3", 0x000000, 0x100000, CRC(ef566734) SHA1(864f5689dbaa82bd8a1be4e53bdb21ec71be03cc))
 
 	ROM_REGION(0x1000, "mpscpu", 0)
