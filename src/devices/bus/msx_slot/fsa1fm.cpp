942945f3e54c1acc6547f4b098525914ad166bef
Wilbert Pol <wilbertpol@users.noreply.github.com>
2022-09-17 13:39:10 +0100

    msx_slot/fsa1fm.cpp: Start adding support for fsa1fm modem and firmware. Modem i8251 is not connected yet

--- src/devices/bus/msx_slot/fsa1fm.cpp
+++ src/devices/bus/msx_slot/fsa1fm.cpp
@@ -11,12 +11,134 @@
 
 
 DEFINE_DEVICE_TYPE(MSX_SLOT_FSA1FM, msx_slot_fsa1fm_device, "msx_slot_fs1afm", "MSX Internal FS-A1FM")
+DEFINE_DEVICE_TYPE(MSX_SLOT_FSA1FM2, msx_slot_fsa1fm2_device, "msx_slot_fs1afm2", "MSX Internal FS-A1FM part 2")
+
+
+msx_slot_fsa1fm2_device::msx_slot_fsa1fm2_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
+	: device_t(mconfig, MSX_SLOT_FSA1FM2, tag, owner, clock)
+	, msx_internal_slot_interface(mconfig, *this)
+	, m_rom_region(*this, finder_base::DUMMY_TAG)
+	, m_region_offset(0)
+	, m_rom(nullptr)
+{
+}
+
+void msx_slot_fsa1fm2_device::device_start()
+{
+	// Sanity checks
+	// There is also a 0x200000 sized rom?
+	if (m_rom_region->bytes() != 0x100000)
+	{
+		fatalerror("Memory region '%s' is not the correct size for the FS-A1FM firmware\n", m_rom_region.finder_tag());
+	}
+
+	m_rom = m_rom_region->base() + m_region_offset;
+
+	save_item(NAME(m_selected_bank2));
+
+	m_selected_bank2[0] = 0x00;
+	m_selected_bank2[1] = 0x00;
+	m_selected_bank2[2] = 0x00;
+	m_selected_bank2[3] = 0x00;
+
+	map_bank();
+}
+
+
+void msx_slot_fsa1fm2_device::device_reset()
+{
+	m_selected_bank2[0] = 0x00;
+	m_selected_bank2[1] = 0x00;
+	m_selected_bank2[2] = 0x00;
+	m_selected_bank2[3] = 0x00;
+
+	map_bank();
+}
+
+
+void msx_slot_fsa1fm2_device::device_post_load()
+{
+	map_bank();
+}
+
+
+void msx_slot_fsa1fm2_device::map_bank()
+{
+	const uint32_t size_mask = m_rom_region->bytes() - 1;
+
+	m_bank_base2[0] = m_rom + ((m_selected_bank2[0] * 0x2000) & size_mask);
+	m_bank_base2[1] = m_rom + ((m_selected_bank2[1] * 0x2000) & size_mask);
+	m_bank_base2[2] = m_rom + ((m_selected_bank2[2] * 0x2000) & size_mask);
+	m_bank_base2[3] = m_rom + ((m_selected_bank2[3] * 0x2000) & size_mask);
+}
+
+
+uint8_t msx_slot_fsa1fm2_device::read(offs_t offset)
+{
+	if (offset < 0x4000)
+	{
+		return m_rom[offset & 0x3fff];
+	}
+	if (offset < 0x6000)
+	{
+		return m_bank_base2[0][offset & 0x1fff];
+	}
+	if (offset < 0x8000)
+	{
+		return m_bank_base2[1][offset & 0x1fff];
+	}
+	if (offset < 0xa000)
+	{
+		return m_bank_base2[2][offset & 0x1fff];
+	}
+	if (offset < 0xc000)
+	{
+		return m_bank_base2[3][offset & 0x1fff];
+	}
+	return 0xff;
+}
+
+
+void msx_slot_fsa1fm2_device::write(offs_t offset, uint8_t data)
+{
+	logerror("%s: write %04x : %02x\n", machine().describe_context(), offset, data);
+	if (offset == 0x6000)
+	{
+		m_selected_bank2[0] = data;
+		map_bank();
+	}
+	if (offset == 0x6800)
+	{
+		m_selected_bank2[1] = data;
+		map_bank();
+	}
+	if (offset == 0x7000)
+	{
+		m_selected_bank2[2] = data;
+		map_bank();
+	}
+	if (offset == 0x7800)
+	{
+		m_selected_bank2[3] = data;
+		map_bank();
+	}
+	if (offset == 0x7ff9)
+	{
+		// writing $04 enables banking registers 6000 / 6800 / 7000 / 7800 ?
+		// followed by read from 7ff2??
+	}
+}
+
+
 
 
 msx_slot_fsa1fm_device::msx_slot_fsa1fm_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
 	: device_t(mconfig, MSX_SLOT_FSA1FM, tag, owner, clock)
 	, msx_internal_slot_interface(mconfig, *this)
 	, m_nvram(*this, "nvram")
+	, m_i8251(*this, "i8251")
+	, m_i8255(*this, "i8255")
+	, m_switch_port(*this, "SWITCH")
 	, m_rom_region(*this, finder_base::DUMMY_TAG)
 	, m_region_offset(0)
 	, m_rom(nullptr)
@@ -27,9 +149,32 @@ msx_slot_fsa1fm_device::msx_slot_fsa1fm_device(const machine_config &mconfig, co
 }
 
 
+static INPUT_PORTS_START(fsa1fm)
+	PORT_START("SWITCH")
+	PORT_CONFNAME(0x04, 0x00, "Firmware is")
+	PORT_CONFSETTING(0x04, "disabled")
+	PORT_CONFSETTING(0x00, "enabled")
+INPUT_PORTS_END
+
+
+ioport_constructor msx_slot_fsa1fm_device::device_input_ports() const
+{
+	return INPUT_PORTS_NAME(fsa1fm);
+}
+
+
 void msx_slot_fsa1fm_device::device_add_mconfig(machine_config &config)
 {
 	NVRAM(config, m_nvram, nvram_device::DEFAULT_ALL_0);
+
+	I8251(config, m_i8251, 0);
+	// TODO
+
+	I8255(config, m_i8255);
+	m_i8255->out_pa_callback().set(FUNC(msx_slot_fsa1fm_device::i8255_port_a_w));
+	m_i8255->out_pb_callback().set(FUNC(msx_slot_fsa1fm_device::i8255_port_b_w));
+	m_i8255->in_pc_callback().set(FUNC(msx_slot_fsa1fm_device::i8255_port_c_r));
+	// TODO
 }
 
 
@@ -47,6 +192,10 @@ void msx_slot_fsa1fm_device::device_start()
 	m_nvram->set_base(m_sram.data(), SRAM_SIZE);
 
 	save_item(NAME(m_selected_bank));
+
+	m_selected_bank[0] = 0x00;
+	m_selected_bank[1] = 0x00;
+	m_selected_bank[2] = 0x00;
 
 	map_bank();
 }
@@ -70,25 +219,60 @@ void msx_slot_fsa1fm_device::device_post_load()
 
 void msx_slot_fsa1fm_device::map_bank()
 {
-	m_bank_base_0000 = m_rom + m_selected_bank[0] * 0x2000;
-	m_bank_base_4000 = m_rom + m_selected_bank[1] * 0x2000;
-	m_bank_base_8000 = m_rom + m_selected_bank[2] * 0x2000;
+	const uint32_t size_mask = m_rom_region->bytes() - 1;
+	// Odd to have this bank per 0x2000?
+	m_bank_base_0000 = m_rom + ((m_selected_bank[0] * 0x2000) & size_mask);
+	m_bank_base_4000 = m_rom + ((m_selected_bank[1] * 0x2000) & size_mask);
+	m_bank_base_8000 = m_rom + ((m_selected_bank[2] * 0x2000) & size_mask);
+}
+
+
+void msx_slot_fsa1fm_device::i8255_port_a_w(uint8_t data)
+{
+	logerror("port A write %02x\n", data);
+	m_selected_bank[1] = data & 0x3f;
+	map_bank();
+}
+
+
+void msx_slot_fsa1fm_device::i8255_port_b_w(uint8_t data)
+{
+	logerror("port B write %02x\n", data);
+}
+
+
+uint8_t msx_slot_fsa1fm_device::i8255_port_c_r()
+{
+	logerror("port C read\n");
+	return m_switch_port->read();
 }
 
 
 uint8_t msx_slot_fsa1fm_device::read(offs_t offset)
 {
 	if (offset < 0x4000)
+	{
 		return m_bank_base_0000[offset & 0x3fff];
+	}
 	if (offset < 0x8000)
 	{
 		// TODO: How much of the sram is visible?
 		if (offset >= 0x6000 && offset < 0x6400)
 			return m_sram[offset & 0x3ff];
+		if (offset == 0x7fc0)
+		{
+			return m_i8251->data_r();
+		}
+		if (offset >= 0x7fc4 && offset <= 0x7fc7)
+		{
+			return m_i8255->read(offset & 0x03);
+		}
 		return m_bank_base_4000[offset & 0x3fff];
 	}
 	if (offset < 0xc000)
+	{
 		return m_bank_base_8000[offset & 0x3fff];
+	}
 	return 0xff;
 }
 
@@ -101,7 +285,7 @@ void msx_slot_fsa1fm_device::write(offs_t offset, uint8_t data)
 		m_sram[offset & 0x3ff] = data;
 		return;
 	}
-	logerror("write %04x : %02x\n", offset, data);
+	logerror("%s: write %04x : %02x\n", machine().describe_context(), offset, data);
 	// Should switch to bank #0 in slot #1
 	// 7fc7 <- 89
 	// 7fc4 <- c0
@@ -141,8 +325,31 @@ void msx_slot_fsa1fm_device::write(offs_t offset, uint8_t data)
 	// 54b1: ld a,(7fc1) and 05 cp 05
 	// 54bc: ld (7fc1),a ld (6083),a ret
 	// 54c3: xor a ld (7fc1),a push af pop af ld (7fc1),a push af pop af ld (7fc1),a  push af pop af ld a,40 ld (7fc1),a push af pop af ld a,(6082) ld (7fc1),a push af pop af ld a,(7fc0)
-	if (offset == 0x7fc4) {
-		m_selected_bank[1] = data & 0x3f;
-		map_bank();
+	if (offset == 0x7ee7)
+	{
+	}
+	if (offset == 0x7ee8)
+	{
+	}
+	if (offset == 0x7ee9)
+	{		
+	}
+	if (offset == 0x7fc0)
+	{
+		m_i8251->data_w(data);
+	}
+	if (offset == 0x7fc1)
+	{
+		m_i8251->control_w(data);
+	}
+	if (offset >= 0x7fc4 && offset <= 0x7fc7)
+	{
+		m_i8255->write(offset & 0x03, data);
+	}
+	if (offset == 0x7fc8)
+	{
+	}
+	if (offset == 0x7fcc)
+	{
 	}
 }
--- src/devices/bus/msx_slot/fsa1fm.h
+++ src/devices/bus/msx_slot/fsa1fm.h
@@ -6,11 +6,40 @@
 #pragma once
 
 #include "slot.h"
+#include "machine/i8251.h"
+#include "machine/i8255.h"
 #include "machine/nvram.h"
 
 
 DECLARE_DEVICE_TYPE(MSX_SLOT_FSA1FM, msx_slot_fsa1fm_device)
+DECLARE_DEVICE_TYPE(MSX_SLOT_FSA1FM2, msx_slot_fsa1fm2_device)
 
+
+class msx_slot_fsa1fm2_device : public device_t, public msx_internal_slot_interface
+{
+public:
+	msx_slot_fsa1fm2_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+	// configuration helpers
+	void set_rom_start(const char *region, uint32_t offset) { m_rom_region.set_tag(region); m_region_offset = offset; }
+
+	virtual uint8_t read(offs_t offset) override;
+	virtual void write(offs_t offset, uint8_t data) override;
+
+protected:
+	virtual void device_start() override;
+	virtual void device_reset() override;
+	virtual void device_post_load() override;
+
+private:
+	required_memory_region m_rom_region;
+	uint32_t m_region_offset;
+	const uint8_t *m_rom;
+	uint8_t m_selected_bank2[4];
+	const uint8_t *m_bank_base2[4];
+
+	void map_bank();
+};
 
 class msx_slot_fsa1fm_device : public device_t, public msx_internal_slot_interface
 {
@@ -28,11 +57,15 @@ protected:
 	virtual void device_reset() override;
 	virtual void device_post_load() override;
 	virtual void device_add_mconfig(machine_config &config) override;
+	virtual ioport_constructor device_input_ports() const override;
 
 private:
 	static constexpr size_t SRAM_SIZE = 0x2000;
 
 	required_device<nvram_device> m_nvram;
+	required_device<i8251_device> m_i8251;
+	required_device<i8255_device> m_i8255;
+	required_ioport m_switch_port;
 	required_memory_region m_rom_region;
 	uint32_t m_region_offset;
 	const uint8_t *m_rom;
@@ -43,6 +76,9 @@ private:
 	const uint8_t *m_bank_base_8000;
 
 	void map_bank();
+	void i8255_port_a_w(uint8_t data);
+	void i8255_port_b_w(uint8_t data);
+	uint8_t i8255_port_c_r();
 };
 
 
