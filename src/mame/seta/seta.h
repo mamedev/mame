7cbb49c5c77270898f58ed67238eb54014aa9a0d
David Haywood <28625134+DavidHaywood@users.noreply.github.com>
2022-11-05 15:35:17 +0000

    x1_012: force a partial update for tilemaps whenever the scroll registers change - needed for a crude raster effect when entering underground areas on Caliber 50 (#10506)

--- src/mame/seta/seta.h
+++ src/mame/seta/seta.h
@@ -23,6 +23,7 @@
 #include "video/x1_001.h"
 #include "x1_012.h"
 #include "emupal.h"
+#include "screen.h"
 #include "tilemap.h"
 
 
@@ -41,6 +42,7 @@ public:
 		driver_device(mconfig, type, tag),
 		m_maincpu(*this, "maincpu"),
 		m_audiocpu(*this, "audiocpu"),
+		m_screen(*this, "screen"),
 		m_spritegen(*this, "spritegen"),
 		m_layers(*this, "layer%u", 1U),
 		m_x1snd(*this, "x1snd"),
@@ -112,6 +114,7 @@ protected:
 
 	required_device<cpu_device> m_maincpu;
 	optional_device<cpu_device> m_audiocpu;
+	required_device<screen_device> m_screen;
 	required_device<x1_001_device> m_spritegen;
 	optional_device_array<x1_012_device, 2> m_layers;
 	optional_device<x1_010_device> m_x1snd;
