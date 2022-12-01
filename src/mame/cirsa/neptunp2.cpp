e46d279454cada2e0a9f2786d382333eacc5a2cd
AJR <ajrhacker@users.noreply.github.com>
2022-12-01 09:07:22 -0500

    bgt.cpp: Remove skeleton driver and fold sets into neptunp2.cpp

--- src/mame/cirsa/neptunp2.cpp
+++ src/mame/cirsa/neptunp2.cpp
@@ -48,6 +48,10 @@ The same hardware from Unidesa/Cirsa was also used on some games from
   Unidesa/Cirsa/Europea Oklahoma Express
   Unidesa/Cirsa/Europea Ruleta de la Fortuna
 
+The "960606-3" version of this hardware was used in several fruit
+machines released for the UK market by BGT Gaming Technology Ltd
+(a division of Cirsa?).
+
 
  CIRSA / UNIDESA 960606-5 CPU BOARD
  _________________________________________________________________
@@ -501,6 +505,71 @@ ROM_START( unk960606b )
 	ROM_LOAD( "pat_063_tibpal16l8-25cn.bin", 0x000, 0x104, NO_DUMP ) // "PAT 063", protected
 ROM_END
 
+ROM_START( bg_ddb )
+	ROM_REGION( 0x100000, "maincpu", 0 )
+	ROM_LOAD( "nkyky_0-15_5", 0x0000, 0x080000, CRC(ac4a5094) SHA1(db4eab0be63e5daddca603af290debd8e929757e) )
+	ROM_RELOAD(0x80000,0x80000)
+
+	ROM_REGION( 0x100000, "oki", ROMREGION_ERASE00 )
+	/* there were sound roms in the 'CoinWorld Ding Dong Bells' set which might belong here, otherwise
+	   roms are probably missing */
+
+	ROM_REGION( 0x800, "eeprom", 0 )
+	ROM_LOAD( "24lc16b.u10", 0x000, 0x800, NO_DUMP )
+
+	ROM_REGION( 0x800, "counters", 0 )
+	ROM_LOAD( "cirsa_cs-4.bin", 0x000, 0x800, NO_DUMP )
+
+	ROM_REGION( 0x104, "plds", 0 )
+	ROM_LOAD( "pat.u6", 0x000, 0x104, NO_DUMP )
+ROM_END
+
+ROM_START( bg_barmy )
+	ROM_REGION( 0x100000, "maincpu", 0 )
+	ROM_LOAD( "barmyarmy_ndp16", 0x0000, 0x080000, CRC(ae488f48) SHA1(c417a3d1a79a0ca54ade2d9a4f6d70467e6c5cb4) )
+	ROM_RELOAD(0x80000,0x80000)
+
+	ROM_REGION( 0x100000, "oki", 0 )
+	ROM_LOAD( "barmyarmy_sound1", 0x000000, 0x080000, CRC(3530d77c) SHA1(c7a42f698090fcd9644f9929b92935cf85183d23) )
+	ROM_LOAD( "barmyarmy_sound2", 0x080000, 0x080000, CRC(48d4c2f3) SHA1(71e64e3e76a55275484a7c72ce2a17232b27a4eb) )
+ROM_END
+
+ROM_START( bg_max )
+	ROM_REGION( 0x100000, "maincpu", 0 )
+	ROM_LOAD( "max_a_million_v014", 0x0000, 0x080000, CRC(32fe9c3b) SHA1(77519657e6e478b3cd1bf2ad2aecc6e191abe554) )
+	ROM_RELOAD(0x80000,0x80000)
+
+	ROM_REGION( 0x100000, "oki", ROMREGION_ERASE00 )
+	/* probably missing */
+
+	ROM_REGION( 0x800, "eeprom", 0 )
+	ROM_LOAD( "24lc16b.u10", 0x000, 0x800, NO_DUMP )
+
+	ROM_REGION( 0x800, "counters", 0 )
+	ROM_LOAD( "cirsa_cs-4.bin", 0x000, 0x800, NO_DUMP )
+
+	ROM_REGION( 0x104, "plds", 0 )
+	ROM_LOAD( "pat.u6", 0x000, 0x104, NO_DUMP )
+ROM_END
+
+ROM_START( bg_maxa )
+	ROM_REGION( 0x100000, "maincpu", 0 )
+	ROM_LOAD( "max_a_million_v114", 0x0000, 0x080000, CRC(a66851e9) SHA1(733ec52fa01615e740ebd40fba4a88efe9d9f24f) )
+	ROM_RELOAD(0x80000,0x80000)
+
+	ROM_REGION( 0x100000, "oki", ROMREGION_ERASE00 )
+	/* probably missing */
+
+	ROM_REGION( 0x800, "eeprom", 0 )
+	ROM_LOAD( "24lc16b.u10", 0x000, 0x800, NO_DUMP )
+
+	ROM_REGION( 0x800, "counters", 0 )
+	ROM_LOAD( "cirsa_cs-4.bin", 0x000, 0x800, NO_DUMP )
+
+	ROM_REGION( 0x104, "plds", 0 )
+	ROM_LOAD( "pat.u6", 0x000, 0x104, NO_DUMP )
+ROM_END
+
 } // Anonymous namespace
 
 //    YEAR   NAME        PARENT COMPAT             MACHINE   INPUT           CLASS       INIT  COMPANY                  FULLNAME                                  FLAGS
@@ -515,3 +584,9 @@ GAME( 2006,  mltpoints,  0,     neptunp2_no_video, c960606,  neptunp2_state, emp
 GAME( 1999,  rockroll,   0,     neptunp2_no_video, c960606,  neptunp2_state, empty_init, ROT0, "Unidesa/Cirsa",         "Rock 'n' Roll",                          MACHINE_IS_SKELETON_MECHANICAL ) // Year taken from parts' manual and sticker on PCB
 GAME( 2001?, unk960606,  0,     neptunp2_no_video, c960606,  neptunp2_state, empty_init, ROT0, "Unidesa/Cirsa",         "unknown 960606-5 based machine (set 1)", MACHINE_IS_SKELETON_MECHANICAL ) // Year taken from sticker on PCB
 GAME( 2001?, unk960606b, 0,     neptunp2_no_video, c960606,  neptunp2_state, empty_init, ROT0, "Unidesa/Cirsa/Europea", "unknown 960606-5 based machine (set 2)", MACHINE_IS_SKELETON_MECHANICAL ) // Year taken from sticker on PCB
+
+// BGT Fruit Machines
+GAME( 199?,  bg_ddb,     0,     neptunp2_no_video, c960606,  neptunp2_state, empty_init, ROT0, "BGT",                   "Ding Dong Bells (BGT)",                  MACHINE_IS_SKELETON_MECHANICAL )
+GAME( 199?,  bg_barmy,   0,     neptunp2_no_video, c960606,  neptunp2_state, empty_init, ROT0, "BGT",                   "Barmy Army (BGT)",                       MACHINE_IS_SKELETON_MECHANICAL )
+GAME( 199?,  bg_max,     0,     neptunp2_no_video, c960606,  neptunp2_state, empty_init, ROT0, "BGT",                   "Max A Million (BGT) (set 1)",            MACHINE_IS_SKELETON_MECHANICAL )
+GAME( 199?,  bg_maxa,    bg_max,neptunp2_no_video, c960606,  neptunp2_state, empty_init, ROT0, "BGT",                   "Max A Million (BGT) (set 2)",            MACHINE_IS_SKELETON_MECHANICAL )
