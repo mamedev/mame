12e6934e13970ffb30609ec78bc579ad933777dc
Vas Crabb <cuavas@users.noreply.github.com>
2022-12-12 02:00:49 +1100

    cococart.cpp: Less churn.

--- src/devices/bus/coco/cococart.cpp
+++ src/devices/bus/coco/cococart.cpp
@@ -678,8 +678,8 @@ device_cococart_interface::~device_cococart_interface()
 void device_cococart_interface::interface_config_complete()
 {
 	m_host = m_owning_slot
-		? dynamic_cast<device_cococart_host_interface *>(m_owning_slot->owner())
-		: nullptr;
+			? dynamic_cast<device_cococart_host_interface *>(m_owning_slot->owner())
+			: nullptr;
 }
 
 
