2511ea04d2ffbe62a207d962c24b9692c8fa7a65
hap <happppp@users.noreply.github.com>
2022-12-27 19:38:14 +0100

    chessking: add section headers

--- src/mame/handheld/chessking.cpp
+++ src/mame/handheld/chessking.cpp
@@ -92,6 +92,47 @@ void chessking_state::machine_start()
 	save_item(NAME(m_beeper_freq));
 }
 
+
+
+/******************************************************************************
+    Video
+******************************************************************************/
+
+uint32_t chessking_state::screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect)
+{
+	// quickly draw from memory (should be handled by LCDC?)
+	for (int y = cliprect.min_y; y <= cliprect.max_y; y++)
+	{
+		int offset = y * 256 / 8;
+
+		uint32_t *dst = &bitmap.pix(y);
+		for (int x = cliprect.min_x; x <= cliprect.max_x; x++)
+		{
+			// 2 256x256 images (160x160 area used) one at c000, one at e000 to form 2bpp graphics
+			uint8_t data = m_mainram[0xc000 + offset + x/8];
+			uint8_t data2 = m_mainram[0xe000 + offset + x/8];
+
+			int xx = x % 8;
+
+			uint8_t pix = (data >> (7 - xx)) & 1;
+			pix |= ((data2 >> (7 - xx)) & 1) << 1;
+
+			rgb_t pens[4] = { rgb_t::white(), rgb_t(0x40,0x40,0x40), rgb_t(0xc0,0xc0,0xc0), rgb_t::black() };
+			dst[x] = pens[pix];
+		}
+	}
+
+	return 0;
+}
+
+
+
+/******************************************************************************
+    I/O
+******************************************************************************/
+
+// misc/unknown
+
 void chessking_state::unk_1f_w(uint8_t data)
 {
 	logerror("%s: 1f write %02x\n", machine().describe_context(), data);
@@ -137,6 +178,9 @@ void chessking_state::unk_7f_w(uint8_t data)
 	logerror("%s: 7f write %02x\n", machine().describe_context(), data);
 }
 
+
+// sound
+
 void chessking_state::beeper_freq_low_w(uint8_t data)
 {
 	//logerror("%s: 8f write %02x\n", machine().describe_context(), data);
@@ -163,6 +207,12 @@ void chessking_state::update_beeper()
 	double step = (9.6_MHz_XTAL).dvalue() / 0x100000;
 	m_beeper->set_clock(((0xffff ^ m_beeper_freq) + 1) * step);
 }
+
+
+
+/******************************************************************************
+    Address Maps
+******************************************************************************/
 
 void chessking_state::chesskng_map(address_map &map)
 {
@@ -191,32 +241,11 @@ void chessking_state::chesskng_io(address_map &map)
 	map(0xaf, 0xaf).w(FUNC(chessking_state::beeper_enable_w)); // beeper enable?
 }
 
-uint32_t chessking_state::screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect)
-{
-	// quickly draw from memory (should be handled by LCDC?)
-	for (int y = cliprect.min_y; y <= cliprect.max_y; y++)
-	{
-		int offset = y * 256 / 8;
 
-		uint32_t *dst = &bitmap.pix(y);
-		for (int x = cliprect.min_x; x <= cliprect.max_x; x++)
-		{
-			// 2 256x256 images (160x160 area used) one at c000, one at e000 to form 2bpp graphics
-			uint8_t data = m_mainram[0xc000 + offset + x/8];
-			uint8_t data2 = m_mainram[0xe000 + offset + x/8];
 
-			int xx = x % 8;
-
-			uint8_t pix = (data >> (7 - xx)) & 1;
-			pix |= ((data2 >> (7 - xx)) & 1) << 1;
-
-			rgb_t pens[4] = { rgb_t::white(), rgb_t(0x40,0x40,0x40), rgb_t(0xc0,0xc0,0xc0), rgb_t::black() };
-			dst[x] = pens[pix];
-		}
-	}
-
-	return 0;
-}
+/******************************************************************************
+    Input Ports
+******************************************************************************/
 
 static INPUT_PORTS_START( chesskng )
 	PORT_START("BUTTONS")
@@ -229,6 +258,12 @@ static INPUT_PORTS_START( chesskng )
 	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_JOYSTICK_DOWN ) PORT_16WAY
 	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_JOYSTICK_LEFT ) PORT_16WAY
 INPUT_PORTS_END
+
+
+
+/******************************************************************************
+    Machine Configs
+******************************************************************************/
 
 INTERRUPT_GEN_MEMBER(chessking_state::interrupt)
 {
@@ -263,6 +298,12 @@ void chessking_state::chesskng(machine_config &config)
 	// Has a cartridge slot
 }
 
+
+
+/******************************************************************************
+    ROM Definitions
+******************************************************************************/
+
 ROM_START( chesskng )
 	ROM_REGION( 0x040000, "maincpu", 0 )
 	ROM_LOAD( "etmate-cch.u6", 0x000000, 0x040000, CRC(a4d1764b) SHA1(ccfae1e985f6ad316ff192206fbc0f8bcd4e44d5) )
@@ -271,4 +312,10 @@ ROM_END
 } // anonymous namespace
 
 
-CONS( 1994, chesskng, 0, 0, chesskng, chesskng, chessking_state, empty_init, "I-Star Co., Ltd.", "Chess King (Model ET-6)", MACHINE_IMPERFECT_SOUND ) // sound not 100% verified against device output
+
+/******************************************************************************
+    Drivers
+******************************************************************************/
+
+//    YEAR  NAME      PARENT  COMPAT  MACHINE   INPUT     CLASS            INIT        COMPANY             FULLNAME                   FLAGS
+CONS( 1994, chesskng, 0,      0,      chesskng, chesskng, chessking_state, empty_init, "I-Star Co., Ltd.", "Chess King (Model ET-6)", MACHINE_IMPERFECT_SOUND ) // sound not 100% verified against device output
