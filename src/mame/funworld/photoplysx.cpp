c1636c4dcf0485e49bd7fbf98523bd1a1df97752
Angelo Salese <angelosa@users.noreply.github.com>
2023-01-19 15:47:18 +0100

    QA cleanups for various PC-based Arcade skeleton drivers (#10830)
    New clones marked not working
    -----------------------------
    Hydro Thunder (1.01b) [archive.org]
    Hydro Thunder (1.00d) [archive.org]
    
    - gaelco/gaelcopc.cpp: add an alternate BIOS set for tokyocop [Gerald (COY), The Dumping Union]
    
    - misc/neomania.cpp, misc/playcenter.cpp, pc/quakeat.cpp, midway/midqslvr.cpp, misc/comebaby.cpp, unico/unianapc.cpp: demote HDD images to BAD_DUMP having bad chs geometries.
    
    - funworld/photoplys.cpp, funworld/photoplysx.cpp, ice/frenzyxprss.cpp ,misc/bntyhunt.cpp, misc/cavepc.cpp, misc/chameleonrx1.cpp, misc/ez2d.cpp, misc/gfamily.cpp, misc/globalvr.cpp, misc/neomania.cpp, misc/odyssey.cpp, misc/playcenter.cpp, misc/radikaldarts.cpp, misc/rawthrillspc.cpp, misc/rfslotspcpent.cpp, misc/silverball.cpp, misc/skopro.cpp, misc/startouch.cpp, misc/xtom3d.cpp, namco/rbowlorama.cpp, pc/quakeat.cpp, pinball/newcanasta.cpp, taito/taitotx.cpp, unico/unianapc.cpp, midway/midqslvr.cpp, misc/comebaby.cpp: rearrange driver base setup and QA notes, for future conversion to PCI drivers.

--- src/mame/funworld/photoplysx.cpp
+++ src/mame/funworld/photoplysx.cpp
@@ -26,7 +26,7 @@ A single wrong login attempt locks the token.
 
 #include "emu.h"
 #include "cpu/i386/i386.h"
-#include "screen.h"
+#include "machine/pci.h"
 
 namespace {
 
@@ -40,56 +40,29 @@ public:
 
 	void photoplaysx(machine_config &config);
 
-protected:
-	virtual void machine_start() override;
-	virtual void machine_reset() override;
-	virtual void video_start() override;
-
 private:
 	required_device<cpu_device> m_maincpu;
 
-	uint32_t screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
 	void photoplaysx_map(address_map &map);
 };
 
-void photoplaysx_state::video_start()
-{
-}
-
-uint32_t photoplaysx_state::screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect)
-{
-	return 0;
-}
-
 void photoplaysx_state::photoplaysx_map(address_map &map)
 {
+	map(0x00000000, 0x0009ffff).ram();
+	map(0x000e0000, 0x000fffff).rom().region("bios", 0x60000);
+	map(0xfff80000, 0xffffffff).rom().region("bios", 0);
 }
 
 static INPUT_PORTS_START( photoplaysx )
 INPUT_PORTS_END
 
-
-void photoplaysx_state::machine_start()
-{
-}
-
-void photoplaysx_state::machine_reset()
-{
-}
-
 void photoplaysx_state::photoplaysx(machine_config &config)
 {
-	// Basic machine hardware
-	PENTIUM4(config, m_maincpu, 100000000); // Actually an Intel CELERON 2 GHz / 128 / 400 (SL6VR)
+	PENTIUM4(config, m_maincpu, 100'000'000); // Actually an Intel CELERON 2 GHz / 128 / 400 (SL6VR)
 	m_maincpu->set_addrmap(AS_PROGRAM, &photoplaysx_state::photoplaysx_map);
 
-	// Video hardware
-	screen_device &screen(SCREEN(config, "screen", SCREEN_TYPE_RASTER));
-	screen.set_refresh_hz(60);
-	screen.set_vblank_time(ATTOSECONDS_IN_USEC(0));
-	screen.set_size(640, 480);
-	screen.set_visarea(0, 640-1, 0, 480-1);
-	screen.set_screen_update(FUNC(photoplaysx_state::screen_update));
+	PCI_ROOT(config, "pci", 0);
+	// ...
 }
 
 /***************************************************************************
@@ -99,7 +72,7 @@ void photoplaysx_state::photoplaysx(machine_config &config)
 ***************************************************************************/
 
 ROM_START( photopsxsp )
-	ROM_REGION(0x80000, "bios", 0)
+	ROM_REGION32_LE(0x80000, "bios", 0)
 	ROM_LOAD("photoplay_bios_pm49fl004t.bin", 0x00000, 0x80000, CRC(50bf84fe) SHA1(d0afe83b57f822d4fdb96dc1e0c6eedeccbfce7b) ) // 03/11/2009-I865G-6A79AD4EC-00
 
 	ROM_REGION(0xe540, "dongle", 0)
@@ -112,7 +85,7 @@ ROM_START( photopsxsp )
 ROM_END
 
 ROM_START( photopsxusp )
-	ROM_REGION(0x80000, "bios", 0)
+	ROM_REGION32_LE(0x80000, "bios", 0)
 	ROM_LOAD("photoplay_bios_pm49fl004t.bin", 0x00000, 0x80000, CRC(50bf84fe) SHA1(d0afe83b57f822d4fdb96dc1e0c6eedeccbfce7b) ) // 03/11/2009-I865G-6A79AD4EC-00
 
 	ROM_REGION(0xe540, "dongle", 0)
