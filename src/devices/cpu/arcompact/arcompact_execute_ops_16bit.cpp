090e7121975d7058f429e73c6de3d0c13d6221c2
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2023-01-21 18:38:39 +0000

    fix silly typo

--- src/devices/cpu/arcompact/arcompact_execute_ops_16bit.cpp
+++ src/devices/cpu/arcompact/arcompact_execute_ops_16bit.cpp
@@ -117,8 +117,8 @@ uint32_t arcompact_device::handleop_MOV_S_b_h_or_limm(uint16_t op) // MOV_S b <-
 	{
 		// opcode        iiii ibbb hhhI Ihhh
 		// MOV_S b, limm 0111 0bbb 1100 1111 [LIMM]   (h == LIMM)
-
 		GET_LIMM_16;
+		size = 6;
 
 		m_regs[breg] = m_regs[h];
 
