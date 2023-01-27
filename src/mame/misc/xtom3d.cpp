bcef19f3705c544e77652fa660539620eb908a94
angelosa <lordkale4@gmail.com>
2023-01-27 03:46:32 +0100

    misc/xtom3d.cpp: cleanups & QA

--- src/mame/misc/xtom3d.cpp
+++ src/mame/misc/xtom3d.cpp
@@ -10,9 +10,18 @@ Customized i440bx Award based BIOS, "OKSAN MK III /EVATE Ver99.04.20"
 Oksan is the old company name that became Andamiro.
 
 TODO:
-- Doesn't really accesses a Super I/O, which implies that the Holtek keyboard and the RTC chips are on a separate ISA plane.
-- DIR texture folder will throw mangled file structure the second time around;
+- DIR texture folder will throw mangled file structure the second time around (when
+  ENABLE_VOODOO is 0 and SVGA is used instead). PAM[5] and [6] areas are written to but
+  they are locked for write ...
+- Voodoo Banshee doesn't handle VGA text modes correctly, it will set the screen to 80 x 25
+  at POST (making MAME UI host to be unusable) without any drawing.
+  Current stall point (texture init) will eventually write data at VGA memory $a0000-$bffff,
+  likely for drawing an error.
+- Pinpoint what i/o $2a8 is for during texture init (alt serial flash transfer?)
+- EEPROM (i/o $2ac r/w)
+- Hookup ISA sound board
 
+===================================================================================================
 
 This game runs on PC-based hardware.
 Major components are....
@@ -68,9 +77,14 @@ MX29F1610MC 16M FlashROM (x7)
 #include "machine/8042kbdc.h"
 #include "video/voodoo_pci.h"
 
+#define ENABLE_VOODOO 1
 
 /*
  * ISA16 Oksan ROM DISK
+ *
+ * "OKSAN (R) ROM DISK for MK-III Version 1.00.0305"
+ * "Copyright (C) OKSAN Co., Ltd. 1989-1999" (!)
+ *
  */
 
 class isa16_oksan_rom_disk : public device_t, public device_isa16_card_interface
@@ -99,8 +113,6 @@ private:
 	u8 m_flash_state = 0;
 };
 
-// "OKSAN (R) ROM DISK for MK-III Version 1.00.0305"
-// "Copyright (C) OKSAN Co., Ltd. 1989-1999" (!)
 DEFINE_DEVICE_TYPE(ISA16_OKSAN_ROM_DISK, isa16_oksan_rom_disk, "isa16_oksan_rom_disk", "ISA16 Oksan ROM DISK for MK-III")
 
 isa16_oksan_rom_disk::isa16_oksan_rom_disk(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
@@ -128,6 +140,8 @@ void isa16_oksan_rom_disk::remap(int space_id, offs_t start, offs_t end)
 	}
 }
 
+// TODO: quick and dirty MX29F1610MC serial flash ROM implementation
+// should really be a serflash_device inheritance ...
 u8 isa16_oksan_rom_disk::read(offs_t offset)
 {
 	//printf("%02x\n", offset);
@@ -135,7 +149,8 @@ u8 isa16_oksan_rom_disk::read(offs_t offset)
 	{
 		if (m_flash_cmd == 0xf0 && m_flash_unlock)
 		{
-			u8 rom_data = m_flash_rom->base()[((m_flash_addr << 1) + (offset & 1)) & 0xffffff];
+			const u32 flash_size = m_flash_rom->bytes() - 1;
+			u8 rom_data = m_flash_rom->base()[((m_flash_addr << 1) + (offset & 1)) & flash_size];
 			if (offset & 1 && !machine().side_effects_disabled())
 				m_flash_addr ++;
 
@@ -163,7 +178,7 @@ void isa16_oksan_rom_disk::write(offs_t offset, u8 data)
 			break;
 		case 0x4:
 			//if (data)
-			//	printf("%02x\n", data);
+			//  printf("%02x\n", data);
 			m_flash_addr &= 0xff00ffff;
 			m_flash_addr |= (data & 0xff) << 16;
 			break;
@@ -198,7 +213,11 @@ void isa16_oksan_rom_disk::write(offs_t offset, u8 data)
 }
 
 /*
- * ISA16 Oksan LPC
+ * ISA16 Oksan Virtual LPC
+ *
+ * Doesn't really accesses a Super I/O, which implies that the Holtek keyboard
+ * and the RTC chips are motherboard ISA resources.
+ *
  */
 
 class isa16_oksan_lpc : public device_t, public device_isa16_card_interface
@@ -219,7 +238,7 @@ private:
 	void remap(int space_id, offs_t start, offs_t end) override;
 };
 
-DEFINE_DEVICE_TYPE(ISA16_OKSAN_LPC, isa16_oksan_lpc, "isa16_oksan_lpc", "ISA16 Oksan LPC")
+DEFINE_DEVICE_TYPE(ISA16_OKSAN_LPC, isa16_oksan_lpc, "isa16_oksan_lpc", "ISA16 Oksan Virtual LPC")
 
 isa16_oksan_lpc::isa16_oksan_lpc(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
 	: device_t(mconfig, ISA16_OKSAN_LPC, tag, owner, clock)
@@ -265,7 +284,7 @@ void isa16_oksan_lpc::remap(int space_id, offs_t start, offs_t end)
 
 namespace {
 
-#define PCI_J4D2_ID "pci:0d.0"
+#define PCI_AGP_ID "pci:01.0:00.0"
 
 class xtom3d_state : public driver_device
 {
@@ -273,7 +292,8 @@ public:
 	xtom3d_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag)
 		, m_maincpu(*this, "maincpu")
-		, m_voodoo(*this, PCI_J4D2_ID)
+		, m_voodoo(*this, PCI_AGP_ID)
+		, m_pci_isa(*this, "pci:07.0")
 	{
 	}
 
@@ -283,12 +303,14 @@ private:
 	required_device<pentium2_device> m_maincpu;
 	// TODO: optional for debugging
 	optional_device<voodoo_banshee_pci_device> m_voodoo;
+	required_device<i82371eb_isa_device> m_pci_isa;
 
 	void xtom3d_map(address_map &map);
 //  void xtom3d_io(address_map &map);
 
+//  DECLARE_WRITE_LINE_MEMBER(vblank_assert);
+
 	static void romdisk_config(device_t *device);
-	static void lpc_config(device_t *device);
 };
 
 
@@ -306,16 +328,7 @@ void xtom3d_isa_cards(device_slot_interface &device)
 void xtom3d_state::romdisk_config(device_t *device)
 {
 	isa16_oksan_rom_disk &romdisk = *downcast<isa16_oksan_rom_disk *>(device);
-	romdisk.set_rom_tag("user2");
-}
-
-void xtom3d_state::lpc_config(device_t *device)
-{
-//  auto *state = device->subdevice<xtom3d_state>(":");
-	//isa16_oksan_lpc &lpc = *downcast<isa16_oksan_lpc *>(device);
-//  lpc.m_kbdc->system_reset_callback().set_inputline(":maincpu", INPUT_LINE_RESET);
-//  m_kbdc->gate_a20_callback().set_inputline(":maincpu", INPUT_LINE_A20);
-//  m_kbdc->input_buffer_full_callback().set(":, FUNC(pic8259_device::ir1_w));
+	romdisk.set_rom_tag("game_rom");
 }
 
 // TODO: unverified PCI config space
@@ -328,6 +341,7 @@ void xtom3d_state::xtom3d(machine_config &config)
 	m_maincpu->smiact().set("pci:00.0", FUNC(i82443bx_host_device::smi_act_w));
 
 	PCI_ROOT(config, "pci", 0);
+	// PCB has ZX marking but BIOS returns BX, shouldn't matter
 	I82443BX_HOST(config, "pci:00.0", 0, "maincpu", 32*1024*1024);
 	I82443BX_BRIDGE(config, "pci:01.0", 0 ); //"pci:01.0:00.0");
 	//I82443BX_AGP   (config, "pci:01.0:00.0");
@@ -346,26 +360,27 @@ void xtom3d_state::xtom3d(machine_config &config)
 	SMBUS        (config, "pci:07.3:smbus", 0);
 
 	ISA16_SLOT(config, "board1", 0, "pci:07.0:isabus", xtom3d_isa_cards, "oksan_romdisk", true).set_option_machine_config("oksan_romdisk", romdisk_config);
-	ISA16_SLOT(config, "board2", 0, "pci:07.0:isabus", xtom3d_isa_cards, "oksan_lpc", true).set_option_machine_config("oksan_lpc", lpc_config);
+	ISA16_SLOT(config, "board2", 0, "pci:07.0:isabus", xtom3d_isa_cards, "oksan_lpc", true);
+	// TODO: another ISA slot for sound system + EEPROM
 	ISA16_SLOT(config, "isa1", 0, "pci:07.0:isabus", pc_isa16_cards, nullptr, false);
 	ISA16_SLOT(config, "isa2", 0, "pci:07.0:isabus", pc_isa16_cards, nullptr, false);
 
-	// YMF740G goes thru "pci:0c.0"
 	// Expansion slots, mapping SVGA for debugging
-	// TODO: all untested, check clock
 	// TODO: confirm Voodoo going in J4D2, may really go in AGP slot under bridge instead?
-	#if 0
+	#if ENABLE_VOODOO
 	VOODOO_BANSHEE_PCI(config, m_voodoo, 0, m_maincpu, "screen"); // "pci:0d.0" J4D2
+	// TODO: confirm values
 	m_voodoo->set_fbmem(2);
 	m_voodoo->set_tmumem(4, 4);
 	m_voodoo->set_status_cycles(1000);
+	//subdevice<generic_voodoo_device>(PCI_AGP_ID":voodoo")->vblank_callback().set(FUNC(xtom3d_state::vblank_assert));
 
 	// TODO: fix legacy raw setup here
 	screen_device &screen(SCREEN(config, "screen", SCREEN_TYPE_RASTER));
 	screen.set_refresh_hz(57);
 	screen.set_size(640, 480);
 	screen.set_visarea(0, 640 - 1, 0, 480 - 1);
-	screen.set_screen_update(PCI_J4D2_ID, FUNC(voodoo_banshee_pci_device::screen_update));
+	screen.set_screen_update(PCI_AGP_ID, FUNC(voodoo_banshee_pci_device::screen_update));
 	#else
 	VIRGE_PCI(config, "pci:0e.0", 0); // J4C1
 	#endif
@@ -378,16 +393,18 @@ ROM_START( xtom3d )
 	ROM_REGION32_LE(0x20000, "pci:07.0", 0)
 	ROM_LOAD( "bios.u22", 0x000000, 0x020000, CRC(f7c58044) SHA1(fd967d009e0d3c8ed9dd7be852946f2b9dee7671) )
 
-	ROM_REGION(0x1000000, "board1:user2", ROMREGION_ERASEFF)
+	ROM_REGION32_LE(0x1000000, "board1:game_rom", ROMREGION_ERASEFF )
 	ROM_LOAD( "u3",  0x000000, 0x200000, CRC(f332e030) SHA1(f04fc7fc97e6ada8122ea7d111455043d7cc42df) )
 	ROM_LOAD( "u4",  0x200000, 0x200000, CRC(ac40ea0b) SHA1(6fcb86f493885d62d20df6bddaa1a1b19d478c65) )
 	ROM_LOAD( "u5",  0x400000, 0x200000, CRC(0fb98a20) SHA1(d21f33b0ca65dc6f90a411a9682f960e9c60244c) )
 	ROM_LOAD( "u6",  0x600000, 0x200000, CRC(5c092c58) SHA1(d347e1ed957cc989dc71f4f347af926589ae926d) )
 	ROM_LOAD( "u7",  0x800000, 0x200000, CRC(833c179c) SHA1(586555f5a4066a762fc05a43ef01be9fa202bb7f) )
-	ROM_LOAD( "u19", 0xa00000, 0x200000, CRC(a1ae73d0) SHA1(232c73bfee426b5f651a015c505c26b8ed7176b7) )
-	ROM_LOAD( "u20", 0xc00000, 0x200000, CRC(452131d9) SHA1(f62a0f1a7da9025ac1f7d5de4df90166871ac1e5) )
+
+	ROM_REGION(0x400000, "board3:sound_rom", ROMREGION_ERASEFF )
+	ROM_LOAD( "u19", 0x000000, 0x200000, CRC(a1ae73d0) SHA1(232c73bfee426b5f651a015c505c26b8ed7176b7) )
+	ROM_LOAD( "u20", 0x200000, 0x200000, CRC(452131d9) SHA1(f62a0f1a7da9025ac1f7d5de4df90166871ac1e5) )
 ROM_END
 
 } // anonymous namespace
 
-GAME(1999, xtom3d, 0, xtom3d, 0, xtom3d_state, empty_init, ROT0, "Jamie System Development", "X Tom 3D", MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
+GAME(1999, xtom3d, 0, xtom3d, 0, xtom3d_state, empty_init, ROT0, "Andamiro / Jamie System Development", "X Tom 3D", MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
