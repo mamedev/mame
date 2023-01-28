ce0f583d3ca851dff151c9db58b2174dd96979c8
Vas Crabb <vas@vastheman.com>
2023-01-29 07:20:51 +1100

    render/bgfx: Fixed Linux build.

--- src/osd/modules/render/bgfx/valueuniform.cpp
+++ src/osd/modules/render/bgfx/valueuniform.cpp
@@ -11,12 +11,16 @@
 
 #include "valueuniform.h"
 
+#include <algorithm>
+#include <cassert>
 
-bgfx_value_uniform::bgfx_value_uniform(bgfx_uniform* uniform, float* values, const int count)
+
+bgfx_value_uniform::bgfx_value_uniform(bgfx_uniform* uniform, const float* values, const int count)
 	: bgfx_entry_uniform(uniform)
 	, m_count(count)
 {
-	memcpy(m_values, values, sizeof(float) * count);
+	assert(count <= std::size(m_values));
+	std::copy_n(values, count, m_values);
 }
 
 void bgfx_value_uniform::bind()
