83bc31189751285262c53c97842452a8fa8c39ec
Miodrag Milanovic <mmicko@gmail.com>
2023-02-06 18:18:23 +0100

    Remove SDL2 from sources

--- 3rdparty/SDL2/build-scripts/update-copyright.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-find . -type f -exec grep -Il "Copyright" {} \;     \
-| grep -v \.hg                             \
-| while read i;                            \
-do \
-  LC_ALL=C sed -ie "s/\(.*Copyright.*\)[0-9]\{4\}\( *Sam Lantinga\)/\1`date +%Y`\2/" "$i"; \
-  rm "${i}e"; \
-done
