f23916abd92387e64ce40e61293552292ecf98b0
hap <happppp@users.noreply.github.com>
2023-02-16 21:54:04 +0100

    nes zapper: improve trigger, put bandai lightgun in its own file

--- src/devices/bus/nes_ctrl/bandaihs.h
+++ src/devices/bus/nes_ctrl/bandaihs.h
@@ -2,12 +2,12 @@
 // copyright-holders:Fabio Priuli
 /**********************************************************************
 
-    Nintendo Family Computer & Entertainment System Zapper Lightgun
+    Nintendo Family Computer Bandai Hyper Shot Lightgun
 
 **********************************************************************/
 
-#ifndef MAME_BUS_NES_CTRL_ZAPPER
-#define MAME_BUS_NES_CTRL_ZAPPER
+#ifndef MAME_BUS_NES_CTRL_BANDAIHS
+#define MAME_BUS_NES_CTRL_BANDAIHS
 
 #pragma once
 
@@ -19,19 +19,17 @@
 //  TYPE DEFINITIONS
 //**************************************************************************
 
-// ======================> nes_zapper_device
+// ======================> nes_bandaihs_device
 
-class nes_zapper_device : public device_t,
+class nes_bandaihs_device : public device_t,
 							public device_nes_control_port_interface
 {
 public:
 	// construction/destruction
-	nes_zapper_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
+	nes_bandaihs_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
 
-	virtual u8 read_bit34() override;
 	virtual u8 read_exp(offs_t offset) override;
-
-	DECLARE_INPUT_CHANGED_MEMBER(trigger);
+	virtual void write(u8 data) override;
 
 protected:
 	// device-level overrides
@@ -43,11 +41,13 @@ private:
 	required_device<nes_zapper_sensor_device> m_sensor;
 	required_ioport m_lightx;
 	required_ioport m_lighty;
-	emu_timer *m_trigger;
+	required_ioport m_trigger;
+	required_ioport m_joypad;
+	u8 m_latch;
 };
 
 
 // device type definition
-DECLARE_DEVICE_TYPE(NES_ZAPPER, nes_zapper_device)
+DECLARE_DEVICE_TYPE(NES_BANDAIHS, nes_bandaihs_device)
 
-#endif // MAME_BUS_NES_CTRL_ZAPPER
+#endif // MAME_BUS_NES_CTRL_BANDAIHS
