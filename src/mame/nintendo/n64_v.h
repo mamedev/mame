6d72820d8d46e4dd479612c6d4cf3496ab0f0b00
Erik <erik_newton@hotmail.com>
2023-02-26 19:04:11 +0000

    nintendo/n64_v.cpp: Simplified N64 RDP disassembler. (#10919)
    Removes all snprintf and reduces duplication.

--- src/mame/nintendo/n64_v.h
+++ src/mame/nintendo/n64_v.h
@@ -163,7 +163,7 @@ public:
 
 	void        process_command_list();
 	uint64_t    read_data(uint32_t address);
-	std::string disassemble(uint64_t *cmd_buf);
+	std::string disassemble(const uint64_t *cmd_buf);
 
 	void        set_machine(running_machine& machine) { m_machine = &machine; }
 	void        set_n64_periphs(n64_periphs* periphs) { m_n64_periphs = periphs; }
