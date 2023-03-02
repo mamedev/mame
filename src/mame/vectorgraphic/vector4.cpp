d2b1965881fa7115d061789f76c8adae784c70b5
Eric Anderson <ejona86@gmail.com>
2023-03-02 08:34:19 -0800

    vectorgraphic/v4_kbd.cpp: Emulated Vector 4 keyboard. (#10944)
    vectorgraphic/vector4.cpp: Replaced high-level keyboard simulation.

--- src/mame/vectorgraphic/vector4.cpp
+++ src/mame/vectorgraphic/vector4.cpp
@@ -36,7 +36,6 @@ https://archive.org/details/7200-0001-vector-4-technical-information-sep-82
 https://www.bitsavers.org/pdf/vectorGraphic/vector_4/7100-0001_Vector_4_Users_Manual_Feb83.pdf
 
 TODO:
-- keyboard mcu
 - S-100 interrupts and ready
 - parallel port
 - WAIT CPU states
@@ -47,6 +46,7 @@ TODO:
 #include "emu.h"
 
 #include "sbcvideo.h"
+#include "v4_kbd.h"
 
 #include "bus/rs232/rs232.h"
 #include "bus/s100/s100.h"
@@ -78,6 +78,7 @@ public:
 		, m_romenbl(*this, "romenbl")
 		, m_sbc_video(*this, "video")
 		, m_s100(*this, "s100")
+		, m_uart0(*this, "uart0")
 	{ }
 
 	void vector4(machine_config &config);
@@ -103,6 +104,7 @@ private:
 	memory_view m_romenbl;
 	required_device<vector_sbc_video_device> m_sbc_video;
 	required_device<s100_bus_device> m_s100;
+	required_device<i8251_device> m_uart0;
 };
 
 
@@ -126,7 +128,7 @@ void vector4_state::vector4_8088mem(address_map &map)
 void vector4_state::vector4_io(address_map &map)
 {
 	map.unmap_value_high();
-	map(0x00, 0x01).mirror(0xff00).rw("uart0", FUNC(i8251_device::read), FUNC(i8251_device::write)); // keyboard
+	map(0x00, 0x01).mirror(0xff00).rw(m_uart0, FUNC(i8251_device::read), FUNC(i8251_device::write)); // keyboard
 	map(0x02, 0x03).mirror(0xff00).w(FUNC(vector4_state::spr_w)); // subsystem port register
 	map(0x04, 0x05).mirror(0xff00).rw("uart1", FUNC(i8251_device::read), FUNC(i8251_device::write)); // modem
 	map(0x06, 0x07).mirror(0xff00).rw("uart2", FUNC(i8251_device::read), FUNC(i8251_device::write)); // serial printer
@@ -148,15 +150,6 @@ static void vector4_s100_devices(device_slot_interface &device)
 
 static INPUT_PORTS_START( vector4 )
 INPUT_PORTS_END
-
-// 7200-0001 page 102 (II 5-11)
-DEVICE_INPUT_DEFAULTS_START(keyboard)
-	DEVICE_INPUT_DEFAULTS("RS232_TXBAUD",    0x00ff, RS232_BAUD_300)
-	DEVICE_INPUT_DEFAULTS("RS232_RXBAUD",    0x00ff, RS232_BAUD_300)
-	DEVICE_INPUT_DEFAULTS("RS232_DATABITS",  0x00ff, RS232_DATABITS_8)
-	DEVICE_INPUT_DEFAULTS("RS232_PARITY",    0x00ff, RS232_PARITY_NONE)
-	DEVICE_INPUT_DEFAULTS("RS232_STOPBITS",  0x00ff, RS232_STOPBITS_2)
-DEVICE_INPUT_DEFAULTS_END
 
 void vector4_state::vector4(machine_config &config)
 {
@@ -201,13 +194,12 @@ void vector4_state::vector4(machine_config &config)
 
 	// 7200-0001 page 210 D13, D1
 	clock_device &keyboard_clock(CLOCK(config, "keyboard_clock", _2mclk/26/16));
-	i8251_device &uart0(I8251(config, "uart0", 0));
-	rs232_port_device &rs232keyboard(RS232_PORT(config, "rs232keyboard", default_rs232_devices, "keyboard"));
-	keyboard_clock.signal_handler().set(uart0, FUNC(i8251_device::write_txc));
-	keyboard_clock.signal_handler().append(uart0, FUNC(i8251_device::write_rxc));
-	uart0.txd_handler().set(rs232keyboard, FUNC(rs232_port_device::write_txd));
-	rs232keyboard.rxd_handler().set(uart0, FUNC(i8251_device::write_rxd));
-	rs232keyboard.set_option_device_input_defaults("keyboard", DEVICE_INPUT_DEFAULTS_NAME(keyboard));
+	I8251(config, m_uart0, 0);
+	vector4_keyboard_device &v4kbd(VECTOR4_KEYBOARD(config, "rs232keyboard", 0));
+	keyboard_clock.signal_handler().set(m_uart0, FUNC(i8251_device::write_txc));
+	keyboard_clock.signal_handler().append(m_uart0, FUNC(i8251_device::write_rxc));
+	m_uart0->txd_handler().set(v4kbd, FUNC(vector4_keyboard_device::write_rxd));
+	v4kbd.txd_handler().set(m_uart0, FUNC(i8251_device::write_rxd));
 
 	// D3
 	i8251_device &uart1(I8251(config, "uart1", 0));
@@ -248,6 +240,8 @@ void vector4_state::machine_start()
 	m_8088cpu->space(AS_PROGRAM).install_ram(0, m_ram->mask(), m_ram->size() & 0x20000, m_ram->pointer());
 	for (int bank = 0; bank < (1<<5); bank++)
 		m_rambanks[bank]->configure_entries(0, 1<<7, m_ram->pointer(), 1<<11);
+	// Missing from schematic, but jumper wire present on the board.
+	m_uart0->write_cts(0);
 }
 
 void vector4_state::machine_reset()
