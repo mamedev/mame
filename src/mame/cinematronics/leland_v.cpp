d2c2ea0d01a2d5fdceaf3a4033c6fe7cf765ab34
cam900 <dbtlrchl@naver.com>
2023-03-21 19:46:59 +0900

    cinematronics/leland_*.cpp: Modernize log function using logmacro

--- src/mame/cinematronics/leland_v.cpp
+++ src/mame/cinematronics/leland_v.cpp
@@ -19,7 +19,11 @@ static constexpr int QRAM_SIZE = 0x10000;
 
 
 /* debugging */
-#define LOG_COMM    0
+#define LOG_WARN    (1 << 1)
+#define LOG_COMM    (1 << 2)
+
+#define VERBOSE     LOG_WARN
+#include "logmacro.h"
 
 /*************************************
  *
@@ -242,15 +246,15 @@ int leland_state::vram_port_r(offs_t offset, int num)
 			break;
 
 		default:
-			logerror("%s: Warning: Unknown video port %02x read (address=%04x)\n",
+			LOGMASKED(LOG_WARN, "%s: Warning: Unknown video port %02x read (address=%04x)\n",
 						machine().describe_context(), offset, addr);
 			ret = 0;
 			break;
 	}
 	state->m_addr = addr;
 
-	if (LOG_COMM && addr >= 0xf000)
-		logerror("%s:%s comm read %04X = %02X\n", machine().describe_context(), num ? "slave" : "master", addr, ret);
+	if (addr >= 0xf000)
+		LOGMASKED(LOG_COMM, "%s:%s comm read %04X = %02X\n", machine().describe_context(), num ? "slave" : "master", addr, ret);
 
 	return ret;
 }
@@ -276,8 +280,8 @@ void leland_state::vram_port_w(offs_t offset, u8 data, int num)
 	if (scanline > 0)
 		m_screen->update_partial(scanline - 1);
 
-	if (LOG_COMM && addr >= 0xf000)
-		logerror("%s:%s comm write %04X = %02X\n", machine().describe_context(), num ? "slave" : "master", addr, data);
+	if (addr >= 0xf000)
+		LOGMASKED(LOG_COMM, "%s:%s comm write %04X = %02X\n", machine().describe_context(), num ? "slave" : "master", addr, data);
 
 	/* based on the low 3 bits of the offset, update the destination */
 	switch (offset & 7)
@@ -328,7 +332,7 @@ void leland_state::vram_port_w(offs_t offset, u8 data, int num)
 			break;
 
 		default:
-			logerror("%s:Warning: Unknown video port write (address=%04x value=%02x)\n",
+			LOGMASKED(LOG_WARN, "%s:Warning: Unknown video port write (address=%04x value=%02x)\n",
 						machine().describe_context(), offset, addr);
 			break;
 	}
