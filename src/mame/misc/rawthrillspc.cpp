48cee63645d444ca16f471808458e3527911eea0
ClawGrip <clawgrip@hotmail.com>
2023-03-27 21:51:08 +0200

    misc/rawthrillspc.cpp: Add two new BIOS dumps for 'doodlejmp' [jordigahan, Recreativas.org]

--- src/mame/misc/rawthrillspc.cpp
+++ src/mame/misc/rawthrillspc.cpp
@@ -119,7 +119,7 @@ ROM_END
 
 /*
  Doodle Jump Arcade, by default, uses a different PC than other Raw Thrills games:
-   -HP with an Asus M2N68-LA "Narra 3" motherboard (Socket AM3, nForce-based, nVidia MCP61P chipset)
+   -HP/Compaq Presario VS459AA-ABE CQ5211ES with an Asus M2N68-LA "Narra 6" motherboard (Socket AM3, nForce-based, nVidia MCP61P chipset)
      * Fintek F8000 + Realtek RTL8201EL + Realtek ALC662 + nVidia NF-6100-430-N-A3 + ST L6740L.
    -CPU Athlon II CPU (ADX2400CK23GQ).
    -2GB RAM (single SIMM, PC3-12800) [HP P/N 655409-150].
@@ -130,8 +130,13 @@ ROM_END
 */
 ROM_START( doodlejmp )
 	ROM_REGION32_LE( 0x100000, "bios", 0 )
-	ROM_SYSTEM_BIOS( 0, "1001", "ASUS 1001 (11/17/2010)" )
-	ROMX_LOAD( "m2n68-asus-1001.rom", 0x000000, 0x100000, CRC(6f4ff699) SHA1(d43baae25fcfb442ccf2cc716e6051b3795296cd), ROM_BIOS(0) )
+	ROM_DEFAULT_BIOS("0515")
+	ROM_SYSTEM_BIOS( 0, "0515", "Compaq 5.15 (11/06/2009)" )
+	ROMX_LOAD( "w25x80a.bin",         0x000000, 0x100000, CRC(e91538ee) SHA1(32add79eba2049205a98fc4e854976e11d102a4c), ROM_BIOS(0) ) // BIOS dumped from the actual arcade machine
+	ROM_SYSTEM_BIOS( 1, "0522", "HP 5.22 (04/15/2010)" )
+	ROMX_LOAD( "vle5.22.bin",         0x000000, 0x100000, CRC(4fd46c0a) SHA1(3aa05278e8bbca5bd9b16f22faff1bcb059c0cd9), ROM_BIOS(1) ) // BIOS update from HP
+	ROM_SYSTEM_BIOS( 2, "1001", "ASUS 1001 (11/17/2010)" )
+	ROMX_LOAD( "m2n68-asus-1001.rom", 0x000000, 0x100000, CRC(6f4ff699) SHA1(d43baae25fcfb442ccf2cc716e6051b3795296cd), ROM_BIOS(2) ) // BIOS update from ASUS
 
 	DISK_REGION( "ide:0:hdd:image" )
 	DISK_IMAGE( "doodlejmp", 0, NO_DUMP )
