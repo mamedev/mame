6f25d9860ef91162181565fc96554525fe7c2fdc
AJR <ajrhacker@users.noreply.github.com>
2023-04-09 18:27:39 -0400

    tmp68301: Fix confusion between internal copy of SR and interrupt in-service register

--- src/devices/cpu/m68000/tmp68301.cpp
+++ src/devices/cpu/m68000/tmp68301.cpp
@@ -596,7 +596,7 @@ u8 tmp68301_device::interrupt_callback()
 	if(slot < 3)
 		standard_irq_callback(slot, m_pc);
 	if(vector != 0x1f) {
-		m_isr |= 1 << slot;
+		m_iisr |= 1 << slot;
 		if(slot >= 3 || !(m_icr[slot] & 0x08))
 			m_ipr &= ~(1 << slot);
 		m_interrupt_state &= ~(1U << vector);
