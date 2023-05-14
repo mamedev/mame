1c1d46aa4c95d3c1b4d884774c3b3cdf0ec731b0
wilbertpol <wilbertpol@users.noreply.github.com>
2023-05-14 21:54:13 +0100

    msx: Refactored to standard layout and output usage. (#11216)
    Made separate layout files per 'region' and drive count, and simplified the code for LED outputs.

--- src/mame/msx/msx1_bruc100.cpp
+++ src/mame/msx/msx1_bruc100.cpp
@@ -13,6 +13,8 @@
 #include "bus/msx/slot/ram.h"
 #include "bus/msx/slot/ram_mm.h"
 
+#include "msx_nocode.lh"
+
 using namespace msx_keyboard;
 
 
@@ -73,8 +75,7 @@ void bruc100_state::bruc100(machine_config &config)
 	// Expansion slot in slot 1
 	add_internal_slot(config, MSX_SLOT_RAM_MM, "ram_mm", 3, 0, 4).set_total_size(0x10000);   // 64KB RAM
 
-	m_hw_def.no_code_led();
-	msx1(VDP_TMS9129, SND_AY8910, config, REGION_INTERNATIONAL);
+	msx1(VDP_TMS9129, SND_AY8910, config, layout_msx_nocode);
 	m_maincpu->set_addrmap(AS_IO, &bruc100_state::io_map);
 }
 
@@ -109,8 +110,7 @@ void bruc100_state::bruc100a(machine_config &config)
 	add_cartridge_slot<1>(config, 2);
 	// Expansion slot in slot 3
 
-	m_hw_def.no_code_led();
-	msx1(VDP_TMS9129, SND_AY8910, config, REGION_INTERNATIONAL);
+	msx1(VDP_TMS9129, SND_AY8910, config, layout_msx_nocode);
 	m_maincpu->set_addrmap(AS_IO, &bruc100_state::io_map);
 }
 
