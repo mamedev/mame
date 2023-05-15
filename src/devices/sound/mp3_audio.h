03ac035cc21a684eecaaabb7033d610f77de2099
987123879113 <63495610+987123879113@users.noreply.github.com>
2023-05-15 15:49:28 +0900

    sound/mp3_audio: Implement save states for MP3 decoder

--- src/devices/sound/mp3_audio.h
+++ src/devices/sound/mp3_audio.h
@@ -19,6 +19,8 @@ public:
 	mp3_audio(const void *base);
 	~mp3_audio();
 
+	void register_save(device_t &host);
+
 	bool decode_buffer(int &pos, int limit, short *output, int &output_samples, int &sample_rate, int &channels);
 
 	void clear();
