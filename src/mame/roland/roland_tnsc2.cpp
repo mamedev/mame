e732c8fefd5def410e98958f1bebe54f1e4f3847
Valley Bell <valleybell@gmx.de>
2023-06-07 21:45:39 +0200

    add Roland TN-SC2 softlist

--- src/mame/roland/roland_tnsc2.cpp
+++ src/mame/roland/roland_tnsc2.cpp
@@ -8,17 +8,17 @@
     Once a supported system is dumped this can be removed and the list can be hooked up to that
 
     Possible systems:
-      Roland E-5
-      Roland E-20
-      Roland E-30
       Roland E-35
+      Roland E-36
+      Roland E-56
       Roland E-70
-      Roland Pro-E
-      Roland E/RA-50
       Roland RA-90
-      Roland CA-30
-      Roland KR-500
-      Roland KR-3000
+      Roland KR-650
+      Roland KR-3500
+      Roland KR-4500
+      Roland KR-5500
+
+    The ROM dump of TN-SC2-04 begins with "Roland E-70A", so this was probably the first system the cards were intended for.
 */
 
 #include "emu.h"
@@ -29,25 +29,25 @@
 
 namespace {
 
-class rlndtnsc1_state : public driver_device
+class rlndtnsc2_state : public driver_device
 {
 public:
-	rlndtnsc1_state(const machine_config &mconfig, device_type type, const char *tag) :
+	rlndtnsc2_state(const machine_config &mconfig, device_type type, const char *tag) :
 		driver_device(mconfig, type, tag),
 		m_cart(*this, "cartslot")
 	{ }
 
-	void rlndtnsc1(machine_config &config);
+	void rlndtnsc2(machine_config &config);
 protected:
 	DECLARE_DEVICE_IMAGE_LOAD_MEMBER(cart_load);
 	optional_device<generic_slot_device> m_cart;
 };
 
 
-static INPUT_PORTS_START( rlndtnsc1 )
+static INPUT_PORTS_START( rlndtnsc2 )
 INPUT_PORTS_END
 
-DEVICE_IMAGE_LOAD_MEMBER(rlndtnsc1_state::cart_load)
+DEVICE_IMAGE_LOAD_MEMBER(rlndtnsc2_state::cart_load)
 {
 	uint32_t size = m_cart->common_get_size("rom");
 	m_cart->rom_alloc(size, GENERIC_ROM16_WIDTH, ENDIANNESS_LITTLE);
@@ -55,20 +55,20 @@ DEVICE_IMAGE_LOAD_MEMBER(rlndtnsc1_state::cart_load)
 	return std::make_pair(std::error_condition(), std::string());
 }
 
-void rlndtnsc1_state::rlndtnsc1(machine_config &config)
+void rlndtnsc2_state::rlndtnsc2(machine_config &config)
 {
-	GENERIC_CARTSLOT(config, m_cart, generic_plain_slot, "roland_tnsc1");
+	GENERIC_CARTSLOT(config, m_cart, generic_plain_slot, "roland_tnsc2");
 	m_cart->set_width(GENERIC_ROM16_WIDTH);
-	m_cart->set_device_load(FUNC(rlndtnsc1_state::cart_load));
+	m_cart->set_device_load(FUNC(rlndtnsc2_state::cart_load));
 	m_cart->set_must_be_loaded(true);
 
-	SOFTWARE_LIST(config, "cart_list").set_original("roland_tnsc1");
+	SOFTWARE_LIST(config, "cart_list").set_original("roland_tnsc2");
 }
 
-ROM_START( rlndtnsc1 )
+ROM_START( rlndtnsc2 )
 ROM_END
 
 } // anonymous namespace
 
 
-CONS( 198?, rlndtnsc1, 0, 0, rlndtnsc1, rlndtnsc1, rlndtnsc1_state, empty_init, "Roland", "Roland Music Style Card TN-SC1 Software List holder", MACHINE_IS_SKELETON )
+CONS( 198?, rlndtnsc2, 0, 0, rlndtnsc2, rlndtnsc2, rlndtnsc2_state, empty_init, "Roland", "Roland Music Style Card TN-SC2 Software List holder", MACHINE_IS_SKELETON )
