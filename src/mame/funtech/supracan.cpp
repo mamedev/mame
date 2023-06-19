f99251f5244df79cec12cf6c24d1510e0c3fc50f
DavidHaywood <28625134+DavidHaywood@users.noreply.github.com>
2023-06-19 09:56:13 +0100

    typo

--- src/mame/funtech/supracan.cpp
+++ src/mame/funtech/supracan.cpp
@@ -2145,10 +2145,10 @@ ROM_START( supracan )
 	// 68k internal ROM (security related, not currently used by driver)
 	ROM_LOAD( "internal_68k.bin", 0x0000,  0x1000, CRC(8d575662) SHA1(a8e75633662978d0a885f16a4ed0f898f278a10a) )
 
-	ROM_REGION(0x8000, "internal6502", ROMREGION_ERASEFF)
+	ROM_REGION(0x2000, "internal6502", ROMREGION_ERASEFF)
 	// 2 additional blocks of ROM(?) can be seen next to the 68k ROM on a die shot from Furrtek
 	ROM_LOAD( "internal_6502_1.bin", 0x0000,  0x1000, NO_DUMP )
-	ROM_LOAD( "internal_6502_2.bin", 0x4000,  0x1000, NO_DUMP )
+	ROM_LOAD( "internal_6502_2.bin", 0x1000,  0x1000, NO_DUMP )
 ROM_END
 
 } // Anonymous namespace
