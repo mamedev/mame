02192d18e1a47b44549bfc008a1c2bce3f39f8b1
AJR <ajrhacker@users.noreply.github.com>
2023-06-19 09:32:26 -0400

    New machines marked as NOT_WORKING ---------------------------------- Orla HK1000 Orla XM200 Orchestra Module

--- scripts/src/cpu.lua
+++ scripts/src/cpu.lua
@@ -3835,9 +3835,16 @@ if opt_tool(CPUS, "INTERDATA16") then
 end
 
 --------------------------------------------------
--- SGS-Thomson ST9, disassembler only
+-- SGS-Thomson ST9
 --@src/devices/cpu/st9/st9.h,CPUS["ST9"] = true
 --------------------------------------------------
+
+if CPUS["ST9"] then
+	files {
+		MAME_DIR .. "src/devices/cpu/st9/st905x.cpp",
+		MAME_DIR .. "src/devices/cpu/st9/st905x.h",
+	}
+end
 
 if opt_tool(CPUS, "ST9") then
 	table.insert(disasm_files , MAME_DIR .. "src/devices/cpu/st9/st9dasm.cpp")
