9987894fbb8a01ebaec45da48355a22c8325232d
Бранимир Караџић <branimirkaradzic@gmail.com>
2023-06-20 09:25:01 -0700

    Removed non-existing C++ overloads from IDL.

--- 3rdparty/bgfx/include/bgfx/c99/bgfx.h
+++ 3rdparty/bgfx/include/bgfx/c99/bgfx.h
@@ -2614,7 +2614,6 @@ BGFX_C_API void bgfx_encoder_set_instance_data_buffer(bgfx_encoder_t* _this, con
  * @param[in] _handle Vertex buffer.
  * @param[in] _startVertex First instance data.
  * @param[in] _num Number of data instances.
- *  Set instance data buffer for draw primitive.
  *
  */
 BGFX_C_API void bgfx_encoder_set_instance_data_from_vertex_buffer(bgfx_encoder_t* _this, bgfx_vertex_buffer_handle_t _handle, uint32_t _startVertex, uint32_t _num);
@@ -3198,7 +3197,6 @@ BGFX_C_API void bgfx_set_instance_data_buffer(const bgfx_instance_data_buffer_t*
  * @param[in] _handle Vertex buffer.
  * @param[in] _startVertex First instance data.
  * @param[in] _num Number of data instances.
- *  Set instance data buffer for draw primitive.
  *
  */
 BGFX_C_API void bgfx_set_instance_data_from_vertex_buffer(bgfx_vertex_buffer_handle_t _handle, uint32_t _startVertex, uint32_t _num);
