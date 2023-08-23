a2c58c1abc4ae5a638577d9c61da1f666ace9e82
ArcadeShadow <arcadeshadow@gmail.com>
2023-08-23 22:23:34 +0100

    Forced UTF8 encoding
    Forced UTF8 encoding without escape code

--- src/devices/bus/samcoupe/expansion/expansion.cpp
+++ src/devices/bus/samcoupe/expansion/expansion.cpp
@@ -16,7 +16,7 @@
 //  DEVICE DEFINITIONS
 //**************************************************************************
 
-DEFINE_DEVICE_TYPE(SAMCOUPE_EXPANSION, samcoupe_expansion_device, "samcoupe_expansion", "SAM Coup\xC3\xA9 Expansion Bus")
+DEFINE_DEVICE_TYPE(SAMCOUPE_EXPANSION, samcoupe_expansion_device, "samcoupe_expansion", u8"SAM Coupé Expansion Bus")
 
 
 //**************************************************************************
