17a84d11ad787daf0cfae19c0aa0ae5312c4c3c6
Olivier Galibert <galibert@pobox.com>
2023-09-19 09:53:11 +0200

    ks1064: Identify the correct decompression table [windyfairy]

--- src/devices/sound/ks0164.h
+++ src/devices/sound/ks0164.h
@@ -46,6 +46,8 @@ private:
 		MPUS_RX_INT  = 0x80
 	};
 
+	static const u16 sample_dec[0x100];
+
 	devcb_write_line m_midi_tx;
 
 	optional_memory_region m_mem_region;
@@ -106,8 +108,6 @@ private:
 	void midi_w(u8 data);
 	u8 midi_status_r();
 	void midi_status_w(u8 data);
-
-	static inline u16 uncomp_8_16(u8 value);
 };
 
 DECLARE_DEVICE_TYPE(KS0164, ks0164_device)
