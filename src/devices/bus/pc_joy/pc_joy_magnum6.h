66943154738be10cc2b957600d36379ba0378fe5
angelosa <lordkale4@gmail.com>
2023-09-21 20:46:32 +0200

    bus/pc_joy/pc_joy_magnum6.h: add imperfect flag for feature::CONTROLS

--- src/devices/bus/pc_joy/pc_joy_magnum6.h
+++ src/devices/bus/pc_joy/pc_joy_magnum6.h
@@ -11,6 +11,8 @@ class pc_joy_magnum6_device : public pc_basic_joy_device
 public:
 	pc_joy_magnum6_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock = 0);
 
+	static constexpr feature_type imperfect_features() { return feature::CONTROLS; }
+
 	virtual uint8_t x2(int delta) override { return BIT(m_btn->read(), 4); }
 	virtual uint8_t y2(int delta) override { return BIT(m_btn->read(), 5); }
 
