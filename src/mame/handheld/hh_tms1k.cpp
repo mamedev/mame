3f1795bbc3f3177084bedbde291d71d9cb090348
hap <happppp@users.noreply.github.com>
2023-09-30 13:55:03 +0200

    bcheetah: update notes
    New working clones
    -------------------
    Gorf (program 1, with French Language ROM) [René Balke]

--- src/mame/handheld/hh_tms1k.cpp
+++ src/mame/handheld/hh_tms1k.cpp
@@ -957,15 +957,17 @@ ROM_END
   * TMS1000NLL MP0915 (die label: 1000B, MP0915)
   * 2 motors (one for back axis, one for steering), no sound
 
-  It's a programmable buggy, like Big Trak but much simpler. To add a command
-  step in program-mode, press a direction key and one of the time delay number
-  keys at the same time. To run the program(max 24 steps), switch to run-mode
-  and press the go key.
+  It's a programmable buggy modeled after the Lamborghini Cheetah, it's like
+  Big Trak but much simpler. To add a command step in program-mode, press a
+  direction key and one of the time delay number keys at the same time. To run
+  the program(max 24 steps), switch to run-mode and press the go key.
 
   known releases:
   - Japan: System Control Car: Cheetah, published by Bandai
   - USA: The Incredible Brain Buggy, published by Fundimensions
   - UK: The Incredible Brain Buggy, published by Palitoy (same as USA version)
+
+  Bandai also made an RC car version of the Cheetah with the same mould.
 
 *******************************************************************************/
 
