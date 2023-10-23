db8f36493380afb550a891cc990c545a19057af1
David Haywood <hazemamewip@hotmail.com>
2023-10-23 13:40:45 +0100

    use the device for bigrun and cischeat too

--- src/mame/jaleco/jalcrpt.h
+++ src/mame/jaleco/jalcrpt.h
@@ -1,8 +1,5 @@
 // license:BSD-3-Clause
 // copyright-holders:Luca Elia, David Haywood
-void jaleco_gs88000_rom_decode(running_machine &machine, const char *region);
-void jaleco_d65006_rom_decode(running_machine &machine, const char *region);
-void rodland_rom_decode (running_machine &machine, const char *region);
 
 void decrypt_ms32_tx(running_machine &machine, int addr_xor,int data_xor, const char *region);
 void decrypt_ms32_bg(running_machine &machine, int addr_xor,int data_xor, const char *region);
