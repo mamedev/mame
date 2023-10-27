c80742a7e18d2fbcb4e59fdb25ffde1c7db47401
Vas Crabb <cuavas@users.noreply.github.com>
2023-10-28 02:53:57 +1100

    sound/ap2010pcm.h: Mark sound as imperfect. [skip ci]

--- src/devices/sound/ap2010pcm.h
+++ src/devices/sound/ap2010pcm.h
@@ -18,6 +18,8 @@
 class ap2010pcm_device : public device_t, public device_sound_interface
 {
 public:
+	static constexpr feature_type imperfect_features() { return feature::SOUND; }
+
 	ap2010pcm_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock = 0);
 
 	uint32_t reg_r(offs_t offset);
@@ -31,13 +33,13 @@ protected:
 	virtual void sound_stream_update(sound_stream &stream, std::vector<read_stream_view> const &inputs, std::vector<write_stream_view> &outputs) override;
 
 private:
+	// FIXME: Games check this against 0x1ff, but samples are lost with that limit
+	static inline constexpr uint16_t FIFO_MAX_SIZE = 0x800;
+
 	uint16_t fifo_pop();
 	uint16_t fifo_fast_pop();
 	void fifo_push(uint16_t sample);
 	void fifo_fast_push(uint16_t sample);
-
-	// FIXME: Games check this against 0x1ff, but samples are lost with that limit
-	static inline constexpr uint16_t FIFO_MAX_SIZE = 0x800;
 
 	std::unique_ptr<uint32_t[]> m_regs;
 
@@ -45,15 +47,15 @@ private:
 
 	uint32_t m_sample_rate;
 
-	uint16_t m_fifo_data[FIFO_MAX_SIZE]{};
-	uint16_t m_fifo_size = 0;
-	uint16_t m_fifo_head = 0;
-	uint16_t m_fifo_tail = 0;
+	uint16_t m_fifo_data[FIFO_MAX_SIZE];
+	uint16_t m_fifo_size;
+	uint16_t m_fifo_head;
+	uint16_t m_fifo_tail;
 
-	uint16_t m_fifo_fast_data[FIFO_MAX_SIZE]{};
-	uint16_t m_fifo_fast_size = 0;
-	uint16_t m_fifo_fast_head = 0;
-	uint16_t m_fifo_fast_tail = 0;
+	uint16_t m_fifo_fast_data[FIFO_MAX_SIZE];
+	uint16_t m_fifo_fast_size;
+	uint16_t m_fifo_fast_head;
+	uint16_t m_fifo_fast_tail;
 
 	sound_stream *m_stream;
 };
