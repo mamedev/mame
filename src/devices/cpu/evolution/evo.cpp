52871addbe8afed892ee94c8ef24304450df1059
Vas Crabb <cuavas@users.noreply.github.com>
2023-11-03 03:07:55 +1100

    cpu/evolution: Trim down CPU.

--- src/devices/cpu/evolution/evo.cpp
+++ src/devices/cpu/evolution/evo.cpp
@@ -3,14 +3,18 @@
 
 #include "emu.h"
 #include "evo.h"
+
 #include "evod.h"
+
 
 DEFINE_DEVICE_TYPE(EVOLUTION_CPU, evo_cpu_device, "evo_cpu", "Evolution CPU")
 
 evo_cpu_device::evo_cpu_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
 	: cpu_device(mconfig, EVOLUTION_CPU, tag, owner, clock)
 	, m_program_config("program", ENDIANNESS_LITTLE, 16, 24, -1)
-	, m_pc(0), m_program(nullptr), m_icount(0), m_debugger_temp(0)
+	, m_pc(0)
+	, m_program(nullptr)
+	, m_icount(0)
 {
 }
 
@@ -31,31 +35,9 @@ void evo_cpu_device::device_start()
 	m_pc = 0;
 	m_debugger_temp = 0;
 	m_program = &space(AS_PROGRAM);
-	state_add(EVO_PC,  "PC", m_debugger_temp).callimport().callexport().formatstr("%08X");
-	state_add(STATE_GENPCBASE, "CURPC", m_debugger_temp).callimport().callexport().noshow();
+	state_add(STATE_GENPC,     "PC",    m_pc);
+	state_add(STATE_GENPCBASE, "CURPC", m_pc).noshow();
 	set_icountptr(m_icount);
-}
-
-void evo_cpu_device::state_export(const device_state_entry &entry)
-{
-	switch (entry.index())
-	{
-		case EVO_PC:
-		case STATE_GENPCBASE:
-			m_debugger_temp = m_pc;
-			break;
-	}
-}
-
-void evo_cpu_device::state_import(const device_state_entry &entry)
-{
-	switch (entry.index())
-	{
-		case EVO_PC:
-		case STATE_GENPCBASE:
-			m_pc = (m_debugger_temp & 0xffffffff);
-			break;
-	}
 }
 
 void evo_cpu_device::device_reset()
@@ -69,10 +51,5 @@ void evo_cpu_device::execute_set_input(int irqline, int state)
 
 void evo_cpu_device::execute_run()
 {
-	while (m_icount > 0)
-	{
-		debugger_instruction_hook(m_pc);
-		m_pc++;
-		m_icount--;
-	}
+	m_icount = 0;
 }
