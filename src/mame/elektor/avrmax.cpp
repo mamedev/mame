b774bb28849fe2e103902bf75456d8c415312283
MooglyGuy <MooglyGuy@users.noreply.github.com>
2023-11-05 18:58:36 +0100

    Optimizations for Atmel AVR-8 core (#11622)
    * -avr8: Optimized timers; roughly 1.5x speedup. [Ryan Holtz]
    ---------
    Co-authored-by: Ryan Holtz <TheMogMiner>

--- src/mame/elektor/avrmax.cpp
+++ src/mame/elektor/avrmax.cpp
@@ -78,7 +78,7 @@ protected:
 
 private:
 	// devices/pointers
-	required_device<avr8_device> m_maincpu;
+	required_device<atmega88_device> m_maincpu;
 	optional_device<pwm_display_device> m_digit_pwm;
 	optional_device<hd44780_device> m_lcd;
 	required_ioport_array<4> m_inputs;
@@ -254,8 +254,8 @@ void avrmax_state::base(machine_config &config)
 	m_maincpu->set_addrmap(AS_PROGRAM, &avrmax_state::main_map);
 	m_maincpu->set_addrmap(AS_DATA, &avrmax_state::data_map);
 	m_maincpu->set_eeprom_tag("eeprom");
-	m_maincpu->gpio_in<AVR8_IO_PORTB>().set(FUNC(avrmax_state::input_r));
-	m_maincpu->gpio_out<AVR8_IO_PORTC>().set(FUNC(avrmax_state::input_w));
+	m_maincpu->gpio_in<atmega88_device::GPIOB>().set(FUNC(avrmax_state::input_r));
+	m_maincpu->gpio_out<atmega88_device::GPIOC>().set(FUNC(avrmax_state::input_w));
 }
 
 void avrmax_state::avrmax(machine_config &config)
@@ -264,8 +264,8 @@ void avrmax_state::avrmax(machine_config &config)
 
 	// basic machine hardware
 	m_maincpu->set_clock(8000000); // internal R/C clock
-	m_maincpu->gpio_out<AVR8_IO_PORTC>().set(FUNC(avrmax_state::digit_w));
-	m_maincpu->gpio_out<AVR8_IO_PORTD>().set(FUNC(avrmax_state::segment_w));
+	m_maincpu->gpio_out<atmega88_device::GPIOC>().set(FUNC(avrmax_state::digit_w));
+	m_maincpu->gpio_out<atmega88_device::GPIOD>().set(FUNC(avrmax_state::segment_w));
 
 	// video hardware
 	PWM_DISPLAY(config, m_digit_pwm).set_size(4, 8);
@@ -278,7 +278,7 @@ void avrmax_state::atm18mcc(machine_config &config)
 	base(config);
 
 	// basic machine hardware
-	m_maincpu->gpio_out<AVR8_IO_PORTD>().set(FUNC(avrmax_state::lcd_w));
+	m_maincpu->gpio_out<atmega88_device::GPIOD>().set(FUNC(avrmax_state::lcd_w));
 
 	// video hardware
 	auto &screen(SCREEN(config, "screen", SCREEN_TYPE_LCD));
