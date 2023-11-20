2b29694fdacb0289ff8c4d882d1887c9e0b6d17e
angelosa <lordkale4@gmail.com>
2023-11-20 22:47:40 +0100

    taito/cchance.cpp: add I/O irq source, add DSWC, add TAITOIO_OPTO, overhaul inputs

--- src/mame/taito/cchance.cpp
+++ src/mame/taito/cchance.cpp
@@ -9,10 +9,9 @@ driver by David Haywood & Angelo Salese
 A cherry-type game that uses the tnzs video chip,might be a modified board as well.
 
 TODO:
--imperfect hopper emulation makes this game to not coin up and gives "hopper time out errors" when you win (put dsw 1
-(0x01) and 8 (0x80) to enable hopper full-mode);
--Wrong colors,caused by missing color proms;
--Verify clock dividers for Z80 and YM2149
+- hopper emulation, similar to the other Taito gamblers;
+- Undumped color proms -> ugly colors;
+- Verify clock dividers for Z80 and YM2149;
 
 ============================================================================================================================
 
@@ -39,7 +38,10 @@ cha3    $10d8
 #include "tnzs_video.h"
 
 #include "cpu/z80/z80.h"
+#include "machine/timer.h"
 #include "sound/ay8910.h"
+
+#include "taitoio_opto.h"
 
 #include "speaker.h"
 
@@ -51,6 +53,8 @@ class cchance_state : public tnzs_video_state_base
 public:
 	cchance_state(const machine_config &mconfig, device_type type, const char *tag)
 		: tnzs_video_state_base(mconfig, type, tag)
+		, m_opto(*this, "opto")
+		, m_dswc(*this, "DSWC")
 	{ }
 
 	void cchance(machine_config &config) ATTR_COLD;
@@ -60,35 +64,35 @@ protected:
 	virtual void machine_reset() override ATTR_COLD;
 
 private:
+	required_device<taitoio_opto_device> m_opto;
+	required_ioport m_dswc;
+
 	void output_0_w(uint8_t data);
-	uint8_t input_1_r();
 	void output_1_w(uint8_t data);
 
 	void main_map(address_map &map) ATTR_COLD;
 
-	uint8_t m_hop_io = 0;
-	uint8_t m_bell_io = 0;
+//  uint8_t m_hop_io = 0;
+//  uint8_t m_bell_io = 0;
+
+	bool m_vblank_irq = false, m_io_irq = false;
+	uint8_t m_irq_ack = 0;
+	TIMER_DEVICE_CALLBACK_MEMBER(scanline_cb);
 };
 
 
 void cchance_state::output_0_w(uint8_t data)
 {
 	//---- --x- divider?
-	machine().bookkeeping().coin_lockout_w(0, ~data & 1);
+//  machine().bookkeeping().coin_lockout_w(0, ~data & 1);
 
 //  machine().bookkeeping().coin_counter_w(0, ~data & 1);
 }
 
-
-uint8_t cchance_state::input_1_r()
-{
-	return m_hop_io | m_bell_io | (ioport("SP")->read() & 0xff);
-}
-
 void cchance_state::output_1_w(uint8_t data)
 {
-	m_hop_io = (data & 0x40) >>4;
-	m_bell_io = (data & 0x80) >>4;
+//  m_hop_io = (data & 0x40) >>4;
+//  m_bell_io = (data & 0x80) >>4;
 }
 
 void cchance_state::main_map(address_map &map)
@@ -104,85 +108,110 @@ void cchance_state::main_map(address_map &map)
 	map(0xe300, 0xe303).ram().mirror(0xfc).w(m_spritegen, FUNC(x1_001_device::spritectrl_w8));  // control registers (0x80 mirror used by Arkanoid 2)
 	map(0xe800, 0xe800).w(m_spritegen, FUNC(x1_001_device::spritebgflag_w8));   // enable / disable background transparency
 
-	map(0xf000, 0xf000).nopr().nopw(); //???
-	map(0xf001, 0xf001).r(FUNC(cchance_state::input_1_r)).w(FUNC(cchance_state::output_0_w));
-	map(0xf002, 0xf002).portr("IN0").w(FUNC(cchance_state::output_1_w));
+	map(0xf000, 0xf000).lrw8(
+		NAME([this] () {
+			// DSWC is read during startup and at input test RAM buffer=$c4c2
+			// bits 4, 5 used as irq vector
+			// bit 7 chip selects acknowledge on writes
+			return !m_vblank_irq << 4 | !m_io_irq << 5 | (m_dswc->read() & 0xf);
+		}),
+		NAME([this] (u8 data) {
+			if (BIT(data, 7))
+			{
+				if (!BIT(m_irq_ack, 4) && BIT(data, 4))
+					m_vblank_irq = false;
+				if (!BIT(m_irq_ack, 5) && BIT(data, 5))
+					m_io_irq = false;
+
+				if (!m_vblank_irq && !m_io_irq)
+					m_maincpu->set_input_line(0, CLEAR_LINE);
+			}
+
+			m_irq_ack = data;
+		})
+	);
+	map(0xf001, 0xf001).portr("IN0").w(FUNC(cchance_state::output_0_w));
+	map(0xf002, 0xf002).portr("IN1").w(FUNC(cchance_state::output_1_w));
 	map(0xf800, 0xf801).w("aysnd", FUNC(ay8910_device::address_data_w));
 	map(0xf801, 0xf801).r("aysnd", FUNC(ay8910_device::data_r));
 }
 
 
 static INPUT_PORTS_START( cchance )
+	PORT_START("COIN")
+	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_COIN1 ) PORT_WRITE_LINE_DEVICE_MEMBER("opto", taitoio_opto_device, coin_sense_w)
+
 	PORT_START("IN0")
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_SERVICE1 )
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_CUSTOM ) PORT_READ_LINE_DEVICE_MEMBER("opto", taitoio_opto_device, opto_h_r)
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_CUSTOM ) PORT_READ_LINE_DEVICE_MEMBER("opto", taitoio_opto_device, opto_l_r)
+	// Hopper related? Goes "hopper time out" if IP_ACTIVE_LOW as suggested by service mode (buggy?)
+	PORT_BIT (0x04, IP_ACTIVE_HIGH, IPT_OTHER ) PORT_NAME("Pay Out")
+	PORT_BIT (0x08, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Hopper Over") // "Hop Over"?
+	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_START1 ) PORT_NAME("Slottle")
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Drop SW") // "Coin Drop SW" as per other Taito gamblers?
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_SERVICE1 ) PORT_NAME("Reset Key")
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_SERVICE2 ) PORT_NAME("Last Key")
+
+	PORT_START("IN1")
+	PORT_SERVICE_NO_TOGGLE( 0x01, IP_ACTIVE_LOW )
 	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_UNUSED )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_COIN1 )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_NAME("Bet SW") PORT_CODE(KEYCODE_X)
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_SERVICE2 ) PORT_NAME("Analyzer")
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Payout SW")  PORT_CODE(KEYCODE_L)
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_COIN2 ) PORT_NAME("Credit SW")
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_GAMBLE_BET ) PORT_NAME("Bet SW")
+	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_SERVICE3 ) PORT_NAME("Crt Key") // analyzer
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_GAMBLE_PAYOUT ) PORT_NAME("Payout SW")
 	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNUSED )
 	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_UNUSED )
 
-	PORT_START("SP")
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Opt 1") PORT_CODE(KEYCODE_A)
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Opt 2") PORT_CODE(KEYCODE_S)
-//  PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Payout") PORT_CODE(KEYCODE_D)
-//  PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Hop Over") PORT_CODE(KEYCODE_F)
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_NAME("Slottle") PORT_CODE(KEYCODE_Z)
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Drop SW") PORT_CODE(KEYCODE_H)
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Reset Key") PORT_CODE(KEYCODE_J)
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_OTHER ) PORT_NAME("Last Key") PORT_CODE(KEYCODE_K)
-
-	PORT_START("DSW1")
-	PORT_DIPNAME( 0x01, 0x00, "DSW1" )
-	PORT_DIPSETTING(    0x01, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x02, 0x02, DEF_STR( Unknown ) )
+	PORT_START("DSWA")
+	// debug/cheat
+	PORT_DIPNAME( 0x01, 0x01, "Credits at start" ) PORT_DIPLOCATION("DSWA:1")
+	PORT_DIPSETTING(    0x01, "0" )
+	PORT_DIPSETTING(    0x00, "500" )
+	PORT_DIPNAME( 0x02, 0x02, DEF_STR( Unknown ) ) PORT_DIPLOCATION("DSWA:2")
 	PORT_DIPSETTING(    0x02, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Unknown ) )
+	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Unknown ) ) PORT_DIPLOCATION("DSWA:3")
 	PORT_DIPSETTING(    0x04, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x08, 0x08, DEF_STR( Unknown ) )
+	PORT_DIPNAME( 0x08, 0x08, DEF_STR( Unknown ) ) PORT_DIPLOCATION("DSWA:4")
 	PORT_DIPSETTING(    0x08, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x10, 0x10, DEF_STR( Unknown ) )
+	PORT_DIPNAME( 0x10, 0x10, DEF_STR( Unknown ) ) PORT_DIPLOCATION("DSWA:5")
 	PORT_DIPSETTING(    0x10, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x20, 0x20, DEF_STR( Unknown ) )
+	PORT_DIPNAME( 0x20, 0x20, DEF_STR( Unknown ) ) PORT_DIPLOCATION("DSWA:6")
 	PORT_DIPSETTING(    0x20, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x40, 0x40, DEF_STR( Unknown ) )
+	PORT_DIPNAME( 0x40, 0x40, DEF_STR( Unknown ) ) PORT_DIPLOCATION("DSWA:7")
 	PORT_DIPSETTING(    0x40, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x80, 0x00, DEF_STR( Unknown ) )
+	PORT_DIPNAME( 0x80, 0x00, DEF_STR( Unknown ) ) PORT_DIPLOCATION("DSWA:8")
 	PORT_DIPSETTING(    0x80, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
 
-	PORT_START("DSW2") //likely to be unused
-	PORT_DIPNAME( 0x01, 0x01, "DSW2" )
+	// unused according to service mode
+	PORT_START("DSWB")
+	PORT_DIPUNUSED_DIPLOC( 0x01, IP_ACTIVE_LOW, "DSWB:1")
+	PORT_DIPUNUSED_DIPLOC( 0x02, IP_ACTIVE_LOW, "DSWB:2")
+	PORT_DIPUNUSED_DIPLOC( 0x04, IP_ACTIVE_LOW, "DSWB:3")
+	PORT_DIPUNUSED_DIPLOC( 0x08, IP_ACTIVE_LOW, "DSWB:4")
+	PORT_DIPUNUSED_DIPLOC( 0x10, IP_ACTIVE_LOW, "DSWB:5")
+	PORT_DIPUNUSED_DIPLOC( 0x20, IP_ACTIVE_LOW, "DSWB:6")
+	PORT_DIPUNUSED_DIPLOC( 0x40, IP_ACTIVE_LOW, "DSWB:7")
+	PORT_DIPUNUSED_DIPLOC( 0x80, IP_ACTIVE_LOW, "DSWB:8")
+
+	PORT_START("DSWC")
+	PORT_DIPNAME( 0x01, 0x01, "DSWC" ) PORT_DIPLOCATION("DSWC:1")
 	PORT_DIPSETTING(    0x01, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x02, 0x02, DEF_STR( Unknown ) )
+	PORT_DIPNAME( 0x02, 0x02, DEF_STR( Unknown ) ) PORT_DIPLOCATION("DSWC:2")
 	PORT_DIPSETTING(    0x02, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Unknown ) )
+	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Unknown ) ) PORT_DIPLOCATION("DSWC:3")
 	PORT_DIPSETTING(    0x04, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x08, 0x08, DEF_STR( Unknown ) )
+	PORT_DIPNAME( 0x08, 0x08, DEF_STR( Unknown ) ) PORT_DIPLOCATION("DSWC:4")
 	PORT_DIPSETTING(    0x08, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x10, 0x10, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x10, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x20, 0x20, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x20, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x40, 0x40, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x40, DEF_STR( Off ) )
-	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x80, 0x80, DEF_STR( Unknown ) )
-	PORT_DIPSETTING(    0x80, DEF_STR( Off ) )
 	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
 INPUT_PORTS_END
 
@@ -209,23 +238,52 @@ void cchance_state::machine_start()
 {
 	tnzs_video_state_base::machine_start();
 
-	save_item(NAME(m_hop_io));
-	save_item(NAME(m_bell_io));
+	save_item(NAME(m_vblank_irq));
+	save_item(NAME(m_io_irq));
+	save_item(NAME(m_irq_ack));
+//  save_item(NAME(m_hop_io));
+//  save_item(NAME(m_bell_io));
 }
 
 void cchance_state::machine_reset()
 {
 	tnzs_video_state_base::machine_reset();
 
-	m_hop_io = 0;
-	m_bell_io = 0;
+	m_vblank_irq = m_io_irq = false;
+	m_irq_ack = 0;
+//  m_hop_io = 0;
+//  m_bell_io = 0;
+}
+
+TIMER_DEVICE_CALLBACK_MEMBER(cchance_state::scanline_cb)
+{
+	int scanline = param;
+
+	// vblank irq
+	if (scanline == 30*8)
+	{
+		m_maincpu->set_input_line(0, HOLD_LINE);
+		m_vblank_irq = true;
+		m_io_irq = false;
+	}
+
+	// i/o irq, unknown source
+	if (scanline == 0)
+	{
+		m_maincpu->set_input_line(0, HOLD_LINE);
+		m_vblank_irq = false;
+		m_io_irq = true;
+	}
 }
 
 void cchance_state::cchance(machine_config &config)
 {
 	Z80(config, m_maincpu, 12_MHz_XTAL / 2); // LH0080B
 	m_maincpu->set_addrmap(AS_PROGRAM, &cchance_state::main_map);
-	m_maincpu->set_vblank_int("screen", FUNC(cchance_state::irq0_line_hold));
+	//m_maincpu->set_vblank_int("screen", FUNC(cchance_state::irq0_line_hold));
+	TIMER(config, "scantimer").configure_scanline(FUNC(cchance_state::scanline_cb), "screen", 0, 1);
+
+	TAITOIO_OPTO(config, "opto", 0);
 
 	X1_001(config, m_spritegen, 12_MHz_XTAL, m_palette, gfx_cchance);
 	m_spritegen->set_fg_yoffsets(-0x12, 0x0e);
@@ -234,7 +292,7 @@ void cchance_state::cchance(machine_config &config)
 	// video hardware
 	SCREEN(config, m_screen, SCREEN_TYPE_RASTER);
 	m_screen->set_refresh_hz(57.5);
-	m_screen->set_vblank_time(ATTOSECONDS_IN_USEC(0));
+	m_screen->set_vblank_time(ATTOSECONDS_IN_USEC(2500));
 	m_screen->set_size(32*8, 32*8);
 	m_screen->set_visarea(0*8, 32*8-1, 2*8, 30*8-1);
 	m_screen->set_screen_update(FUNC(cchance_state::screen_update_tnzs));
@@ -246,8 +304,8 @@ void cchance_state::cchance(machine_config &config)
 	SPEAKER(config, "mono").front_center();
 
 	ym2149_device &aysnd(YM2149(config, "aysnd", 12_MHz_XTAL / 8));
-	aysnd.port_a_read_callback().set_ioport("DSW1");
-	aysnd.port_b_read_callback().set_ioport("DSW2");
+	aysnd.port_a_read_callback().set_ioport("DSWA");
+	aysnd.port_b_read_callback().set_ioport("DSWB");
 	aysnd.add_route(ALL_OUTPUTS, "mono", 0.25);
 }
 
