7ef4abaf1b9cac81225837950bd00cbf3043e5ac
David Haywood <hazemamewip@hotmail.com>
2023-11-21 18:47:00 +0000

    basic bootstrapping

--- src/mame/tvgames/spg29x_lexibook_jg7425.cpp
+++ src/mame/tvgames/spg29x_lexibook_jg7425.cpp
@@ -34,7 +34,7 @@ public:
 		m_maincpu(*this, "maincpu"),
 		m_screen(*this, "screen"),
 		m_mainram(*this, "mainram"),
-		m_romregion(*this, "maincpu")
+		m_romregion(*this, "extrom")
 	{ }
 
 	void lexibook_jg7425(machine_config &config);
@@ -51,12 +51,16 @@ private:
 	required_device<score7_cpu_device> m_maincpu;
 	required_device<screen_device> m_screen;
 	required_shared_ptr<uint32_t> m_mainram;
-	required_region_ptr<uint32_t> m_romregion;
+	required_region_ptr<uint8_t> m_romregion;
 };
 
 
 
 void lexibook_jg7425_state::machine_start()
+{
+}
+
+void lexibook_jg7425_state::machine_reset()
 {
 	// I think this code should be running from RAM at least, probably some kind of bootstrap / internal ROM to copy it? (hyperscan.cpp indicates that SoC can have internal ROM at least)
 
@@ -80,15 +84,17 @@ void lexibook_jg7425_state::machine_start()
 
 	*/
 
+	uint32_t loadaddr = 0xa05001fc;
+	uint8_t* rom = (uint8_t*)&m_romregion[0];
 
-	for (int i = 0; i < 0x80000 / 4; i++)
+	for (int i = 0; i < 0x6dbd4; i++)
 	{
-		m_mainram[i+(0x500000/4) - (0x20/4)] = m_romregion[i];
-	}
-}
+		uint32_t data = rom[0x20 + i];
 
-void lexibook_jg7425_state::machine_reset()
-{
+		m_maincpu->space(AS_PROGRAM).write_byte(loadaddr+i, data);
+	}
+
+	m_maincpu->set_state_int(SCORE_PC, 0xa0501000);
 }
 
 static INPUT_PORTS_START( lexibook_jg7425 )
@@ -102,9 +108,11 @@ uint32_t lexibook_jg7425_state::screen_update(screen_device &screen, bitmap_rgb3
 
 void lexibook_jg7425_state::map(address_map &map)
 {
-	map(0x00000000, 0x00ffffff).ram().share("mainram");
+	map(0xa0500000, 0xa0ffffff).ram();
 
 	map(0x9f000000, 0x9fffffff).ram().share("mainram");
+
+	// it quickly ends up jumping to BF000024, which is probably internal ROM - can we simulate what it wants?
 }
 
 
@@ -122,24 +130,24 @@ void lexibook_jg7425_state::lexibook_jg7425(machine_config &config)
 }
 
 ROM_START( lx_jg7425 )
-	ROM_REGION( 0x200000, "maincpu", ROMREGION_32BIT | ROMREGION_LE )
-	ROM_LOAD32_DWORD( "mx29lv160.u6", 0x000000, 0x200000, CRC(43c90080) SHA1(4c9e5c8f880d40bd684357ce67ae45c3f5d24b62) )
+	ROM_REGION( 0x200000, "extrom", ROMREGION_ERASEFF )
+	ROM_LOAD( "mx29lv160.u6", 0x000000, 0x200000, CRC(43c90080) SHA1(4c9e5c8f880d40bd684357ce67ae45c3f5d24b62) )
 
 	DISK_REGION( "ata:0:hdd" ) /* 4GB SD Card */
 	DISK_IMAGE( "lexibook_jg7425_4gbsd", 0, SHA1(dc0985103edec3992efdd493feef6185daedb3fd) )
 ROM_END
 
 ROM_START( lx_aven )
-	ROM_REGION( 0x200000, "maincpu", ROMREGION_32BIT | ROMREGION_LE )
-	ROM_LOAD32_DWORD( "29lv800.bin", 0x000000, 0x100000, CRC(7b107f6c) SHA1(3a8e37e51dab5cab9977261e0ac17ba5194a9370) )
+	ROM_REGION( 0x200000, "extrom", ROMREGION_ERASEFF )
+	ROM_LOAD( "29lv800.bin", 0x000000, 0x100000, CRC(7b107f6c) SHA1(3a8e37e51dab5cab9977261e0ac17ba5194a9370) )
 
 	DISK_REGION( "ata:0:hdd" ) /* 4GB SD Card */
 	DISK_IMAGE( "sd-card", 0, SHA1(911da7bf7dac391e3329e17e3f411caafac52f0f) )
 ROM_END
 
 ROM_START( lx_frozen )
-	ROM_REGION( 0x200000, "maincpu", ROMREGION_32BIT | ROMREGION_LE )
-	ROM_LOAD32_DWORD( "29lv800.bin", 0x000000, 0x100000, CRC(7b107f6c) SHA1(3a8e37e51dab5cab9977261e0ac17ba5194a9370) )
+	ROM_REGION( 0x200000, "extrom", ROMREGION_ERASEFF )
+	ROM_LOAD( "29lv800.bin", 0x000000, 0x100000, CRC(7b107f6c) SHA1(3a8e37e51dab5cab9977261e0ac17ba5194a9370) )
 
 	DISK_REGION( "ata:0:hdd" ) /* 4GB SD Card */
 	DISK_IMAGE( "sdcard", 0, SHA1(0d727815ba06d7bfe8e092007e24d4931b302ef9) )
