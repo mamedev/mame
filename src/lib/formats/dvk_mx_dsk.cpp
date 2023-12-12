93b289777a5528449c8efb77c2da7b00e6b73805
Sergey Svishchev <shattered@users.noreply.github.com>
2023-12-12 19:27:40 +0300

    qbus device: DVK "MX" floppy controller

--- src/lib/formats/dvk_mx_dsk.cpp
+++ src/lib/formats/dvk_mx_dsk.cpp
@@ -23,63 +23,43 @@
 #include "ioprocs.h"
 #include "multibyte.h"
 
+constexpr uint32_t ID_PATTERN = 0xaaaaffaf;
 
 const floppy_image_format_t::desc_e dvk_mx_format::dvk_mx_new_desc[] = {
 	/* 01 */ { FM, 0x00, 8*2 }, // eight 0x0000 words
-	/* 03 */ { FM, 0x00, 1 },
-	/* 02 */ { FM, 0xf3, 1 },   // word 0x00f3
-	/* 05 */ { FM, 0x00, 1 },
-	/* 04 */ { TRACK_ID_FM },   // track number word
-	/* 05 */ { SECTOR_LOOP_START, 0, 10 },  // 11 sectors
+	/* 02 */ { FM, 0x00, 1 },
+	/* 03 */ { FM, 0xf3, 1 },   // word 0x00f3
+	/* 04 */ { FM, 0x00, 1 },
+	/* 05 */ { TRACK_ID_FM },   // track number word
+	/* 06 */ { SECTOR_LOOP_START, 0, 10 },  // 11 sectors
 	/* 07 */ {   SECTOR_DATA_MX, -1 },
-	/* 10 */ { SECTOR_LOOP_END },
-	/* 13 */ { FM, 0x83, 1 },
+	/* 08 */ { SECTOR_LOOP_END },
+	/* 09 */ { FM, 0x83, 1 },
+	/* 10 */ { OFFSET_ID_FM },
+	/* 11 */ { FM, 0x83, 1 },
 	/* 12 */ { OFFSET_ID_FM },
-	/* 15 */ { FM, 0x83, 1 },
+	/* 13 */ { FM, 0x83, 1 },
 	/* 14 */ { OFFSET_ID_FM },
-	/* 17 */ { FM, 0x83, 1 },
-	/* 16 */ { OFFSET_ID_FM },
-	/* 18 */ { END }
+	/* 15 */ { END }
 };
 
 const floppy_image_format_t::desc_e dvk_mx_format::dvk_mx_old_desc[] = {
 	/* 01 */ { FM, 0x00, 30*2 },
-	/* 03 */ { FM, 0x00, 1 },
-	/* 02 */ { FM, 0xf3, 1 },   // word 0x00f3
-	/* 05 */ { FM, 0x00, 1 },
-	/* 04 */ { TRACK_ID_FM },   // track number word
+	/* 02 */ { FM, 0x00, 1 },
+	/* 03 */ { FM, 0xf3, 1 },   // word 0x00f3
+	/* 04 */ { FM, 0x00, 1 },
+	/* 05 */ { TRACK_ID_FM },   // track number word
 	/* 06 */ { SECTOR_LOOP_START, 0, 10 },  // 11 sectors
 	/* 07 */ {   SECTOR_DATA_MX, -1 },
-	/* 10 */ { SECTOR_LOOP_END },
-	/* 13 */ { FM, 0x83, 1 },
-	/* 11 */ { FM, 0x01, 1 },
-	/* 15 */ { FM, 0x83, 1 },
-	/* 14 */ { FM, 0x01, 1 },
-	/* 16 */ { END }
+	/* 08 */ { SECTOR_LOOP_END },
+	/* 09 */ { FM, 0x00, 1 },
+	/* 10 */ { FM, 0116, 1 },
+	/* 11 */ { FM, 0x00, 4*2 }, // four 0x0000 words
+	/* 12 */ { END }
 };
 
 dvk_mx_format::dvk_mx_format()
 {
-}
-
-const char *dvk_mx_format::name() const noexcept
-{
-	return "mx";
-}
-
-const char *dvk_mx_format::description() const noexcept
-{
-	return "DVK MX: floppy image";
-}
-
-const char *dvk_mx_format::extensions() const noexcept
-{
-	return "mx";
-}
-
-bool dvk_mx_format::supports_save() const noexcept
-{
-	return false;
 }
 
 void dvk_mx_format::find_size(util::random_read &io, uint8_t &track_count, uint8_t &head_count, uint8_t &sector_count)
@@ -95,17 +75,17 @@ void dvk_mx_format::find_size(util::random_read &io, uint8_t &track_count, uint8
 	{
 	case 112640:
 		track_count = 40;
-		sector_count = 11;
+		sector_count = MX_SECTORS;
 		head_count = 1;
 		break;
 	case 225280:
 		track_count = 40;
-		sector_count = 11;
+		sector_count = MX_SECTORS;
 		head_count = 2;
 		break;
 	case 450560:
 		track_count = 80;
-		sector_count = 11;
+		sector_count = MX_SECTORS;
 		head_count = 2;
 		break;
 	default:
@@ -143,16 +123,16 @@ bool dvk_mx_format::load(util::random_read &io, uint32_t form_factor, const std:
 	find_size(io, track_count, head_count, sector_count);
 	if (track_count == 0) return false;
 
-	uint8_t sectdata[11 * 256];
-	desc_s sectors[11];
+	uint8_t sectdata[MX_SECTORS * MX_SECTOR_SIZE];
+	desc_s sectors[MX_SECTORS];
 	for (int i = 0; i < sector_count; i++)
 	{
-		sectors[i].data = sectdata + 256 * i;
-		sectors[i].size = 256;
+		sectors[i].data = sectdata + MX_SECTOR_SIZE * i;
+		sectors[i].size = MX_SECTOR_SIZE;
 		sectors[i].sector_id = i;
 	}
 
-	int track_size = sector_count * 256;
+	int track_size = sector_count * MX_SECTOR_SIZE;
 	for (int track = 0; track < track_count; track++)
 	{
 		for (int head = 0; head < head_count; head++)
@@ -179,6 +159,67 @@ bool dvk_mx_format::load(util::random_read &io, uint32_t form_factor, const std:
 		}
 	}
 
+	return true;
+}
+
+bool dvk_mx_format::save(util::random_read_write &io, const std::vector<uint32_t> &variants, const floppy_image &image) const
+{
+	int tracks;
+	int heads;
+	bool res = false;
+	image.get_actual_geometry(tracks, heads);
+
+	for (int cyl = 0; cyl < tracks; cyl++)
+	{
+		for (int head = 0; head < heads; head++)
+		{
+			auto bitstream = generate_bitstream_from_track(cyl, head, 4000, image, 0);
+			uint8_t sector_data[MX_SECTORS * MX_SECTOR_SIZE];
+			if (get_next_sector(bitstream, sector_data))
+			{
+				unsigned offset_in_image = (cyl * heads + head) * MX_SECTOR_SIZE * MX_SECTORS;
+				size_t actual;
+				res = true;
+				io.write_at(offset_in_image, sector_data, MX_SECTOR_SIZE * MX_SECTORS, actual);
+			}
+		}
+	}
+	return res;
+}
+
+bool dvk_mx_format::get_next_sector(const std::vector<bool> &bitstream, uint8_t *sector_data)
+{
+	uint32_t sr = 0;
+	int pos = 0;
+	while (pos < bitstream.size() && sr != ID_PATTERN)
+	{
+		sr = (sr << 1) | bitstream[pos];
+		pos++;
+	}
+	if (pos == bitstream.size())
+	{
+		// End of track reached
+		return false;
+	}
+	unsigned to_dump = MX_SECTORS * (MX_SECTOR_SIZE + 2);
+	pos += 32; // skip track ID
+	for (unsigned i = 0, k = 0; i < to_dump && pos < bitstream.size(); i++)
+	{
+		uint8_t byte = 0;
+		unsigned j;
+		for (j = 0; j < 8 && pos < bitstream.size(); j++)
+		{
+			bool bit = bitstream[pos + 1];
+			pos += 2;
+			byte <<= 1;
+			byte |= bit;
+		}
+		if (j == 8 && (i % (MX_SECTOR_SIZE + 2)) < MX_SECTOR_SIZE)
+		{
+			sector_data[k ^ 1] = byte;
+			k++;
+		}
+	}
 	return true;
 }
 
--- src/lib/formats/dvk_mx_dsk.h
+++ src/lib/formats/dvk_mx_dsk.h
@@ -15,21 +15,26 @@
 class dvk_mx_format : public floppy_image_format_t
 {
 public:
+	static constexpr unsigned MX_SECTORS = 11;
+	static constexpr unsigned MX_SECTOR_SIZE = 256;
+
 	dvk_mx_format();
 
 	virtual int identify(util::random_read &io, uint32_t form_factor, const std::vector<uint32_t> &variants) const override;
 	virtual bool load(util::random_read &io, uint32_t form_factor, const std::vector<uint32_t> &variants, floppy_image &image) const override;
+	virtual bool save(util::random_read_write &io, const std::vector<uint32_t> &variants, const floppy_image &image) const override;
 
-	virtual const char *name() const noexcept override;
-	virtual const char *description() const noexcept override;
-	virtual const char *extensions() const noexcept override;
-	virtual bool supports_save() const noexcept override;
+	const char *name() const noexcept override { return "mx"; }
+	const char *description() const noexcept override { return "DVK MX disk image"; }
+	const char *extensions() const noexcept override { return "mx"; }
+	bool supports_save() const noexcept override { return true; }
 
 	static const desc_e dvk_mx_old_desc[];
 	static const desc_e dvk_mx_new_desc[];
 
 private:
 	static void find_size(util::random_read &io, uint8_t &track_count, uint8_t &head_count, uint8_t &sector_count);
+	static bool get_next_sector(const std::vector<bool> &bitstream , uint8_t *sector_data);
 };
 
 extern const dvk_mx_format FLOPPY_DVK_MX_FORMAT;
