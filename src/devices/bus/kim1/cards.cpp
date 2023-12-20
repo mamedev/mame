73f27a5dae8008d1a2de84f3aabde85c9ed6ec02
arbee <rb6502@users.noreply.github.com>
2023-12-19 20:52:15 -0500

    bus/kim1: Add support for MTU "Visible Memory" (aka "Visable Memory") board. [R. Belmont]

---
+++ src/devices/bus/kim1/cards.cpp
@@ -0,0 +1,19 @@
+// license:BSD-3-Clause
+// copyright-holders:R. Belmont
+/***************************************************************************
+
+    KIM-1 bus slot cards
+
+***************************************************************************/
+
+#include "emu.h"
+#include "cards.h"
+
+#include "k1008_vismem.h"
+#include "k1016_16k.h"
+
+void kim1_cards(device_slot_interface &device)
+{
+	device.option_add("mtuk1008", KIM1BUS_K1008); // MTU K-1008 8K "Visible Memory" mono 320x200 framebuffer
+	device.option_add("mtuk1016", KIM1BUS_K1016); // MTU K-1016 16K RAM card
+}
