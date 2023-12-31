01dde5fbfeb8e4e93ef6e8cef2cb6bf47020183c
David Haywood <hazemamewip@hotmail.com>
2023-12-31 14:49:24 +0000

    maybe paged is worse

--- src/devices/cpu/upd777/upd777dasm.h
+++ src/devices/cpu/upd777/upd777dasm.h
@@ -16,8 +16,8 @@ public:
 protected:
 	// disassembler overrides
 	virtual u32 opcode_alignment() const override { return 1; }
-	virtual u32 interface_flags() const override { return NONLINEAR_PC | PAGED; }
-	virtual u32 page_address_bits() const override { return 7; }
+	//virtual u32 interface_flags() const override { return NONLINEAR_PC | PAGED; }
+	//virtual u32 page_address_bits() const override { return 7; }
 	virtual offs_t pc_linear_to_real(offs_t pc) const override { return (pc & ~0x7f) | m_l2r[pc & 0x7f]; }
 	virtual offs_t pc_real_to_linear(offs_t pc) const override { return (pc & ~0x7f) | m_r2l[pc & 0x7f]; }
 	virtual offs_t disassemble(std::ostream &stream, offs_t pc, const data_buffer &opcodes, const data_buffer &params) override;
