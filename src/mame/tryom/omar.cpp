306c93cdd61b64cb9a9d5cc72206a888c772f963
hap <happppp@users.noreply.github.com>
2024-01-18 23:24:23 +0100

    ctvboy: shorter irq duration, omar2: add lcd svg
    Systems promoted to working
    ---------------------------
    Omar II [hap, Sean Riddle]

--- src/mame/tryom/omar.cpp
+++ src/mame/tryom/omar.cpp
@@ -5,7 +5,7 @@
 
 Tryom Omar, handheld backgammon computer
 
-Hardware notes:
+Hardware notes (Omar I):
 - PCB label: AP0379 REV.8, PC103, JRH
 - Fairchild 3870 MCU, custom label
 - 9-digit 7seg led panel (3 unused), piezo
@@ -15,14 +15,13 @@ Omar II has an LCD instead of 7segs. PCB label is AN2379 REV.6, PC101.
 There are more 'sequels', but assumed to be the same game. The newer ones didn't
 include a backgammon board.
 
-LED versions: Omar(I), Omar IV, Cardinal Electronic Backgammon
+LED versions: Omar I, Omar IV, Cardinal Electronic Backgammon
 LCD versions: Omar II, Omar III, Omar V, Tandy Computerized Backgammon
 
-BTANB:
-- omar piezo buzzes when you hold down a button
+Confirmed to be the same MCU ROM: Omar II = Omar V.
 
-TODO:
-- add omar2 lcd
+BTANB:
+- omar1 piezo buzzes when you hold down a button
 
 *******************************************************************************/
 
@@ -33,9 +32,13 @@ TODO:
 #include "sound/dac.h"
 #include "video/pwm.h"
 
+#include "screen.h"
 #include "speaker.h"
 
-#include "omar.lh"
+#include "multibyte.h"
+
+#include "omar1.lh"
+#include "omar2.lh"
 
 
 namespace {
@@ -54,7 +57,7 @@ public:
 		m_inputs(*this, "IN.%u", 0)
 	{ }
 
-	void omar(machine_config &config);
+	void omar1(machine_config &config);
 
 protected:
 	virtual void machine_start() override;
@@ -102,7 +105,10 @@ protected:
 	virtual void machine_start() override;
 
 private:
+	u8 m_lcd_com = 0;
 	u8 m_lcd_data[4] = { };
+
+	void update_lcd();
 
 	virtual void display1_w(u8 data) override;
 	virtual void display2_w(u8 data) override;
@@ -113,6 +119,8 @@ private:
 void omar2_state::machine_start()
 {
 	omar_state::machine_start();
+
+	save_item(NAME(m_lcd_com));
 	save_item(NAME(m_lcd_data));
 }
 
@@ -185,35 +193,48 @@ void omar_state::display2_w(u8 data)
 
 // LCD version
 
+void omar2_state::update_lcd()
+{
+	m_display->write_row(0, m_lcd_com ? ~get_u32le(m_lcd_data) : get_u32le(m_lcd_data));
+}
+
 void omar2_state::display1_w(u8 data)
 {
 	input1_w(data ^ 0x80);
 
-	// P01-P06: LCD data
+	// P00-P06: LCD data
 	m_lcd_data[0] = (m_lcd_data[0] & 0x80) | (data & 0x7f);
+	update_lcd();
 }
 
 void omar2_state::display2_w(u8 data)
 {
 	input2_w(data);
 
-	// P10-P17: LCD data
-	m_lcd_data[1] = data;
+	// P10: LCD common
+	m_lcd_com = data & 1;
+
+	// P11-P17: LCD data
+	m_lcd_data[1] = (m_lcd_data[1] & 0x01) | (data & 0xfe);
+	update_lcd();
 }
 
 void omar2_state::display3_w(u8 data)
 {
-	// P40: LCD data
-	m_lcd_data[0] = (m_lcd_data[0] & 0x7f) | (data << 7 & 0x80);
+	// P40,P47: LCD data
+	m_lcd_data[0] = (m_lcd_data[0] & 0x7f) | (data & 0x80);
+	m_lcd_data[1] = (m_lcd_data[1] & 0xfe) | (data & 0x01);
 
 	// P41: 4015 data (4015 Q to LCD)
 	m_lcd_data[3] = (m_lcd_data[3] << 1) | (BIT(data, 1));
+	update_lcd();
 }
 
 void omar2_state::display4_w(u8 data)
 {
 	// P50-P57: LCD data
 	m_lcd_data[2] = data;
+	update_lcd();
 }
 
 
@@ -284,7 +305,7 @@ INPUT_PORTS_END
     Machine Configs
 *******************************************************************************/
 
-void omar_state::omar(machine_config &config)
+void omar_state::omar1(machine_config &config)
 {
 	// basic machine hardware
 	F8(config, m_maincpu, 2700000/2); // approximation
@@ -302,7 +323,7 @@ void omar_state::omar(machine_config &config)
 	// video hardware
 	PWM_DISPLAY(config, m_display).set_size(6, 7);
 	m_display->set_segmask(0x3f, 0x7f);
-	config.set_default_layout(layout_omar);
+	config.set_default_layout(layout_omar1);
 
 	// sound hardware
 	SPEAKER(config, "speaker").front_center();
@@ -311,15 +332,21 @@ void omar_state::omar(machine_config &config)
 
 void omar2_state::omar2(machine_config &config)
 {
-	omar(config);
+	omar1(config);
 
 	// basic machine hardware
 	m_psu->write_a().append(FUNC(omar2_state::display3_w));
 	m_psu->write_b().append(FUNC(omar2_state::display4_w));
 
 	// video hardware
+	screen_device &screen(SCREEN(config, "screen", SCREEN_TYPE_SVG));
+	screen.set_refresh_hz(60);
+	screen.set_size(1920/2.5, 412/2.5);
+	screen.set_visarea_full();
+
 	PWM_DISPLAY(config.replace(), m_display).set_size(1, 32);
-	//config.set_default_layout(layout_omar2);
+	m_display->set_bri_levels(0.25);
+	config.set_default_layout(layout_omar2);
 }
 
 
@@ -328,7 +355,7 @@ void omar2_state::omar2(machine_config &config)
     ROM Definitions
 *******************************************************************************/
 
-ROM_START( omar )
+ROM_START( omar1 )
 	ROM_REGION( 0x0800, "maincpu", 0 )
 	ROM_LOAD("lom1ar279", 0x0000, 0x0800, CRC(e1bcee50) SHA1(658d6d8a0af3c1672610c651fea7d0289e4703f7) ) // 3870X-0245
 ROM_END
@@ -336,6 +363,9 @@ ROM_END
 ROM_START( omar2 )
 	ROM_REGION( 0x0800, "maincpu", 0 )
 	ROM_LOAD("xom2ar1779", 0x0000, 0x0800, CRC(e0d6a119) SHA1(40e35020f483245bcc82443923763b037498a98d) ) // 3870X-0248
+
+	ROM_REGION( 43303, "screen", 0 )
+	ROM_LOAD("omar2.svg", 0, 43303, CRC(fc9e757d) SHA1(c52e2f7c01e174286b75efd0c4c137f42c46053f) )
 ROM_END
 
 } // anonymous namespace
@@ -347,5 +377,5 @@ ROM_END
 *******************************************************************************/
 
 //    YEAR  NAME   PARENT  COMPAT  MACHINE  INPUT  CLASS        INIT        COMPANY, FULLNAME, FLAGS
-SYST( 1979, omar,  0,      0,      omar,    omar,  omar_state,  empty_init, "Tryom", "Omar I", MACHINE_SUPPORTS_SAVE | MACHINE_CLICKABLE_ARTWORK )
-SYST( 1979, omar2, 0,      0,      omar2,   omar,  omar2_state, empty_init, "Tryom", "Omar II", MACHINE_SUPPORTS_SAVE | MACHINE_CLICKABLE_ARTWORK | MACHINE_NOT_WORKING )
+SYST( 1979, omar1, 0,      0,      omar1,   omar,  omar_state,  empty_init, "Tryom", "Omar I", MACHINE_SUPPORTS_SAVE | MACHINE_CLICKABLE_ARTWORK )
+SYST( 1979, omar2, 0,      0,      omar2,   omar,  omar2_state, empty_init, "Tryom", "Omar II", MACHINE_SUPPORTS_SAVE | MACHINE_CLICKABLE_ARTWORK )
