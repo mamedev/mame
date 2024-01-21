53c1578555774ee719131bf80dec3bf82dc5ef9e
Vas Crabb <vas@vastheman.com>
2024-01-22 07:00:28 +1100

    cpu/c33: Added synthetics for ext %rs prefix.

--- src/devices/cpu/c33/c33dasm.h
+++ src/devices/cpu/c33/c33dasm.h
@@ -19,15 +19,19 @@ public:
 	virtual offs_t disassemble(std::ostream &stream, offs_t pc, data_buffer const &opcodes, data_buffer const &params) override;
 
 private:
-	offs_t dasm_class_0(std::ostream &stream, offs_t pc, u16 op, u32 ext_imm, unsigned ext_count) const;
-	offs_t dasm_class_1(std::ostream &stream, u16 op, u32 ext_imm, unsigned ext_count) const;
-	offs_t dasm_class_2_3(std::ostream &stream, u16 op, u32 ext_imm, unsigned ext_count) const;
-	offs_t dasm_class_4(std::ostream &stream, u16 op, u32 ext_imm, unsigned ext_count) const;
-	offs_t dasm_class_6(std::ostream &stream, u16 op, u32 ext_imm, unsigned ext_count) const;
-	offs_t dasm_class_5(std::ostream &stream, u16 op, u32 ext_imm, unsigned ext_count) const;
-	offs_t dasm_class_7(std::ostream &stream, u16 op, u32 ext_imm, unsigned ext_count) const;
+	enum class ext_type { NONE, IMM13, IMM26, REG };
 
-	offs_t max_ext(std::ostream &stream, unsigned max, u32 ext_imm, unsigned ext_count) const;
+	struct ext_info;
+
+	offs_t dasm(std::ostream &stream, offs_t pc, u16 op, ext_info const &ext) const;
+
+	offs_t dasm_class_0(std::ostream &stream, offs_t pc, u16 op, ext_info const &ext) const;
+	offs_t dasm_class_1(std::ostream &stream, u16 op, ext_info const &ext) const;
+	offs_t dasm_class_2_3(std::ostream &stream, u16 op, ext_info const &ext) const;
+	offs_t dasm_class_4(std::ostream &stream, u16 op, ext_info const &ext) const;
+	offs_t dasm_class_6(std::ostream &stream, u16 op, ext_info const &ext) const;
+	offs_t dasm_class_5(std::ostream &stream, u16 op, ext_info const &ext) const;
+	offs_t dasm_class_7(std::ostream &stream, u16 op, ext_info const &ext) const;
 };
 
 #endif // MAME_CPU_C33_C33DASM_H
