f1201ba6eb81d3ce00f8f2136700d3b124e8013e
hap <happppp@users.noreply.github.com>
2024-01-26 15:22:32 +0100

    ctvboy: don't invert hs signal

--- src/mame/misc/ctvboy.cpp
+++ src/mame/misc/ctvboy.cpp
@@ -149,7 +149,7 @@ void ctvboy_state::p1_w(u8 data)
 u8 ctvboy_state::p1_r()
 {
 	// P17: MC6847 HS
-	return m_mc6847->hs_r() ? 0 : 0x80;
+	return m_mc6847->hs_r() ? 0x80 : 0;
 }
 
 u8 ctvboy_state::p2_r()
