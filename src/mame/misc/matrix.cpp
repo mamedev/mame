a3495023b869f4ff1e7d1e89f4f0633fa661a443
angelosa <lordkale4@gmail.com>
2024-02-26 03:22:39 +0100

    misc/astropc: hookup Cx5530 devices

--- src/mame/misc/matrix.cpp
+++ src/mame/misc/matrix.cpp
@@ -79,7 +79,6 @@ INPUT_PORTS_END
 
 void matrix_state::matrix(machine_config &config)
 {
-	// basic machine hardware
 	MEDIAGX(config, m_maincpu, 233'000'000); // Cyrix MediaGX GXm-266GP
 	m_maincpu->set_addrmap(AS_PROGRAM, &matrix_state::main_map);
 	m_maincpu->set_irq_acknowledge_callback("pci:12.0:pic8259_master", FUNC(pic8259_device::inta_cb));
