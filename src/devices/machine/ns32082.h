241d31b904a82895481f5989481b62198f0850b0
hap <happppp@users.noreply.github.com>
2024-02-28 21:07:58 +0100

    Revert "pc532: add floating-point unit"
    This reverts commit ca431df745f99606e130b96390bfeb2c38aa236e.

--- src/devices/machine/ns32082.h
+++ src/devices/machine/ns32082.h
@@ -10,25 +10,24 @@
 
 class ns32082_device
 	: public device_t
-	, public ns32000_mmu_interface
 	, public ns32000_slow_slave_interface
+	, public ns32000_mmu_interface
 {
 public:
 	ns32082_device(machine_config const &mconfig, char const *tag, device_t *owner, u32 clock);
 
-	// ns32000_slave_interface implementation
 	virtual void state_add(device_state_interface &parent, int &index) override;
 
-	// ns32000_slow_slave_interface implementation
-	virtual u16 slow_status(int *icount = nullptr) override;
-	virtual u16 slow_read() override;
-	virtual void slow_write(u16 data) override;
+	virtual u16 read_st(int *icount = nullptr) override;
+	virtual u16 read_op() override;
 
-	// ns32000_mmu_interface implementation
+	virtual void write_id(u16 data) override;
+	virtual void write_op(u16 data) override;
+
 	virtual translate_result translate(address_space &space, unsigned st, u32 &address, bool user, bool write, bool pfs = false, bool suppress = false) override;
 
 protected:
-	// device_t implementation
+	// device_t overrides
 	virtual void device_start() override;
 	virtual void device_reset() override;
 
@@ -52,16 +51,16 @@ private:
 	u16 m_opword;
 	struct operand
 	{
-		u32 expected;
-		u32 issued;
+		unsigned expected;
+		unsigned issued;
 		u64 value;
 	}
 	m_op[3];
 	u16 m_status;
 
 	// implementation state
-	u32 m_state;
-	u32 m_tcy;
+	unsigned m_state;
+	unsigned m_tcy;
 };
 
 DECLARE_DEVICE_TYPE(NS32082, ns32082_device)
