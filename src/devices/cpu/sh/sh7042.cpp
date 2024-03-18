b0a26688019fc5e97390ac8ba6503098723e9480
Olivier Galibert <galibert@pobox.com>
2024-03-18 10:16:28 +0100

    sh7042: Start introducing the interrupts

--- src/devices/cpu/sh/sh7042.cpp
+++ src/devices/cpu/sh/sh7042.cpp
@@ -297,3 +297,10 @@ void sh7042_device::pcf_if_w(offs_t, u16 data, u16 mem_mask)
 	COMBINE_DATA(&m_pcf_if);
 	logerror("pcf if = %04x\n", m_pcf_if);
 }
+
+void sh7042_device::set_internal_interrupt(int level, u32 vector)
+{
+	m_sh2_state->internal_irq_level = level;
+	m_internal_irq_vector = vector;
+	m_test_irq = 1;
+}
