9060f36ae80d5392ab83bb0745149349286a37aa
hap <happppp@users.noreply.github.com>
2024-03-18 19:19:33 +0100

    h8: don't save the mode settings, move mac_saturating var to h8s2600 and hook it up

--- src/devices/cpu/h8/h8s2600.h
+++ src/devices/cpu/h8/h8s2600.h
@@ -24,8 +24,13 @@ protected:
 
 	virtual std::unique_ptr<util::disasm_interface> create_disassembler() override;
 
+	virtual void device_start() override;
+	virtual void device_reset() override;
+
 	virtual void do_exec_full() override;
 	virtual void do_exec_partial() override;
+
+	bool m_mac_saturating;
 
 #define O(o) void o ## _full(); void o ## _partial()
 	O(clrmac);
