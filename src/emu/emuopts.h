c29e9629eab3137121852658ab58dddd5c318ff2
Wilbert Pol <wilbertpol@users.noreply.github.com>
2024-03-19 00:08:39 +0000

    Add conf to emuopts.h

--- src/emu/emuopts.h
+++ src/emu/emuopts.h
@@ -172,6 +172,7 @@
 #define OPTION_UI                   "ui"
 #define OPTION_RAMSIZE              "ramsize"
 #define OPTION_NVRAM_SAVE           "nvram_save"
+#define OPTION_CONF                 "conf"
 
 // core comm options
 #define OPTION_COMM_LOCAL_HOST      "comm_localhost"
@@ -456,6 +457,7 @@ public:
 	ui_option ui() const { return m_ui; }
 	const char *ram_size() const { return value(OPTION_RAMSIZE); }
 	bool nvram_save() const { return bool_value(OPTION_NVRAM_SAVE); }
+	const char *conf() const { return value(OPTION_CONF); }
 
 	// core comm options
 	const char *comm_localhost() const { return value(OPTION_COMM_LOCAL_HOST); }
