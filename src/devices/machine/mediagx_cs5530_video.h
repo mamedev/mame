935ad2bcb0471831f0beeb803bdf5ddc10446110
angelosa <lordkale4@gmail.com>
2024-03-20 23:19:08 +0100

    machine/mediagx_host: prepare for MediaGX virtual VGA hookup

---
+++ src/devices/machine/mediagx_cs5530_video.h
@@ -0,0 +1,29 @@
+// license: BSD-3-Clause
+// copyright-holders: Angelo Salese
+
+#ifndef MAME_MACHINE_SIS630_VGA_H
+#define MAME_MACHINE_SIS630_VGA_H
+
+#pragma once
+
+#include "pci.h"
+#include "video/pc_vga.h"
+
+class mediagx_cs5530_video_device : public pci_device
+{
+public:
+	mediagx_cs5530_video_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+protected:
+	virtual void device_start() override;
+	virtual void device_reset() override;
+
+	virtual void config_map(address_map &map) override;
+
+	void io_map(address_map &map);
+};
+
+DECLARE_DEVICE_TYPE(MEDIAGX_CS5530_VIDEO, mediagx_cs5530_video_device)
+
+
+#endif
