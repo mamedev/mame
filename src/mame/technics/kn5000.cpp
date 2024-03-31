c3cd7dcb5c30058d1245469f737e1f039bb40606
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-03-31 01:47:42 -0300

    add flag to enable/disable extension card

--- src/mame/technics/kn5000.cpp
+++ src/mame/technics/kn5000.cpp
@@ -18,6 +18,8 @@
 #include "video/pc_vga.h"
 #include "kn5000.lh"
 
+#define EXTENSION_VIEW  // This needs further debugging to avoid a segfault.
+
 class mn89304_vga_device : public svga_device
 {
 public:
@@ -162,12 +164,12 @@ void kn5000_state::maincpu_mem(address_map &map)
 	map(0x1a0000, 0x1bffff).rw("vga", FUNC(mn89304_vga_device::mem_linear_r), FUNC(mn89304_vga_device::mem_linear_w));
 	map(0x1e0000, 0x1fffff).ram(); // 1Mbit SRAM @ IC21 (CS0)  Note: I think this is the message "ERROR in back-up SRAM"
 
-	/* disabled for now. This needs further debugging to avoid a segfault:
+#ifdef EXTENSION_VIEW
 	map(0x200000, 0x2fffff).view(m_extension_view); // OK
 	m_extension_view[0](0x200000, 0x2fffff).noprw(); // OK
 	m_extension_view[1](0x200000, 0x27ffff).ram(); // OK // optional hsram: 2 * 256k bytes Static RAM @ IC5, IC6 (CS5)
 	m_extension_view[1](0x280000, 0x2fffff).rom(); // OK // 512k bytes FLASH ROM @ IC4 (CS5)
-	*/
+#endif // EXTENSION_VIEW
 
 	map(0x300000, 0x3fffff).rom().region("custom_data", 0); // 8MBit FLASH ROM @ IC19 (CS5)
 	/* OK-ish */	map(0x400000, 0x7fffff).rom().region("rhythm_data", 0); // 32MBit ROM @ IC14 (A22=1 and CS5)
@@ -601,8 +603,7 @@ void kn5000_state::cpanel_leds_w(offs_t offset, uint8_t data)
 
 void kn5000_state::machine_start()
 {
-	/* disabled for now. This needs further debugging to avoid a segfault:
-
+#ifdef EXTENSION_VIEW
 	if(m_extension)
 	{
 		m_extension->rom_map(m_extension_view[1], 0x280000, 0x2fffff);
@@ -612,7 +613,7 @@ void kn5000_state::machine_start()
 	{
 		m_extension_view.select(0);
 	}
-	*/
+#endif // EXTENSION_VIEW
 
 	m_CPL_LED.resolve();
 	m_CPR_LED.resolve();
