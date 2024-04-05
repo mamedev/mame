888715ba3528b37d55f9ca424c7426abc639fd0f
Hannes Janetzek <hannes.janetzek@gmail.com>
2024-04-05 19:35:01 +0200

    i186: only run timer 0,1 when not incremented via timer 2 pre-scaler

--- src/devices/cpu/i86/i186.cpp
+++ src/devices/cpu/i86/i186.cpp
@@ -1308,6 +1308,10 @@ TIMER_CALLBACK_MEMBER(i80186_cpu_device::timer_elapsed)
 void i80186_cpu_device::restart_timer(int which)
 {
 	timer_state *t = &m_timer[which];
+	// Only run timer 0,1 when not incremented via timer 2 pre-scaler
+	if (which != 2 && (t->control & 0x800c) == 0x8008)
+		return;
+
 	int count = (t->control & 0x1000) ? t->maxB : t->maxA;
 	if (!(t->control & 4))
 		t->int_timer->adjust((attotime::from_hz(clock() / 8) * (count ? count : 0x10000)), which);
