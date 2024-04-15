36f1c2d11cd0c03858b47b90bde1486c2cda652b
Sergey Svishchev <shattered@users.noreply.github.com>
2024-04-15 21:54:09 +0300

    uknc.cpp: hook up cassette and floppy

---
+++ src/devices/bus/qbus/uknc_kmd.h
@@ -0,0 +1,46 @@
+// license:BSD-3-Clause
+// copyright-holders:Sergey Svishchev
+/***************************************************************************
+
+    UKNC floppy controller (device driver MZ.SYS)
+
+***************************************************************************/
+
+#ifndef MAME_BUS_QBUS_UKNC_KMD_H
+#define MAME_BUS_QBUS_UKNC_KMD_H
+
+#pragma once
+
+#include "machine/1801vp128.h"
+
+#include "qbus.h"
+
+
+/***************************************************************************
+    TYPE DEFINITIONS
+***************************************************************************/
+
+// ======================> uknc_kmd_device
+
+class uknc_kmd_device : public device_t, public device_qbus_card_interface
+{
+public:
+	// construction/destruction
+	uknc_kmd_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+protected:
+	// device-level overrides
+	virtual void device_start() override;
+	virtual void device_add_mconfig(machine_config &config) override;
+
+	required_device<k1801vp128_device> m_fdc;
+
+private:
+	static void floppy_formats(format_registration &fr);
+};
+
+
+// device type definition
+DECLARE_DEVICE_TYPE(UKNC_KMD, uknc_kmd_device)
+
+#endif
