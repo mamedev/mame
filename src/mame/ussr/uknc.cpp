217c386184375f7b667b33f7d5d434dc7dcfbeab
shattered <shattered@users.noreply.github.com>
2024-04-15 20:18:10 +0000

    ussr/uknc.cpp: Added cassette interface and qbus slots with floppy controller card. (#12254)
    * ussr/uknc.cpp: Added second CPU and corrected CPU clocks.
    * bus/qbus: Added UKNC KMD floppy controller card device.

--- src/mame/ussr/uknc.cpp
+++ src/mame/ussr/uknc.cpp
@@ -14,7 +14,10 @@ Status: both CPUs start in the weeds.
 ****************************************************************************/
 
 #include "emu.h"
+#include "bus/qbus/qbus.h"
 #include "cpu/t11/t11.h"
+#include "imagedev/cassette.h"
+
 #include "emupal.h"
 #include "screen.h"
 
@@ -27,6 +30,10 @@ public:
 	uknc_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag)
 		, m_maincpu(*this, "maincpu")
+		, m_subcpu(*this, "subcpu")
+		, m_qbus(*this, "qbus")
+		, m_cart(*this, "cart")
+		, m_cassette(*this, "cassette")
 	{ }
 
 	void uknc(machine_config &config);
@@ -36,6 +43,10 @@ private:
 	virtual void machine_start() override;
 	uint32_t screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
 	required_device<k1801vm2_device> m_maincpu;
+	required_device<k1801vm2_device> m_subcpu;
+	required_device<qbus_device> m_qbus;
+	required_device<qbus_device> m_cart;
+	required_device<cassette_image_device> m_cassette;
 
 	void uknc_mem(address_map &map);
 	void uknc_sub_mem(address_map &map);
@@ -46,7 +57,6 @@ void uknc_state::uknc_mem(address_map &map)
 {
 	map.unmap_value_high();
 	map(0x0000, 0x7fff).ram();
-	map(0x8000, 0xffff).rom().region("maincpu",0);
 }
 
 void uknc_state::uknc_sub_mem(address_map &map)
@@ -77,13 +87,28 @@ uint32_t uknc_state::screen_update(screen_device &screen, bitmap_rgb32 &bitmap, 
 void uknc_state::uknc(machine_config &config)
 {
 	/* basic machine hardware */
-	K1801VM2(config, m_maincpu, 8'000'000);
+	K1801VM2(config, m_maincpu, XTAL(16'000'000)/4); // external clock is /2 + internal divider /2
 	m_maincpu->set_initial_mode(0x8000);
 	m_maincpu->set_addrmap(AS_PROGRAM, &uknc_state::uknc_mem);
+	m_maincpu->set_disable();
 
-	k1801vm2_device &subcpu(K1801VM2(config, "subcpu", 6'250'000));
-	subcpu.set_initial_mode(0x8000);
-	subcpu.set_addrmap(AS_PROGRAM, &uknc_state::uknc_sub_mem);
+	QBUS(config, m_qbus, 0);
+	m_qbus->set_space(m_maincpu, AS_PROGRAM);
+	m_qbus->birq4().set_inputline(m_maincpu, t11_device::VEC_LINE);
+	QBUS_SLOT(config, "qbus" ":1", qbus_cards, nullptr);
+
+	K1801VM2(config, m_subcpu, XTAL(12'500'000)/4);
+	m_subcpu->set_addrmap(AS_PROGRAM, &uknc_state::uknc_sub_mem);
+	m_subcpu->set_initial_mode(0x8000);
+
+	QBUS(config, m_cart, 0);
+	m_cart->set_space(m_subcpu, AS_PROGRAM);
+	m_cart->birq4().set_inputline(m_subcpu, t11_device::VEC_LINE);
+	QBUS_SLOT(config, "cart" ":1", qbus_cards, "mz");
+	QBUS_SLOT(config, "cart" ":2", qbus_cards, nullptr);
+
+	CASSETTE(config, m_cassette);
+	m_cassette->set_default_state(CASSETTE_STOPPED | CASSETTE_MOTOR_ENABLED | CASSETTE_SPEAKER_ENABLED);
 
 	/* video hardware */
 	screen_device &screen(SCREEN(config, "screen", SCREEN_TYPE_RASTER));
@@ -98,10 +123,8 @@ void uknc_state::uknc(machine_config &config)
 
 /* ROM definition */
 ROM_START( uknc )
-	ROM_REGION( 0x8000, "maincpu", 0 )
-	ROM_LOAD( "uknc.rom", 0x0000, 0x8000, CRC(a1536994) SHA1(b3c7c678c41ffa9b37f654fbf20fef7d19e6407b))
-
-	ROM_REGION( 0x8000, "subcpu", ROMREGION_ERASEFF )
+	ROM_REGION16_LE(0100000, "subcpu", ROMREGION_ERASE00)
+	ROM_LOAD("uknc.rom", 0x0000, 0x8000, CRC(a1536994) SHA1(b3c7c678c41ffa9b37f654fbf20fef7d19e6407b))
 ROM_END
 
 } // anonymous namespace
