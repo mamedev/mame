8ec6190c9c0f30ee4c36b65e7046d19a05aa497e
Wilbert Pol <wilbertpol@users.noreply.github.com>
2024-04-21 10:28:02 +0100

    msx/slot/*: Use arrays of memory views.

--- src/devices/bus/msx/slot/fs4600.cpp
+++ src/devices/bus/msx/slot/fs4600.cpp
@@ -17,9 +17,7 @@ msx_slot_fs4600_device::msx_slot_fs4600_device(const machine_config &mconfig, co
 	, m_nvram(*this, "nvram")
 	, m_rom_region(*this, finder_base::DUMMY_TAG)
 	, m_rombank(*this, "rombank%u", 0U)
-	, m_view0(*this, "view0")
-	, m_view1(*this, "view1")
-	, m_view2(*this, "view2")
+	, m_view{ {*this, "view0"}, {*this, "view1"}, {*this, "view2"} }
 	, m_region_offset(0)
 	, m_sram_address(0)
 {
@@ -46,47 +44,46 @@ void msx_slot_fs4600_device::device_start()
 	for (int i = 0; i < 3; i++)
 		m_rombank[i]->configure_entries(0, 0x40, m_rom_region->base() + m_region_offset, 0x4000);
 
-	page(0)->install_view(0x0000, 0x3fff, m_view0);
-	m_view0[0].install_read_bank(0x0000, 0x3fff, m_rombank[0]);
-	m_view0[1].install_read_bank(0x0000, 0x3fff, m_rombank[0]);
-	m_view0[1].install_write_handler(0x3ffa, 0x3ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_w)));
-	m_view0[1].install_read_handler(0x3ffd, 0x3ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_r)));
+	page(0)->install_view(0x0000, 0x3fff, m_view[0]);
+	m_view[0][0].install_read_bank(0x0000, 0x3fff, m_rombank[0]);
+	m_view[0][1].install_read_bank(0x0000, 0x3fff, m_rombank[0]);
+	m_view[0][1].install_write_handler(0x3ffa, 0x3ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_w)));
+	m_view[0][1].install_read_handler(0x3ffd, 0x3ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_r)));
 
-	page(1)->install_view(0x4000, 0x7fff, m_view1);
-	m_view1[0].install_read_bank(0x4000, 0x7fff, m_rombank[1]);
-	m_view1[0].install_write_handler(0x6000, 0x6000, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<1>)));
-	m_view1[0].install_write_handler(0x6400, 0x6400, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<0>)));
-	m_view1[0].install_write_handler(0x7000, 0x7000, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<2>)));
-	m_view1[1].install_read_bank(0x4000, 0x7fff, m_rombank[1]);
-	m_view1[1].install_write_handler(0x7ffa, 0x7ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_w)));
-	m_view1[1].install_read_handler(0x7ffd, 0x7ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_r)));
-	m_view1[2].install_read_bank(0x4000, 0x7fff, m_rombank[1]);
-	m_view1[2].install_write_handler(0x6000, 0x6000, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<1>)));
-	m_view1[2].install_write_handler(0x6400, 0x6400, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<0>)));
-	m_view1[2].install_write_handler(0x7000, 0x7000, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<2>)));
-	m_view1[2].install_read_handler(0x7ff0, 0x7ff5, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_r)));
-	m_view1[3].install_read_bank(0x4000, 0x7fff, m_rombank[1]);
-	m_view1[3].install_read_handler(0x7ff0, 0x7ff5, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_r)));
-	m_view1[3].install_write_handler(0x7ffa, 0x7ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_w)));
-	m_view1[3].install_read_handler(0x7ffd, 0x7ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_r)));
+	page(1)->install_view(0x4000, 0x7fff, m_view[1]);
+	m_view[1][0].install_read_bank(0x4000, 0x7fff, m_rombank[1]);
+	m_view[1][0].install_write_handler(0x6000, 0x6000, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<1>)));
+	m_view[1][0].install_write_handler(0x6400, 0x6400, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<0>)));
+	m_view[1][0].install_write_handler(0x7000, 0x7000, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<2>)));
+	m_view[1][1].install_read_bank(0x4000, 0x7fff, m_rombank[1]);
+	m_view[1][1].install_write_handler(0x7ffa, 0x7ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_w)));
+	m_view[1][1].install_read_handler(0x7ffd, 0x7ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_r)));
+	m_view[1][2].install_read_bank(0x4000, 0x7fff, m_rombank[1]);
+	m_view[1][2].install_write_handler(0x6000, 0x6000, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<1>)));
+	m_view[1][2].install_write_handler(0x6400, 0x6400, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<0>)));
+	m_view[1][2].install_write_handler(0x7000, 0x7000, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_w<2>)));
+	m_view[1][2].install_read_handler(0x7ff0, 0x7ff5, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_r)));
+	m_view[1][3].install_read_bank(0x4000, 0x7fff, m_rombank[1]);
+	m_view[1][3].install_read_handler(0x7ff0, 0x7ff5, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::bank_r)));
+	m_view[1][3].install_write_handler(0x7ffa, 0x7ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_w)));
+	m_view[1][3].install_read_handler(0x7ffd, 0x7ffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_r)));
 
 	page(1)->install_write_handler(0x7ff9, 0x7ff9, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::control_w)));
 
-	page(2)->install_view(0x8000, 0xbfff, m_view2);
-	m_view2[0].install_read_bank(0x8000, 0xbfff, m_rombank[2]);
-	m_view2[1].install_read_bank(0x8000, 0xbfff, m_rombank[2]);
-	m_view2[1].install_write_handler(0xbffa, 0xbffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_w)));
-	m_view2[1].install_read_handler(0xbffd, 0xbffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_r)));
+	page(2)->install_view(0x8000, 0xbfff, m_view[2]);
+	m_view[2][0].install_read_bank(0x8000, 0xbfff, m_rombank[2]);
+	m_view[2][1].install_read_bank(0x8000, 0xbfff, m_rombank[2]);
+	m_view[2][1].install_write_handler(0xbffa, 0xbffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_w)));
+	m_view[2][1].install_read_handler(0xbffd, 0xbffd, emu::rw_delegate(*this, FUNC(msx_slot_fs4600_device::sram_r)));
 }
 
 void msx_slot_fs4600_device::device_reset()
 {
-	m_view0.select(0);
-	m_view1.select(0);
-	m_view2.select(0);
-	m_rombank[0]->set_entry(0);
-	m_rombank[1]->set_entry(0);
-	m_rombank[2]->set_entry(0);
+	for (int i = 0; i < 3; i++)
+	{
+		m_view[i].select(0);
+		m_rombank[i]->set_entry(0);
+	}
 }
 
 u8 msx_slot_fs4600_device::sram_r()
@@ -132,7 +129,7 @@ void msx_slot_fs4600_device::bank_w(u8 data)
 
 void msx_slot_fs4600_device::control_w(u8 data)
 {
-	m_view0.select((data >> 1) & 0x01);
-	m_view1.select((data >> 1) & 0x03);
-	m_view2.select((data >> 1) & 0x01);
+	m_view[0].select((data >> 1) & 0x01);
+	m_view[1].select((data >> 1) & 0x03);
+	m_view[2].select((data >> 1) & 0x01);
 }
