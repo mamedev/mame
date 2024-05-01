mamedev/mame

commit 293fb66d7f56897f587721254e47d855241d514d

Prevent integer overflow in Z80 CTC device, which causes a runtime error on WebAssembly builds.  [Justin Kerk]
(nw) The issue was introduced in fc20d899ab83925a1169eeac2c61c70561b59ffb

src/devices/machine/z80ctc.cpp
@@ -373,7 +373,10 @@ u8 z80ctc_channel_device::read()
 
 		LOG("CTC clock %f\n", period.as_hz());
 
-		return u8((m_timer->remaining().as_double() / period.as_double()) + 1.0);
+		if(!m_timer->remaining().is_never())
+			return u8((m_timer->remaining().as_double() / period.as_double()) + 1.0);
+		else
+			return 0;
 	}
 }
 

commit c33ec6922fec2f7b64714f2e934c4aac15160a26

machine/z80ctc.cpp: fix channel bus readback on unset timer * fix x1turbo not detecting YM regression caused by 293fb66d7f56897f587721254e47d855241d514d

src/devices/machine/z80ctc.cpp
@@ -368,7 +368,11 @@ u8 z80ctc_channel_device::read()
 		if(!m_timer->remaining().is_never())
 			return u8((m_timer->remaining().as_double() / period.as_double()) + 1.0);
 		else
-			return 0;
+        {
+            // value read-back is required by x1turbo for YM internal board detection.
+            // cfr. x1turbo40 argus wpiset 0x704,1,rw
+			return m_down;
+        }
 	}
 }
 