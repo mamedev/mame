1d31b570b58c9e9bae3e6c123fe8108b89ca413a
Jason Flatt <jflatt@cox.net>
2024-05-01 11:07:04 -0700

    Update with latest changes used in flathub

--- flatpak/build_local.sh
+++ flatpak/build_local.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 flatpak-builder --user --install --force-clean --ccache --jobs=8 build org.mamedev.MAME.yaml
+flatpak install --user --reinstall --assumeyes "$(pwd)/.flatpak-builder/cache" org.mamedev.MAME.Debug
