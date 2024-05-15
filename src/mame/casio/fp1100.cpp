ffbae4263545a14aa14de1f6cc4562f615436bba
ClawGrip <clawgrip@hotmail.com>
2024-05-15 11:33:49 +0200

    Mark FP1000 as compatible with FP1100

--- src/mame/casio/fp1100.cpp
+++ src/mame/casio/fp1100.cpp
@@ -730,5 +730,5 @@ ROM_END
 
 // Drivers
 
-COMP( 1983, fp1100, 0,      0,      fp1100,  fp1100, fp1100_state, empty_init, "Casio", "FP-1100", MACHINE_NOT_WORKING)
-COMP( 1982, fp1000, 0,      0,      fp1100,  fp1100, fp1100_state, empty_init, "Casio", "FP-1000", MACHINE_NOT_WORKING)
+COMP( 1983, fp1100, 0,      0, fp1100, fp1100, fp1100_state, empty_init, "Casio", "FP-1100", MACHINE_NOT_WORKING)
+COMP( 1982, fp1000, 0, fp1100, fp1100, fp1100, fp1100_state, empty_init, "Casio", "FP-1000", MACHINE_NOT_WORKING)
