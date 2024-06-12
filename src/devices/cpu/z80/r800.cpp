31dc7e99cc329520847d52e19fe71274c353c3f5
Wilbert Pol <wilbertpol@users.noreply.github.com>
2024-06-12 21:22:52 +0100

    cpu/z80/r800.cpp: Sync with main.

--- src/devices/cpu/z80/r800.cpp
+++ src/devices/cpu/z80/r800.cpp
@@ -178,10 +178,5 @@ void r800_device::muluw(u16 value)
 
 void r800_device::do_op()
 {
-	const bool is_rop = m_ref >= 0xffff00;
 	#include "cpu/z80/r800.hxx"
-	if (!is_rop)
-	{
-		m_ref = 0xffff00;
-	}
 }
