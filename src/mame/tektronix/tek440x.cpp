09242726bbba4a5c37b63e4875111295feb3a85e
Adam <adam@light-up.co.uk>
2024-06-22 19:46:57 +0100

    - added derived ncr5385 that blinks an LED during activity

--- src/mame/tektronix/tek440x.cpp
+++ src/mame/tektronix/tek440x.cpp
@@ -84,7 +84,44 @@
 #define MAXRAM 0x200000	// +1MB
 //#define MAXRAM 0x400000	// +3MB (which was never a Thing)
 
+class led_ncr5385_device : public ncr5385_device
+{
+	output_finder<> *m_disk_led = nullptr;
+	
+	public:
+	
+	using ncr5385_device::ncr5385_device;
+	
+	void led_cmd_w(u8 data)
+	{
+		ncr5385_device::cmd_w(data);
+
+		// blinky blinky
+		if (m_disk_led)
+			*m_disk_led = !BIT(data, 3);
+	}
+
+	void map(address_map &map)
+	{
+		ncr5385_device::map(map);
+		
+		// hook one so we can show activity
+		map(0x1, 0x1).rw(FUNC(led_ncr5385_device::cmd_r), FUNC(led_ncr5385_device::led_cmd_w));
+		
+	}
+	
+	void useled(output_finder<> *led)
+	{
+		m_disk_led = led;
+	}
+
+};
+DECLARE_DEVICE_TYPE(LED_NCR5385, led_ncr5385_device)
+
+DEFINE_DEVICE_TYPE(LED_NCR5385, led_ncr5385_device, "led_ncr5385", "NCR 5385 SCSI Protocol Controller")
+
 namespace {
+
 
 	enum {
 		PHASE_STATIC = 0,
@@ -104,7 +141,7 @@ public:
 		m_snsnd(*this, "snsnd"),
 		m_timer(*this, "timer"),
 		m_rtc(*this, "rtc"),
-		m_scsi(*this, "scsi:7:ncr5385"),
+		m_scsi(*this, "scsi:7:led_ncr5385"),
 		m_vint(*this, "vint"),
 		m_screen(*this, "screen"),
 		m_acia(*this, "acia"),
@@ -196,7 +233,7 @@ private:
 	required_device<sn76496_device> m_snsnd;
 	required_device<am9513_device> m_timer;
 	required_device<mc146818_device> m_rtc;
-	required_device<ncr5385_device> m_scsi;
+	required_device<led_ncr5385_device> m_scsi;
 	required_device<input_merger_all_high_device> m_vint;
 	required_device<screen_device> m_screen;
 	required_device<mos6551_device> m_acia;
@@ -254,9 +291,10 @@ void tek440x_state::machine_start()
 	m_led_2.resolve();
 	m_led_4.resolve();
 	m_led_8.resolve();
+	
 	m_led_disk.resolve();
+	m_scsi->useled(&m_led_disk);
 }
-
 
 
 /*************************************
@@ -470,14 +508,6 @@ void tek440x_state::mapcntl_w(u8 data)
 	LOG("mapcntl_w mmu_enable   %2d\n", BIT(data, MAP_VM_ENABLE));
 	LOG("mapcntl_w write_enable %2d\n", BIT(data, MAP_SYS_WR_ENABLE));
 	LOG("mapcntl_w pte PID    0x%02x\n", data & 15);
-#endif
-
-#if 0
-	// think this is just wrong
-	if (BIT(data, MAP_VM_ENABLE))
-		m_map_view.select(0);
-	else
-		m_map_view.disable();
 #endif
 
 	// NB bit 6 & 7 is not used
@@ -783,14 +813,10 @@ void tek440x_state::duart_w(offs_t offset, u8 data)
 void tek440x_state::logical_map(address_map &map)
 {
 	map(0x000000, 0x7fffff).rw(FUNC(tek440x_state::memory_r), FUNC(tek440x_state::memory_w));
-#if 0
-	map(0x800000, 0xffffff).view(m_map_view);
-	m_map_view[0](0x800000, 0xffffff).rw(FUNC(tek440x_state::map_r), FUNC(tek440x_state::map_w));
-#else
-	map(0x800000, 0x801fff).rw(FUNC(tek440x_state::map_r), FUNC(tek440x_state::map_w));
-#endif
 
-	map(0x802000, 0xffffff).noprw();	// selftest writes from 0x802000 - 0xffffffff
+	map(0x800000, 0x803fff).rw(FUNC(tek440x_state::map_r), FUNC(tek440x_state::map_w));
+
+	map(0x804000, 0xffffff).noprw();	// selftest writes from 0x804000 - 0xffffff
 }
 
 void tek440x_state::physical_map(address_map &map)
@@ -841,7 +867,7 @@ void tek440x_state::physical_map(address_map &map)
 			LOG("scsi bus reset %d\n", BIT(data, 7));
 			
 		}, "scsi_addr"); // 7bc000-7bdfff: SCSI bus address registers
-	map(0x7be000, 0x7be01f).m(m_scsi, FUNC(ncr5385_device::map)).umask16(0xff00); //.mirror(0x1fe0) .cswidth(16);
+	map(0x7be000, 0x7be01f).m(m_scsi, FUNC(led_ncr5385_device::map)).umask16(0xff00); //.mirror(0x1fe0) .cswidth(16);
 
 	// 7c0000-7fffff EPROM application space
 	map(0x7c0000, 0x7fffff).nopr();
@@ -951,10 +977,10 @@ m_printer->in_pb_callback().set_constant(0xbf);		// HACK:  vblank always checks 
 	NSCSI_CONNECTOR(config, "scsi:4", scsi_devices, nullptr);
 	NSCSI_CONNECTOR(config, "scsi:5", scsi_devices, nullptr);
 	NSCSI_CONNECTOR(config, "scsi:6", scsi_devices, nullptr);
-	NSCSI_CONNECTOR(config, "scsi:7").option_set("ncr5385", NCR5385).clock(40_MHz_XTAL / 4).machine_config(
+	NSCSI_CONNECTOR(config, "scsi:7").option_set("led_ncr5385", LED_NCR5385).clock(40_MHz_XTAL / 4).machine_config(
 		[this](device_t *device)
 		{
-			ncr5385_device &adapter = downcast<ncr5385_device &>(*device);
+			led_ncr5385_device &adapter = downcast<led_ncr5385_device &>(*device);
 
 			adapter.irq().set_inputline(m_maincpu, M68K_IRQ_3);
 		});
