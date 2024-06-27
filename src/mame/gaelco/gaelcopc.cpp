2df90659a82fb10fe029785c63e8b659551c5186
ClawGrip <clawgrip@hotmail.com>
2024-06-27 21:29:22 +0200

    gaelco/gaelcopc.cpp: Add an alt BIOS for Gaelco Championship Tuning Race [Museo del Recreativo, Recreativas.org]
    Also add a note about Gaelco Championship Tuning Race I/O board and clean up comments

--- src/mame/gaelco/gaelcopc.cpp
+++ src/mame/gaelco/gaelcopc.cpp
@@ -1,5 +1,6 @@
 // license:BSD-3-Clause
 // copyright-holders:David Haywood
+
 /* Gaelco PC based hardware
 
 TODO:
@@ -68,8 +69,8 @@ INPUT_PORTS_END
 
 void gaelcopc_state::gaelcopc(machine_config &config)
 {
-	/* basic machine hardware */
-	// bios mentions Socket 370, so at very least a Celeron or a Pentium 3 class CPU
+	// basic machine hardware
+	// BIOS mentions Socket 370, so at very least a Celeron or a Pentium 3 class CPU
 	// TODO: lowered rate for debugging aid, needs a slot option anyway
 	PENTIUM3(config, m_maincpu, 100'000'000);
 	m_maincpu->set_addrmap(AS_PROGRAM, &gaelcopc_state::gaelcopc_map); // TODO: remove me
@@ -81,44 +82,44 @@ void gaelcopc_state::gaelcopc(machine_config &config)
 // TODO: All of the provided BIOSes just have different ACFG table configs at $10000, investigate
 
 ROM_START(tokyocop)
-	ROM_REGION32_LE(0x80000, "bios", 0)  /* motherboard bios */
+	ROM_REGION32_LE(0x80000, "bios", 0)  // motherboard BIOS
 	// $40 Award 6.00 PG 12/19/2002
 	// $7413c Intel 815 Hardware version v0.0 08/13/1999
-	ROM_SYSTEM_BIOS(0, "default", "v0.0 08/13/1999")
-	ROMX_LOAD("al1.u10", 0x000000, 0x80000, CRC(e426e030) SHA1(52bdb6d46c12150077169ac3add8c450326ad4af), ROM_BIOS(0) )
-	ROM_SYSTEM_BIOS(1, "alt_acfg", "v0.0 08/13/1999 (alt ACFG)")
-	ROMX_LOAD("al1 49lf004a.u10", 0x00000, 0x80000, CRC(db8e4a37) SHA1(9ef4bf1d72955a89e7dc9b984e76ce86c824b461), ROM_BIOS(1) )
+	ROM_SYSTEM_BIOS( 0, "default", "v0.0 08/13/1999" )
+	ROMX_LOAD( "al1.u10",          0x000000, 0x80000, CRC(e426e030) SHA1(52bdb6d46c12150077169ac3add8c450326ad4af), ROM_BIOS(0) )
+	ROM_SYSTEM_BIOS( 1, "alt_acfg", "v0.0 08/13/1999 (alt ACFG)")
+	ROMX_LOAD( "al1 49lf004a.u10", 0x000000, 0x80000, CRC(db8e4a37) SHA1(9ef4bf1d72955a89e7dc9b984e76ce86c824b461), ROM_BIOS(1) )
 
 /* Dumper's note: The drive was ordered from Gaelco and they used a 250 GB drive that apparently used to have something
 else on it because when I ripped the entire drive and compressed it, the compressed image was 30 GB which is too much for me
 to upload. So I just ripped the partitions (it had 3) and the size was reasonable. This rip was burned into another drive and
-tested working on the real hardware. It uses the same hardware and bios as the kit version.*/
+tested working on the real hardware. It uses the same hardware and BIOS as the kit version.*/
 
 	DISK_REGION( "disks" )
 	DISK_IMAGE( "tokyocop", 0, SHA1(f3b60046da7094743822191473e05ee9cbc1af86) )
 ROM_END
 
 ROM_START(tokyocopk)
-	ROM_REGION32_LE(0x80000, "bios", 0)  /* motherboard bios */
+	ROM_REGION32_LE(0x80000, "bios", 0)  // motherboard BIOS
 	// $40 6.00 PG 12/19/2002
 	// $7413c v0.0 08/13/1999
-	ROM_LOAD("al1.u10", 0x000000, 0x80000, CRC(e426e030) SHA1(52bdb6d46c12150077169ac3add8c450326ad4af) )
+	ROM_LOAD( "al1.u10", 0x000000, 0x80000, CRC(e426e030) SHA1(52bdb6d46c12150077169ac3add8c450326ad4af) )
 
 	DISK_REGION( "disks" ) // Maxtor 2F040J0310613 VAM051JJ0
 	DISK_IMAGE( "tokyocopk", 0, SHA1(3805e41903719d8ed163f9879db65e71aba2e3e7) )
 ROM_END
 
 ROM_START(tokyocopi)
-	ROM_REGION32_LE(0x80000, "bios", 0)  /* motherboard bios */
+	ROM_REGION32_LE(0x80000, "bios", 0)  // motherboard BIOS
 	// Not specifically provided, assume "works right" with same BIOS.
-	ROM_LOAD("al1.u10", 0x000000, 0x80000, CRC(e426e030) SHA1(52bdb6d46c12150077169ac3add8c450326ad4af) )
+	ROM_LOAD( "al1.u10", 0x000000, 0x80000, CRC(e426e030) SHA1(52bdb6d46c12150077169ac3add8c450326ad4af) )
 
 	DISK_REGION( "disks" )
 	DISK_IMAGE( "tokyocopi", 0, SHA1(a3cf011c8ef8ec80724c28e1534191b40ae8515d) )
 ROM_END
 
 ROM_START(rriders)
-	ROM_REGION32_LE(0x80000, "bios", 0)  /* motherboard bios */
+	ROM_REGION32_LE(0x80000, "bios", 0)  // motherboard BIOS
 	// $40 6.00 PG 12/19/2002
 	// $7413c v0.0 08/13/1999
 	ROM_LOAD("22-03.u10", 0x000000, 0x80000, CRC(0ccae12f) SHA1(a8878fa73d5a4f5e9b6e3f35994fddea08cd3c2d) )
@@ -127,11 +128,13 @@ ROM_START(rriders)
 	DISK_IMAGE( "rriders", 0, SHA1(46e10517ee1b383e03c88cac67a54318c227e3e1) )
 ROM_END
 
+// Gaelco I/O card "REF. 050210" (Altera Cyclone EP1C3T144C8, AD9288, etc.)
 ROM_START(tuningrc)
-	ROM_REGION32_LE(0x80000, "bios", 0)  /* motherboard bios */
-	// $40 6.00 PG 12/19/2002
-	// $7413c v0.0 08/13/1999
-	ROM_LOAD("310j.u10", 0x000000, 0x80000, CRC(70b0797a) SHA1(696f602e83359d5d36798d4d2962ee85171e3622) )
+	ROM_REGION32_LE(0x80000, "bios", 0)  // motherboard BIOS
+	ROM_SYSTEM_BIOS( 0, "60",  "V4-60" )
+	ROMX_LOAD( "310j.u10",             0x000000, 0x80000, CRC(70b0797a) SHA1(696f602e83359d5d36798d4d2962ee85171e3622), ROM_BIOS(0) ) // $40 6.00 PG 12/19/2002, $7413c v0.0 08/13/1999
+	ROM_SYSTEM_BIOS( 1, "60a", "V4-60 (alt)" )
+	ROMX_LOAD( "3-01_sst49lf004a.u10", 0x000000, 0x80000, CRC(768fc24d) SHA1(81c04fab834700dbe9bb2275151a773fe6e0161d), ROM_BIOS(1) ) // Internal compressed file 100% the same as "V4-60"
 
 	DISK_REGION( "disks" ) // Hitachi HDS728080PLAT20 ATA/IDE
 	DISK_IMAGE( "tuningrc", 0, SHA1(4055cdc0b0c0e99252b90fbfafc48b693b144d67) )
