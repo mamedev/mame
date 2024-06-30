e29730798ae19db73d30846350871fe270936a9c
Wilbert Pol <wilbertpol@users.noreply.github.com>
2024-06-30 12:47:11 +0100

    cpu/z80/r800.cpp: - Removed undocumented Z80 instructions that are not supported by the R800 - Updated basic instruction timing - Implement MULUB and MULUW Other R800 features are not implemented

--- src/devices/cpu/z80/r800.h
+++ src/devices/cpu/z80/r800.h
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:AJR
+// copyright-holders:AJR,Wilbert Pol
 /***************************************************************************
 
     ASCII R800 CPU
@@ -24,10 +24,25 @@ public:
 	// device type constructor
 	r800_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
 
+	static constexpr feature_type imperfect_features() { return feature::TIMING; }
+
 protected:
+	// device_t implementation
+	virtual void device_validity_check(validity_checker &valid) const override;
+
 	// device_execute_interface overrides
+	virtual u32 execute_min_cycles() const noexcept override { return 1; }
 	virtual u64 execute_clocks_to_cycles(u64 clocks) const noexcept override { return (clocks + 4 - 1) / 4; }
 	virtual u64 execute_cycles_to_clocks(u64 cycles) const noexcept override { return (cycles * 4); }
+
+	// device_disasm_interface implementation
+	virtual std::unique_ptr<util::disasm_interface> create_disassembler() override;
+
+	u8 r800_sll(u8 value);
+	void mulub(u8 value);
+	void muluw(u16 value);
+
+	virtual void do_op() override;
 };
 
 // device type declaration
