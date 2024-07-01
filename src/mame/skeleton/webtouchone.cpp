5ecfef188ed46ee46c7452e16cebff26190e80e4
ClawGrip <clawgrip@hotmail.com>
2024-07-01 14:39:38 +0200

    Fix speaker

--- src/mame/skeleton/webtouchone.cpp
+++ src/mame/skeleton/webtouchone.cpp
@@ -132,7 +132,7 @@ void webtouchone_state::webtouchone(machine_config &config)
 	m_screen->set_visarea(0, 640-1, 0, 480-1);
 	m_screen->set_screen_update(FUNC(webtouchone_state::screen_update_webtouchone));
 
-	SPEAKER(config, "mono").front_left();
+	SPEAKER(config, "mono").front_center();
 }
 
 // Spanish ROM for Terra (now Telefónica)
