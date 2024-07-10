53ecc08464bb83d79995c336fe138b139c467d91
flama12333 <143599905+flama12333@users.noreply.github.com>
2024-07-10 18:50:08 -0500

    Fixed psg and divisor marywu.cpp
    this will allow to sound psg

--- src/mame/misc/marywu.cpp
+++ src/mame/misc/marywu.cpp
@@ -14,10 +14,14 @@
     31 LEDs, 13 modules of double-digit 7-seg displays and 4 push-buttons).
   * we may also have user inputs from the coin slot and from the
     cabinet buttons, for making bets.
+	
+// 09/07/2024
+psg fixed by flama12333.
 **************************************************************************/
 
 #include "emu.h"
 #include "cpu/mcs51/mcs51.h"
+#include "machine/nvram.h"
 #include "machine/i8279.h"
 #include "sound/ay8910.h"
 #include "speaker.h"
@@ -103,14 +107,14 @@ static INPUT_PORTS_START( marywu )
 		PORT_DIPSETTING(0x00, DEF_STR( Off ) )
 
 	PORT_START("PUSHBUTTONS")
-	PORT_BIT(0x80, IP_ACTIVE_HIGH, IPT_UNUSED )
-	PORT_BIT(0x40, IP_ACTIVE_HIGH, IPT_UNUSED )
-	PORT_BIT(0x20, IP_ACTIVE_HIGH, IPT_UNUSED )
-	PORT_BIT(0x10, IP_ACTIVE_HIGH, IPT_UNUSED )
-	PORT_BIT(0x08, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_CODE(KEYCODE_A)
-	PORT_BIT(0x04, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_CODE(KEYCODE_S)
-	PORT_BIT(0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_CODE(KEYCODE_D)
-	PORT_BIT(0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_CODE(KEYCODE_F)
+	PORT_BIT(0x80, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT(0x40, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_CODE(KEYCODE_A)
+	PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_CODE(KEYCODE_S)
+	PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_CODE(KEYCODE_D)
+	PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_CODE(KEYCODE_F)
 INPUT_PORTS_END
 
 void marywu_state::ay1_port_a_w(uint8_t data)
@@ -174,11 +178,9 @@ void marywu_state::program_map(address_map &map)
 
 void marywu_state::io_map(address_map &map)
 {
-	map(0x8000, 0x87ff).mirror(0x0800).ram(); /* HM6116: 2kbytes of Static RAM */
-	map(0x9000, 0x9000).mirror(0x0ffc).w("ay1", FUNC(ay8910_device::data_address_w));
-	map(0x9001, 0x9001).mirror(0x0ffc).rw("ay1", FUNC(ay8910_device::data_r), FUNC(ay8910_device::data_w));
-	map(0x9002, 0x9002).mirror(0x0ffc).w("ay2", FUNC(ay8910_device::data_address_w));
-	map(0x9003, 0x9003).mirror(0x0ffc).rw("ay2", FUNC(ay8910_device::data_r), FUNC(ay8910_device::data_w));
+	map(0x8000, 0x87ff).mirror(0x0800).ram().share("nvram"); /* HM6116: 2kbytes of Static RAM */
+	map(0x9000, 0x9001).mirror(0x0ffc).rw("ay1", FUNC(ay8910_device::data_r), FUNC(ay8910_device::address_data_w));
+	map(0x9002, 0x9003).mirror(0x0ffc).rw("ay2", FUNC(ay8910_device::data_r), FUNC(ay8910_device::address_data_w));
 	map(0xb000, 0xb001).mirror(0x0ffe).rw("i8279", FUNC(i8279_device::read), FUNC(i8279_device::write));
 	map(0xf000, 0xf000).noprw(); /* TODO: Investigate this. There's something going on at this address range. */
 }
@@ -202,18 +204,19 @@ void marywu_state::marywu(machine_config &config)
 	kbdc.out_sl_callback().set(FUNC(marywu_state::multiplex_7seg_w));   // select  block of 7seg modules by multiplexing the SL scan lines
 	kbdc.in_rl_callback().set(FUNC(marywu_state::keyboard_r));          // keyboard Return Lines
 	kbdc.out_disp_callback().set(FUNC(marywu_state::display_7seg_data_w));
+    NVRAM(config, "nvram", nvram_device::DEFAULT_ALL_0);
 
 	/* Video */
 	config.set_default_layout(layout_marywu);
 
 	/* sound hardware */
 	SPEAKER(config, "mono").front_center();
-	ay8910_device &ay1(AY8910(config, "ay1", XTAL(10'738'635))); /* should it be perhaps a fraction of the XTAL clock ? */
+	ay8910_device &ay1(AY8910(config, "ay1", XTAL(10'738'635) / 6)); /* should it be perhaps a fraction of the XTAL clock ? */ divisor not verified
 	ay1.add_route(ALL_OUTPUTS, "mono", 0.50);
 	ay1.port_a_write_callback().set(FUNC(marywu_state::ay1_port_a_w));
 	ay1.port_b_write_callback().set(FUNC(marywu_state::ay1_port_b_w));
-
-	ay8910_device &ay2(AY8910(config, "ay2", XTAL(10'738'635))); /* should it be perhaps a fraction of the XTAL clock ? */
+   
+	ay8910_device &ay2(AY8910(config, "ay2", XTAL(10'738'635) / 6)); /* should it be perhaps a fraction of the XTAL clock ? */ divisor not verified
 	ay2.add_route(ALL_OUTPUTS, "mono", 0.50);
 	ay2.port_a_write_callback().set(FUNC(marywu_state::ay2_port_a_w));
 	ay2.port_b_write_callback().set(FUNC(marywu_state::ay2_port_b_w));
