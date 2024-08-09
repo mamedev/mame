1972e1b96c56193247f22b1df2586f1183fe497f
flama12333 <143599905+flama12333@users.noreply.github.com>
2024-08-08 19:51:31 -0500

    Add a Skeleton driver for Chang yu? unknown 6 ball pinball
    Dumped by Jose Soto
    sadly not pcb pictures. atleast for diagram but for clone hansen.
    https://vyr-entretenimiento.blogspot.com/2014/01/tarjeta-madre-de-la-pinball-tipo-perla.html?view=flipcard
    
    no second mcu and adpcm rom dump.
    clock are guessed.
    it is possible that the manufacturer is Chang yu according to his music.

---
+++ src/mame/misc/pearlorient.cpp
@@ -0,0 +1,137 @@
+// license:GPL-2.0+
+// copyright-holders:flama12333
+/*************************************************************************
+driverbased of marywu  but reworked to use a 8052c 
+there are second mcu. if is not present will marked error 10.
+
+Notes:
+Pearl in the  East 6 ball version
+
+New feature: buy a 6th ball toup the odds.
+
+src: for reference https://youtu.be/GszrVAWQ7d0
+
+5 ball Version. Oriental Pearl
+no have these features.
+src for reference: https://youtu.be/XEaOan6NWKQ?t=18
+
+It is possible that the different voices are different versions?
+*/
+#include "emu.h"
+#include "cpu/mcs51/mcs51.h"
+#include "machine/i8279.h"
+#include "machine/i8255.h"
+#include "sound/ay8910.h"
+#include "sound/ymopl.h"
+#include "sound/okim6295.h"
+#include "speaker.h"
+
+namespace {
+
+class pearlorient_state : public driver_device
+{
+public:
+	pearlorient_state(const machine_config &mconfig, device_type type, const char *tag)
+		: driver_device(mconfig, type, tag)
+	{ }
+
+	void pearlorient(machine_config &config);
+
+private:
+	void io_map(address_map &map);
+	void program_map(address_map &map);
+	virtual void machine_start() override;
+
+
+};
+
+static INPUT_PORTS_START( pearlorient )
+	PORT_START("IN0")
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_UNKNOWN )
+
+	PORT_START("IN1")
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	
+	PORT_START("DSW0")
+	PORT_DIPUNKNOWN_DIPLOC(0x01, 0x01, "SW0:1")
+	PORT_DIPUNKNOWN_DIPLOC(0x02, 0x02, "SW0:2")
+	PORT_DIPUNKNOWN_DIPLOC(0x04, 0x04, "SW0:3")
+	PORT_DIPUNKNOWN_DIPLOC(0x08, 0x08, "SW0:4")
+	PORT_DIPUNKNOWN_DIPLOC(0x10, 0x10, "SW0:5")
+	PORT_DIPUNKNOWN_DIPLOC(0x20, 0x20, "SW0:6")
+	PORT_DIPUNKNOWN_DIPLOC(0x40, 0x40, "SW0:7")
+	PORT_DIPUNKNOWN_DIPLOC(0x80, 0x80, "SW0:8")
+
+	PORT_START("DSW1")
+	PORT_DIPUNKNOWN_DIPLOC(0x01, 0x01, "SW1:1")
+	PORT_DIPUNKNOWN_DIPLOC(0x02, 0x02, "SW1:2")
+	PORT_DIPUNKNOWN_DIPLOC(0x04, 0x04, "SW1:3")
+	PORT_DIPUNKNOWN_DIPLOC(0x08, 0x08, "SW1:4")
+	PORT_DIPUNKNOWN_DIPLOC(0x10, 0x10, "SW1:5")
+	PORT_DIPUNKNOWN_DIPLOC(0x20, 0x20, "SW1:6")
+	PORT_DIPUNKNOWN_DIPLOC(0x40, 0x40, "SW1:7")
+	PORT_DIPUNKNOWN_DIPLOC(0x80, 0x80, "SW1:8")
+
+INPUT_PORTS_END
+
+
+void pearlorient_state::program_map(address_map &map)
+{
+	map(0x0000, 0xffff).rom();
+}
+// todo Where opll and adpcm are mapped?
+void pearlorient_state::io_map(address_map &map)
+{
+    map(0xfa00, 0xfa01).rw("kdc", FUNC(i8279_device::read), FUNC(i8279_device::write));
+    map(0xfb02, 0xfb03).w("psg", FUNC(ay8910_device::address_data_w));
+	
+}
+void pearlorient_state::machine_start()
+{
+}
+
+void pearlorient_state::pearlorient(machine_config &config)
+{
+	/* basic machine hardware */
+	i8052_device &maincpu(I8052(config, "maincpu", XTAL(10'738'000)));
+	maincpu.set_addrmap(AS_PROGRAM, &pearlorient_state::program_map);
+	maincpu.set_addrmap(AS_IO, &pearlorient_state::io_map);
+	/* Keyboard & display interface */
+	I8279(config, "kdc", XTAL(10'738'000) / 6); 
+	/* sound hardware */
+	SPEAKER(config, "mono").front_center();
+	ay8910_device &psg(AY8910(config, "psg", XTAL(10'738'000) / 6));  // Divider not verified
+	psg.add_route(ALL_OUTPUTS, "mono", 1.0);
+	ym2413_device &opll(YM2413(config, "opll", 3.579545_MHz_XTAL));
+	opll.add_route(ALL_OUTPUTS, "mono", 1.0);
+	OKIM6295(config, "oki", XTAL(10'738'000) / 6, okim6295_device::PIN7_HIGH).add_route(ALL_OUTPUTS, "mono", 1.0);  // Clock frequency & pin 7 not verified
+}
+
+ROM_START( pearlorient )
+	ROM_REGION( 0x10000, "maincpu", 0 )
+	ROM_LOAD( "w27c512.bin", 0x00000, 0x10000, CRC(8D3D1E91) SHA1(b80907df0878057a1ded8b56225059e06382b9d6) ) // main program
+	ROM_REGION( 0x1000, "mcu", 0 )
+	ROM_LOAD( "at89s51.bin", 0x0000, 0x1000, NO_DUMP ) // mcu. protection
+	ROM_REGION( 0x40000, "oki", 0 )
+	ROM_LOAD( "w27c020.bin", 0x00000, 0x40000, NO_DUMP ) //  oki rom voice
+ROM_END
+
+} // anonymous namespace
+
+
+//    YEAR  NAME    PARENT   MACHINE   INPUT   STATE         INIT        ROT   COMPANY      FULLNAME                                                FLAGS
+GAME( 2001?, pearlorient, 0,       pearlorient,   pearlorient, pearlorient_state, empty_init, ROT0, "Chang Yu Electronic?", "Unknown 6 Ball Pinball Gambling", MACHINE_IS_SKELETON_MECHANICAL ) // EAST8  v1.05  string . this was dumped from soccer santiago II 6 ball pinball
