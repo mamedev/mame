b989286652053a75f60aa71b3807c88f45d194dd
as-tb-dev <178102874+as-tb-dev@users.noreply.github.com>
2024-08-25 09:32:30 -0400

    a2bus: Add support for Wico Trackball, Wico Command Control Joystick Adapter, and 4 paddles connected to the Sirius JoyPort to the Apple ][/][+ (#12666)

---
+++ src/devices/bus/a2gameio/wico_joystick.h
@@ -0,0 +1,20 @@
+// license:BSD-3-Clause
+// copyright-holders:R. Belmont
+/*********************************************************************
+
+    Apple II Wico Command Control Joystick Adapter for Atari-style
+    digital joysticks
+
+*********************************************************************/
+
+#ifndef MAME_BUS_A2GAMEIO_WICO_JOYSTICK_H
+#define MAME_BUS_A2GAMEIO_WICO_JOYSTICK_H
+
+#pragma once
+
+#include "bus/a2gameio/gameio.h"
+
+// device type declaration
+DECLARE_DEVICE_TYPE(APPLE2_WICO_JOYSTICK, device_a2gameio_interface)
+
+#endif // MAME_BUS_A2GAMEIO_WICO_JOYSTICK_H
