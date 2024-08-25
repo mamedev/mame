eb819283e7552740794f6aff6965eacc19d968a4
as-tb-dev <178102874+as-tb-dev@users.noreply.github.com>
2024-08-25 09:02:46 -0400

    define Wico Trackball class in .CPP file instead of .H file and use DEFINE_DEVICE_TYPE_PRIVATE

--- src/devices/bus/a2bus/a2wico_trackball.cpp
+++ src/devices/bus/a2bus/a2wico_trackball.cpp
@@ -18,6 +18,44 @@
 #include "emu.h"
 #include "a2wico_trackball.h"
 
+//**************************************************************************
+//  TYPE DEFINITIONS
+//**************************************************************************
+
+class a2bus_wicotrackball_device:
+		public device_t,
+		public device_a2bus_card_interface
+{
+public:
+	// construction/destruction
+	a2bus_wicotrackball_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+	// optional information overrides
+	virtual ioport_constructor device_input_ports() const override;
+
+protected:
+	a2bus_wicotrackball_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock);
+
+	virtual void device_add_mconfig(machine_config &config) override;
+	virtual void device_start() override;
+	virtual void device_reset() override;
+
+	// overrides of standard a2bus slot functions
+	virtual uint8_t read_c0nx(uint8_t offset) override;
+	virtual void write_c0nx(uint8_t offset, uint8_t data) override;
+
+	required_ioport m_wicotrackballb;
+	required_ioport_array<2> m_wicotrackballxy;
+
+private:
+	bool m_speed[2];
+	uint8_t m_buttons;
+	bool m_wraparound;
+	uint8_t m_axis[2];
+	uint32_t m_last_pos[2];
+	uint8_t read_position(int axis);
+};
+
 /***************************************************************************
     CONSTANTS
 ***************************************************************************/
@@ -32,7 +70,7 @@
     GLOBAL VARIABLES
 ***************************************************************************/
 
-DEFINE_DEVICE_TYPE(A2BUS_WICOTRACKBALL, a2bus_wicotrackball_device, "a2wicotrackball", "Apple II Wico Trackball Card")
+DEFINE_DEVICE_TYPE_PRIVATE(A2BUS_WICOTRACKBALL, device_a2bus_card_interface, a2bus_wicotrackball_device, "a2wicotrackball", "Apple II Wico Trackball Card")
 
 static INPUT_PORTS_START( wicotrackball )
 	PORT_START(WICOTRACKBALL_BUTTONS_TAG) /* Trackball - buttons */
