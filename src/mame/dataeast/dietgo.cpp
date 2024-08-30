84a639343483e3036c3afbd414758aabfc37d2b2
hap <happppp@users.noreply.github.com>
2024-08-30 18:08:31 +0200

    dietgo: revert bios setting

--- src/mame/dataeast/dietgo.cpp
+++ src/mame/dataeast/dietgo.cpp
@@ -232,7 +232,7 @@ static INPUT_PORTS_START( dietgo )
 	PORT_DIPSETTING(      0x0000, DEF_STR( On ) )
 INPUT_PORTS_END
 
-static INPUT_PORTS_START( dietgoe ) // European version with optional alternative graphics
+static INPUT_PORTS_START( dietgon ) // New European version with optional alternative graphics
 	PORT_INCLUDE( dietgo )
 
 	PORT_MODIFY("DSW")
@@ -335,7 +335,7 @@ void dietgo_state::dietgo(machine_config &config)
 	OKIM6295(config, "oki", 32.22_MHz_XTAL / 32, okim6295_device::PIN7_HIGH).add_route(ALL_OUTPUTS, "mono", 0.60); // verified on PCB
 }
 
-ROM_START( dietgo ) // same version 1.1 and same date as dietgoe but newer version in ROM labels
+ROM_START( dietgo ) // same version 1.1 and same program ROMs as dietgoe but newer sprite ROMs
 	ROM_REGION( 0x80000, "maincpu", 0 ) // DE102 code (encrypted)
 	ROM_LOAD16_BYTE( "jy_00-3.4h", 0x000001, 0x040000, CRC(a863ad0c) SHA1(61bf2fe5dce92e3995791a7e9ef813d64bcc2b93) )
 	ROM_LOAD16_BYTE( "jy_01-3.5h", 0x000000, 0x040000, CRC(ef243eda) SHA1(b8efbb80c5bf40ef6c26a06fc7232d6e63596cb4) )
@@ -346,15 +346,9 @@ ROM_START( dietgo ) // same version 1.1 and same date as dietgoe but newer versi
 	ROM_REGION( 0x100000, "tiles", 0 )
 	ROM_LOAD( "may-00.10a", 0x00000, 0x100000, CRC(234d1f8d) SHA1(42d23aad20df20cbd2359cc12bdd47636b2027d3) )
 
-	ROM_DEFAULT_BIOS("m04")
-	ROM_SYSTEM_BIOS(0, "m01", "MAY-01/02")
-	ROM_SYSTEM_BIOS(1, "m04", "MAY-04/05") // the one that supports the alternative graphics
-
 	ROM_REGION( 0x200000, "sprites", 0 )
-	ROMX_LOAD( "may-01.14a",               0x000000, 0x100000, CRC(2da57d04) SHA1(3898e9fef365ecaa4d86aa11756b527a4fffb494), ROM_BIOS(0) )
-	ROMX_LOAD( "may-02.16a",               0x100000, 0x100000, CRC(3a66a713) SHA1(beeb99156332cf4870738f7769b719a02d7b40af), ROM_BIOS(0) )
-	ROMX_LOAD( "may-04_w78_9235kd011.14a", 0x000000, 0x100000, CRC(dedd2dd3) SHA1(c1021edb0b377a030ab9593c838083f0f3b996b2), ROM_BIOS(1) )
-	ROMX_LOAD( "may-05_w79_9235kd019.16a", 0x100000, 0x100000, CRC(cb23835f) SHA1(c504ff99f9029355f69e7fd7e9528d647bd491bf), ROM_BIOS(1) )
+	ROM_LOAD( "may-04_w78_9235kd011.14a", 0x000000, 0x100000, CRC(dedd2dd3) SHA1(c1021edb0b377a030ab9593c838083f0f3b996b2) )
+	ROM_LOAD( "may-05_w79_9235kd019.16a", 0x100000, 0x100000, CRC(cb23835f) SHA1(c504ff99f9029355f69e7fd7e9528d647bd491bf) )
 
 	ROM_REGION( 0x80000, "oki", 0 )
 	ROM_LOAD( "may-03.11l", 0x00000, 0x80000, CRC(b6e42bae) SHA1(c282cdf7db30fb63340cc609bf00f5ab63a75583) )
@@ -365,7 +359,31 @@ ROM_START( dietgo ) // same version 1.1 and same date as dietgoe but newer versi
 	ROM_LOAD( "pal16r6a_vd-02.11h", 0x0400, 0x0104, NO_DUMP ) // PAL is read protected
 ROM_END
 
-ROM_START( dietgoe ) // weird, still version 1.1 and same date
+ROM_START( dietgoe ) // same version 1.1 and same date as dietgoea but newer version in ROM labels
+	ROM_REGION( 0x80000, "maincpu", 0 ) // DE102 code (encrypted)
+	ROM_LOAD16_BYTE( "jy_00-3.4h", 0x000001, 0x040000, CRC(a863ad0c) SHA1(61bf2fe5dce92e3995791a7e9ef813d64bcc2b93) )
+	ROM_LOAD16_BYTE( "jy_01-3.5h", 0x000000, 0x040000, CRC(ef243eda) SHA1(b8efbb80c5bf40ef6c26a06fc7232d6e63596cb4) )
+
+	ROM_REGION( 0x10000, "audiocpu", 0 )
+	ROM_LOAD( "jy_02.14m", 0x00000, 0x10000, CRC(4e3492a5) SHA1(5f302bdbacbf95ea9f3694c48545a1d6bba4b019) )
+
+	ROM_REGION( 0x100000, "tiles", 0 )
+	ROM_LOAD( "may-00.10a", 0x00000, 0x100000, CRC(234d1f8d) SHA1(42d23aad20df20cbd2359cc12bdd47636b2027d3) )
+
+	ROM_REGION( 0x200000, "sprites", 0 )
+	ROM_LOAD( "may-01.14a", 0x000000, 0x100000, CRC(2da57d04) SHA1(3898e9fef365ecaa4d86aa11756b527a4fffb494) )
+	ROM_LOAD( "may-02.16a", 0x100000, 0x100000, CRC(3a66a713) SHA1(beeb99156332cf4870738f7769b719a02d7b40af) )
+
+	ROM_REGION( 0x80000, "oki", 0 )
+	ROM_LOAD( "may-03.11l", 0x00000, 0x80000, CRC(b6e42bae) SHA1(c282cdf7db30fb63340cc609bf00f5ab63a75583) )
+
+	ROM_REGION( 0x0600, "plds", 0 )
+	ROM_LOAD( "pal16l8b_vd-00.6h",  0x0000, 0x0104, NO_DUMP ) // PAL is read protected
+	ROM_LOAD( "pal16l8b_vd-01.7h",  0x0200, 0x0104, NO_DUMP ) // PAL is read protected
+	ROM_LOAD( "pal16r6a_vd-02.11h", 0x0400, 0x0104, NO_DUMP ) // PAL is read protected
+ROM_END
+
+ROM_START( dietgoea ) // weird, still version 1.1 and same date
 	ROM_REGION( 0x80000, "maincpu", 0 ) // DE102 code (encrypted)
 	ROM_LOAD16_BYTE( "jy_00-2.4h", 0x000001, 0x040000, CRC(014dcf62) SHA1(1a28ce4a643ec8b6f062b1200342ed4dc6db38a1) )
 	ROM_LOAD16_BYTE( "jy_01-2.5h", 0x000000, 0x040000, CRC(793ebd83) SHA1(b9178f18ce6e9fca848cbbf9dce3f3856672bf94) )
@@ -376,15 +394,9 @@ ROM_START( dietgoe ) // weird, still version 1.1 and same date
 	ROM_REGION( 0x100000, "tiles", 0 )
 	ROM_LOAD( "may-00.10a", 0x00000, 0x100000, CRC(234d1f8d) SHA1(42d23aad20df20cbd2359cc12bdd47636b2027d3) )
 
-	ROM_DEFAULT_BIOS("m04")
-	ROM_SYSTEM_BIOS(0, "m01", "MAY-01/02")
-	ROM_SYSTEM_BIOS(1, "m04", "MAY-04/05") // the one that supports the alternative graphics
-
 	ROM_REGION( 0x200000, "sprites", 0 )
-	ROMX_LOAD( "may-01.14a",               0x000000, 0x100000, CRC(2da57d04) SHA1(3898e9fef365ecaa4d86aa11756b527a4fffb494), ROM_BIOS(0) )
-	ROMX_LOAD( "may-02.16a",               0x100000, 0x100000, CRC(3a66a713) SHA1(beeb99156332cf4870738f7769b719a02d7b40af), ROM_BIOS(0) )
-	ROMX_LOAD( "may-04_w78_9235kd011.14a", 0x000000, 0x100000, CRC(dedd2dd3) SHA1(c1021edb0b377a030ab9593c838083f0f3b996b2), ROM_BIOS(1) )
-	ROMX_LOAD( "may-05_w79_9235kd019.16a", 0x100000, 0x100000, CRC(cb23835f) SHA1(c504ff99f9029355f69e7fd7e9528d647bd491bf), ROM_BIOS(1) )
+	ROM_LOAD( "may-01.14a", 0x000000, 0x100000, CRC(2da57d04) SHA1(3898e9fef365ecaa4d86aa11756b527a4fffb494) )
+	ROM_LOAD( "may-02.16a", 0x100000, 0x100000, CRC(3a66a713) SHA1(beeb99156332cf4870738f7769b719a02d7b40af) )
 
 	ROM_REGION( 0x80000, "oki", 0 )
 	ROM_LOAD( "may-03.11l", 0x00000, 0x80000, CRC(b6e42bae) SHA1(c282cdf7db30fb63340cc609bf00f5ab63a75583) )
@@ -395,7 +407,7 @@ ROM_START( dietgoe ) // weird, still version 1.1 and same date
 	ROM_LOAD( "pal16r6a_vd-02.11h", 0x0400, 0x0104, NO_DUMP ) // PAL is read protected
 ROM_END
 
-ROM_START( dietgoea ) // weird, still version 1.1 but different (earlier) date
+ROM_START( dietgoeb ) // weird, still version 1.1 but different (earlier) date
 	ROM_REGION( 0x80000, "maincpu", 0 ) // DE102 code (encrypted)
 	ROM_LOAD16_BYTE( "jy_00-1.4h", 0x000001, 0x040000, CRC(8bce137d) SHA1(55f5b1c89330803c6147f9656f2cabe8d1de8478) )
 	ROM_LOAD16_BYTE( "jy_01-1.5h", 0x000000, 0x040000, CRC(eca50450) SHA1(1a24117e3b1b66d7dbc5484c94cc2c627d34e6a3) )
@@ -406,15 +418,9 @@ ROM_START( dietgoea ) // weird, still version 1.1 but different (earlier) date
 	ROM_REGION( 0x100000, "tiles", 0 )
 	ROM_LOAD( "may-00.10a", 0x00000, 0x100000, CRC(234d1f8d) SHA1(42d23aad20df20cbd2359cc12bdd47636b2027d3) )
 
-	ROM_DEFAULT_BIOS("m04")
-	ROM_SYSTEM_BIOS(0, "m01", "MAY-01/02")
-	ROM_SYSTEM_BIOS(1, "m04", "MAY-04/05") // the one that supports the alternative graphics
-
 	ROM_REGION( 0x200000, "sprites", 0 )
-	ROMX_LOAD( "may-01.14a",               0x000000, 0x100000, CRC(2da57d04) SHA1(3898e9fef365ecaa4d86aa11756b527a4fffb494), ROM_BIOS(0) )
-	ROMX_LOAD( "may-02.16a",               0x100000, 0x100000, CRC(3a66a713) SHA1(beeb99156332cf4870738f7769b719a02d7b40af), ROM_BIOS(0) )
-	ROMX_LOAD( "may-04_w78_9235kd011.14a", 0x000000, 0x100000, CRC(dedd2dd3) SHA1(c1021edb0b377a030ab9593c838083f0f3b996b2), ROM_BIOS(1) )
-	ROMX_LOAD( "may-05_w79_9235kd019.16a", 0x100000, 0x100000, CRC(cb23835f) SHA1(c504ff99f9029355f69e7fd7e9528d647bd491bf), ROM_BIOS(1) )
+	ROM_LOAD( "may-01.14a", 0x000000, 0x100000, CRC(2da57d04) SHA1(3898e9fef365ecaa4d86aa11756b527a4fffb494) )
+	ROM_LOAD( "may-02.16a", 0x100000, 0x100000, CRC(3a66a713) SHA1(beeb99156332cf4870738f7769b719a02d7b40af) )
 
 	ROM_REGION( 0x80000, "oki", 0 )
 	ROM_LOAD( "may-03.11l", 0x00000, 0x80000, CRC(b6e42bae) SHA1(c282cdf7db30fb63340cc609bf00f5ab63a75583) )
@@ -483,8 +489,9 @@ void dietgo_state::init_dietgo()
 } // Anonymous namespace
 
 
-GAME( 1992, dietgo,   0,      dietgo, dietgoe, dietgo_state, init_dietgo, ROT0, "Data East Corporation", "Diet Go Go (Europe v1.1 1992.09.26, set 1)", MACHINE_SUPPORTS_SAVE )
-GAME( 1992, dietgoe,  dietgo, dietgo, dietgoe, dietgo_state, init_dietgo, ROT0, "Data East Corporation", "Diet Go Go (Europe v1.1 1992.09.26, set 2)", MACHINE_SUPPORTS_SAVE )
-GAME( 1992, dietgoea, dietgo, dietgo, dietgoe, dietgo_state, init_dietgo, ROT0, "Data East Corporation", "Diet Go Go (Europe v1.1 1992.08.04)",        MACHINE_SUPPORTS_SAVE )
+GAME( 1992, dietgo,   0,      dietgo, dietgon, dietgo_state, init_dietgo, ROT0, "Data East Corporation", "Diet Go Go (Europe v1.1 1992.09.26, set 1)", MACHINE_SUPPORTS_SAVE )
+GAME( 1992, dietgoe,  dietgo, dietgo, dietgo,  dietgo_state, init_dietgo, ROT0, "Data East Corporation", "Diet Go Go (Europe v1.1 1992.09.26, set 2)", MACHINE_SUPPORTS_SAVE )
+GAME( 1992, dietgoea, dietgo, dietgo, dietgo,  dietgo_state, init_dietgo, ROT0, "Data East Corporation", "Diet Go Go (Europe v1.1 1992.09.26, set 3)", MACHINE_SUPPORTS_SAVE )
+GAME( 1992, dietgoeb, dietgo, dietgo, dietgo,  dietgo_state, init_dietgo, ROT0, "Data East Corporation", "Diet Go Go (Europe v1.1 1992.08.04)",        MACHINE_SUPPORTS_SAVE )
 GAME( 1992, dietgou,  dietgo, dietgo, dietgo,  dietgo_state, init_dietgo, ROT0, "Data East Corporation", "Diet Go Go (USA v1.1 1992.09.26)",           MACHINE_SUPPORTS_SAVE )
 GAME( 1992, dietgoj,  dietgo, dietgo, dietgo,  dietgo_state, init_dietgo, ROT0, "Data East Corporation", "Diet Go Go (Japan v1.1 1992.09.26)",         MACHINE_SUPPORTS_SAVE )
