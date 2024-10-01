4dff490ac93b53b692ce267bda422c0bcf9c9e5c
mamehaze <140764005+mamehaze@users.noreply.github.com>
2024-10-01 17:11:42 +0100

    new NOT WORKING systems (TV Dear) (#12836)
    --------------------
    TV Dear [David Haywood, Team Europe]

---
+++ src/mame/skeleton/tvdear.cpp
@@ -0,0 +1,53 @@
+// license:BSD-3-Clause
+// copyright-holders:David Haywood
+
+// TV Word Processor, with printer
+
+#include "emu.h"
+#include "cpu/nec/v25.h"
+
+namespace {
+
+class tvdear_state : public driver_device
+{
+public:
+	tvdear_state(const machine_config &mconfig, device_type type, const char *tag)
+		: driver_device(mconfig, type, tag)
+		, m_maincpu(*this, "systemcpu")
+	{
+	}
+
+	void tvdear(machine_config &config);
+
+private:
+	void mem_map(address_map &map);
+
+	required_device<v25_device> m_maincpu;
+};
+
+
+void tvdear_state::mem_map(address_map &map)
+{
+	map(0xe0000, 0xfffff).rom().region("maincpu", 0xe0000); // wrong
+}
+
+static INPUT_PORTS_START(tvdear)
+INPUT_PORTS_END
+
+void tvdear_state::tvdear(machine_config &config)
+{
+	V25(config, m_maincpu, 16000000);
+	m_maincpu->set_addrmap(AS_PROGRAM, &tvdear_state::mem_map);
+}
+
+ROM_START(tvdear)
+	ROM_REGION(0x200000, "maincpu", 0)
+	ROM_LOAD("d23c160000.u5", 0x00000, 0x200000, CRC(41ec9890) SHA1(20cfdfec7eeb39a9ce971f23fdc97b42a5d68301) )
+
+	ROM_REGION(0x20000, "cart", 0) // TODO: move this to a software list
+	ROM_LOAD("lc371100.u15", 0x00000, 0x20000, CRC(f70696d1) SHA1(21da45720a48e18fd6173f2482bd7db4988d1548) )
+ROM_END
+
+} // anonymous namespace
+
+CONS( 1996, tvdear,  0,          0,  tvdear,  tvdear, tvdear_state, empty_init, "Takara", "TV Dear", MACHINE_IS_SKELETON )
