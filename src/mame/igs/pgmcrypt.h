7716e688772c6e1a2fd7b7ad70d016c306fad63e
Ivan Vangelista <mesgnet@yahoo.it>
2024-10-13 21:17:37 +0200

    Update pgmcrypt.h

--- src/mame/igs/pgmcrypt.h
+++ src/mame/igs/pgmcrypt.h
@@ -56,7 +56,7 @@ void olympic5_decrypt(running_machine &machine);
 void icescape_decrypt(running_machine &machine);
 void tripfev_decrypt(running_machine &machine);
 void wldfruit_decrypt(running_machine &machine);
-void lthy_decrypt(running_machine &machine);
+void lthyp_decrypt(running_machine &machine);
 void lhdmg_decrypt(running_machine &machine);
 void tripslot_decrypt(running_machine &machine);
 void extradrw_decrypt(running_machine &machine);
