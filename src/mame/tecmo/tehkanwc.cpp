28d5c27560bab2a326bda45911f6a9659ca22ef5
Jose Tejada <jose.tejada@jotego.es>
2024-11-03 18:28:09 +0100

    correct refresh rate

--- src/mame/tecmo/tehkanwc.cpp
+++ src/mame/tecmo/tehkanwc.cpp
@@ -429,8 +429,8 @@ void tehkanwc_state::shared_mem(address_map &map)
 	map(0xc800, 0xcfff).ram().share("shareram");
 	map(0xd000, 0xd3ff).ram().w(FUNC(tehkanwc_state::videoram_w)).share("videoram");
 	map(0xd400, 0xd7ff).ram().w(FUNC(tehkanwc_state::colorram_w)).share("colorram");
-	map(0xd800, 0xddff).writeonly().w(m_palette, FUNC(palette_device::write8)).share("palette");
-	map(0xde00, 0xdfff).nopw(); // unused part of the palette RAM, I think? Gridiron writes here
+	map(0xd800, 0xddff).ram().w(m_palette, FUNC(palette_device::write8)).share("palette");
+	map(0xde00, 0xdfff).ram(); // unused part of the palette RAM, I think? Gridiron writes here
 	map(0xe000, 0xe7ff).ram().w(FUNC(tehkanwc_state::videoram2_w)).share("videoram2");
 	map(0xe800, 0xebff).ram().share("spriteram");
 	map(0xec00, 0xec01).w(FUNC(tehkanwc_state::scroll_x_w));
@@ -820,8 +820,8 @@ void tehkanwc_state::tehkanwc(machine_config &config)
 
 	/* video hardware */
 	screen_device &screen(SCREEN(config, "screen", SCREEN_TYPE_RASTER));
-	screen.set_refresh_hz(60);
-	screen.set_vblank_time(ATTOSECONDS_IN_USEC(2500)); /* not accurate */
+	screen.set_refresh_hz(60.61);
+	screen.set_vblank_time(ATTOSECONDS_IN_USEC(2500));
 	screen.set_size(32*8, 32*8);
 	screen.set_visarea(0*8, 32*8-1, 2*8, 30*8-1);
 	screen.set_screen_update(FUNC(tehkanwc_state::screen_update));
