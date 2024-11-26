24cb27c366bbeb4cf4f5e84cb0ecea784032c6b9
hap <happppp@users.noreply.github.com>
2024-11-26 04:07:18 +0100

    source org: rename mr to modelracing

--- src/mame/modelracing/nl_dribling.h
+++ src/mame/modelracing/nl_dribling.h
@@ -1,10 +1,10 @@
 // license:BSD-3-Clause
 // copyright-holders: Enzo Lombardi
-#ifndef MAME_MR_NL_DRIBLING_H
-#define MAME_MR_NL_DRIBLING_H
+#ifndef MAME_MODELRACING_NL_DRIBLING_H
+#define MAME_MODELRACING_NL_DRIBLING_H
 
 #include "netlist/nl_setup.h"
 
 NETLIST_EXTERNAL(dribling)
 
-#endif // MAME_MR_NL_DRIBLING_H
+#endif // MAME_MODELRACING_NL_DRIBLING_H
