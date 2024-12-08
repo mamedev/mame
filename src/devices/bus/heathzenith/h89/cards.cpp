65b35b09b78dd42d461c04665a275eef3c6ce220
Mark Garlanger <mgarlanger@users.noreply.github.com>
2024-12-07 23:41:35 -0600

    heathzenith/h89.cpp: Add h89 clone with CDR hardware (#13027)

--- src/devices/bus/heathzenith/h89/cards.cpp
+++ src/devices/bus/heathzenith/h89/cards.cpp
@@ -9,6 +9,7 @@
 #include "emu.h"
 #include "cards.h"
 
+#include "cdr_fdc_880h.h"
 #include "h_88_3.h"
 #include "h_88_5.h"
 #include "mms77316_fdc.h"
@@ -24,6 +25,7 @@ void h89_left_cards(device_slot_interface &device)
 
 void h89_right_cards(device_slot_interface &device)
 {
+	device.option_add("cdr_fdc", H89BUS_CDR_FDC_880H);
 	device.option_add("h_88_3", H89BUS_H_88_3);
 	device.option_add("ha_88_3", H89BUS_HA_88_3);
 	device.option_add("h_88_5", H89BUS_H_88_5);
