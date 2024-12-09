e0a6955dd3cfd0cd3ad29ad06a0bbee798b90a4e
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-12-08 23:28:04 -0500

    Update segas32_v.cpp

--- src/mame/sega/segas32_v.cpp
+++ src/mame/sega/segas32_v.cpp
@@ -740,10 +740,10 @@ void segas32_state::update_tilemap_zoom(screen_device &screen, segas32_state::la
 	srcy += (m_videoram[0x1ff14/2 + 4 * bgnum] & 0xfe00) << 4;
 
 	/*
-	   then account for the destination center coordinates - we currently expand the resolution
+	   Then account for the destination center coordinates - We currently expand the resolution
 	   from 9 bit to 10 bit while zooming which correctly centers the bg in harddunk during attract
 	   mode at the low resolution and the course selection screen in radr at the higher resolution.
-	   this behavior has not been verified to real hardware and maybe a hack.
+	   This behavior has not been verified to real hardware and might be a hack.
 	*/
 	srcx_start -= util::sext(m_videoram[0x1ff30/2 + 2 * bgnum], (dstxstep != 0x200) ? 10 : 9) * srcxstep;
 	srcy -= util::sext(m_videoram[0x1ff32/2 + 2 * bgnum], (dstystep != 0x200) ? 10 : 9) * srcystep;
