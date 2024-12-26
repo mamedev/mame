f625fbb8bd24cb4906c5017f09b0a4c314d40bfa
RetroAND <jlopezm@andorra.ad>
2024-12-26 20:29:35 +0100

    Corrected further errors into the driver; driver now compiles!

--- src/mame/ibm/system23.cpp
+++ src/mame/ibm/system23.cpp
@@ -4,13 +4,14 @@
 
 #include "ibmsystem23.lh"
 
-namespace system23
+namespace
 {
 	class system23_state: public driver_device
 	{
 		public:
 			system23_state(const machine_config &mconfig, device_type type, const char *tag)
 				: driver_device(mconfig, type, tag),
+				m_maincpu(*this, "maincpu"),
 				m_ppi8255(*this,"ppi8255"),
 				m_diag_digits(*this, "digit%u", 0U)
 			{
@@ -24,6 +25,7 @@ namespace system23
 			virtual void machine_reset() override ATTR_COLD;
 
 		private:
+			required_device<i8085a_cpu_device> m_maincpu;
 			required_device<i8255_device> m_ppi8255;
 			output_finder<2> m_diag_digits;
 
@@ -59,17 +61,25 @@ namespace system23
 
 		config.set_perfect_quantum("maincpu");
 		config.set_default_layout(layout_ibmsystem23);
+	}
 
+	void system23_state::machine_start()
+	{
+
+	}
+
+	void system23_state::machine_reset()
+	{
 
 	}
 
 	ROM_START( system23 )
 		ROM_REGION(0x4000, "ros_unpaged", 0)
-		ROM_LOAD("02_61c9866a_4481186.bin", 0x0000, 0x2000, CRC(61c9866a) SHA1(3b51a6b72d2ccae2459ddb2e16fbd21b19dfa2b8) )
-		ROM_LOAD("09_07843020_8493747.bin", 0x2000, 0x2000, CRC(07843020) SHA1(078405ad202e26b7bac7132b06682fb01270af63) )
+		ROM_LOAD("02_61c9866a_4481186.bin", 0x0000, 0x2000, CRC(61c9866a) SHA1(43f2bed5cc2374c7fde4632948329062e57e994b) )
+		ROM_LOAD("09_07843020_8493747.bin", 0x2000, 0x2000, CRC(07843020) SHA1(828ca0199af1246f6caf58bcb785f791c3a7e34e) )
 	ROM_END
 
+}
 
 	COMP( 1981, system23, 0,      0,      system23, 0,     system23_state, empty_init, "IBM",   "IBM System/23 Datamaster", MACHINE_NO_SOUND | MACHINE_NOT_WORKING)
 
-}
