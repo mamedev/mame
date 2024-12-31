6a28eb0f231089b92e1edf0f777492b3d771eb99
David Haywood <hazemamewip@hotmail.com>
2024-12-31 16:24:35 +0000

    new NOT WORKING machines ------------------------------- EA Sports Classics: NHL 95 & FIFA 95 [David Haywood, Team Europe]

--- src/mame/tvgames/spg110.cpp
+++ src/mame/tvgames/spg110.cpp
@@ -12,10 +12,10 @@
         Conny TV Virtual Tennis
         Conny Ping Pong
         Conny TV Virtual Fighter
+        JAKKS EA Sports (NHL 95 + Fifa 96) (EU)
 
         assumed:
         JAKKS EA Sports (NHL 95 + Madden 95) (US)
-        JAKKS EA Sports (NHL 95 + Fifa 96) (US)
         JAKKS Bob the Builder
         JAKKS Disney (original release)
 
@@ -47,6 +47,8 @@ public:
 	void spg110_base(machine_config &config);
 	void spg110_spdmo(machine_config& config);
 
+	void spg110_base_pal(machine_config &config);
+
 	ioport_value plunger_r();
 
 protected:
@@ -54,6 +56,20 @@ protected:
 	required_device<screen_device> m_screen;
 
 	virtual void mem_map(address_map &map) ATTR_COLD;
+};
+
+class spg110_easports_game_state : public spg110_game_state
+{
+public:
+	spg110_easports_game_state(const machine_config &mconfig, device_type type, const char *tag) :
+		spg110_game_state(mconfig, type, tag)
+	{ }
+public:
+	void easports_pal(machine_config &config);
+
+protected:
+private:
+	virtual void mem_map(address_map &map) override ATTR_COLD;
 };
 
 class spg110_sstarkar_game_state : public spg110_game_state
@@ -90,6 +106,13 @@ void spg110_game_state::mem_map(address_map &map)
 void spg110_sstarkar_game_state::mem_map_cart(address_map &map)
 {
 	map(0x004000, 0x0fffff).bankr("cartrom");
+}
+
+void spg110_easports_game_state::mem_map(address_map &map)
+{
+	//map(0x001000, 0x007fff).ram(); // ??
+	map(0x004000, 0x2fffff).rom().region("maincpu", 0x8000);
+	map(0x3e0000, 0x3fffff).ram();
 }
 
 
@@ -374,9 +397,7 @@ static INPUT_PORTS_START( jak_spdmo )
 
 INPUT_PORTS_END
 
-
-
-static INPUT_PORTS_START( conyteni )
+static INPUT_PORTS_START( spg110_base )
 	PORT_START("PA")
 	PORT_DIPNAME( 0x0001, 0x0000, "PA" )
 	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
@@ -431,15 +452,27 @@ static INPUT_PORTS_START( conyteni )
 	PORT_DIPNAME( 0x0001, 0x0000, "PB" )
 	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
 	PORT_DIPSETTING(      0x0001, DEF_STR( On ) )
-	PORT_BIT( 0x0002, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_NAME("Console Back")
-	PORT_BIT( 0x0004, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_NAME("Console Enter")
+	PORT_DIPNAME( 0x0002, 0x0000, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0002, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0004, 0x0000, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0004, DEF_STR( On ) )
 	PORT_DIPNAME( 0x0008, 0x0000, DEF_STR( Unknown ) )
 	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
 	PORT_DIPSETTING(      0x0008, DEF_STR( On ) )
-	PORT_BIT( 0x0010, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_NAME("Console Right")
-	PORT_BIT( 0x0020, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN )  PORT_NAME("Console Down")
-	PORT_BIT( 0x0040, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT )  PORT_NAME("Console Left")
-	PORT_BIT( 0x0080, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP )    PORT_NAME("Console Up")
+	PORT_DIPNAME( 0x0010, 0x0000, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0010, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0020, 0x0000, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0020, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0040, 0x0000, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0040, DEF_STR( On ) )
+	PORT_DIPNAME( 0x0080, 0x0000, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
+	PORT_DIPSETTING(      0x0080, DEF_STR( On ) )
 	PORT_DIPNAME( 0x0100, 0x0000, DEF_STR( Unknown ) )
 	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
 	PORT_DIPSETTING(      0x0100, DEF_STR( On ) )
@@ -518,7 +551,44 @@ static INPUT_PORTS_START( conyteni )
 	PORT_START("JOYX")
 
 	PORT_START("JOYY")
+INPUT_PORTS_END
 
+static INPUT_PORTS_START( easports )
+	PORT_INCLUDE( spg110_base )
+
+	PORT_MODIFY("PA")
+	// not all units had 2 control pads
+	// hold right and button 2 (B) on startup for ROM test
+	PORT_BIT( 0x0001, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(2)
+	PORT_BIT( 0x0002, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(2)
+	PORT_BIT( 0x0004, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN ) PORT_PLAYER(2)
+	PORT_BIT( 0x0008, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP ) PORT_PLAYER(2)
+	PORT_BIT( 0x0010, IP_ACTIVE_HIGH, IPT_BUTTON4 ) PORT_PLAYER(2) // START
+	PORT_BIT( 0x0020, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_PLAYER(2) // B
+	PORT_BIT( 0x0040, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_PLAYER(2) // A
+	PORT_BIT( 0x0080, IP_ACTIVE_HIGH, IPT_BUTTON3 ) PORT_PLAYER(2) // C
+
+	PORT_BIT( 0x0100, IP_ACTIVE_HIGH, IPT_BUTTON4 ) PORT_PLAYER(1) // START
+	PORT_BIT( 0x0200, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_PLAYER(1) // B
+	PORT_BIT( 0x0400, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_PLAYER(1) // A
+	PORT_BIT( 0x0800, IP_ACTIVE_HIGH, IPT_BUTTON3 ) PORT_PLAYER(1) // C
+	PORT_BIT( 0x1000, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_PLAYER(1)
+	PORT_BIT( 0x2000, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT ) PORT_PLAYER(1)
+	PORT_BIT( 0x4000, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN ) PORT_PLAYER(1)
+	PORT_BIT( 0x8000, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP ) PORT_PLAYER(1)
+
+INPUT_PORTS_END
+
+static INPUT_PORTS_START( conyteni )
+	PORT_INCLUDE( spg110_base )
+
+	PORT_MODIFY("PB")
+	PORT_BIT( 0x0002, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_NAME("Console Back")
+	PORT_BIT( 0x0004, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_NAME("Console Enter")
+	PORT_BIT( 0x0010, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT ) PORT_NAME("Console Right")
+	PORT_BIT( 0x0020, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN )  PORT_NAME("Console Down")
+	PORT_BIT( 0x0040, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT )  PORT_NAME("Console Left")
+	PORT_BIT( 0x0080, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP )    PORT_NAME("Console Up")
 INPUT_PORTS_END
 
 void spg110_game_state::spg110_base(machine_config &config)
@@ -544,9 +614,22 @@ void spg110_game_state::spg110_base(machine_config &config)
 	m_maincpu->add_route(ALL_OUTPUTS, "rspeaker", 0.5);
 }
 
+void spg110_game_state::spg110_base_pal(machine_config &config)
+{
+	spg110_base(config);
+	m_screen->set_refresh_hz(50);
+	m_maincpu->set_video_irq_spidman(true);
+}
+
 void spg110_game_state::spg110_spdmo(machine_config& config)
 {
 	spg110_base(config);
+	m_maincpu->set_video_irq_spidman(true);
+}
+
+void spg110_easports_game_state::easports_pal(machine_config &config)
+{
+	spg110_base_pal(config);
 	m_maincpu->set_video_irq_spidman(true);
 }
 
@@ -603,6 +686,13 @@ ROM_START( jak_spdmoa )
 	ROM_LOAD16_WORD_SWAP( "spiderman.bin", 0x000000, 0x200000, CRC(b2a5a55a) SHA1(93c87ac0387997b753d4f0fb894a0dd02138b460) )
 ROM_END
 
+ROM_START( easports )
+	ROM_REGION( 0x800000, "maincpu", ROMREGION_ERASE00 )
+	ROM_LOAD16_WORD_SWAP( "ea.u3", 0x000000, 0x400000, CRC(d750089e) SHA1(426f04c3d841103d434a892561db55ade684db54) )
+	ROM_LOAD16_WORD_SWAP( "ea.u4", 0x400000, 0x200000, CRC(20a63445) SHA1(8c3383a353638c7d6b795d15a751275043eacbd0) )
+ROM_END
+
+
 ROM_START( conyteni )
 	ROM_REGION( 0x800000, "maincpu", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "tennis_m29w160eb.bin", 0x000000, 0x200000, CRC(70050f17) SHA1(929f0d8599b7380b5994684424bb91063c4f6569) )
@@ -650,3 +740,6 @@ CONS( 200?, conyfght,  0,        0, spg110_base, conyteni,  spg110_game_state, e
 
 // The unit contains no BIOS ROM, was sold by Taikee as Singing Star Karaoke, but also by Imaginarium / ItsMagical in Spain as Karao Kids.  Cartridges are compatible.
 CONS( 200?, sstarkar,  0,        0, sstarkar, conyteni,  spg110_sstarkar_game_state, empty_init, "Taikee",      "Singing Star Karaoke (World) / Karao Kids (Spain)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND | MACHINE_IMPERFECT_GRAPHICS ) // "ItsMagical" brand from Imaginarium
+
+CONS( 2004, easports,  0,        0, easports_pal, easports,  spg110_easports_game_state, empty_init, "JAKKS Pacific Inc / Digital Eclipse",      "EA Sports Classics: NHL 95 & FIFA 95 (JAKKS Pacific TV Game)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND | MACHINE_IMPERFECT_GRAPHICS )
+// US release has Madden instead of FIFA
