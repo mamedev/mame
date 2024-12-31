9497c50e95d5d1d5f72cb6b5d8768bee7fb73f1b
RetroAND <jlopezm@andorra.ad>
2024-12-31 19:32:43 +0100

    Implemented test 4;

--- src/mame/ibm/system23.cpp
+++ src/mame/ibm/system23.cpp
@@ -4,6 +4,8 @@
 #include "emu.h"
 #include "cpu/i8085/i8085.h"
 #include "machine/i8255.h"
+#include "machine/i8257.h"
+#include "machine/ram.h"
 
 #include "ibmsystem23.lh"
 
@@ -17,8 +19,9 @@ namespace
 				m_maincpu(*this, "maincpu"),
 				m_ppi_kbd(*this, "ppi_kbd"),
 				m_ppi_diag(*this, "ppi_diag"),
-				m_ram(*this, "ram"),
-				m_paged_ram(*this, "paged_ram"),
+				m_ppi_settings(*this, "ppi_settings"),
+				m_dma(*this,"dma"),
+				m_ram(*this, RAM_TAG),
 				m_diag_digits(*this, "digit%u", 0U)
 
 			{
@@ -35,8 +38,9 @@ namespace
 			required_device<i8085a_cpu_device> m_maincpu;
 			required_device<i8255_device> m_ppi_kbd;
 			required_device<i8255_device> m_ppi_diag;
-			required_shared_ptr<u8> m_ram;
-			required_shared_ptr<u8> m_paged_ram;
+			required_device<i8255_device> m_ppi_settings;
+			required_device<i8257_device> m_dma;
+			required_device<ram_device> m_ram;
 			output_finder<2> m_diag_digits;
 
 			uint8_t m_bus_test_register = 0;
@@ -46,6 +50,8 @@ namespace
 
 			void cpu_test_register_w(uint8_t data);
 			uint8_t cpu_test_register_r();
+
+			uint8_t memory_settings_r();
 
 			uint8_t sid_sod_connection;
 			void sod_w(int state);
@@ -71,9 +77,14 @@ namespace
 		return m_bus_test_register;
 	}
 
+	uint8_t system23_state::memory_settings_r()
+	{
+		return 0;	//Patch to make the memory test work while port 2e is being studied
+	}
+
 	int system23_state::sid_r()
 	{
-		return ASSERT_LINE; // patch to bypass the test
+		return ASSERT_LINE; // Actually, SID is tied to VCC at the motherboard
 	}
 
 	void system23_state::sod_w(int state)
@@ -84,6 +95,8 @@ namespace
 	void system23_state::system23_io(address_map &map)
 	{
 		map.unmap_value_high();
+		map(0x00, 0x0f).rw(m_dma, FUNC(i8257_device::read), FUNC(i8257_device::write));
+		map(0x2c, 0x2f).rw(m_ppi_settings, FUNC(i8255_device::read), FUNC(i8255_device::write));
 		map(0x40, 0x43).rw(m_ppi_diag, FUNC(i8255_device::read), FUNC(i8255_device::write));
 		map(0x4c, 0x4f).rw(m_ppi_kbd, FUNC(i8255_device::read), FUNC(i8255_device::write));
 	}
@@ -92,13 +105,13 @@ namespace
 	{
 		map.unmap_value_high();
 		map(0x0000, 0x3fff).rom().region("ros_unpaged",0);
-		map(0x8000, 0xbfff).ram().share("ram");
-		map(0xC000, 0xffff).ram().share("paged_ram");
+		map(0x8000, 0xbfff).ram();
+		//map(0xC000, 0xffff).ram();
 	}
 
 	void system23_state::system23(machine_config &config)
 	{
-		i8085a_cpu_device &maincpu(I8085A(config, "maincpu", 6_MHz_XTAL)); //frequency needs to be adjusted
+		i8085a_cpu_device &maincpu(I8085A(config, "maincpu", 6.144_MHz_XTAL)); //frequency needs to be adjusted
 		maincpu.set_addrmap(AS_PROGRAM, &system23_state::system23_mem);
 		maincpu.set_addrmap(AS_IO, &system23_state::system23_io);
 		maincpu.in_sid_func().set(FUNC(system23_state::sid_r));
@@ -110,6 +123,13 @@ namespace
 
 		I8255(config, m_ppi_diag);
 		m_ppi_diag->out_pb_callback().set(FUNC(system23_state::diag_digits_w));
+
+		I8255(config, m_ppi_settings);
+		m_ppi_settings->in_pc_callback().set(FUNC(system23_state::memory_settings_r));
+
+		I8257(config, m_dma);
+
+		RAM(config, m_ram).set_default_size("16k");
 
 		config.set_perfect_quantum("maincpu");
 		config.set_default_layout(layout_ibmsystem23);
