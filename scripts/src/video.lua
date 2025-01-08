31bd02566b447342fbd6713565f09132ca72e877
Vas Crabb <vas@vastheman.com>
2025-01-09 05:02:10 +1100

    konami/rungun.cpp: Added a slightly earlier US version of Run and Gun.
    New working clones
    ------------------
    Run and Gun (ver UAB 1993  9.10, dedicated twin cabinet) [Bryan McPhail]

--- scripts/src/video.lua
+++ scripts/src/video.lua
@@ -1511,6 +1511,18 @@ end
 
 --------------------------------------------------
 --
+--@src/devices/video/bt431.h,VIDEOS["BT431"] = true
+--------------------------------------------------
+
+if (VIDEOS["BT431"]~=null) then
+	files {
+		MAME_DIR .. "src/devices/video/bt431.cpp",
+		MAME_DIR .. "src/devices/video/bt431.h",
+	}
+end
+
+--------------------------------------------------
+--
 --@src/devices/video/bt459.h,VIDEOS["BT459"] = true
 --------------------------------------------------
 
@@ -1518,6 +1530,42 @@ if (VIDEOS["BT459"]~=null) then
 	files {
 		MAME_DIR .. "src/devices/video/bt459.cpp",
 		MAME_DIR .. "src/devices/video/bt459.h",
+	}
+end
+
+--------------------------------------------------
+--
+--@src/devices/video/bt45x.h,VIDEOS["BT45X"] = true
+--------------------------------------------------
+
+if (VIDEOS["BT45X"]~=null) then
+	files {
+		MAME_DIR .. "src/devices/video/bt45x.cpp",
+		MAME_DIR .. "src/devices/video/bt45x.h",
+	}
+end
+
+--------------------------------------------------
+--
+--@src/devices/video/bt47x.h,VIDEOS["BT47X"] = true
+--------------------------------------------------
+
+if (VIDEOS["BT47X"]~=null) then
+	files {
+		MAME_DIR .. "src/devices/video/bt47x.cpp",
+		MAME_DIR .. "src/devices/video/bt47x.h",
+	}
+end
+
+--------------------------------------------------
+--
+--@src/devices/video/bt48x.h,VIDEOS["BT48X"] = true
+--------------------------------------------------
+
+if (VIDEOS["BT48X"]~=null) then
+	files {
+		MAME_DIR .. "src/devices/video/bt48x.cpp",
+		MAME_DIR .. "src/devices/video/bt48x.h",
 	}
 end
 
@@ -1542,18 +1590,6 @@ if (VIDEOS["DP8510"]~=null) then
 	files {
 		MAME_DIR .. "src/devices/video/dp8510.cpp",
 		MAME_DIR .. "src/devices/video/dp8510.h",
-	}
-end
-
---------------------------------------------------
---
---@src/devices/video/bt45x.h,VIDEOS["BT45X"] = true
---------------------------------------------------
-
-if (VIDEOS["BT45X"]~=null) then
-	files {
-		MAME_DIR .. "src/devices/video/bt45x.cpp",
-		MAME_DIR .. "src/devices/video/bt45x.h",
 	}
 end
 
@@ -1615,30 +1651,6 @@ end
 
 --------------------------------------------------
 --
---@src/devices/video/bt47x.h,VIDEOS["BT47X"] = true
---------------------------------------------------
-
-if (VIDEOS["BT47X"]~=null) then
-	files {
-		MAME_DIR .. "src/devices/video/bt47x.cpp",
-		MAME_DIR .. "src/devices/video/bt47x.h",
-	}
-end
-
---------------------------------------------------
---
---@src/devices/video/bt431.h,VIDEOS["BT431"] = true
---------------------------------------------------
-
-if (VIDEOS["BT431"]~=null) then
-	files {
-		MAME_DIR .. "src/devices/video/bt431.cpp",
-		MAME_DIR .. "src/devices/video/bt431.h",
-	}
-end
-
---------------------------------------------------
---
 --@src/devices/video/vrender0.h,VIDEOS["VRENDER0"] = true
 --------------------------------------------------
 
@@ -1682,17 +1694,5 @@ if (VIDEOS["ZR36110"]~=null) then
 	files {
 		MAME_DIR .. "src/devices/video/zr36110.cpp",
 		MAME_DIR .. "src/devices/video/zr36110.h",
-	}
-end
-
---------------------------------------------------
---
---@src/devices/video/bt48x.h,VIDEOS["BT48X"] = true
---------------------------------------------------
-
-if (VIDEOS["BT48X"]~=null) then
-	files {
-		MAME_DIR .. "src/devices/video/bt48x.cpp",
-		MAME_DIR .. "src/devices/video/bt48x.h",
 	}
 end
