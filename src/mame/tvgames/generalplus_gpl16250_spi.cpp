54389c6684aa212e709aaf7c73469cc88ce0e840
David Haywood <hazemamewip@hotmail.com>
2025-01-22 17:16:05 +0000

    reword

--- src/mame/tvgames/generalplus_gpl16250_spi.cpp
+++ src/mame/tvgames/generalplus_gpl16250_spi.cpp
@@ -216,10 +216,10 @@ CONS(2015, bkrankp,  0, 0, generalplus_gpspispi_bkrankp, gcm394, generalplus_gps
 // but these just seem to be brands, manufacturer is unknown.
 // Various case styles are available, the unit here was styled after a Nintendo Switch
 // 
-// Architecture is unknown, it contains many of the games beijuehh / bornkidh (generalplus_gpl16250_rom.cpp) but
-// is running from SPI flash and has 'Loading' screens between menus and after selecting a game.
+// Architecture is unknown, it contains many of the games in beijuehh / bornkidh (generalplus_gpl16250_rom.cpp)
+// but is running from SPI flash and has 'Loading' screens between menus and after selecting a game.
 // 
-// While those are GeneralPlus based platforms, it's possible the games were ported to something else, the SPI ROM
+// While those are GeneralPlus based platforms, it's possible the games were ported to something else, the SPI
 // appears to contain a filesystem, but data looks to be compressed / encrypted with no obvious code.
 // There is no GPspi header in the SPI ROM.
 CONS(202?, bk139in1,  0, 0, generalplus_gpspispi, gcm394, generalplus_gpspispi_game_state, empty_init, "<unknown>", "BornKid 32 Bit Preloaded 139-in-1 Handheld Game Console", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
