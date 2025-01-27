170c07b48bd65375a54a09ac631abf78aa9d2da8
Kirk Meyer <kirk.meyer@alpaxo.com>
2025-01-26 21:50:52 -0700

    MC68EZ328: Fuller implementation of timer
    The existing implementation left the timer counter (TCN) at zero until
    the compare value (TCMP) was reached. However, some software relies on
    reading the TCN values to measure elapsed time.
    
    The timer counter (TCN) now increments on every timer tick.

--- src/devices/machine/mc68328.cpp
+++ src/devices/machine/mc68328.cpp
@@ -2677,7 +2677,7 @@ void mc68328_base_device::update_gptimer_state()
 		}
 		else
 		{
-			timer->adjust(attotime::from_ticks(regs.tcmp, get_timer_frequency<Timer>()));
+			timer->adjust(attotime::from_hz(get_timer_frequency<Timer>()));
 		}
 	}
 	else
@@ -2692,39 +2692,30 @@ TIMER_CALLBACK_MEMBER(mc68328_base_device::timer_tick)
 	timer_regs &regs = get_timer_regs(Timer);
 	emu_timer *timer = get_timer(Timer);
 
-	regs.tcn = regs.tcmp;
-	regs.tstat |= TSTAT_COMP;
-
-	if ((regs.tctl & TCTL_FRR) == TCTL_FRR_RESTART)
+	u32 frequency = get_timer_frequency<Timer>();
+	if (frequency > 0)
 	{
-		u32 frequency = get_timer_frequency<Timer>();
-		if (frequency > 0)
-		{
-			attotime period = attotime::from_hz(frequency) * regs.tcmp;
-			regs.tcn = 0x0000;
-			timer->adjust(period);
-		}
-		else
-		{
-			timer->adjust(attotime::never);
-		}
+		attotime period = attotime::from_hz(frequency);
+		timer->adjust(period);
 	}
 	else
 	{
-		u32 frequency = get_timer_frequency<Timer>();
-		if (frequency > 0)
-		{
-			attotime period = attotime::from_hz(frequency) * 0x10000;
-			timer->adjust(period);
-		}
-		else
-		{
-			timer->adjust(attotime::never);
-		}
+		timer->adjust(attotime::never);
 	}
-	if ((regs.tctl & TCTL_IRQEN) == TCTL_IRQEN_ENABLE)
+
+	regs.tcn++;
+	if (regs.tcn == regs.tcmp)
 	{
-		set_interrupt_line(get_timer_int(Timer), 1);
+		regs.tstat |= TSTAT_COMP;
+		if ((regs.tctl & TCTL_FRR) == TCTL_FRR_RESTART)
+		{
+			regs.tcn = 0x0000;
+		}
+
+		if ((regs.tctl & TCTL_IRQEN) == TCTL_IRQEN_ENABLE)
+		{
+			set_interrupt_line(get_timer_int(Timer), 1);
+		}
 	}
 }
 
