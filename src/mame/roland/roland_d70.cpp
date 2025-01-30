b9014f7775d0ba9d3ed2db6a1a6313d734e5dcf7
Vas Crabb <cuavas@users.noreply.github.com>
2025-01-31 09:13:21 +1100

    roland/roland_d70.cpp: Corrected name of layout.

--- src/mame/roland/roland_d70.cpp
+++ src/mame/roland/roland_d70.cpp
@@ -545,7 +545,7 @@ void roland_d70_state::d70(machine_config &config) {
 	MIDI_PORT(config, "mdout", midiout_slot, "midiout");
 	MIDI_PORT(config, "mdthru", midiout_slot, "midiout");
 
-	config.set_default_layout(layout_d70);
+	config.set_default_layout(layout_roland_d70);
 }
 
 void roland_d70_state::init_d70() {
