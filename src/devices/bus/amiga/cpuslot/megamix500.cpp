1c491a10945af5a980036b807dee8e44d187cbb7
Dirk Best <mail@dirk-best.de>
2025-02-02 23:59:31 +0100

    amiga: Untangle CPU slot bus from Zorro-II slot
    - Rewrite DMAC DMA controller. Now actually transfers data.
    - Add preliminary support for the A590 hard disk controller
    - Add preliminary support for the Action Replay freezer devices
    - Add support for the MegaMix 500 RAM expansion
    - Add preliminary support for the A2091 hard disk controller
    
    New working software list additions
    -----------------------------------
    A590 Setup Disk 1.7 (Western Digital Harddisk) [Turran FTP]
    A2091 Setup Disk 1.27 [Amiga Hardware Database]

--- src/devices/bus/amiga/cpuslot/megamix500.cpp
+++ src/devices/bus/amiga/cpuslot/megamix500.cpp
@@ -2,16 +2,19 @@
 // copyright-holders: Dirk Best
 /***************************************************************************
 
-    Commodore A2058
+    3-State MegaMix 500
 
-    Zorro-II RAM Expansion (2, 4 or 8 MB)
+    External RAM expansion for the A500
+
+    TODO: Passthrough
 
 ***************************************************************************/
 
 #include "emu.h"
-#include "a2058.h"
+#include "megamix500.h"
 
-#define VERBOSE 1
+#define VERBOSE (LOG_GENERAL)
+
 #include "logmacro.h"
 
 
@@ -19,13 +22,13 @@
 //  DEVICE DEFINITIONS
 //**************************************************************************
 
-DEFINE_DEVICE_TYPE(AMIGA_A2058, bus::amiga::zorro::a2058_device, "amiga_a2058", "Commodore A2058 RAM Expansion Card")
+DEFINE_DEVICE_TYPE(AMIGA_CPUSLOT_MEGAMIX500, bus::amiga::cpuslot::megamix500_device, "amiga_megamix500", "3-State MegaMix 500")
 
-namespace bus::amiga::zorro {
+namespace bus::amiga::cpuslot {
 
-a2058_device::a2058_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) :
-	device_t(mconfig, AMIGA_A2058, tag, owner, clock),
-	device_zorro2_card_interface(mconfig, *this),
+megamix500_device::megamix500_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) :
+	device_t(mconfig, AMIGA_CPUSLOT_MEGAMIX500, tag, owner, clock),
+	device_amiga_cpuslot_interface(mconfig, *this),
 	m_config(*this, "config"),
 	m_ram_size(0)
 {
@@ -36,18 +39,18 @@ a2058_device::a2058_device(const machine_config &mconfig, const char *tag, devic
 //  INPUT DEFINITIONS
 //**************************************************************************
 
-static INPUT_PORTS_START( a2058 )
+static INPUT_PORTS_START( megamix500 )
 	PORT_START("config")
-	PORT_CONFNAME(0x03, 0x02, "A2058 Installed RAM")
-	PORT_CONFSETTING(0x00, "2 MB")
-	PORT_CONFSETTING(0x01, "4 MB")
-	PORT_CONFSETTING(0x02, "8 MB")
-	PORT_CONFSETTING(0x03, "Auto-Config disabled")
+	PORT_CONFNAME(0x03, 0x03, "Installed RAM")
+	PORT_CONFSETTING(0x00, "Disabled")
+	PORT_CONFSETTING(0x01, "2 MB")
+	PORT_CONFSETTING(0x02, "4 MB")
+	PORT_CONFSETTING(0x03, "8 MB")
 INPUT_PORTS_END
 
-ioport_constructor a2058_device::device_input_ports() const
+ioport_constructor megamix500_device::device_input_ports() const
 {
-	return INPUT_PORTS_NAME( a2058 );
+	return INPUT_PORTS_NAME( megamix500 );
 }
 
 
@@ -55,13 +58,30 @@ ioport_constructor a2058_device::device_input_ports() const
 //  MACHINE EMULATION
 //**************************************************************************
 
-void a2058_device::device_start()
+void megamix500_device::device_start()
 {
 	// setup ram
 	m_ram = make_unique_clear<uint16_t[]>(0x800000/2);
 
 	// register for save states
 	save_pointer(NAME(m_ram), 0x800000/2);
+	save_item(NAME(m_ram_size));
+	save_item(NAME(m_base_address));
+}
+
+void megamix500_device::rst_w(int state)
+{
+	if (state == 0)
+	{
+		// on reset, remove ram
+		if (m_ram_size > 0)
+		{
+			LOG("unmapping ram, base = %06x\n", m_base_address);
+			m_host->space().unmap_readwrite(m_base_address, m_base_address + (m_ram_size << 20) - 1);
+		}
+
+		m_ram_size = 0;
+	}
 }
 
 
@@ -69,53 +89,49 @@ void a2058_device::device_start()
 //  AUTOCONFIG
 //**************************************************************************
 
-void a2058_device::autoconfig_base_address(offs_t address)
+void megamix500_device::autoconfig_base_address(offs_t address)
 {
-	LOG("%s: autoconfig_base_address received: 0x%06x\n", shortname(), address);
-	LOG("-> installing a2058\n");
+	LOG("autoconfig_base_address received: 0x%06x\n", address);
+	LOG("-> installing megamix500\n");
 
-	// stop responding to default autoconfig
-	m_zorro->space().unmap_readwrite(0xe80000, 0xe8007f);
+	m_base_address = address;
 
-	// install access to the rom space
-	m_zorro->space().install_ram(address, address + (m_ram_size << 20) - 1, m_ram.get());
+	m_host->space().unmap_readwrite(0xe80000, 0xe8007f);
+	m_host->space().install_ram(address, address + (m_ram_size << 20) - 1, m_ram.get());
 
-	// we're done
-	m_zorro->cfgout_w(0);
+	m_host->cfgout_w(0);
 }
 
-void a2058_device::cfgin_w(int state)
+void megamix500_device::cfgin_w(int state)
 {
-	LOG("%s: cfgin_w (%d)\n", shortname(), state);
+	LOG("cfgin_w: %d\n", state);
 
-	if (state == 0)
+	uint8_t cfg = m_config->read();
+
+	if (state == 0 && cfg)
 	{
 		// setup autoconfig
 		autoconfig_board_type(BOARD_TYPE_ZORRO2);
 
 		// setup ram
-		switch (m_config->read())
+		switch (cfg)
 		{
-		case 0:
+		case 1:
 			autoconfig_board_size(BOARD_SIZE_2M);
 			m_ram_size = 0x200000 >> 20;
 			break;
-		case 1:
+		case 2:
 			autoconfig_board_size(BOARD_SIZE_4M);
 			m_ram_size = 0x400000 >> 20;
 			break;
-		case 2:
+		case 3:
 			autoconfig_board_size(BOARD_SIZE_8M);
 			m_ram_size = 0x800000 >> 20;
 			break;
-		case 3:
-			// auto-config disabled
-			m_ram_size = 0;
-			return;
 		}
 
-		autoconfig_product(10);
-		autoconfig_manufacturer(514);
+		autoconfig_product(2);
+		autoconfig_manufacturer(512);
 		autoconfig_serial(0x00000000);
 
 		autoconfig_link_into_memory(true);
@@ -125,10 +141,10 @@ void a2058_device::cfgin_w(int state)
 		autoconfig_can_shutup(true); // ?
 
 		// install autoconfig handler
-		m_zorro->space().install_readwrite_handler(0xe80000, 0xe8007f,
+		m_host->space().install_readwrite_handler(0xe80000, 0xe8007f,
 			read16_delegate(*this, FUNC(amiga_autoconfig::autoconfig_read)),
 			write16_delegate(*this, FUNC(amiga_autoconfig::autoconfig_write)), 0xffff);
 	}
 }
 
-} // namespace bus::amiga::zorro
+} // namespace bus::amiga::cpuslot
