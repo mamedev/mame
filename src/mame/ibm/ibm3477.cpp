5cf7a25a99e00d0f4a99bbc731fa913a2c7cb4bb
ClawGrip <clawgrip@hotmail.com>
2025-02-09 21:43:55 +0100

    Remove m_p_charge

--- src/mame/ibm/ibm3477.cpp
+++ src/mame/ibm/ibm3477.cpp
@@ -49,7 +49,6 @@ public:
 	ibm3477_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag)
 		, m_maincpu(*this, "maincpu")
-		, m_p_chargen(*this, "chargen")
 		{ }
 
 	void ibm3477(machine_config &config);
@@ -60,7 +59,6 @@ private:
 	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 
 	required_device<cpu_device> m_maincpu;
-	required_region_ptr<u8> m_p_chargen;
 };
 
 
