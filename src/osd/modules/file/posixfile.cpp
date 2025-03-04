3c1d14f76e78bf6b7567f229522480b9c4e37721
Vas Crabb <vas@vastheman.com>
2025-03-05 03:20:30 +1100

    posixfile.cpp: Pass in explcit alignment to operator new just in case.

--- src/osd/modules/file/posixfile.cpp
+++ src/osd/modules/file/posixfile.cpp
@@ -380,7 +380,11 @@ std::unique_ptr<osd::directory::entry> osd_stat(const std::string &path)
 
 	// create an osd_directory_entry; be sure to make sure that the caller can
 	// free all resources by just freeing the resulting osd_directory_entry
-	auto const result = reinterpret_cast<osd::directory::entry *>(::operator new(sizeof(osd::directory::entry) + path.length() + 1, std::nothrow));
+	auto const result = reinterpret_cast<osd::directory::entry *>(
+			::operator new(
+				sizeof(osd::directory::entry) + path.length() + 1,
+				std::align_val_t(alignof(osd::directory::entry)),
+				std::nothrow));
 	if (!result) return nullptr;
 	new (result) osd::directory::entry;
 
