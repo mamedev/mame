41712d2c5b812aeeeaabff3c5287332b143320ca
cam900 <dbtlrchl@naver.com>
2025-03-07 14:57:01 +0900

    shared/seibusound.cpp: Make coin IO port optional (for removal nmk16.cpp bootlegs unused IO region), Cleanups:
    - Fix typename for boolean flags
    - Fix typename consistency
    - Make some variables constant
    - Fix spacing
    - Fix naming of Z80 banked area

--- src/mame/seibu/airraid.cpp
+++ src/mame/seibu/airraid.cpp
@@ -284,7 +284,7 @@ void airraid_state::sound_map(address_map &map)
 	map(0x4008, 0x4009).rw(m_seibu_sound, FUNC(seibu_sound_device::ym_r), FUNC(seibu_sound_device::ym_w));
 	map(0x4010, 0x4011).r(m_seibu_sound, FUNC(seibu_sound_device::soundlatch_r));
 	map(0x4012, 0x4012).r(m_seibu_sound, FUNC(seibu_sound_device::main_data_pending_r));
-	map(0x4013, 0x4013).portr("COIN");
+	map(0x4013, 0x4013).r(m_seibu_sound, FUNC(seibu_sound_device::coin_r));
 	map(0x4018, 0x4019).w(m_seibu_sound, FUNC(seibu_sound_device::main_data_w));
 	map(0x401b, 0x401b).w(m_seibu_sound, FUNC(seibu_sound_device::coin_w));
 	map(0x8000, 0xffff).rom();
@@ -449,6 +449,7 @@ void airraid_state::airraid(machine_config &config)
 
 	SEIBU_SOUND(config, m_seibu_sound, 0);
 	m_seibu_sound->int_callback().set_inputline(m_audiocpu, 0);
+	m_seibu_sound->coin_io_callback().set_ioport("COIN");
 	m_seibu_sound->set_rom_tag("audiocpu");
 	m_seibu_sound->ym_read_callback().set("ymsnd", FUNC(ym2151_device::read));
 	m_seibu_sound->ym_write_callback().set("ymsnd", FUNC(ym2151_device::write));
