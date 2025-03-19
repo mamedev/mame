b51f4b947bd07e46cb04b302259124c802c5651a
angelosa <lordkale4@gmail.com>
2025-03-19 14:08:45 +0100

    video/tea1002.cpp: fix misspelled amplitude variable

--- src/devices/video/tea1002.cpp
+++ src/devices/video/tea1002.cpp
@@ -18,7 +18,7 @@
 
 const float tea1002_device::m_luminance[] =
 {
-		0, 22.5, 44, 66.5,  8.5, 31, 52.5, 100, // INV = 0
+	 0, 22.5, 44, 66.5,  8.5, 31, 52.5, 100, // INV = 0
 	75, 52.5, 31,  8.5, 66.5, 44, 22.5, 0    // INV = 1
 };
 
@@ -28,7 +28,7 @@ const int tea1002_device::m_phase[] =
 	0, 283,  61, 347, 167, 241, 103, 0  // INV = 1
 };
 
-const int tea1002_device::m_amplitute[] =
+const int tea1002_device::m_amplitude[] =
 {
 	0, 48, 44, 33, 33, 44, 48, 0, // INV = 0
 	0, 24, 22, 17, 17, 22, 24, 0  // INV = 1
@@ -47,7 +47,7 @@ DEFINE_DEVICE_TYPE(TEA1002, tea1002_device, "tea1002", "Mullard TEA1002 PAL colo
 //**************************************************************************
 
 //-------------------------------------------------
-//  paula_device - constructor
+//  tea1002_device - constructor
 //-------------------------------------------------
 
 tea1002_device::tea1002_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) :
@@ -74,8 +74,8 @@ rgb_t tea1002_device::color(int index)
 {
 	// calculate yuv
 	double y = m_luminance[index] / 100;
-	double u = cos((m_phase[index] + m_tint) * M_PI / 180) * m_amplitute[index] / 100;
-	double v = sin((m_phase[index] + m_tint) * M_PI / 180) * m_amplitute[index] / 100;
+	double u = cos((m_phase[index] + m_tint) * M_PI / 180) * m_amplitude[index] / 100;
+	double v = sin((m_phase[index] + m_tint) * M_PI / 180) * m_amplitude[index] / 100;
 
 	// and convert to rgb
 	double r = y + v * 1.14;
