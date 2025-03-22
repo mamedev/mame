7f771a00d67a3561c3f5175fe178e8e4d7f927e1
Ivan Vangelista <mesgnet@yahoo.it>
2025-03-22 16:10:04 +0100

    Update m50dass.h: include guards

--- src/mame/sega/m50dass.h
+++ src/mame/sega/m50dass.h
@@ -1,25 +1,23 @@
 // license:BSD-3-Clause
 // copyright-holders:
 
-#ifndef MAME_SEGA_MEGALO50_DASS_H
-#define MAME_SEGA_MEGALO50_DASS_H
+#ifndef MAME_SEGA_M50DASS_H
+#define MAME_SEGA_M50DASS_H
 
 #pragma once
 
 #include "cpu/z80/z80.h"
 #include "sound/ymopl.h"
 
-DECLARE_DEVICE_TYPE(MEGALO50_DASS, m50dass_device)
-
 class m50dass_device : public device_t
 {
 public:
 	m50dass_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock = 0);
-	void m50dass(machine_config &config);
+	void m50dass(machine_config &config) ATTR_COLD;
 
 protected:
-	virtual void device_start() override;
-	virtual void device_add_mconfig(machine_config &config) override;
+	virtual void device_start() override ATTR_COLD;
+	virtual void device_add_mconfig(machine_config &config) override ATTR_COLD;
 	virtual const tiny_rom_entry *device_rom_region() const override ATTR_COLD;
 
 private:
@@ -27,4 +25,6 @@ private:
 	required_device<ym2413_device> m_ym2413;
 };
 
-#endif // MAME_SEGA_MEGALO50_DASS_H
+DECLARE_DEVICE_TYPE(MEGALO50_DASS, m50dass_device)
+
+#endif // MAME_SEGA_M50DASS_H
