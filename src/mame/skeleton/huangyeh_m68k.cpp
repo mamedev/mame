0a489460d898bb6e17ec66763407d021ce70b3ac
flama12333 <143599905+flama12333@users.noreply.github.com>
2025-04-02 17:11:21 -0500

    add debug breakpoint notes. huangyeh_m68k.cpp
    suggested by angelosa.

--- src/mame/skeleton/huangyeh_m68k.cpp
+++ src/mame/skeleton/huangyeh_m68k.cpp
@@ -7,6 +7,11 @@ TODO:
 - OKI bank (standard 0/0x40000 layout);
 - I/Os;
 
+wlzb debug Breakpoint: 
+1 Open a debugger session
+2 focus maincpu
+3 bp 3c9d2,1,{curpc+=2;g} (patches main CPU to skip ACRTC tight loop with FIFO status)
+4 F5 or F12 (starts running)
 ===================================================================================================
 
 Wu Lin Zhengba, Huang Yeh, 1999?
