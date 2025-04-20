7864c26e17496d944640b55621e9634530cd3bc8
Olivier Galibert <galibert@pobox.com>
2025-04-21 01:37:39 +0200

    Try helping the CI

--- 3rdparty/bgfx/3rdparty/dear-imgui/imgui_widgets.cpp
+++ 3rdparty/bgfx/3rdparty/dear-imgui/imgui_widgets.cpp
@@ -2426,6 +2426,8 @@ TYPE ImGui::RoundScalarWithFormatT(const char* format, ImGuiDataType data_type, 
     return v;
 }
 
+template float ImGui::RoundScalarWithFormatT(const char* format, ImGuiDataType data_type, float v);
+
 //-------------------------------------------------------------------------
 // [SECTION] Widgets: DragScalar, DragFloat, DragInt, etc.
 //-------------------------------------------------------------------------
