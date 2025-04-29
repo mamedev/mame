4c85b8421077fb973db025cd6831a23b237c66da
m1macrophage <168948267+m1macrophage@users.noreply.github.com>
2025-04-28 21:56:37 -0700

    moog/source.cpp: emulating envelope generators and LFO timing.

--- src/devices/sound/va_eg.h
+++ src/devices/sound/va_eg.h
@@ -17,6 +17,16 @@
 // - Start decay:           rc_eg.set_r(decay_r).set_target_v(sustain_v);
 // - Start release:         rc_eg.set_r(release_r).set_target_v(0);
 //
+// The set_*() methods expect a monotonically increasing machine time, and will
+// assert()-fail if that's not the case.
+// If a single instance is used by multiple CPUs, this requirement can be met by
+// calling set_*() from within timer callbacks invoked with:
+// machine().scheduler().synchronize(timer_expired_delegate(...)).
+// See src/mame/moog/source.cpp for examples.
+//
+// Depending on the details of the multi-CPU system being emulated, additional
+// synchronization mitigations may be warranted, such as add_quantum(),
+// set_perfect_quantum(), etc.
 class va_rc_eg_device : public device_t, public device_sound_interface
 {
 public:
