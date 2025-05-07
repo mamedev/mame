91ae97a03a55b97abadb54b087d95732658feb0d
m1macrophage <168948267+m1macrophage@users.noreply.github.com>
2025-05-07 01:50:41 -0700

    moog/source.cpp: Moved netlist declaration to nl_source.h. (#13680)

---
+++ src/mame/moog/nl_source.h
@@ -0,0 +1,12 @@
+// license:BSD-3-Clause
+// copyright-holders:m1macrophage
+#ifndef MAME_MOOG_NL_SOURCE_H
+#define MAME_MOOG_NL_SOURCE_H
+
+#pragma once
+
+#include "netlist/nl_setup.h"
+
+NETLIST_EXTERNAL(moogsource)
+
+#endif // MAME_MOOG_NL_SOURCE_H
