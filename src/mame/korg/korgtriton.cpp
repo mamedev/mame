ed8c5639d2c854a9d9eeafea09a330cb92e5af8d
hap <happppp@users.noreply.github.com>
2025-06-20 20:18:16 +0200

    New clones marked not working ----------------------------- Triton Music Workstation/Sampler (v2.5.3) [hap] Triton Music Workstation/Sampler (v2.5.0) [hap]

--- src/mame/korg/korgtriton.cpp
+++ src/mame/korg/korgtriton.cpp
@@ -57,17 +57,17 @@ private:
 	required_device<screen_device> m_screen;
 	required_device<i8251_device> m_scu;
 
-	u8 m_lcdcio[SCREEN_WIDTH / 8 * SCREEN_HEIGHT]; // 4 bytes per pixel
+	u8 m_lcdcio[SCREEN_WIDTH / 8 * SCREEN_HEIGHT] = { }; // 4 bytes per pixel
 
 	// SCU: hack to provide data that makes the software proceed,
 	//      instead of using the actual device, see also comment
 	//      in map() below
-	int m_scu_hack_status_idx;
-	int m_scu_hack_data_idx;
+	u8 m_scu_hack_status_idx = 0;
+	u8 m_scu_hack_data_idx = 0;
 
-	u16 m_tgl[0x1000];
+	u16 m_tgl[0x1000] = { };
 
-	int m_moss_hack_idx;
+	u8 m_moss_hack_idx = 0;
 
 	void map(address_map &map) ATTR_COLD;
 
@@ -150,11 +150,11 @@ void korgtriton_state::korgtriton(machine_config &config)
 	m_maincpu->write_porte().set(FUNC(korgtriton_state::pe_w));
 
 	// 320x240 screen, should be a simple framebuffer at LCDCIO
-	SCREEN(config, m_screen, SCREEN_TYPE_RASTER);
-	m_screen->set_refresh_hz(15);
+	SCREEN(config, m_screen, SCREEN_TYPE_LCD);
+	m_screen->set_refresh_hz(60);
 	m_screen->set_vblank_time(ATTOSECONDS_IN_USEC(0));
 	m_screen->set_size(SCREEN_WIDTH, SCREEN_HEIGHT);
-	m_screen->set_visarea(0, SCREEN_WIDTH - 1, 0, SCREEN_HEIGHT - 1);
+	m_screen->set_visarea_full();
 	m_screen->set_screen_update(FUNC(korgtriton_state::screen_update));
 
 	I8251(config, m_scu, 1021800);
@@ -232,7 +232,7 @@ u8 korgtriton_state::scu_r(offs_t offs)
 	if (offs == 0)
 	{
 		const static u8 data[] = { 0x66, 0x31 };
-		res = data[m_scu_hack_data_idx % 2];
+		res = data[m_scu_hack_data_idx & 1];
 		if (!machine().side_effects_disabled())
 			m_scu_hack_data_idx++;
 	}
@@ -292,21 +292,45 @@ void korgtriton_state::moss_w(offs_t offs, u8 data)
 {
 }
 
-// the following rom images are taken from the Triton OS 2.0.0 floppy disks
-// the romfiles are missing the first two bytes (checksum) present in the
-// files obtainable from floppy disks online.
+// The following rom images are taken from the Triton OS floppy disks.
+// The ROM files are missing the first two bytes (checksum) present in
+// the files obtainable from floppy disks online.
 
 ROM_START( korgtriton )
 	ROM_REGION( 0x00800000, "maincpu", 0 )
+	ROM_LOAD("int01092.710", 0x00000000, 0x00008000, CRC(135bfd09) SHA1(8e57c6d8460801ebcce22310cecf90eb6a807099))
+	ROM_LOAD("int13092.710", 0x00008000, 0x00018000, CRC(d1b8ce69) SHA1(7cff969ed4c663c3aca89014dd776e780b23d81e))
 
+	ROM_LOAD("ext03092.710", 0x00400000, 0x000c0000, CRC(84166ac3) SHA1(e53be4deb982621365463f9f90b20e55160d1c19))
+	ROM_LOAD("ext34092.710", 0x004c0000, 0x00100000, CRC(bd949d24) SHA1(509461138549921d1e3b4e266f81b9dbd5d71ade))
+	ROM_LOAD("ext74092.710", 0x005c0000, 0x00100000, CRC(e50c0186) SHA1(def100dc06dc05dd09ba41524f2843f1e778c504))
+	ROM_LOAD("extb1092.710", 0x006c0000, 0x00040000, CRC(d49503f8) SHA1(47280c1cd423e1046cca31fd125cdae54c46b8f1))
+ROM_END
+
+ROM_START( korgtritona )
+	ROM_REGION( 0x00800000, "maincpu", 0 )
+	ROM_LOAD("int01088.710", 0x00000000, 0x00008000, CRC(135bfd09) SHA1(8e57c6d8460801ebcce22310cecf90eb6a807099))
+	ROM_LOAD("int13088.710", 0x00008000, 0x00018000, CRC(d1b8ce69) SHA1(7cff969ed4c663c3aca89014dd776e780b23d81e))
+
+	ROM_LOAD("ext03088.710", 0x00400000, 0x000c0000, CRC(ceb89b44) SHA1(63b03113bf355b59fdd8c19c2d00410f9def3289))
+	ROM_LOAD("ext34088.710", 0x004c0000, 0x00100000, CRC(eb16fc15) SHA1(b8b08668c9bc5407838974b09d9357762909c12e))
+	ROM_LOAD("ext74088.710", 0x005c0000, 0x00100000, CRC(c6c4bc87) SHA1(9dd42877b989653fe1fd4afae8240dc2c9852fae))
+	ROM_LOAD("extb1088.710", 0x006c0000, 0x00040000, CRC(dd234fd3) SHA1(80e315a95932ed5cb079f65c018b8b9188f9eaa9))
+ROM_END
+
+ROM_START( korgtritonb )
+	ROM_REGION( 0x00800000, "maincpu", 0 )
 	ROM_LOAD("int01080.710", 0x00000000, 0x00008000, CRC(ef0d7b3a) SHA1(5b20814dbd118df746437a5aa0b9bc371b8bada0))
 	ROM_LOAD("int13080.710", 0x00008000, 0x00018000, CRC(295bd0f8) SHA1(5f21cbaa95d9622aa0cfcf8b453d8c98b5dc54cd))
 
 	ROM_LOAD("ext03080.710", 0x00400000, 0x000c0000, CRC(12e681b1) SHA1(fb3299cffd8fc11ccbf5d7228e1d35f8a824579c))
 	ROM_LOAD("ext34080.710", 0x004c0000, 0x00100000, CRC(a84ea53c) SHA1(c949bef347f0cf02a16a6a332aeedcff31c6b956))
 	ROM_LOAD("ext74080.710", 0x005c0000, 0x00100000, CRC(281cef3b) SHA1(f348fb53f2ce954fda8004d310566bec663374f4))
+	ROM_LOAD("extb1080.710", 0x006c0000, 0x00040000, CRC(83fdb6dd) SHA1(c0b2573484bbdc5261e1aece42f995e8700a55ab))
 ROM_END
 
 } // anonymous namespace
 
-SYST(1999, korgtriton, 0, 0, korgtriton,  korgtriton, korgtriton_state, empty_init, "Korg", "TRITON", MACHINE_NO_SOUND | MACHINE_NOT_WORKING )
+SYST(1999, korgtriton,  0,          0, korgtriton, korgtriton, korgtriton_state, empty_init, "Korg", "Triton Music Workstation/Sampler (v2.5.3)", MACHINE_NO_SOUND | MACHINE_NOT_WORKING )
+SYST(1999, korgtritona, korgtriton, 0, korgtriton, korgtriton, korgtriton_state, empty_init, "Korg", "Triton Music Workstation/Sampler (v2.5.0)", MACHINE_NO_SOUND | MACHINE_NOT_WORKING )
+SYST(1999, korgtritonb, korgtriton, 0, korgtriton, korgtriton, korgtriton_state, empty_init, "Korg", "Triton Music Workstation/Sampler (v2.0.0)", MACHINE_NO_SOUND | MACHINE_NOT_WORKING )
