d1bf2657ba18f2422d212de1676a31912f93671f
Vas Crabb <vas@vastheman.com>
2025-06-27 07:31:26 +1000

    Fix copy/paste error breaking build, and some space indents.

--- src/osd/modules/sound/js_sound.cpp
+++ src/osd/modules/sound/js_sound.cpp
@@ -119,7 +119,7 @@ private:
 
 #else // SDLMAME_EMSCRIPTEN
 
-namespace osd { namespace { MODULE_NOT_SUPPORTED(sound_wasapi, OSD_SOUND_PROVIDER, "js") } }
+namespace osd { namespace { MODULE_NOT_SUPPORTED(sound_js, OSD_SOUND_PROVIDER, "js") } }
 
 #endif // SDLMAME_EMSCRIPTEN
 
