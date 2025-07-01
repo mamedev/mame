fbb9fbb735dde792b7a9b13916ef2afeccb4a1ea
David Haywood <hazemamewip@hotmail.com>
2025-07-01 19:21:22 +0100

    fix date

--- src/mame/tvgames/elan_eu3a05.cpp
+++ src/mame/tvgames/elan_eu3a05.cpp
@@ -1076,7 +1076,7 @@ CONS( 2005, sudelan, 0,         0, elan_eu3a13_pal,  sudoku,   elan_eu3a13_state
 
 CONS( 2005, sudoku2p, 0,        0, elan_eu3a13_pal,  sudoku2p, elan_eu3a13_state, empty_init,  "<unknown>",  "Sudoku TV Game (PAL, 2 players)", MACHINE_NOT_WORKING ) // a pair of yellow controllers with 'TV Sudoku Awesome Puzzles' on their label
 
-CONS( 2005, rad_ftet, 0,        0, elan_eu3a13,      rad_ftet, elan_eu3a13_state, empty_init,  "Radica",  "Family Tetris", MACHINE_NOT_WORKING )
+CONS( 2006, rad_ftet, 0,        0, elan_eu3a13,      rad_ftet, elan_eu3a13_state, empty_init,  "Radica",  "Family Tetris", MACHINE_NOT_WORKING )
 // rad_ftet shows UK logo if set to PAL
 
 CONS( 200?, carlecfg, 0,        0, elan_eu3a13,      carlecfg, elan_eu3a13_state, empty_init,  "Excalibur Electronics",  "Carl Edwards' Chase For Glory", MACHINE_NOT_WORKING )
