f08fdf36ad1977ebe50a44e220ea96b99f50f5be
stonedDiscord <Tukz@gmx.de>
2025-07-01 20:16:09 +0200

    logging

--- src/devices/machine/i8256.cpp
+++ src/devices/machine/i8256.cpp
@@ -2,7 +2,7 @@
 #include "emu.h"
 #include "i8256.h"
 
-//#define VERBOSE 1
+#define VERBOSE 1
 #include "logmacro.h"
 
 DEFINE_DEVICE_TYPE(I8256, i8256_device, "intel_8256", "Intel 8256AH MULTIFUNCTION MICROPROCESSOR SUPPORT CONTROLLER")
@@ -70,6 +70,7 @@ void i8256_device::write(offs_t offset, u8 data)
 {
     // In the 8-bit mode, AD0-AD3 are used to select the proper register, while AD1-AD4 are used in the 16-bit mode.
     // AD4 in the 8-bit mote is ignored as an address, while AD0 in the 16-bit mode is used as a second chip select, active low.
+
     if (BIT(m_command1,CMD1_8086))
 	{
 		offset = offset >> 1;
@@ -77,13 +78,22 @@ void i8256_device::write(offs_t offset, u8 data)
         
 	u8 reg = offset & 0x0F;
 
+	LOG("I8256 Write %02x to %02x\n", data, reg);
+
 	switch (reg)
 	{
 		case REG_CMD1:
             m_command1 = data;
+			LOG("I8256 Character length: %u\n", 8-(m_command1 & 0xC0));
 			break;
 		case REG_CMD2:
             m_command2 = data;
+			LOG("I8256 Baud rate: %u\n", baudRates[m_command2 & 0x0F]);
+			if((clock() / sysclockDivider[(m_command2 & 0x30 >> 4)])!=1024000)
+			{
+				osd_printf_warning("I8256 Clock mismatch! Scale: %u\n", sysclockDivider[(m_command2 & 0x30 >> 4)]);
+				osd_printf_warning("I8256 Internal Clock should be 1024000, acutally is: %u\n", (clock() / sysclockDivider[(m_command2 & 0x30 >> 4)]));
+			}
 			break;
 		case REG_CMD3:
 			m_command3 = data;
@@ -93,7 +103,6 @@ void i8256_device::write(offs_t offset, u8 data)
 			break;
 		case REG_PORT1C:
             m_port1_control = data;
-			LOG("I8256 Port 1 Control Write: %02x\n", data);
 			break;
 		case REG_PORT1:
 			m_port1_int = data;
