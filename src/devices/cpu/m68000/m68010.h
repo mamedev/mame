34e5823210ca62322de509a0d35ea6382206b8a3
Adam <adam@light-up.co.uk>
2025-07-21 08:55:39 +0200

    - make derived class ctor protected

--- src/devices/cpu/m68000/m68010.h
+++ src/devices/cpu/m68000/m68010.h
@@ -9,6 +9,10 @@
 
 class m68010_device : public m68000_musashi_device
 {
+protected:
+	m68010_device(const machine_config &mconfig, const device_type type, const char *tag, device_t *owner, u32 clock);
+	m68010_device(const machine_config &mconfig, const device_type type, const char *tag, device_t *owner, u32 clock, address_map_constructor internal_map);
+
 public:
 	// construction/destruction
 	m68010_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
