68921b10c949f0179a5bd780889ee7e855bc31b0
hap <happppp@users.noreply.github.com>
2025-07-31 14:32:32 +0200

    k054539: add m_ prefix to class variables

--- src/devices/sound/k054539.h
+++ src/devices/sound/k054539.h
@@ -75,28 +75,28 @@ protected:
 
 private:
 	struct channel {
-		uint32_t pos;
-		uint32_t pfrac;
-		int32_t val;
-		int32_t pval;
+		uint32_t pos = 0;
+		uint32_t pfrac = 0;
+		int32_t val = 0;
+		int32_t pval = 0;
 	};
 
-	double voltab[256];
-	double pantab[0xf];
+	double m_voltab[256];
+	double m_pantab[0xf];
 
-	double gain[8];
-	uint8_t posreg_latch[8][3];
-	int flags;
+	double m_gain[8];
+	uint8_t m_posreg_latch[8][3];
+	int m_flags;
 
-	unsigned char regs[0x230];
-	std::unique_ptr<uint8_t []> ram;
-	int reverb_pos;
+	unsigned char m_regs[0x230];
+	std::unique_ptr<uint8_t []> m_ram;
+	int m_reverb_pos;
 
-	int32_t cur_ptr;
-	uint32_t rom_addr;
+	int32_t m_cur_ptr;
+	uint32_t m_rom_addr;
 
-	channel channels[8];
-	sound_stream *stream;
+	channel m_channels[8];
+	sound_stream *m_stream;
 
 	emu_timer *m_timer;
 	uint32_t m_timer_state;
