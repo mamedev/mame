ab6867ca1996a571dd3bdc99d92b95b176e804ad
Brice Onken <32155223+briceonk@users.noreply.github.com>
2025-08-09 10:31:57 -0700

    Fixes and refactoring are just starting

--- src/mame/sony/news_38xx.cpp
+++ src/mame/sony/news_38xx.cpp
@@ -67,7 +67,6 @@ public:
 		, m_serial(*this, "serial%u", 0U)
 		, m_scsibus(*this, "scsi%u", 0U)
 		, m_serial_irq(*this, "serial_irq")
-		, m_floppy_irq(*this, "floppy_irq")
 		, m_eprom(*this, "eprom")
 //      , m_vram(*this, "vram")
 //      , m_led(*this, "led%u", 0U)
@@ -95,25 +94,37 @@ public:
 protected:
 	u32 screen_update(screen_device &screen, bitmap_rgb32 &bitmap, rectangle const &cliprect);
 
-	u8 intst_r() { return m_intst; }
+	// INTST only has some of the IRQ status info
+	u8 intst_r() { return m_intst & 0x7f; } // TODO: distinguish between FDCDRQ and PERR since this reg only has PERR
 
-	enum irq_number : unsigned
+	enum class iop_irq_number : unsigned
 	{
-		SLOT   = 0,
-		CENTRO = 1,
-		FDC    = 2,
-		LANCE  = 3,
-		IPC    = 4,
-		SCSI1  = 5,
-		SCSI0  = 6,
-		PERR   = 7,
+		SLOT   = 0,   // Expansion I/O bus interrupts
+		PARALLEL = 1, // Centronics interface
+		FDC    = 2,   // FDCIRQ
+		LANCE  = 3,   // Ethernet controller interrupts
+		IPC    = 4,   // CPU and UBUS interrupts
+		SCSI1  = 5,   // SCSI bus 0 interrupts
+		SCSI0  = 6,   // SCSI bus 1 interrupts
+		PERR = 7  // Main memory parity error
 	};
-	template <irq_number Number> void irq_w(int state);
-	void int_check();
+	template <iop_irq_number Number> void irq_w(int state);
+	void int_check_iop();
+
+	enum class cpu_irq_number : unsigned
+	{
+		UBUS = 0,   // Interprocessor communication interrupt from UPU
+		IOP = 1,    // Interprocessor communication interrupt from IOP
+		TIMER = 2,  // 100Hz timer
+		FPA = 3,    // FPU interrupt
+		WRBERR = 4, // async bus write error
+		PERR = 5    // Main memory parity error
+	};
 
 	u32 bus_error_r();
 	void timer_w(u8 data);
 	void timer(s32 param);
+	void poweron_w(u8 data);
 
 	// devices
 //  required_device<r3000a_device> m_cpu;
@@ -132,7 +143,6 @@ protected:
 	required_device_array<nscsi_bus_device, 2> m_scsibus;
 
 	required_device<input_merger_device> m_serial_irq;
-	required_device<input_merger_device> m_floppy_irq;
 
 	required_region_ptr<u32> m_eprom;
 	//required_shared_ptr<u32> m_vram;
@@ -146,7 +156,7 @@ protected:
 	u8 m_inten = 0;
 
 	bool m_int_state[3]{};
-	bool m_scc_irq_state = 0;
+	bool m_scc_irq_state = false;
 };
 
 void news_38xx_state::machine_start()
@@ -190,91 +200,103 @@ void news_38xx_state::cpu_map(address_map &map)
 
 void news_38xx_state::iop_map(address_map &map)
 {
-	map(0xd8000000, 0xd803ffff).ram(); // iop ram
+	map.global_mask(0x3fffffff); // A29-A0 are connected
+	map(0x18000000, 0x1803ffff).ram(); // IOP ram
 
-	map(0xe0000000, 0xe000ffff).rom().region("eprom", 0);
+	map(0x20000000, 0x2000ffff).rom().region("eprom", 0).mirror(0x1fff0000);
 
-	map(0xe2000000, 0xe2000007).nopw(); // ignore for now
-	// 0xe2000000 // power control
-	map(0xe2000001, 0xe2000001).lw8([this](u8 data) { m_iop->space(0).install_ram(0x00000000, m_ram->mask(), 0xc0000000, m_ram->pointer()); }, "ram_enable");
-	map(0xe2000002, 0xe2000002).lw8([this](u8 data) { m_inten |= 0x80; }, "pie_w");
-	map(0xe2000003, 0xe2000003).w(FUNC(news_38xx_state::timer_w));
-	// 0xe2000004 // soft interrupt
-	// 0xe2000005 // ast interrupt
-	// 0xe2000006 // led 0
-	// 0xe2000007 // led 1
+	map(0x22000000, 0x22000000).w(FUNC(news_38xx_state::poweron_w));
+	map(0x22000001, 0x22000001).lw8([this](u8 data) { m_iop->space(0).install_ram(0x00000000, m_ram->mask(), 0x00000000, m_ram->pointer()); }, "ram_enable");
+	map(0x22000002, 0x22000002).lw8([this](u8 data) { m_inten |= 0x80; }, "pie_w");
+	map(0x22000003, 0x22000003).w(FUNC(news_38xx_state::timer_w));
+	// 0x22000004 // soft interrupt
+	// 0x22000005 // ast interrupt
+	// 0x22000006 // iopled 0
+	// 0x22000007 // iopled 1
 
-	map(0xe3000000, 0xe3000000).r(FUNC(news_38xx_state::intst_r));
-	// 0xe3800000 // inter-processor interrupt
+	// 0x22800000-0x2280001f Inter-processor Control Registers
+	// 0x22800000 CPUSTART -> Run the main CPU
+	// 0x22800000 HPUSTART -> Run the UPU (UBUS bus master, expansion slot A??)
+	// 0x22800008 IPINTCP  -> Send level 2 IRQ to CPU
+	// 0x22800009 IPINTHP  -> Send level 2 IRQ to UPU
+	// 0x22800010 IPENICP  -> Enable level 4 IRQ from CPU (writing 0 will not clear IRQ)
+	// 0x22800011 IPENIHP  -> Enable level 4 IRQ from UPU (writing 0 will not clear IRQ)
+	// 0x22800018 IPCLICP  -> Clear level 4 IRQ from CPU
+	// 0x22800019 IPCLIHP  -> Clear level 4 IRQ from UPU
 
-	map(0xe4000000, 0xe4000007).m(m_fdc, FUNC(n82077aa_device::map));
-	// 0xe4000105 // fdc psuedo-dma port
+	map(0x23000000, 0x23000000).r(FUNC(news_38xx_state::intst_r));
+	// 0x23800000 // inter-processor interrupt status -> bit 0 = CPU, bit 1 = UPU
 
-	// 0xe6040000 // centronics
-	map(0xe6040003, 0xe6040003).lw8([this](u8 data) { m_inten |= 0x02; }, "cie_w");
-	map(0xe6080000, 0xe6080007).m(m_scsi[0], FUNC(cxd1180_device::map));
-	map(0xe60c0000, 0xe60c0007).m(m_scsi[1], FUNC(cxd1180_device::map));
+	map(0x24000000, 0x24000007).m(m_fdc, FUNC(n82077aa_device::map));
+	map(0x24000105, 0x24000105).rw(m_fdc, FUNC(n82077aa_device::dma_r), FUNC(n82077aa_device::dma_w));
 
-	map(0xe6100000, 0xe6100007).m(m_hid, FUNC(news_hid_hle_device::map_68k));
-	map(0xe6140000, 0xe6140003).rw(m_scc, FUNC(z80scc_device::ab_dc_r), FUNC(z80scc_device::ab_dc_w));
-	map(0xe6180000, 0xe618000f).rw(m_rtc, FUNC(rtc62421_device::read), FUNC(rtc62421_device::write));
-	// 0xe61c0000 // beep frequency
+	// 0x26040000 // centronics data
+	// 0x26040001 centronics strobe
+	// 0x26040002 centronics IRQ clear
+	map(0x26040003, 0x26040003).lw8([this](u8 data) { m_inten |= 0x02; }, "cie_w");
 
-	map(0xe6200000, 0xe6203fff).lrw16(
+	map(0x26080000, 0x26080007).m(m_scsi[0], FUNC(cxd1180_device::map));
+	map(0x260c0000, 0x260c0007).m(m_scsi[1], FUNC(cxd1180_device::map));
+
+	map(0x26100000, 0x26100007).m(m_hid, FUNC(news_hid_hle_device::map_68k));
+	map(0x26140000, 0x26140003).rw(m_scc, FUNC(z80scc_device::ab_dc_r), FUNC(z80scc_device::ab_dc_w));
+	map(0x26180000, 0x2618000f).rw(m_rtc, FUNC(rtc62421_device::read), FUNC(rtc62421_device::write));
+	// 0x261c0000 // kb beep frequency LOWTONE 0x1 = low, 0x0 = high
+
+	map(0x26200000, 0x26203fff).lrw16(
 		[this](offs_t offset) { return m_net_ram[offset]; }, "net_ram_r",
 		[this](offs_t offset, u16 data, u16 mem_mask) { COMBINE_DATA(&m_net_ram[offset]); }, "net_ram_w");
-	// 0xe62c0000 // audio
+	// 0x262c0000 // audio
 
-	map(0xe6300000, 0xe6300003).rw(m_net, FUNC(am7990_device::regs_r), FUNC(am7990_device::regs_w));
-	// 0xe6340000 // park
-	// 0xe6380000 // divide fdd index pulse?
+	map(0x26300000, 0x26300003).rw(m_net, FUNC(am7990_device::regs_r), FUNC(am7990_device::regs_w));
+	// 0x26340000 // park status (SCC and parallel info, TODO: double-check that park has this)
+	// 0x26380000 // divide fdd index pulse for ECMA format (1 = 1/2x 0 = 1x)
 
-	map(0xe8000000, 0xe8000017).m(m_dma[0], FUNC(dmac_0266_device::map));
-	map(0xea000000, 0xea000017).m(m_dma[1], FUNC(dmac_0266_device::map));
+	map(0x28000000, 0x28000017).m(m_dma[0], FUNC(dmac_0266_device::map));
+	map(0x2a000000, 0x2a000017).m(m_dma[1], FUNC(dmac_0266_device::map));
 
-	map(0xec000000, 0xec0000ff).rom().region("idrom", 0);
-	map(0xec000100, 0xec000103).portr("SW1");
+	map(0x2c000000, 0x2c0000ff).rom().region("idrom", 0);
+	map(0x2c000100, 0x2c000103).portr("SW1");
 
-	map(0xee000000, 0xeeffffff).r(FUNC(news_38xx_state::bus_error_r));
+	map(0x2e000000, 0x2effffff).r(FUNC(news_38xx_state::bus_error_r)).mirror(0x10000000); // Expansion I/O and mirror
 }
 
 void news_38xx_state::iop_vector_map(address_map &map)
 {
-	map(0xfffffff3, 0xfffffff3).lr8(NAME([]() { return m68000_base_device::autovector(1); }));
-	map(0xfffffff5, 0xfffffff5).lr8(NAME([]() { return m68000_base_device::autovector(2); }));
-	map(0xfffffff7, 0xfffffff7).lr8(NAME([]() { return m68000_base_device::autovector(3); }));
-	map(0xfffffff9, 0xfffffff9).lr8(NAME([]() { return m68000_base_device::autovector(4); }));
-	map(0xfffffffb, 0xfffffffb).lr8(NAME([this]() { return m_scc_irq_state ? m_scc->m1_r() : m68000_base_device::autovector(5); }));
-	map(0xfffffffd, 0xfffffffd).lr8(NAME([]() { return m68000_base_device::autovector(6); }));
-	map(0xffffffff, 0xffffffff).lr8(NAME([]() { return m68000_base_device::autovector(7); }));
+	map(0xfffffff3, 0xfffffff3).lr8(NAME([] { return m68000_base_device::autovector(1); }));
+	map(0xfffffff5, 0xfffffff5).lr8(NAME([] { return m68000_base_device::autovector(2); }));
+	map(0xfffffff7, 0xfffffff7).lr8(NAME([] { return m68000_base_device::autovector(3); }));
+	map(0xfffffff9, 0xfffffff9).lr8(NAME([] { return m68000_base_device::autovector(4); }));
+	map(0xfffffffb, 0xfffffffb).lr8(NAME([this] { return m_scc_irq_state ? m_scc->m1_r() : m68000_base_device::autovector(5); }));
+	map(0xfffffffd, 0xfffffffd).lr8(NAME([] { return m68000_base_device::autovector(6); }));
+	map(0xffffffff, 0xffffffff).lr8(NAME([] { return m68000_base_device::autovector(7); }));
 }
 
-template <news_38xx_state::irq_number Number> void news_38xx_state::irq_w(int state)
+template <news_38xx_state::iop_irq_number Number> void news_38xx_state::irq_w(int state)
 {
-	LOG("irq number %d state %d\n",  Number, state);
+	LOG("irq number %d state %d\n",  static_cast<unsigned>(Number), state);
 
 	if (state)
-		m_intst |= 1U << Number;
+		m_intst |= 1U << static_cast<unsigned>(Number);
 	else
-		m_intst &= ~(1U << Number);
+		m_intst &= ~(1U << static_cast<unsigned>(Number));
 
-	int_check();
+	int_check_iop();
 }
 
-void news_38xx_state::int_check()
+void news_38xx_state::int_check_iop()
 {
-	// TODO: assume 74444333
-	static int const int_line[] = { INPUT_LINE_IRQ7, INPUT_LINE_IRQ4, INPUT_LINE_IRQ3 };
-	static u8 const int_mask[] = { 0x80, 0x78, 0x07 };
+	static int constexpr intst_line[] = { INPUT_LINE_IRQ7, INPUT_LINE_IRQ4, INPUT_LINE_IRQ3 };
+	static u8 constexpr intst_mask[] = { 0x80, 0x78, 0x07 };
 
 	for (unsigned i = 0; i < std::size(m_int_state); i++)
 	{
-		bool const int_state = m_intst & m_inten & int_mask[i];
+		bool const int_state = m_intst & m_inten & intst_mask[i];
 
 		if (m_int_state[i] != int_state)
 		{
 			m_int_state[i] = int_state;
-			m_iop->set_input_line(int_line[i], int_state);
+			m_iop->set_input_line(intst_line[i], int_state);
 		}
 	}
 }
@@ -303,7 +325,17 @@ void news_38xx_state::timer(s32 param)
 		m_iop->set_input_line(INPUT_LINE_IRQ6, ASSERT_LINE);
 }
 
-static void news_scsi_devices(device_slot_interface &device)
+void news_38xx_state::poweron_w(uint8_t data)
+{
+	LOG("Write POWERON = 0x%x (%s)\n", data, machine().describe_context());
+
+	if (!data && !machine().side_effects_disabled())
+	{
+		machine().schedule_exit();
+	}
+}
+
+void news_scsi_devices(device_slot_interface &device)
 {
 	device.option_add("harddisk", NSCSI_HARDDISK);
 	device.option_add("cdrom", NSCSI_CDROM);
@@ -360,16 +392,13 @@ void news_38xx_state::common(machine_config &config)
 	m_scc->out_txdb_callback().set(m_serial[1], FUNC(rs232_port_device::write_txd));
 
 	AM7990(config, m_net);
-	m_net->intr_out().set(FUNC(news_38xx_state::irq_w<LANCE>)).invert();
+	m_net->intr_out().set(FUNC(news_38xx_state::irq_w<iop_irq_number::LANCE>)).invert();
 	m_net->dma_in().set([this](offs_t offset) { return m_net_ram[offset >> 1]; });
 	m_net->dma_out().set([this](offs_t offset, u16 data, u16 mem_mask) { COMBINE_DATA(&m_net_ram[offset >> 1]); });
 
-	INPUT_MERGER_ANY_HIGH(config, m_floppy_irq);
-	m_floppy_irq->output_handler().set(FUNC(news_38xx_state::irq_w<FDC>));
-
-	N82077AA(config, m_fdc, 16_MHz_XTAL);
-	m_fdc->intrq_wr_callback().set(m_floppy_irq, FUNC(input_merger_any_high_device::in_w<0>));
-	m_fdc->drq_wr_callback().set(m_floppy_irq, FUNC(input_merger_any_high_device::in_w<1>));
+	N82077AA(config, m_fdc, 16_MHz_XTAL, n82077aa_device::mode_t::PS2);
+	m_fdc->intrq_wr_callback().set(FUNC(news_38xx_state::irq_w<iop_irq_number::FDC>));
+	m_fdc->drq_wr_callback().set(FUNC(news_38xx_state::irq_w<iop_irq_number::PERR>)); // TODO: don't just use PERR here
 	FLOPPY_CONNECTOR(config, "fdc:0", "35hd", FLOPPY_35_HD, true, floppy_image_device::default_pc_floppy_formats).enable_sound(false);
 
 	// scsi bus 0 and devices
@@ -386,9 +415,9 @@ void news_38xx_state::common(machine_config &config)
 	NSCSI_CONNECTOR(config, "scsi0:7").option_set("cxd1180", CXD1180).machine_config(
 		[this](device_t *device)
 		{
-			cxd1180_device &adapter = downcast<cxd1180_device &>(*device);
+			auto &adapter = downcast<cxd1180_device &>(*device);
 
-			adapter.irq_handler().set(*this, FUNC(news_38xx_state::irq_w<SCSI0>));
+			adapter.irq_handler().set(*this, FUNC(news_38xx_state::irq_w<iop_irq_number::SCSI0>));
 			adapter.irq_handler().append(m_dma[0], FUNC(dmac_0266_device::eop_w));
 			adapter.drq_handler().set(m_dma[0], FUNC(dmac_0266_device::req_w));
 
@@ -411,9 +440,9 @@ void news_38xx_state::common(machine_config &config)
 	NSCSI_CONNECTOR(config, "scsi1:7").option_set("cxd1180", CXD1180).machine_config(
 		[this](device_t *device)
 		{
-			cxd1180_device &adapter = downcast<cxd1180_device &>(*device);
+			auto &adapter = downcast<cxd1180_device &>(*device);
 
-			adapter.irq_handler().set(*this, FUNC(news_38xx_state::irq_w<SCSI1>));
+			adapter.irq_handler().set(*this, FUNC(news_38xx_state::irq_w<iop_irq_number::SCSI1>));
 			adapter.irq_handler().append(m_dma[1], FUNC(dmac_0266_device::eop_w));
 			adapter.drq_handler().set(m_dma[1], FUNC(dmac_0266_device::req_w));
 
