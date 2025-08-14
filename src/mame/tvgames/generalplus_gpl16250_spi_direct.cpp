96d3a1e7d712cfeaedeef348db02ee86fbeae0dc
David Haywood <hazemamewip@hotmail.com>
2025-08-14 21:48:16 +0100

    use official name from box

--- src/mame/tvgames/generalplus_gpl16250_spi_direct.cpp
+++ src/mame/tvgames/generalplus_gpl16250_spi_direct.cpp
@@ -297,7 +297,7 @@ CONS(2021, punij1pu, punirune, 0, generalplus_gpspi_direct, bfspyhnt, generalplu
 CONS(2021, punij2pk, punirune, 0, generalplus_gpspi_direct, bfspyhnt, generalplus_gpspi_direct_game_state, empty_init, "Takara Tomy", "Punirunes (PUNIRUNZU_MAIN_V2, pink, Japan)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
 
 // has a link feature
-CONS(2021, punifrnd, 0,        0, generalplus_gpspi_direct, bfspyhnt, generalplus_gpspi_direct_game_state, empty_init, "Takara Tomy", "Punirunes Friendship (hot pink, Japan)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
+CONS(2021, punifrnd, 0,        0, generalplus_gpspi_direct, bfspyhnt, generalplus_gpspi_direct_game_state, empty_init, "Takara Tomy", u8"Punirunes Punitomo Tsūshin (hot pink, Japan)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
 
 // Pokemon ガチッとゲットだぜ! モンスターボール 
 CONS(2021, pokgoget, 0,        0, generalplus_gpspi_direct, bfspyhnt, generalplus_gpspi_direct_game_state, empty_init, "Takara Tomy", "Pokemon Go! Get It! (210406, Japan)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
