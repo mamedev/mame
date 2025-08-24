0d4a67fcfacd762300bdd64bacbbc0975ba2450f
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2025-08-24 10:18:12 -0300

    Drafting serial port implementation for TMP94C241 that is
    used for control panel and MIDI on Technics KN5000.

---
+++ src/mame/matsushita/kn5000_cpanel.cpp
@@ -0,0 +1,88 @@
+// license:GPL2+
+// copyright-holders:Felipe Sanches
+/***************************************************************************
+
+	KN5000 control panel
+
+***************************************************************************/
+
+#include "emu.h"
+#include "kn5000_cpanel.h"
+
+#include "logmacro.h"
+
+DEFINE_DEVICE_TYPE(KN5000_CPANEL, kn5000_cpanel_device, "kn5000_cpanel", "KN5000 Control Panel")
+
+kn5000_cpanel_device::kn5000_cpanel_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) :
+	device_t(mconfig, KN5000_CPANEL, tag, owner, clock),
+	m_sck_cb(*this),
+	m_sck_in(1),
+	m_sck_out(1),
+	m_clock_count(0),
+	m_tx_shift_register(0),
+	m_rx_shift_register(0)
+{
+}
+
+void kn5000_cpanel_device::device_start()
+{
+	save_item(NAME(m_sck_in));
+	save_item(NAME(m_sck_in_prev));
+	save_item(NAME(m_sck_out));
+	save_item(NAME(m_serial_in));
+	save_item(NAME(m_clock_count));
+	save_item(NAME(m_tx_shift_register));
+	save_item(NAME(m_rx_shift_register));
+
+	m_sck_cb(m_sck_out);
+	sck_in(0);
+	serial_in(0);
+
+	update_serial();
+}
+
+void kn5000_cpanel_device::device_reset()
+{
+	update_serial();
+}
+
+void kn5000_cpanel_device::sck_in(int state)
+{
+	if (m_sck_in != state)
+	{
+		m_sck_in_prev = m_sck_in;
+		m_sck_in = state;
+
+		// logerror("sck_in state=%d\n", state);
+		update_serial();
+	}
+}
+
+void kn5000_cpanel_device::serial_in(int state)
+{
+	if (m_serial_in != state)
+	{
+		m_serial_in = state;
+
+//		logerror("serial_in state=%d\n", state);
+		update_serial();
+	}
+}
+
+
+void kn5000_cpanel_device::update_serial()
+{
+	if (m_sck_in != m_sck_in_prev && m_sck_in == 1)
+	{
+		m_rx_shift_register >>= 1;
+		m_rx_shift_register |= (m_serial_in << 7);
+
+		logerror("           rx bit #%d: %d  cur_value=%02X\n", m_clock_count, m_serial_in, m_rx_shift_register);
+
+		if (++m_clock_count == 8)
+		{
+			logerror("           Received: %02X\n", m_rx_shift_register);
+			m_clock_count = 0;
+		}
+	}
+}
