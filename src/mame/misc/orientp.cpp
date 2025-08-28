0321011319b203ad9019cbdc2d9666c95fecf3cf
flama12333 <143599905+flama12333@users.noreply.github.com>
2025-08-27 23:16:23 -0500

    add mcu dump and flag by bad_dump orientp.cpp
    dumped by my friend. notes
    The 5- or 6-ball pinball machines have too many PCB versions, with different ICs and programs. Those from Youal Jifh “YJ” are very different from those from Sheng Yang “SY”; their programs are different, as are their electronics. Youal Jifh probably uses the YM2413 for music, while Sheng Yang uses an additional EPROM for music.

--- src/mame/misc/orientp.cpp
+++ src/mame/misc/orientp.cpp
@@ -166,8 +166,7 @@ ROM_START( east8 )
 	ROM_LOAD( "w27c512.u33", 0x00000, 0x10000, CRC(8d3d1e91) SHA1(b80907df0878057a1ded8b56225059e06382b9d6) ) // Main program.  EAST8  v1.05 string
 
     ROM_REGION( 0x1000, "mcu", ROMREGION_ERASE00 )
-    ROM_LOAD( "at89s51.u39", 0x0000, 0x1000, NO_DUMP ) // Microcontroller Protection
-	
+    ROM_LOAD( "at89s51.u39", 0x0000, 0x1000, BAD_DUMP CRC(a55b63a8) SHA1(9ef88bba4a46ccd969d80882e9c36eb2f0c9e4bf) ) //  Microcontroller Protection. Read Unprotected, Wrong
     ROM_REGION( 0x40000, "oki", ROMREGION_ERASE00 )
     ROM_LOAD( "w27c020.bin", 0x00000, 0x40000, NO_DUMP ) //  Voices Rom
 
