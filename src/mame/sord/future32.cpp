d7bac115eaa0e20365ca517dbfd969d3ce716114
AJR <ariedlmayer@gmail.com>
2025-09-04 19:43:53 -0400

    Fix typos in 840d976ff39211f5c682ebb063762ebec77796e9

--- src/mame/sord/future32.cpp
+++ src/mame/sord/future32.cpp
@@ -77,7 +77,7 @@ private:
 	void tx_start();
 };
 
-DEFINE_DEVICE_TYPE(FUTURE32_KBD, future32_kbd_device, "future32)kbd", "Sord Future32 keyboard");
+DEFINE_DEVICE_TYPE(FUTURE32_KBD, future32_kbd_device, "future32_kbd", "Sord Future32 keyboard");
 
 future32_kbd_device::future32_kbd_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock)
 	: device_t(mconfig, FUTURE32_KBD, tag, owner, clock)
