05f3c4acefdef26a9585eeb801731d525f62e114
flama12333 <143599905+flama12333@users.noreply.github.com>
2025-09-10 00:14:49 -0500

    Update toysoldier.cpp

--- src/mame/misc/toysoldier.cpp
+++ src/mame/misc/toysoldier.cpp
@@ -7,7 +7,7 @@ known undumped games
 W&P CATCHER - lucky dog 
 https://youtu.be/e1Gp7Cr6KIQ
 
-Fantasy World - Diferent version.
+Fantasy World - set 2.
 https://youtu.be/I7l6sOW40gE
 seem to be used in lucky loco
 https://youtu.be/vCyPViEF5pk
@@ -27,11 +27,15 @@ https://youtu.be/aArV8y5TuzM?t=13
 
 amusements 
 https://youtu.be/qUiQgHpiAyI
+
 */
+
 #include "emu.h"
+
 #include "cpu/mcs51/mcs51.h"
 #include "sound/ay8910.h"
 #include "sound/ymopl.h"
+
 #include "speaker.h"
 
 namespace {
@@ -46,20 +50,20 @@ public:
 
 	void toysoldier(machine_config &config);
 
+protected:
+	virtual void machine_start() override;
+
 private:
 	void ay_port_a_w(uint8_t data);
 	void ay_port_b_w(uint8_t data);
 	void io_map(address_map &map);
 	void program_map(address_map &map);
 
-protected:
-	virtual void machine_start() override;
-
 };
 
 static INPUT_PORTS_START( toysoldier )
    
-PORT_START("IN1") //  Claw input 
+PORT_START("IN1") //  Claw input? 
 	PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_UNKNOWN )
 	PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_UNKNOWN )
 	PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_UNKNOWN )
@@ -123,12 +127,12 @@ void toysoldier_state::io_map(address_map &map)
 	map(0x9000, 0x9001).rw("ay", FUNC(ay8910_device::data_r), FUNC(ay8910_device::address_data_w));
     map(0xa000, 0xa000).portr("IN1"); //  claw up claw down return input
 //	map(0xa000, 0xa000).nopw(); // ???
-//  map(0xb000, 0xb000).nopw(); //  aplus ic
+//  map(0xb000, 0xb000).nopw(); //  aplus ic.
     map(0xb000, 0xb000).portr("IN2"); // joystick
 //	map(0xc000, 0xc000).nopw(); // ???
     map(0xc000, 0xc000).portr("IN3");  // Coin
-//  map(0xd000, 0xd000).unmapw(); // 7 segment display  control
-    map(0xe000, 0xe000).nopw(); // Super Card
+//  map(0xd000, 0xd000).unmapw(); // 7 segment display  control?
+//  map(0xe000, 0xe000).nopw(); // Super Card led
 }
 
 void toysoldier_state::machine_start()
@@ -145,25 +149,28 @@ void toysoldier_state::toysoldier(machine_config &config)
 	/* sound hardware */
 	SPEAKER(config, "mono").front_center();
 	
-    ay8910_device &ay(AY8910(config, "ay", XTAL(12'000'000) / 6));  // It’s clocked by the ALE output of the 8032 – it will be 1/6 of the 8032 clock frequency, with the issue that data external memory accesses cause it to drop pulses.
-    ay.add_route(ALL_OUTPUTS, "mono", 1.0);
+    ay8910_device &ay(AY8910(config, "ay", XTAL(12'000'000) / 6)); 
 	ay.port_a_read_callback().set_ioport("DSW1");
 	ay.port_b_read_callback().set_ioport("DSW2");
 	
 	ym2413_device &opll(YM2413(config, "opll", 3.579545_MHz_XTAL));
 	opll.add_route(ALL_OUTPUTS, "mono", 1.0);
+
 }
 
 ROM_START( toysoldier )
 	ROM_REGION( 0x10000, "maincpu", 0 )
 	ROM_LOAD( "27c512.u3", 0x00000, 0x10000, CRC(BD52C1AE) SHA1(a76c10f93f9087bc2d01bfe866c0e66a006b4ddd) )
-	ROM_REGION( 0x40000, "voice", 0 )
+  
+    ROM_REGION( 0x40000, "voice", 0 )
     ROM_LOAD( "api8108a.u8", 0x00000, 0x40000, NO_DUMP ) // api8108a voice rom
-	ROM_REGION( 0x200, "eeprom", 0 ) // according to diagram
+	
+    ROM_REGION( 0x200, "eeprom", 0 ) // according to diagram
 	ROM_LOAD( "93c66.u39", 0x000, 0x200, NO_DUMP ) // probably for storing internal settings.
+
 ROM_END
 
 } // anonymous namespace
 
-//    YEAR  NAME    PARENT   MACHINE   INPUT   STATE         INIT        ROT   COMPANY      FULLNAME                                                FLAGS
-GAME( 2003, toysoldier, 0, toysoldier, toysoldier, toysoldier_state, empty_init, ROT0, "Paokai", "Toy Soldier (Coastal Amusements)", MACHINE_MECHANICAL | MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND ) // This was dumped from an Coastal branded Toy Soldier from around 2003.
+//    YEAR  NAME        PARENT  MACHINE      INPUT        STATE             INIT        ROT   COMPANY   FULLNAME                            FLAGS
+GAME( 2003, toysoldier, 0,      toysoldier,  toysoldier,  toysoldier_state, empty_init, ROT0, "Paokai", "Toy Soldier (Coastal Amusements)", MACHINE_MECHANICAL | MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND ) // This was dumped from an Coastal branded Toy Soldier around 2003.
