848d751bc90370cf3c1fa1cef12f396c7a323e53
flama12333 <143599905+flama12333@users.noreply.github.com>
2025-09-10 21:14:26 -0500

    Update orientp.cpp

--- src/mame/misc/orientp.cpp
+++ src/mame/misc/orientp.cpp
@@ -369,11 +369,11 @@ ROM_START( unk6bp )
 } // anonymous namespace
 
 
-// unsorted: in which board are from? Dump unverified for now.
-//    YEAR  NAME            PARENT   MACHINE   INPUT  STATE   INIT        ROT   COMPANY      FULLNAME                         FLAGS
+// unsorted: in which board are from? Dump Not verified for now.
+//    YEAR  NAME             PARENT    MACHINE  INPUT  STATE   INIT        ROT   COMPANY      FULLNAME                        FLAGS
 GAME( 199?, east8v100,   0,  orientp,  east8,   orientp_state, empty_init, ROT0, "<unknown>", "Unknown EAST8 (v1.00)",        MACHINE_NOT_WORKING | MACHINE_UNEMULATED_PROTECTION | MACHINE_IMPERFECT_SOUND | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK ) // error 10
-GAME( 199?, east8v105,   0,  orientp,  east8,   orientp_state, empty_init, ROT0, "<unknown>", "Unknown EAST8 (v1.05)",        MACHINE_NOT_WORKING | MACHINE_UNEMULATED_PROTECTION | MACHINE_IMPERFECT_SOUND | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK ) // 
-GAME( 199?, east8v105a,  0,  orientp,  east8,   orientp_state, empty_init, ROT0, "<unknown>", "Unknown EAST8 (v1.05) set 1",  MACHINE_NOT_WORKING | MACHINE_UNEMULATED_PROTECTION | MACHINE_IMPERFECT_SOUND | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK ) // 
-GAME( 199?, east8v105b,  0,  orientp,  east8,   orientp_state, empty_init, ROT0, "<unknown>", "Unknown EAST8 (v1.05) set 2",  MACHINE_NOT_WORKING | MACHINE_UNEMULATED_PROTECTION | MACHINE_IMPERFECT_SOUND | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK ) //
-GAME( 199?, unk6bp,      0,  orientp,  east8,   orientp_state, empty_init, ROT0, "<unknown>", "Unknown 6 Ball Pinball",       MACHINE_NOT_WORKING | MACHINE_UNEMULATED_PROTECTION | MACHINE_IMPERFECT_SOUND | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK ) // No String. no 7 segemnt display output. bad dump?
+GAME( 199?, east8v105,   0,  orientp,  east8,   orientp_state, empty_init, ROT0, "<unknown>", "Unknown EAST8 (v1.05)",        MACHINE_NOT_WORKING | MACHINE_UNEMULATED_PROTECTION | MACHINE_IMPERFECT_SOUND | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK ) // error 10
+GAME( 199?, east8v105a,  0,  orientp,  east8,   orientp_state, empty_init, ROT0, "<unknown>", "Unknown EAST8 (v1.05) set 1",  MACHINE_NOT_WORKING | MACHINE_UNEMULATED_PROTECTION | MACHINE_IMPERFECT_SOUND | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK ) // error 10
+GAME( 199?, east8v105b,  0,  orientp,  east8,   orientp_state, empty_init, ROT0, "<unknown>", "Unknown EAST8 (v1.05) set 2",  MACHINE_NOT_WORKING | MACHINE_UNEMULATED_PROTECTION | MACHINE_IMPERFECT_SOUND | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK ) // error 10
+GAME( 199?, unk6bp,      0,  orientp,  east8,   orientp_state, empty_init, ROT0, "<unknown>", "Unknown 6 Ball Pinball",       MACHINE_NOT_WORKING | MACHINE_UNEMULATED_PROTECTION | MACHINE_IMPERFECT_SOUND | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK ) // No String. Error sound but no display post. bad dump?
 
