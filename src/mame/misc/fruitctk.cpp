c7dba74f4f1d94e44fd440f0a58835d2644f3516
flama12333 <143599905+flama12333@users.noreply.github.com>
2025-10-02 01:47:33 -0500

    spaced at 329 fruitctk.cpp

--- src/mame/misc/fruitctk.cpp
+++ src/mame/misc/fruitctk.cpp
@@ -325,10 +325,6 @@ ROM_START( fruitctk )
     ROM_END
 
 
-
 } // anonymous namespace
-
-
-//   YEAR   NAME    PARENT   MACHINE      INPUT     STATE           INIT        ROT    COMPANY       FULLNAME                 FLAGS
-GAME( 2002?, fruitctk,   0,  fruitctk,     fruitctk, fruitctk_state, empty_init, ROT0,  "<unknown>", "Fruit Cocktail",  MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND )
-
+//    YEAR    NAME        PARENT   MACHINE       INPUT      STATE            INIT        ROT    COMPANY        FULLNAME            FLAGS
+GAME( 2002?,  fruitctk,   0,       fruitctk,     fruitctk,  fruitctk_state,  empty_init, ROT0,  "<unknown>",  "Fruit Cocktail",    MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND )
