db4e8e6abee9c3ea3344aa0aa108affae13b474d
cam900 <dbtlrchl@naver.com>
2025-10-02 20:53:47 +0900

    seibu/raiden2.cpp: Minor cleanups: - Fix bankswitching in reset - Simplify CPU bankswitching method - Use logmacro.h for logging - Fix save state supports - Reduce preprocessor defines - Make some variables constant - Use generic gfx decode layout - Fix CPU ROM region size - Use BIT macro for single bit values - Reduce runtime tag lookups - Fix namings - Fix spacings - Fix filenames - Add notes
    seibu/r2dx_v33.cpp: Minor cleanups:
    - Split driver states per configurations
    - Fix save state support
    - Fix bankswitching in reset
    - Use BIT macro for single bit value
    - Use hexadecimal value for address value
    - Reduce runtime tag lookups
    - Add ROMREGION_ERASEFF for regions to prevent possible transparency issue
    - Fix spacings
    - Fix namings
    
    seibu/seibu_helper.cpp: Split partial_carry_sum into seibu_helper.cpp for reduce including unnecessary functions
    
    seibu/r2crypt.cpp: Allow variable ROM region and size in sprite ROM decryption functions
    
    seibu/seibuspi_m.cpp, seibu/seibuspi_v.cpp: Fix spacings
    
    seibu/legionna.cpp: Fix naming

---
+++ src/mame/seibu/seibu_helper.h
@@ -0,0 +1,7 @@
+// license:BSD-3-Clause
+// copyright-holders:Ville Linde, Nicola Salmoria
+
+u32 partial_carry_sum(u32 add1, u32 add2, u32 carry_mask, int bits);
+
+u32 partial_carry_sum32(u32 add1, u32 add2, u32 carry_mask);
+u32 partial_carry_sum24(u32 add1, u32 add2, u32 carry_mask);
