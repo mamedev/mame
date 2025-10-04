ba361e8550f1212d7364f37fb67f950783b91c26
Roman Donchenko <rdonchen@outlook.com>
2025-10-04 21:11:42 +0300

    formats/ap2_dsk.cpp: remove the RWTS18 format (#14262)
    It is completely non-functional. The loading code is commented out,
    and the saving code crashes, because it attempts to access 70 tracks
    regardless of how many there actually are.
    
    The code has been in this state ever since it's been added in 36ea482a,
    so it seems unlikely that it'll improve. Moreover, its utility seems
    questionable: I haven't been able to find a single floppy image with an .rti
    extension (and there are none listed in the software list). OTOH, for every
    game mentioned in the description, there are WOZ images that can be used
    instead (except for The Toy Shop, which has DSK images).

--- src/lib/formats/ap2_dsk.h
+++ src/lib/formats/ap2_dsk.h
@@ -123,28 +123,6 @@ public:
 extern const a2_16sect_dos_format FLOPPY_A216S_DOS_FORMAT;
 extern const a2_16sect_prodos_format FLOPPY_A216S_PRODOS_FORMAT;
 
-class a2_rwts18_format : public floppy_image_format_t
-{
-public:
-	a2_rwts18_format();
-
-	virtual int identify(util::random_read &io, uint32_t form_factor, const std::vector<uint32_t> &variants) const override;
-	virtual bool load(util::random_read &io, uint32_t form_factor, const std::vector<uint32_t> &variants, floppy_image &image) const override;
-	virtual bool save(util::random_read_write &io, const std::vector<uint32_t> &variants, const floppy_image &image) const override;
-
-	virtual const char *name() const noexcept override;
-	virtual const char *description() const noexcept override;
-	virtual const char *extensions() const noexcept override;
-	virtual bool supports_save() const noexcept override;
-
-private:
-	static const desc_e mac_gcr[];
-
-	static uint8_t gb(const std::vector<bool> &buf, int &pos, int &wrap);
-};
-
-extern const a2_rwts18_format FLOPPY_RWTS18_FORMAT;
-
 
 class a2_edd_format : public floppy_image_format_t
 {
