a03386095849c175cff953b8bf21018d03e78197
Vas Crabb <cuavas@users.noreply.github.com>
2025-10-12 03:11:12 +1100

    gaelco/splash.h: More cold.

--- src/mame/gaelco/splash.h
+++ src/mame/gaelco/splash.h
@@ -5,12 +5,14 @@
 
 #pragma once
 
+#include "machine/74259.h"
 #include "machine/eepromser.h"
 #include "machine/gen_latch.h"
-#include "machine/74259.h"
 #include "sound/msm5205.h"
+
 #include "emupal.h"
 #include "tilemap.h"
+
 
 class splash_state : public driver_device
 {
@@ -33,36 +35,13 @@ public:
 
 	void splash(machine_config &config) ATTR_COLD;
 
-	void init_splash10();
-	void init_splash();
+	void init_splash10() ATTR_COLD;
+	void init_splash() ATTR_COLD;
 
 protected:
 	virtual void machine_start() override ATTR_COLD;
 	virtual void machine_reset() override ATTR_COLD;
 	virtual void video_start() override ATTR_COLD;
-
-	required_device<cpu_device> m_maincpu;
-	required_device<cpu_device> m_audiocpu;
-	optional_device_array<msm5205_device, 2> m_msm;
-	required_device<gfxdecode_device> m_gfxdecode;
-	required_device<palette_device> m_palette;
-	required_device<generic_latch_8_device> m_soundlatch;
-	optional_device<ls259_device> m_outlatch;
-
-	required_shared_ptr<uint16_t> m_pixelram;
-	required_shared_ptr<uint16_t> m_videoram;
-	required_shared_ptr<uint16_t> m_vregs;
-	required_shared_ptr<uint16_t> m_spriteram;
-
-	optional_memory_bank m_sound_bank;
-
-	// driver init configuration
-	int m_sprite_attr2_shift = 0;
-
-	tilemap_t *m_bg_tilemap[2]{};
-
-	// splash specific
-	uint8_t m_adpcm_data = 0;
 
 	// common
 	void vram_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
@@ -87,6 +66,29 @@ protected:
 	void funystrp_sound_map(address_map &map) ATTR_COLD;
 	void splash_map(address_map &map) ATTR_COLD;
 	void splash_sound_map(address_map &map) ATTR_COLD;
+
+	required_device<cpu_device> m_maincpu;
+	required_device<cpu_device> m_audiocpu;
+	optional_device_array<msm5205_device, 2> m_msm;
+	required_device<gfxdecode_device> m_gfxdecode;
+	required_device<palette_device> m_palette;
+	required_device<generic_latch_8_device> m_soundlatch;
+	optional_device<ls259_device> m_outlatch;
+
+	required_shared_ptr<uint16_t> m_pixelram;
+	required_shared_ptr<uint16_t> m_videoram;
+	required_shared_ptr<uint16_t> m_vregs;
+	required_shared_ptr<uint16_t> m_spriteram;
+
+	optional_memory_bank m_sound_bank;
+
+	// driver init configuration
+	int m_sprite_attr2_shift = 0;
+
+	tilemap_t *m_bg_tilemap[2]{};
+
+	// splash specific
+	uint8_t m_adpcm_data = 0;
 };
 
 class roldfrog_state : public splash_state
@@ -99,8 +101,8 @@ public:
 
 	void roldfrog(machine_config &config) ATTR_COLD;
 
-	void init_roldfrog();
-	void init_rebus();
+	void init_roldfrog() ATTR_COLD;
+	void init_rebus() ATTR_COLD;
 
 protected:
 	virtual void machine_start() override ATTR_COLD;
@@ -109,6 +111,13 @@ protected:
 	virtual void draw_bitmap(bitmap_ind16 &bitmap, const rectangle &cliprect) override;
 
 private:
+	INTERRUPT_GEN_MEMBER(roldfrog_interrupt);
+	void roldfrog_update_irq();
+
+	void roldfrog_map(address_map &map) ATTR_COLD;
+	void roldfrog_sound_io_map(address_map &map) ATTR_COLD;
+	void roldfrog_sound_map(address_map &map) ATTR_COLD;
+
 	required_shared_ptr<uint16_t> m_bitmap_mode;
 
 	//roldfrog specific
@@ -121,13 +130,6 @@ private:
 	void roldfrog_vblank_ack_w(uint8_t data);
 	uint8_t roldfrog_unk_r();
 	void ym_irq(int state);
-
-	INTERRUPT_GEN_MEMBER(roldfrog_interrupt);
-	void roldfrog_update_irq();
-
-	void roldfrog_map(address_map &map) ATTR_COLD;
-	void roldfrog_sound_io_map(address_map &map) ATTR_COLD;
-	void roldfrog_sound_map(address_map &map) ATTR_COLD;
 };
 
 class funystrp_state : public splash_state
@@ -144,8 +146,8 @@ public:
 	void funystrp(machine_config &config) ATTR_COLD;
 	void ringball(machine_config &config) ATTR_COLD;
 
-	void init_funystrp();
-	void init_ringball();
+	void init_funystrp() ATTR_COLD;
+	void init_ringball() ATTR_COLD;
 
 protected:
 	virtual void machine_start() override ATTR_COLD;
