fd616af62ad1e854dfb2a36b3bef1eddec53c491
m1macrophage <168948267+m1macrophage@users.noreply.github.com>
2025-10-11 13:00:38 -0700

    luaengine_input.cpp: Export minvalue and maxvalue for IPT_ADJUSTER. (#14317)

--- src/frontend/mame/luaengine_input.cpp
+++ src/frontend/mame/luaengine_input.cpp
@@ -395,12 +395,14 @@ void lua_engine::initialize_input(sol::table &emu)
 	ioport_field_type["minvalue"] = sol::property(
 			[] (ioport_field const &f)
 			{
-				return f.is_analog() ? std::make_optional(f.minval()) : std::nullopt;
+				const bool has_minmax = f.is_analog() || f.type() == IPT_ADJUSTER;
+				return has_minmax ? std::make_optional(f.minval()) : std::nullopt;
 			});
 	ioport_field_type["maxvalue"] = sol::property(
 			[] (ioport_field const &f)
 			{
-				return f.is_analog() ? std::make_optional(f.maxval()) : std::nullopt;
+				const bool has_minmax = f.is_analog() || f.type() == IPT_ADJUSTER;
+				return has_minmax ? std::make_optional(f.maxval()) : std::nullopt;
 			});
 	ioport_field_type["sensitivity"] = sol::property(
 			[] (ioport_field const &f)
