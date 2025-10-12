21cabc8cfedc89bb05b8bd87412db8bbca9d8bcb
stonedDiscord <Tukz@gmx.de>
2025-10-12 19:33:52 +0200

    io map

--- src/mame/skeleton/digijet.cpp
+++ src/mame/skeleton/digijet.cpp
@@ -28,14 +28,25 @@
 	|________________________|
 
 	Connector
+	1  RPM
+	2  Coolant temperture
 	3  GND
+	4  Throttle switch
+	5  Lambda sensor
 	6  GND
 	7  GND
 	9  GND
 	10 GND
+	12 Injectors
+	13 Ignition
+	14 Air in temperature
+	15 Air amount
 	16 GND
 	17 GND
 	18 GND
+	19 Air sensor power
+	20 Fuel pump
+	22 GND
 
 **************************************************************************/
 
@@ -63,6 +74,7 @@ public:
 	}
 
 	void digijet(machine_config &config);
+	void digijet90(machine_config &config);
 
 private:
 	required_device<cpu_device> m_maincpu;
@@ -70,10 +82,20 @@ private:
 	virtual void machine_start() override { }
 	virtual void machine_reset() override { }
 	void io_map(address_map &map) ATTR_COLD;
+	void io_map90(address_map &map) ATTR_COLD;
 };
 
 void digijet_state::io_map(address_map &map)
 {
+	map(0x32,0x32).noprw();;
+	map(0x33,0x33).nopr();;
+	map(0x3f,0x3f).noprw();;
+}
+
+void digijet_state::io_map90(address_map &map)
+{
+	map(0x37,0x37).noprw();;
+	map(0x38,0x38).nopr();;
 }
 
 static INPUT_PORTS_START( digijet )
@@ -86,12 +108,19 @@ void digijet_state::digijet(machine_config &config)
 	m_maincpu->set_addrmap(AS_IO, &digijet_state::io_map);
 }
 
+void digijet_state::digijet90(machine_config &config)
+{
+	/* basic machine hardware */
+	I8049(config, m_maincpu, XTAL(7'372'800));
+	m_maincpu->set_addrmap(AS_IO, &digijet_state::io_map90);
+}
+
 ROM_START( digijet )
 	ROM_REGION( 0x800, I8049_TAG, 0 )
 	ROM_LOAD( "vanagon_85_usa_ca.bin", 0x000, 0x800, CRC(2ed7c4c5) SHA1(ae48d8892b44fe76b48bcefd293c15cd47af3fba) ) // Volkswagen Vanagon, 1985, USA, California
 ROM_END
 
-ROM_START( digijet2 )
+ROM_START( digijet90 )
 	ROM_REGION( 0x2000, I8049_TAG, 0 )
 	ROM_LOAD( "fabb05_03_03.bin", 0x0000, 0x2000, CRC(8c96bcdf) SHA1(73b26914cd15ca3a5e0d7427de9ce4b4e311fb00) ) // Volkswagen 1990, Germany
 ROM_END
@@ -99,6 +128,6 @@ ROM_END
 } // anonymous namespace
 
 
-//    YEAR  NAME       PARENT     COMPAT  MACHINE  INPUT    CLASS          INIT        COMPANY       FULLNAME          FLAGS
-CONS( 1985, digijet,   digijet90, 0,      digijet, digijet, digijet_state, empty_init, "Volkswagen", "Digijet",        MACHINE_NOT_WORKING | MACHINE_NO_SOUND_HW )
-CONS( 1990, digijet90, 0,         0,      digijet, digijet, digijet_state, empty_init, "Volkswagen", "Digijet (1990)", MACHINE_NOT_WORKING | MACHINE_NO_SOUND_HW )
+//    YEAR  NAME       PARENT     COMPAT  MACHINE    INPUT    CLASS          INIT        COMPANY       FULLNAME          FLAGS
+CONS( 1985, digijet,   digijet90, 0,      digijet,   digijet, digijet_state, empty_init, "Volkswagen", "Digijet",        MACHINE_NOT_WORKING | MACHINE_NO_SOUND_HW )
+CONS( 1990, digijet90, 0,         0,      digijet90, digijet, digijet_state, empty_init, "Volkswagen", "Digijet (1990)", MACHINE_NOT_WORKING | MACHINE_NO_SOUND_HW )
