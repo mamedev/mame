7b2ad1b8d38b1e2b47a0caa09a4533fc574c0f76
Andrei Holub <andrei.holub@gmail.com>
2025-10-19 12:48:43 -0400

    separate dehrust

---
+++ src/mame/sinclair/spec_snqk_hrust.h
@@ -0,0 +1,9 @@
+// license:BSD-3-Clause
+// copyright-holders:HalfElf, Andrei I. Holub
+
+#ifndef MAME_SINCLAIR_SPEC_SNQK_HRUST_H
+#define MAME_SINCLAIR_SPEC_SNQK_HRUST_H
+
+#pragma once
+
+#endif // MAME_SINCLAIR_SPEC_SNQK_HRUST_H
