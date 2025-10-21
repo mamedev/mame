6292a4dd6ecd46bc7a675d62014b0f38ea8d95a5
David Haywood <hazemamewip@hotmail.com>
2025-10-21 13:08:40 +0100

    added Retro TV Game Console - Disney Cars - 300 Games (JG7800DC-1)

--- src/mame/nintendo/nes_vt369_vtunknown.cpp
+++ src/mame/nintendo/nes_vt369_vtunknown.cpp
@@ -634,12 +634,17 @@ ROM_END
 
 ROM_START( rtvgc300fz )
 	ROM_REGION( 0x8000000, "mainrom", 0 )
-	// some of the higher address lines might be swapped
 	ROM_LOAD( "jg7800fz.bin", 0x00000, 0x4000000, CRC(c9d319d2) SHA1(9d0d1435b802f63ce11b94ce54d11f4065b324cc) )
 
 	VT3XX_INTERNAL_NO_SWAP // not verified for this set, used for testing
 ROM_END
 
+ROM_START( rtvgc300cr )
+	ROM_REGION( 0x8000000, "mainrom", 0 )
+	ROM_LOAD( "jg7800dc-1.u6", 0x00000, 0x4000000, CRC(a21af365) SHA1(8af1370ccfd79c34de39d14c53438246519b16ba) )
+
+	VT3XX_INTERNAL_NO_SWAP // not verified for this set, used for testing
+ROM_END
 
 ROM_START( dgun2561 ) // all games selectable
 	ROM_REGION( 0x4000000, "mainrom", 0 )
@@ -1215,6 +1220,7 @@ CONS( 2012, dgun2561,  0,  0,  vt36x_32mb_2banks_lexi, vt369, vt36x_state, empty
 // GB-NO13-Main-VT389-2 on PCBs - uses higher resolution mode (twice usual h-res?)
 CONS( 2016, rtvgc300,  0,  0,  vt36x_32mb_2banks_lexi300, vt369, vt36x_state, empty_init,    "Lexibook", "Retro TV Game Console - 300 Games", MACHINE_NOT_WORKING )
 CONS( 2017, rtvgc300fz,0,  0,  vt36x_32mb_2banks_lexi300, vt369, vt36x_state, empty_init,    "Lexibook", "Retro TV Game Console - Frozen - 300 Games", MACHINE_NOT_WORKING )
+CONS( 2017, rtvgc300cr,0,  0,  vt36x_32mb_2banks_lexi300, vt369, vt36x_state, empty_init,    "Lexibook", "Retro TV Game Console - Disney Cars - 300 Games (JG7800DC-1)", MACHINE_NOT_WORKING )
 
 
 /* The following are also confirmed to be NES/VT derived units, most having a standard set of games with a handful of lazy graphic mods thrown in to fit the unit theme
@@ -1230,7 +1236,6 @@ CONS( 2017, rtvgc300fz,0,  0,  vt36x_32mb_2banks_lexi300, vt369, vt36x_state, em
     Lexibook Console Colour - Barbie
 
     (units for use with TV)
-    Lexibook Retro TV Game Console (300 Games) - Cars
     Lexibook Retro TV Game Console (300 Games) - PJ Masks
 
     (more?)
