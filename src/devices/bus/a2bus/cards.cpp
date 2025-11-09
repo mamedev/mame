b6dbdb8eea2f4ad225376a79000eec31fe3798f5
AJR <ariedlmayer@gmail.com>
2025-11-08 19:55:25 -0500

    bus/a2bus: Allow ROM cards to be loaded into slots other than 0 on apple2/apple2p/apple2e

--- src/devices/bus/a2bus/cards.cpp
+++ src/devices/bus/a2bus/cards.cpp
@@ -157,6 +157,9 @@ void apple2_cards(device_slot_interface &device)
 	device.option_add("vistaa800", A2BUS_VISTAA800);           // Vista A800 8" Disk Controller Card
 	device.option_add("wicotrackball", A2BUS_WICOTRACKBALL);   // Wico Trackball
 	device.option_add("ieee488", A2BUS_IEEE488);               // Apple II IEEE-488 Interface Card
+	device.option_add("romcard", A2BUS_ROMCARDUSER);           // Apple II ROM Card that loads a custom ROM image
+	device.option_add("romcardfp", A2BUS_ROMCARDFP);           // Apple II ROM Card with Autostart Monitor + Applesoft BASIC
+	device.option_add("romcardint", A2BUS_ROMCARDINT);         // Apple II ROM Card with Autostart Monitor + Integer BASIC
 }
 
 void apple2e_cards(device_slot_interface &device)
@@ -240,6 +243,8 @@ void apple2e_cards(device_slot_interface &device)
 	device.option_add("vistaa800", A2BUS_VISTAA800);           // Vista A800 8" Disk Controller Card
 	device.option_add("wicotrackball", A2BUS_WICOTRACKBALL);   // Wico Trackball
 	device.option_add("ieee488", A2BUS_IEEE488);               // Apple II IEEE-488 Interface Card
+	device.option_add("romcard", A2BUS_ROMCARDUSER);           // Apple II ROM Card that loads a custom ROM image
+	device.option_add("romcardint", A2BUS_ROMCARDINT);         // Apple II ROM Card with Autostart Monitor + Integer BASIC
 }
 
 void apple2gs_cards(device_slot_interface &device)
