da07d0b3e975a23fc7949675dbe819ee22e08099
AJR <ariedlmayer@gmail.com>
2025-11-10 14:54:30 -0500

    a2softcard: Hold the Z80 in reset rather than halting it

--- src/devices/bus/a2bus/a2softcard.cpp
+++ src/devices/bus/a2bus/a2softcard.cpp
@@ -79,14 +79,14 @@ void a2bus_softcard_device::reset_from_bus()
 	m_bEnabled = false;
 
 	m_FirstZ80Boot = true;
-	m_z80->set_input_line(INPUT_LINE_HALT, ASSERT_LINE);
+	m_z80->set_input_line(INPUT_LINE_RESET, ASSERT_LINE);
 }
 
 void a2bus_softcard_device::write_cnxx(uint8_t offset, uint8_t data)
 {
 	if (!m_bEnabled)
 	{
-		m_z80->set_input_line(INPUT_LINE_HALT, CLEAR_LINE);
+		m_z80->set_input_line(INPUT_LINE_RESET, CLEAR_LINE);
 		raise_slot_dma();
 
 		if (m_FirstZ80Boot)
@@ -99,7 +99,7 @@ void a2bus_softcard_device::write_cnxx(uint8_t offset, uint8_t data)
 	}
 	else
 	{
-		m_z80->set_input_line(INPUT_LINE_HALT, ASSERT_LINE);
+		m_z80->set_input_line(INPUT_LINE_RESET, ASSERT_LINE);
 		lower_slot_dma();
 		m_bEnabled = false;
 	}
