8fcded3ffdb0090913b88ab08e97ff20509038b2
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2025-11-17 13:45:42 -0300

    [patinho] No need for a header file here.

--- src/mame/usp/patinho_feio.cpp
+++ src/mame/usp/patinho_feio.cpp
@@ -1,20 +1,72 @@
 // license:GPL-2.0+
 // copyright-holders:Felipe Sanches
 /*
-    Patinho Feio
+    Patinho Feio - University of São Paulo, 1972
 */
 
 #include "emu.h"
 #include "bus/generic/slot.h"
 #include "bus/generic/carts.h"
-#include "softlist.h"
 #include "cpu/patinhofeio/patinhofeio_cpu.h"
-#include "patinhofeio.h"
+#include "teleprinter.h"
+#include "softlist.h"
+
 #include "patinho.lh"
 
-/*
-    driver init function
-*/
+class patinho_feio_state : public driver_device {
+public:
+	patinho_feio_state(const machine_config &mconfig, device_type type, const char *tag)
+		: driver_device(mconfig, type, tag)
+		, m_maincpu(*this, "maincpu")
+		, m_decwriter(*this, "decwriter")
+		, m_tty(*this, "teletype")
+		, m_mode_button(*this, "MODE_BUTTON%u", 0U)
+	{ }
+
+	void init_patinho_feio();
+
+	void decwriter_data_w(uint8_t data);
+	void decwriter_kbd_input(u8 data);
+	TIMER_CALLBACK_MEMBER(decwriter_callback);
+
+	void teletype_data_w(uint8_t data);
+	void teletype_kbd_input(u8 data);
+	TIMER_CALLBACK_MEMBER(teletype_callback);
+
+	DECLARE_DEVICE_IMAGE_LOAD_MEMBER( tape_load );
+
+	void update_panel(uint8_t ACC, uint8_t opcode, uint8_t mem_data, uint16_t mem_addr, uint16_t PC, uint8_t FLAGS, uint16_t RC, uint8_t mode);
+
+	void patinho_feio(machine_config &config);
+protected:
+	virtual void machine_start() override ATTR_COLD;
+
+	void load_tape(const char* name);
+	void load_raw_data(const char* name, unsigned int start_address, unsigned int data_length);
+
+	required_device<patinho_feio_cpu_device> m_maincpu;
+	required_device<teleprinter_device> m_decwriter;
+	required_device<teleprinter_device> m_tty;
+
+private:
+	output_finder<6> m_mode_button;
+	uint8_t* paper_tape_data = nullptr;
+	uint32_t paper_tape_length = 0;
+	uint32_t paper_tape_address = 0;
+
+	emu_timer *m_decwriter_timer = nullptr;
+	emu_timer *m_teletype_timer = nullptr;
+	output_manager *m_out = nullptr;
+	uint8_t m_prev_ACC = 0;
+	uint8_t m_prev_opcode = 0;
+	uint8_t m_prev_mem_data = 0;
+	uint16_t m_prev_mem_addr = 0;
+	uint16_t m_prev_PC = 0;
+	uint8_t m_prev_FLAGS = 0;
+	uint16_t m_prev_RC = 0;
+};
+
+
 void patinho_feio_state::init_patinho_feio()
 {
 	m_out = &output();
