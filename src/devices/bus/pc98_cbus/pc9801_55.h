e53a4fc1f8d2922b50417f59313011bb6f5ebed9
angelosa <lordkale4@gmail.com>
2025-12-01 13:08:05 +0100

    bus/pc98_cbus/lha201.cpp: add enough remapping so it can coexist with base IDE BIOS

--- src/devices/bus/pc98_cbus/pc9801_55.h
+++ src/devices/bus/pc98_cbus/pc9801_55.h
@@ -51,23 +51,24 @@ protected:
 	virtual void dack_w(int line, u8 data) override;
 
 	virtual void internal_map(address_map &map) ATTR_COLD;
+	virtual void io_map(address_map &map) ATTR_COLD;
 
 	address_space_config m_space_io_config;
 
-private:
 	required_device<nscsi_bus_device> m_scsi_bus;
 	required_device<wd33c9x_base_device> m_wdc;
 	required_memory_region m_bios;
 	required_ioport m_dsw1;
 	required_ioport m_dsw2;
 
-	void io_map(address_map &map) ATTR_COLD;
+	u8 m_rom_bank;
+	int m_int_line;
+private:
 	void increment_addr();
 
 	u8 m_ar;
 	u8 m_port30;
 	u8 m_pkg_id;
-	u8 m_rom_bank;
 	bool m_dma_enable;
 };
 
