68057665e001bde004b3cd878e9dcc0c77842caf
vtarchive <lvespongebobfan@proton.me>
2025-12-24 18:50:38 +0100

    Update vtech_eu3a12.cpp

--- src/mame/vtech/vtech_eu3a12.cpp
+++ src/mame/vtech/vtech_eu3a12.cpp
@@ -4,6 +4,7 @@
 // CPU die (epoxy blob) is an Elan EU3A12 (Elan "RISC II Series" quasi-PIC with 16-bit opcodes)
 
 /*
+
 VTech Laptops on Elan EU3A12 hardware
 
 Other known undumped international versions (and possibly on similar hardware):
@@ -12,12 +13,17 @@ Genius Notebook
 Nitro Jams Notebook (US version of Musical Laptop)
 Nitro Jr. Notebook (without low battery screen)
 Nitro Jr. Notebook (with low battery splash)
+Genius Notebook (2005)
+Challenger Laptop (US version of Genius Notebook)
 Nitro Notebook (US version of Reader Laptop E)
 Nitro Web Notebook (US Web connected version of Reader Laptop E) 
 Manege Laptop (Dutch version of Reader Laptop E)
 Language Lab Laptop
 Cars 2: Lightning McQueen™ Learning Laptop
-Advance Xtra/Xtra Pink Notebook (UK version of Reader Laptop E, Advance Xtra is the blue variant, Xtra Pink Notebook is the pink variant)
+Cars 2: Lightning McQueen Learn and Go
+Advance Xtra/Xtra Pink Notebook (UK version of Reader Laptop E, Advance Xtra is the blue variant, Xtra Pink Notebook is the pink variant and they all share the same ROM)
+
+*/
 
 #include "emu.h"
 #include "cpu/rii/riscii.h"
