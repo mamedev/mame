678558d5c6fd79b573aa2e56208e1d94554b334c
cam900 <dbtlrchl@naver.com>
2025-12-29 01:40:14 +0900

    seibu/raiden2_v.cpp: Draw sprites using priority for xsedae (no blending). (#14731)
    seibu/raiden2.cpp: Moved systems with blending to a separate state class, reduced code duplication.

--- src/mame/seibu/raiden2_v.cpp
+++ src/mame/seibu/raiden2_v.cpp
@@ -9,12 +9,14 @@
 #include "emu.h"
 #include "raiden2.h"
 
-void raiden2_state::m_videoram_private_w(offs_t offset, u16 data)
+#define DUMP_COP (0)
+
+void xsedae_state::m_videoram_private_w(offs_t offset, u16 data)
 {
-	//  map(0x0d000, 0x0d7ff).ram().w(FUNC(raiden2_state::background_w)).share("back_data");
-	//  map(0x0d800, 0x0dfff).ram().w(FUNC(raiden2_state::foreground_w).share("fore_data");
-	//  map(0x0e000, 0x0e7ff).ram().w(FUNC(raiden2_state::midground_w).share("mid_data");
-	//  map(0x0e800, 0x0f7ff).ram().w(FUNC(raiden2_state::text_w).share("text_data");
+	//  map(0x0d000, 0x0d7ff).ram().w(FUNC(xsedae_state::background_w)).share("back_data");
+	//  map(0x0d800, 0x0dfff).ram().w(FUNC(xsedae_state::foreground_w).share("fore_data");
+	//  map(0x0e000, 0x0e7ff).ram().w(FUNC(xsedae_state::midground_w).share("mid_data");
+	//  map(0x0e800, 0x0f7ff).ram().w(FUNC(xsedae_state::text_w).share("text_data");
 
 	if (offset < 0x800 / 2)
 	{
@@ -38,7 +40,7 @@ void raiden2_state::m_videoram_private_w(offs_t offset, u16 data)
 }
 
 
-void raiden2_state::background_w(offs_t offset, u16 data)
+void xsedae_state::background_w(offs_t offset, u16 data)
 {
 	if (m_back_data[offset] != data)
 	{
@@ -47,7 +49,7 @@ void raiden2_state::background_w(offs_t offset, u16 data)
 	}
 }
 
-void raiden2_state::midground_w(offs_t offset, u16 data)
+void xsedae_state::midground_w(offs_t offset, u16 data)
 {
 	if (m_mid_data[offset] != data)
 	{
@@ -56,7 +58,7 @@ void raiden2_state::midground_w(offs_t offset, u16 data)
 	}
 }
 
-void raiden2_state::foreground_w(offs_t offset, u16 data)
+void xsedae_state::foreground_w(offs_t offset, u16 data)
 {
 	if (m_fore_data[offset] != data)
 	{
@@ -65,7 +67,7 @@ void raiden2_state::foreground_w(offs_t offset, u16 data)
 	}
 }
 
-void raiden2_state::text_w(offs_t offset, u16 data)
+void xsedae_state::text_w(offs_t offset, u16 data)
 {
 	if (m_text_data[offset] != data)
 	{
@@ -74,12 +76,12 @@ void raiden2_state::text_w(offs_t offset, u16 data)
 	}
 }
 
-void raiden2_state::tilemap_enable_w(offs_t offset, u16 data, u16 mem_mask)
+void xsedae_state::tilemap_enable_w(offs_t offset, u16 data, u16 mem_mask)
 {
 	COMBINE_DATA(&m_tilemap_enable);
 }
 
-void raiden2_state::tile_scroll_w(offs_t offset, u16 data, u16 mem_mask)
+void xsedae_state::tile_scroll_w(offs_t offset, u16 data, u16 mem_mask)
 {
 	tilemap_t *tm = nullptr;
 	switch (offset / 2)
@@ -154,7 +156,7 @@ void raiden2_state::raidendx_cop_bank_2_w(offs_t offset, u16 data, u16 mem_mask)
 }
 
 
-TILE_GET_INFO_MEMBER(raiden2_state::get_back_tile_info)
+TILE_GET_INFO_MEMBER(xsedae_state::get_back_tile_info)
 {
 	u32 tile = m_back_data[tile_index];
 	const u32 color = (tile >> 12) | (0 << 4);
@@ -164,7 +166,7 @@ TILE_GET_INFO_MEMBER(raiden2_state::get_back_tile_info)
 	tileinfo.set(1, tile, color, 0);
 }
 
-TILE_GET_INFO_MEMBER(raiden2_state::get_mid_tile_info)
+TILE_GET_INFO_MEMBER(xsedae_state::get_mid_tile_info)
 {
 	u32 tile = m_mid_data[tile_index];
 	const u32 color = (tile >> 12) | (2 << 4);
@@ -174,7 +176,7 @@ TILE_GET_INFO_MEMBER(raiden2_state::get_mid_tile_info)
 	tileinfo.set(1, tile, color, 0);
 }
 
-TILE_GET_INFO_MEMBER(raiden2_state::get_fore_tile_info)
+TILE_GET_INFO_MEMBER(xsedae_state::get_fore_tile_info)
 {
 	u32 tile = m_fore_data[tile_index];
 	const u32 color = (tile >> 12) | (1 << 4);
@@ -184,7 +186,7 @@ TILE_GET_INFO_MEMBER(raiden2_state::get_fore_tile_info)
 	tileinfo.set(1, tile, color, 0);
 }
 
-TILE_GET_INFO_MEMBER(raiden2_state::get_text_tile_info)
+TILE_GET_INFO_MEMBER(xsedae_state::get_text_tile_info)
 {
 	u32 tile = m_text_data[tile_index];
 	const u32 color = (tile >> 12) & 0xf;
@@ -194,11 +196,8 @@ TILE_GET_INFO_MEMBER(raiden2_state::get_text_tile_info)
 	tileinfo.set(0, tile, color, 0);
 }
 
-void raiden2_state::video_start()
+void xsedae_state::common_video_start()
 {
-	m_screen->register_screen_bitmap(m_tile_bitmap);
-	m_screen->register_screen_bitmap(m_sprite_bitmap);
-
 	m_back_data = make_unique_clear<u16[]>(0x800/2);
 	m_fore_data = make_unique_clear<u16[]>(0x800/2);
 	m_mid_data = make_unique_clear<u16[]>(0x800/2);
@@ -212,10 +211,46 @@ void raiden2_state::video_start()
 	save_pointer(NAME(m_text_data), 0x1000/2);
 	save_pointer(NAME(m_palette_data), 0x1000/2);
 
-	m_text_layer       = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(*this, FUNC(raiden2_state::get_text_tile_info)), TILEMAP_SCAN_ROWS,  8, 8, 64,32 );
-	m_background_layer = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(*this, FUNC(raiden2_state::get_back_tile_info)), TILEMAP_SCAN_ROWS, 16,16, 32,32 );
-	m_midground_layer  = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(*this, FUNC(raiden2_state::get_mid_tile_info)),  TILEMAP_SCAN_ROWS, 16,16, 32,32 );
-	m_foreground_layer = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(*this, FUNC(raiden2_state::get_fore_tile_info)), TILEMAP_SCAN_ROWS, 16,16, 32,32 );
+	m_text_layer       = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(*this, FUNC(xsedae_state::get_text_tile_info)), TILEMAP_SCAN_ROWS,  8, 8, 64,32);
+	m_background_layer = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(*this, FUNC(xsedae_state::get_back_tile_info)), TILEMAP_SCAN_ROWS, 16,16, 32,32);
+	m_midground_layer  = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(*this, FUNC(xsedae_state::get_mid_tile_info)),  TILEMAP_SCAN_ROWS, 16,16, 32,32);
+	m_foreground_layer = &machine().tilemap().create(*m_gfxdecode, tilemap_get_info_delegate(*this, FUNC(xsedae_state::get_fore_tile_info)), TILEMAP_SCAN_ROWS, 16,16, 32,32);
+}
+
+void xsedae_state::video_start()
+{
+	common_video_start();
+
+	m_text_layer->set_transparent_pen(15);
+	m_background_layer->set_transparent_pen(15); // TODO: Opaque?
+	m_midground_layer->set_transparent_pen(15);
+	m_foreground_layer->set_transparent_pen(15);
+}
+
+void raiden2_state::video_start()
+{
+	common_video_start();
+	m_screen->register_screen_bitmap(m_tile_bitmap);
+	m_screen->register_screen_bitmap(m_sprite_bitmap);
+
+	save_item(NAME(m_tile_bitmap));
+	save_item(NAME(m_sprite_bitmap));
+}
+
+u32 xsedae_state::pri_callback(u8 pri)
+{
+	switch (pri)
+	{
+		// above background
+		case 0: return GFX_PMASK_2 | GFX_PMASK_4 | GFX_PMASK_8;
+		// above background and midground
+		case 1: return GFX_PMASK_4 | GFX_PMASK_8;
+		// above background, midground and foreground
+		case 2: return GFX_PMASK_8;
+		// above everything
+		case 3:
+		default: return 0; 
+	}
 }
 
 void raiden2_state::blend_layer(bitmap_rgb32 &bitmap, const rectangle &cliprect, bitmap_ind16 &source, int layer)
@@ -252,6 +287,35 @@ void raiden2_state::tilemap_draw_and_blend(screen_device &screen, bitmap_rgb32 &
 	blend_layer(bitmap, cliprect, m_tile_bitmap, 0);
 }
 
+u32 xsedae_state::screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect)
+{
+	bitmap.fill(m_palette->black_pen(), cliprect);
+	screen.priority().fill(0, cliprect);
+
+	if (BIT(~m_tilemap_enable, 0))
+		m_background_layer->draw(screen, bitmap, cliprect, 0, 1);
+
+	if (BIT(~m_tilemap_enable, 1))
+		m_midground_layer->draw(screen, bitmap, cliprect, 0, 2);
+
+	if (BIT(~m_tilemap_enable, 2))
+		m_foreground_layer->draw(screen, bitmap, cliprect, 0, 4);
+
+	if (BIT(~m_tilemap_enable, 3))
+		m_text_layer->draw(screen, bitmap, cliprect, 0, 8);
+
+	if (BIT(~m_tilemap_enable, 4))
+		m_spritegen->draw_prio(screen, bitmap, cliprect, m_spriteram->buffer(), m_spriteram->bytes());
+
+	if (DUMP_COP)
+	{
+		if (m_raiden2cop && machine().input().code_pressed_once(KEYCODE_Z))
+			m_raiden2cop->dump_table();
+	}
+
+	return 0;
+}
+
 u32 raiden2_state::screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect)
 {
 	bitmap.fill(m_palette->black_pen(), cliprect);
@@ -286,8 +350,11 @@ u32 raiden2_state::screen_update(screen_device &screen, bitmap_rgb32 &bitmap, co
 	if (BIT(~m_tilemap_enable, 4))
 		blend_layer(bitmap, cliprect, m_sprite_bitmap, m_cur_spri[4]);
 
-	if (machine().input().code_pressed_once(KEYCODE_Z))
-		if (m_raiden2cop) m_raiden2cop->dump_table();
+	if (DUMP_COP)
+	{
+		if (m_raiden2cop && machine().input().code_pressed_once(KEYCODE_Z))
+			m_raiden2cop->dump_table();
+	}
 
 	return 0;
 }
