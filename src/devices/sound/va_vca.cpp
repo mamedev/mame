c35ee63c0894517d7c023063c40938c8056f70ab
m1macrophage <168948267+m1macrophage@users.noreply.github.com>
2025-12-28 15:20:28 -0800

    sequential/prophet5.cpp: Implemented skeleton audio pipeline. * A440 tone. * Noise generator. * Master volume. * Voice summing. * Voice gating.
    sound/va_vca.cpp: Implemented the CA3280, linearized and non-linearized variants.
    
    sound/mm5837.h: Configuration interface simplifications.

--- src/devices/sound/va_vca.cpp
+++ src/devices/sound/va_vca.cpp
@@ -39,27 +39,173 @@ float va_vca_device::cv_to_gain(float cv) const
 	return cv;
 }
 
+float va_vca_device::distorted(float s) const
+{
+	fatalerror("A va_vca_device subclass enabled distortion without implementing the distorted() function.\n");
+	return s;
+}
+
 void va_vca_device::device_start()
 {
 	if (get_sound_requested_inputs_mask() != 0x01 && get_sound_requested_inputs_mask() != 0x03)
 		fatalerror("%s: Input 0 must be connected, input 1 can optionally be connected. No other inputs allowed.\n", tag());
 
-	m_stream = stream_alloc(get_sound_requested_inputs(), 1, SAMPLE_RATE_OUTPUT_ADAPTIVE);
+	// Upsample when using distortion, to reduce aliasing.
+	const u32 sample_rate = has_distortion() ? std::max(96000, machine().sample_rate()) : SAMPLE_RATE_OUTPUT_ADAPTIVE;
+	m_stream = stream_alloc(get_sound_requested_inputs(), 1, sample_rate);
 	save_item(NAME(m_fixed_gain));
 }
 
 void va_vca_device::sound_stream_update(sound_stream &stream)
 {
-	if (BIT(get_sound_requested_inputs_mask(), INPUT_GAIN))
+	// Implementing 4 variants to minimize unnecessary virtual function calls.
+	if (has_distortion())
 	{
-		for (int i = 0; i < stream.samples(); i++)
-			stream.put(0, i, stream.get(INPUT_AUDIO, i) * cv_to_gain(stream.get(INPUT_GAIN, i)));
+		if (BIT(get_sound_requested_inputs_mask(), INPUT_GAIN))
+		{
+			for (int i = 0; i < stream.samples(); i++)
+				stream.put(0, i, distorted(stream.get(INPUT_AUDIO, i)) * cv_to_gain(stream.get(INPUT_GAIN, i)));
+		}
+		else
+		{
+			for (int i = 0; i < stream.samples(); i++)
+				stream.put(0, i, distorted(stream.get(INPUT_AUDIO, i)) * m_fixed_gain);
+		}
 	}
 	else
 	{
-		for (int i = 0; i < stream.samples(); i++)
-			stream.put(0, i, stream.get(INPUT_AUDIO, i) * m_fixed_gain);
+		if (BIT(get_sound_requested_inputs_mask(), INPUT_GAIN))
+		{
+			for (int i = 0; i < stream.samples(); i++)
+				stream.put(0, i, stream.get(INPUT_AUDIO, i) * cv_to_gain(stream.get(INPUT_GAIN, i)));
+		}
+		else
+		{
+			for (int i = 0; i < stream.samples(); i++)
+				stream.put(0, i, stream.get(INPUT_AUDIO, i) * m_fixed_gain);
+		}
 	}
+}
+
+
+ca3280_vca_device::ca3280_vca_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
+	: va_vca_device(mconfig, CA3280_VCA, tag, owner, 0)
+	, m_input_scale(1)
+	, m_output_scale(1)
+{
+}
+
+ca3280_vca_device &ca3280_vca_device::configure_input_divider(float r_in, float r_gnd)
+{
+	m_input_scale = r_gnd / (r_in + r_gnd);
+	return *this;
+}
+
+ca3280_vca_device &ca3280_vca_device::configure_voltage_output(float r_out)
+{
+	// output voltage = output_current * Rout, where Rout is the resistor to
+	// ground at the output.
+	m_output_scale = r_out;
+	return *this;
+}
+
+// The output current for a non-linearized OTA can be determined by:
+//   Iout = Ioutmax * tanh(DVin / (2 * VT))
+// For the CA3280:
+//   Ioutmax = 4.1 / 5.0 * Iabc = 0.82 * Iabc (look for "Peak Output Current"
+//   in the datasheet).
+// Therefore:
+//   Iout = 0.82 * Iabc * tanh(DVin / (2 * VT))
+// Where:
+//   Iabc ~ transconductance (gain) control current.
+//   DVIn ~ (Vin+ - Vin-). Vin- is typically 0V.
+//   VT ~ Thermal voltage. A temperature-dependent constant.
+
+float ca3280_vca_device::cv_to_gain(float cv) const
+{
+	// cv = Iabc. The gain calculation below also incorporates any output
+	// scaling (conversion of current to voltage) due to configure_voltate_output().
+	constexpr float IOUT_MAX_SCALE = 4.1F / 5.0F;  // Ioutmax = 0.82 * Iabc.
+	return IOUT_MAX_SCALE * cv * m_output_scale;
+}
+
+float ca3280_vca_device::distorted(float s) const
+{
+	constexpr float VT = 25.7E-3;  // Thermal voltage at 25 degrees C.
+	return tanhf((s * m_input_scale) / (2 * VT));
+}
+
+
+ca3280_lin_vca_device::ca3280_lin_vca_device(const machine_config &mconfig, const char *tag, device_t *owner, float i_d)
+	: va_vca_device(mconfig, CA3280_LIN_VCA, tag, owner, 0)
+	, m_i_d_inv(1.0F / i_d)
+	, m_input_scale(1)
+	, m_output_scale(1)
+	, m_cv_scale(1)
+{
+	update_cv_scale();
+}
+
+ca3280_lin_vca_device::ca3280_lin_vca_device(const machine_config &mconfig, const char *tag, device_t *owner, float r, float v_r, float v_minus)
+	: ca3280_lin_vca_device(mconfig, tag, owner, i_d(r, v_r, v_minus))
+{
+}
+
+ca3280_lin_vca_device::ca3280_lin_vca_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock)
+	: ca3280_lin_vca_device(mconfig, tag, owner, 1.0F)
+{
+}
+
+ca3280_lin_vca_device &ca3280_lin_vca_device::configure_voltage_input(float r_in)
+{
+	m_input_scale = 1.0F / r_in;
+	update_cv_scale();
+	return *this;
+}
+
+ca3280_lin_vca_device &ca3280_lin_vca_device::configure_voltage_output(float r_out)
+{
+	// output voltage = output_current * R, where R is the resistor to ground
+	// at the output.
+	m_output_scale = r_out;
+	update_cv_scale();
+	return *this;
+}
+
+// The output current for a linearized OTA can be determined by:
+//   Iout = Ioutmax * Iin / Id
+// For the CA3280:
+//   Ioutmax = 4.1 / 5.0 * Iabc = 0.82 * Iabc (look for "Peak Output Current"
+//   in the datasheet).
+// Therefore:
+//   Iout = 0.82 * Iabc * Iin / Id
+// Where:
+//   Iabc ~ transconductance (gain) control current.
+//   Iin ~ current at in+, assuming in- is connected to ground (via a
+//         resistor).
+//   Id ~ diode current. The current supplied to the Id pin.
+
+float ca3280_lin_vca_device::cv_to_gain(float cv) const
+{
+	// cv = Iabc.
+	// m_cv_scale incorporates 1 / Id, along with the optional factors to
+	// convert the input voltage to a current, and the output current to a
+	// a voltage (see update_cv_scale() and the various configure_*() methods).
+	return cv * m_cv_scale;
+}
+
+void ca3280_lin_vca_device::update_cv_scale()
+{
+	m_cv_scale = m_input_scale * m_i_d_inv * m_output_scale;
+}
+
+float ca3280_lin_vca_device::i_d(float r, float v_r, float v_minus)
+{
+	// Voltage at the Id input changes with current. So VD is just an
+	// approximation. Not sure if this is 1 or 2 diode drops for the CA3280.
+	// Going with 2 for now.
+	constexpr float VD = 2 * 0.6;
+	return (v_r - (v_minus + VD)) / r;
 }
 
 
@@ -77,4 +223,6 @@ float cem3360_vca_device::cv_to_gain(float cv) const
 
 
 DEFINE_DEVICE_TYPE(VA_VCA, va_vca_device, "va_vca", "Voltage-controlled amplifier")
+DEFINE_DEVICE_TYPE(CA3280_VCA, ca3280_vca_device, "ca3280_vca", "CA3280-based VCA")
+DEFINE_DEVICE_TYPE(CA3280_LIN_VCA, ca3280_lin_vca_device, "ca3280_lin_vca", "Linearized CA3280-based VCA")
 DEFINE_DEVICE_TYPE(CEM3360_VCA, cem3360_vca_device, "cem3360_vca", "CEM3360-based VCA")
