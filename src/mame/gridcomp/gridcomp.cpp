38df457eeb9321b68344887af08b0e9ae5492315
Valerii Klachkov <vklachkov@pm.me>
2026-01-15 21:56:39 +0100

    Map DAC memory addresses for GRiD Compass II

--- src/mame/gridcomp/gridcomp.cpp
+++ src/mame/gridcomp/gridcomp.cpp
@@ -325,6 +325,7 @@ void gridcomp_state::grid1101_map(address_map &map)
 {
 	map.unmap_value_high();
 	map(0xdfe40, 0xdfe42).w(FUNC(gridcomp_state::grid_sound_w));
+	map(0xdfe44, 0xdfe4f).unmaprw(); // modem controller??
 	map(0xdfe80, 0xdfe83).rw("i7220", FUNC(i7220_device::read), FUNC(i7220_device::write)).umask16(0x00ff);
 	map(0xdfea0, 0xdfeaf).unmaprw(); // ??
 	map(0xdfec0, 0xdfecf).rw(FUNC(gridcomp_state::grid_modem_r), FUNC(gridcomp_state::grid_modem_w)).umask16(0x00ff); // incl. DTMF generator
@@ -344,7 +345,8 @@ void gridcomp_state::grid1121_map(address_map &map)
 	map(0xc0000, 0xcffff).unmaprw(); // ?? ROM slot -- signature expected: 0x4554, 0x5048
 	map(0xdfa00, 0xdfdff).rw(FUNC(gridcomp_state::grid_dma_r), FUNC(gridcomp_state::grid_dma_w)); // DMA
 	map(0xdfe00, 0xdfe1f).unmaprw(); // .rw("uart8274", FUNC(i8274_device::ba_cd_r), FUNC(i8274_device::ba_cd_w)).umask16(0x00ff);
-	map(0xdfe40, 0xdfe4f).unmaprw(); // ?? diagnostic 8274
+	map(0xdfe40, 0xdfe42).w(FUNC(gridcomp_state::grid_sound_w));
+	map(0xdfe44, 0xdfe4f).unmaprw(); // modem controller??
 	map(0xdfe80, 0xdfe83).rw("i7220", FUNC(i7220_device::read), FUNC(i7220_device::write)).umask16(0x00ff);
 	map(0xdfea0, 0xdfeaf).unmaprw(); // ??
 	map(0xdfec0, 0xdfecf).rw(FUNC(gridcomp_state::grid_modem_r), FUNC(gridcomp_state::grid_modem_w)).umask16(0x00ff); // incl. DTMF generator
