837e8dc631945d15155cf3b5825ed0a0195fa349
flama12333 <143599905+flama12333@users.noreply.github.com>
2026-01-24 00:19:58 -0500

    update hw info in ymball.cpp
    and some misc.

--- src/mame/misc/ymball.cpp
+++ src/mame/misc/ymball.cpp
@@ -2,23 +2,25 @@
 // copyright-holders:flama12333
 /*************************************************************************
 Basic Hardware info: (Wip)
-U2 W78E052C40DL - MCU (8kb) 8052 Microcontroller with internal rom.
-U7 HM6264LP-12  - RAM (8kb) 
-U17 U6295       - ADPCM Clone of MSM6295
-U18 UM3567      - OPLL Clone of ym2413
-U22 ATF1508AS   - EEPLD
-U49 D8255AC-2   - PPI1 for led
-U50 M5L8255AP-5 - PPI2  for led
+U2 W78E052C40DL        - MCU (8kb) 8052 Microcontroller with internal rom.
+U7 HM6264LP-12         - RAM (8kb) 
+U17 U6295              - ADPCM Clone of MSM6295
+U18 UM3567             - OPLL Clone of ym2413
+U22 ATF1508AS          - EEPLD
+U49 D8255AC-2          - PPI1 for led
+U50 M5L8255AP-5        - PPI2  for led
 EEPROM:
-NM27C010        - ROM (128kb) program rom. first 8kb empty.
-m27c801-100f1   -   (ROM1024kb) adpcm rom.
+NM27C010 or TMS27C010A - ROM (128kb) program rom. first 8kb empty.
+m27c801-100f1          - ROM (1024kb) adpcm rom.
+
 xtal:
-X1 3.579MHZ - ???
-X2 10.738M - mcu
-X3 3.57mhz - OPLL 
+X1 3.579MHZ            - ???
+X2 10.738M             - mcu
+X3 3.57mhz             - OPLL 
 
 Todo:
 Loop at 378 ljmp loop. waiting for timer?. 
+Check if the eeprom are encrypted?.
 
 */
 
@@ -77,20 +79,18 @@ void ymball_state::machine_start()
 void ymball_state::ymball(machine_config &config)
 {
     // basic machine hardware
-	i80c52_device &maincpu(I80C52(config, "maincpu", XTAL(10'738'635))); // X2 10.738M
+	i80c52_device &maincpu(I80C52(config, "maincpu", XTAL(10'738'635))); // Actual cpu is W78E052C40DL
 	maincpu.set_addrmap(AS_PROGRAM, &ymball_state::program_map);
 	maincpu.set_addrmap(AS_DATA, &ymball_state::data_map);
 	
-    
-	// Programmable Peripheral Interface
+    // Programmable Peripheral Interface
 	I8255A(config, "ppi1");
 	I8255A(config, "ppi2");
 
-
-   	// sound hardware
+  	// sound hardware
 	SPEAKER(config, "mono").front_center();
 	
-	ym2413_device &opll(YM2413(config, "opll", 3.579545_MHz_XTAL)); // Actual cpu is W78E052C40DL. X3 3.57mhz
+	ym2413_device &opll(YM2413(config, "opll", 3.579545_MHz_XTAL));
 	opll.add_route(ALL_OUTPUTS, "mono", 1.0);
 	OKIM6295(config, m_oki,  XTAL(10'738'635) / 4, okim6295_device::PIN7_LOW).add_route(ALL_OUTPUTS, "mono", 1.00);  // Clock frequency & pin 7 not verified
 
@@ -98,7 +98,7 @@ void ymball_state::ymball(machine_config &config)
 
 ROM_START( ymball )
 	ROM_REGION( 0x02000, "maincpu", 0 )
-   	ROM_LOAD( "w78e052c.u2", 0x00000, 0x02000, CRC(b950c825) SHA1(822cd26c2df0c4966ab0bbed5a87af1f98cfd7a3) ) // 
+   	ROM_LOAD( "w78e052c.u2", 0x00000, 0x02000, CRC(b950c825) SHA1(822cd26c2df0c4966ab0bbed5a87af1f98cfd7a3) ) // Unprotected
 	
 	ROM_REGION( 0x20000, "eeprom", 0 )
 	ROM_LOAD( "nm27c010.u8", 0x00000, 0x20000,  CRC(fc9b528d) SHA1(4d47945e6aab4a85ffdc37027707377dd6640108) ) // Hex FF filled at 0x0000-0x1fff  
