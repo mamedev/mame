fab24c20c50eeb79073b5dfe4da9835db9745123
TheLostNinja <116140256+TheLostNinja@users.noreply.github.com>
2026-01-30 07:08:04 +0300

    315_5195.cpp: Looks like we have a deal with a structel allergy.

--- src/mame/sega/315_5195.cpp
+++ src/mame/sega/315_5195.cpp
@@ -437,19 +437,19 @@ void sega_315_5195_mapper_device::device_start()
 	m_decrypted_space = m_cpu->has_space(AS_OPCODES) ? &m_cpu->space(AS_OPCODES) : nullptr;
 
 	// register for saves
-	save_item(STRUCT_MEMBER(info, size_mask));
+	//save_item(STRUCT_MEMBER(info, size_mask));
 	//save_item(STRUCT_MEMBER(info, base));
 	//save_item(STRUCT_MEMBER(info, mirror));
 	//save_item(STRUCT_MEMBER(info, start));
 	//save_item(STRUCT_MEMBER(info, end));
-	//save_item(NAME(m_start));
-	//save_item(NAME(m_end));
-	//save_item(NAME(m_rgnoffs));
-	//save_item(NAME(m_regs));
+	save_item(NAME(m_start));
+	save_item(NAME(m_end));
+	save_item(NAME(m_rgnoffs));
+	save_item(NAME(m_regs));
 	//save_item(NAME(m_curregion));
-	//save_item(NAME(m_banks));
-	//save_item(NAME(m_to_sound));
-	//save_item(NAME(m_from_sound));
+	save_item(NAME(m_banks));
+	save_item(NAME(m_to_sound));
+	save_item(NAME(m_from_sound));
 	//save_item(NAME(m_open_bus_recurse));
 }
 
