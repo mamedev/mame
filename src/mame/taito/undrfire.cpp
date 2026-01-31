6f8249d53822caea86df978c2fadb0b31aa0dc46
TheLostNinja <116140256+TheLostNinja@users.noreply.github.com>
2026-01-31 15:21:51 +0300

    undrfire.cpp: Array allergy test.

--- src/mame/taito/undrfire.cpp
+++ src/mame/taito/undrfire.cpp
@@ -311,7 +311,7 @@ void undrfire_state::video_start()
 
 	m_frame_counter = 0;
 
-	save_item(NAME(m_dislayer));
+	//save_item(NAME(m_dislayer));
 }
 
 /***************************************************************
@@ -908,7 +908,7 @@ void undrfire_state::machine_start()
 
 	save_item(NAME(m_port_sel));
 	save_item(NAME(m_frame_counter));
-	save_item(NAME(m_rotate_ctrl));
+	//save_item(NAME(m_rotate_ctrl));
 }
 
 /**********************************************************
