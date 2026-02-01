43473a63e1d068771000f7fbcab4b9158d3d5ab6
dotkoval <alexander@k0valchuk.com>
2026-02-01 23:11:27 +0100

    bus/spectrum: Add Kempston Mouse Interface slot device
    * classic 2-button mouse interface
    * available for classic ZX Spectrum models (48/128/+2/+3)

---
+++ src/devices/bus/spectrum/kempmouse.h
@@ -0,0 +1,52 @@
+// license:BSD-3-Clause
+// copyright-holders: Oleksandr Kovalchuk
+// Thanks to: Nigel Barnes (based on kempjoy implementation)
+/**********************************************************************
+
+    Kempston Mouse Interface
+
+**********************************************************************/
+
+#ifndef MAME_BUS_SPECTRUM_KEMPMOUSE_H
+#define MAME_BUS_SPECTRUM_KEMPMOUSE_H
+
+#pragma once
+
+
+#include "exp.h"
+
+//**************************************************************************
+//  TYPE DEFINITIONS
+//**************************************************************************
+
+// ======================> spectrum_kempmouse_device
+
+class spectrum_kempmouse_device :
+	public device_t,
+	public device_spectrum_expansion_interface
+{
+public:
+	// construction/destruction
+	spectrum_kempmouse_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+protected:
+	// device-level overrides
+	virtual void device_start() override ATTR_COLD;
+
+	// optional information overrides
+	virtual ioport_constructor device_input_ports() const override ATTR_COLD;
+
+	virtual uint8_t iorq_r(offs_t offset) override;
+
+private:
+	required_ioport m_mouse_x;
+	required_ioport m_mouse_y;
+	required_ioport m_mouse_buttons;
+};
+
+
+// device type definition
+DECLARE_DEVICE_TYPE(SPECTRUM_KEMPMOUSE, spectrum_kempmouse_device)
+
+
+#endif // MAME_BUS_SPECTRUM_KEMPMOUSE_H
