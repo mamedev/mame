56713303553dafc223296f300a37b1bd387af917
TheLostNinja <116140256+TheLostNinja@users.noreply.github.com>
2026-02-01 10:28:17 +0300

    Update rltennis.cpp

--- src/mame/tch/rltennis.cpp
+++ src/mame/tch/rltennis.cpp
@@ -68,9 +68,12 @@ player - when there's nothing to play - first, empty 2k of ROMs are selected.
 
 #include "cpu/m68000/m68000.h"
 #include "machine/eeprompar.h"
+#include "sound/dac.h"
 #include "video/ramdac.h"
 #include "screen.h"
 #include "speaker.h"
+
+#include <algorithm>
 
 #define RLT_REFRESH_RATE   60
 #define RLT_TIMER_FREQ     (RLT_REFRESH_RATE*256)
@@ -93,6 +96,9 @@ player - when there's nothing to play - first, empty 2k of ROMs are selected.
 
 #define SRC_SHIFT           8
 
+#define RLT_NUM_BLITTER_REGS    8
+#define RLT_NUM_BITMAPS         8
+
 enum
 {
 	BLT_X_START = 0,
@@ -111,6 +117,58 @@ enum
 	BITMAP_FG_1,
 	BITMAP_FG_2,
 	BITMAP_FG_DISPLAY
+};
+
+namespace {
+
+class rltennis_state : public driver_device
+{
+public:
+	rltennis_state(const machine_config &mconfig, device_type type, const char *tag) :
+		driver_device(mconfig, type, tag),
+		m_maincpu(*this, "maincpu"),
+		m_dac(*this, "dac%u", 1U),
+		m_samples(*this, "samples%u", 1U),
+		m_gfx(*this, "gfx"),
+		m_data760000(0), m_data740000(0), m_dac_counter(0),
+		m_offset_shift(0)
+	{
+		std::fill(std::begin(m_sample_rom_offset), std::end(m_sample_rom_offset), 0);
+	}
+
+	void rltennis(machine_config &config);
+
+private:
+	required_device<cpu_device> m_maincpu;
+	required_device_array<dac_byte_interface, 2> m_dac;
+	required_region_ptr_array<uint8_t, 2> m_samples;
+	required_region_ptr<uint8_t> m_gfx;
+
+	uint16_t m_blitter[RLT_NUM_BLITTER_REGS];
+	int32_t m_data760000;
+	int32_t m_data740000;
+	int32_t m_dac_counter;
+	int32_t m_sample_rom_offset[2];
+	int32_t m_offset_shift;
+	int32_t m_unk_counter = 0;
+	std::unique_ptr<bitmap_ind16> m_tmp_bitmap[RLT_NUM_BITMAPS];
+	emu_timer *m_timer = nullptr;
+
+	uint16_t io_r();
+	void snd1_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void snd2_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void blitter_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+
+	virtual void machine_start() override ATTR_COLD;
+	virtual void machine_reset() override ATTR_COLD;
+	virtual void video_start() override ATTR_COLD;
+
+	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+
+	INTERRUPT_GEN_MEMBER(interrupt);
+	TIMER_CALLBACK_MEMBER(sample_player);
+	void ramdac_map(address_map &map) ATTR_COLD;
+	void rltennis_main(address_map &map) ATTR_COLD;
 };
 
 /****************************************************************************************
@@ -493,6 +551,8 @@ ROM_START( rltennisa )
 	ROM_LOAD( "gal16v8.ic23", 0x000, 0x117, CRC(7bc89e80) SHA1(6b34527121e4dbac37fbbd79c57a7eab81de0198) )
 	ROM_LOAD( "gal22v10.ic2", 0x117, 0x2e5, CRC(b5a7cb92) SHA1(05d6e8c8208ac486e849de3322f049bcd38561e1) )
 ROM_END
+
+} // anonymous namespace
 
 GAME( 1993, rltennis,         0, rltennis, rltennis, rltennis_state, empty_init, ROT0, "TCH", "Reality Tennis (set 1)", MACHINE_IMPERFECT_GRAPHICS | MACHINE_IMPERFECT_TIMING | MACHINE_SUPPORTS_SAVE )
 GAME( 1993, rltennisa, rltennis, rltennis, rltennis, rltennis_state, empty_init, ROT0, "TCH", "Reality Tennis (set 2)", MACHINE_IMPERFECT_GRAPHICS | MACHINE_IMPERFECT_TIMING | MACHINE_SUPPORTS_SAVE )
