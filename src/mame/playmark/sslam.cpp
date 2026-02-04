254d15ce055d1b9180d5c87f0ca5b0f007267a83
TheLostNinja <116140256+TheLostNinja@users.noreply.github.com>
2026-02-04 11:55:34 +0300

    Update sslam.cpp

--- src/mame/playmark/sslam.cpp
+++ src/mame/playmark/sslam.cpp
@@ -80,16 +80,16 @@ Notes:
                        (unfortunately, it's protected)
 */
 
-
-
 #include "emu.h"
-#include "sslam.h"
+#include "emupal.h"
 
 #include "cpu/m68000/m68000.h"
+#include "cpu/mcs51/i80c51.h"
+#include "machine/gen_latch.h"
+#include "sound/okim6295.h"
 #include "screen.h"
+#include "tilemap.h"
 #include "speaker.h"
-
-
 
 /**************************************************************************
    This table converts commands sent from the main CPU, into sample numbers
@@ -112,7 +112,6 @@ static const uint8_t sslam_snd_cmd[64] =
 /*38*/  0x29, 0x2a, 0x2b, 0xff, 0x69, 0xff, 0x20, 0xff
 };
 
-
 /**************************************************************************
    Music Sequencing:
    The columns contain the sequence (from left to right) of samples to play in each backing music track
@@ -134,9 +133,6 @@ static const uint8_t sslam_snd_loop[8][19] =
 /*6B*/  { 0x6b, 0x6a, 0x6a, 0x6b, 0x6a, 0xff },
 /*6C*/  { 0x6c, 0xff }
 };
-
-
-
 
 /* Sound numbers in the Super Slam sample ROM
 01 "Out"
@@ -207,6 +203,110 @@ static const uint8_t sslam_snd_loop[8][19] =
 75 "Game and Third Set"
 76 "Game Set and Match" -------------
 */
+
+namespace {
+
+class sslam_state : public driver_device
+{
+public:
+	sslam_state(const machine_config &mconfig, device_type type, const char *tag) :
+		driver_device(mconfig, type, tag),
+		m_maincpu(*this, "maincpu"),
+		m_audiocpu(*this, "audiocpu"),
+		m_oki(*this, "oki"),
+		m_gfxdecode(*this, "gfxdecode"),
+		m_palette(*this, "palette"),
+		m_soundlatch(*this, "soundlatch"),
+		m_bg_tileram(*this, "bg_tileram"),
+		m_md_tileram(*this, "md_tileram"),
+		m_tx_tileram(*this, "tx_tileram"),
+		m_regs(*this, "regs"),
+		m_spriteram(*this, "spriteram")
+	{ }
+
+	void sslam(machine_config &config);
+
+	void init_sslam();
+
+protected:
+	virtual void machine_start() override ATTR_COLD;
+	virtual void video_start() override ATTR_COLD;
+
+	required_device<cpu_device> m_maincpu;
+	required_device<mcs51_cpu_device> m_audiocpu;
+	required_device<okim6295_device> m_oki;
+	required_device<gfxdecode_device> m_gfxdecode;
+	required_device<palette_device> m_palette;
+	optional_device<generic_latch_8_device> m_soundlatch;
+
+	required_shared_ptr<uint16_t> m_bg_tileram;
+	optional_shared_ptr<uint16_t> m_md_tileram;
+	optional_shared_ptr<uint16_t> m_tx_tileram;
+	required_shared_ptr<uint16_t> m_regs;
+	required_shared_ptr<uint16_t> m_spriteram;
+
+	emu_timer *m_music_timer = nullptr;
+
+	int m_sound = 0;
+	int m_melody = 0;
+	int m_bar = 0;
+	int m_track = 0;
+	int m_snd_bank = 0;
+
+	uint8_t m_oki_control = 0;
+	uint8_t m_oki_command = 0;
+	uint8_t m_oki_bank = 0;
+
+	uint32_t m_unk_458 = 0;
+
+	tilemap_t *m_bg_tilemap = nullptr;
+	tilemap_t *m_tx_tilemap = nullptr;
+	tilemap_t *m_md_tilemap = nullptr;
+
+	int m_sprites_x_offset = 0;
+	uint8_t playmark_snd_command_r();
+	void playmark_oki_w(uint8_t data);
+	void playmark_snd_control_w(uint8_t data);
+	void sslam_tx_tileram_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void sslam_md_tileram_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void sslam_bg_tileram_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void sslam_snd_w(uint8_t data);
+
+	TILE_GET_INFO_MEMBER(get_sslam_tx_tile_info);
+	TILE_GET_INFO_MEMBER(get_sslam_md_tile_info);
+	TILE_GET_INFO_MEMBER(get_sslam_bg_tile_info);
+	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	TIMER_CALLBACK_MEMBER(music_playback);
+	void draw_sprites(bitmap_ind16 &bitmap, const rectangle &cliprect);
+	void sslam_play(int track, int data);
+
+	void sslam_program_map(address_map &map) ATTR_COLD;
+};
+
+class powerbls_state : public sslam_state
+{
+public:
+	powerbls_state(const machine_config &mconfig, device_type type, const char *tag) :
+		sslam_state(mconfig, type, tag)
+	{ }
+
+	void powerbls(machine_config &config);
+
+	void init_powerbls();
+
+protected:
+	virtual void machine_start() override ATTR_COLD;
+	virtual void video_start() override ATTR_COLD;
+
+	void powerbls_map(address_map &map) ATTR_COLD;
+
+	TILE_GET_INFO_MEMBER(get_powerbls_bg_tile_info);
+
+	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+
+	void powerbls_sound_w(uint16_t data);
+	void powerbls_bg_tileram_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+};
 
 void sslam_state::draw_sprites(bitmap_ind16 &bitmap, const rectangle &cliprect)
 {
@@ -1121,6 +1221,8 @@ ROM_START( powerbals )
 	ROM_COPY( "oki", 0x00000,0x40000, 0x20000)
 	ROM_COPY( "oki", 0x00000,0x80000, 0x20000)
 ROM_END
+
+} // anonymous namespace
 
 GAME( 1993, sslam,    0,        sslam,    sslam,    sslam_state,    empty_init, ROT0, "Playmark", "Super Slam (set 1)",                  MACHINE_SUPPORTS_SAVE )
 GAME( 1993, sslama,   sslam,    sslam,    sslam,    sslam_state,    empty_init, ROT0, "Playmark", "Super Slam (set 2)",                  MACHINE_SUPPORTS_SAVE )
