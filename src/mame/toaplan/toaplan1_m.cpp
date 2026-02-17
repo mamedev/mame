12af52a99c94fbddef4421c26f494646282a3c78
cam900 <dbtlrchl@naver.com>
2026-02-17 20:26:13 +0900

    toaplan/*.cpp: Make BCU, FCU, Video controller hardware to separated device, Updates
    toaplan/toaplan1.cpp:
    - Split driver for fireshrk and clones to toaplan/fireshrk.cpp
    - Split driver for rallybik and clones to toaplan/rallybik.cpp
      - These are enough to describe 'non-standard toaplan1 driver' so that can be splitted.
    - Fix outzonecv raw params
      - It uploads same video timing parameter values as parent set to CRTC.
    - Make sprite size LUT RAM as memory_share_creator
    - Reduce duplicates
    - Use separated palette devices for each layer
    - Reduce unnecessary function
    - Add/Fix notes
    
    toaplan/toaplan_scu.cpp:
    - Move color base initialization into device_start
    - Add notes
    - Fix initializers
    - Fix spacings

--- src/mame/toaplan/toaplan1_m.cpp
+++ src/mame/toaplan/toaplan1_m.cpp
@@ -1,5 +1,5 @@
 // license:BSD-3-Clause
-// copyright-holders:Darren Olafson, Quench,Stephane Humbert
+// copyright-holders:Darren Olafson, Quench, Stephane Humbert
 /***************************************************************************
                 ToaPlan game hardware from 1988-1991
                 ------------------------------------
@@ -7,18 +7,6 @@
 
 #include "emu.h"
 #include "toaplan1.h"
-
-
-void toaplan1_state::interrupt()
-{
-	if (m_intenable)
-		m_maincpu->set_input_line(4, HOLD_LINE);
-}
-
-void toaplan1_state::intenable_w(u8 data)
-{
-	m_intenable = data;
-}
 
 
 void toaplan1_demonwld_state::dsp_host_addr_cb(u16 data, u32 &seg, u32 &addr)
@@ -88,12 +76,6 @@ void toaplan1_demonwld_state::dsp_ctrl_w(u8 data)
 }
 
 
-u8 toaplan1_samesame_state::port_6_word_r()
-{
-	/* Bit 0x80 is secondary CPU (HD647180) ready signal */
-	return (m_soundlatch->pending_r() ? 0 : 0x80) | m_tjump_io->read();
-}
-
 u8 toaplan1_state::shared_r(offs_t offset)
 {
 	return m_sharedram[offset];
@@ -108,42 +90,16 @@ void toaplan1_state::shared_w(offs_t offset, u8 data)
 void toaplan1_state::reset_sound()
 {
 	/* Reset the secondary CPU and sound chip */
-	/* rallybik, truxton, hellfire, demonwld write to a port to cause a reset */
+	/* truxton, hellfire, demonwld write to a port to cause a reset */
 	/* zerowing, fireshrk, outzone, vimana use a RESET instruction instead */
 	m_ymsnd->reset();
 	m_audiocpu->pulse_input_line(INPUT_LINE_RESET, attotime::zero);
 }
 
-void toaplan1_samesame_state::reset_sound()
-{
-	toaplan1_state::reset_sound();
-	m_soundlatch->acknowledge_w();
-}
-
 void toaplan1_state::reset_sound_w(u8 data)
 {
-	if (data == 0) reset_sound();
-}
-
-
-void toaplan1_rallybik_state::coin_counter_1_w(int state)
-{
-	machine().bookkeeping().coin_counter_w(0, state);
-}
-
-void toaplan1_rallybik_state::coin_counter_2_w(int state)
-{
-	machine().bookkeeping().coin_counter_w(1, state);
-}
-
-void toaplan1_rallybik_state::coin_lockout_1_w(int state)
-{
-	machine().bookkeeping().coin_lockout_w(0, !state);
-}
-
-void toaplan1_rallybik_state::coin_lockout_2_w(int state)
-{
-	machine().bookkeeping().coin_lockout_w(1, !state);
+	if (data == 0)
+		reset_sound();
 }
 
 
@@ -163,11 +119,5 @@ void toaplan1_state::reset_callback(int state)
 
 void toaplan1_state::machine_reset()
 {
-	m_intenable = 0;
 	machine().bookkeeping().coin_lockout_global_w(0);
-}
-
-void toaplan1_state::machine_start()
-{
-	save_item(NAME(m_intenable));
 }
