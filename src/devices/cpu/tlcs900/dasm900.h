e584ffa386c11c3e9a6964e17c71bf08411046ea
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2026-02-19 21:12:55 +0000

    tlcs900: Add model-specific LDC control register names for DMA registers (#14970)

--- src/devices/cpu/tlcs900/dasm900.h
+++ src/devices/cpu/tlcs900/dasm900.h
@@ -17,10 +17,18 @@ Toshiba TLCS-900/H disassembly
 class tlcs900_disassembler : public util::disasm_interface
 {
 public:
+	// Control register symbol entry for model-specific LDC operand names
+	struct cr_sym { u8 size; u8 encoding; char const *name; };
+
 	tlcs900_disassembler();
 	template <size_t N>
 	tlcs900_disassembler(std::pair<u16, char const *> const (&symbols)[N])
 		: tlcs900_disassembler(symbols, N)
+	{
+	}
+	template <size_t N, size_t CRN>
+	tlcs900_disassembler(std::pair<u16, char const *> const (&symbols)[N], cr_sym const (&cr_symbols)[CRN])
+		: tlcs900_disassembler(symbols, N, cr_symbols, CRN)
 	{
 	}
 
@@ -30,13 +38,21 @@ public:
 private:
 	std::pair<u16, char const *> const *m_symbols;
 	std::size_t m_symbol_count;
+	cr_sym const *m_cr_symbols;
+	std::size_t m_cr_symbol_count;
 
 	tlcs900_disassembler(std::pair<u16, char const *> const symbols[], std::size_t symbol_count)
-		: m_symbols(symbols), m_symbol_count(symbol_count)
+		: m_symbols(symbols), m_symbol_count(symbol_count), m_cr_symbols(nullptr), m_cr_symbol_count(0)
+	{
+	}
+	tlcs900_disassembler(std::pair<u16, char const *> const symbols[], std::size_t symbol_count,
+	                     cr_sym const cr_symbols[], std::size_t cr_symbol_count)
+		: m_symbols(symbols), m_symbol_count(symbol_count), m_cr_symbols(cr_symbols), m_cr_symbol_count(cr_symbol_count)
 	{
 	}
 
 	template <typename T> std::string address(T offset, int size) const;
+	char const *cr_name(u8 size, u8 encoding) const;
 };
 
 #endif // MAME_CPU_TLCS900_DASM900_H
