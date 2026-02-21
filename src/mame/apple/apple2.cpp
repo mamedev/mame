mamedev/mame

commit f8ecff4d11fca5000d5d581995d5341c32cb4440

apple2, apple2p & clones: Keyboard interface expansion - Made existing AY-5-3600 based keyboard a slot option; add lowercase mode. [AJR] - Added dump and emulation of Videx Keyboard Enhancer ][ together with clone system with complementary lowercase chip. [AJR, Apple II Documentation Project] - Added dump and emulation of Key Tronic KB 200. [AJR, Bitsavers] - am64, am100, ivelultr, uniap2en/uniap2pt: Add emulation of keyboards previously included in ROM sets. With this change, uniap2ti is now emulated as a slot-based keyboard device and is no longer a runnable system. [AJR] - Add optional control key mod required by one word processor. [AJR] - ivelultr: Hook up character set switch to unique keyboard output. [AJR]
New working clones
------------------
Apple ][+ (Enhancer ][ lowercase mod) [AJR, Apple II Documentation Project]

New working software items (apple2_flop_misc)
---------------------------------------------
Enhancer ][ Utility Diskette [AJR, ASIMOV Apple II Collection]

src/mame/apple/apple2video.cpp
@@ -438,6 +438,16 @@ unsigned a2_video_device::get_text_character(uint32_t code, int row)
 			code |= (m_GS_langsel >> 5) * 0x100;
 		}
 	}
+	else if (Model == model::IVEL_ULTRA)
+	{
+		// this model doesn't seem to support flashing characters
+		if (code < 0x80)
+			invert_mask ^= 0x7f;
+
+		// YU/ASCII select
+		if (m_an2)
+			code |= 0x100;
+	}
 	else    // original II and II Plus
 	{
 		if ((code >= 0x40) && (code <= 0x7f))
@@ -451,7 +461,7 @@ unsigned a2_video_device::get_text_character(uint32_t code, int row)
 				invert_mask ^= 0x7f;
 			}
 		}
-		else if (code < 0x40 && Model != model::IVEL_ULTRA) // inverse: flip FG and BG
+		else if (code < 0x40) // inverse: flip FG and BG
 		{
 			invert_mask ^= 0x7f;
 		}
@@ -464,7 +474,7 @@ unsigned a2_video_device::get_text_character(uint32_t code, int row)
 
 	/* look up the character data */
 	unsigned bits = m_char_ptr[code * 8 + row];
-	bits = (Model == model::IVEL_ULTRA) ? (bits >> 1) : (bits & 0x7f);
+	bits = (Invert && !Flip) ? (bits >> 1) : (bits & 0x7f);
 	bits ^= invert_mask;
 	return Flip ? reverse_7_bits[bits] : bits;
 }
@@ -610,6 +620,7 @@ void a2_video_device::text_update(screen_device &screen, bitmap_ind16 &bitmap, c
 	}
 }
 
+// explicit instantiations
 template void a2_video_device::text_update<a2_video_device::model::II, true, true>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int beginrow, int endrow);
 template void a2_video_device::text_update<a2_video_device::model::II, true, false>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int beginrow, int endrow);
 template void a2_video_device::text_update<a2_video_device::model::II, false, true>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int beginrow, int endrow);
@@ -621,6 +632,7 @@ template void a2_video_device::text_update<a2_video_device::model::IIE, false, f
 template void a2_video_device::text_update<a2_video_device::model::IIGS, false, false>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int beginrow, int endrow);
 template void a2_video_device::text_update<a2_video_device::model::II_J_PLUS, true, true>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int beginrow, int endrow);
 template void a2_video_device::text_update<a2_video_device::model::IVEL_ULTRA, true, false>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int beginrow, int endrow);
+template void a2_video_device::text_update<a2_video_device::model::IVEL_ULTRA, false, true>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int beginrow, int endrow);
 
 void a2_video_device::hgr_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect, int beginrow, int endrow)
 {
@@ -1037,6 +1049,7 @@ uint32_t a2_video_device::screen_update(screen_device &screen, bitmap_ind16 &bit
 	return 0;
 }
 
+// explicit instantiations
 template uint32_t a2_video_device::screen_update<a2_video_device::model::II, true, true>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 template uint32_t a2_video_device::screen_update<a2_video_device::model::II, true, false>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 template uint32_t a2_video_device::screen_update<a2_video_device::model::II, false, true>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
@@ -1048,6 +1061,7 @@ template uint32_t a2_video_device::screen_update<a2_video_device::model::IIE, fa
 template uint32_t a2_video_device::screen_update<a2_video_device::model::IIGS, false, false>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 template uint32_t a2_video_device::screen_update<a2_video_device::model::II_J_PLUS, true, true>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 template uint32_t a2_video_device::screen_update<a2_video_device::model::IVEL_ULTRA, true, false>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+template uint32_t a2_video_device::screen_update<a2_video_device::model::IVEL_ULTRA, false, true>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 
 static INPUT_PORTS_START( a2_vidconfig_composite );
 	PORT_START("a2_video_config")

hash/apple2_flop_misc.xml
@@ -7546,4 +7546,15 @@ Generated by SLIST 0.2.1
 		</part>
 	</software>
 
+	<software name="videnh2">
+		<description>Enhancer ][ Utilities Diskette</description>
+		<year>1982</year>
+		<publisher>Videx</publisher>
+		<part name="flop1" interface="floppy_5_25">
+			<dataarea name="flop" size="143360">
+				<rom name="videxenhancerii.do" size="143360" crc="8b141996" sha1="b8c3fcc44a21c58ab7c4b30bf868487f55a6c16a"/>
+			</dataarea>
+		</part>
+	</software>
+
 </softwarelist>

scripts/src/bus.lua
@@ -3313,6 +3313,30 @@ end
 
 ---------------------------------------------------
 --
+--@src/devices/bus/a2kbd/a2kbd.h,BUSES["A2KBD"] = true
+---------------------------------------------------
+
+if (BUSES["A2KBD"]~=null) then
+	files {
+		MAME_DIR .. "src/devices/bus/a2kbd/a2kbd.cpp",
+		MAME_DIR .. "src/devices/bus/a2kbd/a2kbd.h",
+		MAME_DIR .. "src/devices/bus/a2kbd/am100kbd.cpp",
+		MAME_DIR .. "src/devices/bus/a2kbd/am100kbd.h",
+		MAME_DIR .. "src/devices/bus/a2kbd/ivelultrkb.cpp",
+		MAME_DIR .. "src/devices/bus/a2kbd/ivelultrkb.h",
+		MAME_DIR .. "src/devices/bus/a2kbd/kb200.cpp",
+		MAME_DIR .. "src/devices/bus/a2kbd/kb200.h",
+		MAME_DIR .. "src/devices/bus/a2kbd/nkbd.cpp",
+		MAME_DIR .. "src/devices/bus/a2kbd/nkbd.h",
+		MAME_DIR .. "src/devices/bus/a2kbd/tk10.cpp",
+		MAME_DIR .. "src/devices/bus/a2kbd/tk10.h",
+		MAME_DIR .. "src/devices/bus/a2kbd/videnh2.cpp",
+		MAME_DIR .. "src/devices/bus/a2kbd/videnh2.h",
+	}
+end
+
+---------------------------------------------------
+--
 --@src/devices/bus/nscsi/cd.h,BUSES["NSCSI"] = true
 --@src/devices/bus/nscsi/devices.h,BUSES["NSCSI"] = true
 --@src/devices/bus/nscsi/hd.h,BUSES["NSCSI"] = true

src/devices/bus/a2kbd/a2kbd.cpp
@@ -1,0 +1,132 @@
+// license:BSD-3-Clause
+// copyright-holders:AJR
+/*********************************************************************
+
+    Apple II Keyboard Connector
+
+    The Apple II receives parallel input from its keyboard through a
+    16-pin DIP socket, much like the Apple I and several other early
+    8-bit microcomputer systems (whose specific connector pinouts
+    were almost never compatible with each other).
+
+    Keypresses are translated into 7-bit ASCII codes placed on B1–B7
+    (positive-true TTL levels). The setting of an eighth bit is
+    triggered by the rising edge of the STROBE output, which
+    notifies the host that a new keycode is present.
+
+    The keyboard is also connected to the system's master RESET
+    signal (active low), and should assert it when one or more
+    special keys is pressed. In theory this line is bidirectional,
+    but the motherboard and slot cards normally drive it only
+    through power-up reset circuits (which were missing in the
+    earliest Apple IIs).
+
+    The state of the keyboard's Shift key(s) is not normally exposed
+    on any of the connector pins. However, a widely installed
+    hardware modification ran a wire from it to one of the Game I/O-
+    related inputs of the LS251 multiplexer on the motherboard, most
+    commonly SW2 (which became the standard location for polling the
+    shift key on the Apple IIe and later models). Software that
+    expects this modification to be installed (including some
+    80-column card firmware) will convert uppercase letters to
+    lowercase when they sense that Shift is not pressed and convert
+    ], ^ and @ back to (uppercase) M, N and P when Shift is pressed.
+    Some Apple II word processors also expect the Control key to be
+    connected to another LS251 input.
+
+    With the dubious exception of RESET, there are no outputs from
+    the main unit to the keyboard... as standard. Videx's Enhancer ][
+    appropriated two unused pins to carry the positive output of the
+    key strobe flip-flop and one of the annunciators back to the
+    keyboard to improve its efficiency and functionality.
+
+    This parallel keyboard interface is specific to the Apple II,
+    Apple II+ and some clones. Though the keyboard encoders used in
+    the Apple III and in the Apple IIe & IIc are from the same
+    family as that used in the newer II/II+ keyboard, they reside on
+    the motherboard and cannot be easily replaced, and the actual
+    keyboard connector on those systems carries only strobes and
+    return lines.
+
+                             ┌─────────┐
+                      +5V  1 │•        │ 16  N.C.
+                   STROBE  2 │         │ 15  −12V
+                    RESET  3 │         │ 14  N.C.
+                    (AN3)  4 │         │ 13  B2
+                       B6  5 │         │ 12  B1
+                       B5  6 │         │ 11  B4
+                       B7  7 │         │ 10  B3
+                      GND  8 │         │ 9   (ACK)
+                             └─────────┘
+
+*********************************************************************/
+
+#include "emu.h"
+#include "a2kbd.h"
+
+#include "am100kbd.h"
+#include "ivelultrkb.h"
+#include "kb200.h"
+#include "nkbd.h"
+#include "tk10.h"
+#include "videnh2.h"
+
+
+//**************************************************************************
+//  KEYBOARD CONNECTOR DEVICE
+//**************************************************************************
+
+// device type definition
+DEFINE_DEVICE_TYPE(A2KBD_CONNECTOR, a2kbd_connector_device, "a2kbd_connector", "Apple II Keyboard Connector")
+
+a2kbd_connector_device::a2kbd_connector_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock)
+	: device_t(mconfig, A2KBD_CONNECTOR, tag, owner, clock)
+	, device_single_card_slot_interface<device_a2kbd_interface>(mconfig, *this)
+	, m_b_callback(*this)
+	, m_strobe_callback(*this)
+	, m_reset_callback(*this)
+	, m_mode_callback(*this)
+{
+}
+
+void a2kbd_connector_device::default_options(device_slot_interface &slot)
+{
+	//slot.option_add("okbd", APPLE2_OKBD);
+	slot.option_add("nkbd", APPLE2_NKBD);
+	slot.option_add("am100", AM100_KEYBOARD);
+	slot.option_add("ivelultr", IVEL_ULTRA_KEYBOARD);
+	slot.option_add("kb200", A2KBD_KB200);
+	slot.option_add("tk10", A2KBD_TK10);
+	slot.option_add("videnh2", A2KBD_VIDENH2);
+	slot.option_add("uniap2ti", A2KBD_UNIAP2TI);
+}
+
+void a2kbd_connector_device::device_config_complete()
+{
+	m_intf = get_card_device();
+}
+
+void a2kbd_connector_device::device_resolve_objects()
+{
+	if (m_intf)
+		m_intf->m_connector = this;
+}
+
+void a2kbd_connector_device::device_start()
+{
+}
+
+
+//**************************************************************************
+//  KEYBOARD DEVICE INTERFACE
+//**************************************************************************
+
+device_a2kbd_interface::device_a2kbd_interface(const machine_config &mconfig, device_t &device)
+	: device_interface(device, "a2kbd")
+	, m_connector(nullptr)
+{
+}
+
+device_a2kbd_interface::~device_a2kbd_interface()
+{
+}

src/devices/bus/a2kbd/a2kbd.h
@@ -1,0 +1,137 @@
+// license:BSD-3-Clause
+// copyright-holders:AJR
+/*********************************************************************
+
+    Apple II Keyboard Connector
+
+*********************************************************************/
+
+#ifndef MAME_BUS_A2KBD_A2KBD_H
+#define MAME_BUS_A2KBD_A2KBD_H
+
+#pragma once
+
+
+//**************************************************************************
+//  TYPE DEFINITIONS
+//**************************************************************************
+
+// forward declaration
+class device_a2kbd_interface;
+
+// ======================> a2kbd_connector_device
+
+class a2kbd_connector_device : public device_t, public device_single_card_slot_interface<device_a2kbd_interface>
+{
+	friend class device_a2kbd_interface;
+
+public:
+	// construction/destruction
+	a2kbd_connector_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
+
+	template <typename T>
+	a2kbd_connector_device(const machine_config &mconfig, const char *tag, device_t *owner, T &&opts, const char *dflt)
+		: a2kbd_connector_device(mconfig, tag, owner, 0U)
+	{
+		option_reset();
+		opts(*this);
+		set_default_option(dflt);
+		set_fixed(false);
+	}
+
+	// standard options
+	static void default_options(device_slot_interface &slot);
+
+	// output callback configuration
+	auto b_callback() { return m_b_callback.bind(); }
+	auto strobe_callback() { return m_strobe_callback.bind(); }
+	auto reset_callback() { return m_reset_callback.bind(); }
+	auto mode_callback() { return m_mode_callback.bind(); }
+
+	// shift and control key inputs (modification)
+	inline int shift_r();
+	inline int control_r();
+
+	// outputs to keyboard (modification)
+	inline void ack_w(int state);
+	inline void an3_w(int state);
+
+protected:
+	// device-level overrides
+	virtual void device_config_complete() override ATTR_COLD;
+	virtual void device_resolve_objects() override ATTR_COLD;
+	virtual void device_start() override ATTR_COLD;
+
+private:
+	// callback objects
+	devcb_write8 m_b_callback;
+	devcb_write_line m_strobe_callback;
+	devcb_write_line m_reset_callback;
+	devcb_write_line m_mode_callback;
+
+	// selected device
+	device_a2kbd_interface *m_intf;
+};
+
+// ======================> device_a2kbd_interface
+
+class device_a2kbd_interface : public device_interface
+{
+	friend class a2kbd_connector_device;
+
+public:
+	virtual ~device_a2kbd_interface();
+
+protected:
+	// construction/destruction
+	device_a2kbd_interface(const machine_config &mconfig, device_t &device);
+
+	// input overrides
+	virtual int shift_r() { return 0; }
+	virtual int control_r() { return 0; }
+
+	// optional output overrides
+	virtual void ack_w(int state) { }
+	virtual void an3_w(int state) { }
+
+	void b_w(u8 data) { m_connector->m_b_callback(data & 0x7f); }
+	void strobe_w(int state) { m_connector->m_strobe_callback(state); }
+	void reset_w(int state) { m_connector->m_reset_callback(state); }
+	void mode_w(int state) { m_connector->m_mode_callback(state); }
+
+private:
+	a2kbd_connector_device *m_connector;
+};
+
+// device type declaration
+DECLARE_DEVICE_TYPE(A2KBD_CONNECTOR, a2kbd_connector_device)
+
+//**************************************************************************
+//  INLINE FUNCTIONS
+//**************************************************************************
+
+int a2kbd_connector_device::shift_r()
+{
+	// Poll shift key(s) (0 = pressed, 1 = not pressed)
+	return (m_intf != nullptr) ? m_intf->shift_r() : 0;
+}
+
+int a2kbd_connector_device::control_r()
+{
+	// Poll control key(s) (0 = pressed, 1 = not pressed)
+	return (m_intf != nullptr) ? m_intf->control_r() : 1;
+}
+
+void a2kbd_connector_device::ack_w(int state)
+{
+	if (m_intf != nullptr)
+		m_intf->ack_w(state);
+}
+
+void a2kbd_connector_device::an3_w(int state)
+{
+	if (m_intf != nullptr)
+		m_intf->an3_w(state);
+}
+
+#endif // MAME_BUS_A2KBD_A2KBD_H

commit 1ba32aeed57f11b6b4b91e719ebd5bedc1ce1b32

apple/apple2video.cpp: Fixed 'dodo' and 'albert' regressions from f8ecff4. (Github #14999) [R. Belmont]

src/mame/apple/apple2.cpp
@@ -946,13 +946,13 @@ void apple2_state::apple2jp(machine_config &config)
 void apple2_state::dodo(machine_config &config)
 {
 	apple2p(config);
-	m_screen->set_screen_update(m_video, NAME((&a2_video_device::screen_update<a2_video_device::model::II, true, false>)));
+	m_screen->set_screen_update(m_video, NAME((&a2_video_device::screen_update<a2_video_device::model::DODO, true, false>)));
 }
 
 void apple2_state::albert(machine_config &config)
 {
 	apple2p(config);
-	m_screen->set_screen_update(m_video, NAME((&a2_video_device::screen_update<a2_video_device::model::IVEL_ULTRA, false, true>)));
+	m_screen->set_screen_update(m_video, NAME((&a2_video_device::screen_update<a2_video_device::model::DODO, false, true>)));
 }
 
 void apple2_state::ivelultr(machine_config &config)

src/mame/apple/apple2video.cpp
@@ -474,7 +474,7 @@ unsigned a2_video_device::get_text_character(uint32_t code, int row)
 
 	/* look up the character data */
 	unsigned bits = m_char_ptr[code * 8 + row];
-	bits = (Model == model::IVEL_ULTRA && Invert && !Flip) ? (bits >> 1) : (bits & 0x7f);
+	bits = ((Model == model::IVEL_ULTRA && Invert && !Flip) || Model == model::DODO) ? (bits >> 1) : (bits & 0x7f);
 	bits ^= invert_mask;
 	return Flip ? reverse_7_bits[bits] : bits;
 }
@@ -1063,6 +1063,8 @@ template uint32_t a2_video_device::screen_update<a2_video_device::model::IIGS, t
 template uint32_t a2_video_device::screen_update<a2_video_device::model::II_J_PLUS, true, true>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 template uint32_t a2_video_device::screen_update<a2_video_device::model::IVEL_ULTRA, true, false>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 template uint32_t a2_video_device::screen_update<a2_video_device::model::IVEL_ULTRA, false, true>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+template uint32_t a2_video_device::screen_update<a2_video_device::model::DODO, true, false>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+template uint32_t a2_video_device::screen_update<a2_video_device::model::DODO, false, true>(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 
 static INPUT_PORTS_START( a2_vidconfig_composite );
 	PORT_START("a2_video_config")