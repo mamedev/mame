f990afa38175bd1a9c3dfd1334a7705ea9bd69df
flama12333 <143599905+flama12333@users.noreply.github.com>
2026-02-27 12:45:41 -0500

    change att time from hz and opll mhz clock pachislo_z80
    Currently are guessed.

--- src/mame/universal/pachislo_z80.cpp
+++ src/mame/universal/pachislo_z80.cpp
@@ -72,13 +72,13 @@ void pachislo_z80_state::pachislo_z80(machine_config &config)
 {
 	Z80(config, m_maincpu, 3'000'000); // clock not verified
 	m_maincpu->set_addrmap(AS_PROGRAM, &pachislo_z80_state::program_map);
-	m_maincpu->set_periodic_int(FUNC(pachislo_z80_state::irq0_line_hold), attotime::from_hz(4*60)); // source?
+	m_maincpu->set_periodic_int(FUNC(pachislo_z80_state::irq0_line_hold), attotime::from_hz(12*61)); // Guessed. Probably Wrong. source?
 
 	// TODO: other hw?
 
 	SPEAKER(config, "mono").front_center();
 
-	YM2413(config, "ym", 3'000'000).add_route(ALL_OUTPUTS, "mono", 1.0); // clock not verified
+	YM2413(config, "ym", 579545_MHz_XTAL).add_route(ALL_OUTPUTS, "mono", 1.0); // Guessed
 }
 
 
