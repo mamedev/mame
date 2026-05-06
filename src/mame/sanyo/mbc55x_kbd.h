0dc245f282153c6fa2b240cfdacd33d7945f85c3
Rick Companje <rick@companje.nl>
2026-05-06 17:02:49 +0200

    mame/mbc55x: restore keyboard device rom region

--- src/mame/sanyo/mbc55x_kbd.h
+++ src/mame/sanyo/mbc55x_kbd.h
@@ -34,6 +34,7 @@ protected:
 	// device_t implementation
 	virtual ioport_constructor device_input_ports() const override ATTR_COLD;
 	virtual void device_start() override ATTR_COLD;
+	virtual const tiny_rom_entry *device_rom_region() const override ATTR_COLD;
 	virtual void device_reset() override ATTR_COLD;
 	virtual void device_add_mconfig(machine_config &config) override ATTR_COLD;
 
