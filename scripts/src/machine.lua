dc3de67a07c3062fba16cd77257a8d6725760a09
Devin Acker <d@revenant1.net>
2026-06-04 15:18:53 -0400

    mb87013: move out of roland dir; akai/x7000.cpp: add mb87013, other small corrections (#15408)
    * mb87013: move out of roland dir, add to s700 and x7000
    
    * s612: correct DRAM size [skip ci]
    
    * s700, x7000: correct polarity of a couple of inputs

--- scripts/src/machine.lua
+++ scripts/src/machine.lua
@@ -2330,6 +2330,18 @@ end
 
 ---------------------------------------------------
 --
+--@src/devices/machine/mb87013.h,MACHINES["MB87013"] = true
+---------------------------------------------------
+
+if MACHINES["MB87013"] then
+	files {
+		MAME_DIR .. "src/devices/machine/mb87013.cpp",
+		MAME_DIR .. "src/devices/machine/mb87013.h",
+	}
+end
+
+---------------------------------------------------
+--
 --@src/devices/machine/mb87030.h,MACHINES["MB87030"] = true
 ---------------------------------------------------
 
