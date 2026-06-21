a466db146fea7289f8677397faf8f3666947624d
angelosa <lordkale4@gmail.com>
2026-06-21 14:34:27 +0200

    bus/pc8801/pc8801_31.cpp: bulk replace SASI -> SCSI wording

--- src/devices/bus/nscsi/pc8801_30.h
+++ src/devices/bus/nscsi/pc8801_30.h
@@ -25,6 +25,7 @@ public:
 	bool is_motor_on() { return image->exists(); }
 
 protected:
+	// TODO: device_type constructor for pcecd and CDR-35D
 	virtual void device_start() override ATTR_COLD;
 	virtual void device_reset() override ATTR_COLD;
 	virtual void device_add_mconfig(machine_config &config) override ATTR_COLD;
