7b6e993b1c4bbf3dc51dea78ba7a59a6b6891c19
angelosa <lordkale4@gmail.com>
2026-06-27 01:02:58 +0200

    comad/zerozone.cpp: audiocpu runs in IM1, no need for set_input_line_and_vector

--- src/mame/comad/zerozone.cpp
+++ src/mame/comad/zerozone.cpp
@@ -162,7 +162,7 @@ uint32_t zerozone_state::screen_update(screen_device &screen, bitmap_ind16 &bitm
 void zerozone_state::sound_w(uint8_t data)
 {
 	m_soundlatch->write(data);
-	m_audiocpu->set_input_line_and_vector(0, HOLD_LINE, 0xff); // Z80
+	m_audiocpu->set_input_line(0, HOLD_LINE); // Z80 IM1
 }
 
 
