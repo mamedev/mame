0da52ffaf2fda6e3242953e9db97dba6f8214603
angelosa <lordkale4@gmail.com>
2026-06-27 12:36:15 +0200

    taito/capr1.cpp, taito/capr2.cpp: guess release dates for csesame1 and csesame2

--- src/mame/taito/capr1.cpp
+++ src/mame/taito/capr1.cpp
@@ -289,4 +289,4 @@ ROM_END
 
 
 GAME( 1996, cspin2,    0, cspin2, cspin2, capr1_state, empty_init, ROT0, "Taito", "Capriccio Spin 2", MACHINE_NO_SOUND | MACHINE_NOT_WORKING | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK )
-GAME( 199?, csesame1,  0, cspin2, cspin2, capr1_state, empty_init, ROT0, "Taito", "Capriccio Sesame", MACHINE_NO_SOUND | MACHINE_NOT_WORKING | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK )
+GAME( 2000?, csesame1,  0, cspin2, cspin2, capr1_state, empty_init, ROT0, "Taito", "Capriccio Sesame", MACHINE_NO_SOUND | MACHINE_NOT_WORKING | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK ) // between E95 (manpukuh) and E98 (styphp)
