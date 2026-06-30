c6c70fb7a7815fe089c63f039fcfa2990a0ae7fb
angelosa <lordkale4@gmail.com>
2026-06-30 23:53:32 +0200

    skeleton/carnival37.cpp: bump RAMDAC colors, fix GFX decoding, add inputs, flip screen and coin counters

--- src/mame/skeleton/carnival37.cpp
+++ src/mame/skeleton/carnival37.cpp
@@ -16,11 +16,11 @@ YM2149F PSG
 reset button
 
 TODO:
-- correct GFX decode
-- inputs / outputs (remaining CPU ports)
+- hopper
+- player 2 inputs
+- verify if we aren't missing any inputs from the pinout (game has no service mode)
 - colors are very dark but seem to be dark on original snaps, too. BTANB?
 */
-
 
 #include "emu.h"
 
@@ -44,19 +44,21 @@ public:
 		driver_device(mconfig, type, tag),
 		m_maincpu(*this, "maincpu"),
 		m_gfxdecode(*this, "gfxdecode"),
+		m_ramdac(*this, "ramdac"),
 		m_tileram(*this, "tileram"),
 		m_attrram(*this, "attrram")
 	{ }
 
 	void carniv37(machine_config &config) ATTR_COLD;
 
+	void init_gfx() ATTR_COLD;
 protected:
 	virtual void video_start() override ATTR_COLD;
 
 private:
 	required_device<tmpz84c011_device> m_maincpu;
 	required_device<gfxdecode_device> m_gfxdecode;
-
+	required_device<ramdac_device> m_ramdac;
 	required_shared_ptr<uint8_t> m_tileram;
 	required_shared_ptr<uint8_t> m_attrram;
 
@@ -99,10 +101,7 @@ void carnival37_state::attrram_w(offs_t offset, uint8_t data)
 
 uint32_t carnival37_state::screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect)
 {
-	bitmap.fill(rgb_t::black(), cliprect);
-
 	m_tilemap->draw(screen, bitmap, cliprect, 0, 0);
-
 	return 0;
 }
 
@@ -127,42 +126,64 @@ void carnival37_state::io_map(address_map &map)
 	map(0xa2, 0xa2).w("ramdac", FUNC(ramdac_device::mask_w));
 }
 
+// writes 4 bit data over a regular RGB666?
 void carnival37_state::ramdac_map(address_map &map)
 {
-	map(0x000, 0x2ff).rw("ramdac", FUNC(ramdac_device::ramdac_pal_r), FUNC(ramdac_device::ramdac_rgb666_w));
+	map(0x000, 0x2ff).lrw8(
+		NAME([this] (offs_t offset)  {
+			return m_ramdac->ramdac_pal_r(offset) << 2;
+		}),
+		NAME([this] (offs_t offset, u8 data) {
+			m_ramdac->ramdac_rgb666_w(offset, data << 2);
+		})
+	);
 }
 
 
-static INPUT_PORTS_START( carniv37 ) // TODO: do inputs once GFX is correctly decoded
+static INPUT_PORTS_START( carniv37 )
 	PORT_START("IN0")
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_PLAYER(2)
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_PLAYER(2)
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_BUTTON3 ) PORT_PLAYER(2)
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_BUTTON4 ) PORT_PLAYER(2)
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_BUTTON5 ) PORT_PLAYER(2)
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_BUTTON6 ) PORT_PLAYER(2)
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_BUTTON7 ) PORT_PLAYER(2)
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_BUTTON8 ) PORT_PLAYER(2)
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_START1 )
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_GAMBLE_LOW )
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_GAMBLE_BET )
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_GAMBLE_TAKE )
+	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_GAMBLE_D_UP )
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_GAMBLE_HIGH )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_UNKNOWN )
 
 	PORT_START("IN1")
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_PLAYER(3)
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_PLAYER(3)
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_BUTTON3 ) PORT_PLAYER(3)
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_BUTTON4 ) PORT_PLAYER(3)
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_BUTTON5 ) PORT_PLAYER(3)
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_BUTTON6 ) PORT_PLAYER(3)
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_BUTTON7 ) PORT_PLAYER(3)
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_BUTTON8 ) PORT_PLAYER(3)
+	// TODO: IPT_GAMBLE has no support for PORT_COCKTAIL/PORT_PLAYER(2) (same mapping as above)
+	PORT_DIPNAME( 0x01, 0x01, "IN1" )
+	PORT_DIPSETTING(    0x01, DEF_STR( Off ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+	PORT_DIPNAME( 0x02, 0x02, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(    0x02, DEF_STR( Off ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(    0x04, DEF_STR( Off ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+	PORT_DIPNAME( 0x08, 0x08, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(    0x08, DEF_STR( Off ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+	PORT_DIPNAME( 0x10, 0x10, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(    0x10, DEF_STR( Off ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+	PORT_DIPNAME( 0x20, 0x20, DEF_STR( Unknown ) )
+	PORT_DIPSETTING(    0x20, DEF_STR( Off ) )
+	PORT_DIPSETTING(    0x00, DEF_STR( On ) )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_UNKNOWN )
 
 	PORT_START("IN2")
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_PLAYER(4) // coin 1
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_PLAYER(4) // coin 2
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_BUTTON3 ) PORT_PLAYER(4) // coin 3 / keyin?
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_BUTTON4 ) PORT_PLAYER(4) // coin 4 / keyin?
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_BUTTON5 ) PORT_PLAYER(4)
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_BUTTON6 ) PORT_PLAYER(4)
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_BUTTON7 ) PORT_PLAYER(4) // payout (shows hopper empty message)
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_BUTTON8 ) PORT_PLAYER(4) // keyout?
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_GAMBLE_KEYIN ) // has no coin counter write
+	// all coin chutes are very sensible (prone to "coinjam"s)
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_COIN1 ) PORT_IMPULSE(1)
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_COIN2 ) PORT_IMPULSE(1)
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_COIN3 ) PORT_IMPULSE(1)
+	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_GAMBLE_BOOK ) // PORT_NAME("Analyzer")
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNKNOWN ) // hopper status? (shows hopper empty message)
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_GAMBLE_PAYOUT )
 
 	PORT_START("DSW1")
 	PORT_DIPUNKNOWN_DIPLOC(0x01, 0x01, "SW1:1")
@@ -186,8 +207,8 @@ static INPUT_PORTS_START( carniv37 ) // TODO: do inputs once GFX is correctly de
 INPUT_PORTS_END
 
 
-static GFXDECODE_START( gfx ) // TODO: wrong
-	GFXDECODE_ENTRY( "tiles", 0, gfx_8x8x4_packed_msb, 0, 1 )
+static GFXDECODE_START( gfx )
+	GFXDECODE_ENTRY( "decoded_tiles", 0, gfx_8x8x4_packed_msb, 0, 1 )
 GFXDECODE_END
 
 
@@ -208,7 +229,16 @@ void carnival37_state::carniv37(machine_config &config)
 	m_maincpu->in_pa_callback().set_ioport("IN0");
 	m_maincpu->in_pb_callback().set_ioport("IN1");
 	m_maincpu->in_pc_callback().set_ioport("IN2");
-	m_maincpu->out_pd_callback().set([this] (uint8_t data) { logerror("%s CPU port D write: %02x\n", machine().describe_context(), data); }); // seems coin / payout counters, hopper mottor
+	m_maincpu->out_pd_callback().set([this] (uint8_t data) {
+		machine().bookkeeping().coin_counter_w(0, BIT(~data, 0));
+		machine().bookkeeping().coin_counter_w(1, BIT(~data, 1));
+		machine().bookkeeping().coin_counter_w(2, BIT(~data, 2));
+		// bit 3 hopper motor, active low
+		// bit 4 unknown
+		// bit 5-6 high at payout
+		flip_screen_set(BIT(data, 7));
+		logerror("%s CPU port D write: %02x\n", machine().describe_context(), data);
+	});
 
 	// video hardware
 	screen_device &screen(SCREEN(config, "screen", SCREEN_TYPE_RASTER));
@@ -218,13 +248,13 @@ void carnival37_state::carniv37(machine_config &config)
 	screen.set_visarea(0*8, 36*8-1, 0*8, 28*8-1);
 	screen.set_screen_update(FUNC(carnival37_state::screen_update));
 
-	HD6845S(config, "crtc", 12_MHz_XTAL / 3); // actually HD6445P4, a superset according to datasheet
+	HD6845S(config, "crtc", 12_MHz_XTAL / 3); // actually HD6445P4 CRTC-II
 
 	GFXDECODE(config, m_gfxdecode, "palette", gfx);
 
 	PALETTE(config, "palette").set_entries(0x100);
 
-	RAMDAC(config, "ramdac", "palette").set_addrmap(0, &carnival37_state::ramdac_map);
+	RAMDAC(config, m_ramdac, "palette").set_addrmap(0, &carnival37_state::ramdac_map);
 
 	// sound hardware
 	SPEAKER(config, "mono").front_center();
@@ -235,17 +265,34 @@ void carnival37_state::carniv37(machine_config &config)
 	ym.add_route(ALL_OUTPUTS, "mono", 1.00);
 }
 
+void carnival37_state::init_gfx()
+{
+	u8 *tiles = memregion("tiles")->base();
+	u8 *decoded = memregion("decoded_tiles")->base();
+
+	for (int base_addr = 0; base_addr < 0x10000; base_addr += 0x4000)
+	{
+		for (int i = 0; i < 0x2000; i ++)
+		{
+			decoded[base_addr | (i << 1) | 0] = tiles[base_addr | i | 0x0000];
+			decoded[base_addr | (i << 1) | 1] = tiles[base_addr | i | 0x2000];
+		}
+	}
+}
 
 ROM_START( carniv37 )
 	ROM_REGION( 0x8000, "maincpu", 0 )
 	ROM_LOAD( "256.ic28", 0x0000, 0x8000, CRC(7d7d3f2a) SHA1(76223d42995f3925379d45ba0524541380c4f43b) ) // handwritten label
 
 	ROM_REGION( 0x10000, "tiles", 0 )
-	ROM_LOAD( "512.ic40", 0x00000, 0x10000, CRC(789a2336) SHA1(3f441f063c113b787aff919259ae974b3b4e4a62) ) // handwritten label
+	ROM_LOAD( "512.ic40", 0x0000, 0x10000, CRC(789a2336) SHA1(3f441f063c113b787aff919259ae974b3b4e4a62) ) // handwritten label
+
+	ROM_REGION( 0x10000, "decoded_tiles", ROMREGION_ERASEFF)
+
 	// ic41 not populated
 ROM_END
 
 } // anonymous namespace
 
 
-GAME( 1992, carniv37, 0, carniv37, carniv37, carnival37_state, empty_init, ROT0, "Able", "Carnival 37", MACHINE_IMPERFECT_GRAPHICS | MACHINE_NOT_WORKING )
+GAME( 1992, carniv37, 0, carniv37, carniv37, carnival37_state, init_gfx, ROT0, "Able", "Carnival 37", MACHINE_IMPERFECT_GRAPHICS | MACHINE_NOT_WORKING | MACHINE_NO_COCKTAIL )
