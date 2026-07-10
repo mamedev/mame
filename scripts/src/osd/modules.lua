b4a75ce0a481153b312316bd047b16a216a719f4
Andrei I. Holub <andrei.holub@gmail.com>
2026-07-10 13:22:31 -0400

    revet disabled OPENGL

--- scripts/src/osd/modules.lua
+++ scripts/src/osd/modules.lua
@@ -625,7 +625,7 @@ newoption {
 }
 
 if not _OPTIONS["NO_OPENGL"] then
-	if _OPTIONS["targetos"] == "android" or _OPTIONS["targetos"]=="asmjs" then
+	if _OPTIONS["targetos"] == "android" then
 		_OPTIONS["NO_OPENGL"] = "1"
 	else
 		_OPTIONS["NO_OPENGL"] = "0"
