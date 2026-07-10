02815c6ce6f12216993dad721df0e215d8df5a41
AJR <ariedlmayer@gmail.com>
2026-07-09 22:54:42 -0400

    ussr/b2m.cpp, ussr/mikro80.cpp, ussr/vector06.cpp: Consolidate each driver in single file

--- src/mame/ussr/b2m.cpp
+++ src/mame/ussr/b2m.cpp
@@ -33,12 +33,371 @@ Need a schematic so that the fdc could be repaired.
 
 #include "emu.h"
 #include "cpu/i8085/i8085.h"
-#include "machine/i8251.h"
-#include "b2m.h"
 #include "formats/smx_dsk.h"
+#include "imagedev/floppy.h"
+#include "machine/i8251.h"
+#include "machine/i8255.h"
+#include "machine/pic8259.h"
+#include "machine/pit8253.h"
+#include "machine/ram.h"
+#include "machine/wd_fdc.h"
+#include "sound/spkrdev.h"
+#include "emupal.h"
 #include "screen.h"
 #include "softlist_dev.h"
 #include "speaker.h"
+
+
+namespace {
+
+class b2m_state : public driver_device
+{
+public:
+	b2m_state(const machine_config &mconfig, device_type type, const char *tag)
+		: driver_device(mconfig, type, tag)
+		, m_maincpu(*this, "maincpu")
+		, m_speaker(*this, "speaker")
+		, m_pit(*this, "pit")
+		, m_ram(*this, RAM_TAG)
+		, m_palette(*this, "palette")
+		, m_fdc(*this, "fdc")
+		, m_fd(*this, "fdc:%u", 0U)
+		, m_pic(*this, "pic")
+	{ }
+
+	void b2mrom(machine_config &config) ATTR_COLD;
+	void b2m(machine_config &config) ATTR_COLD;
+
+private:
+	uint8_t keyboard_r(offs_t offset);
+	void palette_w(offs_t offset, uint8_t data);
+	uint8_t palette_r(offs_t offset);
+	void localmachine_w(uint8_t data);
+	uint8_t localmachine_r();
+
+	void b2m_palette(palette_device &palette) const;
+	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	INTERRUPT_GEN_MEMBER(vblank_interrupt);
+	void pit_out1(int state);
+	void ppi1_porta_w(uint8_t data);
+	void ppi1_portb_w(uint8_t data);
+	void ppi1_portc_w(uint8_t data);
+	uint8_t ppi1_portb_r();
+	void ppi2_portc_w(uint8_t data);
+	uint8_t fdc_status_hack_r();
+	uint8_t romdisk_porta_r();
+	void romdisk_portb_w(uint8_t data);
+	void romdisk_portc_w(uint8_t data);
+	void fdc_drq(int state);
+	static void b2m_floppy_formats(format_registration &fr);
+
+	void b2m_io(address_map &map) ATTR_COLD;
+	void b2m_mem(address_map &map) ATTR_COLD;
+	void b2m_rom_io(address_map &map) ATTR_COLD;
+	void machine_start() override ATTR_COLD;
+	void machine_reset() override ATTR_COLD;
+
+	void postload();
+	void set_bank(int bank);
+
+	uint8_t m_porta = 0U;
+	uint8_t m_video_scroll = 0U;
+	uint8_t m_portc = 0U;
+
+	bool m_drq_interlock = false;
+
+	uint8_t m_video_page = 0U;
+
+	uint8_t m_romdisk_lsb = 0U;
+	uint8_t m_romdisk_msb = 0U;
+
+	uint8_t m_color[4]{};
+	uint8_t m_localmachine = 0U;
+	uint8_t m_vblank_state = 0U;
+
+	required_device<cpu_device> m_maincpu;
+	required_device<speaker_sound_device> m_speaker;
+	required_device<pit8253_device> m_pit;
+	required_device<ram_device> m_ram;
+	required_device<palette_device> m_palette;
+
+	/* devices */
+	optional_device<fd1793_device> m_fdc;
+	optional_device_array<floppy_connector, 2> m_fd;
+	optional_device<pic8259_device> m_pic;
+};
+
+
+uint8_t b2m_state::keyboard_r(offs_t offset)
+{
+	uint8_t key = 0x00;
+	if (offset < 0x100) {
+		if ((offset & 0x01)!=0) { key |= ioport("LINE0")->read(); }
+		if ((offset & 0x02)!=0) { key |= ioport("LINE1")->read(); }
+		if ((offset & 0x04)!=0) { key |= ioport("LINE2")->read(); }
+		if ((offset & 0x08)!=0) { key |= ioport("LINE3")->read(); }
+		if ((offset & 0x10)!=0) { key |= ioport("LINE4")->read(); }
+		if ((offset & 0x20)!=0) { key |= ioport("LINE5")->read(); }
+		if ((offset & 0x40)!=0) { key |= ioport("LINE6")->read(); }
+		if ((offset & 0x80)!=0) { key |= ioport("LINE7")->read(); }
+	} else {
+		if ((offset & 0x01)!=0) { key |= ioport("LINE8")->read(); }
+		if ((offset & 0x02)!=0) { key |= ioport("LINE9")->read(); }
+		if ((offset & 0x04)!=0) { key |= ioport("LINE10")->read(); }
+	}
+	return key;
+}
+
+
+void b2m_state::set_bank(int bank)
+{
+	uint8_t *rom;
+	address_space &space = m_maincpu->space(AS_PROGRAM);
+	uint8_t *ram = m_ram->pointer();
+
+	space.install_ram(0x0000, 0xffff, ram);
+
+	rom = memregion("maincpu")->base();
+	switch(bank) {
+	case 0 :
+	case 1 :
+		space.unmap_write(0xe000, 0xffff);
+		space.install_rom(0xe000, 0xffff, rom);
+		break;
+	case 2 :
+		space.unmap_write(0x2800, 0x2fff);
+		space.install_read_handler(0x2800, 0x2fff, read8sm_delegate(*this, FUNC(b2m_state::keyboard_r)));
+		space.install_ram(0x3000, 0x6fff, ram + 0x10000);
+		space.unmap_write(0xe000, 0xffff);
+		space.install_rom(0xe000, 0xffff, rom);
+		break;
+	case 3 :
+		space.unmap_write(0x2800, 0x2fff);
+		space.install_read_handler(0x2800, 0x2fff, read8sm_delegate(*this, FUNC(b2m_state::keyboard_r)));
+		space.install_ram(0x3000, 0x6fff, ram + 0x14000);
+		space.unmap_write(0xe000, 0xffff);
+		space.install_rom(0xe000, 0xffff, rom);
+		break;
+	case 4 :
+		space.install_read_handler(0x2800, 0x2fff, read8sm_delegate(*this, FUNC(b2m_state::keyboard_r)));
+		space.install_ram(0x3000, 0x6fff, ram + 0x18000);
+		space.unmap_write(0xe000, 0xffff);
+		space.install_rom(0xe000, 0xffff, rom);
+		break;
+	case 5 :
+		space.unmap_write(0x2800, 0x2fff);
+		space.install_read_handler(0x2800, 0x2fff, read8sm_delegate(*this, FUNC(b2m_state::keyboard_r)));
+		space.install_ram(0x3000, 0x6fff, ram + 0x1c000);
+		space.unmap_write(0xe000, 0xffff);
+		space.install_rom(0xe000, 0xffff, rom);
+		break;
+	case 6 :
+		break;
+	case 7 :
+		space.unmap_write(0x0000, 0xffff);
+		space.unmap_read(0x0000, 0xffff);
+
+		space.install_rom(0x0000, 0x1fff, rom);
+		space.install_rom(0x2800, 0x2fff, rom);
+		space.install_rom(0x3000, 0x4fff, rom);
+		space.install_rom(0x7000, 0x8fff, rom);
+		space.install_rom(0xe000, 0xffff, rom);
+		break;
+	}
+}
+
+
+void b2m_state::pit_out1(int state)
+{
+	m_speaker->level_w(state);
+}
+
+void b2m_state::ppi1_porta_w(uint8_t data)
+{
+	m_porta = data;
+}
+
+void b2m_state::ppi1_portb_w(uint8_t data)
+{
+	m_video_scroll = data;
+}
+
+void b2m_state::ppi1_portc_w(uint8_t data)
+{
+	m_portc = data;
+	set_bank(m_portc & 7);
+	m_video_page = BIT(m_portc, 7);
+}
+
+uint8_t b2m_state::ppi1_portb_r()
+{
+	return m_video_scroll;
+}
+
+void b2m_state::fdc_drq(int state)
+{
+	/* Clears HALT state of CPU when data is ready to read */
+	if (state && m_drq_interlock)
+		m_maincpu->set_input_line(INPUT_LINE_HALT, CLEAR_LINE);
+}
+
+
+void b2m_state::ppi2_portc_w(uint8_t data)
+{
+	uint8_t drive = BIT(~data, 1);
+	uint8_t side  = BIT(~data, 0);
+
+	floppy_image_device *floppy = nullptr;
+	if (m_fd[drive].found())
+		floppy = m_fd[drive]->get_device();
+
+	if (floppy)
+		floppy->mon_w(0);
+	m_fdc->set_floppy(floppy);
+
+	if (floppy)
+		floppy->ss_w(side);
+
+	/*
+	    When bit 5 is set CPU is in HALT state and stay there until
+	    DRQ is triggered from floppy side
+	*/
+	m_drq_interlock = BIT(data, 5);
+}
+
+uint8_t b2m_state::fdc_status_hack_r()
+{
+	if (!machine().side_effects_disabled() && m_drq_interlock && !m_fdc->drq_r())
+		m_maincpu->set_input_line(INPUT_LINE_HALT, ASSERT_LINE);
+	return m_fdc->status_r();
+}
+
+uint8_t b2m_state::romdisk_porta_r()
+{
+	uint8_t *romdisk = memregion("maincpu")->base() + 0x2000;
+	return romdisk[m_romdisk_msb*256+m_romdisk_lsb];
+}
+
+void b2m_state::romdisk_portb_w(uint8_t data)
+{
+	m_romdisk_lsb = data;
+}
+
+void b2m_state::romdisk_portc_w(uint8_t data)
+{
+	m_romdisk_msb = data & 0x7f;
+}
+
+void b2m_state::palette_w(offs_t offset, uint8_t data)
+{
+	uint8_t b = BIT(~data, 6, 2) * 0x55;
+	uint8_t g = BIT(~data, 4, 2) * 0x55;
+	uint8_t r = BIT(~data, 2, 2) * 0x55;
+
+	uint8_t bw = BIT(~data, 0, 2) * 0x55;
+
+	m_color[offset & 3] = data;
+
+	if (ioport("MONITOR")->read()==1)
+		m_palette->set_pen_color(offset, r, g, b);
+	else
+		m_palette->set_pen_color(offset, bw, bw, bw);
+}
+
+uint8_t b2m_state::palette_r(offs_t offset)
+{
+	return m_color[offset];
+}
+
+void b2m_state::localmachine_w(uint8_t data)
+{
+	m_localmachine = data;
+}
+
+uint8_t b2m_state::localmachine_r()
+{
+	return m_localmachine;
+}
+
+void b2m_state::postload()
+{
+	set_bank(m_portc & 7);
+}
+
+void b2m_state::machine_start()
+{
+	/* register for state saving */
+	save_item(NAME(m_porta));
+	save_item(NAME(m_video_scroll));
+	save_item(NAME(m_portc));
+	save_item(NAME(m_video_page));
+	save_item(NAME(m_romdisk_lsb));
+	save_item(NAME(m_romdisk_msb));
+	save_item(NAME(m_color));
+	save_item(NAME(m_localmachine));
+	save_item(NAME(m_vblank_state));
+
+	machine().save().register_postload(save_prepost_delegate(FUNC(b2m_state::postload), this));
+}
+
+INTERRUPT_GEN_MEMBER(b2m_state::vblank_interrupt)
+{
+	m_vblank_state++;
+	if (m_vblank_state>1) m_vblank_state=0;
+	m_pic->ir0_w(m_vblank_state);
+}
+
+void b2m_state::machine_reset()
+{
+	m_vblank_state = 0;
+	set_bank(7);
+
+	m_maincpu->set_input_line(INPUT_LINE_HALT, CLEAR_LINE);
+}
+
+uint32_t b2m_state::screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect)
+{
+	u8 const *const ram = m_ram->pointer();
+
+	for (int x = 0; x < 48; x++)
+	{
+		for (int y = 0; y < 256; y++)
+		{
+			u16 const t = x*256 + ((y + m_video_scroll) & 0xff);
+			u8 code1, code2;
+			if (m_video_page==0)
+			{
+				code1 = ram[0x11000 + t];
+				code2 = ram[0x15000 + t];
+			}
+			else
+			{
+				code1 = ram[0x19000 + t];
+				code2 = ram[0x1d000 + t];
+			}
+			for (int b = 7; b >= 0; b--)
+			{
+				u8 const col = (BIT(code2, b)<<1) + BIT(code1, b);
+				bitmap.pix(y, x*8+b) =  col;
+			}
+		}
+	}
+
+	return 0;
+}
+
+void b2m_state::b2m_palette(palette_device &palette) const
+{
+	static constexpr rgb_t b2m_pens[4] = {
+		{ 0x00, 0x00, 0x00 }, // 0
+		{ 0x00, 0x00, 0x00 }, // 1
+		{ 0x00, 0x00, 0x00 }, // 2
+		{ 0x00, 0x00, 0x00 }, // 3
+	};
+
+	palette.set_pen_colors(0, b2m_pens);
+}
+
 
 /* Address maps */
 void b2m_state::b2m_mem(address_map &map)
@@ -285,6 +644,7 @@ ROM_START( b2mrom )
 	ROM_LOAD( "ramdos.sys", 0x2000, 0x60c0, CRC(91ed6df0) SHA1(4fd040f2647a6b7930c330c75560a035027d0606) )
 ROM_END
 
+} // anonymous namespace
 
 /* Driver */
 
