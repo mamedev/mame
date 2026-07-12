b2c2e3d00e49140685bc640c1f7b5f1f7321d07f
mamehaze <140764005+mamehaze@users.noreply.github.com>
2026-07-12 18:15:59 +0100

    some Mobigo changes (moved Mobigo 2 to it's own driver, added a missing boot ROM, added a French MobiGo 1 set) (#15684)
    - added missing SPI ROM to mobigo2 set, this appears to be the boot ROM which explains why the NAND had no GPnandnand header
    - identified that (at least some) Mobigo 1 units have an SPI ROM too, but it isn't used for booting, but instead either user content, or built in games
    - moved mobigo2 to it's own file due to the differences in hardware setup compared to the original mobigo
    - moved a few things out of generalplus_gpl16250_spi.cpp that do not actually appear to be gpl16250 after further research on them
    
    new NOT WORKING clones
    --------
    MobiGo (France) [Team Europe]

---
+++ src/mame/tvgames/generalplus_gpl16250_spi.h
@@ -0,0 +1,54 @@
+// license:BSD-3-Clause
+// copyright-holders:David Haywood
+#ifndef MAME_TVGAMES_GENERALPLUS_GPL16250_SPI_H
+#define MAME_TVGAMES_GENERALPLUS_GPL16250_SPI_H
+
+#pragma once
+
+#include "generalplus_gpl16250.h"
+
+#include "bus/generic/carts.h"
+#include "bus/generic/slot.h"
+#include "machine/generalplus_gpl162xx_soc.h"
+
+#include "screen.h"
+#include "speaker.h"
+
+
+class generalplus_gpspispi_game_state : public gcm394_game_state
+{
+public:
+	generalplus_gpspispi_game_state(const machine_config &mconfig, device_type type, const char *tag) :
+		gcm394_game_state(mconfig, type, tag)
+	{
+	}
+
+	void generalplus_gpspispi(machine_config &config) ATTR_COLD;
+
+	void init_spi() ATTR_COLD;
+
+protected:
+	virtual void machine_start() override ATTR_COLD;
+	virtual void machine_reset() override ATTR_COLD;
+};
+
+
+class generalplus_gpspispi_bkrankp_game_state : public generalplus_gpspispi_game_state
+{
+public:
+	generalplus_gpspispi_bkrankp_game_state(const machine_config &mconfig, device_type type, const char *tag) :
+		generalplus_gpspispi_game_state(mconfig, type, tag),
+		m_cart(*this, "cartslot")
+	{
+	}
+
+	void generalplus_gpspispi_bkrankp(machine_config &config) ATTR_COLD;
+
+protected:
+	DECLARE_DEVICE_IMAGE_LOAD_MEMBER(cart_load);
+
+	required_device<generic_slot_device> m_cart;
+};
+
+
+#endif // MAME_TVGAMES_GENERALPLUS_GPL16250_SPI_H
