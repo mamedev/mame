ae7c6288cdc0cf546505d810e156e721556d21b4
AJR <ariedlmayer@gmail.com>
2026-07-13 10:41:30 -0400

    tvgames/bk139in1.cpp: An attempt at partial decryption

--- src/mame/tvgames/bk139in1.cpp
+++ src/mame/tvgames/bk139in1.cpp
@@ -5,6 +5,7 @@
 // the NES emulation seems comparable to other SPG293 platforms
 
 #include "emu.h"
+#include "cpu/score/score.h"
 
 #include "screen.h"
 #include "speaker.h"
@@ -21,12 +22,16 @@ public:
 
 	void bk139in1(machine_config &config) ATTR_COLD;
 
+	void init_bk139in1() ATTR_COLD;
+
 protected:
 	virtual void machine_start() override ATTR_COLD;
 	virtual void machine_reset() override ATTR_COLD;
 
 private:
 	uint32_t screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
+
+	void mem_map(address_map &map) ATTR_COLD;
 
 	required_device<screen_device> m_screen;
 };
@@ -44,11 +49,18 @@ void bk139in1_state::machine_reset()
 {
 }
 
+void bk139in1_state::mem_map(address_map &map)
+{
+	map(0x00000000, 0x001fffff).rom();
+}
+
 static INPUT_PORTS_START( bk139in1 )
 INPUT_PORTS_END
 
 void bk139in1_state::bk139in1(machine_config &config)
 {
+	SCORE7(config, "maincpu", 27'000'000).set_addrmap(AS_PROGRAM, &bk139in1_state::mem_map);
+
 	SCREEN(config, m_screen, SCREEN_TYPE_RASTER);
 	m_screen->set_refresh_hz(60);
 	m_screen->set_size(320, 262);
@@ -93,6 +105,19 @@ ROM_START( starbuck )
 	ROM_LOAD( "by25q16est.bin", 0x0000, 0x200000,  CRC(926c1499) SHA1(edd88b11f350e0016ee7dc76e872f9ae8c00aa6c) )
 ROM_END
 
+void bk139in1_state::init_bk139in1()
+{
+	u32 *rom = &memregion("maincpu")->as_u32(0xc00 / 4);
+	u32 len = (memregion("maincpu")->bytes() - 0xc00) / 4;
+	for (u32 j = 0; j < len; j++)
+	{
+		rom[j] ^= 0x32383024;
+		rom[j] ^= bitswap<9>(j, 0, 1, 2, 3, 4, 5, 6, 7, 8);
+		rom[j] ^= bitswap<16>(j, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15) << 16;
+		// TODO: probably another bitswap on top of this
+	}
+}
+
 
 // This can be found listed as a ZHISHAN / Aojiao / Bornkid 32 Bit Preloaded 139-in-1 Handheld Game Console
 // but these just seem to be brands, manufacturer is unknown.
@@ -104,12 +129,12 @@ ROM_END
 // While those are GeneralPlus based platforms, it's possible the games were ported to something else, the SPI
 // appears to contain a filesystem, but data looks to be compressed / encrypted with no obvious code.
 // There is no GPspi header in the SPI ROM.
-CONS(202?, bk139in1,  0, 0, bk139in1, bk139in1, bk139in1_state, empty_init, "<unknown>", "BornKid 32 Bit Preloaded 139-in-1 Handheld Game Console", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
+CONS(202?, bk139in1,  0, 0, bk139in1, bk139in1, bk139in1_state, init_bk139in1, "<unknown>", "BornKid 32 Bit Preloaded 139-in-1 Handheld Game Console", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
 // same unknown hardware as above, fewer games
-CONS(2021, lxcyrace,  0, 0, bk139in1, bk139in1, bk139in1_state, empty_init, "Lexibook", "Cyber Arcade Racing (JL3150)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
-CONS(2021, lxcymsm,   0, 0, bk139in1, bk139in1, bk139in1_state, empty_init, "Lexibook", "Cyber Arcade Motion - Superman (JL3180SU)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
-CONS(2021, lxcympp,   0, 0, bk139in1, bk139in1, bk139in1_state, empty_init, "Lexibook", "Cyber Arcade Motion - Paw Patrol (JL3180PA)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
-CONS(2021, lxcymls,   0, 0, bk139in1, bk139in1, bk139in1_state, empty_init, "Lexibook", "Cyber Arcade Motion - Lilo & Stitch (JL3180D_01)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
-CONS(2021, dgun3944,  0, 0, bk139in1, bk139in1, bk139in1_state, empty_init, "dreamGEAR", "My Arcade All Star Sports (Pixel Pocket, DGUNL3944)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
+CONS(2021, lxcyrace,  0, 0, bk139in1, bk139in1, bk139in1_state, init_bk139in1, "Lexibook", "Cyber Arcade Racing (JL3150)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
+CONS(2021, lxcymsm,   0, 0, bk139in1, bk139in1, bk139in1_state, init_bk139in1, "Lexibook", "Cyber Arcade Motion - Superman (JL3180SU)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
+CONS(2021, lxcympp,   0, 0, bk139in1, bk139in1, bk139in1_state, init_bk139in1, "Lexibook", "Cyber Arcade Motion - Paw Patrol (JL3180PA)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
+CONS(2021, lxcymls,   0, 0, bk139in1, bk139in1, bk139in1_state, init_bk139in1, "Lexibook", "Cyber Arcade Motion - Lilo & Stitch (JL3180D_01)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
+CONS(2021, dgun3944,  0, 0, bk139in1, bk139in1, bk139in1_state, init_bk139in1, "dreamGEAR", "My Arcade All Star Sports (Pixel Pocket, DGUNL3944)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
 // sold by Starbucks China, contains a bunch of NES hacks (including a version of Super Mario Bros) probably running on an emulator
-CONS(2022, starbuck,  0, 0, bk139in1, bk139in1, bk139in1_state, empty_init, "Subor",     "Starbucks x Subor (OEM Q2, China)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
+CONS(2022, starbuck,  0, 0, bk139in1, bk139in1, bk139in1_state, init_bk139in1, "Subor",     "Starbucks x Subor (OEM Q2, China)", MACHINE_NOT_WORKING | MACHINE_IMPERFECT_SOUND)
