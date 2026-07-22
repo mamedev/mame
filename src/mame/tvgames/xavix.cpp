f7e2f08cb2dfb2a858de36992a5b5e190ad65bd6
mamehaze <140764005+mamehaze@users.noreply.github.com>
2026-07-22 13:17:23 +0100

    new WORKING machines (Video Challenger PL-1000 XaviX) (#15740)
    New working systems
    -------------------
    Video Challenger (PL-1000) [Team Europe, SRC543, David Haywood]
    
    * Improved XaviX sprite horizontal position handling, removed old hack, improving sprite rendering in many XaviX titles [David Haywood]

--- src/mame/tvgames/xavix.cpp
+++ src/mame/tvgames/xavix.cpp
@@ -1486,7 +1486,30 @@ static INPUT_PORTS_START( multiplt )
 	PORT_DIPSETTING(    0x02, "TV Output")
 INPUT_PORTS_END
 
+// for test mode hold P1 Button 1, P1 Button 2, P2 Button 2 on startup (currently fails the hardware tests)
+static INPUT_PORTS_START( pl1000 )
+	PORT_INCLUDE(xavix)
 
+	PORT_MODIFY("IN1")
+	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_PLAYER(1)
+	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_PLAYER(1)
+
+	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_BUTTON1 ) PORT_PLAYER(2)
+	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_BUTTON2 ) PORT_PLAYER(2)
+
+	PORT_START("GUN1_0")
+	PORT_BIT( 0xff, 0x80, IPT_LIGHTGUN_X ) PORT_CROSSHAIR(X, 1.0, 0.0, 0) PORT_SENSITIVITY(35) PORT_KEYDELTA(15) PORT_PLAYER(1)
+
+	PORT_START("GUN1_1")
+	PORT_BIT( 0xff, 0x80, IPT_LIGHTGUN_Y ) PORT_CROSSHAIR(Y, 1.0, 0.0, 0) PORT_SENSITIVITY(35) PORT_KEYDELTA(15) PORT_PLAYER(1)
+
+	PORT_START("GUN2_0")
+	PORT_BIT( 0xff, 0x80, IPT_LIGHTGUN_X ) PORT_CROSSHAIR(X, 1.0, 0.0, 0) PORT_SENSITIVITY(35) PORT_KEYDELTA(15) PORT_PLAYER(2)
+
+	PORT_START("GUN2_1")
+	PORT_BIT( 0xff, 0x80, IPT_LIGHTGUN_Y ) PORT_CROSSHAIR(Y, 1.0, 0.0, 0) PORT_SENSITIVITY(35) PORT_KEYDELTA(15) PORT_PLAYER(2)
+
+INPUT_PORTS_END
 
 static INPUT_PORTS_START( tcarnavi )
 	PORT_INCLUDE(xavix)
@@ -1612,8 +1635,6 @@ void xavix_state::xavix(machine_config &config)
 	/* basic machine hardware */
 	XAVIX(config, m_maincpu, MAIN_CLOCK);
 	set_xavix_cpumaps(config);
-
-	TIMER(config, "scantimer").configure_scanline(FUNC(xavix_state::scanline_cb), "screen", 0, 1);
 
 	XAVIX_ADC(config, m_adc);
 	m_adc->read_0_callback().set_ioport("AN0");
@@ -2089,7 +2110,72 @@ void xavix_cart_state::xavix_cart_ddrfammt(machine_config &config)
 	SOFTWARE_LIST(config, "cart_list").set_original("ekara_cart").set_filter("FAMMAT");
 }
 
+void xavix_pl1000_state::xavix_pl1000(machine_config &config)
+{
+	xavix_2mb_nv(config);
+	TIMER(config, "scantimer").configure_scanline(FUNC(xavix_pl1000_state::scanline_cb), "screen", 0, 1);
+}
 
+u8 xavix_pl1000_state::lightgun_r(offs_t offset)
+{
+	u16 gunx = 0, guny = 0;
+
+	if ((m_io1_out & 0x80) == 0)
+	{
+		gunx = m_lightgun[0]->read();
+		guny = m_lightgun[1]->read();
+	}
+	else
+	{
+		gunx = m_lightgun2[0]->read();
+		guny = m_lightgun2[1]->read();
+	}
+
+	gunx += 0x20;
+	guny = guny * (224.0f / 256.0f);
+	guny += 0x20;
+
+	switch (offset)
+	{
+	case 0x00:
+		return gunx;
+	case 0x01:
+		return gunx >> 8;
+	case 0x02:
+		return guny;
+
+	}
+	return 0x00;
+}
+
+void xavix_pl1000_state::machine_start()
+{
+	xavix_state::machine_start();
+	save_item(NAME(m_io1_out));
+}
+
+void xavix_pl1000_state::machine_reset()
+{
+	xavix_state::machine_reset();
+	m_io1_out = 0x00;
+}
+
+TIMER_DEVICE_CALLBACK_MEMBER(xavix_pl1000_state::scanline_cb)
+{
+	// this needs 8 ioevent_trg08 triggers per frame to read the guns
+	// where are these coming from?
+	//
+	// guns are read on alternate frames?
+	// set by bit 0x80 of io1
+
+	if ((param >= 40) && (param <= 110))
+	{
+		if ((param % 10) == 0)
+		{
+			ioevent_trg08(1);
+		}
+	}
+}
 
 void xavix_state::init_xavix()
 {
@@ -2102,6 +2188,21 @@ void xavix_state::init_xavix_slowenv()
 	init_xavix();
 	m_default_audio_tempo_override = 0x40;
 }
+
+void xavix_pl1000_state::init_pl1000()
+{
+	init_xavix();
+
+	// this could be the earliest XaviX SoC as it doesn't seem to have an A15 line, meaning we need
+	// to mirror the ROM data so that everything appears in the correct place
+	u8 *plainrom = memregion("plainrom")->base();
+	for (int i = 0; i < m_rgnlen; i++)
+	{
+		u32 addr = bitswap<20>(i, 20, 19, 18, 17, 16, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+		m_rgn[i] = plainrom[addr]; 
+	}
+}
+
 
 /***************************************************************************
 
@@ -2686,6 +2787,14 @@ ROM_START( multiplt )
 	ROM_LOAD( "2en1multipilotefr.u2", 0x000000, 0x200000, CRC(77f2ebf6) SHA1(0181bc7d3b6ea5a4a247ffe02fee2cb4942c1623) )
 ROM_END
 
+ROM_START( pl1000 )
+	ROM_REGION( 0x200000, "bios", ROMREGION_ERASE00 )
+	// code is expanded from 'plainrom' region below
+
+	ROM_REGION( 0x100000, "plainrom", ROMREGION_ERASE00 )
+	ROM_LOAD( "video_challenger_pl-1000.u2", 0x000000, 0x100000, CRC(039e6f07) SHA1(d554976913beaa03828bd920d24ede6e433a8b4d) )
+ROM_END
+
 /* XaviX hardware titles (1st Generation)
 
     These use
@@ -2697,6 +2806,8 @@ ROM_END
     only new opcodes are callf and retf?
 
 */
+
+CONS( 1999, pl1000, 0,         0,  xavix_pl1000, pl1000,xavix_pl1000_state,      init_pl1000,    "Pelican Accessories / Mani Industries / Gameone Systems",             "Video Challenger (PL-1000)", MACHINE_IMPERFECT_GRAPHICS | MACHINE_IMPERFECT_SOUND )
 
 // product code 80-32705.
 // Some sites say 1997, but 1999 is what SSD had listed, and seems more fitting.
