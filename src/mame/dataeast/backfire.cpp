916e8dd94ab3ff2016e0cd9a634e267e78b14ecc
cam900 <dbtlrchl@naver.com>
2026-07-30 08:55:56 +0900

    dataeast/backfire.cpp: Fix sound routing (#15797)

--- src/mame/dataeast/backfire.cpp
+++ src/mame/dataeast/backfire.cpp
@@ -420,8 +420,8 @@ void backfire_state::backfire(machine_config &config)
 	SPEAKER(config, "rspeaker").front_center();
 
 	ymz280b_device &ymz(YMZ280B(config, "ymz", 28000000 / 2));
-	ymz.add_route(0, "lspeaker", 1.0, 0);
-	ymz.add_route(1, "rspeaker", 1.0, 1);
+	ymz.add_route(0, "lspeaker", 1.0);
+	ymz.add_route(1, "rspeaker", 1.0);
 }
 
 
