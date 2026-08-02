fac1935ff3b4be2c4b6a01cb695176d137d31737
AJFA <culichi@gmail.com>
2026-08-01 22:49:11 -0600

    aic6250: initialise DMA byte counter and stop the prefetch at the transfer length (#15825)

--- src/devices/machine/aic6250.cpp
+++ src/devices/machine/aic6250.cpp
@@ -172,6 +172,7 @@ void aic6250_device::device_start()
 	save_item(NAME(m_scsi_latch_data));
 
 	m_rev_cntrl = 0x02;
+	m_dma_count = 0;
 
 	m_state_timer = timer_alloc(FUNC(aic6250_device::state_loop), this);
 	m_state = IDLE;
@@ -1022,7 +1023,8 @@ void aic6250_device::back_w(int state)
 	if (!(m_control_reg_0 & R07W_P_MEM_CYCLE_REQ))
 	{
 		if (m_dma_cntrl & R05W_TRANSFER_DIR)
-			if (m_fifo.full() || m_dma_count < 8)
+			// stop the prefetch once the FIFO holds the rest of the transfer
+			if (m_fifo.full() || m_fifo.queue_length() >= m_dma_count)
 				m_state_timer->adjust(attotime::zero);
 			else
 				m_breq_cb(1);
