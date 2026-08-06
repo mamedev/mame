1db5d500e058a65622608141d197264222eac0e6
angelosa <lordkale4@gmail.com>
2026-08-06 13:52:21 +0200

    video/pc_vga.cpp: clear buffer on each frame for SVGA modes * avoid transitional artifacts for 320x240 modes

--- src/devices/video/pc_vga.cpp
+++ src/devices/video/pc_vga.cpp
@@ -1917,6 +1917,9 @@ void svga_device::svga_vh_rgb8(bitmap_rgb32 &bitmap, const rectangle &cliprect)
 	const uint16_t mask_comp = line_compare_mask();
 	int curr_addr = 0;
 
+	// clear the backbuffer for 320x240 linear modes, avoid transitional artifacts
+	bitmap.fill(0, cliprect);
+
 //  uint16_t line_length;
 //  if(vga.crtc.dw)
 //      line_length = vga.crtc.offset << 3;  // doubleword mode
@@ -1970,6 +1973,8 @@ void svga_device::svga_vh_rgb15(bitmap_rgb32 &bitmap, const rectangle &cliprect)
 	(void)curr_addr;
 	int yi = 0;
 
+	bitmap.fill(0, cliprect);
+
 	for (int addr = vga.crtc.start_addr << 2, line = 0; line < TLINES; line+=height, addr+=offset(), curr_addr+=offset())
 	{
 		uint32_t *const bitmapline = &bitmap.pix(line);
@@ -2010,6 +2015,8 @@ void svga_device::svga_vh_rgb16(bitmap_rgb32 &bitmap, const rectangle &cliprect)
 	int curr_addr = 0;
 	(void)curr_addr;
 	int yi = 0;
+
+	bitmap.fill(0, cliprect);
 
 	for (int addr = vga.crtc.start_addr << 2, line = 0; line < TLINES; line += height, addr += offset(), curr_addr += offset())
 	{
@@ -2052,6 +2059,8 @@ void svga_device::svga_vh_rgb24(bitmap_rgb32 &bitmap, const rectangle &cliprect)
 	(void)curr_addr;
 	int yi = 0;
 
+	bitmap.fill(0, cliprect);
+
 	for (int addr = vga.crtc.start_addr << 3, line=0; line < TLINES; line+=height, addr += offset(), curr_addr += offset())
 	{
 		uint32_t *const bitmapline = &bitmap.pix(line);
@@ -2092,6 +2101,8 @@ void svga_device::svga_vh_rgb32(bitmap_rgb32 &bitmap, const rectangle &cliprect)
 	int curr_addr = 0;
 	(void)curr_addr;
 	int yi = 0;
+
+	bitmap.fill(0, cliprect);
 
 	for (int addr = vga.crtc.start_addr << 2, line = 0; line < TLINES; line+=height, addr += offset(), curr_addr += offset())
 	{
