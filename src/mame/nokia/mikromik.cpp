1cfef7e8190e9f2b1b87ec05136447511b706e39
Curt Coder <curtcoder@mail.com>
2026-08-09 15:31:14 +0300

    nokia/mikromik: Wire up the MPSC. [Curt Coder]

--- src/mame/nokia/mikromik.cpp
+++ src/mame/nokia/mikromik.cpp
@@ -55,17 +55,6 @@
 
 */
 
-/*
-
-    TODO:
-
-    - M7 boot floppy
-    - accurate video timing
-    - PCB layouts
-    - NEC uPD7201 MPSC
-
-*/
-
 #include "emu.h"
 #include "mikromik.h"
 #include "softlist_dev.h"
@@ -155,6 +144,7 @@ void mm1_state::write(offs_t offset, uint8_t data)
 void mm1_state::recall_w(int state)
 {
 	LOG("RECALL %u\n", state);
+
 	m_recall = state;
 	m_fdc->reset_w(state);
 
@@ -395,21 +385,6 @@ void mm1_state::dma_hrq_w(int state)
 	m_dmac->hack_w(state);
 }
 
-uint8_t mm1_state::mpsc_dack_r()
-{
-	// clear data request
-	m_dmac->dreq2_w(CLEAR_LINE);
-
-	return 1;//m_mpsc->dtra_r();
-}
-
-void mm1_state::mpsc_dack_w(uint8_t data)
-{
-	//m_mpsc->hai_w(data);
-
-	// clear data request
-	m_dmac->dreq1_w(CLEAR_LINE);
-}
 
 void mm1_state::dma_eop_w(int state)
 {
@@ -417,59 +392,6 @@ void mm1_state::dma_eop_w(int state)
 
 	m_tc = state;
 	update_tc();
-}
-
-void mm1_state::dack3_w(int state)
-{
-	m_dack3 = state;
-	update_tc();
-}
-
-void mm1_state::itxc_w(int state)
-{
-	if (!m_intc)
-	{
-		m_mpsc->txca_w(state);
-	}
-}
-
-void mm1_state::irxc_w(int state)
-{
-	if (!m_intc)
-	{
-		m_mpsc->rxca_w(state);
-	}
-}
-
-void mm1_state::auxc_w(int state)
-{
-	m_mpsc->txcb_w(state);
-	m_mpsc->rxcb_w(state);
-}
-
-//-------------------------------------------------
-//  UPD7201
-//-------------------------------------------------
-
-void mm1_state::drq2_w(int state)
-{
-	if (state)
-	{
-		m_dmac->dreq2_w(ASSERT_LINE);
-	}
-}
-
-void mm1_state::drq1_w(int state)
-{
-	if (state)
-	{
-		m_dmac->dreq1_w(ASSERT_LINE);
-	}
-}
-
-int mm1_state::dsra_r()
-{
-	return 1;
 }
 
 
@@ -524,9 +446,10 @@ void mm1_state::machine_start()
 void mm1_state::common(machine_config &config)
 {
 	// basic system hardware
-	I8085A(config, m_maincpu, 6.144_MHz_XTAL);
+	I8085A(config, m_maincpu, XTAL(6'144'000));
 	m_maincpu->set_addrmap(AS_PROGRAM, &mm1_state::mm1_map);
-	m_maincpu->in_sid_func().set(FUNC(mm1_state::dsra_r));
+	m_maincpu->in_inta_func().set(m_mpsc, FUNC(upd7201_device::inta_r));
+	m_maincpu->in_sid_func().set(m_rs232a, FUNC(rs232_port_device::dsr_r));
 	m_maincpu->out_sod_func().set(KB_TAG, FUNC(mm1_keyboard_device::bell_w)).invert();
 
 	config.set_perfect_quantum(m_maincpu);
@@ -542,53 +465,63 @@ void mm1_state::common(machine_config &config)
 	m_iop->di_rd_callback().set(KB_TAG, FUNC(mm1_keyboard_device::read));
 
 	LS259(config, m_outlatch);
-	m_outlatch->q_out_cb<0>().set(FUNC(mm1_state::a8_w)); // IC24 A8
-	m_outlatch->q_out_cb<1>().set(FUNC(mm1_state::recall_w)); // RECALL
-	m_outlatch->q_out_cb<2>().set(FUNC(mm1_state::rx21_w)); // _RV28/RX21
-	m_outlatch->q_out_cb<3>().set(FUNC(mm1_state::tx21_w)); // _TX21
-	m_outlatch->q_out_cb<4>().set(FUNC(mm1_state::rcl_w)); // _RCL
-	m_outlatch->q_out_cb<5>().set(FUNC(mm1_state::intc_w)); // _INTC
-	m_outlatch->q_out_cb<6>().set(FUNC(mm1_state::leen_w)); // LEEN
-	m_outlatch->q_out_cb<7>().set(FUNC(mm1_state::motor_on_w)); // MOTOR ON
+	m_outlatch->q_out_cb<0>().set(FUNC(mm1_state::a8_w));
+	m_outlatch->q_out_cb<1>().set(FUNC(mm1_state::recall_w));
+	m_outlatch->q_out_cb<2>().set(FUNC(mm1_state::rx21_w));
+	m_outlatch->q_out_cb<3>().set(FUNC(mm1_state::tx21_w));
+	m_outlatch->q_out_cb<4>().set(FUNC(mm1_state::rcl_w));
+	m_outlatch->q_out_cb<5>().set(FUNC(mm1_state::intc_w));
+	m_outlatch->q_out_cb<6>().set(FUNC(mm1_state::leen_w));
+	m_outlatch->q_out_cb<7>().set(FUNC(mm1_state::motor_on_w));
 
-	AM9517A(config, m_dmac, 6.144_MHz_XTAL/2);
+	AM9517A(config, m_dmac, XTAL(6'144'000)/2);
 	m_dmac->out_hreq_callback().set(FUNC(mm1_state::dma_hrq_w));
 	m_dmac->out_eop_callback().set(FUNC(mm1_state::dma_eop_w));
 	m_dmac->in_memr_callback().set(FUNC(mm1_state::read));
 	m_dmac->out_memw_callback().set(FUNC(mm1_state::write));
-	m_dmac->in_ior_callback<2>().set(FUNC(mm1_state::mpsc_dack_r));
+	m_dmac->in_ior_callback<2>().set(m_mpsc, FUNC(upd7201_device::da_r));
 	m_dmac->in_ior_callback<3>().set(m_fdc, FUNC(upd765_family_device::dma_r));
 	m_dmac->out_iow_callback<0>().set(m_crtc, FUNC(i8275_device::dack_w));
-	m_dmac->out_iow_callback<1>().set(FUNC(mm1_state::mpsc_dack_w));
+	m_dmac->out_iow_callback<1>().set(m_mpsc, FUNC(upd7201_device::da_w));
 	m_dmac->out_iow_callback<3>().set(m_fdc, FUNC(upd765_family_device::dma_w));
+	m_dmac->out_dack_callback<1>().set(FUNC(mm1_state::dack1_w));
+	m_dmac->out_dack_callback<2>().set(FUNC(mm1_state::dack2_w));
 	m_dmac->out_dack_callback<3>().set(FUNC(mm1_state::dack3_w));
 
 	PIT8253(config, m_pit);
-	m_pit->set_clk<0>(6.144_MHz_XTAL/2/2);
+	m_pit->set_clk<0>(XTAL(6'144'000)/2/2);
 	m_pit->out_handler<0>().set(FUNC(mm1_state::itxc_w));
-	m_pit->set_clk<1>(6.144_MHz_XTAL/2/2);
+	m_pit->set_clk<1>(XTAL(6'144'000)/2/2);
 	m_pit->out_handler<1>().set(FUNC(mm1_state::irxc_w));
-	m_pit->set_clk<2>(6.144_MHz_XTAL/2/2);
+	m_pit->set_clk<2>(XTAL(6'144'000)/2/2);
 	m_pit->out_handler<2>().set(FUNC(mm1_state::auxc_w));
 
 	UPD765A(config, m_fdc, 16_MHz_XTAL/2, true, true);
 	m_fdc->intrq_wr_callback().set_inputline(m_maincpu, I8085_RST55_LINE);
 	m_fdc->drq_wr_callback().set(m_dmac, FUNC(am9517a_device::dreq3_w));
 
-	UPD7201(config, m_mpsc, 6.144_MHz_XTAL/2);
-	m_mpsc->out_txda_callback().set(m_rs232a, FUNC(rs232_port_device::write_txd));
-	m_mpsc->out_dtra_callback().set(m_rs232a, FUNC(rs232_port_device::write_dtr));
-	m_mpsc->out_rtsa_callback().set(m_rs232a, FUNC(rs232_port_device::write_rts));
+	UPD7201(config, m_mpsc, XTAL(6'144'000)/2);
+	m_mpsc->out_int_callback().set_inputline(m_maincpu, I8085_INTR_LINE);
 	m_mpsc->out_rxdrqa_callback().set(FUNC(mm1_state::drq2_w));
 	m_mpsc->out_txdrqa_callback().set(FUNC(mm1_state::drq1_w));
+	m_mpsc->out_txda_callback().set(m_rs232a, FUNC(rs232_port_device::write_txd));
+	m_mpsc->out_rtsa_callback().set(m_rs232a, FUNC(rs232_port_device::write_rts));
+	m_mpsc->out_txdb_callback().set(m_rs232b, FUNC(rs232_port_device::write_txd));
+	m_mpsc->out_rtsb_callback().set(m_rs232b, FUNC(rs232_port_device::write_rts));
 
 	RS232_PORT(config, m_rs232a, default_rs232_devices, nullptr);
-	m_rs232a->cts_handler().set(m_mpsc, FUNC(upd7201_device::rxa_w));
-	RS232_PORT(config, m_rs232b, default_rs232_devices, nullptr);
-	RS232_PORT(config, m_rs232c, default_rs232_devices, nullptr);
-	m_rs232c->cts_handler().set(m_mpsc, FUNC(upd7201_device::ctsb_w));
+	m_rs232a->rxd_handler().set(m_mpsc, FUNC(upd7201_device::rxa_w));
+	m_rs232a->dcd_handler().set(m_mpsc, FUNC(upd7201_device::dcda_w));
+	m_rs232a->cts_handler().set(m_mpsc, FUNC(upd7201_device::ctsa_w));
+	m_rs232a->rxc_handler().set(FUNC(mm1_state::erxc_w));
+	m_rs232a->txc_handler().set(FUNC(mm1_state::etxc_w));
 
-	mm1_keyboard_device &kb(MM1_KEYBOARD(config, KB_TAG, 2500)); // actual KBCLK is 6.144_MHz_XTAL/2/16
+	RS232_PORT(config, m_rs232b, default_rs232_devices, nullptr);
+	m_rs232b->rxd_handler().set(m_mpsc, FUNC(upd7201_device::rxb_w));
+	m_rs232b->dcd_handler().set(m_mpsc, FUNC(upd7201_device::dcdb_w));
+	m_rs232b->cts_handler().set(m_mpsc, FUNC(upd7201_device::ctsb_w));
+
+	mm1_keyboard_device &kb(MM1_KEYBOARD(config, KB_TAG, 2500)); // actual KBCLK is XTAL(6'144'000)/2/16
 	kb.kbst_wr_callback().set(m_iop, FUNC(i8212_device::stb_w));
 
 	// internal ram
--- src/mame/nokia/mikromik.h
+++ src/mame/nokia/mikromik.h
@@ -64,7 +64,6 @@ public:
 		m_sasi(*this, "sasi:7:scsicb"),
 		m_rs232a(*this, RS232_A_TAG),
 		m_rs232b(*this, RS232_B_TAG),
-		m_rs232c(*this, RS232_C_TAG),
 		m_ram(*this, RAM_TAG),
 		m_rom(*this, I8085A_TAG),
 		m_mmu_rom(*this, "address"),
@@ -111,7 +110,6 @@ private:
 	optional_device<nscsi_callback_device> m_sasi;
 	required_device<rs232_port_device> m_rs232a;
 	required_device<rs232_port_device> m_rs232b;
-	required_device<rs232_port_device> m_rs232c;
 	required_device<ram_device> m_ram;
 	required_memory_region m_rom;
 	required_memory_region m_mmu_rom;
@@ -165,13 +163,16 @@ private:
 	uint8_t mpsc_dack_r();
 	void mpsc_dack_w(uint8_t data);
 	void dma_eop_w(int state);
-	void dack3_w(int state);
-	void itxc_w(int state);
-	void irxc_w(int state);
-	void auxc_w(int state);
-	void drq2_w(int state);
-	void drq1_w(int state);
-	int dsra_r();
+	void dack1_w(int state) { if (!state) m_dmac->dreq1_w(0); }
+	void dack2_w(int state) { if (!state) m_dmac->dreq2_w(0); }
+	void dack3_w(int state) { m_dack3 = state; update_tc(); };
+	void erxc_w(int state) { if (m_intc) { m_mpsc->rxca_w(state); } }
+	void etxc_w(int state) { if (m_intc) { m_mpsc->txca_w(state); } }
+	void itxc_w(int state) { if (!m_intc) { m_mpsc->txca_w(state); } }
+	void irxc_w(int state) { if (!m_intc) { m_mpsc->rxca_w(state); } }
+	void auxc_w(int state) { m_mpsc->txcb_w(state); m_mpsc->rxcb_w(state); }
+	void drq1_w(int state) { if (!state) m_dmac->dreq1_w(1); }
+	void drq2_w(int state) { if (!state) m_dmac->dreq2_w(1); }
 
 	void update_tc();
 
