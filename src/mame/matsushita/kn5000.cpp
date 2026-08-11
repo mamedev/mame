aa7455705228f1355cf20230bfc02d79fa899a44
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2026-08-11 10:21:17 +0100

    kn5000: correct the IC14 rhythm data ROM dump
    The previous dump was read with address lines A19 and A21 transposed, which
    permutes the eight 512 KiB blocks of the chip. No byte differs, only their
    order. Two thirds of the factory rhythms and the Feature Demo were silent.

--- src/mame/matsushita/kn5000.cpp
+++ src/mame/matsushita/kn5000.cpp
@@ -727,7 +727,7 @@ ROM_START(kn5000)
 	ROMX_LOAD("kn5000_subprogram_v140_compressed.rom", 0x0e0000, 0x16bc4, CRC(5b182629) SHA1(13098dd150c5a6083a5d15a63d5d785802d8e8ae), ROM_BIOS(5)) // v5
 
 	ROM_REGION16_LE(0x400000, "rhythm_data", 0)
-	ROM_LOAD("kn5000_rhythm_data_rom.ic14", 0x000000, 0x400000, CRC(76d11a5e) SHA1(e4b572d318c9fe7ba00e5b44ea783e89da9c68bd))
+	ROM_LOAD("kn5000_rhythm_data_rom.ic14", 0x000000, 0x400000, CRC(aa4917ce) SHA1(fef7f1927935d8fdada2afbdbfac29aac56e1c3c))
 
 	ROM_REGION16_LE(0x1000000, "waveform", 0)
 	ROM_LOAD("kn5000_waveform_rom.ic304", 0x000000, 0x400000, NO_DUMP)
