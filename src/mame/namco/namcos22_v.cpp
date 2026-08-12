147675b420a91229e36346be4f79788e95aba1f8
FlykeSpice <flykespice@gmail.com>
2026-08-12 12:31:38 -0400

    Convert to use helper function instead of macro, as requested by hap..

--- src/mame/namco/namcos22_v.cpp
+++ src/mame/namco/namcos22_v.cpp
@@ -280,7 +280,16 @@ void namcos22_renderer::renderscanline_sprite(int32_t scanline, const extent_t &
 	}
 }
 
-
+inline void namcos22_renderer::dispatch_scanline_poly(scanline_func callback, int clipverts, vertex_t const *clipv)
+{
+	switch (clipverts)
+	{
+		case 3: render_triangle<4>(m_cliprect, render_delegate(callback, this), clipv[0], clipv[1], clipv[2]); break;
+		case 4: render_polygon<4,4>(m_cliprect, render_delegate(callback, this), clipv); break;
+		case 5: render_polygon<5,4>(m_cliprect, render_delegate(callback, this), clipv); break;
+		case 6: render_polygon<6,4>(m_cliprect, render_delegate(callback, this), clipv); break;
+	}
+}
 
 /*********************************************************************************************/
 
@@ -452,22 +461,10 @@ void namcos22_renderer::poly3d_drawquad(screen_device &screen, bitmap_rgb32 &bit
 		extra.fogfactor = 0;
 	}
 
-#define RENDER_SCANLINES(callback)\
-	switch (clipverts)\
-	{\
-		case 3: render_triangle<4>(m_cliprect, render_delegate(&callback, this), clipv[0], clipv[1], clipv[2]); break;\
-		case 4: render_polygon<4,4>(m_cliprect, render_delegate(&callback, this), clipv); break;\
-		case 5: render_polygon<5,4>(m_cliprect, render_delegate(&callback, this), clipv); break;\
-		case 6: render_polygon<6,4>(m_cliprect, render_delegate(&callback, this), clipv); break;\
-	}
-
-
 	if (m_state.m_is_ss22)
-		RENDER_SCANLINES(namcos22_renderer::renderscanline_poly_ss22)
+		dispatch_scanline_poly(&namcos22_renderer::renderscanline_poly_ss22, clipverts, clipv);
 	else
-		RENDER_SCANLINES(namcos22_renderer::renderscanline_poly)
-
-#undef RENDER_SCANLINES
+		dispatch_scanline_poly(&namcos22_renderer::renderscanline_poly, clipverts, clipv);
 }
 
 
