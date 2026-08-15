84f9bb97c118b362b97f958bc311c094edca41cd
Mark <113266992+MagikalUnicorn@users.noreply.github.com>
2026-08-15 13:39:03 +0100

    mc68340: Implement DMA transfers and external interrupt handling (#15896)
    * mc68340: Implement DMA transfers and external interrupt handling
    
    Convert the DMA module to a device and implement dual-address transfers, external request handshakes and interrupt arbitration. Support configured external autovectors and restrict MBAR accesses to CPU space.
    
    * bfm/cobra3: Hook up MC68340 DMA and correct board signals
    
    Read multiplexed inputs from the connected byte lane, pass the video controller interrupt at its asserted polarity, and connect the active-high SCSI request to the DMA controller's active-low DREQ input.

--- src/mame/bfm/bfm_cobra3.cpp
+++ src/mame/bfm/bfm_cobra3.cpp
@@ -82,7 +82,6 @@ protected:
 
 	uint32_t screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect);
 
-	void dma1_drq(int state);
 	void scc66470_irq(int state);
 
 	void bfm_cobra3_map(address_map &map) ATTR_COLD;
@@ -145,8 +144,7 @@ uint16_t bfm_cobra3_state::mem_r(offs_t offset, uint16_t mem_mask)
 
 					case 0x400:
 						{
-							//input reads, haven't got far enough to trigger any
-							return m_strobein[m_active_strobe]->read();
+							return m_strobein[m_active_strobe]->read() << 8;
 						}
 					case 0x500: //SCSI DMA
 						if (ACCESSING_BITS_8_15)
@@ -382,14 +380,9 @@ void bfm_cobra3_state::machine_start()
 }
 
 
-void bfm_cobra3_state::dma1_drq(int state)
-{
-//  m_maincpu->dma_dreq1_w(state);
-}
-
 void bfm_cobra3_state::scc66470_irq(int state)
 {
-	m_maincpu->set_input_line(5, !state);
+	m_maincpu->set_input_line(5, state);
 }
 
 uint32_t bfm_cobra3_state::screen_update(screen_device &screen, bitmap_rgb32 &bitmap, const rectangle &cliprect)
@@ -490,7 +483,7 @@ void bfm_cobra3_state::bfm_cobra3(machine_config &config)
 
 	NCR5380(config, m_scsic);
 	scsi.set_external_device(6, m_scsic);
-	m_scsic->drq_handler().set(DEVICE_SELF, FUNC(bfm_cobra3_state::dma1_drq));
+	m_scsic->drq_handler().set(m_maincpu, FUNC(m68340_cpu_device::dma_dreq1_w)).invert();
 
 	WATCHDOG_TIMER(config, m_watchdog).set_time(PERIOD_OF_555_MONOSTABLE(120000,100e-9)); //TODO: Check timings
 	METERS(config, m_meters).set_number(4);
