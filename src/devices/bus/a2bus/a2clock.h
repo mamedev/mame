c8b09cb04162ad4641ad5d7f68a92239643e6fb5
AJR <ariedlmayer@gmail.com>
2026-08-25 08:55:53 -0400

    bus/a2bus: Add new header inadvertendly omitted from f9e1f942670

---
+++ src/devices/bus/a2bus/a2clock.h
@@ -0,0 +1,12 @@
+// license:BSD-3-Clause
+// copyright-holders:AJR
+
+#ifndef MAME_BUS_A2BUS_A2CLOCK_H
+#define MAME_BUS_A2BUS_A2CLOCK_H
+
+#include "a2bus.h"
+
+// device type declarations
+DECLARE_DEVICE_TYPE(APPLE_CLOCK, device_a2bus_card_interface)
+
+#endif // MAME_BUS_A2BUS_A2CLOCK_H
