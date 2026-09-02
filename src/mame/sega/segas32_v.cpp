b276894159032501bf44bd1692c18b095184443b
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2026-09-02 00:41:42 -0400

    Update segas32_v.cpp

--- src/mame/sega/segas32_v.cpp
+++ src/mame/sega/segas32_v.cpp
@@ -23,12 +23,6 @@
     - Sonic while globally flipped via the service menu, fails to flip
       the "SEGA" and "SEGASONIC" sprite based logos on the title screen.
 
-    - titlef NBG0 and NBG2 layers are currently hidden during gameplay.
-      It sets $31ff02 with either $7be0 and $2960 (and $31ff8e is $c00).
-      Game actually uses the "rowscroll/rowselect" tables for a line window
-      effect to draw the boxing ring over NBG0.
-      Same deal for ga2 when in stage 2 cave a wall torch is lit.
-
     - Wrong priority cases (parenthesis for the level setup):
       dbzvrvs: draws text layer ($e) behind sprite-based gauges ($f).
       dbzvrvs: Sheng-Long speech balloon during Piccoro ending (fixme: check levels).
@@ -564,7 +558,7 @@ bool segas32_state::compute_clipping_extents(screen_device &screen, bool enable,
 	const bool flip = BIT(m_videoram[0x1ff00 / 2], 9);
 	rectangle tempclip;
 
-	// expand our cliprect to exclude the bottom-right
+	// expand our cliprect to include the bottom-right
 	tempclip = cliprect;
 	tempclip.max_x++;
 	tempclip.max_y++;
@@ -580,7 +574,7 @@ bool segas32_state::compute_clipping_extents(screen_device &screen, bool enable,
 		return 1;
 	}
 
-	// extract the from videoram into locals, and apply the cliprect
+	// extract the clips from videoram into locals, and apply the cliprect
 	rectangle clips[5];
 	int sorted[5];
 	for (int i = 0; i < 5; i++)
@@ -656,7 +650,136 @@ bool segas32_state::compute_clipping_extents(screen_device &screen, bool enable,
 		for (int i = 0; i < 5; i++)
 			if ((BIT(clipmask , i)) && y >= clips[i].min_y && y < clips[i].max_y)
 				sect |= 1 << i;
-		list->scan_extent[y] = sect;
+
+		/*
+		 * $1FF04 bits 4/5 enable per-line clip windows.
+		 *
+		 * clip 2:
+		 *   +000 = min X
+		 *   +200 = max X
+		 *
+		 * clip 3:
+		 *   +100 = min X
+		 *   +300 = max X
+		 */
+		if (BIT(m_videoram[0x1ff04 / 2], 4) || BIT(m_videoram[0x1ff04 / 2], 5))
+		{
+			rectangle lineclips[5];
+			int linesorted[5];
+			const rectangle &visarea = screen.visible_area();
+			int line = flip ? visarea.max_y - y : y;
+			uint16_t *table = &m_videoram[(m_videoram[0x1ff04/2] >> 10) * 0x400];
+
+			for (i = 0; i < 5; i++)
+			{
+				lineclips[i] = clips[i];
+				linesorted[i] = i;
+			}
+
+			// clip window 2
+			if (BIT(m_videoram[0x1ff04 / 2], 4) && BIT(clipmask, 2))
+			{
+				uint16_t minx = table[0x000 + line];
+				uint16_t maxx = table[0x200 + line];
+
+				if (minx == 0xffff || maxx == 0xffff)
+				{
+					sect &= ~(1 << 2);
+				}
+				else if (!flip)
+				{
+					lineclips[2].min_x = minx & 0x1ff;
+					lineclips[2].max_x = (maxx & 0x1ff) + 1;
+				}
+				else
+				{
+					lineclips[2].min_x = tempclip.max_x - ((maxx & 0x1ff) + 1);
+					lineclips[2].max_x = tempclip.max_x - (minx & 0x1ff);
+				}
+
+				lineclips[2] &= tempclip;
+
+				if (lineclips[2].min_x >= lineclips[2].max_x)
+					sect &= ~(1 << 2);
+			}
+
+			// clip window 3
+			if (BIT(m_videoram[0x1ff04 / 2], 5) && BIT(clipmask, 3))
+			{
+				uint16_t minx = table[0x100 + line];
+				uint16_t maxx = table[0x300 + line];
+
+				if (minx == 0xffff || maxx == 0xffff)
+				{
+					sect &= ~(1 << 3);
+				}
+				else if (!flip)
+				{
+					lineclips[3].min_x = minx & 0x1ff;
+					lineclips[3].max_x = (maxx & 0x1ff) + 1;
+				}
+				else
+				{
+					lineclips[3].min_x = tempclip.max_x - ((maxx & 0x1ff) + 1);
+					lineclips[3].max_x = tempclip.max_x - (minx & 0x1ff);
+				}
+
+				lineclips[3] &= tempclip;
+
+				if (lineclips[3].min_x >= lineclips[3].max_x)
+					sect &= ~(1 << 3);
+			}
+
+			/*
+			 * Build the extent for this individual scanline.
+			 * 0-31 remain the normal combinations.
+			 * 32+y is the per-line extent.
+			 */
+			{
+				uint16_t *extent = &list->extent[32 + y][0];
+
+				for (i = 0; i < 5; i++)
+					linesorted[i] = i;
+
+				for (i = 0; i < 5; i++)
+					for (j = i + 1; j < 5; j++)
+						if (lineclips[linesorted[i]].min_x > lineclips[linesorted[j]].min_x)
+						{
+							int temp = linesorted[i];
+							linesorted[i] = linesorted[j];
+							linesorted[j] = temp;
+						}
+
+				*extent++ = tempclip.min_x;
+
+				for (j = 0; j < 5; j++)
+				{
+					if (BIT(sect, linesorted[j]))
+					{
+						const rectangle *cur = &lineclips[linesorted[j]];
+
+						if (extent != &list->extent[32 + y][1] && cur->min_x <= extent[-1])
+						{
+							if (cur->max_x > extent[-1])
+								extent[-1] = cur->max_x;
+						}
+						else
+						{
+							*extent++ = cur->min_x;
+							*extent++ = cur->max_x;
+						}
+					}
+				}
+
+				*extent++ = tempclip.max_x;
+			}
+
+			list->scan_extent[y] = 32 + y;
+		}
+		else
+		{
+			list->scan_extent[y] = sect;
+		}
 	}
 
 	return clipout;
@@ -1127,9 +1250,12 @@ void segas32_state::update_background(segas32_state::layer_info &layer, const re
 {
 	bitmap_ind16 &bitmap = layer.bitmap;
 
+	// determine if we're flipped
+	bool flip = BIT(m_videoram[0x1ff00 / 2], 9);
+
 	for (int y = cliprect.min_y; y <= cliprect.max_y; y++)
 	{
-		uint16_t *const dst = &bitmap.pix(y);
+		uint16_t *const dst = &bitmap.pix(flip ? cliprect->max_y - y : y);
 		int color;
 
 		/* determine the color */
