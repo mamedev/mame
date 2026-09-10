44a74bfa9e4712a56ba52234633ec7b38cdf9d30
arekkusu <arekkusu42@users.noreply.github.com>
2026-09-10 08:56:56 -0700

    m6502/w65816.cpp: fix regressed status flag bits (like #14279)
    Also adjust register order and restore IRQ/NMI state, to match g65816.

--- src/devices/cpu/m6502/w65816.cpp
+++ src/devices/cpu/m6502/w65816.cpp
@@ -63,9 +63,11 @@ void w65816_device::init()
 	state_add(W65816_P,        "P",         m_P).callimport();
 	state_add(W65816_S,        "SP",        m_SP).callimport();
 	state_add(W65816_D,        "D",         m_D);
-	state_add(W65816_DB,       "DB",        m_DB);
 	state_add(W65816_PB,       "PB",        m_PB);
-	state_add(W65816_E,        "E",         m_E).callimport();
+	state_add(W65816_DB,       "DB",        m_DB);
+	state_add(W65816_E,        "E",         m_E).mask(1).callimport().formatstr("%1X");
+	state_add(W65816_IRQ,      "IRQ",       m_irq_state).mask(1).callimport().formatstr("%1X");
+	state_add(W65816_NMI,      "NMI",       m_nmi_state).mask(1).callimport().formatstr("%1X");
 	state_add(W65816_IR,       "IR",        m_IR);
 
 	save_item(NAME(m_PPC));
@@ -298,8 +300,8 @@ void w65816_device::state_string_export(const device_state_entry &entry, std::st
 		str = string_format("%c%c%c%c%c%c%c%c",
 						m_P & F_N ? 'N' : '.',
 						m_P & F_V ? 'V' : '.',
-						m_E ? 'E' : (m_P & F_M ? 'M' : '.'),
-						m_E ? '-' : (m_P & F_X ? 'X' : '.'),
+						m_P & F_M ? (m_E ? ' ' : 'M') : '.',
+						m_P & F_X ? (m_E ? 'B' : 'X') : '.',
 						m_P & F_D ? 'D' : '.',
 						m_P & F_I ? 'I' : '.',
 						m_P & F_Z ? 'Z' : '.',
