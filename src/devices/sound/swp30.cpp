23dead97ae7c94ad81b6172cf4ef12514321cbe3
tarboh <50641757+tarboh@users.noreply.github.com>
2026-09-13 23:33:46 +0900

    swp30: iir2 register order, and a leak in the dpcm accumulator (#16115)
    * sound/swp30: iir2's a1 and b1 registers were swapped and the dpcm accumulator leaked in compressor modes 0-2
    AI assisted by Claude Opus 5

--- src/devices/sound/swp30.cpp
+++ src/devices/sound/swp30.cpp
@@ -155,11 +155,11 @@ TODOs:
   aaaaaa 100011  MEG/Data         cccc cccc cccc cccc                      constant index 6*a + 1
   cccccc 100100  AWM2/IIR         vvvv vvvv vvvv vvvv                      IIR1 a0
   aaaaaa 100101  MEG/Data         cccc cccc cccc cccc                      constant index 6*a + 2
-  cccccc 100110  AWM2/IIR         vvvv vvvv vvvv vvvv                      IIR1 a1
+  cccccc 100110  AWM2/IIR         vvvv vvvv vvvv vvvv                      IIR2 b1
   aaaaaa 100111  MEG/Data         cccc cccc cccc cccc                      constant index 6*a + 3
-  cccccc 101000  AWM2/IIR         vvvv vvvv vvvv vvvv                      IIR1 b1
+  cccccc 101000  AWM2/IIR         vvvv vvvv vvvv vvvv                      IIR2 a1
   aaaaaa 101001  MEG/Data         cccc cccc cccc cccc                      constant index 6*a + 4
-  cccccc 101010  AWM2/IIR         vvvv vvvv vvvv vvvv                      IIR1 a0
+  cccccc 101010  AWM2/IIR         vvvv vvvv vvvv vvvv                      IIR2 a0
   aaaaaa 101011  MEG/Data         cccc cccc cccc cccc                      constant index 6*a + 5
 
 
@@ -622,7 +622,10 @@ void swp30_device::streaming_block::dpcm_step(u8 input)
 	m_dpcm_s2 = m_dpcm_s3;
 
 	s32 delta = m_dpcm_delta + dpcm_expand[input];
-	s32 sample = m_dpcm_s3 + (delta << scale);
+	s32 acc = m_dpcm_s3;
+	if(mode != 3)
+		acc -= s32((s64(acc) * 3) >> 7);
+	s32 sample = acc + (delta << scale);
 
 	if(sample < -0x8000) {
 		sample = -0x8000;
@@ -1970,8 +1973,8 @@ void swp30_device::map(address_map &map)
 	rchan(map, 0x20).rw(FUNC(swp30_device::a1_r<0>), FUNC(swp30_device::a1_w<0>));
 	rchan(map, 0x22).rw(FUNC(swp30_device::b1_r<0>), FUNC(swp30_device::b1_w<0>));
 	rchan(map, 0x24).rw(FUNC(swp30_device::a0_r<0>), FUNC(swp30_device::a0_w<0>));
-	rchan(map, 0x26).rw(FUNC(swp30_device::a1_r<1>), FUNC(swp30_device::a1_w<1>));
-	rchan(map, 0x28).rw(FUNC(swp30_device::b1_r<1>), FUNC(swp30_device::b1_w<1>));
+	rchan(map, 0x26).rw(FUNC(swp30_device::b1_r<1>), FUNC(swp30_device::b1_w<1>));
+	rchan(map, 0x28).rw(FUNC(swp30_device::a1_r<1>), FUNC(swp30_device::a1_w<1>));
 	rchan(map, 0x2a).rw(FUNC(swp30_device::a0_r<1>), FUNC(swp30_device::a0_w<1>));
 	// 2c-2f missing
 
