c23e3db7b845d687ee7c6397fd498564fc1d6f64
Vincent-Halver <Vincent.Halver@gmail.com>
2026-09-12 21:10:07 -0700

    cdi220_lcd: Make it a device
    The cdislave hle currently has partial ownership of the LCD panel's state. This makes it harder to replace the slave_HLE with the DSP chip.
    
    This change provides a cleaner separation by using a callback, similar to other devices the slave interacts with.

--- src/mame/philips/cdislavehle.h
+++ src/mame/philips/cdislavehle.h
@@ -40,9 +40,8 @@ public:
 	auto read_mousey() { return m_read_mousey.bind(); }
 	auto read_mousebtn() { return m_read_mousebtn.bind(); }
 	auto atten_callback() { return m_atten_w.bind(); }
+	auto lcd_callback() { return m_lcd_w.bind(); }
 	auto testplug_callback() { return m_testplug_cb.bind(); }
-
-	uint8_t* get_lcd_state() { return m_lcd_state; }
 
 	uint16_t slave_r(offs_t offset);
 	void slave_w(offs_t offset, uint16_t data);
@@ -69,6 +68,7 @@ private:
 
 	required_device_array<dmadac_sound_device, 2> m_dmadac;
 	devcb_write32 m_atten_w;
+	devcb_write8 m_lcd_w;
 	devcb_read_line m_testplug_cb;
 	required_device<cdrom_image_device> m_cdrom;
 
@@ -91,8 +91,6 @@ private:
 	uint8_t m_debug_mode;
 
 	uint8_t m_xbus_interrupt_enable;
-
-	uint8_t m_lcd_state[16];
 
 	uint16_t m_input_mouse_x;
 	uint16_t m_input_mouse_y;
