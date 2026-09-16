a3da6bb2a0a0ea82a7bb7f0820a51dbdb325c3ed
David Haywood <hazemamewip@hotmail.com>
2026-09-16 20:29:51 +0100

    refactor, add cart hookup

--- src/mame/skeleton/licocai.cpp
+++ src/mame/skeleton/licocai.cpp
@@ -55,16 +55,18 @@ private:
 
 	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 
+	void vdp_data_upload(uint16_t data, uint16_t mem_mask);
+
 	void vdp_dest_select_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
 	void vdp_data_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
 	u16 lico_2a0000_r(offs_t offset, uint16_t mem_mask = ~0);
 	u16 lico_2a000a_r(offs_t offset, uint16_t mem_mask = ~0);
+	void lico_200008_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
+	void lico_20000a_w(offs_t offset, uint16_t data, uint16_t mem_mask = ~0);
 
 	void licocai_map(address_map &map) ATTR_COLD;
 
 	TIMER_DEVICE_CALLBACK_MEMBER(scanline);
-
-	DECLARE_DEVICE_IMAGE_LOAD_MEMBER(cart_load) ATTR_COLD;
 
 	u16 m_vdp_dest;
 	u16 m_vdp_write_type;
@@ -72,72 +74,89 @@ private:
 	std::unique_ptr<u8[]> m_vram;
 };
 
-DEVICE_IMAGE_LOAD_MEMBER(licocai_state::cart_load)
-{
-	// carts seem to be full 68000 programs so probably replace the System ROM when loaded
-
-	uint32_t const size = m_cart->common_get_size("rom");
-
-	m_cart->rom_alloc(size, GENERIC_ROM16_WIDTH, ENDIANNESS_LITTLE);
-	m_cart->common_load_rom(m_cart->get_rom_base(), size, "rom");
-
-	return std::make_pair(std::error_condition(), std::string());
-}
-
 void licocai_state::vdp_dest_select_w(offs_t offset, uint16_t data, uint16_t mem_mask)
 {
 	m_vdp_dest = data & mem_mask;
 	logerror("%s: set m_vdp_dest to %04x\n", machine().describe_context(), m_vdp_dest);
 }
 
-void licocai_state::vdp_data_w(offs_t offset, uint16_t data, uint16_t mem_mask)
+void licocai_state::vdp_data_upload(uint16_t data, uint16_t mem_mask)
 {
-	if (m_vdp_dest == 0x0002) // uploads some 16x16x4 tiles to RAM
+	if (m_vdp_write_type == 0x0000)
 	{
-		if (m_vdp_write_type == 0x0000)
-		{
-			logerror("%s: write to vdp_data_w with m_vdp_dest %02x addr %04x: %04x %04x (gfxdata?)\n", machine().describe_context(), m_vdp_dest, m_vdp_addr, data, mem_mask);
+		logerror("%s: write to vdp_data_w with m_vdp_dest %02x addr %04x: %04x %04x (data_upload?)\n", machine().describe_context(), m_vdp_dest, m_vdp_addr, data, mem_mask);
 
-			m_vram[(m_vdp_addr + 0) & 0xffff] = (data >> 8) & 0x00ff;
-			m_vram[(m_vdp_addr + 1) & 0xffff] = (data >> 0) & 0x00ff;
+		m_vram[(m_vdp_addr + 0) & 0xffff] = (data >> 8) & 0x00ff;
+		m_vram[(m_vdp_addr + 1) & 0xffff] = (data >> 0) & 0x00ff;
 
-			m_gfxdecode->gfx(0)->mark_dirty(m_vdp_addr / 0x80);
-			m_vdp_addr += 2;
-		}
-		else
-		{
-			logerror("%s: write to vdp_data_w with m_vdp_dest %02x addr %04x: %04x %04x (write type %04x)\n", machine().describe_context(), m_vdp_dest, m_vdp_addr, data, mem_mask, m_vdp_write_type);
-		}
-
-	}
-	else if (m_vdp_dest == 0x0000)
-	{
-		logerror("%s: write to vdp_data_w with m_vdp_dest %02x: %04x %04x (vram position?)\n", machine().describe_context(), m_vdp_dest, data, mem_mask);
-		m_vdp_addr = data;
-	}
-	else if (m_vdp_dest == 0x0005)
-	{
-		logerror("%s: write to vdp_data_w with m_vdp_dest %02x: %04x %04x (write type?)\n", machine().describe_context(), m_vdp_dest, data, mem_mask);
-		m_vdp_write_type = data;
+		m_gfxdecode->gfx(0)->mark_dirty(m_vdp_addr / 0x80);
+		m_vdp_addr += 2;
 	}
 	else
 	{
+		logerror("%s: write to vdp_data_w with m_vdp_dest %02x addr %04x: %04x %04x (write type %04x)\n", machine().describe_context(), m_vdp_dest, m_vdp_addr, data, mem_mask, m_vdp_write_type);
+	}
+}
+
+void licocai_state::vdp_data_w(offs_t offset, uint16_t data, uint16_t mem_mask)
+{
+	switch (m_vdp_dest)
+	{
+	case 0x0000:
+		logerror("%s: write to vdp_data_w with m_vdp_dest %02x: %04x %04x (vram position?)\n", machine().describe_context(), m_vdp_dest, data, mem_mask);
+		m_vdp_addr = data;
+		break;
+
+	case 0x0002:
+		vdp_data_upload(data, mem_mask);
+		break;
+
+	case 0x0005: // init to 0000
+		logerror("%s: write to vdp_data_w with m_vdp_dest %02x: %04x %04x (write type?)\n", machine().describe_context(), m_vdp_dest, data, mem_mask);
+		m_vdp_write_type = data;
+		break;
+
+	case 0x0006: // inited to 0000
+	case 0x0007: // inited to 0000
+	case 0x0008: // inited to 0000
+	case 0x0009: // inited to 0000, again after 0x13, also as 0000
+	case 0x000a: // inited to 0202
+	case 0x000b: // inited to 031f
+	case 0x000c: // inited to 0f02
+	case 0x000d: // inited to 00ef
+	case 0x000e: // inited to 0003
+	case 0x000f: // inited to 0010
 		logerror("%s: write to vdp_data_w with m_vdp_dest %02x: %04x %04x (unknown)\n", machine().describe_context(), m_vdp_dest, data, mem_mask);
+		break;
+
+	case 0x0013: // more than once, always 7800
+		logerror("%s: write to vdp_data_w with m_vdp_dest %02x: %04x %04x (unknown - writes 7800)\n", machine().describe_context(), m_vdp_dest, data, mem_mask);
+		break;
+
+
+	default:
+		fatalerror("%s: write to vdp_data_w with m_vdp_dest %02x: %04x %04x (unknown)\n", machine().describe_context(), m_vdp_dest, data, mem_mask);
+		break;
 	}
 }
 
 
 u16 licocai_state::lico_2a0000_r(offs_t offset, uint16_t mem_mask)
 {
+	// read at end of IRQs, ack?
+	logerror("%s: lico_2a0000_r\n", machine().describe_context());
 	return machine().rand();
 }
 
 u16 licocai_state::lico_2a000a_r(offs_t offset, uint16_t mem_mask)
 {
+	// read at end of IRQs, ack?
+	logerror("%s: lico_2a000a_r\n", machine().describe_context());
 	return machine().rand();
 }
 
-static const gfx_layout tile16_layout =
+// there seem to be other formats uploaded too?
+static const gfx_layout tile16_ram_4bpp_layout =
 {
 	16,16,
 	0x200,
@@ -148,17 +167,47 @@ static const gfx_layout tile16_layout =
 	16*16*4
 };
 
+static const gfx_layout tile16_ram_1bpp_layout =
+{
+	16,16,
+	0x800,
+	1,
+	{ 0 },
+	{ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 },
+	{ 0*16,1*16,2*16,3*16, 4*16,5*16,6*16,7*16,8*16,9*16,10*16,11*16,12*16,13*16,14*16,15*16 },
+	16*16
+};
+
+static const gfx_layout tile16_1bpp_layout =
+{
+	16,16,
+	RGN_FRAC(1,1),
+	1,
+	{ 0 },
+	{ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 },
+	{ 0*16,1*16,2*16,3*16, 4*16,5*16,6*16,7*16,8*16,9*16,10*16,11*16,12*16,13*16,14*16,15*16 },
+	16*16
+};
+
+
+static GFXDECODE_START( gfx_licocai )
+	GFXDECODE_ENTRY( "maincpu", 0, tile16_1bpp_layout,   0, 1  ) // entry 0 - debug (has a 16x16x1bpp font from tiles 5580+)
+	// entry 1 created in init
+	// entry 2 created in init
+GFXDECODE_END
+
 void licocai_state::machine_start()
 {
 	save_item(NAME(m_vdp_dest));
 	save_item(NAME(m_vdp_addr));
 	save_item(NAME(m_vdp_write_type));
 
-	// clears 0x10000 bytes on startup
+	// clears 0x10000 bytes on startup, so assume main VRAM is that size
 	m_vram = make_unique_clear<u8[]>(0x10000);
 	save_pointer(NAME(m_vram), 0x10000);
 
-	m_gfxdecode->set_gfx(0, std::make_unique<gfx_element>(m_palette, tile16_layout, &m_vram[0x0], 0, m_palette->entries() / 16, 0));
+	m_gfxdecode->set_gfx(1, std::make_unique<gfx_element>(m_palette, tile16_ram_4bpp_layout, &m_vram[0x0], 0, m_palette->entries() / 16, 0));
+	m_gfxdecode->set_gfx(2, std::make_unique<gfx_element>(m_palette, tile16_ram_1bpp_layout, &m_vram[0x0], 0, m_palette->entries() / 2, 0));
 }
 
 void licocai_state::machine_reset()
@@ -177,10 +226,41 @@ uint32_t licocai_state::screen_update(screen_device &screen, bitmap_ind16 &bitma
 	return 0;
 }
 
+void licocai_state::lico_200008_w(offs_t offset, uint16_t data, uint16_t mem_mask)
+{
+	if (mem_mask & 0xff00)
+	{
+		fatalerror("write to upper byte lico_200008_w %04x %04x\n", data, mem_mask);
+	}
+	else
+	{
+		logerror("%s: lico_200008_w %02x\n", machine().describe_context(), data & 0xff);
+	}
+}
+
+void licocai_state::lico_20000a_w(offs_t offset, uint16_t data, uint16_t mem_mask)
+{
+	if (mem_mask & 0xff00)
+	{
+		fatalerror("write to upper byte lico_20000a_w %04x %04x\n", data, mem_mask);
+	}
+	else
+	{
+		logerror("%s: lico_20000a_w %02x\n", machine().describe_context(), data & 0xff);
+	}
+}
+
 void licocai_state::licocai_map(address_map &map)
 {
-	map(0x000000, 0x0fffff).rom();
-	// reads from 19E564 etc. why? (is the ROM the proper size?)
+	map(0x000000, 0x0fffff).mirror(0x100000).rom();
+	// there are data reads from 19E564 etc. why? (is the ROM the proper size?) - handled with mirror for now
+	// could be there's a gap in how the ROM maps?
+
+	// 0x200004, 0x200005 // similar to lico_200008_w  reg num?  (could be sound?)
+	// 0x200006, 0x200007 // similar to lico_20000a_w  value?
+
+	map(0x200008, 0x200009).w(FUNC(licocai_state::lico_200008_w)); // used as a pair
+	map(0x20000a, 0x20000b).w(FUNC(licocai_state::lico_20000a_w));
 
 	map(0x210000, 0x210001).w(FUNC(licocai_state::vdp_dest_select_w));
 	map(0x210002, 0x210003).w(FUNC(licocai_state::vdp_data_w));
@@ -188,9 +268,12 @@ void licocai_state::licocai_map(address_map &map)
 	map(0x2a0000, 0x2a0001).r(FUNC(licocai_state::lico_2a0000_r));
 	map(0x2a000a, 0x2a000b).r(FUNC(licocai_state::lico_2a000a_r));
 
-// tests from 0x7e0000 if not mapped it writes 'system ram error' to the start of vram (but hasn't uploaded a font?)
-	map(0x7e0000, 0x7effff).ram();
-	map(0x7f0000, 0x7fffff).ram();
+	// tests from 0x7e0000 if not mapped it writes 'system ram error' to the start of vram (but hasn't uploaded a font?)
+	map(0x7e0000, 0x7fffff).ram();
+
+	// cart ROMs map here if present
+	// [:maincpu] ':maincpu' (010D48): unmapped program memory read from 800100 & FF00
+	map(0x800000, 0x8fffff).r(m_cart, FUNC(generic_slot_device::read16_rom));
 }
 
 static INPUT_PORTS_START( licocai )
@@ -227,13 +310,12 @@ void licocai_state::licocai(machine_config &config)
 
 	TIMER(config, "scantimer").configure_scanline(FUNC(licocai_state::scanline), "screen", 0, 1);
 
-	GFXDECODE(config, m_gfxdecode, m_palette, gfxdecode_device::empty);
+	GFXDECODE(config, m_gfxdecode, m_palette, gfx_licocai);
 
 	SPEAKER(config, "speaker", 2).front();
 
-	GENERIC_CARTSLOT(config, m_cart, generic_plain_slot, "licocai_cart");
+	GENERIC_CARTSLOT(config, m_cart, generic_linear_slot, "licocai_cart");
 	m_cart->set_width(GENERIC_ROM16_WIDTH);
-	m_cart->set_device_load(FUNC(licocai_state::cart_load));
 	SOFTWARE_LIST(config, "cart_list").set_original("licocai_cart");
 }
 
