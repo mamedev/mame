9cae1b0cff3c788cb80e0c1129334bc69b53339b
SamDelorean <184150317+SamDelorean@users.noreply.github.com>
2026-09-17 21:46:47 -0600

    asma2k: emulate PC Send text output

--- src/devices/cpu/mc68hc11/mc68hc11.cpp
+++ src/devices/cpu/mc68hc11/mc68hc11.cpp
@@ -70,6 +70,7 @@ mc68hc11_cpu_device::mc68hc11_cpu_device(const machine_config &mconfig, device_t
 	, m_analog_cb(*this, 0)
 	, m_spi2_data_input_cb(*this, 0xff)
 	, m_spi2_data_output_cb(*this)
+	, m_instruction_cb(*this)
 	, m_ram_view(*this, "ram")
 	, m_reg_view(*this, "regs")
 	, m_eeprom_view(*this, "eeprom")
@@ -1239,6 +1240,7 @@ void mc68hc11_cpu_device::execute_run()
 		else
 		{
 			m_ppc = m_pc;
+			m_instruction_cb(m_pc);
 			debugger_instruction_hook(m_pc);
 
 			op = FETCH();
