183246225fe88fd96e49d87428d1ffaa67f58490
Curt Coder <curtcoder@mail.com>
2026-09-18 19:21:55 +0300

    Update comments.

--- src/mame/wang/wangpc.cpp
+++ src/mame/wang/wangpc.cpp
@@ -10,14 +10,6 @@
 
 */
 
-/*
-
-    TODO:
-
-    - with quantum perfect cpu gets stuck @ 49c3 mov ss,cs:[52ah]
-
-*/
-
 #include "emu.h"
 #include "softlist_dev.h"
 #include "bus/centronics/ctronics.h"
