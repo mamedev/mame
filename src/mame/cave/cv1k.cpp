67dd33d57d8e28b06ec08606c22f988ac26ec210
buffis <bjorn.kempen@gmail.com>
2026-09-20 10:03:27 +0200

    Add MACHINE_IMPERFECT_TIMING back

--- src/mame/cave/cv1k.cpp
+++ src/mame/cave/cv1k.cpp
@@ -1181,4 +1181,4 @@ GAME( 2010, dfkbl,      0,        cv1k_d, cv1k, cv1k_state, init_ddpdfk,   ROT27
 //GAME( 2012, ddpsdoj,    0,        cv1k_d, cv1k, cv1k_state, init_ddpdfk,   ROT270, "Cave",                 "DoDonPachi SaiDaiOuJou (Japan, 2012/ 4/20)",                                           MACHINE_IMPERFECT_TIMING )
 
 // CMDL01 Medal Mahjong Moukari Bancho
-GAME( 2007, mmmbanc,    0,        cv1k_touch, mmmbanc, cv1k_state, init_pinkswts, ROT0,   "Cave (AMI license)",   "Medal Mahjong Moukari Bancho (Japan, 2007/06/05 MASTER VER.)",                     0 )
+GAME( 2007, mmmbanc,    0,        cv1k_touch, mmmbanc, cv1k_state, init_pinkswts, ROT0,   "Cave (AMI license)",   "Medal Mahjong Moukari Bancho (Japan, 2007/06/05 MASTER VER.)",                  MACHINE_IMPERFECT_TIMING )
