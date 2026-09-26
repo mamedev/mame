529a76456504f7559c3a8beb900ff462ba7598e1
alansley <264338927+alansley@users.noreply.github.com>
2026-09-26 15:07:38 +1000

    plugins/data: Don't assign to generic for loop control variables. (#16263)
    Lua 5.5 treats loop control variables as constants, so load_dat.lua and
    data_story.lua fail to compile.  Copy the modified values to new locals
    instead.

--- plugins/data/data_story.lua
+++ plugins/data/data_story.lua
@@ -14,9 +14,9 @@ function dat.check(set, softlist)
 	end
 	local lines = {}
 	data = data:gsub('MAMESCORE records : ([^\n]+)', 'MAMESCORE records :\t\n%1', 1)
-	for line in data:gmatch('[^\n]*') do
-		if (line ~= '') or ((#lines ~= 0) and (lines[#lines] ~= '')) then
-			line = line:gsub('^(.-)_+([0-9.]+)$', '%1\t%2')
+	for rawline in data:gmatch('[^\n]*') do
+		if (rawline ~= '') or ((#lines ~= 0) and (lines[#lines] ~= '')) then
+			local line = rawline:gsub('^(.-)_+([0-9.]+)$', '%1\t%2')
 			table.insert(lines, line)
 		end
 	end
